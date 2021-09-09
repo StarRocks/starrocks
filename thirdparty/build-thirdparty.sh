06f8b1b11ca238c8e13b25d493195156217f30ff
Pslydhh <luouyuyou@gmail.com>
2021-09-09 13:09:11 +0800

    [SR-3554] Provide hyperscan to instead re2 in like/regexp (#64)

--- thirdparty/build-thirdparty.sh
+++ thirdparty/build-thirdparty.sh
@@ -748,6 +748,29 @@ build_jdk() {
     cp -r $TP_SOURCE_DIR/$JDK_SOURCE $TP_INSTALL_DIR/
 }
 
+# ragel
+# ragel-6.9+ is used by hypercan, so we build it first. 
+build_ragel() {
+    check_if_source_exist $RAGEL_SOURCE
+    cd $TP_SOURCE_DIR/$RAGEL_SOURCE
+    ./configure --prefix=$TP_INSTALL_DIR --disable-shared --enable-static
+    make -j$PARALLEL && make install    
+}
+
+#hyperscan
+build_hyperscan() {
+    check_if_source_exist $HYPERSCAN_SOURCE
+    cd $TP_SOURCE_DIR/$HYPERSCAN_SOURCE
+    export PATH=$TP_INSTALL_DIR/bin:$PATH
+    $CUSTOM_CMAKE -DCMAKE_BUILD_TYPE=Release -DBOOST_ROOT=$DORIS_THIRDPARTY/installed/include -DCMAKE_CXX_COMPILER=$DORIS_GCC_HOME/bin/g++ -DCMAKE_C_COMPILER=$DORIS_GCC_HOME/bin/gcc
+    make -j$PARALLEL && make install
+    cp $TP_SOURCE_DIR/$HYPERSCAN_SOURCE/include/hs_common.h $TP_INSTALL_DIR/include
+    cp $TP_SOURCE_DIR/$HYPERSCAN_SOURCE/include/hs_compile.h $TP_INSTALL_DIR/include
+    cp $TP_SOURCE_DIR/$HYPERSCAN_SOURCE/include/hs.h $TP_INSTALL_DIR/include
+    cp $TP_SOURCE_DIR/$HYPERSCAN_SOURCE/include/hs_runtime.h $TP_INSTALL_DIR/include
+    cp $TP_SOURCE_DIR/$HYPERSCAN_SOURCE/lib/libhs.a $TP_INSTALL_DIR/lib
+}
+
 build_libevent
 build_zlib
 build_lz4
@@ -782,5 +805,7 @@ build_ryu
 build_breakpad
 build_hadoop
 build_jdk
-
+build_ragel
+build_hyperscan
 echo "Finihsed to build all thirdparties"
+
