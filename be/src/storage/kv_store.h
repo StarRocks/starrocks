72ac78a376e308a086ccc2ecdddb39497539829d
sunyuejun <1171871069@qq.com>
2021-09-10 00:18:00 -0700

    replay url kv_s tore to olap_meta

--- be/src/storage/kv_store.h
+++ be/src/storage/kv_store.h
@@ -1,6 +1,6 @@
 // This file is made available under Elastic License 2.0.
 // This file is based on code available under the Apache license here:
-//   https://github.com/apache/incubator-doris/blob/master/be/src/olap/kv_store.h
+//   https://github.com/apache/incubator-doris/blob/master/be/src/olap/olap_meta.h
 
 // Licensed to the Apache Software Foundation (ASF) under one
 // or more contributor license agreements.  See the NOTICE file
