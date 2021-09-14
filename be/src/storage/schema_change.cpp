31336bd12aa7acfff082f6ccf48f353010fed8be
sevev <953361637@qq.com>
2021-09-14 21:34:04 +0800

    add comments && modify function name for understand easily

--- be/src/storage/schema_change.cpp
+++ be/src/storage/schema_change.cpp
@@ -854,7 +854,7 @@ RowBlockAllocator::~RowBlockAllocator() {
     }
 }
 
-bool RowBlockAllocator::can_sort(size_t num_rows, size_t allocated_rows) {
+bool RowBlockAllocator::is_memory_enough_to_sort(size_t num_rows, size_t allocated_rows) {
     size_t row_block_size = _row_len * (num_rows - allocated_rows);
     return _memory_allocated + row_block_size < _memory_limitation;
 }
@@ -1285,8 +1285,10 @@ bool SchemaChangeWithSorting::process(RowsetReaderSharedPtr rowset_reader, Rowse
             goto SORTING_PROCESS_ERR;
         } else {
             // check allocator has enougth memory to do row-block sort
-            if (!_row_block_allocator->can_sort(ref_row_block->row_block_info().row_num,
-                                                row_block_sorter.allocated_rows())) {
+            // in case of insufficient memory, row-block sort will fail which cause scheam change fail
+            // if memory is not enough to do block sort, do internal-sorting frist to free memory
+            if (!_row_block_allocator->is_memory_enough_to_sort(ref_row_block->row_block_info().row_num,
+                                                                row_block_sorter.allocated_rows())) {
                 if (new_row_block != nullptr) {
                     _row_block_allocator->release(new_row_block);
                     new_row_block = nullptr;
