1240e2e25472acc71f9ad0b2affe375f5f208d4c
sduzh <zhuming9011@gmail.com>
2021-09-17 00:04:38 -0500

    Unlock _store_lock before calling TabletManager::drop_tablets_on_error_root_path (#276)
    No need to call TabletManager::drop_tablets_on_error_root_path() with _store_lock acquired.

--- be/src/storage/storage_engine.cpp
+++ be/src/storage/storage_engine.cpp
@@ -424,18 +424,20 @@ bool StorageEngine::_delete_tablets_on_unused_root_path() {
     uint32_t unused_root_path_num = 0;
     uint32_t total_root_path_num = 0;
 
-    std::lock_guard<std::mutex> l(_store_lock);
-    if (_store_map.size() == 0) {
-        return false;
-    }
-
-    for (auto& it : _store_map) {
-        ++total_root_path_num;
-        if (it.second->is_used()) {
-            continue;
+    {
+        std::lock_guard<std::mutex> l(_store_lock);
+        if (_store_map.size() == 0) {
+            return false;
         }
-        it.second->clear_tablets(&tablet_info_vec);
-        ++unused_root_path_num;
+
+        for (auto& it : _store_map) {
+            ++total_root_path_num;
+            if (it.second->is_used()) {
+                continue;
+            }
+            it.second->clear_tablets(&tablet_info_vec);
+            ++unused_root_path_num;
+        }
     }
 
     if (too_many_disks_are_failed(unused_root_path_num, total_root_path_num)) {
