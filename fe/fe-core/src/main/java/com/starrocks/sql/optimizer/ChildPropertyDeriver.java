b019d6a8698cc5f2c877fc5ba6bff9ac3bc69d54
Seaven <seaven_7@qq.com>
2021-09-22 15:04:54 +0800

    Fix rollup aggregate result has duplicate NULL (#322)

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/ChildPropertyDeriver.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/ChildPropertyDeriver.java
@@ -273,7 +273,7 @@ public class ChildPropertyDeriver extends OperatorVisitor<Void, ExpressionContex
      *
      * */
     private boolean tryColocate(HashDistributionSpec leftShuffleDistribution,
-                             HashDistributionSpec rightShuffleDistribution) {
+                                HashDistributionSpec rightShuffleDistribution) {
         if (Config.disable_colocate_join || ConnectContext.get().getSessionVariable().isDisableColocateJoin()) {
             return false;
         }
@@ -978,7 +978,6 @@ public class ChildPropertyDeriver extends OperatorVisitor<Void, ExpressionContex
     public Void visitPhysicalRepeat(PhysicalRepeatOperator node, ExpressionContext context) {
         // Pass through the requirements to the child
         if (getRequiredLocalDesc().isPresent()) {
-            outputInputProps.add(new Pair<>(distributeRequirements(), Lists.newArrayList(distributeRequirements())));
             return visitOperator(node, context);
         }
         outputInputProps.add(new Pair<>(PhysicalPropertySet.EMPTY, Lists.newArrayList(PhysicalPropertySet.EMPTY)));
