52b017aa5201fb2e9f9f3e3fe46e75af0b180526
Chun Zhao <buaa.zhaoc@gmail.com>
2021-09-24 11:26:58 +0000

    Fix modernize-replace-random-shuffle by clang-tidy
    The reason for removing random_shuffle in C++17 is, that the iterator
    only version is usually depending on std::rand, which is now also
    discussed for deprecation, and should be replaced with the classes of
    the <random> header, as std::rand is considered harmful. Also the
    iterator-only random_shuffle version usually depends on a global state.
    The shuffle algorithm is the replacement, and has URBG as its 3rd
    parameter.

--- be/src/exec/pipeline/exchange/exchange_sink_operator.cpp
+++ be/src/exec/pipeline/exchange/exchange_sink_operator.cpp
@@ -10,6 +10,7 @@
 #include <functional>
 #include <iostream>
 #include <memory>
+#include <random>
 
 #include "exec/pipeline/exchange/sink_buffer.h"
 #include "exprs/expr.h"
@@ -251,7 +252,7 @@ Status ExchangeSinkOperator::prepare(RuntimeState* state) {
     if (_part_type == TPartitionType::UNPARTITIONED || _part_type == TPartitionType::RANDOM) {
         // Randomize the order we open/transmit to channels to avoid thundering herd problems.
         srand(reinterpret_cast<uint64_t>(this));
-        std::random_shuffle(_channels.begin(), _channels.end());
+        std::shuffle(_channels.begin(), _channels.end(), std::mt19937(std::random_device()()));
     } else if (_part_type == TPartitionType::HASH_PARTITIONED ||
                _part_type == TPartitionType::BUCKET_SHFFULE_HASH_PARTITIONED) {
         _partitions_columns.resize(_partition_expr_ctxs.size());
