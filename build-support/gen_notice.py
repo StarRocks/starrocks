f868d81dd8e522c31f0f184f8dcdb85fe290204b
Binglin Chang <decstery@gmail.com>
2021-09-26 21:54:04 +0800

    Fix gen_notice.py encoding issue (#437)

--- build-support/gen_notice.py
+++ build-support/gen_notice.py
@@ -18,15 +18,15 @@ def gen_combined_notice(licenses_paths, outpath, gen_all):
             for l in licenses:
                 name = re.search(r'LICENSE-(.+)\.txt', l).group(1)
                 all[name][0] = l
-    with open(outpath, 'wt') as fout:
+    with open(outpath, 'wt', encoding='utf-8') as fout:
         fout.write('StarRocks\n\nCopyright 2021 StarRocks Limited\n')
         for name in sorted(all.keys(), key=str.lower):
             e = all[name]
             if e[1]:
-                with open(e[1], 'rt') as fin:
+                with open(e[1], 'rt', encoding='utf-8') as fin:
                     fout.write('\n---------------------------\n%s NOTICE\n---------------------------\n%s\n' % (name, fin.read()))
             if e[0]:
-                with open(e[0], 'rt') as fin:
+                with open(e[0], 'rt', encoding='utf-8') as fin:
                     fout.write('\n---------------------------\n%s LICENSE\n---------------------------\n%s\n' % (name, fin.read()))
 
 if __name__ == "__main__":
