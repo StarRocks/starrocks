c8665eb181d01792058670d4e20194d06ff54654
mklzl <36362816+mklzl@users.noreply.github.com>
2021-09-26 15:54:23 +0800

    Update FrontendOptions.java : add some log (#361)
    Print some logs to remind users to properly configure Priority_networks #349

--- fe/fe-core/src/main/java/com/starrocks/service/FrontendOptions.java
+++ fe/fe-core/src/main/java/com/starrocks/service/FrontendOptions.java
@@ -67,6 +67,7 @@ public class FrontendOptions {
         }
 
         InetAddress loopBack = null;
+        boolean hasMatchedIp = false;
         for (InetAddress addr : hosts) {
             LOG.debug("check ip address: {}", addr);
             if (addr instanceof Inet4Address) {
@@ -75,15 +76,19 @@ public class FrontendOptions {
                 } else if (!priorityCidrs.isEmpty()) {
                     if (isInPriorNetwork(addr.getHostAddress())) {
                         localAddr = addr;
+                        hasMatchedIp = true;
                         break;
-                    }
+                    } 
                 } else {
                     localAddr = addr;
                     break;
                 }
             }
         }
-
+        //if all ips not match the priority_networks then print the warning log
+        if (!priorityCidrs.isEmpty() && !hasMatchedIp) {
+            LOG.warn("ip address range configured for priority_networks does not include the current IP address");
+        }
         // nothing found, use loopback addr
         if (localAddr == null) {
             localAddr = loopBack;
