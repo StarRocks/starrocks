e1a1b40460813e033d4620f22b9263fface8dec3
Pslydhh <luouyuyou@gmail.com>
2021-09-28 11:23:33 +0800

    Remove re-use operators (#456)

--- be/src/exec/vectorized/aggregate/distinct_streaming_node.cpp
+++ be/src/exec/vectorized/aggregate/distinct_streaming_node.cpp
@@ -208,7 +208,7 @@ std::vector<std::shared_ptr<pipeline::OperatorFactory> > DistinctStreamingNode::
         pipeline::PipelineBuilderContext* context) {
     using namespace pipeline;
     OpFactories operators_with_sink = _children[0]->decompose_to_pipeline(context);
-    context->maybe_interpolate_local_exchange(operators_with_sink);
+    operators_with_sink = context->maybe_interpolate_local_exchange(operators_with_sink);
 
     // shared by sink operator and source operator
     AggregatorPtr aggregator = std::make_shared<Aggregator>(_tnode, child(0)->row_desc());
