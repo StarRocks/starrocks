5827806d559619141df77ee2f6935906c1162660
zhangqiang <953361637@qq.com>
2021-09-28 10:41:22 +0800

    Fix bug of crash in schema change of table with Array<string> (#463)
    Data in string type will be speculated to be dictionary encoding or not.
    If user create a table is a column with array<string>, the string column in array is not speculated.
    As a result, it will be throw a null pointer.

--- be/src/storage/rowset/segment_v2/column_writer.cpp
+++ be/src/storage/rowset/segment_v2/column_writer.cpp
@@ -179,6 +179,12 @@ public:
     Status append(const vectorized::Column& column) override;
 
     Status append(const uint8_t* data, const uint8_t* null_flags, size_t count, bool has_null) override {
+        // if column is Array<String>, encoding maybe not set
+        // check _is_speculated again to avoid _page_builder is not initialized
+        if (!_is_speculated) {
+            _scalar_column_writer->set_encoding(DEFAULT_ENCODING);
+            _is_speculated = true;
+        }
         return _scalar_column_writer->append(data, null_flags, count, has_null);
     };
 
