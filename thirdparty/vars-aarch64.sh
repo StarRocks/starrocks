30a82a1f176f79cb24d5f8859049b8f37c554164
ricky <rickif@qq.com>
2021-09-29 10:49:26 +0800

    Fix hadoop/openjdk compiling bug on ARM architecture(aarch64) (#452)
    This PR is to solve the compiling problems of thirdparty libraries on  ARM architecture(aarch64), including the following modifications.
      1. Add a new crc_hash functions to be compatible with aarch64
      2. Add a new download link of hadoop for aarch64
      3. OpenJDK for aarch64
      4. correcting Apache Arrow 5.0.0 building option
      5. Use CDN to improve the download speed of third-party libraries
      6. Change oracle release repository to download sleepycat-7.3.7

---
+++ thirdparty/vars-aarch64.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# This file is licensed under the Elastic License 2.0. Copyright 2021 StarRocks Limited.
+
+#####################################################
+# Download url, filename and unpaced filename
+# of all thirdparties
+# 
+# vars-${arch}.sh defines the thirdparties that are
+# architecure-related.
+#####################################################
+
+# HADOOP
+HADOOP_DOWNLOAD="https://cdn-thirdparty.starrocks.com/hadoop-3.3.0-aarch64.tar.gz"
+HADOOP_NAME="hadoop-3.3.0-aarch64.tar.gz"
+HADOOP_SOURCE="hadoop-3.3.0-aarch64"
+HADOOP_MD5SUM="4e2e3e0dc8a1d80e1940565bbf6199a4"
+
+# OPEN JDK FOR aarch64, provided by huawei kunpeng (https://www.hikunpeng.com/zh/developer/devkit/compiler)
+JDK_DOWNLOAD="https://mirror.iscas.ac.cn/kunpeng/archive/compiler/bisheng_jdk/bisheng-jdk-8u262-linux-aarch64.tar.gz"
+JDK_NAME="bisheng-jdk-8u262-linux-aarch64.tar.gz"
+JDK_SOURCE="bisheng-jdk1.8.0_262"
+JDK_MD5SUM="a1254dea3728e0a86e53a55d8debfbeb"
+
+# HYPERSCAN for aarch64, provided by huawei kunpeng.
+HYPERSCAN_DOWNLOAD="https://github.com/kunpengcompute/hyperscan/archive/refs/tags/v5.3.0.aarch64.tar.gz"
+HYPERSCAN_NAME="hyperscan-5.3.0.aarch64.tar.gz"
+HYPERSCAN_SOURCE="hyperscan-5.3.0.aarch64"
+HYPERSCAN_MD5SUM="ef337257bde6583242a739fab6fb161f"
