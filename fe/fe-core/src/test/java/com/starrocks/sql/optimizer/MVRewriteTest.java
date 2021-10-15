551b85b27fc93306caada627e8e80e74afb97a60
Youngwb <yangwenbo_mailbox@163.com>
2021-10-15 19:32:56 +0800

    Fix cast Exception for MaterializedViewRule (#689)

--- fe/fe-core/src/test/java/com/starrocks/sql/optimizer/MVRewriteTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/optimizer/MVRewriteTest.java
@@ -406,6 +406,14 @@ public class MVRewriteTest {
     }
 
     @Test
+    public void testAggregateMVAggregateFuncs6() throws Exception {
+        String createEmpsMVSQL = "create materialized view " + EMPS_MV_NAME + " as select deptno, empid, count(salary) "
+                + "from " + EMPS_TABLE_NAME + " group by empid, deptno;";
+        String query = "select deptno, sum(if(empid=0,0,1)) from " + EMPS_TABLE_NAME + " group by deptno";
+        System.out.println(starRocksAssert.withMaterializedView(createEmpsMVSQL).query(query).explainQuery());
+    }
+
+    @Test
     public void testAggregateMVCalcGroupByQuery1() throws Exception {
         String createEmpsMVSQL = "create materialized view " + EMPS_MV_NAME + " as select deptno, empid, sum(salary) "
                 + "from " + EMPS_TABLE_NAME + " group by empid, deptno;";
