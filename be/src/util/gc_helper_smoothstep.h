6c7e98daa20d2e5315cb8dde7baefe60c8abf89b
starrocks-xueli <xueli@dorisdb.com>
2021-10-19 20:41:15 +0800

    optimize BE memory gc with smooth step function (#672)
    * optimize BE memory gc with smooth step function (#183)
    
    * make gc default period 180 and add `tc_gc_period` argument
    
    * change default gc period to 60 seconds
    
    * change default gc interval to memory_maintenance_sleep_time_s

---
+++ be/src/util/gc_helper_smoothstep.h
@@ -0,0 +1,14 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021 StarRocks Limited.
+
+#pragma once
+
+#include <cstddef>
+
+namespace starrocks {
+
+#define SMOOTHSTEP_NSTEPS 180
+#define SMOOTHSTEP_BFP 24
+
+size_t get_smoothstep_at(size_t index);
+
+} // namespace starrocks
