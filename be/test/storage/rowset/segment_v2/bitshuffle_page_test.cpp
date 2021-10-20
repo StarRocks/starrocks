0251af8c2cffb8fd92d5988726440dbc735a7a91
DeepThinker666 <548440636@qq.com>
2021-10-20 23:13:30 +0800

    Optimize reading bitshuffle decode by reducing mem copy
    When read large chunk, the data can be decompressed directly to chunk column's memory,
    which can save mem copy from page to column

--- be/test/storage/rowset/segment_v2/bitshuffle_page_test.cpp
+++ be/test/storage/rowset/segment_v2/bitshuffle_page_test.cpp
@@ -25,11 +25,13 @@
 
 #include <memory>
 
+#include "column/datum_convert.h"
 #include "runtime/mem_pool.h"
 #include "runtime/mem_tracker.h"
 #include "storage/rowset/segment_v2/options.h"
 #include "storage/rowset/segment_v2/page_builder.h"
 #include "storage/rowset/segment_v2/page_decoder.h"
+#include "storage/vectorized/chunk_helper.h"
 #include "util/logging.h"
 
 using starrocks::segment_v2::PageBuilderOptions;
@@ -107,6 +109,71 @@ public:
             CppType ret;
             copy_one<Type, PageDecoderType>(&page_decoder, &ret);
             EXPECT_EQ(decoded[seek_off], ret);
+        }
+    }
+
+    template <FieldType Type, class PageBuilderType, class PageDecoderType>
+    void test_encode_decode_page_vectorize() {
+        using CppType = typename CppTypeTraits<Type>::CppType;
+        auto src = vectorized::ChunkHelper::column_from_field_type(Type, false);
+        CppType value = 0;
+        size_t count = 64 * 1024 / sizeof(CppType);
+        src->reserve(count);
+        for (size_t i = 0; i < count; ++i) {
+            (void)src->append_numbers(&value, sizeof(CppType));
+            value = value + 1;
+        }
+
+        PageBuilderOptions options;
+        options.data_page_size = 64 * 1024;
+        PageBuilderType page_builder(options);
+        size_t added = page_builder.add(reinterpret_cast<const uint8_t*>(src->raw_data()), count);
+        ASSERT_EQ(count, added);
+        OwnedSlice s = page_builder.finish()->build();
+
+        Slice encoded_data = s.slice();
+        {
+            // read whole the page
+            segment_v2::PageDecoderOptions decoder_options;
+            PageDecoderType page_decoder(encoded_data, decoder_options);
+            Status status = page_decoder.init();
+            ASSERT_TRUE(status.ok());
+            ASSERT_EQ(0, page_decoder.current_index());
+
+            auto dst = vectorized::ChunkHelper::column_from_field_type(Type, false);
+            dst->reserve(count);
+            size_t size = count;
+            status = page_decoder.next_batch(&size, dst.get());
+            ASSERT_TRUE(status.ok());
+            ASSERT_EQ(size, count);
+            TypeInfoPtr type_info = get_type_info(Type);
+            for (int i = 0; i < count; ++i) {
+                ASSERT_EQ(0, type_info->cmp(src->get(i), dst->get(i)))
+                        << " row " << i << ": " << datum_to_string(type_info.get(), src->get(i)) << " vs "
+                        << datum_to_string(type_info.get(), dst->get(i));
+            }
+        }
+
+        {
+            // read half of the page
+            segment_v2::PageDecoderOptions decoder_options;
+            PageDecoderType page_decoder(encoded_data, decoder_options);
+            Status status = page_decoder.init();
+            ASSERT_TRUE(status.ok());
+            ASSERT_EQ(0, page_decoder.current_index());
+
+            auto dst = vectorized::ChunkHelper::column_from_field_type(Type, false);
+            size_t size = count / 2;
+            dst->reserve(size);
+            status = page_decoder.next_batch(&size, dst.get());
+            ASSERT_TRUE(status.ok());
+            ASSERT_EQ(size, count / 2);
+            TypeInfoPtr type_info = get_type_info(Type);
+            for (int i = 0; i < size; ++i) {
+                ASSERT_EQ(0, type_info->cmp(src->get(i), dst->get(i)))
+                        << " row " << i << ": " << datum_to_string(type_info.get(), src->get(i)) << " vs "
+                        << datum_to_string(type_info.get(), dst->get(i));
+            }
         }
     }
 
@@ -355,6 +422,17 @@ TEST_F(BitShufflePageTest, TestReserveHead) {
 
     test_encode_decode_page_template<OLAP_FIELD_TYPE_INT, segment_v2::BitshufflePageBuilder<OLAP_FIELD_TYPE_INT>,
                                      segment_v2::BitShufflePageDecoder<OLAP_FIELD_TYPE_INT>, 4>(ints.get(), size);
+}
+
+TEST_F(BitShufflePageTest, TestDecodeVectorized) {
+    test_encode_decode_page_vectorize<OLAP_FIELD_TYPE_TINYINT, segment_v2::BitshufflePageBuilder<OLAP_FIELD_TYPE_TINYINT>,
+                                     segment_v2::BitShufflePageDecoder<OLAP_FIELD_TYPE_TINYINT>>();
+    test_encode_decode_page_vectorize<OLAP_FIELD_TYPE_SMALLINT, segment_v2::BitshufflePageBuilder<OLAP_FIELD_TYPE_SMALLINT>,
+                                     segment_v2::BitShufflePageDecoder<OLAP_FIELD_TYPE_SMALLINT>>();
+    test_encode_decode_page_vectorize<OLAP_FIELD_TYPE_INT, segment_v2::BitshufflePageBuilder<OLAP_FIELD_TYPE_INT>,
+                                     segment_v2::BitShufflePageDecoder<OLAP_FIELD_TYPE_INT>>();
+    test_encode_decode_page_vectorize<OLAP_FIELD_TYPE_BIGINT, segment_v2::BitshufflePageBuilder<OLAP_FIELD_TYPE_BIGINT>,
+                                     segment_v2::BitShufflePageDecoder<OLAP_FIELD_TYPE_BIGINT>>();
 }
 
 } // namespace starrocks
