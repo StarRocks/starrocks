25e4c0aaafe031cc30754ad2f4a919f823490408
DeepThinker666 <548440636@qq.com>
2021-10-25 21:57:14 +0800

    optimize null flags encoding (#757) (#760)
    use lz4 to compress null flags for better compression ratio and decompression speed. In my test, Bitshuffle has no compression ratio for null flags. In a test with 266882552 records, the compressed data size and decompression time is as following:
    bitshuffle lz4 rle
    size 268MB 30MB 20MB
    time 246ms 119ms 262ms
    Add config::null_encoding to decide the default encoding type of null flags. Make sure to upgrade all BEs before change
    the config.

--- be/test/storage/rowset/segment_v2/column_reader_writer_test.cpp
+++ be/test/storage/rowset/segment_v2/column_reader_writer_test.cpp
@@ -65,7 +65,9 @@ protected:
     void TearDown() override { _tracker.release(_tracker.consumption()); }
 
     template <FieldType type, EncodingTypePB encoding, uint32_t version, bool adaptive = true>
-    void test_nullable_data(const vectorized::Column& src) {
+    void test_nullable_data(const vectorized::Column& src, const std::string null_encoding = "0") {
+        config::set_config("null_encoding", null_encoding);
+
         using Type = typename TypeTraits<type>::CppType;
         TypeInfoPtr type_info = get_type_info(type);
         int num_rows = src.size();
@@ -264,6 +266,123 @@ protected:
         }
     }
 
+    template <uint32_t version>
+    void test_int_array(std::string null_encoding = "0") {
+        config::set_config("null_encoding", null_encoding);
+        auto env = std::make_unique<EnvMemory>();
+        auto block_mgr = std::make_unique<fs::FileBlockManager>(env.get(), fs::BlockManagerOptions());
+        ASSERT_TRUE(env->create_dir(TEST_DIR).ok());
+
+        TabletColumn array_column = create_array(0, true, sizeof(Collection));
+        TabletColumn int_column = create_int_value(0, OLAP_FIELD_AGGREGATION_NONE, true);
+        array_column.add_sub_column(int_column);
+
+        auto src_offsets = vectorized::UInt32Column::create();
+        auto src_elements = vectorized::Int32Column::create();
+        vectorized::ColumnPtr src_column = vectorized::ArrayColumn::create(src_elements, src_offsets);
+
+        // insert [1, 2, 3], [4, 5, 6]
+        src_elements->append(1);
+        src_elements->append(2);
+        src_elements->append(3);
+        src_offsets->append(3);
+
+        src_elements->append(4);
+        src_elements->append(5);
+        src_elements->append(6);
+        src_offsets->append(6);
+
+        int num_rows = src_column->size();
+
+        TypeInfoPtr type_info = get_type_info(array_column);
+        ColumnMetaPB meta;
+
+        // delete test file.
+        const std::string fname = TEST_DIR + "/test_array_int.data";
+        // write data
+        {
+            std::unique_ptr<fs::WritableBlock> wblock;
+            fs::CreateBlockOptions opts({fname});
+            Status st = block_mgr->create_block(opts, &wblock);
+            ASSERT_TRUE(st.ok()) << st.get_error_msg();
+
+            ColumnWriterOptions writer_opts;
+            writer_opts.page_format = version;
+            writer_opts.meta = &meta;
+            writer_opts.meta->set_column_id(0);
+            writer_opts.meta->set_unique_id(0);
+            writer_opts.meta->set_type(OLAP_FIELD_TYPE_ARRAY);
+            writer_opts.meta->set_length(0);
+            writer_opts.meta->set_encoding(DEFAULT_ENCODING);
+            writer_opts.meta->set_compression(starrocks::LZ4_FRAME);
+            writer_opts.meta->set_is_nullable(false);
+            writer_opts.need_zone_map = false;
+
+            // init integer sub column
+            ColumnMetaPB* element_meta = writer_opts.meta->add_children_columns();
+            element_meta->set_column_id(0);
+            element_meta->set_unique_id(0);
+            element_meta->set_type(int_column.type());
+            element_meta->set_length(int_column.length());
+            element_meta->set_encoding(DEFAULT_ENCODING);
+            element_meta->set_compression(LZ4_FRAME);
+            element_meta->set_is_nullable(false);
+
+            std::unique_ptr<ColumnWriter> writer;
+            ColumnWriter::create(writer_opts, &array_column, wblock.get(), &writer);
+            st = writer->init();
+            ASSERT_TRUE(st.ok()) << st.to_string();
+
+            ASSERT_TRUE(writer->append(*src_column).ok());
+
+            ASSERT_TRUE(writer->finish().ok());
+            ASSERT_TRUE(writer->write_data().ok());
+            ASSERT_TRUE(writer->write_ordinal_index().ok());
+
+            // close the file
+            ASSERT_TRUE(wblock->close().ok());
+        }
+
+        // read and check
+        {
+            ColumnReaderOptions reader_opts;
+            reader_opts.block_mgr = block_mgr.get();
+            reader_opts.storage_format_version = 2;
+            std::unique_ptr<ColumnReader> reader;
+            auto st = ColumnReader::create(&_tracker, reader_opts, meta, num_rows, fname, &reader);
+            ASSERT_TRUE(st.ok());
+
+            ColumnIterator* iter = nullptr;
+            ASSERT_TRUE(reader->new_iterator(&iter).ok());
+            std::unique_ptr<ColumnIterator> guard(iter);
+            std::unique_ptr<fs::ReadableBlock> rblock;
+            block_mgr->open_block(fname, &rblock);
+
+            ColumnIteratorOptions iter_opts;
+            OlapReaderStatistics stats;
+            iter_opts.stats = &stats;
+            iter_opts.rblock = rblock.get();
+            ASSERT_TRUE(iter->init(iter_opts).ok());
+
+            // sequence read
+            {
+                st = iter->seek_to_first();
+                ASSERT_TRUE(st.ok()) << st.to_string();
+
+                auto dst_offsets = vectorized::UInt32Column::create();
+                auto dst_elements = vectorized::Int32Column::create();
+                auto dst_column = vectorized::ArrayColumn::create(dst_elements, dst_offsets);
+                size_t rows_read = src_column->size();
+                st = iter->next_batch(&rows_read, dst_column.get());
+                ASSERT_TRUE(st.ok());
+                ASSERT_EQ(src_column->size(), rows_read);
+
+                ASSERT_EQ("[1, 2, 3]", dst_column->debug_item(0));
+                ASSERT_EQ("[4, 5, 6]", dst_column->debug_item(1));
+            }
+        }
+    }
+
     template <FieldType type>
     vectorized::ColumnPtr numeric_data(int null_ratio) {
         using CppType = typename CppTypeTraits<type>::CppType;
@@ -372,14 +491,17 @@ protected:
         auto col = numeric_data<type>(1);
         test_nullable_data<type, BIT_SHUFFLE, 1>(*col);
         test_nullable_data<type, BIT_SHUFFLE, 2>(*col);
+        test_nullable_data<type, BIT_SHUFFLE, 2>(*col, "1");
 
         col = numeric_data<type>(4);
         test_nullable_data<type, BIT_SHUFFLE, 1>(*col);
         test_nullable_data<type, BIT_SHUFFLE, 2>(*col);
+        test_nullable_data<type, BIT_SHUFFLE, 2>(*col, "1");
 
         col = numeric_data<type>(10000);
         test_nullable_data<type, BIT_SHUFFLE, 1>(*col);
         test_nullable_data<type, BIT_SHUFFLE, 2>(*col);
+        test_nullable_data<type, BIT_SHUFFLE, 2>(*col, "1");
     }
 
     MemTracker _tracker;
@@ -401,6 +523,7 @@ TEST_F(ColumnReaderWriterTest, test_date) {
     auto col = date_values(100);
     test_nullable_data<OLAP_FIELD_TYPE_DATE_V2, BIT_SHUFFLE, 1>(*col);
     test_nullable_data<OLAP_FIELD_TYPE_DATE_V2, BIT_SHUFFLE, 2>(*col);
+    test_nullable_data<OLAP_FIELD_TYPE_DATE_V2, BIT_SHUFFLE, 2>(*col, "1");
 }
 
 // NOLINTNEXTLINE
@@ -408,6 +531,7 @@ TEST_F(ColumnReaderWriterTest, test_datetime) {
     auto col = datetime_values(100);
     test_nullable_data<OLAP_FIELD_TYPE_TIMESTAMP, BIT_SHUFFLE, 1>(*col);
     test_nullable_data<OLAP_FIELD_TYPE_TIMESTAMP, BIT_SHUFFLE, 2>(*col);
+    test_nullable_data<OLAP_FIELD_TYPE_TIMESTAMP, BIT_SHUFFLE, 2>(*col, "1");
 }
 
 // NOLINTNEXTLINE
@@ -415,16 +539,20 @@ TEST_F(ColumnReaderWriterTest, test_binary) {
     auto c = low_cardinality_strings(10000);
     test_nullable_data<OLAP_FIELD_TYPE_VARCHAR, DICT_ENCODING, 1>(*c);
     test_nullable_data<OLAP_FIELD_TYPE_VARCHAR, DICT_ENCODING, 2>(*c);
+    test_nullable_data<OLAP_FIELD_TYPE_VARCHAR, DICT_ENCODING, 2>(*c, "1");
 
     test_nullable_data<OLAP_FIELD_TYPE_CHAR, DICT_ENCODING, 1>(*c);
     test_nullable_data<OLAP_FIELD_TYPE_CHAR, DICT_ENCODING, 2>(*c);
+    test_nullable_data<OLAP_FIELD_TYPE_CHAR, DICT_ENCODING, 2>(*c, "1");
 
     c = high_cardinality_strings(100);
     test_nullable_data<OLAP_FIELD_TYPE_VARCHAR, DICT_ENCODING, 1>(*c);
     test_nullable_data<OLAP_FIELD_TYPE_VARCHAR, DICT_ENCODING, 2>(*c);
+    test_nullable_data<OLAP_FIELD_TYPE_VARCHAR, DICT_ENCODING, 2>(*c, "1");
 
     test_nullable_data<OLAP_FIELD_TYPE_CHAR, DICT_ENCODING, 1>(*c);
     test_nullable_data<OLAP_FIELD_TYPE_CHAR, DICT_ENCODING, 2>(*c);
+    test_nullable_data<OLAP_FIELD_TYPE_CHAR, DICT_ENCODING, 2>(*c, "1");
 }
 
 // NOLINTNEXTLINE
@@ -477,118 +605,8 @@ TEST_F(ColumnReaderWriterTest, test_default_value) {
 
 // test array<int>, and nullable
 TEST_F(ColumnReaderWriterTest, test_array_int) {
-    auto env = std::make_unique<EnvMemory>();
-    auto block_mgr = std::make_unique<fs::FileBlockManager>(env.get(), fs::BlockManagerOptions());
-    ASSERT_TRUE(env->create_dir(TEST_DIR).ok());
-
-    TabletColumn array_column = create_array(0, true, sizeof(Collection));
-    TabletColumn int_column = create_int_value(0, OLAP_FIELD_AGGREGATION_NONE, true);
-    array_column.add_sub_column(int_column);
-
-    auto src_offsets = vectorized::UInt32Column::create();
-    auto src_elements = vectorized::Int32Column::create();
-    vectorized::ColumnPtr src_column = vectorized::ArrayColumn::create(src_elements, src_offsets);
-
-    // insert [1, 2, 3], [4, 5, 6]
-    src_elements->append(1);
-    src_elements->append(2);
-    src_elements->append(3);
-    src_offsets->append(3);
-
-    src_elements->append(4);
-    src_elements->append(5);
-    src_elements->append(6);
-    src_offsets->append(6);
-
-    int num_rows = src_column->size();
-
-    TypeInfoPtr type_info = get_type_info(array_column);
-    ColumnMetaPB meta;
-
-    // delete test file.
-    const std::string fname = TEST_DIR + "/test_array_int.data";
-    // write data
-    {
-        std::unique_ptr<fs::WritableBlock> wblock;
-        fs::CreateBlockOptions opts({fname});
-        Status st = block_mgr->create_block(opts, &wblock);
-        ASSERT_TRUE(st.ok()) << st.get_error_msg();
-
-        ColumnWriterOptions writer_opts;
-        writer_opts.page_format = 2;
-        writer_opts.meta = &meta;
-        writer_opts.meta->set_column_id(0);
-        writer_opts.meta->set_unique_id(0);
-        writer_opts.meta->set_type(OLAP_FIELD_TYPE_ARRAY);
-        writer_opts.meta->set_length(0);
-        writer_opts.meta->set_encoding(DEFAULT_ENCODING);
-        writer_opts.meta->set_compression(starrocks::LZ4_FRAME);
-        writer_opts.meta->set_is_nullable(false);
-        writer_opts.need_zone_map = false;
-
-        // init integer sub column
-        ColumnMetaPB* element_meta = writer_opts.meta->add_children_columns();
-        element_meta->set_column_id(0);
-        element_meta->set_unique_id(0);
-        element_meta->set_type(int_column.type());
-        element_meta->set_length(int_column.length());
-        element_meta->set_encoding(DEFAULT_ENCODING);
-        element_meta->set_compression(LZ4_FRAME);
-        element_meta->set_is_nullable(false);
-
-        std::unique_ptr<ColumnWriter> writer;
-        ColumnWriter::create(writer_opts, &array_column, wblock.get(), &writer);
-        st = writer->init();
-        ASSERT_TRUE(st.ok()) << st.to_string();
-
-        ASSERT_TRUE(writer->append(*src_column).ok());
-
-        ASSERT_TRUE(writer->finish().ok());
-        ASSERT_TRUE(writer->write_data().ok());
-        ASSERT_TRUE(writer->write_ordinal_index().ok());
-
-        // close the file
-        ASSERT_TRUE(wblock->close().ok());
-    }
-
-    // read and check
-    {
-        ColumnReaderOptions reader_opts;
-        reader_opts.block_mgr = block_mgr.get();
-        reader_opts.storage_format_version = 2;
-        std::unique_ptr<ColumnReader> reader;
-        auto st = ColumnReader::create(&_tracker, reader_opts, meta, num_rows, fname, &reader);
-        ASSERT_TRUE(st.ok());
-
-        ColumnIterator* iter = nullptr;
-        ASSERT_TRUE(reader->new_iterator(&iter).ok());
-        std::unique_ptr<ColumnIterator> guard(iter);
-        std::unique_ptr<fs::ReadableBlock> rblock;
-        block_mgr->open_block(fname, &rblock);
-
-        ColumnIteratorOptions iter_opts;
-        OlapReaderStatistics stats;
-        iter_opts.stats = &stats;
-        iter_opts.rblock = rblock.get();
-        ASSERT_TRUE(iter->init(iter_opts).ok());
-
-        // sequence read
-        {
-            st = iter->seek_to_first();
-            ASSERT_TRUE(st.ok()) << st.to_string();
-
-            auto dst_offsets = vectorized::UInt32Column::create();
-            auto dst_elements = vectorized::Int32Column::create();
-            auto dst_column = vectorized::ArrayColumn::create(dst_elements, dst_offsets);
-            size_t rows_read = src_column->size();
-            st = iter->next_batch(&rows_read, dst_column.get());
-            ASSERT_TRUE(st.ok());
-            ASSERT_EQ(src_column->size(), rows_read);
-
-            ASSERT_EQ("[1, 2, 3]", dst_column->debug_item(0));
-            ASSERT_EQ("[4, 5, 6]", dst_column->debug_item(1));
-        }
-    }
+    test_int_array<2>();
+    test_int_array<2>("1");
 }
 
 } // namespace starrocks::segment_v2
