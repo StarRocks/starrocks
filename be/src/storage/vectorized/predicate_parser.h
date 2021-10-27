4b3b43dd233f66719acec3ce0866b59a9cc67ad7
yan.zhang <dirtysalt1987@gmail.com>
2021-10-27 12:52:24 +0800

    Support expr as column predicate in scan node (#822)

--- be/src/storage/vectorized/predicate_parser.h
+++ be/src/storage/vectorized/predicate_parser.h
@@ -6,6 +6,9 @@ namespace starrocks {
 
 class TabletSchema;
 class TCondition;
+class ExprContext;
+class SlotDescriptor;
+class RuntimeState;
 
 namespace vectorized {
 
@@ -19,7 +22,9 @@ public:
 
     // Parse |condition| into a predicate that can be pushed down.
     // return nullptr if parse failed.
-    ColumnPredicate* parse(const TCondition& condition) const;
+    ColumnPredicate* parse_thrift_cond(const TCondition& condition) const;
+
+    ColumnPredicate* parse_expr_ctx(const SlotDescriptor& slot_desc, RuntimeState*, ExprContext* expr_ctx) const;
 
 private:
     const TabletSchema& _schema;
