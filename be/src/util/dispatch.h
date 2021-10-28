b49f42e66407dd290b993e6b881c16d4acfbe0ed
stdpain <drfeng08@gmail.com>
2021-10-28 21:37:49 +0800

    optimize no-nullable IF expr for no-nullable int8/int32/float type
    case:
    ```
    SELECT SUM(IF(LO_SUPPLYCOST + 10000 > 100000, P_SIZE, LO_REVENUE) ) FROM ssb.lineorder_flat;
    ```
    before:10.19 after:1.69
    
    ```
    SELECT SUM(IF(LO_SUPPLYCOST + 10000 > 100000, 0, 1) ) FROM ssb.lineorder_flat;
    ```
    before:10.44 after:1.22

---
+++ be/src/util/dispatch.h
@@ -0,0 +1,25 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021 StarRocks Limited.
+
+#pragma once
+#include "column/column.h"
+#include "runtime/primitive_type.h"
+
+namespace starrocks::vectorized {
+template <template <bool, bool, PrimitiveType Type, typename... Args> typename Function, PrimitiveType Type,
+          typename... Args>
+ColumnPtr dispatch_nonull_template(ColumnPtr& col0, ColumnPtr& col1, Args&&... args) {
+    bool a = col0->is_constant();
+    bool b = col1->is_constant();
+    if (a && b) {
+        return Function<true, true, Type>::eval(col0, col1, std::forward<Args>(args)...);
+    } else if (a && !b) {
+        return Function<true, false, Type>::eval(col0, col1, std::forward<Args>(args)...);
+    } else if (!a && b) {
+        return Function<false, true, Type>::eval(col0, col1, std::forward<Args>(args)...);
+    } else if (!a && !b) {
+        return Function<false, false, Type>::eval(col0, col1, std::forward<Args>(args)...);
+    }
+    __builtin_unreachable();
+};
+
+} // namespace starrocks::vectorized
