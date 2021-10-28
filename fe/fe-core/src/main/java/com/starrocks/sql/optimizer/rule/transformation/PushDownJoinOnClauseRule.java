72725210b262052a29d813fad36194b257296bb9
Seaven <seaven_7@qq.com>
2021-10-28 16:13:34 +0800

    update

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/PushDownJoinOnClauseRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/PushDownJoinOnClauseRule.java
@@ -69,13 +69,13 @@ public class PushDownJoinOnClauseRule extends TransformationRule {
             return Utils.compoundAnd(on, derivedPredicate);
         } else if (join.getJoinType().isLeftOuterJoin()) {
             for (ScalarOperator p : derivedPredicates) {
-                if (rightOutputColumns.contains(derivedPredicate.getUsedColumns())) {
+                if (rightOutputColumns.contains(p.getUsedColumns())) {
                     pushDown.add(p);
                 }
             }
         } else if (join.getJoinType().isRightOuterJoin()) {
             for (ScalarOperator p : derivedPredicates) {
-                if (leftOutputColumns.contains(derivedPredicate.getUsedColumns())) {
+                if (leftOutputColumns.contains(p.getUsedColumns())) {
                     pushDown.add(p);
                 }
             }
