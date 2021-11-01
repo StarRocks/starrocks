f8d6aa9bdd4adce01805cfe94d350e124e7b1ce4
zihe.liu <ziheliu1024@gmail.com>
2021-11-01 18:47:52 +0800

    Decompose except node to piepline (#933)

--- be/src/exec/vectorized/cross_join_node.cpp
+++ be/src/exec/vectorized/cross_join_node.cpp
@@ -536,7 +536,8 @@ pipeline::OpFactories CrossJoinNode::decompose_to_pipeline(pipeline::PipelineBui
 
     // step 0: construct pipeline end with cross join right operator.
     OpFactories operator_before_cross_join_right = _children[1]->decompose_to_pipeline(context);
-    operator_before_cross_join_right = context->maybe_interpolate_local_exchange(operator_before_cross_join_right);
+    operator_before_cross_join_right =
+            context->maybe_interpolate_local_passthrough_exchange(operator_before_cross_join_right);
 
     // communication with CrossJoinLeft through shared_datas.
     auto right_factory =
@@ -547,7 +548,8 @@ pipeline::OpFactories CrossJoinNode::decompose_to_pipeline(pipeline::PipelineBui
 
     // step 1: construct pipeline end with cross join left operator(cross join left maybe not sink operator).
     OpFactories operator_before_cross_join_left = _children[0]->decompose_to_pipeline(context);
-    operator_before_cross_join_left = context->maybe_interpolate_local_exchange(operator_before_cross_join_left);
+    operator_before_cross_join_left =
+            context->maybe_interpolate_local_passthrough_exchange(operator_before_cross_join_left);
 
     // communication with CrossJoioRight through shared_datas.
     auto left_factory = std::make_shared<CrossJoinLeftOperatorFactory>(
--- be/test/exec/pipeline/pipeline_test_base.cpp
+++ be/test/exec/pipeline/pipeline_test_base.cpp
@@ -22,7 +22,7 @@ void PipelineTestBase::start_test() {
     _execute();
 }
 
-OpFactories PipelineTestBase::maybe_interpolate_local_exchange(OpFactories& pred_operators) {
+OpFactories PipelineTestBase::maybe_interpolate_local_passthrough_exchange(OpFactories& pred_operators) {
     DCHECK(!pred_operators.empty() && pred_operators[0]->is_source());
     auto* source_operator = down_cast<SourceOperatorFactory*>(pred_operators[0].get());
     if (source_operator->degree_of_parallelism() > 1) {
