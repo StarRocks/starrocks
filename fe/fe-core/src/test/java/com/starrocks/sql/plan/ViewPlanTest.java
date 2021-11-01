eeecda8b60af7f100305f9c208e40a827d477571
Seaven <seaven_7@qq.com>
2021-11-01 14:59:12 +0800

    Fix create view with colum name bug (#972)

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/ViewPlanTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/ViewPlanTest.java
@@ -1515,15 +1515,28 @@ public class ViewPlanTest extends PlanTestBase {
         testView(sql);
     }
 
+//    @Test
+//    public void testWithSelectStar() throws Exception {
+//        String sql = "with xx1 as (select v1 + 1, v2 + 2 from t0) select * from xx1";
+//        testView(sql);
+//
+//        sql = "with xx1 as (select * from t0) select * from xx1";
+//        testView(sql);
+//
+//        sql = "with xx1 as (select v1, v2 from t0) select * from xx1";
+//        testView(sql);
+//    }
+
     @Test
-    public void testWithSelectStar() throws Exception {
-        String sql = "with xx1 as (select v1 + 1, v2 + 2 from t0) select * from xx1";
-        testView(sql);
+    public void testAliasView() throws Exception {
+        String sql = "select * from (select a.A, count(*) as cnt from ( SELECT 1 AS A UNION ALL SELECT 2 UNION ALL " +
+                "SELECT 2 UNION ALL SELECT 5 UNION ALL SELECT 3 UNION ALL SELECT 0 AS A ) a group by a.A order by a.A ) b;";
+        String createView = "create view alias_view(col1, col2) as " + sql;
+        starRocksAssert.withView(createView);
 
-        sql = "with xx1 as (select * from t0) select * from xx1";
-        testView(sql);
-
-        sql = "with xx1 as (select v1, v2 from t0) select * from xx1";
-        testView(sql);
+        String sqlPlan = getFragmentPlan(sql);
+        String viewPlan = getFragmentPlan("select * from alias_view");
+        Assert.assertEquals(sqlPlan, viewPlan);
+        starRocksAssert.dropView("alias_view");
     }
 }
