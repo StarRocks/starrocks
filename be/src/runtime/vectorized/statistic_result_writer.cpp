f3cbc4495d6726bf0219daced8cbfc5967ceaf0d
starrocks-xueli <xueli@dorisdb.com>
2021-11-02 16:27:14 +0800

    make BufferControlBlock exception safe

--- be/src/runtime/vectorized/statistic_result_writer.cpp
+++ be/src/runtime/vectorized/statistic_result_writer.cpp
@@ -22,7 +22,9 @@ StatisticResultWriter::StatisticResultWriter(BufferControlBlock* sinker,
                                              starrocks::RuntimeProfile* parent_profile)
         : _sinker(sinker), _output_expr_ctxs(output_expr_ctxs), _parent_profile(parent_profile) {}
 
-StatisticResultWriter::~StatisticResultWriter() = default;
+StatisticResultWriter::~StatisticResultWriter() {
+    close();
+}
 
 Status StatisticResultWriter::init(RuntimeState* state) {
     _init_profile();
@@ -65,13 +67,13 @@ Status StatisticResultWriter::append_chunk(vectorized::Chunk* chunk) {
 
     int version = down_cast<Int32Column*>(ColumnHelper::get_data_column(result_columns[0].get()))->get_data()[0];
 
-    auto* result = new (std::nothrow) TFetchDataResult();
+    std::unique_ptr<TFetchDataResult> result = std::make_unique<TFetchDataResult>();
 
     // Step 3: fill statistic data
     if (version == STATISTIC_DATA_VERSION1) {
-        _fill_statistic_data_v1(version, result_columns, chunk, result);
+        _fill_statistic_data_v1(version, result_columns, chunk, result.get());
     } else if (version == DICT_STATISTIC_DATA_VERSION) {
-        _fill_dict_statistic_data(version, result_columns, chunk, result);
+        _fill_dict_statistic_data(version, result_columns, chunk, result.get());
     }
 
     // Step 4: send
@@ -84,7 +86,6 @@ Status StatisticResultWriter::append_chunk(vectorized::Chunk* chunk) {
     }
 
     LOG(WARNING) << "Append statistic result to sink failed.";
-    delete result;
     return status;
 }
 
