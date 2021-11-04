095b2bcd5083c923ef4a9e26aa96d2f15598a5ba
xueyan.li <astralidea@163.com>
2021-11-04 17:50:24 +0800

    Return success when drop not exist materialized view #1026
    The use send a DDL statement to drop materialized view like the following.
    ```
    DROP MATERIALIZED VIEW if exists t2;
    ```
    If t2 is not exist, the DDL should success instead of throw an exception.
    And also, the related unit test is no longer used and to be removed.

--- fe/fe-core/src/test/java/com/starrocks/analysis/DropMaterializedViewStmtTest.java
+++ fe/fe-core/src/test/java/com/starrocks/analysis/DropMaterializedViewStmtTest.java
@@ -21,22 +21,94 @@
 
 package com.starrocks.analysis;
 
+import com.starrocks.catalog.Catalog;
+import com.starrocks.catalog.Column;
+import com.starrocks.catalog.Database;
+import com.starrocks.catalog.KeysType;
+import com.starrocks.catalog.MaterializedIndex;
+import com.starrocks.catalog.OlapTable;
+import com.starrocks.catalog.Partition;
+import com.starrocks.catalog.SinglePartitionInfo;
+import com.starrocks.catalog.Type;
+import com.starrocks.common.AnalysisException;
 import com.starrocks.common.UserException;
+import com.starrocks.common.jmockit.Deencapsulation;
 import com.starrocks.mysql.privilege.Auth;
+import com.starrocks.mysql.privilege.MockedAuth;
+import com.starrocks.qe.ConnectContext;
+import com.starrocks.thrift.TStorageType;
+import mockit.Mock;
+import mockit.MockUp;
 import mockit.Mocked;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class DropMaterializedViewStmtTest {
 
     Analyzer analyzer;
     @Mocked
     Auth auth;
+    private Catalog catalog;
+    @Mocked
+    private ConnectContext connectContext;
 
     @Before
     public void setUp() {
         analyzer = AccessTestUtil.fetchAdminAnalyzer(true);
+        MockedAuth.mockedAuth(auth);
+        catalog = Deencapsulation.newInstance(Catalog.class);
+        analyzer = new Analyzer(catalog, connectContext);
+        Database db = new Database(50000L, "test");
+
+        Column column1 = new Column("col1", Type.BIGINT);
+        Column column2 = new Column("col2", Type.DOUBLE);
+
+        List<Column> baseSchema = new LinkedList<>();
+        baseSchema.add(column1);
+        baseSchema.add(column2);
+
+        SinglePartitionInfo singlePartitionInfo = new SinglePartitionInfo();
+        OlapTable table = new OlapTable(30000, "table",
+                baseSchema, KeysType.AGG_KEYS, singlePartitionInfo, null);
+        table.setBaseIndexId(100);
+        db.createTable(table);
+        table.addPartition(new Partition(100, "p",
+                new MaterializedIndex(200, MaterializedIndex.IndexState.NORMAL), null));
+        table.setIndexMeta(200, "mvname", baseSchema, 0, 0, (short) 0,
+                TStorageType.COLUMN, KeysType.AGG_KEYS);
+
+        new MockUp<Catalog>() {
+            @Mock
+            Catalog getCurrentCatalog() {
+                return catalog;
+            }
+
+            @Mock
+            Auth getAuth() {
+                return auth;
+            }
+
+            @Mock
+            Database getDb(long dbId) {
+                return db;
+            }
+
+            @Mock
+            Database getDb(String dbName) {
+                return db;
+            }
+        };
+
+        new MockUp<Analyzer>() {
+            @Mock
+            String getClusterName() {
+                return "testCluster";
+            }
+        };
     }
 
     @Test
@@ -54,7 +126,8 @@ public class DropMaterializedViewStmtTest {
     @Test
     public void testRepeatedDB() {
         DropMaterializedViewStmt stmt =
-                new DropMaterializedViewStmt(false, new TableName("test", "mvname"), new TableName("test", "table"));
+                new DropMaterializedViewStmt(false, new TableName("test", "mvname"),
+                        new TableName("test", "table"));
         try {
             stmt.analyze(analyzer);
             Assert.fail();
@@ -66,7 +139,8 @@ public class DropMaterializedViewStmtTest {
     @Test
     public void testFromDB() {
         DropMaterializedViewStmt stmt =
-                new DropMaterializedViewStmt(false, new TableName("test", "mvname"), new TableName("", "table"));
+                new DropMaterializedViewStmt(false, new TableName("test", "mvname"),
+                        new TableName("", "table"));
         try {
             stmt.analyze(analyzer);
             Assert.fail();
@@ -77,7 +151,8 @@ public class DropMaterializedViewStmtTest {
 
     @Test
     public void testNormal() {
-        DropMaterializedViewStmt stmt = new DropMaterializedViewStmt(false, new TableName("test", "mvname"), null);
+        DropMaterializedViewStmt stmt = new DropMaterializedViewStmt(false,
+                new TableName("test", "mvname"), null);
         try {
             stmt.analyze(analyzer);
         } catch (UserException e) {
@@ -86,5 +161,19 @@ public class DropMaterializedViewStmtTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Test(expected = AnalysisException.class)
+    public void testIfExists() throws UserException {
+        DropMaterializedViewStmt stmt = new DropMaterializedViewStmt(false,
+                new TableName("test", "mvname2"), null);
+        stmt.analyze(analyzer);
+    }
+
+    @Test
+    public void testIfNotExists() throws UserException {
+        DropMaterializedViewStmt stmt = new DropMaterializedViewStmt(true,
+                new TableName("test", "mvname2"), null);
+        stmt.analyze(analyzer);
     }
 }
