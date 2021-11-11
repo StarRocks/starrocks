883037e626e53d350974c7e7c32707128c8b49a5
liuyehcf <1559500551@qq.com>
2021-11-11 09:47:12 +0800

    Paralize non-finalize aggregate blocking operator (#1185)

--- be/src/exec/pipeline/aggregate/aggregate_blocking_source_operator.h
+++ be/src/exec/pipeline/aggregate/aggregate_blocking_source_operator.h
@@ -32,17 +32,18 @@ private:
 
 class AggregateBlockingSourceOperatorFactory final : public SourceOperatorFactory {
 public:
-    AggregateBlockingSourceOperatorFactory(int32_t id, int32_t plan_node_id, AggregatorPtr aggregator)
+    AggregateBlockingSourceOperatorFactory(int32_t id, int32_t plan_node_id, AggregatorFactoryPtr aggregator_factory)
             : SourceOperatorFactory(id, "aggregate_blocking_source", plan_node_id),
-              _aggregator(std::move(aggregator)) {}
+              _aggregator_factory(std::move(aggregator_factory)) {}
 
     ~AggregateBlockingSourceOperatorFactory() override = default;
 
     OperatorPtr create(int32_t degree_of_parallelism, int32_t driver_sequence) override {
-        return std::make_shared<AggregateBlockingSourceOperator>(_id, _plan_node_id, _aggregator);
+        return std::make_shared<AggregateBlockingSourceOperator>(_id, _plan_node_id,
+                                                                 _aggregator_factory->get_or_create(driver_sequence));
     }
 
 private:
-    AggregatorPtr _aggregator = nullptr;
+    AggregatorFactoryPtr _aggregator_factory = nullptr;
 };
 } // namespace starrocks::pipeline
--- be/src/exec/pipeline/aggregate/aggregate_streaming_source_operator.h
+++ be/src/exec/pipeline/aggregate/aggregate_streaming_source_operator.h
@@ -41,12 +41,11 @@ public:
     ~AggregateStreamingSourceOperatorFactory() override = default;
 
     OperatorPtr create(int32_t degree_of_parallelism, int32_t driver_sequence) override {
-        return std::make_shared<AggregateStreamingSourceOperator>(
-                _id, _plan_node_id, _aggregator_factory->get_or_create(_aggregator_idx++));
+        return std::make_shared<AggregateStreamingSourceOperator>(_id, _plan_node_id,
+                                                                  _aggregator_factory->get_or_create(driver_sequence));
     }
 
 private:
     AggregatorFactoryPtr _aggregator_factory = nullptr;
-    size_t _aggregator_idx = 0;
 };
 } // namespace starrocks::pipeline
