79dca4cbdd65eaf642e6b1e719e69888346ac90e
stdpain <drfeng08@gmail.com>
2021-11-12 20:51:29 +0800

    add some comment for select if

--- be/src/simd/selector.h
+++ be/src/simd/selector.h
@@ -116,6 +116,12 @@ inline void avx2_select_if_common_implement(uint8_t*& selector, T*& dst, const T
             }
         }
 
+        // Each loop can handle the size of the selection vector,
+        // using int16 as an example,
+        // because each mask outer loop has to handle 32 int16, but since m256 can only handle 16 int16,
+        // we need to use two loops to handle 16 each, so each loop needs to use 0xFFFF to get 16 bits to process.
+
+        // In addition, since data_size is constexpr, the loop here will be expanded by the compiler
         constexpr uint32_t mask_table[] = {0, 0xFFFFFFFF, 0xFFFF, 0, 0xFF, 0, 0, 0, 0x0F, 0, 0, 0, 0, 0, 0, 0, 0x03};
         constexpr uint8_t each_loop_handle_sz = 32 / data_size;
         for (int i = 0; i < data_size; ++i) {
@@ -123,9 +129,19 @@ inline void avx2_select_if_common_implement(uint8_t*& selector, T*& dst, const T
             // how to get mask from load mask
             __m256i select_vector;
             if constexpr (data_size == 2) {
+                // First expand the 16 bits into m256
+                // each bit expand to 4 bytes
+                // 0b0110 -> 0x00 0x00 0x00 0x01 0x00 0x01 0x00 0x00
                 select_vector = _mm256_set1_epi16(select_mask);
+                // set is usually a more costly operation, but since it is a constant,
+                // there is no need to worry about the performance impact
+                // data_mask could help acquire bit at x in locate x
                 const __m256i data_mask = _mm256_setr_epi16(0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x100,
                                                             0x200, 0x400, 0x800, 0x1000, 0x2000, 0x4000, 0x8000);
+                // eg: input vec is (0xabcd), (0xabcd)...x16
+                // data_mask bitwise and input_vec was
+                // (0xabcd and 0x01),(0xabcd and 0x02)...x16
+                // then select_vector was the expended of 16 bits mask
                 select_vector &= data_mask;
                 // AVX/AVX2 couldn't provide a compare not equal
                 // we use compare and NOT instead of it
@@ -133,6 +149,9 @@ inline void avx2_select_if_common_implement(uint8_t*& selector, T*& dst, const T
                 select_vector = ~select_vector;
             } else if constexpr (data_size == 4) {
                 select_vector = _mm256_set1_epi8(select_mask);
+                // the same method as int16
+                // expand 8 bits into m256
+                // each bit expand to 4 bytes
                 // clang-format off
                 const __m256i data_mask = _mm256_setr_epi8(
                         0x00, 0x00, 0x00, 0x01, 
@@ -149,6 +168,8 @@ inline void avx2_select_if_common_implement(uint8_t*& selector, T*& dst, const T
                 select_vector = ~select_vector;
             } else if constexpr (data_size == 8) {
                 select_vector = _mm256_set1_epi8(select_mask);
+                // the same method as int16
+                // each bit expand to 8 bytes
                 // clang-format off
                 const __m256i data_mask = _mm256_setr_epi8(
                         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 
