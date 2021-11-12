ba41ec518136a1c5bbc70abfa335afb7dc0ecc2c
zihe.liu <ziheliu1024@gmail.com>
2021-11-12 10:24:51 +0800

    [pipeline] parallelize cross join operators (#1186)

--- be/src/exec/pipeline/crossjoin/cross_join_context.h
+++ be/src/exec/pipeline/crossjoin/cross_join_context.h
@@ -2,28 +2,48 @@
 
 #pragma once
 
+#include <algorithm>
+#include <atomic>
+
+#include "column/chunk.h"
 #include "column/vectorized_fwd.h"
 
-namespace starrocks {
-namespace pipeline {
+namespace starrocks::pipeline {
+
 class CrossJoinContext {
 public:
-    CrossJoinContext() : _build_chunk(std::make_shared<vectorized::Chunk>()), _right_table_complete(false) {}
+    explicit CrossJoinContext(const int32_t num_right_sinkers)
+            : _num_right_sinkers(num_right_sinkers), _build_chunks(num_right_sinkers) {}
 
-    const vectorized::ChunkPtr& get_build_chunk() { return _build_chunk; }
+    bool is_build_chunk_empty() const {
+        return std::all_of(_build_chunks.begin(), _build_chunks.end(),
+                           [](const vectorized::ChunkPtr& chunk) { return chunk == nullptr || chunk->is_empty(); });
+    }
 
-    void set_build_chunk(const vectorized::ChunkPtr& build_chunk) { _build_chunk = build_chunk; }
+    int32_t num_build_chunks() const { return _num_right_sinkers; }
 
-    bool is_right_complete() { return _right_table_complete.load(std::memory_order_acquire); }
+    vectorized::Chunk* get_build_chunk(int32_t build_id) const { return _build_chunks[build_id].get(); }
 
-    void set_right_complete() { _right_table_complete.store(true, std::memory_order_release); }
+    void set_build_chunk(const int32_t sinker_id, const vectorized::ChunkPtr& build_chunk) {
+        _build_chunks[sinker_id] = build_chunk;
+    }
+
+    void finish_one_right_sinker() { _num_finished_right_sinkers.fetch_add(1, std::memory_order_release); }
+
+    bool is_right_finished() const {
+        return _num_finished_right_sinkers.load(std::memory_order_acquire) == _num_right_sinkers;
+    }
 
 private:
-    // Used in operators to reference right table's datas.
-    vectorized::ChunkPtr _build_chunk;
-    // used in operators to mark that the right table has been constructed.
-    std::atomic<bool> _right_table_complete;
+    const int32_t _num_right_sinkers;
+    // A CrossJoinLeftOperator waits for all the CrossJoinRightSinkOperators to be finished,
+    // and then reads _build_chunks written by the CrossJoinRightSinkOperators.
+    // _num_finished_right_sinkers is used to ensure CrossJoinLeftOperator can see all the parts
+    // of _build_chunks, when it sees all the CrossJoinRightSinkOperators are finished.
+    std::atomic<int32_t> _num_finished_right_sinkers = 0;
+
+    // _build_chunks[i] contains all the rows from i-th CrossJoinRightSinkOperator.
+    std::vector<vectorized::ChunkPtr> _build_chunks;
 };
 
-} // namespace pipeline
-} // namespace starrocks
+} // namespace starrocks::pipeline
