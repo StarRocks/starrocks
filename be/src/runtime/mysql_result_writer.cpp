6e9a1bb3ae19e88b84b8303d3f381c7b5b5a7e37
c1oudman <1027011800@qq.com>
2021-11-17 16:24:09 +0800

    fix format
    Signed-off-by: c1oudman <1027011800@qq.com>

--- be/src/runtime/mysql_result_writer.cpp
+++ be/src/runtime/mysql_result_writer.cpp
@@ -309,8 +309,8 @@ StatusOr<TFetchDataResultPtr> MysqlResultWriter::process_chunk(vectorized::Chunk
     for (int i = 0; i < num_columns; ++i) {
         ColumnPtr column = _output_expr_ctxs[i]->evaluate(chunk);
         column = _output_expr_ctxs[i]->root()->type().type == TYPE_TIME
-                 ? vectorized::ColumnHelper::convert_time_column_from_double_to_str(column)
-                 : column;
+                         ? vectorized::ColumnHelper::convert_time_column_from_double_to_str(column)
+                         : column;
         result_columns.emplace_back(std::move(column));
     }
 
