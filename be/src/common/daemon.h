8331e2f7d9fb4026c71b6881f44e18f1633ae4f0
chaoyli <lichaoyong158@gmail.com>
2021-11-18 15:39:08 +0800

    Make TMalloc GC Thread and CurrentThread can be exited gracefully.
    The TCMalloc GC Thread is not been joined when exited.
    It may be referenced resource already been released.
    The CurrentThread thread local variable is used to
    gather the memory statistics and is defined in ExecEnv.
    Because the exit of ExecEnv is after the StorageEngine.
    So we can check the exit state of StorageEngine to exit
    the CurrentThread as soon as possible.

--- be/src/common/daemon.h
+++ be/src/common/daemon.h
@@ -22,11 +22,29 @@
 #ifndef STARROCKS_BE_SRC_COMMON_COMMON_DAEMON_H
 #define STARROCKS_BE_SRC_COMMON_COMMON_DAEMON_H
 
+#include <atomic>
+#include <thread>
 #include <vector>
 
 #include "storage/options.h"
 
 namespace starrocks {
+
+class Daemon {
+public:
+    Daemon() = default;
+    ~Daemon() = default;
+
+    void init(int argc, char** argv, const std::vector<StorePath>& paths);
+    void stop();
+    bool stopped();
+
+private:
+    std::atomic<bool> _stopped{false};
+
+    std::vector<std::thread> _daemon_threads;
+    DISALLOW_COPY_AND_ASSIGN(Daemon);
+};
 
 // Initialises logging, flags etc. Callers that want to override default gflags
 // variables should do so before calling this method; no logging should be
