c7a52fdf5922457dfdcbc9bb2f200e6df0f3b98c
liuyehcf <1559500551@qq.com>
2021-11-18 19:05:03 +0800

    SinkBuffer replace thread with threadPool (#1320)

--- be/src/exec/pipeline/fragment_executor.cpp
+++ be/src/exec/pipeline/fragment_executor.cpp
@@ -191,7 +191,7 @@ Status FragmentExecutor::prepare(ExecEnv* exec_env, const TExecPlanFragmentParam
                                                                     driver_id++, is_root);
                 driver->set_morsel_queue(morsel_queue.get());
                 auto* scan_operator = down_cast<ScanOperator*>(driver->source_operator());
-                scan_operator->set_io_threads(exec_env->pipeline_io_thread_pool());
+                scan_operator->set_io_threads(exec_env->pipeline_scan_io_thread_pool());
                 setup_profile_hierarchy(runtime_state, driver);
                 drivers.emplace_back(std::move(driver));
             }
