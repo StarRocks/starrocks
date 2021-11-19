eeec8c1bbd718213b8f90bb609f3126cc8f1c799
Youngwb <yangwenbo_mailbox@163.com>
2021-11-19 17:40:33 +0800

    compute Count function column statistics

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/PlanFragmentTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/PlanFragmentTest.java
@@ -2121,7 +2121,6 @@ public class PlanFragmentTest extends PlanTestBase {
         sql = " SELECT c2, count(*) FROM (SELECT t1.c2 as c2 FROM db1.tbl3 as t1 INNER JOIN [shuffle] db1.tbl4 " +
                 "as t2 ON t1.c2=t2.c2 WHERE t1.c1<10) as t3 GROUP BY c2;";
         plan = getFragmentPlan(sql);
-
         Assert.assertEquals(1, StringUtils.countMatches(plan, "AGGREGATE (merge finalize)"));
 
         sql = "SELECT c2, count(*) FROM db1.tbl5 GROUP BY c2;";
