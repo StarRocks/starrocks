2083968b84fbc0819ef1b52cb3f55ffffdeb4990
yanz <dirtysalt1987@gmail.com>
2021-11-23 14:19:31 +0800

    fix bug

--- be/src/exec/exec_node.cpp
+++ be/src/exec/exec_node.cpp
@@ -711,6 +711,8 @@ void ExecNode::eval_filter_null_values(vectorized::Chunk* chunk) {
             sel[i] &= (1 - bits[i]);
         }
     }
+    if (selection.size() == 0) return;
+
     size_t after_size = SIMD::count_zero(selection);
     if (after_size != before_size) {
         VLOG_FILE << "filter null values. before_size = " << before_size << ", after_size = " << after_size;
