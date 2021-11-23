3cdf74a5e51fc1373fe841ff81733af4e5d5689b
Youngwb <yangwenbo_mailbox@163.com>
2021-11-23 20:35:58 +0800

    Compute case when operator and if function column statistics (#1472)

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/DistributedEnvPlanWithCostTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/DistributedEnvPlanWithCostTest.java
@@ -430,7 +430,7 @@ public class DistributedEnvPlanWithCostTest extends DistributedEnvPlanTestBase {
         sql = "SELECT COUNT(*)  FROM lineitem JOIN orders ON l_orderkey * 2 = o_orderkey + 1  GROUP BY l_shipmode, l_shipinstruct, o_orderdate, o_orderstatus;";
         plan = getCostExplain(sql);
         Assert.assertTrue(plan.contains("equal join conjunct: [29: multiply, BIGINT, true] = [30: add, BIGINT, true]\n" +
-                "  |  cardinality: 600000000"));
+                        "  |  cardinality: 600000000"));
     }
 
     @Test
@@ -581,5 +581,44 @@ public class DistributedEnvPlanWithCostTest extends DistributedEnvPlanTestBase {
         Assert.assertTrue(execPlan.getFragments().get(1).getPlanRoot() instanceof AggregationNode);
         AggregationNode aggregationNode = (AggregationNode) execPlan.getFragments().get(1).getPlanRoot();
         Assert.assertTrue(aggregationNode.isColocate());
+    }
+
+    @Test
+    public void testCaseWhenCardinalityEstimate() throws Exception {
+        String sql = "select (case when `O_ORDERKEY` = 0 then 'ALGERIA' when `O_ORDERKEY` = 1 then 'ARGENTINA' " +
+                "else 'others' end) a from orders group by 1";
+        String plan = getCostExplain(sql);
+        Assert.assertTrue(plan.contains("cardinality: 3"));
+        Assert.assertTrue(plan.contains("* case-->[-Infinity, Infinity, 0.0, 16.0, 3.0]"));
+
+        sql = "select (case when `O_ORDERKEY` = 0 then 'ALGERIA' when `O_ORDERKEY` = 1 then 'ARGENTINA' end) a " +
+                "from orders group by 1";
+        plan = getCostExplain(sql);
+        Assert.assertTrue(plan.contains("cardinality: 2"));
+        Assert.assertTrue(plan.contains("* case-->[-Infinity, Infinity, 0.0, 16.0, 2.0]"));
+
+        sql = "select (case when `O_ORDERKEY` = 0 then O_ORDERSTATUS when `O_ORDERKEY` = 1 then 'ARGENTINA' " +
+                "else 'other' end) a from orders group by 1";
+        plan = getCostExplain(sql);
+        Assert.assertTrue(plan.contains("* case-->[-Infinity, Infinity, 0.0, 16.0, 5.0] ESTIMATE"));
+    }
+
+    @Test
+    public void testIFFunctionCardinalityEstimate() throws Exception {
+        String sql = "select (case when `O_ORDERKEY` = 0 then 'ALGERIA' else 'others' end) a from orders group by 1";
+        String plan = getCostExplain(sql);
+        Assert.assertTrue(plan.contains("* case-->[-Infinity, Infinity, 0.0, 16.0, 2.0] ESTIMATE"));
+
+        sql = "select if(`O_ORDERKEY` = 0, 'ALGERIA', 'others') a from orders group by 1";
+        plan = getCostExplain(sql);
+        Assert.assertTrue(plan.contains("* if-->[-Infinity, Infinity, 0.0, 16.0, 2.0] ESTIMATE"));
+
+        sql = "select if(`O_ORDERKEY` = 0, 'ALGERIA', if (`O_ORDERKEY` = 1, 'ARGENTINA', 'others')) a from orders group by 1";
+        plan = getCostExplain(sql);
+        Assert.assertTrue(plan.contains("* if-->[-Infinity, Infinity, 0.0, 16.0, 3.0] ESTIMATE"));
+
+        sql = "select if(`O_ORDERKEY` = 0, 'ALGERIA', if (`O_ORDERKEY` = 1, 'ARGENTINA', if(`O_ORDERKEY` = 2, 'BRAZIL', 'Others'))) a from orders group by 1";
+        plan = getCostExplain(sql);
+        Assert.assertTrue(plan.contains("* if-->[-Infinity, Infinity, 0.0, 16.0, 4.0] ESTIMATE"));
     }
 }
