444d4ab891fbf84f133213ef089972fd2efd4a9a
ZiheLiu <ziheliu1024@gmail.com>
2021-11-26 15:00:56 +0800

    update

--- be/src/runtime/data_stream_recvr.cc
+++ be/src/runtime/data_stream_recvr.cc
@@ -344,19 +344,23 @@ Status DataStreamRecvr::SenderQueue::_add_chunks_internal(const PTransmitChunkPa
         std::unique_lock<std::mutex> l(_lock);
         wait_timer.stop();
 
-        if (!_is_cancelled) {
-            for (auto& pair : chunks) {
-                _chunk_queue.emplace_back(std::move(pair));
-            }
-            // if done is nullptr, this function can't delay this response
-            if (done != nullptr && _recvr->exceeds_limit(total_chunk_bytes)) {
-                MonotonicStopWatch monotonicStopWatch;
-                DCHECK(*done != nullptr);
-                _pending_closures.emplace_back(*done, monotonicStopWatch);
-                *done = nullptr;
-            }
-            _recvr->_num_buffered_bytes += total_chunk_bytes;
+        // _is_cancelled may be modified after checking _is_cancelled above,
+        // because lock is release temporarily when deserializing chunk.
+        if (_is_cancelled) {
+            return Status::OK();
         }
+
+        for (auto& pair : chunks) {
+            _chunk_queue.emplace_back(std::move(pair));
+        }
+        // if done is nullptr, this function can't delay this response
+        if (done != nullptr && _recvr->exceeds_limit(total_chunk_bytes)) {
+            MonotonicStopWatch monotonicStopWatch;
+            DCHECK(*done != nullptr);
+            _pending_closures.emplace_back(*done, monotonicStopWatch);
+            *done = nullptr;
+        }
+        _recvr->_num_buffered_bytes += total_chunk_bytes;
     }
     cb();
     return Status::OK();
