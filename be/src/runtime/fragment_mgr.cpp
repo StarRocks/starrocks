6aa65290ba30096a5aded343558d28ef953f8e4d
trueeyu <lxhhust350@qq.com>
2021-11-27 16:59:01 +0800

    Add comment

--- be/src/runtime/fragment_mgr.cpp
+++ be/src/runtime/fragment_mgr.cpp
@@ -446,6 +446,8 @@ Status FragmentMgr::exec_plan_fragment(const TExecPlanFragmentParams& params, co
         }
         return Status::InternalError(error_msg);
     } else {
+        // It is necessary to ensure that ExecState is not destructed,
+        // so do not reset ExecState before calling start_cb, otherwise "heap use after free" may appear
         start_cb(exec_state->executor());
     }
 
