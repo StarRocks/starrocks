f86231dfc66673e4d79bd8bd98a74361ea5e5382
stephen <91597003+stephen-shelby@users.noreply.github.com>
2021-11-29 19:08:32 +0800

    Fix display rollup information incorrect (#1621)
    The information is not right when using show alter table rollup. The value is not corresponds to the tab bar.
    The pull request adjust the position to make it be right.

--- fe/fe-core/src/main/java/com/starrocks/analysis/ShowAlterStmt.java
+++ fe/fe-core/src/main/java/com/starrocks/analysis/ShowAlterStmt.java
@@ -254,9 +254,9 @@ public class ShowAlterStmt extends ShowStmt {
         ShowResultSetMetaData.Builder builder = ShowResultSetMetaData.builder();
 
         ImmutableList<String> titleNames = null;
-        if (type == AlterType.ROLLUP) {
+        if (type == AlterType.ROLLUP || type == AlterType.MATERIALIZED_VIEW) {
             titleNames = RollupProcDir.TITLE_NAMES;
-        } else if (type == AlterType.COLUMN || type == AlterType.MATERIALIZED_VIEW) {
+        } else if (type == AlterType.COLUMN) {
             titleNames = SchemaChangeProcDir.TITLE_NAMES;
         }
 
