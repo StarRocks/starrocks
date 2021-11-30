92c9ac1e11e7f73c884cd418a57c6d846d17d581
stdpain <34912776+stdpain@users.noreply.github.com>
2021-11-30 11:17:18 +0800

    Support mysql table sink in BE (#1581)

--- be/src/runtime/mysql_table_writer.h
+++ be/src/runtime/mysql_table_writer.h
@@ -23,9 +23,15 @@
 #define STARROCKS_BE_RUNTIME_MYSQL_TABLE_WRITER_H
 
 #include <string>
+#include <string_view>
+#include <variant>
 #include <vector>
 
+#include "column/binary_column.h"
+#include "column/column_viewer.h"
+#include "column/vectorized_fwd.h"
 #include "common/status.h"
+#include "fmt/format.h"
 
 #ifndef __StarRocksMysql
 #define __StarRocksMysql void
@@ -43,13 +49,37 @@ struct MysqlConnInfo {
     std::string debug_string() const;
 };
 
-class RowBatch;
-class TupleRow;
 class ExprContext;
+
+#define APPLY_FOR_ALL_PRIMITIVE_TYPE(M) \
+    M(TYPE_TINYINT)                     \
+    M(TYPE_SMALLINT)                    \
+    M(TYPE_INT)                         \
+    M(TYPE_BIGINT)                      \
+    M(TYPE_LARGEINT)                    \
+    M(TYPE_FLOAT)                       \
+    M(TYPE_DOUBLE)                      \
+    M(TYPE_VARCHAR)                     \
+    M(TYPE_CHAR)                        \
+    M(TYPE_DATE)                        \
+    M(TYPE_DATETIME)                    \
+    M(TYPE_DECIMALV2)                   \
+    M(TYPE_DECIMAL32)                   \
+    M(TYPE_DECIMAL64)                   \
+    M(TYPE_DECIMAL128)                  \
+    M(TYPE_BOOLEAN)
+
+#define APPLY_FOR_ALL_NULL_TYPE(N) M(TYPE_NULL)
 
 class MysqlTableWriter {
 public:
-    MysqlTableWriter(const std::vector<ExprContext*>& output_exprs);
+    using VariantViewer = std::variant<
+#define M(NAME) vectorized::ColumnViewer<NAME>,
+            APPLY_FOR_ALL_PRIMITIVE_TYPE(M)
+#undef M
+                    vectorized::ColumnViewer<TYPE_NULL>>;
+
+    MysqlTableWriter(const std::vector<ExprContext*>& output_exprs, int batch_size);
     ~MysqlTableWriter();
 
     // connnect to mysql server
@@ -57,18 +87,25 @@ public:
 
     Status begin_trans() { return Status::OK(); }
 
-    Status append(RowBatch* batch);
+    Status append(vectorized::Chunk* chunk);
 
     Status abort_tarns() { return Status::OK(); }
 
     Status finish_tarns() { return Status::OK(); }
 
 private:
-    Status insert_row(TupleRow* row);
+    Status _build_viewers(vectorized::Columns& columns);
+    Status _build_insert_sql(int from, int to, std::string_view* sql);
 
     const std::vector<ExprContext*>& _output_expr_ctxs;
+    std::vector<VariantViewer> _viewers;
+
+    fmt::memory_buffer _stmt_buffer;
+    std::string _escape_buffer;
+
     std::string _mysql_tbl;
     __StarRocksMysql* _mysql_conn;
+    int _batch_size;
 };
 
 } // namespace starrocks
