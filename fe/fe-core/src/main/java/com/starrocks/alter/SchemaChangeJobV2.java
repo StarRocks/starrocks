b3dd2994305c326a206a2a729baab3b791822a91
zhangqiang <953361637@qq.com>
2021-12-01 22:13:11 +0800

    BugFix: fix error shadow column name upon upgrade (#1732) (#1764)
    Before StarRocks-1.18, StarRocks use __doris_shadow_ prefix to do schema change.
    After StarRocks-1.18, StarRocks use __starrocks_shadow_ prefix to do schema change.
    StarRocks after version 1.18 can't identify __doris_shadow_ prefix and will handle it as normal column name.
    Add __doris_shadow_ prefix check to fix it

--- fe/fe-core/src/main/java/com/starrocks/alter/SchemaChangeJobV2.java
+++ fe/fe-core/src/main/java/com/starrocks/alter/SchemaChangeJobV2.java
@@ -818,7 +818,7 @@ public class SchemaChangeJobV2 extends AlterJobV2 {
             info.add(TimeUtils.longToTimeString(createTimeMs));
             info.add(TimeUtils.longToTimeString(finishedTimeMs));
             // only show the origin index name
-            info.add(indexIdToName.get(shadowIndexId).substring(SchemaChangeHandler.SHADOW_NAME_PRFIX.length()));
+            info.add(Column.removeNamePrefix(indexIdToName.get(shadowIndexId)));
             info.add(shadowIndexId);
             info.add(entry.getValue());
             info.add(indexSchemaVersionAndHashMap.get(shadowIndexId).toString());
