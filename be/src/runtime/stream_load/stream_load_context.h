064d81d2f4a54a415ef62f06e8855883f4c28c3a
trueeyu <lxhhust350@qq.com>
2021-12-02 11:13:12 +0800

    add comment

--- be/src/runtime/stream_load/stream_load_context.h
+++ be/src/runtime/stream_load/stream_load_context.h
@@ -113,9 +113,12 @@ public:
     bool unref() { return _refs.fetch_sub(1) == 1; }
 
 public:
-    // Before the stream load receiving thread exits, Fragment may have been destructed.
+    // 1) Before the stream load receiving thread exits, Fragment may have been destructed.
     // At this time, mem_tracker may have been destructed,
     // so add shared_ptr here to prevent this from happening.
+    //
+    // 2) query_mem_tracker is the parent of instance_mem_tracker
+    // runtime_profile will be used by [consumption] of mem_tracker to record peak memory
     std::shared_ptr<RuntimeProfile> runtime_profile;
     std::shared_ptr<MemTracker> query_mem_tracker;
     std::shared_ptr<MemTracker> instance_mem_tracker;
