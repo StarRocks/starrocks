b287175dabb4940712935211dce511c6896e511d
liuyehcf <1559500551@qq.com>
2021-12-04 13:56:28 +0800

    Setup plan_node_id for ExchangeSink/SourceOperator and LocalExchangeSink/SourceOperator (#1798)

--- be/src/exec/pipeline/result_sink_operator.h
+++ be/src/exec/pipeline/result_sink_operator.h
@@ -72,9 +72,9 @@ private:
 
 class ResultSinkOperatorFactory final : public OperatorFactory {
 public:
-    ResultSinkOperatorFactory(int32_t id, int32_t plan_node_id, TResultSinkType::type sink_type,
-                              std::vector<TExpr> t_output_expr, FragmentContext* const fragment_ctx)
-            : OperatorFactory(id, "result_sink", plan_node_id),
+    ResultSinkOperatorFactory(int32_t id, TResultSinkType::type sink_type, std::vector<TExpr> t_output_expr,
+                              FragmentContext* const fragment_ctx)
+            : OperatorFactory(id, "result_sink", Operator::s_pseudo_plan_node_id_for_result_sink),
               _sink_type(sink_type),
               _t_output_expr(std::move(t_output_expr)),
               _fragment_ctx(fragment_ctx) {}
