55e6c042969bef0769dcd6ca86b94160d45a1504
zihe.liu <ziheliu1024@gmail.com>
2021-12-06 18:52:22 +0800

    [pipeline] add eval_filter_null_values and insert LimitOperator after HashJoinProbeOperator (#1758)

--- be/src/exec/vectorized/hash_joiner.h
+++ be/src/exec/vectorized/hash_joiner.h
@@ -106,6 +106,10 @@ public:
             _phase.compare_exchange_strong(old_phase, HashJoinPhase::EOS);
         }
     }
+    void enter_eos_phase() {
+        _phase = HashJoinPhase::EOS;
+        _ht.close();
+    }
     // build phase
     Status append_chunk_to_ht(RuntimeState* state, const ChunkPtr& chunk);
     Status build_ht(RuntimeState* state);
