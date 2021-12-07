90b1dd6535fb0674826e7b96308af65185f81458
Youngwb <yangwenbo_mailbox@163.com>
2021-12-07 20:53:35 +0800

    Fix physical value node equality (#1931)

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/PlanFragmentTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/PlanFragmentTest.java
@@ -4818,4 +4818,22 @@ public class PlanFragmentTest extends PlanTestBase {
                 "  |  <slot 10> : 0\n" +
                 "  |  \n"));
     }
+
+    @Test
+    public void testEmptyNodeWithJoin() throws Exception {
+        // check no exception
+        String sql = "SELECT\n" +
+                "        subq_0.c3, ref_2.id_datetime        \n" +
+                "FROM (\n" +
+                "        SELECT\n" +
+                "                ref_0.id_date AS c3\n" +
+                "        FROM\n" +
+                "                test_all_type AS ref_0 WHERE FALSE) AS subq_0\n" +
+                "        INNER JOIN test_all_type AS ref_1 ON (subq_0.c3 = ref_1.id_date)\n" +
+                "        INNER JOIN test_all_type AS ref_2 ON (subq_0.c3 = ref_2.id_datetime)\n" +
+                "WHERE\n" +
+                "        ref_2.t1a >= ref_1.t1a";
+        String plan = getFragmentPlan(sql);
+        System.out.println(plan);
+    }
 }
