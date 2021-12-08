15aa1d953cbb96a56a3571891e790458ca51329d
trueeyu <lxhhust350@qq.com>
2021-12-08 10:32:15 +0800

    Add more check point for large mem allocate (#1902)

--- be/src/exec/vectorized/topn_node.cpp
+++ be/src/exec/vectorized/topn_node.cpp
@@ -144,7 +144,6 @@ Status TopNNode::_consume_chunks(RuntimeState* state, ExecNode* child) {
     bool eos = false;
     _chunks_sorter->setup_runtime(runtime_profile(), "ChunksSorter");
     do {
-        RETURN_IF_ERROR(state->check_mem_limit("Sort"));
         RETURN_IF_CANCELLED(state);
         ChunkPtr chunk;
         timer.stop();
@@ -156,10 +155,20 @@ Status TopNNode::_consume_chunks(RuntimeState* state, ExecNode* child) {
         if (chunk != nullptr && chunk->num_rows() > 0) {
             ChunkPtr materialize_chunk = ChunksSorter::materialize_chunk_before_sort(
                     chunk.get(), _materialized_tuple_desc, _sort_exec_exprs, _order_by_types);
-            RETURN_IF_ERROR(_chunks_sorter->update(state, materialize_chunk));
+            try {
+                RETURN_IF_ERROR(_chunks_sorter->update(state, materialize_chunk));
+                RETURN_IF_ERROR(state->check_mem_limit("Sort"));
+            } catch (std::bad_alloc const&) {
+                return Status::MemoryLimitExceeded("Mem usage has exceed the limit of BE");
+            }
         }
     } while (!eos);
-    RETURN_IF_ERROR(_chunks_sorter->done(state));
+    try {
+        RETURN_IF_ERROR(_chunks_sorter->done(state));
+        RETURN_IF_ERROR(state->check_mem_limit("Sort"));
+    } catch (std::bad_alloc const&) {
+        return Status::MemoryLimitExceeded("Mem usage has exceed the limit of BE");
+    }
     return Status::OK();
 }
 
