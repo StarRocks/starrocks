d65cf3e3d12e03815ae738bb30a7d1e036a1543c
wyb <wybb86@gmail.com>
2021-12-09 14:06:00 +0800

    [Vertical compaction 5/5] Support duplicate, unique and aggregate key table (#1450)
    Choose compaction algorithm according to tablet schema, max columns per group, and segment iterator num.
    The key columns will be merged and written into the rowset segments in vertical compaction.
    Then the non-key columns are merged by groups.

--- be/test/storage/vectorized/row_source_mask_test.cpp
+++ be/test/storage/vectorized/row_source_mask_test.cpp
@@ -70,7 +70,7 @@ TEST_F(RowSourceMaskTest, memory_masks) {
     buffer.flush();
 
     // --- read ---
-    buffer.flip();
+    buffer.flip_to_read();
     auto st = buffer.has_remaining();
     ASSERT_TRUE(st.ok());
     ASSERT_TRUE(st.value());
@@ -115,7 +115,7 @@ TEST_F(RowSourceMaskTest, memory_masks) {
     ASSERT_FALSE(st.value());
 
     // --- read again and check has same source ---
-    buffer.flip();
+    buffer.flip_to_read();
     ASSERT_TRUE(buffer.has_remaining().value());
     mask = buffer.current();
     ASSERT_EQ(0, mask.get_source_num());
@@ -151,7 +151,7 @@ TEST_F(RowSourceMaskTest, memory_masks_with_persistence) {
     buffer.flush();
 
     // --- read ---
-    buffer.flip();
+    buffer.flip_to_read();
     auto st = buffer.has_remaining();
     ASSERT_TRUE(st.ok());
     ASSERT_TRUE(st.value());
@@ -196,7 +196,7 @@ TEST_F(RowSourceMaskTest, memory_masks_with_persistence) {
     ASSERT_FALSE(st.value());
 
     // --- read again and check has same source ---
-    buffer.flip();
+    buffer.flip_to_read();
     ASSERT_TRUE(buffer.has_remaining().value());
     mask = buffer.current();
     ASSERT_EQ(0, mask.get_source_num());
