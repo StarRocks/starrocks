a5bb5457707247223a187715e70e356088cca8a1
HangyuanLiu <460660596@qq.com>
2021-12-09 14:08:11 +0800

    Unify RelationFields and OutputScope in Relation to Scope (#1971)
    * commit-1
    
    * rename QuerySpecification => SelectRelation

--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/relation/ExceptRelation.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/relation/ExceptRelation.java
@@ -2,16 +2,14 @@
 package com.starrocks.sql.analyzer.relation;
 
 import com.starrocks.analysis.Expr;
-import com.starrocks.sql.analyzer.Scope;
 import com.starrocks.sql.optimizer.base.SetQualifier;
 
 import java.util.List;
 
 public class ExceptRelation extends SetOperationRelation {
 
-    public ExceptRelation(List<QueryRelation> relations, SetQualifier qualifier, List<Expr> outputExpressions,
-                          Scope outputScope) {
-        super(relations, qualifier, outputExpressions, outputScope);
+    public ExceptRelation(List<QueryRelation> relations, SetQualifier qualifier, List<Expr> outputExpressions) {
+        super(relations, qualifier, outputExpressions);
     }
 
     public <R, C> R accept(RelationVisitor<R, C> visitor, C context) {
