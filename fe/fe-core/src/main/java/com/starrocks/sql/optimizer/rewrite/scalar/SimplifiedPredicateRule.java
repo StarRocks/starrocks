6d987d83f7731d15a07599d758c0b51d28fdd2e5
Seaven <seaven_7@qq.com>
2021-12-10 14:26:22 +0800

    If function support time type

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rewrite/scalar/SimplifiedPredicateRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rewrite/scalar/SimplifiedPredicateRule.java
@@ -2,7 +2,6 @@
 
 package com.starrocks.sql.optimizer.rewrite.scalar;
 
-import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.starrocks.analysis.Expr;
@@ -70,7 +69,9 @@ public class SimplifiedPredicateRule extends BottomUpScalarOperatorRewriteRule {
         Type[] argTypes = args.stream().map(ScalarOperator::getType).toArray(Type[]::new);
         Function fn = Expr.getBuiltinFunction(FunctionSet.IF, argTypes, Function.CompareMode.IS_IDENTICAL);
 
-        Preconditions.checkState(fn != null);
+        if (fn == null) {
+            return operator;
+        }
         if (operator.getChildren().stream().anyMatch(s -> s.getType().isDecimalV3())) {
             Function decimalFn = ScalarFunction.createVectorizedBuiltin(fn.getId(), fn.getFunctionName().getFunction(),
                     Arrays.stream(argTypes).collect(Collectors.toList()), fn.hasVarArgs(), operator.getType());
