e18e61f652c96c4ec7f657fede4962c74305b829
sevev <953361637@qq.com>
2021-12-13 19:10:05 +0800

    code style

--- be/src/storage/rowset/segment_v2/storage_page_decoder.h
+++ be/src/storage/rowset/segment_v2/storage_page_decoder.h
@@ -9,7 +9,6 @@
 #include "storage/rowset/segment_v2/common.h"
 #include "util/slice.h"
 
-
 namespace starrocks {
 
 namespace segment_v2 {
@@ -27,37 +26,6 @@ public:
                                     std::unique_ptr<char[]>* page, Slice* page_slice) {
         return Status::OK();
     }
-};
-
-class BitShuffleDataDecoder : public DataDecoder {
-public:
-    BitShuffleDataDecoder() = default;
-    ~BitShuffleDataDecoder() = default;
-
-    void reserve_head(uint8_t head_size) override {
-        DCHECK(_reserve_head_size == 0);
-        _reserve_head_size = head_size;
-    }
-    Status decode_page_data(PageFooterPB* footer, uint32_t footer_size, EncodingTypePB encoding,
-                            std::unique_ptr<char[]>* page, Slice* page_slice) override;
-
-private:
-    uint8_t _reserve_head_size = 0;
-};
-
-class BinaryDictDataDecoder : public DataDecoder {
-public:
-    BinaryDictDataDecoder() {
-        _bit_shuffle_decoder = std::make_unique<BitShuffleDataDecoder>();
-        _bit_shuffle_decoder->reserve_head(BINARY_DICT_PAGE_HEADER_SIZE);
-    }
-    ~BinaryDictDataDecoder() = default;
-
-    Status decode_page_data(PageFooterPB* footer, uint32_t footer_size, EncodingTypePB encoding,
-                            std::unique_ptr<char[]>* page, Slice* page_slice) override;
-
-private:
-    std::unique_ptr<BitShuffleDataDecoder> _bit_shuffle_decoder;
 };
 
 class StoragePageDecoder {
