b747186713e8efd1b844543250d0a7feff2b4b63
ricky <rickif@qq.com>
2021-12-18 17:47:04 +0800

    Reduce overhead of casting type(int->string) when loading json file (#1782)
    Summary: this PR is to improve the performance of loading JSON by removing the extra casting.

--- be/src/exprs/vectorized/json_functions.h
+++ be/src/exprs/vectorized/json_functions.h
@@ -107,8 +107,6 @@ public:
 
     static void parse_json_paths(const std::string& path_strings, std::vector<JsonPath>* parsed_paths);
 
-    static Status minify_json_to_string(simdjson::ondemand::value& val, std::unique_ptr<char[]>& buf, size_t& buflen);
-
 private:
     static Status _get_parsed_paths(const std::vector<std::string>& path_exprs, std::vector<JsonPath>* parsed_paths);
 
