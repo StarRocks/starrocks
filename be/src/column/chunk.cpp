fe8d8d2283138179c7f07487b3f490ee4a84485e
yanz <dirtysalt1987@gmail.com>
2021-12-18 10:03:00 +0800

    fix comment

--- be/src/column/chunk.cpp
+++ be/src/column/chunk.cpp
@@ -214,9 +214,8 @@ Status Chunk::deserialize(const uint8_t* src, size_t len, const RuntimeChunkMeta
     // `len` and `expected` are both "estimated" serialized size. it could be larger than real serialized size.
     // `serialized_size` and `read_size` are both "real" serialized size. it's exactly how much bytes are written into buffer.
     // For some object column types like bitmap/hll/percentile, "estimated" and "real" are not always the same.
-    // And fr bitmap, sometimes `len` and `expected` are different. So to fix that problem, we fallback to compare "real" serialized size.
+    // And for bitmap, sometimes `len` and `expected` are different. So to fix that problem, we fallback to compare "real" serialized size.
 
-    // kks gives a proposal, and I think it's a truly great idea.
     // We compare "real" serialized size first. It may fails because of backward compatibility. For old version of BE,
     // there is no "serialized_size" this field(which means the value is zero), and we fallback to compare "estimated" serialized size.
     // And for new version of BE, the "real" serialized size always matches, and we can save the cost of calling `serialzied_size`.
