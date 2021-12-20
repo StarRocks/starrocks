99ebc12188761867f5bc280bf3bab2827eb8aee5
trueeyu <lxhhust350@qq.com>
2021-12-20 18:06:52 +0800

    Add more catch for std::bad_alloc

--- be/src/exec/pipeline/aggregate/aggregate_blocking_sink_operator.cpp
+++ be/src/exec/pipeline/aggregate/aggregate_blocking_sink_operator.cpp
@@ -2,6 +2,8 @@
 
 #include "aggregate_blocking_sink_operator.h"
 
+#include "runtime/current_thread.h"
+
 namespace starrocks::pipeline {
 
 Status AggregateBlockingSinkOperator::prepare(RuntimeState* state) {
@@ -64,10 +66,11 @@ Status AggregateBlockingSinkOperator::push_chunk(RuntimeState* state, const vect
     if (!_aggregator->is_none_group_by_exprs()) {
         if (false) {
         }
-#define HASH_MAP_METHOD(NAME)                                                                          \
-    else if (_aggregator->hash_map_variant().type == vectorized::HashMapVariant::Type::NAME)           \
-            _aggregator->build_hash_map<decltype(_aggregator->hash_map_variant().NAME)::element_type>( \
-                    *_aggregator->hash_map_variant().NAME, chunk_size, agg_group_by_with_limit);
+#define HASH_MAP_METHOD(NAME)                                                                                          \
+    else if (_aggregator->hash_map_variant().type == vectorized::HashMapVariant::Type::NAME) {                         \
+        TRY_CATCH_BAD_ALLOC(_aggregator->build_hash_map<decltype(_aggregator->hash_map_variant().NAME)::element_type>( \
+                *_aggregator->hash_map_variant().NAME, chunk_size, agg_group_by_with_limit));                          \
+    }
         APPLY_FOR_VARIANT_ALL(HASH_MAP_METHOD)
 #undef HASH_MAP_METHOD
 
