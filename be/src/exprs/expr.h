e2ca4eeff61b1e167efb08827dacfb41d527737a
satanson <ranpanf@gmail.com>
2021-12-21 17:05:12 +0800

    [ISSUE-2319] Bugfix: concurrent assignments of shared_ptr Expr::_constant_column (#2324)

--- be/src/exprs/expr.h
+++ be/src/exprs/expr.h
@@ -279,6 +279,7 @@ protected:
     /// doesn't call RegisterFunctionContext().
     int _fn_context_index;
 
+    std::once_flag _constant_column_evaluate_once;
     ColumnPtr _constant_column;
 
     /// Simple debug string that provides no expr subclass-specific information
