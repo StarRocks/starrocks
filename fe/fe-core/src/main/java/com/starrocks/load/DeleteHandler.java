9dae3e062604a48e115a054ee8921b51b4208d83
gengjun-git <gengjun@dorisdb.com>
2021-12-21 15:37:25 +0800

    fix

--- fe/fe-core/src/main/java/com/starrocks/load/DeleteHandler.java
+++ fe/fe-core/src/main/java/com/starrocks/load/DeleteHandler.java
@@ -744,6 +744,10 @@ public class DeleteHandler implements Writable {
         if (deleteInfo == null) {
             return;
         }
+        if ((System.currentTimeMillis() - deleteInfo.getCreateTimeMs()) / 1000 > Config.label_keep_max_second) {
+            LOG.info("delete info outdated, create time: {}, ignore", deleteInfo.getCreateTimeMs());
+            return;
+        }
         long dbId = deleteInfo.getDbId();
         LOG.info("replay delete, dbId {}", dbId);
         updateTableDeleteInfo(catalog, dbId, deleteInfo.getTableId());
@@ -760,6 +764,10 @@ public class DeleteHandler implements Writable {
     public void replayMultiDelete(MultiDeleteInfo deleteInfo, Catalog catalog) {
         // add to deleteInfos
         if (deleteInfo == null) {
+            return;
+        }
+        if ((System.currentTimeMillis() - deleteInfo.getCreateTimeMs()) / 1000 > Config.label_keep_max_second) {
+            LOG.info("delete info outdated, create time: {}, ignore", deleteInfo.getCreateTimeMs());
             return;
         }
         long dbId = deleteInfo.getDbId();
@@ -795,7 +803,20 @@ public class DeleteHandler implements Writable {
     }
 
     public static DeleteHandler read(DataInput in) throws IOException {
-        String json = Text.readString(in);
+        String json;
+        try {
+            json = Text.readString(in);
+
+            // In older versions of fe, the information in the deleteHandler is not cleaned up,
+            // and if there are many delete statements, it will cause an int overflow
+            // and report an IllegalArgumentException.
+            //
+            // dbToDeleteInfos is only used to record history delete info,
+            // discarding it doesn't make much of a difference
+        } catch (IllegalArgumentException e) {
+            LOG.warn("read delete handler json string failed, ignore", e);
+            return new DeleteHandler();
+        }
         return GsonUtils.GSON.fromJson(json, DeleteHandler.class);
     }
 
