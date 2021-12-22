d06f97f4947573d2a48cfaa55e218459bbf7393e
liuyehcf <1559500551@qq.com>
2021-12-22 19:07:09 +0800

    Fix the problem of 'Fail to report exec state' (#2363)

--- be/src/exec/pipeline/pipeline_driver_dispatcher.cpp
+++ be/src/exec/pipeline/pipeline_driver_dispatcher.cpp
@@ -160,8 +160,13 @@ void GlobalDriverDispatcher::report_exec_state(FragmentContext* fragment_ctx, co
     auto report_task = [=]() {
         auto status = ExecStateReporter::report_exec_status(params, exec_env, fe_addr);
         if (!status.ok()) {
-            LOG(WARNING) << "[Driver] Fail to report exec state: fragment_instance_id=" << print_id(fragment_id)
-                         << ", status: " << status.to_string();
+            if (status.is_not_found()) {
+                LOG(INFO) << "[Driver] Fail to report exec state due to query not found: fragment_instance_id="
+                          << print_id(fragment_id);
+            } else {
+                LOG(WARNING) << "[Driver] Fail to report exec state: fragment_instance_id=" << print_id(fragment_id)
+                             << ", status: " << status.to_string();
+            }
         } else {
             LOG(INFO) << "[Driver] Succeed to report exec state: fragment_instance_id=" << print_id(fragment_id);
         }
