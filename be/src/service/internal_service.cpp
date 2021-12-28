382dc9a35d128b477d076e21672603fb4fb7a59a
sduzh <zhuming9011@qq.com>
2021-12-28 14:36:29 +0800

    typo

--- be/src/service/internal_service.cpp
+++ be/src/service/internal_service.cpp
@@ -117,7 +117,7 @@ void PInternalServiceImpl<T>::transmit_runtime_filter(google::protobuf::RpcContr
                                                       PTransmitRuntimeFilterResult* response,
                                                       google::protobuf::Closure* done) {
     VLOG_FILE << "transmit runtime filter: fragment_instance_id=" << print_id(request->finst_id())
-              << " query_id=" << print_id(request->query_id()) << ", is_partital=" << request->is_partial()
+              << " query_id=" << print_id(request->query_id()) << ", is_partial=" << request->is_partial()
               << ", filter_id=" << request->filter_id() << ", is_pipeline=" << request->is_pipeline();
     ClosureGuard closure_guard(done);
     _exec_env->runtime_filter_worker()->receive_runtime_filter(*request);
