a0d2c2d510d53572640af9bd970c0cf7a44188a6
mofei <mofei@starrocks.com>
2021-12-28 18:32:48 +0800

    fix license

--- fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzeCTETest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzeCTETest.java
@@ -1,3 +1,4 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021 StarRocks Limited.
 package com.starrocks.sql.analyzer;
 
 import com.starrocks.sql.analyzer.relation.QueryRelation;
