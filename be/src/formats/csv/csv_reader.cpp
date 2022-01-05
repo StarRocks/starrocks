a388e62674334b125790280e87f5e10185060dd7
zhoukang <zhoukang199191@gmail.com>
2022-01-05 12:26:50 +0800

    Add support for org.apache.hadoop.mapred.TextInputFormat especially for CSV (#1999)
    Close #1873 
    Add support for TextInputFormat, 
    ```
    CREATE EXTERNAL RESOURCE "hive0" PROPERTIES (   "type" = "hive",   "hive.metastore.uris" = "thrift://xxxx:9083" );
    
    CREATE EXTERNAL TABLE `csv_test` (
      `countryid` int NULL,
      `countryname` string NULL,
      `capital` string NULL,
      `population` string NULL
    ) ENGINE=HIVE
    PROPERTIES (
      "resource" = "hive0",
      "database" = "default",
      "table" = "countries_list"
    );
    
    MySQL [hive_test]> select population from csv_test limit 2;
    +------------+
    | population |
    +------------+
    | 328 |
    | 80         |
    +------------+
    MySQL [hive_test]> select * from csv_test;
    +-----------+-------------+---------+------------+
    | countryid | countryname | capital | population |
    +-----------+-------------+---------+------------+
    |         0 | China       | BeiJing | 80         |
    |         1 | USA         | Wash    | 328        |
    |         0 | China       | BeiJing | 80         |
    |         1 | USA         | Wash    | 328        |
    +-----------+-------------+---------+------------+
    MySQL [hive_test]> select distinct(countryid) from csv_test;
    +-----------+
    | countryid |
    +-----------+
    |         0 |
    |         1 |
    +-----------+
    3 rows in set (1.25 sec)
    ```
    Since we can not get schema from file, we just parse column from row record in order. For example, if we create table
    ```
    CREATE EXTERNAL TABLE `csv_test` (
      `countryid` int NULL,
      `population` string NULL
    ) ENGINE=HIVE
    PROPERTIES (
      "resource" = "hive0",
      "database" = "default",
      "table" = "countries_list"
    );
    ```
    It will always return `countryname` for `population`. Some other system like hive and impala has the same semantics.

---
+++ be/src/formats/csv/csv_reader.cpp
@@ -0,0 +1,73 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021 StarRocks Limited.
+
+#include "formats/csv/csv_reader.h"
+
+namespace starrocks::vectorized {
+
+Status CSVReader::next_record(Record* record) {
+    if (_limit > 0 && _parsed_bytes > _limit) {
+        return Status::EndOfFile("Reached limit");
+    }
+    char* d;
+    size_t pos = 0;
+    while ((d = _buff.find(_record_delimiter, pos)) == nullptr) {
+        pos = _buff.available();
+        _buff.compact();
+        if (_buff.free_space() == 0) {
+            RETURN_IF_ERROR(_expand_buffer());
+        }
+        RETURN_IF_ERROR(_fill_buffer());
+    }
+    size_t l = d - _buff.position();
+    *record = Record(_buff.position(), l);
+    _buff.skip(l + 1);
+    //               ^^ skip record delimiter.
+    _parsed_bytes += l + 1;
+    return Status::OK();
+}
+
+Status CSVReader::_expand_buffer() {
+    if (UNLIKELY(_storage.size() >= kMaxBufferSize)) {
+        return Status::InternalError("CSV line length exceed limit " + std::to_string(kMaxBufferSize));
+    }
+    size_t new_capacity = std::min(_storage.size() * 2, kMaxBufferSize);
+    DCHECK_EQ(_storage.data(), _buff.position()) << "should compact buffer before expand";
+    _storage.resize(new_capacity);
+    CSVBuffer new_buff(_storage.data(), _storage.size());
+    new_buff.add_limit(_buff.available());
+    DCHECK_EQ(_storage.data(), new_buff.position());
+    DCHECK_EQ(_buff.available(), new_buff.available());
+    _buff = new_buff;
+    return Status::OK();
+}
+
+void CSVReader::split_record(const Record& record, Fields* fields) const {
+    const char* value = record.data;
+    const char* ptr = record.data;
+    const size_t size = record.size;
+
+    if (_field_delimiter.size() == 1) {
+        for (size_t i = 0; i < size; ++i, ++ptr) {
+            if (*ptr == _field_delimiter[0]) {
+                fields->emplace_back(value, ptr - value);
+                value = ptr + 1;
+            }
+        }
+    } else {
+        const auto fd_size = _field_delimiter.size();
+        const auto* const base = ptr;
+
+        do {
+            ptr = static_cast<char*>(memmem(value, size - (value - base), _field_delimiter.data(), fd_size));
+            if (ptr != nullptr) {
+                fields->emplace_back(value, ptr - value);
+                value = ptr + fd_size;
+            }
+        } while (ptr != nullptr);
+
+        ptr = record.data + size;
+    }
+    fields->emplace_back(value, ptr - value);
+}
+
+} // namespace starrocks::vectorized
