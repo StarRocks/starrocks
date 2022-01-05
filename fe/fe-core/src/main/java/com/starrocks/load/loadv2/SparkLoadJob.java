0ce13fc56cdbe128e5ea364a9d2e033f3c068857
gengjun-git <gengjun@dorisdb.com>
2022-01-05 15:08:38 +0800

    Fix the bug of null pointer exception when cleaning up spark loading task (#2583)
    1. Check if sparkLoadAppHandle is null before clearing the spark load log.
    2. Catch task exceptions in advance to avoid blocking the cleanup of other tasks.

--- fe/fe-core/src/main/java/com/starrocks/load/loadv2/SparkLoadJob.java
+++ fe/fe-core/src/main/java/com/starrocks/load/loadv2/SparkLoadJob.java
@@ -128,11 +128,10 @@ public class SparkLoadJob extends BulkLoadJob {
     // members below updated when job state changed to loading
     // { tableId.partitionId.indexId.bucket.schemaHash -> (etlFilePath, etlFileSize) }
     private Map<String, Pair<String, Long>> tabletMetaToFileInfo = Maps.newHashMap();
+    private SparkLoadAppHandle sparkLoadAppHandle = new SparkLoadAppHandle();
 
     // --- members below not persist ---
     private ResourceDesc resourceDesc;
-    // for spark standalone
-    private SparkLoadAppHandle sparkLoadAppHandle = new SparkLoadAppHandle();
     // for straggler wait long time to commit transaction
     private long quorumFinishTimestamp = -1;
     // below for push task
@@ -695,7 +694,6 @@ public class SparkLoadJob extends BulkLoadJob {
                 }
             }
             // clear job infos that not persist
-            sparkLoadAppHandle = null;
             resourceDesc = null;
             tableToLoadPartitions.clear();
             indexToPushBrokerReaderParams.clear();
@@ -760,6 +758,10 @@ public class SparkLoadJob extends BulkLoadJob {
     }
 
     public void clearSparkLauncherLog() {
+        if (sparkLoadAppHandle == null) {
+            return;
+        }
+
         String logPath = sparkLoadAppHandle.getLogPath();
         if (!Strings.isNullOrEmpty(logPath)) {
             File file = new File(logPath);
