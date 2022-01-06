e8aad9dbacc4387475d7ed6e90dfc2173593b801
Pslydhh <luouyuyou@gmail.com>
2022-01-06 21:37:55 +0800

    Push down limit to mysql external table (#2657)

--- gensrc/thrift/PlanNodes.thrift
+++ gensrc/thrift/PlanNodes.thrift
@@ -240,6 +240,7 @@ struct TMySQLScanNode {
   2: required string table_name
   3: required list<string> columns
   4: required list<string> filters
+  5: optional i64 limit
 }
 
 struct TFileScanNode {
