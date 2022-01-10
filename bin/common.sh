65626104be33a1ea6a80598ba2a2da91708d7758
lichaoyong <lichaoyong158@gmail.com>
2022-01-10 10:21:14 +0800

    Add "Copyright 2021-present" to all file headers (#2612)

--- bin/common.sh
+++ bin/common.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# This file is licensed under the Elastic License 2.0. Copyright 2021 StarRocks Limited.
+# This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
 
 # get jdk version, return version as an Integer.
 # 1.8 => 8, 13.0 => 13
