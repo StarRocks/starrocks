23706daf519f7509aed8b2607ceca185cb30a0bb
lichaoyong <lichaoyong158@gmail.com>
2022-01-10 10:21:14 +0800

    Add "Copyright 2021-present" to all file headers (#2612)

--- gensrc/script/vectorized/vectorized_functions.py
+++ gensrc/script/vectorized/vectorized_functions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-# This file is licensed under the Elastic License 2.0. Copyright 2021 StarRocks Limited.
+# This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
 
 # The format is:
 #   <function id> <name>, <return_type>, [<args>], <backend fn>,
