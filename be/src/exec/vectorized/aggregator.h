2d5f4c4e66a4a575531b80f7ed6b74f6ce67f2ea
trueeyu <lxhhust350@qq.com>
2022-01-11 13:09:51 +0800

    Remove unused code of aggr (#2751)
    the batch_finalize of aggregator is not override
    
    the interface is
    
    ```
    void batch_finalize(FunctionContext* ctx, size_t chunk_size, const Buffer<AggDataPtr>& agg_states,
                            size_t state_offset, Column* to) const
    ```
    
    but the implement is 
    
    ```
    void batch_finalize(size_t chunk_size, const Buffer<AggDataPtr>& agg_states, size_t state_offsets,
                            Column* to) const
    ```
    
    override the batch_finalize has no improve on performance

--- be/src/exec/vectorized/aggregator.h
+++ be/src/exec/vectorized/aggregator.h
@@ -333,8 +333,8 @@ public:
                 }
             } else {
                 for (size_t i = 0; i < _agg_fn_ctxs.size(); i++) {
-                    _agg_functions[i]->batch_serialize(read_index, _tmp_agg_states, _agg_states_offsets[i],
-                                                       agg_result_column[i].get());
+                    _agg_functions[i]->batch_serialize(_agg_fn_ctxs[i], read_index, _tmp_agg_states,
+                                                       _agg_states_offsets[i], agg_result_column[i].get());
                 }
             }
         }
--- be/src/exprs/agg/sum.h
+++ be/src/exprs/agg/sum.h
@@ -91,8 +91,8 @@ public:
         down_cast<ResultColumnType*>(to)->append(this->data(state).sum);
     }
 
-    void batch_serialize(size_t chunk_size, const Buffer<AggDataPtr>& agg_states, size_t state_offset,
-                         Column* to) const override {
+    void batch_serialize(FunctionContext* ctx, size_t chunk_size, const Buffer<AggDataPtr>& agg_states,
+                         size_t state_offset, Column* to) const override {
         ResultColumnType* column = down_cast<ResultColumnType*>(to);
         Buffer<ResultType>& result_data = column->get_data();
         for (size_t i = 0; i < chunk_size; i++) {
@@ -104,11 +104,6 @@ public:
                             Column* to) const override {
         DCHECK(to->is_numeric() || to->is_decimal());
         down_cast<ResultColumnType*>(to)->append(this->data(state).sum);
-    }
-
-    void batch_finalize(size_t chunk_size, const Buffer<AggDataPtr>& agg_states, size_t state_offset,
-                        Column* to) const {
-        batch_serialize(chunk_size, agg_states, state_offset, to);
     }
 
     void convert_to_serialize_format(const Columns& src, size_t chunk_size, ColumnPtr* dst) const override {
