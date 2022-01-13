StarRocks/starrocks

commit 5ebaa718d8db190fdedf75b2e6fd3762068c4334

upgrade openssl from 1.0.2k to 1.1.1m and related stuff (#2807)
Backgrounds:
1. Refer to PR 2733(https://github.com/StarRocks/starrocks/pull/2733) first.
2. When compiling mariadb-connector-c 3.1.14 on ARM platform, it failed with openssl "relocation of symbol OPENSSL_armcap_P" error.
3. Openssl has fixed the relocation issue by PR 13056(https://github.com/openssl/openssl/pull/13056) and merged to release of openssl after version 1.1.1i.
4. PR 2733 fixed the above problem by bypassing the asm part of SHA.
5. To fix this issue completely, decided to upgrade openssl to 1.1.1m.

This fix includes following modifications:
1. upgrade openssl             from 1.0.2k to 1.1.1m
   * simplify build_openssl() function in thirdparty/build-thirdparty.sh
   * modify AesUtil::entrypt() and AesUtil::decrypt() according to the API changes of openssl from 1.0.x to 1.1.x
2. upgrade curl                from 7.54.1 to 7.79.0
3. upgrade mariadb-connector-c from 3.1.14 to 3.2.5
   * modify build_mariadb() function in thirdparty/build-thirdparty.sh for static link
   * add a patch mariadb-connector-c-3.2.5-for-starrocks-static-link.patch for static link
     * refer to https://mariadb.com/kb/en/configuration-settings-for-building-connectorc/
4. add platform check for be/src/formats module for compatible with ARM platform
5. verified on:
   * CentOS 7.9.2009(Core) / i5-7200U
   * Kylin Linux Advanced Server release V10(Tercel) / FT-2000+/64

TODOs
1. Need a complete test, especially for test cases related TLS/SSL communication.
2. The performance after this thirdparty libraries upgrade is currently unknown, need more measurement by performance test.

Potential risks
1. All newly imported thirdparty libraries need be compatible with openssl 1.1.x APIs.

be/src/util/aes_util.cpp
@@ -104,7 +104,6 @@ static int do_encrypt(EVP_CIPHER_CTX* aes_ctx, const EVP_CIPHER* cipher, const u
 
 int AesUtil::encrypt(AesMode mode, const unsigned char* source, uint32_t source_length, const unsigned char* key,
                      uint32_t key_length, const unsigned char* iv, bool padding, unsigned char* encrypt) {
-    EVP_CIPHER_CTX aes_ctx;
     const EVP_CIPHER* cipher = get_evp_type(mode);
     /* The encrypt key to be used for encryption */
     unsigned char encrypt_key[AES_MAX_KEY_LENGTH / 8];
@@ -113,10 +112,11 @@ int AesUtil::encrypt(AesMode mode, const unsigned char* source, uint32_t source_
     if (cipher == nullptr || (EVP_CIPHER_iv_length(cipher) > 0 && !iv)) {
         return AES_BAD_DATA;
     }
-    EVP_CIPHER_CTX_init(&aes_ctx);
+
+    EVP_CIPHER_CTX *aes_ctx = EVP_CIPHER_CTX_new();
     int length = 0;
-    int ret = do_encrypt(&aes_ctx, cipher, source, source_length, encrypt_key, iv, padding, encrypt, &length);
-    EVP_CIPHER_CTX_cleanup(&aes_ctx);
+    int ret = do_encrypt(aes_ctx, cipher, source, source_length, encrypt_key, iv, padding, encrypt, &length);
+    EVP_CIPHER_CTX_free(aes_ctx);
     if (ret == 0) {
         ERR_clear_error();
         return AES_BAD_DATA;
@@ -149,7 +149,6 @@ static int do_decrypt(EVP_CIPHER_CTX* aes_ctx, const EVP_CIPHER* cipher, const u
 
 int AesUtil::decrypt(AesMode mode, const unsigned char* encrypt, uint32_t encrypt_length, const unsigned char* key,
                      uint32_t key_length, const unsigned char* iv, bool padding, unsigned char* decrypt_content) {
-    EVP_CIPHER_CTX aes_ctx;
     const EVP_CIPHER* cipher = get_evp_type(mode);
 
     /* The encrypt key to be used for decryption */
@@ -160,10 +159,10 @@ int AesUtil::decrypt(AesMode mode, const unsigned char* encrypt, uint32_t encryp
         return AES_BAD_DATA;
     }
 
-    EVP_CIPHER_CTX_init(&aes_ctx);
+    EVP_CIPHER_CTX *aes_ctx = EVP_CIPHER_CTX_new();
     int length = 0;
-    int ret = do_decrypt(&aes_ctx, cipher, encrypt, encrypt_length, encrypt_key, iv, padding, decrypt_content, &length);
-    EVP_CIPHER_CTX_cleanup(&aes_ctx);
+    int ret = do_decrypt(aes_ctx, cipher, encrypt, encrypt_length, encrypt_key, iv, padding, decrypt_content, &length);
+    EVP_CIPHER_CTX_free(aes_ctx);
     if (ret > 0) {
         return length;
     } else {

be/src/formats/CMakeLists.txt
@@ -23,8 +23,10 @@ add_library(Formats STATIC
 
 # simdjson Runtime Implement Dispatch: https://github.com/simdjson/simdjson/blob/master/doc/implementation-selection.md#runtime-cpu-detection
 # Unset architecture-specific flags to avoid breaking implement runtime dispatch.
-set_source_files_properties(json/nullable_column.cpp PROPERTIES COMPILE_FLAGS -mno-avx)
-set_source_files_properties(json/binary_column.cpp PROPERTIES COMPILE_FLAGS -mno-avx)
-set_source_files_properties(json/numeric_column.cpp PROPERTIES COMPILE_FLAGS -mno-avx)
+if ("${CMAKE_BUILD_TARGET_ARCH}" STREQUAL "x86" OR "${CMAKE_BUILD_TARGET_ARCH}" STREQUAL "x86_64")
+    set_source_files_properties(json/nullable_column.cpp PROPERTIES COMPILE_FLAGS -mno-avx)
+    set_source_files_properties(json/binary_column.cpp PROPERTIES COMPILE_FLAGS -mno-avx)
+    set_source_files_properties(json/numeric_column.cpp PROPERTIES COMPILE_FLAGS -mno-avx)
+endif()
 
 add_subdirectory(orc/apache-orc)

thirdparty/build-thirdparty.sh
@@ -179,27 +179,8 @@ build_openssl() {
     LDFLAGS="-L${TP_LIB_DIR}" \
     CFLAGS="-fPIC" \
     LIBDIR="lib" \
-    ./Configure --prefix=$TP_INSTALL_DIR -zlib -shared no-asm ${OPENSSL_PLATFORM}
-    make -j$PARALLEL && make install
-    if [ -f $TP_INSTALL_DIR/lib64/libcrypto.a ]; then
-        mkdir -p $TP_INSTALL_DIR/lib && \
-        cp $TP_INSTALL_DIR/lib64/libcrypto.a $TP_INSTALL_DIR/lib/libcrypto.a && \
-        cp $TP_INSTALL_DIR/lib64/libssl.a $TP_INSTALL_DIR/lib/libssl.a
-    fi
-    # NOTE(zc): remove this dynamic library files to make libcurl static link.
-    # If I don't remove this files, I don't known how to make libcurl link static library
-    if [ -f $TP_INSTALL_DIR/lib64/libcrypto.so ]; then
-        rm -rf $TP_INSTALL_DIR/lib64/libcrypto.so*
-    fi
-    if [ -f $TP_INSTALL_DIR/lib64/libssl.so ]; then
-        rm -rf $TP_INSTALL_DIR/lib64/libssl.so*
-    fi
-    if [ -f $TP_INSTALL_DIR/lib/libcrypto.so ]; then
-        rm -rf $TP_INSTALL_DIR/lib/libcrypto.so*
-    fi
-    if [ -f $TP_INSTALL_DIR/lib/libssl.so ]; then
-        rm -rf $TP_INSTALL_DIR/lib/libssl.so*
-    fi
+    ./Configure --prefix=$TP_INSTALL_DIR -zlib -no-shared ${OPENSSL_PLATFORM}
+    make -j$PARALLEL && make install_sw
 }
 
 # thrift
@@ -730,11 +711,20 @@ build_hyperscan() {
 }
 
 #mariadb-connector-c
+# static link plugins refer to:
+# https://mariadb.com/kb/en/configuration-settings-for-building-connectorc/
 build_mariadb() {
     check_if_source_exist $MARIADB_SOURCE
     cd $TP_SOURCE_DIR/$MARIADB_SOURCE
     mkdir -p build && cd build
-    $CMAKE_CMD .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${TP_INSTALL_DIR}
+    $CMAKE_CMD .. -DCMAKE_BUILD_TYPE=Release                \
+                  -DCMAKE_INSTALL_PREFIX=${TP_INSTALL_DIR}  \
+                  -DCLIENT_PLUGIN_SHA256_PASSWORD=STATIC    \
+                  -DCLIENT_PLUGIN_AUTH_GSSAPI=STATIC        \
+                  -DCLIENT_PLUGIN_CLEARTEXT=STATIC          \
+                  -DCLIENT_PLUGIN_DIALOG=STATIC             \
+                  -DOPENSSL_ROOT_DIR=${TP_INSTALL_DIR}      \
+                  -DOPENSSL_LIBRARYIES=${TP_INSTALL_DIR}/lib
     make -j$PARALLEL && make install
 }
 

thirdparty/download-thirdparty.sh
@@ -339,14 +339,16 @@ if [ ! -f $PATCHED_MARK ] && [ $GPERFTOOLS_SOURCE = "gperftools-gperftools-2.7" 
     touch $PATCHED_MARK
 fi
 cd -
+echo "Finished patching $GPERFTOOLS_SOURCE"
 
-# patch openssl-1.0.2k
-# This patch is based on https://github.com/openssl/openssl/pull/11464.
-cd $TP_SOURCE_DIR/$OPENSSL_SOURCE
-if [ ! -f $PATCHED_MARK ] && [ $OPENSSL_SOURCE = "openssl-1.0.2k" ]; then
-    patch -p1 < $TP_PATCH_DIR/openssl-1.0.2k.patch
+# patch mariadb-connector-c-3.2.5
+cd $TP_SOURCE_DIR/$MARIADB_SOURCE
+if [ ! -f $PATCHED_MARK ] && [ $MARIADB_SOURCE = "mariadb-connector-c-3.2.5" ]; then
+    patch -p0 < $TP_PATCH_DIR/mariadb-connector-c-3.2.5-for-starrocks-static-link.patch
     touch $PATCHED_MARK
+    echo "Finished patching $MARIADB_SOURCE"
+else
+    echo "$MARIADB_SOURCE not patched"
 fi
 cd -
 
-echo "Finished patching $GPERFTOOLS_SOURCE"

thirdparty/patches/mariadb-connector-c-3.2.5-for-starrocks-static-link.patch
@@ -1,0 +1,11 @@
+--- CMakeLists.txt     2021-11-03 23:35:27.000000000 +0800
++++ CMakeLists.txt     2022-01-11 21:11:17.317456510 +0800
+@@ -384,7 +384,7 @@
+   ENDIF()
+ ENDIF()
+ IF(WITH_SSL)
+-  SET(SYSTEM_LIBS ${SYSTEM_LIBS} ${SSL_LIBRARIES})
++  SET(SYSTEM_LIBS ${SSL_LIBRARIES} ${SYSTEM_LIBS})
+ ENDIF()
+ MESSAGE1(SYSTEM_LIBS "SYSTEM_LIBS ${SYSTEM_LIBS}")
+ MARK_AS_ADVANCED(SYSTEM_LIBS)

thirdparty/patches/openssl-1.0.2k.patch
@@ -1,24 +1,0 @@
---- a/crypto/sha/asm/sha1-armv8.pl
-+++ b/crypto/sha/asm/sha1-armv8.pl
-@@ -303,7 +303,7 @@ $code.=<<___;
- .quad  OPENSSL_armcap_P-.
- .asciz "SHA1 block transform for ARMv8, CRYPTOGAMS by <appro\@openssl.org>"
- .align 2
--.comm  OPENSSL_armcap_P,4,4
-+.hidden OPENSSL_armcap_P
- ___
- }}}
- 
-diff --git a/crypto/sha/asm/sha512-armv8.pl b/crypto/sha/asm/sha512-armv8.pl
-index f7b36b986a..5e83e56385 100644
---- a/crypto/sha/asm/sha512-armv8.pl
-+++ b/crypto/sha/asm/sha512-armv8.pl
-@@ -389,7 +389,7 @@ ___
- }
- 
- $code.=<<___;
--.comm  OPENSSL_armcap_P,4,4
-+.hidden        OPENSSL_armcap_P
- ___
- 
- {   my  %opcode = (

thirdparty/vars.sh
@@ -71,10 +71,10 @@ LIBEVENT_SOURCE=libevent-24236aed01798303745470e6c498bf606e88724a
 LIBEVENT_MD5SUM="c6c4e7614f03754b8c67a17f68177649"
 
 # openssl
-OPENSSL_DOWNLOAD="https://www.openssl.org/source/old/1.0.2/openssl-1.0.2k.tar.gz"
-OPENSSL_NAME=openssl-1.0.2k.tar.gz
-OPENSSL_SOURCE=openssl-1.0.2k
-OPENSSL_MD5SUM="f965fc0bf01bf882b31314b61391ae65"
+OPENSSL_DOWNLOAD="https://github.com/openssl/openssl/archive/OpenSSL_1_1_1m.tar.gz"
+OPENSSL_NAME=openssl-OpenSSL_1_1_1m.tar.gz
+OPENSSL_SOURCE=openssl-OpenSSL_1_1_1m
+OPENSSL_MD5SUM="710c2368d28f1a25ab92e25b5b9b11ec"
 
 # thrift
 THRIFT_DOWNLOAD="http://archive.apache.org/dist/thrift/0.13.0/thrift-0.13.0.tar.gz"
@@ -149,10 +149,10 @@ SIMDJSON_SOURCE=simdjson-1.0.2
 SIMDJSON_MD5SUM="5bb34cca7087a99c450dbdfe406bdc7d"
 
 # curl
-CURL_DOWNLOAD="https://curl.haxx.se/download/curl-7.54.1.tar.gz"
-CURL_NAME=curl-7.54.1.tar.gz
-CURL_SOURCE=curl-7.54.1
-CURL_MD5SUM="21a6e5658fd55103a90b11de7b2a8a8c"
+CURL_DOWNLOAD="https://curl.se/download/curl-7.79.0.tar.gz"
+CURL_NAME=curl-7.79.0.tar.gz
+CURL_SOURCE=curl-7.79.0
+CURL_MD5SUM="b40e4dc4bbc9e109c330556cd58c8ec8"
 
 # RE2
 RE2_DOWNLOAD="https://github.com/google/re2/archive/2017-05-01.tar.gz"
@@ -270,10 +270,10 @@ RAGEL_SOURCE="ragel-6.10"
 RAGEL_MD5SUM="748cae8b50cffe9efcaa5acebc6abf0d"
 
 # mariadb-connector-c
-MARIADB_DOWNLOAD="https://github.com/mariadb-corporation/mariadb-connector-c/archive/refs/tags/v3.1.14.tar.gz"
-MARIADB_NAME="mariadb-connector-c-3.1.14.tar.gz"
-MARIADB_SOURCE="mariadb-connector-c-3.1.14"
-MARIADB_MD5SUM="86c4052adeb8447900bf33b4e2ddd1f9"
+MARIADB_DOWNLOAD="https://github.com/mariadb-corporation/mariadb-connector-c/archive/refs/tags/v3.2.5.tar.gz"
+MARIADB_NAME="mariadb-connector-c-3.2.5.tar.gz"
+MARIADB_SOURCE="mariadb-connector-c-3.2.5"
+MARIADB_MD5SUM="9ab9205418a933b0a7920e7cf424cc36"
 
 # aliyun_oss_jars
 ALIYUN_OSS_JARS_DOWNLOAD="http://cdn-thirdparty.starrocks.com/aliyun-oss-sdk-3.7.2.tar.gz"

commit 4fa2e73e5f88493e5627a57a8c816d29180e684f

Fix clang-format caused by #2807

be/src/util/aes_util.cpp
@@ -113,7 +113,7 @@ int AesUtil::encrypt(AesMode mode, const unsigned char* source, uint32_t source_
         return AES_BAD_DATA;
     }
 
-    EVP_CIPHER_CTX *aes_ctx = EVP_CIPHER_CTX_new();
+    EVP_CIPHER_CTX* aes_ctx = EVP_CIPHER_CTX_new();
     int length = 0;
     int ret = do_encrypt(aes_ctx, cipher, source, source_length, encrypt_key, iv, padding, encrypt, &length);
     EVP_CIPHER_CTX_free(aes_ctx);
@@ -159,7 +159,7 @@ int AesUtil::decrypt(AesMode mode, const unsigned char* encrypt, uint32_t encryp
         return AES_BAD_DATA;
     }
 
-    EVP_CIPHER_CTX *aes_ctx = EVP_CIPHER_CTX_new();
+    EVP_CIPHER_CTX* aes_ctx = EVP_CIPHER_CTX_new();
     int length = 0;
     int ret = do_decrypt(aes_ctx, cipher, encrypt, encrypt_length, encrypt_key, iv, padding, decrypt_content, &length);
     EVP_CIPHER_CTX_free(aes_ctx);