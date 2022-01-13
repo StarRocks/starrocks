38a32eba418a51f977c630c4ac717949b6edcb42
stdpain <34912776+stdpain@users.noreply.github.com>
2022-01-13 22:02:28 +0800

    Fix overflow when a condition has GE and LE type::max (#2841)

--- be/src/exec/olap_common.h
+++ be/src/exec/olap_common.h
@@ -538,9 +538,16 @@ inline void ColumnValueRange<T>::convert_to_fixed_value() {
         }
         _fixed_op = FILTER_IN;
     } else {
-        for (T v = _low_value; v <= _high_value; helper::increase(v)) {
+        // if _low_value == _high_value == type::max
+        // v will overflow after increase, so we have to
+        // do some special treatment
+        if (_low_value <= _high_value) {
+            _fixed_values.insert(_high_value);
+        }
+        for (T v = _low_value; v < _high_value; helper::increase(v)) {
             _fixed_values.insert(v);
         }
+
         _fixed_op = FILTER_IN;
     }
 }
