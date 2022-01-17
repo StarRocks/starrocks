4eb92c98fb854588f61861d626592be97016399e
satanson <ranpanf@gmail.com>
2022-01-17 14:03:12 +0800

    [ISSUE-2721] turn DecimalLiteral into NullLiteral when it overflows (#2788)

--- fe/fe-core/src/test/java/com/starrocks/sql/optimizer/rewrite/scalar/ReduceCastRuleTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/optimizer/rewrite/scalar/ReduceCastRuleTest.java
@@ -2,13 +2,20 @@
 
 package com.starrocks.sql.optimizer.rewrite.scalar;
 
+import com.starrocks.catalog.ScalarType;
 import com.starrocks.catalog.Type;
 import com.starrocks.sql.optimizer.operator.OperatorType;
+import com.starrocks.sql.optimizer.operator.scalar.BinaryPredicateOperator;
 import com.starrocks.sql.optimizer.operator.scalar.CastOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ConstantOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ScalarOperator;
 import com.starrocks.sql.optimizer.rewrite.ScalarOperatorRewriteRule;
+import javassist.expr.Cast;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -60,5 +67,75 @@ public class ReduceCastRuleTest {
 
         assertTrue(result.getType().isChar());
         assertEquals(OperatorType.CONSTANT, result.getOpType());
+    }
+
+    private ConstantOperator createConstOperatorFromType(Type type) {
+        if (type.isTinyint()) {
+            return ConstantOperator.createTinyInt(Byte.MAX_VALUE);
+        }
+        if (type.isSmallint()) {
+            return ConstantOperator.createSmallInt(Short.MAX_VALUE);
+        }
+        if (type.isInt()) {
+            return ConstantOperator.createInt(Integer.MAX_VALUE);
+        }
+        if (type.isBigint()) {
+            return ConstantOperator.createBigint(Long.MAX_VALUE);
+        }
+        if (type.isLargeint()) {
+            return ConstantOperator.createLargeInt(BigInteger.ONE);
+        }
+        if (type.isDecimalV3()) {
+            return ConstantOperator.createDecimal(BigDecimal.ONE, type);
+        }
+        return ConstantOperator.createNull(type);
+    }
+
+    @Test
+    public void testBinaryPredicateInvolvingDecimalSuccess() {
+        Type[][] typeListList = new Type[][]{
+                {Type.TINYINT, Type.SMALLINT, ScalarType.createDecimalV3NarrowestType(16,9)},
+                {Type.TINYINT, Type.SMALLINT, ScalarType.createDecimalV3NarrowestType(9,0)},
+                {ScalarType.createDecimalV3NarrowestType(4,0), Type.SMALLINT, Type.BIGINT},
+                {ScalarType.createDecimalV3NarrowestType(18, 0), Type.BIGINT, Type.LARGEINT},
+                {ScalarType.createDecimalV3NarrowestType(2, 0), Type.TINYINT, Type.INT},
+        };
+
+        ScalarOperatorRewriteRule reduceCastRule = new ReduceCastRule();
+        ScalarOperatorRewriteRule foldConstantsRule = new FoldConstantsRule();
+        for (Type[] types : typeListList) {
+            ScalarOperator binPredRhs = createConstOperatorFromType(types[0]);
+            ScalarOperator castChild = createConstOperatorFromType(types[1]);
+            CastOperator binPredLhs = new CastOperator(types[2], castChild);
+            BinaryPredicateOperator binPred = new BinaryPredicateOperator(
+                    BinaryPredicateOperator.BinaryType.GE, binPredLhs, binPredRhs);
+            ScalarOperator result = reduceCastRule.apply(binPred, null);
+            result = foldConstantsRule.apply(result, null);
+            Assert.assertTrue(result instanceof ConstantOperator);
+        }
+    }
+
+    @Test
+    public void testBinaryPredicateInvolvingDecimalFail() {
+        Type[][] typeListList = new Type[][]{
+                {Type.TINYINT, Type.SMALLINT, ScalarType.createDecimalV3NarrowestType(13,9)},
+                {Type.INT, Type.SMALLINT, ScalarType.createDecimalV3NarrowestType(9,0)},
+                {ScalarType.createDecimalV3NarrowestType(6,0), Type.SMALLINT, Type.BIGINT},
+                {ScalarType.createDecimalV3NarrowestType(19, 0), Type.BIGINT, Type.LARGEINT},
+                {ScalarType.createDecimalV3NarrowestType(10, 0), Type.TINYINT, Type.INT},
+        };
+
+        ScalarOperatorRewriteRule reduceCastRule = new ReduceCastRule();
+        ScalarOperatorRewriteRule foldConstantsRule = new FoldConstantsRule();
+        for (Type[] types : typeListList) {
+            ScalarOperator binPredRhs = createConstOperatorFromType(types[0]);
+            ScalarOperator castChild = createConstOperatorFromType(types[1]);
+            CastOperator binPredLhs = new CastOperator(types[2], castChild);
+            BinaryPredicateOperator binPred = new BinaryPredicateOperator(
+                    BinaryPredicateOperator.BinaryType.GE, binPredLhs, binPredRhs);
+            ScalarOperator result = reduceCastRule.apply(binPred, null);
+            result = foldConstantsRule.apply(result, null);
+            Assert.assertTrue(!(result instanceof ConstantOperator));
+        }
     }
 }
