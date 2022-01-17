65471c5626335c95eb268891dbd03371eef00f7b
Youngwb <yangwenbo_mailbox@163.com>
2022-01-17 19:28:41 +0800

    Fix Fe ut (#2886)

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/DistributedEnvPlanWithCostTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/DistributedEnvPlanWithCostTest.java
@@ -1117,17 +1117,14 @@ public class DistributedEnvPlanWithCostTest extends DistributedEnvPlanTestBase {
 
     @Test
     public void testCountDistinctGroupByFunction() throws Exception {
-        FeConstants.runningUnitTest = true;
         String sql = "select L_LINENUMBER, date_trunc(\"day\",L_SHIPDATE) as day ,count(distinct L_ORDERKEY) from " +
                 "lineitem group by L_LINENUMBER, day";
         String plan = getFragmentPlan(sql);
-        System.out.println(plan);
         // check use two stage aggregate
         Assert.assertTrue(plan.contains("2:AGGREGATE (update serialize)\n" +
                 "  |  STREAMING\n" +
                 "  |  output: multi_distinct_count(1: L_ORDERKEY)"));
         Assert.assertTrue(plan.contains("4:AGGREGATE (merge finalize)\n" +
                 "  |  output: multi_distinct_count(19: count)"));
-        FeConstants.runningUnitTest = false;
     }
 }
