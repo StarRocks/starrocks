46aa66d74e887b53479c616182b5698758ba6f2c
mofei <mofei@starrocks.com>
2022-01-20 20:13:04 +0800

    refactor primitive type

--- be/src/runtime/primitive_type.cpp
+++ be/src/runtime/primitive_type.cpp
@@ -20,6 +20,7 @@
 // under the License.
 
 #include "runtime/primitive_type.h"
+#include "runtime/primitive_type_infra.h"
 
 #include <sstream>
 
@@ -39,154 +40,31 @@ TExprOpcode::type to_in_opcode(PrimitiveType t) {
 
 PrimitiveType thrift_to_type(TPrimitiveType::type ttype) {
     switch (ttype) {
+    // TODO(mofei) rename these two type
     case TPrimitiveType::INVALID_TYPE:
         return INVALID_TYPE;
-
     case TPrimitiveType::NULL_TYPE:
         return TYPE_NULL;
-
-    case TPrimitiveType::BOOLEAN:
-        return TYPE_BOOLEAN;
-
-    case TPrimitiveType::TINYINT:
-        return TYPE_TINYINT;
-
-    case TPrimitiveType::SMALLINT:
-        return TYPE_SMALLINT;
-
-    case TPrimitiveType::INT:
-        return TYPE_INT;
-
-    case TPrimitiveType::BIGINT:
-        return TYPE_BIGINT;
-
-    case TPrimitiveType::LARGEINT:
-        return TYPE_LARGEINT;
-
-    case TPrimitiveType::FLOAT:
-        return TYPE_FLOAT;
-
-    case TPrimitiveType::DOUBLE:
-        return TYPE_DOUBLE;
-
-    case TPrimitiveType::DATE:
-        return TYPE_DATE;
-
-    case TPrimitiveType::DATETIME:
-        return TYPE_DATETIME;
-
-    case TPrimitiveType::TIME:
-        return TYPE_TIME;
-
-    case TPrimitiveType::VARCHAR:
-        return TYPE_VARCHAR;
-
-    case TPrimitiveType::BINARY:
-        return TYPE_BINARY;
-
-    case TPrimitiveType::DECIMAL:
-        return TYPE_DECIMAL;
-
-    case TPrimitiveType::DECIMALV2:
-        return TYPE_DECIMALV2;
-
-    case TPrimitiveType::DECIMAL32:
-        return TYPE_DECIMAL32;
-
-    case TPrimitiveType::DECIMAL64:
-        return TYPE_DECIMAL64;
-
-    case TPrimitiveType::DECIMAL128:
-        return TYPE_DECIMAL128;
-
-    case TPrimitiveType::CHAR:
-        return TYPE_CHAR;
-
-    case TPrimitiveType::HLL:
-        return TYPE_HLL;
-
-    case TPrimitiveType::OBJECT:
-        return TYPE_OBJECT;
-
-    case TPrimitiveType::PERCENTILE:
-        return TYPE_PERCENTILE;
+#define M(ttype) case TPrimitiveType::ttype: return TYPE_##ttype;
+    APPLY_FOR_ALL_THRIFT_TYPE(M)
+#undef M
     }
+
     return INVALID_TYPE;
 }
 
 TPrimitiveType::type to_thrift(PrimitiveType ptype) {
+
     switch (ptype) {
+    // TODO(mofei) rename these two type
     case INVALID_TYPE:
         return TPrimitiveType::INVALID_TYPE;
-
     case TYPE_NULL:
         return TPrimitiveType::NULL_TYPE;
 
-    case TYPE_BOOLEAN:
-        return TPrimitiveType::BOOLEAN;
-
-    case TYPE_TINYINT:
-        return TPrimitiveType::TINYINT;
-
-    case TYPE_SMALLINT:
-        return TPrimitiveType::SMALLINT;
-
-    case TYPE_INT:
-        return TPrimitiveType::INT;
-
-    case TYPE_BIGINT:
-        return TPrimitiveType::BIGINT;
-
-    case TYPE_LARGEINT:
-        return TPrimitiveType::LARGEINT;
-
-    case TYPE_FLOAT:
-        return TPrimitiveType::FLOAT;
-
-    case TYPE_DOUBLE:
-        return TPrimitiveType::DOUBLE;
-
-    case TYPE_DATE:
-        return TPrimitiveType::DATE;
-
-    case TYPE_DATETIME:
-        return TPrimitiveType::DATETIME;
-
-    case TYPE_TIME:
-        return TPrimitiveType::TIME;
-
-    case TYPE_VARCHAR:
-        return TPrimitiveType::VARCHAR;
-
-    case TYPE_BINARY:
-        return TPrimitiveType::BINARY;
-
-    case TYPE_DECIMAL:
-        return TPrimitiveType::DECIMAL;
-
-    case TYPE_DECIMALV2:
-        return TPrimitiveType::DECIMALV2;
-
-    case TYPE_DECIMAL32:
-        return TPrimitiveType::DECIMAL32;
-
-    case TYPE_DECIMAL64:
-        return TPrimitiveType::DECIMAL64;
-
-    case TYPE_DECIMAL128:
-        return TPrimitiveType::DECIMAL128;
-
-    case TYPE_CHAR:
-        return TPrimitiveType::CHAR;
-
-    case TYPE_HLL:
-        return TPrimitiveType::HLL;
-
-    case TYPE_OBJECT:
-        return TPrimitiveType::OBJECT;
-
-    case TYPE_PERCENTILE:
-        return TPrimitiveType::PERCENTILE;
+#define M(thrift_name) case TYPE_##thrift_name: return TPrimitiveType::thrift_name;
+    APPLY_FOR_ALL_THRIFT_TYPE(M)
+#undef M
 
     case TYPE_ARRAY:
     case TYPE_MAP:
@@ -198,86 +76,11 @@ TPrimitiveType::type to_thrift(PrimitiveType ptype) {
 
 std::string type_to_string(PrimitiveType t) {
     switch (t) {
-    case INVALID_TYPE:
-        return "INVALID";
-
-    case TYPE_NULL:
-        return "NULL";
-
-    case TYPE_BOOLEAN:
-        return "BOOL";
-
-    case TYPE_TINYINT:
-        return "TINYINT";
-
-    case TYPE_SMALLINT:
-        return "SMALLINT";
-
-    case TYPE_INT:
-        return "INT";
-
-    case TYPE_BIGINT:
-        return "BIGINT";
-
-    case TYPE_LARGEINT:
-        return "LARGEINT";
-
-    case TYPE_FLOAT:
-        return "FLOAT";
-
-    case TYPE_DOUBLE:
-        return "DOUBLE";
-
-    case TYPE_DATE:
-        return "DATE";
-
-    case TYPE_DATETIME:
-        return "DATETIME";
-
-    case TYPE_TIME:
-        return "TIME";
-
-    case TYPE_VARCHAR:
-        return "VARCHAR";
-
-    case TYPE_BINARY:
-        return "BINARY";
-
-    case TYPE_DECIMAL:
-        return "DECIMAL";
-
-    case TYPE_DECIMALV2:
-        return "DECIMALV2";
-
-    case TYPE_DECIMAL32:
-        return "DECIMAL32";
-
-    case TYPE_DECIMAL64:
-        return "DECIMAL64";
-
-    case TYPE_DECIMAL128:
-        return "DECIMAL128";
-
-    case TYPE_CHAR:
-        return "CHAR";
-
-    case TYPE_HLL:
-        return "HLL";
-
-    case TYPE_OBJECT:
-        return "OBJECT";
-
-    case TYPE_PERCENTILE:
-        return "PERCENTILE";
-
-    case TYPE_STRUCT:
-        return "STRUCT";
-
-    case TYPE_ARRAY:
-        return "ARRAY";
-
-    case TYPE_MAP:
-        return "MAP";
+    case INVALID_TYPE: return "INVALID";
+    case TYPE_NULL: return "NULL";
+#define M(ptype) case ptype: return #ptype;
+    APPLY_FOR_ALL_TYPE(M)
+#undef M
     }
     return "";
 }
@@ -285,78 +88,17 @@ std::string type_to_string(PrimitiveType t) {
 std::string type_to_odbc_string(PrimitiveType t) {
     // ODBC driver requires types in lower case
     switch (t) {
-    default:
     case INVALID_TYPE:
         return "invalid";
+        
+#define M(ttype) case TYPE_##ttype: return #ttype;
+    APPLY_FOR_ALL_THRIFT_TYPE(M)
+#undef M
 
-    case TYPE_NULL:
-        return "null";
-
-    case TYPE_BOOLEAN:
-        return "boolean";
-
-    case TYPE_TINYINT:
-        return "tinyint";
-
-    case TYPE_SMALLINT:
-        return "smallint";
-
-    case TYPE_INT:
-        return "int";
-
-    case TYPE_BIGINT:
-        return "bigint";
-
-    case TYPE_LARGEINT:
-        return "largeint";
-
-    case TYPE_FLOAT:
-        return "float";
-
-    case TYPE_DOUBLE:
-        return "double";
-
-    case TYPE_DATE:
-        return "date";
-
-    case TYPE_DATETIME:
-        return "datetime";
-
-    case TYPE_VARCHAR:
-        return "string";
-
-    case TYPE_BINARY:
-        return "binary";
-
-    case TYPE_DECIMAL:
-        return "decimal";
-
-    case TYPE_DECIMALV2:
-        return "decimalv2";
-
-    case TYPE_CHAR:
-        return "char";
-
-    case TYPE_HLL:
-        return "hll";
-
-    case TYPE_OBJECT:
-        return "object";
-
-    case TYPE_PERCENTILE:
-        return "percentile";
-
-    case TYPE_DECIMAL32:
-        return "decimal32";
-
-    case TYPE_DECIMAL64:
-        return "decimal64";
-
-    case TYPE_DECIMAL128:
-        return "decimal128";
+    default: 
+        return "unknown";
     }
 
-    return "unknown";
 }
 
 // for test only
--- be/src/runtime/primitive_type_infra.h
+++ be/src/runtime/primitive_type_infra.h
@@ -6,53 +6,66 @@
 #include "runtime/primitive_type.h"
 
 namespace starrocks {
-    
-// type_dispatch_all: 
+
+// type_dispatch_all:
 // macro to enumate all types
-#define TYPE_DISPATCH_ALL(TEMPLATE_FUNC, typeKind, ...)             \
-[&]() {                                                         \
-    switch (typeKind) {                                         \
-    case TYPE_INT: return TEMPLATE_FUNC<TYPE_INT>(__VA_ARGS__); \
-    default: CHECK(false) << "TODO";                            \
-    }                                                           \
-}();
+#define TYPE_DISPATCH_ALL(TEMPLATE_FUNC, typeKind, ...)  \
+    [&]() {                                              \
+        switch (typeKind) {                              \
+        case TYPE_INT:                                   \
+            return TEMPLATE_FUNC<TYPE_INT>(__VA_ARGS__); \
+        default:                                         \
+            CHECK(false) << "TODO";                      \
+        }                                                \
+    }();
 
+#define TYPE_DISPATCH_ALL_WITH_NULL(TEMPLATE_FUNC, typeKind, ...) \
+    [&]() {                                                       \
+        switch (typeKind) {                                       \
+        case TYPE_INT:                                            \
+            return TEMPLATE_FUNC<TYPE_INT>(__VA_ARGS__);          \
+        case TYPE_NULL:                                           \
+            return TEMPLATE_FUNC<TYPE_NULL>(__VA_ARGS__);         \
+        default:                                                  \
+            CHECK(false) << "TODO";                               \
+        }                                                         \
+    }();
 
-// type_dispatch_all: 
+// type_dispatch_all:
 // Dispatch dynamic ptype to static template instance Functor
 template <class Functor>
 auto type_dispatch_all(PrimitiveType ptype, Functor fun) {
-    
 #define _TYPE_DISPATCH_CASE(type) \
-    case type: return fun.template operator()<type>(); 
-    
+    case type:                    \
+        return fun.template operator()<type>();
+
     switch (ptype) {
-    _TYPE_DISPATCH_CASE(TYPE_NULL)
-    _TYPE_DISPATCH_CASE(TYPE_BOOLEAN)          
-    _TYPE_DISPATCH_CASE(TYPE_TINYINT)          
-    _TYPE_DISPATCH_CASE(TYPE_SMALLINT)         
-    _TYPE_DISPATCH_CASE(TYPE_INT)              
-    _TYPE_DISPATCH_CASE(TYPE_BIGINT)           
-    _TYPE_DISPATCH_CASE(TYPE_LARGEINT)         
-    _TYPE_DISPATCH_CASE(TYPE_FLOAT)            
-    _TYPE_DISPATCH_CASE(TYPE_DOUBLE)           
-    _TYPE_DISPATCH_CASE(TYPE_VARCHAR)          
-    _TYPE_DISPATCH_CASE(TYPE_DATE)             
-    _TYPE_DISPATCH_CASE(TYPE_DATETIME)         
-    // _TYPE_DISPATCH_CASE(TYPE_BINARY)           
-    // _TYPE_DISPATCH_CASE(TYPE_DECIMAL)          
-    _TYPE_DISPATCH_CASE(TYPE_CHAR)             
-    // _TYPE_DISPATCH_CASE(TYPE_STRUCT)           
-    // _TYPE_DISPATCH_CASE(TYPE_ARRAY)            
-    // _TYPE_DISPATCH_CASE(TYPE_MAP)              
-    _TYPE_DISPATCH_CASE(TYPE_HLL)              
-    _TYPE_DISPATCH_CASE(TYPE_TIME)             
-    _TYPE_DISPATCH_CASE(TYPE_OBJECT)           
-    _TYPE_DISPATCH_CASE(TYPE_PERCENTILE)       
-    _TYPE_DISPATCH_CASE(TYPE_DECIMALV2)        
-    _TYPE_DISPATCH_CASE(TYPE_DECIMAL32)        
-    _TYPE_DISPATCH_CASE(TYPE_DECIMAL64)        
-    _TYPE_DISPATCH_CASE(TYPE_DECIMAL128)       
+        _TYPE_DISPATCH_CASE(TYPE_NULL)
+        _TYPE_DISPATCH_CASE(TYPE_BOOLEAN)
+        _TYPE_DISPATCH_CASE(TYPE_TINYINT)
+        _TYPE_DISPATCH_CASE(TYPE_SMALLINT)
+        _TYPE_DISPATCH_CASE(TYPE_INT)
+        _TYPE_DISPATCH_CASE(TYPE_BIGINT)
+        _TYPE_DISPATCH_CASE(TYPE_LARGEINT)
+        _TYPE_DISPATCH_CASE(TYPE_FLOAT)
+        _TYPE_DISPATCH_CASE(TYPE_DOUBLE)
+        _TYPE_DISPATCH_CASE(TYPE_VARCHAR)
+        _TYPE_DISPATCH_CASE(TYPE_DATE)
+        _TYPE_DISPATCH_CASE(TYPE_DATETIME)
+        // _TYPE_DISPATCH_CASE(TYPE_BINARY)
+        // _TYPE_DISPATCH_CASE(TYPE_DECIMAL)
+        _TYPE_DISPATCH_CASE(TYPE_CHAR)
+        // _TYPE_DISPATCH_CASE(TYPE_STRUCT)
+        // _TYPE_DISPATCH_CASE(TYPE_ARRAY)
+        // _TYPE_DISPATCH_CASE(TYPE_MAP)
+        _TYPE_DISPATCH_CASE(TYPE_HLL)
+        _TYPE_DISPATCH_CASE(TYPE_TIME)
+        _TYPE_DISPATCH_CASE(TYPE_OBJECT)
+        _TYPE_DISPATCH_CASE(TYPE_PERCENTILE)
+        _TYPE_DISPATCH_CASE(TYPE_DECIMALV2)
+        _TYPE_DISPATCH_CASE(TYPE_DECIMAL32)
+        _TYPE_DISPATCH_CASE(TYPE_DECIMAL64)
+        _TYPE_DISPATCH_CASE(TYPE_DECIMAL128)
     default:
         LOG(FATAL) << "Unknown type " << ptype;
     }
@@ -60,5 +73,62 @@ auto type_dispatch_all(PrimitiveType ptype, Functor fun) {
 #undef _TYPE_DISPATCH_CASE
 }
 
+#define APPLY_FOR_ALL_PRIMITIVE_TYPE(M) \
+    M(TYPE_TINYINT)                     \
+    M(TYPE_SMALLINT)                    \
+    M(TYPE_INT)                         \
+    M(TYPE_BIGINT)                      \
+    M(TYPE_LARGEINT)                    \
+    M(TYPE_FLOAT)                       \
+    M(TYPE_DOUBLE)                      \
+    M(TYPE_VARCHAR)                     \
+    M(TYPE_CHAR)                        \
+    M(TYPE_DATE)                        \
+    M(TYPE_DATETIME)                    \
+    M(TYPE_DECIMALV2)                   \
+    M(TYPE_DECIMAL32)                   \
+    M(TYPE_DECIMAL64)                   \
+    M(TYPE_DECIMAL128)                  \
+    M(TYPE_BOOLEAN)
+
+#define APPLY_FOR_ALL_TYPE(M)       \
+    APPLY_FOR_ALL_PRIMITIVE_TYPE(M) \
+    M(TYPE_STRUCT)                  \
+    M(TYPE_ARRAY)                   \
+    M(TYPE_MAP)                     \
+    M(TYPE_HLL)                     \
+    M(TYPE_OBJECT)                  \
+    M(TYPE_PERCENTILE)              \
+    M(TYPE_DECIMAL)                 \
+    M(TYPE_TIME)                    \
+    M(TYPE_BINARY)
+
+#define APPLY_FOR_ALL_PRIMITIVE_TYPE_WITH_NULL(M) \
+    APPLY_FOR_ALL_PRIMITIVE_TYPE(M)               \
+    M(TYPE_NULL)
+
+#define APPLY_FOR_ALL_THRIFT_TYPE(M) \
+    M(BOOLEAN)                       \
+    M(TINYINT)                       \
+    M(SMALLINT)                      \
+    M(INT)                           \
+    M(BIGINT)                        \
+    M(FLOAT)                         \
+    M(DOUBLE)                        \
+    M(DATE)                          \
+    M(DATETIME)                      \
+    M(BINARY)                        \
+    M(DECIMAL)                       \
+    M(CHAR)                          \
+    M(LARGEINT)                      \
+    M(VARCHAR)                       \
+    M(HLL)                           \
+    M(DECIMALV2)                     \
+    M(TIME)                          \
+    M(OBJECT)                        \
+    M(PERCENTILE)                    \
+    M(DECIMAL32)                     \
+    M(DECIMAL64)                     \
+    M(DECIMAL128)
 
 } // namespace starrocks
