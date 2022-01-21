4a90c664924bc933b9f3ce1a8b163be7d8a20b53
liuyehcf <1559500551@qq.com>
2022-01-21 11:10:01 +0800

    Update

--- be/src/runtime/data_stream_recvr.cc
+++ be/src/runtime/data_stream_recvr.cc
@@ -116,8 +116,7 @@ public:
 private:
     struct ChunkItem {
         int64_t chunk_bytes = 0;
-        // -1 means disable pipeline level shuffle
-        // >=0 means enable pipeline level shuffle
+        // Invalid if SenderQueue::_is_pipeline_level_shuffle is false
         int32_t driver_sequence = -1;
         ChunkUniquePtr chunk_ptr;
         // When the memory of the ChunkQueue exceeds the limit,
@@ -180,7 +179,7 @@ void DataStreamRecvr::SenderQueue::short_circuit_for_pipeline(const int32_t driv
 
     auto iter = _chunk_queue.begin();
     while (iter != _chunk_queue.end()) {
-        if (iter->driver_sequence == driver_sequence) {
+        if (_is_pipeline_level_shuffle && iter->driver_sequence == driver_sequence) {
             if (iter->closure != nullptr) {
                 iter->closure->Run();
             }
@@ -196,11 +195,15 @@ bool DataStreamRecvr::SenderQueue::has_output_for_pipeline(const int32_t driver_
     // This method may be invoked by PipelineDriverPoller and GlobalDriverDispatcher simultaneously
     // when some of the driver_sequences has no chunks to get but the others do, then GlobalDriverDispatcher
     // may be starved because PipelineDriverPoller will call this method repeatedly with almost no interval
+    // Both false positives and false negatives are allowed here
     {
         if (_is_cancelled) {
             return false;
         }
         if (_is_pipeline_level_shuffle && !_has_chunks_per_driver_sequence[driver_sequence]) {
+            return false;
+        }
+        if (_chunk_queue.empty()) {
             return false;
         }
     }
@@ -220,12 +223,10 @@ bool DataStreamRecvr::SenderQueue::has_output_for_pipeline(const int32_t driver_
             return false;
         }
 
-        auto iter = _chunk_queue.begin();
-        while (iter != _chunk_queue.end()) {
-            if (iter->driver_sequence == -1 || iter->driver_sequence == driver_sequence) {
+        for (auto& item : _chunk_queue) {
+            if (!_is_pipeline_level_shuffle || item.driver_sequence == driver_sequence) {
                 return true;
             }
-            ++iter;
         }
 
         if (_is_pipeline_level_shuffle) {
@@ -329,7 +330,7 @@ Status DataStreamRecvr::SenderQueue::get_chunk_for_pipeline(vectorized::Chunk** 
 
     auto iter = _chunk_queue.begin();
     while (iter != _chunk_queue.end()) {
-        if (iter->driver_sequence == -1 || iter->driver_sequence == driver_sequence) {
+        if (!_is_pipeline_level_shuffle || iter->driver_sequence == driver_sequence) {
             *chunk = iter->chunk_ptr.release();
             auto* closure = iter->closure;
             _recvr->_num_buffered_bytes -= iter->chunk_bytes;
@@ -511,11 +512,12 @@ Status DataStreamRecvr::SenderQueue::add_chunks(const PTransmitChunkParams& requ
         const auto original_size = _chunk_queue.size();
         for (auto& item : chunks) {
             // This chunks may contains different driver_sequence
-            if (item.driver_sequence >= 0 &&
-                _short_circuit_driver_sequences.find(item.driver_sequence) != _short_circuit_driver_sequences.end()) {
-                continue;
-            }
             if (_is_pipeline_level_shuffle) {
+                // Some pipelines may be short-circuit, so we just drop the chunk we received
+                if (_short_circuit_driver_sequences.find(item.driver_sequence) !=
+                    _short_circuit_driver_sequences.end()) {
+                    continue;
+                }
                 _has_chunks_per_driver_sequence[item.driver_sequence] = true;
             }
             _chunk_queue.emplace_back(std::move(item));
@@ -659,17 +661,19 @@ Status DataStreamRecvr::SenderQueue::add_chunks_and_keep_order(const PTransmitCh
             // Now, all the packets with sequance <= unprocessed_sequence have been received
             // so chunks of unprocessed_sequence can be flushed to ready queue
             for (auto& item : unprocessed_chunk_queue) {
-                // This chunks may contains different driver_sequence
-                if (item.driver_sequence >= 0 && _short_circuit_driver_sequences.find(item.driver_sequence) !=
-                                                         _short_circuit_driver_sequences.end()) {
-                    // We may buffered closure in last reception, but the branch of the driver_sequence may
-                    // become short-circuit now, so we make sure to invoke the closure
-                    if (item.closure != nullptr) {
-                        item.closure->Run();
-                    }
-                    continue;
-                }
+                // This chunks may contains different driver_sequence when enable local pass through
+                // So we use driver_sequence of each chunk instead of request's driver_sequence
                 if (_is_pipeline_level_shuffle) {
+                    // Some pipelines may be short-circuit, so we just drop the chunk we received
+                    if (_short_circuit_driver_sequences.find(item.driver_sequence) !=
+                        _short_circuit_driver_sequences.end()) {
+                        // We may buffered closure in last reception, but the branch of the driver_sequence may
+                        // become short-circuit now, so we make sure to invoke the closure
+                        if (item.closure != nullptr) {
+                            item.closure->Run();
+                        }
+                        continue;
+                    }
                     _has_chunks_per_driver_sequence[item.driver_sequence] = true;
                 }
                 _chunk_queue.emplace_back(std::move(item));
