86934f1f4d8275a498c2563fddfdf2b6efa596bb
trueeyu <lxhhust350@qq.com>
2022-01-24 19:36:56 +0800

    Support array_agg (#3011)

--- fe/fe-core/src/main/java/com/starrocks/analysis/FunctionCallExpr.java
+++ fe/fe-core/src/main/java/com/starrocks/analysis/FunctionCallExpr.java
@@ -396,7 +396,7 @@ public class FunctionCallExpr extends Expr {
             return;
         }
 
-        if (fnName.getFunction().equalsIgnoreCase("group_concat")) {
+        if (fnName.getFunction().equalsIgnoreCase(FunctionSet.GROUP_CONCAT)) {
             if (children.size() > 2 || children.isEmpty()) {
                 throw new AnalysisException(
                         "group_concat requires one or two parameters: " + this.toSql());
@@ -453,6 +453,15 @@ public class FunctionCallExpr extends Expr {
         Expr arg = getChild(0);
         if (arg == null) {
             return;
+        }
+
+        if (fnName.getFunction().equalsIgnoreCase(FunctionSet.ARRAY_AGG)) {
+            if (fnParams.isDistinct()) {
+                throw new AnalysisException("array_agg does not support DISTINCT");
+            }
+            if (arg.type.isDecimalV3()) {
+                throw new AnalysisException("array_agg does not support DecimalV3");
+            }
         }
 
         // SUM and AVG cannot be applied to non-numeric types
