a62698605dea1827c53deb20efecd62cbb43d96a
Binglin Chang <decstery@gmail.com>
2022-01-27 18:47:27 +0800

    Optimize KVStore::iterate to prevent unecessary read of deleted entries (#3150)
    When BE starts, it will load all tablets; for the primary key tablet, the load process will call `KVStore::iterate` several times to load logs, pending_rowsets, rowsets and delvecs. They all call `rocksdb::Iterator::Seek` to do some prefix range query. Currently, its implementation is suboptimal; for example, suppose the records in RocksDB is:
    ```
    'a' 'd0001'(deleted) 'd0002'(deleted) .. 'd9999'(deleted) 'e'
    ``` 
    `Seek('c')` will first iterate all 9999 deleted entries, then seek to 'e', then find there is no record with prefix 'c', which is inefficient. 
    This PR sets the iterate_upper_bound option when doing prefix range query, so for the above example, seek will stop at record 'd0001' and stop.
    
    In my test with data provided by the user in #3098, before this PR, KVStore::iterate takes >100ms, and the whole load process takes >20min; after this PR, each call takes <1ms the entire load process only takes 3s.

--- be/src/storage/kv_store.cpp
+++ be/src/storage/kv_store.cpp
@@ -182,10 +182,37 @@ Status KVStore::remove(ColumnFamilyIndex column_family_index, const std::string&
     return to_status(s);
 }
 
+// get iterate upper bound for prefix range query, for example:
+//
+//   prefix(ascii):      00001
+//   prefix_end(ascii):  00002
+//
+//   prefix(hex):      00ffff
+//   prefix_end(hex):  010000
+static std::string get_iterate_upper_bound(const std::string& prefix) {
+    std::string ret = prefix;
+    for (ssize_t i = (ssize_t)ret.size() - 1; i >= 0; i--) {
+        if ((uint8_t)ret[i] < 255) {
+            ret[i]++;
+            return ret;
+        } else {
+            ret[i] = 0;
+        }
+    }
+    return std::string();
+}
+
 Status KVStore::iterate(ColumnFamilyIndex column_family_index, const std::string& prefix,
                         std::function<bool(std::string_view, std::string_view)> const& func) {
     rocksdb::ColumnFamilyHandle* handle = _handles[column_family_index];
-    std::unique_ptr<Iterator> it(_db->NewIterator(ReadOptions(), handle));
+    auto opts = ReadOptions();
+    std::string upper_bound = get_iterate_upper_bound(prefix);
+    rocksdb::Slice upper_bound_slice;
+    if (!upper_bound.empty()) {
+        upper_bound_slice = rocksdb::Slice(upper_bound);
+        opts.iterate_upper_bound = &upper_bound_slice;
+    }
+    std::unique_ptr<Iterator> it(_db->NewIterator(opts, handle));
     if (prefix.empty()) {
         it->SeekToFirst();
     } else {
