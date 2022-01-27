5d815852be76ebfcfd9949c54473515415ccbe77
caneGuy <zhoukang199191@gmail.com>
2022-01-27 12:25:49 +0800

    add ut

---
+++ fe/fe-core/src/test/java/com/starrocks/sql/optimizer/rule/transformation/PruneHDFSScanColumnRuleTest.java
@@ -0,0 +1,15 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
+
+package com.starrocks.sql.optimizer.rule.transformation;
+
+import com.starrocks.sql.optimizer.operator.logical.LogicalIcebergScanOperator;
+import mockit.Mocked;
+import org.junit.Test;
+
+public class PruneHDFSScanColumnRuleTest {
+
+    @Test
+    public void transform(@Mocked LogicalIcebergScanOperator operator) {
+
+    }
+}
