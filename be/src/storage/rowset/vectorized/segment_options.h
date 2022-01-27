ad2b673aabdc960862c65131d0f7b0a41689a295
stdpain <34912776+stdpain@users.noreply.github.com>
2022-01-27 14:21:23 +0800

    [UT] add UT to cover global dict (#3119)

--- be/src/storage/rowset/vectorized/segment_options.h
+++ be/src/storage/rowset/vectorized/segment_options.h
@@ -59,7 +59,7 @@ public:
 
     const ColumnIdToGlobalDictMap* global_dictmaps = &EMPTY_GLOBAL_DICTMAPS;
     const std::unordered_set<uint32_t>* unused_output_column_ids = nullptr;
-    ;
+
     bool has_delete_pred = false;
 };
 
