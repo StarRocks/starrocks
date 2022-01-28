052d1cb268fe21b09c87e03e54e85a65c481a9c8
DeepThinker666 <548440636@qq.com>
2022-01-28 17:28:56 +0800

    fix comment

--- be/src/storage/task/engine_clone_task.cpp
+++ be/src/storage/task/engine_clone_task.cpp
@@ -72,13 +72,12 @@ EngineCloneTask::EngineCloneTask(MemTracker* mem_tracker, const TCloneReq& clone
 Status EngineCloneTask::execute() {
     MemTracker* prev_tracker = tls_thread_status.set_mem_tracker(_mem_tracker.get());
     auto tablet_manager = StorageEngine::instance()->tablet_manager();
+    // Prevent the snapshot directory from been removed by the path GC worker.
+    tablet_manager->register_clone_tablet(_clone_req.tablet_id);
     DeferOp op([&] {
         tls_thread_status.set_mem_tracker(prev_tracker);
         tablet_manager->unregister_clone_tablet(_clone_req.tablet_id);
     });
-
-    // Prevent the snapshot directory from been removed by the path GC worker.
-    tablet_manager->register_clone_tablet(_clone_req.tablet_id);
     auto tablet = tablet_manager->get_tablet(_clone_req.tablet_id, false);
     if (tablet != nullptr) {
         std::shared_lock rlock(tablet->get_migration_lock(), std::try_to_lock);
