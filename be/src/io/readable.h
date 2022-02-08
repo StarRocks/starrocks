ed27a516a77a85ab3ddb99258233a08beb463585
sduzh <zhuming9011@qq.com>
2022-02-08 17:18:29 +0800

    Add read_fully() and read_at_fully() methods
    Summary:
    Add Readable::read_fully() interface and defalut implementation;
    Add RanndomAccessFile::read_at_fully() interface and default
    implementation;

--- be/src/io/readable.h
+++ be/src/io/readable.h
@@ -12,9 +12,17 @@ class Readable {
 public:
     virtual ~Readable() = default;
 
-    // Read at most |count| bytes data into |data|.
+    // Read up to |count| bytes into |data|.
     // Returns the number of bytes read, 0 means EOF.
     virtual StatusOr<int64_t> read(void* data, int64_t count) = 0;
+
+    // Read exactly |count| bytes into |data|.
+    // This method does not return the number of bytes read because either
+    // (1) the entire |count| bytes is read
+    // (2) the end of the stream is reached
+    // If the eof is reached, an IO error is returned, leave the content of
+    // |data| buffer unspecified.
+    virtual Status read_fully(void* data, int64_t count);
 };
 
 } // namespace starrocks::io
