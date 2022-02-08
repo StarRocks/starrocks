cf19e3c8762061d63b1df2e63447ea6bff63135d
Binglin Chang <decstery@gmail.com>
2022-02-08 11:07:02 +0800

    Implement MutableIndex basic operations

--- be/src/storage/persistent_index.h
+++ be/src/storage/persistent_index.h
@@ -47,46 +47,58 @@ struct IndexHash {
     uint64_t tag() const { return hash & 0xff; }
 };
 
-struct L1Checks {
+struct KeysInfo {
     std::vector<uint32_t> key_idxes;
     std::vector<uint64_t> hashes;
 };
 
 class MutableIndex {
 public:
-    virtual ~MutableIndex() {}
+    MutableIndex();
+    virtual ~MutableIndex();
 
     // batch get
     // |n|: size of key/value array
     // |keys|: key array as raw buffer
     // |values|: value array for return values
-    // |l1_checks|: information of keys that need to be further checked in L1
-    // |num_found|: add the number of keys found in L0 to this argument
-    virtual Status get(size_t n, const uint8_t* keys, IndexValue* values, L1Checks* l1_checks, size_t* num_found);
+    // |not_found|: information of keys not found, which need to be further checked in next level
+    // |num_found|: add the number of keys found to this argument
+    virtual Status get(size_t n, const void* keys, IndexValue* values, KeysInfo* not_found,
+                       size_t* num_found) const = 0;
+
+    // batch upsert and get old value
+    // |n|: size of key/value array
+    // |keys|: key array as raw buffer
+    // |values|: value array
+    // |old_values|: return old values for updates, or set to NullValue for inserts
+    // |not_found|: information of keys not found, which need to be further checked in next level
+    // |num_found|: add the number of keys found to this argument
+    virtual Status upsert(size_t n, const void* keys, const IndexValue* values, IndexValue* old_values,
+                          KeysInfo* not_found, size_t* num_found) = 0;
 
     // batch upsert
     // |n|: size of key/value array
     // |keys|: key array as raw buffer
     // |values|: value array
-    // |old_values|: return old values for updates, or set to NullValue for inserts
-    // |l1_checks|: information of keys that need to be further checked in L1
-    // |num_found|: add the number of keys found in L0 to this argument
-    virtual Status upsert(size_t n, const uint8_t* keys, const IndexValue* values, IndexValue* old_values,
-                          L1Checks* l1_checks, size_t* num_found);
+    // |not_found|: information of keys not found, which need to be further checked in next level
+    // |num_found|: add the number of keys found(or already exist) to this argument
+    virtual Status upsert(size_t n, const void* keys, const IndexValue* values, KeysInfo* not_found,
+                          size_t* num_found) = 0;
 
     // batch insert
     // |n|: size of key/value array
     // |keys|: key array as raw buffer
     // |values|: value array
-    virtual Status insert(size_t n, const uint8_t* keys, const IndexValue* values);
+    virtual Status insert(size_t n, const void* keys, const IndexValue* values) = 0;
 
     // batch erase(delete)
     // |n|: size of key/value array
     // |keys|: key array as raw buffer
     // |old_values|: return old values for updates, or set to NullValue if not exists
-    // |l1_checks|: information of keys that need to be further checked in L1
-    // |num_found|: add the number of keys found in L0 to this argument
-    virtual Status erase(size_t n, const uint8_t* keys, IndexValue* old_values, L1Checks* l1_checks, size_t* num_found);
+    // |not_found|: information of keys not found, which need to be further checked in next level
+    // |num_found|: add the number of keys found to this argument
+    virtual Status erase(size_t n, const void* keys, IndexValue* old_values, KeysInfo* not_found,
+                         size_t* num_found) = 0;
 
     static StatusOr<std::unique_ptr<MutableIndex>> create(size_t key_size);
 };
@@ -96,13 +108,13 @@ public:
     // batch get
     // |n|: size of key/value array
     // |keys|: key array as raw buffer
-    // |l1_checks|: information of keys that need to be further checked in L1
+    // |not_found|: information of keys not found in upper level, which needs to be checked in this level
     // |values|: value array for return values
     // |num_found|: add the number of keys found in L1 to this argument
-    Status get(size_t n, const uint8_t* keys, const L1Checks& l1_checks, IndexValue* values, size_t* num_found);
+    Status get(size_t n, const void* keys, const KeysInfo& keys_info, IndexValue* values, size_t* num_found) const;
 
     // batch check key existence
-    Status check_not_exist(size_t n, const uint8_t* keys);
+    Status check_not_exist(size_t n, const void* keys);
 };
 
 // A persistent primary index contains an in-memory L0 and an on-SSD/NVMe L1,
@@ -156,27 +168,27 @@ public:
     // |n|: size of key/value array
     // |keys|: key array as raw buffer
     // |values|: value array for return values
-    Status get(size_t n, const uint8_t* keys, IndexValue* values);
+    Status get(size_t n, const void* keys, IndexValue* values);
 
     // batch upsert
     // |n|: size of key/value array
     // |keys|: key array as raw buffer
     // |values|: value array
     // |old_values|: return old values for updates, or set to NullValue for inserts
-    Status upsert(size_t n, const uint8_t* keys, const IndexValue* values, IndexValue* old_values);
+    Status upsert(size_t n, const void* keys, const IndexValue* values, IndexValue* old_values);
 
     // batch insert, return error if key already exists
     // |n|: size of key/value array
     // |keys|: key array as raw buffer
     // |values|: value array
     // |check_l1|: also check l1 for insertion consistency(key must not exist previously), may imply heavy IO costs
-    Status insert(size_t n, const uint8_t* keys, const IndexValue* values, bool check_l1);
+    Status insert(size_t n, const void* keys, const IndexValue* values, bool check_l1);
 
     // batch erase
     // |n|: size of key/value array
     // |keys|: key array as raw buffer
     // |old_values|: return old values if key exist, or set to NullValue if not
-    Status erase(size_t n, const uint8_t* keys, IndexValue* old_values);
+    Status erase(size_t n, const void* keys, IndexValue* old_values);
 
 private:
     // index storage directory
