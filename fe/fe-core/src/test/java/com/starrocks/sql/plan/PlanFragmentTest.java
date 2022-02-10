df13d874055742de9289ea661e95a4983fbdc746
zihe.liu <ziheliu1024@gmail.com>
2022-02-10 18:39:43 +0800

    Fix: bitmap_count(null) returns 0 instead of null (#3268)

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/PlanFragmentTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/PlanFragmentTest.java
@@ -5630,4 +5630,14 @@ public class PlanFragmentTest extends PlanTestBase {
                 "  |  <slot 4> : 4: sum\n" +
                 "  |  <slot 8> : if(CAST(1: v1 AS BOOLEAN), 1, NULL)"));
     }
+
+    @Test
+    public void testBitmapCount() throws Exception {
+        String sql = "SELECT 1 FROM t0 LEFT OUTER JOIN t1 ON t0.v1=t1.v4 " +
+                "WHERE NOT CAST(bitmap_count(CASE WHEN t1.v4 in (10000) THEN bitmap_hash('abc') END) AS BOOLEAN)";
+        String plan = getFragmentPlan(sql);
+        Assert.assertTrue(plan.contains("join op: LEFT OUTER JOIN (BROADCAST)"));
+        Assert.assertTrue(plan.contains(
+                "other predicates: NOT (CAST(bitmap_count(if(4: v4 = 10000, bitmap_hash('abc'), NULL)) AS BOOLEAN))"));
+    }
 }
