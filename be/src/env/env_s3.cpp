c51711ad012b0785b41c7f496591ecbd0b15f4e5
sduzh <zhuming9011@qq.com>
2022-02-17 20:27:30 +0800

    Fix compile

--- be/src/env/env_s3.cpp
+++ be/src/env/env_s3.cpp
@@ -13,12 +13,12 @@ namespace starrocks {
 
 // ==================================  S3RandomAccessFile  ==========================================
 
-// class for remote read hdfs file
+// class for remote read S3 object file.
 // Now this is not thread-safe.
 class S3RandomAccessFile : public RandomAccessFile {
 public:
-    S3RandomAccessFile(std::unique_ptr<ObjectStoreClient> object_store, std::string bucket, std::string object)
-            : _object_store(std::move(object_store)),
+    S3RandomAccessFile(std::unique_ptr<ObjectStoreClient> client, std::string bucket, std::string object)
+            : _client(std::move(client)),
               _bucket(std::move(bucket)),
               _object(std::move(object)),
               _object_path(fmt::format("s3://{}/{}", _bucket, _object)),
@@ -30,24 +30,15 @@ public:
     Status read_at(uint64_t offset, const Slice& res) const override;
     Status readv_at(uint64_t offset, const Slice* res, size_t res_cnt) const override;
     Status size(uint64_t* size) const override;
-
     const std::string& file_name() const override { return _object_path; }
 
 private:
-    std::unique_ptr<ObjectStoreClient> _object_store;
+    std::unique_ptr<ObjectStoreClient> _client;
     std::string _bucket;
     std::string _object;
     std::string _object_path;
     mutable uint64_t _object_size;
 };
-
-S3RandomAccessFile::S3RandomAccessFile(hdfsFS fs, const std::string& file_name, size_t file_size, bool usePread)
-        : _opened(false),
-          _fs(fs),
-          _file(nullptr),
-          _object_path(file_name),
-          _object_size(file_size),
-          _usePread(usePread) {}
 
 Status S3RandomAccessFile::read(uint64_t offset, Slice* res) const {
     size_t read_size = 0;
@@ -76,17 +67,9 @@ Status S3RandomAccessFile::readv_at(uint64_t offset, const Slice* res, size_t re
 
 Status S3RandomAccessFile::size(uint64_t* size) const {
     if (_object_size == 0) {
-        RETURN_IF_ERROR(_object_store->get_object_size(_bucket, _object, &_object_size));
+        RETURN_IF_ERROR(_client->get_object_size(_bucket, _object, &_object_size));
     }
     *size = _object_size;
-    return Status::OK();
-}
-
-Status S3RandomAccessFile::readv_at(uint64_t offset, const Slice* res, size_t res_cnt) const {
-    for (size_t i = 0; i < res_cnt; i++) {
-        RETURN_IF_ERROR(S3RandomAccessFile::read_at(offset, res[i]));
-        offset += res[i].size;
-    }
     return Status::OK();
 }
 
@@ -96,15 +79,20 @@ StatusOr<std::unique_ptr<RandomAccessFile>> EnvS3::new_random_access_file(const 
 
 StatusOr<std::unique_ptr<RandomAccessFile>> EnvS3::new_random_access_file(const RandomAccessFileOptions& opts,
                                                                           const std::string& path) {
-    if (!is_object_storage_path(path)) {
+    if (!is_object_storage_path(path.c_str())) {
         return Status::InvalidArgument(fmt::format("Invalid S3 path {}", path));
     }
-    std::string bucket = get_bucket_from_namenode(get_namenode_from_path(path));
-    std::string object = file_path.substr(nn.size(), file_path.size() - nn.size());
-    Aws::Client::ClientConfiguration config{.scheme = Aws::Http::Scheme::HTTP,
-                                            .endpointOverride = config::aws_s3_endpoint,
-                                            .maxConnections = config::aws_s3_max_connection};
-    S3Credential cred{.access_key_id = config::aws_access_key_id, .secret_access_key = config::aws_secret_access_key};
+    std::string namenode;
+    RETURN_IF_ERROR(get_namenode_from_path(path, &namenode));
+    std::string bucket = get_bucket_from_namenode(namenode);
+    std::string object = path.substr(namenode.size(), path.size() - namenode.size());
+    Aws::Client::ClientConfiguration config;
+    config.scheme = Aws::Http::Scheme::HTTP;
+    config.endpointOverride = config::aws_s3_endpoint;
+    config.maxConnections = config::aws_s3_max_connection;
+    S3Credential cred;
+    cred.access_key_id = config::aws_access_key_id;
+    cred.secret_access_key = config::aws_secret_access_key;
     auto s3_client = std::make_unique<S3Client>(config, &cred, false);
     return std::make_unique<S3RandomAccessFile>(std::move(s3_client), std::move(bucket), std::move(object));
 }
