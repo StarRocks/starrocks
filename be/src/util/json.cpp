c4078d6342fd7112e748192658399ef7ed2fe42d
mofei <mofei@starrocks.com>
2022-02-20 19:45:06 +0800

    optimize json loading

--- be/src/util/json.cpp
+++ be/src/util/json.cpp
@@ -63,67 +63,167 @@ JsonValue JsonValue::from_string(const Slice& value) {
     return JsonValue(builder.slice());
 }
 
-StatusOr<JsonValue> JsonValue::from_simdjson(simdjson::ondemand::value* value) {
-    namespace sj = simdjson::ondemand;
+namespace so = simdjson::ondemand;
 
-    try {
-        sj::json_type tp = value->type();
-        switch (tp) {
-        case sj::json_type::null: {
-            return from_null();
-        }
-        case sj::json_type::number: {
-            switch (value->get_number_type()) {
-            case sj::number_type::signed_integer:
-                return from_int(value->get_int64());
-            case sj::number_type::unsigned_integer:
-                return from_uint(value->get_uint64());
-            case sj::number_type::floating_point_number:
-                return from_double(value->get_double());
-            }
-        }
-
-        case sj::json_type::string: {
-            std::string_view view = value->get_string();
-            return from_string(Slice(view.data(), view.size()));
-        }
-
-        case sj::json_type::boolean: {
-            return from_bool(value->get_bool());
-        }
-
-        case sj::json_type::array:
-        case sj::json_type::object: {
-            // TODO(mofei) optimize this, avoid convert to string then parse it
-            std::string_view view = simdjson::to_json_string(*value);
-            return parse(Slice(view.data(), view.size()));
-        }
-
-        default: {
-            auto err_msg = strings::Substitute("Unsupported json type: $0", int(tp));
+// Convert SIMD-JSON object to a JsonValue
+class SimdJsonConverter {
+public:
+    static StatusOr<JsonValue> create(so::value value) {
+        try {
+            vpack::Builder builder;
+            RETURN_IF_ERROR(convert(value, {}, &builder));
+            return JsonValue(builder.slice());
+        } catch (simdjson::simdjson_error& e) {
+            std::string_view view(value.get_raw_json_string().raw());
+            auto err_msg = strings::Substitute("Failed to convert simdjson value, json=$0, error=$1", view.data(),
+                                               simdjson::error_message(e.error()));
             return Status::DataQualityError(err_msg);
         }
-        }
-    } catch (simdjson::simdjson_error& e) {
-        std::string_view view = simdjson::to_json_string(*value);
-        auto err_msg = strings::Substitute("Failed to parse value, json=$0, error=$1", view.data(),
-                                           simdjson::error_message(e.error()));
-        return Status::DataQualityError(err_msg);
     }
 
-    return Status::OK();
+    static StatusOr<JsonValue> create(so::object value) {
+        try {
+            vpack::Builder builder;
+            RETURN_IF_ERROR(convert(value, {}, &builder));
+            return JsonValue(builder.slice());
+        } catch (simdjson::simdjson_error& e) {
+            std::string_view view(value.raw_json());
+            auto err_msg = strings::Substitute("Failed to convert simdjson value, json=$0, error=$1", view.data(),
+                                               simdjson::error_message(e.error()));
+            return Status::DataQualityError(err_msg);
+        }
+    }
+
+private:
+    static Status convert(so::value value, std::string_view field_name, vpack::Builder* builder) {
+        switch (value.type()) {
+        case so::json_type::array: {
+            convert(value.get_array().value(), field_name, builder);
+            break;
+        }
+        case so::json_type::object: {
+            convert(value.get_object().value(), field_name, builder);
+            break;
+        }
+        case so::json_type::number: {
+            convert(value.get_number().value(), field_name, builder);
+            break;
+        }
+        case so::json_type::string: {
+            convert(value.get_string().value(), field_name, builder);
+            break;
+        }
+        case so::json_type::boolean: {
+            convert(value.get_bool().value(), field_name, builder);
+            break;
+        }
+        case so::json_type::null: {
+            convert_null(field_name, builder);
+            break;
+        }
+        }
+        return Status::OK();
+    }
+
+    static Status convert(so::object obj, std::string_view field_name, vpack::Builder* builder) {
+        if (field_name.empty()) {
+            builder->add(vpack::Value(vpack::ValueType::Object));
+        } else {
+            builder->add(toStringRef(field_name), vpack::Value(vpack::ValueType::Object));
+        }
+        for (auto field : obj) {
+            std::string_view key(field.key().raw());
+            auto value = field.value().value();
+            RETURN_IF_ERROR(convert(value, key, builder));
+        }
+        builder->close();
+        return Status::OK();
+    }
+
+    static Status convert(so::array arr, std::string_view field_name, vpack::Builder* builder) {
+        if (field_name.empty()) {
+            builder->add(vpack::Value(vpack::ValueType::Array));
+        } else {
+            builder->add(toStringRef(field_name), vpack::Value(vpack::ValueType::Array));
+        }
+        for (auto element : arr) {
+            convert(element.value(), {}, builder);
+        }
+        builder->close();
+        return Status::OK();
+    }
+
+    static inline Status convert(so::number num, std::string_view field_name, vpack::Builder* builder) {
+        switch (num.get_number_type()) {
+        case so::number_type::floating_point_number: {
+            if (field_name.empty()) {
+                builder->add(vpack::Value((num.get_double())));
+            } else {
+                builder->add(toStringRef(field_name), vpack::Value((num.get_double())));
+            }
+            break;
+        }
+        case so::number_type::signed_integer: {
+            if (field_name.empty()) {
+                builder->add(vpack::Value((num.get_int64())));
+            } else {
+                builder->add(toStringRef(field_name), vpack::Value((num.get_int64())));
+            }
+            break;
+        }
+        case so::number_type::unsigned_integer: {
+            if (field_name.empty()) {
+                builder->add(vpack::Value((num.get_uint64())));
+            } else {
+                builder->add(toStringRef(field_name), vpack::Value((num.get_uint64())));
+            }
+            break;
+        }
+        default:
+            __builtin_unreachable();
+        }
+        return Status::OK();
+    }
+
+    static inline Status convert(std::string_view str, std::string_view field_name, vpack::Builder* builder) {
+        if (field_name.empty()) {
+            builder->add(toStringRef(field_name), vpack::Value(str));
+        } else {
+            builder->add(vpack::Value(str));
+        }
+        return Status::OK();
+    }
+
+    static inline Status convert(bool value, std::string_view field_name, vpack::Builder* builder) {
+        if (field_name.empty()) {
+            builder->add(toStringRef(field_name), vpack::Value(value));
+        } else {
+            builder->add(vpack::Value(value));
+        }
+        return Status::OK();
+    }
+
+    static inline Status convert_null(std::string_view field_name, vpack::Builder* builder) {
+        if (field_name.empty()) {
+            builder->add(toStringRef(field_name), vpack::Value(vpack::ValueType::Null));
+        } else {
+            builder->add(vpack::Value(vpack::ValueType::Null));
+        }
+        return Status::OK();
+    }
+
+private:
+    static inline vpack::StringRef toStringRef(std::string_view view) {
+        return vpack::StringRef(view.data(), view.length());
+    }
+};
+
+StatusOr<JsonValue> JsonValue::from_simdjson(simdjson::ondemand::value* value) {
+    return SimdJsonConverter::create(*value);
 }
 
 StatusOr<JsonValue> JsonValue::from_simdjson(simdjson::ondemand::object* obj) {
-    // TODO(mofei) optimize this, avoid convert to string then parse it
-    std::string_view view = obj->raw_json();
-    try {
-        return parse(Slice(view.data(), view.size()));
-    } catch (simdjson::simdjson_error& e) {
-        auto err_msg = strings::Substitute("Failed to parse value, json=$0, error=$1", view.data(),
-                                           simdjson::error_message(e.error()));
-        return Status::DataQualityError(err_msg);
-    }
+    return SimdJsonConverter::create(*obj);
 }
 
 StatusOr<JsonValue> JsonValue::parse(const Slice& src) {
