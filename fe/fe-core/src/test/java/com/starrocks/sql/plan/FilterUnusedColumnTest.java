e9a7382b12c75f8f425931ff7ee2e716698b257c
HangyuanLiu <460660596@qq.com>
2022-02-22 11:15:03 +0800

    Remove output columns in physical scan pass by logical scan  (#3541)

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/FilterUnusedColumnTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/FilterUnusedColumnTest.java
@@ -2,9 +2,6 @@
 
 package com.starrocks.sql.plan;
 
-import javax.swing.text.html.parser.DTD;
-
-import com.starrocks.catalog.ScalarType;
 import com.starrocks.common.FeConstants;
 import com.starrocks.utframe.StarRocksAssert;
 import org.junit.Assert;
@@ -18,7 +15,8 @@ public class FilterUnusedColumnTest extends PlanTestBase {
         StarRocksAssert starRocksAssert = new StarRocksAssert(connectContext);
         starRocksAssert.withTable("CREATE TABLE tpcds_100g_date_dim (d_dow INTEGER NOT NULL,\n" +
                 "                             d_day_name char(9) NULL,\n" +
-                "                             d_current_week char(1) NULL) \n" +
+                "                             d_current_week char(1) NULL,\n" +
+                "                             d_date DATE NULL) \n" +
                 "ENGINE=OLAP\n" +
                 "DUPLICATE KEY(`d_dow`)\n" +
                 "COMMENT \"OLAP\"\n" +
@@ -49,6 +47,17 @@ public class FilterUnusedColumnTest extends PlanTestBase {
                 "            ref_0.d_dow as c1 from tpcds_100g_date_dim as ref_0 \n" +
                 "            where ref_0.d_day_name = \"dd\" limit 137;\n";
         String plan = getThriftPlan(sql);
+        System.out.println(plan);
         Assert.assertTrue(plan.contains("unused_output_column_name:[d_day_name]"));
+    }
+
+    @Test
+    public void testFilterProjection() throws Exception {
+        connectContext.getSessionVariable().enableTrimOnlyFilteredColumnsInScanStage();
+        String sql = "select\n" +
+                "            ref_0.d_dow as c1, year(d_date) as year from tpcds_100g_date_dim as ref_0 \n" +
+                "            where ref_0.d_date = \'1997-12-31\' limit 137;\n";
+        String plan = getThriftPlan(sql);
+        Assert.assertTrue(plan.contains("unused_output_column_name:[]"));
     }
 }
