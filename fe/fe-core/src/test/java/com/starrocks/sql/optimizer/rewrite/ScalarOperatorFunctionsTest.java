8b8e9ba9b2f83572143178ff7a39bcfd970ca73e
Seaven <seaven_7@qq.com>
2022-02-23 17:36:20 +0800

    Fix Scalar Functions bugs (#3631)

--- fe/fe-core/src/test/java/com/starrocks/sql/optimizer/rewrite/ScalarOperatorFunctionsTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/optimizer/rewrite/ScalarOperatorFunctionsTest.java
@@ -219,6 +219,11 @@ public class ScalarOperatorFunctionsTest {
         Assert.assertThrows(IllegalArgumentException.class, () -> ScalarOperatorFunctions
                 .dateFormat(ConstantOperator.createDate(LocalDateTime.of(2020, 2, 21, 13, 4, 5)),
                         ConstantOperator.createVarchar("%X")).getVarchar());
+        assertTrue(ScalarOperatorFunctions.dateFormat(testDate, ConstantOperator.createVarchar(""))
+                .isNull());
+        assertEquals("  ",
+                ScalarOperatorFunctions.dateFormat(testDate, ConstantOperator.createVarchar("  "))
+                        .getVarchar());
     }
 
     @Test
@@ -571,11 +576,6 @@ public class ScalarOperatorFunctionsTest {
                         .toString());
         assertTrue(ScalarOperatorFunctions.modDecimal(O_DECIMAL128P38S20_100, O_DECIMAL128P38S20_100).getType()
                 .isDecimalV3());
-    }
-
-    @Test
-    public void modLargeInt() {
-        assertEquals("0", ScalarOperatorFunctions.modLargeInt(O_LI_100, O_LI_100).getLargeInt().toString());
     }
 
     @Test
