2cae589356d44fc11a7c182191e9eb565b5113b8
dorianzheng <xingzhengde72@gmail.com>
2022-02-23 20:56:20 +0800

    metadata compatibility for unknown external table
    Signed-off-by: dorianzheng <xingzhengde72@gmail.com>

---
+++ fe/fe-core/src/main/java/com/starrocks/catalog/UnrecognizedTable.java
@@ -0,0 +1,26 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
+
+package com.starrocks.catalog;
+
+import com.starrocks.common.io.Text;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.DataInput;
+import java.io.IOException;
+
+public class UnrecognizedTable extends Table {
+    private static final Logger LOG = LogManager.getLogger(IcebergTable.class);
+
+    public UnrecognizedTable() {
+        super(TableType.UNRECOGNIZED);
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        super.readFields(in);
+
+        String json = Text.readString(in);
+        LOG.error("Unrecognized table metadata {}", json);
+    }
+}
