a079fc6d435c07e3316066eab1dfdd5ab7fd4914
xlwh <1079039435@qq.com>
2022-02-24 21:02:51 +0800

    Fix clang-format

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -212,7 +212,6 @@ CONF_mInt64(column_dictionary_key_size_threshold, "0");
 // The memory_limitation_per_thread_for_schema_change unit GB.
 CONF_mInt32(memory_limitation_per_thread_for_schema_change, "2");
 
-
 CONF_mInt32(update_cache_expire_sec, "360");
 CONF_mInt32(file_descriptor_cache_clean_interval, "3600");
 CONF_mInt32(disk_stat_monitor_interval, "5");
