999c664c1309c924b04d5501f40667939ee8337c
eyes_on_me <3675229+silverbullet233@users.noreply.github.com>
2022-03-01 15:24:56 +0800

    [JDBC External Table] Phase 2: support create jdbc table (#3353)

--- fe/fe-core/src/main/java/com/starrocks/catalog/Table.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/Table.java
@@ -60,7 +60,8 @@ public class Table extends MetaObject implements Writable {
         HIVE,
         ICEBERG,
         HUDI,
-        ODBC
+        ODBC,
+        JDBC,
     }
 
     protected long id;
@@ -201,6 +202,8 @@ public class Table extends MetaObject implements Writable {
             table = new ExternalOlapTable();
         } else if (type == TableType.ICEBERG) {
             table = new IcebergTable();
+        } else if (type == TableType.JDBC) {
+            table = new JDBCTable();
         } else {
             throw new IOException("Unknown table type: " + type.name());
         }
