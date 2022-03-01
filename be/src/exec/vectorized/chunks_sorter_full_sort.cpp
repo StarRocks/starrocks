4e2d14c74dce4d371d2a5a5022e8d02140622b33
mofei <mofei@starrocks.com>
2022-03-01 16:59:22 +0800

    refactor SortHelper comparator

--- be/src/exec/vectorized/chunks_sorter_full_sort.cpp
+++ be/src/exec/vectorized/chunks_sorter_full_sort.cpp
@@ -2,6 +2,8 @@
 
 #include "chunks_sorter_full_sort.h"
 
+#include <type_traits>
+
 #include "column/type_traits.h"
 #include "exec/vectorized/chunks_sorter.h"
 #include "exprs/expr.h"
@@ -16,6 +18,123 @@ namespace starrocks::vectorized {
 // SortHelper functions only work for full sort.
 class SortHelper {
 public:
+    template <typename CppTypeName>
+    struct SortItem {
+        CppTypeName value;
+        uint32_t index_in_chunk;
+        uint32_t permutation_index; // sequence index for keeping sort stable.
+    };
+
+    template <class CppType, bool less>
+    struct SortItemComparator {
+        static constexpr int compareInt(int lhs, int rhs) {
+            if (lhs > rhs) {
+                return 1;
+            } else if (lhs == rhs) {
+                return 0;
+            } else {
+                return -1;
+            }
+        }
+
+        int compare(const SortItem<CppType>& lhs, const SortItem<CppType>& rhs) {
+            using cpp_type = std::remove_cv_t<CppType>;
+            if constexpr (!std::is_same_v<cpp_type, Slice>) {
+                if (lhs.value == rhs.value) {
+                    return compareInt(lhs.permutation_index, rhs.permutation_index);
+                } else if (lhs.value < rhs.value) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+
+            } else {
+                int x = lhs.value.compare(rhs.value);
+                if (x == 0) {
+                    return compareInt(lhs.permutation_index, rhs.permutation_index);
+                } else {
+                    return x;
+                }
+            }
+        }
+
+        bool operator()(const SortItem<CppType>& lhs, const SortItem<CppType>& rhs) {
+            if constexpr (less) {
+                return compare(lhs, rhs) < 0;
+            } else {
+                return compare(lhs, rhs) > 0;
+            }
+        }
+    };
+
+    template <PrimitiveType ptype, bool less, bool stable>
+    struct PermutationItemComparator {
+        using ColumnType = RunTimeColumnType<ptype>;
+
+    private:
+        // down_cast to avoid virtual fucntion call
+        ColumnType* _lhs_column;
+        ColumnType* _rhs_column;
+
+    public:
+        PermutationItemComparator(Column* lhs_column, Column* rhs_column)
+                : _lhs_column(down_cast<ColumnType*>(lhs_column)), _rhs_column(down_cast<ColumnType*>(rhs_column)) {}
+
+        bool operator()(const PermutationItem& lhs, const PermutationItem& rhs) {
+            int x = _lhs_column->compare_at(lhs.index_in_chunk, rhs.index_in_chunk, *_rhs_column, 1);
+            if constexpr (stable) {
+                if (x == 0) {
+                    return lhs.permutation_index < rhs.permutation_index;
+                }
+                if constexpr (less) {
+                    return x < 0;
+                } else {
+                    return x > 0;
+                }
+            } else {
+                if constexpr (less) {
+                    return x < 0;
+                } else {
+                    return x > 0;
+                }
+            }
+        }
+    };
+
+    struct SingleColumnSortDesc {
+        PrimitiveType sort_type;
+        bool nullable;
+        bool stable;
+        bool is_asc;
+        bool null_first;
+    };
+
+    struct SingleColumnSorter {
+        template <PrimitiveType ptype>
+        Status operator()(RuntimeState* state, Column* column, const SingleColumnSortDesc& desc, Permutation& perm) {
+            if (desc.stable) {
+                if (desc.nullable) {
+                    return sort_on_nullable_column<ptype, true>(state, column, desc.is_asc, desc.null_first, perm);
+                } else {
+                    return sort_on_not_null_column<ptype, true>(state, column, desc.is_asc, perm);
+                }
+            } else {
+                if (desc.nullable) {
+                    return sort_on_nullable_column<ptype, false>(state, column, desc.is_asc, desc.null_first, perm);
+                } else {
+                    return sort_on_not_null_column<ptype, false>(state, column, desc.is_asc, perm);
+                }
+            }
+            return Status::OK();
+        }
+    };
+
+    // Sort a single column
+    static Status sort_single_column(RuntimeState* state, Column* column, const SingleColumnSortDesc& desc,
+                                     Permutation& perm) {
+        return type_dispatch_sortable(desc.sort_type, SingleColumnSorter(), state, column, desc, perm);
+    }
+
     // Sort on type-known column, and the column has no NULL value in sorting range.
     template <PrimitiveType PT, bool stable>
     static Status sort_on_not_null_column(RuntimeState* state, Column* column, bool is_asc_order, Permutation& perm) {
@@ -57,25 +176,6 @@ public:
                                                                 is_asc_order, perm, data_offset, data_count);
     }
 
-    // Sort on column that with unknown data type.
-    static Status sort_on_other_column(RuntimeState* state, Column* column, int sort_order_flag, int null_first_flag,
-                                       Permutation& perm) {
-        // decides whether element l precedes element r.
-        auto cmp_fn = [&column, &sort_order_flag, &null_first_flag](const PermutationItem& l,
-                                                                    const PermutationItem& r) -> bool {
-            int cmp = column->compare_at(l.index_in_chunk, r.index_in_chunk, *column, null_first_flag);
-            if (cmp == 0) {
-                return l.permutation_index < r.permutation_index;
-            } else {
-                cmp *= sort_order_flag;
-                return cmp < 0;
-            }
-        };
-        pdqsort(state->cancelled_ref(), perm.begin(), perm.end(), cmp_fn);
-        RETURN_IF_CANCELLED(state);
-        return Status::OK();
-    }
-
 private:
     // Sort on type-known column, and the column has no NULL value in sorting range.
     template <PrimitiveType PT, bool stable>
@@ -84,156 +184,53 @@ private:
         using ColumnTypeName = typename RunTimeTypeTraits<PT>::ColumnType;
         using CppTypeName = typename RunTimeTypeTraits<PT>::CppType;
 
-        // for numeric column: integers, floats, date, datetime, decimals
-        if constexpr (pt_is_fixedlength<PT>) {
-            return sort_on_not_null_fixed_size_column<CppTypeName, stable>(state, column, is_asc_order, perm, offset,
-                                                                           count);
-        }
-
-        // for binary column
-        if constexpr (pt_is_binary<PT>) {
-            return sort_on_not_null_binary_column<stable>(state, column, is_asc_order, perm, offset, count);
-        }
-
-        // for other columns
-        const ColumnTypeName* col = down_cast<ColumnTypeName*>(column);
-        auto less_fn = [&col](const PermutationItem& l, const PermutationItem& r) -> bool {
-            int c = col->compare_at(l.index_in_chunk, r.index_in_chunk, *col, 1);
-            if constexpr (stable) {
-                if (c == 0) {
-                    return l.permutation_index < r.permutation_index;
-                }
-                return c < 0;
-            } else {
-                return c < 0;
-            }
-        };
-        auto greater_fn = [&col](const PermutationItem& l, const PermutationItem& r) -> bool {
-            int c = col->compare_at(l.index_in_chunk, r.index_in_chunk, *col, 1);
-            if constexpr (stable) {
-                if (c == 0) {
-                    return l.permutation_index < r.permutation_index; // first element is greater.
-                }
-                return c > 0;
-            } else {
-                return c > 0;
-            }
-        };
-        size_t end_pos = (count == 0 ? perm.size() : offset + count);
-        if (end_pos > perm.size()) {
-            end_pos = perm.size();
-        }
-        if (is_asc_order) {
-            pdqsort(state->cancelled_ref(), perm.begin() + offset, perm.begin() + end_pos, less_fn);
+        if constexpr (pt_is_fixedlength<PT> || pt_is_binary<PT>) {
+            return _sort_by_sortitem<PT, stable>(state, column, is_asc_order, perm, offset);
         } else {
-            pdqsort(state->cancelled_ref(), perm.begin() + offset, perm.begin() + end_pos, greater_fn);
+            return _sort_by_permutation<PT, stable>(state, column, is_asc_order, perm, offset);
         }
-        RETURN_IF_CANCELLED(state);
-        return Status::OK();
     }
 
-    template <typename CppTypeName>
-    struct SortItem {
-        CppTypeName value;
-        uint32_t index_in_chunk;
-        uint32_t permutation_index; // sequence index for keeping sort stable.
-    };
+    template <PrimitiveType PT, bool stable>
+    static Status _sort_by_sortitem(RuntimeState* state, Column* column, bool is_asc_order, Permutation& perm,
+                                    size_t offset, size_t count = 0) {
+        using ColumnTypeName = typename RunTimeTypeTraits<PT>::ColumnType;
+        using CppTypeName = typename RunTimeTypeTraits<PT>::CppType;
 
-    // Sort string
-    template <bool stable>
-    static Status sort_on_not_null_binary_column(RuntimeState* state, Column* column, bool is_asc_order,
-                                                 Permutation& perm, size_t offset, size_t count = 0) {
         const size_t row_num = (count == 0 || offset + count > perm.size()) ? (perm.size() - offset) : count;
-        auto* binary_column = reinterpret_cast<BinaryColumn*>(column);
-        auto& data = binary_column->get_data();
-        std::vector<SortItem<Slice>> sort_items(row_num);
-        for (uint32_t i = 0; i < row_num; ++i) {
-            sort_items[i] = {data[perm[i + offset].index_in_chunk], perm[i + offset].index_in_chunk, i};
-        }
-        auto less_fn = [](const SortItem<Slice>& l, const SortItem<Slice>& r) -> bool {
-            if constexpr (stable) {
-                int res = l.value.compare(r.value);
-                if (res == 0) {
-                    return l.permutation_index < r.permutation_index;
-                } else {
-                    return res < 0;
-                }
-            } else {
-                int res = l.value.compare(r.value);
-                return res < 0;
-            }
-        };
-        auto greater_fn = [](const SortItem<Slice>& l, const SortItem<Slice>& r) -> bool {
-            if constexpr (stable) {
-                int res = l.value.compare(r.value);
-                if (res == 0) {
-                    return l.permutation_index < r.permutation_index;
-                } else {
-                    return res > 0;
-                }
-            } else {
-                int res = l.value.compare(r.value);
-                return res > 0;
-            }
-        };
-
-        if (is_asc_order) {
-            pdqsort(state->cancelled_ref(), sort_items.begin(), sort_items.end(), less_fn);
-        } else {
-            pdqsort(state->cancelled_ref(), sort_items.begin(), sort_items.end(), greater_fn);
-        }
-        RETURN_IF_CANCELLED(state);
-        for (size_t i = 0; i < row_num; ++i) {
-            perm[i + offset].index_in_chunk = sort_items[i].index_in_chunk;
-        }
-        return Status::OK();
-    }
-
-    // Sort on some numeric column which has no NULL value in sorting range.
-    // Only supports: integers, floats. Not Slice, DecimalV2Value.
-    template <typename CppTypeName, bool stable>
-    static Status sort_on_not_null_fixed_size_column(RuntimeState* state, Column* column, bool is_asc_order,
-                                                     Permutation& perm, size_t offset, size_t count = 0) {
-        // column->size() == perm.size()
-        const size_t row_num = (count == 0 || offset + count > perm.size()) ? (perm.size() - offset) : count;
-        const CppTypeName* data = static_cast<CppTypeName*>((void*)column->mutable_raw_data());
+        ColumnTypeName* col = down_cast<ColumnTypeName*>(column);
+        auto& data = col->get_data();
         std::vector<SortItem<CppTypeName>> sort_items(row_num);
         for (uint32_t i = 0; i < row_num; ++i) {
             sort_items[i] = {data[perm[i + offset].index_in_chunk], perm[i + offset].index_in_chunk, i};
         }
-        auto less_fn = [](const SortItem<CppTypeName>& l, const SortItem<CppTypeName>& r) -> bool {
-            if constexpr (stable) {
-                if (l.value == r.value) {
-                    return l.permutation_index < r.permutation_index; // for stable sort
-                } else {
-                    return l.value < r.value;
-                }
-            } else {
-                return l.value < r.value;
-            }
-        };
-        auto greater_fn = [](const SortItem<CppTypeName>& l, const SortItem<CppTypeName>& r) -> bool {
-            if constexpr (stable) {
-                if (l.value == r.value) {
-                    return l.permutation_index < r.permutation_index; // for stable sort
-                } else {
-                    return l.value > r.value;
-                }
-            } else {
-                return l.value > r.value;
-            }
-        };
 
         if (is_asc_order) {
-            pdqsort(state->cancelled_ref(), sort_items.begin(), sort_items.end(), less_fn);
+            pdqsort(state->cancelled_ref(), sort_items.begin(), sort_items.end(),
+                    SortItemComparator<CppTypeName, true>());
         } else {
-            pdqsort(state->cancelled_ref(), sort_items.begin(), sort_items.end(), greater_fn);
+            pdqsort(state->cancelled_ref(), sort_items.begin(), sort_items.end(),
+                    SortItemComparator<CppTypeName, false>());
         }
         RETURN_IF_CANCELLED(state);
-        // output permutation
         for (size_t i = 0; i < row_num; ++i) {
             perm[i + offset].index_in_chunk = sort_items[i].index_in_chunk;
         }
+        return Status::OK();
+    }
+
+    template <PrimitiveType PT, bool stable>
+    static Status _sort_by_permutation(RuntimeState* state, Column* column, bool is_asc_order, Permutation& perm,
+                                       size_t offset, size_t count = 0) {
+        size_t end_pos = std::min(perm.size(), (count == 0 ? perm.size() : offset + count));
+        if (is_asc_order) {
+            PermutationItemComparator<PT, true, stable> cmp(column, column);
+            pdqsort(state->cancelled_ref(), perm.begin() + offset, perm.begin() + end_pos, cmp);
+        } else {
+            PermutationItemComparator<PT, false, stable> cmp(column, column);
+            pdqsort(state->cancelled_ref(), perm.begin() + offset, perm.begin() + end_pos, cmp);
+        }
+        RETURN_IF_CANCELLED(state);
         return Status::OK();
     }
 };
@@ -415,30 +412,6 @@ Status ChunksSorterFullSort::_sort_by_row_cmp(RuntimeState* state) {
     return Status::OK();
 }
 
-#define CASE_FOR_NULLABLE_COLUMN_SORT(PrimitiveTypeName)                                    \
-    case PrimitiveTypeName: {                                                               \
-        if (stable) {                                                                       \
-            RETURN_IF_ERROR((SortHelper::sort_on_nullable_column<PrimitiveTypeName, true>(  \
-                    state, column, is_asc_order, is_null_first, _sorted_permutation)));     \
-        } else {                                                                            \
-            RETURN_IF_ERROR((SortHelper::sort_on_nullable_column<PrimitiveTypeName, false>( \
-                    state, column, is_asc_order, is_null_first, _sorted_permutation)));     \
-        }                                                                                   \
-        break;                                                                              \
-    }
-
-#define CASE_FOR_NOT_NULL_COLUMN_SORT(PrimitiveTypeName)                                                               \
-    case PrimitiveTypeName: {                                                                                          \
-        if (stable) {                                                                                                  \
-            RETURN_IF_ERROR((SortHelper::sort_on_not_null_column<PrimitiveTypeName, true>(state, column, is_asc_order, \
-                                                                                          _sorted_permutation)));      \
-        } else {                                                                                                       \
-            RETURN_IF_ERROR((SortHelper::sort_on_not_null_column<PrimitiveTypeName, false>(                            \
-                    state, column, is_asc_order, _sorted_permutation)));                                               \
-        }                                                                                                              \
-        break;                                                                                                         \
-    }
-
 // Sort in column style to avoid calling virtual methods of Column.
 Status ChunksSorterFullSort::_sort_by_columns(RuntimeState* state) {
     SCOPED_TIMER(_sort_timer);
@@ -447,13 +420,14 @@ Status ChunksSorterFullSort::_sort_by_columns(RuntimeState* state) {
         return Status::OK();
     }
 
+    // TODO(mofei) optimize it with incremental sort
     for (int col_index = static_cast<int>(_get_number_of_order_by_columns()) - 1; col_index >= 0; --col_index) {
         Column* column = _sorted_segment->order_by_columns[col_index].get();
-        bool stable = col_index != _get_number_of_order_by_columns() - 1;
         if (column->is_constant()) {
             continue;
         }
 
+        bool stable = col_index != _get_number_of_order_by_columns() - 1;
         bool is_asc_order = (_sort_order_flag[col_index] == 1);
         bool is_null_first;
         if (is_asc_order) {
@@ -461,26 +435,13 @@ Status ChunksSorterFullSort::_sort_by_columns(RuntimeState* state) {
         } else {
             is_null_first = (_null_first_flag[col_index] == 1);
         }
+
         ExprContext* expr_ctx = (*_sort_exprs)[col_index];
-        if (column->is_nullable()) {
-            switch (expr_ctx->root()->type().type) {
-                APPLY_FOR_ALL_SCALAR_TYPE(CASE_FOR_NULLABLE_COLUMN_SORT)
-            default: {
-                RETURN_IF_ERROR(SortHelper::sort_on_other_column(state, column, _sort_order_flag[col_index],
-                                                                 _null_first_flag[col_index], _sorted_permutation));
-                break;
-            }
-            }
-        } else {
-            switch (expr_ctx->root()->type().type) {
-                APPLY_FOR_ALL_SCALAR_TYPE(CASE_FOR_NOT_NULL_COLUMN_SORT)
-            default: {
-                RETURN_IF_ERROR(SortHelper::sort_on_other_column(state, column, _sort_order_flag[col_index],
-                                                                 _null_first_flag[col_index], _sorted_permutation));
-                break;
-            }
-            }
-        }
+        PrimitiveType sort_type = expr_ctx->root()->type().type;
+        SortHelper::SingleColumnSortDesc sort_desc{sort_type, stable, column->is_nullable(), is_asc_order,
+                                                   is_null_first};
+        SortHelper::sort_single_column(state, column, sort_desc, _sorted_permutation);
+
         // reset permutation_index
         const size_t size = _sorted_permutation.size();
         for (size_t i = 0; i < size; ++i) {
