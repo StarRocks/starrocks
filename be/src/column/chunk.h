d03d666091fda15d7a8f006cbc7bc57b4683e8a3
trueeyu <lxhhust350@qq.com>
2022-03-02 19:38:28 +0800

    opt the memory usage of memtable sort (#3766)
    Chunk::append_selective() will append all column and then return, so when chunk has many columns and rows, MemTable::sort will use a lot of memory.
    
    The pr add one new function Chunk::rolling_append_selective, it will reserve, copy, destroy column one by one, it will save a lot of memory.
    
    test with lineorder table of SSB (only one bucket, one memtable size limit is 100M):
    
    before optimize: peak memory usage is 279M
    after optimize: peak memory usage is 180M

--- be/src/column/chunk.h
+++ be/src/column/chunk.h
@@ -152,6 +152,11 @@ public:
     // This function will copy the [3, 2] row of src to this chunk.
     void append_selective(const Chunk& src, const uint32_t* indexes, uint32_t from, uint32_t size);
 
+    // This function will append data from src according to the input indexes.
+    // The columns of src chunk will be destroyed after append。
+    // Peak memory usage can be reduced when src chunk has a large number of rows and columns
+    void rolling_append_selective(Chunk& src, const uint32_t* indexes, uint32_t from, uint32_t size);
+
     // Remove rows from this chunk according to the vector |selection|.
     // The n-th row will be removed if selection[n] is zero.
     // The size of |selection| must be equal to the number of rows.
