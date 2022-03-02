c1cec4605c9a68163bc50d93d4b8c324e491a4cf
yan.zhang <dirtysalt1987@gmail.com>
2022-03-03 07:27:16 +0800

    Code refactor on `ScanOperator`  (#3756)

--- be/src/exec/pipeline/olap_scan_operator.h
+++ be/src/exec/pipeline/olap_scan_operator.h
@@ -2,124 +2,48 @@
 
 #pragma once
 
-#include <optional>
-
-#include "exec/pipeline/source_operator.h"
-#include "exec/workgroup/work_group_fwd.h"
-#include "exprs/vectorized/runtime_filter_bank.h"
-#include "runtime/global_dicts.h"
-#include "util/blocking_queue.hpp"
-#include "util/priority_thread_pool.hpp"
+#include "exec/pipeline/pipeline_builder.h"
+#include "exec/pipeline/scan_operator.h"
+#include "exec/scan_node.h"
 
 namespace starrocks {
 
 class Rowset;
 using RowsetSharedPtr = std::shared_ptr<Rowset>;
 
-namespace vectorized {
-class RuntimeFilterProbeCollector;
-}
+}; // namespace starrocks
 
-namespace pipeline {
+namespace starrocks::pipeline {
 
-class OlapScanOperator final : public SourceOperator {
+class OlapScanOperatorFactory final : public ScanOperatorFactory {
 public:
-    OlapScanOperator(OperatorFactory* factory, int32_t id, int32_t plan_node_id, const TOlapScanNode& olap_scan_node,
-                     const std::vector<ExprContext*>& conjunct_ctxs, int64_t limit)
-            : SourceOperator(factory, id, "olap_scan", plan_node_id),
-              _olap_scan_node(olap_scan_node),
-              _conjunct_ctxs(conjunct_ctxs),
-              _limit(limit),
-              _is_io_task_running(MAX_IO_TASKS_PER_OP),
-              _chunk_sources(MAX_IO_TASKS_PER_OP) {}
+    OlapScanOperatorFactory(int32_t id, ScanNode* scan_node);
+
+    ~OlapScanOperatorFactory() override = default;
+
+    Status do_prepare(RuntimeState* state) override;
+    void do_close(RuntimeState* state) override;
+    OperatorPtr do_create(int32_t dop, int32_t driver_sequence) override;
+};
+
+class OlapScanOperator final : public ScanOperator {
+public:
+    OlapScanOperator(OperatorFactory* factory, int32_t id, ScanNode* scan_node);
 
     ~OlapScanOperator() override = default;
 
-    Status prepare(RuntimeState* state) override;
-
-    Status close(RuntimeState* state) override;
-
-    bool has_output() const override;
-
-    bool pending_finish() const override;
-
-    bool is_finished() const override;
-
-    void set_finishing(RuntimeState* state) override;
-
-    StatusOr<vectorized::ChunkPtr> pull_chunk(RuntimeState* state) override;
-
-    void set_io_threads(PriorityThreadPool* io_threads) { _io_threads = io_threads; }
-    void set_workgroup(workgroup::WorkGroupPtr wg);
+    Status do_prepare(RuntimeState* state) override;
+    void do_close(RuntimeState* state) override;
+    ChunkSourcePtr create_chunk_source(MorselPtr morsel) override;
 
 private:
-    static constexpr int MAX_IO_TASKS_PER_OP = 4;
-
-    const size_t _buffer_size = config::pipeline_io_buffer_size;
-
-    // This method is only invoked when current morsel is reached eof
-    // and all cached chunk of this morsel has benn read out
-    Status _pickup_morsel(RuntimeState* state, int chunk_source_index);
-    Status _trigger_next_scan(RuntimeState* state, int chunk_source_index);
-    Status _try_to_trigger_next_scan(RuntimeState* state);
-
     Status _capture_tablet_rowsets();
-
-private:
-    // TODO(hcf) ugly, remove this later
-    RuntimeState* _state = nullptr;
-
-    bool _is_finished = false;
-    int32_t _io_task_retry_cnt = 0;
 
     // The row sets of tablets will become stale and be deleted, if compaction occurs
     // and these row sets aren't referenced, which will typically happen when the tablets
     // of the left table are compacted at building the right hash table. Therefore, reference
     // the row sets into _tablet_rowsets in the preparation phase to avoid the row sets being deleted.
     std::vector<std::vector<RowsetSharedPtr>> _tablet_rowsets;
-    const TOlapScanNode& _olap_scan_node;
-    const std::vector<ExprContext*>& _conjunct_ctxs;
-    PriorityThreadPool* _io_threads = nullptr;
-    std::vector<std::string> _unused_output_columns;
-    // Pass limit info to scan operator in order to improve sql:
-    // select * from table limit x;
-    int64_t _limit; // -1: no limit
-
-    std::atomic<int> _num_running_io_tasks = 0;
-    std::vector<std::atomic<bool>> _is_io_task_running;
-    std::vector<ChunkSourcePtr> _chunk_sources;
-
-    workgroup::WorkGroupPtr _workgroup = nullptr;
 };
 
-class OlapScanOperatorFactory final : public SourceOperatorFactory {
-public:
-    OlapScanOperatorFactory(int32_t id, int32_t plan_node_id, const TOlapScanNode& olap_scan_node,
-                            std::vector<ExprContext*>&& conjunct_ctxs, int64_t limit)
-            : SourceOperatorFactory(id, "olap_scan", plan_node_id),
-              _olap_scan_node(olap_scan_node),
-              _conjunct_ctxs(std::move(conjunct_ctxs)),
-              _limit(limit) {}
-
-    ~OlapScanOperatorFactory() override = default;
-
-    OperatorPtr create(int32_t degree_of_parallelism, int32_t driver_sequence) override {
-        return std::make_shared<OlapScanOperator>(this, _id, _plan_node_id, _olap_scan_node, _conjunct_ctxs, _limit);
-    }
-
-    // OlapScanOperator needs to attach MorselQueue.
-    bool with_morsels() const override { return true; }
-
-    Status prepare(RuntimeState* state) override;
-    void close(RuntimeState* state) override;
-
-private:
-    const TOlapScanNode& _olap_scan_node;
-    std::vector<ExprContext*> _conjunct_ctxs;
-    // Pass limit info to scan operator in order to improve sql:
-    // select * from table limit x;
-    int64_t _limit; // -1: no limit
-};
-
-} // namespace pipeline
-} // namespace starrocks
+} // namespace starrocks::pipeline
---
+++ be/src/exec/pipeline/scan_operator.cpp
@@ -0,0 +1,277 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
+
+#include "exec/pipeline/scan_operator.h"
+
+#include "column/chunk.h"
+#include "exec/pipeline/limit_operator.h"
+#include "exec/pipeline/pipeline_builder.h"
+#include "exec/vectorized/olap_scan_node.h"
+#include "exec/workgroup/scan_executor.h"
+#include "exec/workgroup/work_group.h"
+#include "runtime/current_thread.h"
+#include "runtime/exec_env.h"
+
+namespace starrocks::pipeline {
+
+using starrocks::workgroup::WorkGroupManager;
+
+// ========== ScanOperator ==========
+
+ScanOperator::ScanOperator(OperatorFactory* factory, int32_t id, ScanNode* scan_node)
+        : SourceOperator(factory, id, "olap_scan", scan_node->id()),
+          _scan_node(scan_node),
+          _is_io_task_running(MAX_IO_TASKS_PER_OP),
+          _chunk_sources(MAX_IO_TASKS_PER_OP) {}
+
+Status ScanOperator::prepare(RuntimeState* state) {
+    RETURN_IF_ERROR(SourceOperator::prepare(state));
+
+    if (_workgroup == nullptr) {
+        DCHECK(_io_threads != nullptr);
+        auto num_scan_operators = 1 + state->exec_env()->increment_num_scan_operators(1);
+        if (num_scan_operators > _io_threads->get_queue_capacity()) {
+            state->exec_env()->decrement_num_scan_operators(1);
+            return Status::TooManyTasks(
+                    strings::Substitute("num_scan_operators exceeds queue capacity($0) of pipeline_pool_thread",
+                                        _io_threads->get_queue_capacity()));
+        }
+    }
+
+    RETURN_IF_ERROR(do_prepare(state));
+
+    return Status::OK();
+}
+
+Status ScanOperator::close(RuntimeState* state) {
+    DCHECK(_num_running_io_tasks == 0);
+
+    if (_workgroup == nullptr) {
+        state->exec_env()->decrement_num_scan_operators(1);
+    }
+    for (auto& chunk_source : _chunk_sources) {
+        if (chunk_source != nullptr) {
+            chunk_source->close(state);
+            chunk_source = nullptr;
+        }
+    }
+
+    do_close(state);
+    return Operator::close(state);
+}
+
+bool ScanOperator::has_output() const {
+    if (_is_finished) {
+        return false;
+    }
+
+    for (const auto& chunk_source : _chunk_sources) {
+        if (chunk_source != nullptr && chunk_source->has_output()) {
+            return true;
+        }
+    }
+
+    if (_num_running_io_tasks >= MAX_IO_TASKS_PER_OP) {
+        return false;
+    }
+
+    // Because committing i/o task is trigger ONLY in pull_chunk,
+    // return true if more i/o tasks can be committed.
+
+    // Can pick up more morsels.
+    if (!_morsel_queue->empty()) {
+        return true;
+    }
+
+    // Can trigger_next_scan for the picked-up morsel.
+    for (int i = 0; i < MAX_IO_TASKS_PER_OP; ++i) {
+        if (_chunk_sources[i] != nullptr && !_is_io_task_running[i] && _chunk_sources[i]->has_next_chunk()) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool ScanOperator::pending_finish() const {
+    DCHECK(_is_finished);
+    // If there isn't next morsel, and any io task is active,
+    // we just wait for the io thread to end.
+    return _num_running_io_tasks > 0;
+}
+
+bool ScanOperator::is_finished() const {
+    if (_is_finished) {
+        return true;
+    }
+
+    // Any io task is running or needs to run.
+    if (_num_running_io_tasks > 0 || !_morsel_queue->empty()) {
+        return false;
+    }
+
+    for (const auto& chunk_source : _chunk_sources) {
+        if (chunk_source != nullptr && (chunk_source->has_output() || chunk_source->has_next_chunk())) {
+            return false;
+        }
+    }
+
+    // This scan operator is finished, if no more io tasks are running
+    // or need to run, and all the read chunks are consumed.
+    return true;
+}
+
+void ScanOperator::set_finishing(RuntimeState* state) {
+    _is_finished = true;
+}
+
+StatusOr<vectorized::ChunkPtr> ScanOperator::pull_chunk(RuntimeState* state) {
+    RETURN_IF_ERROR(_try_to_trigger_next_scan(state));
+    if (_workgroup != nullptr) {
+        _workgroup->incr_period_ask_chunk_num(1);
+    }
+
+    for (auto& chunk_source : _chunk_sources) {
+        if (chunk_source != nullptr && chunk_source->has_output()) {
+            auto&& chunk = chunk_source->get_next_chunk_from_buffer();
+            eval_runtime_bloom_filters(chunk.value().get());
+
+            return std::move(chunk);
+        }
+    }
+
+    return nullptr;
+}
+
+Status ScanOperator::_try_to_trigger_next_scan(RuntimeState* state) {
+    if (_num_running_io_tasks >= MAX_IO_TASKS_PER_OP) {
+        return Status::OK();
+    }
+
+    // Firstly, find the picked-up morsel, whose can commit an io task.
+    for (int i = 0; i < MAX_IO_TASKS_PER_OP; ++i) {
+        if (_chunk_sources[i] != nullptr && !_is_io_task_running[i] && _chunk_sources[i]->has_next_chunk()) {
+            RETURN_IF_ERROR(_trigger_next_scan(state, i));
+        }
+    }
+
+    // Secondly, find the unused position of _chunk_sources to pick up a new morsel.
+    if (!_morsel_queue->empty()) {
+        for (int i = 0; i < MAX_IO_TASKS_PER_OP; ++i) {
+            if (_chunk_sources[i] == nullptr || (!_is_io_task_running[i] && !_chunk_sources[i]->has_output())) {
+                RETURN_IF_ERROR(_pickup_morsel(state, i));
+            }
+        }
+    }
+
+    return Status::OK();
+}
+
+Status ScanOperator::_trigger_next_scan(RuntimeState* state, int chunk_source_index) {
+    _num_running_io_tasks++;
+    _is_io_task_running[chunk_source_index] = true;
+
+    bool offer_task_success = false;
+    if (_workgroup != nullptr) {
+        workgroup::ScanTask task = workgroup::ScanTask(_workgroup, [this, state, chunk_source_index](int worker_id) {
+            {
+                SCOPED_THREAD_LOCAL_MEM_TRACKER_SETTER(state->instance_mem_tracker());
+
+                size_t num_read_chunks = 0;
+                _chunk_sources[chunk_source_index]->buffer_next_batch_chunks_blocking_for_workgroup(
+                        _buffer_size, _is_finished, &num_read_chunks, worker_id, _workgroup);
+                // TODO (by laotan332): More detailed information is needed
+                _workgroup->incr_period_scaned_chunk_num(num_read_chunks);
+            }
+
+            _num_running_io_tasks--;
+            _is_io_task_running[chunk_source_index] = false;
+        });
+
+        offer_task_success = ExecEnv::GetInstance()->scan_executor()->submit(std::move(task));
+    } else {
+        PriorityThreadPool::Task task;
+        task.work_function = [this, state, chunk_source_index]() {
+            {
+                SCOPED_THREAD_LOCAL_MEM_TRACKER_SETTER(state->instance_mem_tracker());
+                _chunk_sources[chunk_source_index]->buffer_next_batch_chunks_blocking(_buffer_size, _is_finished);
+            }
+
+            _num_running_io_tasks--;
+            _is_io_task_running[chunk_source_index] = false;
+        };
+        // TODO(by satanson): set a proper priority
+        task.priority = 20;
+
+        offer_task_success = _io_threads->try_offer(task);
+    }
+
+    if (offer_task_success) {
+        _io_task_retry_cnt = 0;
+    } else {
+        _num_running_io_tasks--;
+        _is_io_task_running[chunk_source_index] = false;
+        // TODO(hcf) set a proper retry times
+        LOG(WARNING) << "ScanOperator failed to offer io task due to thread pool overload, retryCnt="
+                     << _io_task_retry_cnt;
+        if (++_io_task_retry_cnt > 100) {
+            return Status::RuntimeError("ScanOperator failed to offer io task due to thread pool overload");
+        }
+    }
+
+    return Status::OK();
+}
+
+void ScanOperator::set_workgroup(starrocks::workgroup::WorkGroupPtr wg) {
+    _workgroup = wg;
+}
+
+Status ScanOperator::_pickup_morsel(RuntimeState* state, int chunk_source_index) {
+    DCHECK(_morsel_queue != nullptr);
+    if (_chunk_sources[chunk_source_index] != nullptr) {
+        _chunk_sources[chunk_source_index]->close(state);
+        _chunk_sources[chunk_source_index] = nullptr;
+    }
+
+    auto maybe_morsel = _morsel_queue->try_get();
+    if (maybe_morsel.has_value()) {
+        auto morsel = std::move(maybe_morsel.value());
+        DCHECK(morsel);
+        _chunk_sources[chunk_source_index] = create_chunk_source(std::move(morsel));
+        auto status = _chunk_sources[chunk_source_index]->prepare(state);
+        if (!status.ok()) {
+            _chunk_sources[chunk_source_index] = nullptr;
+            _is_finished = true;
+            return status;
+        }
+        RETURN_IF_ERROR(_trigger_next_scan(state, chunk_source_index));
+    }
+
+    return Status::OK();
+}
+
+// ========== ScanOperatorFactory ==========
+
+ScanOperatorFactory::ScanOperatorFactory(int32_t id, ScanNode* scan_node)
+        : SourceOperatorFactory(id, "olap_scan", scan_node->id()), _scan_node(scan_node) {}
+
+Status ScanOperatorFactory::prepare(RuntimeState* state) {
+    RETURN_IF_ERROR(OperatorFactory::prepare(state));
+    const auto& conjunct_ctxs = _scan_node->conjunct_ctxs();
+    RETURN_IF_ERROR(Expr::prepare(conjunct_ctxs, state));
+    RETURN_IF_ERROR(Expr::open(conjunct_ctxs, state));
+    RETURN_IF_ERROR(do_prepare(state));
+    return Status::OK();
+}
+
+OperatorPtr ScanOperatorFactory::create(int32_t degree_of_parallelism, int32_t driver_sequence) {
+    return do_create(degree_of_parallelism, driver_sequence);
+}
+
+void ScanOperatorFactory::close(RuntimeState* state) {
+    do_close(state);
+    const auto& conjunct_ctxs = _scan_node->conjunct_ctxs();
+    Expr::close(conjunct_ctxs, state);
+    OperatorFactory::close(state);
+}
+
+} // namespace starrocks::pipeline
