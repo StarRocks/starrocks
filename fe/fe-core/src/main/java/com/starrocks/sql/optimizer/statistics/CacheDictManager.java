afbabe115be3439529069c6a6dcdf8673b240b2e
stdpain <drfeng08@gmail.com>
2022-03-03 23:04:38 -0800

    fix for no-set ids

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/statistics/CacheDictManager.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/statistics/CacheDictManager.java
@@ -129,37 +129,36 @@ public class CacheDictManager implements IDictManager {
         }
         TGlobalDict tGlobalDict = statisticData.dict;
         ImmutableMap.Builder<String, Integer> dicts = ImmutableMap.builder();
-        if (tGlobalDict.isSetIds()) {
-            int dictSize = tGlobalDict.getIdsSize();
-            ColumnIdentifier columnIdentifier = new ColumnIdentifier(statisticData.tableId, statisticData.columnName);
-            if (dictSize > 256) {
+        if (!tGlobalDict.isSetIds()) {
+            return Optional.empty();
+        }
+        int dictSize = tGlobalDict.getIdsSize();
+        ColumnIdentifier columnIdentifier = new ColumnIdentifier(statisticData.tableId, statisticData.columnName);
+        if (dictSize > 256) {
+            noDictStringColumns.add(columnIdentifier);
+            return Optional.empty();
+        } else {
+            int dictDataSize = 0;
+            for (int i = 0; i < dictSize; i++) {
+                // a UTF-8 code may take up to 3 bytes
+                dictDataSize += tGlobalDict.strings.get(i).length() * 3;
+                // string offsets
+                dictDataSize += 4;
+            }
+            // 1M
+            final int DICT_PAGE_MAX_SIZE = 1024 * 1024;
+            // If the dictionary data size exceeds 1M,
+            // we won't use the global dictionary optimization.
+            // In this case BE cannot guarantee that the dictionary page
+            // will be generated after the compaction.
+            // Additional 32 bytes reserved for security.
+            if (dictDataSize > DICT_PAGE_MAX_SIZE - 32) {
                 noDictStringColumns.add(columnIdentifier);
                 return Optional.empty();
-            } else {
-                int dictDataSize = 0;
-                for (int i = 0; i < dictSize; i++) {
-                    // a UTF-8 code may take up to 3 bytes
-                    dictDataSize += tGlobalDict.strings.get(i).length() * 3;
-                    // string offsets
-                    dictDataSize += 4;
-                }
-                // 1M
-                final int DICT_PAGE_MAX_SIZE = 1024 * 1024;
-                // If the dictionary data size exceeds 1M,
-                // we won't use the global dictionary optimization.
-                // In this case BE cannot guarantee that the dictionary page
-                // will be generated after the compaction.
-                // Additional 32 bytes reserved for security.
-                if (dictDataSize > DICT_PAGE_MAX_SIZE - 32) {
-                    noDictStringColumns.add(columnIdentifier);
-                    return Optional.empty();
-                }
             }
-            
-
-            for (int i = 0; i < dictSize; ++i) {
-                dicts.put(tGlobalDict.strings.get(i), tGlobalDict.ids.get(i));
-            }
+        }
+        for (int i = 0; i < dictSize; ++i) {
+            dicts.put(tGlobalDict.strings.get(i), tGlobalDict.ids.get(i));
         }
         return Optional.of(new ColumnDict(dicts.build(), statisticData.meta_version));
     }
