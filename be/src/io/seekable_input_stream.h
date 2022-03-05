59e5fa0d8a2d20fe390b33b4265134356ee10c4f
sduzh <zhuming9011@qq.com>
2022-03-05 15:04:03 +0800

    Merge io::SeekableInputStream and io::RandomAccessFile into one class

--- be/src/io/seekable_input_stream.h
+++ be/src/io/seekable_input_stream.h
@@ -24,6 +24,26 @@ public:
     // Returns the current offset location as measured in bytes from
     // the beginning of the InputStream
     virtual StatusOr<int64_t> position() = 0;
+
+    // Read up to |count| bytes data from given position |offset|.
+    //
+    // This method will NOT change the internal offset.
+    //
+    // Return the number of bytes read, or error.
+    virtual StatusOr<int64_t> read_at(int64_t offset, void* out, int64_t count) = 0;
+
+    // Read exactly the number of |count| bytes data from given position |offset|.
+    // This method does not return the number of bytes read because either
+    // (1) the entire |count| bytes is read
+    // (2) the end of the stream is reached
+    // If the eof is reached, an IO error is returned, leave the content of
+    // |data| buffer unspecified.
+    //
+    // This method will NOT change the internal offset.
+    virtual Status read_at_fully(int64_t offset, void* out, int64_t count);
+
+    // Return the total file size in bytes, or error.
+    virtual StatusOr<int64_t> get_size() = 0;
 };
 
 } // namespace starrocks::io
