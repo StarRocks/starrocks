ed519ec7708e08d7ae1ce2f0033dcbfeb7c5152e
eyes_on_me <3675229+silverbullet233@users.noreply.github.com>
2022-03-05 08:48:12 +0800

    Fix predicates pushdown in external tables (#3803)

---
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rewrite/ExternalTablePredicateExtractor.java
@@ -0,0 +1,140 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
+
+package com.starrocks.sql.optimizer.rewrite;
+
+import com.starrocks.sql.optimizer.Utils;
+import com.starrocks.sql.optimizer.operator.OperatorType;
+import com.starrocks.sql.optimizer.operator.scalar.BetweenPredicateOperator;
+import com.starrocks.sql.optimizer.operator.scalar.BinaryPredicateOperator;
+import com.starrocks.sql.optimizer.operator.scalar.CastOperator;
+import com.starrocks.sql.optimizer.operator.scalar.ColumnRefOperator;
+import com.starrocks.sql.optimizer.operator.scalar.CompoundPredicateOperator;
+import com.starrocks.sql.optimizer.operator.scalar.ConstantOperator;
+import com.starrocks.sql.optimizer.operator.scalar.InPredicateOperator;
+import com.starrocks.sql.optimizer.operator.scalar.IsNullPredicateOperator;
+import com.starrocks.sql.optimizer.operator.scalar.ScalarOperator;
+import com.starrocks.sql.optimizer.operator.scalar.ScalarOperatorVisitor;
+
+import java.util.LinkedList;
+import java.util.List;
+
+// Extract predicates that can be pushed down to external table
+// and predicates that must be reserved
+// from the entire predicate
+// To be safe, we only allow push down simple  predicates
+public class ExternalTablePredicateExtractor {
+    private List<ScalarOperator> pushedPredicates = new LinkedList<>();
+    private List<ScalarOperator> reservedPredicates = new LinkedList<>();
+
+    public ScalarOperator getPushPredicate() {
+        return Utils.compoundAnd(pushedPredicates);
+    }
+
+    public ScalarOperator getReservePredicate() {
+        return Utils.compoundAnd(reservedPredicates);
+    }
+
+    public void extract(ScalarOperator op) {
+        pushedPredicates.clear();
+        reservedPredicates.clear();
+
+        if (op.getOpType().equals(OperatorType.COMPOUND)) {
+            CompoundPredicateOperator operator = (CompoundPredicateOperator) op;
+            switch (operator.getCompoundType()) {
+                case AND: {
+                    List<ScalarOperator> conjuncts = Utils.extractConjuncts(operator);
+                    // for CNF, we can push down each predicate independently
+                    for (ScalarOperator conjunct : conjuncts) {
+                        if (conjunct.accept(new CanFullyPushDownVisitor(), null)) {
+                            pushedPredicates.add(conjunct);
+                        } else {
+                            reservedPredicates.add(conjunct);
+                        }
+                    }
+                    return;
+                }
+                case OR: {
+                    // for DNF, pushdown is only possible if all children can be pushed down
+                    for (ScalarOperator child : operator.getChildren()) {
+                        if (!child.accept(new CanFullyPushDownVisitor(), null)) {
+                            reservedPredicates.add(op);
+                            return;
+                        }
+                    }
+                    pushedPredicates.add(operator);
+                    return;
+                }
+                case NOT: {
+                    extract(op.getChild(0));
+                    return;
+                }
+            }
+            return;
+        }
+        if (op.accept(new CanFullyPushDownVisitor(), null)) {
+            pushedPredicates.add(op);
+        } else {
+            reservedPredicates.add(op);
+        }
+    }
+
+    // check whether a predicate can be pushed down as a whole
+    private static class CanFullyPushDownVisitor extends ScalarOperatorVisitor<Boolean, Void> {
+        public CanFullyPushDownVisitor() {
+        }
+
+        private Boolean visitAllChildren(ScalarOperator op, Void context) {
+            for (ScalarOperator child : op.getChildren()) {
+                if (!child.accept(this, context)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        @Override
+        public Boolean visit(ScalarOperator scalarOperator, Void context) {
+            return false;
+        }
+
+        @Override
+        public Boolean visitConstant(ConstantOperator op, Void context) {
+            return true;
+        }
+
+        @Override
+        public Boolean visitVariableReference(ColumnRefOperator op, Void context) {
+            return true;
+        }
+
+        @Override
+        public Boolean visitBetweenPredicate(BetweenPredicateOperator op, Void context) {
+            return visitAllChildren(op, context);
+        }
+
+        @Override
+        public Boolean visitBinaryPredicate(BinaryPredicateOperator op, Void context) {
+            return visitAllChildren(op, context);
+        }
+
+        @Override
+        public Boolean visitCompoundPredicate(CompoundPredicateOperator op, Void context) {
+            return visitAllChildren(op, context);
+        }
+
+        @Override
+        public Boolean visitInPredicate(InPredicateOperator op, Void context) {
+            return visitAllChildren(op, context);
+        }
+
+        @Override
+        public Boolean visitIsNullPredicate(IsNullPredicateOperator op, Void context) {
+            return visitAllChildren(op, context);
+        }
+
+        @Override
+        public Boolean visitCastOperator(CastOperator op, Void context) {
+            return visitAllChildren(op, context);
+        }
+    }
+}
--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/MergePredicateRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/MergePredicateRule.java
@@ -32,10 +32,6 @@ public class MergePredicateRule extends TransformationRule {
             new MergePredicateRule(OperatorType.LOGICAL_HIVE_SCAN);
     public static final MergePredicateRule SCHEMA_SCAN =
             new MergePredicateRule(OperatorType.LOGICAL_SCHEMA_SCAN);
-    public static final MergePredicateRule MYSQL_SCAN =
-            new MergePredicateRule(OperatorType.LOGICAL_MYSQL_SCAN);
-    public static final MergePredicateRule JDBC_SCAN =
-            new MergePredicateRule(OperatorType.LOGICAL_JDBC_SCAN);
 
     public MergePredicateRule(OperatorType type) {
         super(RuleType.TF_MERGE_PREDICATE_SCAN, Pattern.create(OperatorType.LOGICAL_FILTER, type));
