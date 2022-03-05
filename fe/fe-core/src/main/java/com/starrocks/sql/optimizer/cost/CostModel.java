21f021ca82ef9c49bf4bdcdbab6eed2b128d9506
Youngwb <yangwenbo_mailbox@163.com>
2022-03-05 16:30:45 +0800

    Disable compute extra cost when can not get accurate table row count (#3818)

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/cost/CostModel.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/cost/CostModel.java
@@ -187,6 +187,10 @@ public class CostModel {
         public CostEstimate computeAggFunExtraCost(PhysicalHashAggregateOperator node, Statistics statistics,
                                                    Statistics inputStatistics) {
             CostEstimate costEstimate = CostEstimate.zero();
+            // If the statistics with inaccurate row count，It don't need to compute the extra cost.
+            if (statistics.isTableRowCountMayInaccurate()) {
+                return costEstimate;
+            }
             int distinctCount =
                     node.getAggregations().values().stream().filter(aggregation -> isDistinctAggFun(aggregation, node))
                             .collect(Collectors.toList()).size();
