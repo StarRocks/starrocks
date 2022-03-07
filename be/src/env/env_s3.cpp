ac12b7c1caa3df2190d07647c5adfc090b34151e
zhoukang <zhoukang199191@gmail.com>
2022-03-08 06:50:04 +0800

    Aws sdk can be slow when get ec2 metadata in Aws::Client::ClientConfiguration (#3912)

--- be/src/env/env_s3.cpp
+++ be/src/env/env_s3.cpp
@@ -5,7 +5,6 @@
 #include <aws/core/Aws.h>
 #include <aws/core/auth/AWSCredentialsProvider.h>
 #include <aws/core/utils/threading/Executor.h>
-#include <aws/s3/model/BucketLocationConstraint.h>
 #include <aws/s3/model/CreateBucketRequest.h>
 #include <aws/s3/model/DeleteBucketRequest.h>
 #include <aws/s3/model/DeleteObjectRequest.h>
@@ -13,7 +12,6 @@
 #include <aws/s3/model/HeadObjectRequest.h>
 #include <aws/s3/model/ListObjectsRequest.h>
 #include <aws/s3/model/PutObjectRequest.h>
-#include <aws/transfer/TransferHandle.h>
 #include <fmt/core.h>
 #include <time.h>
 
@@ -125,6 +123,13 @@ bool operator==(const Aws::Client::ClientConfiguration& lhs, const Aws::Client::
 
 class S3ClientFactory {
 public:
+    // We cached config here and make a deep copy each time.Since aws sdk has changed the
+    // Aws::Client::ClientConfiguration default constructor to search for the region
+    // (where as before 1.8 it has been hard coded default of "us-east-1").
+    // Part of that change is looking through the ec2 metadata, which can take a long time.
+    // For more details, please refer https://github.com/aws/aws-sdk-cpp/issues/1440
+    static Aws::Client::ClientConfiguration s_config;
+
     using ClientConfiguration = Aws::Client::ClientConfiguration;
     using S3Client = Aws::S3::S3Client;
     using S3ClientPtr = std::shared_ptr<S3Client>;
@@ -155,6 +160,8 @@ private:
     S3ClientPtr _clients[kMaxItems];
     Random _rand;
 };
+
+Aws::Client::ClientConfiguration S3ClientFactory::s_config;
 
 S3ClientFactory::S3ClientFactory() : _items(0), _rand((int)::time(NULL)) {}
 
@@ -189,7 +196,7 @@ S3ClientFactory::S3ClientPtr S3ClientFactory::new_client(const ClientConfigurati
 }
 
 static std::shared_ptr<Aws::S3::S3Client> new_s3client(const S3URI& uri) {
-    Aws::Client::ClientConfiguration config;
+    Aws::Client::ClientConfiguration config = S3ClientFactory::s_config;
     config.scheme = Aws::Http::Scheme::HTTP; // TODO: use the scheme in uri
     if (!uri.endpoint().empty()) {
         config.endpointOverride = uri.endpoint();
