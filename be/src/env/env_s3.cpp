e35cdbfb5070bf737ca599ac6ddf416ba84326fd
caneGuy <zhoukang199191@gmail.com>
2022-03-07 22:56:57 +0800

    fix segment fault

--- be/src/env/env_s3.cpp
+++ be/src/env/env_s3.cpp
@@ -128,6 +128,13 @@ bool operator==(const Aws::Client::ClientConfiguration& lhs, const Aws::Client::
 
 class S3ClientFactory {
 public:
+    // We cached config here and make a deep copy each time.Since aws sdk has changed the
+    // Aws::Client::ClientConfiguration default constructor to search for the region
+    // (where as before 1.8 it has been hard coded default of "us-east-1").
+    // Part of that change is looking through the ec2 metadata, which can take a long time.
+    // For more details, please refer https://github.com/aws/aws-sdk-cpp/issues/1440
+    static Aws::Client::ClientConfiguration s_config;
+
     using ClientConfiguration = Aws::Client::ClientConfiguration;
     using S3Client = Aws::S3::S3Client;
     using S3ClientPtr = std::shared_ptr<S3Client>;
@@ -158,6 +165,8 @@ private:
     S3ClientPtr _clients[kMaxItems];
     Random _rand;
 };
+
+Aws::Client::ClientConfiguration S3ClientFactory::s_config;
 
 S3ClientFactory::S3ClientFactory() : _items(0), _rand((int)::time(NULL)) {}
 
@@ -191,15 +200,8 @@ S3ClientFactory::S3ClientPtr S3ClientFactory::new_client(const ClientConfigurati
     return client;
 }
 
-// We cached config here and make a deep copy each time.Since aws sdk has changed the
-// Aws::Client::ClientConfiguration default constructor to search for the region
-// (where as before 1.8 it has been hard coded default of "us-east-1").
-// Part of that change is looking through the ec2 metadata, which can take a long time.
-// For more details, please refer https://github.com/aws/aws-sdk-cpp/issues/1440
-static Aws::Client::ClientConfiguration s_config;
-
 static std::shared_ptr<Aws::S3::S3Client> new_s3client(const S3URI& uri) {
-    Aws::Client::ClientConfiguration config = s_config;
+    Aws::Client::ClientConfiguration config = S3ClientFactory::s_config;
     config.scheme = Aws::Http::Scheme::HTTP; // TODO: use the scheme in uri
     if (!uri.endpoint().empty()) {
         config.endpointOverride = uri.endpoint();
