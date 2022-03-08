StarRocks/starrocks

commit 091f30b06e172046b85d2e048ceb5e2bfcbde2ce

Aws sdk can be slow when get ec2 metadata in Aws::Client::ClientConfiguration (#3912)

be/src/env/env_s3.cpp
@@ -5,7 +5,6 @@
 #include <aws/core/Aws.h>
 #include <aws/core/auth/AWSCredentialsProvider.h>
 #include <aws/core/utils/threading/Executor.h>
-#include <aws/s3/model/BucketLocationConstraint.h>
 #include <aws/s3/model/CreateBucketRequest.h>
 #include <aws/s3/model/DeleteBucketRequest.h>
 #include <aws/s3/model/DeleteObjectRequest.h>
@@ -13,7 +12,6 @@
 #include <aws/s3/model/HeadObjectRequest.h>
 #include <aws/s3/model/ListObjectsRequest.h>
 #include <aws/s3/model/PutObjectRequest.h>
-#include <aws/transfer/TransferHandle.h>
 #include <fmt/core.h>
 #include <time.h>
 
@@ -125,6 +123,13 @@ bool operator==(const Aws::Client::ClientConfiguration& lhs, const Aws::Client::
 
 class S3ClientFactory {
 public:
+    // We cached config here and make a deep copy each time.Since aws sdk has changed the
+    // Aws::Client::ClientConfiguration default constructor to search for the region
+    // (where as before 1.8 it has been hard coded default of "us-east-1").
+    // Part of that change is looking through the ec2 metadata, which can take a long time.
+    // For more details, please refer https://github.com/aws/aws-sdk-cpp/issues/1440
+    static Aws::Client::ClientConfiguration s_config;
+
     using ClientConfiguration = Aws::Client::ClientConfiguration;
     using S3Client = Aws::S3::S3Client;
     using S3ClientPtr = std::shared_ptr<S3Client>;
@@ -155,6 +160,8 @@ private:
     S3ClientPtr _clients[kMaxItems];
     Random _rand;
 };
+
+Aws::Client::ClientConfiguration S3ClientFactory::s_config;
 
 S3ClientFactory::S3ClientFactory() : _items(0), _rand((int)::time(NULL)) {}
 
@@ -189,7 +196,7 @@ S3ClientFactory::S3ClientPtr S3ClientFactory::new_client(const ClientConfigurati
 }
 
 static std::shared_ptr<Aws::S3::S3Client> new_s3client(const S3URI& uri) {
-    Aws::Client::ClientConfiguration config;
+    Aws::Client::ClientConfiguration config = S3ClientFactory::s_config;
     config.scheme = Aws::Http::Scheme::HTTP; // TODO: use the scheme in uri
     if (!uri.endpoint().empty()) {
         config.endpointOverride = uri.endpoint();

commit 25916c171c01470859f384d3e95cdf663624bf91

Fix be crash caused by #3912 and #3825 (#3934)
* be will crash with #3912 since we should init config after 
```
Aws::SDKOptions aws_sdk_options;  Aws::InitAPI(aws_sdk_options); 
```
being called. we have called the two apis in starrocks_main.cpp, so we use lazy singleton instance for AWS ClientConfiguration to avoid be crash during starting
* iceberg table has no partitions, after #3825 be will crash when we query iceberg table.We should check has_partition in _find_and_insert_hdfs_file and _init_partition_values_map

be/src/env/env_s3.cpp
@@ -123,13 +123,6 @@ bool operator==(const Aws::Client::ClientConfiguration& lhs, const Aws::Client::
 
 class S3ClientFactory {
 public:
-    // We cached config here and make a deep copy each time.Since aws sdk has changed the
-    // Aws::Client::ClientConfiguration default constructor to search for the region
-    // (where as before 1.8 it has been hard coded default of "us-east-1").
-    // Part of that change is looking through the ec2 metadata, which can take a long time.
-    // For more details, please refer https://github.com/aws/aws-sdk-cpp/issues/1440
-    static Aws::Client::ClientConfiguration s_config;
-
     using ClientConfiguration = Aws::Client::ClientConfiguration;
     using S3Client = Aws::S3::S3Client;
     using S3ClientPtr = std::shared_ptr<S3Client>;
@@ -148,6 +141,16 @@ public:
 
     S3ClientPtr new_client(const ClientConfiguration& config);
 
+    static ClientConfiguration& getClientConfig() {
+        // We cached config here and make a deep copy each time.Since aws sdk has changed the
+        // Aws::Client::ClientConfiguration default constructor to search for the region
+        // (where as before 1.8 it has been hard coded default of "us-east-1").
+        // Part of that change is looking through the ec2 metadata, which can take a long time.
+        // For more details, please refer https://github.com/aws/aws-sdk-cpp/issues/1440
+        static ClientConfiguration instance;
+        return instance;
+    }
+
 private:
     S3ClientFactory();
 
@@ -160,8 +163,6 @@ private:
     S3ClientPtr _clients[kMaxItems];
     Random _rand;
 };
-
-Aws::Client::ClientConfiguration S3ClientFactory::s_config;
 
 S3ClientFactory::S3ClientFactory() : _items(0), _rand((int)::time(NULL)) {}
 
@@ -196,7 +197,7 @@ S3ClientFactory::S3ClientPtr S3ClientFactory::new_client(const ClientConfigurati
 }
 
 static std::shared_ptr<Aws::S3::S3Client> new_s3client(const S3URI& uri) {
-    Aws::Client::ClientConfiguration config = S3ClientFactory::s_config;
+    Aws::Client::ClientConfiguration config = S3ClientFactory::getClientConfig();
     config.scheme = Aws::Http::Scheme::HTTP; // TODO: use the scheme in uri
     if (!uri.endpoint().empty()) {
         config.endpointOverride = uri.endpoint();

be/src/exec/vectorized/hdfs_scan_node.cpp
@@ -581,15 +581,15 @@ Status HdfsScanNode::set_scan_ranges(const std::vector<TScanRangeParams>& scan_r
 }
 
 void HdfsScanNode::_init_partition_values_map() {
-    if (_scan_ranges.empty()) {
+    if (_scan_ranges.empty() || !_lake_table->has_partition()) {
         return;
     }
 
     for (auto& scan_range : _scan_ranges) {
         auto* partition_desc = _lake_table->get_partition(scan_range.partition_id);
-
-        if (_partition_values_map.find(scan_range.partition_id) == _partition_values_map.end()) {
-            _partition_values_map[scan_range.partition_id] = partition_desc->partition_key_value_evals();
+        auto partition_id = scan_range.partition_id;
+        if (_partition_values_map.find(partition_id) == _partition_values_map.end()) {
+            _partition_values_map[partition_id] = partition_desc->partition_key_value_evals();
         }
     }
 
@@ -654,7 +654,7 @@ Status HdfsScanNode::_find_and_insert_hdfs_file(const THdfsScanRange& scan_range
 
     COUNTER_UPDATE(_profile.scan_files_counter, 1);
     std::string native_file_path = scan_range.full_path;
-    if (_lake_table != nullptr) {
+    if (_lake_table != nullptr && _lake_table->has_partition()) {
         auto* partition_desc = _lake_table->get_partition(scan_range.partition_id);
 
         SCOPED_TIMER(_profile.open_file_timer);