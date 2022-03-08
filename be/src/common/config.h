f09a14f242e1af265392198c1e7a9b449483a98a
stephen <91597003+stephen-shelby@users.noreply.github.com>
2022-03-08 16:21:20 +0800

    aws sdk adapts to Tencent cos (#3936)

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -662,6 +662,8 @@ CONF_Bool(rewrite_partial_segment, "true");
 CONF_String(object_storage_access_key_id, "");
 CONF_String(object_storage_secret_access_key, "");
 CONF_String(object_storage_endpoint, "");
+// Tencent cos needs to add region information
+CONF_String(object_storage_region, "");
 CONF_Int64(object_storage_max_connection, "102400");
 
 CONF_Bool(enable_orc_late_materialization, "true");
