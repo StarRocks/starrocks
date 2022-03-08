171ca5a2b7346482004ac4d4e4e3a11a418479e6
Seaven <seaven_7@qq.com>
2022-03-08 16:06:31 +0800

    UT-Refactor2.Move FE UT case (#3924)

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/SubqueryTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/SubqueryTest.java
@@ -2,5 +2,57 @@
 
 package com.starrocks.sql.plan;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SubqueryTest extends PlanTestBase {
+
+    @Test
+    public void testCountConstantWithSubquery() throws Exception {
+        String sql = "SELECT 1 FROM (SELECT COUNT(1) FROM t0 WHERE false) t;";
+        String thriftPlan = getThriftPlan(sql);
+        Assert.assertTrue(thriftPlan.contains("function_name:count"));
+    }
+
+    @Test
+    public void testSubqueryGatherJoin() throws Exception {
+        String sql = "select t1.v5 from (select * from t0 limit 1) as x inner join t1 on x.v1 = t1.v4";
+        String plan = getFragmentPlan(sql);
+        Assert.assertTrue(plan.contains(" OUTPUT EXPRS:\n"
+                + "  PARTITION: RANDOM\n"
+                + "\n"
+                + "  STREAM DATA SINK\n"
+                + "    EXCHANGE ID: 02\n"
+                + "    UNPARTITIONED\n"
+                + "\n"
+                + "  1:OlapScanNode\n"
+                + "     TABLE: t0"));
+    }
+
+    @Test
+    public void testSubqueryBroadJoin() throws Exception {
+        String sql = "select t1.v5 from t0 inner join[broadcast] t1 on cast(t0.v1 as int) = cast(t1.v4 as int)";
+        String plan = getFragmentPlan(sql);
+        Assert.assertTrue(plan.contains("  |  equal join conjunct: 7: cast = 8: cast\n"));
+        Assert.assertTrue(plan.contains("<slot 7> : CAST(1: v1 AS INT)"));
+        Assert.assertTrue(plan.contains("<slot 8> : CAST(4: v4 AS INT)"));
+    }
+
+    @Test
+    public void testMultiScalarSubquery() throws Exception {
+        String sql = "SELECT CASE \n"
+                + "    WHEN (SELECT count(*) FROM t1 WHERE v4 BETWEEN 1 AND 20) > 74219\n"
+                + "    THEN ( \n"
+                + "        SELECT avg(v7) FROM t2 WHERE v7 BETWEEN 1 AND 20\n"
+                + "        )\n"
+                + "    ELSE (\n"
+                + "        SELECT avg(v8) FROM t2 WHERE v8 BETWEEN 1 AND 20\n"
+                + "        ) END AS bucket1\n"
+                + "FROM t0\n"
+                + "WHERE v1 = 1;";
+        String plan = getFragmentPlan(sql);
+        Assert.assertNotNull(plan);
+    }
+
+
 }
