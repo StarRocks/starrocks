6e8c897689ee039680af1aa17ed8fb8cfb96e149
liuyehcf <1559500551@qq.com>
2022-03-08 19:04:57 +0800

    reduce datetime to date cast (#3914)

--- fe/fe-core/src/test/java/com/starrocks/sql/optimizer/rewrite/scalar/ReduceCastRuleTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/optimizer/rewrite/scalar/ReduceCastRuleTest.java
@@ -7,6 +7,8 @@ import com.starrocks.catalog.Type;
 import com.starrocks.sql.optimizer.operator.OperatorType;
 import com.starrocks.sql.optimizer.operator.scalar.BinaryPredicateOperator;
 import com.starrocks.sql.optimizer.operator.scalar.CastOperator;
+import com.starrocks.sql.optimizer.operator.scalar.ColumnRefOperator;
+import com.starrocks.sql.optimizer.operator.scalar.CompoundPredicateOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ConstantOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ScalarOperator;
 import com.starrocks.sql.optimizer.rewrite.ScalarOperatorRewriteRule;
@@ -15,6 +17,9 @@ import org.junit.Test;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -156,6 +161,254 @@ public class ReduceCastRuleTest {
             ScalarOperator result = reduceCastRule.apply(binPred, null);
             result = foldConstantsRule.apply(result, null);
             Assert.assertTrue(!(result instanceof ConstantOperator));
+        }
+    }
+
+    @Test
+    public void testReduceDateToDatetimeCast() {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        ReduceCastRule reduceCastRule = new ReduceCastRule();
+        {
+            CastOperator castOperator =
+                    new CastOperator(Type.DATETIME, new ColumnRefOperator(0, Type.DATE, "id_date", false));
+            ConstantOperator constantOperator =
+                    ConstantOperator.createDatetime(LocalDateTime.parse("2021-12-28 11:11:11", dateTimeFormatter));
+            BinaryPredicateOperator beforeOptimize =
+                    new BinaryPredicateOperator(BinaryPredicateOperator.BinaryType.GE, castOperator, constantOperator);
+            ScalarOperator afterOptimize = reduceCastRule.apply(
+                    beforeOptimize,
+                    null);
+
+            Assert.assertTrue(afterOptimize instanceof BinaryPredicateOperator);
+            Assert.assertEquals(BinaryPredicateOperator.BinaryType.GE,
+                    ((BinaryPredicateOperator) afterOptimize).getBinaryType());
+            Assert.assertTrue(afterOptimize.getChild(0) instanceof ColumnRefOperator);
+            Assert.assertTrue(afterOptimize.getChild(1) instanceof ConstantOperator);
+            Assert.assertEquals("2021-12-29",
+                    ((ConstantOperator) afterOptimize.getChild(1)).getDate().format(dateFormat));
+        }
+        {
+            CastOperator castOperator =
+                    new CastOperator(Type.DATETIME, new ColumnRefOperator(0, Type.DATE, "id_date", false));
+            ConstantOperator constantOperator =
+                    ConstantOperator.createDatetime(LocalDateTime.parse("2021-12-28 00:00:00", dateTimeFormatter));
+            BinaryPredicateOperator beforeOptimize =
+                    new BinaryPredicateOperator(BinaryPredicateOperator.BinaryType.GE, castOperator, constantOperator);
+            ScalarOperator afterOptimize = reduceCastRule.apply(
+                    beforeOptimize,
+                    null);
+
+            Assert.assertTrue(afterOptimize instanceof BinaryPredicateOperator);
+            Assert.assertEquals(BinaryPredicateOperator.BinaryType.GE,
+                    ((BinaryPredicateOperator) afterOptimize).getBinaryType());
+            Assert.assertTrue(afterOptimize.getChild(0) instanceof ColumnRefOperator);
+            Assert.assertTrue(afterOptimize.getChild(1) instanceof ConstantOperator);
+            Assert.assertEquals("2021-12-28",
+                    ((ConstantOperator) afterOptimize.getChild(1)).getDate().format(dateFormat));
+        }
+        {
+            CastOperator castOperator =
+                    new CastOperator(Type.DATETIME, new ColumnRefOperator(0, Type.DATE, "id_date", false));
+            ConstantOperator constantOperator =
+                    ConstantOperator.createDatetime(LocalDateTime.parse("2021-12-28 00:00:00", dateTimeFormatter));
+            BinaryPredicateOperator beforeOptimize =
+                    new BinaryPredicateOperator(BinaryPredicateOperator.BinaryType.LE, castOperator, constantOperator);
+            ScalarOperator afterOptimize = reduceCastRule.apply(
+                    beforeOptimize,
+                    null);
+
+            Assert.assertTrue(afterOptimize instanceof BinaryPredicateOperator);
+            Assert.assertEquals(BinaryPredicateOperator.BinaryType.LE,
+                    ((BinaryPredicateOperator) afterOptimize).getBinaryType());
+            Assert.assertTrue(afterOptimize.getChild(0) instanceof ColumnRefOperator);
+            Assert.assertTrue(afterOptimize.getChild(1) instanceof ConstantOperator);
+            Assert.assertEquals("2021-12-28",
+                    ((ConstantOperator) afterOptimize.getChild(1)).getDate().format(dateFormat));
+        }
+        {
+            CastOperator castOperator =
+                    new CastOperator(Type.DATETIME, new ColumnRefOperator(0, Type.DATE, "id_date", false));
+            ConstantOperator constantOperator =
+                    ConstantOperator.createDatetime(LocalDateTime.parse("2021-12-28 11:11:11", dateTimeFormatter));
+            BinaryPredicateOperator beforeOptimize =
+                    new BinaryPredicateOperator(BinaryPredicateOperator.BinaryType.LT, castOperator, constantOperator);
+            ScalarOperator afterOptimize = reduceCastRule.apply(
+                    beforeOptimize,
+                    null);
+
+            Assert.assertTrue(afterOptimize instanceof BinaryPredicateOperator);
+            Assert.assertEquals(BinaryPredicateOperator.BinaryType.LE,
+                    ((BinaryPredicateOperator) afterOptimize).getBinaryType());
+            Assert.assertTrue(afterOptimize.getChild(0) instanceof ColumnRefOperator);
+            Assert.assertTrue(afterOptimize.getChild(1) instanceof ConstantOperator);
+            Assert.assertEquals("2021-12-28",
+                    ((ConstantOperator) afterOptimize.getChild(1)).getDate().format(dateFormat));
+        }
+        {
+            CastOperator castOperator =
+                    new CastOperator(Type.DATETIME, new ColumnRefOperator(0, Type.DATE, "id_date", false));
+            ConstantOperator constantOperator =
+                    ConstantOperator.createDatetime(LocalDateTime.parse("2021-12-28 00:00:00", dateTimeFormatter));
+            BinaryPredicateOperator beforeOptimize =
+                    new BinaryPredicateOperator(BinaryPredicateOperator.BinaryType.LT, castOperator, constantOperator);
+            ScalarOperator afterOptimize = reduceCastRule.apply(
+                    beforeOptimize,
+                    null);
+
+            Assert.assertTrue(afterOptimize instanceof BinaryPredicateOperator);
+            Assert.assertEquals(BinaryPredicateOperator.BinaryType.LE,
+                    ((BinaryPredicateOperator) afterOptimize).getBinaryType());
+            Assert.assertTrue(afterOptimize.getChild(0) instanceof ColumnRefOperator);
+            Assert.assertTrue(afterOptimize.getChild(1) instanceof ConstantOperator);
+            Assert.assertEquals("2021-12-27",
+                    ((ConstantOperator) afterOptimize.getChild(1)).getDate().format(dateFormat));
+        }
+        {
+            CastOperator castOperator =
+                    new CastOperator(Type.DATETIME, new ColumnRefOperator(0, Type.DATE, "id_date", false));
+            ConstantOperator constantOperator =
+                    ConstantOperator.createDatetime(LocalDateTime.parse("2021-12-28 00:00:00", dateTimeFormatter));
+            BinaryPredicateOperator beforeOptimize =
+                    new BinaryPredicateOperator(BinaryPredicateOperator.BinaryType.EQ, castOperator, constantOperator);
+            ScalarOperator afterOptimize = reduceCastRule.apply(
+                    beforeOptimize,
+                    null);
+
+            Assert.assertTrue(afterOptimize instanceof BinaryPredicateOperator);
+            Assert.assertEquals(BinaryPredicateOperator.BinaryType.EQ,
+                    ((BinaryPredicateOperator) afterOptimize).getBinaryType());
+            Assert.assertTrue(afterOptimize.getChild(0) instanceof ColumnRefOperator);
+            Assert.assertTrue(afterOptimize.getChild(1) instanceof ConstantOperator);
+            Assert.assertEquals("2021-12-28",
+                    ((ConstantOperator) afterOptimize.getChild(1)).getDate().format(dateFormat));
+        }
+        {
+            CastOperator castOperator =
+                    new CastOperator(Type.DATETIME, new ColumnRefOperator(0, Type.DATE, "id_date", false));
+            ConstantOperator constantOperator =
+                    ConstantOperator.createDatetime(LocalDateTime.parse("2021-12-29 11:11:11", dateTimeFormatter));
+            BinaryPredicateOperator beforeOptimize =
+                    new BinaryPredicateOperator(BinaryPredicateOperator.BinaryType.EQ, castOperator, constantOperator);
+            ScalarOperator afterOptimize = reduceCastRule.apply(
+                    beforeOptimize,
+                    null);
+
+            Assert.assertSame(beforeOptimize, afterOptimize);
+        }
+    }
+
+    @Test
+    public void testReduceDatetimeToDateCast() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        ReduceCastRule reduceCastRule = new ReduceCastRule();
+        {
+            CastOperator castOperator =
+                    new CastOperator(Type.DATE, new ColumnRefOperator(0, Type.DATETIME, "id_datetime", false));
+            ConstantOperator constantOperator =
+                    ConstantOperator.createDate(LocalDate.parse("2021-12-28").atTime(0, 0, 0, 0));
+            BinaryPredicateOperator beforeOptimize =
+                    new BinaryPredicateOperator(BinaryPredicateOperator.BinaryType.GE, castOperator, constantOperator);
+            ScalarOperator afterOptimize = reduceCastRule.apply(
+                    beforeOptimize,
+                    null);
+
+            Assert.assertTrue(afterOptimize instanceof BinaryPredicateOperator);
+            Assert.assertEquals(BinaryPredicateOperator.BinaryType.GE,
+                    ((BinaryPredicateOperator) afterOptimize).getBinaryType());
+            Assert.assertTrue(afterOptimize.getChild(0) instanceof ColumnRefOperator);
+            Assert.assertTrue(afterOptimize.getChild(1) instanceof ConstantOperator);
+            Assert.assertEquals("2021-12-28 00:00:00",
+                    ((ConstantOperator) afterOptimize.getChild(1)).getDatetime().format(formatter));
+        }
+        {
+            CastOperator castOperator =
+                    new CastOperator(Type.DATE, new ColumnRefOperator(0, Type.DATETIME, "id_datetime", false));
+            ConstantOperator constantOperator =
+                    ConstantOperator.createDate(LocalDate.parse("2021-12-28").atTime(0, 0, 0, 0));
+            BinaryPredicateOperator beforeOptimize =
+                    new BinaryPredicateOperator(BinaryPredicateOperator.BinaryType.GT, castOperator, constantOperator);
+            ScalarOperator afterOptimize = reduceCastRule.apply(
+                    beforeOptimize,
+                    null);
+
+            Assert.assertTrue(afterOptimize instanceof BinaryPredicateOperator);
+            Assert.assertEquals(BinaryPredicateOperator.BinaryType.GE,
+                    ((BinaryPredicateOperator) afterOptimize).getBinaryType());
+            Assert.assertTrue(afterOptimize.getChild(0) instanceof ColumnRefOperator);
+            Assert.assertTrue(afterOptimize.getChild(1) instanceof ConstantOperator);
+            Assert.assertEquals("2021-12-29 00:00:00",
+                    ((ConstantOperator) afterOptimize.getChild(1)).getDatetime().format(formatter));
+        }
+        {
+            CastOperator castOperator =
+                    new CastOperator(Type.DATE, new ColumnRefOperator(0, Type.DATETIME, "id_datetime", false));
+            ConstantOperator constantOperator =
+                    ConstantOperator.createDate(LocalDate.parse("2021-12-28").atTime(0, 0, 0, 0));
+            BinaryPredicateOperator beforeOptimize =
+                    new BinaryPredicateOperator(BinaryPredicateOperator.BinaryType.LE, castOperator, constantOperator);
+            ScalarOperator afterOptimize = reduceCastRule.apply(
+                    beforeOptimize,
+                    null);
+
+            Assert.assertTrue(afterOptimize instanceof BinaryPredicateOperator);
+            Assert.assertEquals(BinaryPredicateOperator.BinaryType.LT,
+                    ((BinaryPredicateOperator) afterOptimize).getBinaryType());
+            Assert.assertTrue(afterOptimize.getChild(0) instanceof ColumnRefOperator);
+            Assert.assertTrue(afterOptimize.getChild(1) instanceof ConstantOperator);
+            Assert.assertEquals("2021-12-29 00:00:00",
+                    ((ConstantOperator) afterOptimize.getChild(1)).getDatetime().format(formatter));
+        }
+        {
+            CastOperator castOperator =
+                    new CastOperator(Type.DATE, new ColumnRefOperator(0, Type.DATETIME, "id_datetime", false));
+            ConstantOperator constantOperator =
+                    ConstantOperator.createDate(LocalDate.parse("2021-12-28").atTime(0, 0, 0, 0));
+            BinaryPredicateOperator beforeOptimize =
+                    new BinaryPredicateOperator(BinaryPredicateOperator.BinaryType.LT, castOperator, constantOperator);
+            ScalarOperator afterOptimize = reduceCastRule.apply(
+                    beforeOptimize,
+                    null);
+
+            Assert.assertTrue(afterOptimize instanceof BinaryPredicateOperator);
+            Assert.assertEquals(BinaryPredicateOperator.BinaryType.LT,
+                    ((BinaryPredicateOperator) afterOptimize).getBinaryType());
+            Assert.assertTrue(afterOptimize.getChild(0) instanceof ColumnRefOperator);
+            Assert.assertTrue(afterOptimize.getChild(1) instanceof ConstantOperator);
+            Assert.assertEquals("2021-12-28 00:00:00",
+                    ((ConstantOperator) afterOptimize.getChild(1)).getDatetime().format(formatter));
+        }
+        {
+            CastOperator castOperator =
+                    new CastOperator(Type.DATE, new ColumnRefOperator(0, Type.DATETIME, "id_datetime", false));
+            ConstantOperator constantOperator =
+                    ConstantOperator.createDate(LocalDate.parse("2021-12-28").atTime(0, 0, 0, 0));
+            BinaryPredicateOperator beforeOptimize =
+                    new BinaryPredicateOperator(BinaryPredicateOperator.BinaryType.EQ, castOperator, constantOperator);
+            ScalarOperator afterOptimize = reduceCastRule.apply(
+                    beforeOptimize,
+                    null);
+
+            Assert.assertTrue(afterOptimize instanceof CompoundPredicateOperator);
+            Assert.assertTrue(((CompoundPredicateOperator) afterOptimize).isAnd());
+            ScalarOperator left = afterOptimize.getChild(0);
+            ScalarOperator right = afterOptimize.getChild(1);
+            Assert.assertTrue(left instanceof BinaryPredicateOperator);
+            Assert.assertTrue(right instanceof BinaryPredicateOperator);
+            Assert.assertEquals(BinaryPredicateOperator.BinaryType.GE,
+                    ((BinaryPredicateOperator) left).getBinaryType());
+            Assert.assertTrue(left.getChild(0) instanceof ColumnRefOperator);
+            Assert.assertTrue(left.getChild(1) instanceof ConstantOperator);
+            Assert.assertEquals("2021-12-28 00:00:00",
+                    ((ConstantOperator) left.getChild(1)).getDatetime().format(formatter));
+            Assert.assertEquals(BinaryPredicateOperator.BinaryType.LT,
+                    ((BinaryPredicateOperator) right).getBinaryType());
+            Assert.assertTrue(right.getChild(0) instanceof ColumnRefOperator);
+            Assert.assertTrue(right.getChild(1) instanceof ConstantOperator);
+            Assert.assertEquals("2021-12-29 00:00:00",
+                    ((ConstantOperator) right.getChild(1)).getDatetime().format(formatter));
         }
     }
 }
