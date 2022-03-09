f7bc22a0cfd0e6e7bedb5a2d0e48e6c83bafa943
liuyehcf <1559500551@qq.com>
2022-03-09 14:40:02 +0800

    Fix the problem of truncate_decimal (#3920)

--- be/test/exec/pipeline/pipeline_control_flow_test.cpp
+++ be/test/exec/pipeline/pipeline_control_flow_test.cpp
@@ -364,7 +364,7 @@ TEST_F(TestPipelineControlFlow, test_two_operatories) {
 
     start_test();
 
-    ASSERT_TRUE(_fragment_future.wait_for(std::chrono::seconds(5)) == std::future_status::ready);
+    ASSERT_EQ(std::future_status::ready, _fragment_future.wait_for(std::chrono::seconds(15)));
     ASSERT_COUNTER_CHUNK_NUM(sourceCounter, 0, chunk_num);
     ASSERT_COUNTER_CHUNK_NUM(sinkCounter, chunk_num, 0);
     ASSERT_EQ(lifecycle_error_num, 0);
@@ -394,7 +394,7 @@ TEST_F(TestPipelineControlFlow, test_three_operatories) {
 
     start_test();
 
-    ASSERT_TRUE(_fragment_future.wait_for(std::chrono::seconds(5)) == std::future_status::ready);
+    ASSERT_EQ(std::future_status::ready, _fragment_future.wait_for(std::chrono::seconds(15)));
     ASSERT_COUNTER_CHUNK_NUM(sourceCounter, 0, chunk_num);
     ASSERT_COUNTER_CHUNK_NUM(normalCounter, chunk_num, chunk_num);
     ASSERT_COUNTER_CHUNK_NUM(sinkCounter, chunk_num, 0);
@@ -433,7 +433,7 @@ TEST_F(TestPipelineControlFlow, test_multi_operators) {
 
         start_test();
 
-        ASSERT_TRUE(_fragment_future.wait_for(std::chrono::seconds(5)) == std::future_status::ready);
+        ASSERT_EQ(std::future_status::ready, _fragment_future.wait_for(std::chrono::seconds(15)));
         ASSERT_COUNTER_CHUNK_NUM(sourceCounter, 0, chunk_num);
         for (size_t j = 0; j < i; ++j) {
             ASSERT_COUNTER_CHUNK_NUM(normalCounters[j], chunk_num, chunk_num);
@@ -467,7 +467,7 @@ TEST_F(TestPipelineControlFlow, test_full_chunk_size) {
 
     start_test();
 
-    ASSERT_TRUE(_fragment_future.wait_for(std::chrono::seconds(5)) == std::future_status::ready);
+    ASSERT_EQ(std::future_status::ready, _fragment_future.wait_for(std::chrono::seconds(15)));
     ASSERT_COUNTER_CHUNK_NUM(sourceCounter, 0, chunk_num);
     ASSERT_COUNTER_CHUNK_NUM(normalCounter, chunk_num, chunk_num);
     ASSERT_COUNTER_CHUNK_NUM(sinkCounter, chunk_num, 0);
@@ -498,7 +498,7 @@ TEST_F(TestPipelineControlFlow, test_multi_chunks) {
 
     start_test();
 
-    ASSERT_TRUE(_fragment_future.wait_for(std::chrono::seconds(5)) == std::future_status::ready);
+    ASSERT_EQ(std::future_status::ready, _fragment_future.wait_for(std::chrono::seconds(15)));
     ASSERT_COUNTER_CHUNK_NUM(sourceCounter, 0, chunk_num);
     ASSERT_COUNTER_CHUNK_NUM(normalCounter, chunk_num, chunk_num);
     ASSERT_COUNTER_CHUNK_NUM(sinkCounter, chunk_num, 0);
@@ -537,7 +537,7 @@ TEST_F(TestPipelineControlFlow, test_local_exchange_operator_with_non_full_chunk
 
         start_test();
 
-        ASSERT_TRUE(_fragment_future.wait_for(std::chrono::seconds(5)) == std::future_status::ready);
+        ASSERT_EQ(std::future_status::ready, _fragment_future.wait_for(std::chrono::seconds(15)));
         ASSERT_COUNTER_CHUNK_NUM(sourceCounter, 0, chunk_num * i);
         if (i == 1) {
             // Without local exchange sink/source operator
@@ -587,7 +587,7 @@ TEST_F(TestPipelineControlFlow, test_local_exchange_operator_with_full_chunk) {
 
         start_test();
 
-        ASSERT_TRUE(_fragment_future.wait_for(std::chrono::seconds(15)) == std::future_status::ready);
+        ASSERT_EQ(std::future_status::ready, _fragment_future.wait_for(std::chrono::seconds(15)));
         ASSERT_COUNTER_CHUNK_NUM(sourceCounter, 0, chunk_num * i);
         ASSERT_COUNTER_CHUNK_NUM(normalCounter, chunk_num * i, chunk_num * i);
         ASSERT_COUNTER_CHUNK_NUM(sinkCounter, chunk_num * i, 0);
