73d5abd0cfc05222ed7063b83b0a2bd0b0f76c1d
wyb <wybb86@gmail.com>
2022-03-09 15:21:21 +0800

    Update

--- fe/fe-core/src/main/java/com/starrocks/catalog/StarOSTablet.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/StarOSTablet.java
@@ -2,10 +2,9 @@
 
 package com.starrocks.catalog;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import com.google.gson.annotations.SerializedName;
 import com.starrocks.common.io.Text;
+import com.starrocks.persist.gson.GsonUtils;
 
 import java.io.DataInput;
 import java.io.DataOutput;
@@ -27,10 +26,6 @@ public class StarOSTablet extends Tablet {
     private long dataSize = 0L;
     @SerializedName(value = JSON_KEY_ROW_COUNT)
     private long rowCount = 0L;
-
-    public StarOSTablet() {
-        this(-1L, -1L);
-    }
 
     public StarOSTablet(long id, long shardId) {
         super(id);
@@ -62,30 +57,12 @@ public class StarOSTablet extends Tablet {
 
     @Override
     public void write(DataOutput out) throws IOException {
-        super.write(out);
-
-        JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty(JSON_KEY_ID, id);
-        jsonObject.addProperty(JSON_KEY_SHARD_ID, shardId);
-        jsonObject.addProperty(JSON_KEY_DATA_SIZE, dataSize);
-        jsonObject.addProperty(JSON_KEY_ROW_COUNT, rowCount);
-        Text.writeString(out, jsonObject.toString());
-    }
-
-    @Override
-    public void readFields(DataInput in) throws IOException {
-        super.readFields(in);
-
-        JsonObject jsonObject = JsonParser.parseString(Text.readString(in)).getAsJsonObject();
-        id = jsonObject.getAsJsonPrimitive(JSON_KEY_ID).getAsLong();
-        shardId = jsonObject.getAsJsonPrimitive(JSON_KEY_SHARD_ID).getAsLong();
-        dataSize = jsonObject.getAsJsonPrimitive(JSON_KEY_DATA_SIZE).getAsLong();
-        rowCount = jsonObject.getAsJsonPrimitive(JSON_KEY_ROW_COUNT).getAsLong();
+        String json = GsonUtils.GSON.toJson(this);
+        Text.writeString(out, json);
     }
 
     public static StarOSTablet read(DataInput in) throws IOException {
-        StarOSTablet tablet = new StarOSTablet();
-        tablet.readFields(in);
-        return tablet;
+        String json = Text.readString(in);
+        return GsonUtils.GSON.fromJson(json, StarOSTablet.class);
     }
 }
