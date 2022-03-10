4af62619120c0b51ac7df0df0c10277d5f276fed
eyes_on_me <3675229+silverbullet233@users.noreply.github.com>
2022-03-10 11:23:57 +0800

    [JDBC External Table] Phase 4: add jdbc bridge (#3723)

---
+++ jdbc_bridge/src/main/java/com/starrocks/jdbcbridge/JDBCBridge.java
@@ -0,0 +1,37 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
+
+package com.starrocks.jdbcbridge;
+
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+
+/*
+* In order to simplify the implementation of jni cpp code, we add JDBCBridge as a bridge,
+* encapsulate some complex logic, and only provide the simplest interface for C++ calls.
+*
+* Currently, the implementation is relatively simple.
+* Each query will create a separate thread, load the JDBC driver class and create a short connection.
+* In fact, this has additional overhead.
+*
+* @TODO(silverbullet233):
+*   We can consider letting JDBCBridge manage the driver class loading and JDBC connection pool in the future.
+* */
+public class JDBCBridge {
+
+    public JDBCBridge() {
+    }
+
+    public void setClassLoader(String driverLocation) throws Exception {
+        URLClassLoader loader = URLClassLoader.newInstance(new URL[] {
+                new File(driverLocation).toURI().toURL(),
+        });
+        Thread.currentThread().setContextClassLoader(loader);
+    }
+
+    public JDBCScanner getScanner(JDBCScanContext scanContext) throws Exception {
+        JDBCScanner scanner = new JDBCScanner(scanContext);
+        return scanner;
+    }
+}
