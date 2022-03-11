e4a6336f342b8dfddd8d4a6982a5066746aa53fb
Binglin Chang <decstery@gmail.com>
2022-03-11 13:32:24 +0800

    Bugfix: make sure __op always return 0/1 string in json_scanner

--- be/src/exec/vectorized/json_scanner.cpp
+++ be/src/exec/vectorized/json_scanner.cpp
@@ -325,15 +325,19 @@ Status JsonReader::read_chunk(Chunk* chunk, int32_t rows_to_read, const std::vec
                     }
                     rapidjson::Value* json_values = JsonFunctions::get_json_object_from_parsed_json(
                             _scanner->_json_paths[i], objectValue, _origin_json_doc.GetAllocator());
-                    if (json_values == nullptr) {
-                        if (strcmp(column_name, "__op") == 0) {
-                            // special treatment for __op column, fill default value '0' rather than null
-                            column->append_strings(literal_0_slice_vector);
+                    if (strcmp(column_name, "__op") == 0) {
+                        // special treatment for __op column, fill default value '0' rather than null
+                        if (json_values != nullptr && json_values->IsInt() && json_values->GetInt() == 1) {
+                            column->append_strings(literal_1_slice_vector);
                         } else {
-                            column->append_nulls(1);
+                            column->append_strings(literal_0_slice_vector);
                         }
                     } else {
-                        _construct_column(*json_values, column.get(), slot_descs[i]->type());
+                        if (json_values == nullptr) {
+                            column->append_nulls(1);
+                        } else {
+                            _construct_column(*json_values, column.get(), slot_descs[i]->type());
+                        }
                     }
                 }
             }
