ddc4f893ee335e3fa4ff5544284c9188adac200a
ABingHuang <codekhuang@163.com>
2022-03-14 15:09:25 +0800

    add horizontal and vertical compaction tasks for new compaction framework

--- be/src/storage/compaction_task_factory.h
+++ be/src/storage/compaction_task_factory.h
@@ -28,6 +28,9 @@ public:
     std::shared_ptr<CompactionTask> create_compaction_task();
 
 private:
+    StatusOr<size_t> _get_segment_iterator_num();
+
+private:
     Version _output_version;
     TabletSharedPtr _tablet;
     std::vector<RowsetSharedPtr> _input_rowsets;
