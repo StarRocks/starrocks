655a6612c5f8d9018807218ff43d8726422d5f22
HangyuanLiu <460660596@qq.com>
2022-03-15 15:07:17 +0800

    Support PrivilegeChecker in new parser and fix create analyze fail bug #4098

--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/PrivilegeChecker.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/PrivilegeChecker.java
@@ -1,92 +1,117 @@
 // This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
 package com.starrocks.sql.analyzer;
 
-import com.google.common.base.Strings;
 import com.starrocks.analysis.AlterViewStmt;
 import com.starrocks.analysis.AlterWorkGroupStmt;
 import com.starrocks.analysis.CreateViewStmt;
 import com.starrocks.analysis.CreateWorkGroupStmt;
-import com.starrocks.analysis.DdlStmt;
 import com.starrocks.analysis.DropWorkGroupStmt;
-import com.starrocks.analysis.InlineViewRef;
 import com.starrocks.analysis.InsertStmt;
-import com.starrocks.analysis.QueryStmt;
-import com.starrocks.analysis.SelectStmt;
+import com.starrocks.analysis.ShowTableStatusStmt;
 import com.starrocks.analysis.StatementBase;
 import com.starrocks.analysis.TableName;
-import com.starrocks.analysis.TableRef;
 import com.starrocks.catalog.Catalog;
-import com.starrocks.catalog.View;
-import com.starrocks.cluster.ClusterNamespace;
-import com.starrocks.common.AnalysisException;
+import com.starrocks.catalog.Table;
 import com.starrocks.common.ErrorCode;
 import com.starrocks.common.ErrorReport;
-import com.starrocks.mysql.privilege.Auth;
 import com.starrocks.mysql.privilege.PrivPredicate;
 import com.starrocks.qe.ConnectContext;
+import com.starrocks.sql.ast.AstVisitor;
+import com.starrocks.sql.ast.QueryStatement;
+
+import java.util.Map;
 
 public class PrivilegeChecker {
-    public static void check(StatementBase statement, Auth auth, ConnectContext session) throws AnalysisException {
-        if (statement instanceof SelectStmt) {
-            SelectStmt selectStmt = (SelectStmt) statement;
+    public static void check(StatementBase statement, ConnectContext session) {
+        new PrivilegeCheckerVisitor().check(statement, session);
+    }
 
-            for (TableRef tblRef : selectStmt.getTableRefs()) {
-                if (tblRef instanceof InlineViewRef) {
-                    QueryStmt inlineStmt = ((InlineViewRef) tblRef).getViewStmt();
-                    if (selectStmt.hasWithClause()) {
-                        inlineStmt.setWithClause(selectStmt.getWithClause());
-                    }
-                    PrivilegeChecker.check(inlineStmt, auth, session);
-                } else {
-                    TableName tbName = tblRef.getName();
-                    String dbName = tbName.getDb();
-                    if (selectStmt.hasWithClause()) {
-                        for (View view : selectStmt.getWithClause().getViews()) {
-                            if (view.getName().equals(tblRef.getName().toString())) {
-                                PrivilegeChecker.check(view.getQueryStmt(), auth, session);
-                            }
-                        }
-                        continue;
-                    }
-                    if (Strings.isNullOrEmpty(dbName)) {
-                        dbName = session.getDatabase();
-                    } else {
-                        dbName = ClusterNamespace.getFullName(session.getClusterName(), tbName.getDb());
-                    }
+    private static class PrivilegeCheckerVisitor extends AstVisitor<Void, ConnectContext> {
+        public void check(StatementBase statement, ConnectContext session) {
+            visit(statement, session);
+        }
 
-                    if (!auth.checkTblPriv(session, dbName, tbName.getTbl(), PrivPredicate.SELECT)) {
-                        ErrorReport.reportAnalysisException(ErrorCode.ERR_TABLEACCESS_DENIED_ERROR, "SELECT",
-                                session.getQualifiedUser(), session.getRemoteIP(), tbName.getTbl());
-                    }
+        @Override
+        public Void visitAlterWorkGroupStatement(AlterWorkGroupStmt statement, ConnectContext session) {
+            if (!Catalog.getCurrentCatalog().getAuth().checkGlobalPriv(session, PrivPredicate.ADMIN)) {
+                ErrorReport.reportSemanticException(ErrorCode.ERR_ACCESS_DENIED_ERROR, "ALTER RESOURCE_GROUP");
+            }
+            return null;
+        }
+
+        @Override
+        public Void visitAlterViewStatement(AlterViewStmt statement, ConnectContext session) {
+            TableName tableName = statement.getTableName();
+            if (!Catalog.getCurrentCatalog().getAuth().checkTblPriv(session, tableName.getDb(),
+                    tableName.getTbl(), PrivPredicate.ALTER)) {
+                ErrorReport.reportSemanticException(ErrorCode.ERR_TABLEACCESS_DENIED_ERROR, "ALTER VIEW",
+                        session.getQualifiedUser(), session.getRemoteIP(), tableName.getTbl());
+            }
+            check(statement.getQueryStatement(), session);
+            return null;
+        }
+
+        @Override
+        public Void visitCreateViewStatement(CreateViewStmt statement, ConnectContext session) {
+            TableName tableName = statement.getTableName();
+            if (!Catalog.getCurrentCatalog().getAuth().checkTblPriv(session, tableName.getDb(),
+                    tableName.getTbl(), PrivPredicate.CREATE)) {
+                ErrorReport.reportSemanticException(ErrorCode.ERR_SPECIFIC_ACCESS_DENIED_ERROR, "CREATE");
+            }
+
+            check(statement.getQueryStatement(), session);
+            return null;
+        }
+
+        @Override
+        public Void visitCreateWorkGroupStatement(CreateWorkGroupStmt statement, ConnectContext session) {
+            if (!Catalog.getCurrentCatalog().getAuth().checkGlobalPriv(session, PrivPredicate.ADMIN)) {
+                ErrorReport.reportSemanticException(ErrorCode.ERR_ACCESS_DENIED_ERROR, "CREATE RESOURCE_GROUP");
+            }
+            return null;
+        }
+
+        @Override
+        public Void visitDropWorkGroupStatement(DropWorkGroupStmt statement, ConnectContext session) {
+            if (!Catalog.getCurrentCatalog().getAuth().checkGlobalPriv(session, PrivPredicate.ADMIN)) {
+                ErrorReport.reportSemanticException(ErrorCode.ERR_ACCESS_DENIED_ERROR, "DROP RESOURCE_GROUP");
+            }
+            return null;
+        }
+
+        @Override
+        public Void visitInsertStatement(InsertStmt statement, ConnectContext session) {
+            TableName tableName = statement.getTableName();
+            if (!Catalog.getCurrentCatalog().getAuth().checkTblPriv(session, tableName.getDb(),
+                    tableName.getTbl(), PrivPredicate.LOAD)) {
+                ErrorReport.reportSemanticException(ErrorCode.ERR_TABLEACCESS_DENIED_ERROR, "LOAD",
+                        session.getQualifiedUser(), session.getRemoteIP(), tableName.getTbl());
+            }
+            check(statement.getQueryStatement(), session);
+            return null;
+        }
+
+        @Override
+        public Void visitQueryStatement(QueryStatement stmt, ConnectContext session) {
+            Map<TableName, Table> tables = AnalyzerUtils.collectAllTable(stmt);
+            for (Map.Entry<TableName, Table> table : tables.entrySet()) {
+                TableName tableName = table.getKey();
+                if (!Catalog.getCurrentCatalog().getAuth().checkTblPriv(session, tableName.getDb(),
+                        tableName.getTbl(), PrivPredicate.SELECT)) {
+                    ErrorReport.reportSemanticException(ErrorCode.ERR_TABLEACCESS_DENIED_ERROR, "SELECT",
+                            session.getQualifiedUser(), session.getRemoteIP(), tableName.getTbl());
                 }
             }
-        } else if (statement instanceof InsertStmt) {
-            InsertStmt insertStmt = (InsertStmt) statement;
-            insertStmt.getTableName().normalization(session);
-            TableName tblName = insertStmt.getTableName();
+            return null;
+        }
 
-            if (!auth.checkTblPriv(session, tblName.getDb(), tblName.getTbl(), PrivPredicate.LOAD)) {
-                ErrorReport.reportAnalysisException(ErrorCode.ERR_TABLEACCESS_DENIED_ERROR, "LOAD",
-                        session.getQualifiedUser(), session.getRemoteIP(), tblName.getTbl());
+        @Override
+        public Void visitShowTableStatusStmt(ShowTableStatusStmt statement, ConnectContext session) {
+            String db = statement.getDb();
+            if (!Catalog.getCurrentCatalog().getAuth().checkDbPriv(session, db, PrivPredicate.SHOW)) {
+                ErrorReport.reportSemanticException(ErrorCode.ERR_DB_ACCESS_DENIED, session.getQualifiedUser(), db);
             }
-        } else if (statement instanceof DdlStmt) {
-            String hintMsg;
-            if (statement instanceof CreateWorkGroupStmt) {
-                hintMsg = "CREATE RESOURCE_GROUP";
-            } else if (statement instanceof DropWorkGroupStmt) {
-                hintMsg = "DROP RESOURCE_GROUP";
-            } else if (statement instanceof AlterWorkGroupStmt) {
-                hintMsg = "ALTER RESOURCE_GROUP";
-            } else if (statement instanceof AlterViewStmt) {
-                hintMsg = "ALTER VIEW";
-            } else if (statement instanceof CreateViewStmt) {
-                hintMsg = "CREATE VIEW";
-            } else {
-                throw new AnalysisException("Unsupported DdlStmt");
-            }
-            if (!Catalog.getCurrentCatalog().getAuth().checkGlobalPriv(ConnectContext.get(), PrivPredicate.ADMIN)) {
-                ErrorReport.reportAnalysisException(ErrorCode.ERR_ACCESS_DENIED_ERROR, hintMsg);
-            }
+            return null;
         }
     }
 }
