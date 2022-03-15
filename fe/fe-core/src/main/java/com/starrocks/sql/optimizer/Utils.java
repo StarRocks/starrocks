51d7a8b22c9a6eb44bff2d4284e6c56f28bc545e
liuyehcf <1559500551@qq.com>
2022-03-15 13:49:56 +0800

    Relax join reorder limitation

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/Utils.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/Utils.java
@@ -9,11 +9,13 @@ import com.starrocks.catalog.Catalog;
 import com.starrocks.catalog.Column;
 import com.starrocks.catalog.HiveMetaStoreTable;
 import com.starrocks.catalog.IcebergTable;
+import com.starrocks.catalog.KeysType;
 import com.starrocks.catalog.LocalTablet;
 import com.starrocks.catalog.MaterializedIndex;
 import com.starrocks.catalog.OlapTable;
 import com.starrocks.catalog.Partition;
 import com.starrocks.catalog.ScalarType;
+import com.starrocks.catalog.Table;
 import com.starrocks.catalog.Type;
 import com.starrocks.external.hive.HiveColumnStats;
 import com.starrocks.external.iceberg.cost.IcebergTableStatisticCalculator;
@@ -370,8 +372,16 @@ public class Utils {
                     scanOperator.getColRefToColumnMetaMap().values().stream().map(Column::getName).collect(
                             Collectors.toList());
             if (operator instanceof LogicalOlapScanOperator) {
+                Table table = scanOperator.getTable();
+                if (table instanceof OlapTable) {
+                    // Currently, aggregate model has no column statistical information
+                    // It is still allowed to perform ReorderJoinRule
+                    if (KeysType.AGG_KEYS.equals(((OlapTable) table).getKeysType())) {
+                        return false;
+                    }
+                }
                 List<ColumnStatistic> columnStatisticList =
-                        Catalog.getCurrentStatisticStorage().getColumnStatistics(scanOperator.getTable(), colNames);
+                        Catalog.getCurrentStatisticStorage().getColumnStatistics(table, colNames);
                 return columnStatisticList.stream().anyMatch(ColumnStatistic::isUnknown);
             } else if (operator instanceof LogicalHiveScanOperator || operator instanceof LogicalHudiScanOperator) {
                 HiveMetaStoreTable hiveMetaStoreTable = (HiveMetaStoreTable) scanOperator.getTable();
