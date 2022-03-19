85ad152cd556eedb93487ec4dae4fe4595d95107
Murphy <96611012+mofeiatwork@users.noreply.github.com>
2022-03-19 14:15:34 +0800

    Disable join on json type (#4255)

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/JsonTypeTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/JsonTypeTest.java
@@ -38,6 +38,23 @@ public class JsonTypeTest extends PlanTestBase {
         );
     }
 
+    @Test
+    public void testJoin() {
+        ExceptionChecker.expectThrowsWithMsg(SemanticException.class,
+                "Type percentile/hll/bitmap/json not support aggregation/group-by/order-by/union/join",
+                () -> getFragmentPlan("select * from tjson_test t1 join tjson_test t2 using(v_json)"));
+
+        ExceptionChecker.expectThrowsWithMsg(SemanticException.class,
+                "Type percentile/hll/bitmap/json not support aggregation/group-by/order-by/union/join",
+                () -> getFragmentPlan("select * from tjson_test t1 join tjson_test t2 on t1.v_json = t2.v_json"));
+        ExceptionChecker.expectThrowsWithMsg(SemanticException.class,
+                "Type percentile/hll/bitmap/json not support aggregation/group-by/order-by/union/join",
+                () -> getFragmentPlan("select * from tjson_test t1 join tjson_test t2 on t1.v_json > t2.v_json"));
+
+        ExceptionChecker.expectThrowsNoException(
+                () -> getFragmentPlan("select * from tjson_test t1 join tjson_test t2 on t1.v_id = t2.v_id"));
+    }
+
     /**
      * Arrow expression should be rewrite to json_query
      */
