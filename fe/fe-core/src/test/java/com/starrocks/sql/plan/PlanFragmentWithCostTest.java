e6f4caf84b7675c56c8383531a3184814010c6c1
stdpain <drfeng08@gmail.com>
2022-03-23 05:02:58 -0700

    fix UT

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/PlanFragmentWithCostTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/PlanFragmentWithCostTest.java
@@ -870,7 +870,7 @@ public class PlanFragmentWithCostTest extends PlanTestBase {
 
         sql = "select * from test_mv limit 10";
         planFragment = getFragmentPlan(sql);
-        Assert.assertTrue(planFragment.contains("tabletList=11356,11358,11360,"));
+        Assert.assertTrue(planFragment.contains("tabletList=11322,11324,11326"));
     }
 
     @Test
