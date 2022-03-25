018753ee782c651a93592a2eed9953a5a3db4a99
silverbullet233 <3675229+silverbullet233@users.noreply.github.com>
2022-03-25 13:58:19 +0800

    Revert "try to fix tscancode error"
    This reverts commit 48fef357baf3f838155f0cbc40a7f45319429a40.

--- be/src/exec/vectorized/olap_scan_node.cpp
+++ be/src/exec/vectorized/olap_scan_node.cpp
@@ -214,7 +214,6 @@ void OlapScanNode::_fill_chunk_pool(int count, bool force_column_pool) {
 }
 
 void OlapScanNode::_scanner_thread(TabletScanner* scanner) {
-    DCHECK(scanner != nullptr);
     MemTracker* prev_tracker = tls_thread_status.set_mem_tracker(scanner->runtime_state()->instance_mem_tracker());
     DeferOp op([&] {
         tls_thread_status.set_mem_tracker(prev_tracker);
@@ -305,13 +304,8 @@ void OlapScanNode::_scanner_thread(TabletScanner* scanner) {
             _close_pending_scanners();
         }
     } else {
-        if (scanner != nullptr) {
-            scanner->close(_runtime_state);
-            _closed_scanners.fetch_add(1, std::memory_order_release);
-            _close_pending_scanners();
-        } else {
-            _close_pending_scanners();
-        }
+        scanner->close(_runtime_state);
+        _closed_scanners.fetch_add(1, std::memory_order_release);
         _close_pending_scanners();
     }
 
