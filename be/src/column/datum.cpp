d77512f9f3f24e251ebf4ff23c1fee29ee87c760
Hongbin <xlwh@users.noreply.github.com>
2022-03-25 22:02:53 +0800

    Remove unused file (#4459)

--- be/src/column/datum.cpp
+++
@@ -1,9 +0,0 @@
-// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
-
-#include "column/datum.h"
-
-#include "gutil/strings/substitute.h"
-#include "runtime/mem_pool.h"
-#include "storage/types.h"
-
-namespace starrocks::vectorized {} // namespace starrocks::vectorized
