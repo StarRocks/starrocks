stdpain/starrocks-2

support more lowcardinality optimization
1. case when
2. if
3. projection predicate

fe/fe-core/src/main/java/com/starrocks/sql/plan/ScalarOperatorToExpr.java
@@ -15,6 +15,7 @@ import com.starrocks.analysis.CastExpr;
 import com.starrocks.analysis.CompoundPredicate;
 import com.starrocks.analysis.DateLiteral;
 import com.starrocks.analysis.DecimalLiteral;
+import com.starrocks.analysis.DictExpr;
 import com.starrocks.analysis.Expr;
 import com.starrocks.analysis.FloatLiteral;
 import com.starrocks.analysis.FunctionCallExpr;
@@ -27,7 +28,9 @@ import com.starrocks.analysis.IsNullPredicate;
 import com.starrocks.analysis.LargeIntLiteral;
 import com.starrocks.analysis.LikePredicate;
 import com.starrocks.analysis.NullLiteral;
+import com.starrocks.analysis.SlotRef;
 import com.starrocks.analysis.StringLiteral;
+import com.starrocks.analysis.PlaceHolderExpr;
 import com.starrocks.catalog.Function;
 import com.starrocks.catalog.Type;
 import com.starrocks.sql.optimizer.operator.scalar.ArrayElementOperator;
@@ -40,6 +43,7 @@ import com.starrocks.sql.optimizer.operator.scalar.CastOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ColumnRefOperator;
 import com.starrocks.sql.optimizer.operator.scalar.CompoundPredicateOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ConstantOperator;
+import com.starrocks.sql.optimizer.operator.scalar.DictMappingOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ExistsPredicateOperator;
 import com.starrocks.sql.optimizer.operator.scalar.InPredicateOperator;
 import com.starrocks.sql.optimizer.operator.scalar.IsNullPredicateOperator;
@@ -443,5 +447,22 @@ public class ScalarOperatorToExpr {
             result.setType(operator.getType());
             return result;
         }
+
+        @Override
+        public Expr visitDictMappingOperator(DictMappingOperator operator, FormatterContext context) {
+            final ColumnRefOperator argument = operator.getArgument();
+            final SlotRef dictExpr = (SlotRef) argument.accept(this, context);
+            final ScalarOperator call = operator.getInnerOperator();
+            final ColumnRefOperator key =
+                    new ColumnRefOperator(call.getUsedColumns().getFirstId(), Type.VARCHAR,
+                            operator.getArgument().getName(),
+                            dictExpr.isNullable());
+            context.colRefToExpr.put(key, new PlaceHolderExpr(key.getId(), dictExpr.isNullable(), Type.VARCHAR));
+            final Expr callExpr = buildExecExpression(call, context);
+            Expr result = new DictExpr(dictExpr, callExpr);
+            result.setType(operator.getType());
+            return result;
+        }
+
     }
 }

gensrc/thrift/Exprs.thrift
@@ -57,6 +57,9 @@ enum TExprNodeType {
   ARRAY_SLICE_EXPR,
 
   TABLE_FUNCTION_EXPR,
+
+  DICT_EXPR,
+  PLACEHOLDER_EXPR,
 }
 
 //enum TAggregationOp {
@@ -136,6 +139,11 @@ struct TSlotRef {
   2: required Types.TTupleId tuple_id
 }
 
+struct TPlaceHolder {
+  1: required bool nullable;
+  2: required i32 slot_id;
+}
+
 struct TStringLiteral {
   1: required string value;
 }
@@ -189,6 +197,8 @@ struct TExprNode {
   // If set, child[vararg_start_idx] is the first vararg child.
   27: optional i32 vararg_start_idx
   28: optional Types.TPrimitiveType child_type
+
+  29: optional TPlaceHolder vslot_ref;
 
   // For vector query engine
   50: optional bool use_vectorized

StarRocks/starrocks

fe/fe-core/src/main/java/com/starrocks/sql/plan/ScalarOperatorToExpr.java
@@ -15,6 +15,7 @@ import com.starrocks.analysis.CastExpr;
 import com.starrocks.analysis.CompoundPredicate;
 import com.starrocks.analysis.DateLiteral;
 import com.starrocks.analysis.DecimalLiteral;
+import com.starrocks.analysis.DictMappingExpr;
 import com.starrocks.analysis.Expr;
 import com.starrocks.analysis.FloatLiteral;
 import com.starrocks.analysis.FunctionCallExpr;
@@ -27,6 +28,8 @@ import com.starrocks.analysis.IsNullPredicate;
 import com.starrocks.analysis.LargeIntLiteral;
 import com.starrocks.analysis.LikePredicate;
 import com.starrocks.analysis.NullLiteral;
+import com.starrocks.analysis.PlaceHolderExpr;
+import com.starrocks.analysis.SlotRef;
 import com.starrocks.analysis.StringLiteral;
 import com.starrocks.catalog.Function;
 import com.starrocks.catalog.Type;
@@ -40,6 +43,7 @@ import com.starrocks.sql.optimizer.operator.scalar.CastOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ColumnRefOperator;
 import com.starrocks.sql.optimizer.operator.scalar.CompoundPredicateOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ConstantOperator;
+import com.starrocks.sql.optimizer.operator.scalar.DictMappingOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ExistsPredicateOperator;
 import com.starrocks.sql.optimizer.operator.scalar.InPredicateOperator;
 import com.starrocks.sql.optimizer.operator.scalar.IsNullPredicateOperator;
@@ -443,5 +447,22 @@ public class ScalarOperatorToExpr {
             result.setType(operator.getType());
             return result;
         }
+
+        @Override
+        public Expr visitDictMappingOperator(DictMappingOperator operator, FormatterContext context) {
+            final ColumnRefOperator dictColumn = operator.getDictColumn();
+            final SlotRef dictExpr = (SlotRef) dictColumn.accept(this, context);
+            final ScalarOperator call = operator.getOriginScalaOperator();
+            final ColumnRefOperator key =
+                    new ColumnRefOperator(call.getUsedColumns().getFirstId(), Type.VARCHAR,
+                            operator.getDictColumn().getName(),
+                            dictExpr.isNullable());
+            context.colRefToExpr.put(key, new PlaceHolderExpr(key.getId(), dictExpr.isNullable(), Type.VARCHAR));
+            final Expr callExpr = buildExecExpression(call, context);
+            Expr result = new DictMappingExpr(dictExpr, callExpr);
+            result.setType(operator.getType());
+            return result;
+        }
+
     }
 }

gensrc/thrift/Exprs.thrift
@@ -57,6 +57,9 @@ enum TExprNodeType {
   ARRAY_SLICE_EXPR,
 
   TABLE_FUNCTION_EXPR,
+
+  DICT_EXPR,
+  PLACEHOLDER_EXPR,
 }
 
 //enum TAggregationOp {
@@ -136,6 +139,11 @@ struct TSlotRef {
   2: required Types.TTupleId tuple_id
 }
 
+struct TPlaceHolder {
+  1: optional bool nullable;
+  2: optional i32 slot_id;
+}
+
 struct TStringLiteral {
   1: required string value;
 }
@@ -189,6 +197,8 @@ struct TExprNode {
   // If set, child[vararg_start_idx] is the first vararg child.
   27: optional i32 vararg_start_idx
   28: optional Types.TPrimitiveType child_type
+
+  29: optional TPlaceHolder vslot_ref;
 
   // For vector query engine
   50: optional bool use_vectorized