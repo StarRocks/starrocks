da96a52473979ce6a7d286337215d0cb3bec36b8
trueeyu <lxhhust350@qq.com>
2022-03-29 09:41:59 +0800

    Fix

--- be/src/exec/pipeline/table_function_operator.h
+++ be/src/exec/pipeline/table_function_operator.h
@@ -19,7 +19,7 @@ public:
 
     Status prepare(RuntimeState* state) override;
 
-    void close(RuntimeState* state) override;
+    Status close(RuntimeState* state) override;
 
     bool has_output() const override;
 
