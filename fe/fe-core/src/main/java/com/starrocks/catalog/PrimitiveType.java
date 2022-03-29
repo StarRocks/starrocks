19bfddced90479887e67eeaec520152b484d5ac4
Murphy <96611012+mofeiatwork@users.noreply.github.com>
2022-03-29 14:27:51 +0800

    Fix statistic of json type (#4513)
    (cherry picked from commit d385444cc4e7b3fe1d2ef3ebf99e677e35641e89)

--- fe/fe-core/src/main/java/com/starrocks/catalog/PrimitiveType.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/PrimitiveType.java
@@ -357,6 +357,11 @@ public enum PrimitiveType {
                 // 1MB
                 typeSize = 1024 * 1024;
                 break;
+            case JSON:
+                typeSize = 1024;
+                break;
+            default:
+                Preconditions.checkState(false, "unreachable");
         }
         return typeSize;
     }
@@ -403,6 +408,10 @@ public enum PrimitiveType {
 
     public boolean isStringType() {
         return (this == VARCHAR || this == CHAR || this == HLL);
+    }
+
+    public boolean isJsonType() {
+        return this == JSON;
     }
 
     public boolean isCharFamily() {
