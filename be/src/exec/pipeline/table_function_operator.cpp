16627eece5ebec3eee5e664fb92641eda36da9b8
stdpain <drfeng08@gmail.com>
2022-03-29 19:48:55 +0800

    fix wrong order of destructions in pipeline engine

--- be/src/exec/pipeline/table_function_operator.cpp
+++ be/src/exec/pipeline/table_function_operator.cpp
@@ -66,7 +66,8 @@ Status TableFunctionOperator::prepare(RuntimeState* state) {
         return_types.emplace_back(return_type.type);
     }
 
-    _table_function = vectorized::get_table_function(table_function_name, arg_types, return_types);
+    _table_function =
+            vectorized::get_table_function(table_function_name, arg_types, return_types, table_fn.binary_type);
     if (_table_function == nullptr) {
         return Status::InternalError("can't find table function " + table_function_name);
     }
