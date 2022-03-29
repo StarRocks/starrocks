81a8fbbcd823232b3b0eba93a11097b640d6c11f
trueeyu <lxhhust350@qq.com>
2022-03-29 13:00:38 +0800

    [UT] fix BE UT crash in some case (#3487) (#4532)
    `mock_consumer` may be schedule after submit_task.
    
    Co-authored-by: stdpain <34912776+stdpain@users.noreply.github.com>

--- be/src/runtime/routine_load/routine_load_task_executor.cpp
+++ be/src/runtime/routine_load/routine_load_task_executor.cpp
@@ -327,6 +327,7 @@ void RoutineLoadTaskExecutor::err_handler(StreamLoadContext* ctx, const Status& 
 
 // for test only
 Status RoutineLoadTaskExecutor::_execute_plan_for_test(StreamLoadContext* ctx) {
+    ctx->ref();
     auto mock_consumer = [this, ctx]() {
         std::shared_ptr<StreamLoadPipe> pipe = _exec_env->load_stream_mgr()->get(ctx->id);
         bool eof = false;
@@ -353,6 +354,9 @@ Status RoutineLoadTaskExecutor::_execute_plan_for_test(StreamLoadContext* ctx) {
             } else {
                 ss << one;
             }
+        }
+        if (ctx->unref()) {
+            delete ctx;
         }
     };
 
