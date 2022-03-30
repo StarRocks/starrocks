61d4226af53e898a364e39922d1ca3483750840e
HangyuanLiu <460660596@qq.com>
2022-03-30 14:57:51 +0800

    Support array slice in fe (#4578)

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/transformer/SqlToScalarOperatorTranslator.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/transformer/SqlToScalarOperatorTranslator.java
@@ -7,6 +7,7 @@ import com.starrocks.analysis.AnalyticExpr;
 import com.starrocks.analysis.ArithmeticExpr;
 import com.starrocks.analysis.ArrayElementExpr;
 import com.starrocks.analysis.ArrayExpr;
+import com.starrocks.analysis.ArraySliceExpr;
 import com.starrocks.analysis.ArrowExpr;
 import com.starrocks.analysis.BetweenPredicate;
 import com.starrocks.analysis.BinaryPredicate;
@@ -43,6 +44,7 @@ import com.starrocks.sql.common.StarRocksPlannerException;
 import com.starrocks.sql.optimizer.base.ColumnRefFactory;
 import com.starrocks.sql.optimizer.operator.scalar.ArrayElementOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ArrayOperator;
+import com.starrocks.sql.optimizer.operator.scalar.ArraySliceOperator;
 import com.starrocks.sql.optimizer.operator.scalar.BetweenPredicateOperator;
 import com.starrocks.sql.optimizer.operator.scalar.BinaryPredicateOperator;
 import com.starrocks.sql.optimizer.operator.scalar.CallOperator;
@@ -199,6 +201,14 @@ public final class SqlToScalarOperatorTranslator {
             ScalarOperator arrayOperator = visit(node.getChild(0));
             ScalarOperator subscriptOperator = visit(node.getChild(1));
             return new ArrayElementOperator(node.getType(), arrayOperator, subscriptOperator);
+        }
+
+        @Override
+        public ScalarOperator visitArraySliceExpr(ArraySliceExpr node, Void context) {
+            ScalarOperator arrayOperator = visit(node.getChild(0));
+            ScalarOperator lowerBound = visit(node.getChild(1));
+            ScalarOperator upperBound = visit(node.getChild(2));
+            return new ArraySliceOperator(node.getType(), Lists.newArrayList(arrayOperator, lowerBound, upperBound));
         }
 
         @Override
