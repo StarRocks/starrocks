382778cb4e7137821a87cf454a89528fa674e871
yanz <dirtysalt1987@gmail.com>
2022-04-01 17:28:42 +0800

    skip zero length file at frontend

--- fe/fe-core/src/main/java/com/starrocks/external/RemoteScanRangeLocations.java
+++ fe/fe-core/src/main/java/com/starrocks/external/RemoteScanRangeLocations.java
@@ -132,6 +132,9 @@ public class RemoteScanRangeLocations {
         for (int i = 0; i < partitions.size(); i++) {
             descTbl.addReferencedPartitions(table, partitionInfos.get(i));
             for (HdfsFileDesc fileDesc : partitions.get(i).getFiles()) {
+                if (fileDesc.getLength() == 0) {
+                    continue;
+                }
                 for (HdfsFileBlockDesc blockDesc : fileDesc.getBlockDescs()) {
                     addScanRangeLocations(partitionInfos.get(i).getId(), partitions.get(i), fileDesc, blockDesc);
                     LOG.debug("Add scan range success. partition: {}, file: {}, block: {}-{}",
