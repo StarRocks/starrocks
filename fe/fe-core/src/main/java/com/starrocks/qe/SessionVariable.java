556e18dd02e834405c9564fbf79b9f92c0205b06
satanson <ranpanf@gmail.com>
2022-04-06 10:10:18 +0800

    set runtime_filter_scan_wait_time = 20 in default

--- fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
@@ -196,8 +196,8 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
 
     public static final String RUNTIME_FILTER_SCAN_WAIT_TIME = "runtime_filter_scan_wait_time";
 
-    @VariableMgr.VarAttr(name = RUNTIME_FILTER_SCAN_WAIT_TIME)
-    private long runtimeFilterScanWaitTime = 60L;
+    @VariableMgr.VarAttr(name = RUNTIME_FILTER_SCAN_WAIT_TIME, flag = VariableMgr.INVISIBLE)
+    private long runtimeFilterScanWaitTime = 20L;
 
     @VariableMgr.VarAttr(name = ENABLE_PIPELINE_ENGINE)
     private boolean enablePipelineEngine = false;
