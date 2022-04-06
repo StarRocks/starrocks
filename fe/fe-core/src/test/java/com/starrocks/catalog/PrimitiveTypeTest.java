f64b3e98dba4375d7a678212b58796b0183fadaa
Murphy <96611012+mofeiatwork@users.noreply.github.com>
2022-04-06 14:15:00 +0800

    Fix getTypeSize of INVALID_TYPE (#4824)

---
+++ fe/fe-core/src/test/java/com/starrocks/catalog/PrimitiveTypeTest.java
@@ -0,0 +1,17 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
+
+package com.starrocks.catalog;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PrimitiveTypeTest {
+
+    @Test
+    public void testGetTypeSize() {
+        for (PrimitiveType ptype : PrimitiveType.values()) {
+            int size = ptype.getTypeSize();
+            Assert.assertTrue(size >= 0);
+        }
+    }
+}
