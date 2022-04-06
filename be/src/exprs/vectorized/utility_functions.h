55737da0f15a58970298fb63f847aa9238af8df2
stdpain <34912776+stdpain@users.noreply.github.com>
2022-04-06 19:00:55 +0800

    support a faster unique id implement (#4701)

--- be/src/exprs/vectorized/utility_functions.h
+++ be/src/exprs/vectorized/utility_functions.h
@@ -27,6 +27,12 @@ public:
      * returns uuid.
      */
     DEFINE_VECTORIZED_FN(uuid);
+    /**
+     * Returns an approximate UUID.
+     * timestamp(64bit) + backend_id(32bit: hash(ip) ^ port) + rand (16bit) +
+     * tid(thread id 32 bit) + i (increment 16 bit)
+     */
+    DEFINE_VECTORIZED_FN(uuid_numeric);
 };
 
 } // namespace vectorized
