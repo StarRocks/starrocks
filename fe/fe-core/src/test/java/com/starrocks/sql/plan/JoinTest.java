f1c877fd61522bad5fd8feea1ffcb058bea8ec81
zihe.liu <ziheliu1024@gmail.com>
2022-04-07 14:00:29 +0800

    Use fragment instance parallel for bucket shuffle and colocate join (#4831)
    Local bucket shuffle and colocate join should use fragment instance parallel instead of pipeline parallel, to avoid local shuffle and too large in-filter in the left scan node, even if local bucket shuffle join is succeeded by a broadcast join.
    Otherwise, the merged in-filter may be too large to push down to the storage level.

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/JoinTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/JoinTest.java
@@ -2,5 +2,80 @@
 
 package com.starrocks.sql.plan;
 
+import com.starrocks.common.FeConstants;
+import com.starrocks.planner.PlanFragment;
+import com.starrocks.qe.SessionVariable;
+import com.starrocks.system.BackendCoreStat;
+import com.starrocks.thrift.TExplainLevel;
+import mockit.Mock;
+import mockit.MockUp;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class JoinTest extends PlanTestBase {
+    @Test
+    public void testParallelism() throws Exception {
+        int numCores = 8;
+        int expectedParallelism = numCores / 2;
+        new MockUp<BackendCoreStat>() {
+            @Mock
+            public int getAvgNumOfHardwareCoresOfBe() {
+                return numCores;
+            }
+        };
+
+        SessionVariable sessionVariable = connectContext.getSessionVariable();
+        boolean enablePipeline = sessionVariable.isEnablePipelineEngine();
+        int pipelineDop = sessionVariable.getPipelineDop();
+        int parallelExecInstanceNum = sessionVariable.getParallelExecInstanceNum();
+
+        try {
+            // Enable DopAutoEstimate.
+            sessionVariable.setEnablePipelineEngine(true);
+            sessionVariable.setPipelineDop(0);
+            sessionVariable.setParallelExecInstanceNum(1);
+            FeConstants.runningUnitTest = true;
+
+            // Case 1: local bucket shuffle join should use fragment instance parallel.
+            String sql = "select a.v1 from t0 a join [bucket] t0 b on a.v1 = b.v2 and a.v2 = b.v1";
+            ExecPlan plan = getExecPlan(sql);
+            PlanFragment fragment = plan.getFragments().get(1);
+            assertContains(fragment.getExplainString(TExplainLevel.NORMAL), "join op: INNER JOIN (BUCKET_SHUFFLE)");
+            Assert.assertEquals(expectedParallelism, fragment.getParallelExecNum());
+            Assert.assertEquals(1, fragment.getPipelineDop());
+
+            // Case 2: colocate join should use fragment instance parallel.
+            sql = "SELECT * from t0 join t0 as b on t0.v1 = b.v1;";
+            plan = getExecPlan(sql);
+            fragment = plan.getFragments().get(1);
+            assertContains(fragment.getExplainString(TExplainLevel.NORMAL), "join op: INNER JOIN (COLOCATE)");
+            Assert.assertEquals(expectedParallelism, fragment.getParallelExecNum());
+            Assert.assertEquals(1, fragment.getPipelineDop());
+
+            // Case 3: broadcast join should use pipeline parallel.
+            sql = "select a.v1 from t0 a join [broadcast] t0 b on a.v1 = b.v2 and a.v2 = b.v1";
+            plan = getExecPlan(sql);
+            fragment = plan.getFragments().get(1);
+            assertContains(fragment.getExplainString(TExplainLevel.NORMAL), "join op: INNER JOIN (BROADCAST)");
+            Assert.assertEquals(1, fragment.getParallelExecNum());
+            Assert.assertEquals(expectedParallelism, fragment.getPipelineDop());
+
+            // Case 4: local bucket shuffle join succeeded by broadcast should use fragment instance parallel.
+            sql = "select a.v1 from t0 a " +
+                    "join [bucket] t0 b on a.v1 = b.v2 and a.v2 = b.v1 " +
+                    "join [broadcast] t0 c on a.v1 = c.v2";
+            plan = getExecPlan(sql);
+            fragment = plan.getFragments().get(1);
+            String fragmentString = fragment.getExplainString(TExplainLevel.NORMAL);
+            assertContains(fragmentString, "join op: INNER JOIN (BROADCAST)");
+            assertContains(fragmentString, "join op: INNER JOIN (BUCKET_SHUFFLE)");
+            Assert.assertEquals(expectedParallelism, fragment.getParallelExecNum());
+            Assert.assertEquals(1, fragment.getPipelineDop());
+        } finally {
+            sessionVariable.setEnablePipelineEngine(enablePipeline);
+            sessionVariable.setPipelineDop(pipelineDop);
+            sessionVariable.setParallelExecInstanceNum(parallelExecInstanceNum);
+            FeConstants.runningUnitTest = false;
+        }
+    }
 }
