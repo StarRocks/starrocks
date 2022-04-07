09798f388d23ebb7b7022a90e41e478d892bb685
HangyuanLiu <460660596@qq.com>
2022-04-07 12:46:18 +0800

    Array function type set to null when polymorphic type can't determine (#4842)

--- fe/fe-core/src/main/java/com/starrocks/catalog/FunctionSet.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/FunctionSet.java
@@ -881,8 +881,8 @@ public class FunctionSet {
         } else if (typeElement != null) {
             typeArray = new ArrayType(typeElement);
         } else {
-            LOG.warn("could not determine polymorphic type because has unknown types");
-            return null;
+            typeElement = Type.NULL;
+            typeArray = new ArrayType(Type.NULL);
         }
 
         if (!typeArray.getItemType().matchesType(typeElement)) {
