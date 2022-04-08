0b218acb1c0e60c1bc396127565d009bf6d51d3f
xyz <a997647204@gmail.com>
2022-04-08 15:10:01 +0800

    [optimize] Reducing the memory footprint of columnReader. (#4873) (#4875)
    In a large wide table scenario, when there are many columns, a large number of Column Readers will occupy a lot of memory. This pr is mainly to strip the data structures that can be shared between Column Readers and put them into Segment.
    Share these data field between ColumnReaders:

--- be/src/storage/rowset/segment.cpp
+++ be/src/storage/rowset/segment.cpp
@@ -67,8 +67,9 @@ StatusOr<std::shared_ptr<Segment>> Segment::open(MemTracker* mem_tracker, fs::Bl
                                                  const std::string& filename, uint32_t segment_id,
                                                  const TabletSchema* tablet_schema, size_t* footer_length_hint,
                                                  const FooterPointerPB* partial_rowset_footer) {
-    auto segment = std::shared_ptr<Segment>(new Segment(private_type(0), blk_mgr, filename, segment_id, tablet_schema),
-                                            DeleterWithMemTracker<Segment>(mem_tracker));
+    auto segment = std::shared_ptr<Segment>(
+            new Segment(private_type(0), blk_mgr, filename, segment_id, tablet_schema, mem_tracker),
+            DeleterWithMemTracker<Segment>(mem_tracker));
     mem_tracker->consume(segment->mem_usage());
 
     RETURN_IF_ERROR(segment->_open(mem_tracker, footer_length_hint, partial_rowset_footer));
@@ -166,8 +167,12 @@ Status Segment::parse_segment_footer(fs::ReadableBlock* rblock, SegmentFooterPB*
 }
 
 Segment::Segment(const private_type&, fs::BlockManager* blk_mgr, std::string fname, uint32_t segment_id,
-                 const TabletSchema* tablet_schema)
-        : _block_mgr(blk_mgr), _fname(std::move(fname)), _tablet_schema(tablet_schema), _segment_id(segment_id) {}
+                 const TabletSchema* tablet_schema, MemTracker* mem_tracker)
+        : _block_mgr(blk_mgr),
+          _fname(std::move(fname)),
+          _tablet_schema(tablet_schema),
+          _segment_id(segment_id),
+          _mem_tracker(mem_tracker) {}
 
 Status Segment::_open(MemTracker* mem_tracker, size_t* footer_length_hint,
                       const FooterPointerPB* partial_rowset_footer) {
@@ -270,11 +275,7 @@ Status Segment::_create_column_readers(MemTracker* mem_tracker, SegmentFooterPB*
             continue;
         }
 
-        ColumnReaderOptions opts;
-        opts.block_mgr = _block_mgr;
-        opts.storage_format_version = footer->version();
-        opts.kept_in_memory = _tablet_schema->is_in_memory();
-        auto res = ColumnReader::create(mem_tracker, opts, footer->mutable_columns(iter->second), _fname);
+        auto res = ColumnReader::create(footer->mutable_columns(iter->second), this);
         if (!res.ok()) {
             return res.status();
         }
