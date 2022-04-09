5208e36b5b9dedb21ab4751bf3a8d45990b2f2b2
hongbin <1079039435@qq.com>
2022-04-09 10:59:55 +0800

    fix comments

--- be/src/common/tracer.cpp
+++ be/src/common/tracer.cpp
@@ -2,58 +2,53 @@
 
 #include "tracer.h"
 
-#include "opentelemetry/exporters/ostream/span_exporter.h"
+#include "opentelemetry/exporters/jaeger/jaeger_exporter.h"
 
 namespace starrocks {
 
 namespace sdktrace = opentelemetry::sdk::trace;
 namespace trace = opentelemetry::trace;
-namespace nostd = opentelemetry::nostd;
 
-Tracer::Tracer(opentelemetry::nostd::string_view service_name) {
+Tracer::Tracer(const std::string& service_name, const TracerOptions& tracer_opts) : _tracer_options(tracer_opts) {
     init(service_name);
 }
 
-void Tracer::init(opentelemetry::nostd::string_view service_name) {
-    if (!tracer) {
-        auto exporter =
-                std::unique_ptr<sdktrace::SpanExporter>(new opentelemetry::exporter::trace::OStreamSpanExporter);
+void Tracer::init(const std::string& service_name) {
+    if (_tracer == nullptr) {
+        opentelemetry::exporter::jaeger::JaegerExporterOptions opts;
+        opts.endpoint = _tracer_options.jaeger_endpoint;
+        opts.server_port = _tracer_options.jaeger_server_port;
+        auto jaeger_exporter = std::unique_ptr<opentelemetry::sdk::trace::SpanExporter>(
+                new opentelemetry::exporter::jaeger::JaegerExporter(opts));
         auto processor = std::unique_ptr<opentelemetry::sdk::trace::SpanProcessor>(
-                new opentelemetry::sdk::trace::SimpleSpanProcessor(std::move(exporter)));
+                new opentelemetry::sdk::trace::SimpleSpanProcessor(std::move(jaeger_exporter)));
         const auto jaeger_resource = opentelemetry::sdk::resource::Resource::Create(
                 std::move(opentelemetry::sdk::resource::ResourceAttributes{{"service.name", service_name}}));
         const auto provider = opentelemetry::nostd::shared_ptr<opentelemetry::trace::TracerProvider>(
                 new opentelemetry::sdk::trace::TracerProvider(std::move(processor), jaeger_resource));
-        tracer = provider->GetTracer(service_name, OPENTELEMETRY_SDK_VERSION);
+        _tracer = provider->GetTracer(service_name, OPENTELEMETRY_SDK_VERSION);
     }
 }
 
 void Tracer::shutdown() {
-    if (tracer) {
-        tracer->CloseWithMicroseconds(1);
+    if (_tracer != nullptr) {
+        _tracer->CloseWithMicroseconds(1);
     }
 }
 
-Span Tracer::start_trace(opentelemetry::nostd::string_view trace_name) {
-    return tracer->StartSpan(trace_name);
+Span Tracer::start_trace(const std::string& trace_name) {
+    return _tracer->StartSpan(trace_name);
 }
 
-Span Tracer::add_span(opentelemetry::nostd::string_view span_name, const Span& parent_span) {
-    if (is_enabled() && parent_span) {
-        const auto parent_ctx = parent_span->GetContext();
-        return add_span(span_name, parent_ctx);
-    }
+Span Tracer::add_span(const std::string& span_name, const Span& parent_span) {
+    const auto parent_ctx = parent_span->GetContext();
+    return add_span(span_name, parent_ctx);
 }
 
-Span Tracer::add_span(opentelemetry::nostd::string_view span_name, const SpanContext& parent_ctx) {
+Span Tracer::add_span(const std::string& span_name, const SpanContext& parent_ctx) {
     opentelemetry::trace::StartSpanOptions span_opts;
     span_opts.parent = parent_ctx;
-
-    return tracer->StartSpan(span_name, span_opts);
-}
-
-bool Tracer::is_enabled() const {
-    return true;
+    return _tracer->StartSpan(span_name, span_opts);
 }
 
 } // namespace starrocks
--- be/src/common/tracer.h
+++ be/src/common/tracer.h
@@ -2,42 +2,56 @@
 
 #pragma once
 
-#include "opentelemetry/trace/provider.h"
 #include "opentelemetry/sdk/trace/simple_processor.h"
 #include "opentelemetry/sdk/trace/tracer_provider.h"
+#include "opentelemetry/trace/provider.h"
 
 namespace starrocks {
 
 using Span = opentelemetry::nostd::shared_ptr<opentelemetry::trace::Span>;
 using SpanContext = opentelemetry::trace::SpanContext;
 
-class Tracer {
-private:
-    opentelemetry::nostd::shared_ptr<opentelemetry::trace::Tracer> tracer;
+// The tracer options.
+struct TracerOptions {
+    std::string jaeger_endpoint;
+    int jaeger_server_port;
+};
 
+/*
+ * Support trace and export to jaeger.
+ * Use example:
+ *    std::unique_ptr<Tracer> tracer = std::make_unique<Tracer>("BasicTest");
+ *    auto f1_span = tracer->start_trace("span1");
+ *    auto f2_span = tracer->add_span("span2", f1_span);
+ *    f2_span->End();
+ *
+ */
+class Tracer {
 public:
     Tracer() = default;
-    Tracer(opentelemetry::nostd::string_view service_name);
+    Tracer(const std::string& service_name, const TracerOptions& tracer_opts = {"localhost", 6381});
 
     // Init the tracer.
-    void init(opentelemetry::nostd::string_view service_name);
+    void init(const std::string& service_name);
+
     // Shutdown the tracer.
     void shutdown();
 
-    bool is_enabled() const;
-
     // Creates and returns a new span with `trace_name`
     // this span represents a trace, since it has no parent.
-    Span start_trace(opentelemetry::nostd::string_view trace_name);
+    Span start_trace(const std::string& trace_name);
 
     // Creates and returns a new span with `span_name` which parent span is `parent_span'.
-    Span add_span(opentelemetry::nostd::string_view span_name, const Span& parent_span);
+    Span add_span(const std::string& span_name, const Span& parent_span);
 
     // Creates and return a new span with `span_name`
     // the span is added to the trace which it's context is `parent_ctx`.
     // parent_ctx contains the required information of the trace.
-    Span add_span(opentelemetry::nostd::string_view span_name, const SpanContext& parent_ctx);
+    Span add_span(const std::string& span_name, const SpanContext& parent_ctx);
+
+private:
+    opentelemetry::nostd::shared_ptr<opentelemetry::trace::Tracer> _tracer;
+    TracerOptions _tracer_options;
 };
 
-}
-
+} // namespace starrocks
