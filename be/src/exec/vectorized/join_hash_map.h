1897f093a44478253a89d02cc148d95c224c88fc
trueeyu <lxhhust350@qq.com>
2022-04-14 17:25:15 +0800

    HashJoin support large binary column (#5064)

--- be/src/exec/vectorized/join_hash_map.h
+++ be/src/exec/vectorized/join_hash_map.h
@@ -103,6 +103,7 @@ struct JoinHashTableItems {
     bool need_create_tuple_columns = true;
     bool left_to_nullable = false;
     bool right_to_nullable = false;
+    bool has_large_column = false;
 
     TJoinOp::type join_type = TJoinOp::INNER_JOIN;
 
@@ -574,9 +575,9 @@ public:
     void create(const HashTableParam& param);
     void close();
 
-    void build(RuntimeState* state);
-    void probe(RuntimeState* state, const Columns& key_columns, ChunkPtr* probe_chunk, ChunkPtr* chunk, bool* eos);
-    void probe_remain(RuntimeState* state, ChunkPtr* chunk, bool* eos);
+    Status build(RuntimeState* state);
+    Status probe(RuntimeState* state, const Columns& key_columns, ChunkPtr* probe_chunk, ChunkPtr* chunk, bool* eos);
+    Status probe_remain(RuntimeState* state, ChunkPtr* chunk, bool* eos);
 
     void append_chunk(RuntimeState* state, const ChunkPtr& chunk, const Columns& key_columns);
 
@@ -594,6 +595,8 @@ public:
 private:
     JoinHashMapType _choose_join_hash_map();
     static size_t _get_size_of_fixed_and_contiguous_type(PrimitiveType data_type);
+
+    Status _upgrade_key_columns_if_overflow();
 
     void _remove_duplicate_index_for_left_outer_join(Column::Filter* filter);
     void _remove_duplicate_index_for_left_semi_join(Column::Filter* filter);
