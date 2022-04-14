ee3dd08605ceded2f7e353cb886756e10955e857
mofei <mofei@starrocks.com>
2022-04-14 09:51:04 +0800

    implement cascade mergerj

--- be/src/runtime/data_stream_recvr.h
+++ be/src/runtime/data_stream_recvr.h
@@ -38,6 +38,7 @@ namespace starrocks {
 
 namespace vectorized {
 class SortedChunksMerger;
+class CascadeChunkMerger;
 }
 
 class DataStreamMgr;
@@ -166,6 +167,7 @@ private:
 
     // vectorized::SortedChunksMerger merges chunks from different senders.
     std::unique_ptr<vectorized::SortedChunksMerger> _chunks_merger;
+    std::unique_ptr<vectorized::CascadeChunkMerger> _cascade_merger;
 
     // Pool of sender queues.
     ObjectPool _sender_queue_pool;
