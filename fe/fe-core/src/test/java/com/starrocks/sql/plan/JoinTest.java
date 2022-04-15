30abf7520e6b9d2c72e34dbb40f4292d7903a5c3
Seaven <seaven_7@qq.com>
2022-04-15 15:49:55 +0800

    Fix limit on subquery lose gather required (#5022)

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/JoinTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/JoinTest.java
@@ -2,7 +2,6 @@
 
 package com.starrocks.sql.plan;
 
-import com.starrocks.analysis.StatementBase;
 import com.starrocks.catalog.Catalog;
 import com.starrocks.catalog.Database;
 import com.starrocks.catalog.OlapTable;
@@ -16,7 +15,6 @@ import com.starrocks.sql.optimizer.rule.RuleSet;
 import com.starrocks.sql.optimizer.rule.transformation.JoinAssociativityRule;
 import com.starrocks.system.BackendCoreStat;
 import com.starrocks.thrift.TExplainLevel;
-import com.starrocks.utframe.UtFrameUtils;
 import mockit.Expectations;
 import mockit.Mock;
 import mockit.MockUp;
@@ -2142,7 +2140,7 @@ public class JoinTest extends PlanTestBase {
                 "limit \n" +
                 "  155;";
         String plan = getFragmentPlan(sql);
-        Assert.assertTrue(plan.contains("6:Project\n" +
+        Assert.assertTrue(plan.contains("7:Project\n" +
                 "  |  <slot 2> : 2: v2"));
     }
 
