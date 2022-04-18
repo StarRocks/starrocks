df403a3d3b1e381b41a50d90f7c6d70da49a4f4a
mofei <mofei@starrocks.com>
2022-04-18 15:27:37 +0800

    fix conflict

--- be/test/exec/vectorized/sorting_test.cpp
+++ be/test/exec/vectorized/sorting_test.cpp
@@ -133,58 +133,6 @@ INSTANTIATE_TEST_SUITE_P(
 ));
 // clang-format on
 
-TEST(MergeTest, merge_sorter_chunks_two_way) {
-    TypeDescriptor type_desc = TypeDescriptor(TYPE_INT);
-    ColumnPtr col1 = ColumnHelper::create_column(type_desc, false);
-    ColumnPtr col2 = ColumnHelper::create_column(type_desc, false);
-    ColumnPtr col3 = ColumnHelper::create_column(type_desc, false);
-    ColumnPtr col4 = ColumnHelper::create_column(type_desc, false);
-    std::vector<int32_t> elements_col1{1, 1, 1, 2, 2, 3, 4, 5, 6};
-    std::vector<int32_t> elements_col2{1, 2, 2, 2, 3, 3, 6, 7, 8};
-    std::vector<int32_t> elements_col3{1, 2, 2, 2, 3, 3, 6, 7, 8};
-    std::vector<int32_t> elements_col4{2, 3, 4, 4, 4, 8, 9, 7, 11};
-
-    for (int i = 0; i < elements_col1.size(); i++) {
-        col1->append_datum(Datum(elements_col1[i]));
-        col2->append_datum(Datum(elements_col2[i]));
-        col3->append_datum(Datum(elements_col3[i]));
-        col4->append_datum(Datum(elements_col4[i]));
-    }
-    Chunk::SlotHashMap map;
-    map[0] = 0;
-    map[1] = 1;
-
-    ChunkPtr chunk1 = std::make_shared<Chunk>(Columns{col1, col3}, map);
-    ChunkPtr chunk2 = std::make_shared<Chunk>(Columns{col2, col4}, map);
-    Permutation perm;
-    SortDescs sort_desc({1, 1}, {-1, -1});
-    merge_sorted_chunks_two_way(sort_desc, chunk1, chunk2, &perm);
-
-    size_t expected_size = col1->size() + col2->size();
-    std::unique_ptr<Chunk> output = chunk1->clone_empty();
-    append_by_permutation(output.get(), std::vector<ChunkPtr>{chunk1, chunk2}, perm);
-    ASSERT_EQ(expected_size, perm.size());
-    Int32Column* output_column1 = down_cast<Int32Column*>(output->get_column_by_index(0).get());
-    Int32Column* output_column2 = down_cast<Int32Column*>(output->get_column_by_index(0).get());
-    Int32Column::Container& data1 = output_column1->get_data();
-    Int32Column::Container& data2 = output_column2->get_data();
-    std::vector<std::tuple<int32_t, int32_t>> rows;
-    for (int i = 0; i < data1.size(); i++) {
-        rows.emplace_back(data1[i], data2[i]);
-    }
-
-    ASSERT_EQ(expected_size, output_column1->size());
-    ASSERT_EQ(expected_size, data1.size());
-    ASSERT_TRUE(std::is_sorted(rows.begin(), rows.end(),
-                               [](auto x, auto y) {
-                                   if (std::get<0>(x) != std::get<0>(y)) {
-                                       return std::get<0>(x) < std::get<0>(y);
-                                   }
-                                   return std::get<1>(x) < std::get<1>(y);
-                               }))
-            << "merged data: " << fmt::format("{}", fmt::join(data1, ", "));
-}
-
 static ColumnPtr build_sorted_column(TypeDescriptor type_desc, int slot_index, int32_t start, int32_t count,
                                      int32_t step) {
     DCHECK_EQ(TYPE_INT, type_desc.type);
@@ -284,7 +232,7 @@ TEST(MergeTest, merge_sorted_stream) {
         for (int i = 0; i < chunk->num_rows(); i++) {
             fmt::print("row: {}\n", chunk->debug_row(i));
             if (i > 0) {
-                int x = compare_chunk_row(sort_desc, *chunk, *chunk, i - 1, i);
+                int x = compare_chunk_row(sort_desc, chunk->columns(), chunk->columns(), i - 1, i);
                 ASSERT_LE(x, 0);
             }
         }
