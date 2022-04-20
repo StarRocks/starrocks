bc9f6730474c1494bb2f60cb6b74fa584c6c4b3b
gengjun-git <gengjun@starrocks.com>
2022-04-20 14:06:38 +0800

    BugFix: Storage medium cannot be changed to hdd when cool_down_time is reached #5232
    Change storage_medium to HDD instead of default_storage_medium when cool_down_time is reached.

--- fe/fe-core/src/main/java/com/starrocks/catalog/Catalog.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/Catalog.java
@@ -5499,7 +5499,8 @@ public class Catalog {
                         if (dataProperty.getStorageMedium() == TStorageMedium.SSD
                                 && dataProperty.getCooldownTimeMs() < currentTimeMs) {
                             // expire. change to HDD.
-                            partitionInfo.setDataProperty(partition.getId(), new DataProperty(TStorageMedium.HDD));
+                            DataProperty hdd = new DataProperty(TStorageMedium.HDD);
+                            partitionInfo.setDataProperty(partition.getId(), hdd);
                             storageMediumMap.put(partitionId, TStorageMedium.HDD);
                             LOG.debug("partition[{}-{}-{}] storage medium changed from SSD to HDD",
                                     dbId, tableId, partitionId);
@@ -5508,7 +5509,7 @@ public class Catalog {
                             ModifyPartitionInfo info =
                                     new ModifyPartitionInfo(db.getId(), olapTable.getId(),
                                             partition.getId(),
-                                            DataProperty.DEFAULT_DATA_PROPERTY,
+                                            hdd,
                                             (short) -1,
                                             partitionInfo.getIsInMemory(partition.getId()));
                             editLog.logModifyPartition(info);
