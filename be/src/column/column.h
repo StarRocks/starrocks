e4b44f9958fcded163648604861e04d797d55380
imay <buaa.zhaoc@gmail.com>
2022-04-22 19:16:37 +0800

    Put some function implementation to column.cpp (#5401)

--- be/src/column/column.h
+++ be/src/column/column.h
@@ -9,18 +9,20 @@
 
 #include "column/column_visitor.h"
 #include "column/column_visitor_mutable.h"
-#include "column/datum.h"
 #include "column/vectorized_fwd.h"
 #include "gutil/casts.h"
 #include "storage/delete_condition.h" // for DelCondSatisfied
-#include "util/slice.h"
 
 namespace starrocks {
 
 class MemPool;
 class MysqlRowBuffer;
+class Slice;
 
 namespace vectorized {
+
+// Forward declaration
+class Datum;
 
 class Column {
 public:
@@ -226,25 +228,7 @@ public:
 
     // A dedicated serialization method used by NullableColumn to serialize data columns with null_masks.
     virtual void serialize_batch_with_null_masks(uint8_t* dst, Buffer<uint32_t>& slice_sizes, size_t chunk_size,
-                                                 uint32_t max_one_row_size, uint8_t* null_masks, bool has_null) {
-        uint32_t* sizes = slice_sizes.data();
-
-        if (!has_null) {
-            for (size_t i = 0; i < chunk_size; ++i) {
-                memcpy(dst + i * max_one_row_size + sizes[i], &has_null, sizeof(bool));
-                sizes[i] += sizeof(bool) + serialize(i, dst + i * max_one_row_size + sizes[i] + sizeof(bool));
-            }
-        } else {
-            for (size_t i = 0; i < chunk_size; ++i) {
-                memcpy(dst + i * max_one_row_size + sizes[i], null_masks + i, sizeof(bool));
-                sizes[i] += sizeof(bool);
-
-                if (!null_masks[i]) {
-                    sizes[i] += serialize(i, dst + i * max_one_row_size + sizes[i]);
-                }
-            }
-        }
-    }
+                                                 uint32_t max_one_row_size, uint8_t* null_masks, bool has_null);
 
     // deserialize one data and append to this column
     virtual const uint8_t* deserialize_and_append(const uint8_t* pos) = 0;
@@ -346,6 +330,9 @@ public:
     virtual void check_or_die() const = 0;
 
 protected:
+    static StatusOr<ColumnPtr> downgrade_helper_func(ColumnPtr* col);
+    static StatusOr<ColumnPtr> upgrade_helper_func(ColumnPtr* col);
+
     DelCondSatisfied _delete_state = DEL_NOT_SATISFIED;
 };
 
