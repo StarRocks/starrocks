9c1c8a60510616fa24cd7b81a7c12308f7bd8218
dreamay <869486496@qq.com>
2022-04-22 12:33:46 +0800

    fix privilegeChecker

--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/PrivilegeChecker.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/PrivilegeChecker.java
@@ -1,6 +1,7 @@
 // This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
 package com.starrocks.sql.analyzer;
 
+import com.starrocks.analysis.AdminSetConfigStmt;
 import com.starrocks.analysis.AdminSetReplicaStatusStmt;
 import com.starrocks.analysis.AlterViewStmt;
 import com.starrocks.analysis.AlterWorkGroupStmt;
@@ -50,6 +51,15 @@ public class PrivilegeChecker {
                         session.getQualifiedUser(), session.getRemoteIP(), tableName.getTbl());
             }
             check(statement.getQueryStatement(), session);
+            return null;
+        }
+
+        @Override
+        public Void visitAdminSetConfigStatement(AdminSetConfigStmt statement, ConnectContext session) {
+            // check auth
+            if (!Catalog.getCurrentCatalog().getAuth().checkGlobalPriv(session, PrivPredicate.ADMIN)) {
+                ErrorReport.reportSemanticException(ErrorCode.ERR_SPECIFIC_ACCESS_DENIED_ERROR, "ADMIN");
+            }
             return null;
         }
 
