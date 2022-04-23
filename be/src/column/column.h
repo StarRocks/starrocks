e65609206077a3b3b01933c1c9acdd37a836879b
zombee0 <flylucas_10@163.com>
2022-04-23 22:22:20 +0800

    Delete virtual on append_selective (#5429)
    Signed-off-by: zombee0 <flylucas_10@163.com>

--- be/src/column/column.h
+++ be/src/column/column.h
@@ -168,7 +168,7 @@ public:
     // This function will copy the [3, 2] row of src to this column.
     virtual void append_selective(const Column& src, const uint32_t* indexes, uint32_t from, uint32_t size) = 0;
 
-    virtual void append_selective(const Column& src, const Buffer<uint32_t>& indexes) {
+    void append_selective(const Column& src, const Buffer<uint32_t>& indexes) {
         return append_selective(src, indexes.data(), 0, indexes.size());
     }
 
