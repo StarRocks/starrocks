f42f92d8d2f0d37ac3961285bab8b8671cfd1a13
Murphy <96611012+mofeiatwork@users.noreply.github.com>
2022-04-26 12:50:24 +0800

    Reduce chunk_size in scan for huge length types (#5484)
    (cherry picked from commit 1d8fdffa0a711e633d3ad43835d9e4554ea7ca9a)

--- be/src/exec/pipeline/olap_chunk_source.h
+++ be/src/exec/pipeline/olap_chunk_source.h
@@ -71,6 +71,7 @@ private:
     Status _read_chunk_from_storage([[maybe_unused]] RuntimeState* state, vectorized::Chunk* chunk);
     void _update_counter();
     void _update_realtime_counter(vectorized::Chunk* chunk);
+    void _decide_chunk_size();
 
     vectorized::TabletReaderParams _params = {};
 
