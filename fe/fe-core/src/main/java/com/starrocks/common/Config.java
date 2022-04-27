d2d50a0bc792f9e4a0eeabbc36d21b553fdc8d78
blackstar-baba <535650957@qq.com>
2022-04-27 18:33:24 +0800

    Add fe config for mv (#5566)
    Add switch in fe config for new mv

--- fe/fe-core/src/main/java/com/starrocks/common/Config.java
+++ fe/fe-core/src/main/java/com/starrocks/common/Config.java
@@ -1402,4 +1402,7 @@ public class Config extends ConfigBase {
      */
     @ConfField(mutable = true)
     public static int default_bucket_num = 10;
+
+    @ConfField(mutable = true)
+    public static boolean enable_experimental_mv = false;
 }
