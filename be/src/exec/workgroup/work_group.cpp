8d7f5814d15af78a2ae74034ae30846410425400
mofei <mofei@starrocks.com>
2022-04-29 17:21:53 +0800

    remove unused comment

--- be/src/exec/workgroup/work_group.cpp
+++ be/src/exec/workgroup/work_group.cpp
@@ -198,8 +198,6 @@ void WorkGroup::estimate_trend_factor_period() {
 }
 
 bool WorkGroup::is_big_query(const QueryContext& query_context) {
-    // If there is only one query, skip it
-
     // Check big query run time
     if (_big_query_cpu_core_second_limit) {
         int64_t wg_growth_cpu_use_cost = total_cpu_cost() - query_context.init_wg_cpu_cost();
