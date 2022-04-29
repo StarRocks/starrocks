ce6edcbeeaa9b54398424ef901ccffae3ac0e36c
padmejin <89557328+padmejin@users.noreply.github.com>
2022-04-29 10:17:36 +0800

    [Enhancement] simple password validation & password reuse check (#5637)
    This PR is a temporary implementation of password validation & password reuse check. The rules are mainly hardcoded and cannot be fully configurable except for a switch to enable it. We will refactor the whole user privilege framework later to fix this ultimately.
    
    Set enable_validate_password to true to validate the password by these rules:
    a. The length of the password should be no less than 8.
    b. The password should contain at least one digit, one lowercase letter, one uppercase letter
    Set enable_password_reuse to false to prevent changing the password to the previous one.

--- fe/fe-core/src/main/java/com/starrocks/common/Config.java
+++ fe/fe-core/src/main/java/com/starrocks/common/Config.java
@@ -1411,4 +1411,18 @@ public class Config extends ConfigBase {
 
     @ConfField(mutable = true)
     public static boolean enable_dict_optimize_stream_load = true;
+
+    /**
+     * If set to true, the following rules will apply to see if the password is secure upon the creation of a user.
+     * 1. The length of the password should be no less than 8.
+     * 2. The password should contain at least one digit, one lowercase letter, one uppercase letter
+     */
+    @ConfField(mutable = true)
+    public static boolean enable_validate_password = false;
+
+    /**
+     * If set to false, changing the password to the previous one is not allowed.
+     */
+    @ConfField(mutable = true)
+    public static boolean enable_password_reuse = true;
 }
