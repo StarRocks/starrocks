13a585eda351d7b29e15d058ddc84dfb017b331d
caneGuy <zhoukang199191@gmail.com>
2022-05-07 21:37:13 +0800

    fix

--- fe/fe-core/src/main/java/com/starrocks/external/iceberg/IcebergUtil.java
+++ fe/fe-core/src/main/java/com/starrocks/external/iceberg/IcebergUtil.java
@@ -19,6 +19,8 @@ import org.apache.iceberg.exceptions.NoSuchTableException;
 import org.apache.iceberg.expressions.Expression;
 import org.apache.iceberg.expressions.Expressions;
 import org.apache.iceberg.util.ThreadPools;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 import java.util.Map;
@@ -26,9 +28,11 @@ import java.util.Optional;
 import java.util.Properties;
 
 public class IcebergUtil {
+    private static final Logger LOG = LogManager.getLogger(IcebergUtil.class);
 
     static {
         if (Config.enable_iceberg_custom_worker_thread) {
+            LOG.info("Default iceberg worker thread number changed " + Config.iceberg_worker_num_threads);
             Properties props = System.getProperties();
             props.setProperty(ThreadPools.WORKER_THREAD_POOL_SIZE_PROP, String.valueOf(Config.iceberg_worker_num_threads));
         }
