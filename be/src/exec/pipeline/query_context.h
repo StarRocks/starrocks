14bf49e6976f5b5c89106d906701e9b9f996cfc2
stdpain <34912776+stdpain@users.noreply.github.com>
2022-05-10 15:00:58 +0800

    [Bug] Fix memory Leak in Pipline when prepare failed (#5882)
    When prepare stage failed, pipeline driver won't be added in
    driver_exec_queue. but query_ctx or fragment_ctx has been created. So
    they won't clean.
    
    In this patch, we add a function `_fail_cleanup` which could help
    cleanup query_ctx and fragment_ctx.
    
    In addition to that I have reduced the dependency of executor to
    fragment_ctx. If prepare fails, ctx will be automatically destructured,
    because fragment_ctx was created with a unique_ptr which will cause
    executor hold a invalid pointer.

--- be/src/exec/pipeline/query_context.h
+++ be/src/exec/pipeline/query_context.h
@@ -76,7 +76,7 @@ public:
     void init_mem_tracker(int64_t bytes_limit, MemTracker* parent);
     std::shared_ptr<MemTracker> mem_tracker() { return _mem_tracker; }
 
-    void init_query(workgroup::WorkGroup* wg);
+    bool init_query(workgroup::WorkGroup* wg);
 
     void incr_cpu_cost(int64_t cost) { _cur_cpu_cost += cost; }
     int64_t cpu_cost() const { return _cur_cpu_cost; }
