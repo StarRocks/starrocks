StarRocks/starrocks

commit 2ee389fe57c02890f35df4e260e890232ade90ed

[Enhancement] Make segment iterator scan full chunk in each time  (#5701)
In each `_do_get_next`, segment iterators will return a result chunk with all of its elements passing the predicates _filter. To achieve this, it will scan some(chunk_capacity - chunk current size) rows, and filters these rows that are unsatisfied with the predicates. Assume the chunk size is 4096 when the chunk current size reaches 4095. Segment iterator will only scan more rows in `_read` and `_filter` it, when there are a number of rows unsatisfied with the predicates, this will have degradation to the performance.

To solve this problem, I make the segment iterator `_read` scan full rows(4096 rows, chunk_capacity) each time, and _filter 4096 rows in batch, which improves a lot to the scan speed. However, this may lead to the resulting chunk containing more than 4096 rows finally. We will save these overflow rows in _overflow_read_chunk chunk and reload them in next do_get_next.

This pr makes the scan speed has an overall improvement, in my test case, the best improvement is around 30%

be/src/storage/rowset/segment_iterator.cpp
@@ -275,6 +275,10 @@ private:
 
     bool _inited = false;
     bool _has_bitmap_index = false;
+
+    // save overflow rows in read_chunk
+    std::shared_ptr<Chunk> _overflow_read_chunk;
+    std::vector<uint32_t> _overflow_read_chunk_rowids;
 };
 
 SegmentIterator::SegmentIterator(std::shared_ptr<Segment> segment, vectorized::Schema schema,
@@ -282,7 +286,8 @@ SegmentIterator::SegmentIterator(std::shared_ptr<Segment> segment, vectorized::S
         : ChunkIterator(std::move(schema), options.chunk_size),
           _segment(std::move(segment)),
           _opts(std::move(options)),
-          _predicate_columns(_opts.predicates.size()) {}
+          _predicate_columns(_opts.predicates.size()),
+          _overflow_read_chunk(nullptr) {}
 
 Status SegmentIterator::_init() {
     SCOPED_RAW_TIMER(&_opts.stats->segment_init_ns);
@@ -306,8 +311,8 @@ Status SegmentIterator::_init() {
         }
     }
 
-    _selection.resize(_opts.chunk_size);
-    _selected_idx.resize(_opts.chunk_size);
+    _selection.resize(_opts.chunk_size + _opts.chunk_size / 4 + 1);
+    _selected_idx.resize(_opts.chunk_size + _opts.chunk_size / 4 + 1);
     StarRocksMetrics::instance()->segment_read_total.increment(1);
     // get file handle from file descriptor of segment
     RETURN_IF_ERROR(_opts.block_mgr->open_block(_segment->file_name(), &_rblock));
@@ -667,10 +672,22 @@ Status SegmentIterator::_do_get_next(Chunk* result, vector<rowid_t>* rowid) {
     _context->_final_chunk->reset();
     _context->_adapt_global_dict_chunk->reset();
 
+    // If _overflow_read_chunk contains some rows, it means that in previous round of `_do_get_next`,
+    // _read_chunk reads more rows than its capacity, the overflow rows in saved in _overflow_read_chunk.
+    // Here we reload those overflow rows to current context's _read_chunk.
+    if (_overflow_read_chunk != nullptr && !_overflow_read_chunk->is_empty()) {
+        _context->_read_chunk->swap_chunk(*_overflow_read_chunk);
+        if (rowid != nullptr) {
+            DCHECK_EQ(_context->_read_chunk->num_rows(), _overflow_read_chunk_rowids.size());
+            rowid->insert(rowid->end(), _overflow_read_chunk_rowids.begin(), _overflow_read_chunk_rowids.end());
+            _overflow_read_chunk_rowids.clear();
+        }
+    }
+
     Chunk* chunk = _context->_read_chunk.get();
 
     while ((chunk_start < chunk_capacity) & _range_iter.has_more()) {
-        RETURN_IF_ERROR(_read(chunk, rowid, chunk_capacity - chunk_start));
+        RETURN_IF_ERROR(_read(chunk, rowid, std::max(chunk_capacity - chunk_start, chunk_capacity / 4)));
         chunk->check_or_die();
         size_t next_start = chunk->num_rows();
 
@@ -680,6 +697,25 @@ Status SegmentIterator::_do_get_next(Chunk* result, vector<rowid_t>* rowid) {
         }
         chunk_start = next_start;
         DCHECK_EQ(chunk_start, chunk->num_rows());
+    }
+
+    // If _read_chunk contains more rows than its capacity, we save the overflow rows in _overflow_read_chunk.
+    if (chunk_start > chunk_capacity) {
+        if (_overflow_read_chunk == nullptr) {
+            _overflow_read_chunk = chunk->clone_empty(_opts.chunk_size / 4 + 1);
+            if (rowid != nullptr) {
+                _overflow_read_chunk_rowids.reserve(_opts.chunk_size / 4 + 1);
+            }
+        }
+        DCHECK(_overflow_read_chunk->is_empty());
+        _overflow_read_chunk->append(*chunk, chunk_capacity, chunk_start - chunk_capacity);
+        if (rowid != nullptr) {
+            DCHECK(_overflow_read_chunk_rowids.empty());
+            _overflow_read_chunk_rowids.insert(_overflow_read_chunk_rowids.end(), rowid->begin() + chunk_capacity,
+                                               rowid->end());
+            rowid->resize(chunk_capacity);
+        }
+        chunk->set_num_rows(chunk_capacity);
     }
 
     size_t raw_chunk_size = chunk->num_rows();
@@ -770,7 +806,7 @@ void SegmentIterator::_switch_context(ScanContext* to) {
     }
 
     if (to->_read_chunk == nullptr) {
-        to->_read_chunk = ChunkHelper::new_chunk(to->_read_schema, _opts.chunk_size);
+        to->_read_chunk = ChunkHelper::new_chunk(to->_read_schema, _opts.chunk_size + _opts.chunk_size / 4 + 1);
     }
 
     if (to->_has_dict_column) {

commit 2868d20250ab70a37585df179bcd489bf96319eb

[BugFix] move _overflow_read_chunk into scan context
This pr fix the bug introduced by #5701

Under late materialization scenario, context 0 and context 1 have different read schema and column iterators. The overflow rows in _overflow_read_chunk by context0 will be used by context1 and leads to a crash.

This pr moves the _overflow_read_chunk into scan context, each context has its own _overflow_read_chunk.

Signed-off-by: xyz <a997647204@gmail.com>

be/src/storage/rowset/segment_iterator.cpp
@@ -166,6 +166,10 @@ private:
 
         // not all dict encode
         bool _has_force_dict_encode{false};
+
+        // save overflow rows in read_chunk
+        std::shared_ptr<Chunk> _overflow_read_chunk;
+        std::vector<uint32_t> _overflow_read_chunk_rowids;
     };
 
     Status _init();
@@ -275,10 +279,6 @@ private:
 
     bool _inited = false;
     bool _has_bitmap_index = false;
-
-    // save overflow rows in read_chunk
-    std::shared_ptr<Chunk> _overflow_read_chunk;
-    std::vector<uint32_t> _overflow_read_chunk_rowids;
 };
 
 SegmentIterator::SegmentIterator(std::shared_ptr<Segment> segment, vectorized::Schema schema,
@@ -286,8 +286,7 @@ SegmentIterator::SegmentIterator(std::shared_ptr<Segment> segment, vectorized::S
         : ChunkIterator(std::move(schema), options.chunk_size),
           _segment(std::move(segment)),
           _opts(std::move(options)),
-          _predicate_columns(_opts.predicates.size()),
-          _overflow_read_chunk(nullptr) {}
+          _predicate_columns(_opts.predicates.size()) {}
 
 Status SegmentIterator::_init() {
     SCOPED_RAW_TIMER(&_opts.stats->segment_init_ns);
@@ -675,12 +674,14 @@ Status SegmentIterator::_do_get_next(Chunk* result, vector<rowid_t>* rowid) {
     // If _overflow_read_chunk contains some rows, it means that in previous round of `_do_get_next`,
     // _read_chunk reads more rows than its capacity, the overflow rows in saved in _overflow_read_chunk.
     // Here we reload those overflow rows to current context's _read_chunk.
-    if (_overflow_read_chunk != nullptr && !_overflow_read_chunk->is_empty()) {
-        _context->_read_chunk->swap_chunk(*_overflow_read_chunk);
+    if (_context->_overflow_read_chunk != nullptr && !_context->_overflow_read_chunk->is_empty()) {
+        DCHECK_EQ(_context->_read_chunk->num_columns(), _context->_overflow_read_chunk->num_columns());
+        _context->_read_chunk->swap_chunk(*_context->_overflow_read_chunk);
         if (rowid != nullptr) {
-            DCHECK_EQ(_context->_read_chunk->num_rows(), _overflow_read_chunk_rowids.size());
-            rowid->insert(rowid->end(), _overflow_read_chunk_rowids.begin(), _overflow_read_chunk_rowids.end());
-            _overflow_read_chunk_rowids.clear();
+            DCHECK_EQ(_context->_read_chunk->num_rows(), _context->_overflow_read_chunk_rowids.size());
+            rowid->insert(rowid->end(), _context->_overflow_read_chunk_rowids.begin(),
+                          _context->_overflow_read_chunk_rowids.end());
+            _context->_overflow_read_chunk_rowids.clear();
         }
     }
 
@@ -701,18 +702,18 @@ Status SegmentIterator::_do_get_next(Chunk* result, vector<rowid_t>* rowid) {
 
     // If _read_chunk contains more rows than its capacity, we save the overflow rows in _overflow_read_chunk.
     if (chunk_start > chunk_capacity) {
-        if (_overflow_read_chunk == nullptr) {
-            _overflow_read_chunk = chunk->clone_empty(_opts.chunk_size / 4 + 1);
+        if (_context->_overflow_read_chunk == nullptr) {
+            _context->_overflow_read_chunk = chunk->clone_empty(_opts.chunk_size / 4 + 1);
             if (rowid != nullptr) {
-                _overflow_read_chunk_rowids.reserve(_opts.chunk_size / 4 + 1);
+                _context->_overflow_read_chunk_rowids.reserve(_opts.chunk_size / 4 + 1);
             }
         }
-        DCHECK(_overflow_read_chunk->is_empty());
-        _overflow_read_chunk->append(*chunk, chunk_capacity, chunk_start - chunk_capacity);
+        DCHECK(_context->_overflow_read_chunk->is_empty());
+        _context->_overflow_read_chunk->append(*chunk, chunk_capacity, chunk_start - chunk_capacity);
         if (rowid != nullptr) {
-            DCHECK(_overflow_read_chunk_rowids.empty());
-            _overflow_read_chunk_rowids.insert(_overflow_read_chunk_rowids.end(), rowid->begin() + chunk_capacity,
-                                               rowid->end());
+            DCHECK(_context->_overflow_read_chunk_rowids.empty());
+            _context->_overflow_read_chunk_rowids.insert(_context->_overflow_read_chunk_rowids.end(),
+                                                         rowid->begin() + chunk_capacity, rowid->end());
             rowid->resize(chunk_capacity);
         }
         chunk->set_num_rows(chunk_capacity);