afec8dfa213c1c04e7d377992c014b8197f4b67c
silverbullet233 <3675229+silverbullet233@users.noreply.github.com>
2022-05-11 20:38:48 +0800

    make timeout message more friendly

--- fe/fe-core/src/main/java/com/starrocks/qe/ResultReceiver.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/ResultReceiver.java
@@ -48,6 +48,7 @@ public class ResultReceiver {
     private boolean isCancel = false;
     private long packetIdx = 0;
     private final long timeoutTs;
+    private final int expireSeconds;
     private final TNetworkAddress address;
     private final PUniqueId finstId;
     private final Long backendId;
@@ -60,6 +61,7 @@ public class ResultReceiver {
         this.backendId = backendId;
         this.address = address;
         this.timeoutTs = System.currentTimeMillis() + timeoutMs;
+        this.expireSeconds = timeoutMs / 1000;
     }
 
     public RowBatch getNext(Status status) throws TException {
@@ -125,14 +127,15 @@ public class ResultReceiver {
             LOG.warn("fetch result execution exception, finstId={}", finstId, e);
             if (e.getMessage().contains("time out")) {
                 // if timeout, we set error code to TIMEOUT, and it will not retry querying.
-                status.setStatus(new Status(TStatusCode.TIMEOUT, "query timeout"));
+                status.setStatus(new Status(TStatusCode.TIMEOUT,
+                        String.format("Query exceeded time limit of %d seconds", expireSeconds)));
             } else {
                 status.setRpcStatus(e.getMessage());
                 SimpleScheduler.addToBlacklist(backendId);
             }
         } catch (TimeoutException e) {
             LOG.warn("fetch result timeout, finstId={}", finstId, e);
-            status.setStatus("query timeout");
+            status.setStatus(String.format("Query exceeded time limit of %d seconds", expireSeconds));
             if (MetricRepo.isInit) {
                 MetricRepo.COUNTER_QUERY_TIMEOUT.increase(1L);
             }
