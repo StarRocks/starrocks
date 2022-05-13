3277bb61f736c187eb3df3c93565f34f901ba99d
adzfolc <multigraph@krutt.org>
2022-05-13 16:15:16 +0800

    [Refactor]Remove unused test func

--- fe/fe-core/src/test/java/com/starrocks/analysis/ShowCatalogsStmtTest.java
+++ fe/fe-core/src/test/java/com/starrocks/analysis/ShowCatalogsStmtTest.java
@@ -2,17 +2,6 @@
 
 package com.starrocks.analysis;
 
-import com.starrocks.common.AnalysisException;
-import com.starrocks.sql.ast.ShowCatalogsStmt;
-import org.junit.Assert;
-import org.junit.Test;
-
 public class ShowCatalogsStmtTest {
-    @Test
-    public void testShowCatalogsParserAndAnalyzer() throws AnalysisException {
-        // final Analyzer analyzer = AccessTestUtil.fetchBlockAnalyzer();
-        // ShowCatalogsStmt stmt = new ShowCatalogsStmt();
-        // stmt.analyze(analyzer);
-        // Assert.assertEquals("SHOW CATALOGS", stmt.toSql());
-    }
+
 }
