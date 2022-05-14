068b61a3309d3afadcdfd1e345ccd41b279b0090
yan.zhang <dirtysalt1987@gmail.com>
2022-05-14 17:12:26 +0800

    move jdbc bdrige to com.starrocks package (#6096)

--- java-extensions/jdbc-bridge/src/main/java/com/starrocks/jdbcbridge/JDBCUtil.java
+++ java-extensions/jdbc-bridge/src/main/java/com/starrocks/jdbcbridge/JDBCUtil.java
@@ -1,6 +1,6 @@
 // This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
 
-package jdbcbridge;
+package com.starrocks.jdbcbridge;
 
 import java.sql.Date;
 import java.text.SimpleDateFormat;
