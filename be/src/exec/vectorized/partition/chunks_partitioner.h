ba587dbb0dd43b17208a7bec4243407eca410f6a
liuyehcf <1559500551@qq.com>
2022-05-14 10:26:56 +0800

    [Enhancement] Rank-window-function optimization, partitioner (1)

---
+++ be/src/exec/vectorized/partition/chunks_partitioner.h
@@ -0,0 +1,166 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
+
+#pragma once
+
+#include <any>
+
+#include "column/chunk.h"
+#include "column/vectorized_fwd.h"
+#include "common/statusor.h"
+#include "exprs/expr_context.h"
+#include "partition_hash_variant.h"
+
+namespace starrocks::vectorized {
+
+struct PartitionColumnType {
+    TypeDescriptor result_type;
+    bool is_nullable;
+};
+
+class ChunksPartitioner;
+
+using ChunksPartitionerPtr = std::shared_ptr<ChunksPartitioner>;
+
+class ChunksPartitioner {
+public:
+    ChunksPartitioner(const bool has_nullable_partition_column, const std::vector<ExprContext*>& partition_exprs,
+                      const std::vector<PartitionColumnType>& partition_types);
+
+    Status prepare(RuntimeState* state);
+
+    // Chunk is divided into multiple parts by partition columns,
+    // and each partition corresponds to a key-value pair in the hash map
+    Status offer(const ChunkPtr& chunk);
+
+    // Number of partitions
+    int32_t num_partitions();
+
+    // Consumers consume from the hash map
+    // method signature is: bool consumer(int32_t partition_idx, const ChunkPtr& chunk)
+    // The return value of the consumer denote whether to continue or not
+    template <typename Consumer>
+    Status accept(Consumer&& consumer);
+
+private:
+    bool _is_partition_columns_fixed_size(const std::vector<ExprContext*>& partition_expr_ctxs,
+                                          const std::vector<PartitionColumnType>& partition_types, size_t* max_size,
+                                          bool* has_null);
+    void _init_hash_map_variant();
+
+private:
+    const bool _has_nullable_partition_column;
+    const std::vector<ExprContext*> _partition_exprs;
+    const std::vector<PartitionColumnType> _partition_types;
+
+    RuntimeState* _state = nullptr;
+    MemPool* _pool = nullptr;
+
+    Columns _partition_columns;
+    // Hash map which holds chunks of different partitions
+    PartitionHashMapVariant _hash_map_variant;
+
+    // Iterator of partitions
+    std::any _partition_it;
+    // Iterator of chunks of current partition
+    std::any _chunk_it;
+    // Offset of current consuming partition
+    int32_t _partition_idx = -1;
+    bool _hash_map_eos = false;
+    bool _null_key_eos = false;
+
+private:
+    template <typename HashMapWithKey>
+    void split_chunk_by_partition(HashMapWithKey& hash_map_with_key, const ChunkPtr& chunk) {
+        hash_map_with_key.append_chunk(chunk, _partition_columns, _pool);
+    }
+
+    // Fetch chunks from hash map, return true if reaches eos
+    template <typename HashMapWithKey, typename Consumer>
+    bool fetch_chunks_from_hash_map(HashMapWithKey& hash_map_with_key, Consumer&& consumer) {
+        if (_hash_map_eos) {
+            return true;
+        }
+        if (!_partition_it.has_value()) {
+            _partition_it = hash_map_with_key.hash_map.begin();
+            _partition_idx = 0;
+        }
+
+        using PartitionIterator = typename HashMapWithKey::Iterator;
+        PartitionIterator partition_it = std::any_cast<PartitionIterator>(_partition_it);
+        const PartitionIterator partition_end = hash_map_with_key.hash_map.end();
+
+        using ChunkIterator = typename std::vector<ChunkPtr>::iterator;
+        ChunkIterator chunk_it;
+        DeferOp defer([&]() {
+            if (partition_it == partition_end) {
+                _hash_map_eos = true;
+                _partition_it.reset();
+                _chunk_it.reset();
+            } else {
+                _partition_it = partition_it;
+                _chunk_it = chunk_it;
+            }
+        });
+
+        while (partition_it != partition_end) {
+            std::vector<ChunkPtr>& chunks = partition_it->second.chunks;
+            if (!_chunk_it.has_value()) {
+                _chunk_it = chunks.begin();
+            }
+
+            chunk_it = std::any_cast<ChunkIterator>(_chunk_it);
+            ChunkIterator chunk_end = chunks.end();
+
+            while (chunk_it != chunk_end) {
+                if (!consumer(_partition_idx, *chunk_it++)) {
+                    return false;
+                }
+            }
+
+            // Move to next partition
+            ++partition_it;
+            ++_partition_idx;
+            _chunk_it.reset();
+        }
+
+        return true;
+    }
+
+    // Fetch chunks from HashMapWithKey.null_key_value, return true if reaches eos
+    template <typename HashMapWithKey, typename Consumer>
+    bool fetch_chunks_from_null_key_value(HashMapWithKey& hash_map_with_key, Consumer&& consumer) {
+        if (_null_key_eos) {
+            return true;
+        }
+
+        std::vector<ChunkPtr>& chunks = hash_map_with_key.null_key_value.chunks;
+
+        if (!_chunk_it.has_value()) {
+            _chunk_it = chunks.begin();
+        }
+
+        using ChunkIterator = typename std::vector<ChunkPtr>::iterator;
+        ChunkIterator chunk_it = std::any_cast<ChunkIterator>(_chunk_it);
+        const ChunkIterator chunk_end = chunks.end();
+
+        DeferOp defer([&]() {
+            if (chunk_it == chunk_end) {
+                _null_key_eos = true;
+                _chunk_it.reset();
+            } else {
+                _chunk_it = chunk_it;
+            }
+        });
+
+        while (chunk_it != chunk_end) {
+            // Because we first fetch chunks from hash_map, so the _partition_idx here
+            // is already set to hash_map.size()
+            if (!consumer(_partition_idx, *chunk_it++)) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+} // namespace starrocks::vectorized
---
+++ be/src/exec/vectorized/partition/partition_hash_map.h
@@ -0,0 +1,282 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
+
+#pragma once
+
+#include "column/chunk.h"
+#include "column/column_hash.h"
+#include "column/column_helper.h"
+#include "column/hash_set.h"
+#include "column/type_traits.h"
+#include "column/vectorized_fwd.h"
+#include "gutil/strings/fastmem.h"
+#include "util/phmap/phmap.h"
+
+namespace starrocks::vectorized {
+
+struct PartitionChunks {
+    Chunks chunks;
+
+    // Used to save the indexes of chunk of this partition, to avoid calling Chunk::append_selective many times
+    // It's a temporary data struct which is only valid in current invocation of append_chunk_for_one_key
+    // or append_chunk_for_one_nullable_key, and after invokcation, it will be clean up
+    std::vector<uint32_t> select_indexes;
+    // Used to save the remain size of last chunk in chunks
+    // Avoid virtual function call `chunks->back()->num_rows()`
+    int32_t remain_size = 0;
+};
+
+// =====================
+// one level partition hash map
+template <PhmapSeed seed>
+using Int8PartitionHashMap = phmap::flat_hash_map<int8_t, PartitionChunks, StdHashWithSeed<int8_t, seed>>;
+template <PhmapSeed seed>
+using Int16PartitionHashMap = phmap::flat_hash_map<int16_t, PartitionChunks, StdHashWithSeed<int16_t, seed>>;
+template <PhmapSeed seed>
+using Int32PartitionHashMap = phmap::flat_hash_map<int32_t, PartitionChunks, StdHashWithSeed<int32_t, seed>>;
+template <PhmapSeed seed>
+using Int64PartitionHashMap = phmap::flat_hash_map<int64_t, PartitionChunks, StdHashWithSeed<int64_t, seed>>;
+template <PhmapSeed seed>
+using Int128PartitionHashMap = phmap::flat_hash_map<int128_t, PartitionChunks, Hash128WithSeed<seed>>;
+template <PhmapSeed seed>
+using DatePartitionHashMap = phmap::flat_hash_map<DateValue, PartitionChunks, StdHashWithSeed<DateValue, seed>>;
+template <PhmapSeed seed>
+using TimeStampPartitionHashMap =
+        phmap::flat_hash_map<TimestampValue, PartitionChunks, StdHashWithSeed<TimestampValue, seed>>;
+template <PhmapSeed seed>
+using SlicePartitionHashMap = phmap::flat_hash_map<Slice, PartitionChunks, SliceHashWithSeed<seed>, SliceEqual>;
+
+// ==================
+// one level fixed size slice hash map
+template <PhmapSeed seed>
+using FixedSize4SlicePartitionHashMap =
+        phmap::flat_hash_map<SliceKey4, PartitionChunks, FixedSizeSliceKeyHash<SliceKey4, seed>>;
+template <PhmapSeed seed>
+using FixedSize8SlicePartitionHashMap =
+        phmap::flat_hash_map<SliceKey8, PartitionChunks, FixedSizeSliceKeyHash<SliceKey8, seed>>;
+template <PhmapSeed seed>
+using FixedSize16SlicePartitionHashMap =
+        phmap::flat_hash_map<SliceKey16, PartitionChunks, FixedSizeSliceKeyHash<SliceKey16, seed>>;
+
+struct PartitionHashMapBase {
+    const int32_t chunk_size;
+
+    PartitionHashMapBase(int32_t chunk_size) : chunk_size(chunk_size) {}
+
+protected:
+    void alloc_new_buffer(PartitionChunks& value, const ChunkPtr& chunk) {
+        value.chunks.push_back(chunk->clone_empty());
+        value.select_indexes.clear();
+        value.remain_size = chunk_size;
+    }
+
+    void flush(PartitionChunks& value, const ChunkPtr& chunk) {
+        if (!value.chunks.empty() && !value.select_indexes.empty()) {
+            value.chunks.back()->append_selective(*chunk, value.select_indexes.data(), 0, value.select_indexes.size());
+            value.select_indexes.clear();
+            value.remain_size = chunk_size - value.chunks.back()->num_rows();
+        }
+    }
+
+    // Append chunk to the hash_map for one non-nullable key
+    // Each key mapped to a PartitionChunks which holds an array of chunks
+    // chunk will be divided into multiply parts by partition key, and each parts will be append to the
+    // last chunk of PartitionChunks.chunks. New chunk will be allocated if the last chunk reaches its capacity
+    // @key_loader used to load key for number type or slice type
+    template <typename HashMap, typename KeyLoader, typename KeyAllocator>
+    void append_chunk_for_one_key(HashMap& hash_map, ChunkPtr chunk, KeyLoader&& key_loader,
+                                  KeyAllocator&& key_allocator) {
+        const auto size = chunk->num_rows();
+        for (uint32_t i = 0; i < size; i++) {
+            const auto& key = key_loader(i);
+            auto iter = hash_map.lazy_emplace(
+                    key, [&](const auto& ctor) { return ctor(key_allocator(key), PartitionChunks()); });
+            auto& value = iter->second;
+            if (value.chunks.empty() || value.remain_size <= 0) {
+                if (!value.chunks.empty()) {
+                    value.chunks.back()->append_selective(*chunk, value.select_indexes.data(), 0,
+                                                          value.select_indexes.size());
+                }
+
+                alloc_new_buffer(value, chunk);
+            }
+            value.select_indexes.push_back(i);
+            value.remain_size--;
+        }
+
+        for (auto& [key, value] : hash_map) {
+            flush(value, chunk);
+        }
+    }
+
+    // Append chunk to the hash_map for one nullable key
+    // We maintain an additional PartitionChunks called null_key_value for null key
+    // Each key mapped to a PartitionChunks which holds an array of chunks
+    // chunk will be divided into multiply parts by partition key, and each parts will be append to the
+    // last chunk of PartitionChunks.chunks. New chunk will be allocated if the last chunk reaches its capacity
+    // @key_loader used to load key for number type or slice type
+    template <typename HashMap, typename KeyLoader, typename KeyAllocator>
+    void append_chunk_for_one_nullable_key(HashMap& hash_map, PartitionChunks& null_key_value, ChunkPtr chunk,
+                                           const NullableColumn* nullable_key_column, KeyLoader&& key_loader,
+                                           KeyAllocator&& key_allocator) {
+        if (nullable_key_column->only_null()) {
+            const auto size = chunk->num_rows();
+            if (null_key_value.chunks.empty() || null_key_value.remain_size <= 0) {
+                if (!null_key_value.chunks.empty()) {
+                    null_key_value.chunks.back()->append_selective(*chunk, null_key_value.select_indexes.data(), 0,
+                                                                   null_key_value.select_indexes.size());
+                }
+
+                alloc_new_buffer(null_key_value, chunk);
+            }
+            int32_t offset = 0;
+            auto cur_remain_size = size;
+            while (null_key_value.remain_size < cur_remain_size) {
+                null_key_value.chunks.back()->append(*chunk, offset, null_key_value.remain_size);
+                offset += null_key_value.remain_size;
+                cur_remain_size -= null_key_value.remain_size;
+
+                alloc_new_buffer(null_key_value, chunk);
+            }
+            null_key_value.chunks.back()->append(*chunk, offset, cur_remain_size);
+            null_key_value.remain_size = chunk_size - null_key_value.chunks.back()->num_rows();
+        } else {
+            const auto& null_flag_data = nullable_key_column->null_column()->get_data();
+            const auto size = chunk->num_rows();
+
+            for (uint32_t i = 0; i < size; i++) {
+                PartitionChunks* value_ptr = nullptr;
+                if (null_flag_data[i] == 1) {
+                    value_ptr = &null_key_value;
+                } else {
+                    const auto& key = key_loader(i);
+                    auto iter = hash_map.lazy_emplace(
+                            key, [&](const auto& ctor) { return ctor(key_allocator(key), PartitionChunks()); });
+                    value_ptr = &iter->second;
+                }
+
+                auto& value = *value_ptr;
+                if (value.chunks.empty() || value.remain_size <= 0) {
+                    if (!value.chunks.empty()) {
+                        value.chunks.back()->append_selective(*chunk, value.select_indexes.data(), 0,
+                                                              value.select_indexes.size());
+                    }
+
+                    alloc_new_buffer(value, chunk);
+                }
+                value.select_indexes.push_back(i);
+                value.remain_size--;
+            }
+
+            for (auto& [_, value] : hash_map) {
+                flush(value, chunk);
+            }
+            flush(null_key_value, chunk);
+        }
+    }
+};
+
+template <PrimitiveType primitive_type, typename HashMap>
+struct PartitionHashMapWithOneNumberKey : public PartitionHashMapBase {
+    using Iterator = typename HashMap::iterator;
+    using ColumnType = RunTimeColumnType<primitive_type>;
+    using FieldType = RunTimeCppType<primitive_type>;
+    HashMap hash_map;
+
+    PartitionHashMapWithOneNumberKey(int32_t chunk_size) : PartitionHashMapBase(chunk_size) {}
+
+    void append_chunk(ChunkPtr chunk, const Columns& key_columns, MemPool* pool) {
+        DCHECK(!key_columns[0]->is_nullable());
+        const auto* key_column = down_cast<ColumnType*>(key_columns[0].get());
+        const auto& key_column_data = key_column->get_data();
+        append_chunk_for_one_key(
+                hash_map, chunk, [&](uint32_t offset) { return key_column_data[offset]; },
+                [](const FieldType& key) { return key; });
+    }
+};
+
+template <PrimitiveType primitive_type, typename HashMap>
+struct PartitionHashMapWithOneNullableNumberKey : public PartitionHashMapBase {
+    using Iterator = typename HashMap::iterator;
+    using ColumnType = RunTimeColumnType<primitive_type>;
+    using FieldType = RunTimeCppType<primitive_type>;
+    HashMap hash_map;
+    PartitionChunks null_key_value;
+
+    PartitionHashMapWithOneNullableNumberKey(int32_t chunk_size) : PartitionHashMapBase(chunk_size) {}
+
+    void append_chunk(ChunkPtr chunk, const Columns& key_columns, MemPool* pool) {
+        DCHECK(key_columns[0]->is_nullable());
+        const auto* nullable_key_column = ColumnHelper::as_raw_column<NullableColumn>(key_columns[0].get());
+        const auto& key_column_data = down_cast<ColumnType*>(nullable_key_column->data_column().get())->get_data();
+        append_chunk_for_one_nullable_key(
+                hash_map, null_key_value, chunk, nullable_key_column,
+                [&](uint32_t offset) { return key_column_data[offset]; }, [](const FieldType& key) { return key; });
+    }
+};
+
+template <typename HashMap>
+struct PartitionHashMapWithOneStringKey : public PartitionHashMapBase {
+    using Iterator = typename HashMap::iterator;
+    HashMap hash_map;
+
+    PartitionHashMapWithOneStringKey(int32_t chunk_size) : PartitionHashMapBase(chunk_size) {}
+
+    void append_chunk(ChunkPtr chunk, const Columns& key_columns, MemPool* pool) {
+        DCHECK(!key_columns[0]->is_nullable());
+        const auto* key_column = down_cast<BinaryColumn*>(key_columns[0].get());
+        append_chunk_for_one_key(
+                hash_map, chunk, [&](uint32_t offset) { return key_column->get_slice(offset); },
+                [&](const Slice& key) {
+                    uint8_t* pos = pool->allocate(key.size);
+                    strings::memcpy_inlined(pos, key.data, key.size);
+                    return Slice{pos, key.size};
+                });
+    }
+};
+
+template <typename HashMap>
+struct PartitionHashMapWithOneNullableStringKey : public PartitionHashMapBase {
+    using Iterator = typename HashMap::iterator;
+    HashMap hash_map;
+    PartitionChunks null_key_value;
+
+    PartitionHashMapWithOneNullableStringKey(int32_t chunk_size) : PartitionHashMapBase(chunk_size) {}
+
+    void append_chunk(ChunkPtr chunk, const Columns& key_columns, MemPool* pool) {
+        DCHECK(key_columns[0]->is_nullable());
+        const auto* nullable_key_column = ColumnHelper::as_raw_column<NullableColumn>(key_columns[0].get());
+        const auto* key_column = down_cast<BinaryColumn*>(nullable_key_column->data_column().get());
+        append_chunk_for_one_nullable_key(
+                hash_map, null_key_value, chunk, nullable_key_column,
+                [&](uint32_t offset) { return key_column->get_slice(offset); },
+                [&](const Slice& key) {
+                    uint8_t* pos = pool->allocate(key.size);
+                    strings::memcpy_inlined(pos, key.data, key.size);
+                    return Slice{pos, key.size};
+                });
+    }
+};
+
+// TODO(hcf) to be implemented
+template <typename HashMap>
+struct PartitionHashMapWithSerializedKey {
+    using Iterator = typename HashMap::iterator;
+    HashMap hash_map;
+
+    PartitionHashMapWithSerializedKey(int32_t chunk_size) {}
+    void append_chunk(ChunkPtr chunk, const Columns& key_columns, MemPool* pool) {}
+};
+
+// TODO(hcf) to be implemented
+template <typename HashMap>
+struct PartitionHashMapWithSerializedKeyFixedSize {
+    using Iterator = typename HashMap::iterator;
+    HashMap hash_map;
+    bool has_null_column = false;
+    int fixed_byte_size = -1; // unset state
+
+    PartitionHashMapWithSerializedKeyFixedSize(int32_t chunk_size) {}
+    void append_chunk(ChunkPtr chunk, const Columns& key_columns, MemPool* pool) {}
+};
+
+} // namespace starrocks::vectorized
