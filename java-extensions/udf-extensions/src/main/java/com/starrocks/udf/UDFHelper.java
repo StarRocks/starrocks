2399a939edbdd1ad5be4dc1d5ec588721329be99
eyes_on_me <3675229+silverbullet233@users.noreply.github.com>
2022-05-14 18:37:33 +0800

    [Enhancement]add back checkstyle to java-extensions (#6100)

--- java-extensions/udf-extensions/src/main/java/com/starrocks/udf/UDFHelper.java
+++ java-extensions/udf-extensions/src/main/java/com/starrocks/udf/UDFHelper.java
@@ -1,4 +1,4 @@
-// This file is licensed under the Elastic License 2.0. Copyright 2021 StarRocks Limited.
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
 
 package com.starrocks.udf;
 
@@ -6,14 +6,13 @@ import sun.misc.Unsafe;
 
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.IntBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 public class UDFHelper {
     public static final int TYPE_BOOLEAN = 2;
