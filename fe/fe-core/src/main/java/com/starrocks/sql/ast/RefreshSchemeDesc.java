172b61746ee92af530a7e046f488b333da8ad4d7
blackstar-baba <535650957@qq.com>
2022-05-16 21:54:53 +0800

    [Feature] [MaterializedView] Modify CreateMaterializedViewStmt (#5647)

---
+++ fe/fe-core/src/main/java/com/starrocks/sql/ast/RefreshSchemeDesc.java
@@ -0,0 +1,20 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
+
+package com.starrocks.sql.ast;
+
+import com.starrocks.analysis.ParseNode;
+import com.starrocks.catalog.RefreshType;
+
+public class RefreshSchemeDesc implements ParseNode {
+
+    protected RefreshType type;
+
+    public RefreshSchemeDesc(RefreshType type) {
+        this.type = type;
+    }
+
+    public RefreshType getType() {
+        return type;
+    }
+
+}
