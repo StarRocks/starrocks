ed506fb67472203bd813be0031fb2948941d6508
Binglin Chang <decstery@gmail.com>
2022-05-16 10:31:05 +0800

    clang-format

--- be/src/storage/tablet_updates.h
+++ be/src/storage/tablet_updates.h
@@ -321,7 +321,7 @@ private:
                                      const std::unique_ptr<RowsetWriter>& rowset_writer);
 
     void _check_creation_time_increasing();
-    
+
     // these functions is only used in ut
     void stop_apply(bool apply_stopped) { _apply_stopped = apply_stopped; }
 
