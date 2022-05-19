46232092f29eded1ba9842f9926c3485f50e0257
RowenWoo <luowen@starrocks.com>
2022-05-19 15:02:45 +0800

    [Starrocks On ES] support configurable node sniffing propertity for es exteranl table

--- fe/fe-core/src/main/java/com/starrocks/external/elasticsearch/SearchContext.java
+++ fe/fe-core/src/main/java/com/starrocks/external/elasticsearch/SearchContext.java
@@ -81,11 +81,15 @@ public class SearchContext {
     // the ES cluster version
     private EsMajorVersion version;
 
+    // whether discovery the nodes where shards reside in
+    private boolean wanOnly;
+
     public SearchContext(EsTable table) {
         this.table = table;
         fullSchema = table.getFullSchema();
         sourceIndex = table.getIndexName();
         type = table.getMappingType();
+        wanOnly = table.wanOnly();
     }
 
     public String sourceIndex() {
@@ -135,5 +139,9 @@ public class SearchContext {
     // this will be refactor soon
     public EsTablePartitions tablePartitions() throws Exception {
         return EsTablePartitions.fromShardPartitions(table, shardPartitions);
+    }
+
+    public boolean wanOnly() {
+        return wanOnly;
     }
 }
