f296436f22df0e33366443c5b94646fedee28134
lichaoyong <lichaoyong158@gmail.com>
2022-05-20 16:39:20 +0800

    [License] Keep the Apache License for not changed files (#6363)
    Many files come from Apache Doris and have not been changed.
    According to the License rule, the files should keep the Apache License.

--- fe/fe-core/src/main/java/com/starrocks/common/publish/Listener.java
+++ fe/fe-core/src/main/java/com/starrocks/common/publish/Listener.java
@@ -1,7 +1,3 @@
-// This file is made available under Elastic License 2.0.
-// This file is based on code available under the Apache license here:
-//   https://github.com/apache/incubator-doris/blob/master/fe/fe-core/src/main/java/org/apache/doris/common/publish/Listener.java
-
 // Licensed to the Apache Software Foundation (ASF) under one
 // or more contributor license agreements.  See the NOTICE file
 // distributed with this work for additional information
