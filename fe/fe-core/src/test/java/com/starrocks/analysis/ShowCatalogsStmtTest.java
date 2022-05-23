a86653e7c55626f0a0b30ad1e3d13bac4e188cfe
stdpain <drfeng08@gmail.com>
2022-05-23 07:54:27 -0700

    [UT] fix unstable UT in FE
    give a sequential order for some unstable UT https://stackoverflow.com/questions/9667944/how-to-make-junit-test-cases-to-run-in-sequential-order

--- fe/fe-core/src/test/java/com/starrocks/analysis/ShowCatalogsStmtTest.java
+++ fe/fe-core/src/test/java/com/starrocks/analysis/ShowCatalogsStmtTest.java
@@ -19,8 +19,11 @@ import com.starrocks.utframe.UtFrameUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ShowCatalogsStmtTest {
     private static StarRocksAssert starRocksAssert;
     private ConnectContext ctx;
