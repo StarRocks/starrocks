46abf0fa2776bef484b64d28ee1e250b0f3aaeda
Murphy <96611012+mofeiatwork@users.noreply.github.com>
2022-05-23 20:06:02 +0800

    record memory cost in audit log

--- fe/fe-core/src/main/java/com/starrocks/plugin/AuditEvent.java
+++ fe/fe-core/src/main/java/com/starrocks/plugin/AuditEvent.java
@@ -71,6 +71,8 @@ public class AuditEvent {
     public long returnRows = -1;
     @AuditField(value = "CpuCostNs")
     public long cpuCostNs = -1;
+    @AuditField(value = "MemCostBytes")
+    public long memCostBytes = 0;
     @AuditField(value = "StmtId")
     public long stmtId = -1;
     @AuditField(value = "QueryId")
@@ -150,6 +152,11 @@ public class AuditEvent {
          */
         public AuditEventBuilder setCpuCostNs(long cpuNs) {
             auditEvent.cpuCostNs = cpuNs;
+            return this;
+        }
+
+        public AuditEventBuilder setMemCostBytes(long memCostBytes) {
+            auditEvent.memCostBytes = memCostBytes;
             return this;
         }
 
