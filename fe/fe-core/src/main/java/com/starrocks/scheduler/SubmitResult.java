cfb706e9f7c545d5fc523f4052e8b9b52f508d24
xueyan.li <astralidea@163.com>
2022-05-26 21:31:01 +0800

    [Feature] Support Task metadata persistence (#6350)
    Need to consider the case of logs and images.
    You should also consider the GC of Task and TaskRun. The task is cleaned up according to createTime, and TaskRun is cleaned up in the order in which it is put into the queue.

---
+++ fe/fe-core/src/main/java/com/starrocks/scheduler/SubmitResult.java
@@ -0,0 +1,35 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
+
+package com.starrocks.scheduler;
+
+public class SubmitResult {
+    String queryId;
+    SubmitStatus status;
+
+    public SubmitResult(String queryId, SubmitStatus status) {
+        this.queryId = queryId;
+        this.status = status;
+    }
+
+    public String getQueryId() {
+        return queryId;
+    }
+
+    public void setQueryId(String queryId) {
+        this.queryId = queryId;
+    }
+
+    public SubmitStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(SubmitStatus status) {
+        this.status = status;
+    }
+
+    enum SubmitStatus {
+        SUBMITTED,
+        REJECTED,
+        FAILED
+    }
+}
---
+++ fe/fe-core/src/main/java/com/starrocks/scheduler/persist/DropTaskRunsLog.java
@@ -0,0 +1,37 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
+
+package com.starrocks.scheduler.persist;
+
+import com.google.gson.annotations.SerializedName;
+import com.starrocks.common.io.Text;
+import com.starrocks.common.io.Writable;
+import com.starrocks.persist.gson.GsonUtils;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.util.List;
+
+public class DropTaskRunsLog implements Writable {
+
+    @SerializedName("queryIdList")
+    List<String> queryIdList;
+
+    public DropTaskRunsLog(List<String> queryIdList) {
+        this.queryIdList = queryIdList;
+    }
+
+    public List<String> getQueryIdList() {
+        return queryIdList;
+    }
+
+    public static DropTaskRunsLog read(DataInput in) throws IOException {
+        return GsonUtils.GSON.fromJson(Text.readString(in), DropTaskRunsLog.class);
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        String json = GsonUtils.GSON.toJson(this);
+        Text.writeString(out, json);
+    }
+}
