f3517802ada6cf5703e2f4eb809b98777e2b346e
ricky <rickif@qq.com>
2022-05-27 11:56:46 +0800

    fix: build json body

--- be/src/http/action/stream_load.h
+++ be/src/http/action/stream_load.h
@@ -58,6 +58,9 @@ private:
 
 private:
     ExecEnv* _exec_env;
+
+    const size_t _min_buf_sz = 64 * 1024;
+    ByteBufferPtr _buf = ByteBuffer::allocate(_min_buf_sz);
 };
 
 } // namespace starrocks
