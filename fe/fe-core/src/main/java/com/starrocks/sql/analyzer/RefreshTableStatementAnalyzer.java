df0d114d8f612c66f8f01bab0d3c883964ede91b
caneGuy <zhoukang199191@gmail.com>
2022-05-31 14:57:10 +0800

    resolve

--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/RefreshTableStatementAnalyzer.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/RefreshTableStatementAnalyzer.java
@@ -4,9 +4,12 @@ package com.starrocks.sql.analyzer;
 
 import com.starrocks.analysis.DdlStmt;
 import com.starrocks.analysis.TableName;
+import com.starrocks.catalog.Table;
 import com.starrocks.common.ErrorCode;
 import com.starrocks.common.ErrorReport;
 import com.starrocks.qe.ConnectContext;
+import com.starrocks.server.GlobalStateMgr;
+import com.starrocks.server.MetadataMgr;
 import com.starrocks.sql.ast.AstVisitor;
 import com.starrocks.sql.ast.RefreshTableStmt;
 import com.starrocks.sql.common.MetaUtils;
@@ -17,6 +20,12 @@ public class RefreshTableStatementAnalyzer {
     }
 
     static class RefreshTableStatementAnalyzerVisitor extends AstVisitor<Void, ConnectContext> {
+        private final MetadataMgr metadataMgr;
+
+        public RefreshTableStatementAnalyzerVisitor() {
+            this.metadataMgr = GlobalStateMgr.getCurrentState().getMetadataMgr();
+        }
+
         public void analyze(DdlStmt statement, ConnectContext session) {
             visit(statement, session);
         }
@@ -28,6 +37,12 @@ public class RefreshTableStatementAnalyzer {
             if (tableName == null) {
                 ErrorReport.reportSemanticException(ErrorCode.ERR_NO_TABLES_USED);
             }
+
+            Table table = metadataMgr.getTable(tableName.getCatalog(), tableName.getDb(), tableName.getTbl());
+            if (table == null) {
+                ErrorReport.reportSemanticException(ErrorCode.ERR_BAD_TABLE_ERROR, tableName);
+            }
+
             return null;
         }
     }
