ed1d1c7e9646b06b40f391e675863b2805edac41
waittting <491771532@qq.com>
2022-05-31 14:31:16 +0800

    [BugFix]Can not see the loaded data when the state of broker load turn into finished from follower node (#6269)
    Show Load & Show Routine do not use FORWARD_WITH_SYNC. If the metadata of the imported data results are not synchronized to followers, querying these data will report an error. Using FORWARD_WITH_SYNC will keep the metadata consistent for these queries.

---
+++ fe/fe-core/src/test/java/com/starrocks/analysis/ShowRoutineLoadTaskStmtTest.java
@@ -0,0 +1,16 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
+
+package com.starrocks.analysis;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class ShowRoutineLoadTaskStmtTest {
+    
+    @Test 
+    public void testGetRedirectStatus() {
+        ShowRoutineLoadTaskStmt loadStmt = new ShowRoutineLoadTaskStmt("", null);
+        Assert.assertTrue(loadStmt.getRedirectStatus().equals(RedirectStatus.FORWARD_WITH_SYNC));
+    }
+}
