70eb9c9c8b8d405de1a7fa44c8573abc78d93004
Crystal-LiuJing <crystal_liujing_bj@163.com>
2022-06-02 01:24:09 +0800

    support ShowMaterializedViewStmt for new MaterializedView

--- fe/fe-core/src/test/java/com/starrocks/catalog/CreateTableTest.java
+++ fe/fe-core/src/test/java/com/starrocks/catalog/CreateTableTest.java
@@ -64,19 +64,6 @@ public class CreateTableTest {
     }
 
     @Test
-    public void testMine(){
-        ExceptionChecker.expectThrowsNoException(
-                () -> createTable("CREATE TABLE test.sales_records(\n" +
-                        "    record_id int,\n" +
-                        "    seller_id int,\n" +
-                        "    store_id int,\n" +
-                        "    sale_date int,\n" +
-                        "    sale_amt bigint\n" +
-                        ") distributed BY hash(record_id)\n" +
-                        "properties(\"replication_num\" = \"1\");"));
-    }
-
-    @Test
     public void testNormal() throws DdlException {
 
         ExceptionChecker.expectThrowsNoException(
