79acdf4b875e6e11d9da791faff59d9b8448a98e
padmejin <tvtvcode@163.com>
2022-06-02 11:08:45 +0800

    license changed

--- fe/fe-core/src/test/java/com/starrocks/catalog/ColocateTableIndexTest.java
+++ fe/fe-core/src/test/java/com/starrocks/catalog/ColocateTableIndexTest.java
@@ -1,28 +1,11 @@
-// Licensed to the Apache Software Foundation (ASF) under one
-// or more contributor license agreements.  See the NOTICE file
-// distributed with this work for additional information
-// regarding copyright ownership.  The ASF licenses this file
-// to you under the Apache License, Version 2.0 (the
-// "License"); you may not use this file except in compliance
-// with the License.  You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
 
 package com.starrocks.catalog;
 
-import com.google.common.collect.Maps;
 import com.starrocks.analysis.CreateDbStmt;
 import com.starrocks.analysis.CreateTableStmt;
 import com.starrocks.analysis.DropDbStmt;
 import com.starrocks.analysis.DropTableStmt;
-import com.starrocks.catalog.ColocateTableIndex.GroupId;
 import com.starrocks.qe.ConnectContext;
 import com.starrocks.server.GlobalStateMgr;
 import com.starrocks.utframe.UtFrameUtils;
@@ -32,34 +15,13 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.CopyOnWriteArraySet;
 
 public class ColocateTableIndexTest {
     private static final Logger LOG = LogManager.getLogger(ColocateTableIndexTest.class);
 
     @Test
-    public void testGroupId() {
-        GroupId groupId1 = new GroupId(1000, 2000);
-        GroupId groupId2 = new GroupId(1000, 2000);
-        Map<GroupId, Long> map = Maps.newHashMap();
-        Assert.assertTrue(groupId1.equals(groupId2));
-        Assert.assertTrue(groupId1.hashCode() == groupId2.hashCode());
-        map.put(groupId1, 1000L);
-        Assert.assertTrue(map.containsKey(groupId2));
-
-        Set<GroupId> balancingGroups = new CopyOnWriteArraySet<GroupId>();
-        balancingGroups.add(groupId1);
-        Assert.assertTrue(balancingGroups.size() == 1);
-        balancingGroups.remove(groupId2);
-        Assert.assertTrue(balancingGroups.isEmpty());
-    }
-
-    @Test
     public void testDropTable() throws Exception {
         UtFrameUtils.createMinStarRocksCluster();
-
         ConnectContext connectContext = UtFrameUtils.createDefaultCtx();
 
         // create db1
@@ -124,8 +86,6 @@ public class ColocateTableIndexTest {
 
         // drop db1.table1_1
         sql = "DROP TABLE db1.table1_1;";
-        // must use parseStmtWithNewParser() instead of parseAndAnalyzeStmt(), otherwise will cause exception
-        // don't know why...
         DropTableStmt dropTableStmt = (DropTableStmt) UtFrameUtils.parseStmtWithNewParser(sql, connectContext);
         GlobalStateMgr.getCurrentState().dropTable(dropTableStmt);
         // group1 -> table1_1*, table1_2
