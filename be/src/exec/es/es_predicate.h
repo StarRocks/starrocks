0bd6c4d55970a75199b940c819dcd72cd1e6e8ff
imay <buaa.zhaoc@gmail.com>
2022-06-03 23:51:40 +0800

    [Refactor] Make tsan happy (#6876)

--- be/src/exec/es/es_predicate.h
+++ be/src/exec/es/es_predicate.h
@@ -184,12 +184,12 @@ private:
 
     const SlotDescriptor* get_slot_desc(SlotId slot_id);
 
-    ExprContext* _context;
+    ExprContext* _context = nullptr;
     int _disjuncts_num = 0;
-    const TupleDescriptor* _tuple_desc;
+    const TupleDescriptor* _tuple_desc = nullptr;
     std::vector<ExtPredicate*> _disjuncts;
     Status _es_query_status;
-    ObjectPool* _pool;
+    ObjectPool* _pool = nullptr;
     std::map<std::string, std::string> _field_context;
 };
 
