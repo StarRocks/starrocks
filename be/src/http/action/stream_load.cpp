84106dc6f37017de83dba75d29e4d286fde404cf
rickif <rickif@qq.com>
2022-06-09 06:32:16 +0000

    Update be/src/http/action/stream_load.cpp
    Co-authored-by: Alex Zhu <zhuming9011@qq.com>

--- be/src/http/action/stream_load.cpp
+++ be/src/http/action/stream_load.cpp
@@ -314,7 +314,7 @@ void StreamLoadAction::on_chunk_data(HttpRequest* req) {
     while ((len = evbuffer_get_length(evbuf)) > 0) {
         if (ctx->buffer == nullptr) {
             // Initialize buffer.
-            ctx->buffer = ByteBuffer::allocate(ctx->kDefaultBufferSize);
+            ctx->buffer = ByteBuffer::allocate(ctx->format == TFileFormatType::FORMAT_JSON ? std::max(len, kDefaultBufferSize) : len);
 
         } else if (ctx->buffer->remaining() < len) {
             if (ctx->format == TFileFormatType::FORMAT_JSON) {
