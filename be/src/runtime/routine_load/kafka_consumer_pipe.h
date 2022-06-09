1894c9d43653891b6e0093b48bb6732d67cf1dc1
ricky <rickif@qq.com>
2022-06-09 22:04:16 +0800

    fix: flip buffer in append_json

--- be/src/runtime/routine_load/kafka_consumer_pipe.h
+++ be/src/runtime/routine_load/kafka_consumer_pipe.h
@@ -55,6 +55,7 @@ public:
         // For efficiency reasons, simdjson requires a string with a few bytes (simdjson::SIMDJSON_PADDING) at the end.
         auto buf = ByteBuffer::allocate(size + simdjson::SIMDJSON_PADDING);
         buf->put_bytes(data, size);
+        buf->flip();
         return append(std::move(buf));
     }
 };
