82fe19486f505dff0306776cc5b13c5a8a7250b3
Youngwb <yangwenbo_mailbox@163.com>
2022-06-10 10:45:51 +0800

    [BugFix] Fix Enforcer record error required property (#7014)

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/task/EnforceAndCostTask.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/task/EnforceAndCostTask.java
@@ -458,7 +458,7 @@ public class EnforceAndCostTask extends OptimizerTask implements Cloneable {
         curTotalCost += CostModel.calculateCost(enforcer);
 
         if (enforcer.updatePropertyWithCost(newOutputProperty, Lists.newArrayList(oldOutputProperty), curTotalCost)) {
-            enforcer.setOutputPropertySatisfyRequiredProperty(newOutputProperty, context.getRequiredProperty());
+            enforcer.setOutputPropertySatisfyRequiredProperty(newOutputProperty, newOutputProperty);
         }
         groupExpression.getGroup().setBestExpression(enforcer, curTotalCost, newOutputProperty);
         if (ConnectContext.get().getSessionVariable().isSetUseNthExecPlan()) {
