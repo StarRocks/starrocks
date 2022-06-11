e8a53ce8703aefc0acb5fe0d97b6a15fd1447d01
Murphy <96611012+mofeiatwork@users.noreply.github.com>
2022-06-11 13:55:18 +0800

    Fix the default encoding of largeint (#7113) (#7117)
    (cherry picked from commit dc22c8d0b7d9ed9d72a9ad45c6e208a0cd43ce2a)

--- be/test/storage/rowset/encoding_info_test.cpp
+++ be/test/storage/rowset/encoding_info_test.cpp
@@ -26,6 +26,7 @@
 #include <iostream>
 
 #include "common/logging.h"
+#include "gen_cpp/segment.pb.h"
 #include "storage/olap_common.h"
 #include "storage/types.h"
 
@@ -50,6 +51,53 @@ TEST_F(EncodingInfoTest, no_encoding) {
     const EncodingInfo* encoding_info = nullptr;
     auto status = EncodingInfo::get(OLAP_FIELD_TYPE_BIGINT, DICT_ENCODING, &encoding_info);
     ASSERT_FALSE(status.ok());
+}
+
+TEST_F(EncodingInfoTest, default_encoding) {
+    std::map<FieldType, EncodingTypePB> default_expected = {
+            {OLAP_FIELD_TYPE_TINYINT, BIT_SHUFFLE},
+            {OLAP_FIELD_TYPE_SMALLINT, BIT_SHUFFLE},
+            {OLAP_FIELD_TYPE_INT, BIT_SHUFFLE},
+            {OLAP_FIELD_TYPE_BIGINT, BIT_SHUFFLE},
+            {OLAP_FIELD_TYPE_LARGEINT, BIT_SHUFFLE},
+
+            {OLAP_FIELD_TYPE_FLOAT, BIT_SHUFFLE},
+            {OLAP_FIELD_TYPE_DOUBLE, BIT_SHUFFLE},
+
+            {OLAP_FIELD_TYPE_CHAR, DICT_ENCODING},
+            {OLAP_FIELD_TYPE_VARCHAR, DICT_ENCODING},
+
+            {OLAP_FIELD_TYPE_BOOL, RLE},
+
+            {OLAP_FIELD_TYPE_DATE, BIT_SHUFFLE},
+            {OLAP_FIELD_TYPE_DATE_V2, BIT_SHUFFLE},
+            {OLAP_FIELD_TYPE_DATETIME, BIT_SHUFFLE},
+            {OLAP_FIELD_TYPE_TIMESTAMP, BIT_SHUFFLE},
+
+            {OLAP_FIELD_TYPE_DECIMAL, BIT_SHUFFLE},
+            {OLAP_FIELD_TYPE_DECIMAL_V2, BIT_SHUFFLE},
+
+            {OLAP_FIELD_TYPE_HLL, PLAIN_ENCODING},
+            {OLAP_FIELD_TYPE_OBJECT, PLAIN_ENCODING},
+            {OLAP_FIELD_TYPE_PERCENTILE, PLAIN_ENCODING},
+    };
+    std::map<FieldType, EncodingTypePB> value_seek_expected = {
+            {OLAP_FIELD_TYPE_TINYINT, FOR_ENCODING},    {OLAP_FIELD_TYPE_SMALLINT, FOR_ENCODING},
+            {OLAP_FIELD_TYPE_INT, FOR_ENCODING},        {OLAP_FIELD_TYPE_BIGINT, FOR_ENCODING},
+            {OLAP_FIELD_TYPE_LARGEINT, FOR_ENCODING},   {OLAP_FIELD_TYPE_CHAR, PREFIX_ENCODING},
+            {OLAP_FIELD_TYPE_VARCHAR, PREFIX_ENCODING}, {OLAP_FIELD_TYPE_BOOL, PLAIN_ENCODING},
+            {OLAP_FIELD_TYPE_DATE, FOR_ENCODING},       {OLAP_FIELD_TYPE_DATE_V2, FOR_ENCODING},
+            {OLAP_FIELD_TYPE_DATETIME, FOR_ENCODING},   {OLAP_FIELD_TYPE_TIMESTAMP, FOR_ENCODING},
+            {OLAP_FIELD_TYPE_DECIMAL_V2, BIT_SHUFFLE},  {OLAP_FIELD_TYPE_DECIMAL, BIT_SHUFFLE},
+    };
+    for (auto [type, encoding] : default_expected) {
+        auto default_encoding = EncodingInfo::get_default_encoding(type, false);
+        EXPECT_EQ(default_encoding, encoding);
+    }
+    for (auto [type, encoding] : value_seek_expected) {
+        auto default_encoding = EncodingInfo::get_default_encoding(type, true);
+        EXPECT_EQ(default_encoding, encoding);
+    }
 }
 
 } // namespace starrocks
