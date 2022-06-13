5f77a1cdb5a4bba592dc1ae6d19b7a398c1cf7a8
ricky <rickif@qq.com>
2022-06-13 22:22:50 +0800

    test: add get_json_string_array

--- be/test/exprs/vectorized/json_functions_test.cpp
+++ be/test/exprs/vectorized/json_functions_test.cpp
@@ -226,6 +226,42 @@ TEST_F(JsonFunctionsTest, get_json_string_casting) {
                         .ok());
 }
 
+TEST_F(JsonFunctionsTest, get_json_string_array) {
+    std::unique_ptr<FunctionContext> ctx(FunctionContext::create_test_context());
+    Columns columns;
+    auto strings = BinaryColumn::create();
+    auto strings2 = BinaryColumn::create();
+
+    std::string values[] = {R"([{"key":    1}", {"key": 2 }])", R"([{"key":    1}", {"key": 2 }])",
+                            R"([{"key":    1}", {"key": 2 }])"};
+
+    std::string strs[] = {"$..key", "$.[*].key", "$[*].key"};
+    std::string length_strings[] = {"[1, 2]", "[1, 2]", "[1, 2]"};
+
+    for (int j = 0; j < sizeof(values) / sizeof(values[0]); ++j) {
+        strings->append(values[j]);
+        strings2->append(strs[j]);
+    }
+
+    columns.emplace_back(strings);
+    columns.emplace_back(strings2);
+
+    ctx.get()->impl()->set_constant_columns(columns);
+    ASSERT_TRUE(JsonFunctions::json_path_prepare(ctx.get(), FunctionContext::FunctionStateScope::FRAGMENT_LOCAL).ok());
+
+    ColumnPtr result = JsonFunctions::get_json_string(ctx.get(), columns);
+
+    auto v = ColumnHelper::cast_to<TYPE_VARCHAR>(result);
+
+    for (int j = 0; j < sizeof(values) / sizeof(values[0]); ++j) {
+        ASSERT_EQ(length_strings[j], v->get_data()[j].to_string());
+    }
+
+    ASSERT_TRUE(JsonFunctions::json_path_close(ctx.get(),
+                                               FunctionContext::FunctionContext::FunctionStateScope::FRAGMENT_LOCAL)
+                        .ok());
+}
+
 TEST_F(JsonFunctionsTest, get_json_emptyTest) {
     {
         std::unique_ptr<FunctionContext> ctx(FunctionContext::create_test_context());
