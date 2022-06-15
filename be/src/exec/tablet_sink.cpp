c11075e77fc376ff8a4e6b24311c0a9655c05e24
sduzh <zhuming9011@qq.com>
2022-06-15 21:01:54 +0800

    [Lake] Create LakeTabletsChannel on demand

--- be/src/exec/tablet_sink.cpp
+++ be/src/exec/tablet_sink.cpp
@@ -140,6 +140,7 @@ void NodeChannel::open() {
     request.set_index_id(_index_id);
     request.set_txn_id(_parent->_txn_id);
     request.set_allocated_schema(_parent->_schema->to_protobuf());
+    request.set_is_lake_tablet(_parent->_is_lake_table);
     for (auto& tablet : _all_tablets) {
         auto ptablet = request.add_tablets();
         ptablet->set_partition_id(tablet.partition_id);
@@ -564,6 +565,7 @@ Status OlapTableSink::init(const TDataSink& t_sink) {
     _num_repicas = table_sink.num_replicas;
     _need_gen_rollup = table_sink.need_gen_rollup;
     _tuple_desc_id = table_sink.tuple_id;
+    _is_lake_table = table_sink.is_lake_table;
     _schema = std::make_shared<OlapTableSchemaParam>();
     RETURN_IF_ERROR(_schema->init(table_sink.schema));
     _vectorized_partition = _pool->add(new vectorized::OlapTablePartitionParam(_schema, table_sink.partition));
