ec43ec84c6c7c3c4198780d4846f7db3f3538e03
stdpain <34912776+stdpain@users.noreply.github.com>
2022-06-16 14:00:49 +0800

    [feature] support sql_mode MODE_DOUBLE_LITERAL (#7240)
    For accuracy, in the past our literal would give preference to the decimal type,
    but in some cases we don't care about a precise value, so we provide a new SQL_MODE that gives preference to the float/double type for literals

--- fe/fe-core/src/main/java/com/starrocks/sql/parser/AstBuilder.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/parser/AstBuilder.java
@@ -2203,14 +2203,19 @@ public class AstBuilder extends StarRocksBaseVisitor<ParseNode> {
     @Override
     public ParseNode visitDoubleValue(StarRocksParser.DoubleValueContext context) {
         try {
-            BigDecimal decimal = new BigDecimal(context.getText());
-            int precision = DecimalLiteral.getRealPrecision(decimal);
-            int scale = DecimalLiteral.getRealScale(decimal);
-            int integerPartWidth = precision - scale;
-            if (integerPartWidth > 38) {
+            if (SqlModeHelper.check(sqlMode, SqlModeHelper.MODE_DOUBLE_LITERAL)) {
                 return new FloatLiteral(context.getText());
+            } else {
+                BigDecimal decimal = new BigDecimal(context.getText());
+                int precision = DecimalLiteral.getRealPrecision(decimal);
+                int scale = DecimalLiteral.getRealScale(decimal);
+                int integerPartWidth = precision - scale;
+                if (integerPartWidth > 38) {
+                    return new FloatLiteral(context.getText());
+                }
+                return new DecimalLiteral(decimal);
             }
-            return new DecimalLiteral(decimal);
+
         } catch (AnalysisException | NumberFormatException e) {
             throw new ParsingException(e.getMessage());
         }
@@ -2219,7 +2224,11 @@ public class AstBuilder extends StarRocksBaseVisitor<ParseNode> {
     @Override
     public ParseNode visitDecimalValue(StarRocksParser.DecimalValueContext context) {
         try {
-            return new DecimalLiteral(context.getText());
+            if (SqlModeHelper.check(sqlMode, SqlModeHelper.MODE_DOUBLE_LITERAL)) {
+                return new FloatLiteral(context.getText());
+            } else {
+                return new DecimalLiteral(context.getText());
+            }
         } catch (AnalysisException e) {
             throw new ParsingException(e.getMessage());
         }
