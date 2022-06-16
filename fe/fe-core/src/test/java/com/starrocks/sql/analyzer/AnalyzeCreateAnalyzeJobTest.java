78746c51d6a78ca4b0251bf4aa51815c6ecabc71
ABingHuang <codekhuang@163.com>
2022-06-16 10:54:10 +0800

    fix ut

--- fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzeCreateAnalyzeJobTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzeCreateAnalyzeJobTest.java
@@ -2,6 +2,8 @@
 
 package com.starrocks.sql.analyzer;
 
+import com.starrocks.catalog.Database;
+import com.starrocks.catalog.Table;
 import com.starrocks.sql.ast.CreateAnalyzeJobStmt;
 import com.starrocks.statistic.AnalyzeJob;
 import com.starrocks.utframe.StarRocksAssert;
@@ -45,7 +47,8 @@ public class AnalyzeCreateAnalyzeJobTest {
         String sql = "create analyze full database db";
         CreateAnalyzeJobStmt analyzeStmt = (CreateAnalyzeJobStmt) analyzeSuccess(sql);
 
-        Assert.assertEquals(10110, analyzeStmt.getDbId());
+        Database db = starRocksAssert.getCtx().getGlobalStateMgr().getDb("default_cluster:db");
+        Assert.assertEquals(db.getId(), analyzeStmt.getDbId());
         Assert.assertEquals(AnalyzeJob.DEFAULT_ALL_ID, analyzeStmt.getTableId());
         Assert.assertTrue(analyzeStmt.getColumnNames().isEmpty());
     }
@@ -55,8 +58,10 @@ public class AnalyzeCreateAnalyzeJobTest {
         String sql = "create analyze table db.tbl(kk1, kk2)";
         CreateAnalyzeJobStmt analyzeStmt = (CreateAnalyzeJobStmt) analyzeSuccess(sql);
 
-        Assert.assertEquals(10110, analyzeStmt.getDbId());
-        Assert.assertEquals(10112, analyzeStmt.getTableId());
+        Database db = starRocksAssert.getCtx().getGlobalStateMgr().getDb("default_cluster:db");
+        Assert.assertEquals(db.getId(), analyzeStmt.getDbId());
+        Table table = db.getTable("tbl");
+        Assert.assertEquals(table.getId(), analyzeStmt.getTableId());
         Assert.assertEquals(2, analyzeStmt.getColumnNames().size());
     }
 }
