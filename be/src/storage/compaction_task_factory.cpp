e9e9de2ff43cef0fa82d28cb73974f60398b85ea
xyz <a997647204@gmail.com>
2022-06-16 09:24:31 +0800

    [BugFix] choose wrong compaction algorithm in some case (#7225)
    When performing base compaction on the tablet with the following rowsets:
    
       "rowsets": [
            "[0-175183] 11 DATA NONOVERLAPPING",
            "[175184-175232] 0 DATA NONOVERLAPPING",
            "[175233-175278] 0 DATA NONOVERLAPPING",
            "[175279-175327] 0 DATA NONOVERLAPPING",
            "[175328-175369] 0 DATA NONOVERLAPPING"
        ]
    
    Because there is 11 segments and columns num are more than 5, it will choose the vertical compaction.
    When creating a tablet reader, it will merge 11 segment iterators into one union iterator, and create a HeapMaskIterator with only one union_iterator.
    In the `new_heap_merge_iterator`, it will return the iterator immediately if the input iterators length is 1.
    So here we will then call the get_next(Chunk* chunk, std::vector<RowSourceMask>* source_masks) on the union_iterator,
    However, the union_iterator doesn't implement get_next(Chunk* chunk, std::vector<RowSourceMask>* source_masks), and
    we get the warning `get chunk with sources not supported`.
    
    In fact, we should count the number of segment iterator after creating a tablet reader, only one union iterator in this case,
    and choose the horizontal compaction.
    
    (cherry picked from commit cc75587f461a37096b40f70364b97216e7a81d07)

--- be/src/storage/compaction_task_factory.cpp
+++ be/src/storage/compaction_task_factory.cpp
@@ -18,7 +18,13 @@
 namespace starrocks {
 
 std::shared_ptr<CompactionTask> CompactionTaskFactory::create_compaction_task() {
-    size_t segment_iterator_num = Rowset::get_segment_num(_input_rowsets);
+    auto iterator_num_res = Rowset::get_segment_num(_input_rowsets);
+    if (!iterator_num_res.ok()) {
+        LOG(WARNING) << "fail to get segment iterator num. tablet=" << _tablet->tablet_id()
+                     << ", err=" << iterator_num_res.status().to_string();
+        return nullptr;
+    }
+    size_t segment_iterator_num = iterator_num_res.value();
     int64_t max_columns_per_group = config::vertical_compaction_max_columns_per_group;
     size_t num_columns = _tablet->num_columns();
     CompactionAlgorithm algorithm =
