fa48820488ee2ece58b331beec41b1a5e2857b45
wyb <wybb86@gmail.com>
2022-06-16 13:33:30 +0800

    [Lake] Create lake table in FE (#7063)

--- fe/fe-core/src/test/java/com/starrocks/catalog/StarOSAgentTest.java
+++ fe/fe-core/src/test/java/com/starrocks/catalog/StarOSAgentTest.java
@@ -2,16 +2,28 @@
 
 package com.starrocks.catalog;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.staros.client.StarClient;
 import com.staros.client.StarClientException;
+import com.staros.proto.ReplicaInfo;
+import com.staros.proto.ReplicaRole;
+import com.staros.proto.ShardInfo;
+import com.staros.proto.WorkerInfo;
+import com.staros.proto.WorkerState;
 import com.starrocks.common.DdlException;
 import com.starrocks.common.ExceptionChecker;
-import mockit.Expectations;
+import com.starrocks.common.UserException;
 import com.starrocks.common.jmockit.Deencapsulation;
+import mockit.Expectations;
 import mockit.Mocked;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
 
 public class StarOSAgentTest {
     private StarOSAgent starosAgent;
@@ -167,5 +179,57 @@ public class StarOSAgentTest {
         ExceptionChecker.expectThrowsWithMsg(DdlException.class,
                 "Failed to remove worker.",
                 () -> starosAgent.removeWorker("127.0.0.1:8090"));
+    }
+
+    public void testCreateShards() throws StarClientException, DdlException {
+        new Expectations() {
+            {
+                client.createShard(1L, 2);
+                minTimes = 0;
+                result = Lists.newArrayList(ShardInfo.newBuilder().setShardId(10L).build(),
+                        ShardInfo.newBuilder().setShardId(11L).build());
+            }
+        };
+
+        starosAgent.setServiceId(1L);
+        Assert.assertEquals(Lists.newArrayList(10L, 11L), starosAgent.createShards(2, null));
+    }
+
+    @Test
+    public void testGetBackendByShard() throws StarClientException, UserException {
+        ReplicaInfo replica1 = ReplicaInfo.newBuilder()
+                .setReplicaRole(ReplicaRole.PRIMARY)
+                .setWorkerInfo(WorkerInfo.newBuilder().setWorkerId(1L).setWorkerState(WorkerState.ON).build())
+                .build();
+        ReplicaInfo replica2 = ReplicaInfo.newBuilder()
+                .setReplicaRole(ReplicaRole.SECONDARY)
+                .setWorkerInfo(WorkerInfo.newBuilder().setWorkerId(2L).setWorkerState(WorkerState.ON).build())
+                .build();
+        ReplicaInfo replica3 = ReplicaInfo.newBuilder()
+                .setReplicaRole(ReplicaRole.SECONDARY)
+                .setWorkerInfo(WorkerInfo.newBuilder().setWorkerId(3L).setWorkerState(WorkerState.OFF).build())
+                .build();
+        List<ReplicaInfo> replicas = Lists.newArrayList(replica1, replica2, replica3);
+
+        ShardInfo shard = ShardInfo.newBuilder().setShardId(10L).addAllReplicaInfo(replicas).build();
+        List<ShardInfo> shards = Lists.newArrayList(shard);
+
+        new Expectations() {
+            {
+                client.getShardInfo(1L, Lists.newArrayList(10L));
+                minTimes = 0;
+                result = shards;
+            }
+        };
+
+        Map<Long, Long> workerToBackend = Maps.newHashMap();
+        workerToBackend.put(1L, 10001L);
+        workerToBackend.put(2L, 10002L);
+        workerToBackend.put(3L, 10003L);
+        Deencapsulation.setField(starosAgent, "workerToBackend", workerToBackend);
+
+        starosAgent.setServiceId(1L);
+        Assert.assertEquals(10001L, starosAgent.getPrimaryBackendIdByShard(10L));
+        Assert.assertEquals(Sets.newHashSet(10001L, 10002L, 10003L), starosAgent.getBackendIdsByShard(10L));
     }
 }
