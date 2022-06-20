6692e25a7d4e343936e8baae187d653e7d4a806d
Stephen <dulong41@gmail.com>
2022-06-20 15:06:23 +0800

    Create starrocks_fe_unittest.sh

---
+++ starrocks_fe_unittest.sh
@@ -0,0 +1,104 @@
+#!/usr/bin/bash
+
+source ~/.bash_profile
+set -eo pipefail
+
+ROOT=`dirname "$0"`
+ROOT=`cd "$ROOT/.."; pwd`
+
+GITHUB_PR_NUMBER=${1:?"need GITHUB_PR_NUMBER parameter"}
+GITHUB_PR_TARGET_BRANCH=${2:?"need GITHUB_PR_TARGET_BRANCH parameter"}
+GITHUB_PR_TARGET_BRANCH="main"
+#GITHUB_PR_COMMENT_BODY=${3:-"default"}
+echo "the github number"$GITHUB_PR_NUMBE
+echo "the branch="$GITHUB_PR_TARGET_BRANCH
+echo "the root path="$ROOT
+echo "the env="$GITHUB_REF_NAME
+# chown -R jenkins:jenkins $ROOT/../
+
+#cd $ROOT/resource/starrocks/
+cd $ROOT/starrocks/
+rm -rf fe/fe-core/target
+
+#git config  user.email "wanpengfei91@163.com"
+#git config  user.name "wanpengfei-git"
+#git stash
+#git clean -df
+#git checkout main
+#for i in 1 2 3 4 5; do 
+#    timeout 180 git pull && break || sleep 60
+#done
+#
+#BRANCH_NAME="pr_${GITHUB_PR_NUMBER}_${RANDOM}"
+#echo $BRANCH_NAME
+#for i in 1 2 3 4 5; do 
+#    timeout 180 git fetch origin pull/${GITHUB_PR_NUMBER}/head:${BRANCH_NAME} && break || sleep 60
+#done
+#git checkout ${BRANCH_NAME}
+#git clean -df
+
+#FE_FILE_CHANGE_STATUS=0
+#git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD main) > files_change_list.txt
+#while read line; do if [[ $line =~ ^fe/fe-core/* || $line =~ ^fe/spark-dpp/* || $line =~ ^fe/pom.xml || $line =~ ^run-fe-ut.sh || $line =~ ^build.sh || $line =~ ^gensrc/* ]];then FE_FILE_CHANGE_STATUS=1;break;fi;done < files_change_list.txt
+#
+#if (( $FE_FILE_CHANGE_STATUS == 0 ));then
+#    exit 0
+#fi
+
+# branch like workgroup_main should also run main image
+if [[ $GITHUB_PR_TARGET_BRANCH == *main* ]];then
+    GITHUB_PR_TARGET_BRANCH="main"
+fi
+
+tmp_path=$(echo `cd "$ROOT/.."; pwd` | awk -F '/' '{print $NF}')
+container_name=${GITHUB_PR_TARGET_BRANCH}_${tmp_path}
+echo "===========container_name is $container_name=========="
+
+docker stop $container_name || echo 1
+docker rm $container_name || echo 1
+
+echo "the docker map path="$ROOT
+echo "the root ls="`ls /home/runner/work/starrocks/starrocks`
+m2Path="/home/runner/"
+docker run --privileged -v $m2Path/.m2:/root/.m2 -v $ROOT/starrocks:/root/starrocks -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro --name $container_name -d starrocks/dev-env:$GITHUB_PR_TARGET_BRANCH /bin/bash -c "while true;do echo hello;sleep 1;done"
+sleep 10
+
+echo "run docker for script"
+
+cmd="cd /root/starrocks;
+export FE_UT_PARALLEL=16;
+timeout 3600 sh run-fe-ut.sh --run"
+
+docker exec --privileged $container_name /bin/bash -c "$cmd"
+
+echo "script run over-----"
+
+if [ "$GITHUB_PR_TARGET_BRANCH" == "main" ];then
+    cd $ROOT/resource/starrocks/fe/fe-core/target
+    jacoco_result="jacoco_${GITHUB_PR_NUMBER}.exec"
+    mv jacoco.exec $jacoco_result || true
+
+    java -jar $ROOT/../jacococli.jar report ./$jacoco_result --classfiles ./classes/ --html ./result --sourcefiles $ROOT/resource/starrocks/fe/fe-core/src/main/java/ --encoding utf-8 --name fe-coverage
+
+    cd $ROOT/../cover-checker
+    time_count=0
+    pull_status=1
+    while (( $pull_status != 0 ));do
+        if (( $time_count == 3 ));then
+            exit 1
+        fi
+        timeout 180 java -jar cover-checker-console/target/cover-checker-console-1.4.0-jar-with-dependencies.jar --cover $ROOT/resource/starrocks/fe/fe-core/target/result/ --github-token 66e4c48809eb7e058eb73668b8c816867e6d7cbe  --repo StarRocks/starrocks --threshold 80 --github-url api.github.com  --pr ${GITHUB_PR_NUMBER} -type jacoco
+        pull_status=$?
+        time_count=`expr $time_count + 1`
+    done
+fi
+
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.14.1.1-1.el7_9.x86_64
+export PATH=$JAVA_HOME/bin:$PATH
+
+cd $ROOT/resource/starrocks/fe
+mvn clean -DskipTests verify sonar:sonar   -Dsonar.projectKey=StarRocks-${GITHUB_PR_NUMBER}   -Dsonar.host.url=http://39.101.206.100:9000   -Dsonar.login=5da9593bf8f8af7060968672bc2451700139349f
+rm -rf fe-core/target/generated-sources/
+
+docker stop $container_name || echo 1
+chown -R jenkins:jenkins $ROOT/../
