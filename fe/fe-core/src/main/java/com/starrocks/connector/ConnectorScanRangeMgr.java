22f4833801ac890ce3e02f46ea3a73df4cf1460d
caneGuy <zhoukang199191@gmail.com>
2022-06-20 16:20:33 +0800

    add annotations

--- fe/fe-core/src/main/java/com/starrocks/connector/ConnectorScanRangeMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/ConnectorScanRangeMgr.java
@@ -5,6 +5,12 @@ package com.starrocks.connector;
 import com.starrocks.sql.optimizer.operator.physical.PhysicalScanOperator;
 import com.starrocks.thrift.TScanRangeLocations;
 
+/**
+ * Each connector needs to provide its own scan range calculation logic by implementing this
+ * Interface. We will call ScanRangeMgr#getScanRanges in the core engine, the core engine will
+ * get scan range locations use ConnectorScanRangeMgr for each connector. For example, if we are
+ * using hive connector, then the core engine will call HiveScanRangeMgr to get TScanRangeLocations.
+ */
 public interface ConnectorScanRangeMgr {
 
     /**
