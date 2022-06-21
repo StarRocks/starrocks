8746271de2bd1780fc2a13db4e77d8ba8a5c9910
zombee0 <flylucas_10@163.com>
2022-06-21 23:43:53 +0800

    [Refactor] Refactor ddl-DropDb (#7601)

--- fe/fe-core/src/main/java/com/starrocks/execution/DataDefinitionExecutorFactory.java
+++ fe/fe-core/src/main/java/com/starrocks/execution/DataDefinitionExecutorFactory.java
@@ -5,6 +5,7 @@ package com.starrocks.execution;
 import com.google.common.collect.ImmutableMap;
 import com.starrocks.analysis.CreateDbStmt;
 import com.starrocks.analysis.DdlStmt;
+import com.starrocks.analysis.DropDbStmt;
 import com.starrocks.analysis.StatementBase;
 import com.starrocks.qe.ConnectContext;
 import com.starrocks.qe.DdlExecutor;
@@ -13,8 +14,9 @@ import com.starrocks.qe.ShowResultSet;
 public class DataDefinitionExecutorFactory {
     private static final ImmutableMap<Class<? extends StatementBase>, DataDefinitionExecutor> executorMap =
             new ImmutableMap.Builder<Class<? extends StatementBase>, DataDefinitionExecutor>()
-                    .put(CreateDbStmt.class, new CreateDbExecutor()).
-                    build();
+                    .put(CreateDbStmt.class, new CreateDbExecutor())
+                    .put(DropDbStmt.class, new DropDbExecutor())
+                    .build();
 
     public static ShowResultSet execute(StatementBase stmt, ConnectContext context) throws Exception {
         DataDefinitionExecutor executor = executorMap.get(stmt.getClass());
