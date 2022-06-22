StarRocks/starrocks

commit 60aa03ac4bf489d79804a99b2c6c4b1d1d592ec0

Replace OLAPStatus with Status in RowsetWriter (#2548)
By replacing OLAPStatus with Status we have a chance
to give user more precise error messages

be/src/storage/tablet_manager.cpp
@@ -34,7 +34,6 @@ DIAGNOSTIC_POP
 #include <memory>
 
 #include "env/env.h"
-#include "gutil/strings/strcat.h"
 #include "runtime/current_thread.h"
 #include "storage/data_dir.h"
 #include "storage/olap_common.h"
@@ -46,18 +45,11 @@ DIAGNOSTIC_POP
 #include "storage/tablet.h"
 #include "storage/tablet_meta.h"
 #include "storage/tablet_meta_manager.h"
-#include "storage/tablet_updates.h"
 #include "storage/update_manager.h"
 #include "storage/utils.h"
 #include "util/file_utils.h"
 #include "util/path_util.h"
-#include "util/scoped_cleanup.h"
 #include "util/starrocks_metrics.h"
-
-using std::list;
-using std::map;
-using std::set;
-using strings::Substitute;
 
 namespace starrocks {
 
@@ -1080,16 +1072,16 @@ Status TabletManager::_create_inital_rowset_unlocked(const TCreateTabletReq& req
                 LOG(WARNING) << "failed to init rowset writer for tablet " << tablet->full_name() << ": " << st;
                 break;
             }
-            if (rowset_writer->flush() != OLAP_SUCCESS) {
-                LOG(WARNING) << "failed to flush rowset writer for tablet " << tablet->full_name();
-                st = Status::InternalError("flush rowset failed");
+            st = rowset_writer->flush();
+            if (!st.ok()) {
+                LOG(WARNING) << "failed to flush rowset writer for tablet " << tablet->full_name() << ": " << st;
                 break;
             }
-
-            new_rowset = rowset_writer->build();
-            st = tablet->add_rowset(new_rowset, false);
+            auto new_rowset = rowset_writer->build();
+            if (!new_rowset.ok()) return new_rowset.status();
+            st = tablet->add_rowset(*new_rowset, false);
             if (!st.ok()) {
-                LOG(WARNING) << "failed to add rowset for tablet " << tablet->full_name();
+                LOG(WARNING) << "failed to add rowset for tablet " << tablet->full_name() << ": " << st;
                 break;
             }
         } while (false);
@@ -1098,7 +1090,7 @@ Status TabletManager::_create_inital_rowset_unlocked(const TCreateTabletReq& req
         if (!st.ok()) {
             LOG(WARNING) << "fail to create initial rowset: " << st << " version=" << version;
             StorageEngine::instance()->add_unused_rowset(new_rowset);
-            return Status::InternalError("fail to create initial rowset");
+            return Status::InternalError(fmt::format("fail to create initial rowset: {}", st.to_string()));
         }
     }
     tablet->set_cumulative_layer_point(request.version + 1);

be/src/storage/memtable_flush_executor.cpp
@@ -56,11 +56,7 @@ std::ostream& operator<<(std::ostream& os, const FlushStatistic& stat) {
 }
 
 Status FlushToken::submit(std::unique_ptr<vectorized::MemTable> memtable) {
-    if (_flush_status.load() != OLAP_SUCCESS) {
-        std::stringstream ss;
-        ss << "tablet_id = " << memtable->tablet_id() << " flush_status error ";
-        return Status::InternalError(ss.str());
-    }
+    RETURN_IF_ERROR(status());
     // Does not acount the size of MemtableFlushTask into any memory tracker
     SCOPED_THREAD_LOCAL_MEM_TRACKER_SETTER(nullptr);
     auto task = std::make_shared<MemtableFlushTask>(this, std::move(memtable));
@@ -71,24 +67,19 @@ void FlushToken::cancel() {
     _flush_token->shutdown();
 }
 
-OLAPStatus FlushToken::wait() {
+Status FlushToken::wait() {
     _flush_token->wait();
-    return _flush_status.load();
+    std::lock_guard l(_status_lock);
+    return _status;
 }
 
 void FlushToken::_flush_memtable(vectorized::MemTable* memtable) {
     // If previous flush has failed, return directly
-    if (_flush_status.load() != OLAP_SUCCESS) {
-        return;
-    }
+    if (!status().ok()) return;
 
     MonotonicStopWatch timer;
     timer.start();
-    _flush_status.store(memtable->flush());
-    if (_flush_status.load() != OLAP_SUCCESS) {
-        return;
-    }
-
+    set_status(memtable->flush());
     _stats.flush_time_ns += timer.elapsed_time();
     _stats.flush_count++;
     _stats.flush_size_bytes += memtable->memory_usage();

be/src/storage/memtable_flush_executor.h
@@ -25,7 +25,9 @@
 #include <memory>
 #include <vector>
 
+#include "common/status.h"
 #include "storage/olap_define.h"
+#include "util/spinlock.h"
 #include "util/threadpool.h"
 
 namespace starrocks {
@@ -58,7 +60,7 @@ std::ostream& operator<<(std::ostream& os, const FlushStatistic& stat);
 class FlushToken {
 public:
     explicit FlushToken(std::unique_ptr<ThreadPoolToken> flush_pool_token)
-            : _flush_token(std::move(flush_pool_token)), _flush_status(OLAP_SUCCESS) {}
+            : _flush_token(std::move(flush_pool_token)), _status() {}
 
     Status submit(std::unique_ptr<vectorized::MemTable> mem_table);
 
@@ -67,10 +69,21 @@ public:
     void cancel();
 
     // wait all tasks in token to be completed.
-    OLAPStatus wait();
+    Status wait();
 
     // get flush operations' statistics
     const FlushStatistic& get_stats() const { return _stats; }
+
+    Status status() const {
+        std::lock_guard l(_status_lock);
+        return _status;
+    }
+
+    void set_status(const Status& status) {
+        if (status.ok()) return;
+        std::lock_guard l(_status_lock);
+        if (_status.ok()) _status = status;
+    }
 
 private:
     friend class MemtableFlushTask;
@@ -79,9 +92,10 @@ private:
 
     std::unique_ptr<ThreadPoolToken> _flush_token;
 
+    mutable SpinLock _status_lock;
     // Records the current flush status of the tablet.
-    // Note: Once its value is set to Failed, it cannot return to SUCCESS.
-    std::atomic<OLAPStatus> _flush_status;
+    // Note: Once its value is set to Failed, it cannot return to OK.
+    Status _status;
 
     FlushStatistic _stats;
 };

be/src/storage/rowset/beta_rowset_writer.h
@@ -25,6 +25,7 @@
 #include <mutex>
 #include <vector>
 
+#include "common/statusor.h"
 #include "storage/rowset/rowset_writer.h"
 
 namespace starrocks {
@@ -40,9 +41,9 @@ public:
     explicit BetaRowsetWriter(const RowsetWriterContext& context);
     ~BetaRowsetWriter() override = default;
 
-    OLAPStatus init() override;
+    Status init() override;
 
-    RowsetSharedPtr build() override;
+    StatusOr<RowsetSharedPtr> build() override;
 
     Version version() override { return _context.version; }
     int64_t num_rows() override { return _num_rows_written; }
@@ -81,25 +82,24 @@ public:
     explicit HorizontalBetaRowsetWriter(const RowsetWriterContext& context);
     ~HorizontalBetaRowsetWriter() override;
 
-    OLAPStatus add_chunk(const vectorized::Chunk& chunk) override;
-    OLAPStatus add_chunk_with_rssid(const vectorized::Chunk& chunk, const vector<uint32_t>& rssid) override;
+    Status add_chunk(const vectorized::Chunk& chunk) override;
+    Status add_chunk_with_rssid(const vectorized::Chunk& chunk, const vector<uint32_t>& rssid) override;
 
-    OLAPStatus flush_chunk(const vectorized::Chunk& chunk) override;
-    OLAPStatus flush_chunk_with_deletes(const vectorized::Chunk& upserts, const vectorized::Column& deletes) override;
+    Status flush_chunk(const vectorized::Chunk& chunk) override;
+    Status flush_chunk_with_deletes(const vectorized::Chunk& upserts, const vectorized::Column& deletes) override;
 
     // add rowset by create hard link
-    OLAPStatus add_rowset(RowsetSharedPtr rowset) override;
-    OLAPStatus add_rowset_for_linked_schema_change(RowsetSharedPtr rowset,
-                                                   const SchemaMapping& schema_mapping) override;
+    Status add_rowset(RowsetSharedPtr rowset) override;
+    Status add_rowset_for_linked_schema_change(RowsetSharedPtr rowset, const SchemaMapping& schema_mapping) override;
 
-    OLAPStatus flush() override;
+    Status flush() override;
 
-    RowsetSharedPtr build() override;
+    StatusOr<RowsetSharedPtr> build() override;
 
 private:
-    std::unique_ptr<SegmentWriter> _create_segment_writer();
+    StatusOr<std::unique_ptr<SegmentWriter>> _create_segment_writer();
 
-    OLAPStatus _flush_segment_writer(std::unique_ptr<SegmentWriter>* segment_writer);
+    Status _flush_segment_writer(std::unique_ptr<SegmentWriter>* segment_writer);
 
     Status _final_merge();
 
@@ -112,18 +112,21 @@ public:
     explicit VerticalBetaRowsetWriter(const RowsetWriterContext& context);
     ~VerticalBetaRowsetWriter() override;
 
-    OLAPStatus add_columns(const vectorized::Chunk& chunk, const std::vector<uint32_t>& column_indexes,
-                           bool is_key) override;
-    OLAPStatus add_columns_with_rssid(const vectorized::Chunk& chunk, const std::vector<uint32_t>& column_indexes,
-                                      const std::vector<uint32_t>& rssid) override;
+    Status add_columns(const vectorized::Chunk& chunk, const std::vector<uint32_t>& column_indexes,
+                       bool is_key) override;
 
-    OLAPStatus flush_columns() override;
-    OLAPStatus final_flush() override;
+    Status add_columns_with_rssid(const vectorized::Chunk& chunk, const std::vector<uint32_t>& column_indexes,
+                                  const std::vector<uint32_t>& rssid) override;
+
+    Status flush_columns() override;
+
+    Status final_flush() override;
 
 private:
-    std::unique_ptr<SegmentWriter> _create_segment_writer(const std::vector<uint32_t>& column_indexes, bool is_key);
+    StatusOr<std::unique_ptr<SegmentWriter>> _create_segment_writer(const std::vector<uint32_t>& column_indexes,
+                                                                    bool is_key);
 
-    OLAPStatus _flush_columns(std::unique_ptr<SegmentWriter>* segment_writer);
+    Status _flush_columns(std::unique_ptr<SegmentWriter>* segment_writer);
 
     std::vector<std::unique_ptr<SegmentWriter>> _segment_writers;
     size_t _current_writer_index = 0;

be/src/storage/rowset/rowset_factory.cpp
@@ -80,7 +80,7 @@ Status RowsetFactory::create_rowset_writer(const RowsetWriterContext& context, s
         DCHECK(context.writer_type == kVertical);
         *output = std::make_unique<VerticalBetaRowsetWriter>(context);
     }
-    return (*output)->init() == OLAP_SUCCESS ? Status::OK() : Status::InternalError("fail to init rowset writer");
+    return (*output)->init();
 }
 
 } // namespace starrocks

be/src/storage/rowset/rowset_writer.h
@@ -22,6 +22,7 @@
 #ifndef STARROCKS_BE_SRC_OLAP_ROWSET_ROWSET_WRITER_H
 #define STARROCKS_BE_SRC_OLAP_ROWSET_ROWSET_WRITER_H
 
+#include "common/statusor.h"
 #include "gen_cpp/types.pb.h"
 #include "gutil/macros.h"
 #include "runtime/global_dicts.h"
@@ -30,9 +31,6 @@
 #include "storage/rowset/rowset_writer_context.h"
 
 namespace starrocks {
-
-struct ContiguousRow;
-class RowCursor;
 
 namespace vectorized {
 class Chunk;
@@ -76,57 +74,59 @@ public:
     RowsetWriter() = default;
     virtual ~RowsetWriter() = default;
 
-    virtual OLAPStatus init() = 0;
+    RowsetWriter(const RowsetWriter&) = delete;
+    const RowsetWriter& operator=(const RowsetWriter&) = delete;
 
-    virtual OLAPStatus add_chunk(const vectorized::Chunk& chunk) { return OLAP_ERR_FUNC_NOT_IMPLEMENTED; }
+    virtual Status init() = 0;
+
+    virtual Status add_chunk(const vectorized::Chunk& chunk) { return Status::NotSupported("RowsetWriter::add_chunk"); }
 
     // Used for updatable tablet compaction (BetaRowsetWriter), need to write src rssid with segment
-    virtual OLAPStatus add_chunk_with_rssid(const vectorized::Chunk& chunk, const vector<uint32_t>& rssid) {
-        return OLAP_ERR_FUNC_NOT_IMPLEMENTED;
+    virtual Status add_chunk_with_rssid(const vectorized::Chunk& chunk, const vector<uint32_t>& rssid) {
+        return Status::NotSupported("RowsetWriter::add_chunk_with_rssid");
     }
 
     // Used for vertical compaction
     // |Chunk| contains partial columns data corresponding to |column_indexes|.
-    virtual OLAPStatus add_columns(const vectorized::Chunk& chunk, const std::vector<uint32_t>& column_indexes,
-                                   bool is_key) {
-        return OLAP_ERR_FUNC_NOT_IMPLEMENTED;
+    virtual Status add_columns(const vectorized::Chunk& chunk, const std::vector<uint32_t>& column_indexes,
+                               bool is_key) {
+        return Status::NotSupported("RowsetWriter::add_columns");
     }
 
-    virtual OLAPStatus add_columns_with_rssid(const vectorized::Chunk& chunk,
-                                              const std::vector<uint32_t>& column_indexes,
-                                              const std::vector<uint32_t>& rssid) {
-        return OLAP_ERR_FUNC_NOT_IMPLEMENTED;
+    virtual Status add_columns_with_rssid(const vectorized::Chunk& chunk, const std::vector<uint32_t>& column_indexes,
+                                          const std::vector<uint32_t>& rssid) {
+        return Status::NotSupported("RowsetWriter::add_columns_with_rssid");
     }
 
-    // This routine is free to modify the content of |chunk|.
-    virtual OLAPStatus flush_chunk(const vectorized::Chunk& chunk) { return OLAP_ERR_FUNC_NOT_IMPLEMENTED; }
+    virtual Status flush_chunk(const vectorized::Chunk& chunk) {
+        return Status::NotSupported("RowsetWriter::flush_chunk");
+    }
 
-    virtual OLAPStatus flush_chunk_with_deletes(const vectorized::Chunk& upserts, const vectorized::Column& deletes) {
-        return OLAP_ERR_FUNC_NOT_IMPLEMENTED;
+    virtual Status flush_chunk_with_deletes(const vectorized::Chunk& upserts, const vectorized::Column& deletes) {
+        return Status::NotSupported("RowsetWriter::flush_chunk_with_deletes");
     }
 
     // Precondition: the input `rowset` should have the same type of the rowset we're building
-    virtual OLAPStatus add_rowset(RowsetSharedPtr rowset) { return OLAP_ERR_FUNC_NOT_IMPLEMENTED; }
+    virtual Status add_rowset(RowsetSharedPtr rowset) { return Status::NotSupported("RowsetWriter::add_rowset"); }
 
     // Precondition: the input `rowset` should have the same type of the rowset we're building
-    virtual OLAPStatus add_rowset_for_linked_schema_change(RowsetSharedPtr rowset,
-                                                           const SchemaMapping& schema_mapping) {
-        return OLAP_ERR_FUNC_NOT_IMPLEMENTED;
+    virtual Status add_rowset_for_linked_schema_change(RowsetSharedPtr rowset, const SchemaMapping& schema_mapping) {
+        return Status::NotSupported("RowsetWriter::add_rowset_for_linked_schema_change");
     }
 
     // explicit flush all buffered rows into segment file.
-    // note that `add_row` could also trigger flush when certain conditions are met
-    virtual OLAPStatus flush() { return OLAP_ERR_FUNC_NOT_IMPLEMENTED; }
+    virtual Status flush() { return Status::NotSupported("RowsetWriter::flush"); }
 
     // Used for vertical compaction
     // flush columns data and index
-    virtual OLAPStatus flush_columns() { return OLAP_ERR_FUNC_NOT_IMPLEMENTED; }
+    virtual Status flush_columns() { return Status::NotSupported("RowsetWriter::flush_columns"); }
+
     // flush segments footer
-    virtual OLAPStatus final_flush() { return OLAP_ERR_FUNC_NOT_IMPLEMENTED; }
+    virtual Status final_flush() { return Status::NotSupported("RowsetWriter::final_flush"); }
 
     // finish building and return pointer to the built rowset (guaranteed to be inited).
     // return nullptr when failed
-    virtual RowsetSharedPtr build() = 0;
+    virtual StatusOr<RowsetSharedPtr> build() = 0;
 
     virtual Version version() = 0;
 
@@ -142,10 +142,6 @@ public:
 
 protected:
     vectorized::DictColumnsValidMap _global_dict_columns_valid_info;
-
-private:
-    RowsetWriter(const RowsetWriter&) = delete;
-    const RowsetWriter& operator=(const RowsetWriter&) = delete;
 };
 
 } // namespace starrocks

be/src/storage/rowset/vectorized/rowset_writer_adapter.cpp
@@ -18,52 +18,47 @@ RowsetWriterAdapter::RowsetWriterAdapter(const RowsetWriterContext& context) {
     _in_schema = context.tablet_schema->convert_to_format(context.memory_format_version);
     _out_schema = context.tablet_schema->convert_to_format(context.storage_format_version);
     if (_in_schema == nullptr || _out_schema == nullptr) {
-        _status = OLAP_ERR_INVALID_SCHEMA;
+        _status = Status::InvalidArgument("invalid schema");
     } else {
         RowsetWriterContext rowset_context = context;
         rowset_context.memory_format_version = rowset_context.storage_format_version;
-        _status = RowsetFactory::create_rowset_writer(rowset_context, &_writer).ok() ? OLAP_SUCCESS
-                                                                                     : OLAP_ERR_OTHER_ERROR;
+        _status = RowsetFactory::create_rowset_writer(rowset_context, &_writer);
     }
 }
 
-OLAPStatus RowsetWriterAdapter::init() {
+Status RowsetWriterAdapter::init() {
     return _status;
 }
 
-OLAPStatus RowsetWriterAdapter::add_chunk(const vectorized::Chunk& chunk) {
+Status RowsetWriterAdapter::add_chunk(const vectorized::Chunk& chunk) {
     if (_chunk_converter == nullptr) {
-        RETURN_NOT_OK(_init_chunk_converter());
+        RETURN_IF_ERROR(_init_chunk_converter());
     }
     return _writer->add_chunk(*_chunk_converter->copy_convert(chunk));
 }
 
-OLAPStatus RowsetWriterAdapter::flush_chunk(const vectorized::Chunk& chunk) {
+Status RowsetWriterAdapter::flush_chunk(const vectorized::Chunk& chunk) {
     if (_chunk_converter == nullptr) {
-        RETURN_NOT_OK(_init_chunk_converter());
+        RETURN_IF_ERROR(_init_chunk_converter());
     }
     return _writer->flush_chunk(*_chunk_converter->copy_convert(chunk));
 }
 
-OLAPStatus RowsetWriterAdapter::_init_chunk_converter() {
+Status RowsetWriterAdapter::_init_chunk_converter() {
     _chunk_converter = std::make_unique<ChunkConverter>();
-    auto st =
-            _chunk_converter->init(ChunkHelper::convert_schema(*_in_schema), ChunkHelper::convert_schema(*_out_schema));
-    if (!st.ok()) {
-        _chunk_converter.reset();
-        return OLAP_ERR_INVALID_SCHEMA;
-    }
+    RETURN_IF_ERROR(_chunk_converter->init(ChunkHelper::convert_schema(*_in_schema),
+                                           ChunkHelper::convert_schema(*_out_schema)));
     // |_in_schema| and |_out_schema| can be freed now.
     _in_schema.reset();
     _out_schema.reset();
-    return OLAP_SUCCESS;
+    return Status::OK();
 }
 
-OLAPStatus RowsetWriterAdapter::flush_chunk_with_deletes(const vectorized::Chunk& upserts,
-                                                         const vectorized::Column& deletes) {
+Status RowsetWriterAdapter::flush_chunk_with_deletes(const vectorized::Chunk& upserts,
+                                                     const vectorized::Column& deletes) {
     LOG(WARNING) << "updatable tablet should not use RowsetWriterAdapter";
     if (_chunk_converter == nullptr) {
-        RETURN_NOT_OK(_init_chunk_converter());
+        RETURN_IF_ERROR(_init_chunk_converter());
     }
     return _writer->flush_chunk_with_deletes(*_chunk_converter->copy_convert(upserts), deletes);
 }

commit 64ae8571cd33742c4bda00d471050f5ed76be2bd

Fix the bug of create inital rowset (#7553) (#7573)
introduced by the commit: https://github.com/StarRocks/starrocks/commit/60aa03ac4bf489d79804a99b2c6c4b1d1d592ec0

new_rowset will always be null when st is not ok

be/src/storage/tablet_manager.cpp
@@ -1116,9 +1116,10 @@ Status TabletManager::_create_inital_rowset_unlocked(const TCreateTabletReq& req
                 LOG(WARNING) << "failed to flush rowset writer for tablet " << tablet->full_name() << ": " << st;
                 break;
             }
-            auto new_rowset = rowset_writer->build();
-            if (!new_rowset.ok()) return new_rowset.status();
-            st = tablet->add_rowset(*new_rowset, false);
+            auto ret = rowset_writer->build();
+            if (!ret.ok()) return ret.status();
+            new_rowset = std::move(ret.value());
+            st = tablet->add_rowset(new_rowset, false);
             if (!st.ok()) {
                 LOG(WARNING) << "failed to add rowset for tablet " << tablet->full_name() << ": " << st;
                 break;

be/src/storage/tablet_manager.h
@@ -186,7 +186,7 @@ private:
 
     Status _update_tablet_map_and_partition_info(const TabletSharedPtr& tablet);
 
-    Status _create_inital_rowset_unlocked(const TCreateTabletReq& request, Tablet* tablet);
+    static Status _create_inital_rowset_unlocked(const TCreateTabletReq& request, Tablet* tablet);
 
     Status _drop_tablet_directly_unlocked(TTabletId tablet_id, TabletDropFlag flag);
 