a66ada931b00bd8cbcf608efcacc17ebadc606ce
Crystal-LiuJing <33692385+Crystal-LiuJing@users.noreply.github.com>
2022-06-22 11:20:16 +0800

    [MaterializedView] modify ShowMaterializedViewStmt for MaterializedView (#6889)

--- fe/fe-core/src/main/java/com/starrocks/analysis/ShowMaterializedViewStmt.java
+++ fe/fe-core/src/main/java/com/starrocks/analysis/ShowMaterializedViewStmt.java
@@ -21,11 +21,16 @@
 
 package com.starrocks.analysis;
 
+import com.google.common.base.Strings;
 import com.starrocks.catalog.Column;
+import com.starrocks.catalog.InfoSchemaDb;
 import com.starrocks.catalog.ScalarType;
 import com.starrocks.common.AnalysisException;
 import com.starrocks.qe.ShowResultSetMetaData;
 import com.starrocks.sql.ast.AstVisitor;
+import com.starrocks.sql.ast.QueryStatement;
+import com.starrocks.sql.ast.SelectRelation;
+import com.starrocks.sql.ast.TableRelation;
 
 // Show rollup statement, used to show rollup information of one table.
 //
@@ -41,10 +46,30 @@ public class ShowMaterializedViewStmt extends ShowStmt {
                     .addColumn(new Column("rows", ScalarType.createVarchar(50)))
                     .build();
 
+    private static final TableName TABLE_NAME = new TableName(InfoSchemaDb.DATABASE_NAME, "materialized_views");
+
     private String db;
+
+    private final String pattern;
+
+    private Expr where;
 
     public ShowMaterializedViewStmt(String db) {
         this.db = db;
+        this.pattern = null;
+        this.where = null;
+    }
+
+    public ShowMaterializedViewStmt(String db, String pattern) {
+        this.db = db;
+        this.pattern = pattern;
+        this.where = null;
+    }
+
+    public ShowMaterializedViewStmt(String db, Expr where) {
+        this.db = db;
+        this.pattern = null;
+        this.where = where;
     }
 
     public String getDb() {
@@ -55,14 +80,57 @@ public class ShowMaterializedViewStmt extends ShowStmt {
         this.db = db;
     }
 
+    public String getPattern() {
+        return pattern;
+    }
+
     @Override
     public void analyze(Analyzer analyzer) throws AnalysisException {
     }
 
     @Override
+    public QueryStatement toSelectStmt() throws AnalysisException {
+        if (where == null) {
+            return null;
+        }
+        // Columns
+        SelectList selectList = new SelectList();
+        ExprSubstitutionMap aliasMap = new ExprSubstitutionMap(false);
+        // id
+        SelectListItem item = new SelectListItem(new SlotRef(TABLE_NAME, "MATERIALIZED_VIEW_ID"), "id");
+        selectList.addItem(item);
+        aliasMap.put(new SlotRef(null, "id"), item.getExpr().clone(null));
+        // name
+        item = new SelectListItem(new SlotRef(TABLE_NAME, "TABLE_NAME"), "name");
+        selectList.addItem(item);
+        aliasMap.put(new SlotRef(null, "name"), item.getExpr().clone(null));
+        // database_name
+        item = new SelectListItem(new SlotRef(TABLE_NAME, "TABLE_SCHEMA"), "database_name");
+        selectList.addItem(item);
+        aliasMap.put(new SlotRef(null, "database_name"), item.getExpr().clone(null));
+        // text
+        item = new SelectListItem(new SlotRef(TABLE_NAME, "MATERIALIZED_VIEW_DEFINITION"), "text");
+        selectList.addItem(item);
+        aliasMap.put(new SlotRef(null, "text"), item.getExpr().clone(null));
+        // rows
+        item = new SelectListItem(new SlotRef(TABLE_NAME, "TABLE_ROWS"), "rows");
+        selectList.addItem(item);
+        aliasMap.put(new SlotRef(null, "rows"), item.getExpr().clone(null));
+        where = where.substitute(aliasMap);
+        return new QueryStatement(new SelectRelation(selectList, new TableRelation(TABLE_NAME),
+                where, null, null));
+    }
+
+    @Override
     public String toSql() {
         StringBuilder sb = new StringBuilder();
-        sb.append("SHOW MATERIALIZED VIEW FROM ").append(db);
+        sb.append("SHOW MATERIALIZED VIEW");
+        if (!Strings.isNullOrEmpty(db)) {
+            sb.append(" FROM ").append(db);
+        }
+        if (pattern != null) {
+            sb.append(" LIKE '").append(pattern).append("'");
+        }
         return sb.toString();
     }
 
