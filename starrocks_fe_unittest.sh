6f22c69b0decc0af882cdcd56ddb0103cfba3d44
Stephen <dulong41@gmail.com>
2022-06-22 12:12:06 +0800

    Update starrocks_fe_unittest.sh

--- starrocks_fe_unittest.sh
+++ starrocks_fe_unittest.sh
@@ -62,7 +62,7 @@ echo "run docker for script"
 
 cmd="cd /root/starrocks;
 export FE_UT_PARALLEL=16;
-timeout 3600 sh run-fe-ut.sh --run com.starrocks.utframe.Demo#testCreateDbAndTable+test2"
+timeout 3600 sh run-fe-ut.sh --run com.starrocks.utframe.Demo"
 
 docker exec --privileged $container_name /bin/bash -c "$cmd"
 
