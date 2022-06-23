5d455143084175dfe1e2d57dc329da6da423ca52
stdpain <34912776+stdpain@users.noreply.github.com>
2022-06-23 14:25:43 +0800

    [cherry-pick][branch-2.1][Bugfix] keep the aggregate expr order when rewrite aggregate operator (#7657)
    we should keep the aggregate expr order when rewrite, because we will use
    singleDistinctFunctionPos to decide call update or merge

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rewrite/AddDecodeNodeForDictStringRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rewrite/AddDecodeNodeForDictStringRule.java
@@ -476,7 +476,8 @@ public class AddDecodeNodeForDictStringRule implements PhysicalOperatorTreeRewri
                                                                  DecodeContext context) {
             Map<Integer, Integer> newStringToDicts = Maps.newHashMap();
 
-            Map<ColumnRefOperator, CallOperator> newAggMap = Maps.newHashMap(aggOperator.getAggregations());
+            final List<Map.Entry<ColumnRefOperator, CallOperator>> newAggMapEntry = Lists.newArrayList();
+
             for (Map.Entry<ColumnRefOperator, CallOperator> kv : aggOperator.getAggregations().entrySet()) {
                 boolean canApplyDictDecodeOpt = (kv.getValue().getUsedColumns().cardinality() > 0) &&
                         (PhysicalHashAggregateOperator.couldApplyLowCardAggregateFunction.contains(
@@ -495,7 +496,7 @@ public class AddDecodeNodeForDictStringRule implements PhysicalOperatorTreeRewri
                                 Collections.singletonList(dictColumn), newFunction,
                                 oldCall.isDistinct());
                         ColumnRefOperator outputColumn = kv.getKey();
-                        newAggMap.put(outputColumn, newCall);
+                        newAggMapEntry.add(Maps.immutableEntry(outputColumn, newCall));
                     } else if (context.stringColumnIdToDictColumnIds.containsKey(columnId)) {
                         Integer dictColumnId = context.stringColumnIdToDictColumnIds.get(columnId);
                         ColumnRefOperator dictColumn = context.columnRefFactory.getColumnRef(dictColumnId);
@@ -526,7 +527,6 @@ public class AddDecodeNodeForDictStringRule implements PhysicalOperatorTreeRewri
                             ColumnRefOperator outputStringColumn = kv.getKey();
                             final ColumnRefOperator newDictColumn = context.columnRefFactory.create(
                                     dictColumn.getName(), ID_TYPE, dictColumn.isNullable());
-                            newAggMap.remove(outputStringColumn);
                             newStringToDicts.put(outputStringColumn.getId(), newDictColumn.getId());
 
                             for (Pair<Integer, ColumnDict> globalDict : context.globalDicts) {
@@ -545,10 +545,15 @@ public class AddDecodeNodeForDictStringRule implements PhysicalOperatorTreeRewri
                                 newArguments, newFunction,
                                 oldCall.isDistinct());
 
-                        newAggMap.put(outputColumn, newCall);
+                        newAggMapEntry.add(Maps.immutableEntry(outputColumn, newCall));
+                    } else {
+                        newAggMapEntry.add(kv);
                     }
+                } else {
+                    newAggMapEntry.add(kv);
                 }
             }
+            Map<ColumnRefOperator, CallOperator> newAggMap = ImmutableMap.copyOf(newAggMapEntry);
 
             List<ColumnRefOperator> newGroupBys = Lists.newArrayList();
             for (ColumnRefOperator groupBy : aggOperator.getGroupBys()) {
