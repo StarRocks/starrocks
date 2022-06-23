fbe16d8c298c5321769db775f23b2bb7a6ca8618
mofei <mofei@starrocks.com>
2022-06-23 08:03:25 +0800

    support json functions in loading

--- fe/fe-core/src/main/java/com/starrocks/analysis/DataDescription.java
+++ fe/fe-core/src/main/java/com/starrocks/analysis/DataDescription.java
@@ -89,7 +89,13 @@ public class DataDescription {
             FunctionSet.REPLACE_VALUE,
             FunctionSet.NOW,
             FunctionSet.HLL_HASH,
-            FunctionSet.SUBSTITUTE);
+            FunctionSet.SUBSTITUTE,
+            "get_json_string",
+            "get_json_double",
+            "get_json_int",
+            FunctionSet.PARSE_JSON,
+            FunctionSet.JSON_QUERY
+            );
 
     private final String tableName;
     private final PartitionNames partitionNames;
@@ -415,6 +421,16 @@ public class DataDescription {
             validateNowFunction(mappingColumn);
         } else if (functionName.equalsIgnoreCase(FunctionSet.SUBSTITUTE)) {
             validateSubstituteFunction(args, columnNameMap);
+        } else if (functionName.equalsIgnoreCase("get_json_string")) {
+            validateGetJsonFunction(args, columnNameMap);
+        } else if (functionName.equalsIgnoreCase("get_json_double")) {
+            validateGetJsonFunction(args, columnNameMap);
+        } else if (functionName.equalsIgnoreCase("get_json_int")) {
+            validateGetJsonFunction(args, columnNameMap);
+        } else if (functionName.equalsIgnoreCase(FunctionSet.PARSE_JSON)) {
+            validateParseJsonFunction(args, columnNameMap);
+        } else if (functionName.equalsIgnoreCase(FunctionSet.JSON_QUERY)) {
+            validateJsonQueryFunction(args, columnNameMap);
         } else {
             if (isHadoopLoad) {
                 throw new AnalysisException("Unknown function: " + functionName);
@@ -428,6 +444,54 @@ public class DataDescription {
             throws AnalysisException {
         if (args.size() != 1) {
             throw new AnalysisException("Should has only one argument: " + args);
+        }
+
+        String argColumn = args.get(0);
+        if (!columnNameMap.containsKey(argColumn)) {
+            throw new AnalysisException("Column is not in sources, column: " + argColumn);
+        }
+
+        args.set(0, columnNameMap.get(argColumn));
+    }
+
+    // eg: k2 = get_json_string(k1, "$.id")
+    // this is used for creating derivative column from existing column
+    private static void validateGetJsonFunction(List<String> args, Map<String, String> columnNameMap)
+            throws AnalysisException {
+        if (args.size() != 2) {
+            throw new AnalysisException("Should has only two arguments: " + args);
+        }
+
+        String argColumn = args.get(0);
+        if (!columnNameMap.containsKey(argColumn)) {
+            throw new AnalysisException("Column is not in sources, column: " + argColumn);
+        }
+
+        args.set(0, columnNameMap.get(argColumn));
+    }
+
+    // eg: k2 = parse_json(k1)
+    // this is used for creating derivative column from existing column
+    private static void validateParseJsonFunction(List<String> args, Map<String, String> columnNameMap)
+            throws AnalysisException {
+        if (args.size() != 1) {
+            throw new AnalysisException("Should has only one argument: " + args);
+        }
+
+        String argColumn = args.get(0);
+        if (!columnNameMap.containsKey(argColumn)) {
+            throw new AnalysisException("Column is not in sources, column: " + argColumn);
+        }
+
+        args.set(0, columnNameMap.get(argColumn));
+    }
+
+    // eg: k2 = parse_json(k1)
+    // this is used for creating derivative column from existing column
+    private static void validateJsonQueryFunction(List<String> args, Map<String, String> columnNameMap)
+            throws AnalysisException {
+        if (args.size() != 2) {
+            throw new AnalysisException("Should has only two arguments: " + args);
         }
 
         String argColumn = args.get(0);
