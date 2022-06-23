c3b962f593d240135d537e8a8b6090d60e988a3b
zombee0 <flylucas_10@163.com>
2022-06-23 23:28:43 +0800

    Refactor ddl executor (#7725)
    Signed-off-by: zombee0 <flylucas_10@163.com>

--- fe/fe-core/src/main/java/com/starrocks/execution/DataDefinitionExecutorFactory.java
+++ fe/fe-core/src/main/java/com/starrocks/execution/DataDefinitionExecutorFactory.java
@@ -3,18 +3,32 @@
 package com.starrocks.execution;
 
 import com.google.common.collect.ImmutableMap;
+import com.starrocks.analysis.AlterRoutineLoadStmt;
+import com.starrocks.analysis.AlterTableStmt;
+import com.starrocks.analysis.AlterViewStmt;
+import com.starrocks.analysis.CancelAlterTableStmt;
+import com.starrocks.analysis.CancelLoadStmt;
 import com.starrocks.analysis.CreateDbStmt;
 import com.starrocks.analysis.CreateFunctionStmt;
+import com.starrocks.analysis.CreateMaterializedViewStmt;
+import com.starrocks.analysis.CreateRoutineLoadStmt;
 import com.starrocks.analysis.CreateTableLikeStmt;
 import com.starrocks.analysis.CreateTableStmt;
 import com.starrocks.analysis.DdlStmt;
 import com.starrocks.analysis.DropDbStmt;
 import com.starrocks.analysis.DropFunctionStmt;
+import com.starrocks.analysis.DropMaterializedViewStmt;
 import com.starrocks.analysis.DropTableStmt;
+import com.starrocks.analysis.LoadStmt;
+import com.starrocks.analysis.PauseRoutineLoadStmt;
+import com.starrocks.analysis.ResumeRoutineLoadStmt;
 import com.starrocks.analysis.StatementBase;
+import com.starrocks.analysis.StopRoutineLoadStmt;
 import com.starrocks.qe.ConnectContext;
 import com.starrocks.qe.DdlExecutor;
 import com.starrocks.qe.ShowResultSet;
+import com.starrocks.sql.ast.AlterMaterializedViewStatement;
+import com.starrocks.sql.ast.CreateMaterializedViewStatement;
 
 public class DataDefinitionExecutorFactory {
     private static final ImmutableMap<Class<? extends StatementBase>, DataDefinitionExecutor> executorMap =
@@ -26,6 +40,20 @@ public class DataDefinitionExecutorFactory {
                     .put(CreateTableStmt.class, new CreateTableExecutor())
                     .put(CreateTableLikeStmt.class, new CreateTableLikeExecutor())
                     .put(DropTableStmt.class, new DropTableExecutor())
+                    .put(CreateMaterializedViewStmt.class, new CreateMaterializedViewExecutor())
+                    .put(CreateMaterializedViewStatement.class, new CreateMaterializedViewStatementExecutor())
+                    .put(DropMaterializedViewStmt.class, new DropMaterializedViewExecutor())
+                    .put(AlterMaterializedViewStatement.class, new AlterMaterializedViewExecutor())
+                    .put(AlterTableStmt.class, new AlterTableExecutor())
+                    .put(AlterViewStmt.class, new AlterViewExecutor())
+                    .put(CancelAlterTableStmt.class, new CancelAlterTableExecutor())
+                    .put(LoadStmt.class, new LoadExecutor())
+                    .put(CancelLoadStmt.class, new CancelLoadExecutor())
+                    .put(CreateRoutineLoadStmt.class, new CreateRoutineLoadExecutor())
+                    .put(PauseRoutineLoadStmt.class, new PauseRoutineLoadExecutor())
+                    .put(ResumeRoutineLoadStmt.class, new ResumeRoutineLoadExecutor())
+                    .put(StopRoutineLoadStmt.class, new StopRoutineLoadExecutor())
+                    .put(AlterRoutineLoadStmt.class, new AlterRoutineLoadExecutor())
                     .build();
 
     public static ShowResultSet execute(StatementBase stmt, ConnectContext context) throws Exception {
