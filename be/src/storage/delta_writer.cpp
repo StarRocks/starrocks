f35f17cfa1a5389309ad5dd54e8b7b4adc0cb2d0
trueeyu <lxhhust350@qq.com>
2022-06-24 11:40:53 +0800

    [BugFix] fix the bug of get_tablet with schema hash (#7736)
    our usage covert `schema hash` to `include_deleted`
    
    which was introduced the the pr: https://github.com/StarRocks/starrocks/commit/3fd83add0a3a399d1a8235d7268d896d8c149516

--- be/src/storage/delta_writer.cpp
+++ be/src/storage/delta_writer.cpp
@@ -118,7 +118,7 @@ Status DeltaWriter::_init() {
         TabletSharedPtr new_tablet;
         if (!_tablet->is_migrating()) {
             // maybe migration just finish, get the tablet again
-            new_tablet = tablet_mgr->get_tablet(_opt.tablet_id, _opt.schema_hash);
+            new_tablet = tablet_mgr->get_tablet(_opt.tablet_id);
             if (new_tablet == nullptr) {
                 _set_state(kAborted);
                 return Status::NotFound(fmt::format("Not found tablet. tablet_id: {}", _opt.tablet_id));
