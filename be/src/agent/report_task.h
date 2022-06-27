ed225e366b1366b5f0b48e65d3174c996d163b68
Alex Zhu <zhuming9011@gmail.com>
2022-06-27 19:58:32 +0800

    [Refactor] Make TaskWorkerPool does not rely on AgentServer (#7891)

---
+++ be/src/agent/report_task.h
@@ -0,0 +1,14 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
+
+#pragma once
+
+#include "agent/status.h"
+
+namespace starrocks {
+
+class TReportRequest;
+class TMasterResult;
+
+AgentStatus report_task(const TReportRequest& request, TMasterResult* result);
+
+} // namespace starrocks
