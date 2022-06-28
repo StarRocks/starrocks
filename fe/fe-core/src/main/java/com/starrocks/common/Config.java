aabb610e87c8878d0f46ada8a99eda037afa2da3
adzfolc <multigraph@krutt.org>
2022-06-28 10:37:47 +0800

    1. modify refresh partition to parallel 2. modify hive_partition_refresh_concurrency to System's core num

--- fe/fe-core/src/main/java/com/starrocks/common/Config.java
+++ fe/fe-core/src/main/java/com/starrocks/common/Config.java
@@ -1394,7 +1394,7 @@ public class Config extends ConfigBase {
      * num of thread to refresh hive external table partition info
      */
     @ConfField
-    public static int hive_partition_refresh_concurrency = 50;
+    public static int hive_partition_refresh_concurrency = Runtime.getRuntime().availableProcessors();
 
     @ConfField
     public static long hive_meta_cache_refresh_interval_s = 3600L * 2L;
