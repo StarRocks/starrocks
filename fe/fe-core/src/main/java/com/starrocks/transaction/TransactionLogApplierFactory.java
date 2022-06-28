83451d0bf3011554ae48b2639fb60a8a2da174e3
blackstar-baba <535650957@qq.com>
2022-06-28 09:37:15 +0800

    fix review

--- fe/fe-core/src/main/java/com/starrocks/transaction/TransactionLogApplierFactory.java
+++ fe/fe-core/src/main/java/com/starrocks/transaction/TransactionLogApplierFactory.java
@@ -11,7 +11,7 @@ public class TransactionLogApplierFactory {
         if (table.isLakeTable()) {
             return new LakeTableTxnLogApplier((LakeTable) table);
         }
-        if (table.isOlapTable() || table.isMaterializedView()) {
+        if (table.isNativeTable()) {
             return new OlapTableTxnLogApplier((OlapTable) table);
         }
         return null;
