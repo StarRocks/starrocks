355d49895e94ed15f44d2d365312dd5e75e224d8
Binglin Chang <decstery@gmail.com>
2022-06-29 15:48:06 +0800

    [Bugfix] Ignore EAGAIN error for futex wait (#7779) (#8018)

--- be/src/storage/rowset/bitmap_index_reader.cpp
+++ be/src/storage/rowset/bitmap_index_reader.cpp
@@ -54,7 +54,7 @@ StatusOr<bool> BitmapIndexReader::load(fs::BlockManager* fs, const std::string& 
         }
         if (curr_state == kLoading) {
             int r = bthread::futex_wait_private(&_state, curr_state, nullptr);
-            PLOG_IF(ERROR, r != 0) << " bthread::futex_wait_private";
+            PLOG_IF(ERROR, r != 0 && errno != EAGAIN) << " bthread::futex_wait_private";
         }
     }
 }
