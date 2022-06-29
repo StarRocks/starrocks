StarRocks/starrocks

commit 83d72d6216d8cf8e5528cc7ff7fe1250461f04bb

[BugFix] JournalCursor retry on bdb exception (#7455)
This PR improves the exception handling mechanism for the BDBJournalCursor class, so that corner cases such as read failure can be handled and retried properly.

fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
@@ -29,7 +29,6 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Queues;
 import com.google.common.collect.Range;
-import com.sleepycat.je.rep.InsufficientLogException;
 import com.starrocks.alter.Alter;
 import com.starrocks.alter.AlterJob;
 import com.starrocks.alter.AlterJob.JobType;
@@ -159,8 +158,9 @@ import com.starrocks.ha.HAProtocol;
 import com.starrocks.ha.MasterInfo;
 import com.starrocks.journal.JournalCursor;
 import com.starrocks.journal.JournalEntity;
+import com.starrocks.journal.JournalException;
+import com.starrocks.journal.JournalInconsistentException;
 import com.starrocks.journal.JournalWriter;
-import com.starrocks.journal.bdbje.BDBJEJournal;
 import com.starrocks.journal.bdbje.Timestamp;
 import com.starrocks.load.DeleteHandler;
 import com.starrocks.load.ExportChecker;
@@ -1489,12 +1489,6 @@ public class GlobalStateMgr {
                 try {
                     hasLog = replayJournal(-1);
                     metaReplayState.setOk();
-                } catch (InsufficientLogException insufficientLogEx) {
-                    // for InsufficientLogException we should refresh the log and
-                    // then exit the process because we may have read dirty data.
-                    LOG.error("catch insufficient log exception. please restart", insufficientLogEx);
-                    ((BDBJEJournal) editLog.getJournal()).getBdbEnvironment().refreshLog(insufficientLogEx);
-                    System.exit(-1);
                 } catch (Throwable e) {
                     LOG.error("replayer thread catch an exception when replay journal.", e);
                     metaReplayState.setException(e);
@@ -1681,19 +1675,32 @@ public class GlobalStateMgr {
         }
 
         LOG.info("replayed journal id is {}, replay to journal id is {}", replayedJournalId, newToJournalId);
-        JournalCursor cursor = editLog.read(replayedJournalId.get() + 1, newToJournalId);
-        if (cursor == null) {
-            LOG.warn("failed to get cursor from {} to {}", replayedJournalId.get() + 1, newToJournalId);
+        JournalCursor cursor = null;
+        try {
+            cursor = editLog.read(replayedJournalId.get() + 1, newToJournalId);
+        } catch (JournalException e) {
+            LOG.warn("failed to get cursor from {} to {}", replayedJournalId.get() + 1, newToJournalId, e);
             return false;
         }
 
         long startTime = System.currentTimeMillis();
         boolean hasLog = false;
         while (true) {
-            JournalEntity entity = cursor.next();
+            JournalEntity entity = null;
+            try {
+                entity = cursor.next();
+            } catch (InterruptedException | JournalException | JournalInconsistentException e) {
+                LOG.warn("got exception when get next, will exit, ", e);
+                // TODO exit gracefully
+                Util.stdoutWithTime(e.getMessage());
+                System.exit(-1);
+            }
+
+            // EOF or aggressive retry
             if (entity == null) {
                 break;
             }
+
             hasLog = true;
             EditLog.loadJournal(this, entity);
             replayedJournalId.incrementAndGet();

fe/fe-core/src/main/java/com/starrocks/journal/Journal.java
@@ -45,7 +45,7 @@ public interface Journal {
 
     // Get all the journals whose id: fromKey <= id <= toKey
     // toKey = -1 means toKey = Long.Max_Value
-    public JournalCursor read(long fromKey, long toKey);
+    public JournalCursor read(long fromKey, long toKey) throws JournalException;
 
     // Write a journal and sync to disk
     // Only keep this method for test, will remove in next PR

fe/fe-core/src/main/java/com/starrocks/journal/JournalCursor.java
@@ -20,8 +20,11 @@ package com.starrocks.journal;
 // This class is like JDBC ResultSet.
 public interface JournalCursor {
 
-    // Return the next journal. return null when there is no more journals
-    public JournalEntity next();
+    // Return the next journal.
+    // Return null when there is no more journals, or if need to retry from outside.
+    // Raise a JournalException if there is an error in reading from the underlying storage.
+    // Raise a JournalInconsistentException if read dirty data and need to exit
+    public JournalEntity next() throws InterruptedException, JournalException, JournalInconsistentException;
 
     public void close();
 

fe/fe-core/src/main/java/com/starrocks/journal/JournalInconsistentException.java
@@ -1,0 +1,12 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
+
+package com.starrocks.journal;
+
+/**
+ * if inconsistent issue occurs when handling journal, this exception is called, indicating it's time to quit the process
+ */
+public class JournalInconsistentException extends Exception {
+    public JournalInconsistentException(String errMsg)  {
+        super(errMsg);
+    }
+}

fe/fe-core/src/main/java/com/starrocks/journal/bdbje/BDBJEJournal.java
@@ -220,7 +220,7 @@ public class BDBJEJournal implements Journal {
     }
 
     @Override
-    public JournalCursor read(long fromKey, long toKey) {
+    public JournalCursor read(long fromKey, long toKey) throws JournalException {
         return BDBJournalCursor.getJournalCursor(bdbEnvironment, fromKey, toKey);
     }
 

fe/fe-core/src/main/java/com/starrocks/journal/bdbje/BDBJournalCursor.java
@@ -23,19 +23,27 @@ package com.starrocks.journal.bdbje;
 
 import com.sleepycat.bind.tuple.TupleBinding;
 import com.sleepycat.je.DatabaseEntry;
+import com.sleepycat.je.DatabaseException;
 import com.sleepycat.je.LockMode;
 import com.sleepycat.je.OperationStatus;
+import com.sleepycat.je.rep.InsufficientLogException;
+import com.sleepycat.je.rep.RestartRequiredException;
 import com.starrocks.journal.JournalCursor;
 import com.starrocks.journal.JournalEntity;
+import com.starrocks.journal.JournalException;
+import com.starrocks.journal.JournalInconsistentException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
+import java.io.IOException;
 import java.util.List;
 
 public class BDBJournalCursor implements JournalCursor {
     private static final Logger LOG = LogManager.getLogger(JournalCursor.class);
+    private static final int RETRY_TIME = 3;
+    private static final long SLEEP_INTERVAL_SEC = 3;
 
     private long toKey;
     private long currentKey;
@@ -43,29 +51,22 @@ public class BDBJournalCursor implements JournalCursor {
     private List<Long> dbNames;
     private CloseSafeDatabase database;
     private int nextDbPositionIndex;
-    private final int maxTryTime = 3;
 
-    public static BDBJournalCursor getJournalCursor(BDBEnvironment env, long fromKey, long toKey) {
+    public static BDBJournalCursor getJournalCursor(BDBEnvironment env, long fromKey, long toKey) throws
+            JournalException {
         if (toKey < fromKey || fromKey < 0) {
-            LOG.error("Invalid key range!");
-            return null;
+            throw new JournalException(String.format("Invalid key range! fromKey %s toKey %s", fromKey, toKey));
         }
-        BDBJournalCursor cursor = null;
-        try {
-            cursor = new BDBJournalCursor(env, fromKey, toKey);
-        } catch (Exception e) {
-            LOG.error("new BDBJournalCursor error.", e);
-        }
-        return cursor;
+        return new BDBJournalCursor(env, fromKey, toKey);
     }
 
-    private BDBJournalCursor(BDBEnvironment env, long fromKey, long toKey) throws Exception {
+    protected BDBJournalCursor(BDBEnvironment env, long fromKey, long toKey) throws JournalException {
         this.environment = env;
         this.toKey = toKey;
         this.currentKey = fromKey;
         this.dbNames = env.getDatabaseNames();
         if (dbNames == null) {
-            throw new NullPointerException("dbNames is null.");
+            throw new JournalException("failed to get db names!");
         }
         this.nextDbPositionIndex = 0;
 
@@ -79,54 +80,112 @@ public class BDBJournalCursor implements JournalCursor {
                 break;
             }
         }
+        nextDbPositionIndex -= 1;
 
         if (dbName == null) {
-            LOG.error("Can not find the key:{}, fail to get journal cursor. will exit.", fromKey);
-            System.exit(-1);
+            throw new JournalException(String.format("Can not find the key:%d, fail to get journal cursor!", fromKey));
         }
-        this.database = env.openDatabase(dbName);
+    }
+
+    private boolean shouldOpenDatabase() {
+        // the very first time
+        if (database == null) {
+            return true;
+        }
+        // if current db does not contain any more data, then we go to search the next db
+        return nextDbPositionIndex < dbNames.size() && currentKey == dbNames.get(nextDbPositionIndex);
+    }
+
+    protected void openDatabaseIfNecessary() throws InterruptedException, JournalException, JournalInconsistentException {
+        if (!shouldOpenDatabase()) {
+            return;
+        }
+
+        String dbName = Long.toString(dbNames.get(nextDbPositionIndex));
+        JournalException exception = null;
+        for (int i = 0; i < RETRY_TIME; ++ i) {
+            try {
+                if (i != 0) {
+                    Thread.sleep(SLEEP_INTERVAL_SEC * 1000);
+                }
+
+                database = environment.openDatabase(dbName);
+                nextDbPositionIndex++;
+                return;
+            } catch (RestartRequiredException e) {
+                String errMsg = String.format(
+                        "failed to open database because of RestartRequiredException, will exit. db[%s]", database);
+                LOG.warn(errMsg, e);
+                if (e instanceof InsufficientLogException) {
+                    // for InsufficientLogException we should refresh the log and
+                    // then exit the process because we may have read dirty data.
+                    environment.refreshLog((InsufficientLogException) e);
+                }
+                JournalInconsistentException journalInconsistentException = new JournalInconsistentException(errMsg);
+                journalInconsistentException.initCause(e);
+                throw journalInconsistentException;
+            } catch (DatabaseException e) {
+                String errMsg = String.format("failed to open %s for %s times!", dbName, i + 1);
+                LOG.warn(errMsg);
+                exception = new JournalException(errMsg);
+                exception.initCause(e);
+            }
+        }
+
+        // failed after retry
+        throw exception;
+    }
+
+    protected JournalEntity deserializeData(DatabaseEntry data) throws JournalException {
+        DataInputStream in = new DataInputStream(new ByteArrayInputStream(data.getData()));
+        JournalEntity ret = new JournalEntity();
+        try {
+            ret.readFields(in);
+        } catch (IOException e) {
+            // bad data, will not retry
+            String errMsg = String.format("fail to read journal entity key=%s, data=%s",
+                    currentKey, data);
+            LOG.error(errMsg, e);
+            JournalException exception = new JournalException(errMsg);
+            exception.initCause(e);
+            throw exception;
+        }
+        return ret;
     }
 
     @Override
-    public JournalEntity next() {
-        JournalEntity ret = null;
+    public JournalEntity next() throws InterruptedException, JournalException, JournalInconsistentException {
+        // EOF
         if (currentKey > toKey) {
-            return ret;
+            return null;
         }
+
+        // if current db does not contain any more data, then we go to search the next db
+        openDatabaseIfNecessary();
+
+        // make the key
         Long key = currentKey;
         DatabaseEntry theKey = new DatabaseEntry();
         TupleBinding<Long> myBinding = TupleBinding.getPrimitiveBinding(Long.class);
         myBinding.objectToEntry(key, theKey);
 
         DatabaseEntry theData = new DatabaseEntry();
-        // if current db does not contain any more data, then we go to search the next db
-        try {
-            int tryTimes = 0;
-            while (true) {
-                // null means perform the operation without transaction protection.
-                // READ_COMMITTED guarantees no dirty read.
+        JournalException exception = null;
+        for (int i = 0; i < RETRY_TIME; i++) {
+            // 1. sleep after retry
+            if (i != 0) {
+                Thread.sleep(SLEEP_INTERVAL_SEC * 1000);
+            }
+
+            // 2. read from bdb & error handling
+            try {
                 OperationStatus operationStatus = database.get(null, theKey, theData, LockMode.READ_COMMITTED);
+
                 if (operationStatus == OperationStatus.SUCCESS) {
-                    // Recreate the data String.
-                    byte[] retData = theData.getData();
-                    DataInputStream in = new DataInputStream(new ByteArrayInputStream(retData));
-                    ret = new JournalEntity();
-                    try {
-                        ret.readFields(in);
-                    } catch (Exception e) {
-                        LOG.error("fail to read journal entity key={}, will exit", currentKey, e);
-                        System.exit(-1);
-                    }
+                    // 3. serialized
+                    JournalEntity entity = deserializeData(theData);
                     currentKey++;
-                    return ret;
-                } else if (nextDbPositionIndex < dbNames.size() && currentKey == dbNames.get(nextDbPositionIndex)) {
-                    database = environment.openDatabase(dbNames.get(nextDbPositionIndex).toString());
-                    nextDbPositionIndex++;
-                    tryTimes = 0;
-                } else if (tryTimes < maxTryTime) {
-                    tryTimes++;
-                    LOG.warn("fail to get journal {}, will try again. status: {}", currentKey, operationStatus);
-                    Thread.sleep(3000);
+                    return entity;
                 } else if (operationStatus == OperationStatus.NOTFOUND) {
                     // In the case:
                     // On non-master FE, the replayer will first get the max journal id,
@@ -137,21 +196,43 @@ public class BDBJournalCursor implements JournalCursor {
                     // is hanging and waiting to be aborted after timeout). and after this log abort,
                     // we will get NOTFOUND.
                     // So we simply throw a exception and let the replayer get the max id again.
-                    throw new Exception(
-                            "Failed to find key " + currentKey + " in database " + database.getDb().getDatabaseName());
+                    LOG.warn("canot find journal {} in db {}, maybe because master switched, will try again.",
+                            key, database);
+                    return null;
                 } else {
-                    LOG.error("fail to get journal {}, status: {}, will exit", currentKey, operationStatus);
-                    System.exit(-1);
+                    // other error status, will record error message and retry
+                    String errMsg = String.format("failed to read after retried %d times! key = %d, db = %s, status = %s",
+                            i + 1, key, database, operationStatus);
+                    LOG.warn(errMsg);
+                    exception = new JournalException(errMsg);
                 }
+            } catch (RestartRequiredException e) {
+                String errMsg = String.format(
+                        "failed to read next because of RestartRequiredException, will exit. db[%s], current key[%s]",
+                        database, theKey);
+                LOG.warn(errMsg, e);
+                if (e instanceof InsufficientLogException) {
+                    // for InsufficientLogException we should refresh the log and
+                    // then exit the process because we may have read dirty data.
+                    environment.refreshLog((InsufficientLogException) e);
+                }
+                JournalInconsistentException journalInconsistentException = new JournalInconsistentException(errMsg);
+                journalInconsistentException.initCause(e);
+                throw journalInconsistentException;
+            } catch (DatabaseException e) {
+                String errMsg = String.format("failed to read after retried %d times! key = %d, db = %s",
+                        i + 1, key, database);
+                LOG.error(errMsg, e);
+                exception = new JournalException(errMsg);
+                exception.initCause(e);
             }
-        } catch (Exception e) {
-            LOG.warn("Catch an exception when get next JournalEntity. key:{}", currentKey, e);
-            return null;
-        }
+        } // for i in retry
+
+        // failed after retry
+        throw exception;
     }
 
     @Override
     public void close() {
-
     }
 }

fe/fe-core/src/main/java/com/starrocks/persist/EditLog.java
@@ -50,6 +50,7 @@ import com.starrocks.ha.MasterInfo;
 import com.starrocks.journal.Journal;
 import com.starrocks.journal.JournalCursor;
 import com.starrocks.journal.JournalEntity;
+import com.starrocks.journal.JournalException;
 import com.starrocks.journal.JournalFactory;
 import com.starrocks.journal.JournalTask;
 import com.starrocks.journal.bdbje.Timestamp;
@@ -118,7 +119,7 @@ public class EditLog {
         return journal.getMaxJournalId();
     }
 
-    public JournalCursor read(long fromId, long toId) {
+    public JournalCursor read(long fromId, long toId) throws JournalException {
         return journal.read(fromId, toId);
     }
 

commit 197cb8defc5e2a0e70408a84a277466b5051eaad

[BugFix] raise exception and retry on read fails (#7995)
This bug was introduced by #7455, which adds a retry policy in the inner loop when read fails, but forgets to raise to the outer loop after all attempts fail.

fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
@@ -1680,7 +1680,7 @@ public class GlobalStateMgr {
         listener.setMetaContext(metaContext);
     }
 
-    public synchronized boolean replayJournal(long toJournalId) {
+    public synchronized boolean replayJournal(long toJournalId) throws JournalException {
         long newToJournalId = toJournalId;
         if (newToJournalId == -1) {
             newToJournalId = getMaxJournalId();
@@ -1704,8 +1704,8 @@ public class GlobalStateMgr {
             JournalEntity entity = null;
             try {
                 entity = cursor.next();
-            } catch (InterruptedException | JournalException | JournalInconsistentException e) {
-                LOG.warn("got exception when get next, will exit, ", e);
+            } catch (InterruptedException | JournalInconsistentException e) {
+                LOG.warn("got interrupt exception or inconsistent exception when get next, will exit, ", e);
                 // TODO exit gracefully
                 Util.stdoutWithTime(e.getMessage());
                 System.exit(-1);