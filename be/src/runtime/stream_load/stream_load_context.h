d4e687f3973be5fd36498c7e5386be9cf378fb31
ricky <rickif@qq.com>
2022-06-29 21:44:45 +0800

    fix: constant overflow

--- be/src/runtime/stream_load/stream_load_context.h
+++ be/src/runtime/stream_load/stream_load_context.h
@@ -200,7 +200,7 @@ public:
     // buffer for reading data from ev_buffer
     static constexpr size_t kDefaultBufferSize = 64 * 1024;
     // max buffer size for JSON format is 4GB.
-    static constexpr int64_t kJSONMaxBufferSize = 4 * 1024 * 1024 * 1024;
+    static constexpr int64_t kJSONMaxBufferSize = 4294967296;
     ByteBufferPtr buffer = nullptr;
 
 public:
