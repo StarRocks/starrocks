b57c7802b54dea69658dfa85c1b07dacb6d48786
HangyuanLiu <460660596@qq.com>
2022-07-01 15:02:22 +0800

    Add InsertTxnCommitAttachment to support version rollback

---
+++ fe/fe-core/src/main/java/com/starrocks/transaction/InsertTxnCommitAttachment.java
@@ -0,0 +1,34 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
+package com.starrocks.transaction;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class InsertTxnCommitAttachment extends TxnCommitAttachment {
+    private long loadedRows;
+
+    public InsertTxnCommitAttachment() {
+        super(TransactionState.LoadJobSourceType.INSERT_STREAMING);
+    }
+
+    public InsertTxnCommitAttachment(long loadedRows) {
+        super(TransactionState.LoadJobSourceType.INSERT_STREAMING);
+        this.loadedRows = loadedRows;
+    }
+
+    public long getLoadedRows() {
+        return loadedRows;
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        super.write(out);
+        out.writeLong(loadedRows);
+    }
+
+    public void readFields(DataInput in) throws IOException {
+        super.readFields(in);
+        loadedRows = in.readLong();
+    }
+}
