2f2b1f084486f082f736ddf74da7e4647806fd5f
Stephen <dulong41@gmail.com>
2022-07-01 13:43:15 +0800

    Update starrocks_fe_unittest.sh

--- starrocks_fe_unittest.sh
+++ starrocks_fe_unittest.sh
@@ -77,21 +77,21 @@ docker exec --privileged $container_name /bin/bash -c "$cmd"
 
 echo "script run over-----"
 
-sudo chmod 755 $ROOT/starrocks/*.jar
-if [ "$GITHUB_PR_TARGET_BRANCH" == "main" ];then
-    cd $ROOT/starrocks/fe/fe-core/target
-    #jacoco_result="jacoco_${GITHUB_PR_NUMBER}.exec"
-    mv jacoco.exec $jacoco_result || true
-    java -jar $ROOT/starrocks/jacococli.jar report ./$jacoco_result --classfiles ./classes/ --html ./result --sourcefiles $ROOT/starrocks/fe/fe-core/src/main/java/ --encoding utf-8 --name fe-coverage
-    ls -al $ROOT/starrocks/fe/fe-core/target
-    time_count=0
-    pull_status=1
-    while (( $pull_status != 0 ));do
-        if (( $time_count == 3 ));then
-            exit 1
-        fi
-        timeout 180 java -jar $ROOT/starrocks/cover-checker-console-1.4.0-jar-with-dependencies.jar --cover $ROOT/starrocks/fe/fe-core/target/result/ --github-token 66e4c48809eb7e058eb73668b8c816867e6d7cbe  --repo StarRocks/starrocks --threshold 80 --github-url api.github.com  --pr ${GITHUB_PR_NUMBER} -type jacoco
-        pull_status=$?
-        time_count=`expr $time_count + 1`
-    done
-fi
+#sudo chmod 755 $ROOT/starrocks/*.jar
+#if [ "$GITHUB_PR_TARGET_BRANCH" == "main" ];then
+#    cd $ROOT/starrocks/fe/fe-core/target
+#    #jacoco_result="jacoco_${GITHUB_PR_NUMBER}.exec"
+#    mv jacoco.exec $jacoco_result || true
+#    java -jar $ROOT/starrocks/jacococli.jar report ./$jacoco_result --classfiles ./classes/ --html ./result --sourcefiles $ROOT/starrocks/fe/fe-core/src/main/java/ --encoding utf-8 --name fe-coverage
+#   ls -al $ROOT/starrocks/fe/fe-core/target
+#    time_count=0
+#    pull_status=1
+#    while (( $pull_status != 0 ));do
+#        if (( $time_count == 3 ));then
+#            exit 1
+#        fi
+#        timeout 180 java -jar $ROOT/starrocks/cover-checker-console-1.4.0-jar-with-dependencies.jar --cover $ROOT/starrocks/fe/fe-core/target/result/ --github-token 66e4c48809eb7e058eb73668b8c816867e6d7cbe  --repo StarRocks/starrocks --threshold 80 --github-url api.github.com  --pr ${GITHUB_PR_NUMBER} -type jacoco
+#        pull_status=$?
+#        time_count=`expr $time_count + 1`
+#    done
+#fi
