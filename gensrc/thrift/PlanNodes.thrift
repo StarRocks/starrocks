dd19b4f11bb600d03148466ca051ff92c7f8d9b1
wyb <wybb86@gmail.com>
2022-07-01 10:59:46 +0800

    Fix compile

--- gensrc/thrift/PlanNodes.thrift
+++ gensrc/thrift/PlanNodes.thrift
@@ -318,6 +318,7 @@ struct TOlapScanNode {
   23: optional map<i32, i32> dict_string_id_to_int_ids
   // which columns only be used to filter data in the stage of scan data
   24: optional list<string> unused_output_column_name
+  25: optional bool is_lake_table
 }
 
 struct TJDBCScanNode {
