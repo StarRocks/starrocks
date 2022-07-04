03680c15d9c54fcff5dd85c9f064e803929a208b
HangyuanLiu <460660596@qq.com>
2022-07-04 19:56:36 +0800

    InsertTxnCommitAttachment read and write meta use GSON (#8210)

--- fe/fe-core/src/main/java/com/starrocks/transaction/InsertTxnCommitAttachment.java
+++ fe/fe-core/src/main/java/com/starrocks/transaction/InsertTxnCommitAttachment.java
@@ -1,11 +1,16 @@
 // This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
 package com.starrocks.transaction;
 
+import com.google.gson.annotations.SerializedName;
+import com.starrocks.common.io.Text;
+import com.starrocks.persist.gson.GsonUtils;
+
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
 public class InsertTxnCommitAttachment extends TxnCommitAttachment {
+    @SerializedName("loadedRows")
     private long loadedRows;
 
     public InsertTxnCommitAttachment() {
@@ -24,11 +29,15 @@ public class InsertTxnCommitAttachment extends TxnCommitAttachment {
     @Override
     public void write(DataOutput out) throws IOException {
         super.write(out);
-        out.writeLong(loadedRows);
+        String s = GsonUtils.GSON.toJson(this);
+        Text.writeString(out, s);
     }
 
     public void readFields(DataInput in) throws IOException {
         super.readFields(in);
-        loadedRows = in.readLong();
+        String s = Text.readString(in);
+        InsertTxnCommitAttachment insertTxnCommitAttachment =
+                GsonUtils.GSON.fromJson(s, InsertTxnCommitAttachment.class);
+        this.loadedRows = insertTxnCommitAttachment.getLoadedRows();
     }
 }
