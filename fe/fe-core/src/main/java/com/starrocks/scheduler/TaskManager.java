c7d68718eac4e7965f2d0b8eb68f687df6835c5e
Astralidea <astralidea@163.com>
2022-07-04 18:47:30 +0800

    Opt.

--- fe/fe-core/src/main/java/com/starrocks/scheduler/TaskManager.java
+++ fe/fe-core/src/main/java/com/starrocks/scheduler/TaskManager.java
@@ -26,7 +26,6 @@ import com.starrocks.server.GlobalStateMgr;
 import com.starrocks.sql.ast.SubmitTaskStmt;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.spark_project.guava.collect.MinMaxPriorityQueue;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
