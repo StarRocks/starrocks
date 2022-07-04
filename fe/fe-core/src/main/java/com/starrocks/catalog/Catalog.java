c2888f98fe2b8ab3c7be165d0b29027fe5d31c37
gengjun-git <gengjun@starrocks.com>
2022-07-04 14:07:47 +0800

    [BugFix] Fix bug of master exit wrong when adding follower #8130
    Set the BDB-JE electable group size to (the_total_number_of_followers - the_number_of_adding_followers). After the follower is joined successfully, reset the electable group size to the_total_number_of_followers.

--- fe/fe-core/src/main/java/com/starrocks/catalog/Catalog.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/Catalog.java
@@ -2548,6 +2548,7 @@ public class Catalog {
             if (role == FrontendNodeType.FOLLOWER || role == FrontendNodeType.REPLICA) {
                 bdbha.addHelperSocket(host, editLogPort);
                 helperNodes.add(Pair.create(host, editLogPort));
+                bdbha.addUnstableNode(host, getFollowerCnt());
             }
 
             // In some cases, for example, fe starts with the outdated meta, the node name that has been dropped
@@ -2584,6 +2585,9 @@ public class Catalog {
             if (fe.getRole() == FrontendNodeType.FOLLOWER || fe.getRole() == FrontendNodeType.REPLICA) {
                 haProtocol.removeElectableNode(fe.getNodeName());
                 helperNodes.remove(Pair.create(host, port));
+
+                BDBHA ha = (BDBHA) haProtocol;
+                ha.removeUnstableNode(host, getFollowerCnt());
             }
             editLog.logRemoveFrontend(fe);
         } finally {
@@ -2616,6 +2620,16 @@ public class Catalog {
             }
         }
         return null;
+    }
+
+    public int getFollowerCnt() {
+        int cnt = 0;
+        for (Frontend fe : frontends.values()) {
+            if (fe.getRole() == FrontendNodeType.FOLLOWER) {
+                cnt++;
+            }
+        }
+        return cnt;
     }
 
     // The interface which DdlExecutor needs.
