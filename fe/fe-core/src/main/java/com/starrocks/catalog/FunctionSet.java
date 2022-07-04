2174d0d0c627cbd7032734ea739b35b1e06ad81f
Ruby <33128462+rubiesvelt@users.noreply.github.com>
2022-07-04 16:12:40 +0800

    [Feature] add percentile_cont agg function with unit test (#7737)
    add percentile_cont agg function with unit test
    https://github.com/StarRocks/starrocks/issues/6922
    
    performance test
    num of rows —— time cost
    100000 —— 0.02 sec
    200000 —— 0.02 sec
    200000 —— 0.03 sec
    1000000 —— 0.06 sec
    10000000 —— 0.10 sec
    211819093 —— 16.83 sec
    742991422 —— 1 min 14.80 sec
    1106646823 —— 2 min 55.81 sec
    
    (cherry picked from commit da3a96e5486ed920c226e80c76f15a551bc74883)
    
    fixup

--- fe/fe-core/src/main/java/com/starrocks/catalog/FunctionSet.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/FunctionSet.java
@@ -76,6 +76,7 @@ public class FunctionSet {
     public static final String PERCENTILE_APPROX = "percentile_approx";
     public static final String PERCENTILE_APPROX_RAW = "percentile_approx_raw";
     public static final String PERCENTILE_UNION = "percentile_union";
+    public static final String PERCENTILE_CONT = "percentile_cont";
     public static final String BITMAP_UNION = "bitmap_union";
     public static final String BITMAP_UNION_COUNT = "bitmap_union_count";
     public static final String BITMAP_UNION_INT = "bitmap_union_int";
@@ -707,6 +708,17 @@ public class FunctionSet {
 
         addBuiltin(AggregateFunction.createBuiltin(RETENTION, Lists.newArrayList(Type.ARRAY_BOOLEAN),
                 Type.ARRAY_BOOLEAN, Type.BIGINT, false, false, false));
+
+        // PercentileCont
+        addBuiltin(AggregateFunction.createBuiltin(FunctionSet.PERCENTILE_CONT,
+                Lists.newArrayList(Type.DATE, Type.DOUBLE), Type.DATE, Type.VARCHAR,
+                false, false, false));
+        addBuiltin(AggregateFunction.createBuiltin(FunctionSet.PERCENTILE_CONT,
+                Lists.newArrayList(Type.DATETIME, Type.DOUBLE), Type.DATETIME, Type.VARCHAR,
+                false, false, false));
+        addBuiltin(AggregateFunction.createBuiltin(FunctionSet.PERCENTILE_CONT,
+                Lists.newArrayList(Type.DOUBLE, Type.DOUBLE), Type.DOUBLE, Type.VARCHAR,
+                false, false, false));
 
         // Avg
         // TODO: switch to CHAR(sizeof(AvgIntermediateType) when that becomes available
