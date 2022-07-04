a8bfd17dc32c6eae3e98dae5b4186453c199021a
zombee0 <flylucas_10@163.com>
2022-07-04 23:03:57 +0800

    antlr4 for recoverDb (#8240)
    Signed-off-by: zombee0 <flylucas_10@163.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/ast/AstVisitor.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/ast/AstVisitor.java
@@ -59,6 +59,7 @@ import com.starrocks.analysis.ModifyBackendAddressClause;
 import com.starrocks.analysis.ModifyFrontendAddressClause;
 import com.starrocks.analysis.OrderByElement;
 import com.starrocks.analysis.ParseNode;
+import com.starrocks.analysis.RecoverDbStmt;
 import com.starrocks.analysis.SetStmt;
 import com.starrocks.analysis.ShowColumnStmt;
 import com.starrocks.analysis.ShowCreateDbStmt;
@@ -299,8 +300,11 @@ public abstract class AstVisitor<R, C> {
         return visitStatement(statement, context);
     }
 
-
     public R visitAlterDatabaseRename(AlterDatabaseRename statement, C context) {
+        return visitStatement(statement, context);
+    }
+
+    public R visitRecoverDbStmt(RecoverDbStmt statement, C context) {
         return visitStatement(statement, context);
     }
 
