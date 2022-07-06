e6d58c822cb3a5af716c0e7d10bed8667c7c6529
liuyehcf <1559500551@qq.com>
2022-07-06 10:52:31 +0800

    update

--- fe/fe-core/src/main/java/com/starrocks/analysis/AnalyticWindow.java
+++ fe/fe-core/src/main/java/com/starrocks/analysis/AnalyticWindow.java
@@ -383,7 +383,7 @@ public class AnalyticWindow implements ParseNode {
 
     @Override
     public int hashCode() {
-        return Objects.hash(type_, leftBoundary_, rightBoundary_, toSqlString_);
+        return Objects.hash(type_, leftBoundary_, rightBoundary_);
     }
 
     @Override
