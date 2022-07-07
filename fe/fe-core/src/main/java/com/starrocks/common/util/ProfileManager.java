11d13d848e325261491db358719e1b4602157dfb
Murphy <96611012+mofeiatwork@users.noreply.github.com>
2022-07-07 12:59:45 +0800

    [Enhance] add session variables in profile (backport #8156) (#8365)
    * [Enhance] add session variables in profile (#8156)
    
    (cherry picked from commit 7590d50b5af77897da95235840ca9b265c7646ef)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/common/util/ProfileManager.java
    
    * add variables in profile

--- fe/fe-core/src/main/java/com/starrocks/common/util/ProfileManager.java
+++ fe/fe-core/src/main/java/com/starrocks/common/util/ProfileManager.java
@@ -63,6 +63,7 @@ public class ProfileManager {
     public static final String DEFAULT_DB = "Default Db";
     public static final String QUERY_CPU_COST = "QueryCpuCost";
     public static final String QUERY_MEM_COST = "QueryMemCost";
+    public static final String VARIABLES = "Variables";
 
     public static final ArrayList<String> PROFILE_HEADERS = new ArrayList(
             Arrays.asList(QUERY_ID, USER, DEFAULT_DB, SQL_STATEMENT, QUERY_TYPE,
