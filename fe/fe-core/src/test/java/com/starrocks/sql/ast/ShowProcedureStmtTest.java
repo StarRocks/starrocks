241ee6d3e5063cc32d536df0391ac9baa9b105fe
fgump <smartlxh@gmail.com>
2022-07-07 22:29:21 +0800

    SR SQL Planner Summer Camp #7975 (#8348)
    migrate the ShowProcedureStmt to the new Parser/Analyzer framework

---
+++ fe/fe-core/src/test/java/com/starrocks/sql/ast/ShowProcedureStmtTest.java
@@ -0,0 +1,43 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
+
+package com.starrocks.sql.ast;
+
+import com.starrocks.analysis.ShowDbStmt;
+import com.starrocks.qe.ConnectContext;
+import com.starrocks.qe.ShowExecutor;
+import com.starrocks.qe.ShowResultSet;
+import com.starrocks.qe.ShowResultSetMetaData;
+import mockit.Mocked;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ShowProcedureStmtTest {
+    @Mocked
+    private ConnectContext ctx;
+
+    @Test
+    public void testNormal() throws Exception {
+
+        ShowProcedureStmt stmt = (ShowProcedureStmt) com.starrocks.sql.parser.SqlParser.parse(
+                "SHOW PROCEDURE STATUS",32).get(0);
+        com.starrocks.sql.analyzer.Analyzer.analyze(stmt, ctx);
+        Assert.assertEquals("SHOW PROCEDURE STATUS", stmt.toString());
+
+        stmt = (ShowProcedureStmt) com.starrocks.sql.parser.SqlParser.parse(
+                "SHOW PROCEDURE STATUS LIKE 'abc'",32).get(0);
+        com.starrocks.sql.analyzer.Analyzer.analyze(stmt, ctx);
+        Assert.assertEquals("SHOW PROCEDURE STATUS LIKE 'abc'", stmt.toString());
+
+        stmt = (ShowProcedureStmt) com.starrocks.sql.parser.SqlParser.parse(
+                "SHOW PROCEDURE STATUS where name='abc'",32).get(0);
+        com.starrocks.sql.analyzer.Analyzer.analyze(stmt, ctx);
+        Assert.assertNotNull( stmt.toString());
+
+        stmt = new ShowProcedureStmt("abc");
+        Assert.assertNotNull(stmt.getPattern());
+        Assert.assertEquals("SHOW PROCEDURE STATUS LIKE 'abc'", stmt.toString());
+        Assert.assertEquals(11, stmt.getMetaData().getColumnCount());
+        Assert.assertEquals("Db", stmt.getMetaData().getColumn(0).getName());
+
+    }
+}
