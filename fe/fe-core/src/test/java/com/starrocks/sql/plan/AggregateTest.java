775971a088e8408fe8d46360a7e9d86ef2128575
pslydhh <luouyuyou@gmail.com>
2022-07-08 08:58:33 +0800

    Add ut for invalid window

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/AggregateTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/AggregateTest.java
@@ -820,15 +820,11 @@ public class AggregateTest extends PlanTestBase {
         plan = getFragmentPlan(sql);
         assertContains(plan, "window_funnel(1800, 11: L_SHIPDATE, 3, 18: expr)");
 
+        expectedException.expect(SemanticException.class);
+        expectedException.expectMessage("window argument must >= 0");
         sql =
                 "select L_ORDERKEY,window_funnel(-1, L_SHIPDATE, 3, [L_PARTKEY = 1]) from lineitem_partition_colocate group by L_ORDERKEY;";
-	try {
-            plan = getFragmentPlan(sql);
-        } catch (SemanticException e) {
-            Assert.assertEquals(
-                    "window argument must >= 0",
-                    e.getMessage());
-	}
+        plan = getFragmentPlan(sql);
 
         FeConstants.runningUnitTest = false;
     }
