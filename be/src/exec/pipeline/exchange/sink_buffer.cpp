76a3bbb105e51fb26a08824ec4edd2b72fd82cb0
zihe.liu <ziheliu1024@gmail.com>
2022-07-08 10:26:55 +0800

    [Bugfix] use correct mem_tracker for exchange/result sink (#8402)

--- be/src/exec/pipeline/exchange/sink_buffer.cpp
+++ be/src/exec/pipeline/exchange/sink_buffer.cpp
@@ -283,7 +283,7 @@ void SinkBuffer::_try_to_send_rpc(const TUniqueId& instance_id, std::function<vo
         }
 
         auto* closure = new DisposableClosure<PTransmitChunkResult, ClosureContext>(
-                {instance_id, request.params->sequence(), GetCurrentTimeNanos()});
+                {instance_id, request.params->sequence(), GetCurrentTimeNanos()}, _mem_tracker);
 
         closure->addFailedHandler([this](const ClosureContext& ctx) noexcept {
             _is_finishing = true;
