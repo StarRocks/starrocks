62963c890afa1b76946e9569c9f1221ed5195756
yanz <dirtysalt1987@gmail.com>
2022-07-08 11:30:17 +0800

    fix ut

--- be/test/exec/pipeline/pipeline_file_scan_node_test.cpp
+++ be/test/exec/pipeline/pipeline_file_scan_node_test.cpp
@@ -278,7 +278,7 @@ void PipeLineFileScanNodeTest::generate_morse_queue(std::vector<starrocks::vecto
     for (auto& i : scan_nodes) {
         ScanNode* scan_node = (ScanNode*)(i);
         auto morsel_queue_factory = scan_node->convert_scan_range_to_morsel_queue_factory(
-                scan_ranges, no_scan_ranges_per_driver_seq, scan_node->id(), request, degree_of_parallelism);
+                scan_ranges, no_scan_ranges_per_driver_seq, scan_node->id(), degree_of_parallelism, true);
         DCHECK(morsel_queue_factory.ok());
         morsel_queue_factories.emplace(scan_node->id(), std::move(morsel_queue_factory).value());
     }
