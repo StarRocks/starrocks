9048b150400df6ee2c07ae2fd133811c326dbde7
Napoleon <Linkerist@163.com>
2022-07-11 18:55:32 +0800

    [Enhancement] to get row num and data size together in order to faster, avoid a lock/duplicate code (#8357)

--- be/src/storage/tablet_updates.h
+++ be/src/storage/tablet_updates.h
@@ -66,6 +66,9 @@ public:
     // get total size of latest version's rowset files
     size_t data_size() const;
 
+    // get number of rows and total size of latest version's rowset files together
+    std::pair<int64_t, int64_t> num_rows_and_data_size() const;
+
     // get latest version's number of rowsets
     size_t num_rowsets() const;
 
