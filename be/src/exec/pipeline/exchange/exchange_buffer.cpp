2a441c2772ed380b1e6d3fcd9bbb33015695822d
silverbullet233 <3675229+silverbullet233@users.noreply.github.com>
2022-07-12 18:57:24 +0800

    add license

--- be/src/exec/pipeline/exchange/exchange_buffer.cpp
+++ be/src/exec/pipeline/exchange/exchange_buffer.cpp
@@ -1,3 +1,5 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
+
 #include "exec/pipeline/exchange/exchange_buffer.h"
 
 #include "brpc/periodic_task.h"
