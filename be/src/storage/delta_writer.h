c2f42eeff2456822f850c9a999f1010cea23131e
trueeyu <lxhhust350@qq.com>
2022-07-14 15:43:31 +0800

    remove unused code

--- be/src/storage/delta_writer.h
+++ be/src/storage/delta_writer.h
@@ -24,16 +24,12 @@ namespace vectorized {
 class MemTable;
 class MemTableSink;
 
-enum WriteType { LOAD = 1, LOAD_DELETE = 2, DELETE = 3 };
-
 struct DeltaWriterOptions {
     int64_t tablet_id;
     int32_t schema_hash;
-    WriteType write_type;
     int64_t txn_id;
     int64_t partition_id;
     PUniqueId load_id;
-    TupleDescriptor* tuple_desc;
     // slots are in order of tablet's schema
     const std::vector<SlotDescriptor*>* slots;
     vectorized::GlobalDictByNameMaps* global_dicts = nullptr;
@@ -106,7 +102,6 @@ private:
     DeltaWriter(const DeltaWriterOptions& opt, MemTracker* parent, StorageEngine* storage_engine);
 
     Status _init();
-    Status _prepare();
     Status _flush_memtable_async();
     Status _flush_memtable();
     const char* _state_name(State state) const;
