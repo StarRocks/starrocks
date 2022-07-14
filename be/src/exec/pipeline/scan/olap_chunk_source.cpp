43290de111be136b7afda56f2e2a2fbe492de3ba
Murphy <96611012+mofeiatwork@users.noreply.github.com>
2022-07-14 23:04:00 +0800

    [Enhance] use max chunk rows to estimate chunk memory (#8706)

--- be/src/exec/pipeline/scan/olap_chunk_source.cpp
+++ be/src/exec/pipeline/scan/olap_chunk_source.cpp
@@ -462,8 +462,9 @@ void OlapChunkSource::_update_realtime_counter(vectorized::Chunk* chunk) {
     // Update local counters.
     _local_sum_row_bytes += chunk->memory_usage();
     _local_num_rows += chunk->num_rows();
+    _local_max_chunk_rows = std::max(_local_max_chunk_rows, chunk->num_rows());
     if (_local_sum_chunks++ % UPDATE_AVG_ROW_BYTES_FREQUENCY == 0) {
-        _buffer_limiter->update_avg_row_bytes(_local_sum_row_bytes, _local_num_rows);
+        _buffer_limiter->update_avg_row_bytes(_local_sum_row_bytes, _local_num_rows, _local_max_chunk_rows);
         _local_sum_row_bytes = 0;
         _local_num_rows = 0;
     }
