370b5e9e5dd244e97bd2702f590036e914f8f076
adzfolc <multigraph@krutt.org>
2022-07-16 00:04:35 +0800

    1. add UT for `create external table like` DDL

---
+++ fe/fe-core/src/test/java/com/starrocks/analysis/CreateExternalTableLikeTest.java
@@ -0,0 +1,33 @@
+package com.starrocks.analysis;
+
+import com.starrocks.qe.ConnectContext;
+import com.starrocks.sql.analyzer.AnalyzeTestUtil;
+import com.starrocks.utframe.StarRocksAssert;
+import com.starrocks.utframe.UtFrameUtils;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.validation.constraints.AssertTrue;
+
+public class CreateExternalTableLikeTest {
+    private static StarRocksAssert starRocksAssert;
+    private static ConnectContext ctx;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        UtFrameUtils.createMinStarRocksCluster();
+        AnalyzeTestUtil.init();
+        starRocksAssert = new StarRocksAssert();
+        starRocksAssert.withDatabase("db1").useDatabase("tbl1");
+        ctx = new ConnectContext(null);
+        ctx.setGlobalStateMgr(AccessTestUtil.fetchAdminCatalog());
+    }
+
+    @Test
+    public void testCreateTableLike() {
+        String sql_1 = "CREATE EXTERNAL TABLE hive_test_like LIKE hive_test";
+        com.starrocks.analysis.StatementBase statementBase = AnalyzeTestUtil.analyzeSuccess(sql_1);
+        Assert.assertTrue(statementBase instanceof CreateTableLikeStmt);
+    }
+}
