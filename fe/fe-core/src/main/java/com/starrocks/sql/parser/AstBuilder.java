5d699092b4d2f560e31c6a75e10600d9fd3aba75
melt_code <1350722344@qq.com>
2022-07-17 04:03:31 +0800

    merge

--- fe/fe-core/src/main/java/com/starrocks/sql/parser/AstBuilder.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/parser/AstBuilder.java
@@ -122,7 +122,6 @@ import com.starrocks.analysis.ShowDeleteStmt;
 import com.starrocks.analysis.ShowIndexStmt;
 import com.starrocks.analysis.ShowMaterializedViewStmt;
 import com.starrocks.analysis.ShowProcStmt;
-import com.starrocks.analysis.ShowStatusStmt;
 import com.starrocks.analysis.ShowRestoreStmt;
 import com.starrocks.analysis.ShowProcStmt;
 import com.starrocks.analysis.ShowStatusStmt;
