04f0701692d5904f69936ab6a13c846478917712
padmejin <89557328+padmejin@users.noreply.github.com>
2022-07-18 21:21:39 +0800

    [BugFix] set bdb timeout to 1 minutes for UT (#8855)

--- fe/fe-core/src/test/java/com/starrocks/journal/bdbje/BDBEnvironmentTest.java
+++ fe/fe-core/src/test/java/com/starrocks/journal/bdbje/BDBEnvironmentTest.java
@@ -41,6 +41,10 @@ public class BDBEnvironmentTest {
         // give master time to update membership
         // otherwise may get error Conflicting node types: uses: SECONDARY Replica is configured as type: ELECTABLE
         BDBEnvironment.SLEEP_INTERVAL_SEC = 1;
+        // set timeout to a really long time so that ut can pass even when IO load is very high
+        Config.bdbje_heartbeat_timeout_second = 60;
+        Config.bdbje_replica_ack_timeout_second = 60;
+        Config.bdbje_lock_timeout_second = 60;
     }
 
     @After
