97b306f3f4294d4aff00e4eaa1884152f45faeea
trueeyu <lxhhust350@qq.com>
2022-07-18 18:15:23 +0800

    [Others] Add config for librdkafka debug (#8783)

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -23,8 +23,7 @@
 
 #include "configbase.h"
 
-namespace starrocks {
-namespace config {
+namespace starrocks::config {
 // The cluster id.
 CONF_Int32(cluster_id, "-1");
 // The port on which ImpalaInternalService is exported.
@@ -761,6 +760,13 @@ CONF_String(starlet_cache_dir, "");
 
 CONF_Int64(lake_metadata_cache_limit, /*2GB=*/"2147483648");
 
-} // namespace config
+CONF_mBool(dependency_librdkafka_debug_enable, "false");
 
-} // namespace starrocks
+// A comma-separated list of debug contexts to enable.
+// Producer debug context: broker, topic, msg
+// Consumer debug context: consumer, cgrp, topic, fetch
+// Other debug context: generic, metadata, feature, queue, protocol, security, interceptor, plugin
+// admin, eos, mock, assigner, conf
+CONF_String(dependency_librdkafka_debug, "all");
+
+} // namespace starrocks::config
