dd289ca259bed65107619f2ccec363e3094d7aff
waittting <491771532@qq.com>
2022-07-18 09:34:06 +0800

    Modify comments

--- fe/fe-core/src/main/java/com/starrocks/system/ComputeNode.java
+++ fe/fe-core/src/main/java/com/starrocks/system/ComputeNode.java
@@ -406,11 +406,11 @@ public class ComputeNode implements IComputable, Writable {
                 heartbeatErrMsg = hbResponse.getMsg() == null ? "Unknown error" : hbResponse.getMsg();
                 lastMissingHeartbeatTime = System.currentTimeMillis();
             }
-            // When master received hb info status not ok, the hb info also need to be synced to follower.
-            // Otherwise, the failed heartbeat information will not be synchronized to the follower.
+            // When the master receives an error heartbeat info which status not ok, 
+            // this heartbeat info also need to be synced to follower.
             // Since the failed heartbeat info also modifies fe's memory, (this.heartbeatRetryTimes++;)
-            // if it is not synchronized to the follower, 
-            // this will cause the master and follower's metadata to be inconsistent
+            // if this heartbeat is not synchronized to the follower, 
+            // that will cause the Follower and master’s memory to be inconsistent
             isChanged = true;
         }
 
