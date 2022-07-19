32b52cdee87a26d27e0d67c18770125b5a5d8923
朱学通 <594754793@qq.com>
2022-07-19 12:01:17 +0800

    replace HashSet to ImmutableSet

--- fe/fe-core/src/main/java/com/starrocks/analysis/ShowPartitionsStmt.java
+++ fe/fe-core/src/main/java/com/starrocks/analysis/ShowPartitionsStmt.java
@@ -22,6 +22,7 @@
 package com.starrocks.analysis;
 
 import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableSet;
 import com.starrocks.catalog.Column;
 import com.starrocks.catalog.ScalarType;
 import com.starrocks.common.AnalysisException;
@@ -31,12 +32,9 @@ import com.starrocks.common.util.OrderByPair;
 import com.starrocks.qe.ShowResultSetMetaData;
 import com.starrocks.sql.ast.AstVisitor;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class ShowPartitionsStmt extends ShowStmt {
 
@@ -47,16 +45,16 @@ public class ShowPartitionsStmt extends ShowStmt {
     public static final String FILTER_REPLICATION_NUM = "ReplicationNum";
     public static final String FILTER_LAST_CONSISTENCY_CHECK_TIME = "LastConsistencyCheckTime";
 
-    public static final Set<String> FILTER_COLUMNS;
+    public static final ImmutableSet<String> FILTER_COLUMNS;
 
     static {
-        FILTER_COLUMNS = new HashSet<>();
-        FILTER_COLUMNS.add(FILTER_PARTITION_ID);
-        FILTER_COLUMNS.add(FILTER_PARTITION_NAME);
-        FILTER_COLUMNS.add(FILTER_STATE);
-        FILTER_COLUMNS.add(FILTER_BUCKETS);
-        FILTER_COLUMNS.add(FILTER_REPLICATION_NUM);
-        FILTER_COLUMNS.add(FILTER_LAST_CONSISTENCY_CHECK_TIME);
+        FILTER_COLUMNS = ImmutableSet.<String>builder()
+                .add(FILTER_PARTITION_ID)
+                .add(FILTER_PARTITION_NAME)
+                .add(FILTER_STATE)
+                .add(FILTER_BUCKETS)
+                .add(FILTER_REPLICATION_NUM)
+                .add(FILTER_LAST_CONSISTENCY_CHECK_TIME).build();
     }
 
     private String dbName;
