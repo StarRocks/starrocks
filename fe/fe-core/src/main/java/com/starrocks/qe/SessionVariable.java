8d55c6524fda1250f257f7ae436033e18f1cc60f
gengjun-git <gengjun@starrocks.com>
2022-07-20 17:03:52 +0800

    add

--- fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
@@ -124,6 +124,7 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
     public static final String ENABLE_SPILLING = "enable_spilling";
     // if set to true, some of stmt will be forwarded to leader FE to get result
     public static final String FORWARD_TO_MASTER = "forward_to_master";
+    public static final String FORWARD_TO_LEADER = "forward_to_leader";
     // user can set instance num after exchange, no need to be equal to nums of before exchange
     public static final String PARALLEL_EXCHANGE_INSTANCE_NUM = "parallel_exchange_instance_num";
     public static final String EVENT_SCHEDULER = "event_scheduler";
@@ -454,6 +455,9 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
     @VariableMgr.VarAttr(name = FORWARD_TO_MASTER)
     private boolean forwardToMaster = false;
 
+    @VariableMgr.VarAttr(name = FORWARD_TO_LEADER)
+    private boolean forwardToLeader = false;
+
     // compatible with some mysql client connect, say DataGrip of JetBrains
     @VariableMgr.VarAttr(name = EVENT_SCHEDULER)
     private String eventScheduler = "OFF";
@@ -735,8 +739,8 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
         this.enableInsertStrict = enableInsertStrict;
     }
 
-    public boolean getForwardToMaster() {
-        return forwardToMaster;
+    public boolean getForwardToLeader() {
+        return forwardToMaster || forwardToLeader;
     }
 
     public void setMaxScanKeyNum(int maxScanKeyNum) {
