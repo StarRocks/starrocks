49dc4ab776d9c61a0e5a0988df5f81e7a5fd3217
HangyuanLiu <460660596@qq.com>
2022-07-22 18:33:27 +0800

    [Enhancement] Optimized statistics scheduling and not collecting empty tables and partitions (#8958)

---
+++ fe/fe-core/src/test/java/com/starrocks/statistic/StatisticsCollectJobTest.java
@@ -0,0 +1,187 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021 StarRocks Limited.
+package com.starrocks.statistic;
+
+import com.google.common.collect.Maps;
+import com.starrocks.catalog.Database;
+import com.starrocks.catalog.OlapTable;
+import com.starrocks.catalog.Partition;
+import com.starrocks.server.GlobalStateMgr;
+import com.starrocks.sql.plan.PlanTestBase;
+import jersey.repackaged.com.google.common.collect.Lists;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StatisticsCollectJobTest extends PlanTestBase {
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        PlanTestBase.beforeClass();
+        GlobalStateMgr globalStateMgr = connectContext.getGlobalStateMgr();
+
+        starRocksAssert.withTable("CREATE TABLE `t0_stats` (\n" +
+                "  `v1` bigint NULL COMMENT \"\",\n" +
+                "  `v2` bigint NULL COMMENT \"\",\n" +
+                "  `v3` bigint NULL\n" +
+                ") ENGINE=OLAP\n" +
+                "DUPLICATE KEY(`v1`, `v2`, v3)\n" +
+                "DISTRIBUTED BY HASH(`v1`) BUCKETS 3\n" +
+                "PROPERTIES (\n" +
+                "\"replication_num\" = \"1\",\n" +
+                "\"in_memory\" = \"false\",\n" +
+                "\"storage_format\" = \"DEFAULT\"\n" +
+                ");");
+
+        OlapTable t0 = (OlapTable) globalStateMgr.getDb("default_cluster:test").getTable("t0_stats");
+        Partition partition = new ArrayList<>(t0.getPartitions()).get(0);
+        partition.updateVisibleVersion(2, LocalDateTime.of(2022, 1, 1, 1, 1, 1)
+                .atZone(Clock.systemDefaultZone().getZone()).toEpochSecond() * 1000);
+        setTableStatistics(t0, 20000000);
+
+        starRocksAssert.withTable("CREATE TABLE `t1_stats` (\n" +
+                "  `v4` bigint NULL COMMENT \"\",\n" +
+                "  `v5` bigint NULL COMMENT \"\",\n" +
+                "  `v6` bigint NULL\n" +
+                ") ENGINE=OLAP\n" +
+                "DUPLICATE KEY(`v4`, `v5`, v6)\n" +
+                "DISTRIBUTED BY HASH(`v4`) BUCKETS 3\n" +
+                "PROPERTIES (\n" +
+                "\"replication_num\" = \"1\",\n" +
+                "\"in_memory\" = \"false\",\n" +
+                "\"storage_format\" = \"DEFAULT\"\n" +
+                ");");
+
+        OlapTable t1 = (OlapTable) globalStateMgr.getDb("default_cluster:test").getTable("t1_stats");
+        new ArrayList<>(t1.getPartitions()).get(0).updateVisibleVersion(2);
+        setTableStatistics(t1, 20000000);
+    }
+
+    @Test
+    public void testAnalyzeALLDB() {
+        List<StatisticsCollectJob> jobs = StatisticsCollectJobFactory.buildStatisticsCollectJob(
+                new AnalyzeJob(StatsConstants.DEFAULT_ALL_ID, StatsConstants.DEFAULT_ALL_ID, null,
+                        StatsConstants.AnalyzeType.FULL, StatsConstants.ScheduleType.SCHEDULE,
+                        Maps.newHashMap(),
+                        StatsConstants.ScheduleStatus.PENDING,
+                        LocalDateTime.MIN));
+        Assert.assertEquals(2, jobs.size());
+        Assert.assertTrue(jobs.get(0) instanceof FullStatisticsCollectJob);
+        FullStatisticsCollectJob fullStatisticsCollectJob = (FullStatisticsCollectJob) jobs.get(0);
+        Assert.assertEquals("[v1, v2, v3]", fullStatisticsCollectJob.getColumns().toString());
+        Assert.assertTrue(jobs.get(1) instanceof FullStatisticsCollectJob);
+        fullStatisticsCollectJob = (FullStatisticsCollectJob) jobs.get(1);
+        Assert.assertEquals("[v4, v5, v6]", fullStatisticsCollectJob.getColumns().toString());
+    }
+
+    @Test
+    public void testAnalyzeDB() {
+        List<StatisticsCollectJob> jobs = StatisticsCollectJobFactory.buildStatisticsCollectJob(
+                new AnalyzeJob(10002, StatsConstants.DEFAULT_ALL_ID, null,
+                        StatsConstants.AnalyzeType.FULL, StatsConstants.ScheduleType.SCHEDULE,
+                        Maps.newHashMap(),
+                        StatsConstants.ScheduleStatus.PENDING,
+                        LocalDateTime.MIN));
+        Assert.assertEquals(2, jobs.size());
+        Assert.assertTrue(jobs.get(0) instanceof FullStatisticsCollectJob);
+        FullStatisticsCollectJob fullStatisticsCollectJob = (FullStatisticsCollectJob) jobs.get(0);
+        Assert.assertEquals("[v1, v2, v3]", fullStatisticsCollectJob.getColumns().toString());
+        Assert.assertTrue(jobs.get(1) instanceof FullStatisticsCollectJob);
+        fullStatisticsCollectJob = (FullStatisticsCollectJob) jobs.get(1);
+        Assert.assertEquals("[v4, v5, v6]", fullStatisticsCollectJob.getColumns().toString());
+    }
+
+    @Test
+    public void testAnalyzeTable() {
+        List<StatisticsCollectJob> jobs = StatisticsCollectJobFactory.buildStatisticsCollectJob(
+                new AnalyzeJob(10002, 16325, null,
+                        StatsConstants.AnalyzeType.FULL, StatsConstants.ScheduleType.SCHEDULE,
+                        Maps.newHashMap(),
+                        StatsConstants.ScheduleStatus.PENDING,
+                        LocalDateTime.MIN));
+        Assert.assertEquals(1, jobs.size());
+        Assert.assertTrue(jobs.get(0) instanceof FullStatisticsCollectJob);
+        FullStatisticsCollectJob fullStatisticsCollectJob = (FullStatisticsCollectJob) jobs.get(0);
+        Assert.assertEquals("t0_stats", fullStatisticsCollectJob.getTable().getName());
+        Assert.assertEquals("[v1, v2, v3]", fullStatisticsCollectJob.getColumns().toString());
+    }
+
+    @Test
+    public void testAnalyzeColumn() {
+        List<StatisticsCollectJob> jobs = StatisticsCollectJobFactory.buildStatisticsCollectJob(
+                new AnalyzeJob(10002, 16325, Lists.newArrayList("v2"),
+                        StatsConstants.AnalyzeType.FULL, StatsConstants.ScheduleType.SCHEDULE,
+                        Maps.newHashMap(),
+                        StatsConstants.ScheduleStatus.PENDING,
+                        LocalDateTime.MIN));
+        Assert.assertEquals(1, jobs.size());
+        Assert.assertTrue(jobs.get(0) instanceof FullStatisticsCollectJob);
+        FullStatisticsCollectJob fullStatisticsCollectJob = (FullStatisticsCollectJob) jobs.get(0);
+        Assert.assertEquals("[v2]", fullStatisticsCollectJob.getColumns().toString());
+    }
+
+    @Test
+    public void testAnalyzeColumnSample() {
+        List<StatisticsCollectJob> jobs = StatisticsCollectJobFactory.buildStatisticsCollectJob(
+                new AnalyzeJob(10002, 16325, Lists.newArrayList("v2"),
+                        StatsConstants.AnalyzeType.SAMPLE, StatsConstants.ScheduleType.SCHEDULE,
+                        Maps.newHashMap(),
+                        StatsConstants.ScheduleStatus.PENDING,
+                        LocalDateTime.MIN));
+        Assert.assertEquals(1, jobs.size());
+        Assert.assertTrue(jobs.get(0) instanceof SampleStatisticsCollectJob);
+        SampleStatisticsCollectJob sampleStatisticsCollectJob = (SampleStatisticsCollectJob) jobs.get(0);
+        Assert.assertEquals("[v2]", sampleStatisticsCollectJob.getColumns().toString());
+    }
+
+    @Test
+    public void testAnalyzeColumnSample2() {
+        Database db = GlobalStateMgr.getCurrentState().getDb(10002);
+        OlapTable olapTable = (OlapTable) db.getTable("t0_stats");
+
+        BasicStatsMeta basicStatsMeta = new BasicStatsMeta(10002, olapTable.getId(), StatsConstants.AnalyzeType.SAMPLE,
+                LocalDateTime.of(2020, 1, 1, 1, 1, 1), Maps.newHashMap());
+        basicStatsMeta.increaseUpdateRows(10000000L);
+        GlobalStateMgr.getCurrentAnalyzeMgr().addBasicStatsMeta(basicStatsMeta);
+
+        List<StatisticsCollectJob> jobs = StatisticsCollectJobFactory.buildStatisticsCollectJob(
+                new AnalyzeJob(10002, olapTable.getId(), Lists.newArrayList("v2"),
+                        StatsConstants.AnalyzeType.SAMPLE, StatsConstants.ScheduleType.SCHEDULE,
+                        Maps.newHashMap(),
+                        StatsConstants.ScheduleStatus.PENDING,
+                        LocalDateTime.MIN));
+        Assert.assertEquals(1, jobs.size());
+
+        jobs = StatisticsCollectJobFactory.buildStatisticsCollectJob(
+                new AnalyzeJob(10002, olapTable.getId(), Lists.newArrayList("v2"),
+                        StatsConstants.AnalyzeType.FULL, StatsConstants.ScheduleType.SCHEDULE,
+                        Maps.newHashMap(),
+                        StatsConstants.ScheduleStatus.PENDING,
+                        LocalDateTime.MIN));
+        Assert.assertEquals(1, jobs.size());
+
+        BasicStatsMeta basicStatsMeta2 = new BasicStatsMeta(10002, olapTable.getId(), StatsConstants.AnalyzeType.SAMPLE,
+                LocalDateTime.of(2022, 1, 1, 1, 1, 1), Maps.newHashMap());
+        GlobalStateMgr.getCurrentAnalyzeMgr().addBasicStatsMeta(basicStatsMeta2);
+
+        List<StatisticsCollectJob> jobs2 = StatisticsCollectJobFactory.buildStatisticsCollectJob(
+                new AnalyzeJob(10002, olapTable.getId(), Lists.newArrayList("v2"),
+                        StatsConstants.AnalyzeType.SAMPLE, StatsConstants.ScheduleType.SCHEDULE,
+                        Maps.newHashMap(),
+                        StatsConstants.ScheduleStatus.PENDING,
+                        LocalDateTime.MIN));
+        Assert.assertEquals(0, jobs2.size());
+
+        jobs2 = StatisticsCollectJobFactory.buildStatisticsCollectJob(
+                new AnalyzeJob(10002, olapTable.getId(), Lists.newArrayList("v2"),
+                        StatsConstants.AnalyzeType.FULL, StatsConstants.ScheduleType.SCHEDULE,
+                        Maps.newHashMap(),
+                        StatsConstants.ScheduleStatus.PENDING,
+                        LocalDateTime.MIN));
+        Assert.assertEquals(1, jobs2.size());
+        GlobalStateMgr.getCurrentAnalyzeMgr().getBasicStatsMetaMap().remove(olapTable.getId());
+    }
+}
