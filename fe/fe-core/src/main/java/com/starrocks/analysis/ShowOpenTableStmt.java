d4c44c90c40e413667dc40d76cafd037cd2ab9a4
amorynan <amorywang111@gmail.com>
2022-07-22 17:27:49 +0800

    Revert "update for ShowOpenTables"
    This reverts commit 674dabf6bdde5f7f6c77dcba385369330f2b0d86.

--- fe/fe-core/src/main/java/com/starrocks/analysis/ShowOpenTableStmt.java
+++ fe/fe-core/src/main/java/com/starrocks/analysis/ShowOpenTableStmt.java
@@ -17,19 +17,12 @@
 
 package com.starrocks.analysis;
 
-import com.google.common.base.Strings;
 import com.starrocks.catalog.Column;
-import com.starrocks.catalog.InfoSchemaDb;
 import com.starrocks.catalog.ScalarType;
 import com.starrocks.qe.ShowResultSetMetaData;
-import com.starrocks.sql.ast.AstVisitor;
-import com.starrocks.sql.ast.QueryStatement;
-import com.starrocks.sql.ast.SelectRelation;
-import com.starrocks.sql.ast.TableRelation;
 
 // SHOW OPEN TABLES
 public class ShowOpenTableStmt extends ShowStmt {
-    private static final TableName TABLE_NAME = new TableName(InfoSchemaDb.DATABASE_NAME, "tables");
     private static final ShowResultSetMetaData META_DATA =
             ShowResultSetMetaData.builder()
                     .addColumn(new Column("Database", ScalarType.createVarchar(64)))
@@ -38,92 +31,6 @@ public class ShowOpenTableStmt extends ShowStmt {
                     .addColumn(new Column("Name_locked", ScalarType.createVarchar(64)))
                     .build();
 
-    private String dbName;
-    private String pattern;
-    private Expr where;
-
-    public String getDbName() {
-        return dbName;
-    }
-
-    public void setDbName(String dbName) {
-        this.dbName = dbName;
-    }
-
-    public String getPattern() {
-        return pattern;
-    }
-
-    public void setPattern(String pattern) {
-        this.pattern = pattern;
-    }
-
-    public Expr getWhere() {
-        return where;
-    }
-
-    public void setWhere(Expr where) {
-        this.where = where;
-    }
-
-    public ShowOpenTableStmt() {}
-    public ShowOpenTableStmt(String dbName, String pattern, Expr where) {
-        setDbName(dbName);
-        setPattern(pattern);
-        setWhere(where);
-    }
-
-    @Override
-    public String toSql() {
-        StringBuilder sb = new StringBuilder("SHOW OPEN TABLES");
-        if (!Strings.isNullOrEmpty(dbName)){
-            sb.append(" From ").append(dbName);
-        }
-        if (pattern != null) {
-            sb.append(" LIKE '").append(pattern).append("'");
-        }
-        if (where != null) {
-            sb.append(" WHERE '").append(where).append("'");
-        }
-        return sb.toString();
-    }
-
-    public QueryStatement toSelectStmt() {
-        if (where == null) {
-            return null;
-        }
-
-        SelectList selectList = new SelectList();
-        ExprSubstitutionMap aliasMap = new ExprSubstitutionMap(false);
-        // Database
-        SelectListItem item = new SelectListItem(new SlotRef(TABLE_NAME, "DATABASE"), "Database");
-        selectList.addItem(item);
-        aliasMap.put(new SlotRef(null, "Database"), item.getExpr().clone(null));
-        // Table
-        item = new SelectListItem(new SlotRef(TABLE_NAME, "TABLE"), "Table");
-        selectList.addItem(item);
-        aliasMap.put(new SlotRef(null, "Table"), item.getExpr().clone(null));
-        // In_use
-        item = new SelectListItem(new SlotRef(TABLE_NAME, "IN_USE"), "In_use");
-        selectList.addItem(item);
-        aliasMap.put(new SlotRef(null, "In_use"), item.getExpr().clone(null));
-        // Name_Locked
-        item = new SelectListItem(new SlotRef(TABLE_NAME, "NAME_LOCKED"), "Name_Locked");
-        selectList.addItem(item);
-        aliasMap.put(new SlotRef(null, "Name_Locked"), item.getExpr().clone(null));
-
-        where = where.substitute(aliasMap);
-        return new QueryStatement(new SelectRelation(selectList, new TableRelation(TABLE_NAME),
-                where, null, null));
-    }
-
-    public <R, C> R accept(AstVisitor<R, C> visitor, C context) {
-        return visitor.visitShowOpenTablesStmt(this, context);
-    }
-
-    public boolean isSupportNewPlanner() {
-        return true;
-    }
     @Override
     public void analyze(Analyzer analyzer) {
     }
