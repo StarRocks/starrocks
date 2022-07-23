0e1f29b49f8c50664667c26e38199db840990c6d
Seaven <seaven_7@qq.com>
2022-07-23 11:38:34 +0800

    [Enhancement] Update some class path

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rewrite/scalar/ScalarOperatorRewriteRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rewrite/scalar/ScalarOperatorRewriteRule.java
@@ -1,8 +1,9 @@
 // This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
 
-package com.starrocks.sql.optimizer.rewrite;
+package com.starrocks.sql.optimizer.rewrite.scalar;
 
 import com.starrocks.sql.optimizer.operator.scalar.ScalarOperator;
+import com.starrocks.sql.optimizer.rewrite.ScalarOperatorRewriteContext;
 
 public interface ScalarOperatorRewriteRule {
     boolean isBottomUp();
