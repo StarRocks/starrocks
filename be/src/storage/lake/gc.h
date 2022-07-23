3644c8334125264e22084dc66f8fddefddc53f18
Alex Zhu <zhuming9011@gmail.com>
2022-07-23 15:06:56 +0800

    [Enhancement] lake tablet GC (#9074)

---
+++ be/src/storage/lake/gc.h
@@ -0,0 +1,17 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
+
+#pragma once
+
+#include <string_view>
+
+#include "common/status.h"
+
+namespace starrocks::lake {
+
+class TabletManager;
+
+Status metadata_gc(std::string_view root_location, TabletManager* tablet_mgr);
+
+Status segment_gc(std::string_view root_location, TabletManager* tablet_mgr);
+
+} // namespace starrocks::lake
