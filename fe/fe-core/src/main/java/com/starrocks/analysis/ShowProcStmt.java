f4127ee494a628152b2cee492659643bcf8222e5
gengjun-git <gengjun@starrocks.com>
2022-07-25 15:50:29 +0800

    [Refactor] [Step 2/2]Change the name from master to leader (#8954)
    Change the master related code to leader.
    Split the task into two steps: 
    1. change the name from master to leader in the internal code paths.
    2. change the name from master to leader in the interface.
    
    The advantage of this is that we only need to rollback the step 2 if If there are compatibility problems.
    
    This is PR is the step 2 of the task. 
    1. remove the `isMaster` attribute from the result of `show frontends`.
    2. add `forward_to_leader` session variable to replace the `forward_to_master`. But we still remain `forward_to_master` for compatibility.

--- fe/fe-core/src/main/java/com/starrocks/analysis/ShowProcStmt.java
+++ fe/fe-core/src/main/java/com/starrocks/analysis/ShowProcStmt.java
@@ -77,7 +77,7 @@ public class ShowProcStmt extends ShowStmt {
 
     @Override
     public RedirectStatus getRedirectStatus() {
-        if (ConnectContext.get().getSessionVariable().getForwardToMaster()) {
+        if (ConnectContext.get().getSessionVariable().getForwardToLeader()) {
             return RedirectStatus.FORWARD_NO_SYNC;
         } else {
             return RedirectStatus.NO_FORWARD;
