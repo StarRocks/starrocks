d575baddae0f1d06c1091606007ef8268e01fd60
Seaven <seaven_7@qq.com>
2022-07-27 14:34:45 +0800

    update

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rewrite/scalar/ImplicitCastRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rewrite/scalar/ImplicitCastRule.java
@@ -113,7 +113,8 @@ public class ImplicitCastRule extends TopDownScalarOperatorRewriteRule {
             if (op.isPresent()) {
                 predicate.getChildren().set(0, op.get());
                 return predicate;
-            } else if (rightChild.getType().isDateType()) {
+            } else if (rightChild.getType().isDateType() && Type.canCastTo(leftChild.getType(), rightChild.getType())) {
+                // For like MySQL, convert to date type as much as possible
                 addCastChild(rightChild.getType(), predicate, 0);
                 return predicate;
             }
@@ -122,7 +123,8 @@ public class ImplicitCastRule extends TopDownScalarOperatorRewriteRule {
             if (op.isPresent()) {
                 predicate.getChildren().set(1, op.get());
                 return predicate;
-            } else if (leftChild.getType().isDateType()) {
+            } else if (leftChild.getType().isDateType() && Type.canCastTo(rightChild.getType(), leftChild.getType())) {
+                // For like MySQL, convert to date type as much as possible
                 addCastChild(leftChild.getType(), predicate, 1);
                 return predicate;
             }
