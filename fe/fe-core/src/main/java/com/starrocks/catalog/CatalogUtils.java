f2e9fb20bd7fd9362a40ffac42b4486a71e27325
abc982627271 <liuxuefen@starrocks.com>
2022-07-28 15:16:29 +0800

    change back

--- fe/fe-core/src/main/java/com/starrocks/catalog/CatalogUtils.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/CatalogUtils.java
@@ -33,9 +33,8 @@ public class CatalogUtils {
 
     // check table type is OLAP
     public static void checkTableTypeOLAP(Database db, Table table) throws DdlException {
-        if (!table.isOlapOrLakeTable() && table.getType() != Table.TableType.MATERIALIZED_VIEW) {
-            throw new DdlException("Table[" + table.getName() +
-                    "] is not OLAP table or LAKE table");
+        if (table.getType() != Table.TableType.OLAP && table.getType() != Table.TableType.MATERIALIZED_VIEW) {
+            throw new DdlException("Table[" + table.getName() + "] is not OLAP table");
         }
     }
 
