7754f5f9bc23837b096cd261364ba121772001a1
padmejin <89557328+padmejin@users.noreply.github.com>
2022-08-04 23:24:59 +0800

    [BugFix] Discard expired backup/restore job (#9531)

--- fe/fe-core/src/main/java/com/starrocks/catalog/Catalog.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/Catalog.java
@@ -7706,5 +7706,10 @@ public class Catalog {
         } catch (Throwable t) {
             LOG.warn("routine load manager clean old routine load jobs failed", t);
         }
+        try {
+            backupHandler.removeOldJobs();
+        } catch (Throwable t) {
+            LOG.warn("backup handler clean old jobs failed", t);
+        }
     }
 }
