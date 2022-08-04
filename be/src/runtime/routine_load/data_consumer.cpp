10faba72ff2414eb4f1e9bd2ba79eb9cd3efcda0
ricky <rickif@qq.com>
2022-08-04 16:36:17 +0800

    *: revert be/src/runtime/routine_load/data_consumer.cpp

--- be/src/runtime/routine_load/data_consumer.cpp
+++ be/src/runtime/routine_load/data_consumer.cpp
@@ -136,7 +136,7 @@ Status KafkaDataConsumer::init(StreamLoadContext* ctx) {
         return Status::InternalError("PAUSE: failed to create kafka consumer: " + errstr);
     }
 
-    LOG(INFO) << "finished to init kafka consumer. " << ctx->brief();
+    VLOG(3) << "finished to init kafka consumer. " << ctx->brief();
 
     _init = true;
     return Status::OK();
