ea6c6961b541f62e43daf407d4ddf56deaf6bd8f
Felix Li <astralidea@163.com>
2022-08-05 12:34:08 +0800

    Fix delete throw unknown error when enable query detail (#9597)
    This because if FE config enable_collect_query_detail_info = true
    in StmtExecutor context.getQueryDetail != null will apply and will call buildExplaianString
    but marked delete does not have explainString so execPlan will be null cause of NPE.

--- fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
@@ -1004,7 +1004,12 @@ public class StmtExecutor {
                 explainString += "RESOURCE GROUP: " + resourceGroupStr + "\n\n";
             }
         }
-        explainString += execPlan.getExplainString(parsedStmt.getExplainLevel());
+        // marked delete will get execPlan null
+        if (execPlan == null) {
+            explainString += "NOT AVAILABLE";
+        } else {
+            explainString += execPlan.getExplainString(parsedStmt.getExplainLevel());
+        }
         return explainString;
     }
 
