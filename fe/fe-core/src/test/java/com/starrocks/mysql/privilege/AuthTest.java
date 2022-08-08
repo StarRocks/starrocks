0354c1eb5dc13dff7bc93d90856cbc9c8a5d36cd
padmejin <89557328+padmejin@users.noreply.github.com>
2022-08-08 19:45:49 +0800

    [Feature] Support all-digit user name (#9715)
    Allow all-digit user name, such as create user '12345';.

--- fe/fe-core/src/test/java/com/starrocks/mysql/privilege/AuthTest.java
+++ fe/fe-core/src/test/java/com/starrocks/mysql/privilege/AuthTest.java
@@ -2279,4 +2279,12 @@ public class AuthTest {
         newAuth.replayRevokeImpersonate(infos.get(0));
         Assert.assertFalse(newAuth.canImpersonate(harry, gregory));
     }
+
+    @Test
+    public void testUserNamePureDigit() throws Exception {
+        String sql = "CREATE USER '12345' IDENTIFIED BY '12345'";
+        CreateUserStmt createUserStmt = (CreateUserStmt) UtFrameUtils.parseStmtWithNewParser(sql, ctx);
+        auth.createUser(createUserStmt);
+        Assert.assertNotNull(auth.getUserProperties("default_cluster:'12345'@'%'"));
+    }
 }
