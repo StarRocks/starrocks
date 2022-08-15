6dd04dd0b37e78b464daf00be36b747aa6499c51
stdpain <drfeng08@gmail.com>
2022-08-15 16:03:20 +0800

    extract as getAggregateFunction

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/SubqueryUtils.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/SubqueryUtils.java
@@ -24,6 +24,18 @@ import java.util.Map;
 
 public class SubqueryUtils {
 
+    private static Function getAggregateFunction(String functionName, Type[] argTypes) {
+        Function func = Expr.getBuiltinFunction(functionName, argTypes,
+                Function.CompareMode.IS_IDENTICAL);
+        if (argTypes.length > 0 && argTypes[0].isDecimalV3()) {
+            func =
+                    DecimalV3FunctionAnalyzer.rectifyAggregationFunction((AggregateFunction) func,
+                            argTypes[0],
+                            argTypes[0]);
+        }
+        return func;
+    }
+
     // ApplyNode doesn't need to check the number of subquery's return rows
     // when the correlation predicate meets these requirements:
     // 1. All predicate is Binary.EQ
@@ -99,28 +111,18 @@ public class SubqueryUtils {
     }
 
     public static CallOperator createCountRowsOperator() {
-        Function count = Expr.getBuiltinFunction(FunctionSet.COUNT, new Type[] {Type.BIGINT},
-                Function.CompareMode.IS_IDENTICAL);
+        Function count = getAggregateFunction(FunctionSet.COUNT, new Type[] {Type.BIGINT});
         return new CallOperator(FunctionSet.COUNT, Type.BIGINT, Lists.newArrayList(ConstantOperator.createBigint(1)),
-                count,
-                false);
+                count, false);
     }
 
     public static CallOperator createCountRowsOperator(ScalarOperator column) {
-        Function count = Expr.getBuiltinFunction(FunctionSet.COUNT, new Type[] {Type.BIGINT},
-                Function.CompareMode.IS_IDENTICAL);
+        Function count = getAggregateFunction(FunctionSet.COUNT, new Type[] {Type.BIGINT});
         return new CallOperator(FunctionSet.COUNT, Type.BIGINT, Lists.newArrayList(column), count, false);
     }
 
     public static CallOperator createAnyValueOperator(ScalarOperator column) {
-        Function anyValueFn = Expr.getBuiltinFunction(FunctionSet.ANY_VALUE, new Type[] {column.getType()},
-                Function.CompareMode.IS_IDENTICAL);
-        if (column.getType().isDecimalV3()) {
-            anyValueFn =
-                    DecimalV3FunctionAnalyzer.rectifyAggregationFunction((AggregateFunction) anyValueFn,
-                            column.getType(),
-                            column.getType());
-        }
+        Function anyValueFn = getAggregateFunction(FunctionSet.ANY_VALUE, new Type[] {column.getType()});
         return new CallOperator(FunctionSet.ANY_VALUE, column.getType(), Lists.newArrayList(column), anyValueFn);
     }
 
