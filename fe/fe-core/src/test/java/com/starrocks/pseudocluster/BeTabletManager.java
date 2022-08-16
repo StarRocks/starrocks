2ff81ddd161ec428b3d1a7d842b75c562cdb3fa5
Binglin Chang <decstery@gmail.com>
2022-08-16 21:29:25 +0800

    [Bugfix] Handle null scan ranges in OLAPScanNode in UT (#10048)
    Some OLAPScanNode do not have scan range, this PR adds code to handle this situation.

--- fe/fe-core/src/test/java/com/starrocks/pseudocluster/BeTabletManager.java
+++ fe/fe-core/src/test/java/com/starrocks/pseudocluster/BeTabletManager.java
@@ -45,15 +45,15 @@ public class BeTabletManager {
 
     public synchronized void dropTablet(long tabletId, boolean force) {
         Tablet removed = tablets.remove(tabletId);
-        Set<Long> tabletIds = tabletIdsByPartition.get(removed.partitionId);
-        tabletIds.remove(tabletId);
-        if (tabletIds.isEmpty()) {
-            tabletIdsByPartition.remove(removed.partitionId);
-        }
         if (removed != null) {
+            Set<Long> tabletIds = tabletIdsByPartition.get(removed.partitionId);
+            tabletIds.remove(tabletId);
+            if (tabletIds.isEmpty()) {
+                tabletIdsByPartition.remove(removed.partitionId);
+            }
             LOG.info("Dropped tablet {} force:{}", removed.id, force);
         } else {
-            LOG.info("Drop Tablet {} not found", tabletId);
+            LOG.warn("Drop Tablet {} not found", tabletId);
         }
     }
 
