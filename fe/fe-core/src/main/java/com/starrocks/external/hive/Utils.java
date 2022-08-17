8660bf1748fe5cd603ea72b0155019e417c8b989
adzfolc <multigraph@krutt.org>
2022-08-17 16:50:31 +0800

    1. fix code smells

--- fe/fe-core/src/main/java/com/starrocks/external/hive/Utils.java
+++ fe/fe-core/src/main/java/com/starrocks/external/hive/Utils.java
@@ -30,7 +30,7 @@ public class Utils {
     public static final String ARRAY_PATTERN = "^array<([0-9a-z<>(),]+)>";
     public static final String CHAR_PATTERN = "^char\\(([0-9]+)\\)";
     public static final String VARCHAR_PATTERN = "^varchar\\(([0-9,-1]+)\\)";
-    public static final List<String> HIVE_UNSUPPORTED_TYPES = Arrays.asList("STRUCT", "BINARY", "MAP", "UNIONTYPE");
+    private static final List<String> HIVE_UNSUPPORTED_TYPES = Arrays.asList("STRUCT", "BINARY", "MAP", "UNIONTYPE");
 
     public static PartitionKey createPartitionKey(List<String> values, List<Column> columns) throws AnalysisException {
         return createPartitionKey(values, columns, false);
