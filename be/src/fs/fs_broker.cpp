9388d83ac8f519df297dc970c292912a48bf769a
meegoo <hujie-dlut@qq.com>
2022-08-18 21:02:49 +0800

    [Refactor] Refactor access broker error log (#10146)

--- be/src/fs/fs_broker.cpp
+++ be/src/fs/fs_broker.cpp
@@ -17,6 +17,8 @@
 #include "runtime/client_cache.h"
 #include "runtime/exec_env.h"
 
+using namespace fmt::literals;
+
 namespace starrocks {
 
 using BrokerServiceClient = TFileBrokerServiceClient;
@@ -322,7 +324,7 @@ StatusOr<std::unique_ptr<WritableFile>> BrokerFileSystem::new_writable_file(cons
                                                                             const std::string& path) {
     if (opts.mode == FileSystem::CREATE_OR_OPEN_WITH_TRUNCATE) {
         if (auto st = _path_exists(path); st.ok()) {
-            return Status::NotSupported("Cannot truncate a file by broker");
+            return Status::NotSupported("Cannot truncate a file by broker, path={}"_format(path));
         }
     } else if (opts.mode == MUST_CREATE) {
         if (auto st = _path_exists(path); st.ok()) {
@@ -332,7 +334,7 @@ StatusOr<std::unique_ptr<WritableFile>> BrokerFileSystem::new_writable_file(cons
         return Status::NotSupported("Open with MUST_EXIST not supported by broker");
     } else if (opts.mode == CREATE_OR_OPEN) {
         if (auto st = _path_exists(path); st.ok()) {
-            return Status::NotSupported("Cannot write an already exists file through broker");
+            return Status::NotSupported("Cannot open an already exists file through broker, path={}"_format(path));
         }
     } else {
         auto msg = strings::Substitute("Unsupported open mode $0", opts.mode);
