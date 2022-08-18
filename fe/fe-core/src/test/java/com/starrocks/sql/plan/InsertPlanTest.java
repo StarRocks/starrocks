9b1a4672bfe51699c020bf49b3442aaddbe90f3d
Seaven <seaven_7@qq.com>
2022-08-18 10:11:47 +0800

    [BugFix] Insert plan required gather when select limit (#10080)

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/InsertPlanTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/InsertPlanTest.java
@@ -696,4 +696,21 @@ public class InsertPlanTest extends PlanTestBase {
                 "  |  <slot 8> : NULL\n" +
                 "  |  <slot 9> : NULL"));
     }
+
+    @Test
+    public void testInsertAggLimit() throws Exception {
+        FeConstants.runningUnitTest = true;
+        InsertPlanner.enableSingleReplicationShuffle = true;
+        {
+            // KesType is AGG_KEYS
+            String sql = "explain insert into baseall select * from baseall limit 1";
+            String plan = getInsertExecPlan(sql);
+            assertContains(plan, "STREAM DATA SINK\n" +
+                    "    EXCHANGE ID: 01\n" +
+                    "    UNPARTITIONED");
+
+            InsertPlanner.enableSingleReplicationShuffle = false;
+            FeConstants.runningUnitTest = false;
+        }
+    }
 }
