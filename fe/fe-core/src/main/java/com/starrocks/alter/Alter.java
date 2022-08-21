3d25d4934193c616b3f335e048279deb441483b6
Xuefen Liu <liuxuefen@starrocks.com>
2022-08-21 18:35:55 +0800

    support swap (#10243)

--- fe/fe-core/src/main/java/com/starrocks/alter/Alter.java
+++ fe/fe-core/src/main/java/com/starrocks/alter/Alter.java
@@ -533,8 +533,8 @@ public class Alter {
         String origTblName = origTable.getName();
         String newTblName = clause.getTblName();
         Table newTbl = db.getTable(newTblName);
-        if (newTbl == null || newTbl.getType() != TableType.OLAP) {
-            throw new DdlException("Table " + newTblName + " does not exist or is not OLAP table");
+        if (newTbl == null || !newTbl.isOlapOrLakeTable()) {
+            throw new DdlException("Table " + newTblName + " does not exist or is not OLAP/LAKE table");
         }
         OlapTable olapNewTbl = (OlapTable) newTbl;
 
