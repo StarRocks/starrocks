15e82dfdc2c9592b59618d88b17d2eb8bd73c23e
Murphy <mofei@starrocks.com>
2022-08-22 21:02:57 +0800

    modify test case

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/NestLoopJoinTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/NestLoopJoinTest.java
@@ -49,22 +49,16 @@ public class NestLoopJoinTest extends PlanTestBase {
     public void testNLJoinExplicit() throws Exception {
         PlanTestBase.connectContext.getSessionVariable().setJoinImplementationMode("nestloop");
         assertNestloopJoin("SELECT * from t0 a join t0 b on a.v1 < b.v1", "INNER JOIN", "1: v1 < 4: v1");
-        assertNestloopJoin("SELECT * from t0 a left join t0 b on a.v1 < b.v1", "RIGHT OUTER JOIN", "1: v1 < 4: v1");
+        assertNestloopJoin("SELECT * from t0 a left join [broadcast] t0 b on a.v1 < b.v1", "LEFT OUTER JOIN", "1: v1 < 4: v1");
         assertNestloopJoin("SELECT * from t0 a right join t0 b on a.v1 < b.v1", "RIGHT OUTER JOIN", "1: v1 < 4: v1");
         assertNestloopJoin("SELECT * from t0 a full join t0 b on a.v1 < b.v1", "FULL OUTER JOIN", "1: v1 < 4: v1");
 
-        // TODO: support equal-predicate join
-        // assertNestloopJoin("SELECT * from t0 a join t0 b on a.v1 = b.v1", "INNER JOIN", "1: v1 = 4: v1");
-        // assertNestloopJoin("SELECT * from t0 a left join t0 b on a.v1 = b.v1", "LEFT OUTER JOIN", "1: v1 = 4: v1");
-        // assertNestloopJoin("SELECT * from t0 a right join t0 b on a.v1 = b.v1", "RIGHT OUTER JOIN", "1: v1 = 4: v1");
-        // assertNestloopJoin("SELECT * from t0 a full join t0 b on a.v1 = b.v1", "FULL OUTER JOIN", "1: v1 = 4: v1");
-
         PlanTestBase.connectContext.getSessionVariable().setJoinImplementationMode("");
         // Non-Equal join could only be implemented by NestLoopJoin
-        // assertNestloopJoin("SELECT * from t0 a join t0 b on a.v1 < b.v1", "INNER JOIN", "1: v1 < 4: v1");
-        // assertNestloopJoin("SELECT * from t0 a left join t0 b on a.v1 < b.v1", "LEFT OUTER JOIN", "1: v1 < 4: v1");
-        // assertNestloopJoin("SELECT * from t0 a right join t0 b on a.v1 < b.v1", "RIGHT OUTER JOIN", "1: v1 < 4: v1");
-        // assertNestloopJoin("SELECT * from t0 a full join t0 b on a.v1 < b.v1", "FULL OUTER JOIN", "1: v1 < 4: v1");
+        assertNestloopJoin("SELECT * from t0 a join t0 b on a.v1 < b.v1", "INNER JOIN", "1: v1 < 4: v1");
+        assertNestloopJoin("SELECT * from t0 a left join [broadcast] t0 b on a.v1 < b.v1", "LEFT OUTER JOIN", "1: v1 < 4: v1");
+        assertNestloopJoin("SELECT * from t0 a right join t0 b on a.v1 < b.v1", "RIGHT OUTER JOIN", "1: v1 < 4: v1");
+        assertNestloopJoin("SELECT * from t0 a full join t0 b on a.v1 < b.v1", "FULL OUTER JOIN", "1: v1 < 4: v1");
     }
 
     // Right outer join needs a GATHER distribution
