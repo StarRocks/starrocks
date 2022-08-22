532a3427881a46025ca1c37c938b9b9921554da8
miomiocat <284487410@qq.com>
2022-08-22 15:31:44 +0800

    [Feature]Support reading hudi MOR table in snapshot mode (#6780)

--- be/src/runtime/descriptors.h
+++ be/src/runtime/descriptors.h
@@ -230,6 +230,19 @@ public:
     HudiTableDescriptor(const TTableDescriptor& tdesc, ObjectPool* pool);
     ~HudiTableDescriptor() override = default;
     bool has_partition() const override { return true; }
+    const std::string& get_base_path() const;
+    const std::string& get_instant_time() const;
+    const std::string& get_hive_column_names() const;
+    const std::string& get_hive_column_types() const;
+    const std::string& get_input_format() const;
+    const std::string& get_serde_lib() const;
+
+private:
+    std::string _hudi_instant_time;
+    std::string _hive_column_names;
+    std::string _hive_column_types;
+    std::string _input_format;
+    std::string _serde_lib;
 };
 
 // ===========================================
