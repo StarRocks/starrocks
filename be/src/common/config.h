13504c20c3891128cbaa664e1b25b0a489caedec
xyz <a997647204@gmail.com>
2022-08-23 18:23:27 +0800

    [BugFix] string column writer will check data's length even it is null
    Signed-off-by: xyz <a997647204@gmail.com>

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -303,6 +303,8 @@ CONF_mInt32(compaction_trace_threshold, "60");
 CONF_Int64(vertical_compaction_max_columns_per_group, "5");
 
 CONF_Bool(enable_event_based_compaction_framework, "true");
+
+CONF_Bool(enable_check_string_lengths, "true");
 // 5GB
 CONF_mInt64(min_cumulative_compaction_size, "5368709120");
 // 20GB
