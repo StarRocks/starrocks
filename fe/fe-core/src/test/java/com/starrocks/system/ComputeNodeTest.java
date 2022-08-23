1ec8c773c4d5cad1b96c63cbd56d61773348417a
waittting <491771532@qq.com>
2022-08-23 16:56:14 +0800

    Add case

--- fe/fe-core/src/test/java/com/starrocks/system/ComputeNodeTest.java
+++ fe/fe-core/src/test/java/com/starrocks/system/ComputeNodeTest.java
@@ -17,4 +17,18 @@ public class ComputeNodeTest {
         boolean needSync = node.handleHbResponse(hbResponse);
         Assert.assertTrue(needSync);
     }
+
+    @Test
+    public void testUpdateStartTime() {
+
+        BackendHbResponse hbResponse = new BackendHbResponse();
+        hbResponse.status = HbStatus.OK;
+        hbResponse.setFirstHeartbeat(true);
+        hbResponse.setIsSetFirstHeartbeat(true);
+        hbResponse.hbTime = 1001L;
+        ComputeNode node = new ComputeNode();
+        boolean needSync = node.handleHbResponse(hbResponse);
+        Assert.assertTrue(node.getLastStartTime() == 1001L);    
+        Assert.assertTrue(needSync);
+    }
 }
