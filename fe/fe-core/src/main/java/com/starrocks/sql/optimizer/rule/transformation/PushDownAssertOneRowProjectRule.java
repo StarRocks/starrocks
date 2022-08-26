0a945a8b80abaed3b50d12ed4e4f4238e6c4d86d
Seaven <seaven_7@qq.com>
2022-08-26 19:55:17 +0800

    [BugFix] add check when push down assertNode on project (#10455)

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/PushDownAssertOneRowProjectRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/PushDownAssertOneRowProjectRule.java
@@ -6,15 +6,29 @@ import com.google.common.collect.Lists;
 import com.starrocks.sql.optimizer.OptExpression;
 import com.starrocks.sql.optimizer.OptimizerContext;
 import com.starrocks.sql.optimizer.operator.OperatorType;
+import com.starrocks.sql.optimizer.operator.logical.LogicalAggregationOperator;
 import com.starrocks.sql.optimizer.operator.pattern.Pattern;
 import com.starrocks.sql.optimizer.rule.RuleType;
 
 import java.util.List;
 
+// @todo: remove the rule directly
 public class PushDownAssertOneRowProjectRule extends TransformationRule {
     public PushDownAssertOneRowProjectRule() {
         super(RuleType.TF_PUSH_DOWN_ASSERT_ONE_ROW_PROJECT, Pattern.create(OperatorType.LOGICAL_ASSERT_ONE_ROW)
                 .addChildren(Pattern.create(OperatorType.LOGICAL_PROJECT, OperatorType.LOGICAL_AGGR)));
+    }
+
+    @Override
+    public boolean check(OptExpression input, OptimizerContext context) {
+        // if child is aggregation and none grouping key, remove AssertOneRow node
+        if (OperatorType.LOGICAL_AGGR.equals(input.getInputs().get(0).getInputs().get(0).getOp().getOpType())) {
+            LogicalAggregationOperator lao =
+                    (LogicalAggregationOperator) input.getInputs().get(0).getInputs().get(0).getOp();
+
+            return lao.getGroupingKeys().isEmpty() && (lao.getPredicate() == null);
+        }
+        return false;
     }
 
     @Override
