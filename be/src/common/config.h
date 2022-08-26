b83e0f7c665cba0a1f3100d2da7ae1484e79f4c1
Zaorang Yang <zaorangy@gmail.com>
2022-08-26 19:31:26 +0800

    [Enhancement] open the page cache by default

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -249,10 +249,10 @@ CONF_Int32(min_file_descriptor_number, "60000");
 CONF_Int64(index_stream_cache_capacity, "10737418240");
 // CONF_Int64(max_packed_row_block_size, "20971520");
 
-// Cache for stoage page size
+// Cache for storage page size
 CONF_String(storage_page_cache_limit, "0");
 // whether to disable page cache feature in storage
-CONF_Bool(disable_storage_page_cache, "true");
+CONF_Bool(disable_storage_page_cache, "false");
 // whether to disable column pool
 CONF_Bool(disable_column_pool, "false");
 
