a904a54a8e26636c9481582a87b7d65eaf570cdc
trueeyu <lxhhust350@qq.com>
2022-08-26 18:55:55 +0800

    [Enhancement] Optimize the mem usage of task worker pool (#10431)
    Convert the agent task to a specific task and put it into the task queue. According to the test results, for tasks like drop tablet, the memory usage is 1/30 of the previous

--- be/src/agent/agent_common.h
+++ be/src/agent/agent_common.h
@@ -33,4 +33,61 @@ enum class TaskWorkerType {
     UPDATE_TABLET_META_INFO
 };
 
+template <class TReq>
+struct AgentTaskRequestWithReqBody {
+    AgentTaskRequestWithReqBody(const TAgentTaskRequest& task, const TReq& t_req, time_t ts) {
+        isset = task.__isset;
+        protocol_version = task.protocol_version;
+        task_type = task.task_type;
+        signature = task.signature;
+        priority = task.priority;
+        recv_time = ts;
+        isset.recv_time = true;
+        task_req = t_req;
+    }
+
+    _TAgentTaskRequest__isset isset;
+    TAgentServiceVersion::type protocol_version;
+    TTaskType::type task_type;
+    int64_t signature;
+    TPriority::type priority;
+    int64_t recv_time;
+    TReq task_req;
+};
+
+struct AgentTaskRequestWithoutReqBody {
+    explicit AgentTaskRequestWithoutReqBody(const TAgentTaskRequest& task, time_t ts) {
+        isset = task.__isset;
+        protocol_version = task.protocol_version;
+        task_type = task.task_type;
+        signature = task.signature;
+        priority = task.priority;
+        recv_time = ts;
+        isset.recv_time = true;
+    }
+
+    _TAgentTaskRequest__isset isset;
+    TAgentServiceVersion::type protocol_version;
+    TTaskType::type task_type;
+    int64_t signature;
+    TPriority::type priority;
+    int64_t recv_time;
+};
+
+using CreateTabletAgentTaskRequest = AgentTaskRequestWithReqBody<TCreateTabletReq>;
+using DropTabletAgentTaskRequest = AgentTaskRequestWithReqBody<TDropTabletReq>;
+using PushReqAgentTaskRequest = AgentTaskRequestWithReqBody<TPushReq>;
+using PublishVersionAgentTaskRequest = AgentTaskRequestWithReqBody<TPublishVersionRequest>;
+using ClearTransactionAgentTaskRequest = AgentTaskRequestWithReqBody<TClearTransactionTaskRequest>;
+using AlterTabletAgentTaskRequest = AgentTaskRequestWithReqBody<TAlterTabletReqV2>;
+using CloneAgentTaskRequest = AgentTaskRequestWithReqBody<TCloneReq>;
+using StorageMediumMigrateTaskRequest = AgentTaskRequestWithReqBody<TStorageMediumMigrateReq>;
+using CheckConsistencyTaskRequest = AgentTaskRequestWithReqBody<TCheckConsistencyReq>;
+using UploadAgentTaskRequest = AgentTaskRequestWithReqBody<TUploadReq>;
+using DownloadAgentTaskRequest = AgentTaskRequestWithReqBody<TDownloadReq>;
+using SnapshotAgentTaskRequest = AgentTaskRequestWithReqBody<TSnapshotRequest>;
+using ReleaseSnapshotAgentTaskRequest = AgentTaskRequestWithReqBody<TReleaseSnapshotRequest>;
+using MoveDirAgentTaskRequest = AgentTaskRequestWithReqBody<TMoveDirReq>;
+using UpdateTabletMetaInfoAgentTaskRequest = AgentTaskRequestWithReqBody<TUpdateTabletMetaInfoReq>;
+
 } // namespace starrocks
