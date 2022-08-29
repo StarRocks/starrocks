937983565dffcc551d147ea5e85e9446d937328b
trueeyu <lxhhust350@qq.com>
2022-08-30 00:30:28 +0800

    [Refactor] change tablet_meta_mem_tracker to metadata_mem_tracker (#10466)
    In some user clusters, the metadata memory usage is too high, but our current metadata memory statistics are not detailed enough, which makes it difficult to troubleshoot the problem.
    
    Therefore, more fine-grained statistical metadata memory is required. such as detail mem statistics for tablet(tablet_meta, tablet_schema, tablet_struct)/rowset(rowset_meta, rowset_struct)/segment/column/index
    
    But tablet_meta_mem_bytes is ambiguous, so it is changed to metadata_mem_bytes.
    Currently, StarRocksManager has not collected these metrics, so there will be no compatibility issues.
    The pr is the first step, change tablet_meta_mem_tracker to metadata_mem_tracker
    
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/storage/tablet_manager.h
+++ be/src/storage/tablet_manager.h
@@ -147,8 +147,7 @@ public:
     Status delete_shutdown_tablet(int64_t tablet_id);
 
     Status delete_shutdown_tablet_before_clone(int64_t tablet_id);
-
-    MemTracker* tablet_meta_mem_tracker() { return _mem_tracker; }
+    MemTracker* metadata_mem_tracker() { return _mem_tracker; }
 
 private:
     using TabletMap = std::unordered_map<int64_t, TabletSharedPtr>;
