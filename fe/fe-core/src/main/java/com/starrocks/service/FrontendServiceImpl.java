2bdd394a45ffd305794a8bf6eebb6f9507960915
sduzh <zhuming9011@qq.com>
2022-09-01 11:12:04 +0800

    [Build] Fix compiler error

--- fe/fe-core/src/main/java/com/starrocks/service/FrontendServiceImpl.java
+++ fe/fe-core/src/main/java/com/starrocks/service/FrontendServiceImpl.java
@@ -166,6 +166,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.thrift.TException;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
