c364aca3d9d423f4ebf4d3682df9b21ad7c99d46
adzfolc <48429020+adzfolc@users.noreply.github.com>
2022-09-01 23:04:35 +0800

    [Feature]if fe rollback version, support unmerged catalog types (#10198)
    (cherry picked from commit b8616b8e65f6f97d54ff8d3fe12ee31deedf7d67)

--- fe/fe-core/src/main/java/com/starrocks/server/CatalogMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/server/CatalogMgr.java
@@ -135,6 +135,11 @@ public class CatalogMgr {
         if (Strings.isNullOrEmpty(type)) {
             throw new DdlException("Missing properties 'type'");
         }
+        if (!CreateCatalogStmt.SUPPORTED_CATALOG.contains(type)) {
+            // if catalog type is not supported, skip it
+            LOG.warn("Replay catalog encounter unknown catalog type: " + type);
+            return;
+        }
 
         readLock();
         try {
