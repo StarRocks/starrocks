2eb65ff536961acda1a563f693b0d01debd421e8
zhoukang <zhoukang199191@gmail.com>
2022-09-05 17:20:56 +0800

    [BugFix] Fix condition will always be true error when check functionset name in mv (#10459)
    (cherry picked from commit 14473a8bc3260fa216e6ed0f38839e026e13ef31)

--- fe/fe-core/src/test/java/com/starrocks/sql/optimizer/rule/mv/MaterializedViewRuleTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/optimizer/rule/mv/MaterializedViewRuleTest.java
@@ -8,9 +8,18 @@ import com.starrocks.server.GlobalStateMgr;
 import com.starrocks.sql.plan.ExecPlan;
 import com.starrocks.sql.plan.PlanTestBase;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class MaterializedViewRuleTest extends PlanTestBase {
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        PlanTestBase.beforeClass();
+        starRocksAssert.withMaterializedView("CREATE MATERIALIZED VIEW lo_count_key_mv as " +
+                "select LO_ORDERDATE, LO_ORDERKEY, count(LO_LINENUMBER) from lineorder_flat_for_mv" +
+                " group by LO_ORDERDATE, LO_ORDERKEY;");
+    }
+
     @Test
     public void testMaterializedViewWithCountSelection() throws Exception {
         String sql = "select LO_ORDERDATE,count(LO_LINENUMBER) from lineorder_flat_for_mv group by LO_ORDERDATE;";
@@ -35,16 +44,12 @@ public class MaterializedViewRuleTest extends PlanTestBase {
         Table table = database.getTable("lineorder_flat_for_mv");
         OlapTable baseTable = (OlapTable) table;
 
-        String sql = "select count(LO_ORDERDATE) from lineorder_flat_for_mv group by LO_ORDERDATE;";
+        String sql = "select LO_ORDERDATE, sum(case when LO_ORDERKEY=0 then 0 else 1 end) as test, " +
+                "sum(case when LO_ORDERKEY=1 then 1 else 0 end) as nontest " +
+                " from lineorder_flat_for_mv group by LO_ORDERDATE;";
         ExecPlan plan = getExecPlan(sql);
         OlapScanNode olapScanNode = (OlapScanNode) plan.getScanNodes().get(0);
         Long selectedIndexid = olapScanNode.getSelectedIndexId();
-        Assert.assertNotEquals(baseTable.getIndexIdByName("lo_count_mv"), selectedIndexid);
-
-        sql = "select max(LO_ORDERDATE) from lineorder_flat_for_mv group by LO_ORDERDATE;";
-        plan = getExecPlan(sql);
-        olapScanNode = (OlapScanNode) plan.getScanNodes().get(0);
-        selectedIndexid = olapScanNode.getSelectedIndexId();
-        Assert.assertEquals(baseTable.getIndexIdByName("lo_count_mv"), selectedIndexid);
+        Assert.assertNotEquals(baseTable.getIndexIdByName("lo_count_key_mv"), selectedIndexid);
     }
 }
