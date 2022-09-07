e506f3c040e8035c87dc74a1ae83d955f8ddebc8
xyz <a997647204@gmail.com>
2022-09-07 11:01:46 +0800

    [Feature] support report for large dataset (#10232)
    1. add a progress report for long-run broker load and insert it into in pipeline and non pipeline model
    2. broker load progress = read_bytes (file scan node) / file_bytes * 100
    3. insert into select progress = read_row (from all olap scan node) / estimate_rows for all olap scan node(may be inaccurate)
    4. insert into values progress = read_row (olap sink node) / values cardinal

---
+++ be/src/runtime/profile_report_worker.h
@@ -0,0 +1,80 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Inc.
+#pragma once
+#include <thread>
+
+#include "gen_cpp/InternalService_types.h"
+#include "gen_cpp/Types_types.h"
+#include "runtime/exec_env.h"
+#include "util/thread.h"
+
+namespace starrocks {
+
+class TUniqueId;
+
+struct NonPipelineReportTask {
+    NonPipelineReportTask(int64_t last_report_time, TQueryType::type task_type)
+            : last_report_time(last_report_time), task_type(task_type) {}
+
+    int64_t last_report_time;
+    TQueryType::type task_type;
+};
+
+struct PipelineReportTask {
+    PipelineReportTask(int64_t last_report_time, TQueryType::type task_type)
+            : last_report_time(last_report_time), task_type(task_type) {}
+
+    int64_t last_report_time;
+    TQueryType::type task_type;
+};
+
+struct PipeLineReportTaskKey {
+    PipeLineReportTaskKey(const TUniqueId& query_id, const TUniqueId& fragment_instance_id)
+            : query_id(query_id), fragment_instance_id(fragment_instance_id) {}
+
+    TUniqueId query_id;
+    TUniqueId fragment_instance_id;
+};
+
+struct PipeLineReportTaskKeyEqual {
+    bool operator()(const PipeLineReportTaskKey& k1, const PipeLineReportTaskKey& k2) const {
+        return k1.query_id == k2.query_id && k1.fragment_instance_id == k2.fragment_instance_id;
+    }
+};
+
+struct PipeLineReportTaskKeyHasher {
+    std::size_t operator()(const PipeLineReportTaskKey& key) const {
+        return (((std::hash<int64_t>()(key.query_id.lo) + (std::hash<int64_t>()(key.query_id.hi) >> 4)) +
+                 (std::hash<int64_t>()(key.fragment_instance_id.hi) >> 8)) +
+                (std::hash<int64_t>()(key.fragment_instance_id.hi) >> 12));
+    }
+};
+
+class ProfileReportWorker {
+public:
+    ProfileReportWorker(ExecEnv* env);
+    ~ProfileReportWorker();
+    void execute();
+    Status register_non_pipeline_load(const TUniqueId& fragment_instance_id);
+    Status unregister_non_pipeline_load(const TUniqueId& fragment_instance_id);
+
+    Status register_pipeline_load(const TUniqueId& query_id, const TUniqueId& fragment_instance_id);
+    Status unregister_pipeline_load(const TUniqueId& query_id, const TUniqueId& fragment_instance_id);
+
+private:
+    void _start_report_profile();
+
+    std::unordered_map<PipeLineReportTaskKey, PipelineReportTask, PipeLineReportTaskKeyHasher,
+                       PipeLineReportTaskKeyEqual>
+            _pipeline_report_tasks;
+    std::mutex _pipeline_report_mutex;
+
+    std::unordered_map<TUniqueId, NonPipelineReportTask> _non_pipeline_report_tasks;
+    std::mutex _non_pipeline_report_mutex;
+
+    ExecEnv* _exec_env;
+    std::thread _thread;
+
+    std::atomic<bool> _stop;
+};
+
+} // namespace starrocks
--- fe/fe-core/src/main/java/com/starrocks/qe/Coordinator.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/Coordinator.java
@@ -88,6 +88,7 @@ import com.starrocks.thrift.TExecBatchPlanFragmentsParams;
 import com.starrocks.thrift.TExecPlanFragmentParams;
 import com.starrocks.thrift.THdfsScanRange;
 import com.starrocks.thrift.TInternalScanRange;
+import com.starrocks.thrift.TLoadJobType;
 import com.starrocks.thrift.TNetworkAddress;
 import com.starrocks.thrift.TPipelineProfileLevel;
 import com.starrocks.thrift.TPlanFragmentDestination;
@@ -345,8 +346,16 @@ public class Coordinator {
         this.queryId = queryId;
     }
 
+    public void setJobId(Long jobId) {
+        this.jobId = jobId;
+    }
+
     public void setQueryType(TQueryType type) {
         this.queryOptions.setQuery_type(type);
+    }
+
+    public void setLoadJobType(TLoadJobType type) {
+        this.queryOptions.setLoad_job_type(type);
     }
 
     public Status getExecStatus() {
@@ -2194,10 +2203,11 @@ public class Coordinator {
             profileDoneSignal.markedCountDown(params.getFragment_instance_id(), -1L);
         }
 
-        if (params.isSetLoaded_rows()) {
+        if (params.isSetLoaded_rows() && params.isSetLoaded_bytes()) {
+
             GlobalStateMgr.getCurrentState().getLoadManager().updateJobPrgress(
                     jobId, params.backend_id, params.query_id, params.fragment_instance_id, params.loaded_rows,
-                    params.done);
+                    params.done, params.loaded_bytes);
         }
     }
 
