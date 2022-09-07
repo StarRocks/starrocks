fd5055dbc73f13e4955cf7ca52080b818a58dba1
zihe.liu <ziheliu1024@gmail.com>
2022-09-07 19:54:49 +0800

    [Bugfix] Connector WorkGroupScanTaskQueue uses different ShcedEntity from Olap (#10950)
    (cherry picked from commit b4c664bafc2d99e7a7a4762b85eaa4f8d3b440de)
    
    # Conflicts:
    #	be/src/runtime/exec_env.cpp

--- be/src/runtime/exec_env.cpp
+++ be/src/runtime/exec_env.cpp
@@ -187,6 +187,38 @@ Status ExecEnv::_init(const std::vector<StorePath>& store_paths) {
     _wg_driver_executor =
             new pipeline::GlobalDriverExecutor("wg_pip_exe", std::move(wg_driver_executor_thread_pool), true);
     _wg_driver_executor->initialize(_max_executor_threads);
+<<<<<<< HEAD
+=======
+
+    int connector_num_io_threads = config::pipeline_hdfs_scan_thread_pool_thread_num;
+    CHECK_GT(connector_num_io_threads, 0) << "pipeline_hdfs_scan_thread_pool_thread_num should greater than 0";
+
+    std::unique_ptr<ThreadPool> connector_scan_worker_thread_pool_without_workgroup;
+    RETURN_IF_ERROR(ThreadPoolBuilder("con_scan_io")
+                            .set_min_threads(0)
+                            .set_max_threads(connector_num_io_threads)
+                            .set_max_queue_size(1000)
+                            .set_idle_timeout(MonoDelta::FromMilliseconds(2000))
+                            .build(&connector_scan_worker_thread_pool_without_workgroup));
+    _connector_scan_executor_without_workgroup = new workgroup::ScanExecutor(
+            std::move(connector_scan_worker_thread_pool_without_workgroup),
+            std::make_unique<workgroup::PriorityScanTaskQueue>(config::pipeline_scan_thread_pool_queue_size));
+    _connector_scan_executor_without_workgroup->initialize(connector_num_io_threads);
+
+    std::unique_ptr<ThreadPool> connector_scan_worker_thread_pool_with_workgroup;
+    RETURN_IF_ERROR(ThreadPoolBuilder("con_wg_scan_io")
+                            .set_min_threads(0)
+                            .set_max_threads(connector_num_io_threads)
+                            .set_max_queue_size(1000)
+                            .set_idle_timeout(MonoDelta::FromMilliseconds(2000))
+                            .build(&connector_scan_worker_thread_pool_with_workgroup));
+    _connector_scan_executor_with_workgroup =
+            new workgroup::ScanExecutor(std::move(connector_scan_worker_thread_pool_with_workgroup),
+                                        std::make_unique<workgroup::WorkGroupScanTaskQueue>(
+                                                workgroup::WorkGroupScanTaskQueue::SchedEntityType::CONNECTOR));
+    _connector_scan_executor_with_workgroup->initialize(connector_num_io_threads);
+
+>>>>>>> b4c664baf ([Bugfix] Connector WorkGroupScanTaskQueue uses different ShcedEntity from Olap (#10950))
     starrocks::workgroup::DefaultWorkGroupInitialization default_workgroup_init;
 
     _master_info = new TMasterInfo();
@@ -224,9 +256,18 @@ Status ExecEnv::_init(const std::vector<StorePath>& store_paths) {
                                 .set_max_threads(num_io_threads)
                                 .set_max_queue_size(1000)
                                 .set_idle_timeout(MonoDelta::FromMilliseconds(2000))
+<<<<<<< HEAD
                                 .build(&scan_worker_thread_pool));
         _scan_executor = new workgroup::ScanExecutor(std::move(scan_worker_thread_pool));
         _scan_executor->initialize(num_io_threads);
+=======
+                                .build(&scan_worker_thread_pool_with_workgroup));
+        _scan_executor_with_workgroup =
+                new workgroup::ScanExecutor(std::move(scan_worker_thread_pool_with_workgroup),
+                                            std::make_unique<workgroup::WorkGroupScanTaskQueue>(
+                                                    workgroup::WorkGroupScanTaskQueue::SchedEntityType::OLAP));
+        _scan_executor_with_workgroup->initialize(num_io_threads);
+>>>>>>> b4c664baf ([Bugfix] Connector WorkGroupScanTaskQueue uses different ShcedEntity from Olap (#10950))
 
         Status status = _load_path_mgr->init();
         if (!status.ok()) {
