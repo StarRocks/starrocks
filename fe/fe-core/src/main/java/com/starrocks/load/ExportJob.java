56096358ef40476083ba37a7948938ea69760933
waittting <491771532@qq.com>
2022-09-07 10:52:15 +0800

    [Enhancement] Add logic to cancel export job when be has crashed before (#10718)
    Fe cannot perceive rebooting BE. So that the export task thread is stuck and the overall task progress is stuck at 99%.
    This PR uses lastStartTime to determine if the backend has restarted, and if it has, it will cancel the corresponding job.

--- fe/fe-core/src/main/java/com/starrocks/load/ExportJob.java
+++ fe/fe-core/src/main/java/com/starrocks/load/ExportJob.java
@@ -117,6 +117,9 @@ public class ExportJob implements Writable {
     private final AtomicInteger nextId = new AtomicInteger(0);
     // backedn_address => snapshot path
     private final List<Pair<TNetworkAddress, String>> snapshotPaths = Lists.newArrayList();
+    // backend id => backend lastStartTime 
+    private final Map<Long, Long> beLastStartTime = Maps.newHashMap();
+
     private long id;
     private UUID queryId;
     private long dbId;
@@ -404,6 +407,14 @@ public class ExportJob implements Writable {
                     id, i, DebugUtil.printId(queryId));
         }
         LOG.info("create {} coordintors for export job: {}", coordList.size(), id);
+    }
+
+    public void setBeStartTime(long beId, long lastStartTime) {
+        this.beLastStartTime.put(beId, lastStartTime);
+    }
+
+    public Map<Long, Long> getBeStartTimeMap() {
+        return this.beLastStartTime;
     }
 
     public long getId() {
