e68d769ae00913e3f9e69a2e434b6f6ffa3b62ad
HangyuanLiu <460660596@qq.com>
2022-09-09 20:23:49 +0800

    Revert "Support RoutineLoadCreate Statement in new Parser and Analyzer (#9981)" (#11069)

--- fe/fe-core/src/main/java/com/starrocks/sql/ast/AstVisitor.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/ast/AstVisitor.java
@@ -19,7 +19,6 @@ import com.starrocks.analysis.CompoundPredicate;
 import com.starrocks.analysis.CreateFunctionStmt;
 import com.starrocks.analysis.CreateMaterializedViewStmt;
 import com.starrocks.analysis.CreateRoleStmt;
-import com.starrocks.analysis.CreateRoutineLoadStmt;
 import com.starrocks.analysis.DdlStmt;
 import com.starrocks.analysis.DefaultValueExpr;
 import com.starrocks.analysis.DeleteStmt;
@@ -357,10 +356,6 @@ public abstract class AstVisitor<R, C> {
 
     public R visitDropMaterializedViewStatement(DropMaterializedViewStmt statement, C context) {
         return visitDDLStatement(statement, context);
-    }
-
-    public R visitCreateRoutineLoadStatement(CreateRoutineLoadStmt statement, C context) {
-        return visitStatement(statement, context);
     }
 
     public R visitStopRoutineLoadStatement(StopRoutineLoadStmt statement, C context) {
