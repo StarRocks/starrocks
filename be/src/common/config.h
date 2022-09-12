5fae106585051a6ff5bce49ae9c62c3b18f822c3
Alex Zhu <zhuming9011@gmail.com>
2022-09-12 09:16:35 +0800

    [BugFix][Lake] Data loss caused by unsigned subtraction overflow (#11092)

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -784,10 +784,10 @@ CONF_String(starlet_cache_dir, "");
 
 CONF_Int64(lake_metadata_cache_limit, /*2GB=*/"2147483648");
 CONF_Int64(lake_gc_metadata_max_versions, "10");
-CONF_Int64(lake_gc_metadata_check_interval, /*10 minutes=*/"600");
+CONF_Int64(lake_gc_metadata_check_interval, /*30 minutes=*/"1800");
 CONF_Int64(lake_gc_segment_check_interval, /*60 minutes=*/"3600");
 // This value should be much larger than the maximum timeout of loading/compaction/schema change jobs.
-CONF_Int64(lake_gc_segment_expire_seconds, /*1 day=*/"86400");
+CONF_Int64(lake_gc_segment_expire_seconds, /*3 days=*/"259200");
 
 CONF_mBool(dependency_librdkafka_debug_enable, "false");
 
