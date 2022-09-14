b58a3ab11b3ba806e7a500c4e8c40c49002b4216
Astralidea <astralidea@163.com>
2022-09-14 11:29:24 +0800

    Opt.

--- fe/fe-core/src/main/java/com/starrocks/scheduler/TaskManager.java
+++ fe/fe-core/src/main/java/com/starrocks/scheduler/TaskManager.java
@@ -498,21 +498,23 @@ public class TaskManager {
             }
 
             // It is possible to update out of order for priority queue.
-            Map<String, TaskRun> pendingIndex = Maps.newHashMap();
+            TaskRun pendingTaskRun = null;
+            List<TaskRun> tempQueue = Lists.newArrayList();
             while (!taskRunQueue.isEmpty()) {
                 TaskRun taskRun = taskRunQueue.poll();
-                pendingIndex.put(taskRun.getStatus().getQueryId(), taskRun);
+                if (taskRun.getStatus().getQueryId().equals(statusChange.getQueryId())) {
+                    pendingTaskRun = taskRun;
+                    break;
+                } else {
+                    tempQueue.add(taskRun);
+                }
             }
+            taskRunQueue.addAll(tempQueue);
 
-            TaskRun pendingTaskRun = pendingIndex.get(statusChange.getQueryId());
             if (pendingTaskRun == null) {
                 LOG.warn("could not find query_id:{}, taskId:{}, when replay update pendingTaskRun",
                         statusChange.getQueryId(), taskId);
-                taskRunQueue.addAll(pendingIndex.values());
                 return;
-            } else {
-                pendingIndex.remove(statusChange.getQueryId());
-                taskRunQueue.addAll(pendingIndex.values());
             }
             TaskRunStatus status = pendingTaskRun.getStatus();
 
