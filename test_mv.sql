fc911b6c55005d4b33cebea413cbc5cf440da17d
Murphy <mofei@starrocks.com>
2022-09-15 16:25:30 +0800

    test case

---
+++ test_mv.sql
@@ -0,0 +1,46 @@
+CREATE TABLE `t0` (
+  `t0_c0` int(11) NULL COMMENT "",
+  `t0_c1` string
+) ENGINE=OLAP
+DUPLICATE KEY(`t0_c0`)
+DISTRIBUTED BY HASH(`t0_c0`) BUCKETS 1
+PROPERTIES ( "replication_num" = "1");
+
+CREATE TABLE `t1` (
+  `t1_c0` int(11) NULL COMMENT "",
+  `t1_c1` string
+) ENGINE=OLAP
+DUPLICATE KEY(`t1_c0`)
+DISTRIBUTED BY HASH(`t1_c0`) BUCKETS 1
+PROPERTIES ( "replication_num" = "1");
+
+-- step 1
+create materialized view mv1  
+distributed by hash(t0_c0) 
+refresh realtime 
+as 
+select t0_c0, t1_c1
+from t0
+join t1 on t0_c0 = t1_c0;
+
+select * from mv1;
+insert into t0 values (1, 'star');
+explain insert into t0 values (1, 'star');
+select * from mv1;
+
+drop materialized view mv1;
+
+-- step 2
+-- correct view-update output columns
+create materialized view mv2
+distributed by hash(t0_c0) 
+refresh realtime 
+as 
+select t0_c0, t0_c1, t1_c0, t1_c1
+from t0
+join t1 on t0_c0 = t1_c0;
+
+
+-- cleanup
+drop table t0;
+drop table t1;
