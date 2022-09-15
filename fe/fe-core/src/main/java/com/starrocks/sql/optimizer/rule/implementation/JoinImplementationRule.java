99c2165c9846619f7263c4ff0887210182107b9c
Murphy <mofei@starrocks.com>
2022-09-15 13:30:29 +0800

    refactor join implementation

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/implementation/JoinImplementationRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/implementation/JoinImplementationRule.java
@@ -2,11 +2,35 @@
 
 package com.starrocks.sql.optimizer.rule.implementation;
 
+import com.starrocks.analysis.JoinOperator;
+import com.starrocks.sql.optimizer.JoinHelper;
+import com.starrocks.sql.optimizer.OptExpression;
+import com.starrocks.sql.optimizer.OptimizerContext;
+import com.starrocks.sql.optimizer.Utils;
+import com.starrocks.sql.optimizer.base.ColumnRefSet;
 import com.starrocks.sql.optimizer.operator.OperatorType;
+import com.starrocks.sql.optimizer.operator.logical.LogicalJoinOperator;
 import com.starrocks.sql.optimizer.operator.pattern.Pattern;
+import com.starrocks.sql.optimizer.operator.scalar.BinaryPredicateOperator;
 import com.starrocks.sql.optimizer.rule.RuleType;
 
+import java.util.List;
+
 public abstract class JoinImplementationRule extends ImplementationRule {
+
+    protected List<BinaryPredicateOperator> extractEqPredicate(OptExpression input, OptimizerContext context) {
+        ColumnRefSet leftChildColumns = input.inputAt(0).getLogicalProperty().getOutputColumns();
+        ColumnRefSet rightChildColumns = input.inputAt(1).getLogicalProperty().getOutputColumns();
+        LogicalJoinOperator joinOperator = (LogicalJoinOperator) input.getOp();
+        return JoinHelper.getEqualsPredicate(leftChildColumns, rightChildColumns,
+                Utils.extractConjuncts(joinOperator.getOnPredicate()));
+    }
+
+    protected JoinOperator getJoinType(OptExpression input) {
+        LogicalJoinOperator joinOperator = (LogicalJoinOperator) input.getOp();
+        return joinOperator.getJoinType();
+    }
+
     protected JoinImplementationRule(RuleType type) {
         super(type, Pattern.create(OperatorType.LOGICAL_JOIN).
                 addChildren(Pattern.create(OperatorType.PATTERN_LEAF), Pattern.create(OperatorType.PATTERN_LEAF)));
