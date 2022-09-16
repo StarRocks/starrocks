516b785a40caba3aa73945e6eb1235e34d08a369
Alittleben <z13131262502@163.com>
2022-09-16 11:48:45 +0800

    [SR JavaUDAF] Support function json_agg in StarRocks

--- java-extensions/udf-examples/src/main/java/com/starrocks/example/udf/JsonAgg.java
+++ java-extensions/udf-examples/src/main/java/com/starrocks/example/udf/JsonAgg.java
@@ -1,3 +1,5 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Inc.
+
 package com.starrocks.example.udf;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
