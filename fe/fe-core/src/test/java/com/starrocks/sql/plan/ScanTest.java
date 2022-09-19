StarRocks/starrocks

commit 46c63424fd140eeab259e859ace9182a2f0db066

[SR GeekCamp] Improve schema scan performance #4028 (#5124)

fe/fe-core/src/main/java/com/starrocks/sql/plan/PlanFragmentBuilder.java
@@ -110,6 +110,7 @@ import com.starrocks.sql.optimizer.operator.physical.PhysicalWindowOperator;
 import com.starrocks.sql.optimizer.operator.scalar.BinaryPredicateOperator;
 import com.starrocks.sql.optimizer.operator.scalar.CallOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ColumnRefOperator;
+import com.starrocks.sql.optimizer.operator.scalar.ConstantOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ScalarOperator;
 import com.starrocks.sql.optimizer.rewrite.AddDecodeNodeForDictStringRule.DecodeVisitor;
 import com.starrocks.sql.optimizer.statistics.Statistics;
@@ -889,6 +890,27 @@ public class PlanFragmentBuilder {
 
             for (ScalarOperator predicate : predicates) {
                 scanNode.getConjuncts().add(ScalarOperatorToExpr.buildExecExpression(predicate, formatterContext));
+                // if user set table_schema or table_name in where condition and is
+                // binary predicate operator, we can set table_schema and table_name
+                // into scan-node, which can reduce time from be to fe
+                if (predicate instanceof BinaryPredicateOperator) {
+                    if (((BinaryPredicateOperator) predicate).getBinaryType() == BinaryPredicateOperator.BinaryType.EQ) {
+                        if (predicate.getChildren().get(0) instanceof ColumnRefOperator) {
+                            ColumnRefOperator columnRefOperator = (ColumnRefOperator) predicate.getChildren().get(0);
+                            ConstantOperator constantOperator = (ConstantOperator) predicate.getChildren().get(1);
+                            switch (columnRefOperator.getName()) {
+                                case "TABLE_SCHEMA":
+                                    scanNode.setSchemaDb(constantOperator.getVarchar());
+                                    break;
+                                case "TABLE_NAME":
+                                    scanNode.setSchemaTable(constantOperator.getVarchar());
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+                }
             }
 
             context.getScanNodes().add(scanNode);

fe/fe-core/src/test/java/com/starrocks/sql/plan/ScanTest.java
@@ -3,6 +3,7 @@
 package com.starrocks.sql.plan;
 
 import com.starrocks.common.FeConstants;
+import com.starrocks.planner.SchemaScanNode;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -354,5 +355,14 @@ public class ScanTest extends PlanTestBase {
         String plan = getFragmentPlan(sql);
         Assert.assertTrue(plan.contains("<slot 4> : nullif(1: v1, 1: v1) + 0"));
         Assert.assertTrue(plan.contains(" OUTPUT EXPRS:4: expr | 4: expr"));
+    }
+
+    @Test
+    public void testSchemaScanWithWhere() throws Exception {
+        String sql = "select column_name, table_name from information_schema.columns" +
+                " where table_schema = 'information_schema' and table_name = 'columns'";
+        ExecPlan plan = getExecPlan(sql);
+        Assert.assertTrue(((SchemaScanNode) plan.getScanNodes().get(0)).getSchemaDb().equals("information_schema"));
+        Assert.assertTrue(((SchemaScanNode) plan.getScanNodes().get(0)).getSchemaTable().equals("columns"));
     }
 }

be/src/exec/vectorized/schema_scan_node.cpp
@@ -63,6 +63,10 @@ Status SchemaScanNode::init(const TPlanNode& tnode, RuntimeState* state) {
     if (tnode.schema_scan_node.__isset.thread_id) {
         _scanner_param.thread_id = tnode.schema_scan_node.thread_id;
     }
+
+    if (tnode.limit > 0) {
+        _scanner_param.limit = tnode.limit;
+    }
     return Status::OK();
 }
 

be/src/exec/vectorized/schema_scanner.h
@@ -31,6 +31,7 @@ struct SchemaScannerParam {
     const std::string* ip{nullptr};                   // frontend ip
     int32_t port{0};                                  // frontend thrift port
     int64_t thread_id = 0;
+    int64_t limit{0};
 
     RuntimeProfile::Counter* _rpc_timer = nullptr;
     RuntimeProfile::Counter* _fill_chunk_timer = nullptr;

be/src/exec/vectorized/schema_scanner/schema_columns_scanner.cpp
@@ -460,6 +460,10 @@ Status SchemaColumnsScanner::get_new_desc() {
         }
     }
 
+    if (_param->limit > 0) {
+        desc_params.__set_limit(_param->limit);
+    }
+
     if (nullptr != _param->ip && 0 != _param->port) {
         RETURN_IF_ERROR(SchemaHelper::describe_table(*(_param->ip), _param->port, desc_params, &_desc_result));
     } else {

be/src/exec/vectorized/schema_scanner/schema_tables_scanner.cpp
@@ -295,6 +295,9 @@ Status SchemaTablesScanner::get_new_table() {
     if (nullptr != _param->wild) {
         table_params.__set_pattern(*(_param->wild));
     }
+    if (nullptr != _param->table) {
+        table_params.__set_pattern(*(_param->table));
+    }
     if (nullptr != _param->current_user_ident) {
         table_params.__set_current_user_ident(*(_param->current_user_ident));
     } else {
@@ -304,6 +307,9 @@ Status SchemaTablesScanner::get_new_table() {
         if (nullptr != _param->user_ip) {
             table_params.__set_user_ip(*(_param->user_ip));
         }
+    }
+    if (_param->limit > 0) {
+        table_params.__set_limit(_param->limit);
     }
 
     if (nullptr != _param->ip && 0 != _param->port) {

fe/fe-core/src/main/java/com/starrocks/planner/SchemaScanNode.java
@@ -57,6 +57,14 @@ public class SchemaScanNode extends ScanNode {
     private String frontendIP;
     private int frontendPort;
 
+    public void setSchemaDb(String schemaDb) { this.schemaDb = schemaDb; }
+
+    public void setSchemaTable(String schemaTable) { this.schemaTable = schemaTable; }
+
+    public String getSchemaDb() { return schemaDb; }
+
+    public String getSchemaTable() { return schemaTable; }
+
     public void setUser(String user) {
         this.user = user;
     }

fe/fe-core/src/main/java/com/starrocks/service/FrontendServiceImpl.java
@@ -263,6 +263,7 @@ public class FrontendServiceImpl implements FrontendService.Iface {
         // database privs should be checked in analysis phrase
 
         Database db = Catalog.getCurrentCatalog().getDb(params.db);
+        long limit = params.isSetLimit() ? params.getLimit() : -1;
         UserIdentity currentUser = null;
         if (params.isSetCurrent_user_ident()) {
             currentUser = UserIdentity.fromThrift(params.current_user_ident);
@@ -305,6 +306,10 @@ public class FrontendServiceImpl implements FrontendService.Iface {
                         status.setDdl_sql(ddlSql);
                     }
                     tablesResult.add(status);
+                    // if user set limit, then only return limit size result
+                    if (limit > 0 && tablesResult.size() >= limit) {
+                        break;
+                    }
                 }
             } finally {
                 db.readUnlock();
@@ -471,6 +476,7 @@ public class FrontendServiceImpl implements FrontendService.Iface {
             return result;
         }
         Database db = Catalog.getCurrentCatalog().getDb(params.db);
+        long limit = params.isSetLimit() ? params.getLimit() : -1;
         if (db != null) {
             db.readLock();
             try {
@@ -508,6 +514,10 @@ public class FrontendServiceImpl implements FrontendService.Iface {
                             colDef.setComment(comment);
                         }
                         columns.add(colDef);
+                        // if user set limit, then only return limit size result
+                        if (limit > 0 && columns.size() >= limit) {
+                            break;
+                        }
                     }
                 }
             } finally {

commit 480fb6879e852d5864d138670da1ab7438174a17

[BugFix] Fix bug query INFORMATION_SCHEMA when eq database() (#11365)
This was introduced in #5124, mainly because the judgment was not complete, and it was thought that the right side could only be ConstantOperator, but it might actually be CallOperator

fe/fe-core/src/main/java/com/starrocks/sql/plan/PlanFragmentBuilder.java
@@ -864,7 +864,8 @@ public class PlanFragmentBuilder {
                 if (predicate instanceof BinaryPredicateOperator) {
                     if (((BinaryPredicateOperator) predicate).getBinaryType() ==
                             BinaryPredicateOperator.BinaryType.EQ) {
-                        if (predicate.getChildren().get(0) instanceof ColumnRefOperator) {
+                        if (predicate.getChildren().get(0) instanceof ColumnRefOperator &&
+                                predicate.getChildren().get(1) instanceof ConstantOperator) {
                             ColumnRefOperator columnRefOperator = (ColumnRefOperator) predicate.getChildren().get(0);
                             ConstantOperator constantOperator = (ConstantOperator) predicate.getChildren().get(1);
                             switch (columnRefOperator.getName()) {

fe/fe-core/src/test/java/com/starrocks/sql/plan/ScanTest.java
@@ -370,4 +370,15 @@ public class ScanTest extends PlanTestBase {
         Assert.assertTrue(((SchemaScanNode) plan.getScanNodes().get(0)).getSchemaDb().equals("information_schema"));
         Assert.assertTrue(((SchemaScanNode) plan.getScanNodes().get(0)).getSchemaTable().equals("columns"));
     }
+
+    @Test
+    public void testSchemaScanWithWhereConstantFunction() throws Exception {
+        String sql = "SELECT TABLE_SCHEMA TABLE_CAT, NULL TABLE_SCHEM, TABLE_NAME, " +
+                "IF(TABLE_TYPE='BASE TABLE' or TABLE_TYPE='SYSTEM VERSIONED', 'TABLE', TABLE_TYPE) as TABLE_TYPE, " +
+                "TABLE_COMMENT REMARKS, NULL TYPE_CAT, NULL TYPE_SCHEM, NULL TYPE_NAME, NULL SELF_REFERENCING_COL_NAME, " +
+                "NULL REF_GENERATION FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = database() AND  " +
+                "TABLE_TYPE IN ('BASE TABLE','SYSTEM VERSIONED','PARTITIONED TABLE','VIEW','FOREIGN TABLE'," +
+                "'MATERIALIZED VIEW','EXTERNAL TABLE') ORDER BY TABLE_TYPE, TABLE_SCHEMA, TABLE_NAME";
+        getExecPlan(sql);
+    }
 }