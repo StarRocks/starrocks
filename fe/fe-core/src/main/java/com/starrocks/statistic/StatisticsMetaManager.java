5c53a5f15f83f3cf73ca1de54f10fe00b0a9ee96
abc982627271 <liuxuefen@starrocks.com>
2022-09-20 20:18:25 +0800

    upt

--- fe/fe-core/src/main/java/com/starrocks/statistic/StatisticsMetaManager.java
+++ fe/fe-core/src/main/java/com/starrocks/statistic/StatisticsMetaManager.java
@@ -122,9 +122,8 @@ public class StatisticsMetaManager extends LeaderDaemon {
         int defaultReplicationNum = Math.min(3, GlobalStateMgr.getCurrentSystemInfo().getTotalBackendNumber());
         properties.put(PropertyAnalyzer.PROPERTIES_REPLICATION_NUM, Integer.toString(defaultReplicationNum));
         // if use_staros, create lake table
-        CreateTableStmt stmt = null;
         String engine = Config.use_staros ? CreateTableStmt.LAKE_ENGINE_NAME : "olap";
-        stmt = new CreateTableStmt(false, false,
+        CreateTableStmt stmt = new CreateTableStmt(false, false,
                 tableName,
                 StatisticUtils.buildStatsColumnDef(StatsConstants.SAMPLE_STATISTICS_TABLE_NAME),
                 engine,
@@ -154,9 +153,8 @@ public class StatisticsMetaManager extends LeaderDaemon {
         int defaultReplicationNum = Math.min(3, GlobalStateMgr.getCurrentSystemInfo().getTotalBackendNumber());
         properties.put(PropertyAnalyzer.PROPERTIES_REPLICATION_NUM, Integer.toString(defaultReplicationNum));
         // if use_staros, create lake table
-        CreateTableStmt stmt = null;
         String engine = Config.use_staros ? CreateTableStmt.LAKE_ENGINE_NAME : "olap";
-        stmt = new CreateTableStmt(false, false,
+        CreateTableStmt stmt = new CreateTableStmt(false, false,
                 tableName,
                 StatisticUtils.buildStatsColumnDef(StatsConstants.FULL_STATISTICS_TABLE_NAME),
                 engine,
@@ -186,9 +184,8 @@ public class StatisticsMetaManager extends LeaderDaemon {
         int defaultReplicationNum = Math.min(3, GlobalStateMgr.getCurrentSystemInfo().getTotalBackendNumber());
         properties.put(PropertyAnalyzer.PROPERTIES_REPLICATION_NUM, Integer.toString(defaultReplicationNum));
         // if use_staros, create lake table
-        CreateTableStmt stmt = null;
         String engine = Config.use_staros ? CreateTableStmt.LAKE_ENGINE_NAME : "olap";
-        stmt = new CreateTableStmt(false, false,
+        CreateTableStmt stmt = new CreateTableStmt(false, false,
                 tableName,
                 StatisticUtils.buildStatsColumnDef(StatsConstants.HISTOGRAM_STATISTICS_TABLE_NAME),
                 engine,
