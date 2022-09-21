3c9d3051044bbcbfb540108791637d9bd246e122
HangyuanLiu <460660596@qq.com>
2022-09-21 19:42:39 +0800

    Fix predicate push down to external table miss NOT predicate

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/ScanTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/ScanTest.java
@@ -383,9 +383,17 @@ public class ScanTest extends PlanTestBase {
     }
 
     @Test
-    public void test() throws Exception {
+    public void testPushDownExternalTableMissNot() throws Exception {
         String sql = "select * from ods_order where order_no not like \"%hehe%\"";
         String plan = getFragmentPlan(sql);
-        System.out.println(plan);
+        assertContains(plan, "predicates: NOT (order_no LIKE '%hehe%')");
+
+        sql = "select * from ods_order where order_no in (1,2,3)";
+        plan = getFragmentPlan(sql);
+        assertContains(plan, "FROM `ods_order` WHERE (order_no IN ('1', '2', '3'))");
+
+        sql = "select * from ods_order where order_no not in (1,2,3)";
+        plan = getFragmentPlan(sql);
+        assertContains(plan, "FROM `ods_order` WHERE (order_no NOT IN ('1', '2', '3'))");
     }
 }
