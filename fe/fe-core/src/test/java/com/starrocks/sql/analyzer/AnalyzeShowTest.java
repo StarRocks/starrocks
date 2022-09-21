43aab143a7d12b7c72565ca107b07d1dc5534dec
padmejin <89557328+padmejin@users.noreply.github.com>
2022-09-21 08:41:57 +0800

    [BugFix] Add predicate on database for `SHOW TABLES WHERE` (#11411)

--- fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzeShowTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzeShowTest.java
@@ -57,7 +57,9 @@ public class AnalyzeShowTest {
     public void testShowTables() throws AnalysisException {
         analyzeSuccess("show tables;");
         ShowStmt statement = (ShowStmt) analyzeSuccess("show tables where table_name = 't1';");
-        Assert.assertEquals("SELECT TABLE_NAME AS Tables_in_test FROM information_schema.tables WHERE table_name = 't1'",
+        Assert.assertEquals(
+                "SELECT TABLE_NAME AS Tables_in_test FROM information_schema.tables"
+                        + " WHERE (TABLE_SCHEMA = 'test') AND (table_name = 't1')",
                 AST2SQL.toString(statement.toSelectStmt()));
         analyzeSuccess("show tables from `test` like 'test'");
     }
