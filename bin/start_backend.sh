7071dfd3368be0db59fef146c4a0661152bb34cf
eyes_on_me <3675229+silverbullet233@users.noreply.github.com>
2022-09-22 16:59:30 +0800

    [Enhancement] disable jemalloc prof and update some other configs (#11541)

--- bin/start_backend.sh
+++ bin/start_backend.sh
@@ -67,7 +67,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-export JEMALLOC_CONF="percpu_arena:percpu,oversize_threshold:0,muzzy_decay_ms:30000,dirty_decay_ms:30000,lg_tcache_max:23,metadata_thp:auto,background_thread:true,prof:true"
+export JEMALLOC_CONF="percpu_arena:percpu,oversize_threshold:0,muzzy_decay_ms:5000,dirty_decay_ms:5000,metadata_thp:auto,background_thread:true"
 
 # enable coredump when BE build with ASAN
 export ASAN_OPTIONS=abort_on_error=1:disable_coredump=0:unmap_shadow_on_exit=1
