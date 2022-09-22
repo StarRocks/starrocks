StarRocks/starrocks

commit 2541515967d80f4190abf5c73372183515f2a0d5

[Feature] Support default uuid()/uuid_numeric() for StreamLoad/BrokerLoad/InsertInto/RoutineLoad (#11155)

fe/fe-core/src/main/java/com/starrocks/catalog/DefaultExpr.java
@@ -1,9 +1,19 @@
 // This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Inc.
 package com.starrocks.catalog;
 
+import com.clearspring.analytics.util.Lists;
+import com.google.common.collect.ImmutableSet;
 import com.google.gson.annotations.SerializedName;
+import com.starrocks.analysis.Expr;
+import com.starrocks.analysis.FunctionCallExpr;
+import com.starrocks.analysis.FunctionName;
+
+import java.util.Set;
 
 public class DefaultExpr {
+
+    public static final Set<String> SUPPORTED_DEFAULT_FNS = ImmutableSet.of("uuid()", "uuid_numeric()");
+
     @SerializedName("expr")
     private String expr;
 
@@ -17,5 +27,17 @@ public class DefaultExpr {
 
     public void setExpr(String expr) {
         this.expr = expr;
+    }
+
+    public Expr obtainExpr() {
+        if (SUPPORTED_DEFAULT_FNS.contains(expr)) {
+            String functionName = expr.replace("()", "");
+            FunctionCallExpr functionCallExpr = new FunctionCallExpr(new FunctionName(functionName), Lists.newArrayList());
+            Function fn = Expr.getBuiltinFunction(functionName, new Type[] {}, Function.CompareMode.IS_IDENTICAL);
+            functionCallExpr.setFn(fn);
+            functionCallExpr.setType(fn.getReturnType());
+            return functionCallExpr;
+        }
+        return null;
     }
 }

fe/fe-core/src/test/java/com/starrocks/catalog/CreateTableTest.java
@@ -391,4 +391,92 @@ public class CreateTableTest {
         Assert.assertNotNull(
                 table.getColumn("oh_my_gosh_this_is_a_long_column_name_look_at_it_it_has_more_than_64_chars"));
     }
+
+    @Test
+    public void testCreateTableDefaultUUID() throws Exception {
+        StarRocksAssert starRocksAssert = new StarRocksAssert(connectContext);
+        starRocksAssert.useDatabase("test");
+        String sql = "CREATE TABLE `test_create_default_uuid` (\n" +
+                "    k1 int,\n" +
+                "    uuid VARCHAR(36) NOT NULL DEFAULT (uuid())\n" +
+                ") ENGINE=OLAP\n" +
+                "DUPLICATE KEY(`k1`)\n" +
+                "COMMENT \"OLAP\"\n" +
+                "DISTRIBUTED BY HASH(`k1`) BUCKETS 2\n" +
+                "PROPERTIES (\n" +
+                "    \"replication_num\" = \"1\",\n" +
+                "    \"in_memory\" = \"false\",\n" +
+                "    \"storage_format\" = \"DEFAULT\"\n" +
+                ");";
+        starRocksAssert.withTable(sql);
+        final Table table = starRocksAssert.getCtx().getGlobalStateMgr().getDb(connectContext.getDatabase())
+                .getTable("test_create_default_uuid");
+        Assert.assertEquals(2, table.getColumns().size());
+
+        String sql2 = "CREATE TABLE `test_create_default_uuid_numeric` (\n" +
+                "    k1 int,\n" +
+                "    uuid LARGEINT NOT NULL DEFAULT (uuid_numeric())\n" +
+                ") ENGINE=OLAP\n" +
+                "DUPLICATE KEY(`k1`)\n" +
+                "COMMENT \"OLAP\"\n" +
+                "DISTRIBUTED BY HASH(`k1`) BUCKETS 2\n" +
+                "PROPERTIES (\n" +
+                "    \"replication_num\" = \"1\",\n" +
+                "    \"in_memory\" = \"false\",\n" +
+                "    \"storage_format\" = \"DEFAULT\"\n" +
+                ");";
+        starRocksAssert.withTable(sql2);
+
+        final Table table2 = starRocksAssert.getCtx().getGlobalStateMgr().getDb(connectContext.getDatabase())
+                .getTable("test_create_default_uuid_numeric");
+        Assert.assertEquals(2, table2.getColumns().size());
+    }
+
+    @Test
+    public void testCreateTableDefaultUUIDFailed() {
+        ExceptionChecker.expectThrowsWithMsg(AnalysisException.class,
+                "Varchar type length must be greater than 36 for uuid function",
+                () -> createTable("CREATE TABLE test.`test_default_uuid_size_not_enough` (\n" +
+                        "    k1 int,\n" +
+                        "    uuid VARCHAR(35) NOT NULL DEFAULT (uuid())\n" +
+                        ") ENGINE=OLAP\n" +
+                        "DUPLICATE KEY(`k1`)\n" +
+                        "COMMENT \"OLAP\"\n" +
+                        "DISTRIBUTED BY HASH(`k1`) BUCKETS 2\n" +
+                        "PROPERTIES (\n" +
+                        "    \"replication_num\" = \"1\",\n" +
+                        "    \"in_memory\" = \"false\",\n" +
+                        "    \"storage_format\" = \"DEFAULT\"\n" +
+                        ");"));
+        ExceptionChecker.expectThrowsWithMsg(AnalysisException.class,
+                "Default function uuid() for type INT is not supported",
+                () -> createTable("CREATE TABLE test.`test_default_uuid_type_not_match` (\n" +
+                        "    k1 int,\n" +
+                        "    uuid INT NOT NULL DEFAULT (uuid())\n" +
+                        ") ENGINE=OLAP\n" +
+                        "DUPLICATE KEY(`k1`)\n" +
+                        "COMMENT \"OLAP\"\n" +
+                        "DISTRIBUTED BY HASH(`k1`) BUCKETS 2\n" +
+                        "PROPERTIES (\n" +
+                        "    \"replication_num\" = \"1\",\n" +
+                        "    \"in_memory\" = \"false\",\n" +
+                        "    \"storage_format\" = \"DEFAULT\"\n" +
+                        ");"));
+
+        ExceptionChecker.expectThrowsWithMsg(AnalysisException.class,
+                "Default function uuid_numeric() for type VARCHAR(1) is not supported",
+                () -> createTable("CREATE TABLE test.`test_default_uuid_type_not_match` (\n" +
+                        "    k1 int,\n" +
+                        "    uuid VARCHAR NOT NULL DEFAULT (uuid_numeric())\n" +
+                        ") ENGINE=OLAP\n" +
+                        "DUPLICATE KEY(`k1`)\n" +
+                        "COMMENT \"OLAP\"\n" +
+                        "DISTRIBUTED BY HASH(`k1`) BUCKETS 2\n" +
+                        "PROPERTIES (\n" +
+                        "    \"replication_num\" = \"1\",\n" +
+                        "    \"in_memory\" = \"false\",\n" +
+                        "    \"storage_format\" = \"DEFAULT\"\n" +
+                        ");"));
+    }
+
 }

fe/fe-core/src/main/java/com/starrocks/analysis/ColumnDef.java
@@ -35,6 +35,8 @@ import com.starrocks.common.FeNameFormat;
 import java.util.ArrayList;
 import java.util.Set;
 
+import static com.starrocks.catalog.DefaultExpr.SUPPORTED_DEFAULT_FNS;
+
 // Column definition which is generated by SQL syntax parser
 // Syntax:
 //      name type [key] [agg_type] [NULL | NOT NULL] [DEFAULT default_value] [comment]
@@ -353,7 +355,9 @@ public class ColumnDef {
         } else if (defaultExpr instanceof FunctionCallExpr) {
             FunctionCallExpr functionCallExpr = (FunctionCallExpr) defaultExpr;
             String functionName = functionCallExpr.getFnName().getFunction();
-            if (!FunctionSet.NOW.equalsIgnoreCase(functionName)) {
+            boolean supported = SUPPORTED_DEFAULT_FNS.contains(functionName + "()");
+
+            if (!supported) {
                 throw new AnalysisException(
                         String.format("Default expr for function %s is not supported", functionName));
             }
@@ -361,6 +365,19 @@ public class ColumnDef {
             // default function current_timestamp currently only support DATETIME type.
             if (FunctionSet.NOW.equalsIgnoreCase(functionName) && type.getPrimitiveType() != PrimitiveType.DATETIME) {
                 throw new AnalysisException(String.format("Default function now() for type %s is not supported", type));
+            }
+            // default function uuid currently only support VARCHAR type.
+            if (FunctionSet.UUID.equalsIgnoreCase(functionName) && type.getPrimitiveType() != PrimitiveType.VARCHAR) {
+                throw new AnalysisException(String.format("Default function uuid() for type %s is not supported", type));
+            }
+            if (FunctionSet.UUID.equalsIgnoreCase(functionName) && type.getColumnSize() < 36) {
+                throw new AnalysisException("Varchar type length must be greater than 36 for uuid function");
+            }
+            // default function uuid_numeric currently only support LARGE INT type.
+            if (FunctionSet.UUID_NUMERIC.equalsIgnoreCase(functionName) &&
+                    type.getPrimitiveType() != PrimitiveType.LARGEINT) {
+                throw new AnalysisException(String.format("Default function uuid_numeric() for type %s is not supported",
+                        type));
             }
         } else if (defaultExpr instanceof NullLiteral) {
             // nothing to check

fe/fe-core/src/main/java/com/starrocks/catalog/Column.java
@@ -435,9 +435,13 @@ public class Column implements Writable {
         } else {
             sb.append("NOT NULL ");
         }
-        if (defaultExpr != null && "now()".equalsIgnoreCase(defaultExpr.getExpr())) {
-            // compatible with mysql
-            sb.append("DEFAULT ").append("CURRENT_TIMESTAMP").append(" ");
+        if (defaultExpr != null) {
+            if ("now()".equalsIgnoreCase(defaultExpr.getExpr())) {
+                // compatible with mysql
+                sb.append("DEFAULT ").append("CURRENT_TIMESTAMP").append(" ");
+            } else {
+                sb.append("DEFAULT ").append("(").append(defaultExpr.getExpr()).append(") ");
+            }
         } else if (defaultValue != null && getPrimitiveType() != PrimitiveType.HLL &&
                 getPrimitiveType() != PrimitiveType.BITMAP) {
             sb.append("DEFAULT \"").append(defaultValue).append("\" ");

fe/fe-core/src/main/java/com/starrocks/catalog/FunctionSet.java
@@ -197,6 +197,7 @@ public class FunctionSet {
     public static final String CURRENT_VERSION = "current_version";
     public static final String LAST_QUERY_ID = "last_query_id";
     public static final String UUID = "uuid";
+    public static final String UUID_NUMERIC = "uuid_numeric";
     public static final String SLEEP = "sleep";
     public static final String ISNULL = "isnull";
     public static final String ASSERT_TRUE = "assert_true";

fe/fe-core/src/main/java/com/starrocks/load/Load.java
@@ -75,6 +75,8 @@ import java.util.Map.Entry;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static com.starrocks.catalog.DefaultExpr.SUPPORTED_DEFAULT_FNS;
+
 public class Load {
     private static final Logger LOG = LogManager.getLogger(Load.class);
     public static final String VERSION = "v1";
@@ -684,8 +686,12 @@ public class Load {
                         if (defaultValueType == Column.DefaultValueType.CONST) {
                             exprs.add(new StringLiteral(column.calculatedDefaultValue()));
                         } else if (defaultValueType == Column.DefaultValueType.VARY) {
-                            throw new UserException("Column(" + columnName + ") has unsupported default value:"
-                                    + column.getDefaultExpr().getExpr());
+                            if (SUPPORTED_DEFAULT_FNS.contains(column.getDefaultExpr().getExpr())) {
+                                exprs.add(column.getDefaultExpr().obtainExpr());
+                            } else {
+                                throw new UserException("Column(" + columnName + ") has unsupported default value:"
+                                        + column.getDefaultExpr().getExpr());
+                            }
                         } else if (defaultValueType == Column.DefaultValueType.NULL) {
                             if (column.isAllowNull()) {
                                 exprs.add(NullLiteral.create(Type.VARCHAR));
@@ -707,8 +713,12 @@ public class Load {
                         if (defaultValueType == Column.DefaultValueType.CONST) {
                             innerIfExprs.add(new StringLiteral(column.calculatedDefaultValue()));
                         } else if (defaultValueType == Column.DefaultValueType.VARY) {
-                            throw new UserException("Column(" + columnName + ") has unsupported default value:"
-                                    + column.getDefaultExpr().getExpr());
+                            if (SUPPORTED_DEFAULT_FNS.contains(column.getDefaultExpr().getExpr())) {
+                                innerIfExprs.add(column.getDefaultExpr().obtainExpr());
+                            } else {
+                                throw new UserException("Column(" + columnName + ") has unsupported default value:"
+                                        + column.getDefaultExpr().getExpr());
+                            }
                         } else if (defaultValueType == Column.DefaultValueType.NULL) {
                             if (column.isAllowNull()) {
                                 innerIfExprs.add(NullLiteral.create(Type.VARCHAR));

fe/fe-core/src/main/java/com/starrocks/planner/FileScanNode.java
@@ -84,6 +84,8 @@ import java.util.Random;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static com.starrocks.catalog.DefaultExpr.SUPPORTED_DEFAULT_FNS;
+
 // Broker scan node
 public class FileScanNode extends LoadScanNode {
     private static final Logger LOG = LogManager.getLogger(FileScanNode.class);
@@ -326,8 +328,12 @@ public class FileScanNode extends LoadScanNode {
                     if (defaultValueType == Column.DefaultValueType.CONST) {
                         expr = new StringLiteral(column.calculatedDefaultValue());
                     } else if (defaultValueType == Column.DefaultValueType.VARY) {
-                        throw new UserException("Column(" + column + ") has unsupported default value:"
-                                + column.getDefaultExpr().getExpr());
+                        if (SUPPORTED_DEFAULT_FNS.contains(column.getDefaultExpr().getExpr())) {
+                            expr = column.getDefaultExpr().obtainExpr();
+                        } else {
+                            throw new UserException("Column(" + column + ") has unsupported default value:"
+                                    + column.getDefaultExpr().getExpr());
+                        }
                     } else if (defaultValueType == Column.DefaultValueType.NULL) {
                         if (column.isAllowNull()) {
                             expr = NullLiteral.create(column.getType());

fe/fe-core/src/main/java/com/starrocks/planner/StreamLoadScanNode.java
@@ -62,6 +62,8 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 
+import static com.starrocks.catalog.DefaultExpr.SUPPORTED_DEFAULT_FNS;
+
 /**
  * used to scan from stream
  */
@@ -207,7 +209,7 @@ public class StreamLoadScanNode extends LoadScanNode {
                 SlotDescriptor srcSlotDesc = slotDescByName.get(dstSlotDesc.getColumn().getName());
                 if (srcSlotDesc != null) {
                     destSidToSrcSidWithoutTrans.put(dstSlotDesc.getId().asInt(), srcSlotDesc.getId().asInt());
-                    // If dest is allow null, we set source to nullable
+                    // If dest is allowed null, we set source to nullable
                     if (dstSlotDesc.getColumn().isAllowNull()) {
                         srcSlotDesc.setIsNullable(true);
                     }
@@ -220,8 +222,12 @@ public class StreamLoadScanNode extends LoadScanNode {
                     if (defaultValueType == Column.DefaultValueType.CONST) {
                         expr = new StringLiteral(column.calculatedDefaultValue());
                     } else if (defaultValueType == Column.DefaultValueType.VARY) {
-                        throw new UserException("Column(" + column + ") has unsupported default value:"
-                                + column.getDefaultExpr().getExpr());
+                        if (SUPPORTED_DEFAULT_FNS.contains(column.getDefaultExpr().getExpr())) {
+                            expr = column.getDefaultExpr().obtainExpr();
+                        } else {
+                            throw new UserException("Column(" + column + ") has unsupported default value:"
+                                    + column.getDefaultExpr().getExpr());
+                        }
                     } else if (defaultValueType == Column.DefaultValueType.NULL) {
                         if (column.isAllowNull()) {
                             expr = NullLiteral.create(column.getType());

commit 3fbbb3e3d1478e9f5d2e3d82669e48a832e13d86

Fix bug default current_timestamp lose efficacy (#11545)
This bug was introduced in #11155, because the supported function is checked first, and the essence of our current_timestamp is the expression now(), so there should be this function in the supported function.

fe/fe-core/src/main/java/com/starrocks/catalog/DefaultExpr.java
@@ -12,7 +12,7 @@ import java.util.Set;
 
 public class DefaultExpr {
 
-    public static final Set<String> SUPPORTED_DEFAULT_FNS = ImmutableSet.of("uuid()", "uuid_numeric()");
+    public static final Set<String> SUPPORTED_DEFAULT_FNS = ImmutableSet.of("now()", "uuid()", "uuid_numeric()");
 
     @SerializedName("expr")
     private String expr;

fe/fe-core/src/test/java/com/starrocks/catalog/CreateTableTest.java
@@ -393,6 +393,27 @@ public class CreateTableTest {
     }
 
     @Test
+    public void testCreateTableDefaultCurrentTimestamp() throws Exception {
+        StarRocksAssert starRocksAssert = new StarRocksAssert(connectContext);
+        starRocksAssert.useDatabase("test");
+        String sql = "CREATE TABLE `test_create_default_current_timestamp` (\n" +
+                "    k1 int,\n" +
+                "    ts datetime NOT NULL DEFAULT CURRENT_TIMESTAMP\n" +
+                ") ENGINE=OLAP\n" +
+                "DUPLICATE KEY(`k1`)\n" +
+                "COMMENT \"OLAP\"\n" +
+                "DISTRIBUTED BY HASH(`k1`) BUCKETS 2\n" +
+                "PROPERTIES (\n" +
+                "    \"replication_num\" = \"1\",\n" +
+                "    \"in_memory\" = \"false\",\n" +
+                "    \"storage_format\" = \"DEFAULT\"\n" +
+                ");";
+        starRocksAssert.withTable(sql);
+        final Table table = starRocksAssert.getCtx().getGlobalStateMgr().getDb(connectContext.getDatabase())
+                .getTable("test_create_default_current_timestamp");
+        Assert.assertEquals(2, table.getColumns().size());
+    }
+    @Test
     public void testCreateTableDefaultUUID() throws Exception {
         StarRocksAssert starRocksAssert = new StarRocksAssert(connectContext);
         starRocksAssert.useDatabase("test");