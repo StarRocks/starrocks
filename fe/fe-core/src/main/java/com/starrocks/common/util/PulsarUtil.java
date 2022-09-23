04f913390052c7bd15158c64c6a0e1521a45796e
jiutianchen <chen9t@gmail.com>
2022-09-23 15:22:44 +0800

    Handle InterruptedException

--- fe/fe-core/src/main/java/com/starrocks/common/util/PulsarUtil.java
+++ fe/fe-core/src/main/java/com/starrocks/common/util/PulsarUtil.java
@@ -142,6 +142,10 @@ public class PulsarUtil {
                 } else {
                     return result;
                 }
+            } catch (InterruptedException ie) {
+                LOG.warn("got interrupted exception when sending proxy request to " + address);
+                Thread.currentThread().interrupt();
+                throw new LoadException("got interrupted exception when sending proxy request to " + address);
             } catch (Exception e) {
                 LOG.warn("failed to send proxy request to " + address + " err " + e.getMessage());
                 throw new LoadException("failed to send proxy request to " + address + " err " + e.getMessage());
