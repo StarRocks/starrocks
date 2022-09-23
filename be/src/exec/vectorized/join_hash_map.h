16b99ea34cdd3c34a3fc8e7b5083d38c35f8394f
packy <wangchao@starrocks.com>
2022-09-23 20:04:22 +0800

    fix clang format

--- be/src/exec/vectorized/join_hash_map.h
+++ be/src/exec/vectorized/join_hash_map.h
@@ -621,8 +621,9 @@ private:
 
     // for null aware anti join with other join conjunct
     template <bool first_probe>
-    void _probe_from_ht_for_null_aware_anti_join_with_other_conjunct(RuntimeState* state, const Buffer<CppType>& build_data,
-                                                               const Buffer<CppType>& probe_data);
+    void _probe_from_ht_for_null_aware_anti_join_with_other_conjunct(RuntimeState* state,
+                                                                     const Buffer<CppType>& build_data,
+                                                                     const Buffer<CppType>& probe_data);
 
     // for one key right outer join with other conjunct
     template <bool first_probe>
