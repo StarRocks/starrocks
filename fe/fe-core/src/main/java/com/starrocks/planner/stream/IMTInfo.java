4f80f793ed5d4a680f68d49027cc20eff17d7113
Murphy <mofei@starrocks.com>
2022-09-23 13:56:46 +0800

    setup imt for StreamJoin

---
+++ fe/fe-core/src/main/java/com/starrocks/planner/stream/IMTInfo.java
@@ -0,0 +1,39 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Inc.
+
+package com.starrocks.planner.stream;
+
+import com.starrocks.catalog.OlapTable;
+import com.starrocks.common.UserException;
+import com.starrocks.thrift.TIMTDescriptor;
+import com.starrocks.thrift.TIMTType;
+
+/**
+ * Intermediate materialized table
+ */
+public class IMTInfo {
+
+    private TIMTType type;
+    private boolean needMaintain;
+    private OlapTableRouteInfo olapTable;
+
+    public static IMTInfo fromOlapTable(long dbId, OlapTable table, boolean needMaintain) throws UserException {
+        IMTInfo res = new IMTInfo();
+        res.type = TIMTType.OLAP_TABLE;
+        res.needMaintain = needMaintain;
+        res.olapTable = OlapTableRouteInfo.create(dbId, table);
+        return res;
+    }
+
+    public TIMTDescriptor toThrift() {
+        TIMTDescriptor desc = new TIMTDescriptor();
+        desc.setImt_type(this.type);
+        desc.setNeed_maintain(this.needMaintain);
+        desc.setOlap_table(this.olapTable.toThrift());
+        return desc;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s/%s", type.toString(), olapTable.getTableName());
+    }
+}
