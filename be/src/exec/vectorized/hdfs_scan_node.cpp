b6d3a0410c0e96cd3dc85a3586025de8e9300317
yanz <dirtysalt1987@gmail.com>
2022-09-28 18:45:02 +0800

    report error when partition id not found

--- be/src/exec/vectorized/hdfs_scan_node.cpp
+++ be/src/exec/vectorized/hdfs_scan_node.cpp
@@ -661,6 +661,11 @@ Status HdfsScanNode::_find_and_insert_hdfs_file(const THdfsScanRange& scan_range
     std::string native_file_path = scan_range.full_path;
     if (_lake_table != nullptr && _lake_table->has_partition()) {
         auto* partition_desc = _lake_table->get_partition(scan_range.partition_id);
+        if (partition_desc == nullptr) {
+            return Status::InternalError(fmt::format(
+                    "Plan inconsistency. scan_range.partition_id = {} not found in partition description map",
+                    scan_range.partition_id));
+        }
 
         SCOPED_TIMER(_profile.open_file_timer);
 
