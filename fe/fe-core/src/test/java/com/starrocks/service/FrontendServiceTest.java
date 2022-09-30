7839573ef77a3565912b47ed28e4774356ff190e
PengFei Li <lpengfei2016@gmail.com>
2022-09-30 11:41:29 +0800

    Fix broken tests

--- fe/fe-core/src/test/java/com/starrocks/service/FrontendServiceTest.java
+++ fe/fe-core/src/test/java/com/starrocks/service/FrontendServiceTest.java
@@ -13,6 +13,8 @@ import com.starrocks.common.Config;
 import com.starrocks.common.DdlException;
 import com.starrocks.common.UserException;
 import com.starrocks.mysql.privilege.Auth;
+import com.starrocks.qe.ConnectContext;
+import com.starrocks.qe.QueryState;
 import com.starrocks.server.GlobalStateMgr;
 import com.starrocks.system.SystemInfoService;
 import com.starrocks.thrift.TAuthenticateParams;
@@ -38,7 +40,8 @@ public class FrontendServiceTest {
     public GlobalStateMgr globalStateMgr;
     @Mocked
     private Analyzer analyzer;
-
+    @Mocked
+    private ConnectContext ctx;
 
     @Before
     public void setUp() throws NoSuchMethodException, SecurityException {
@@ -57,6 +60,26 @@ public class FrontendServiceTest {
                 globalStateMgr.getAuth();
                 minTimes = 0;
                 result = auth;
+
+                ConnectContext.get();
+                minTimes = 0;
+                result = ctx;
+
+                ctx.getQualifiedUser();
+                minTimes = 0;
+                result = "root";
+
+                ctx.getRemoteIP();
+                minTimes = 0;
+                result = "192.168.1.1";
+
+                ctx.getState();
+                minTimes = 0;
+                result = new QueryState();
+
+                ctx.getCurrentUserIdentity();
+                minTimes = 0;
+                result = UserIdentity.createAnalyzedUserIdentWithIp("root", "%");
             }
         };
     }
@@ -67,9 +90,9 @@ public class FrontendServiceTest {
 
         grantTable("abc", "192.168.92.3", "db1", "t1",
                 Collections.singletonList(AccessPrivilege.LOAD_PRIV));
-        grantTable("abc", "192.168.92.3", "db2", "t2",
+        grantTable("abc", "192.168.92.3", "db1", "t2",
                 Collections.singletonList(AccessPrivilege.LOAD_PRIV));
-        grantTable("abc", "192.168.92.3", "db2", "t3",
+        grantTable("abc", "192.168.92.3", "db1", "t3",
                 Collections.singletonList(AccessPrivilege.LOAD_PRIV));
 
         Config.enable_starrocks_external_table_auth_check = true;
@@ -81,23 +104,23 @@ public class FrontendServiceTest {
         // test password check failed
         authParam = createTAuthenticateParams(
                 "abc", "12", "192.168.92.3", "db1", Arrays.asList("t1", "t2", "t3"));
-        verifyCheckResult(false, "Access denied for abc @ 192.168.92.3",
+        verifyCheckResult(false, "Access denied for default_cluster:abc@192.168.92.3",
                 FrontendServiceImpl.checkPasswordAndPrivilege(authParam));
 
         // test privilege check failed on different tables
-        String errMsg = "Access denied; user 'abc'@'192.168.92.3' need (at least one of) the t4 " +
-                        "privilege(s) for table '%s' in db 'default_cluster.db1'";
+        String errMsgFormat = "Access denied; user 'default_cluster:abc'@'192.168.92.3' need (at least one of) the Admin_priv Load_priv " +
+                "privilege(s) for table '%s' in db 'default_cluster:db1'";
         authParam = createTAuthenticateParams(
                 "abc", "123", "192.168.92.3", "db1", Arrays.asList("t4", "t2", "t3"));
-        verifyCheckResult(false, errMsg, FrontendServiceImpl.checkPasswordAndPrivilege(authParam));
+        verifyCheckResult(false, String.format(errMsgFormat, "t4"), FrontendServiceImpl.checkPasswordAndPrivilege(authParam));
 
         authParam = createTAuthenticateParams(
-                "abc", "123", "192.168.92.3", "db1", Arrays.asList("t1", "t4", "t3"));
-        verifyCheckResult(false, errMsg, FrontendServiceImpl.checkPasswordAndPrivilege(authParam));
+                "abc", "123", "192.168.92.3", "db1", Arrays.asList("t1", "t5", "t3"));
+        verifyCheckResult(false, String.format(errMsgFormat, "t5"), FrontendServiceImpl.checkPasswordAndPrivilege(authParam));
 
         authParam = createTAuthenticateParams(
-                "abc", "123", "192.168.92.3", "db1", Arrays.asList("t1", "t2", "t4"));
-        verifyCheckResult(false, errMsg, FrontendServiceImpl.checkPasswordAndPrivilege(authParam));
+                "abc", "123", "192.168.92.3", "db1", Arrays.asList("t1", "t2", "t6"));
+        verifyCheckResult(false, String.format(errMsgFormat, "t6"), FrontendServiceImpl.checkPasswordAndPrivilege(authParam));
 
 
         // test disable check configuration
@@ -111,13 +134,12 @@ public class FrontendServiceTest {
         // incorrect password passed
         authParam = createTAuthenticateParams(
                 "abc", "12", "192.168.92.3", "db1", Arrays.asList("t1", "t2", "t3"));
-        verifyCheckResult(false, null, FrontendServiceImpl.checkPasswordAndPrivilege(authParam));
+        verifyCheckResult(true, null, FrontendServiceImpl.checkPasswordAndPrivilege(authParam));
 
         // incorrect privilege passed
         authParam = createTAuthenticateParams(
                 "abc", "123", "192.168.92.3", "db1", Arrays.asList("t4", "t2", "t3"));
-        verifyCheckResult(false, null, FrontendServiceImpl.checkPasswordAndPrivilege(authParam));
-
+        verifyCheckResult(true, null, FrontendServiceImpl.checkPasswordAndPrivilege(authParam));
     }
 
     private void verifyCheckResult(boolean expectOk, String errMsg, TStatus actualStatus) {
