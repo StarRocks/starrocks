02cac1131241ff4183f8f7d158275028e4ed6bd9
yan.zhang <dirtysalt1987@gmail.com>
2022-10-10 07:03:35 +0800

    [Enhancement] make block cache more flexible. (#11731)

--- fe/fe-core/src/main/java/com/starrocks/qe/Coordinator.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/Coordinator.java
@@ -2157,6 +2157,9 @@ public class Coordinator {
                     fragmentExecParamsMap.get(scanNode.getFragmentId()).scanRangeAssignment;
             if ((scanNode instanceof HdfsScanNode) || (scanNode instanceof IcebergScanNode) ||
                     scanNode instanceof HudiScanNode) {
+                if (connectContext != null) {
+                    queryOptions.setUse_scan_block_cache(connectContext.getSessionVariable().getUseScanBlockCache());
+                }
                 HDFSBackendSelector selector =
                         new HDFSBackendSelector(scanNode, locations, assignment, addressToBackendID, usedBackendIDs,
                                 getSelectorComputeNodes(hasComputeNode),
@@ -3374,8 +3377,9 @@ public class Coordinator {
 
     /**
      * Whether it can use pipeline engine.
+     *
      * @param connectContext It is null for broker broker export.
-     * @param fragments All the fragments need to execute.
+     * @param fragments      All the fragments need to execute.
      * @return true if enabling pipeline in the session variable and all the fragments can use pipeline,
      * otherwise false.
      */
