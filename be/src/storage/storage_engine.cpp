d5af8098aa1db48e8c4caeac92f8c47c69ae860a
TonyShang <shangyiming@starrocks.com>
2022-10-10 16:01:20 +0800

    [cherry-pick][branch-2.1][Enhancement] Flush wal in parallel

--- be/src/storage/storage_engine.cpp
+++ be/src/storage/storage_engine.cpp
@@ -95,7 +95,7 @@ StorageEngine::StorageEngine(const EngineOptions& options)
 
           _file_cache(nullptr),
           _tablet_manager(new TabletManager(options.tablet_meta_mem_tracker, config::tablet_map_shard_size)),
-          _txn_manager(new TxnManager(config::txn_map_shard_size, config::txn_shard_size)),
+          _txn_manager(new TxnManager(config::txn_map_shard_size, config::txn_shard_size, options.store_paths.size())),
           _rowset_id_generator(new UniqueRowsetIdGenerator(options.backend_uid)),
           _memtable_flush_executor(nullptr),
           _block_manager(nullptr),
