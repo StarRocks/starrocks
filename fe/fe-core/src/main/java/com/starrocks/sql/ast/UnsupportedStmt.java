72df2761bfd976f63d45c2a2c84a07b38c5c8a0e
HangyuanLiu <460660596@qq.com>
2022-10-14 14:05:40 +0800

    Support commit/rollup/transaction Statement in new Parser (#12139)

--- fe/fe-core/src/main/java/com/starrocks/sql/ast/UnsupportedStmt.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/ast/UnsupportedStmt.java
@@ -4,7 +4,16 @@ package com.starrocks.sql.ast;
 
 import com.starrocks.analysis.RedirectStatus;
 
-public class EmptyStmt extends UnsupportedStmt {
+public class UnsupportedStmt extends StatementBase {
+
+    public UnsupportedStmt() {
+    }
+
+    @Override
+    public <R, C> R accept(AstVisitor<R, C> visitor, C context) {
+        return visitor.visitUnsupportedStatement(this, context);
+    }
+
     @Override
     public RedirectStatus getRedirectStatus() {
         return RedirectStatus.NO_FORWARD;
