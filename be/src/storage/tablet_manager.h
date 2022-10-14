4f83dbf5eda79b603831e487a5add9679dd9037e
Binglin Chang <decstery@gmail.com>
2022-10-14 11:11:01 +0800

    [BugFix] Fix error when tablet perform drop and clone at same time (#12081)
    This bug is related to #5052
    
    When doing full clone, old tablet may not exists in tablet map, but it may still in shutdown tablet map(tablet path not deleted)
    new tablet's path maybe the same as old tablet's path, so we need to clear that path before cloning new files into that directory.
    
    Note this solution is not optimal, the correct final solution would be:
    
    making create/drop/clone tablet operation atomic by introducing some kind of tablet lock that is more lightweight than the current _get_tablets_shard_lock
    or making the clone process inside lock more lightweight, for example, download files to a temp path outside of lock, then do mvs inside lock

--- be/src/storage/tablet_manager.h
+++ be/src/storage/tablet_manager.h
@@ -142,6 +142,8 @@ public:
 
     Status delete_shutdown_tablet(int64_t tablet_id);
 
+    Status delete_shutdown_tablet_before_clone(int64_t tablet_id);
+
     MemTracker* tablet_meta_mem_tracker() { return _mem_tracker; }
 
     // return true if all tablets visited
