f0b58b805b611969fa658a93840ea5e14493b0df
padmejin <tvtvcode@163.com>
2022-10-14 08:38:47 +0800

    reomve one line: useless import

--- fe/fe-core/src/test/java/com/starrocks/privilege/PrivilegeManagerTest.java
+++ fe/fe-core/src/test/java/com/starrocks/privilege/PrivilegeManagerTest.java
@@ -27,7 +27,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
