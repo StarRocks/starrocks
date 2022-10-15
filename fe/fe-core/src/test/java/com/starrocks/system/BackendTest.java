66cc10c3206382cb56cdb68139d004b95a650e63
Murphy <96611012+mofeiatwork@users.noreply.github.com>
2022-10-15 22:42:24 +0800

    [Bugfix] fix BE cpu cores updating (#12187)
    (cherry picked from commit bcff28a5ed4d780186c22383621b379cff164e1d)
    
    # Conflicts:
    #	fe/fe-core/src/test/java/com/starrocks/system/BackendTest.java

---
+++ fe/fe-core/src/test/java/com/starrocks/system/BackendTest.java
@@ -0,0 +1,28 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Inc.
+
+package com.starrocks.system;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class BackendTest {
+
+    @Test
+    public void testSetHeartbeatPort() {
+        Backend be = new Backend();
+        be.setHeartbeatPort(1000);
+        Assert.assertTrue(be.getHeartbeatPort() == 1000);
+    }
+
+    @Test
+    public void cpuCoreUpdate() {
+        BackendCoreStat.setNumOfHardwareCoresOfBe(1, 8);
+        Assert.assertEquals(8, BackendCoreStat.getAvgNumOfHardwareCoresOfBe());
+        Assert.assertEquals(4, BackendCoreStat.getDefaultDOP());
+
+        BackendCoreStat.setNumOfHardwareCoresOfBe(1, 16);
+        Assert.assertEquals(16, BackendCoreStat.getAvgNumOfHardwareCoresOfBe());
+        Assert.assertEquals(8, BackendCoreStat.getDefaultDOP());
+    }
+}
