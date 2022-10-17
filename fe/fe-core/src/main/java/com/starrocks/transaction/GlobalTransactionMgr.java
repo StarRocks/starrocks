fb17cc5e6faa9c2f3dc7105378904301c5c88307
Binglin Chang <decstery@gmail.com>
2022-10-17 16:28:38 +0800

    [BugFix] fix alter table in new publish; add detail error message for load/insert publish timeout

--- fe/fe-core/src/main/java/com/starrocks/transaction/GlobalTransactionMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/transaction/GlobalTransactionMgr.java
@@ -740,4 +740,12 @@ public class GlobalTransactionMgr implements Writable {
         write(dos);
         return checksum;
     }
+
+    public String getTxnPublishTimeoutDebugInfo(long dbId, long txnId) {
+        DatabaseTransactionMgr dbTransactionMgr = dbIdToDatabaseTransactionMgrs.get(dbId);
+        if (dbTransactionMgr == null) {
+            return "";
+        }
+        return dbTransactionMgr.getTxnPublishTimeoutDebugInfo(txnId);
+    }
 }
