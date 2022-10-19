3f57cde42ae7a10437afe9815f2c5b93b18df2d8
meegoo <meegoo.sr@gmail.com>
2022-10-19 11:20:08 +0800

    [Feature] Support primary key replicated storage (#11854)

--- be/src/http/http_common.h
+++ be/src/http/http_common.h
@@ -51,6 +51,7 @@ static const std::string HTTP_STRIP_OUTER_ARRAY = "strip_outer_array";
 static const std::string HTTP_PARTIAL_UPDATE = "partial_update";
 static const std::string HTTP_TRANSMISSION_COMPRESSION_TYPE = "transmission_compression_type";
 static const std::string HTTP_LOAD_DOP = "load_dop";
+static const std::string HTTP_ENABLE_REPLICATED_STORAGE = "enable_replicated_storage";
 
 static const std::string HTTP_100_CONTINUE = "100-continue";
 
