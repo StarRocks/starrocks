d0c0f83a5456a095b92b206a6aa48169eded95fe
Smith Cruise <chendingchao1@126.com>
2022-10-20 23:10:43 +0800

    Revert "[Enhancement] Support larger length in Hive string type. (#12145)"
    This reverts commit 9405d0a445cdab1323bc7e09c38ab9282204ec1e.

--- be/src/column/binary_column.h
+++ be/src/column/binary_column.h
@@ -285,13 +285,9 @@ public:
 
     std::string debug_string() const override {
         std::stringstream ss;
-        size_t size = this->size();
-        if (size == 0) {
-            return "[]";
-        }
-
         ss << "[";
-        for (size_t i = 0; i < size - 1; i++) {
+        size_t size = this->size();
+        for (int i = 0; i < size - 1; ++i) {
             ss << debug_item(i) << ", ";
         }
         if (size > 0) {
