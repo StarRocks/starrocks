3adf63d8669285f5344f63a8c541f911e7828819
silverbullet233 <3675229+silverbullet233@users.noreply.github.com>
2022-10-20 17:16:21 +0800

    fix fe ut

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/PipelineParallelismTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/PipelineParallelismTest.java
@@ -66,8 +66,8 @@ public class PipelineParallelismTest extends PlanTestBase {
         ExecPlan plan = getExecPlan("select * from t0 [_META_]");
         PlanFragment fragment1 = plan.getFragments().get(1);
         assertContains(fragment1.getExplainString(TExplainLevel.NORMAL), "MetaScan");
-        Assert.assertEquals(parallelExecInstanceNum, fragment1.getParallelExecNum());
-        Assert.assertEquals(1, fragment1.getPipelineDop());
+        Assert.assertEquals(1, fragment1.getParallelExecNum());
+        Assert.assertEquals(numHardwareCores / 2, fragment1.getPipelineDop());
     }
 
     @Test
