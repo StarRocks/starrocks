StarRocks/starrocks

commit 2f11704f851b011052403787d8395d060e0d1744

[Refactor] Removed not used log upon start and unified drop_tablet_unlock interface (#9318)
Many logs upon start is non-sense and the pull request remove these logs.
The AlterTask is already not used and drop_tablet_unlocked can be unified.

be/src/service/backend_options.cpp
@@ -74,7 +74,7 @@ bool BackendOptions::init() {
         LOG(INFO) << "fail to find one valid non-loopback address, use loopback address.";
         _s_localhost = loopback;
     }
-    LOG(INFO) << "local host ip=" << _s_localhost;
+    LOG(INFO) << "local host" << _s_localhost;
     return true;
 }
 

be/src/agent/task_worker_pool.cpp
@@ -1097,7 +1097,6 @@ void* TaskWorkerPool::_report_task_worker_thread_callback(void* arg_this) {
         if (master_address.port == 0) {
             // port == 0 means not received heartbeat yet
             // sleep a short time and try again
-            LOG(INFO) << "Waiting to receive first heartbeat from frontend";
             sleep(1);
             continue;
         }
@@ -1137,7 +1136,6 @@ void* TaskWorkerPool::_report_disk_state_worker_thread_callback(void* arg_this) 
         if (master_address.port == 0) {
             // port == 0 means not received heartbeat yet
             // sleep a short time and try again
-            LOG(INFO) << "Waiting to receive first heartbeat from frontend";
             sleep(config::sleep_one_second);
             continue;
         }
@@ -1196,7 +1194,6 @@ void* TaskWorkerPool::_report_tablet_worker_thread_callback(void* arg_this) {
         if (master_address.port == 0) {
             // port == 0 means not received heartbeat yet
             // sleep a short time and try again
-            LOG(INFO) << "Waiting to receive first heartbeat from frontend";
             sleep(config::sleep_one_second);
             continue;
         }
@@ -1243,7 +1240,6 @@ void* TaskWorkerPool::_report_workgroup_thread_callback(void* arg_this) {
         if (master_address.port == 0) {
             // port == 0 means not received heartbeat yet
             // sleep a short time and try again
-            LOG(INFO) << "Waiting to receive first heartbeat from frontend";
             sleep(config::sleep_one_second);
             continue;
         }

be/src/http/ev_http_server.cpp
@@ -99,7 +99,6 @@ Status EvHttpServer::start() {
     RETURN_IF_ERROR(_bind());
     for (int i = 0; i < _num_workers; ++i) {
         auto worker = [this, i]() {
-            LOG(INFO) << "EvHttpServer worker start, id=" << i;
             struct event_base* base = event_base_new();
             if (base == nullptr) {
                 LOG(WARNING) << "Couldn't create an event_base.";

be/src/service/backend_base.cpp
@@ -65,7 +65,7 @@ std::unique_ptr<ThriftServer> BackendServiceBase::create(ExecEnv* exec_env, int 
     auto thread_factory = std::make_shared<ThreadFactory>();
     auto processor = std::make_shared<BackendServiceProcessor>(handler);
 
-    LOG(INFO) << "StarRocksInternalService listening on " << port;
+    LOG(INFO) << "StarRocksInternalService has started listening port on " << port;
     // TODO: May be rename be_service_threads to thrift_service_threads ?
     return std::make_unique<ThriftServer>("BackendService", processor, port, exec_env->metrics(),
                                           config::be_service_threads);

be/src/service/starrocks_main.cpp
@@ -265,8 +265,6 @@ int main(int argc, char** argv) {
         LOG(ERROR) << "StarRocks BE HeartBeat Service did not start correctly. Error=" << status.to_string();
         starrocks::shutdown_logging();
         exit(1);
-    } else {
-        LOG(INFO) << "StarRocks BE HeartBeat Service started correctly.";
     }
 
 #ifdef USE_STAROS

be/src/storage/data_dir.cpp
@@ -511,8 +511,6 @@ Status DataDir::update_capacity() {
     ASSIGN_OR_RETURN(auto space_info, FileSystem::Default()->space(_path));
     _available_bytes = space_info.available;
     _disk_capacity_bytes = space_info.capacity;
-    LOG(INFO) << "path: " << _path << " total capacity: " << _disk_capacity_bytes
-              << ", available capacity: " << _available_bytes;
     return Status::OK();
 }
 

be/src/storage/olap_server.cpp
@@ -61,21 +61,17 @@ volatile uint32_t g_schema_change_active_threads = 0;
 Status StorageEngine::start_bg_threads() {
     _update_cache_expire_thread = std::thread([this] { _update_cache_expire_thread_callback(nullptr); });
     Thread::set_thread_name(_update_cache_expire_thread, "cache_expire");
-    LOG(INFO) << "update cache expire thread started";
 
     _unused_rowset_monitor_thread = std::thread([this] { _unused_rowset_monitor_thread_callback(nullptr); });
     Thread::set_thread_name(_unused_rowset_monitor_thread, "rowset_monitor");
-    LOG(INFO) << "unused rowset monitor thread started";
 
     // start thread for monitoring the snapshot and trash folder
     _garbage_sweeper_thread = std::thread([this] { _garbage_sweeper_thread_callback(nullptr); });
     Thread::set_thread_name(_garbage_sweeper_thread, "garbage_sweeper");
-    LOG(INFO) << "garbage sweeper thread started";
 
     // start thread for monitoring the tablet with io error
     _disk_stat_monitor_thread = std::thread([this] { _disk_stat_monitor_thread_callback(nullptr); });
     Thread::set_thread_name(_disk_stat_monitor_thread, "disk_monitor");
-    LOG(INFO) << "disk stat monitor thread started";
 
     // convert store map to vector
     std::vector<DataDir*> data_dirs;
@@ -108,7 +104,6 @@ Status StorageEngine::start_bg_threads() {
             });
             Thread::set_thread_name(_base_compaction_threads.back(), "base_compact");
         }
-        LOG(INFO) << "base compaction threads started. number: " << base_compaction_num_threads;
 
         _cumulative_compaction_threads.reserve(cumulative_compaction_num_threads);
         for (uint32_t i = 0; i < cumulative_compaction_num_threads; ++i) {
@@ -117,7 +112,6 @@ Status StorageEngine::start_bg_threads() {
             });
             Thread::set_thread_name(_cumulative_compaction_threads.back(), "cumulat_compact");
         }
-        LOG(INFO) << "cumulative compaction threads started. number: " << cumulative_compaction_num_threads;
     } else {
         // new compaction framework
 
@@ -128,11 +122,9 @@ Status StorageEngine::start_bg_threads() {
             compaction_scheduler.schedule();
         });
         Thread::set_thread_name(_compaction_scheduler, "compact_sched");
-        LOG(INFO) << "compaction scheduler started";
 
         _compaction_checker_thread = std::thread([this] { compaction_check(); });
         Thread::set_thread_name(_compaction_checker_thread, "compact_check");
-        LOG(INFO) << "compaction checker started";
     }
 
     int32_t update_compaction_num_threads_per_disk =
@@ -145,22 +137,18 @@ Status StorageEngine::start_bg_threads() {
         });
         Thread::set_thread_name(_update_compaction_threads.back(), "update_compact");
     }
-    LOG(INFO) << "update compaction threads started. number: " << update_compaction_num_threads;
     _repair_compaction_thread = std::thread([this] { _repair_compaction_thread_callback(nullptr); });
     Thread::set_thread_name(_repair_compaction_thread, "repair_compact");
-    LOG(INFO) << "repair compaction thread started";
 
     // tablet checkpoint thread
     for (auto data_dir : data_dirs) {
         _tablet_checkpoint_threads.emplace_back([this, data_dir] { _tablet_checkpoint_callback((void*)data_dir); });
         Thread::set_thread_name(_tablet_checkpoint_threads.back(), "tablet_check_pt");
     }
-    LOG(INFO) << "tablet checkpoint thread started";
 
     // fd cache clean thread
     _fd_cache_clean_thread = std::thread([this] { _fd_cache_clean_callback(nullptr); });
     Thread::set_thread_name(_fd_cache_clean_thread, "fd_cache_clean");
-    LOG(INFO) << "fd cache clean thread started";
 
     // path scan and gc thread
     if (config::path_gc_check) {
@@ -170,10 +158,9 @@ Status StorageEngine::start_bg_threads() {
             Thread::set_thread_name(_path_scan_threads.back(), "path_scan");
             Thread::set_thread_name(_path_gc_threads.back(), "path_gc");
         }
-        LOG(INFO) << "path scan/gc threads started. number:" << get_stores().size();
     }
 
-    LOG(INFO) << "all storage engine's background threads are started.";
+    LOG(INFO) << "All backgroud threads of storage engine have started.";
     return Status::OK();
 }
 
@@ -408,8 +395,6 @@ void* StorageEngine::_cumulative_compaction_thread_callback(void* arg, DataDir* 
 #ifdef GOOGLE_PROFILER
     ProfilerRegisterThread();
 #endif
-    LOG(INFO) << "try to start cumulative compaction process!";
-
     Status status = Status::OK();
     while (!_bg_worker_stopped.load(std::memory_order_consume)) {
         // must be here, because this thread is start on start and
@@ -480,8 +465,6 @@ void* StorageEngine::_path_gc_thread_callback(void* arg) {
     ProfilerRegisterThread();
 #endif
 
-    LOG(INFO) << "try to start path gc thread!";
-
     while (!_bg_worker_stopped.load(std::memory_order_consume)) {
         LOG(INFO) << "try to perform path gc by tablet!";
         ((DataDir*)arg)->perform_path_gc_by_tablet();
@@ -507,12 +490,10 @@ void* StorageEngine::_path_scan_thread_callback(void* arg) {
     ProfilerRegisterThread();
 #endif
 
-    LOG(INFO) << "wait 10min to start path scan thread";
     while (!_bg_worker_stopped.load(std::memory_order_consume)) {
         SLEEP_IN_BG_WORKER(600);
         break;
     }
-    LOG(INFO) << "try to start path scan thread!";
 
     while (!_bg_worker_stopped.load(std::memory_order_consume)) {
         LOG(INFO) << "try to perform path scan!";
@@ -534,7 +515,6 @@ void* StorageEngine::_tablet_checkpoint_callback(void* arg) {
 #ifdef GOOGLE_PROFILER
     ProfilerRegisterThread();
 #endif
-    LOG(INFO) << "try to start tablet meta checkpoint thread!";
     while (!_bg_worker_stopped.load(std::memory_order_consume)) {
         LOG(INFO) << "begin to do tablet meta checkpoint:" << ((DataDir*)arg)->path();
         int64_t start_time = UnixMillis();

be/src/storage/primary_index.cpp
@@ -79,13 +79,7 @@ public:
     template <class U>
     TraceAlloc(TraceAlloc<U> const&) noexcept {}
 
-    value_type* allocate(std::size_t n) {
-        if (n >= large_mem_alloc_threhold / phmap_hash_table_shard / sizeof(value_type)) {
-            LOG(INFO) << "primary_index large alloc " << n << "*" << sizeof(value_type) << "="
-                      << n * sizeof(value_type);
-        }
-        return static_cast<value_type*>(::operator new(n * sizeof(value_type)));
-    }
+    value_type* allocate(std::size_t n) { return static_cast<value_type*>(::operator new(n * sizeof(value_type))); }
 
     void deallocate(value_type* p, std::size_t) noexcept { ::operator delete(p); }
 };

commit 654b37e24e8e5fe53486010769a0ff97e40ef6f8

[Enhancement] Fix log printing in be (#12497)
Fixes a typo in the log introduced by #9318, and changed a log output location for better debugging in BE

* Fixed the log output for BE IP: `backend_options.cpp:77] local host192.168.145.1`
* Moved the log about a mismatched BE IP address into the WARNING log, instead of INFO, for a better debugging experience.
   ```
   I1024 15:36:38.188383 248318 heartbeat_server.cpp:148] 172.17.0.1 not equal to to backend localhost 192.168.145.1
   W1024 15:36:38.188386 248318 heartbeat_server.cpp:74] Fail to handle heartbeat: Internal error: FE saved address not match backend address cached master info: TMasterInfo(network_address=TNetworkAddress(hostname=, port=0), cluster_id=-1, epoch=0, token=<null>, backend_ip=<null>, http_port=<null>, heartbeat_flags=<null>, backend_id=<null>) received master info: TMasterInfo(network_address=TNetworkAddress(hostname=172.17.0.1, port=9020), cluster_id=452026703, epoch=3, token=c2de331e-6628-4f77-92cc-913a61aa40f8, backend_ip=172.17.0.1, http_port=8030, heartbeat_flags=0, backend_id=10020)
   ```

be/src/agent/heartbeat_server.cpp
@@ -145,8 +145,8 @@ StatusOr<HeartbeatServer::CmpResult> HeartbeatServer::compare_master_info(const 
 
     if (master_info.__isset.backend_ip) {
         if (master_info.backend_ip != BackendOptions::get_localhost()) {
-            LOG(INFO) << master_info.backend_ip << " not equal to to backend localhost "
-                      << BackendOptions::get_localhost();
+            LOG(WARNING) << master_info.backend_ip << " not equal to to backend localhost "
+                         << BackendOptions::get_localhost();
             bool fe_saved_is_valid_ip = is_valid_ip(master_info.backend_ip);
             if (fe_saved_is_valid_ip && is_valid_ip(BackendOptions::get_localhost())) {
                 return Status::InternalError("FE saved address not match backend address");

be/src/service/backend_options.cpp
@@ -74,7 +74,7 @@ bool BackendOptions::init() {
         LOG(INFO) << "fail to find one valid non-loopback address, use loopback address.";
         _s_localhost = loopback;
     }
-    LOG(INFO) << "local host" << _s_localhost;
+    LOG(INFO) << "localhost " << _s_localhost;
     return true;
 }
 