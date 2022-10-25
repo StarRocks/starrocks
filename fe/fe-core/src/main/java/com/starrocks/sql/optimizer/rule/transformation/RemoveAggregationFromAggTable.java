03be93654e12c9f530f30b1a669b004d9a6eee0b
packy <wangchao@starrocks.com>
2022-10-25 19:47:28 +0800

    [BugFix] remain agg having filter when remove aggOperator

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/RemoveAggregationFromAggTable.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/RemoveAggregationFromAggTable.java
@@ -15,6 +15,7 @@ import com.starrocks.sql.optimizer.OptExpression;
 import com.starrocks.sql.optimizer.OptimizerContext;
 import com.starrocks.sql.optimizer.operator.OperatorType;
 import com.starrocks.sql.optimizer.operator.logical.LogicalAggregationOperator;
+import com.starrocks.sql.optimizer.operator.logical.LogicalFilterOperator;
 import com.starrocks.sql.optimizer.operator.logical.LogicalOlapScanOperator;
 import com.starrocks.sql.optimizer.operator.logical.LogicalProjectOperator;
 import com.starrocks.sql.optimizer.operator.pattern.Pattern;
@@ -51,9 +52,6 @@ public class RemoveAggregationFromAggTable extends TransformationRule {
     @Override
     public boolean check(OptExpression input, OptimizerContext context) {
         OptExpression scanExpression = input.getInputs().get(0);
-        if (!(scanExpression.getOp() instanceof LogicalOlapScanOperator)) {
-            return false;
-        }
         LogicalOlapScanOperator scanOperator = (LogicalOlapScanOperator) scanExpression.getOp();
         if (scanOperator.getProjection() != null) {
             return false;
@@ -129,19 +127,28 @@ public class RemoveAggregationFromAggTable extends TransformationRule {
             // in this case, CallOperator must have only one child ColumnRefOperator
             projectMap.put(entry.getKey(), entry.getValue().getChild(0));
         }
+
+        Map<ColumnRefOperator, ScalarOperator> newProjectMap = Maps.newHashMap();
+        ReplaceColumnRefRewriter rewriter = new ReplaceColumnRefRewriter(projectMap);
+
+        OptExpression newChildOpt = input.inputAt(0);
+        if (aggregationOperator.getPredicate() != null) {
+            // rewrite the having predicate. replace the aggFunc by the columnRef
+            ScalarOperator newPredicate = rewriter.rewrite(aggregationOperator.getPredicate());
+            LogicalFilterOperator filterOperator = new LogicalFilterOperator(newPredicate);
+            newChildOpt = OptExpression.create(filterOperator, newChildOpt);
+        }
+
         if (aggregationOperator.getProjection() != null) {
-            Map<ColumnRefOperator, ScalarOperator> newProjectMap =
-                    Maps.newHashMap(aggregationOperator.getProjection().getColumnRefMap());
-            ReplaceColumnRefRewriter rewriter = new ReplaceColumnRefRewriter(projectMap);
             for (Map.Entry<ColumnRefOperator, ScalarOperator> entry :
                     aggregationOperator.getProjection().getColumnRefMap().entrySet()) {
+                // rewrite the projection of this agg. replace the aggFunc by the columnRef
                 ScalarOperator rewrittenOperator = rewriter.rewrite(entry.getValue());
                 newProjectMap.put(entry.getKey(), rewrittenOperator);
             }
-            projectMap.clear();
-            projectMap.putAll(newProjectMap);
         }
-        LogicalProjectOperator projectOperator = new LogicalProjectOperator(projectMap);
-        return Lists.newArrayList(OptExpression.create(projectOperator, input.getInputs().get(0)));
+
+        LogicalProjectOperator projectOperator = new LogicalProjectOperator(newProjectMap);
+        return Lists.newArrayList(OptExpression.create(projectOperator, newChildOpt));
     }
 }
---
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/RemoveAggTest.java
@@ -0,0 +1,66 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Inc.
+
+package com.starrocks.sql.plan;
+
+
+import com.google.common.collect.Lists;
+import com.starrocks.common.FeConstants;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class RemoveAggTest extends PlanTestBase{
+
+    @BeforeAll
+    public static void beforeClass() throws Exception {
+        PlanTestBase.beforeClass();
+        FeConstants.runningUnitTest = true;
+    }
+
+    @ParameterizedTest(name = "sql_{index}: {0}.")
+    @MethodSource("removeAggSqlCases")
+    void removeAggTest(String sql) throws Exception {
+        String plan = getFragmentPlan(sql);
+        assertNotContains(plan, "AGGREGATE");
+        assertContains(plan, "PREAGGREGATION: OFF");
+    }
+
+
+    @ParameterizedTest(name = "sql_{index}: {0}.")
+    @MethodSource("remainAggSqlCases")
+    void remainAggTest(String sql) throws Exception {
+        String plan = getFragmentPlan(sql);
+        assertContains(plan, "AGGREGATE");
+    }
+
+
+    private static Stream<Arguments> removeAggSqlCases() {
+        List<String> sqlList = Lists.newArrayList();
+        sqlList.add("select max(v1) from test_agg group by k1, k2, k3;");
+        sqlList.add("select max(v1) from test_agg group by k1, k2, k3 having(max(v1) = 1)");
+        sqlList.add("select max(v1), min(v2) from test_agg group by k1, k2, k3 " +
+                "having (max(v1) > min(v2) or abs(max(v1)) is null)");
+        sqlList.add("select max(v1), min(v2), abs(123) from test_agg group by k1, k2, k3 having (max(v1) + min(v2) > sum(v3))");
+        sqlList.add("select max(v1) + min(v2) from test_agg group by k1, k2, k3 having (max(v1) + min(v2)) is not null");
+        sqlList.add("select max(v1) + min(v2) from test_agg group by k1, k2, k3 " +
+                "having ((max(v1) + min(v2)) is not null or k1 > 4)");
+        return sqlList.stream().map(e -> Arguments.of(e));
+    }
+
+    private static Stream<Arguments> remainAggSqlCases() {
+        List<String> sqlList = Lists.newArrayList();
+        sqlList.add("select max(v1) from t0 group by v2");
+        sqlList.add("select min(v1) from test_agg group by k1, k2, k3");
+        sqlList.add("select max(v1) from test_agg group by k1");
+        sqlList.add("select max(v1) from test_agg group by abs(k1), k1, k2, k3");
+        sqlList.add("select hll_union_agg(h1) from test_agg group by k1, k2, k3");
+        return sqlList.stream().map(e -> Arguments.of(e));
+    }
+
+
+
+}
