16d724ff4181f9b1662db3aad6422f5056e09482
xyz <a997647204@gmail.com>
2022-10-25 15:01:15 +0800

    [Feature] make pipeline tablet sink can be adjust (#12070)
    The current parallelism of the tablet sink in pipeline mode is always 1.
    This one pr allows the parallelism to be adjusted.
    
    The method is that FE will first pre-set the parallelism of the tablet sink,
    and on the BE side, if it finds that the parallelism of the tablet sink operator
    is not equal to the parallelism of the source operator,
    then BE will convert the parallelism through the local passthrough exchange.
    
    For shuffle load service(primary table), because we have implemented single replica load,
    lator we will disable shuffle load service by default, so here we always set its dop to be 1.

--- fe/fe-core/src/main/java/com/starrocks/load/loadv2/LoadingTaskPlanner.java
+++ fe/fe-core/src/main/java/com/starrocks/load/loadv2/LoadingTaskPlanner.java
@@ -102,6 +102,7 @@ public class LoadingTaskPlanner {
     private List<Pair<Integer, ColumnDict>> globalDicts = Lists.newArrayList();
 
     private Map<String, String> sessionVariables = null;
+    ConnectContext context = null;
 
     public LoadingTaskPlanner(Long loadJobId, long txnId, long dbId, OlapTable table,
             BrokerDesc brokerDesc, List<BrokerFileGroup> brokerFileGroups,
@@ -120,6 +121,10 @@ public class LoadingTaskPlanner {
         this.parallelInstanceNum = Config.load_parallel_instance_num;
         this.startTime = startTime;
         this.sessionVariables = sessionVariables;
+    }
+
+    public void setConnectContext(ConnectContext context) {
+        this.context = context;
     }
 
     public void plan(TUniqueId loadId, List<List<TBrokerFileStatus>> fileStatusesList, int filesAdded)
@@ -188,8 +193,7 @@ public class LoadingTaskPlanner {
         TWriteQuorumType writeQuorum = table.writeQuorum();
 
         List<Long> partitionIds = getAllPartitionIds();
-        // Parallel pipeline loads are currently not supported, so disable the pipeline engine when users need parallel load
-        OlapTableSink olapTableSink = new OlapTableSink(table, tupleDesc, partitionIds, parallelInstanceNum <= 1, writeQuorum);
+        OlapTableSink olapTableSink = new OlapTableSink(table, tupleDesc, partitionIds, true, writeQuorum);
         olapTableSink.init(loadId, txnId, dbId, timeoutS);
         olapTableSink.complete();
 
@@ -197,12 +201,21 @@ public class LoadingTaskPlanner {
         // 3. Plan fragment
         PlanFragment sinkFragment = new PlanFragment(new PlanFragmentId(0), scanNode, DataPartition.RANDOM);
         sinkFragment.setSink(olapTableSink);
-        // At present, we only support dop=1 for olap table sink.
-        // because tablet writing needs to know the number of senders in advance
-        // and guaranteed order of data writing
-        // It can be parallel only in some scenes, for easy use 1 dop now.
-        sinkFragment.setPipelineDop(1);
-        sinkFragment.setParallelExecNum(parallelInstanceNum);
+
+        if (this.context != null) {
+            if (this.context.getSessionVariable().isEnablePipelineEngine() && Config.enable_pipeline_load) {
+                sinkFragment.setPipelineDop(parallelInstanceNum);
+                sinkFragment.setParallelExecNum(1);
+                sinkFragment.setHasOlapTableSink();
+                sinkFragment.setForceAssignScanRangesPerDriverSeq();
+            } else {
+                sinkFragment.setPipelineDop(1);
+                sinkFragment.setParallelExecNum(parallelInstanceNum);
+            }
+        } else {
+            sinkFragment.setPipelineDop(1);
+            sinkFragment.setParallelExecNum(parallelInstanceNum);
+        }
         // After data loading, we need to check the global dict for low cardinality string column
         // whether update.
         sinkFragment.setLoadGlobalDicts(globalDicts);
@@ -231,7 +244,6 @@ public class LoadingTaskPlanner {
 
         // 3. Scan plan fragment
         PlanFragment scanFragment = new PlanFragment(new PlanFragmentId(0), scanNode, DataPartition.RANDOM);
-        scanFragment.setParallelExecNum(parallelInstanceNum);
 
         fragments.add(scanFragment);
 
@@ -256,19 +268,26 @@ public class LoadingTaskPlanner {
         TWriteQuorumType writeQuorum = table.writeQuorum();
 
         List<Long> partitionIds = getAllPartitionIds();
-        // Parallel pipeline loads are currently not supported, so disable the pipeline engine when users need parallel load
-        OlapTableSink olapTableSink = new OlapTableSink(table, tupleDesc, partitionIds, parallelInstanceNum <= 1, writeQuorum);
+
+        OlapTableSink olapTableSink = new OlapTableSink(table, tupleDesc, partitionIds, true, writeQuorum);
         olapTableSink.init(loadId, txnId, dbId, timeoutS);
         olapTableSink.complete();
 
         // 6. Sink plan fragment
         sinkFragment.setSink(olapTableSink);
-        // At present, we only support dop=1 for olap table sink.
-        // because tablet writing needs to know the number of senders in advance
-        // and guaranteed order of data writing
-        // It can be parallel only in some scenes, for easy use 1 dop now.
-        sinkFragment.setPipelineDop(1);
-        sinkFragment.setParallelExecNum(parallelInstanceNum);
+        // For shuffle broker load, we only support tablet sink dop = 1
+        // because for tablet sink dop > 1, local passthourgh exchange will influence the order of sending,
+        // which may lead to inconsistent replica for primary key.
+        // If you want to set tablet sink dop > 1, please enable single tablet loading and disable shuffle service
+        if (this.context != null) {
+            if (this.context.getSessionVariable().isEnablePipelineEngine() && Config.enable_pipeline_load) {
+                sinkFragment.setHasOlapTableSink();
+                sinkFragment.setForceSetTableSinkDop();
+                sinkFragment.setForceAssignScanRangesPerDriverSeq();
+            }
+            sinkFragment.setPipelineDop(1);
+            sinkFragment.setParallelExecNum(parallelInstanceNum);
+        }
         // After data loading, we need to check the global dict for low cardinality string column
         // whether update.
         sinkFragment.setLoadGlobalDicts(globalDicts);
