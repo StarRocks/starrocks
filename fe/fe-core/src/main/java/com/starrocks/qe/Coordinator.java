15b1d045fecf686fe2edc08f5dfc8629bbd48629
ZiheLiu <ziheliu1024@gmail.com>
2022-10-25 16:46:54 +0800

    fix comment

--- fe/fe-core/src/main/java/com/starrocks/qe/Coordinator.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/Coordinator.java
@@ -2196,14 +2196,12 @@ public class Coordinator {
                     });
                 }
 
-                // ScanNode with scanId in BE uses maxDriverSeq+1 in nodeToPerDriverSeqScanRanges[scanId] as pipelineDop.
-                // PipelineDop of all the ScanNodes in the fragment should be the same regardless of the difference of
-                // each ScanNode's tablets, so maxDriverSeq=pipelineDop-1 is forced to set to empty list if it is absent.
-                if (assignPerDriverSeq && instanceParam.pipelineDop > 0) {
-                    int maxDriverSeq = instanceParam.pipelineDop - 1;
-                    instanceParam.nodeToPerDriverSeqScanRanges.forEach((scanId, perDriverSeqScanRanges) ->
-                            perDriverSeqScanRanges.computeIfAbsent(maxDriverSeq, k -> new ArrayList<>())
-                    );
+                if (assignPerDriverSeq) {
+                    instanceParam.nodeToPerDriverSeqScanRanges.forEach((scanId, perDriverSeqScanRanges) -> {
+                        for (int driverSeq = 0; driverSeq < instanceParam.pipelineDop; ++driverSeq) {
+                            perDriverSeqScanRanges.computeIfAbsent(driverSeq, k -> new ArrayList<>());
+                        }
+                    });
                 }
 
                 params.instanceExecParams.add(instanceParam);
