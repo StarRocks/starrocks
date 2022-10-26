ad761e0ad3eb64d6e5147124966ed2c8d3aa7ab1
Smith Cruise <chendingchao1@126.com>
2022-10-26 13:47:48 +0800

    fix compile

--- be/src/column/object_column.h
+++ be/src/column/object_column.h
@@ -247,9 +247,5 @@ private:
     // Only for data loading
     mutable Buffer<Slice> _slices;
     mutable Buffer<uint8_t> _buffer;
-
-    // Just use to hide Clion error warning.
-    // https://stackoverflow.com/questions/18515183/c-overloaded-virtual-function-warning-by-clang
-    using Column::append;
 };
 } // namespace starrocks::vectorized
