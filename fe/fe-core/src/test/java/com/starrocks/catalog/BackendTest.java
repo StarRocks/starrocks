31681857ee89a6836661adeb1b46805da5a5ea8a
waittting <491771532@qq.com>
2022-10-31 09:47:55 +0800

    HeartbeatResponse add prop aliveStatus (cherry-pick #12481)

--- fe/fe-core/src/test/java/com/starrocks/catalog/BackendTest.java
+++ fe/fe-core/src/test/java/com/starrocks/catalog/BackendTest.java
@@ -25,6 +25,7 @@ import com.google.common.collect.ImmutableMap;
 import com.starrocks.analysis.AccessTestUtil;
 import com.starrocks.common.FeConstants;
 import com.starrocks.system.Backend;
+import com.starrocks.system.BackendHbResponse;
 import com.starrocks.thrift.TDisk;
 import com.starrocks.thrift.TStorageMedium;
 import org.junit.Assert;
@@ -224,4 +225,11 @@ public class BackendTest {
 
     }
 
+    @Test
+    public void testHeartbeatOk() throws Exception {
+        Backend be = new Backend();
+        BackendHbResponse hbResponse = new BackendHbResponse();
+        boolean isChanged = be.handleHbResponse(hbResponse, false);
+        Assert.assertTrue(isChanged);
+    }
 }
