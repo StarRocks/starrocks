d8e56564366b8b265c3c80bd8153aa8ee3053c10
gengjun-git <gengjun@starrocks.com>
2022-11-02 12:53:43 +0800

    [BugFix] Fix drop partition log is written behind drop table log bug (#12553)
    * fix
    
    * fix
    
    (cherry picked from commit c88a665ea89290319af519b0e625c96a41b7ad89)

--- fe/fe-core/src/main/java/com/starrocks/clone/DynamicPartitionScheduler.java
+++ fe/fe-core/src/main/java/com/starrocks/clone/DynamicPartitionScheduler.java
@@ -274,7 +274,7 @@ public class DynamicPartitionScheduler extends LeaderDaemon {
 
     private void executeDynamicPartition() {
         Iterator<Pair<Long, Long>> iterator = dynamicPartitionTableInfo.iterator();
-        while (iterator.hasNext()) {
+        OUTER: while (iterator.hasNext()) {
             Pair<Long, Long> tableInfo = iterator.next();
             Long dbId = tableInfo.first;
             Long tableId = tableInfo.second;
@@ -338,7 +338,10 @@ public class DynamicPartitionScheduler extends LeaderDaemon {
             }
 
             for (DropPartitionClause dropPartitionClause : dropPartitionClauses) {
-                db.writeLock();
+                if (!db.writeLockAndCheckExist()) {
+                    LOG.warn("db: {}({}) has been dropped, skip", db.getFullName(), db.getId());
+                    continue OUTER;
+                }
                 try {
                     GlobalStateMgr.getCurrentState().dropPartition(db, olapTable, dropPartitionClause);
                     clearDropPartitionFailedMsg(tableName);
--- fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
+++ fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
@@ -1250,7 +1250,10 @@ public class LocalMetastore implements ConnectorMetadata {
             buildPartitions(db, copiedTable, partitionList);
 
             // check again
-            db.writeLock();
+            if (!db.writeLockAndCheckExist()) {
+                throw new DdlException("db " + db.getFullName()
+                        + "(" + db.getId() + ") has been dropped");
+            }
             Set<String> existPartitionNameSet = Sets.newHashSet();
             try {
                 olapTable = checkTable(db, tableName);
@@ -1404,6 +1407,7 @@ public class LocalMetastore implements ConnectorMetadata {
     }
 
     public void dropPartition(Database db, Table table, DropPartitionClause clause) throws DdlException {
+        CatalogUtils.checkTableExist(db, table.getName());
         OlapTable olapTable = (OlapTable) table;
         Preconditions.checkArgument(db.isWriteLockHeldByCurrentThread());
 
