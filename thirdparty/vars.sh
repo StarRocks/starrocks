bce11de838d35e0777fcaa3c50679bb71acfd201
wanweiqiangintel <weiqiang.wan@intel.com>
2022-11-02 18:39:36 +0800

    [Enhancement] update simdjson version (#12776)

--- thirdparty/vars.sh
+++ thirdparty/vars.sh
@@ -153,10 +153,10 @@ RAPIDJSON_SOURCE=rapidjson-1.1.0
 RAPIDJSON_MD5SUM="badd12c511e081fec6c89c43a7027bce"
 
 # simdjson
-SIMDJSON_DOWNLOAD="https://github.com/simdjson/simdjson/archive/refs/tags/v1.0.2.tar.gz"
-SIMDJSON_NAME=simdjson-v1.0.2.tar.gz
-SIMDJSON_SOURCE=simdjson-1.0.2
-SIMDJSON_MD5SUM="5bb34cca7087a99c450dbdfe406bdc7d"
+SIMDJSON_DOWNLOAD="https://github.com/simdjson/simdjson/archive/refs/tags/v2.2.0.tar.gz"
+SIMDJSON_NAME=simdjson-v2.2.0.tar.gz
+SIMDJSON_SOURCE=simdjson-2.2.0
+SIMDJSON_MD5SUM="9bd0ced53281484d8842a9429065943d"
 
 # curl
 CURL_DOWNLOAD="https://curl.se/download/curl-7.79.0.tar.gz"
