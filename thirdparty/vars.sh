42326013304d944cb33d3f53629e422ebc8bf3ca
stephen <stephen5217@163.com>
2022-11-11 22:18:53 +0800

    modify be thrift version

--- thirdparty/vars.sh
+++ thirdparty/vars.sh
@@ -77,10 +77,10 @@ OPENSSL_SOURCE=openssl-OpenSSL_1_1_1m
 OPENSSL_MD5SUM="710c2368d28f1a25ab92e25b5b9b11ec"
 
 # thrift
-THRIFT_DOWNLOAD="http://archive.apache.org/dist/thrift/0.13.0/thrift-0.13.0.tar.gz"
-THRIFT_NAME=thrift-0.13.0.tar.gz
-THRIFT_SOURCE=thrift-0.13.0
-THRIFT_MD5SUM="38a27d391a2b03214b444cb13d5664f1"
+THRIFT_DOWNLOAD="http://archive.apache.org/dist/thrift/0.14.1/thrift-0.14.1.tar.gz"
+THRIFT_NAME=thrift-0.14.1.tar.gz
+THRIFT_SOURCE=thrift-0.14.1
+THRIFT_MD5SUM="c64434548438df2cb1e53fb27c600e85"
 
 # protobuf
 PROTOBUF_DOWNLOAD="https://github.com/google/protobuf/archive/v3.14.0.tar.gz"
