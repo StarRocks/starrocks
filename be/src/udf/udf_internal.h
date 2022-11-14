60c99b2149194645d7f1243cf2c37d654f89b8b3
stdpain <34912776+stdpain@users.noreply.github.com>
2022-11-14 14:07:58 +0800

    [Refactor] remove some unused code in FunctionContext (#13066)

--- be/src/udf/udf_internal.h
+++ be/src/udf/udf_internal.h
@@ -33,7 +33,6 @@
 
 namespace starrocks {
 
-class FreePool;
 class MemPool;
 class RuntimeState;
 
@@ -54,14 +53,6 @@ public:
             const std::vector<starrocks_udf::FunctionContext::TypeDesc>& arg_types, int varargs_buffer_size,
             bool debug);
 
-    /// Create a FunctionContext for a UDA. Identical to the UDF version except for the
-    /// intermediate type. Caller is responsible for deleting it.
-    static starrocks_udf::FunctionContext* create_context(
-            RuntimeState* state, MemPool* pool, const starrocks_udf::FunctionContext::TypeDesc& intermediate_type,
-            const starrocks_udf::FunctionContext::TypeDesc& return_type,
-            const std::vector<starrocks_udf::FunctionContext::TypeDesc>& arg_types, int varargs_buffer_size,
-            bool debug);
-
     ~FunctionContextImpl();
 
     FunctionContextImpl(starrocks_udf::FunctionContext* parent);
@@ -74,8 +65,6 @@ public:
     starrocks_udf::FunctionContext* clone(MemPool* pool);
 
     void set_constant_columns(std::vector<vectorized::ColumnPtr> columns) { _constant_columns = std::move(columns); }
-
-    uint8_t* varargs_buffer() { return _varargs_buffer; }
 
     bool closed() const { return _closed; }
 
@@ -92,19 +81,6 @@ public:
     size_t mem_usage() { return _mem_usage; }
     void add_mem_usage(size_t size) { _mem_usage += size; }
 
-    // Allocates a buffer of 'byte_size' with "local" memory management. These
-    // allocations are not freed one by one but freed as a pool by FreeLocalAllocations()
-    // This is used where the lifetime of the allocation is clear.
-    // For UDFs, the allocations can be freed at the row level.
-    // TODO: free them at the batch level and save some copies?
-    uint8_t* allocate_local(int64_t byte_size);
-
-    // Returns true if there are no outstanding allocations.
-    bool check_allocations_empty();
-
-    // Returns true if there are no outstanding local allocations.
-    bool check_local_allocations_empty();
-
     RuntimeState* state() { return _state; }
     void set_error(const char* error_msg);
     bool has_error();
@@ -112,19 +88,9 @@ public:
 
     vectorized::JavaUDAFContext* udaf_ctxs() { return _jvm_udaf_ctxs.get(); }
 
-    std::string& string_result() { return _string_result; }
-
 private:
     friend class starrocks_udf::FunctionContext;
     friend class ExprContext;
-
-    /// Preallocated buffer for storing varargs (if the function has any). Allocated and
-    /// owned by this object, but populated by an Expr function.
-    //
-    /// This is the first field in the class so it's easy to access in codegen'd functions.
-    /// Don't move it or add fields above unless you know what you're doing.
-    uint8_t* _varargs_buffer;
-    int _varargs_buffer_size;
 
     // The number of calls to Update()/Remove().
     int64_t _num_updates;
@@ -134,9 +100,6 @@ private:
     starrocks_udf::FunctionContext* _context;
 
     MemPool* _mem_pool = nullptr;
-
-    // Pool to service allocations from.
-    FreePool* _pool;
 
     // We use the query's runtime state to report errors and warnings. NULL for test
     // contexts.
@@ -154,22 +117,9 @@ private:
     // The number of warnings reported.
     int64_t _num_warnings;
 
-    // Allocations made and still owned by the user function.
-    std::map<uint8_t*, int> _allocations;
-    std::vector<uint8_t*> _local_allocations;
-
     /// The function state accessed via FunctionContext::Get/SetFunctionState()
     void* _thread_local_fn_state;
     void* _fragment_local_fn_state;
-
-    // The number of bytes allocated externally by the user function. In some cases,
-    // it is too inconvenient to use the Allocate()/Free() APIs in the FunctionContext,
-    // particularly for existing codebases (e.g. they use std::vector). Instead, they'll
-    // have to track those allocations manually.
-    int64_t _external_bytes_tracked;
-
-    // Type descriptor for the intermediate type of a UDA. Set to INVALID_TYPE for UDFs.
-    starrocks_udf::FunctionContext::TypeDesc _intermediate_type;
 
     // Type descriptor for the return type of the function.
     starrocks_udf::FunctionContext::TypeDesc _return_type;
@@ -181,8 +131,6 @@ private:
 
     // Indicates whether this context has been closed. Used for verification/debugging.
     bool _closed;
-
-    std::string _string_result;
 
     // this is used for count memory usage of aggregate state
     size_t _mem_usage = 0;
