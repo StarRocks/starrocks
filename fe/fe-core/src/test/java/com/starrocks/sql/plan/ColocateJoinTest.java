47ac620ced6285fcb589cf00ea10aa9d390e00ff
packy <wangchao@starrocks.com>
2022-11-14 16:04:36 +0800

    [Enhancement] colocate join need consider column equivalent conduction

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/ColocateJoinTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/ColocateJoinTest.java
@@ -20,7 +20,19 @@ class ColocateJoinTest extends PlanTestBase {
     public static void beforeClass() throws Exception {
         PlanTestBase.beforeClass();
         FeConstants.runningUnitTest = true;
-        starRocksAssert.withTable("CREATE TABLE `colocate_t2_1` (\n" + "  `v7` bigint NULL COMMENT \"\",\n" + "  `v8` bigint NULL COMMENT \"\",\n" + "  `v9` bigint NULL\n" + ") ENGINE=OLAP\n" + "DUPLICATE KEY(`v7`, `v8`, v9)\n" + "DISTRIBUTED BY HASH(`v7`) BUCKETS 3\n" + "PROPERTIES (\n" + "\"replication_num\" = \"1\",\n" + "\"in_memory\" = \"false\",\n" + "\"storage_format\" = \"DEFAULT\",\n" + "\"colocate_with\" = \"colocate_group_1\"" + ");");
+        starRocksAssert.withTable("CREATE TABLE `colocate_t2_1` (\n" +
+                "  `v7` bigint NULL COMMENT \"\",\n" +
+                "  `v8` bigint NULL COMMENT \"\",\n" +
+                "  `v9` bigint NULL\n" +
+                ") ENGINE=OLAP\n" +
+                "DUPLICATE KEY(`v7`, `v8`, v9)\n" +
+                "DISTRIBUTED BY HASH(`v7`) BUCKETS 3\n" +
+                "PROPERTIES (\n" +
+                "\"replication_num\" = \"1\",\n" +
+                "\"in_memory\" = \"false\",\n" +
+                "\"storage_format\" = \"DEFAULT\",\n" +
+                "\"colocate_with\" = \"colocate_group_1\"" +
+                ");");
     }
 
     @ParameterizedTest(name = "sql_{index}: {0}.")
@@ -42,6 +54,9 @@ class ColocateJoinTest extends PlanTestBase {
 
     private static Stream<Arguments> colocateJoinOnceSqls() {
         List<String> sqls = Lists.newArrayList();
+
+        // sqls should colocate join but not support now
+        List<String> unsupportedSqls = Lists.newArrayList();
         sqls.add("select * from colocate_t0 join colocate_t1 on v1 = v5 and v1 = v4");
         sqls.add("select * from colocate_t0 join colocate_t1 on v2 = v4 and v1 = v4");
         sqls.add("select * from colocate_t0 join colocate_t1 on v1 + v2 = v4 + v5 and v1 = v4 + 1 and v1 = v4");
@@ -52,14 +67,16 @@ class ColocateJoinTest extends PlanTestBase {
         sqls.add("select * from colocate_t0, colocate_t1, colocate_t2_1 where  v1 = v5 and v5 = v7");
 
         // now we cannot derive v1 = v7 plan from the below sqls
-//        sqls.add("select * from colocate_t0 join colocate_t1 on v1 = v5 join colocate_t2 on v5 = v7");
-//        sqls.add("select * from colocate_t0 join colocate_t1 on v1 = v5 + v6 join colocate_t2 on v5 + v6 = v7");
-//        sqls.add("select * from colocate_t0, colocate_t1, colocate_t2_1 where  v1 = v5 + v6 and v5 + v6 = v7");
+        unsupportedSqls.add("select * from colocate_t0 join colocate_t1 on v1 = v5 join colocate_t2 on v5 = v7");
+        unsupportedSqls.add("select * from colocate_t0 join colocate_t1 on v1 = v5 + v6 join colocate_t2 on v5 + v6 = v7");
+        unsupportedSqls.add("select * from colocate_t0, colocate_t1, colocate_t2_1 where  v1 = v5 + v6 and v5 + v6 = v7");
         return sqls.stream().map(e -> Arguments.of(e));
     }
 
     private static Stream<Arguments> colocateJoinTwiceSqls() {
         List<String> sqls = Lists.newArrayList();
+        // sqls should colocate join but not support now
+        List<String> unsupportedSqls = Lists.newArrayList();
         sqls.add("select * from colocate_t0 join colocate_t1 on v1 = v4 join colocate_t2_1 on v4 = v7");
         sqls.add("select * from colocate_t0 join colocate_t1 on v1 = v5 and v1 = v4 join colocate_t2_1 on v5 = v7 and v7 = v2");
         sqls.add("select * from colocate_t0 join colocate_t1 on v1 = v5 join colocate_t2_1 on v1 = v4 and v1 = v7");
@@ -71,12 +88,12 @@ class ColocateJoinTest extends PlanTestBase {
 
 
         // the expr col seems not been equivalent conduction
-//        sqls.add("select * from colocate_t0 join colocate_t1 on v1 = v5 and v1 = v4 + v6 and v1 = v4 join colocate_t2_1 on " +
-//                "v4 + v6 = v7");
-//        sqls.add("select * from colocate_t0 join colocate_t1 on v1 + v2 = v4 and v1 + v2 = v5 - v4 join colocate_t2_1 on " +
-//                "v5 - v4 = v7 and v7 = v1");
-//        sqls.add("select * from colocate_t0 join colocate_t1 on v1 + v2 = v4 - v3 and v1 = v4 + v5 join colocate_t2_1 on " +
-//                "v4 + v5 = v4 and v4 + v5 = v7");
+        unsupportedSqls.add("select * from colocate_t0 join colocate_t1 on v1 = v5 and v1 = v4 + v6 and v1 = v4 " +
+                "join colocate_t2_1 on v4 + v6 = v7");
+        unsupportedSqls.add("select * from colocate_t0 join colocate_t1 on v1 + v2 = v4 and v1 + v2 = v5 - v4 " +
+                "join colocate_t2_1 on v5 - v4 = v7 and v7 = v1");
+        unsupportedSqls.add("select * from colocate_t0 join colocate_t1 on v1 + v2 = v4 - v3 and v1 = v4 + v5 " +
+                "join colocate_t2_1 on v4 + v5 = v4 and v4 + v5 = v7");
         return sqls.stream().map(e -> Arguments.of(e));
     }
 
