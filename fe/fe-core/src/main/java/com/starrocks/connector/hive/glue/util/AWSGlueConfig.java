45b1d33402c36275d59302fd890ad511196b4790
Youngwb <yangwenbo_mailbox@163.com>
2022-11-14 22:07:05 +0800

    [Enhancement] Normalizes the naming of the glue property (#13299)

--- fe/fe-core/src/main/java/com/starrocks/connector/hive/glue/util/AWSGlueConfig.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/hive/glue/util/AWSGlueConfig.java
@@ -9,28 +9,28 @@ public final class AWSGlueConfig {
     private AWSGlueConfig() {
     }
 
-    public static final String AWS_GLUE_ENDPOINT = "aws.glue.endpoint";
-    public static final String AWS_REGION = "aws.region";
+    public static final String AWS_GLUE_ENDPOINT = "aws.hive.metastore.glue.endpoint";
+    public static final String AWS_REGION = "aws.hive.metastore.glue.region";
     public static final String AWS_CATALOG_CREDENTIALS_PROVIDER_FACTORY_CLASS
             = "aws.catalog.credentials.provider.factory.class";
 
-    public static final String AWS_GLUE_MAX_RETRY = "aws.glue.max-error-retries";
+    public static final String AWS_GLUE_MAX_RETRY = "aws.hive.metastore.glue.max-error-retries";
     public static final int DEFAULT_MAX_RETRY = 5;
 
-    public static final String AWS_GLUE_MAX_CONNECTIONS = "aws.glue.max-connections";
+    public static final String AWS_GLUE_MAX_CONNECTIONS = "aws.hive.metastore.glue.max-connections";
     public static final int DEFAULT_MAX_CONNECTIONS = ClientConfiguration.DEFAULT_MAX_CONNECTIONS;
 
-    public static final String AWS_GLUE_CONNECTION_TIMEOUT = "aws.glue.connection-timeout";
+    public static final String AWS_GLUE_CONNECTION_TIMEOUT = "aws.hive.metastore.glue.connection-timeout";
     public static final int DEFAULT_CONNECTION_TIMEOUT = ClientConfiguration.DEFAULT_CONNECTION_TIMEOUT;
 
-    public static final String AWS_GLUE_SOCKET_TIMEOUT = "aws.glue.socket-timeout";
+    public static final String AWS_GLUE_SOCKET_TIMEOUT = "aws.hive.metastore.glue.socket-timeout";
     public static final int DEFAULT_SOCKET_TIMEOUT = ClientConfiguration.DEFAULT_SOCKET_TIMEOUT;
 
-    public static final String AWS_GLUE_DB_CACHE_ENABLE = "aws.glue.cache.db.enable";
-    public static final String AWS_GLUE_DB_CACHE_SIZE = "aws.glue.cache.db.size";
-    public static final String AWS_GLUE_DB_CACHE_TTL_MINS = "aws.glue.cache.db.ttl-mins";
+    public static final String AWS_GLUE_DB_CACHE_ENABLE = "aws.hive.metastore.glue.cache.db.enable";
+    public static final String AWS_GLUE_DB_CACHE_SIZE = "aws.hive.metastore.glue.cache.db.size";
+    public static final String AWS_GLUE_DB_CACHE_TTL_MINS = "aws.hive.metastore.glue.cache.db.ttl-mins";
 
-    public static final String AWS_GLUE_TABLE_CACHE_ENABLE = "aws.glue.cache.table.enable";
-    public static final String AWS_GLUE_TABLE_CACHE_SIZE = "aws.glue.cache.table.size";
-    public static final String AWS_GLUE_TABLE_CACHE_TTL_MINS = "aws.glue.cache.table.ttl-mins";
+    public static final String AWS_GLUE_TABLE_CACHE_ENABLE = "aws.hive.metastore.glue.cache.table.enable";
+    public static final String AWS_GLUE_TABLE_CACHE_SIZE = "aws.hive.metastore.glue.cache.table.size";
+    public static final String AWS_GLUE_TABLE_CACHE_TTL_MINS = "aws.hive.metastore.glue.cache.table.ttl-mins";
 }
