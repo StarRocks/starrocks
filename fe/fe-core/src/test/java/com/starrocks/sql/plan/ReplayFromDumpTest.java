dae3f7f70f8cdc4d8e38b44dfbabdbc2e70e3a33
Kevin Li <ming.moriarty@gmail.com>
2022-11-15 15:32:39 +0800

    [BugFix] fix JoinEliminateNulls ut in ReplyFromDumpTest (#13394)

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/ReplayFromDumpTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/ReplayFromDumpTest.java
@@ -192,7 +192,7 @@ public class ReplayFromDumpTest {
         Pair<QueryDumpInfo, String> replayPair =
                 getCostPlanFragment(getDumpInfoFromFile("query_dump/join_eliminate_nulls"), sessionVariable);
         Assert.assertTrue(replayPair.second, replayPair.second.contains("  6:NESTLOOP JOIN\n" +
-                "  |  join op: CROSS JOIN\n" +
+                "  |  join op: INNER JOIN\n" +
                 "  |  other join predicates: CASE 174: type WHEN '1' THEN concat('ocms_', name) " +
                 "= 'ocms_fengyang56' WHEN '0' THEN TRUE ELSE FALSE END\n" +
                 "  |  cardinality: 2500"));
