eede65f4801c8cf03d773758fc010879aaaf4896
ABing <101158374+ABingHuang@users.noreply.github.com>
2022-11-17 18:08:59 +0800

    [Enhancement] Add support for join/aggregation/union mv query rewrite (#13202)

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/RuleSet.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/RuleSet.java
@@ -122,7 +122,9 @@ import com.starrocks.sql.optimizer.rule.transformation.ScalarApply2JoinRule;
 import com.starrocks.sql.optimizer.rule.transformation.SplitAggregateRule;
 import com.starrocks.sql.optimizer.rule.transformation.SplitLimitRule;
 import com.starrocks.sql.optimizer.rule.transformation.SplitTopNRule;
-import com.starrocks.sql.optimizer.rule.transformation.materialization.rule.FilterScanRule;
+import com.starrocks.sql.optimizer.rule.transformation.materialization.rule.AggregateJoinRule;
+import com.starrocks.sql.optimizer.rule.transformation.materialization.rule.AggregateScanRule;
+import com.starrocks.sql.optimizer.rule.transformation.materialization.rule.OnlyJoinRule;
 import com.starrocks.sql.optimizer.rule.transformation.materialization.rule.OnlyScanRule;
 
 import java.util.List;
@@ -348,7 +350,12 @@ public class RuleSet {
 
         REWRITE_RULES.put(RuleSetType.SINGLE_TABLE_MV_REWRITE, ImmutableList.of(
                 OnlyScanRule.getInstance(),
-                FilterScanRule.getInstance()
+                AggregateScanRule.getInstance()
+        ));
+
+        REWRITE_RULES.put(RuleSetType.MULTI_TABLE_MV_REWRITE, ImmutableList.of(
+                OnlyJoinRule.getInstance(),
+                AggregateJoinRule.getInstance()
         ));
     }
 
@@ -365,6 +372,10 @@ public class RuleSet {
 
     public void addJoinCommutativityWithOutInnerRule() {
         transformRules.add(JoinCommutativityWithOutInnerRule.getInstance());
+    }
+
+    public void addMultiTableMvRewriteRule() {
+        transformRules.addAll(REWRITE_RULES.get(RuleSetType.MULTI_TABLE_MV_REWRITE));
     }
 
     public List<Rule> getTransformRules() {
