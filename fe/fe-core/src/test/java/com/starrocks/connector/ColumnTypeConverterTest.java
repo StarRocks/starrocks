df75523b3c24dae48f1a16324cdd1a4686b8224e
stephen <91597003+stephen-shelby@users.noreply.github.com>
2022-11-18 13:18:20 +0800

    [Enhancement] sync column comment when manually executing refresh external table (#13627)
    (cherry picked from commit be4b077137f874fd0b5dfbc94275c3763ba48430)

--- fe/fe-core/src/test/java/com/starrocks/connector/ColumnTypeConverterTest.java
+++ fe/fe-core/src/test/java/com/starrocks/connector/ColumnTypeConverterTest.java
@@ -4,6 +4,7 @@ package com.starrocks.connector;
 
 import com.google.common.collect.Lists;
 import com.starrocks.catalog.ArrayType;
+import com.starrocks.catalog.Column;
 import com.starrocks.catalog.MapType;
 import com.starrocks.catalog.PrimitiveType;
 import com.starrocks.catalog.ScalarType;
@@ -16,6 +17,7 @@ import org.apache.avro.Schema;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static com.starrocks.connector.ColumnTypeConverter.columnEquals;
 import static com.starrocks.connector.ColumnTypeConverter.fromHiveTypeToArrayType;
 import static com.starrocks.connector.ColumnTypeConverter.fromHiveTypeToMapType;
 import static com.starrocks.connector.ColumnTypeConverter.fromHudiType;
@@ -311,5 +313,32 @@ public class ColumnTypeConverterTest {
         unionSchema = Schema.createUnion(Schema.create(Schema.Type.BYTES));
         arraySchema = Schema.createArray(unionSchema);
         Assert.assertEquals(fromHudiType(arraySchema), new ArrayType(ScalarType.createType(PrimitiveType.VARCHAR)));
+    }
+
+    @Test
+    public void testColumnEquals() {
+        Column base = new Column("k1", Type.INT, false);
+        Column other = new Column("k1", Type.INT, false);
+
+        Assert.assertTrue(columnEquals(base, base));
+        Assert.assertTrue(columnEquals(base, other));
+
+        other = new Column("k2", Type.INT, false);
+        Assert.assertFalse(columnEquals(base, other));
+
+        other = new Column("k1", Type.STRING, false);
+        Assert.assertFalse(columnEquals(base, other));
+
+        base = new Column("k1", ScalarType.createCharType(5), false);
+        other = new Column("k1", ScalarType.createCharType(10), false);
+        Assert.assertFalse(columnEquals(base, other));
+
+        base = new Column("k1", ScalarType.createDecimalV3Type(PrimitiveType.DECIMAL128, 5, 5), false);
+        other = new Column("k1", ScalarType.createDecimalV3Type(PrimitiveType.DECIMAL128, 6, 5), false);
+        Assert.assertFalse(columnEquals(base, other));
+
+        base = new Column("k1", ScalarType.createDecimalV3Type(PrimitiveType.DECIMAL128, 5, 5), false);
+        other = new Column("k1", ScalarType.createDecimalV3Type(PrimitiveType.DECIMAL128, 5, 4), false);
+        Assert.assertFalse(columnEquals(base, other));
     }
 }
