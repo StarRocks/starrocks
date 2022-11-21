69e619a4f4fc72a8e99d2fef3d3083ff5dee1c42
Murphy <96611012+mofeiatwork@users.noreply.github.com>
2022-11-21 19:14:28 +0800

    [Refactor] reduce aggregator intermediate binary size (#13718)

--- be/src/exec/pipeline/aggregate/aggregate_distinct_blocking_sink_operator.cpp
+++ be/src/exec/pipeline/aggregate/aggregate_distinct_blocking_sink_operator.cpp
@@ -47,8 +47,7 @@ Status AggregateDistinctBlockingSinkOperator::push_chunk(RuntimeState* state, co
     {
         SCOPED_TIMER(_aggregator->agg_compute_timer());
         bool limit_with_no_agg = _aggregator->limit() != -1;
-        TRY_CATCH_BAD_ALLOC(_aggregator->hash_set_variant().visit(
-                [&](auto& hash_set_with_key) { _aggregator->build_hash_set(*hash_set_with_key, chunk->num_rows()); }););
+        TRY_CATCH_BAD_ALLOC(_aggregator->build_hash_set(chunk->num_rows()));
 
         _mem_tracker->set(_aggregator->hash_set_variant().reserved_memory_usage(_aggregator->mem_pool()));
         TRY_CATCH_BAD_ALLOC(_aggregator->try_convert_to_two_level_set());
