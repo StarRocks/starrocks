cbc4f1444b394fdaef8ad81cd55fff76ce26a4d3
Zhao Chun <buaa.zhaoc@gmail.com>
2022-11-21 06:47:23 +0000

    [Refactor] Make log more clear

--- be/src/service/starrocks_main.cpp
+++ be/src/service/starrocks_main.cpp
@@ -99,7 +99,8 @@ int main(int argc, char** argv) {
     }
 
     if (getenv("TCMALLOC_HEAP_LIMIT_MB") == nullptr) {
-        fprintf(stderr, "you need replace bin dir of be with new version.\n");
+        fprintf(stderr, "Environment variable TCMALLOC_HEAP_LIMIT_MB is not set,"
+                " maybe you forgot to replace bin directory\n");
         exit(-1);
     }
 
