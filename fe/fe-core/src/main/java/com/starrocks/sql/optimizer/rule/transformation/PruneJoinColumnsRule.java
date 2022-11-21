9070821222327ccbf84e1bccefafe77e7f17ed31
Murphy <mofei@starrocks.com>
2022-11-21 15:53:40 +0800

    fix

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/PruneJoinColumnsRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/PruneJoinColumnsRule.java
@@ -4,14 +4,17 @@ package com.starrocks.sql.optimizer.rule.transformation;
 
 import com.starrocks.sql.optimizer.OptExpression;
 import com.starrocks.sql.optimizer.OptimizerContext;
+import com.starrocks.sql.optimizer.Utils;
 import com.starrocks.sql.optimizer.base.ColumnRefSet;
 import com.starrocks.sql.optimizer.operator.OperatorType;
 import com.starrocks.sql.optimizer.operator.logical.LogicalJoinOperator;
 import com.starrocks.sql.optimizer.operator.pattern.Pattern;
+import com.starrocks.sql.optimizer.operator.scalar.ColumnRefOperator;
 import com.starrocks.sql.optimizer.rule.RuleType;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PruneJoinColumnsRule extends TransformationRule {
     public PruneJoinColumnsRule() {
@@ -26,6 +29,25 @@ public class PruneJoinColumnsRule extends TransformationRule {
 
         ColumnRefSet requiredColumns = context.getTaskContext().getRequiredColumns();
         requiredColumns.union(joinOperator.getRequiredChildInputColumns());
+
+        // Retain output columns for SEMI/ANTI join
+        if (joinOperator.getJoinType().isLeftSemiAntiJoin()) {
+            ColumnRefSet leftOutput = input.getChildOutputColumns(0);
+            if (!requiredColumns.containsAny(leftOutput)) {
+                ColumnRefOperator smallestColumn = Utils.findSmallestColumnRef(
+                        leftOutput.getStream().mapToObj(context.getColumnRefFactory()::getColumnRef)
+                                .collect(Collectors.toList()));
+                requiredColumns.union(smallestColumn);
+            }
+        } else if (joinOperator.getJoinType().isRightSemiAntiJoin()) {
+            ColumnRefSet rightOutput = input.getChildOutputColumns(1);
+            if (!requiredColumns.containsAny(rightOutput)) {
+                ColumnRefOperator smallestColumn = Utils.findSmallestColumnRef(
+                        rightOutput.getStream().mapToObj(context.getColumnRefFactory()::getColumnRef)
+                                .collect(Collectors.toList()));
+                requiredColumns.union(smallestColumn);
+            }
+        }
 
         return Collections.emptyList();
     }
