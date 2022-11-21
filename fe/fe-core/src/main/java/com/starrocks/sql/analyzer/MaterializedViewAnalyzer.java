85898ee96f270b5ca1faf474c7fcf6954d7a8f99
Youngwb <yangwenbo_mailbox@163.com>
2022-11-21 14:08:49 +0800

    fix ut

--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/MaterializedViewAnalyzer.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/MaterializedViewAnalyzer.java
@@ -2,7 +2,6 @@
 
 package com.starrocks.sql.analyzer;
 
-import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
