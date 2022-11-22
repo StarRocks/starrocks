66e3d35f7d7094c653e65be257f92151a920e4b6
stephen <91597003+stephen-shelby@users.noreply.github.com>
2022-11-22 13:54:50 +0800

    [BugFix] fix refresh external table when partition or table doen't exist (#13785)
    (cherry picked from commit 108a723e42cd72c60ad5b80dbbf987839e702cb8)

--- fe/fe-core/src/main/java/com/starrocks/connector/hive/CacheUpdateProcessor.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/hive/CacheUpdateProcessor.java
@@ -64,10 +64,29 @@ public class CacheUpdateProcessor {
     public void refreshTable(String dbName, Table table) {
         HiveMetaStoreTable hmsTbl = (HiveMetaStoreTable) table;
         metastore.refreshTable(hmsTbl.getDbName(), hmsTbl.getTableName());
-        refreshRemoteFiles(hmsTbl.getTableLocation(), Operator.UPDATE);
+        refreshRemoteFiles(hmsTbl.getTableLocation(), Operator.UPDATE, getExistPaths(hmsTbl));
         if (isResourceMappingCatalog(catalogName) && table.isHiveTable()) {
             processSchemaChange(dbName, (HiveTable) table);
         }
+    }
+
+    private List<String> getExistPaths(HiveMetaStoreTable table) {
+        List<String> existPaths;
+        String dbName = table.getDbName();
+        String tblName = table.getTableName();
+
+        if (table.isUnPartitioned()) {
+            String path = metastore.getPartition(dbName, tblName, Lists.newArrayList()).getFullPath();
+            existPaths = Lists.newArrayList(path.endsWith("/") ? path : path + "/");
+        } else {
+            List<String> partitionNames = metastore.getPartitionKeys(dbName, tblName);
+            existPaths = metastore.getPartitionsByNames(dbName, tblName, partitionNames)
+                    .values().stream()
+                    .map(Partition::getFullPath)
+                    .map(path -> path.endsWith("/") ? path : path + "/")
+                    .collect(Collectors.toList());
+        }
+        return existPaths;
     }
 
     public void refreshPartition(Table table, List<String> hivePartitionNames) {
@@ -110,12 +129,12 @@ public class CacheUpdateProcessor {
         }
     }
 
-    private void refreshRemoteFiles(String tableLocation, Operator operator) {
+    private void refreshRemoteFiles(String tableLocation, Operator operator, List<String> existPaths) {
         if (remoteFileIO.isPresent()) {
             List<RemotePathKey> presentPathKey = remoteFileIO.get().getPresentPathKeyInCache(tableLocation, isRecursive);
             List<Future<?>> futures = Lists.newArrayList();
             presentPathKey.forEach(pathKey -> {
-                if (operator == Operator.UPDATE) {
+                if (operator == Operator.UPDATE && existPaths.contains(pathKey.getPath())) {
                     futures.add(executor.submit(() -> remoteFileIO.get().updateRemoteFiles(pathKey)));
                 } else {
                     futures.add(executor.submit(() -> remoteFileIO.get().invalidatePartition(pathKey)));
@@ -143,7 +162,7 @@ public class CacheUpdateProcessor {
 
     public void refreshTableByEvent(HiveTable updatedHiveTable, HiveCommonStats commonStats, Partition partition) {
         ((CachingHiveMetastore) metastore).refreshTableByEvent(updatedHiveTable, commonStats, partition);
-        refreshRemoteFiles(updatedHiveTable.getTableLocation(), Operator.UPDATE);
+        refreshRemoteFiles(updatedHiveTable.getTableLocation(), Operator.UPDATE, getExistPaths(updatedHiveTable));
     }
 
     public void refreshPartitionByEvent(HivePartitionName hivePartitionName, HiveCommonStats commonStats, Partition partion) {
@@ -162,7 +181,7 @@ public class CacheUpdateProcessor {
     public void invalidateTable(String dbName, String tableName) {
         String tableLocation = ((HiveMetaStoreTable) metastore.getTable(dbName, tableName)).getTableLocation();
         metastore.invalidateTable(dbName, tableName);
-        remoteFileIO.ifPresent(ignore -> refreshRemoteFiles(tableLocation, Operator.DROP));
+        remoteFileIO.ifPresent(ignore -> refreshRemoteFiles(tableLocation, Operator.DROP, Lists.newArrayList()));
     }
 
     public void invalidatePartition(HivePartitionName partitionName) {
@@ -194,4 +213,5 @@ public class CacheUpdateProcessor {
         }
         return ((CachingHiveMetastore) metastore).getNextEventResponse(lastSyncedEventId, catalogName, getAllEvents);
     }
+    
 }
