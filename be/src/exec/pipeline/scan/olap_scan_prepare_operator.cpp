6c5e2f8fa705c784ff6bfee70b3b7494f3d19ba8
zihe.liu <ziheliu1024@gmail.com>
2022-11-23 00:16:18 +0800

    [BugFix] Acquire rowsets at querying (#13830)
    (cherry picked from commit c0a1b94c883daa1f6810d5bf69239b4619324431)
    
    # Conflicts:
    #	be/src/exec/pipeline/scan/olap_scan_context.cpp
    #	be/src/exec/pipeline/scan/olap_scan_context.h
    #	be/src/exec/pipeline/scan/olap_scan_prepare_operator.cpp
    #	be/src/exec/pipeline/scan/olap_scan_prepare_operator.h

---
+++ be/src/exec/pipeline/scan/olap_scan_prepare_operator.cpp
@@ -0,0 +1,99 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Inc.
+
+#include "exec/pipeline/scan/olap_scan_prepare_operator.h"
+
+#include "exec/vectorized/olap_scan_node.h"
+#include "storage/storage_engine.h"
+
+namespace starrocks::pipeline {
+
+/// OlapScanPrepareOperator
+OlapScanPrepareOperator::OlapScanPrepareOperator(OperatorFactory* factory, int32_t id, const string& name,
+                                                 int32_t plan_node_id, int32_t driver_sequence, OlapScanContextPtr ctx)
+        : SourceOperator(factory, id, name, plan_node_id, driver_sequence), _ctx(std::move(ctx)) {
+    _ctx->ref();
+}
+
+OlapScanPrepareOperator::~OlapScanPrepareOperator() {
+    auto* state = runtime_state();
+    if (state == nullptr) {
+        return;
+    }
+
+    _ctx->unref(state);
+}
+
+Status OlapScanPrepareOperator::prepare(RuntimeState* state) {
+    RETURN_IF_ERROR(SourceOperator::prepare(state));
+
+    RETURN_IF_ERROR(_ctx->prepare(state));
+    RETURN_IF_ERROR(_ctx->capture_tablet_rowsets(_morsel_queue->olap_scan_ranges()));
+
+    return Status::OK();
+}
+
+void OlapScanPrepareOperator::close(RuntimeState* state) {
+    SourceOperator::close(state);
+}
+
+bool OlapScanPrepareOperator::has_output() const {
+    return !is_finished();
+}
+
+bool OlapScanPrepareOperator::is_finished() const {
+    return _ctx->is_prepare_finished() || _ctx->is_finished();
+}
+
+StatusOr<vectorized::ChunkPtr> OlapScanPrepareOperator::pull_chunk(RuntimeState* state) {
+    Status status = _ctx->parse_conjuncts(state, runtime_in_filters(), runtime_bloom_filters());
+
+    _morsel_queue->set_key_ranges(_ctx->key_ranges());
+    _morsel_queue->set_tablets(_ctx->tablets());
+    _morsel_queue->set_tablet_rowsets(_ctx->tablet_rowsets());
+
+    _ctx->set_prepare_finished();
+    if (!status.ok()) {
+        _ctx->set_finished();
+        return status;
+    }
+
+    return nullptr;
+}
+
+/// OlapScanPrepareOperatorFactory
+OlapScanPrepareOperatorFactory::OlapScanPrepareOperatorFactory(int32_t id, int32_t plan_node_id,
+                                                               vectorized::OlapScanNode* const scan_node,
+                                                               OlapScanContextFactoryPtr ctx_factory)
+        : SourceOperatorFactory(id, "olap_scan_prepare", plan_node_id),
+          _scan_node(scan_node),
+          _ctx_factory(std::move(ctx_factory)) {}
+
+Status OlapScanPrepareOperatorFactory::prepare(RuntimeState* state) {
+    RETURN_IF_ERROR(SourceOperatorFactory::prepare(state));
+
+    const auto& conjunct_ctxs = _scan_node->conjunct_ctxs();
+    const auto& tolap_scan_node = _scan_node->thrift_olap_scan_node();
+    auto tuple_desc = state->desc_tbl().get_tuple_descriptor(tolap_scan_node.tuple_id);
+
+    vectorized::DictOptimizeParser::rewrite_descriptor(state, conjunct_ctxs, tolap_scan_node.dict_string_id_to_int_ids,
+                                                       &(tuple_desc->decoded_slots()));
+
+    RETURN_IF_ERROR(Expr::prepare(conjunct_ctxs, state));
+    RETURN_IF_ERROR(Expr::open(conjunct_ctxs, state));
+
+    return Status::OK();
+}
+
+void OlapScanPrepareOperatorFactory::close(RuntimeState* state) {
+    const auto& conjunct_ctxs = _scan_node->conjunct_ctxs();
+    Expr::close(conjunct_ctxs, state);
+
+    SourceOperatorFactory::close(state);
+}
+
+OperatorPtr OlapScanPrepareOperatorFactory::create(int32_t degree_of_parallelism, int32_t driver_sequence) {
+    return std::make_shared<OlapScanPrepareOperator>(this, _id, _name, _plan_node_id, driver_sequence,
+                                                     _ctx_factory->get_or_create(driver_sequence));
+}
+
+} // namespace starrocks::pipeline
---
+++ be/src/exec/pipeline/scan/olap_scan_prepare_operator.h
@@ -0,0 +1,54 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Inc.
+
+#pragma once
+
+#include "exec/pipeline/scan/olap_scan_context.h"
+#include "exec/pipeline/source_operator.h"
+
+namespace starrocks {
+
+class OlapScanNode;
+
+namespace pipeline {
+
+// It does some common preparation works for OlapScan, after its local waiting set is ready
+// and before OlapScanOperator::pull_chunk. That is, OlapScanOperator depends on
+// it and waits until it is finished.
+class OlapScanPrepareOperator final : public SourceOperator {
+public:
+    OlapScanPrepareOperator(OperatorFactory* factory, int32_t id, const string& name, int32_t plan_node_id,
+                            int32_t driver_sequence, OlapScanContextPtr ctx);
+    ~OlapScanPrepareOperator() override;
+
+    Status prepare(RuntimeState* state) override;
+    void close(RuntimeState* state) override;
+
+    bool has_output() const override;
+    bool is_finished() const override;
+
+    StatusOr<vectorized::ChunkPtr> pull_chunk(RuntimeState* state) override;
+
+private:
+    OlapScanContextPtr _ctx;
+};
+
+class OlapScanPrepareOperatorFactory final : public SourceOperatorFactory {
+public:
+    OlapScanPrepareOperatorFactory(int32_t id, int32_t plan_node_id, vectorized::OlapScanNode* const scan_node,
+                                   OlapScanContextFactoryPtr ctx_factory);
+    ~OlapScanPrepareOperatorFactory() override = default;
+
+    bool with_morsels() const override { return true; }
+
+    Status prepare(RuntimeState* state) override;
+    void close(RuntimeState* state) override;
+
+    OperatorPtr create(int32_t degree_of_parallelism, int32_t driver_sequence) override;
+
+private:
+    vectorized::OlapScanNode* const _scan_node;
+    OlapScanContextFactoryPtr _ctx_factory;
+};
+
+} // namespace pipeline
+} // namespace starrocks
