c6efcb8afb82ec2730c6ec2776f4efb273cee624
rickif <rickif@qq.com>
2022-11-24 00:23:59 +0800

    [BugFix] Variable chunk's column types (#13629)
    * Revert "Revert "[BugFix] Different column types in different chunks (#13078)" (#13439)"
    
    This reverts commit 0680a0eec9292006cab7506496250666f08abee2.
    
    (cherry picked from commit c5d9e626b4b5664dab4ec3640574670e82898566)
    
    # Conflicts:
    #	be/src/exec/vectorized/csv_scanner.cpp
    #	be/test/column/column_helper_test.cpp

--- be/test/column/column_helper_test.cpp
+++ be/test/column/column_helper_test.cpp
@@ -20,6 +20,7 @@
 // under the License.
 
 #include "column/column_helper.h"
+#include "column/column_builder.h"
 
 #include "gtest/gtest.h"
 
@@ -27,8 +28,36 @@ namespace starrocks::vectorized {
 
 class ColumnHelperTest : public testing::Test {
 public:
+<<<<<<< HEAD
     virtual void SetUp() {}
     virtual void TearDown() {}
+=======
+    void SetUp() override {}
+    void TearDown() override {}
+
+protected:
+    ColumnPtr create_column() {
+        ColumnBuilder<TYPE_VARCHAR> builder(1);
+        builder.append(Slice("v1"));
+        return builder.build(false);
+    }
+
+    ColumnPtr create_nullable_column() {
+        ColumnBuilder<TYPE_VARCHAR> builder(1);
+        builder.append(Slice("v1"), true);
+        return builder.build(false);
+    }
+>>>>>>> c5d9e626b ([BugFix] Variable chunk's column types (#13629))
 };
+
+TEST_F(ColumnHelperTest, cast_to_nullable_column) {
+    auto col = ColumnHelper::cast_to_nullable_column(create_column());
+    ASSERT_TRUE(col->is_nullable());
+    ASSERT_FALSE(col->is_constant());
+
+    col = ColumnHelper::cast_to_nullable_column(create_nullable_column());
+    ASSERT_TRUE(col->is_nullable());
+    ASSERT_FALSE(col->is_constant());
+}
 
 } // namespace starrocks::vectorized
