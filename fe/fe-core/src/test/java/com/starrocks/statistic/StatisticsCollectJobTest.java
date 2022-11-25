cf53969098ed7cffc3010b8501434dfe3c82ce2b
sychen <sychen@ctrip.com>
2022-11-25 10:40:34 +0800

    style

--- fe/fe-core/src/test/java/com/starrocks/statistic/StatisticsCollectJobTest.java
+++ fe/fe-core/src/test/java/com/starrocks/statistic/StatisticsCollectJobTest.java
@@ -1,8 +1,8 @@
 // This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Inc.
 package com.starrocks.statistic;
 
-import com.google.common.collect.Maps;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.starrocks.catalog.Database;
 import com.starrocks.catalog.OlapTable;
 import com.starrocks.catalog.Partition;
