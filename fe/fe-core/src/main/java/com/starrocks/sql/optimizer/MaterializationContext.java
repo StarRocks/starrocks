3d69f2f39c836ca8b959a5b5ad15332fb0f1f59b
ABing <101158374+ABingHuang@users.noreply.github.com>
2022-11-25 10:50:21 +0800

    [Enhancement] Optimize creation of scan mv in register mv (#13970)
    * The creation of scan mv in register mv during mv query rewrite can be optimized.
    Originally the creation of scan mv is based on sql,which can be enhanced
    by constructing LogicalOlapScanOperator directly.
    
    * move preparation logic to MvRewritePreprocessor

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/MaterializationContext.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/MaterializationContext.java
@@ -7,7 +7,7 @@ import com.starrocks.sql.optimizer.base.ColumnRefFactory;
 import com.starrocks.sql.optimizer.operator.Operator;
 import com.starrocks.sql.optimizer.operator.scalar.ColumnRefOperator;
 
-import java.util.List;
+import java.util.Map;
 
 public class MaterializationContext {
     private MaterializedView mv;
@@ -18,12 +18,6 @@ public class MaterializationContext {
 
     private ColumnRefFactory mvColumnRefFactory;
 
-    // output expressions of mv define sql
-    private List<ColumnRefOperator> mvOutputExpressions;
-
-    // output expressions of select * from mv
-    private List<ColumnRefOperator> scanMvOutputExpressions;
-
     // logical OptExpression for query
     private OptExpression queryExpression;
 
@@ -31,14 +25,16 @@ public class MaterializationContext {
 
     private OptimizerContext optimizerContext;
 
+    private Map<ColumnRefOperator, ColumnRefOperator> outputMapping;
+
     public MaterializationContext(MaterializedView mv,
                                   OptExpression mvExpression,
-                                  ColumnRefFactory columnRefFactory,
-                                  List<ColumnRefOperator> mvOutputExpressions) {
+                                  ColumnRefFactory queryColumnRefFactory,
+                                  ColumnRefFactory mvColumnRefFactory) {
         this.mv = mv;
         this.mvExpression = mvExpression;
-        this.mvColumnRefFactory = columnRefFactory;
-        this.mvOutputExpressions = mvOutputExpressions;
+        this.queryRefFactory = queryColumnRefFactory;
+        this.mvColumnRefFactory = mvColumnRefFactory;
     }
 
     public MaterializedView getMv() {
@@ -59,18 +55,6 @@ public class MaterializationContext {
 
     public ColumnRefFactory getMvColumnRefFactory() {
         return mvColumnRefFactory;
-    }
-
-    public List<ColumnRefOperator> getMvOutputExpressions() {
-        return mvOutputExpressions;
-    }
-
-    public List<ColumnRefOperator> getScanMvOutputExpressions() {
-        return scanMvOutputExpressions;
-    }
-
-    public void setScanMvOutputExpressions(List<ColumnRefOperator> scanMvOutputExpressions) {
-        this.scanMvOutputExpressions = scanMvOutputExpressions;
     }
 
     public OptExpression getQueryExpression() {
@@ -95,5 +79,13 @@ public class MaterializationContext {
 
     public void setOptimizerContext(OptimizerContext optimizerContext) {
         this.optimizerContext = optimizerContext;
+    }
+
+    public Map<ColumnRefOperator, ColumnRefOperator> getOutputMapping() {
+        return outputMapping;
+    }
+
+    public void setOutputMapping(Map<ColumnRefOperator, ColumnRefOperator> outputMapping) {
+        this.outputMapping = outputMapping;
     }
 }
