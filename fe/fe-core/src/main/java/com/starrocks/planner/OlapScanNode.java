4bb8f6cf9f229ba8189dbf8b315bef5c4d24626d
abc982627271 <liuxuefen@starrocks.com>
2022-11-28 16:44:05 +0800

    add KeysType import in OlapScanNode

--- fe/fe-core/src/main/java/com/starrocks/planner/OlapScanNode.java
+++ fe/fe-core/src/main/java/com/starrocks/planner/OlapScanNode.java
@@ -39,6 +39,7 @@ import com.starrocks.analysis.TupleDescriptor;
 import com.starrocks.catalog.Column;
 import com.starrocks.catalog.DistributionInfo;
 import com.starrocks.catalog.HashDistributionInfo;
+import com.starrocks.catalog.KeysType;
 import com.starrocks.catalog.LocalTablet;
 import com.starrocks.catalog.MaterializedIndex;
 import com.starrocks.catalog.OlapTable;
