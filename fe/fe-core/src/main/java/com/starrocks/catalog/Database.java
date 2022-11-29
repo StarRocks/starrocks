StarRocks/starrocks

commit bdb4da184f0a7af1d0c1d262b1c7ac2f843f6716

[Enhancement] Optimize FE memory usage when get table names (#12868)
Optimize memory usage when call db.getTableNamesWithLock()

fe/fe-core/src/main/java/com/starrocks/catalog/Database.java
@@ -52,7 +52,7 @@ import java.io.DataOutput;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -571,7 +571,7 @@ public class Database extends MetaObject implements Writable {
     public Set<String> getTableNamesWithLock() {
         readLock();
         try {
-            return new HashSet<>(this.nameToTable.keySet());
+            return Collections.unmodifiableSet(this.nameToTable.keySet());
         } finally {
             readUnlock();
         }

commit 0b2499bbb5590577c0be274c00c493efcbcec56b

[BugFix] Fix the bug of empty table after recover table (#13921)
Problem:
This Problem was introduced by commit bdb4da1. This commit use Collections.unmodifiableSet instead of the new HashSet to reduce the memory usage of FE. But the problem here is that Collections.unmodifiableSet returns the view of nameToTable.keySet(). Before using the TableNames in recycleDatabase, nameToTable had been dropped, and also the view of it would become empty, which caused this problem.

Solution:
We copy the nameToTable.keySet() only when dropping the database.

fe/fe-core/src/main/java/com/starrocks/catalog/Database.java
@@ -568,7 +568,7 @@ public class Database extends MetaObject implements Writable {
         return materializedViews;
     }
 
-    public Set<String> getTableNamesWithLock() {
+    public Set<String> getTableNamesViewWithLock() {
         readLock();
         try {
             return Collections.unmodifiableSet(this.nameToTable.keySet());

fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
@@ -423,7 +423,7 @@ public class LocalMetastore implements ConnectorMetadata {
                 }
 
                 // save table names for recycling
-                Set<String> tableNames = db.getTableNamesWithLock();
+                Set<String> tableNames = new HashSet(db.getTableNamesViewWithLock());
                 runnableList = unprotectDropDb(db, isForceDrop, false);
                 if (!isForceDrop) {
                     recycleBin.recycleDatabase(db, tableNames);
@@ -479,7 +479,7 @@ public class LocalMetastore implements ConnectorMetadata {
             Database db = fullNameToDb.get(dbName);
             db.writeLock();
             try {
-                Set<String> tableNames = db.getTableNamesWithLock();
+                Set<String> tableNames = new HashSet(db.getTableNamesViewWithLock());
                 runnableList = unprotectDropDb(db, isForceDrop, true);
                 if (!isForceDrop) {
                     recycleBin.recycleDatabase(db, tableNames);

fe/fe-core/src/main/java/com/starrocks/service/FrontendServiceImpl.java
@@ -257,7 +257,7 @@ public class FrontendServiceImpl implements FrontendService.Iface {
             currentUser = UserIdentity.createAnalyzedUserIdentWithIp(params.user, params.user_ip);
         }
         if (db != null) {
-            for (String tableName : db.getTableNamesWithLock()) {
+            for (String tableName : db.getTableNamesViewWithLock()) {
                 LOG.debug("get table: {}, wait to check", tableName);
                 if (!GlobalStateMgr.getCurrentState().getAuth().checkTblPriv(currentUser, params.db,
                         tableName, PrivPredicate.SHOW)) {
@@ -713,7 +713,7 @@ public class FrontendServiceImpl implements FrontendService.Iface {
             }
             Database db = GlobalStateMgr.getCurrentState().getDb(fullName);
             if (db != null) {
-                for (String tableName : db.getTableNamesWithLock()) {
+                for (String tableName : db.getTableNamesViewWithLock()) {
                     LOG.debug("get table: {}, wait to check", tableName);
                     if (!GlobalStateMgr.getCurrentState().getAuth().checkTblPriv(currentUser, fullName,
                             tableName, PrivPredicate.SHOW)) {

fe/fe-core/src/test/java/com/starrocks/analysis/AccessTestUtil.java
@@ -185,7 +185,7 @@ public class AccessTestUtil {
                 minTimes = 0;
                 result = null;
 
-                db.getTableNamesWithLock();
+                db.getTableNamesViewWithLock();
                 minTimes = 0;
                 result = Sets.newHashSet("testTable");
 
@@ -368,7 +368,7 @@ public class AccessTestUtil {
                 minTimes = 0;
                 result = null;
 
-                db.getTableNamesWithLock();
+                db.getTableNamesViewWithLock();
                 minTimes = 0;
                 result = Sets.newHashSet("t");
 

fe/fe-core/src/test/java/com/starrocks/catalog/DatabaseTest.java
@@ -126,8 +126,8 @@ public class DatabaseTest {
         Assert.assertEquals(1, db.getTables().size());
         Assert.assertEquals(table, db.getTables().get(0));
 
-        Assert.assertEquals(1, db.getTableNamesWithLock().size());
-        for (String tableFamilyGroupName : db.getTableNamesWithLock()) {
+        Assert.assertEquals(1, db.getTableNamesViewWithLock().size());
+        for (String tableFamilyGroupName : db.getTableNamesViewWithLock()) {
             Assert.assertEquals(table.getName(), tableFamilyGroupName);
         }
 