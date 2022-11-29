736cffa6353e9059c8a47baff5b28a65cf40b16e
zombee0 <flylucas_10@163.com>
2022-11-29 20:34:40 +0800

    [Feature] use Array<DecimalV3> for DLA (#12952)

--- be/src/formats/orc/orc_chunk_reader.cpp
+++ be/src/formats/orc/orc_chunk_reader.cpp
@@ -1494,7 +1494,7 @@ static void _try_implicit_cast(TypeDescriptor* from, const TypeDescriptor& to) {
             from->type = t2;
         } else if (from->precision > decimal_precision_limit<int64_t>) {
             from->type = LogicalType::TYPE_DECIMAL128;
-        } else if (from->precision > decimal_precision_limit<int32_t>) {
+        } else if (from->precision > decimal_precision_limit<int32_t> || (to.type == LogicalType::TYPE_DECIMAL64)) {
             from->type = LogicalType::TYPE_DECIMAL64;
         } else {
             from->type = LogicalType::TYPE_DECIMAL32;
