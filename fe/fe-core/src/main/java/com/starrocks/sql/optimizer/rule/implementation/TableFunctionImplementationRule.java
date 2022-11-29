8b852718f19d92f22d34c49d4db3b49a804c925b
HangyuanLiu <460660596@qq.com>
2022-11-29 20:32:10 +0800

    [BugFix] Fix the project correspondence in TableFunction was incorrectly pruned bug (#14085)

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/implementation/TableFunctionImplementationRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/implementation/TableFunctionImplementationRule.java
@@ -4,15 +4,14 @@ package com.starrocks.sql.optimizer.rule.implementation;
 import com.google.common.collect.Lists;
 import com.starrocks.sql.optimizer.OptExpression;
 import com.starrocks.sql.optimizer.OptimizerContext;
-import com.starrocks.sql.optimizer.base.ColumnRefSet;
 import com.starrocks.sql.optimizer.operator.OperatorType;
 import com.starrocks.sql.optimizer.operator.logical.LogicalTableFunctionOperator;
 import com.starrocks.sql.optimizer.operator.pattern.Pattern;
 import com.starrocks.sql.optimizer.operator.physical.PhysicalTableFunctionOperator;
 import com.starrocks.sql.optimizer.rule.RuleType;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TableFunctionImplementationRule extends ImplementationRule {
     public TableFunctionImplementationRule() {
@@ -26,7 +25,7 @@ public class TableFunctionImplementationRule extends ImplementationRule {
         PhysicalTableFunctionOperator physicalLateral = new PhysicalTableFunctionOperator(
                 logicalTableFunctionOperator.getFnResultColumnRefSet(),
                 logicalTableFunctionOperator.getFn(),
-                new ColumnRefSet(new ArrayList(logicalTableFunctionOperator.getFnParamColumnProjectMap().keySet())),
+                logicalTableFunctionOperator.getFnParamColumnProject().stream().map(p -> p.first).collect(Collectors.toList()),
                 logicalTableFunctionOperator.getOuterColumnRefSet(),
                 logicalTableFunctionOperator.getLimit(),
                 logicalTableFunctionOperator.getPredicate(),
