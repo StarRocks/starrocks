49e214572b1b44c2b8c90997170b1890936681cb
Smith Cruise <chendingchao1@126.com>
2022-11-29 18:38:35 +0800

    [BugFix][Cherry-Pick][Branch-2.5] Avoid BE crash when ORC's type can't convert to SlotDescriptor's type (#14255)
    Cherry-Pick from #14224

--- be/src/formats/orc/orc_chunk_reader.cpp
+++ be/src/formats/orc/orc_chunk_reader.cpp
@@ -30,7 +30,7 @@ namespace starrocks::vectorized {
 const FillColumnFunction& find_fill_func(PrimitiveType type, bool nullable);
 
 // NOLINTNEXTLINE
-const static std::unordered_map<orc::TypeKind, PrimitiveType> g_orc_starrocks_type_mapping = {
+const static std::unordered_map<orc::TypeKind, PrimitiveType> g_orc_starrocks_primitive_type_mapping = {
         {orc::BOOLEAN, TYPE_BOOLEAN},
         {orc::BYTE, TYPE_TINYINT},
         {orc::SHORT, TYPE_SMALLINT},
@@ -46,11 +46,6 @@ const static std::unordered_map<orc::TypeKind, PrimitiveType> g_orc_starrocks_ty
         {orc::CHAR, TYPE_CHAR},
         {orc::VARCHAR, TYPE_VARCHAR},
         {orc::TIMESTAMP_INSTANT, TYPE_DATETIME},
-};
-
-// NOLINTNEXTLINE
-const static std::set<orc::TypeKind> g_orc_int_type = {
-        orc::BOOLEAN, orc::BYTE, orc::SHORT, orc::INT, orc::LONG, orc::FLOAT, orc::DOUBLE,
 };
 
 // NOLINTNEXTLINE
@@ -1459,8 +1454,8 @@ static Status _create_type_descriptor_by_orc(const TypeDescriptor& origin_type, 
         auto precision = (int)orc_type->getPrecision();
         auto scale = (int)orc_type->getScale();
         auto len = (int)orc_type->getMaximumLength();
-        auto iter = g_orc_starrocks_type_mapping.find(kind);
-        if (iter == g_orc_starrocks_type_mapping.end()) {
+        auto iter = g_orc_starrocks_primitive_type_mapping.find(kind);
+        if (iter == g_orc_starrocks_primitive_type_mapping.end()) {
             return Status::NotSupported("Unsupported ORC type: " + orc_type->toString());
         }
         result->type = iter->second;
