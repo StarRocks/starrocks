ab3f90fb0f14caea2c03c22adecb54ed3c3dd4b1
liuyehcf <1559500551@qq.com>
2022-11-30 15:49:08 +0800

    [BugFix] Fix missing hack NULL_TYPE for field child_type (#14308)
    (cherry picked from commit 53d9e39bc484c9e33c27bbe62dfef096d78f65c8)

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/CTEPlanTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/CTEPlanTest.java
@@ -729,4 +729,20 @@ public class CTEPlanTest extends PlanTestBase {
                 "  |  order by: <slot 14> 14: v3 ASC, <slot 13> 13: v2 ASC\n" +
                 "  |  offset: 0");
     }
+
+    @Test
+    public void testNullTypeHack() throws Exception {
+        String sql = "WITH cte_1 AS (\n" +
+                "  SELECT null v1\n" +
+                ")\n" +
+                "SELECT  \n" +
+                "  CASE \n" +
+                "    WHEN a.v1 = b.v1 THEN 1 \n" +
+                "    ELSE -1 \n" +
+                "  END IS_OK\n" +
+                "FROM cte_1 a, cte_1 b";
+
+        String plan = getThriftPlan(sql);
+        assertNotContains(plan, "NULL_TYPE");
+    }
 }
