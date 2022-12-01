6b6d0439b25b8ce5b442f976768a67c580a933a1
ABingHuang <codekhuang@163.com>
2022-12-01 16:07:47 +0800

    optimize single table rewrite, enable single table rewrite without predicate

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/materialization/rule/OnlyScanRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/materialization/rule/OnlyScanRule.java
@@ -2,18 +2,13 @@
 
 package com.starrocks.sql.optimizer.rule.transformation.materialization.rule;
 
-import com.starrocks.sql.optimizer.OptExpression;
-import com.starrocks.sql.optimizer.OptimizerContext;
-import com.starrocks.sql.optimizer.operator.Operator;
 import com.starrocks.sql.optimizer.operator.OperatorType;
-import com.starrocks.sql.optimizer.operator.logical.LogicalScanOperator;
 import com.starrocks.sql.optimizer.operator.pattern.Pattern;
-import com.starrocks.sql.optimizer.operator.scalar.ScalarOperator;
 import com.starrocks.sql.optimizer.rule.RuleType;
 
 /*
  *
- * Here is the rule for pattern Scan with predicates
+ * Here is the rule for pattern Scan
  *
  */
 public class OnlyScanRule extends SingleTableRewriteBaseRule {
@@ -21,20 +16,6 @@ public class OnlyScanRule extends SingleTableRewriteBaseRule {
 
     public OnlyScanRule() {
         super(RuleType.TF_MV_ONLY_SCAN_RULE, Pattern.create(OperatorType.PATTERN_SCAN));
-    }
-
-    @Override
-    public boolean check(OptExpression input, OptimizerContext context) {
-        boolean ret = super.check(input, context);
-        if (!ret) {
-            return false;
-        }
-        Operator currentOp = input.getOp();
-        if (!(currentOp instanceof LogicalScanOperator)) {
-            return false;
-        }
-        ScalarOperator predicate = input.getOp().getPredicate();
-        return predicate != null;
     }
 
     public static OnlyScanRule getInstance() {
