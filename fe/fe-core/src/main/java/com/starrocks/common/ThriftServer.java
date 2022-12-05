c58309ca3769acd8d5421664c683122143e9b541
gengjun-git <gengjun@starrocks.com>
2022-12-05 10:28:09 +0800

    opt

--- fe/fe-core/src/main/java/com/starrocks/common/ThriftServer.java
+++ fe/fe-core/src/main/java/com/starrocks/common/ThriftServer.java
@@ -117,6 +117,8 @@ public class ThriftServer {
                 .newDaemonFixedThreadPool(Config.thrift_server_max_worker_threads,
                         Config.thrift_server_queue_size,
                         "thrift-server-pool", true);
+        // allow core thread time out so that the thread can be released
+        // if not used for ThreadPoolManager.KEEP_ALIVE_TIME
         threadPoolExecutor.allowCoreThreadTimeOut(true);
         serverArgs.executorService(threadPoolExecutor);
         server = new SRTThreadPoolServer(serverArgs);
