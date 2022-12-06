c3f4eaa833b1eb2d56400dbeb2b08703f3665b4b
Seaven <seaven_7@qq.com>
2022-12-06 14:54:28 +0800

    [Enhancement] replace str_to_date trim (#14598)

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/operator/scalar/ConstantOperator.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/operator/scalar/ConstantOperator.java
@@ -14,6 +14,7 @@ import com.starrocks.sql.common.StarRocksPlannerException;
 import com.starrocks.sql.common.UnsupportedException;
 import com.starrocks.sql.optimizer.base.ColumnRefSet;
 import com.starrocks.sql.optimizer.operator.OperatorType;
+import org.apache.commons.lang3.StringUtils;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
@@ -313,8 +314,7 @@ public final class ConstantOperator extends ScalarOperator implements Comparable
         if (t != o.getType().getPrimitiveType()
                 && (!t.isCharFamily() && !o.getType().getPrimitiveType().isCharFamily())
                 && (!t.isDecimalOfAnyVersion() && !o.getType().getPrimitiveType().isDecimalOfAnyVersion())) {
-            throw new StarRocksPlannerException(
-                    "Constant " + this.toString() + " can't compare with Constant " + o.toString(),
+            throw new StarRocksPlannerException("Constant " + this + " can't compare with Constant " + o,
                     ErrorType.INTERNAL_ERROR);
         }
 
@@ -408,7 +408,7 @@ public final class ConstantOperator extends ScalarOperator implements Comparable
             return ConstantOperator.createDouble(Double.parseDouble(childString));
         } else if (desc.isDate() || desc.isDatetime()) {
             DateLiteral literal;
-            String dateStr = childString.trim();
+            String dateStr = StringUtils.strip(childString, "\r\n\t ");
             try {
                 // DateLiteral will throw Exception if cast failed
                 // 1.try cast by format "yyyy-MM-dd HH:mm:ss"
