096c0944e2acbb72546a09bdb5cf25bfc155b6e4
zihe.liu <ziheliu1024@gmail.com>
2022-12-09 02:00:32 +0800

    [Refactor] make ~ContextWithDependency virtual (#14924)
    The destructor of ContextWithDependency is not virtual.
    Luckily, all the usage of subclasses of ContextWithDependency are shared_ptr, so memory leak doesn't happen for now.

--- be/src/exec/pipeline/scan/olap_scan_context.h
+++ be/src/exec/pipeline/scan/olap_scan_context.h
@@ -48,6 +48,7 @@ public:
     explicit OlapScanContext(vectorized::OlapScanNode* scan_node, int32_t dop, bool shared_scan,
                              BalancedChunkBuffer& chunk_buffer)
             : _scan_node(scan_node), _chunk_buffer(chunk_buffer), _shared_scan(shared_scan) {}
+    ~OlapScanContext() override = default;
 
     Status prepare(RuntimeState* state);
     void close(RuntimeState* state) override;
