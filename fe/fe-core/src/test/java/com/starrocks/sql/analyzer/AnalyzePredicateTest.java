04aff48daf6201a2f4f340cd357b5396bba3d5fb
zombee0 <flylucas_10@163.com>
2022-12-08 13:41:11 +0800

    [BugFix] fix use TreeNode.contains to check the type support join on (#14802)

--- fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzePredicateTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzePredicateTest.java
@@ -20,9 +20,9 @@ public class AnalyzePredicateTest {
     public void testArrayPredicate() {
         analyzeSuccess("select * from tarray where v3 is null");
         analyzeFail("select * from tarray where v3 between [1,2,3] and [4,5,6]",
-                "HLL, BITMAP, PERCENTILE and ARRAY type couldn't as Predicate");
+                "HLL, BITMAP, PERCENTILE and ARRAY, MAP, STRUCT type couldn't as Predicate");
         analyzeFail("select * from tarray where v3 in ([1,2,3], [4,5,6])",
-                "HLL, BITMAP, PERCENTILE and ARRAY type couldn't as Predicate");
+                "HLL, BITMAP, PERCENTILE and ARRAY, MAP, STRUCT type couldn't as Predicate");
     }
 
 
