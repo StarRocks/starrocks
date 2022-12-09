0ab417efc93970dd0369040b5fa15d72e1e99afa
packy92 <110370499+packy92@users.noreply.github.com>
2022-12-09 17:20:19 +0800

    [Enhancement] treat invalid date as null value in partition prune (#14820)
    (cherry picked from commit 4b74eaca0106b067d0147517b961d7bf00b8dd99)

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/operator/ColumnFilterConverter.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/operator/ColumnFilterConverter.java
@@ -88,11 +88,14 @@ public class ColumnFilterConverter {
             return;
         }
 
-        if (predicate.getChildren().stream().skip(1).anyMatch(d -> !OperatorType.CONSTANT.equals(d.getOpType()))) {
+        // rewrite invalid date cast expr to NullLiteral
+        ScalarOperator rewritePredicate = rewriteInvalidDateCast(predicate);
+
+        if (rewritePredicate.getChildren().stream().skip(1).anyMatch(d -> !OperatorType.CONSTANT.equals(d.getOpType()))) {
             return;
         }
 
-        predicate.accept(COLUMN_FILTER_VISITOR, result);
+        rewritePredicate.accept(COLUMN_FILTER_VISITOR, result);
     }
 
     private static boolean checkColumnRefCanPartition(ScalarOperator right, Table table) {
@@ -177,6 +180,31 @@ public class ColumnFilterConverter {
                 (Objects.equals(exprTimeArg, callTimeArg) ||
                         (TIME_MAP.containsKey(exprTimeArg) && TIME_MAP.containsKey(callTimeArg) &&
                                 TIME_MAP.get(exprTimeArg) > TIME_MAP.get(callTimeArg)));
+    }
+
+    // only rewrite cast invalid date value to null like cast('abc' as date)
+    private static ScalarOperator rewriteInvalidDateCast(ScalarOperator scalarOperator) {
+        ScalarOperator copy = scalarOperator.clone();
+        List<ScalarOperator> children = copy.getChildren();
+
+        for (int i = 1; i < children.size(); i++) {
+            ScalarOperator child = children.get(i);
+            if (child instanceof CastOperator) {
+                CastOperator cast = (CastOperator) child;
+                Type toType = cast.getType();
+                if (cast.getChildren().size() == 1
+                        && cast.getChildren().get(0).isConstantRef()
+                        && toType.isDateType()) {
+                    ConstantOperator value = (ConstantOperator) cast.getChildren().get(0);
+                    try {
+                        value.castTo(toType);
+                    } catch (Exception e) {
+                        children.set(i, ConstantOperator.createNull(toType));
+                    }
+                }
+            }
+        }
+        return copy;
     }
 
     private static class ColumnFilterVisitor
