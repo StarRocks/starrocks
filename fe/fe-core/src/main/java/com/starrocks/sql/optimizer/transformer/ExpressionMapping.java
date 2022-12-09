ca56a5ec717f6f270f6ecdf05b359492f3198c4f
fzhedu <fzhedu@gmail.com>
2022-12-09 19:51:11 +0800

    [Enhancement] lambda functions can work with aggregation and window functions.
    Signed-off-by: fzhedu <fzhedu@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/transformer/ExpressionMapping.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/transformer/ExpressionMapping.java
@@ -34,7 +34,7 @@ public class ExpressionMapping {
     /**
      * This structure is responsible for the translation map from Expr to operator
      */
-    private final Map<Expr, ColumnRefOperator> expressionToColumns = new HashMap<>();
+    public Map<Expr, ColumnRefOperator> expressionToColumns = new HashMap<>();
 
     /**
      * The purpose of below property is to hold the current plan built so far,
@@ -63,6 +63,9 @@ public class ExpressionMapping {
             this.scope.setParent(outer.getScope());
             fieldsList.addAll(outer.getFieldMappings());
             this.outerScopeRelationId = outer.getScope().getRelationId();
+            if (scope.isLambdaScope()) { // lambda can use outer scope's expression, like agg.
+                this.expressionToColumns = outer.expressionToColumns;
+            }
         }
         this.fieldMappings = new ColumnRefOperator[fieldsList.size()];
         fieldsList.toArray(this.fieldMappings);
