95a2994847650443af738b8eb3be285e8e6aeca7
ABing <101158374+ABingHuang@users.noreply.github.com>
2022-12-13 13:59:49 +0800

    [BugFix] fix aggregate with key projection rewrite bug (#14933)

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/operator/logical/LogicalAggregationOperator.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/operator/logical/LogicalAggregationOperator.java
@@ -153,10 +153,10 @@ public class LogicalAggregationOperator extends LogicalOperator {
     public Map<ColumnRefOperator, ScalarOperator> getLineage(
             ColumnRefFactory refFactory, ExpressionContext expressionContext) {
         Map<ColumnRefOperator, ScalarOperator> columnRefMap = Maps.newHashMap();
-        columnRefMap.putAll(getColumnRefMap());
         if (projection != null) {
             columnRefMap.putAll(projection.getColumnRefMap());
         }
+        columnRefMap.putAll(getColumnRefMap());
         return columnRefMap;
     }
 
