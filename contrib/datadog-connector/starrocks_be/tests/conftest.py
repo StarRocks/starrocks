8903f2202b1fc3b3fba89ee123f1cac51ca2459a
jerry <fujianhj@gmail.com>
2022-12-15 21:53:58 +0800

    #12619 Support DataDog: 1. `starrocks_fe` for FE 2. `starrocks_be` for BE

---
+++ contrib/datadog-connector/starrocks_be/tests/conftest.py
@@ -0,0 +1,14 @@
+# (C) Datadog, Inc. 2022-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+import pytest
+
+
+@pytest.fixture(scope='session')
+def dd_environment():
+    yield
+
+
+@pytest.fixture
+def instance():
+    return {}
---
+++ contrib/datadog-connector/starrocks_fe/datadog_checks/starrocks_fe/metrics.py
@@ -0,0 +1,6 @@
+FE_METRICS = [
+    # query metrics
+    'starrocks_fe_query_total',
+    'starrocks_fe_query_err',
+    'starrocks_fe_request_total',
+]
