eb27a22424cc9ae807ac33539d10eed36479d1cb
Smith Cruise <chendingchao1@126.com>
2022-12-19 23:07:42 +0800

    support outfile

--- fe/fe-core/src/main/java/com/starrocks/connector/ConnectorMetadata.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/ConnectorMetadata.java
@@ -11,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+
 package com.starrocks.connector;
 
 import com.google.common.collect.Lists;
