cd6a748fed4919dc5e7f5bad2db2dc179cb1743a
leoyy0316 <571684903@qq.com>
2022-12-24 20:52:37 +0800

    fix show tables from external catalog database

--- fe/fe-core/src/test/java/com/starrocks/qe/ShowExecutorTest.java
+++ fe/fe-core/src/test/java/com/starrocks/qe/ShowExecutorTest.java
@@ -103,7 +103,10 @@ import org.junit.rules.ExpectedException;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static com.starrocks.common.util.PropertyAnalyzer.PROPERTIES_STORAGE_COLDOWN_TIME;
 import static com.starrocks.server.CatalogMgr.ResourceMappingCatalog.toResourceName;
