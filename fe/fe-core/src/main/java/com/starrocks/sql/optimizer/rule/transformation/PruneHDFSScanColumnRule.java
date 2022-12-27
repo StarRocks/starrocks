5d9caff9787b50a8449f796d64451fd9b54e87f0
Youngwb <yangwenbo_mailbox@163.com>
2022-12-27 20:46:49 +0800

    [BugFix] Disable query for external table which partition column type is float/timestamp/decimal

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/PruneHDFSScanColumnRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/PruneHDFSScanColumnRule.java
@@ -66,6 +66,8 @@ public class PruneHDFSScanColumnRule extends TransformationRule {
                         .collect(Collectors.toSet());
         scanColumns.addAll(Utils.extractColumnRef(scanOperator.getPredicate()));
 
+        checkPartitionColumnType(scanOperator, scanColumns, context);
+
         // make sure there is at least one materialized column in new output columns.
         // if not, we have to choose one materialized column from scan operator output columns
         // with the minimal cost.
@@ -122,6 +124,24 @@ public class PruneHDFSScanColumnRule extends TransformationRule {
                 throw new StarRocksPlannerException(e.getMessage(), ErrorType.INTERNAL_ERROR);
             }
         }
+    }
+
+    private void checkPartitionColumnType(LogicalScanOperator scanOperator, Set<ColumnRefOperator> scanColumnRefOperators,
+                                          OptimizerContext context) {
+        Table table = scanOperator.getTable();
+        List<Column> partitionColumns = table.getPartitionColumnNames().stream().map(table::getColumn).
+                collect(Collectors.toList());
+        List<Column> scanColumns = scanColumnRefOperators.stream().map(col -> context.getColumnRefFactory().getColumn(col)).
+                collect(Collectors.toList());
+        partitionColumns.retainAll(scanColumns);
+        if (partitionColumns.stream().map(Column::getType).anyMatch(this::notSupportedPartitionColumnType)) {
+            throw new StarRocksPlannerException("Table partition by float/timestamp/decimal datatype is not supported",
+                    ErrorType.UNSUPPORTED);
+        }
+    }
+
+    private boolean notSupportedPartitionColumnType(Type type) {
+        return type.isFloat() || type.isDecimalOfAnyVersion() || type.isDatetime();
     }
 
     private boolean containsMaterializedColumn(LogicalScanOperator scanOperator, Set<ColumnRefOperator> scanColumns) {
