85b7dcd6a43665ce781b5c97eacf4631e2891c8b
yanz <dirtysalt1987@gmail.com>
2022-12-27 15:15:05 +0800

    disable new privilege checker

--- fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
@@ -396,7 +396,7 @@ public class GlobalStateMgr {
 
     // We're developing a new privilege & authentication framework
     // This is used to turned on in hard code.
-    public static final boolean USING_NEW_PRIVILEGE = true;
+    public static final boolean USING_NEW_PRIVILEGE = false;
     // change to true in UT
     private AtomicBoolean usingNewPrivilege;
 
