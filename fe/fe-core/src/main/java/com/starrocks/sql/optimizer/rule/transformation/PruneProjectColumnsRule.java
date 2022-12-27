0f4cc91d60cd187265738f5ead8a2c2057c62e74
packy <wangchao@starrocks.com>
2022-12-27 20:26:58 +0800

    test

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/PruneProjectColumnsRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/PruneProjectColumnsRule.java
@@ -18,6 +18,7 @@ package com.starrocks.sql.optimizer.rule.transformation;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.starrocks.catalog.FunctionSet;
+import com.starrocks.catalog.Type;
 import com.starrocks.sql.optimizer.OptExpression;
 import com.starrocks.sql.optimizer.OptimizerContext;
 import com.starrocks.sql.optimizer.base.ColumnRefSet;
@@ -27,6 +28,7 @@ import com.starrocks.sql.optimizer.operator.logical.LogicalProjectOperator;
 import com.starrocks.sql.optimizer.operator.pattern.Pattern;
 import com.starrocks.sql.optimizer.operator.scalar.CallOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ColumnRefOperator;
+import com.starrocks.sql.optimizer.operator.scalar.ConstantOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ScalarOperator;
 import com.starrocks.sql.optimizer.rule.RuleType;
 
@@ -63,15 +65,10 @@ public class PruneProjectColumnsRule extends TransformationRule {
         }));
 
         LogicalOperator inputOp = (LogicalOperator) input.inputAt(0).getOp();
-        if (newMap.isEmpty() && inputOp.getOpType().equals(OperatorType.LOGICAL_JOIN)) {
-            ColumnRefSet candidates = new ColumnRefSet(projectOperator.getColumnRefMap().keySet());
-            ColumnRefOperator smallestColumn =
-                    inputOp.getSmallestColumn(candidates, context.getColumnRefFactory(), input.inputAt(0));
-            if (smallestColumn != null) {
-                ScalarOperator expr = projectOperator.getColumnRefMap().get(smallestColumn);
-                newMap.put(smallestColumn, expr);
-                requiredInputColumns.union(smallestColumn);
-            }
+        if (newMap.isEmpty()) {
+            ColumnRefOperator constCol = context.getColumnRefFactory()
+                    .create("auto_fill_col", Type.TINYINT, true);
+            newMap.put(constCol, ConstantOperator.createTinyInt((byte) 1));
         }
 
         // Change the requiredOutputColumns in context
