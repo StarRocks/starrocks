defeb9b36e1e0110b2dff6cdabe0a4579d66281a
liuyehcf <1559500551@qq.com>
2022-12-29 11:04:25 +0800

    [BugFix] Fix read_contents return false when reaching eos (#15891)
    (cherry picked from commit 60dca17f3b737e3776249cb306bd79024a52f7d7)

--- be/src/util/file_util.h
+++ be/src/util/file_util.h
@@ -16,25 +16,43 @@
 
 #include <fstream>
 #include <string>
+#include <tuple>
 
 namespace starrocks {
 class FileUtil {
 public:
+    // Return true if it is correctly assigned, otherwise return false
     static bool read_whole_content(const std::string& path, std::string& dest);
 
+    // Return true if all values are correctly assigned, otherwise return false
     template <typename... Args>
     static bool read_contents(const std::string& path, Args&... values) {
         std::ifstream ifs;
         ifs.open(path);
-        if (!ifs.good()) {
-            return false;
-        }
 
-        (ifs >> ... >> values);
-
+        auto tvalues = std::forward_as_tuple(values...);
         bool ok = ifs.good();
-        ifs.close();
+        _constexpr_for<0, sizeof...(values), 1>([&ifs, &tvalues, &ok](auto i) {
+            ok &= ifs.good();
+            if (!ok) {
+                return;
+            }
+            ifs >> std::get<i>(tvalues);
+        });
+
+        if (ifs.is_open()) {
+            ifs.close();
+        }
         return ok;
+    }
+
+private:
+    template <auto Start, auto End, auto Inc, typename F>
+    static constexpr void _constexpr_for(F&& f) {
+        if constexpr (Start < End) {
+            f(std::integral_constant<decltype(Start), Start>());
+            _constexpr_for<Start + Inc, End, Inc>(f);
+        }
     }
 };
 }; // namespace starrocks
