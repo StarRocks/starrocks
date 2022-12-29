d171cbdeeedd33af7dc85fe1304a37530643ff0d
packy <wangchao@starrocks.com>
2022-12-29 14:05:39 +0800

    add ut

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/PruneProjectColumnsRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/PruneProjectColumnsRule.java
@@ -18,7 +18,6 @@ package com.starrocks.sql.optimizer.rule.transformation;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.starrocks.catalog.FunctionSet;
-import com.starrocks.catalog.Type;
 import com.starrocks.sql.optimizer.OptExpression;
 import com.starrocks.sql.optimizer.OptimizerContext;
 import com.starrocks.sql.optimizer.base.ColumnRefSet;
@@ -28,7 +27,6 @@ import com.starrocks.sql.optimizer.operator.logical.LogicalProjectOperator;
 import com.starrocks.sql.optimizer.operator.pattern.Pattern;
 import com.starrocks.sql.optimizer.operator.scalar.CallOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ColumnRefOperator;
-import com.starrocks.sql.optimizer.operator.scalar.ConstantOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ScalarOperator;
 import com.starrocks.sql.optimizer.rule.RuleType;
 
@@ -66,10 +64,15 @@ public class PruneProjectColumnsRule extends TransformationRule {
 
         // Join Operator could not accept an output column, so try to choose a column for it
         LogicalOperator inputOp = (LogicalOperator) input.inputAt(0).getOp();
-        if (newMap.isEmpty()) {
-            ColumnRefOperator constCol = context.getColumnRefFactory()
-                    .create("auto_fill_col", Type.TINYINT, true);
-            newMap.put(constCol, ConstantOperator.createTinyInt((byte) 1));
+        if (newMap.isEmpty() && inputOp.getOpType().equals(OperatorType.LOGICAL_JOIN)) {
+            ColumnRefSet candidates = new ColumnRefSet(projectOperator.getColumnRefMap().keySet());
+            ColumnRefOperator smallestColumn =
+                    inputOp.getSmallestColumn(candidates, context.getColumnRefFactory(), input.inputAt(0));
+            if (smallestColumn != null) {
+                ScalarOperator expr = projectOperator.getColumnRefMap().get(smallestColumn);
+                newMap.put(smallestColumn, expr);
+                requiredInputColumns.union(smallestColumn);
+            }
         }
 
         // Change the requiredOutputColumns in context
