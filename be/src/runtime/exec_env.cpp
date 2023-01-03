62c9212531a55af5a7252a41158bf34779f7991f
dirtysalt <dirtysalt1987@gmail.com>
2023-01-03 14:01:22 +0800

    [Enhancement] set connector scan thread number by 8xcpu (#15403)

--- be/src/runtime/exec_env.cpp
+++ be/src/runtime/exec_env.cpp
@@ -215,8 +215,9 @@ Status ExecEnv::_init(const std::vector<StorePath>& store_paths) {
             new pipeline::GlobalDriverExecutor("wg_pip_exe", std::move(wg_driver_executor_thread_pool), true);
     _wg_driver_executor->initialize(_max_executor_threads);
 
-    int connector_num_io_threads = config::pipeline_hdfs_scan_thread_pool_thread_num;
-    CHECK_GT(connector_num_io_threads, 0) << "pipeline_hdfs_scan_thread_pool_thread_num should greater than 0";
+    int connector_num_io_threads =
+            config::pipeline_connector_scan_thread_num_per_cpu * std::thread::hardware_concurrency();
+    CHECK_GT(connector_num_io_threads, 0) << "pipeline_connector_scan_thread_num_per_cpu should greater than 0";
 
     std::unique_ptr<ThreadPool> connector_scan_worker_thread_pool_without_workgroup;
     RETURN_IF_ERROR(ThreadPoolBuilder("con_scan_io")
