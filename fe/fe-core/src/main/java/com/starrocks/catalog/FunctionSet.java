ca33df889c263fd31c3232a5cb765a3619aacf97
dorianzheng <xingzhengde72@gmail.com>
2023-01-03 15:22:59 +0800

    add to `notAlwaysNullResultWithNullParamFunctions`
    Signed-off-by: dorianzheng <xingzhengde72@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/FunctionSet.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/FunctionSet.java
@@ -400,6 +400,7 @@ public class FunctionSet {
 
     // Struct functions:
     public static final String ROW = "row";
+    public static final String NAMED_STRUCT = "named_struct";
 
     // JSON functions
     public static final Function JSON_QUERY_FUNC = new Function(
@@ -464,7 +465,7 @@ public class FunctionSet {
     // This does not contain any user defined functions. All UDFs handle null values by themselves.
     private final ImmutableSet<String> notAlwaysNullResultWithNullParamFunctions =
             ImmutableSet.of(IF, CONCAT_WS, IFNULL, NULLIF, NULL_OR_EMPTY, COALESCE, BITMAP_HASH, PERCENTILE_HASH,
-                    HLL_HASH, JSON_ARRAY, JSON_OBJECT, ROW);
+                    HLL_HASH, JSON_ARRAY, JSON_OBJECT, ROW, NAMED_STRUCT);
 
     // If low cardinality string column with global dict, for some string functions,
     // we could evaluate the function only with the dict content, not all string column data.
