10d88210ffa535495cd1dab2304e6f42a95e91c1
Binglin Chang <decstery@gmail.com>
2023-01-04 09:49:32 +0800

    [Feature] Add local tablet reader and rpc interface
    Signed-off-by: Binglin Chang <decstery@gmail.com>

--- be/src/service/service_be/internal_service.h
+++ be/src/service/service_be/internal_service.h
@@ -73,6 +73,22 @@ public:
 
     void tablet_writer_cancel(google::protobuf::RpcController* controller, const PTabletWriterCancelRequest* request,
                               PTabletWriterCancelResult* response, google::protobuf::Closure* done) override;
+
+    void local_tablet_reader_open(google::protobuf::RpcController* controller, const PTabletReaderOpenRequest* request,
+                                  PTabletReaderOpenResult* response, google::protobuf::Closure* done) override;
+    void local_tablet_reader_close(google::protobuf::RpcController* controller,
+                                   const PTabletReaderCloseRequest* request, PTabletReaderCloseResult* response,
+                                   google::protobuf::Closure* done) override;
+    void local_tablet_reader_multi_get(google::protobuf::RpcController* controller,
+                                       const PTabletReaderMultiGetRequest* request,
+                                       PTabletReaderMultiGetResult* response, google::protobuf::Closure* done) override;
+    void local_tablet_reader_scan_open(google::protobuf::RpcController* controller,
+                                       const PTabletReaderScanOpenRequest* request,
+                                       PTabletReaderScanOpenResult* response, google::protobuf::Closure* done) override;
+    void local_tablet_reader_scan_get_next(google::protobuf::RpcController* controller,
+                                           const PTabletReaderScanGetNextRequest* request,
+                                           PTabletReaderScanGetNextResult* response,
+                                           google::protobuf::Closure* done) override;
 };
 
 } // namespace starrocks
