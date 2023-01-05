42ba8ec24f00ed54a174987b3cae1e93b155abfa
Alvin <alvin.c.zhao@gmail.com>
2023-01-05 07:07:48 +0000

    [Refactor] Remove legacy code

--- be/test/storage/tablet_meta_manager_test.cpp
+++ be/test/storage/tablet_meta_manager_test.cpp
@@ -69,7 +69,7 @@ TEST_F(TabletMetaManagerTest, test_save_load_tablet_meta) {
     auto meta = std::make_shared<TabletMeta>();
     meta->init_from_pb(&meta_pb);
 
-    ASSERT_TRUE(TabletMetaManager::save(_data_dir.get(), meta).ok());
+    ASSERT_TRUE(TabletMetaManager::save(_data_dir.get(), meta_pb).ok());
     auto load_meta = std::make_shared<TabletMeta>();
     ASSERT_TRUE(
             TabletMetaManager::get_tablet_meta(_data_dir.get(), meta->tablet_id(), meta->schema_hash(), load_meta.get())
