a4bb5193d0fdf226c707abf88078586c84a86299
dorianzheng <xingzhengde72@gmail.com>
2023-01-07 16:25:45 +0800

    Get default user name
    Signed-off-by: dorianzheng <xingzhengde72@gmail.com>

--- conf/hadoop_env.sh
+++ conf/hadoop_env.sh
@@ -13,11 +13,14 @@
 # limitations under the License.
 
 export HADOOP_CLASSPATH=${STARROCKS_HOME}/lib/hadoop/common/*:${STARROCKS_HOME}/lib/hadoop/common/lib/*:${STARROCKS_HOME}/lib/hadoop/hdfs/*:${STARROCKS_HOME}/lib/hadoop/hdfs/lib/*
-if [ -z "${USER}" ]
+if [ -z "${HADOOP_USER_NAME}" ]
 then
-    export HADOOP_USER_NAME=$(id -u -n)
-else
-    export HADOOP_USER_NAME=${USER}
+    if [ -z "${USER}" ]
+    then
+        export HADOOP_USER_NAME=$(id -u -n)
+    else
+        export HADOOP_USER_NAME=${USER}
+    fi
 fi
 
 # the purpose is to use local hadoop configuration first.
