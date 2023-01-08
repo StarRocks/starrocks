c8642bf0268b08dbf85fc710e7fb4961e5fa96a2
Youngwb <yangwenbo_mailbox@163.com>
2023-01-09 06:10:56 +0800

    [Enhancement] Support array type for Trino parser on starrocks (#16339)

--- fe/fe-core/src/main/java/com/starrocks/connector/parser/trino/Trino2SRFunctionCallTransformer.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/parser/trino/Trino2SRFunctionCallTransformer.java
@@ -14,6 +14,7 @@
 
 package com.starrocks.connector.parser.trino;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
@@ -22,6 +23,7 @@ import com.starrocks.analysis.CaseWhenClause;
 import com.starrocks.analysis.Expr;
 import com.starrocks.analysis.FunctionCallExpr;
 import com.starrocks.analysis.IntLiteral;
+import com.starrocks.sql.ast.ArrayExpr;
 
 import java.util.List;
 import java.util.Map;
@@ -61,6 +63,7 @@ public class Trino2SRFunctionCallTransformer {
 
     private static void registerAllFunctionTransformer() {
         registerAggregateFunctionTransformer();
+        registerArrayFunctionTransformer();
         // todo： support more function transform
     }
 
@@ -99,15 +102,54 @@ public class Trino2SRFunctionCallTransformer {
                         new PlaceholderExpr(1, Expr.class), new IntLiteral(1L))), null))));
     }
 
+    private static void registerArrayFunctionTransformer() {
+        // 1. array_union -> array_distinct(array_concat(x, y))
+        registerFunctionTransformer("array_union", 2, new FunctionCallExpr("array_distinct",
+                ImmutableList.of(new FunctionCallExpr("array_concat", ImmutableList.of(
+                        new PlaceholderExpr(1, Expr.class), new PlaceholderExpr(2, Expr.class)
+                )))));
+
+        // 2. concat -> array_concat
+        registerFunctionTransformerWithVarArgs("concat", "array_concat",
+                ImmutableList.of(ArrayExpr.class));
+
+        // 3. contains -> array_contains
+        registerFunctionTransformer("contains", 2, "array_contains",
+                ImmutableList.of(Expr.class, Expr.class));
+
+        // 4. contains_sequence -> array_contains_all
+        registerFunctionTransformer("contains_sequence", 2, "array_contains_all",
+                ImmutableList.of(Expr.class, Expr.class));
+
+        // 5. slice -> array_slice
+        registerFunctionTransformer("slice", 3, "array_slice",
+                ImmutableList.of(Expr.class, Expr.class, Expr.class));
+    }
+
     private static void registerFunctionTransformer(String trinoFnName, int trinoFnArgNums, String starRocksFnName,
                                                     List<Class<? extends Expr>> starRocksArgumentsClass) {
         FunctionCallExpr starRocksFunctionCall = buildStarRocksFunctionCall(starRocksFnName, starRocksArgumentsClass);
         registerFunctionTransformer(trinoFnName, trinoFnArgNums, starRocksFunctionCall);
     }
 
+    private static void registerFunctionTransformerWithVarArgs(String trinoFnName, String starRocksFnName,
+                                                    List<Class<? extends Expr>> starRocksArgumentsClass) {
+        Preconditions.checkState(starRocksArgumentsClass.size() == 1);
+        FunctionCallExpr starRocksFunctionCall = buildStarRocksFunctionCall(starRocksFnName, starRocksArgumentsClass);
+        registerFunctionTransformerWithVarArgs(trinoFnName, starRocksFunctionCall);
+    }
+
     private static void registerFunctionTransformer(String trinoFnName, int trinoFnArgNums,
                                                     FunctionCallExpr starRocksFunctionCall) {
         FunctionCallTransformer transformer = new FunctionCallTransformer(starRocksFunctionCall, trinoFnArgNums);
+
+        List<FunctionCallTransformer> transformerList = TRANSFORMER_MAP.computeIfAbsent(trinoFnName,
+                k -> Lists.newArrayList());
+        transformerList.add(transformer);
+    }
+
+    private static void registerFunctionTransformerWithVarArgs(String trinoFnName, FunctionCallExpr starRocksFunctionCall) {
+        FunctionCallTransformer transformer = new FunctionCallTransformer(starRocksFunctionCall, true);
 
         List<FunctionCallTransformer> transformerList = TRANSFORMER_MAP.computeIfAbsent(trinoFnName,
                 k -> Lists.newArrayList());
