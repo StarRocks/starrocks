3e493a1beda127874d44f88d0a77c2f23ed17fd2
sduzh <zhuming9011@qq.com>
2023-01-09 15:47:17 +0800

    unused import

--- fe/fe-core/src/main/java/com/starrocks/qe/SimpleScheduler.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/SimpleScheduler.java
@@ -54,7 +54,6 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
-import java.util.stream.Collectors;
 import javax.annotation.Nullable;
 
 public class SimpleScheduler {
