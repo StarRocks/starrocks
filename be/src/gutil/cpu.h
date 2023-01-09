5628c33d23b68ad9e34a999a0032857194ae8495
wanweiqiangintel <weiqiang.wan@intel.com>
2023-01-09 13:44:47 +0800

    Update cpu.h

--- be/src/gutil/cpu.h
+++ be/src/gutil/cpu.h
@@ -85,7 +85,6 @@ public:
     bool has_avx512f() const { return has_avx512f_; }
     bool has_avx512bw() const { return has_avx512bw_; }
     bool has_aesni() const { return has_aesni_; }
-    bool has_avx512f() const { return has_avx512f_; }
     bool has_non_stop_time_stamp_counter() const { return has_non_stop_time_stamp_counter_; }
     bool is_running_in_vm() const { return is_running_in_vm_; }
     IntelMicroArchitecture GetIntelMicroArchitecture() const;
@@ -115,7 +114,6 @@ private:
     bool has_avx512f_{false};
     bool has_avx512bw_{false};
     bool has_aesni_{false};
-    bool has_avx512f_{false};
     bool has_non_stop_time_stamp_counter_{false};
     bool is_running_in_vm_{false};
     std::string cpu_vendor_;
