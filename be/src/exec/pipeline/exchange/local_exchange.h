a43d24d83dbcc76208475a8a8948ae028df7510f
Zhuhe Fang <fzhedu@gmail.com>
2023-01-10 21:30:15 +0800

    [Refactor] limit Local exchange buffer size by memory usage rather than by row count (#16177)
    local exchange buffer's size limits the sink's speed, causing low memory usage in some cases. This PR will increase CPU usage from 40+% to 90+% for some agg queries limited by network.
    
    besides, it alleviates the limit of exchange's scalability with dop.
    ```
    select host_name() h,exchange_speed(ss_customer_sk,ss_quantity,ss_sales_price), count(*) from store_sales where ss_customer_sk is not null group by ss_customer_sk;
    ```

--- be/src/exec/pipeline/exchange/local_exchange.h
+++ be/src/exec/pipeline/exchange/local_exchange.h
@@ -114,6 +114,15 @@ class PartitionExchanger final : public LocalExchanger {
             return _partition_row_indexes_start_points[partition_id + 1];
         }
 
+        size_t partition_memory_usage(size_t partition_id) {
+            if (partition_id >= _partition_memory_usage.size() || partition_id < 0) {
+                throw std::runtime_error(fmt::format("invalid index {} to get partition memory usage, whose size = {}.",
+                                                     partition_id, _partition_memory_usage.size()));
+            } else {
+                return _partition_memory_usage[partition_id];
+            }
+        }
+
     private:
         LocalExchangeSourceOperatorFactory* _source;
         const TPartitionType::type _part_type;
@@ -128,6 +137,7 @@ class PartitionExchanger final : public LocalExchanger {
         // It will easy to get number of rows belong to one channel by doing
         // _partition_row_indexes_start_points[i + 1] - _partition_row_indexes_start_points[i]
         std::vector<size_t> _partition_row_indexes_start_points;
+        std::vector<size_t> _partition_memory_usage;
         std::unique_ptr<Shuffler> _shuffler;
     };
 
