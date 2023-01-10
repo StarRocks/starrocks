5ffbafe3e86b054260d8adb18006fb5b5bebe281
zihe.liu <ziheliu1024@gmail.com>
2023-01-10 20:59:54 +0800

    [BugFix] Account finalization cost to scheduler (#16454)

--- be/src/exec/pipeline/pipeline_driver_executor.cpp
+++ be/src/exec/pipeline/pipeline_driver_executor.cpp
@@ -106,6 +106,8 @@ void GlobalDriverExecutor::_worker_thread() {
         auto* query_ctx = driver->query_ctx();
         auto* fragment_ctx = driver->fragment_ctx();
 
+        driver->increment_schedule_times();
+
         SCOPED_SET_TRACE_INFO(driver->driver_id(), query_ctx->query_id(), fragment_ctx->fragment_instance_id());
 
         SET_THREAD_LOCAL_QUERY_TRACE_CONTEXT(query_ctx->query_trace(), fragment_ctx->fragment_instance_id(), driver);
