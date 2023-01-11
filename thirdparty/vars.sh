c81fe3b6034b25889074669abe113d73381a029d
wangruin <58674590+wangruin@users.noreply.github.com>
2023-01-11 10:58:55 +0800

    Revert "Fix brpc compile error (#16418)" (#16479)
    Revert "Fix brpc compile error"

--- thirdparty/vars.sh
+++ thirdparty/vars.sh
@@ -173,17 +173,10 @@ LEVELDB_SOURCE=leveldb-1.20
 LEVELDB_MD5SUM="298b5bddf12c675d6345784261302252"
 
 # brpc
-<<<<<<< HEAD
 BRPC_DOWNLOAD="https://github.com/apache/incubator-brpc/archive/0.9.7.tar.gz"
 BRPC_NAME=incubator-brpc-0.9.7.tar.gz
 BRPC_SOURCE=incubator-brpc-0.9.7
 BRPC_MD5SUM="a5b79339d139d1c55d39689c0a69bcef"
-=======
-BRPC_DOWNLOAD="https://github.com/apache/brpc/archive/refs/tags/1.3.0.tar.gz"
-BRPC_NAME=brpc-1.3.0.tar.gz
-BRPC_SOURCE=brpc-1.3.0
-BRPC_MD5SUM="9470f1a77ec153e82cd8a25dc2148e47"
->>>>>>> a7222ea76 (Fix brpc compile error (#16418))
 
 # rocksdb
 ROCKSDB_DOWNLOAD="https://github.com/facebook/rocksdb/archive/refs/tags/v6.22.1.zip"
