0f960d6c34a28e9a8c793a16354fed7bdfd75d79
yanz <dirtysalt1987@gmail.com>
2023-01-12 17:46:08 +0800

    fix license header

--- bin/start_be.sh
+++ bin/start_be.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# Copyright 2021-present StarRocks, Inc. All rights reserved.
+#
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
