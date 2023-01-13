daeb6726ca6c99c1494ca5bc4d5e216c5255638a
iverson <58965658+wanpengfei-git@users.noreply.github.com>
2023-01-13 17:26:11 +0800

    Revert "Revert "[Enhancement] upgrade bit-shuffle version to support AVX512 (#16360)" (#16589)"
    This reverts commit 684b8f82578c04c1bcb2a7e9226640e30a02dc6d.

--- be/src/gutil/cpu.h
+++ be/src/gutil/cpu.h
@@ -82,8 +82,9 @@ public:
     bool has_popcnt() const { return has_popcnt_; }
     bool has_avx() const { return has_avx_; }
     bool has_avx2() const { return has_avx2_; }
-    bool has_aesni() const { return has_aesni_; }
     bool has_avx512f() const { return has_avx512f_; }
+    bool has_avx512bw() const { return has_avx512bw_; }
+    bool has_aesni() const { return has_aesni_; }
     bool has_non_stop_time_stamp_counter() const { return has_non_stop_time_stamp_counter_; }
     bool is_running_in_vm() const { return is_running_in_vm_; }
     IntelMicroArchitecture GetIntelMicroArchitecture() const;
@@ -110,8 +111,9 @@ private:
     bool has_popcnt_{false};
     bool has_avx_{false};
     bool has_avx2_{false};
-    bool has_aesni_{false};
     bool has_avx512f_{false};
+    bool has_avx512bw_{false};
+    bool has_aesni_{false};
     bool has_non_stop_time_stamp_counter_{false};
     bool is_running_in_vm_{false};
     std::string cpu_vendor_;
