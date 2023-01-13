07a1455dd1a843c40a4dc2711bc3d86596418a61
ABing <101158374+ABingHuang@users.noreply.github.com>
2023-01-13 12:52:44 +0800

    [Enhancement] optimize MV related metas processing (#16576)
    (cherry picked from commit 86e3f477ce58c0e38489d7cc82d03e9eddbde19b)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/scheduler/mv/MVActiveChecker.java
    #	fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java

--- fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
@@ -59,12 +59,14 @@ import com.starrocks.catalog.HiveTable;
 import com.starrocks.catalog.HudiTable;
 import com.starrocks.catalog.IcebergTable;
 import com.starrocks.catalog.Index;
+import com.starrocks.catalog.InternalCatalog;
 import com.starrocks.catalog.JDBCTable;
 import com.starrocks.catalog.KeysType;
 import com.starrocks.catalog.MaterializedIndexMeta;
 import com.starrocks.catalog.MaterializedView;
 import com.starrocks.catalog.MetaReplayState;
 import com.starrocks.catalog.MetaVersion;
+import com.starrocks.catalog.MvId;
 import com.starrocks.catalog.MysqlTable;
 import com.starrocks.catalog.OlapTable;
 import com.starrocks.catalog.Partition;
@@ -190,7 +192,12 @@ import com.starrocks.qe.ShowResultSet;
 import com.starrocks.qe.VariableMgr;
 import com.starrocks.rpc.FrontendServiceProxy;
 import com.starrocks.scheduler.TaskManager;
+<<<<<<< HEAD
 import com.starrocks.scheduler.mv.MVActiveChecker;
+=======
+import com.starrocks.scheduler.mv.MVJobExecutor;
+import com.starrocks.scheduler.mv.MVManager;
+>>>>>>> 86e3f477c ([Enhancement] optimize MV related metas processing (#16576))
 import com.starrocks.sql.ast.AddPartitionClause;
 import com.starrocks.sql.ast.AdminCheckTabletsStmt;
 import com.starrocks.sql.ast.AdminSetConfigStmt;
@@ -407,7 +414,11 @@ public class GlobalStateMgr {
 
     private RoutineLoadTaskScheduler routineLoadTaskScheduler;
 
+<<<<<<< HEAD
     private MVActiveChecker mvActiveChecker;
+=======
+    private MVJobExecutor mvMVJobExecutor;
+>>>>>>> 86e3f477c ([Enhancement] optimize MV related metas processing (#16576))
 
     private SmallFileMgr smallFileMgr;
 
@@ -590,7 +601,11 @@ public class GlobalStateMgr {
         this.loadLoadingChecker = new LoadLoadingChecker(loadManager);
         this.routineLoadScheduler = new RoutineLoadScheduler(routineLoadManager);
         this.routineLoadTaskScheduler = new RoutineLoadTaskScheduler(routineLoadManager);
+<<<<<<< HEAD
         this.mvActiveChecker = new MVActiveChecker();
+=======
+        this.mvMVJobExecutor = new MVJobExecutor();
+>>>>>>> 86e3f477c ([Enhancement] optimize MV related metas processing (#16576))
 
         this.smallFileMgr = new SmallFileMgr();
 
@@ -1174,8 +1189,6 @@ public class GlobalStateMgr {
         // ES state store
         esRepository.start();
         starRocksRepository.start();
-        // materialized view active checker
-        mvActiveChecker.start();
 
         if (Config.enable_hms_events_incremental_sync) {
             metastoreEventsProcessor.start();
@@ -1310,9 +1323,50 @@ public class GlobalStateMgr {
             domainResolver = new DomainResolver(auth);
         }
 
+        try {
+            postLoadImage();
+        } catch (Exception t) {
+            LOG.warn("there is an exception during processing after load image. exception:", t);
+        }
+
         long loadImageEndTime = System.currentTimeMillis();
         this.imageJournalId = storage.getImageJournalId();
         LOG.info("finished to load image in " + (loadImageEndTime - loadImageStartTime) + " ms");
+    }
+
+    private void postLoadImage() {
+        processMvRelatedMeta();
+    }
+
+    private void processMvRelatedMeta() {
+        List<String> dbNames = metadataMgr.listDbNames(InternalCatalog.DEFAULT_INTERNAL_CATALOG_NAME);
+
+        long startMillis = System.currentTimeMillis();
+        for (String dbName : dbNames) {
+            Database db = metadataMgr.getDb(InternalCatalog.DEFAULT_INTERNAL_CATALOG_NAME, dbName);
+            for (MaterializedView mv : db.getMaterializedViews()) {
+                for (MaterializedView.BaseTableInfo baseTableInfo : mv.getBaseTableInfos()) {
+                    Table table = baseTableInfo.getTable();
+                    if (table == null) {
+                        LOG.warn("tableName :{} do not exist. set materialized view:{} to invalid",
+                                baseTableInfo.getTableName(), mv.getId());
+                        mv.setActive(false);
+                        continue;
+                    }
+                    if (table instanceof MaterializedView && !((MaterializedView) table).isActive()) {
+                        LOG.warn("tableName :{} is invalid. set materialized view:{} to invalid",
+                                baseTableInfo.getTableName(), mv.getId());
+                        mv.setActive(false);
+                        continue;
+                    }
+                    MvId mvId = new MvId(db.getId(), mv.getId());
+                    table.addRelatedMaterializedView(mvId);
+                }
+            }
+        }
+
+        long duration = System.currentTimeMillis() - startMillis;
+        LOG.info("finish processing all tables' related materialized views in {}ms", duration);
     }
 
     public long loadHeader(DataInputStream dis, long checksum) throws IOException {
