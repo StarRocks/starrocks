52b6c294343e64eef87eef34d8d399902a921bdf
Astralidea <astralidea@163.com>
2023-01-17 20:57:37 +0800

    support add list partition
    Signed-off-by: Astralidea <astralidea@163.com>

--- fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
+++ fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
@@ -1530,7 +1530,7 @@ public class LocalMetastore implements ConnectorMetadata {
         }
 
         PartitionInfo partitionInfo = olapTable.getPartitionInfo();
-        if (partitionInfo.getType() != PartitionType.RANGE) {
+        if (partitionInfo.getType() == PartitionType.UNPARTITIONED) {
             throw new DdlException("Alter table [" + olapTable.getName() + "] failed. Not a partitioned table");
         }
 
