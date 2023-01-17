91da6880a47a14be5036c28016f216bd05ceaa4a
trueeyu <lxhhust350@qq.com>
2023-01-17 10:17:04 +0800

    [Enhancement] Add catch bad alloc for serialize/finalize/transmit_chunk (#13641) (#16684)
    Co-authored-by: Pslydhh <luouyuyou@gmail.com>

--- be/src/serde/protobuf_serde.cpp
+++ be/src/serde/protobuf_serde.cpp
@@ -6,6 +6,7 @@
 
 #include "column/column_helper.h"
 #include "gutil/strings/substitute.h"
+#include "runtime/current_thread.h"
 #include "runtime/descriptors.h"
 #include "serde/column_array_serde.h"
 #include "util/coding.h"
@@ -174,7 +175,8 @@ StatusOr<vectorized::Chunk> ProtobufChunkSerde::deserialize(const RowDescriptor&
     }
     int64_t deserialized_size = 0;
     ProtobufChunkDeserializer deserializer(*res, &chunk_pb, encode_level);
-    StatusOr<vectorized::Chunk> chunk = deserializer.deserialize(chunk_pb.data(), &deserialized_size);
+    StatusOr<vectorized::Chunk> chunk = Status::OK();
+    TRY_CATCH_BAD_ALLOC(chunk = deserializer.deserialize(chunk_pb.data(), &deserialized_size));
     if (!chunk.ok()) return chunk;
 
     // The logic is a bit confusing here.
