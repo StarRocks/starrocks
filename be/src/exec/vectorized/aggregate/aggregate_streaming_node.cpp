7e7ee4c2fa328de6982b54a3a769b4a65e0a5c2e
ZiheLiu <ziheliu1024@gmail.com>
2023-01-18 11:14:32 +0800

    fix format

--- be/src/exec/vectorized/aggregate/aggregate_streaming_node.cpp
+++ be/src/exec/vectorized/aggregate/aggregate_streaming_node.cpp
@@ -145,11 +145,11 @@ Status AggregateStreamingNode::get_next(RuntimeState* state, ChunkPtr* chunk, bo
                         SCOPED_TIMER(_aggregator->agg_compute_timer());
                         if (false) {
                         }
-#define HASH_MAP_METHOD(NAME)                                                                       \
-    else if (_aggregator->hash_map_variant().type == AggHashMapVariant::Type::NAME) {               \
-        TRY_CATCH_BAD_ALLOC(_aggregator->build_hash_map_with_selection<                             \
-                            typename decltype(_aggregator->hash_map_variant().NAME)::element_type>( \
-                *_aggregator->hash_map_variant().NAME, input_chunk_size));                          \
+#define HASH_MAP_METHOD(NAME)                                                             \
+    else if (_aggregator->hash_map_variant().type == AggHashMapVariant::Type::NAME) {     \
+        TRY_CATCH_BAD_ALLOC(_aggregator->build_hash_map_with_selection<typename decltype( \
+                                    _aggregator->hash_map_variant().NAME)::element_type>( \
+                *_aggregator->hash_map_variant().NAME, input_chunk_size));                \
     }
                         APPLY_FOR_AGG_VARIANT_ALL(HASH_MAP_METHOD)
 #undef HASH_MAP_METHOD
