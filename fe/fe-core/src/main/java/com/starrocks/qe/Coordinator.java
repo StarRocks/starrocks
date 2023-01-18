2f16fa6272b2f0422d2a6df62fd259988e64e084
ZiheLiu <ziheliu1024@gmail.com>
2023-01-18 13:27:14 +0800

    fix

--- fe/fe-core/src/main/java/com/starrocks/qe/Coordinator.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/Coordinator.java
@@ -33,6 +33,7 @@ import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
 import com.starrocks.analysis.DescriptorTable;
 import com.starrocks.catalog.FsBroker;
+import com.starrocks.catalog.ResourceGroup;
 import com.starrocks.catalog.ResourceGroupClassifier;
 import com.starrocks.common.Config;
 import com.starrocks.common.MarkedCountDownLatch;
