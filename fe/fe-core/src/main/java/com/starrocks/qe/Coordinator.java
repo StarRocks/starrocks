bf783ed4f76b3dd846d5a71c80ba80d979e20146
ZiheLiu <ziheliu1024@gmail.com>
2023-01-18 13:11:42 +0800

    fix conflict

--- fe/fe-core/src/main/java/com/starrocks/qe/Coordinator.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/Coordinator.java
@@ -702,6 +702,8 @@ public class Coordinator {
         if (resourceGroup != null) {
             connect.getAuditEventBuilder().setResourceGroup(resourceGroup.getName());
             connect.setResourceGroup(resourceGroup);
+        } else {
+            connect.getAuditEventBuilder().setResourceGroup(ResourceGroup.DEFAULT_RESOURCE_GROUP_NAME);
         }
 
         return resourceGroup;
