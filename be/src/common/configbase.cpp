c86b5b776f8c85636745d65b65cdd135bede6d92
Zuyu Zhang <z.zhang@celonis.com>
2023-01-19 10:16:30 -0800

    Keep backward compatible

--- be/src/common/configbase.cpp
+++ be/src/common/configbase.cpp
@@ -23,6 +23,7 @@
 #include <list>
 #include <map>
 #include <regex>
+#include <string>
 
 #define __IN_CONFIGBASE_CPP__
 #include "common/config.h"
@@ -213,6 +214,10 @@ bool Properties::load(const char* filename) {
 
         // compatible with doris_config
         key = std::regex_replace(key, doris_start, "");
+
+        if (key.compare("webserver_port") == 0) {
+            key = "be_http_port";
+        }
 
         // Insert into 'file_conf_map'.
         file_conf_map[key] = value;
