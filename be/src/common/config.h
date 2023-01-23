9d0a16715a7df4c8b7d7b8af6d5b972686860341
lichaoyong <lichaoyong158@gmail.com>
2023-01-23 15:42:14 -0800

    [Refactor] Default close the event base compaction and size-tiered compaction default in 2.5 (#16836)

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -321,9 +321,9 @@ CONF_mInt32(compaction_trace_threshold, "60");
 // the columns will be divided into groups for vertical compaction.
 CONF_Int64(vertical_compaction_max_columns_per_group, "5");
 
-CONF_Bool(enable_event_based_compaction_framework, "true");
+CONF_Bool(enable_event_based_compaction_framework, "false");
 
-CONF_Bool(enable_size_tiered_compaction_strategy, "true");
+CONF_Bool(enable_size_tiered_compaction_strategy, "false");
 CONF_mInt64(size_tiered_min_level_size, "131072");
 CONF_mInt64(size_tiered_level_multiple, "5");
 CONF_mInt64(size_tiered_level_num, "7");
