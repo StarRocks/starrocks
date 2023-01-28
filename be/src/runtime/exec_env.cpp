54169030eaa85d9966f972f69275e44be80928e6
Murphy <mofei@starrocks.com>
2023-01-28 12:17:31 -0800

    resolve conflict

--- be/src/runtime/exec_env.cpp
+++ be/src/runtime/exec_env.cpp
@@ -275,38 +275,6 @@ Status ExecEnv::_init(const std::vector<StorePath>& store_paths) {
 
     // it means acting as compute node while store_path is empty. some threads are not needed for that case.
     if (!store_paths.empty()) {
-<<<<<<< HEAD
-        int num_io_threads = config::pipeline_scan_thread_pool_thread_num <= 0
-                                     ? std::thread::hardware_concurrency()
-                                     : config::pipeline_scan_thread_pool_thread_num;
-
-        std::unique_ptr<ThreadPool> scan_worker_thread_pool_without_workgroup;
-        RETURN_IF_ERROR(ThreadPoolBuilder("pip_scan_io")
-                                .set_min_threads(0)
-                                .set_max_threads(num_io_threads)
-                                .set_max_queue_size(1000)
-                                .set_idle_timeout(MonoDelta::FromMilliseconds(2000))
-                                .build(&scan_worker_thread_pool_without_workgroup));
-        _scan_executor_without_workgroup = new workgroup::ScanExecutor(
-                std::move(scan_worker_thread_pool_without_workgroup),
-                std::make_unique<workgroup::PriorityScanTaskQueue>(config::pipeline_scan_thread_pool_queue_size));
-        _scan_executor_without_workgroup->initialize(num_io_threads);
-
-        std::unique_ptr<ThreadPool> scan_worker_thread_pool_with_workgroup;
-        RETURN_IF_ERROR(ThreadPoolBuilder("pip_wg_scan_io")
-                                .set_min_threads(0)
-                                .set_max_threads(num_io_threads)
-                                .set_max_queue_size(1000)
-                                .set_idle_timeout(MonoDelta::FromMilliseconds(2000))
-                                .build(&scan_worker_thread_pool_with_workgroup));
-        _scan_executor_with_workgroup =
-                new workgroup::ScanExecutor(std::move(scan_worker_thread_pool_with_workgroup),
-                                            std::make_unique<workgroup::WorkGroupScanTaskQueue>(
-                                                    workgroup::WorkGroupScanTaskQueue::SchedEntityType::OLAP));
-        _scan_executor_with_workgroup->initialize(num_io_threads);
-
-=======
->>>>>>> c5706866e ([BugFix] Init scan executor for compute node (#16933))
         Status status = _load_path_mgr->init();
         if (!status.ok()) {
             LOG(ERROR) << "load path mgr init failed." << status.get_error_msg();
