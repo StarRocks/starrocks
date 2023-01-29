bbcb4affade8bca5cb528da7557a62f55a9869a8
zihe.liu <ziheliu1024@gmail.com>
2023-01-30 00:21:56 +0800

    [BugFix] Own short_key_encode result for LogicalSplitMorselQueue (#16977)
    Signed-off-by: ZiheLiu <ziheliu1024@gmail.com>

--- be/src/exec/pipeline/scan/morsel.cpp
+++ be/src/exec/pipeline/scan/morsel.cpp
@@ -449,10 +449,12 @@ bool LogicalSplitMorselQueue::_valid_range(const ShortKeyOptionPtr& lower, const
     }
 
     Slice lower_key;
+    std::string lower_key_payload;
     // Empty short key of start ShortKeyOption means it is the first splitted key range,
     // so use start original short key to compare.
     if (lower->tuple_key != nullptr) {
-        lower_key = lower->tuple_key->short_key_encode(_short_key_schema->num_fields(), KEY_MINIMAL_MARKER);
+        lower_key_payload = lower->tuple_key->short_key_encode(_short_key_schema->num_fields(), KEY_MINIMAL_MARKER);
+        lower_key = Slice(lower_key_payload);
     } else if (!lower->short_key.empty()) {
         lower_key = lower->short_key;
     } else {
@@ -460,10 +462,12 @@ bool LogicalSplitMorselQueue::_valid_range(const ShortKeyOptionPtr& lower, const
     }
 
     Slice upper_key;
+    std::string upper_key_payload;
     // Empty short key of end ShortKeyOption means it is the last splitted key range,
     // so use end original short key to compare.
     if (upper->tuple_key != nullptr) {
-        upper_key = upper->tuple_key->short_key_encode(_short_key_schema->num_fields(), KEY_MINIMAL_MARKER);
+        upper_key_payload = upper->tuple_key->short_key_encode(_short_key_schema->num_fields(), KEY_MINIMAL_MARKER);
+        upper_key = Slice(upper_key_payload);
     } else if (!upper->short_key.empty()) {
         upper_key = upper->short_key;
     } else {
