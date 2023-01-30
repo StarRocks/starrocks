966a4b7303f98e5a1b4199ba3bff43658428ef2a
Xuefen Liu <liuxuefen@starrocks.com>
2023-01-30 15:55:45 +0800

    [Feature]Multi cluster warehouse  (#16728)
    Multi cluster warehouse

--- fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
@@ -479,6 +479,8 @@ public class GlobalStateMgr {
     // For LakeTable
     private CompactionManager compactionManager;
 
+    private WarehouseManager warehouseMgr;
+
     private ConfigRefreshDaemon configRefreshDaemon;
 
     public List<Frontend> getFrontends(FrontendNodeType nodeType) {
@@ -644,6 +646,7 @@ public class GlobalStateMgr {
         }
 
         this.localMetastore = new LocalMetastore(this, recycleBin, colocateTableIndex, nodeMgr.getClusterInfo());
+        this.warehouseMgr = new WarehouseManager();
         this.connectorMgr = new ConnectorMgr();
         this.metadataMgr = new MetadataMgr(localMetastore, connectorMgr);
         this.catalogMgr = new CatalogMgr(connectorMgr);
@@ -861,6 +864,10 @@ public class GlobalStateMgr {
 
     public InsertOverwriteJobManager getInsertOverwriteJobManager() {
         return insertOverwriteJobManager;
+    }
+
+    public WarehouseManager getWarehouseMgr() {
+        return warehouseMgr;
     }
 
     // Use tryLock to avoid potential dead lock
@@ -1347,6 +1354,8 @@ public class GlobalStateMgr {
             checksum = MVManager.getInstance().reload(dis, checksum);
             remoteChecksum = dis.readLong();
             globalFunctionMgr.loadGlobalFunctions(dis, checksum);
+            checksum = warehouseMgr.loadWarehouses(dis, checksum);
+            remoteChecksum = dis.readLong();
             // TODO put this at the end of the image before 3.0 release
             loadRBACPrivilege(dis);
         } catch (EOFException exception) {
@@ -1661,6 +1670,8 @@ public class GlobalStateMgr {
             checksum = MVManager.getInstance().store(dos, checksum);
             dos.writeLong(checksum);
             globalFunctionMgr.saveGlobalFunctions(dos, checksum);
+            checksum = warehouseMgr.saveWarehouses(dos, checksum);
+            dos.writeLong(checksum);
             // TODO put this at the end of the image before 3.0 release
             saveRBACPrivilege(dos);
         }
@@ -3128,6 +3139,14 @@ public class GlobalStateMgr {
 
     public void cancelAlterCluster(CancelAlterSystemStmt stmt) throws DdlException {
         this.alter.getClusterHandler().cancel(stmt);
+    }
+
+    // Change current warehouse of this session.
+    public void changeWarehouse(ConnectContext ctx, String newWarehouseName) throws AnalysisException {
+        if (!warehouseMgr.warehouseExists(newWarehouseName)) {
+            ErrorReport.reportAnalysisException(ErrorCode.ERR_BAD_WAREHOUSE_ERROR, newWarehouseName);
+        }
+        ctx.setCurrentWarehouse(newWarehouseName);
     }
 
     // Change current catalog of this session.
