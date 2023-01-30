bce9072c113b32ff574eac4d78b4c94c9755fd5f
fzhedu <fzhedu@gmail.com>
2023-01-30 15:26:27 +0800

    serialize data to raw memory rather than memory
    Signed-off-by: fzhedu <fzhedu@gmail.com>

--- be/src/exec/pipeline/exchange/sink_buffer.cpp
+++ be/src/exec/pipeline/exchange/sink_buffer.cpp
@@ -322,7 +322,7 @@ Status SinkBuffer::_try_to_send_rpc(const TUniqueId& instance_id, const std::fun
         }
 
         auto* closure = new DisposableClosure<PTransmitChunkResult, ClosureContext>(
-                {instance_id, request.params->sequence(), GetCurrentTimeNanos()});
+                {instance_id, request.params->sequence(), GetCurrentTimeNanos(), request.chunk_data_ref});
         if (_first_send_time == -1) {
             _first_send_time = MonotonicNanos();
         }
@@ -365,8 +365,8 @@ Status SinkBuffer::_try_to_send_rpc(const TUniqueId& instance_id, const std::fun
 
         // Attachment will be released by process_mem_tracker in closure->Run() in bthread, when receiving the response,
         // so decrease the memory usage of attachment from instance_mem_tracker immediately before sending the request.
-        _mem_tracker->release(request.attachment_physical_bytes);
-        ExecEnv::GetInstance()->process_mem_tracker()->consume(request.attachment_physical_bytes);
+        _mem_tracker->release(request.chunk_data_ref->data_bytes);
+        ExecEnv::GetInstance()->process_mem_tracker()->consume(request.chunk_data_ref->data_bytes);
 
         closure->cntl.Reset();
         closure->cntl.set_timeout_ms(_brpc_timeout_ms);
--- be/src/exec/pipeline/exchange/sink_buffer.h
+++ be/src/exec/pipeline/exchange/sink_buffer.h
@@ -38,6 +38,12 @@ namespace starrocks::pipeline {
 
 using PTransmitChunkParamsPtr = std::shared_ptr<PTransmitChunkParams>;
 
+struct ChunksDataRef {
+    ChunksDataRef(int64_t bytes) : data_bytes(bytes) {}
+    std::vector<std::shared_ptr<string>> chunks_data_ref;
+    int64_t data_bytes;
+};
+
 struct TransmitChunkInfo {
     // For BUCKET_SHUFFLE_HASH_PARTITIONED, multiple channels may be related to
     // a same exchange source fragment instance, so we should use fragment_instance_id
@@ -46,13 +52,14 @@ struct TransmitChunkInfo {
     doris::PBackendService_Stub* brpc_stub;
     PTransmitChunkParamsPtr params;
     butil::IOBuf attachment;
-    int64_t attachment_physical_bytes;
+    std::shared_ptr<ChunksDataRef> chunk_data_ref;
 };
 
 struct ClosureContext {
     TUniqueId instance_id;
     int64_t sequence;
     int64_t send_timestamp;
+    std::shared_ptr<ChunksDataRef> chunk_data_ref;
 };
 
 // TimeTrace is introduced to estimate time more accurately.
