112335c869980becfc42fdc4bb4c0728a02c598e
eyes_on_me <3675229+silverbullet233@users.noreply.github.com>
2023-01-31 13:32:23 +0800

    [Enhancement] add PruneGroupByKeysRule (#16716)
    Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>

--- fe/fe-core/src/test/java/com/starrocks/planner/QueryCacheTest.java
+++ fe/fe-core/src/test/java/com/starrocks/planner/QueryCacheTest.java
@@ -860,7 +860,6 @@ public class QueryCacheTest {
     public void testRandomFunctions() {
         List<String> queryList = Lists.newArrayList(
                 "select sum(v1) from t0 where uuid() like '%s'",
-                "select right(cast(random() as varchar), 2), sum(v1) from t0 where dt between '2022-02-01' and '2022-02-04' group by right(cast(random() as varchar), 2);",
                 "select sum(case when random()>0.5 then 1 else 0 end) from t0");
         Assert.assertTrue(queryList.stream().noneMatch(q -> getCachedFragment(q).isPresent()));
     }
--- fe/fe-core/src/test/resources/sql/subquery/in-subquery.sql
+++ fe/fe-core/src/test/resources/sql/subquery/in-subquery.sql
@@ -444,8 +444,8 @@ NULL AWARE LEFT ANTI JOIN (join-predicate [2: v2 = 9: v5 AND 2: v2 = 8: v4] post
 [sql]
 select v1 from t0 group by v1, v1 in (1) having v1 in (1)
 [result]
-AGGREGATE ([GLOBAL] aggregate [{}] group by [[1: v1, 4: expr]] having [null]
-    AGGREGATE ([LOCAL] aggregate [{}] group by [[1: v1, 4: expr]] having [null]
+AGGREGATE ([GLOBAL] aggregate [{}] group by [[1: v1]] having [null]
+    AGGREGATE ([LOCAL] aggregate [{}] group by [[1: v1]] having [null]
         SCAN (columns[1: v1] predicate[1: v1 = 1])
 [end]
 
