8e94a3f7ef38c96c7826b1196ab0f6994691262f
Seaven <seaven_7@qq.com>
2023-01-31 21:04:17 +0800

    [BugFix] Complex shuffle join with aggregate bug (#16834)
    Signed-off-by: Seaven <seaven_7@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/OutputPropertyDeriver.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/OutputPropertyDeriver.java
@@ -211,7 +211,7 @@ public class OutputPropertyDeriver extends PropertyDeriverBase<PhysicalPropertyS
                     (rightDistributionDesc.isShuffle()) || rightDistributionDesc.isShuffleEnforce()) {
                 // shuffle join
                 return computeHashJoinDistributionPropertyInfo(node,
-                        computeShuffleJoinOutputProperty(leftShuffleColumns),
+                        computeShuffleJoinOutputProperty(leftDistributionDesc.getColumns()),
                         leftOnPredicateColumns,
                         rightOnPredicateColumns, context);
             } else if (leftDistributionDesc.isShuffle() && rightDistributionDesc.isLocal()) {
