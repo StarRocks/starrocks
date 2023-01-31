95ef3d540c6f4841a176ab63cad87075961e2037
liuyehcf <1559500551@qq.com>
2023-01-31 10:03:00 +0800

    [BugFix] Fix assigning wrong type for empty array/map/struct (#16658)

--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/PolymorphicFunctionAnalyzer.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/PolymorphicFunctionAnalyzer.java
@@ -104,12 +104,13 @@ public class PolymorphicFunctionAnalyzer {
             }
 
             // Need to make input be a valid complex type if the input is Type NULL
+            // We choose boolean as the item type of complex type if input type is NULL_TYPE
             if (declType instanceof AnyArrayType) {
-                resolvedTypes[i] = inputType.isNull() ? new ArrayType(Type.NULL) : inputType;
+                resolvedTypes[i] = inputType.isNull() ? Type.ARRAY_BOOLEAN : inputType;
             } else if (declType instanceof AnyMapType) {
-                resolvedTypes[i] = inputType.isNull() ? new MapType(Type.NULL, Type.NULL) : inputType;
+                resolvedTypes[i] = inputType.isNull() ? new MapType(Type.BOOLEAN, Type.BOOLEAN) : inputType;
             } else if (declType instanceof AnyStructType) {
-                resolvedTypes[i] = inputType.isNull() ? new StructType(Lists.newArrayList(Type.NULL)) : inputType;
+                resolvedTypes[i] = inputType.isNull() ? new StructType(Lists.newArrayList(Type.BOOLEAN)) : inputType;
             } else {
                 resolvedTypes[i] = inputType;
             }
@@ -291,8 +292,8 @@ public class PolymorphicFunctionAnalyzer {
         } else if (typeElement != null) {
             typeArray = new ArrayType(typeElement);
         } else {
-            typeElement = Type.NULL;
-            typeArray = new ArrayType(Type.NULL);
+            typeElement = Type.BOOLEAN;
+            typeArray = (ArrayType) Type.ARRAY_BOOLEAN;
         }
 
         if (!typeArray.getItemType().matchesType(typeElement)) {
