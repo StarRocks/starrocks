10271cc95788a0360633deec152f1e6a46f1e107
stephen <91597003+stephen-shelby@users.noreply.github.com>
2023-01-31 18:31:54 +0800

    [Feature] select into outfile support parquet format (#16396)

--- gensrc/thrift/DataSinks.thrift
+++ gensrc/thrift/DataSinks.thrift
@@ -59,6 +59,13 @@ enum TResultSinkType {
     VARIABLE
 }
 
+struct TParquetOptions {
+    // parquet row group max size in bytes
+    1: optional i64 parquet_max_group_bytes
+    2: optional Types.TCompressionType compression_type
+    3: optional bool use_dict
+}
+
 struct TResultFileSinkOptions {
     1: required string file_path
     2: required PlanNodes.TFileFormatType file_format
@@ -73,7 +80,9 @@ struct TResultFileSinkOptions {
     // hdfs_write_buffer_size_kb for writing through lib hdfs directly
     9: optional i32 hdfs_write_buffer_size_kb = 0
     // properties from hdfs-site.xml, core-site.xml and load_properties
-    10: optional PlanNodes.THdfsProperties hdfs_properties 
+    10: optional PlanNodes.THdfsProperties hdfs_properties
+    11: optional TParquetOptions parquet_options
+    12: optional list<string> file_column_names
 }
 
 struct TMemoryScratchSink {
