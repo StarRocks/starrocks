f477e1a3b432553b10d80855154fe7a4c788fd4c
ZiheLiu <ziheliu1024@gmail.com>
2023-02-02 16:58:49 +0800

    opt ScanOperator::has_output
    Signed-off-by: ZiheLiu <ziheliu1024@gmail.com>

--- be/src/exec/pipeline/scan/scan_operator.cpp
+++ be/src/exec/pipeline/scan/scan_operator.cpp
@@ -162,7 +162,7 @@ bool ScanOperator::has_output() const {
         }
     }
 
-    return num_buffered_chunks() > 0;
+    return _num_running_io_tasks == 0 && num_buffered_chunks() > 0;
 }
 
 bool ScanOperator::pending_finish() const {
