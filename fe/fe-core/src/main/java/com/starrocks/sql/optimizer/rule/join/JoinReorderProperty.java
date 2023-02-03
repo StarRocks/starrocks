2f6c94c6e84c08c013cdb67de5a1cd8ddb85c1fe
packy92 <110370499+packy92@users.noreply.github.com>
2023-02-03 10:31:59 +0800

    [BugFix] cannot reorder when bottom join is full outer join (#17092)
    * cannot reorder when bottom join is full outer join
    
    Signed-off-by: packy <wangchao@starrocks.com>
    
    * fix unstable ut
    
    Signed-off-by: packy <wangchao@starrocks.com>
    
    ---------
    
    Signed-off-by: packy <wangchao@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/join/JoinReorderProperty.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/join/JoinReorderProperty.java
@@ -75,13 +75,13 @@ public class JoinReorderProperty {
          *  ----- ---- ---- ---- ---- ---- ---- ---- ---- ---- -----
          *   ⟕     1    1    1    1    0    0    0    2    0    0
          *   ⋉     0    1    1    1    0    0    0    0    0    0
-         *   ▷     0    1    1    1    0    0    0    2    0    0
-         *   ⟗    1    2    0    0    0    0    0    2    0    0
+         *   ▷     0    1    1    1    0    0    0    0   0    0
+         *   ⟗    0    2    0    0    0    0    0    2    0    0
          */
         LEFT_ASSCOM_PROPERTY[JoinOperator.LEFT_OUTER_JOIN.ordinal()] = new int[] {1, 1, 1, 1, 0, 0, 0, 2, 0, 0};
         LEFT_ASSCOM_PROPERTY[JoinOperator.LEFT_SEMI_JOIN.ordinal()] = new int[] {0, 1, 1, 1, 0, 0, 0, 0, 0, 0};
         LEFT_ASSCOM_PROPERTY[JoinOperator.LEFT_ANTI_JOIN.ordinal()] = new int[] {0, 1, 1, 1, 0, 0, 0, 0, 0, 0};
-        LEFT_ASSCOM_PROPERTY[JoinOperator.FULL_OUTER_JOIN.ordinal()] = new int[] {1, 2, 0, 0, 0, 0, 0, 2, 0, 0};
+        LEFT_ASSCOM_PROPERTY[JoinOperator.FULL_OUTER_JOIN.ordinal()] = new int[] {0, 2, 0, 0, 0, 0, 0, 2, 0, 0};
     }
 
     public static int getAssociativityProperty(JoinOperator bottomJoinType, JoinOperator topJoinType, boolean isInnerMode) {
