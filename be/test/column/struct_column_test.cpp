d6f2b37efd7d5406a41175e9b00a8a49423192ff
eyes_on_me <3675229+silverbullet233@users.noreply.github.com>
2023-02-03 21:54:58 +0800

    [BugFix] fix Column::element_memory_usage (#17184) (#17305)

--- be/test/column/struct_column_test.cpp
+++ be/test/column/struct_column_test.cpp
@@ -518,4 +518,31 @@ TEST(StructColumnTest, test_assign) {
     ASSERT_EQ("{id: 1, name: 'smith'}", column->debug_item(1));
 }
 
+TEST(StructColumnTest, test_element_memory_usage) {
+    auto field_name = BinaryColumn::create();
+    field_name->append_string("id");
+    field_name->append_string("name");
+    auto id = NullableColumn::create(UInt64Column::create(), NullColumn::create());
+    auto name = NullableColumn::create(BinaryColumn::create(), NullColumn::create());
+    Columns fields{id, name};
+    auto column = StructColumn::create(fields, field_name);
+
+    column->append_datum(DatumStruct{uint64_t(1), Slice("2")});
+    column->append_datum(DatumStruct{uint64_t(1), Slice("4")});
+    column->append_datum(DatumStruct{uint64_t(1), Slice("6")});
+
+    ASSERT_EQ(45, column->Column::element_memory_usage());
+
+    std::vector<size_t> element_mem_usages = {15, 15, 15};
+    size_t element_num = element_mem_usages.size();
+    for (size_t start = 0; start < element_num; start++) {
+        size_t expected_usage = 0;
+        ASSERT_EQ(0, column->element_memory_usage(start, 0));
+        for (size_t size = 1; start + size <= element_num; size++) {
+            expected_usage += element_mem_usages[start + size - 1];
+            ASSERT_EQ(expected_usage, column->element_memory_usage(start, size));
+        }
+    }
+}
+
 } // namespace starrocks::vectorized
