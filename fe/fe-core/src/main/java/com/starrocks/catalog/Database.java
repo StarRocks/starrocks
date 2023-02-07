63588d83da4b2d673b65880231a79aca730bac31
Felix Li <astralidea@163.com>
2023-02-07 11:59:01 +0800

    [Enhancement] Avoid potential checks leading to FE crash (#17435)
    * Avoid potential checks leading to FE crash
    
    Signed-off-by: Astralidea <astralidea@163.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/Database.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/Database.java
@@ -432,12 +432,12 @@ public class Database extends MetaObject implements Writable {
                 idToTable.put(table.getId(), table);
                 nameToTable.put(table.getName(), table);
 
+                table.onCreate();
                 if (!isReplay) {
                     // Write edit log
                     CreateTableInfo info = new CreateTableInfo(fullQualifiedName, table);
                     GlobalStateMgr.getCurrentState().getEditLog().logCreateTable(info);
                 }
-                table.onCreate();
             }
             return true;
         } finally {
@@ -543,12 +543,13 @@ public class Database extends MetaObject implements Writable {
             } else {
                 idToTable.put(materializedView.getId(), materializedView);
                 nameToTable.put(materializedView.getName(), materializedView);
+                // There are many checks in onCreate. If these checks fail, the log should not be written,
+                // so it should be placed in front of the log
+                materializedView.onCreate();
                 if (!isReplay) {
-                    // Write edit log
                     CreateTableInfo info = new CreateTableInfo(fullQualifiedName, materializedView);
                     GlobalStateMgr.getCurrentState().getEditLog().logCreateMaterializedView(info);
                 }
-                materializedView.onCreate();
             }
             return true;
         } finally {
