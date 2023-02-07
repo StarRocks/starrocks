e0714d30977ba69606cf94bfbec1a544e3767008
Felix Li <astralidea@163.com>
2023-02-07 17:53:03 +0800

    Support for CHARACTER reserved words (#17488)
    Signed-off-by: Astralidea <astralidea@163.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzeSetVariableTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzeSetVariableTest.java
@@ -149,6 +149,10 @@ public class AnalyzeSetVariableTest {
         analyzeSuccess(sql);
         sql = "SET CHAR SET 'utf8mb4'";
         analyzeSuccess(sql);
+        sql = "show character set where charset = 'utf8mb4'";
+        analyzeSuccess(sql);
+        sql = "SET CHARACTER SET utf8";
+        analyzeSuccess(sql);
     }
 
     @Test
