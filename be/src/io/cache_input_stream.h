cc540bde2b63105f66f0ad14733b0c2bebe42478
dirtysalt <dirtysalt1987@gmail.com>
2023-02-08 00:36:49 +0800

    [BugFix] fix block cache key beause of undefined behaviour (#17517)
    Signed-off-by: yanz <dirtysalt1987@gmail.com>

--- be/src/io/cache_input_stream.h
+++ be/src/io/cache_input_stream.h
@@ -33,7 +33,7 @@ public:
     };
 
     static constexpr int64_t BLOCK_SIZE = 1 * 1024 * 1024;
-    explicit CacheInputStream(std::string filename, std::shared_ptr<SeekableInputStream> stream);
+    explicit CacheInputStream(const std::string& filename, std::shared_ptr<SeekableInputStream> stream);
 
     ~CacheInputStream() override = default;
 
