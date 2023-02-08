adcda0a3ab8bc6c46b00c002bee9da2f2e932824
jiutianchen <chen9t@gmail.com>
2023-02-08 11:17:19 +0800

    [Feature] add new function approx_count_distinct_hll_sketch with DataSketches HLL
    Signed-off-by: jiutianchen <chen9t@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/tree/PreAggregateTurnOnRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/tree/PreAggregateTurnOnRule.java
@@ -70,6 +70,7 @@ public class PreAggregateTurnOnRule implements TreeRewriteRule {
                 .add(FunctionSet.NDV)
                 .add(FunctionSet.MULTI_DISTINCT_COUNT)
                 .add(FunctionSet.APPROX_COUNT_DISTINCT)
+                .add(FunctionSet.APPROX_COUNT_DISTINCT_HLL_SKETCH)
                 .add(FunctionSet.BITMAP_UNION_INT.toUpperCase()).build();
 
         @Override
