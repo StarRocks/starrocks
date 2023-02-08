c966a338c001c1190254b882082a201ce12af02a
silverbullet233 <3675229+silverbullet233@users.noreply.github.com>
2023-02-08 21:01:14 +0800

    fix error result when table is empty
    Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>

--- be/src/storage/lake_meta_reader.cpp
+++ be/src/storage/lake_meta_reader.cpp
@@ -35,11 +35,6 @@ Status LakeMetaReader::init(const LakeMetaReaderParams& read_params) {
     RETURN_IF_ERROR(_build_collect_context(read_params));
     RETURN_IF_ERROR(_init_seg_meta_collecters(read_params));
 
-    if (_collect_context.seg_collecters.size() == 0) {
-        _has_more = false;
-        return Status::OK();
-    }
-
     _collect_context.cursor_idx = 0;
     _is_init = true;
     _has_more = true;
