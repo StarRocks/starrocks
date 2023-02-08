9ab7969062614984fa936a90adb6790a595b7312
dirtysalt <dirtysalt1987@gmail.com>
2023-02-08 10:06:15 +0800

    [Feature] Add scan range assignment signature in debug mode  (#17325)
    Signed-off-by: yanz <dirtysalt1987@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/BackendSelector.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/BackendSelector.java
@@ -12,32 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 package com.starrocks.qe;
 
-import com.starrocks.thrift.TScanRangeParams;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public interface BackendSelector {
     void computeScanRangeAssignment() throws Exception;
 
-    static <K, V> V findOrInsert(HashMap<K, V> m, final K key, final V defaultVal) {
+    static <K, V> V findOrInsert(Map<K, V> m, final K key, final V defaultVal) {
         V value = m.get(key);
-        if (value == null) {
-            m.put(key, defaultVal);
-            value = defaultVal;
-        }
-        return value;
-    }
-
-    // weather we can overwrite the first parameter or not?
-    static List<TScanRangeParams> findOrInsert(Map<Integer, List<TScanRangeParams>> m, Integer key,
-                                               ArrayList<TScanRangeParams> defaultVal) {
-        List<TScanRangeParams> value = m.get(key);
         if (value == null) {
             m.put(key, defaultVal);
             value = defaultVal;
