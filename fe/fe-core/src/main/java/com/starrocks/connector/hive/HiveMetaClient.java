f2fb21fc1b0187b255f9321bfb605dbe96aa9081
stephen <91597003+stephen-shelby@users.noreply.github.com>
2023-02-09 11:09:33 +0800

    [BugFix] Remove the closed hiveclient from client pool (#17533)
    Currently, if a hive meta client that could access hive metastore need to be put into the client pool and reused. But if the connection has been closed, it will be also put back in the client pool. If user's hive metastore service ip keep changing, reconnecting the client using the original ip will cause time out. So we need to remove the closed hiveclient from client pool.
    
    (cherry picked from commit f5e41bf31c38d7a5c1b73f143feeac0e70f81694)

--- fe/fe-core/src/main/java/com/starrocks/connector/hive/HiveMetaClient.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/hive/HiveMetaClient.java
@@ -87,7 +87,8 @@ public class HiveMetaClient {
         @Override
         public void close() {
             synchronized (clientPoolLock) {
-                if (clientPool.size() >= MAX_HMS_CONNECTION_POOL_SIZE) {
+                if (clientPool.size() >= MAX_HMS_CONNECTION_POOL_SIZE || (hiveClient instanceof HiveMetaStoreThriftClient &&
+                        !((HiveMetaStoreThriftClient) hiveClient).isConnected())) {
                     hiveClient.close();
                 } else {
                     clientPool.offer(this);
