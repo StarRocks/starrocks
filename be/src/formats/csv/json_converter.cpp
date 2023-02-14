4d64c78ad054d15333c71469ebc53fb7a9024183
Murphy <mofei@starrocks.com>
2023-02-13 22:37:58 -0800

    resolve conflict
    Signed-off-by: Murphy <mofei@starrocks.com>

--- be/src/formats/csv/json_converter.cpp
+++ be/src/formats/csv/json_converter.cpp
@@ -1,16 +1,4 @@
-// Copyright 2021-present StarRocks, Inc. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
 
 #include "formats/csv/json_converter.h"
 
@@ -19,7 +7,7 @@
 #include "gutil/casts.h"
 #include "types/date_value.hpp"
 
-namespace starrocks::csv {
+namespace starrocks::vectorized::csv {
 
 Status JsonConverter::write_string(OutputStream* os, const Column& column, size_t row_num,
                                    const Options& options) const {
@@ -53,4 +41,4 @@ bool JsonConverter::read_quoted_string(Column* column, Slice s, const Options& o
     return read_string(column, s, options);
 }
 
-} // namespace starrocks::csv
+} // namespace starrocks::vectorized::csv
--- be/test/formats/csv/json_converter_test.cpp
+++ be/test/formats/csv/json_converter_test.cpp
@@ -1,16 +1,4 @@
-// Copyright 2021-present StarRocks, Inc. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
 
 #include <gtest/gtest.h>
 
@@ -21,7 +9,7 @@
 #include "runtime/types.h"
 #include "util/json.h"
 
-namespace starrocks::csv {
+namespace starrocks::vectorized::csv {
 
 class JsonConverterTest : public ::testing::Test {
 public:
@@ -94,4 +82,4 @@ TEST_F(JsonConverterTest, test_write_string) {
             buff2.as_string());
 }
 
-} // namespace starrocks::csv
+} // namespace starrocks::vectorized::csv
