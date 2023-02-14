b5eb93df2a5592fa1ee40414ab1f6c8d87340204
Fengan Li <f.li@celonis.com>
2023-02-13 19:05:53 -0600

    Address comments from Deng.

--- celonis/udfrepro/debug.sql
+++ celonis/udfrepro/debug.sql
@@ -5,6 +5,6 @@ RETURNS string
 properties (
     "symbol" = "udffailures.Identity",
     "type" = "StarrocksJar",
-    "file" = "http://127.0.0.1:7000/duplicate-invoice-checker-udf-1.0-SNAPSHOT.jar"
+    "file" = "http://127.0.0.1:7000/udf-failure-reproduce-1.0-SNAPSHOT.jar"
 );
 SELECT Identity("foo");
