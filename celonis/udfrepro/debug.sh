528509b32ced1476a88c19a88e2bd1e6ccdf9894
Deng Liu <dennisliu2005@gmail.com>
2023-02-14 11:57:17 -0800

    use different image with locally built dummy udf

--- celonis/udfrepro/debug.sh
+++ celonis/udfrepro/debug.sh
@@ -3,11 +3,18 @@ log_stdin()
     echo "[`date`] $@" >&1
 }
 
+#ubuntu_image="starrocks-allin1:udffailure"
+ubuntu_image="ghcr.io/celonis/celostar/starrocks-allin1:pr111-2.5-08db8f9a-celo-ubuntu"
+
+
 for VARIABLE in {1..100}
 do
   log_stdin Loop: $VARIABLE "============================="
 
-  docker run -it --name starrocks_allin1_reproduce --env STARTMODE=auto -p 8030:8030 -p 8040:8040 -p 9030:9030 -d starrocks-allin1:udffailure
+  docker run -it --name starrocks_allin1_reproduce --env STARTMODE=auto -p 8030:8030 -p 8040:8040 -p 9030:9030 -d $ubuntu_image
+
+  docker cp ../../java-extensions/udffailures/target/udf-failure-reproduce-1.0-SNAPSHOT-jar-with-dependencies.jar  starrocks_allin1_reproduce:/data/deploy/starrocks/udf/udf-failure-reproduce-1.0-SNAPSHOT.jar
+
   sleep 30
   mysql -uroot -h 127.0.0.1 -P9030 < debug.sql
 
