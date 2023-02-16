215c651bf90f879f969f928577dddde8e4548677
srlch <linzichao@starrocks.com>
2023-02-16 12:05:02 +0800

    [Enhancement] Change default value for download upload worker count (#17902)
    Sass has some requirements for the download/upload speed of backup and restore,
    Change default value for download upload worker count from 1 to 2 the requirements
    can be basically met without consuming too many resources.

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -133,9 +133,9 @@ CONF_Int32(storage_medium_migrate_count, "1");
 // The count of thread to check consistency.
 CONF_Int32(check_consistency_worker_count, "1");
 // The count of thread to upload.
-CONF_Int32(upload_worker_count, "1");
+CONF_Int32(upload_worker_count, "2");
 // The count of thread to download.
-CONF_Int32(download_worker_count, "1");
+CONF_Int32(download_worker_count, "2");
 // The count of thread to make snapshot.
 CONF_Int32(make_snapshot_worker_count, "5");
 // The count of thread to release snapshot.
