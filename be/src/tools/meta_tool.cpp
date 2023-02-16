9fdcd0380c9a99f9cfc253eaeff8642fb279af0d
trueeyu <lxhhust350@qq.com>
2023-02-16 16:26:08 +0800

    tmp

--- be/src/tools/meta_tool.cpp
+++ be/src/tools/meta_tool.cpp
@@ -32,24 +32,20 @@
 #include "gutil/strings/substitute.h"
 #include "json2pb/pb_to_json.h"
 #include "runtime/memory/chunk_allocator.h"
+#include "runtime/vectorized/time_types.h"
 #include "storage/fs/block_manager.h"
 #include "storage/fs/fs_util.h"
 #include "storage/key_coder.h"
 #include "storage/rowset/page_handle.h"
 #include "storage/rowset/segment.h"
-#include "storage/rowset/storage_page_decoder.h"
 #include "storage/rowset/vectorized/segment_options.h"
-#include "storage/short_key_index.h"
 #include "storage/tablet_meta.h"
+#include "storage/vectorized/chunk_helper.h"
 #include "util/block_compression.h"
 #include "util/coding.h"
 #include "util/crc32c.h"
-#include "storage/vectorized/chunk_helper.h"
-#include "runtime/vectorized/time_types.h"
 
 namespace starrocks {
-
-using VarcharDecode = KeyCoderTraits<OLAP_FIELD_TYPE_VARCHAR>;
 
 DEFINE_string(operation, "get_meta", "valid operation: flag");
 DEFINE_string(file, "", "segment file path");
@@ -57,11 +53,20 @@ DEFINE_string(file, "", "segment file path");
 std::string get_usage(const std::string& progname) {
     std::stringstream ss;
     ss << progname << " is the StarRocks BE Meta tool.\n";
-    ss << "Stop BE first before use this tool.\n";
     ss << "Usage:\n";
+
     ss << "./meta_tool --operation=show_segment_footer --file=/path/to/segment/file\n";
     ss << "./meta_tool --operation=dump_data --file=/path/to/segment/file\n";
     ss << "./meta_tool --operation=dump_data2 --file=/path/to/segment/file\n";
+
+    ss << "./meta_tool --operation=check_row_count --file=/path/to/segment/file\n";
+    ss << "./meta_tool --operation=check_short_key_index --file=/path/to/segment/file\n";
+    ss << "./meta_tool --operation=check_row_data --file=/path/to/segment/file\n";
+    ss << "./meta_tool --operation=check_page --file=/path/to/segment/file\n";
+    ss << "./meta_tool --operation=check_search --file=/path/to/segment/file\n";
+    ss << "./meta_tool --operation=check_varchar_length --file=/path/to/segment/file\n";
+    ss << "./meta_tool --operation=check_null --file=/path/to/segment/file\n";
+
     return ss.str();
 }
 
@@ -120,49 +125,6 @@ struct ReadFileOpt {
     int64_t size = 0;   // read size
 };
 
-Status read_page(ReadFileOpt* opt, Slice* body, PageFooterPB* footer, PageHandle* handle) {
-    // Read page
-    std::unique_ptr<char[]> page(new char[opt->size + vectorized::Column::APPEND_OVERFLOW_MAX_SIZE]);
-    Slice page_slice(page.get(), opt->size);
-    auto res = opt->file->read_at_fully(opt->offset, page.get(), opt->size);
-    if (!res.ok()) {
-        std::cout << "Read page failed: " << res << std::endl;
-        return res;
-    }
-
-    // Parse page footer size
-    page_slice.size -= 4;
-    uint32_t footer_size = decode_fixed32_le((uint8_t*)page.get() + page_slice.size - 4);
-
-    // Parse page footer
-    if (!footer->ParseFromArray(page.get() + page_slice.size - 4 - footer_size, static_cast<int>(footer_size))) {
-        return Status::Corruption("Parse page footer failed");
-    }
-
-    // Decompress page
-    uint32_t body_size = page_slice.size - 4 - footer_size;
-    if (body_size != footer->uncompressed_size()) {
-        std::unique_ptr<char[]> decompressed_page(
-                new char[footer->uncompressed_size() + footer_size + 4 + vectorized::Column::APPEND_OVERFLOW_MAX_SIZE]);
-
-        Slice compressed_body(page.get(), body_size);
-        Slice decompressed_body(decompressed_page.get(), footer->uncompressed_size());
-        RETURN_IF_ERROR(opt->codec->decompress(compressed_body, &decompressed_body));
-        if (decompressed_body.size != footer->uncompressed_size()) {
-            return Status::Corruption("decompress failed");
-        }
-        memcpy(decompressed_body.data + decompressed_body.size, page.get() + body_size, footer_size + 4);
-        page = std::move(decompressed_page);
-        page_slice = Slice(page.get(), footer->uncompressed_size() + footer_size + 4);
-    }
-
-    // Decode page
-    RETURN_IF_ERROR(StoragePageDecoder::decode_page(footer, footer_size + 4, opt->encoding_type, &page, &page_slice));
-    *body = Slice(page_slice.data, page_slice.size - 4 - footer_size);
-    *handle = PageHandle(page_slice);
-    return Status::OK();
-}
-
 void show_segment_footer(const std::string& file_name) {
     auto res = Env::Default()->new_random_access_file(file_name);
     if (!res.ok()) {
@@ -220,6 +182,114 @@ TabletSchemaPB create_tablet_schema() {
     return tablet_schema;
 }
 
+void check_row_count(const std::string& file_name) {
+    fs::BlockManager* blk_mgr = fs::fs_util::block_mgr_for_tool();
+    auto mem_tracker = std::make_unique<MemTracker>();
+
+    // create schema
+    TabletSchemaPB schema_pb = create_tablet_schema();
+    auto schema = TabletSchema::create(mem_tracker.get(), schema_pb);
+    auto read_schema = vectorized::ChunkHelper::convert_schema_to_format_v2(*schema);
+
+    // open segment
+    size_t footer_length_hint = 16 * 1024;
+    auto ret = Segment::open(mem_tracker.get(), blk_mgr, file_name, 0, schema.get(), &footer_length_hint, nullptr);
+    if (!ret.ok()) {
+        std::cout << "Segment open failed: " << ret.status() << std::endl;
+        return;
+    }
+    auto segment = ret.value();
+
+    // meta_row_count
+    int64_t meta_row_count = segment->num_rows();
+
+    // real_row_count
+    vectorized::SegmentReadOptions opts(blk_mgr);
+    OlapReaderStatistics stats;
+    opts.stats = &stats;
+    auto res = segment->new_iterator(read_schema, opts);
+    if (!res.ok()) {
+        std::cout << "New segment iterator failed: " << res.ok() << std::endl;
+        return;
+    }
+    auto iter = res.value();
+
+    // output data
+    int64_t data_row_count = 0;
+    do {
+        auto chunk = vectorized::ChunkHelper::new_chunk(read_schema, 0);
+        Status st = res.value()->get_next(chunk.get());
+        if (!st.ok()) {
+            if (!st.is_end_of_file()) {
+                std::cout << "get next chunk failed: " << st.to_string() << std::endl;
+            }
+            break;
+        }
+        if (chunk->num_rows() <= 0) {
+            continue;
+        }
+
+        data_row_count += chunk->num_rows();
+    } while (true);
+
+    std::cout << "meta_row_count=" << meta_row_count << std::endl;
+    std::cout << "data_row_count=" << data_row_count << std::endl;
+    if (meta_row_count != data_row_count) {
+        std::cout << "FAIL" << std::endl;
+    } else {
+        std::cout << "SUCCESS" << std::endl;
+    }
+}
+
+void check_short_key_index(const std::string& file_name) {
+    fs::BlockManager* blk_mgr = fs::fs_util::block_mgr_for_tool();
+    auto mem_tracker = std::make_unique<MemTracker>();
+
+    // create schema
+    TabletSchemaPB schema_pb = create_tablet_schema();
+    auto schema = TabletSchema::create(mem_tracker.get(), schema_pb);
+    auto read_schema = vectorized::ChunkHelper::convert_schema_to_format_v2(*schema);
+
+    // open segment
+    size_t footer_length_hint = 16 * 1024;
+    auto ret = Segment::open(mem_tracker.get(), blk_mgr, file_name, 0, schema.get(), &footer_length_hint, nullptr);
+    if (!ret.ok()) {
+        std::cout << "Segment open failed: " << ret.status() << std::endl;
+        return;
+    }
+    auto segment = ret.value();
+
+    // load short key index
+    auto st = segment->_load_index(mem_tracker.get());
+    if (!st.ok()) {
+        std::cout << "Load index failed: " << st.to_string() << std::endl;
+        return;
+    }
+
+    int64_t real_key_count = segment->_sk_index_decoder->num_items();
+    int64_t expect_key_count = segment->num_rows() / 1024 + 1;
+    std::cout << "REAL_SHORT_KEY_COUNT: " << segment->_sk_index_decoder->num_items() << std::endl;
+    std::cout << "EXPECT_SHORT_KEY_COUNT: " << expect_key_count << std::endl;
+    if (real_key_count == expect_key_count) {
+        std::cout << "SHORT KEY COUNT CHECK SUCCESS" << std::endl;
+    } else {
+        std::cout << "SHORT KEY COUNT CHECK FAILED" << std::endl;
+    }
+
+    size_t num_keys = segment->_sk_index_decoder->num_items();
+    for (size_t i = 1; i < num_keys; i++) {
+        Slice pre_key = segment->_sk_index_decoder->key(i - 1);
+        Slice cur_key = segment->_sk_index_decoder->key(i);
+
+        bool check = pre_key <= cur_key;
+        if (!check) {
+            std::cout << "CHECK SORT KEY FAILED: " << i << ":" << pre_key << ":" << cur_key << std::endl;
+            return;
+        }
+    }
+    std::cout << "CHECK SORT KEY SUCCESS" << std::endl;
+}
+
 void dump_data2(const std::string& file_name) {
     fs::BlockManager* blk_mgr = fs::fs_util::block_mgr_for_tool();
     auto mem_tracker = std::make_unique<MemTracker>();
@@ -262,7 +332,8 @@ void dump_data2(const std::string& file_name) {
             std::cout << "get next chunk failed: " << st.to_string() << std::endl;
 
             if (remain < pre_chunk->num_rows()) {
-                std::cout<<"CHUNK:"<<page_index<<":"<<pre_chunk->debug_row(pre_chunk->num_rows() - 1)<<std::endl;
+                std::cout << "CHUNK:" << page_index << ":" << pre_chunk->debug_row(pre_chunk->num_rows() - 1)
+                          << std::endl;
             }
             break;
         }
@@ -273,7 +344,7 @@ void dump_data2(const std::string& file_name) {
         int64_t tmp_index = remain;
         int64_t num_rows = cur_chunk->num_rows();
         while (tmp_index < num_rows) {
-            std::cout<<"CHUNK:"<<page_index<<":"<<cur_chunk->debug_row(tmp_index)<<std::endl;
+            std::cout << "CHUNK:" << page_index << ":" << cur_chunk->debug_row(tmp_index) << std::endl;
             tmp_index += 1024;
             page_index++;
         }
@@ -323,7 +394,11 @@ int meta_tool_main(int argc, char** argv) {
     gflags::SetUsageMessage(usage);
     google::ParseCommandLineFlags(&argc, &argv, true);
 
-    if (starrocks::FLAGS_operation == "dump_data") {
+    if (starrocks::FLAGS_operation == "check_row_count") {
+        starrocks::check_row_count(starrocks::FLAGS_file);
+    } else if (starrocks::FLAGS_operation == "check_short_key_index") {
+        starrocks::check_short_key_index(starrocks::FLAGS_file);
+    } else if (starrocks::FLAGS_operation == "dump_data") {
         starrocks::dump_data(starrocks::FLAGS_file);
     } else if (starrocks::FLAGS_operation == "dump_data2") {
         starrocks::dump_data2(starrocks::FLAGS_file);
