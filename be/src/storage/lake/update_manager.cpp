c70686b1c0c8390d5f186770bb9e1a21ecab5016
Yixin Luo <18810541851@163.com>
2023-02-18 03:15:42 +0800

    [Enhancement] avoid rebuild lake pk index when retry publish txn (#17988)
    * [Enhancement] avoid rebuild lake pk index when retry publish txn
    
    Signed-off-by: luohaha <18810541851@163.com>
    
    * update log print
    
    Signed-off-by: luohaha <18810541851@163.com>
    
    ---------
    
    Signed-off-by: luohaha <18810541851@163.com>

--- be/src/storage/lake/update_manager.cpp
+++ be/src/storage/lake/update_manager.cpp
@@ -486,10 +486,16 @@ Status UpdateManager::check_meta_version(const Tablet& tablet, int64_t base_vers
         return Status::OK();
     } else {
         auto& index = index_entry->value();
-        if (index.data_version() != base_version) {
+        if (index.data_version() > base_version) {
+            // return error, and ignore this publish later
+            LOG(INFO) << "Primary index version is greater than the base version. tablet_id: " << tablet.id()
+                      << " index_version: " << index.data_version() << " base_version: " << base_version;
+            return Status::AlreadyExist("lake primary publish txn already finish");
+        } else if (index.data_version() < base_version) {
             // clear cache, and continue publish
-            LOG(WARNING) << "Lake check_meta_version and remove primary index cache, tablet_id: " << tablet.id()
-                         << " index_ver: " << index.data_version() << " base_ver: " << base_version;
+            LOG(WARNING)
+                    << "Primary index version is less than the base version, remove primary index cache, tablet_id: "
+                    << tablet.id() << " index_version: " << index.data_version() << " base_version: " << base_version;
             if (!_index_cache.remove(index_entry)) {
                 return Status::InternalError("lake primary index cache ref mismatch");
             }
