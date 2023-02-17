35ec688c7c83f35a0b215ee435e0d49fdd70512e
srlch <111035020+srlch@users.noreply.github.com>
2023-02-17 14:11:07 +0800

    [Feature] Support new feature for backup/restore query and metric (#17526) (#17528)
    add some new feature for backup/restore:
    1. support show backup / show restore to show all databases' backup/restore info
    2. support show snapshot on repo where in (snapshotname) to show specified snapshot
    3. add starrocks_fe_unfinished_backup_job AND starrocks_fe_unfinished_restore_job to indicate the unfinished backup/restore job
    
    eg.
    MySQL [test1]> show backup;
    +-------+--------------+--------+----------+------------+---------------------+----------------------+---------------------+---------------------+-----------------+----------+------------+--------+---------+
    | JobId | SnapshotName | DbName | State    | BackupObjs | CreateTime          | SnapshotFinishedTime | UploadFinishedTime  | FinishedTime        | UnfinishedTasks | Progress | TaskErrMsg | Status | Timeout |
    +-------+--------------+--------+----------+------------+---------------------+----------------------+---------------------+---------------------+-----------------+----------+------------+--------+---------+
    | 10114 | mysnapshot   | test   | FINISHED | [test.t]   | 2023-02-17 08:54:52 | 2023-02-17 08:54:57  | 2023-02-17 08:55:03 | 2023-02-17 08:55:09 |                 |          |            | [OK]   | 86400   |
    | 10118 | mysnapshot1  | test1  | FINISHED | [test1.t]  | 2023-02-17 08:55:05 | 2023-02-17 08:55:09  | 2023-02-17 08:55:15 | 2023-02-17 08:55:21 |                 |          |            | [OK]   | 86400   |
    +-------+--------------+--------+----------+------------+---------------------+----------------------+---------------------+---------------------+-----------------+----------+------------+--------+---------+
    
    MySQL [test1]> show snapshot on test_repo where snapshot in (mysnapshot, mysnapshot1);
    +-------------+-------------------------+--------+
    | Snapshot    | Timestamp               | Status |
    +-------------+-------------------------+--------+
    | mysnapshot  | 2023-02-17-08-54-52-012 | OK     |
    | mysnapshot1 | 2023-02-17-08-55-05-164 | OK     |
    +-------------+-------------------------+--------+
    
    MySQL [test1]> show restore;
    +-------+-------------+-------------------------+--------+----------+-----------+----------------+-------------+---------------------+---------------------+----------------------+----------------------+---------------------+-----------------+----------+------------+--------+---------+
    | JobId | Label       | Timestamp               | DbName | State    | AllowLoad | ReplicationNum | RestoreObjs | CreateTime          | MetaPreparedTime    | SnapshotFinishedTime | DownloadFinishedTime | FinishedTime        | UnfinishedTasks | Progress | TaskErrMsg | Status | Timeout |
    +-------+-------------+-------------------------+--------+----------+-----------+----------------+-------------+---------------------+---------------------+----------------------+----------------------+---------------------+-----------------+----------+------------+--------+---------+
    | 10122 | mysnapshot  | 2023-02-17-08-54-52-012 | test   | FINISHED | false     | 1              | t           | 2023-02-17 08:57:28 | 2023-02-17 08:57:30 | 2023-02-17 08:57:33  | 2023-02-17 08:57:39  | 2023-02-17 08:57:45 |                 |          |            | [OK]   | 86400   |
    | 10140 | mysnapshot1 | 2023-02-17-08-55-05-164 | test1  | FINISHED | false     | 1              | t           | 2023-02-17 08:57:43 | 2023-02-17 08:57:45 | 2023-02-17 08:57:48  | 2023-02-17 08:57:54  | 2023-02-17 08:58:00 |                 |          |            | [OK]   | 86400   |
    +-------+-------------+-------------------------+--------+----------+-----------+----------------+-------------+---------------------+---------------------+----------------------+----------------------+---------------------+-----------------+----------+------------+--------+---------+
    
    Signed-off-by: srlch <linzichao@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
@@ -703,7 +703,6 @@ public class GlobalStateMgr {
         }
     }
 
-    @VisibleForTesting
     public ConcurrentHashMap<Long, Database> getIdToDb() {
         return localMetastore.getIdToDb();
     }
