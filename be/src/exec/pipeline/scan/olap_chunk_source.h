768a52b260cf99f5bb852c5d2f1618c1174ce5ba
kangkaisen <kangkaisen@apache.org>
2023-02-17 14:17:10 +0800

    [Enhancement] Change storage chunk size when no predicate
    Signed-off-by: kangkaisen <kangkaisen@apache.org>

--- be/src/exec/pipeline/scan/olap_chunk_source.h
+++ be/src/exec/pipeline/scan/olap_chunk_source.h
@@ -64,7 +64,7 @@ private:
     Status _read_chunk_from_storage([[maybe_unused]] RuntimeState* state, Chunk* chunk);
     void _update_counter();
     void _update_realtime_counter(Chunk* chunk);
-    void _decide_chunk_size();
+    void _decide_chunk_size(bool has_predicate);
 
 private:
     TabletReaderParams _params{};
