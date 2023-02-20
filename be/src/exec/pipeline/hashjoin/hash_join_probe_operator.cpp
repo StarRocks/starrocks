d49b5ef61d581a8bc65e6ebfd0157b3dd9ad7eb7
ZiheLiu <ziheliu1024@gmail.com>
2023-02-20 11:38:58 +0800

    test query_cache
    Signed-off-by: ZiheLiu <ziheliu1024@gmail.com>

--- be/src/exec/pipeline/hashjoin/hash_join_probe_operator.cpp
+++ be/src/exec/pipeline/hashjoin/hash_join_probe_operator.cpp
@@ -99,6 +99,10 @@ Status HashJoinProbeOperator::_reference_builder_hash_table_once() {
         return Status::OK();
     }
 
+    if (!is_ready()) {
+        return Status::OK();
+    }
+
     if (_join_prober->has_referenced_hash_table()) {
         return Status::OK();
     }
@@ -110,6 +114,7 @@ Status HashJoinProbeOperator::_reference_builder_hash_table_once() {
 }
 
 Status HashJoinProbeOperator::reset_state(RuntimeState* state, const vector<ChunkPtr>& refill_chunks) {
+    _reference_builder_hash_table_once();
     // Reset probe state only when it has valid state after referencing the build hash table.
     if (_join_prober->has_referenced_hash_table()) {
         _join_prober->reset_probe(state);
