647a765b765ab926f5d49f5e428ae9583355ca45
ZiheLiu <ziheliu1024@gmail.com>
2023-02-21 17:07:38 +0800

    fix code style
    Signed-off-by: ZiheLiu <ziheliu1024@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
@@ -162,7 +162,7 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
     public static final String ADAPTIVE_DOP_MAX_BLOCK_ROWS_PER_DRIVER_SEQ =
             "runtime_adaptive_dop_max_block_rows_per_driver_seq";
     public static final String ADAPTIVE_DOP_MAX_OUTPUT_AMPLIFICATION_FACTOR =
-        "runtime_adaptive_dop_max_output_amplification_factor";
+            "runtime_adaptive_dop_max_output_amplification_factor";
 
     public static final String ENABLE_PIPELINE_ENGINE = "enable_pipeline_engine";
     public static final String ENABLE_PIPELINE_QUERY_STATISTIC = "enable_pipeline_query_statistic";
