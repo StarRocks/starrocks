47a6b5213f597ce3104128e447d45478c194b678
sduzh <zhuming9011@qq.com>
2023-02-21 21:09:42 +0800

    [BugFix] Fix wrong result of left join query on lake table
    Fix #18166
    
    Signed-off-by: sduzh <zhuming9011@qq.com>

--- be/src/storage/lake/tablet_reader.cpp
+++ be/src/storage/lake/tablet_reader.cpp
@@ -131,10 +131,13 @@ Status TabletReader::get_segment_iterators(const TabletReaderParams& params, std
     rs_opts.tablet_schema = _tablet_schema.get();
     rs_opts.global_dictmaps = params.global_dictmaps;
     rs_opts.unused_output_column_ids = params.unused_output_column_ids;
+    rs_opts.runtime_range_pruner = params.runtime_range_pruner;
     if (keys_type == KeysType::PRIMARY_KEYS) {
         rs_opts.is_primary_keys = true;
         rs_opts.version = _version;
     }
+    rs_opts.rowid_range_option = params.rowid_range_option;
+    rs_opts.short_key_ranges = params.short_key_ranges;
 
     SCOPED_RAW_TIMER(&_stats.create_segment_iter_ns);
     for (auto& rowset : _rowsets) {
