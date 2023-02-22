3a4334964ffa87aa589d70ecd2fe1c0e705bcfb2
srlch <linzichao@starrocks.com>
2023-02-22 10:16:55 +0800

    [BugFix] bugfix_back_restore_new_feature (#18095)
    two problem introduce by commit f3212de1f0ab2bbee46c6db727a0b65a5a5852a8:
    1. SHOW BACKUP/SHOW RESTORE FROM clause is useless for filtering.
    2. unfinished counter metric is incorrect when restart fe.
    
    This commit fix this two problem:
    1. SHOW BACKUP/SHOW RESTORE FROM clause can be used as before.
    2. unfinished counter metric will be correct when restart fe.
    
    Signed-off-by: srlch <linzichao@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/ShowExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/ShowExecutor.java
@@ -1894,11 +1894,16 @@ public class ShowExecutor {
 
     private void handleShowBackup() throws AnalysisException {
         ShowBackupStmt showStmt = (ShowBackupStmt) stmt;
+        Database fiterDb = GlobalStateMgr.getCurrentState().getDb(showStmt.getDbName());
         List<List<String>> infos = Lists.newArrayList();
         List<Database> dbs = Lists.newArrayList();
 
-        for (Map.Entry<Long, Database> entry : GlobalStateMgr.getCurrentState().getIdToDb().entrySet()) {
-            dbs.add(entry.getValue());
+        if (fiterDb == null) {
+            for (Map.Entry<Long, Database> entry : GlobalStateMgr.getCurrentState().getIdToDb().entrySet()) {
+                dbs.add(entry.getValue());
+            }
+        } else {
+            dbs.add(fiterDb);
         }
 
         for (Database db : dbs) {
@@ -1935,11 +1940,16 @@ public class ShowExecutor {
 
     private void handleShowRestore() throws AnalysisException {
         ShowRestoreStmt showStmt = (ShowRestoreStmt) stmt;
+        Database fiterDb = GlobalStateMgr.getCurrentState().getDb(showStmt.getDbName());
         List<List<String>> infos = Lists.newArrayList();
         List<Database> dbs = Lists.newArrayList();
 
-        for (Map.Entry<Long, Database> entry : GlobalStateMgr.getCurrentState().getIdToDb().entrySet()) {
-            dbs.add(entry.getValue());
+        if (fiterDb == null) {
+            for (Map.Entry<Long, Database> entry : GlobalStateMgr.getCurrentState().getIdToDb().entrySet()) {
+                dbs.add(entry.getValue());
+            }
+        } else {
+            dbs.add(fiterDb);
         }
 
         for (Database db : dbs) {
