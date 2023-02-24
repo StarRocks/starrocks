ee652530346072a371cc4f1786d2954b53375d7a
wyb <wybb86@gmail.com>
2023-02-24 22:02:47 +0800

    [Feature] Add lake materialized view class (#18291)
    LakeMaterializedView extends MaterializedView and storage (cache) related operations are added in class like LakeTable.
    
    Signed-off-by: wyb <wybb86@gmail.com>
    (cherry picked from commit c5b7dc8dcad1c536cebce4489df8db7de8842c26)

--- fe/fe-core/src/main/java/com/starrocks/catalog/OlapTable.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/OlapTable.java
@@ -1560,7 +1560,7 @@ public class OlapTable extends Table {
                 partition.setState(PartitionState.NORMAL);
                 for (MaterializedIndex idx : partition.getMaterializedIndices(extState)) {
                     idx.setState(IndexState.NORMAL);
-                    if (copied.isLakeTable()) {
+                    if (copied.isCloudNativeTable()) {
                         continue;
                     }
                     for (Tablet tablet : idx.getTablets()) {
