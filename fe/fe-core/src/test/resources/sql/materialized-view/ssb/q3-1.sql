5e3c20dff3ac85fc64531af379c0847e9d9717f6
ABing <101158374+ABingHuang@users.noreply.github.com>
2023-02-24 16:53:46 +0800

    [Enhancement] add ssb mv rewrite test (#18360)

--- fe/fe-core/src/test/resources/sql/materialized-view/ssb/q3-1.sql
+++ fe/fe-core/src/test/resources/sql/materialized-view/ssb/q3-1.sql
@@ -25,4 +25,10 @@ ORDER BY
   d_year ASC, 
   revenue DESC;
 [result]
+TOP-N (order by [[37: d_year ASC NULLS FIRST, 50: sum DESC NULLS LAST]])
+    TOP-N (order by [[37: d_year ASC NULLS FIRST, 50: sum DESC NULLS LAST]])
+        AGGREGATE ([GLOBAL] aggregate [{50: sum=sum(50: sum)}] group by [[5: c_nation, 30: s_nation, 37: d_year]] having [null]
+            EXCHANGE SHUFFLE[5, 30, 37]
+                AGGREGATE ([LOCAL] aggregate [{50: sum=sum(21: lo_revenue)}] group by [[5: c_nation, 30: s_nation, 37: d_year]] having [null]
+                    SCAN (columns[63: LO_REVENUE, 71: C_NATION, 72: C_REGION, 78: S_NATION, 79: S_REGION, 92: d_year] predicate[92: d_year <= 1997 AND 92: d_year >= 1992 AND 72: C_REGION = ASIA AND 79: S_REGION = ASIA])
 [end]
