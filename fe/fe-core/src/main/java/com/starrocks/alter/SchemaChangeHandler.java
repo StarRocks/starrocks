c2c1e7ecaeb194a17ee9fd4328f1f247dcdfb6c3
Alex Zhu <zhuming9011@gmail.com>
2023-02-24 21:02:55 +0800

    [BugFix] Make DatabaseTransactionMgr.getMinActiveTxnId() thread safe (#18414)
    Signed-off-by: sduzh <zhuming9011@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/alter/SchemaChangeHandler.java
+++ fe/fe-core/src/main/java/com/starrocks/alter/SchemaChangeHandler.java
@@ -123,8 +123,6 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import javax.annotation.Nullable;
 
-import static java.lang.Math.min;
-
 public class SchemaChangeHandler extends AlterHandler {
     private static final Logger LOG = LogManager.getLogger(SchemaChangeHandler.class);
 
@@ -1127,20 +1125,17 @@ public class SchemaChangeHandler extends AlterHandler {
         getAlterJobV2Infos(db, ImmutableList.copyOf(alterJobsV2.values()), schemaChangeJobInfos);
     }
 
-    @Nullable
-    public Long getMinActiveTxnId() {
-        long result = Long.MAX_VALUE;
+    public Optional<Long> getMinActiveTxnId() {
+        long minId = Long.MAX_VALUE;
         Map<Long, AlterJobV2> alterJobV2Map = getAlterJobsV2();
         for (AlterJobV2 job : alterJobV2Map.values()) {
             AlterJobV2.JobState jobState = job.getJobState();
             if (jobState == AlterJobV2.JobState.FINISHED || jobState == AlterJobV2.JobState.CANCELLED) {
                 continue;
             }
-            if (job instanceof LakeTableSchemaChangeJob) {
-                result = min(result, ((LakeTableSchemaChangeJob) job).getWatershedTxnId());
-            }
+            minId = Math.min(minId, job.getTransactionId().orElse(Long.MAX_VALUE));
         }
-        return result == Long.MAX_VALUE ? null : result;
+        return minId == Long.MAX_VALUE ? Optional.empty() : Optional.of(minId);
     }
 
     @VisibleForTesting
