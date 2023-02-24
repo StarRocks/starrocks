StarRocks/starrocks

commit 2902eba529e8278b62b5a018d78101450a9d28db

[Refactor] Refactor RunMode for running UT easiser (#18470)
- Move RunMode out of GlobalStateMgr, make it's easier to mock the run mode in unit test.
- Support hybrid mode, for test purpose.

Signed-off-by: sduzh <zhuming9011@qq.com>

fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
@@ -118,7 +118,6 @@ import com.starrocks.common.FeConstants;
 import com.starrocks.common.FeMetaVersion;
 import com.starrocks.common.MetaNotFoundException;
 import com.starrocks.common.Pair;
-import com.starrocks.common.RunMode;
 import com.starrocks.common.ThreadPoolManager;
 import com.starrocks.common.UserException;
 import com.starrocks.common.io.Writable;
@@ -493,20 +492,6 @@ public class GlobalStateMgr {
 
     private ConfigRefreshDaemon configRefreshDaemon;
 
-    private RunMode runMode = RunMode.SHARED_NOTHING;
-
-    public boolean isSharedNothingMode() {
-        return runMode.equals(RunMode.SHARED_NOTHING);
-    }
-
-    public boolean isSharedDataMode() {
-        return runMode.equals(RunMode.SHARED_DATA);
-    }
-
-    public void setRunMode(RunMode runmode) {
-        this.runMode = runmode;
-    }
-
     public List<Frontend> getFrontends(FrontendNodeType nodeType) {
         return nodeMgr.getFrontends(nodeType);
     }
@@ -583,6 +568,9 @@ public class GlobalStateMgr {
 
     // if isCkptGlobalState is true, it means that we should not collect thread pool metric
     private GlobalStateMgr(boolean isCkptGlobalState) {
+        if (!isCkptGlobalState) {
+            RunMode.detectRunMode();
+        }
         this.load = new Load();
         this.streamLoadManager = new StreamLoadManager();
         this.routineLoadManager = new RoutineLoadManager();
@@ -664,17 +652,6 @@ public class GlobalStateMgr {
         this.pluginMgr = new PluginMgr();
         this.auditEventProcessor = new AuditEventProcessor(this.pluginMgr);
         this.analyzeManager = new AnalyzeManager();
-
-        if (Config.run_mode.equalsIgnoreCase(RunMode.SHARED_DATA.name())) {
-            runMode = RunMode.SHARED_DATA;
-            this.starOSAgent = new StarOSAgent();
-        } else if (Config.run_mode.equalsIgnoreCase(RunMode.SHARED_NOTHING.name())) {
-            runMode = RunMode.SHARED_NOTHING;
-        } else {
-            LOG.error("Invalid run_mode config: {}, should be shared_data or shared_nothing", Config.run_mode);
-            System.exit(-1);
-        }
-
         this.localMetastore = new LocalMetastore(this, recycleBin, colocateTableIndex, nodeMgr.getClusterInfo());
         this.warehouseMgr = new WarehouseManager();
         this.connectorMgr = new ConnectorMgr();
@@ -994,7 +971,7 @@ public class GlobalStateMgr {
         createTaskCleaner();
 
         // 7. init starosAgent
-        if (isSharedDataMode() && !starOSAgent.init(null)) {
+        if (RunMode.getCurrentRunMode().isAllowCreateLakeTable() && !starOSAgent.init(null)) {
             LOG.error("init starOSAgent failed");
             System.exit(-1);
         }
@@ -1193,7 +1170,7 @@ public class GlobalStateMgr {
 
     // start all daemon threads only running on Master
     private void startLeaderOnlyDaemonThreads() {
-        if (isSharedDataMode()) {
+        if (RunMode.getCurrentRunMode().isAllowCreateLakeTable()) {
             // register service to starMgr
             if (!getStarOSAgent().registerAndBootstrapService()) {
                 System.exit(-1);
@@ -1260,7 +1237,7 @@ public class GlobalStateMgr {
         taskRunStateSynchronizer = new TaskRunStateSynchronizer();
         taskRunStateSynchronizer.start();
 
-        if (isSharedDataMode()) {
+        if (RunMode.getCurrentRunMode().isAllowCreateLakeTable()) {
             shardDeleter.start();
         }
     }
@@ -1284,7 +1261,7 @@ public class GlobalStateMgr {
 
         // domain resolver
         domainResolver.start();
-        if (isSharedDataMode()) {
+        if (RunMode.getCurrentRunMode().isAllowCreateLakeTable()) {
             compactionManager.start();
         }
         configRefreshDaemon.start();

fe/fe-core/src/main/java/com/starrocks/StarRocksFE.java
@@ -53,6 +53,7 @@ import com.starrocks.leader.MetaHelper;
 import com.starrocks.qe.CoordinatorMonitor;
 import com.starrocks.qe.QeService;
 import com.starrocks.server.GlobalStateMgr;
+import com.starrocks.server.RunMode;
 import com.starrocks.service.ExecuteEnv;
 import com.starrocks.service.FeServer;
 import com.starrocks.service.FrontendOptions;
@@ -131,7 +132,7 @@ public class StarRocksFE {
             StateChangeExecutor.getInstance().setMetaContext(
                     GlobalStateMgr.getCurrentState().getMetaContext());
 
-            if (GlobalStateMgr.getCurrentState().isSharedDataMode()) {
+            if (RunMode.getCurrentRunMode().isAllowCreateLakeTable()) {
                 Journal journal = GlobalStateMgr.getCurrentState().getJournal();
                 if (journal instanceof BDBJEJournal) {
                     BDBEnvironment bdbEnvironment = ((BDBJEJournal) journal).getBdbEnvironment();

fe/fe-core/src/main/java/com/starrocks/catalog/ColocateTableIndex.java
@@ -51,6 +51,7 @@ import com.starrocks.lake.LakeTable;
 import com.starrocks.persist.ColocatePersistInfo;
 import com.starrocks.persist.TablePropertyInfo;
 import com.starrocks.server.GlobalStateMgr;
+import com.starrocks.server.RunMode;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -1009,7 +1010,7 @@ public class ColocateTableIndex implements Writable {
     }
 
     private void constructLakeGroups(GlobalStateMgr globalStateMgr) {
-        if (!GlobalStateMgr.getCurrentState().isSharedDataMode()) {
+        if (!RunMode.getCurrentRunMode().isAllowCreateLakeTable()) {
             return;
         }
 

fe/fe-core/src/main/java/com/starrocks/common/Config.java
@@ -1781,7 +1781,7 @@ public class Config extends ConfigBase {
     /**
      * shared_data: means run on cloud-native
      * shared_nothing: means run on local
-     * hybrid: run on both, not support yet.
+     * hybrid: run on both, not production ready, should only be used in test environment now.
      */
     @ConfField
     public static String run_mode = "shared_nothing";

fe/fe-core/src/main/java/com/starrocks/common/RunMode.java
@@ -1,20 +1,0 @@
-// Copyright 2021-present StarRocks, Inc. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package com.starrocks.common;
-
-public enum RunMode {
-    SHARED_DATA,
-    SHARED_NOTHING;
-}

fe/fe-core/src/main/java/com/starrocks/common/proc/BackendsProcDir.java
@@ -47,6 +47,7 @@ import com.starrocks.common.util.DebugUtil;
 import com.starrocks.common.util.ListComparator;
 import com.starrocks.common.util.TimeUtils;
 import com.starrocks.server.GlobalStateMgr;
+import com.starrocks.server.RunMode;
 import com.starrocks.system.Backend;
 import com.starrocks.system.BackendCoreStat;
 import com.starrocks.system.SystemInfoService;
@@ -70,7 +71,7 @@ public class BackendsProcDir implements ProcDirInterface {
                 .add("DataUsedCapacity").add("AvailCapacity").add("TotalCapacity").add("UsedPct")
                 .add("MaxDiskUsedPct").add("ErrMsg").add("Version").add("Status").add("DataTotalCapacity")
                 .add("DataUsedPct").add("CpuCores").add("NumRunningQueries").add("MemUsedPct").add("CpuUsedPct");
-        if (GlobalStateMgr.getCurrentState().isSharedDataMode()) {
+        if (RunMode.getCurrentRunMode().isAllowCreateLakeTable()) {
             builder.add("StarletPort").add("WorkerId");
         }
         TITLE_NAMES = builder.build();
@@ -193,7 +194,7 @@ public class BackendsProcDir implements ProcDirInterface {
             backendInfo.add(String.format("%.2f", memUsedPct * 100) + " %");
             backendInfo.add(String.format("%.1f", backend.getCpuUsedPermille() / 10.0) + " %");
 
-            if (GlobalStateMgr.getCurrentState().isSharedDataMode()) {
+            if (RunMode.getCurrentRunMode().isAllowCreateLakeTable()) {
                 backendInfo.add(String.valueOf(backend.getStarletPort()));
                 long workerId = GlobalStateMgr.getCurrentState().getStarOSAgent().getWorkerIdByBackendId(backendId);
                 backendInfo.add(String.valueOf(workerId));

fe/fe-core/src/main/java/com/starrocks/common/util/PropertyAnalyzer.java
@@ -61,6 +61,7 @@ import com.starrocks.common.Config;
 import com.starrocks.common.Pair;
 import com.starrocks.qe.ConnectContext;
 import com.starrocks.server.GlobalStateMgr;
+import com.starrocks.server.RunMode;
 import com.starrocks.sql.analyzer.AnalyzerUtils;
 import com.starrocks.thrift.TCompressionType;
 import com.starrocks.thrift.TStorageFormat;
@@ -385,7 +386,7 @@ public class PropertyAnalyzer {
         // compute-storage-separation table will be created and in this case the replication_num will
         // be ignored, so there is no need to check whether the number of alive nodes is greater than the
         // replication_num.
-        if (GlobalStateMgr.getCurrentState().isSharedNothingMode()) {
+        if (!RunMode.getCurrentRunMode().isIgnoreReplicationNum()) {
             List<Long> backendIds = GlobalStateMgr.getCurrentSystemInfo().getAvailableBackendIds();
             if (replicationNum > backendIds.size()) {
                 throw new AnalysisException("Replication num should be less than the number of available BE nodes. "

fe/fe-core/src/main/java/com/starrocks/server/NodeMgr.java
@@ -361,7 +361,7 @@ public class NodeMgr {
                 }
             }
             getNewImageOnStartup(rightHelperNode, "");
-            if (GlobalStateMgr.getCurrentState().isSharedDataMode()) { // get star mgr image
+            if (RunMode.getCurrentRunMode().isAllowCreateLakeTable()) { // get star mgr image
                 // subdir might not exist
                 String subDir = this.imageDir + StarMgrServer.IMAGE_SUBDIR;
                 File dir = new File(subDir);

commit 7e7f9ad08df0e9f409c244f83987ec16b01f33cb

[BugFix] Fix the issue that StarOSAgent not initialized
Introduced by #18470

Signed-off-by: sduzh <zhuming9011@qq.com>

fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
@@ -571,6 +571,9 @@ public class GlobalStateMgr {
         if (!isCkptGlobalState) {
             RunMode.detectRunMode();
         }
+        if (RunMode.getCurrentRunMode().isAllowCreateLakeTable()) {
+            this.starOSAgent = new StarOSAgent();
+        }
         this.load = new Load();
         this.streamLoadManager = new StreamLoadManager();
         this.routineLoadManager = new RoutineLoadManager();