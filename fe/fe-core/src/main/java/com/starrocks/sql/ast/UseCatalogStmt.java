349b29265ec0796fa950eb7fcf1ea62a0a8d27d6
Letian Jiang <letian.jiang@outlook.com>
2023-02-27 18:20:14 +0800

    [BugFix] Fix mysql client unable to change current catalog (backport #18266) (#18568)
    * [BugFix] Fix mysql client unable to change current catalog (#18266)
    
    USE 'CATALOG catalog_name' issued by mysql client will be dispatched to handleQuery function. In this case, the current implementation throws parse exception, thus fails to switch the catalog in use.
    
    The changes made are:
    
    change useCatalogStatement : USE CATALOG identifierOrString  to useCatalogStatement : USE string;
    add or update parser/analyzer/executor for useCatalogStatement
    update unit tests
    Limitation:
    Mysql client of version 5.7.31 would raise a benign error as follows.
    
    ERROR:
    USE must be followed by a database name
    But the catalog can be changed successfully.
    
    Signed-off-by: Letian Jiang <letian.jiang@outlook.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/ast/UseCatalogStmt.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/ast/UseCatalogStmt.java
@@ -4,15 +4,41 @@ package com.starrocks.sql.ast;
 
 import com.starrocks.analysis.RedirectStatus;
 
-public class UseCatalogStmt extends StatementBase {
-    private final String catalogName;
+/*
+  Use catalog specified by catalog name
 
-    public UseCatalogStmt(String catalogName) {
-        this.catalogName = catalogName;
+  syntax:
+      USE 'CATALOG catalog_name'
+      USE "CATALOG catalog_name"
+
+      Note:
+        A pair of single/double quotes are required
+
+      Examples:
+        USE 'CATALOG default_catalog'
+        use "catalog default_catalog"
+        USE 'catalog hive_metastore_catalog'
+        use "CATALOG hive_metastore_catalog"
+ */
+public class UseCatalogStmt extends StatementBase {
+    private final String catalogParts;
+
+    private String catalogName;
+
+    public UseCatalogStmt(String catalogParts) {
+        this.catalogParts = catalogParts;
+    }
+
+    public String getCatalogParts() {
+        return catalogParts;
     }
 
     public String getCatalogName() {
         return catalogName;
+    }
+
+    public void setCatalogName(String catalogName) {
+        this.catalogName = catalogName;
     }
 
     @Override
