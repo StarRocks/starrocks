910e0f258a5a4296fe1b4767c3863aa067a138cd
Zijie Lu <wslzj40@gmail.com>
2023-02-28 14:57:34 +0800

    [Enhancement] avoid duplicated local shuffle of lake table
    Signed-off-by: Zijie Lu <wslzj40@gmail.com>

--- be/src/connector/connector.h
+++ be/src/connector/connector.h
@@ -110,6 +110,13 @@ public:
     virtual bool accept_empty_scan_ranges() const { return true; }
 
     virtual bool stream_data_source() const { return false; }
+
+    virtual Status init(ObjectPool* pool, RuntimeState* state) { return Status::OK(); }
+
+    const std::vector<ExprContext*>& bucket_exprs() const { return _bucket_exprs; }
+
+protected:
+    std::vector<ExprContext*> _bucket_exprs;
 };
 using DataSourceProviderPtr = std::unique_ptr<DataSourceProvider>;
 
