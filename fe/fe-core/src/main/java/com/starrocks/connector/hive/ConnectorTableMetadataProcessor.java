818b34881aec3e00e37a733d75c6f7669e604e47
Youngwb <yangwenbo_mailbox@163.com>
2023-02-28 10:08:03 +0800

    [Enhancement] Support periodic refresh registered external table meta (#18539)
    Support periodic refresh registered external table meta
    Signed-off-by: Youngwb <yangwenbo_mailbox@163.com>

--- fe/fe-core/src/main/java/com/starrocks/connector/hive/ConnectorTableMetadataProcessor.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/hive/ConnectorTableMetadataProcessor.java
@@ -14,6 +14,8 @@
 
 package com.starrocks.connector.hive;
 
+import com.google.common.collect.Sets;
+import com.starrocks.catalog.BaseTableInfo;
 import com.starrocks.catalog.Database;
 import com.starrocks.catalog.HiveTable;
 import com.starrocks.catalog.Table;
@@ -27,17 +29,62 @@ import org.spark_project.guava.collect.Lists;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
-public class BackgroundHiveMetadataProcessor extends LeaderDaemon {
-    private static final Logger LOG = LogManager.getLogger(BackgroundHiveMetadataProcessor.class);
+public class ConnectorTableMetadataProcessor extends LeaderDaemon {
+    private static final Logger LOG = LogManager.getLogger(ConnectorTableMetadataProcessor.class);
 
-    public BackgroundHiveMetadataProcessor() {
-        super(BackgroundHiveMetadataProcessor.class.getName(), Config.background_refresh_hive_metadata_interval_millis);
+    private final Set<BaseTableInfo> registeredTableInfos = Sets.newConcurrentHashSet();
+
+    public void registerTableInfo(BaseTableInfo tableInfo) {
+        registeredTableInfos.add(tableInfo);
+    }
+
+    public ConnectorTableMetadataProcessor() {
+        super(ConnectorTableMetadataProcessor.class.getName(), Config.background_refresh_metadata_interval_millis);
     }
 
     @Override
     protected void runAfterCatalogReady() {
+        if (!Config.enable_hms_events_incremental_sync && Config.enable_background_refresh_hive_metadata) {
+            refreshResourceHiveTable();
+        }
+
+        refreshRegisteredTable();
+    }
+
+    private void refreshRegisteredTable() {
+        MetadataMgr metadataMgr = GlobalStateMgr.getCurrentState().getMetadataMgr();
+        List<BaseTableInfo> registeredTableInfoList = Lists.newArrayList(registeredTableInfos);
+        for (BaseTableInfo registeredTableInfo : registeredTableInfoList) {
+            LOG.info("Start to refresh registered table {}.{}.{} metadata in the background",
+                    registeredTableInfo.getCatalogName(), registeredTableInfo.getDbName(),
+                    registeredTableInfo.getTableName());
+            try {
+                Table registeredTable = registeredTableInfo.getTable();
+                if (registeredTable == null) {
+                    LOG.warn("Table {}.{}.{} not exist",  registeredTableInfo.getCatalogName(),
+                            registeredTableInfo.getDbName(), registeredTableInfo.getTableName());
+                    continue;
+                }
+                if (!registeredTable.isHiveTable()) {
+                    continue;
+                }
+                metadataMgr.refreshTable(registeredTableInfo.getCatalogName(), registeredTableInfo.getDbName(),
+                        registeredTable, Lists.newArrayList(), false);
+
+            } catch (Exception e) {
+                LOG.error("Background refresh table metadata failed on {}.{}.{}", registeredTableInfo.getCatalogName(),
+                        registeredTableInfo.getDbName(), registeredTableInfo.getTableName(), e);
+            }
+            LOG.info("Refresh registered table {}.{}.{} metadata success",
+                    registeredTableInfo.getCatalogName(), registeredTableInfo.getDbName(),
+                    registeredTableInfo.getTableName());
+        }
+    }
+
+    private void refreshResourceHiveTable() {
         LOG.info("Start to refresh hive external table metadata in the background");
         GlobalStateMgr gsm = GlobalStateMgr.getCurrentState();
         MetadataMgr metadataMgr = gsm.getMetadataMgr();
@@ -46,7 +93,6 @@ public class BackgroundHiveMetadataProcessor extends LeaderDaemon {
                 .filter(Objects::nonNull)
                 .filter(db -> !db.isInfoSchemaDb())
                 .collect(Collectors.toList());
-
         for (Database db : databases) {
             List<HiveTable> tables = db.getTables().stream()
                     .filter(tbl -> tbl.getType() == Table.TableType.HIVE)
