95ba3c0a7141049b95a41c4ee6ec37d9400c30ab
dirtysalt <dirtysalt1987@gmail.com>
2023-02-28 11:20:24 -0800

    [Enhancement] add block cache stats (#18510)
    (cherry picked from commit 0a51466fe54a43d3fa3f7a330d57547bffc470b4)

--- be/src/block_cache/kv_cache.h
+++ be/src/block_cache/kv_cache.h
@@ -36,6 +36,8 @@ public:
     // Remove data from cache. The offset must be aligned by block size
     virtual Status remove_cache(const std::string& key) = 0;
 
+    virtual std::unordered_map<std::string, double> cache_stats() = 0;
+
     virtual Status shutdown() = 0;
 };
 
