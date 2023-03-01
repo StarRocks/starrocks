c43f183415a0f96b0a13afdf4df0472cc6d84e71
Zijie Lu <wslzj40@gmail.com>
2023-03-01 18:54:12 +0800

    [Enhancement] avoid duplicated local shuffle of lake table (#18630)
    * [Enhancement] avoid duplicated local shuffle of lake table
    
    Signed-off-by: Zijie Lu <wslzj40@gmail.com>
    (cherry picked from commit d300c7ebb0c982b920dade48486941b02b9b377f)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/planner/OlapScanNode.java

--- fe/fe-core/src/main/java/com/starrocks/planner/OlapScanNode.java
+++ fe/fe-core/src/main/java/com/starrocks/planner/OlapScanNode.java
@@ -772,7 +772,15 @@ public class OlapScanNode extends ScanNode {
             if (!olapTable.hasDelete()) {
                 msg.lake_scan_node.setUnused_output_column_name(unUsedOutputStringColumns);
             }
+<<<<<<< HEAD
         } else {
+=======
+
+            if (!bucketExprs.isEmpty()) {
+                msg.lake_scan_node.setBucket_exprs(Expr.treesToThrift(bucketExprs));
+            }
+        } else { // If you find yourself changing this code block, see also the above code block, i.e, if (olapTable.isLakeTable) { ... }.
+>>>>>>> d300c7ebb ([Enhancement] avoid duplicated local shuffle of lake table (#18630))
             msg.node_type = TPlanNodeType.OLAP_SCAN_NODE;
             msg.olap_scan_node =
                     new TOlapScanNode(desc.getId().asInt(), keyColumnNames, keyColumnTypes, isPreAggregation);
