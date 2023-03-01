b90626ae0b342540929956ec9308a3efd2514e7c
HangyuanLiu <460660596@qq.com>
2023-03-01 14:51:51 +0800

    Fix the BUG that did not check the resource permission when CreateTable (#18581)
    Fix the BUG that did not check the resource permission when CreateTable 
    Signed-off-by: HangyuanLiu <460660596@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/CreateTableLikeAnalyzer.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/CreateTableLikeAnalyzer.java
@@ -15,23 +15,55 @@
 
 package com.starrocks.sql.analyzer;
 
+import com.google.common.collect.Lists;
+import com.starrocks.catalog.Database;
+import com.starrocks.catalog.Table;
 import com.starrocks.common.AnalysisException;
 import com.starrocks.common.ErrorCode;
 import com.starrocks.common.ErrorReport;
 import com.starrocks.qe.ConnectContext;
+import com.starrocks.server.GlobalStateMgr;
 import com.starrocks.sql.ast.CreateTableLikeStmt;
+import com.starrocks.sql.ast.CreateTableStmt;
+import com.starrocks.sql.ast.StatementBase;
 import com.starrocks.sql.common.MetaUtils;
+
+import java.util.List;
 
 public class CreateTableLikeAnalyzer {
 
-    public static void analyze(CreateTableLikeStmt statement, ConnectContext context) {
-        MetaUtils.normalizationTableName(context, statement.getDbTbl());
-        MetaUtils.normalizationTableName(context, statement.getExistedDbTbl());
-        String tableName = statement.getTableName();
+    public static void analyze(CreateTableLikeStmt stmt, ConnectContext context) {
+        MetaUtils.normalizationTableName(context, stmt.getDbTbl());
+        MetaUtils.normalizationTableName(context, stmt.getExistedDbTbl());
+        String tableName = stmt.getTableName();
         try {
             FeNameFormat.checkTableName(tableName);
         } catch (AnalysisException e) {
             ErrorReport.reportSemanticException(ErrorCode.ERR_WRONG_TABLE_NAME, tableName);
+        }
+
+        Database db = MetaUtils.getDatabase(context, stmt.getExistedDbTbl());
+        Table table = MetaUtils.getTable(stmt.getExistedDbTbl());
+
+        List<String> createTableStmt = Lists.newArrayList();
+        GlobalStateMgr.getDdlStmt(stmt.getDbName(), table, createTableStmt, null, null, false, false);
+        if (createTableStmt.isEmpty()) {
+            ErrorReport.reportSemanticException(ErrorCode.ERROR_CREATE_TABLE_LIKE_EMPTY, "CREATE");
+        }
+
+        StatementBase statementBase = com.starrocks.sql.parser.SqlParser.parseFirstStatement(createTableStmt.get(0),
+                context.getSessionVariable().getSqlMode());
+        com.starrocks.sql.analyzer.Analyzer.analyze(statementBase, context);
+        if (statementBase instanceof CreateTableStmt) {
+            CreateTableStmt parsedCreateTableStmt = (CreateTableStmt) statementBase;
+            parsedCreateTableStmt.setTableName(stmt.getTableName());
+            if (stmt.isSetIfNotExists()) {
+                parsedCreateTableStmt.setIfNotExists();
+            }
+
+            stmt.setCreateTableStmt(parsedCreateTableStmt);
+        } else {
+            ErrorReport.reportSemanticException(ErrorCode.ERROR_CREATE_TABLE_LIKE_UNSUPPORTED_VIEW);
         }
     }
 }
--- fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzeCreateTableLikeTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzeCreateTableLikeTest.java
@@ -16,46 +16,37 @@
 package com.starrocks.sql.analyzer;
 
 import com.starrocks.sql.ast.CreateTableLikeStmt;
-import com.starrocks.utframe.StarRocksAssert;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static com.starrocks.sql.analyzer.AnalyzeTestUtil.analyzeSuccess;
-import static com.starrocks.sql.analyzer.AnalyzeTestUtil.getStarRocksAssert;
 
 public class AnalyzeCreateTableLikeTest {
-    private static StarRocksAssert starRocksAssert;
-
     @BeforeClass
     public static void beforeClass() throws Exception {
         AnalyzeTestUtil.init();
-
-        starRocksAssert = getStarRocksAssert();
-
-        String createTblStmtStr = "create table db.tbl1(kk1 int, kk2 varchar(32), kk3 int, kk4 int) "
-                + "AGGREGATE KEY(kk1, kk2,kk3,kk4) distributed by hash(kk1) buckets 3 properties('replication_num' = "
-                + "'1');";
-        starRocksAssert = new StarRocksAssert();
-        starRocksAssert.withDatabase("db").useDatabase("db");
-        starRocksAssert.withTable(createTblStmtStr);
     }
 
     @Test
     public void testNormal() {
         CreateTableLikeStmt stmt = (CreateTableLikeStmt) analyzeSuccess(
-                "CREATE TABLE tbl2 LIKE tbl1");
+                "CREATE TABLE tbl2 LIKE t0");
         Assert.assertEquals("test", stmt.getDbName());
         Assert.assertEquals("tbl2", stmt.getTableName());
         Assert.assertEquals("test", stmt.getExistedDbName());
-        Assert.assertEquals("tbl1", stmt.getExistedTableName());
+        Assert.assertEquals("t0", stmt.getExistedTableName());
     }
 
     @Test
     public void testAnalyzeSuccess() {
-        analyzeSuccess("CREATE TABLE test1.table2 LIKE test1.table1;");
-        analyzeSuccess("CREATE TABLE test2.table2 LIKE test1.table1;");
-        analyzeSuccess("CREATE TABLE table2 LIKE table1;");
+        analyzeSuccess("CREATE TABLE test1.table2 LIKE db1.t0;");
+        analyzeSuccess("CREATE TABLE test1.table2 LIKE db2.t0;");
+        analyzeSuccess("CREATE TABLE table2 LIKE t0;");
     }
 
+    @Test
+    public void testCreateTableLike() throws Exception {
+        analyzeSuccess("CREATE EXTERNAL TABLE hive_test_like LIKE db1.t0");
+    }
 }
