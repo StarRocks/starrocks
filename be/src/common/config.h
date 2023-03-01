eb24e5d441acf6d4b5167ef240d7ca8892cc87ea
stdpain <drfeng08@gmail.com>
2023-03-01 15:53:56 +0800

    support skip sync spill files
    Signed-off-by: stdpain <drfeng08@gmail.com>

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -436,12 +436,6 @@ CONF_mBool(enable_prefetch, "true");
 // Otherwise, StarRocks will use all cores returned from "/proc/cpuinfo".
 CONF_Int32(num_cores, "0");
 
-// CONF_Bool(thread_creation_fault_injection, "false");
-
-// Set this to encrypt and perform an integrity
-// check on all data spilled to disk during a query
-// CONF_Bool(disk_spill_encryption, "false");
-
 // When BE start, If there is a broken disk, BE process will exit by default.
 // Otherwise, we will ignore the broken disk,
 CONF_Bool(ignore_broken_disk, "false");
@@ -832,6 +826,7 @@ CONF_Int32(jdbc_connection_idle_timeout_ms, "600000");
 
 // spill dirs
 CONF_String(spill_local_storage_dir, "spill");
+CONF_mBool(experimental_spill_skip_sync, "false");
 
 // Now, only get_info is processed by _async_thread_pool, and only needs a small number of threads.
 // The default value is set as the THREAD_POOL_SIZE of RoutineLoadTaskScheduler of FE.
