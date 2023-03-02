StarRocks/starrocks

commit 859129b35c26345fc2df22314f6fbc7f6159a690

[Enhancement] Flush persistent index in advance to reduce memory usage during data load of primary key (#15729)
StarRocks supports real-time scenarios with PrimaryKey table, but the primary index of PrimaryKey table maybe cost a lot of memory, so we developed persistent index. PrimaryKey with persistent index will not use a lot of memory even if the data of table is very large. However, persistent index may alloc a large memory first if the load is very large which maybe cause apply failed.

This pr aims to resolve the large memory need during a large load of PrimaryKey table with persistent index.  If the memory usage of persistent index is too large, we will flush it to a tmp_l1 and clear the memory until all data is written into persistent index and we will merge all the tmp_l1 to a new l1 at last.

In my test env, one BE with one HDD, using BrokerLoad, create a table with 7 columns, 20 buckets:
```
CREATE TABLE `batch_flush_test` (
  `col_1` bigint(20) NOT NULL COMMENT "",
  `col_2` bigint(20) NOT NULL COMMENT "",
  `col_3` bigint(20) NOT NULL COMMENT "",
  `col_4` bigint(20) NOT NULL COMMENT "",
  `col_5` bigint(20) NOT NULL COMMENT "",
  `col_6` varchar(150) NULL COMMENT "",
  `col_7` varchar(150) NULL COMMENT ""
) ENGINE=OLAP 
PRIMARY KEY(`col_1`, `col_2`, `col_3`, `col_4`, `col_5`)
COMMENT "OLAP"
DISTRIBUTED BY HASH(`col_1`, `col_2`) BUCKETS 20 
PROPERTIES (
"replication_num" = "1",
"in_memory" = "false",
"storage_format" = "V2",
"enable_persistent_index" = "true",
"compression" = "LZ4"
);
```
|PrimaryKey Length| RowNum|BucketNum| Column Num|  Load time(s)| Apply time(ms)| Peak UpdateMemory usage | Note |
|---------------------|-----------|--------------------|------------------------------|----|-----|-----|----|
|40 Bytes| 1Billion | 20 |  7 |  3514| 566379| 63.3G | branch-main |
|40 Bytes| 1Billion | 20 |  7 | 3334| 585724| 2.06G| branch-opt |

be/src/storage/persistent_index.h
@@ -60,9 +60,15 @@ static_assert(sizeof(IndexValue) == kIndexValueSize);
 uint64_t key_index_hash(const void* data, size_t len);
 
 struct KeysInfo {
-    std::vector<uint32_t> key_idxes;
-    std::vector<uint64_t> hashes;
-    size_t size() const { return key_idxes.size(); }
+    std::vector<std::pair<uint32_t, uint64_t>> key_infos;
+    size_t size() const { return key_infos.size(); }
+
+    void set_difference(KeysInfo& input) {
+        std::vector<std::pair<uint32_t, uint64_t>> infos;
+        std::set_difference(key_infos.begin(), key_infos.end(), input.key_infos.begin(), input.key_infos.end(),
+                            std::back_inserter(infos), [](auto& a, auto& b) { return a.first < b.first; });
+        key_infos.swap(infos);
+    }
 };
 
 struct KVRef {
@@ -171,11 +177,9 @@ public:
 
     virtual void reserve(size_t size) = 0;
 
+    virtual void clear() = 0;
+
     virtual size_t memory_usage() = 0;
-
-    virtual void update_overlap_info(size_t overlap_size, size_t overlap_usage) = 0;
-
-    virtual size_t overlap_size() = 0;
 
     static StatusOr<std::unique_ptr<MutableIndex>> create(size_t key_size);
 
@@ -285,7 +289,7 @@ public:
     std::vector<std::vector<size_t>> split_keys_by_shard(size_t nshard, const Slice* keys,
                                                          const std::vector<size_t>& idxes);
 
-    Status flush_to_immutable_index(const std::string& dir, const EditVersion& version);
+    Status flush_to_immutable_index(const std::string& dir, const EditVersion& version, bool write_tmp_l1 = false);
 
     // get the number of entries in the index (including NullIndexValue)
     size_t size();
@@ -294,8 +298,7 @@ public:
 
     size_t memory_usage();
 
-    Status update_overlap_info(size_t key_size, size_t num_overlap, const Slice* keys, const IndexValue* values,
-                               const KeysInfo& keys_info, bool erase);
+    void clear();
 
     static StatusOr<std::unique_ptr<ShardByLengthMutableIndex>> create(size_t key_size, const std::string& path);
 
@@ -328,7 +331,7 @@ public:
     // |values|: value array for return values
     // |num_found|: add the number of keys found in L1 to this argument
     // |key_size|: the key size of keys array
-    Status get(size_t n, const Slice* keys, const KeysInfo& keys_info, IndexValue* values, size_t* num_found,
+    Status get(size_t n, const Slice* keys, const KeysInfo& keys_info, IndexValue* values, KeysInfo* found_keys_info,
                size_t key_size) const;
 
     // batch check key existence
@@ -351,6 +354,29 @@ public:
         }
     }
 
+    void destroy() {
+        if (_file != nullptr) {
+            FileSystem::Default()->delete_file(_file->filename());
+            _file.reset();
+        }
+    }
+
+    size_t total_usage() {
+        size_t usage = 0;
+        for (const auto& shard : _shards) {
+            usage += shard.data_size;
+        }
+        return usage;
+    }
+
+    size_t total_size() {
+        size_t size = 0;
+        for (const auto& shard : _shards) {
+            size += shard.size;
+        }
+        return size;
+    }
+
     static StatusOr<std::unique_ptr<ImmutableIndex>> load(std::unique_ptr<RandomAccessFile>&& rb);
 
 private:
@@ -370,15 +396,15 @@ private:
                               std::unique_ptr<ImmutableIndexShard>* shard) const;
 
     Status _get_in_fixlen_shard(size_t shard_idx, size_t n, const Slice* keys, const KeysInfo& keys_info,
-                                IndexValue* values, size_t* num_found,
+                                IndexValue* values, KeysInfo* found_keys_info,
                                 std::unique_ptr<ImmutableIndexShard>* shard) const;
 
     Status _get_in_varlen_shard(size_t shard_idx, size_t n, const Slice* keys, const KeysInfo& keys_info,
-                                IndexValue* values, size_t* num_found,
+                                IndexValue* values, KeysInfo* found_keys_info,
                                 std::unique_ptr<ImmutableIndexShard>* shard) const;
 
     Status _get_in_shard(size_t shard_idx, size_t n, const Slice* keys, const KeysInfo& keys_info, IndexValue* values,
-                         size_t* num_found) const;
+                         KeysInfo* found_keys_info) const;
 
     Status _check_not_exist_in_fixlen_shard(size_t shard_idx, size_t n, const Slice* keys, const KeysInfo& keys_info,
                                             std::unique_ptr<ImmutableIndexShard>* shard) const;
@@ -411,7 +437,7 @@ class ImmutableIndexWriter {
 public:
     ~ImmutableIndexWriter();
 
-    Status init(const string& dir, const EditVersion& version);
+    Status init(const string& idx_file_path, const EditVersion& version, bool sync_on_close);
 
     // write_shard() must be called serially in the order of key_size and it is caller's duty to guarantee this.
     Status write_shard(size_t key_size, size_t npage_hint, size_t nbucket, const std::vector<KVRef>& kvs);
@@ -419,6 +445,8 @@ public:
     Status write_shard_as_rawbuff(const ImmutableIndex::ShardInfo& old_shard_info, ImmutableIndex* immutable_index);
 
     Status finish();
+
+    size_t total_kv_size() { return _total_kv_size; }
 
 private:
     EditVersion _version;
@@ -536,6 +564,8 @@ public:
     Status try_replace(size_t n, const Slice* keys, const IndexValue* values, const uint32_t max_src_rssid,
                        std::vector<uint32_t>* failed);
 
+    Status flush_advance();
+
     std::vector<int8_t> test_get_move_buckets(size_t target, const uint8_t* bucket_packs_in_page);
 
     Status test_flush_varlen_to_immutable_index(const std::string& dir, const EditVersion& version, size_t num_entry,
@@ -546,11 +576,18 @@ private:
 
     // check _l0 should dump as snapshot or not
     bool _can_dump_directly();
+    bool _need_flush_advance();
+    bool _need_merge_advance();
+    Status _flush_advance_or_append_wal(size_t n, const Slice* keys, const IndexValue* values);
 
     Status _delete_expired_index_file(const EditVersion& l0_version, const EditVersion& l1_version);
+    Status _delete_tmp_index_file();
 
     Status _flush_l0();
 
+    Status _merge_compaction_internal(ImmutableIndexWriter* writer, int l1_start_idx, int l1_end_idx,
+                                      size_t total_usage, size_t total_size);
+    Status _merge_compaction_advance();
     // merge l0 and l1 into new l1, then clear l0
     Status _merge_compaction();
 
@@ -564,15 +601,22 @@ private:
     Status _insert_rowsets(Tablet* tablet, std::vector<RowsetSharedPtr>& rowsets, const VectorizedSchema& pkey_schema,
                            int64_t apply_version, std::unique_ptr<Column> pk_column);
 
+    Status _get_from_immutable_index(size_t n, const Slice* keys, IndexValue* values,
+                                     std::map<size_t, KeysInfo>& keys_info_by_key_size);
+
     // index storage directory
     std::string _path;
     size_t _key_size = 0;
     size_t _size = 0;
+    size_t _usage = 0;
     EditVersion _version;
     // _l1_version is used to get l1 file name, update in on_committed
     EditVersion _l1_version;
     std::unique_ptr<ShardByLengthMutableIndex> _l0;
-    std::unique_ptr<ImmutableIndex> _l1;
+    // add all l1 into vector
+    std::vector<std::unique_ptr<ImmutableIndex>> _l1_vec;
+    std::vector<int> _l1_merged_num;
+    bool _has_l1 = false;
     std::shared_ptr<FileSystem> _fs;
 
     bool _dump_snapshot = false;

be/src/common/config.h
@@ -900,6 +900,8 @@ CONF_Bool(block_cache_checksum_enable, "true");
 
 CONF_mInt64(l0_l1_merge_ratio, "10");
 CONF_mInt64(l0_max_file_size, "209715200"); // 200MB
+CONF_mInt64(l0_max_mem_usage, "67108864");  // 64MB
+CONF_mInt64(max_tmp_l1_num, "10");
 
 // Used by query cache, cache entries are evicted when it exceeds its capacity(500MB in default)
 CONF_Int64(query_cache_capacity, "536870912");

commit 46d715047384e7bc615749319f72c652f960faad

[BugFix] BE maybe crash when we delete all records in PrimaryKey table with persistent index in branch-2.5+ (#18623)
This bug is introduced by the pr(#15729) which enable flush persistent index in advance.

If we delete a record and we will keep the record which value is NullIndexValue. In following _merge_compaction, we will merge them and skip the old record if new record value is NullIndexValue. If we import a lot of data at once, we will select some tmp l1 file do merge compaction in advance and the delete operation in these files will be skip, but there are maybe some record in older l1 files which should be delete. So the delete operation will lost in merge compaction advance. In the following merge compaction, we will create an empty shard if all records in this shard are delete, but we maybe read some expired record which should be delete which will cause BE crash.

be/src/storage/persistent_index.cpp
@@ -3155,8 +3155,114 @@ static Status merge_shard_kvs(size_t key_size, std::vector<KVRef>& l0_kvs, std::
     return Status::OK();
 }
 
+template <size_t KeySize>
+Status merge_shard_kvs_fixed_len_with_delete(std::vector<KVRef>& l0_kvs, std::vector<std::vector<KVRef>>& l1_kvs,
+                                             size_t estimated_size, std::vector<KVRef>& ret) {
+    phmap::flat_hash_set<KVRef, KVRefHash, KVRefEq<KeySize>> kvs_set;
+    kvs_set.reserve(estimated_size);
+    DCHECK(!l1_kvs.empty());
+    for (size_t i = 0; i < l1_kvs.size(); i++) {
+        for (const auto& kv : l1_kvs[i]) {
+            auto [it, inserted] = kvs_set.emplace(kv);
+            if (!inserted) {
+                DCHECK(it->hash == kv.hash) << "upsert kv in set, hash should be the same";
+                kvs_set.erase(it);
+                kvs_set.emplace(kv);
+            }
+        }
+    }
+    for (const auto& kv : l0_kvs) {
+        auto [it, inserted] = kvs_set.emplace(kv);
+        if (!inserted) {
+            DCHECK(it->hash == kv.hash) << "upsert kv in set, hash should be the same";
+            // TODO: find a way to modify iterator directly, currently just erase then re-insert
+            // it->kv_pos = kv.kv_pos;
+            kvs_set.erase(it);
+            kvs_set.emplace(kv);
+        }
+    }
+    ret.reserve(ret.size() + kvs_set.size());
+    for (const auto& kv : kvs_set) {
+        ret.emplace_back(kv);
+    }
+    return Status::OK();
+}
+
+Status merge_shard_kvs_var_len_with_delete(std::vector<KVRef>& l0_kvs, std::vector<std::vector<KVRef>>& l1_kvs,
+                                           size_t estimate_size, std::vector<KVRef>& ret) {
+    phmap::flat_hash_set<KVRef, KVRefHash, KVRefEq<0>> kvs_set;
+    kvs_set.reserve(estimate_size);
+    DCHECK(!l1_kvs.empty());
+    for (size_t i = 0; i < l1_kvs.size(); i++) {
+        for (const auto& kv : l1_kvs[i]) {
+            auto [it, inserted] = kvs_set.emplace(kv);
+            if (!inserted) {
+                DCHECK(it->hash == kv.hash) << "upsert kv in set, hash should be the same";
+                kvs_set.erase(it);
+                kvs_set.emplace(kv);
+            }
+        }
+    }
+    for (auto& kv : l0_kvs) {
+        if (auto [it, inserted] = kvs_set.emplace(kv); !inserted) {
+            DCHECK(it->hash == kv.hash) << "upsert kv in set, hash should be the same";
+            // TODO: find a way to modify iterator directly, currently just erase then re-insert
+            // it->kv_pos = kv.kv_pos;
+            kvs_set.erase(it);
+            kvs_set.emplace(kv);
+        }
+    }
+    ret.reserve(ret.size() + kvs_set.size());
+    for (const auto& kv : kvs_set) {
+        ret.emplace_back(kv);
+    }
+    return Status::OK();
+}
+
+static Status merge_shard_kvs_with_delete(size_t key_size, std::vector<KVRef>& l0_kvs,
+                                          std::vector<std::vector<KVRef>>& l1_kvs, size_t estimated_size,
+                                          std::vector<KVRef>& ret) {
+    if (key_size > 0) {
+#define CASE_SIZE(s) \
+    case s:          \
+        return merge_shard_kvs_fixed_len_with_delete<s>(l0_kvs, l1_kvs, estimated_size, ret);
+#define CASE_SIZE_8(s) \
+    CASE_SIZE(s)       \
+    CASE_SIZE(s + 1)   \
+    CASE_SIZE(s + 2)   \
+    CASE_SIZE(s + 3)   \
+    CASE_SIZE(s + 4)   \
+    CASE_SIZE(s + 5)   \
+    CASE_SIZE(s + 6)   \
+    CASE_SIZE(s + 7)
+        switch (key_size) {
+            CASE_SIZE_8(1)
+            CASE_SIZE_8(9)
+            CASE_SIZE_8(17)
+            CASE_SIZE_8(25)
+            CASE_SIZE_8(33)
+            CASE_SIZE_8(41)
+            CASE_SIZE_8(49)
+            CASE_SIZE_8(57)
+            CASE_SIZE_8(65)
+            CASE_SIZE_8(73)
+            CASE_SIZE_8(81)
+            CASE_SIZE_8(89)
+            CASE_SIZE_8(97)
+            CASE_SIZE_8(105)
+            CASE_SIZE_8(113)
+            CASE_SIZE_8(121)
+#undef CASE_SIZE_8
+#undef CASE_SIZE
+        }
+    } else if (key_size == 0) {
+        return merge_shard_kvs_var_len_with_delete(l0_kvs, l1_kvs, estimated_size, ret);
+    }
+    return Status::OK();
+}
+
 Status PersistentIndex::_merge_compaction_internal(ImmutableIndexWriter* writer, int l1_start_idx, int l1_end_idx,
-                                                   size_t total_usage, size_t total_size) {
+                                                   size_t total_usage, size_t total_size, bool keep_delete) {
     for (const auto& [key_size, shard_info] : _l0->_shard_info_by_key_size) {
         auto [l0_shard_offset, l0_shard_size] = shard_info;
         const auto [nshard, npage_hint] = MutableIndex::estimate_nshard_and_npage(total_usage);
@@ -3232,8 +3338,13 @@ Status PersistentIndex::_merge_compaction_internal(ImmutableIndexWriter* writer,
             for (size_t i = 0; i < merge_l1_num; i++) {
                 l1_kvs[i].swap(l1_kvs_by_shard[i][shard_idx]);
             }
-            RETURN_IF_ERROR(
-                    merge_shard_kvs(key_size, l0_kvs_by_shard[shard_idx], l1_kvs, estimate_size_per_shard, kvs));
+            if (keep_delete) {
+                RETURN_IF_ERROR(merge_shard_kvs_with_delete(key_size, l0_kvs_by_shard[shard_idx], l1_kvs,
+                                                            estimate_size_per_shard, kvs));
+            } else {
+                RETURN_IF_ERROR(
+                        merge_shard_kvs(key_size, l0_kvs_by_shard[shard_idx], l1_kvs, estimate_size_per_shard, kvs));
+            }
             // write shard
             RETURN_IF_ERROR(writer->write_shard(key_size, npage_hint, nbucket, kvs));
             // clear shard
@@ -3261,8 +3372,8 @@ Status PersistentIndex::_merge_compaction() {
     const std::string idx_file_path =
             strings::Substitute("$0/index.l1.$1.$2", _path, _version.major(), _version.minor());
     RETURN_IF_ERROR(writer->init(idx_file_path, _version, true));
-    RETURN_IF_ERROR(_merge_compaction_internal(writer.get(), 0, _l1_vec.size(), _usage, _size));
-    // _usage should be equal to total_kv_size. But they may be differen because of compatibility problemw when we upgrade
+    RETURN_IF_ERROR(_merge_compaction_internal(writer.get(), 0, _l1_vec.size(), _usage, _size, false));
+    // _usage should be equal to total_kv_size. But they may be differen because of compatibility problem when we upgrade
     // from old version and _usage maybe not accurate.
     // so we use total_kv_size to correct the _usage.
     if (_usage != writer->total_kv_size()) {
@@ -3280,14 +3391,15 @@ Status PersistentIndex::_merge_compaction_advance() {
     int merge_l1_start_idx = _l1_vec.size() - config::max_tmp_l1_num;
     int merge_l1_end_idx = _l1_vec.size();
     LOG(INFO) << "merge compaction advance, start_idx: " << merge_l1_start_idx << " end_idx: " << merge_l1_end_idx;
+    bool keep_delete = (merge_l1_start_idx != 0);
     size_t total_usage = _l0->memory_usage();
     size_t total_size = _l0->size();
     for (int i = merge_l1_start_idx; i < merge_l1_end_idx; i++) {
         total_usage += _l1_vec[i]->total_usage();
         total_size += _l1_vec[i]->total_size();
     }
-    RETURN_IF_ERROR(
-            _merge_compaction_internal(writer.get(), merge_l1_start_idx, merge_l1_end_idx, total_usage, total_size));
+    RETURN_IF_ERROR(_merge_compaction_internal(writer.get(), merge_l1_start_idx, merge_l1_end_idx, total_usage,
+                                               total_size, keep_delete));
     RETURN_IF_ERROR(writer->finish());
     std::vector<std::unique_ptr<ImmutableIndex>> new_l1_vec;
     std::vector<int> new_l1_merged_num;

be/src/storage/persistent_index.h
@@ -586,7 +586,7 @@ private:
     Status _flush_l0();
 
     Status _merge_compaction_internal(ImmutableIndexWriter* writer, int l1_start_idx, int l1_end_idx,
-                                      size_t total_usage, size_t total_size);
+                                      size_t total_usage, size_t total_size, bool keep_delete);
     Status _merge_compaction_advance();
     // merge l0 and l1 into new l1, then clear l0
     Status _merge_compaction();