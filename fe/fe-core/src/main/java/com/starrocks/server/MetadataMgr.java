5ffc11c51ee47303bdcb7352dbae55e24d1f9b47
stephen <91597003+stephen-shelby@users.noreply.github.com>
2023-03-02 20:11:51 +0800

    [Enhancement] refactor iceberg metadata [stage 1] (#18809)
    Signed-off-by: stephen <stephen5217@163.com>
    (cherry picked from commit 178f11b3b1bd0d5e4b63d2ea191de0fc52c71765)

--- fe/fe-core/src/main/java/com/starrocks/server/MetadataMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/server/MetadataMgr.java
@@ -19,6 +19,7 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.starrocks.analysis.TableName;
+import com.starrocks.catalog.Column;
 import com.starrocks.catalog.Database;
 import com.starrocks.catalog.PartitionKey;
 import com.starrocks.catalog.Table;
@@ -33,6 +34,7 @@ import com.starrocks.qe.ConnectContext;
 import com.starrocks.sql.ast.DropTableStmt;
 import com.starrocks.sql.optimizer.OptimizerContext;
 import com.starrocks.sql.optimizer.operator.scalar.ColumnRefOperator;
+import com.starrocks.sql.optimizer.operator.scalar.ScalarOperator;
 import com.starrocks.sql.optimizer.statistics.Statistics;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -159,19 +161,25 @@ public class MetadataMgr {
     public Statistics getTableStatistics(OptimizerContext session,
                                          String catalogName,
                                          Table table,
-                                         List<ColumnRefOperator> columns,
-                                         List<PartitionKey> partitionKeys) {
+                                         Map<ColumnRefOperator, Column> columns,
+                                         List<PartitionKey> partitionKeys,
+                                         ScalarOperator predicate) {
         Optional<ConnectorMetadata> connectorMetadata = getOptionalMetadata(catalogName);
         return connectorMetadata.map(metadata ->
-                metadata.getTableStatistics(session, table, columns, partitionKeys)).orElse(null);
+                metadata.getTableStatistics(session, table, columns, partitionKeys, predicate)).orElse(null);
     }
 
     public List<RemoteFileInfo> getRemoteFileInfos(String catalogName, Table table, List<PartitionKey> partitionKeys) {
+        return getRemoteFileInfos(catalogName, table, partitionKeys, -1, null);
+    }
+
+    public List<RemoteFileInfo> getRemoteFileInfos(String catalogName, Table table, List<PartitionKey> partitionKeys,
+                                                   long snapshotId, ScalarOperator predicate) {
         Optional<ConnectorMetadata> connectorMetadata = getOptionalMetadata(catalogName);
         ImmutableSet.Builder<RemoteFileInfo> files = ImmutableSet.builder();
         if (connectorMetadata.isPresent()) {
             try {
-                connectorMetadata.get().getRemoteFileInfos(table, partitionKeys).forEach(files::add);
+                connectorMetadata.get().getRemoteFileInfos(table, partitionKeys, snapshotId, predicate).forEach(files::add);
             } catch (Exception e) {
                 LOG.error("Failed to list remote file's metadata on catalog [{}], table [{}]", catalogName, table, e);
                 throw e;
