e9e60d4b23092d125ea24ac56f20bccfeb599c19
stdpain <34912776+stdpain@users.noreply.github.com>
2023-03-02 13:52:54 +0800

    [BugFix] Fix TOP-N filter may cause wrong result when using with lowcardinality optimize (#18780)
    if we have multi-scanners execute one-by-one, if a scanner has finished building a runtime filter,
    the rest of the runtime filters will be normalized in normalize_join_runtime_filter. which will cause
    a down_cast error (RuntimeFilter Type is LowcardinalityType but cast as varchar type)
    
    Signed-off-by: stdpain <drfeng08@gmail.com>

--- be/src/storage/olap_runtime_range_pruner.hpp
+++ be/src/storage/olap_runtime_range_pruner.hpp
@@ -67,13 +67,13 @@ struct RuntimeColumnPredicateBuilder {
             if constexpr (ltype == TYPE_VARCHAR) {
                 auto cid = parser->column_id(*slot);
                 if (auto iter = global_dictmaps->find(cid); iter != global_dictmaps->end()) {
-                    _build_minmax_range<RangeType, value_type, LowCardDictType, GlobalDictCodeDecoder>(range, rf,
-                                                                                                       iter->second);
+                    build_minmax_range<RangeType, value_type, LowCardDictType, GlobalDictCodeDecoder>(range, rf,
+                                                                                                      iter->second);
                 } else {
-                    _build_minmax_range<RangeType, value_type, mapping_type, DummyDecoder>(range, rf, nullptr);
+                    build_minmax_range<RangeType, value_type, mapping_type, DummyDecoder>(range, rf, nullptr);
                 }
             } else {
-                _build_minmax_range<RangeType, value_type, mapping_type, DummyDecoder>(range, rf, nullptr);
+                build_minmax_range<RangeType, value_type, mapping_type, DummyDecoder>(range, rf, nullptr);
             }
 
             std::vector<TCondition> filters;
@@ -95,12 +95,12 @@ struct RuntimeColumnPredicateBuilder {
         }
     }
 
-private:
     template <class InputType>
     struct DummyDecoder {
         DummyDecoder(std::nullptr_t) {}
         auto decode(InputType input) const { return input; }
     };
+
     template <class InputType>
     struct GlobalDictCodeDecoder {
         GlobalDictCodeDecoder(const GlobalDictMap* dict_map) : _dict_map(dict_map) {}
@@ -140,7 +140,7 @@ private:
     };
 
     template <class Range, class value_type, LogicalType mapping_type, template <class> class Decoder, class... Args>
-    void _build_minmax_range(Range& range, const JoinRuntimeFilter* rf, Args&&... args) {
+    static void build_minmax_range(Range& range, const JoinRuntimeFilter* rf, Args&&... args) {
         const RuntimeBloomFilter<mapping_type>* filter = down_cast<const RuntimeBloomFilter<mapping_type>*>(rf);
         using DecoderType = Decoder<typename RunTimeTypeTraits<mapping_type>::CppType>;
         DecoderType decoder(std::forward<Args>(args)...);
