d8ad7a24bb5eef446f731b830816420a30d6a38e
stephen <stephen5217@163.com>
2023-03-02 21:47:30 +0800

    [Cherry-Pick] support show full columns from external catalog table
    Signed-off-by: stephen <stephen5217@163.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/ShowExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/ShowExecutor.java
@@ -842,11 +842,16 @@ public class ShowExecutor {
     private void handleShowColumn() throws AnalysisException {
         ShowColumnStmt showStmt = (ShowColumnStmt) stmt;
         List<List<String>> rows = Lists.newArrayList();
-        Database db = ctx.getGlobalStateMgr().getDb(showStmt.getDb());
+        String catalogName = showStmt.getCatalog();
+        if (catalogName == null) {
+            catalogName = ctx.getCurrentCatalog();
+        }
+        String dbName = showStmt.getDb();
+        Database db = metadataMgr.getDb(catalogName, dbName);
         MetaUtils.checkDbNullAndReport(db, showStmt.getDb());
         db.readLock();
         try {
-            Table table = db.getTable(showStmt.getTable());
+            Table table = metadataMgr.getTable(catalogName, dbName, showStmt.getTable());
             if (table == null) {
                 ErrorReport.reportAnalysisException(ErrorCode.ERR_BAD_TABLE_ERROR,
                         showStmt.getDb() + "." + showStmt.getTable());
