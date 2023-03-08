StarRocks/starrocks

commit d1de07fe693166500aa796e5e8f928fbbd5449f2

[BugFix] Can not find the correct repo when repo prefix is palo after fe restart (#18015)
Problem:
Now we use __starrocks prefix for repo, but if user have the __palo prefix repo, fe will get wrong
prefix as __starrocks when fe restart.

Solution:
When Fe recover meta data for repo, it should try to list __palo prefix repo to check that if  __palo prefix repo
is exist or not and reset prefix to the correct value. In this way, we can avoid to persist the prefix  value into the Image File.

Signed-off-by: srlch <linzichao@starrocks.com>

fe/fe-core/src/main/java/com/starrocks/backup/Repository.java
@@ -700,5 +700,17 @@ public class Repository implements Writable {
         location = Text.readString(in);
         storage = BlobStorage.read(in);
         createTime = in.readLong();
+
+        // list __palo_repository_ first, if success, prefixRepo = __palo_repository_
+        String listPath = Joiner.on(PATH_DELIMITER).join(location, joinPrefix("__palo_repository_", name), PREFIX_SNAPSHOT_DIR)
+                + "*";
+        List<RemoteFile> result = Lists.newArrayList();
+        Status st = storage.list(listPath, result);
+
+        if (st.ok()) {
+            prefixRepo = "__palo_repository_";
+        } else {
+            prefixRepo = "__starrocks_repository_";
+        }
     }
 }

commit 6903ba365c8f8c574a23fda09e33900275c18fa8

[BugFix] repo prefix incorrect (#19195)
Problem:
This problem is introduced by d1de07fe693. This commit want to reason the correct repo prefix when FE restart. In Repository.readField,
list the snapshot using palo prefix and determine the prefix base on the result. But there are two problem here:

1. We can not just return palo prefix if status is ok. Because in some case, it will return ok even if we can not find any snapshot file in repo by __ palo__repo__name.
2. If the repo is empty, this predicate will also fail.

This will cause other run time problem. If leader FE create a repo, follower will apply edit log and but because the Repository.readField is incorrect, follower will may get a wrong prefix.

Solution:
1. Use checkPathExist to check whether the __ palo__repo__name path is exist instead.
2. Don't execute the code when the function is called by checkpoint thread.

Signed-off-by: srlch <linzichao@starrocks.com>

fe/fe-core/src/main/java/com/starrocks/backup/Repository.java
@@ -701,16 +701,23 @@ public class Repository implements Writable {
         storage = BlobStorage.read(in);
         createTime = in.readLong();
 
-        // list __palo_repository_ first, if success, prefixRepo = __palo_repository_
-        String listPath = Joiner.on(PATH_DELIMITER).join(location, joinPrefix("__palo_repository_", name), PREFIX_SNAPSHOT_DIR)
-                + "*";
-        List<RemoteFile> result = Lists.newArrayList();
-        Status st = storage.list(listPath, result);
+        if (!GlobalStateMgr.isCheckpointThread()) {
+            // check __palo_repository_ first, if success, prefixRepo = __palo_repository_
+            String listPath = Joiner.on(PATH_DELIMITER).join(location, joinPrefix("__palo_repository_", name));
+            Status st;
+            try {
+                st = storage.checkPathExist(listPath);
+            } catch (Exception e) {
+                LOG.warn("check path exist fail");
+                prefixRepo = "__starrocks_repository_";
+                return;
+            }
 
-        if (st.ok()) {
-            prefixRepo = "__palo_repository_";
-        } else {
-            prefixRepo = "__starrocks_repository_";
+            if (st.ok()) {
+                prefixRepo = "__palo_repository_";
+            } else {
+                prefixRepo = "__starrocks_repository_";
+            }
         }
     }
 }