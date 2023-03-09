6d0770522e2840bbef311f627cd9afed4657c9e7
sduzh <zhuming9011@qq.com>
2023-03-09 16:35:01 +0800

    [Refactor] Split table creation logics to different places
    Use abstract factory design pattern split the table creation
    to different places based on the table engine;
    
    Signed-off-by: sduzh <zhuming9011@qq.com>

---
+++ fe/fe-core/src/main/java/com/starrocks/server/JDBCTableFactory.java
@@ -0,0 +1,42 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.starrocks.server;
+
+import com.starrocks.catalog.Column;
+import com.starrocks.catalog.Database;
+import com.starrocks.catalog.JDBCTable;
+import com.starrocks.common.DdlException;
+import com.starrocks.sql.ast.CreateTableStmt;
+
+import java.util.List;
+import java.util.Map;
+
+public class JDBCTableFactory implements AbstractTableFactory {
+    public static final JDBCTableFactory INSTANCE = new JDBCTableFactory();
+
+    private JDBCTableFactory() {
+    }
+
+    @Override
+    public void createTable(LocalMetastore metastore, Database database, CreateTableStmt stmt) throws DdlException {
+        String tableName = stmt.getTableName();
+        List<Column> columns = stmt.getColumns();
+        Map<String, String> properties = stmt.getProperties();
+        long tableId = metastore.getNextId();
+        JDBCTable jdbcTable = new JDBCTable(tableId, tableName, columns, properties);
+
+        metastore.registerTable(database, jdbcTable, stmt);
+    }
+}
