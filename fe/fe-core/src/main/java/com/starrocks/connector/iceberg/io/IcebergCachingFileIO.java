00528f4e931d252922ef93243140e2ff29c0272b
zombee0 <flylucas_10@163.com>
2023-03-10 17:26:46 +0800

    [BugFix] fix the CachingFileIO had wrong length and support pin/unpin for disk cache (#18892)
    Signed-off-by: zombee0 <flylucas_10@163.com>

--- fe/fe-core/src/main/java/com/starrocks/connector/iceberg/io/IcebergCachingFileIO.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/iceberg/io/IcebergCachingFileIO.java
@@ -58,7 +58,7 @@ import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.RemoteIterator;
 import org.apache.iceberg.exceptions.NotFoundException;
-import org.apache.iceberg.exceptions.RuntimeIOException;
+import org.apache.iceberg.hadoop.HadoopInputFile;
 import org.apache.iceberg.hadoop.HadoopOutputFile;
 import org.apache.iceberg.hadoop.Util;
 import org.apache.iceberg.io.FileIO;
@@ -73,6 +73,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.ByteBuffer;
 import java.util.List;
 import java.util.Map;
@@ -95,6 +96,7 @@ public class IcebergCachingFileIO implements FileIO {
     public static final String FILEIO_CACHE_MAX_TOTAL_BYTES = "fileIO.cache.max-total-bytes";
     public static final String METADATA_CACHE_DISK_PATH = Config.iceberg_metadata_cache_disk_path;
     public static final long DISK_CACHE_CAPACITY = Config.iceberg_metadata_disk_cache_capacity;
+    public static final long DISK_CACHE_EXPIRATION_SECONDS = Config.iceberg_metadata_disk_cache_expiration_seconds;
 
     private ContentCache fileContentCache;
     private final FileIO wrappedIO;
@@ -142,10 +144,12 @@ public class IcebergCachingFileIO implements FileIO {
     private static class DiskCacheEntry {
         private final long length;
         private final InputFile inputFile;
+        private int useCount;
 
         private DiskCacheEntry(long length, InputFile inputFile) {
             this.length = length;
             this.inputFile = inputFile;
+            this.useCount = 0;
         }
 
         public SeekableInputStream toSeekableInputStream() {
@@ -153,7 +157,76 @@ public class IcebergCachingFileIO implements FileIO {
         }
 
         public boolean isExistOnDisk() {
-            return this.inputFile != null && this.inputFile.exists();
+            if (this.inputFile == null) {
+                return false;
+            } else {
+                HadoopInputFile hadoopInputFile = (HadoopInputFile) this.inputFile;
+                try {
+                    return hadoopInputFile.getFileSystem().exists(hadoopInputFile.getPath());
+                } catch (IOException e) {
+                    return false;
+                }
+            }
+        }
+
+        public void pin() {
+            useCount += 1;
+        }
+        public void unpin() {
+            useCount -= 1;
+        }
+
+        public static DiskCacheEntry newDiskCacheEntry(SeekableInputStream stream, long fileLength, String key) {
+            long totalBytesToRead = fileLength;
+            HadoopOutputFile tmpOutputFile = (HadoopOutputFile) IOUtil.getTmpOutputFile(
+                    METADATA_CACHE_DISK_PATH, key);
+            PositionOutputStream outputStream = tmpOutputFile.createOrOverwrite();
+            try {
+                Path localFilePath = new Path(IOUtil.remoteToLocalFilePath(METADATA_CACHE_DISK_PATH, key));
+                while (totalBytesToRead > 0) {
+                    // read the stream in 4MB chunk
+                    int bytesToRead = (int) Math.min(BUFFER_CHUNK_SIZE, totalBytesToRead);
+                    byte[] buf = new byte[bytesToRead];
+                    int bytesRead = IOUtil.readRemaining(stream, buf, 0, bytesToRead);
+                    totalBytesToRead -= bytesRead;
+
+                    if (bytesRead < bytesToRead) {
+                        // Read less than it should be, possibly hitting EOF.
+                        // Set smaller ByteBuffer limit and break out of the loop.
+                        outputStream.write(buf, 0, bytesRead);
+                        break;
+                    } else {
+                        outputStream.write(buf);
+                    }
+                }
+                stream.close();
+                outputStream.close();
+                if (!tmpOutputFile.getFileSystem().rename(tmpOutputFile.getPath(), localFilePath)) {
+                    LOG.warn("failed on rename: {} to {}", tmpOutputFile.getPath().toString(),
+                            localFilePath.toString());
+                    tmpOutputFile.getFileSystem().delete(tmpOutputFile.getPath(), false);
+                    // for mapping function :If the specified key is not already associated with a value,
+                    // attempts to compute its value using the given mapping function
+                    // and enters it into this cache unless null.
+                    return null;
+                } else {
+                    return new DiskCacheEntry(fileLength - totalBytesToRead,
+                            IOUtil.getInputFile(localFilePath));
+                }
+            } catch (IOException ex) {
+                try {
+                    IOUtil.closeInputStreamIgnoreException(stream);
+                    IOUtil.closeOutputStreamIgnoreException(outputStream);
+                    tmpOutputFile.getFileSystem().delete(tmpOutputFile.getPath(), false);
+                } catch (IOException ioException) {
+                    LOG.warn("failed on deleting file : {}. msg: {}", tmpOutputFile.getPath(), ioException);
+                }
+                throw new UncheckedIOException(ex);
+            } catch (IllegalArgumentException e) {
+                IOUtil.closeInputStreamIgnoreException(stream);
+                IOUtil.closeOutputStreamIgnoreException(outputStream);
+                throw e;
+            }
         }
     }
 
@@ -176,11 +249,15 @@ public class IcebergCachingFileIO implements FileIO {
 
         public abstract boolean exists(String key);
 
-        public boolean isExistOnDisk(String key) {
+        public boolean isExistOnDiskCache(String key) {
             return false;
         }
 
-        public InputFile getDiskInputFile(String key) {
+        public SeekableInputStream getDiskSeekableStream(String key) {
+            return null;
+        }
+
+        public DiskCacheEntry getDiskCacheEntry(String key, Function<String, DiskCacheEntry> mappingFunction) {
             return null;
         }
     }
@@ -229,11 +306,6 @@ public class IcebergCachingFileIO implements FileIO {
             CacheEntry buf = cache.getIfPresent(key);
             return buf != null;
         }
-
-        @Override
-        public boolean isExistOnDisk(String key) {
-            return false;
-        }
     }
 
     private static class TwoLevelCacheHolder {
@@ -248,18 +320,15 @@ public class IcebergCachingFileIO implements FileIO {
 
             Caffeine<Object, Object> diskCacheBuilder = Caffeine.newBuilder();
             this.diskCache = diskCacheBuilder.maximumWeight(DISK_CACHE_CAPACITY)
-                    .weigher((Weigher<String, DiskCacheEntry>) (key, value) -> (int) Math.min(value.length, Integer.MAX_VALUE))
+                    .expireAfterAccess(DISK_CACHE_EXPIRATION_SECONDS, TimeUnit.SECONDS)
+                    .weigher((Weigher<String, DiskCacheEntry>) (key, value) ->
+                            value.useCount == 0 ? (int) Math.min(value.length, Integer.MAX_VALUE) : 0)
                     .recordStats()
-                    .removalListener(((key, value, cause) -> {
-                        LOG.debug(key + " to be eliminated from disk, reason: " + cause);
-                        HadoopOutputFile hadoopOutputFile = (HadoopOutputFile) IOUtil.getOutputFile(
-                                METADATA_CACHE_DISK_PATH, key);
-                        try {
-                            hadoopOutputFile.getFileSystem().delete(hadoopOutputFile.getPath(), false);
-                        } catch (Exception e) {
-                            LOG.warn("failed on deleting file :" + hadoopOutputFile.getPath());
-                        }
-                    })).build();
+                    // use sync evictionListener to avoid delete file newly generated by another thread
+                    .evictionListener((key, value, cause) -> {
+                        LOG.debug("{} to be eliminated from disk, reason: {}", key, cause);
+                        IOUtil.deleteLocalFileWithRemotePath(key);
+                    }).build();
 
             Caffeine<Object, Object> builder = Caffeine.newBuilder();
             this.memCache = builder.maximumWeight(MEMORY_CACHE_CAPACITY)
@@ -268,38 +337,12 @@ public class IcebergCachingFileIO implements FileIO {
                     .recordStats()
                     .removalListener(((key, value, cause) -> {
                         LOG.debug(key + " to be eliminated to disk, reason: " + cause);
-                        if (isExistOnDisk(key)) {
+                        if (isExistOnDiskCache(key)) {
                             LOG.debug(key + " has cached on disk");
                             return;
                         }
-                        HadoopOutputFile tmpOutputFile = (HadoopOutputFile) IOUtil.getTmpOutputFile(
-                                METADATA_CACHE_DISK_PATH, key);
-                        PositionOutputStream outputStream = tmpOutputFile.createOrOverwrite();
-                        Path localFilePath = new Path(IOUtil.remoteToLocalFilePath(METADATA_CACHE_DISK_PATH, key));
-                        try {
-                            for (ByteBuffer buffer : value.buffers) {
-                                outputStream.write(buffer.array());
-                            }
-                            outputStream.close();
-                            if (!tmpOutputFile.getFileSystem().rename(tmpOutputFile.getPath(), localFilePath)) {
-                                LOG.warn("failed on rename: {} to {}", tmpOutputFile.getPath().toString(),
-                                        localFilePath.toString());
-                                tmpOutputFile.getFileSystem().delete(tmpOutputFile.getPath(), false);
-                            } else {
-                                diskCache.put(key, new DiskCacheEntry(value.length,
-                                        IOUtil.getOutputFile(localFilePath).toInputFile()));
-                            }
-                        } catch (IOException e) {
-                            LOG.warn("failed on writing file to disk" + e.getMessage());
-                            try {
-                                if (outputStream != null) {
-                                    outputStream.close();
-                                }
-                                tmpOutputFile.getFileSystem().delete(tmpOutputFile.getPath(), false);
-                            } catch (IOException ioException) {
-                                LOG.warn("failed on deleting file :" + tmpOutputFile.getPath());
-                            }
-                        }
+                        this.getDiskCacheEntry(key, k -> DiskCacheEntry.newDiskCacheEntry(
+                                ByteBufferInputStream.wrap(value.buffers), value.length, key));
                     })).build();
 
             loadMetadataDiskCache();
@@ -318,9 +361,9 @@ public class IcebergCachingFileIO implements FileIO {
                             continue;
                         }
                         Path localPath = locatedFileStatus.getPath();
-                        OutputFile localOutputFile = IOUtil.getOutputFile(localPath);
-                        String key = IOUtil.localFileToRemote(localPath, METADATA_CACHE_DISK_PATH).toString();
-                        diskCache.put(key, new DiskCacheEntry(locatedFileStatus.getLen(), localOutputFile.toInputFile()));
+                        InputFile localInputFile = IOUtil.getInputFile(localPath);
+                        String key = IOUtil.localFileToRemote(localPath, METADATA_CACHE_DISK_PATH);
+                        diskCache.put(key, new DiskCacheEntry(locatedFileStatus.getLen(), localInputFile));
                         LOG.debug("load metadata to disk cache: {} from {}", key, localPath.toString());
                     }
                 } catch (Exception e) {
@@ -353,28 +396,103 @@ public class IcebergCachingFileIO implements FileIO {
         @Override
         public boolean exists(String key) {
             CacheEntry buf = memCache.getIfPresent(key);
-            return buf != null || isExistOnDisk(key);
+            return buf != null || isExistOnDiskCache(key);
         }
 
         @Override
         public void invalidate(String key) {
             memCache.invalidate(key);
-            diskCache.invalidate(key);
+            diskCache.asMap().computeIfPresent(key, (k, v) -> {
+                IOUtil.deleteLocalFileWithRemotePath(k);
+                return null;
+            });
         }
 
         @Override
-        public boolean isExistOnDisk(String key) {
+        public boolean isExistOnDiskCache(String key) {
             DiskCacheEntry entry = diskCache.getIfPresent(key);
-            return entry != null && entry.isExistOnDisk();
+            if (entry != null && !entry.isExistOnDisk()) {
+                diskCache.asMap().computeIfPresent(key, (k, v) -> {
+                    IOUtil.deleteLocalFileWithRemotePath(k);
+                    return null;
+                });
+                return false;
+            }
+            return entry != null;
         }
 
         @Override
-        public InputFile getDiskInputFile(String key) {
-            if (diskCache.getIfPresent(key) != null) {
-                return diskCache.getIfPresent(key).inputFile;
+        public SeekableInputStream getDiskSeekableStream(String key) {
+            DiskCacheEntry diskCacheEntry = diskCache.asMap().computeIfPresent(key, (k, v) -> {
+                v.pin();
+                return v;
+            });
+            if (diskCacheEntry != null) {
+                try {
+                    SeekableInputStream stream = diskCacheEntry.toSeekableInputStream();
+                    return new DiskCacheSeekableInputStream(stream, diskCache, key);
+                } catch (Exception e) {
+                    diskCache.asMap().computeIfPresent(key, (k, v) -> {
+                        v.unpin();
+                        return v;
+                    });
+                    return null;
+                }
             } else {
                 return null;
             }
+        }
+
+        @Override
+        public DiskCacheEntry getDiskCacheEntry(String key, Function<String, DiskCacheEntry> mappingFunction) {
+            return diskCache.get(key, mappingFunction);
+        }
+    }
+
+    public static class DiskCacheSeekableInputStream extends SeekableInputStream {
+        private final SeekableInputStream stream;
+        private final Cache<String, DiskCacheEntry> diskCache;
+        private final String key;
+
+        DiskCacheSeekableInputStream(SeekableInputStream stream, Cache<String, DiskCacheEntry> diskCache, String key) {
+            this.stream = stream;
+            this.diskCache = diskCache;
+            this.key = key;
+        }
+        @Override
+        public void close() throws IOException {
+            try {
+                stream.close();
+            } finally {
+                diskCache.asMap().computeIfPresent(key, (k, v) -> {
+                    v.unpin();
+                    return v;
+                });
+            }
+        }
+
+        @Override
+        public long getPos() throws IOException {
+            return stream.getPos();
+        }
+
+        @Override
+        public void seek(long newPos) throws IOException {
+            stream.seek(newPos);
+        }
+
+        @Override
+        public int read() throws IOException {
+            return stream.read();
+        }
+
+        @Override
+        public int read(byte[] b, int off, int len) throws IOException {
+            return stream.read(b, off, len);
+        }
+
+        public int read(ByteBuffer buf) throws IOException {
+            return stream.read(buf.array());
         }
     }
 
@@ -389,8 +507,8 @@ public class IcebergCachingFileIO implements FileIO {
 
         @Override
         public long getLength() {
-            return contentCache.getLength(location()) == -1 ?
-                    wrappedInputFile.getLength() : contentCache.getLength(location());
+            long len = contentCache.getLength(location());
+            return len == -1 ? wrappedInputFile.getLength() : len;
         }
 
         @Override
@@ -406,7 +524,8 @@ public class IcebergCachingFileIO implements FileIO {
             } catch (FileNotFoundException e) {
                 throw new NotFoundException(e, "Failed to open input stream for file: %s", wrappedInputFile.location());
             } catch (IOException e) {
-                throw new RuntimeIOException(e, "Failed to open input stream for file: %s", wrappedInputFile.location());
+                throw new UncheckedIOException(
+                        String.format("Failed to open input stream for file: %s", wrappedInputFile.location()), e);
             }
         }
 
@@ -421,16 +540,14 @@ public class IcebergCachingFileIO implements FileIO {
         }
 
         private CacheEntry newCacheEntry() {
+            SeekableInputStream stream = null;
             try {
                 long fileLength = getLength();
                 long totalBytesToRead = fileLength;
-                SeekableInputStream stream;
-                if (contentCache.isExistOnDisk(location())) {
+                if (contentCache.isExistOnDiskCache(location())) {
                     LOG.debug(location() + " hit on disk cache");
-                    InputFile diskFile = contentCache.getDiskInputFile(location());
-                    if (diskFile != null) {
-                        stream = diskFile.newStream();
-                    } else {
+                    stream = contentCache.getDiskSeekableStream(location());
+                    if (stream == null) {
                         LOG.debug(location() + " load from remote");
                         stream = wrappedInputFile.newStream();
                     }
@@ -460,7 +577,10 @@ public class IcebergCachingFileIO implements FileIO {
                 stream.close();
                 return new CacheEntry(fileLength - totalBytesToRead, buffers);
             } catch (IOException ex) {
-                throw new RuntimeIOException(ex);
+                if (stream != null) {
+                    IOUtil.closeInputStreamIgnoreException(stream);
+                }
+                throw new UncheckedIOException(ex);
             }
         }
 
@@ -469,11 +589,16 @@ public class IcebergCachingFileIO implements FileIO {
                 CacheEntry entry = contentCache.get(location(), k -> newCacheEntry());
                 Preconditions.checkNotNull(entry, "CacheEntry should not be null when there is no RuntimeException occurs");
                 return ByteBufferInputStream.wrap(entry.buffers);
-            } catch (RuntimeIOException ex) {
+            } catch (UncheckedIOException ex) {
                 throw ex.getCause();
             } catch (RuntimeException ex) {
                 throw new IOException("Caught an error while reading through cache", ex);
             }
+        }
+
+        @Override
+        public String toString() {
+            return location();
         }
     }
 }
