ae21f6ccf522b44f48378e77e4c623fd989d9a6b
ABing <101158374+ABingHuang@users.noreply.github.com>
2023-03-10 17:35:51 +0800

    [BugFix] fix low cardinality dict on mv rewrite (#19325)
    * fix low cardinality dict on mv rewrite
    
    fix low cardinality dict encode optimization bug for mv rewrite.
    The reason is that taskContext.getAllScanOperators() info is wrong after mv rewrite.
    we should collect the new OlapScanOperators in AddDecodeNodeForDictStringRule.
    
    Signed-off-by: ABingHuang <codekhuang@163.com>

--- fe/fe-core/src/test/resources/sql/materialized-view/ssb/q4-3.sql
+++ fe/fe-core/src/test/resources/sql/materialized-view/ssb/q4-3.sql
@@ -29,10 +29,11 @@ ORDER BY
   s_city,
   p_brand;
 [result]
-TOP-N (order by [[5: d_year ASC NULLS FIRST, 29: s_city ASC NULLS FIRST, 37: p_brand ASC NULLS FIRST]])
-    TOP-N (order by [[5: d_year ASC NULLS FIRST, 29: s_city ASC NULLS FIRST, 37: p_brand ASC NULLS FIRST]])
-        AGGREGATE ([GLOBAL] aggregate [{60: sum=sum(60: sum)}] group by [[5: d_year, 29: s_city, 37: p_brand]] having [null]
-            EXCHANGE SHUFFLE[5, 29, 37]
-                AGGREGATE ([LOCAL] aggregate [{60: sum=sum(59: expr)}] group by [[5: d_year, 29: s_city, 37: p_brand]] having [null]
-                    SCAN (columns[73: LO_REVENUE, 74: LO_SUPPLYCOST, 82: C_REGION, 87: S_CITY, 88: S_NATION, 93: P_CATEGORY, 94: P_BRAND, 102: d_year] predicate[93: P_CATEGORY = MFGR#14 AND 82: C_REGION = AMERICA AND 88: S_NATION = UNITED STATES AND 102: d_year IN (1997, 1998)])
+Decode
+    TOP-N (order by [[5: d_year ASC NULLS FIRST, 116: S_CITY ASC NULLS FIRST, 119: P_BRAND ASC NULLS FIRST]])
+        TOP-N (order by [[5: d_year ASC NULLS FIRST, 116: S_CITY ASC NULLS FIRST, 119: P_BRAND ASC NULLS FIRST]])
+            AGGREGATE ([GLOBAL] aggregate [{60: sum=sum(60: sum)}] group by [[5: d_year, 116: S_CITY, 119: P_BRAND]] having [null]
+                EXCHANGE SHUFFLE[5, 116, 119]
+                    AGGREGATE ([LOCAL] aggregate [{60: sum=sum(59: expr)}] group by [[5: d_year, 116: S_CITY, 119: P_BRAND]] having [null]
+                        SCAN (columns[115: C_REGION, 116: S_CITY, 117: S_NATION, 102: d_year, 118: P_CATEGORY, 119: P_BRAND, 73: LO_REVENUE, 74: LO_SUPPLYCOST] predicate[DictMapping(118: P_CATEGORY{93: P_CATEGORY = MFGR#14}) AND DictMapping(115: C_REGION{82: C_REGION = AMERICA}) AND DictMapping(117: S_NATION{88: S_NATION = UNITED STATES}) AND 102: d_year IN (1997, 1998)])
 [end]
