4535458678618da7cc58a95d5b856d7c178ab916
meegoo <meegoo.sr@gmail.com>
2023-03-10 21:19:59 +0800

    [BugFix] Fix insert stmt wrong timeout info (#19319)
    Signed-off-by: meegoo <hujie-dlut@qq.com>
    (cherry picked from commit b2719e7b0bfd891f40e832c42b451c32bb73e7e0)

--- fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
@@ -1385,7 +1385,8 @@ public class StmtExecutor {
                     EtlJobType.INSERT,
                     createTime,
                     estimateScanRows,
-                    type);
+                    type,
+                    ConnectContext.get().getSessionVariable().getQueryTimeoutS());
             coord.setJobId(jobId);
 
             QeProcessorImpl.INSTANCE.registerQuery(context.getExecutionId(), coord);
