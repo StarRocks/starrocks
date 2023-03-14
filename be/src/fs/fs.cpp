eec25a57fced2181d18f1237cb06741cbd2e1375
Smith Cruise <chendingchao1@126.com>
2023-03-14 17:18:07 +0800

    [Feature] Support Azure Storage IAM (#18981)
    Signed-off-by: Smith Cruise <chendingchao1@126.com>
    Signed-off-by: blanklin030 <luis01@foxmail.com>

--- be/src/fs/fs.cpp
+++ be/src/fs/fs.cpp
@@ -19,6 +19,7 @@
 #include "fs/fs_hdfs.h"
 #include "fs/fs_posix.h"
 #include "fs/fs_s3.h"
+#include "fs/fs_util.h"
 #include "runtime/file_result_writer.h"
 #ifdef USE_STAROS
 #include "fs/fs_starlet.h"
@@ -63,33 +64,17 @@ inline std::shared_ptr<FileSystem> get_tls_fs_starlet() {
 }
 #endif
 
-inline bool starts_with(std::string_view s, std::string_view prefix) {
-    return (s.size() >= prefix.size()) && (memcmp(s.data(), prefix.data(), prefix.size()) == 0);
-}
-
-inline bool is_s3_uri(std::string_view uri) {
-    return starts_with(uri, "oss://") || starts_with(uri, "s3n://") || starts_with(uri, "s3a://") ||
-           starts_with(uri, "s3://") || starts_with(uri, "cos://") || starts_with(uri, "cosn://") ||
-           starts_with(uri, "obs://") || starts_with(uri, "ks3://") || starts_with(uri, "gs://") ||
-           starts_with(uri, "tos://");
-}
-
-/*
-inline bool is_hdfs_uri(std::string_view uri) {
-    return starts_with(uri, "hdfs://") || starts_with(uri, "viewfs://");
-}
- */
-
-inline bool is_posix_uri(std::string_view uri) {
-    return (memchr(uri.data(), ':', uri.size()) == nullptr) || starts_with(uri, "posix://");
-}
-
 StatusOr<std::unique_ptr<FileSystem>> FileSystem::CreateUniqueFromString(std::string_view uri, FSOptions options) {
-    if (is_posix_uri(uri)) {
+    if (fs::is_posix_uri(uri)) {
         return new_fs_posix();
     }
-    if (is_s3_uri(uri)) {
+    if (fs::is_s3_uri(uri)) {
         return new_fs_s3(options);
+    }
+    if (fs::is_azure_uri(uri)) {
+        // TODO(SmithCruise):
+        // Now we use LibHdfs to access azure storage, we can use Azure CPP SDK to improve performance in the future.
+        return new_fs_hdfs(options);
     }
 #ifdef USE_STAROS
     if (is_starlet_uri(uri)) {
@@ -102,10 +87,10 @@ StatusOr<std::unique_ptr<FileSystem>> FileSystem::CreateUniqueFromString(std::st
 }
 
 StatusOr<std::shared_ptr<FileSystem>> FileSystem::CreateSharedFromString(std::string_view uri) {
-    if (is_posix_uri(uri)) {
+    if (fs::is_posix_uri(uri)) {
         return get_tls_fs_posix();
     }
-    if (is_s3_uri(uri)) {
+    if (fs::is_s3_uri(uri)) {
         return get_tls_fs_s3();
     }
 #ifdef USE_STAROS
--- be/src/runtime/hdfs/hdfs_fs_cache.cpp
+++ be/src/runtime/hdfs/hdfs_fs_cache.cpp
@@ -21,6 +21,23 @@
 
 namespace starrocks {
 
+// Try to get azure cloud properties from FSOptions, if azure credentials not existed, return nullptr.
+// TODO(SmithCruise): Should remove when using azure cpp sdk
+static const std::vector<TCloudProperty>* get_azure_cloud_properties(const FSOptions& options) {
+    const TCloudConfiguration* cloud_configuration = nullptr;
+    if (options.cloud_configuration != nullptr) {
+        // This branch is used by data lake
+        cloud_configuration = options.cloud_configuration;
+    } else if (options.hdfs_properties() != nullptr && options.hdfs_properties()->__isset.cloud_configuration) {
+        // This branch is used by broker load
+        cloud_configuration = &options.hdfs_properties()->cloud_configuration;
+    }
+    if (cloud_configuration != nullptr && cloud_configuration->cloud_type == TCloudType::AZURE) {
+        return &cloud_configuration->cloud_properties;
+    }
+    return nullptr;
+}
+
 static Status create_hdfs_fs_handle(const std::string& namenode, HdfsFsHandle* handle, const FSOptions& options) {
     auto hdfs_builder = hdfsNewBuilder();
     hdfsBuilderSetNameNode(hdfs_builder, namenode.c_str());
@@ -33,6 +50,16 @@ static Status create_hdfs_fs_handle(const std::string& namenode, HdfsFsHandle* h
             hdfsBuilderSetForceNewInstance(hdfs_builder);
         }
     }
+
+    // Insert azure cloud credential into Hadoop configuration
+    // TODO(SmithCruise): Should remove when using azure cpp sdk
+    const std::vector<TCloudProperty>* azure_cloud_properties = get_azure_cloud_properties(options);
+    if (azure_cloud_properties != nullptr) {
+        for (const auto& cloud_property : *azure_cloud_properties) {
+            hdfsBuilderConfSetStr(hdfs_builder, cloud_property.key.data(), cloud_property.value.data());
+        }
+    }
+
     handle->hdfs_fs = hdfsBuilderConnect(hdfs_builder);
     if (handle->hdfs_fs == nullptr) {
         return Status::InternalError(strings::Substitute("fail to connect hdfs namenode, namenode=$0, err=$1", namenode,
@@ -44,18 +71,28 @@ static Status create_hdfs_fs_handle(const std::string& namenode, HdfsFsHandle* h
 Status HdfsFsCache::get_connection(const std::string& namenode, HdfsFsHandle* handle, const FSOptions& options) {
     {
         std::lock_guard<std::mutex> l(_lock);
-        std::string id = namenode;
+        std::string cache_key = namenode;
         const THdfsProperties* properties = options.hdfs_properties();
         if (properties != nullptr && properties->__isset.hdfs_username) {
-            id += properties->hdfs_username;
+            cache_key += properties->hdfs_username;
         }
-        auto it = _cache.find(id);
+
+        // Insert azure cloud credential into cache key
+        const std::vector<TCloudProperty>* azure_cloud_properties = get_azure_cloud_properties(options);
+        if (azure_cloud_properties != nullptr) {
+            for (const auto& cloud_property : *azure_cloud_properties) {
+                cache_key += cloud_property.key;
+                cache_key += cloud_property.value;
+            }
+        }
+
+        auto it = _cache.find(cache_key);
         if (it != _cache.end()) {
             *handle = it->second;
         } else {
             handle->namenode = namenode;
             RETURN_IF_ERROR(create_hdfs_fs_handle(namenode, handle, options));
-            _cache[id] = *handle;
+            _cache[cache_key] = *handle;
         }
     }
     return Status::OK();
