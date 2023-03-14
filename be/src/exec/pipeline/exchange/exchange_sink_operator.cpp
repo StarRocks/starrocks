7ea4e17ca856656968b7c4b0580cf735bcd63f93
Zhuhe Fang <fzhedu@gmail.com>
2023-03-14 14:39:49 +0800

    refine
    Signed-off-by: Zhuhe Fang <fzhedu@gmail.com>

--- be/src/exec/pipeline/exchange/exchange_sink_operator.cpp
+++ be/src/exec/pipeline/exchange/exchange_sink_operator.cpp
@@ -251,8 +251,8 @@ Status ExchangeSinkOperator::Channel::send_one_chunk(RuntimeState* state, const 
         }
         butil::IOBuf attachment;
         int64_t attachment_physical_bytes = _parent->construct_brpc_attachment(_chunk_request, attachment);
-        TransmitChunkInfo info = {this->_fragment_instance_id, _brpc_stub, std::move(_chunk_request), attachment,
-                                  attachment_physical_bytes, _brpc_dest_addr};
+        TransmitChunkInfo info = {this->_fragment_instance_id, _brpc_stub,     std::move(_chunk_request), attachment,
+                                  attachment_physical_bytes,   _brpc_dest_addr};
         RETURN_IF_ERROR(_parent->_buffer->add_request(info));
         _current_request_bytes = 0;
         _chunk_request.reset();
