6feee9df4c133ab90bbd90df2cd67e708a6048e2
Youngwb <yangwenbo_mailbox@163.com>
2023-03-15 17:31:41 +0800

    [Enhancement] Support auto_refresh_partitions_limit for periodic refresh mv task (#19494)
    * [Enhancement] Support auto_refresh_partitions_limit for periodic refresh mv task
    
    Signed-off-by: Youngwb <yangwenbo_mailbox@163.com>

--- fe/fe-core/src/main/java/com/starrocks/scheduler/TaskRunBuilder.java
+++ fe/fe-core/src/main/java/com/starrocks/scheduler/TaskRunBuilder.java
@@ -8,6 +8,7 @@ import java.util.Map;
 public class TaskRunBuilder {
     private final Task task;
     private Map<String, String> properties;
+    private Constants.TaskType type;
 
     public static TaskRunBuilder newBuilder(Task task) {
         return new TaskRunBuilder(task);
@@ -23,12 +24,17 @@ public class TaskRunBuilder {
         taskRun.setTaskId(task.getId());
         taskRun.setProperties(mergeProperties());
         taskRun.setTask(task);
+        taskRun.setType(getTaskType());
         if (task.getSource().equals(Constants.TaskSource.MV)) {
             taskRun.setProcessor(new PartitionBasedMaterializedViewRefreshProcessor());
         } else {
             taskRun.setProcessor(new SqlTaskRunProcessor());
         }
         return taskRun;
+    }
+
+    private Constants.TaskType getTaskType() {
+        return type != null ? type : task.getType();
     }
 
     private Map<String, String> mergeProperties() {
@@ -50,6 +56,13 @@ public class TaskRunBuilder {
 
     public TaskRunBuilder properties(Map<String, String> properties) {
         this.properties = properties;
+        return this;
+    }
+
+    public TaskRunBuilder type(ExecuteOption option) {
+        if (option.isManual()) {
+            this.type = Constants.TaskType.MANUAL;
+        }
         return this;
     }
 
