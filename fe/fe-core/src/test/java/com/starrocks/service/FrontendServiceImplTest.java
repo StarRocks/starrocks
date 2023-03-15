bc2d5ee470193ae8cc916b7ca50c22b427ef8546
Felix Li <astralidea@163.com>
2023-03-15 17:44:24 +0800

    [BugFix] Fix bug automatic partition create border partition (#19550)
    * Fix bug automatic partition create border partition
    
    Signed-off-by: Astralidea <astralidea@163.com>

--- fe/fe-core/src/test/java/com/starrocks/service/FrontendServiceImplTest.java
+++ fe/fe-core/src/test/java/com/starrocks/service/FrontendServiceImplTest.java
@@ -158,6 +158,19 @@ public class FrontendServiceImplTest {
                         "PROPERTIES(\n" +
                         "    \"replication_num\" = \"1\"\n" +
                         ");")
+                .withTable("CREATE TABLE site_access_border (\n" +
+                        "    event_day DATETIME NOT NULL,\n" +
+                        "    site_id INT DEFAULT '10',\n" +
+                        "    city_code VARCHAR(100),\n" +
+                        "    user_name VARCHAR(32) DEFAULT '',\n" +
+                        "    pv BIGINT DEFAULT '0'\n" +
+                        ")\n" +
+                        "DUPLICATE KEY(event_day, site_id, city_code, user_name)\n" +
+                        "PARTITION BY date_trunc('day', event_day)\n" +
+                        "DISTRIBUTED BY HASH(event_day, site_id)\n" +
+                        "PROPERTIES(\n" +
+                        "    \"replication_num\" = \"1\"\n" +
+                        ");")
                 .withTable("CREATE TABLE site_access_hour (\n" +
                         "    event_day DATETIME,\n" +
                         "    site_id INT DEFAULT '10',\n" +
@@ -331,6 +344,34 @@ public class FrontendServiceImplTest {
         partition = impl.createPartition(request);
         Assert.assertEquals(2, partition.partitions.size());
     }
+
+    @Test
+    public void testCreatePartitionApiBorder() throws TException {
+        Database db = GlobalStateMgr.getCurrentState().getDb("test");
+        Table table = db.getTable("site_access_border");
+        List<List<String>> partitionValues = Lists.newArrayList();
+        List<String> values = Lists.newArrayList();
+        values.add("NULL");
+        values.add("0000-01-01");
+        values.add("9999-12-31");
+        partitionValues.add(values);
+
+        FrontendServiceImpl impl = new FrontendServiceImpl(exeEnv);
+        TCreatePartitionRequest request = new TCreatePartitionRequest();
+        request.setDb_id(db.getId());
+        request.setTable_id(table.getId());
+        request.setPartition_values(partitionValues);
+        TCreatePartitionResult partition = impl.createPartition(request);
+
+        Assert.assertEquals(TStatusCode.OK, partition.getStatus().getStatus_code());
+        Partition p00000101 = table.getPartition("p00000101");
+        Assert.assertNotNull(p00000101);
+        Partition p99991231 = table.getPartition("p99991231");
+        Assert.assertNotNull(p99991231);
+        partition = impl.createPartition(request);
+        Assert.assertEquals(2, partition.partitions.size());
+    }
+
 
     @Test
     public void testCreatePartitionApiHour() throws TException {
