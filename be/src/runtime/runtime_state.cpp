853ba01159b80cb8873338098ebacab12f14c6c5
trueeyu <lxhhust350@qq.com>
2023-03-16 15:11:30 +0800

    Fix
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/runtime/runtime_state.cpp
+++ be/src/runtime/runtime_state.cpp
@@ -147,8 +147,9 @@ void RuntimeState::_init(const TUniqueId& fragment_instance_id, const TQueryOpti
     // if BE was in grayscale upgrade. old BE chunk size was 4096.
     // if FE set a zero batch_size, batch_size will be set to DEFAULT_CHUNK_SIZE
     // (DEFAULT_CHUNK_SIZE was 2048 before version 2.0/2.1.0). which will cause overflow
+    LOG(INFO) << "CHUNK_SIZE: " << _query_options.batch_size;
     if (_query_options.batch_size <= 0) {
-        _query_options.batch_size = DEFAULT_CHUNK_SIZE;
+        _query_options.batch_size = 2048;
     }
 
     _runtime_filter_port = _obj_pool->add(new RuntimeFilterPort(this));
