faece2bd147f1277151f3cc78179faaba6e020ff
blackstar-baba <535650957@qq.com>
2023-03-17 15:21:12 +0800

    Materialized views do not support automatic cooling down (#19626)
    Signed-off-by: blackstar-baba <535650957@qq.com>

--- fe/fe-core/src/test/java/com/starrocks/server/LocalMetaStoreTest.java
+++ fe/fe-core/src/test/java/com/starrocks/server/LocalMetaStoreTest.java
@@ -18,6 +18,7 @@ package com.starrocks.server;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
+import com.starrocks.catalog.DataProperty;
 import com.starrocks.catalog.Database;
 import com.starrocks.catalog.OlapTable;
 import com.starrocks.catalog.Partition;
@@ -26,9 +27,14 @@ import com.starrocks.catalog.Table;
 import com.starrocks.common.DdlException;
 import com.starrocks.common.FeConstants;
 import com.starrocks.common.util.UUIDUtil;
+import com.starrocks.persist.EditLog;
+import com.starrocks.persist.ModifyPartitionInfo;
 import com.starrocks.qe.ConnectContext;
+import com.starrocks.thrift.TStorageMedium;
 import com.starrocks.utframe.StarRocksAssert;
 import com.starrocks.utframe.UtFrameUtils;
+import mockit.Mock;
+import mockit.MockUp;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -39,6 +45,7 @@ import java.util.Set;
 
 public class LocalMetaStoreTest {
     private static ConnectContext connectContext;
+    private static StarRocksAssert starRocksAssert;
 
     @BeforeClass
     public static void beforeClass() throws Exception {
@@ -50,7 +57,7 @@ public class LocalMetaStoreTest {
         // create connect context
         connectContext = UtFrameUtils.createDefaultCtx();
         connectContext.setQueryId(UUIDUtil.genUUID());
-        StarRocksAssert starRocksAssert = new StarRocksAssert(connectContext);
+        starRocksAssert = new StarRocksAssert(connectContext);
 
         starRocksAssert.withDatabase("test").useDatabase("test")
                 .withTable(
@@ -87,5 +94,37 @@ public class LocalMetaStoreTest {
         olapTable.replacePartition("t1", "t1_100");
 
         Assert.assertEquals(newPartition.getId(), olapTable.getPartition("t1").getId());
+    }
+
+    @Test
+    public void testGetPartitionIdToStorageMediumMap() throws Exception {
+        starRocksAssert.withMaterializedView(
+                "CREATE MATERIALIZED VIEW test.mv1\n" +
+                        "distributed by hash(k1) buckets 3\n" +
+                        "refresh async\n" +
+                        "properties(\n" +
+                        "'replication_num' = '1'\n" +
+                        ")\n" +
+                        "as\n" +
+                        "select k1,k2 from test.t1;");
+        Database db = GlobalStateMgr.getCurrentState().getDb("test");
+        new MockUp<PartitionInfo>() {
+            @Mock
+            public DataProperty getDataProperty(long partitionId) {
+                return new DataProperty(TStorageMedium.SSD, 0);
+            }
+        };
+        new MockUp<EditLog>() {
+            @Mock
+            public void logModifyPartition(ModifyPartitionInfo info) {
+                Assert.assertNotNull(info);
+                Assert.assertTrue(db.getTable(info.getTableId()).isLocalTable());
+                Assert.assertEquals(TStorageMedium.HDD, info.getDataProperty().getStorageMedium());
+                Assert.assertEquals(DataProperty.MAX_COOLDOWN_TIME_MS, info.getDataProperty().getCooldownTimeMs());
+            }
+        };
+
+        LocalMetastore localMetastore = connectContext.getGlobalStateMgr().getLocalMetastore();
+        localMetastore.getPartitionIdToStorageMediumMap();
     }
 }
