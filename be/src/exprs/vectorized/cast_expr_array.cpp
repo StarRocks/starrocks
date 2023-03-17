797d17b3667e0bbb685d65fa5061f6a4bb492c4c
stdpain <34912776+stdpain@users.noreply.github.com>
2023-03-17 11:40:32 +0800

    [BugFix] Fix cast json to array may cause a wrong result (#19662)
    Signed-off-by: stdpain <drfeng08@gmail.com>
    (cherry picked from commit 96842f4b71c83c60537e9726e7eb5bbbdcad4d8e)

--- be/src/exprs/vectorized/cast_expr_array.cpp
+++ be/src/exprs/vectorized/cast_expr_array.cpp
@@ -283,7 +283,6 @@ ColumnPtr CastJsonToArray::evaluate(ExprContext* context, vectorized::Chunk* inp
             null_column->append(1);
             continue;
         }
-        null_column->append(0);
         const JsonValue* json_value = src.value(i);
         if (json_value && json_value->get_type() == JsonType::JSON_ARRAY) {
             vpack::Slice json_slice = json_value->to_vslice();
@@ -293,6 +292,7 @@ ColumnPtr CastJsonToArray::evaluate(ExprContext* context, vectorized::Chunk* inp
                 json_column_builder.append(std::move(element_value));
             }
             offset += json_slice.length();
+            null_column->append(0);
         } else {
             null_column->append(1);
         }
