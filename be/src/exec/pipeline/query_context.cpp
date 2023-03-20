61b6acb2547e47073f990ba724dc380e95b1aba1
eyes_on_me <3675229+silverbullet233@users.noreply.github.com>
2023-03-20 10:16:54 +0800

    [Enhancement][Refactor] Reduce the number of spill files (#18828)
    Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>
    Signed-off-by: blanklin030 <luis01@foxmail.com>

--- be/src/exec/pipeline/query_context.cpp
+++ be/src/exec/pipeline/query_context.cpp
@@ -144,9 +144,7 @@ Status QueryContext::init_query_once(workgroup::WorkGroup* wg) {
                 st = maybe_token.status();
             }
 
-            _spill_manager = std::make_unique<QuerySpillManager>();
-            auto init_status = _spill_manager->init(_query_id);
-            st = init_status.ok() ? st : init_status;
+            _spill_manager = std::make_unique<spill::QuerySpillManager>(_query_id);
         });
     }
 
