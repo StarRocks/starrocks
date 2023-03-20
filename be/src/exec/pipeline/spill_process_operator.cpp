e8b0953df24c1371221ca46dcb30157cc7d2790f
eyes_on_me <3675229+silverbullet233@users.noreply.github.com>
2023-03-20 10:16:54 +0800

    [Enhancement][Refactor] Reduce the number of spill files (#18828)
    Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>

--- be/src/exec/pipeline/spill_process_operator.cpp
+++ be/src/exec/pipeline/spill_process_operator.cpp
@@ -44,7 +44,7 @@ StatusOr<ChunkPtr> SpillProcessOperator::pull_chunk(RuntimeState* state) {
         auto chunk = chunk_st.value();
         if (chunk != nullptr && !chunk->is_empty()) {
             RETURN_IF_ERROR(_channel->spiller()->spill(state, std::move(chunk_st.value()), *_channel->io_executor(),
-                                                       MemTrackerGuard(tls_mem_tracker)));
+                                                       spill::MemTrackerGuard(tls_mem_tracker)));
         }
     } else if (chunk_st.status().is_end_of_file()) {
         _channel->current_task().reset();
