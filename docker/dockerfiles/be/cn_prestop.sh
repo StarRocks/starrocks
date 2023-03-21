a85e30ecd921589dc3e29c7f4ddb1e99ee5b071c
Kevin Xiaohua Cai <caixiaohua@starrocks.com>
2023-03-21 23:01:58 +0800

    [Tool][Docker] CN node adapt BE image
    * change $STARROCKS_HOME of CN script to /opt/starrocks/be, so the BE
      image can be directly used as CN image
    * won't build CN image separately in future, just an alias of BE image.
    
    Signed-off-by: Kevin Xiaohua Cai <caixiaohua@starrocks.com>

--- docker/dockerfiles/be/cn_prestop.sh
+++ docker/dockerfiles/be/cn_prestop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 STARROCKS_ROOT=${STARROCKS_ROOT:-"/opt/starrocks"}
-STARROCKS_HOME=${STARROCKS_ROOT}/cn
+STARROCKS_HOME=${STARROCKS_ROOT}/be
 
 # graceful stop cn
 $STARROCKS_HOME/bin/stop_cn.sh -g
