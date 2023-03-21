f1ad7139dc29cdd1cf3bf9e3f0665269bf439759
Youngwb <yangwenbo_mailbox@163.com>
2023-03-21 19:04:39 +0800

    [Enhancement] disable refresh resource external table by default (#19886)
    * [Enhancement] disable refresh resource external table by default
    
    Signed-off-by: Youngwb <yangwenbo_mailbox@163.com>

--- fe/fe-core/src/main/java/com/starrocks/common/Config.java
+++ fe/fe-core/src/main/java/com/starrocks/common/Config.java
@@ -1695,6 +1695,12 @@ public class Config extends ConfigBase {
     public static boolean enable_background_refresh_connector_metadata = false;
 
     /**
+     * Enable background refresh all external tables all partitions metadata based on resource in internal catalog.
+     */
+    @ConfField
+    public static boolean enable_background_refresh_resource_table_metadata = false;
+
+    /**
      * Number of threads to refresh remote file's metadata concurrency.
      */
     @ConfField
