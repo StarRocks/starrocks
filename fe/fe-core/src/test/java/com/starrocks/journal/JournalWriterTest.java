9e9afdd25e24009b0c509730b1edf0aed31fb230
Alex Zhu <zhuming9011@gmail.com>
2023-03-22 17:01:29 +0800

    Fix compile error (#20003)
    Signed-off-by: sduzh <zhuming9011@qq.com>
    (cherry picked from commit 70831689693488ac7c59090a05a6d42549fb66dd)

--- fe/fe-core/src/test/java/com/starrocks/journal/JournalWriterTest.java
+++ fe/fe-core/src/test/java/com/starrocks/journal/JournalWriterTest.java
@@ -12,6 +12,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
