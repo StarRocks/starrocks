0d7d80c5ea8fe8cd8b74160d375c3b3ff628332b
Zhuhe Fang <fzhedu@gmail.com>
2023-03-23 22:04:56 +0800

    fix be ut
    Signed-off-by: Zhuhe Fang <fzhedu@gmail.com>

--- be/src/exprs/function_context.cpp
+++ be/src/exprs/function_context.cpp
@@ -53,7 +53,7 @@ FunctionContext* FunctionContext::create_context(RuntimeState* state, MemPool* p
 
 FunctionContext* FunctionContext::create_test_context() {
     auto* context = new FunctionContext();
-    context->_state = nullptr;
+    context->_state = new RuntimeState();
     return context;
 }
 
