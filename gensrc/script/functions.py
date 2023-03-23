a7320b2a2750280d66ce4b63f50df67a89e6868a
dirtysalt <dirtysalt1987@gmail.com>
2023-03-23 14:33:52 -0700

    [Enhancement] unixtime related functions support int64 (#20119)
    Signed-off-by: yanz <dirtysalt1987@gmail.com>
    (cherry picked from commit 78fa0816c923d96bb54803d475d362c225841c2d)
    
    # Conflicts:
    #	be/src/exprs/time_functions.cpp
    #	fe/fe-core/src/main/java/com/starrocks/qe/CoordinatorPreprocessor.java
    #	fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rewrite/ScalarOperatorFunctions.java
    #	gensrc/thrift/InternalService.thrift

--- gensrc/script/functions.py
+++ gensrc/script/functions.py
@@ -394,6 +394,15 @@ vectorized_functions = [
     [50243, 'str2date', 'DATE', ['VARCHAR', 'VARCHAR'], 'TimeFunctions::str2date', 'TimeFunctions::str_to_date_prepare', 'TimeFunctions::str_to_date_close'],
     [50250, 'time_to_sec', 'BIGINT', ['TIME'], 'TimeFunctions::time_to_sec'],
 
+    # unix timestamp extended version to int64
+    # be sure to put before int32 version, so fe will find signature in order.
+    [50284, 'unix_timestamp', 'BIGINT', [], 'TimeFunctions::to_unix_for_now'],
+    [50285, 'unix_timestamp', 'BIGINT', ['DATETIME'], 'TimeFunctions::to_unix_from_datetime'],
+    [50286, 'unix_timestamp', 'BIGINT', ['DATE'], 'TimeFunctions::to_unix_from_date'],
+    [50287, 'unix_timestamp', 'BIGINT', ['VARCHAR', 'VARCHAR'], 'TimeFunctions::to_unix_from_datetime_with_format'],
+    [50288, 'from_unixtime', 'VARCHAR', ['BIGINT'], 'TimeFunctions::from_unix_to_datetime'],
+    [50289, 'from_unixtime', 'VARCHAR', ['BIGINT', 'VARCHAR'], 'TimeFunctions::from_unix_to_datetime_with_format', 'TimeFunctions::from_unix_prepare', 'TimeFunctions::from_unix_close'],
+
     [50300, 'unix_timestamp', 'INT', [], 'TimeFunctions::to_unix_for_now'],
     [50301, 'unix_timestamp', 'INT', ['DATETIME'], 'TimeFunctions::to_unix_from_datetime'],
     [50302, 'unix_timestamp', 'INT', ['DATE'], 'TimeFunctions::to_unix_from_date'],
