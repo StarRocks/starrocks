e198b1957ad8c6494b16286c9fa6696e7d324708
liuzhongjun89 <liuzhongjun_leo@163.com>
2023-03-23 11:57:38 +0800

    [License]change license header of airbyte connector plugin
    Signed-off-by: liuzhongjun89 <liuzhongjun_leo@163.com>

--- contrib/destination-starrocks/src/main/java/io/airbyte/integrations/destination/starrocks/io/StreamLoadStream.java
+++ contrib/destination-starrocks/src/main/java/io/airbyte/integrations/destination/starrocks/io/StreamLoadStream.java
@@ -1,24 +1,19 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package io.airbyte.integrations.destination.starrocks.io;
 
-import com.fasterxml.jackson.core.io.JsonStringEncoder;
 import io.airbyte.commons.json.Jsons;
 import io.airbyte.protocol.models.v0.AirbyteRecordMessage;
 
@@ -41,11 +36,9 @@ public class StreamLoadStream extends InputStream {
     private byte[] cache;
     private int pos;
     private boolean endStream = false;
-    private JsonStringEncoder jsonEncoder;
 
     public StreamLoadStream(Iterator<AirbyteRecordMessage> recordIter) {
         this.recordIter = recordIter;
-        jsonEncoder = JsonStringEncoder.getInstance();
 
         buffer = ByteBuffer.allocate(DEFAULT_BUFFER_SIZE);
         buffer.position(buffer.capacity());
