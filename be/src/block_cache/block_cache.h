3632fae6ea7f219c80b53798f7a627b611c88cca
GavinMar <yangguansuo@starrocks.com>
2023-03-23 09:01:17 +0000

    [Enhancement] Import starcache submodule to support DLA block cache.
    Signed-off-by: GavinMar <yangguansuo@starrocks.com>

--- be/src/block_cache/block_cache.h
+++ be/src/block_cache/block_cache.h
@@ -1,4 +1,4 @@
-// Copyright 2021-present StarRocks, Inc. All rights reserved.
+// Copyright 2023-present StarRocks, Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -50,10 +50,10 @@ public:
     size_t block_size() const { return _block_size; }
 
 private:
-    BlockCache();
+    BlockCache() = default;
 
     size_t _block_size = 0;
-    std::unique_ptr<KvCache> _kv_cache;
+    std::unique_ptr<KvCache> _kv_cache = nullptr;
 };
 
 } // namespace starrocks
