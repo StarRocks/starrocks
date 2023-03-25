ff2f855fd510a18f78ec890bf356068064e7e13a
dirtysalt <dirtysalt1987@gmail.com>
2023-03-24 22:35:56 -0700

    [Refactor] clean some be configs rarely modified (#20290)
    Signed-off-by: yanz <dirtysalt1987@gmail.com>

--- be/src/exec/connector_scan_node.cpp
+++ be/src/exec/connector_scan_node.cpp
@@ -157,8 +157,8 @@ pipeline::OpFactories ConnectorScanNode::decompose_to_pipeline(pipeline::Pipelin
     bool stream_data_source = _data_source_provider->stream_data_source();
     bool is_stream_pipeline = context->is_stream_pipeline();
     int64_t mem_limit = runtime_state()->query_mem_tracker_ptr()->limit() * config::scan_use_query_mem_ratio;
-    if (config::connector_dynamic_chunk_buffer_limiter_enable && mem_limit > 0) {
-        // port from olap scan node.
+    if (mem_limit > 0) {
+        // port from olap scan node. to control chunk buffer usage, we can control memory consumption to avoid OOM.
         size_t max_buffer_capacity = pipeline::ScanOperator::max_buffer_capacity() * dop;
         size_t default_buffer_capacity = max_buffer_capacity;
         pipeline::ChunkBufferLimiterPtr buffer_limiter = std::make_unique<pipeline::DynamicChunkBufferLimiter>(
@@ -168,14 +168,6 @@ pipeline::OpFactories ConnectorScanNode::decompose_to_pipeline(pipeline::Pipelin
                                       : std::make_shared<pipeline::StreamScanOperatorFactory>(
                                                 context->next_operator_id(), this, dop, std::move(buffer_limiter),
                                                 is_stream_pipeline);
-    } else {
-        scan_op = !stream_data_source
-                          ? std::make_shared<pipeline::ConnectorScanOperatorFactory>(
-                                    context->next_operator_id(), this, dop,
-                                    std::make_unique<pipeline::UnlimitedChunkBufferLimiter>())
-                          : std::make_shared<pipeline::StreamScanOperatorFactory>(
-                                    context->next_operator_id(), this, dop,
-                                    std::make_unique<pipeline::UnlimitedChunkBufferLimiter>(), is_stream_pipeline);
     }
 
     auto&& rc_rf_probe_collector = std::make_shared<RcRfProbeCollector>(1, std::move(this->runtime_filter_collector()));
