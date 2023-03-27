8d90f2f1ff003cde2c642289c5f789c3d4b3b7e2
Kevin Li <ming.moriarty@gmail.com>
2023-03-27 20:56:17 +0800

    fix comments
    Signed-off-by: Kevin Li <ming.moriarty@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/ShowExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/ShowExecutor.java
@@ -483,14 +483,12 @@ public class ShowExecutor {
                     }
                 }
             }
-
-            List<List<String>> rowSets = listMaterializedViewStatus(dbName, materializedViews, singleTableMVs);
-            resultSet = new ShowResultSet(stmt.getMetaData(), rowSets);
-        } catch (Exception e) {
-            LOG.warn("listMaterializedViews failed:", e);
         } finally {
             db.readUnlock();
         }
+
+        List<List<String>> rowSets = listMaterializedViewStatus(dbName, materializedViews, singleTableMVs);
+        resultSet = new ShowResultSet(stmt.getMetaData(), rowSets);
     }
 
     public static List<List<String>> listMaterializedViewStatus(
