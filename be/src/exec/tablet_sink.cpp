StarRocks/starrocks

commit 12b61a4dc89682189386e612f7d5fcb55be67555

[BugFix] Be crash when init AUTO_INCREMENT map and Delete op lead to id wasting (#19526)
Problem 1:
When a new auto increment map for a table init in BE, BE may crash because of this flag, there will be a race condition, which will lead to wrong id assignment, and eventually crash because the vector is out of bounds.

Problem 2:
When using stream load with partial_update = false and delete flag for some data, the chunk in OlapTableSink::send_chunk contain the data which will be delete. And auto increment id will also allocated for those rows means that delete op will waste the auto increment id in this case.

Solution:
1. Check if the row has op = delete in OlapTableSink::send_chunk
2. The need_init is not needed now, we can remove it safely

Signed-off-by: srlch <linzichao@starrocks.com>

be/src/exec/tablet_sink.cpp
@@ -1632,7 +1632,24 @@ Status OlapTableSink::_fill_auto_increment_id_internal(Chunk* chunk, SlotDescrip
     ColumnPtr& data_col = std::dynamic_pointer_cast<NullableColumn>(col)->data_column();
     std::vector<uint8_t> filter(std::dynamic_pointer_cast<NullableColumn>(col)->immutable_null_column_data());
 
+    if (_keys_type == TKeysType::PRIMARY_KEYS) {
+        size_t op_column_id = chunk->num_columns() - 1;
+        ColumnPtr& op_col = chunk->get_column_by_index(op_column_id);
+        const uint8_t* ops = std::dynamic_pointer_cast<NullableColumn>(op_col)->raw_data();
+        size_t row = chunk->num_rows();
+
+        for (size_t i = 0; i < row; ++i) {
+            if (ops[i] == TOpType::DELETE) {
+                filter[i] = 0;
+            }
+        }
+    }
+
     uint32_t null_rows = SIMD::count_nonzero(filter);
+
+    if (null_rows == 0) {
+        return Status::OK();
+    }
 
     switch (slot->type().type) {
     case TYPE_BIGINT: {

be/src/storage/storage_engine.cpp
@@ -1161,7 +1161,11 @@ void StorageEngine::remove_increment_map_by_table_id(int64_t table_id) {
 }
 
 Status StorageEngine::get_next_increment_id_interval(int64_t tableid, size_t num_row, std::vector<int64_t>& ids) {
-    bool need_init = false;
+    if (num_row == 0) {
+        return Status::OK();
+    }
+    DCHECK_EQ(num_row, ids.size());
+
     std::shared_ptr<AutoIncrementMeta> meta;
 
     {
@@ -1169,7 +1173,6 @@ Status StorageEngine::get_next_increment_id_interval(int64_t tableid, size_t num
 
         if (UNLIKELY(_auto_increment_meta_map.find(tableid) == _auto_increment_meta_map.end())) {
             _auto_increment_meta_map.insert({tableid, std::make_shared<AutoIncrementMeta>()});
-            need_init = true;
         }
         meta = _auto_increment_meta_map[tableid];
     }
@@ -1184,7 +1187,7 @@ Status StorageEngine::get_next_increment_id_interval(int64_t tableid, size_t num
 
     size_t cur_avaliable_rows = cur_max_id - cur_avaliable_min_id;
     auto ids_iter = ids.begin();
-    if (UNLIKELY(need_init || num_row > cur_avaliable_rows)) {
+    if (UNLIKELY(num_row > cur_avaliable_rows)) {
         size_t alloc_rows = num_row - cur_avaliable_rows;
         // rpc request for the new available interval from fe
         TAllocateAutoIncrementIdParam alloc_params;

commit 9cbd657c6e2d8edb02d8e5d4525267deafbdd8b4

[BugFix] auto increment insert crash
Problem:
This problem is introduced by commit 12b61a4dc89682189386e612f7d5fcb55be67555. This commit want to check the op column if the loading
contain delete_op. But for the normal INSERT statement, there is no op column and the code here does not check if the col exist or not so led to the crash.

Solution:
check the op column exist or not

Signed-off-by: srlch <linzichao@starrocks.com>

be/src/exec/tablet_sink.cpp
@@ -1680,7 +1680,7 @@ Status OlapTableSink::_fill_auto_increment_id_internal(Chunk* chunk, SlotDescrip
     ColumnPtr& data_col = std::dynamic_pointer_cast<NullableColumn>(col)->data_column();
     std::vector<uint8_t> filter(std::dynamic_pointer_cast<NullableColumn>(col)->immutable_null_column_data());
 
-    if (_keys_type == TKeysType::PRIMARY_KEYS) {
+    if (_keys_type == TKeysType::PRIMARY_KEYS && _output_tuple_desc->slots().back()->col_name() == "__op") {
         size_t op_column_id = chunk->num_columns() - 1;
         ColumnPtr& op_col = chunk->get_column_by_index(op_column_id);
         const uint8_t* ops = std::dynamic_pointer_cast<NullableColumn>(op_col)->raw_data();