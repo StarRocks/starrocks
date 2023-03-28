213ef3636d25f0028549a3a883d5e43ee2419d25
wyb <wybb86@gmail.com>
2023-03-28 17:26:06 +0800

    [Enhancement] Not fill old version data into local cache in compaction and schema change
    In lake tablet compaction and schema change, new version will be created,
    so the old version data is not necessary to be filled into local cache.
    Set skip_fill_local_cache true when reading segment, including footer,
    data and all kinds of index.
    
    Signed-off-by: wyb <wybb86@gmail.com>

--- be/src/storage/rowset/bloom_filter_index_reader.cpp
+++ be/src/storage/rowset/bloom_filter_index_reader.cpp
@@ -54,10 +54,9 @@ BloomFilterIndexReader::~BloomFilterIndexReader() {
     MEM_TRACKER_SAFE_RELEASE(ExecEnv::GetInstance()->bloom_filter_index_mem_tracker(), _mem_usage());
 }
 
-StatusOr<bool> BloomFilterIndexReader::load(FileSystem* fs, const std::string& filename, const BloomFilterIndexPB& meta,
-                                            bool use_page_cache, bool kept_in_memory) {
+StatusOr<bool> BloomFilterIndexReader::load(const IndexReadOptions& opts, const BloomFilterIndexPB& meta) {
     return success_once(_load_once, [&]() {
-        Status st = _do_load(fs, filename, meta, use_page_cache, kept_in_memory);
+        Status st = _do_load(opts, meta);
         if (st.ok()) {
             MEM_TRACKER_SAFE_CONSUME(ExecEnv::GetInstance()->bloom_filter_index_mem_tracker(),
                                      _mem_usage() - sizeof(BloomFilterIndexReader));
@@ -68,14 +67,13 @@ StatusOr<bool> BloomFilterIndexReader::load(FileSystem* fs, const std::string& f
     });
 }
 
-Status BloomFilterIndexReader::_do_load(FileSystem* fs, const std::string& filename, const BloomFilterIndexPB& meta,
-                                        bool use_page_cache, bool kept_in_memory) {
+Status BloomFilterIndexReader::_do_load(const IndexReadOptions& opts, const BloomFilterIndexPB& meta) {
     _typeinfo = get_type_info(TYPE_VARCHAR);
     _algorithm = meta.algorithm();
     _hash_strategy = meta.hash_strategy();
     const IndexedColumnMetaPB& bf_index_meta = meta.bloom_filter();
-    _bloom_filter_reader = std::make_unique<IndexedColumnReader>(fs, filename, bf_index_meta);
-    RETURN_IF_ERROR(_bloom_filter_reader->load(use_page_cache, kept_in_memory));
+    _bloom_filter_reader = std::make_unique<IndexedColumnReader>(opts, bf_index_meta);
+    RETURN_IF_ERROR(_bloom_filter_reader->load());
     return Status::OK();
 }
 
--- be/src/storage/rowset/indexed_column_reader.cpp
+++ be/src/storage/rowset/indexed_column_reader.cpp
@@ -44,10 +44,7 @@ namespace starrocks {
 
 using strings::Substitute;
 
-Status IndexedColumnReader::load(bool use_page_cache, bool kept_in_memory) {
-    _use_page_cache = use_page_cache;
-    _kept_in_memory = kept_in_memory;
-
+Status IndexedColumnReader::load() {
     _type_info = get_type_info((LogicalType)_meta.data_type());
     if (_type_info == nullptr) {
         return Status::NotSupported(strings::Substitute("unsupported type=$0", _meta.data_type()));
@@ -56,7 +53,8 @@ Status IndexedColumnReader::load(bool use_page_cache, bool kept_in_memory) {
     RETURN_IF_ERROR(get_block_compression_codec(_meta.compression(), &_compress_codec));
     _validx_key_coder = get_key_coder(_type_info->type());
 
-    ASSIGN_OR_RETURN(auto read_file, _fs->new_random_access_file(_file_name));
+    RandomAccessFileOptions file_opts{.skip_fill_local_cache = _opts.skip_fill_local_cache};
+    ASSIGN_OR_RETURN(auto read_file, _fs->new_random_access_file(file_opts, _file_name));
     // read and parse ordinal index page when exists
     if (_meta.has_ordinal_index_meta()) {
         if (_meta.ordinal_index_meta().is_root_data_page()) {
@@ -99,15 +97,16 @@ Status IndexedColumnReader::read_page(RandomAccessFile* read_file, const PagePoi
     opts.codec = _compress_codec;
     OlapReaderStatistics tmp_stats;
     opts.stats = &tmp_stats;
-    opts.use_page_cache = _use_page_cache;
-    opts.kept_in_memory = _kept_in_memory;
+    opts.use_page_cache = _opts.use_page_cache;
+    opts.kept_in_memory = _opts.kept_in_memory;
     opts.encoding_type = _encoding_info->encoding();
 
     return PageIO::read_and_decompress_page(opts, handle, body, footer);
 }
 
 Status IndexedColumnReader::new_iterator(std::unique_ptr<IndexedColumnIterator>* iter) {
-    ASSIGN_OR_RETURN(auto file, _fs->new_random_access_file(_file_name));
+    RandomAccessFileOptions file_opts{.skip_fill_local_cache = _opts.skip_fill_local_cache};
+    ASSIGN_OR_RETURN(auto file, _fs->new_random_access_file(file_opts, _file_name));
     iter->reset(new IndexedColumnIterator(this, std::move(file)));
     return Status::OK();
 }
