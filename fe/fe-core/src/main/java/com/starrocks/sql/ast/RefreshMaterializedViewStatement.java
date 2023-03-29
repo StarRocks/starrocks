24f1c66e9b115e1a250ed7152c1829c84e5a3d76
fenghuang <8199608+huangfeng1993@users.noreply.github.com>
2023-03-29 09:54:05 +0800

    [Enhancement] return taskId when manually refresh materialized view. (#20232)
    Signed-off-by: huangfeng1993 <715187657@qq.com>
    (cherry picked from commit 1b70fea7f6527cfc1c7eda76e0125571ad0d5b08)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/sql/ast/RefreshMaterializedViewStatement.java

--- fe/fe-core/src/main/java/com/starrocks/sql/ast/RefreshMaterializedViewStatement.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/ast/RefreshMaterializedViewStatement.java
@@ -2,11 +2,23 @@
 package com.starrocks.sql.ast;
 
 import com.starrocks.analysis.TableName;
+<<<<<<< HEAD
+=======
+import com.starrocks.catalog.Column;
+import com.starrocks.catalog.ScalarType;
+import com.starrocks.qe.ShowResultSetMetaData;
+import com.starrocks.sql.parser.NodePosition;
+>>>>>>> 1b70fea7f ([Enhancement] return taskId when manually refresh materialized view. (#20232))
 
 public class RefreshMaterializedViewStatement extends DdlStmt {
     private final TableName mvName;
     private final PartitionRangeDesc partitionRangeDesc;
     private final boolean forceRefresh;
+
+    public static final ShowResultSetMetaData META_DATA =
+            ShowResultSetMetaData.builder()
+                    .addColumn(new Column("QUERY_ID", ScalarType.createVarchar(60)))
+                    .build();
 
     public RefreshMaterializedViewStatement(TableName mvName,
                                             PartitionRangeDesc partitionRangeDesc,
