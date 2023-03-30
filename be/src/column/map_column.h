1970823592055abccebea60a54a723a77d9e57aa
trueeyu <lxhhust350@qq.com>
2023-03-30 18:11:28 +0800

    Optimize the performance of bitmap_contains on cross join for non-pipeline engine (#20653)
    For large bitmap, if we deep make many copies (chunk size), it will consume much memory.
    
    Currently, in the executor and the expr framework, the elements in the Column will not be modified in place, so it is safe to do so. Currently only ObjectColumn real supports shallow copy.
    
    Later, Column::append and Column::append_selective also need to be supported, which can be extended to more scenarios
    
    select t1.c1, bitmap_count(t1.c2) from l2, t1 where bitmap_contains(c2, lo_orderkey);
    
    Before Optimization: peak memory (3.36G), consume time(7s)
    After Optimization: peak memory (3M), consume time (0.01s)
    
    Signed-off-by: trueeyu <lxhhust350@qq.com>
    (cherry picked from commit 52317c7eba9425b7b7ff70bcaece88c726d753ae)
    
    # Conflicts:
    #	be/src/column/adaptive_nullable_column.cpp
    #	be/src/column/adaptive_nullable_column.h
    #	be/src/column/array_column.cpp
    #	be/src/column/column.h
    #	be/src/column/map_column.cpp
    #	be/src/column/map_column.h
    #	be/src/column/object_column.cpp
    #	be/src/column/struct_column.cpp
    #	be/src/column/struct_column.h

--- be/src/column/map_column.h
+++ be/src/column/map_column.h
@@ -1,4 +1,16 @@
-// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #pragma once
 
@@ -6,38 +18,42 @@
 
 #include "column/column.h"
 #include "column/fixed_length_column.h"
+#include "column/nullable_column.h"
+#include "column/vectorized_fwd.h"
 
-namespace starrocks::vectorized {
+namespace starrocks {
 
-class ArrayColumn final : public ColumnFactory<Column, ArrayColumn> {
-    friend class ColumnFactory<Column, ArrayColumn>;
+class MapColumn final : public ColumnFactory<Column, MapColumn> {
+    friend class ColumnFactory<Column, MapColumn>;
 
 public:
     using ValueType = void;
 
-    ArrayColumn(ColumnPtr elements, UInt32Column::Ptr offests);
+    MapColumn(ColumnPtr keys, ColumnPtr values, UInt32Column::Ptr offests);
 
-    ArrayColumn(const ArrayColumn& rhs)
-            : _elements(rhs._elements->clone_shared()),
+    MapColumn(const MapColumn& rhs)
+            : _keys(rhs._keys->clone_shared()),
+              _values(rhs._values->clone_shared()),
               _offsets(std::static_pointer_cast<UInt32Column>(rhs._offsets->clone_shared())) {}
 
-    ArrayColumn(ArrayColumn&& rhs) noexcept : _elements(std::move(rhs._elements)), _offsets(std::move(rhs._offsets)) {}
+    MapColumn(MapColumn&& rhs) noexcept
+            : _keys(std::move(rhs._keys)), _values(std::move(rhs._values)), _offsets(std::move(rhs._offsets)) {}
 
-    ArrayColumn& operator=(const ArrayColumn& rhs) {
-        ArrayColumn tmp(rhs);
+    MapColumn& operator=(const MapColumn& rhs) {
+        MapColumn tmp(rhs);
         this->swap_column(tmp);
         return *this;
     }
 
-    ArrayColumn& operator=(ArrayColumn&& rhs) noexcept {
-        ArrayColumn tmp(std::move(rhs));
+    MapColumn& operator=(MapColumn&& rhs) noexcept {
+        MapColumn tmp(std::move(rhs));
         this->swap_column(tmp);
         return *this;
     }
 
-    ~ArrayColumn() override = default;
+    ~MapColumn() override = default;
 
-    bool is_array() const override { return true; }
+    bool is_map() const override { return true; }
 
     const uint8_t* raw_data() const override;
 
@@ -47,9 +63,9 @@ public:
 
     size_t capacity() const override;
 
-    size_t type_size() const override { return sizeof(DatumArray); }
+    size_t type_size() const override { return sizeof(DatumMap); }
 
-    size_t byte_size() const override { return _elements->byte_size() + _offsets->byte_size(); }
+    size_t byte_size() const override { return _keys->byte_size() + _values->byte_size() + _offsets->byte_size(); }
     size_t byte_size(size_t from, size_t size) const override;
 
     size_t byte_size(size_t idx) const override;
@@ -107,8 +123,10 @@ public:
 
     int compare_at(size_t left, size_t right, const Column& right_column, int nan_direction_hint) const override;
 
-    void crc32_hash_at(uint32_t* seed, int32_t idx) const override;
-    void fnv_hash_at(uint32_t* seed, int32_t idx) const override;
+    bool equals(size_t left, const Column& rhs, size_t right) const override;
+
+    void crc32_hash_at(uint32_t* seed, uint32_t idx) const override;
+    void fnv_hash_at(uint32_t* seed, uint32_t idx) const override;
     void fnv_hash(uint32_t* hash, uint32_t from, uint32_t to) const override;
 
     void crc32_hash(uint32_t* hash, uint32_t from, uint32_t to) const override;
@@ -117,58 +135,74 @@ public:
 
     void put_mysql_row_buffer(MysqlRowBuffer* buf, size_t idx) const override;
 
-    std::string get_name() const override { return "array"; }
+    std::string get_name() const override { return "map"; }
 
     Datum get(size_t idx) const override;
 
-    size_t get_element_size(size_t idx) const;
-
     bool set_null(size_t idx) override;
 
-    size_t memory_usage() const override { return _elements->memory_usage() + _offsets->memory_usage(); }
-
-    size_t container_memory_usage() const override {
-        return _elements->container_memory_usage() + _offsets->container_memory_usage();
+    size_t memory_usage() const override {
+        return _keys->memory_usage() + _values->memory_usage() + _offsets->memory_usage();
     }
 
-    size_t element_memory_usage(size_t from, size_t size) const override;
+    size_t container_memory_usage() const override {
+        return _keys->container_memory_usage() + _values->container_memory_usage() + _offsets->container_memory_usage();
+    }
+
+    size_t reference_memory_usage(size_t from, size_t size) const override;
 
     void swap_column(Column& rhs) override;
 
     void reset_column() override;
-
-    const Column& elements() const { return *_elements; }
-    ColumnPtr& elements_column() { return _elements; }
 
     const UInt32Column& offsets() const { return *_offsets; }
     UInt32Column::Ptr& offsets_column() { return _offsets; }
 
     bool is_nullable() const override { return false; }
 
-    std::string debug_item(uint32_t idx) const override;
+    std::string debug_item(size_t idx) const override;
 
     std::string debug_string() const override;
 
     bool capacity_limit_reached(std::string* msg = nullptr) const override {
-        return _elements->capacity_limit_reached(msg) || _offsets->capacity_limit_reached(msg);
+        return _keys->capacity_limit_reached(msg) || _values->capacity_limit_reached(msg) ||
+               _offsets->capacity_limit_reached(msg);
     }
 
     StatusOr<ColumnPtr> upgrade_if_overflow() override;
 
     StatusOr<ColumnPtr> downgrade() override;
 
-    bool has_large_column() const override { return _elements->has_large_column(); }
+    bool has_large_column() const override { return _keys->has_large_column() || _values->has_large_column(); }
 
     void check_or_die() const override;
 
+    const Column& keys() const { return *_keys; }
+    ColumnPtr& keys_column() { return _keys; }
+    ColumnPtr keys_column() const { return _keys; }
+
+    const Column& values() const { return *_values; }
+    ColumnPtr& values_column() { return _values; }
+    ColumnPtr values_column() const { return _values; }
+
+    size_t get_map_size(size_t idx) const;
+    std::pair<size_t, size_t> get_map_offset_size(size_t idx) const;
+
+    Status unfold_const_children(const starrocks::TypeDescriptor& type) override;
+
+    void remove_duplicated_keys();
+
 private:
-    // _elements must be NullableColumn
-    ColumnPtr _elements;
-    // Offsets column will store the start position of every array element.
+    // Keys must be NullableColumn to facilitate handling nested types.
+    ColumnPtr _keys;
+    // Values must be NullableColumn to facilitate handling nested types.
+    ColumnPtr _values;
+    // Offsets column will store the start position of every map element.
     // Offsets store more one data to indicate the end position.
-    // For example, [1, 2, 3], [4, 5, 6].
+    // For example, map Column: m1keys [k1, k2, k3], m2keys [k4, k5, k6]
+    //                          m1vals [v1, v2, v3], m2vals [v4, v5, v6]
     // The two element array has three offsets(0, 3, 6)
     UInt32Column::Ptr _offsets;
 };
 
-} // namespace starrocks::vectorized
+} // namespace starrocks
