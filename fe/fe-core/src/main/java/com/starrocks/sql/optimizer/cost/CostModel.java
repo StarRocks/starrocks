b74156d3ed3fdc5634ce95bad093f4b6565f8caa
ABingHuang <codekhuang@163.com>
2023-03-30 17:08:21 +0800

    modify cost
    Signed-off-by: ABingHuang <codekhuang@163.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/cost/CostModel.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/cost/CostModel.java
@@ -130,7 +130,7 @@ public class CostModel {
             Statistics statistics = context.getStatistics();
             Preconditions.checkNotNull(statistics);
             if (node.getTable().isMaterializedView() && context.hasExtraStatistics()) {
-                return CostEstimate.zero();
+                return CostEstimate.of(1, 0, 0);
                 /*
                 ColumnRefSet usedColumns = node.getUsedColumns();
                 return CostEstimate.of(statistics.getOutputSize(usedColumns), 0, 0);
