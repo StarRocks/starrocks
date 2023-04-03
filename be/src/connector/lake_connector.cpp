372e9ea8665c0bc30ce3f2037ec444f4437181a6
wyb <wybb86@gmail.com>
2023-04-03 10:33:19 +0800

    [BugFix] Make cloud native table shared scan same as olap table (#20775)
    Signed-off-by: wyb <wybb86@gmail.com>

--- be/src/connector/lake_connector.cpp
+++ be/src/connector/lake_connector.cpp
@@ -163,6 +163,9 @@ public:
 
     Status init(ObjectPool* pool, RuntimeState* state) override;
 
+    // Make cloud native table behavior same as olap table
+    bool always_shared_scan() const override { return false; }
+
 protected:
     ConnectorScanNode* _scan_node;
     const TLakeScanNode _t_lake_scan_node;
