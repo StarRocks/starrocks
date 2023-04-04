3be9c3f3b9fa967a0794025f2f8c04357ec048ab
yanz <dirtysalt1987@gmail.com>
2023-04-03 17:35:03 -0700

    fix fe side
    Signed-off-by: yanz <dirtysalt1987@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/CoordinatorPreprocessor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/CoordinatorPreprocessor.java
@@ -1018,7 +1018,7 @@ public class CoordinatorPreprocessor {
         queryOptions.setEnable_populate_block_cache(sv.getEnablePopulateBlockCache());
         queryOptions.setHudi_mor_force_jni_reader(sv.getHudiMORForceJNIReader());
         queryOptions.setIo_tasks_per_scan_operator(sv.getIoTasksPerScanOperator());
-        queryOptions.setIo_tasks_connector_max_ratio(sv.getIoTasksConnectorMaxRatio());
+        queryOptions.setConnector_io_tasks_per_scan_operator(sv.getConnectorIoTasksPerScanOperator());
 
         // set scan ranges/locations for scan nodes
         for (ScanNode scanNode : scanNodes) {
