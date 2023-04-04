564f8952f2c1f733621d510d496d1cbb73ee163e
yanz <dirtysalt1987@gmail.com>
2023-04-03 17:30:06 -0700

    fix be side
    Signed-off-by: yanz <dirtysalt1987@gmail.com>

--- be/src/exec/connector_scan_node.cpp
+++ be/src/exec/connector_scan_node.cpp
@@ -128,11 +128,10 @@ Status ConnectorScanNode::init(const TPlanNode& tnode, RuntimeState* state) {
 
 void ConnectorScanNode::_adjust_io_tasks_per_scan_operator(RuntimeState* state) {
     const TQueryOptions& options = state->query_options();
-    int32_t ratio = config::io_tasks_connector_max_ratio;
-    if (options.__isset.io_tasks_connector_max_ratio) {
-        ratio = options.io_tasks_connector_max_ratio;
+    _io_tasks_per_scan_operator = config::connector_io_tasks_per_scan_operator;
+    if (options.__isset.connector_io_tasks_per_scan_operator) {
+        _io_tasks_per_scan_operator = options.connector_io_tasks_per_scan_operator;
     }
-    _io_tasks_per_scan_operator *= ratio;
 }
 
 void ConnectorScanNode::_estimate_scan_row_bytes() {
