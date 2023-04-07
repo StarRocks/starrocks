5cc540ee1baf57fb730a9e604225664371db0141
srlch <111035020+srlch@users.noreply.github.com>
2023-04-07 16:26:13 +0800

    [Enhancement] remove delete restriction on the auto-increment table(#20702) (#20703)
    In current implementation, when the auto increment column is value column but not the key column,
    this kind of table can not support delete. The pr will remove restriction, but has some side affect as following:
    
    1. If the delete-partial update conflict happen, partial update operation maybe lost.
    2. If it is the streamload combine with the delete and partial update ops and manipulate on a row which has existed
       in the previous version, all the partial update ops after delete ops maybe lost for this row if they contained in
       different segment file.
    
    Signed-off-by: srlch <linzichao@starrocks.com>

--- gensrc/thrift/DataSinks.thrift
+++ gensrc/thrift/DataSinks.thrift
@@ -194,7 +194,7 @@ struct TOlapTableSink {
     20: optional string merge_condition
     21: optional bool null_expr_in_auto_increment
     22: optional bool miss_auto_increment_column
-    23: optional bool abort_delete
+    23: optional bool abort_delete // Deprecated
     24: optional i32 auto_increment_slot_id
     25: optional Types.TPartialUpdateMode partial_update_mode
 }
