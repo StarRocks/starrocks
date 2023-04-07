a7add89f7b7639fd182a3874d1a136ae28239761
zaorangyang <zaorangy@gmail.com>
2023-04-07 17:49:05 +0800

    [BugFix] Fix disk space occupation problems of multiple replicas (#20590)

--- fe/fe-core/src/main/java/com/starrocks/clone/TabletScheduler.java
+++ fe/fe-core/src/main/java/com/starrocks/clone/TabletScheduler.java
@@ -1172,8 +1172,7 @@ public class TabletScheduler extends LeaderDaemon {
             // process.
             sendDeleteReplicaTask(replica.getBackendId(), tabletCtx.getTabletId(), tabletCtx.getSchemaHash());
         }
-
-        invertedIndex.markTabletForceDelete(tabletCtx.getTabletId());
+        invertedIndex.markTabletForceDelete(tabletCtx.getTabletId(), replica.getBackendId());
 
         // write edit log
         ReplicaPersistInfo info = ReplicaPersistInfo.createForDelete(tabletCtx.getDbId(),
