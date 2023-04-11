f2fbbf2108dfc2ecbed9e68553641d1f7d6bfc64
Gavin <yangguansuo@starrocks.com>
2023-04-11 20:39:58 +0800

    [Enhancement] Import starcache submodule to support DLA block cache. (#20927)
    Import starcache submodule as a new block cache engine which support KV interfaces, and provides memory and disk cache. Currently, we support both cachelib and starcache engines, and we set the starcache as the default engine.
    
    Signed-off-by: GavinMar <yangguansuo@starrocks.com>

--- be/src/block_cache/block_cache.h
+++ be/src/block_cache/block_cache.h
@@ -50,7 +50,7 @@ public:
     size_t block_size() const { return _block_size; }
 
 private:
-    BlockCache();
+    BlockCache() = default;
 
     size_t _block_size = 0;
     std::unique_ptr<KvCache> _kv_cache;
