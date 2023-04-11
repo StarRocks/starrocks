625b49e11772b7c8debbfbbd5efe08fd9a89e5ba
mlimwxxnn <53778822+mlimwxxnn@users.noreply.github.com>
2023-04-11 20:52:05 +0800

    [Feature] Support function next_day (#21135)
    NEW FUNCTION: `next_day`
    
    Test sql:
    ```sql
    CREATE TABLE IF NOT EXISTS `next_day_test` (
      `d1` DATE,
      `d2` DATETIME
    )
    DISTRIBUTED BY HASH(`d1`)
    PROPERTIES(
      "replication_num" = "1"
    );
    
    INSERT INTO `next_day_test`(d1, d2)
    VALUES ('2023-02-27', '2023-02-27 20:13:14'),
           ('2023-02-28', '2023-02-28 20:13:14'),
           ('2024-02-28', '2024-02-28 20:13:14'),
           ('2024-02-29', '2024-02-29 20:13:14');
    
    CREATE TABLE IF NOT EXISTS `dow_test` (
      `d3` DATE,
      `dow_2` CHAR(20),
      `dow_3` VARCHAR(30),
      `dow_full` STRING
    )
    DISTRIBUTED BY HASH(`d3`)
    PROPERTIES(
      "replication_num" = "1"
    );
    
    INSERT INTO `dow_test`(d3, dow_2, dow_3, dow_full)
    VALUES ('2023-04-01', 'Mo', 'Mon', 'Monday'),
           ('2023-04-02', 'Tu', 'Tue', 'Tuesday'),
           ('2023-04-03', 'We', 'Wed', 'Wednesday'),
           ('2023-02-28', 'Th', 'Thu', 'Thursday'),
           ('2024-02-29', 'Fr', 'Fri', 'Friday');
    ```
    Test results:
    ```Plain Text
    MySQL > select next_day('2023-04-06', 'Monday');
    +----------------------------------+
    | next_day('2023-04-06', 'Monday') |
    +----------------------------------+
    | 2023-04-10                       |
    +----------------------------------+
    
    MySQL > select next_day('2023-04-06', 'Tue');
    +-------------------------------+
    | next_day('2023-04-06', 'Tue') |
    +-------------------------------+
    | 2023-04-11                    |
    +-------------------------------+
    
    MySQL > select next_day('2023-04-06 20:13:14', 'Fr');
    +---------------------------------------+
    | next_day('2023-04-06 20:13:14', 'Fr') |
    +---------------------------------------+
    | 2023-04-07                            |
    +---------------------------------------+
    
    MySQL >  select d1, next_day(d1, 'Saturday'), d2, next_day(d2, 'Saturday') from next_day_test order by d1;
    +------------+--------------------------+---------------------+--------------------------+
    | d1         | next_day(d1, 'Saturday') | d2                  | next_day(d2, 'Saturday') |
    +------------+--------------------------+---------------------+--------------------------+
    | 2023-02-27 | 2023-03-04               | 2023-02-27 20:13:14 | 2023-03-04               |
    | 2023-02-28 | 2023-03-04               | 2023-02-28 20:13:14 | 2023-03-04               |
    | 2024-02-28 | 2024-03-02               | 2024-02-28 20:13:14 | 2024-03-02               |
    | 2024-02-29 | 2024-03-02               | 2024-02-29 20:13:14 | 2024-03-02               |
    +------------+--------------------------+---------------------+--------------------------+
    
    MySQL > select d3, dow_2, next_day(d3, dow_2), dow_3, next_day(d3, dow_3) from dow_test order by d3;
    +------------+-------+---------------------+-------+---------------------+
    | d3         | dow_2 | next_day(d3, dow_2) | dow_3 | next_day(d3, dow_3) |
    +------------+-------+---------------------+-------+---------------------+
    | 2023-02-28 | Th    | 2023-03-02          | Thu   | 2023-03-02          |
    | 2023-04-01 | Mo    | 2023-04-03          | Mon   | 2023-04-03          |
    | 2023-04-02 | Tu    | 2023-04-04          | Tue   | 2023-04-04          |
    | 2023-04-03 | We    | 2023-04-05          | Wed   | 2023-04-05          |
    | 2024-02-29 | Fr    | 2024-03-01          | Fri   | 2024-03-01          |
    +------------+-------+---------------------+-------+---------------------+
    
    MySQL > select d1, dow_full, next_day(d1, dow_full) from next_day_test a join dow_test b on a.d1 = b.d3;
    +------------+----------+------------------------+
    | d1         | dow_full | next_day(d1, dow_full) |
    +------------+----------+------------------------+
    | 2023-02-28 | Thursday | 2023-03-02             |
    | 2024-02-29 | Friday   | 2024-03-01             |
    +------------+----------+------------------------+
    ```
    
    ---------
    
    Signed-off-by: mlimwxxnn <534370904@qq.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/optimizer/rewrite/ScalarOperatorFunctionsTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/optimizer/rewrite/ScalarOperatorFunctionsTest.java
@@ -551,6 +551,16 @@ public class ScalarOperatorFunctionsTest {
         assertEquals(now, ScalarOperatorFunctions.curDate().getDate());
     }
 
+
+    @Test
+    public void nextDay() {
+        assertEquals("2015-03-29T09:23:55", ScalarOperatorFunctions.nextDay(O_DT_20150323_092355,
+                ConstantOperator.createVarchar("Sunday")).getDate().toString());
+        Assert.assertThrows("undefine_dow not supported in next_day dow_string", IllegalArgumentException.class,
+                () -> ScalarOperatorFunctions.nextDay(O_DT_20150323_092355, ConstantOperator.createVarchar("undefine_dow"))
+                        .getVarchar());
+    }
+
     @Test
     public void floor() {
         assertEquals(100, ScalarOperatorFunctions.floor(O_FLOAT_100).getBigint());
