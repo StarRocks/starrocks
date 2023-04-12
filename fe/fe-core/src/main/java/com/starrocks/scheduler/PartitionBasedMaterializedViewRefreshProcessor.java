30dbe89124a03ab4cf25233b8738e5124f48fd9d
Husky Zeng <zengqingdong.zqd@alibaba-inc.com>
2023-04-12 19:15:29 +0800

    [BugFix] Fix mv may not completely refresh when create with property partition_refresh_number. (#21442)
    Signed-off-by: zengqingdong <zengqingdong.zqd@alibaba-inc.com>

--- fe/fe-core/src/main/java/com/starrocks/scheduler/PartitionBasedMaterializedViewRefreshProcessor.java
+++ fe/fe-core/src/main/java/com/starrocks/scheduler/PartitionBasedMaterializedViewRefreshProcessor.java
@@ -340,6 +340,9 @@ public class PartitionBasedMaterializedViewRefreshProcessor extends BaseTaskRunP
         for (Map.Entry<Long, Map<String, MaterializedView.BasePartitionInfo>> tableEntry
                 : changedTablePartitionInfos.entrySet()) {
             Long tableId = tableEntry.getKey();
+            if (mvContext.hasNextBatchPartition() && tableId != materializedView.getId()) {
+                continue;
+            }
             if (!currentVersionMap.containsKey(tableId)) {
                 currentVersionMap.put(tableId, Maps.newHashMap());
             }
@@ -373,6 +376,9 @@ public class PartitionBasedMaterializedViewRefreshProcessor extends BaseTaskRunP
         for (Map.Entry<BaseTableInfo, Map<String, MaterializedView.BasePartitionInfo>> tableEntry
                 : changedTablePartitionInfos.entrySet()) {
             BaseTableInfo baseTableInfo = tableEntry.getKey();
+            if (mvContext.hasNextBatchPartition() && baseTableInfo.getTableId() != materializedView.getId()) {
+                continue;
+            }
             if (!currentVersionMap.containsKey(baseTableInfo)) {
                 currentVersionMap.put(baseTableInfo, Maps.newHashMap());
             }
