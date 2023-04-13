2971707a42bbf78951235e11f3f660f78a3889d0
Youngwb <yangwenbo_mailbox@163.com>
2023-04-13 19:39:51 +0800

    [UT] add some map type test for trino parser
    Signed-off-by: Youngwb <yangwenbo_mailbox@163.com>

--- fe/fe-core/src/test/java/com/starrocks/connector/parser/trino/TrinoFunctionTransformTest.java
+++ fe/fe-core/src/test/java/com/starrocks/connector/parser/trino/TrinoFunctionTransformTest.java
@@ -65,9 +65,6 @@ public class TrinoFunctionTransformTest extends TrinoTestBase {
         sql = "select contains_sequence(c1, array['1','2']) from test_array";
         assertPlanContains(sql, "array_contains_all(2: c1, ['1','2'])");
 
-        sql = "select contains_sequence(c1, array['1','2']) from test_array";
-        assertPlanContains(sql, "array_contains_all(2: c1, ['1','2'])");
-
         sql = "select slice(array[1,2,3,4], 2, 2)";
         assertPlanContains(sql, "array_slice([1,2,3,4], 2, 2)");
     }
