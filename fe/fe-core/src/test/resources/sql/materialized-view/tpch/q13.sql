7ebfc0905094aee7e351c2063e8f5ab970f47747
Kevin Li <ming.moriarty@gmail.com>
2023-04-14 00:09:54 +0800

    [BugFix] Distinguish join's on-predicates and where-predicates for non inner/cross join (#21178)
    (cherry picked from commit 45eee6b95d6f694fda7b640beb6389064239b93c)

--- fe/fe-core/src/test/resources/sql/materialized-view/tpch/q13.sql
+++ fe/fe-core/src/test/resources/sql/materialized-view/tpch/q13.sql
@@ -20,14 +20,16 @@ order by
     custdist desc,
     c_count desc ;
 [result]
-TOP-N (order by [[19: count DESC NULLS LAST, 18: count DESC NULLS LAST]])
-    TOP-N (order by [[19: count DESC NULLS LAST, 18: count DESC NULLS LAST]])
-        AGGREGATE ([GLOBAL] aggregate [{19: count=count(19: count)}] group by [[18: count]] having [null]
-            EXCHANGE SHUFFLE[18]
-                AGGREGATE ([LOCAL] aggregate [{19: count=count()}] group by [[18: count]] having [null]
-                    AGGREGATE ([GLOBAL] aggregate [{89: count=sum(89: count)}] group by [[81: c_custkey]] having [null]
-                        EXCHANGE SHUFFLE[81]
-                            AGGREGATE ([LOCAL] aggregate [{89: count=sum(83: c_count)}] group by [[81: c_custkey]] having [null]
-                                SCAN (mv[customer_order_mv_agg_mv1] columns[81: c_custkey, 82: o_comment, 83: c_count] predicate[NOT 82: o_comment LIKE %unusual%deposits%])
+TOP-N (order by [[: count DESC NULLS LAST, : count DESC NULLS LAST]])
+    TOP-N (order by [[: count DESC NULLS LAST, : count DESC NULLS LAST]])
+        AGGREGATE ([GLOBAL] aggregate [{: count=count(: count)}] group by [[: count]] having [null]
+            EXCHANGE SHUFFLE[]
+                AGGREGATE ([LOCAL] aggregate [{: count=count()}] group by [[: count]] having [null]
+                    AGGREGATE ([GLOBAL] aggregate [{: count=count(: o_orderkey)}] group by [[: c_custkey]] having [null]
+                        RIGHT OUTER JOIN (join-predicate [: o_custkey = : c_custkey] post-join-predicate [null])
+                            EXCHANGE SHUFFLE[]
+                                SCAN (table[orders] columns[: o_comment, : o_orderkey, : o_custkey] predicate[NOT : o_comment LIKE %unusual%deposits%])
+                            EXCHANGE SHUFFLE[]
+                                SCAN (table[customer] columns[: c_custkey] predicate[null])
 [end]
 
