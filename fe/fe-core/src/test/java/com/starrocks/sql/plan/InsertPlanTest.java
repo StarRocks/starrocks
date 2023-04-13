095718a1ff28d6a5926d6b7d72495c682c8a9f02
Zhuhe Fang <fzhedu@gmail.com>
2023-04-13 17:49:10 +0800

    update tests
    Signed-off-by: Zhuhe Fang <fzhedu@gmail.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/InsertPlanTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/InsertPlanTest.java
@@ -738,10 +738,13 @@ public class InsertPlanTest extends PlanTestBase {
     @Test
     public void testInsertMapColumn() throws Exception {
         String explainString = getInsertExecPlan("insert into tmap values (2,2,{})");
-        Assert.assertTrue(explainString.contains("2 | 2 | MAP<bigint(20),char(20)>{}"));
+        Assert.assertTrue(explainString.contains("2 | 2 | {}"));
 
         explainString = getInsertExecPlan("insert into tmap values (2,2,null)");
         Assert.assertTrue(explainString.contains("2 | 2 | NULL"));
+
+        explainString = getInsertExecPlan("insert into tmap values (2,2,{2:2})");
+        Assert.assertTrue(explainString.contains("2 | 2 | {2:'2'}")); // implicit cast
     }
 
     @Test
