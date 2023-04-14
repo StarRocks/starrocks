e7316ac65c3cdba56e264aaf5975e510c30d82e7
Alex Zhu <zhuming9011@gmail.com>
2023-04-14 16:17:13 +0800

    [Enhancement] Exclude be/src/thirdparty from clang-format (#21622)
    Signed-off-by: sduzh <zhuming9011@qq.com>

--- build-support/clang-format.sh
+++ build-support/clang-format.sh
@@ -30,6 +30,8 @@ export STARROCKS_HOME=`cd "${ROOT}/.."; pwd`
 
 CLANG_FORMAT=${CLANG_FORMAT_BINARY:=$(which clang-format)}
 
-python3 ${STARROCKS_HOME}/build-support/run_clang_format.py --clang_format_binary="${CLANG_FORMAT}" --fix --source_dirs="${STARROCKS_HOME}/be/src","${STARROCKS_HOME}/be/test"
+python3 ${STARROCKS_HOME}/build-support/run_clang_format.py --clang_format_binary="${CLANG_FORMAT}" --fix \
+	--source_dirs="${STARROCKS_HOME}/be/src","${STARROCKS_HOME}/be/test" \
+        --exclude_globs="${STARROCKS_HOME}/build-support/excludes"
 
 
