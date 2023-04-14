8255a2195e3a18be432aa63f9476642d181dd2b2
packy92 <wangchao@starrocks.com>
2023-04-14 13:37:58 +0800

    fix ut
    Signed-off-by: packy92 <wangchao@starrocks.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/SubqueryTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/SubqueryTest.java
@@ -232,8 +232,9 @@ public class SubqueryTest extends PlanTestBase {
                     ")IS NULL";
 
             String plan = getFragmentPlan(sql);
-            assertContains(plan, "  18:Project\n" +
+            assertContains(plan, "18:Project\n" +
                     "  |  <slot 15> : 1\n" +
+                    "  |  limit: 1\n" +
                     "  |  \n" +
                     "  17:NESTLOOP JOIN\n" +
                     "  |  join op: INNER JOIN\n" +
@@ -251,8 +252,9 @@ public class SubqueryTest extends PlanTestBase {
                     "IS NULL";
 
             String plan = getFragmentPlan(sql);
-            assertContains(plan, "  18:Project\n" +
+            assertContains(plan, "18:Project\n" +
                     "  |  <slot 15> : 1\n" +
+                    "  |  limit: 1\n" +
                     "  |  \n" +
                     "  17:NESTLOOP JOIN\n" +
                     "  |  join op: INNER JOIN\n" +
