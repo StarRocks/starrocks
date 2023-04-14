0933c2cd9d021286bb62177ff4f6159cae6d462b
meegoo <meegoo.sr@gmail.com>
2023-04-14 19:13:52 +0800

    [BugFix] Remove unused alter job type
    Signed-off-by: meegoo <meegoo.sr@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/alter/AlterJobV2.java
+++ fe/fe-core/src/main/java/com/starrocks/alter/AlterJobV2.java
@@ -80,7 +80,7 @@ public abstract class AlterJobV2 implements Writable {
 
     public enum JobType {
         // DECOMMISSION_BACKEND is for compatible with older versions of metadata
-        ROLLUP, SCHEMA_CHANGE, DECOMMISSION_BACKEND, COMPACTION
+        ROLLUP, SCHEMA_CHANGE, DECOMMISSION_BACKEND 
     }
 
     @SerializedName(value = "type")
