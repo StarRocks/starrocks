664b6d24cd376799f11b8135bac75123e6b8b481
Felix Li <astralidea@163.com>
2023-04-17 18:50:00 +0800

    [Enhancement] Control the writing of metadata, for task_runs (#21710)
    * Control the writing of metadata, for task_runs
    
    Signed-off-by: Astralidea <astralidea@163.com>
    
    (cherry picked from commit 777734bfc0a32526f7a25c9a53eebee8221af32f)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/scheduler/TaskRunHistory.java

--- fe/fe-core/src/main/java/com/starrocks/scheduler/TaskRunHistory.java
+++ fe/fe-core/src/main/java/com/starrocks/scheduler/TaskRunHistory.java
@@ -2,10 +2,21 @@
 
 package com.starrocks.scheduler;
 
+<<<<<<< HEAD
 import com.google.common.collect.Queues;
 import com.starrocks.scheduler.persist.TaskRunStatus;
 
 import java.util.Deque;
+=======
+import com.google.common.collect.Maps;
+import com.starrocks.common.Config;
+import com.starrocks.scheduler.persist.TaskRunStatus;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+>>>>>>> 777734bfc ([Enhancement] Control the writing of metadata, for task_runs (#21710))
 
 public class TaskRunHistory {
 
@@ -15,7 +26,37 @@ public class TaskRunHistory {
         historyDeque.addFirst(status);
     }
 
+<<<<<<< HEAD
     public Deque<TaskRunStatus> getAllHistory() {
         return historyDeque;
+=======
+    public TaskRunStatus getTask(String queryId) {
+        if (queryId == null) {
+            return null;
+        }
+        return historyTaskRunMap.get(queryId);
+    }
+
+    public void removeTask(String queryId) {
+        if (queryId == null) {
+            return;
+        }
+        historyTaskRunMap.remove(queryId);
+    }
+
+    // Reserve historyTaskRunMap values to keep the last insert at the first.
+    public List<TaskRunStatus> getAllHistory() {
+        List<TaskRunStatus> historyRunStatus =
+                new ArrayList<>(historyTaskRunMap.values());
+        Collections.reverse(historyRunStatus);
+        return historyRunStatus;
+>>>>>>> 777734bfc ([Enhancement] Control the writing of metadata, for task_runs (#21710))
+    }
+
+    public void forceGC() {
+        List<TaskRunStatus> allHistory = getAllHistory();
+        int startIndex = Math.max(0, allHistory.size() - Config.task_runs_max_history_number);
+        allHistory.subList(startIndex, allHistory.size())
+                .forEach(taskRunStatus -> historyTaskRunMap.remove(taskRunStatus.getQueryId()));
     }
 }
