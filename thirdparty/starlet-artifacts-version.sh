32b418d1de34c5bc5d8d9c76d075acc916dc0d9c
Kevin Cai <caixiaohua@starrocks.com>
2023-04-18 13:54:04 +0800

    [Enhancement] update starlet version to v1.0.0 (#21795)
    * a bunch of bugfixes and enhancement in starlet, bump to v1.0.0
    
    Signed-off-by: Kevin Xiaohua Cai <caixiaohua@starrocks.com>

--- thirdparty/starlet-artifacts-version.sh
+++ thirdparty/starlet-artifacts-version.sh
@@ -9,4 +9,4 @@
 #   https://hub.docker.com/r/starrocks/starlet-artifacts-centos7/tags
 #
 # Update the following tag when STARLET releases a new version.
-export STARLET_ARTIFACTS_TAG=v0.2.7rc1
+export STARLET_ARTIFACTS_TAG=v1.0.0
