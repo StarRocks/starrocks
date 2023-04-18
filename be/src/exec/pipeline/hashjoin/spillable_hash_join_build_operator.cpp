fb229d2bdaaf4d40624c72be84f76b5df5a6b1bb
stdpain <drfeng08@gmail.com>
2023-04-18 19:53:08 +0800

    [BugFix] Fix wrong schema when hash join enable auto-spill
    Signed-off-by: stdpain <drfeng08@gmail.com>

--- be/src/exec/pipeline/hashjoin/spillable_hash_join_build_operator.cpp
+++ be/src/exec/pipeline/hashjoin/spillable_hash_join_build_operator.cpp
@@ -158,9 +158,11 @@ Status SpillableHashJoinBuildOperator::push_chunk(RuntimeState* state, const Chu
 
     // TODO: materialize chunk (const/nullable)
 
-    RETURN_IF_ERROR(append_hash_columns(chunk));
+    auto& ht = _join_builder->hash_join_builder()->hash_table();
+    ASSIGN_OR_RETURN(auto spill_chunk, ht.convert_to_serialize_format(chunk));
+    RETURN_IF_ERROR(append_hash_columns(spill_chunk));
 
-    RETURN_IF_ERROR(_join_builder->append_chunk_to_spill_buffer(state, chunk));
+    RETURN_IF_ERROR(_join_builder->append_chunk_to_spill_buffer(state, spill_chunk));
 
     if (_is_first_time_spill) {
         _is_first_time_spill = false;
@@ -185,7 +187,7 @@ std::function<StatusOr<ChunkPtr>()> SpillableHashJoinBuildOperator::_convert_has
     _hash_table_build_chunk_slice.reset(build_chunk);
     _hash_table_build_chunk_slice.skip(kHashJoinKeyColumnOffset);
 
-    return [this]() mutable -> StatusOr<ChunkPtr> {
+    return [this]() -> StatusOr<ChunkPtr> {
         if (_hash_table_build_chunk_slice.empty()) {
             _join_builder->hash_join_builder()->reset(_join_builder->hash_table_param());
             return Status::EndOfFile("eos");
