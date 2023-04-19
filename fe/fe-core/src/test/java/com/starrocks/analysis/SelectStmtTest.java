0f4be5e38ce52340cdbe302d163efde5f2752166
satanson <ranpanf@gmail.com>
2023-04-19 10:25:07 +0800

    fixup 1
    Signed-off-by: satanson <ranpanf@gmail.com>

--- fe/fe-core/src/test/java/com/starrocks/analysis/SelectStmtTest.java
+++ fe/fe-core/src/test/java/com/starrocks/analysis/SelectStmtTest.java
@@ -349,7 +349,13 @@ public class SelectStmtTest {
         String[] sqlList = {
                 "select count(distinct k1, k2), count(distinct k3) from db1.tbl1 limit 1",
                 "select * from (select count(distinct k1, k2), count(distinct k3) from db1.tbl1) t1 limit 1",
-                "with t1 as (select count(distinct k1, k2) as a, count(distinct k3) as b from db1.tbl1) select * from t1 limit 1",
+                "with t1 as (select count(distinct k1, k2) as a, count(distinct k3) as b from db1.tbl1) " +
+                        "select * from t1 limit 1",
+                "select count(distinct k1, k2), count(distinct k3) from db1.tbl1 group by k4 limit 1",
+                "select * from (select count(distinct k1, k2), count(distinct k3) from db1.tbl1 group by k4, k3) t1" +
+                        " limit 1",
+                "with t1 as (select count(distinct k1, k2) as a, count(distinct k3) as b from db1.tbl1 " +
+                        "group by k2, k3, k4) select * from t1 limit 1",
         };
         boolean cboCteReuse = starRocksAssert.getCtx().getSessionVariable().isCboCteReuse();
         try {
