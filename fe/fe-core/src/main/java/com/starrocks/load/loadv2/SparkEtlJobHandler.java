d13684750b8c1946178cbbe90f03923730d380c4
blanklin030 <luis01@foxmail.com>
2023-04-19 17:51:49 +0800

    fix bug
    Signed-off-by: blanklin030 <luis01@foxmail.com>

--- fe/fe-core/src/main/java/com/starrocks/load/loadv2/SparkEtlJobHandler.java
+++ fe/fe-core/src/main/java/com/starrocks/load/loadv2/SparkEtlJobHandler.java
@@ -192,6 +192,13 @@ public class SparkEtlJobHandler {
         }
 
         if (fromSparkState(state) == TEtlState.CANCELLED) {
+            if (state == State.KILLED) {
+                try {
+                    killYarnApplication(handle, loadJobId, resource);
+                } catch (UserException e) {
+                    LOG.warn(errMsg, e);
+                }
+            }
             throw new LoadException(
                     errMsg + "spark app state: " + state.toString() + ", loadJobId:" + loadJobId + ", logPath:" +
                             logPath);
@@ -205,6 +212,32 @@ public class SparkEtlJobHandler {
         // success
         attachment.setAppId(appId);
         attachment.setHandle(handle);
+    }
+
+    public void killYarnApplication(SparkLoadAppHandle handle, long loadJobId, SparkResource resource)
+            throws UserException {
+        if (!resource.isYarnMaster()) {
+            return;
+        }
+        if (Strings.isNullOrEmpty(handle.getAppId())) {
+            LOG.warn("yarn application kill failed, app id is empty");
+            return;
+        }
+        // prepare yarn config
+        String configDir = resource.prepareYarnConfig();
+        // yarn client path
+        String yarnClient = resource.getYarnClientPath();
+        // command: yarn --config configDir application -kill appId
+        String yarnKillCmd = String.format(YARN_KILL_CMD, yarnClient, configDir, handle.getAppId());
+        LOG.info(yarnKillCmd);
+        String[] envp = {"LC_ALL=" + Config.locale, "JAVA_HOME=" + System.getProperty("java.home")};
+        CommandResult result = Util.executeCommand(yarnKillCmd, envp, EXEC_CMD_TIMEOUT_MS);
+        LOG.info("yarn application -kill {}, output: {}", handle.getAppId(), result.getStdout());
+        if (result.getReturnCode() != 0) {
+            String stderr = result.getStderr();
+            LOG.warn("yarn application kill failed. app id: {}, load job id: {}, msg: {}", handle.getAppId(), loadJobId,
+                    stderr);
+        }
     }
 
     public EtlStatus getEtlJobStatus(SparkLoadAppHandle handle, String appId, long loadJobId, String etlOutputPath,
@@ -308,22 +341,12 @@ public class SparkEtlJobHandler {
                     handle.kill();
                     return;
                 }
+            } else {
+                if (Strings.isNullOrEmpty(handle.getAppId())) {
+                    handle.setAppId(appId);
+                }
             }
-            // prepare yarn config
-            String configDir = resource.prepareYarnConfig();
-            // yarn client path
-            String yarnClient = resource.getYarnClientPath();
-            // command: yarn --config configDir application -kill appId
-            String yarnKillCmd = String.format(YARN_KILL_CMD, yarnClient, configDir, appId);
-            LOG.info(yarnKillCmd);
-            String[] envp = {"LC_ALL=" + Config.locale, "JAVA_HOME=" + System.getProperty("java.home")};
-            CommandResult result = Util.executeCommand(yarnKillCmd, envp, EXEC_CMD_TIMEOUT_MS);
-            LOG.info("yarn application -kill {}, output: {}", appId, result.getStdout());
-            if (result.getReturnCode() != 0) {
-                String stderr = result.getStderr();
-                LOG.warn("yarn application kill failed. app id: {}, load job id: {}, msg: {}", appId, loadJobId,
-                        stderr);
-            }
+            killYarnApplication(handle, loadJobId, resource);
         } else {
             if (handle != null) {
                 handle.stop();
