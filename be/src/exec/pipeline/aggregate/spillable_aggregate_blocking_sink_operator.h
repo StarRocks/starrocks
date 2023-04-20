f87e55de512575492d27ab0b2ea854a6a6b10886
stdpain <drfeng08@gmail.com>
2023-04-20 20:22:12 +0800

    [Feature] Support spillable aggregate distinct
    Signed-off-by: stdpain <drfeng08@gmail.com>

--- be/src/exec/pipeline/aggregate/spillable_aggregate_blocking_sink_operator.h
+++ be/src/exec/pipeline/aggregate/spillable_aggregate_blocking_sink_operator.h
@@ -22,6 +22,7 @@
 #include "runtime/runtime_state.h"
 
 namespace starrocks::pipeline {
+// TODO: implements reset_state
 class SpillableAggregateBlockingSinkOperator : public AggregateBlockingSinkOperator {
 public:
     template <class... Args>
@@ -51,10 +52,7 @@ public:
 
 private:
     Status _spill_all_inputs(RuntimeState* state, const ChunkPtr& chunk);
-    Status _spill_aggregated_data(RuntimeState* state);
-
-    std::function<StatusOr<ChunkPtr>()> _generate_spill_task(RuntimeState* state);
-
+    std::function<StatusOr<ChunkPtr>()> _build_spill_task(RuntimeState* state);
     spill::SpillStrategy _spill_strategy = spill::SpillStrategy::NO_SPILL;
 
     bool _is_finished = false;
