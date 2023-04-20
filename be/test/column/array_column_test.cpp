0e5f609a0dfc99351aff1c2e29efe921244553cd
Zhuhe Fang <fzhedu@gmail.com>
2023-04-20 21:52:06 +0800

    [BugFix] cp ArrayColumn::assign() from 2.5
    Signed-off-by: Zhuhe Fang <fzhedu@gmail.com>

--- be/test/column/array_column_test.cpp
+++ be/test/column/array_column_test.cpp
@@ -987,6 +987,62 @@ PARALLEL_TEST(ArrayColumnTest, test_update_rows) {
     ASSERT_EQ("[203, 204]", column->debug_item(3));
 }
 
+PARALLEL_TEST(ArrayColumnTest, test_assign) {
+    /// test assign comment arrays
+    auto offsets = UInt32Column::create();
+    auto elements = Int32Column::create();
+    auto column = ArrayColumn::create(elements, offsets);
+
+    // insert [1, 2, 3], [4, 5, 6]
+    elements->append(1);
+    elements->append(2);
+    elements->append(3);
+    offsets->append(3);
+
+    elements->append(4);
+    elements->append(5);
+    elements->append(6);
+    offsets->append(6);
+
+    // assign
+    column->assign(4, 0);
+    ASSERT_EQ(4, column->size());
+    ASSERT_EQ("[1,2,3]", column->debug_item(0));
+    ASSERT_EQ("[1,2,3]", column->debug_item(1));
+    ASSERT_EQ("[1,2,3]", column->debug_item(2));
+    ASSERT_EQ("[1,2,3]", column->debug_item(3));
+
+    /// test assign [null]
+    elements = Int32Column::create();
+    auto nullable_elements = NullableColumn::create(std::move(elements), NullColumn::create());
+    offsets = UInt32Column ::create();
+    column = ArrayColumn::create(std::move(nullable_elements), std::move(offsets));
+    column->append_datum(DatumArray{Datum()});
+
+    column->assign(5, 0);
+    ASSERT_EQ(5, column->size());
+    ASSERT_TRUE(column->get(0).get_array()[0].is_null());
+    ASSERT_TRUE(column->get(4).get_array()[0].is_null());
+
+    /// test assign []
+    column->reset_column();
+    column->append_datum(DatumArray{});
+
+    column->assign(5, 0);
+    ASSERT_EQ(5, column->size());
+    ASSERT_TRUE(column->get(0).get_array().empty());
+    ASSERT_TRUE(column->get(4).get_array().empty());
+
+    /// test assign [null,null]
+    column->reset_column();
+    column->append_datum(DatumArray{Datum(), Datum()});
+
+    column->assign(5, 0);
+    ASSERT_EQ(5, column->size());
+    ASSERT_TRUE(column->get(0).get_array()[0].is_null());
+    ASSERT_TRUE(column->get(4).get_array()[1].is_null());
+}
+
 PARALLEL_TEST(ArrayColumnTest, test_element_memory_usage) {
     auto offsets = UInt32Column::create();
     auto elements = Int32Column::create();
