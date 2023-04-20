9c053ec44a764faa524f1cd5c17d0c57fb400b52
liuyehcf <1559500551@qq.com>
2023-04-20 13:14:57 +0800

    [BugFix] Fix the problem of updating GroupExpression's op or inputs without re-inserting it (#21829)
    * [BugFix] Fix the problem of updating GroupExpression's op or inputs without re-inserting it
    
    Signed-off-by: liuyehcf <1559500551@qq.com>
    
    (cherry picked from commit ff501bf3f0a0fd546fb26e12bc2a256203678cc7)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/sql/optimizer/Memo.java
    #	fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/PruneUnionColumnsRule.java

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/Memo.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/Memo.java
@@ -1,4 +1,20 @@
+<<<<<<< HEAD
 // This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
+=======
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+>>>>>>> ff501bf3f ([BugFix] Fix the problem of updating GroupExpression's op or inputs without re-inserting it (#21829))
 
 package com.starrocks.sql.optimizer;
 
@@ -325,7 +341,10 @@ public class Memo {
 
     private void removeGroupInitLogicExpression(Group group) {
         GroupExpression initGroupExpression = group.getFirstLogicalExpression();
-        groupExpressions.remove(initGroupExpression);
+        // This remove must be successful, otherwise GroupExpression::op or GroupExpression::inputs
+        // may be updated without re-inserted.
+        Preconditions.checkNotNull(groupExpressions.remove(initGroupExpression),
+                "GroupExpression has been updated without re-inserting");
 
         Preconditions.checkState(group.isValidInitState());
 
--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/PruneUnionColumnsRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/PruneUnionColumnsRule.java
@@ -1,12 +1,29 @@
+<<<<<<< HEAD
 // This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
+=======
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+>>>>>>> ff501bf3f ([BugFix] Fix the problem of updating GroupExpression's op or inputs without re-inserting it (#21829))
 
 package com.starrocks.sql.optimizer.rule.transformation;
 
+import com.google.common.collect.Lists;
 import com.starrocks.sql.optimizer.OptExpression;
 import com.starrocks.sql.optimizer.OptimizerContext;
 import com.starrocks.sql.optimizer.base.ColumnRefSet;
 import com.starrocks.sql.optimizer.operator.OperatorType;
-import com.starrocks.sql.optimizer.operator.logical.LogicalSetOperator;
+import com.starrocks.sql.optimizer.operator.logical.LogicalUnionOperator;
 import com.starrocks.sql.optimizer.operator.pattern.Pattern;
 import com.starrocks.sql.optimizer.operator.scalar.ColumnRefOperator;
 import com.starrocks.sql.optimizer.rule.RuleType;
@@ -14,6 +31,7 @@ import com.starrocks.sql.optimizer.rule.RuleType;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PruneUnionColumnsRule extends TransformationRule {
     public PruneUnionColumnsRule() {
@@ -26,8 +44,15 @@ public class PruneUnionColumnsRule extends TransformationRule {
     public List<OptExpression> transform(OptExpression input, OptimizerContext context) {
         ColumnRefSet requiredOutputColumns = context.getTaskContext().getRequiredColumns();
 
-        LogicalSetOperator lso = (LogicalSetOperator) input.getOp();
-        List<ColumnRefOperator> outputs = lso.getOutputColumnRefOp();
+        LogicalUnionOperator luo = new LogicalUnionOperator.Builder()
+                .withOperator((LogicalUnionOperator) input.getOp())
+                .setOutputColumnRefOp(Lists.newArrayList(((LogicalUnionOperator) input.getOp()).getOutputColumnRefOp()))
+                .setChildOutputColumns(((LogicalUnionOperator) input.getOp()).getChildOutputColumns().stream()
+                        .map(Lists::newArrayList)
+                        .collect(Collectors.toList()))
+                .build();
+
+        List<ColumnRefOperator> outputs = luo.getOutputColumnRefOp();
 
         List<List<ColumnRefOperator>> childOutputsAfterPruned = new ArrayList<>();
         for (int childIdx = 0; childIdx < input.arity(); ++childIdx) {
@@ -39,7 +64,7 @@ public class PruneUnionColumnsRule extends TransformationRule {
             if (requiredOutputColumns.contains(outputs.get(idx))) {
                 needOutput = true;
                 for (int childIdx = 0; childIdx < input.arity(); ++childIdx) {
-                    ColumnRefOperator columnRefOperator = lso.getChildOutputColumns().get(childIdx).get(idx);
+                    ColumnRefOperator columnRefOperator = luo.getChildOutputColumns().get(childIdx).get(idx);
 
                     requiredOutputColumns.union(columnRefOperator);
                     childOutputsAfterPruned.get(childIdx).add(columnRefOperator);
@@ -50,7 +75,7 @@ public class PruneUnionColumnsRule extends TransformationRule {
         // must output least 1
         if (!needOutput) {
             for (int childIdx = 0; childIdx < input.arity(); ++childIdx) {
-                ColumnRefOperator columnRefOperator = lso.getChildOutputColumns().get(childIdx).get(0);
+                ColumnRefOperator columnRefOperator = luo.getChildOutputColumns().get(childIdx).get(0);
 
                 requiredOutputColumns.union(columnRefOperator);
                 childOutputsAfterPruned.get(childIdx).add(columnRefOperator);
@@ -70,9 +95,9 @@ public class PruneUnionColumnsRule extends TransformationRule {
          * Because the output columns of the child may be the same because of expression reuse
          */
         for (int childIdx = 0; childIdx < input.arity(); ++childIdx) {
-            lso.getChildOutputColumns().set(childIdx, childOutputsAfterPruned.get(childIdx));
+            luo.getChildOutputColumns().set(childIdx, childOutputsAfterPruned.get(childIdx));
         }
 
-        return Collections.emptyList();
+        return Collections.singletonList(OptExpression.create(luo, input.getInputs()));
     }
 }
