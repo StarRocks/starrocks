717d41c534070ef40af68a539d44e1dffb527c95
ABing <101158374+ABingHuang@users.noreply.github.com>
2023-04-20 16:17:30 +0800

    [BugFix] Fix statistics related mv bug (#21189)

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/ReplayFromDumpTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/ReplayFromDumpTest.java
@@ -799,7 +799,7 @@ public class ReplayFromDumpTest {
         Pair<QueryDumpInfo, String> replayPair =
                 getPlanFragment(getDumpInfoFromFile("query_dump/hive_tpch02_catalog"), null,
                         TExplainLevel.NORMAL);
-        Assert.assertTrue(replayPair.second, replayPair.second.contains("19:HASH JOIN\n" +
+        Assert.assertTrue(replayPair.second, replayPair.second.contains("5:HASH JOIN\n" +
                 "  |  join op: INNER JOIN (PARTITIONED)\n" +
                 "  |  colocate: false, reason: \n" +
                 "  |  equal join conjunct: 17: ps_partkey = 1: p_partkey"));
