d07b3d17ea62d498a220d6ca30eaf3094b27d14c
evenhuang <986025158@qq.com>
2023-04-21 14:19:05 +0800

    implement to_days function in FE
    Signed-off-by: evenhuang <986025158@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rewrite/ScalarOperatorFunctions.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rewrite/ScalarOperatorFunctions.java
@@ -233,6 +233,17 @@ public class ScalarOperatorFunctions {
     }
 
     @ConstantFunction.List(list = {
+            @ConstantFunction(name = "to_days", argTypes = {DATETIME}, returnType = INT, isMonotonic = true),
+            @ConstantFunction(name = "to_days", argTypes = {DATE}, returnType = INT, isMonotonic = true)
+    })
+    public static ConstantOperator to_days(ConstantOperator first) {
+        ConstantOperator second = ConstantOperator.createDatetime(LocalDateTime.of(0000, 01, 01, 00, 00, 00));
+        return ConstantOperator.createInt((int) Duration.between(
+                second.getDatetime().truncatedTo(ChronoUnit.DAYS),
+                first.getDatetime().truncatedTo(ChronoUnit.DAYS)).toDays());
+    }
+
+    @ConstantFunction.List(list = {
             @ConstantFunction(name = "years_add", argTypes = {DATETIME,
                     INT}, returnType = DATETIME, isMonotonic = true),
             @ConstantFunction(name = "years_add", argTypes = {DATE, INT}, returnType = DATE, isMonotonic = true)
