a49287a10fc088832bb1cdffcde08ddfc9caedd9
eyes_on_me <3675229+silverbullet233@users.noreply.github.com>
2023-04-21 19:17:20 +0800

    [Enhancement] support configurable encoding in query spilling (#19354)
    Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>

--- be/src/serde/protobuf_serde.cpp
+++ be/src/serde/protobuf_serde.cpp
@@ -28,64 +28,6 @@
 
 namespace starrocks::serde {
 
-EncodeContext::EncodeContext(const int col_num, const int encode_level) : _session_encode_level(encode_level) {
-    for (auto i = 0; i < col_num; ++i) {
-        _column_encode_level.emplace_back(_session_encode_level);
-        _raw_bytes.emplace_back(0);
-        _encoded_bytes.emplace_back(0);
-    }
-    DCHECK(_session_encode_level != 0);
-    // the lowest bit is set and other bits are not zero, then enable adjust.
-    if (_session_encode_level & 1 && (_session_encode_level >> 1)) {
-        _enable_adjust = true;
-    }
-}
-
-void EncodeContext::update(const int col_id, uint64_t mem_bytes, uint64_t encode_byte) {
-    DCHECK(_session_encode_level != 0);
-    if (!_enable_adjust) {
-        return;
-    }
-    // decide to encode or not by the encoding ratio of the first EncodeSamplingNum of every _frequency chunks
-    if (_times % _frequency < EncodeSamplingNum) {
-        _raw_bytes[col_id] += mem_bytes;
-        _encoded_bytes[col_id] += encode_byte;
-    }
-}
-
-// if encode ratio < EncodeRatioLimit, encode it, otherwise not.
-void EncodeContext::_adjust(const int col_id) {
-    auto old_level = _column_encode_level[col_id];
-    if (_encoded_bytes[col_id] < _raw_bytes[col_id] * EncodeRatioLimit) {
-        _column_encode_level[col_id] = _session_encode_level;
-    } else {
-        _column_encode_level[col_id] = 0;
-    }
-    if (old_level != _column_encode_level[col_id] || _session_encode_level < -1) {
-        VLOG_ROW << "Old encode level " << old_level << " is changed to " << _column_encode_level[col_id]
-                 << " because the first " << EncodeSamplingNum << " of " << _frequency << " in total " << _times
-                 << " chunks' compression ratio is " << _encoded_bytes[col_id] * 1.0 / _raw_bytes[col_id]
-                 << " higher than limit " << EncodeRatioLimit;
-    }
-    _encoded_bytes[col_id] = 0;
-    _raw_bytes[col_id] = 0;
-}
-
-void EncodeContext::set_encode_levels_in_pb(ChunkPB* const res) {
-    res->mutable_encode_level()->Reserve(static_cast<int>(_column_encode_level.size()));
-    for (const auto& level : _column_encode_level) {
-        res->mutable_encode_level()->Add(level);
-    }
-    ++_times;
-    // must adjust after writing the current encode_level
-    if (_enable_adjust && (_times % _frequency == EncodeSamplingNum)) {
-        for (auto col_id = 0; col_id < _column_encode_level.size(); ++col_id) {
-            _adjust(col_id);
-        }
-        _frequency = _frequency > 1000000000 ? _frequency : _frequency * 2;
-    }
-}
-
 int64_t ProtobufChunkSerde::max_serialized_size(const Chunk& chunk, const std::shared_ptr<EncodeContext>& context) {
     int64_t serialized_size = 8; // 4 bytes version plus 4 bytes row number
 
