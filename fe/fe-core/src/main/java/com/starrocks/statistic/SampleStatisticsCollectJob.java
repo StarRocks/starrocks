66e9a9eca71331183e81f4229fae77a6105c40f8
Seaven <seaven_7@qq.com>
2023-04-21 19:45:26 +0800

    [BugFix] Fix sample statistics table name error (#22077)
    Signed-off-by: Seaven <seaven_7@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/statistic/SampleStatisticsCollectJob.java
+++ fe/fe-core/src/main/java/com/starrocks/statistic/SampleStatisticsCollectJob.java
@@ -37,12 +37,13 @@ import java.util.stream.Collectors;
 public class SampleStatisticsCollectJob extends StatisticsCollectJob {
 
     private static final String INSERT_SELECT_METRIC_SAMPLE_TEMPLATE =
-            "SELECT $tableId, '$columnNameStr', $dbId, '$tableNameStr', '$dbNameStr', COUNT(1) * $ratio, "
+            "SELECT $tableId, '$columnNameStr', $dbId, '$dbNameStr.$tableNameStr', '$dbNameStr', COUNT(1) * $ratio, "
                     + "$dataSize * $ratio, 0, 0, '', '', NOW() "
                     + "FROM (SELECT `$columnName` as column_key FROM `$dbName`.`$tableName` $hints ) as t ";
 
     private static final String INSERT_SELECT_TYPE_SAMPLE_TEMPLATE =
-            "SELECT $tableId, '$columnNameStr', $dbId, '$tableNameStr', '$dbNameStr', IFNULL(SUM(t1.count), 0) * $ratio, "
+            "SELECT $tableId, '$columnNameStr', $dbId, '$dbNameStr.$tableNameStr', '$dbNameStr', "
+                    + "       IFNULL(SUM(t1.count), 0) * $ratio, "
                     + "       $dataSize * $ratio, $countDistinctFunction, "
                     + "       IFNULL(SUM(IF(t1.`column_key` IS NULL, t1.count, 0)), 0) * $ratio, "
                     + "       $maxFunction, $minFunction, NOW() "
