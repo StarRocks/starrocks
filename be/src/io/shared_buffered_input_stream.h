36c019a084d68c3b1aa6f0498cfb70648d1bc266
yanz <dirtysalt1987@gmail.com>
2023-04-21 10:01:09 -0700

    fix conflict
    Signed-off-by: yanz <dirtysalt1987@gmail.com>

--- be/src/io/shared_buffered_input_stream.h
+++ be/src/io/shared_buffered_input_stream.h
@@ -86,6 +86,7 @@ private:
     StatusOr<SharedBuffer*> _find_shared_buffer(size_t offset, size_t count);
     Status _read_stream_buffer(SharedBuffer& sb, size_t offset, size_t count);
     std::shared_ptr<SeekableInputStream> _stream;
+    std::string _filename;
     std::map<int64_t, SharedBuffer> _map;
     CoalesceOptions _options;
     int64_t _offset = 0;
