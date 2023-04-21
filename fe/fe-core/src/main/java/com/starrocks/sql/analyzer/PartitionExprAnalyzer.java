fc046ea6e6322a33a2c0bae9526ff5641bee5cf5
meegoo <meegoo.sr@gmail.com>
2023-04-21 18:26:07 +0800

    [BugFix] Fix asan crash since FuntionCallExpr not set type (#22154)
    Signed-off-by: meegoo <meegoo.sr@gmail.com>
    (cherry picked from commit 955cfc8aba81d7effe6c9fbe7313337d84c1027e)

--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/PartitionExprAnalyzer.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/PartitionExprAnalyzer.java
@@ -41,6 +41,7 @@ public class PartitionExprAnalyzer {
             }
 
             functionCallExpr.setFn(builtinFunction);
+            functionCallExpr.setType(targetColType);
         }
     }
 }
