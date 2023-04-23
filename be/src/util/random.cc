d4c920ebd1cc89128161672ed2a3e9313824987b
sduzh <zhuming9011@qq.com>
2023-04-23 15:37:07 +0800

    format
    Signed-off-by: sduzh <zhuming9011@qq.com>

--- be/src/util/random.cc
+++ be/src/util/random.cc
@@ -34,43 +34,43 @@
 namespace starrocks {
 
 Random* Random::GetTLSInstance() {
-  STORAGE_DECL Random* tls_instance;
-  STORAGE_DECL std::aligned_storage<sizeof(Random)>::type tls_instance_bytes;
+    STORAGE_DECL Random* tls_instance;
+    STORAGE_DECL std::aligned_storage<sizeof(Random)>::type tls_instance_bytes;
 
-  auto rv = tls_instance;
-  if (UNLIKELY(rv == nullptr)) {
-    size_t seed = std::hash<std::thread::id>()(std::this_thread::get_id());
-    rv = new (&tls_instance_bytes) Random((uint32_t)seed);
-    tls_instance = rv;
-  }
-  return rv;
+    auto rv = tls_instance;
+    if (UNLIKELY(rv == nullptr)) {
+        size_t seed = std::hash<std::thread::id>()(std::this_thread::get_id());
+        rv = new (&tls_instance_bytes) Random((uint32_t)seed);
+        tls_instance = rv;
+    }
+    return rv;
 }
 
 std::string Random::HumanReadableString(int len) {
-  std::string ret;
-  ret.resize(len);
-  for (int i = 0; i < len; ++i) {
-    ret[i] = static_cast<char>('a' + Uniform(26));
-  }
-  return ret;
+    std::string ret;
+    ret.resize(len);
+    for (int i = 0; i < len; ++i) {
+        ret[i] = static_cast<char>('a' + Uniform(26));
+    }
+    return ret;
 }
 
 std::string Random::RandomString(int len) {
-  std::string ret;
-  ret.resize(len);
-  for (int i = 0; i < len; i++) {
-    ret[i] = static_cast<char>(' ' + Uniform(95));  // ' ' .. '~'
-  }
-  return ret;
+    std::string ret;
+    ret.resize(len);
+    for (int i = 0; i < len; i++) {
+        ret[i] = static_cast<char>(' ' + Uniform(95)); // ' ' .. '~'
+    }
+    return ret;
 }
 
 std::string Random::RandomBinaryString(int len) {
-  std::string ret;
-  ret.resize(len);
-  for (int i = 0; i < len; i++) {
-    ret[i] = static_cast<char>(Uniform(CHAR_MAX));
-  }
-  return ret;
+    std::string ret;
+    ret.resize(len);
+    for (int i = 0; i < len; i++) {
+        ret[i] = static_cast<char>(Uniform(CHAR_MAX));
+    }
+    return ret;
 }
 
-}  // namespace starrocks
+} // namespace starrocks
