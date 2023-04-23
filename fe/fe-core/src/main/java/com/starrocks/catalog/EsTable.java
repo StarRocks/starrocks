85ea0a4a12d988179ede45183c314af0376dbf3a
zombee0 <flylucas_10@163.com>
2023-04-23 14:47:24 +0800

    Update getUUID for iceberg/delta lake/elasticsearch/jdbc table
    we use uuid to identify the external table. in catalog scenario,
    tableid and table createtime changed offenly in deferent queries,
    we should use external table's catalog, remote dbname, remote
    tablename to identify the external table. To deal with that table
    created after deleted, we also use the original table's createtime.
    for iceberg table, we can use native uuid instread of createtime.
    
    TODO: for jdbctable estable we can't identify the table created
    after deleted.
    
    Signed-off-by: zombee0 <flylucas_10@163.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/EsTable.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/EsTable.java
@@ -129,6 +129,9 @@ public class EsTable extends Table {
 
     // record the latest and recently exception when sync ES table metadata (mapping, shard location)
     private Throwable lastMetaDataSyncException = null;
+    // used for catalog to identify the remote table.
+    private String catalogName = null;
+    private String dbName = null;
 
     public EsTable() {
         super(TableType.ELASTICSEARCH);
@@ -138,6 +141,15 @@ public class EsTable extends Table {
                    Map<String, String> properties, PartitionInfo partitionInfo) throws DdlException {
         super(id, name, TableType.ELASTICSEARCH, schema);
         this.partitionInfo = partitionInfo;
+        validate(properties);
+    }
+
+    public EsTable(long id, String catalogName, String dbName, String name, List<Column> schema, Map<String, String> properties,
+                    PartitionInfo partitionInfo) throws DdlException {
+        super(id, name, TableType.ELASTICSEARCH, schema);
+        this.partitionInfo = partitionInfo;
+        this.catalogName = catalogName;
+        this.dbName = dbName;
         validate(properties);
     }
 
@@ -306,6 +318,16 @@ public class EsTable extends Table {
                 fullSchema.size(), 0, getName(), "");
         tTableDescriptor.setEsTable(tEsTable);
         return tTableDescriptor;
+    }
+
+    // TODO, identify the remote table that created after deleted
+    @Override
+    public String getUUID() {
+        if (!Strings.isNullOrEmpty(catalogName)) {
+            return String.join(".", catalogName, dbName, name);
+        } else {
+            return Long.toString(id);
+        }
     }
 
     @Override
