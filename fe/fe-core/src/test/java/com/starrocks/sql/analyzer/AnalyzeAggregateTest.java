adbd72888e429a7e80d1a0fcfe55caa186da53d1
before-Sunrise <71162020+before-Sunrise@users.noreply.github.com>
2023-04-23 14:00:36 +0800

    enhance analyse for percentile_approx (#21962)
    Signed-off-by: before-Sunrise <unclejyj@gmail.com>
    (cherry picked from commit 25f96f4e94f81ce4fd35d95d7863fc0880c135c9)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/sql/analyzer/FunctionAnalyzer.java
    #	fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzeAggregateTest.java

--- fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzeAggregateTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzeAggregateTest.java
@@ -214,6 +214,20 @@ public class AnalyzeAggregateTest {
     }
 
     @Test
+<<<<<<< HEAD
+=======
+    public void testPercentileFunction() {
+        analyzeFail("select percentile_approx(0.5) from tall group by tb");
+        analyzeFail("select percentile_approx('c',0.5) from tall group by tb");
+        analyzeFail("select percentile_approx(1,'c') from tall group by tb");
+        analyzeFail("select percentile_approx(1,5) from tall group by tb");
+        analyzeFail("select percentile_approx(1,1,'c') from tall group by tb");
+        analyzeSuccess("select percentile_approx(1,0.5,1047) from tall group by tb");
+        analyzeSuccess("select percentile_disc(tj,0.5) from tall group by tb");
+    }
+
+    @Test
+>>>>>>> 25f96f4e9 (enhance analyse for percentile_approx (#21962))
     public void testWindowFunnelFunction() {
         // For the argument `window_size`.
         analyzeFail("SELECT window_funnel(-1, ti, 0, [ta='a', ta='b']) FROM tall",
