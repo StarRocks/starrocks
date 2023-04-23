c0975b6d9db608a9160722647918cbb7f5230581
yiming <107105845+nshangyiming@users.noreply.github.com>
2023-04-23 16:30:51 +0800

    [BugFix] Fix cannot set an empty array for array type conf (#22225)
    Usage: ``admin set frontend config ("an_array_type_conf"="");``
    Signed-off-by: Dejun Xia <xiadejun@starrocks.com>
    (cherry picked from commit f9f1c4d3c6684f822db5574aa711049af1c2d9d3)
    
    # Conflicts:
    #	fe/fe-core/src/test/java/com/starrocks/common/ConfigTest.java

--- fe/fe-core/src/test/java/com/starrocks/common/ConfigTest.java
+++ fe/fe-core/src/test/java/com/starrocks/common/ConfigTest.java
@@ -27,7 +27,7 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class ConfigTest {
-    private Config config = new Config();
+    private final Config config = new Config();
 
     private static class ConfigForTest extends ConfigBase {
         @ConfField(mutable = true, aliases = {"schedule_slot_num_per_path", "schedule_slot_num_per_path_only_for_test"})
@@ -37,11 +37,12 @@ public class ConfigTest {
     @Before
     public void setUp() throws Exception {
         URL resource = getClass().getClassLoader().getResource("conf/config_test.properties");
+        assert resource != null;
         config.init(Paths.get(resource.toURI()).toFile().getAbsolutePath());
     }
 
     @Test
-    public void testGetConfigFromPropertyFile() throws AnalysisException, DdlException {
+    public void testGetConfigFromPropertyFile() throws DdlException {
         PatternMatcher matcher = PatternMatcher.createMysqlPattern("tablet_sched_slot_num_per_path", false);
         List<List<String>> configs = Config.getConfigInfo(matcher);
         Assert.assertEquals("3", configs.get(0).get(2));
@@ -50,6 +51,7 @@ public class ConfigTest {
     @Test
     public void testConfigGetCompatibleWithOldName() throws Exception {
         URL resource = getClass().getClassLoader().getResource("conf/config_test2.properties");
+        assert resource != null;
         config.init(Paths.get(resource.toURI()).toFile().getAbsolutePath());
         PatternMatcher matcher = PatternMatcher.createMysqlPattern("schedule_slot_num_per_path", false);
         List<List<String>> configs = Config.getConfigInfo(matcher);
@@ -64,6 +66,7 @@ public class ConfigTest {
     public void testMultiAlias() throws Exception {
         ConfigForTest configForTest = new ConfigForTest();
         URL resource = getClass().getClassLoader().getResource("conf/config_test3.properties");
+        assert resource != null;
         configForTest.init(Paths.get(resource.toURI()).toFile().getAbsolutePath());
         PatternMatcher matcher = PatternMatcher.createMysqlPattern("schedule_slot_num_per_path_only_for_test", false);
         List<List<String>> configs = ConfigForTest.getConfigInfo(matcher);
@@ -81,4 +84,45 @@ public class ConfigTest {
         Assert.assertEquals("4", configs.get(0).get(2));
         Assert.assertEquals(4, Config.tablet_sched_slot_num_per_path);
     }
+<<<<<<< HEAD
+=======
+
+    private static class ConfigForArray extends ConfigBase {
+
+        @ConfField(mutable = true)
+        public static short[] prop_array_short = new short[] {1, 1};
+        @ConfField(mutable = true)
+        public static int[] prop_array_int = new int[] {2, 2};
+        @ConfField(mutable = true)
+        public static long[] prop_array_long = new long[] {3L, 3L};
+        @ConfField(mutable = true)
+        public static double[] prop_array_double = new double[] {1.1, 1.1};
+        @ConfField(mutable = true)
+        public static String[] prop_array_string = new String[] {"1", "2"};
+    }
+
+    @Test
+    public void testConfigArray() throws Exception {
+        ConfigForArray configForArray = new ConfigForArray();
+        URL resource = getClass().getClassLoader().getResource("conf/config_test3.properties");
+        assert resource != null;
+        configForArray.init(Paths.get(resource.toURI()).toFile().getAbsolutePath());
+        List<List<String>> configs = ConfigForArray.getConfigInfo(null);
+        Assert.assertEquals("[1, 1]", configs.get(0).get(2));
+        Assert.assertEquals("short[]", configs.get(0).get(3));
+        Assert.assertEquals("[2, 2]", configs.get(1).get(2));
+        Assert.assertEquals("int[]", configs.get(1).get(3));
+        Assert.assertEquals("[3, 3]", configs.get(2).get(2));
+        Assert.assertEquals("long[]", configs.get(2).get(3));
+        Assert.assertEquals("[1.1, 1.1]", configs.get(3).get(2));
+        Assert.assertEquals("double[]", configs.get(3).get(3));
+        Assert.assertEquals("[1, 2]", configs.get(4).get(2));
+        Assert.assertEquals("String[]", configs.get(4).get(3));
+
+        // check set an empty array works
+        ConfigForArray.setConfigField(ConfigForArray.getAllMutableConfigs().get("prop_array_long"), "");
+        configs = ConfigForArray.getConfigInfo(null);
+        Assert.assertEquals("[]", configs.get(2).get(2));
+    }
+>>>>>>> f9f1c4d3c ([BugFix] Fix cannot set an empty array for array type conf (#22225))
 }
