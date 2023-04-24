a73d174ca8d9c890f282d6dab7c93f61d758f648
Zhuhe Fang <fzhedu@gmail.com>
2023-04-24 18:09:34 +0800

    [BugFix] analyze all nest array_map before set lambda inputs in array_map() (backport #22260)
    Signed-off-by: Zhuhe Fang <fzhedu@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/ExpressionAnalyzer.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/ExpressionAnalyzer.java
@@ -165,6 +165,10 @@ public class ExpressionAnalyzer {
         for (int i = 1; i < childSize; ++i) {
             Expr expr = expression.getChild(i);
             bottomUpAnalyze(visitor, expr, scope);
+        }
+        // putting lambda inputs should after analyze
+        for (int i = 1; i < childSize; ++i) {
+            Expr expr = expression.getChild(i);
             if (expr instanceof NullLiteral) {
                 expr.setType(Type.ARRAY_INT); // Let it have item type.
             }
@@ -184,6 +188,7 @@ public class ExpressionAnalyzer {
         if (res != null) {
             visitor.visit(res, scope);
         }
+        scope.clearLambdaInputs();
     }
 
     private void bottomUpAnalyze(Visitor visitor, Expr expression, Scope scope) {
