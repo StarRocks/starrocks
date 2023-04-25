123de56883cdf7fce94a3c02ddcc2dc2b17fea6d
Yixin Luo <18810541851@163.com>
2023-04-25 08:38:41 +0800

    [Enhancement] primary key's value column using zonemap (#22423)
    Value column in primary key using the zonemap index by default.
    
    Signed-off-by: luohaha <18810541851@163.com>

--- be/src/storage/rowset/segment_writer.cpp
+++ be/src/storage/rowset/segment_writer.cpp
@@ -144,8 +144,12 @@ Status SegmentWriter::init(const std::vector<uint32_t>& column_indexes, bool has
         // now we create zone map for key columns
         // and not support zone map for array type.
         // TODO(mofei) refactor it to type specification
-        opts.need_zone_map = column.is_key() ||
-                             (_tablet_schema->keys_type() == KeysType::DUP_KEYS && is_zone_map_key_type(column.type()));
+        const bool enable_pk_zone_map = config::enable_pk_value_column_zonemap &&
+                                        _tablet_schema->keys_type() == KeysType::PRIMARY_KEYS &&
+                                        is_zone_map_key_type(column.type());
+        const bool enable_dup_zone_map =
+                _tablet_schema->keys_type() == KeysType::DUP_KEYS && is_zone_map_key_type(column.type());
+        opts.need_zone_map = column.is_key() || enable_pk_zone_map || enable_dup_zone_map;
         if (column.type() == LogicalType::TYPE_ARRAY) {
             opts.need_zone_map = false;
         }
