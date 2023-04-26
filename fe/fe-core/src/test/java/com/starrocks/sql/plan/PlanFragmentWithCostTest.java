b3b5e10c6c1782a8b3bc9c95ec9e872bec520735
ZiheLiu <ziheliu1024@gmail.com>
2023-04-26 12:23:23 +0800

    fix ut
    Signed-off-by: ZiheLiu <ziheliu1024@gmail.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/PlanFragmentWithCostTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/PlanFragmentWithCostTest.java
@@ -1512,7 +1512,7 @@ public class PlanFragmentWithCostTest extends PlanTestBase {
                 "join [shuffle] (select 1 as v1_c1 where false) v1 on t1a=v1 and t1a=v1_c1");
         assertContains(plan, "  9:HASH JOIN\n" +
                 "  |  join op: INNER JOIN (PARTITIONED)\n" +
-                "  |  equal join conjunct: [4: t1a, VARCHAR, true] = [16: cast, VARCHAR(1048576), false]\n" +
+                "  |  equal join conjunct: [4: t1a, VARCHAR, true] = [16: cast, VARCHAR(1048576), true]\n" +
                 "  |  output columns: 1, 4\n" +
                 "  |  cardinality: 9000\n" +
                 "  |  \n" +
