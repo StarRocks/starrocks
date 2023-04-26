38be17dd7b4fb415d89dd07976c45779fa29b6b8
Smith Cruise <chendingchao1@126.com>
2023-04-26 18:59:33 +0800

    fix ut
    Signed-off-by: Smith Cruise <chendingchao1@126.com>

--- be/src/formats/parquet/meta_helper.cpp
+++ be/src/formats/parquet/meta_helper.cpp
@@ -68,8 +68,10 @@ void IcebergMetaHelper::set_existed_column_names(std::unordered_set<std::string>
     const auto& fields = _t_iceberg_schema->fields;
     for (const auto& field : fields) {
         if (_file_metadata->schema().contain_field_id(field.field_id)) {
-            // Use _file_metadata->schema()'s column name, it's already formatted
-            names->emplace(_file_metadata->schema().get_stored_column_by_field_id(field.field_id)->name);
+            // We can't use _file_metadata->schema()'s column name, because column name from
+            // _file_metadata->schema() was set by parquet metadata, it may not the same as column name from FE.
+            // names->emplace(_file_metadata->schema().get_stored_column_by_field_id(field.field_id)->name);
+            names->emplace(_case_sensitive ? field.name : boost::algorithm::to_lower_copy(field.name));
         }
     }
 }
