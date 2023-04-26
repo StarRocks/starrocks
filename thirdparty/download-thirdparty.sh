603408195407d9cb72dcaeeb13af5ad56e1a6b49
iverson <58965658+wanpengfei-git@users.noreply.github.com>
2023-04-26 15:14:42 +0800

    Revert "[Refactor] force arrow links to external jemalloc static library (#21897) (#22237)"
    This reverts commit 93e70e850b52f0ef5da30235511c1c9a714b7a8b.

--- thirdparty/download-thirdparty.sh
+++ thirdparty/download-thirdparty.sh
@@ -451,13 +451,3 @@ fi
 echo "Finished patching $SERDES_SOURCE"
 cd -
 
-# patch arrows to use our built jemalloc
-if [[ -d $TP_SOURCE_DIR/$ARROW_SOURCE ]] ; then
-    cd $TP_SOURCE_DIR/$ARROW_SOURCE
-    if [ ! -f $PATCHED_MARK ] && [ $ARROW_SOURCE = "arrow-apache-arrow-5.0.0" ] ; then
-        patch -p1 < $TP_PATCH_DIR/arrow-5.0.0-force-use-external-jemalloc.patch
-        touch $PATCHED_MARK
-    fi
-    cd -
-    echo "Finished patching $ARROW_SOURCE"
-fi
