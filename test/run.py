bd1db36865572ec06c5b6d86d11dbe4fc57811d0
andyziye <108652123+andyziye@users.noreply.github.com>
2023-04-26 14:27:33 +0800

    [Tool] Optimize version information in remote execution (#22538)

--- test/run.py
+++ test/run.py
@@ -118,7 +118,9 @@ if __name__ == "__main__":
 
     version = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
     print("Version: %s" % version)
-    os.environ["version"] = version
+    # os.environ["version"] = version
+    with open('version', 'w') as f:
+        f.write(version)
 
     # set environment
     os.environ["record_mode"] = "true" if record else "false"
@@ -157,5 +159,7 @@ if __name__ == "__main__":
     nose.run(argv=argv)
 
     # record mode
-    if record:
+    if record and not collect:
         sr_sql_lib.StarrocksSQLApiLib().save_r_into_file(part)
+
+    os.remove("version")
