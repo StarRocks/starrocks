774357fb35b0ae911f7f06cf71e14568e570d4b1
Alex Zhu <zhuming9011@gmail.com>
2023-04-27 03:12:43 -0500

    [BugFix] Fix update table be_configs failed but no error returned (#22658)
    Signed-off-by: sduzh <zhuming9011@qq.com>
    (cherry picked from commit 31dd6f367ec6a621bf9499d4fb08ce422313532c)

--- be/src/runtime/schema_table_sink.cpp
+++ be/src/runtime/schema_table_sink.cpp
@@ -112,6 +112,7 @@ static Status write_be_configs_table(const StarRocksNodesInfo& nodes_info, int64
         LOG(WARNING) << "write_be_configs_table ignored: UpdateConfigAction is not inited";
         return Status::OK();
     }
+    Status ret;
     for (size_t i = 0; i < columns[0]->size(); ++i) {
         int64_t be_id = columns[0]->get(i).get_int64();
         const auto& name = columns[1]->get(i).get_slice().to_string();
@@ -133,8 +134,9 @@ static Status write_be_configs_table(const StarRocksNodesInfo& nodes_info, int64
         } else {
             LOG(WARNING) << "set_config " << mode << " " << name << "=" << value << " failed " << s.to_string();
         }
+        ret.update(s);
     }
-    return Status::OK();
+    return ret;
 }
 
 Status SchemaTableSink::send_chunk(RuntimeState* state, Chunk* chunk) {
