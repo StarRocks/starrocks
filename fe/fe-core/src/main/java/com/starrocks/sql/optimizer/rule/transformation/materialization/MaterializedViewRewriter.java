fcae6b8bf587be7b8c750d1d09e2519e374cdabb
ABing <101158374+ABingHuang@users.noreply.github.com>
2023-04-27 20:42:25 +0800

    [BugFix] fix mv rewrite bug for join/agg with limit (#22684)
    Signed-off-by: ABingHuang <codekhuang@163.com>
    (cherry picked from commit d146a0f90ef54bf7d0f530b2201dc43a6ceb43d7)
    
    # Conflicts:
    #	fe/fe-core/src/test/java/com/starrocks/planner/MaterializedViewTest.java

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/materialization/MaterializedViewRewriter.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/materialization/MaterializedViewRewriter.java
@@ -334,6 +334,11 @@ public class MaterializedViewRewriter {
 
             OptExpression rewrittenExpression = tryRewriteForRelationMapping(rewriteContext, compensationJoinColumns);
             if (rewrittenExpression != null) {
+                // copy limit into rewritten plan
+                // limit will affect the statistics of rewritten plan
+                if (rewriteContext.getQueryExpression().getOp().hasLimit()) {
+                    rewrittenExpression.getOp().setLimit(rewriteContext.getQueryExpression().getOp().getLimit());
+                }
                 return rewrittenExpression;
             }
         }
