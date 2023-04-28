8a638d45bf8b05335899adeaf207c457660115d1
gengjun-git <gengjun@starrocks.com>
2023-04-28 16:35:09 +0800

    [Enhancement] Add login info to audit log (#22729)
    Signed-off-by: gengjun-git <gengjun@starrocks.com>
    (cherry picked from commit 519d955188324da226a3ac98598beecfec1d55af)

--- fe/fe-core/src/main/java/com/starrocks/common/Config.java
+++ fe/fe-core/src/main/java/com/starrocks/common/Config.java
@@ -127,7 +127,7 @@ public class Config extends ConfigBase {
     @ConfField
     public static int audit_log_roll_num = 90;
     @ConfField
-    public static String[] audit_log_modules = {"slow_query", "query"};
+    public static String[] audit_log_modules = {"slow_query", "query", "connection"};
     @ConfField(mutable = true)
     public static long qe_slow_log_ms = 5000;
     @ConfField
