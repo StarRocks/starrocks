2b625c84560a2376344c794842782b9a69775695
zihe.liu <ziheliu1024@gmail.com>
2023-04-28 10:43:32 +0800

    [BugFix] Clear bucketColumns if not all OlapScanNode use it (#22483) (#22548)
    Signed-off-by: ZiheLiu <ziheliu1024@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/common/FeConstants.java
+++ fe/fe-core/src/main/java/com/starrocks/common/FeConstants.java
@@ -71,6 +71,8 @@ public class FeConstants {
     public static int checkpoint_interval_second = 60; // 1 minutes
     // set to true to skip some step when running FE unit test
     public static boolean runningUnitTest = false;
+    // Set this flag to false to suppress showing local shuffle columns in verbose explain, when running FE unit tests.
+    public static boolean showLocalShuffleColumnsInExplain = true;
     // default scheduler interval is 10 seconds
     public static int default_scheduler_interval_millisecond = 10000;
 
