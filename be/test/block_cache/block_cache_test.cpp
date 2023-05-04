ee1ef2700dbb5bee3f33bc0ab6971f11375e634b
GavinMar <yangguansuo@starrocks.com>
2023-05-04 05:40:57 +0000

    [Enhancement] Automatically create block cache directories if it is configured but not exist.
    Signed-off-by: GavinMar <yangguansuo@starrocks.com>

--- be/test/block_cache/block_cache_test.cpp
+++ be/test/block_cache/block_cache_test.cpp
@@ -19,8 +19,8 @@ protected:
 };
 
 TEST_F(BlockCacheTest, hybrid_cache) {
-    BlockCache* cache = BlockCache::instance();
-    const size_t block_size = 4 * 1024;
+    std::unique_ptr<BlockCache> cache(new BlockCache);
+    const size_t block_size = 1 * 1024 * 1024;
 
     CacheOptions options;
     options.mem_space_size = 20 * 1024 * 1024;
@@ -71,6 +71,48 @@ TEST_F(BlockCacheTest, hybrid_cache) {
     // not found
     res = cache->read_cache(cache_key, block_size * 1000, batch_size, value);
     ASSERT_TRUE(res.status().is_not_found());
+}
+
+TEST_F(BlockCacheTest, auto_create_disk_cache_path) {
+    std::unique_ptr<BlockCache> cache(new BlockCache);
+    const size_t block_size = 1 * 1024 * 1024;
+
+    CacheOptions options;
+    options.mem_space_size = 20 * 1024 * 1024;
+    size_t quota = 500 * 1024 * 1024;
+    options.disk_spaces.push_back({.path = "./ut_dir/final_entry_not_exist", .size = quota});
+    options.block_size = block_size;
+    options.engine = "starcache";
+    Status status = cache->init(options);
+    ASSERT_TRUE(status.ok());
+
+    const size_t batch_size = block_size - 1234;
+    const size_t rounds = 3;
+    const std::string cache_key = "test_file";
+
+    // write cache
+    off_t offset = 0;
+    for (size_t i = 0; i < rounds; ++i) {
+        char ch = 'a' + i % 26;
+        std::string value(batch_size, ch);
+        Status st = cache->write_cache(cache_key + std::to_string(i), 0, batch_size, value.c_str());
+        ASSERT_TRUE(st.ok());
+        offset += batch_size;
+    }
+
+    // read cache
+    offset = 0;
+    for (size_t i = 0; i < rounds; ++i) {
+        char ch = 'a' + i % 26;
+        std::string expect_value(batch_size, ch);
+        char value[batch_size] = {0};
+        auto res = cache->read_cache(cache_key + std::to_string(i), 0, batch_size, value);
+        ASSERT_TRUE(res.status().ok());
+        ASSERT_EQ(memcmp(value, expect_value.c_str(), batch_size), 0);
+        offset += batch_size;
+    }
+
+    cache->shutdown();
 }
 
 } // namespace starrocks
