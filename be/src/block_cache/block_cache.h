00ad664bacdcae1ed064cb544d20a2d8c8f9b501
Gavin <ygsmangyuan@gmail.com>
2023-05-06 20:50:48 +0800

    Revert "[Enhancement] Automatically create block cache directories if it is configured but not exist. (#22816)" (#22928)

--- be/src/block_cache/block_cache.h
+++ be/src/block_cache/block_cache.h
@@ -38,9 +38,7 @@ public:
     size_t block_size() const { return _block_size; }
 
 private:
-#ifndef BE_TEST
-    BlockCache() = default;
-#endif
+    BlockCache();
 
     size_t _block_size = 0;
     std::unique_ptr<KvCache> _kv_cache;
