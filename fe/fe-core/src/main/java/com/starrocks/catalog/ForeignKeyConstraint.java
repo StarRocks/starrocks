14c0c4a2ba059550349b792592a27497fe5aba23
Youngwb <yangwenbo_mailbox@163.com>
2023-05-09 20:31:40 +0800

    [Enhancement] Support materialized view delta join for hive table [2/N] (#22883)
    Signed-off-by: Youngwb <yangwenbo_mailbox@163.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/ForeignKeyConstraint.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/ForeignKeyConstraint.java
@@ -37,13 +37,17 @@ import java.util.stream.Collectors;
 // a table may have multi foreign key constraints.
 public class ForeignKeyConstraint {
     private static final Logger LOG = LogManager.getLogger(ForeignKeyConstraint.class);
+    private static final String FOREIGN_KEY_REGEX = "((\\.?\\w+:?)*)\\s*\\(((,?\\s*\\w+\\s*)+)\\)\\s+((?i)REFERENCES)\\s+" +
+            "((\\.?\\w+:?)+)\\s*\\(((,?\\s*\\w+\\s*)+)\\)";
 
-    private static final String FOREIGN_KEY_REGEX = "\\(((,?\\s*\\w+\\s*)+)\\)\\s+((?i)REFERENCES)\\s+" +
-            "((\\.?\\w+)+)\\s*\\(((,?\\s*\\w+\\s*)+)\\)";
     public static final Pattern FOREIGN_KEY_PATTERN = Pattern.compile(FOREIGN_KEY_REGEX);
+
     // table with primary key or unique key
     // if parent table is dropped, the foreign key is not dropped cascade now.
     private final BaseTableInfo parentTableInfo;
+
+    // table with foreign key, it only used for materialized view.
+    private final BaseTableInfo childTableInfo;
 
     // here id is preferred, but meta of column does not have id.
     // have to use name here, so column rename is not supported
@@ -52,8 +56,10 @@ public class ForeignKeyConstraint {
 
     public ForeignKeyConstraint(
             BaseTableInfo parentTableInfo,
+            BaseTableInfo childTableInfo,
             List<Pair<String, String>> columnRefPairs) {
         this.parentTableInfo = parentTableInfo;
+        this.childTableInfo = childTableInfo;
         this.columnRefPairs = columnRefPairs;
     }
 
@@ -61,24 +67,32 @@ public class ForeignKeyConstraint {
         return parentTableInfo;
     }
 
+    public BaseTableInfo getChildTableInfo() {
+        return childTableInfo;
+    }
+
     public List<Pair<String, String>> getColumnRefPairs() {
         return columnRefPairs;
     }
 
-    // for default catalog, the format is: (column1, column2) REFERENCES default_catalog.dbid.tableid(column1', column2')
-    // for other catalog, the format is: (column1, column2) REFERENCES default_catalog.dbname.tablename(column1', column2')
+    // for olap table, the format is: (column1, column2) REFERENCES default_catalog.dbid.tableid(column1', column2')
+    // for materialized view, the format is: catalog1.dbName1.tableName1(column1, column2) REFERENCES
+    // catalog2.dbName2.tableName2(column1', column2')
     @Override
     public String toString() {
         if (parentTableInfo == null || columnRefPairs == null) {
             return "";
         }
         StringBuilder sb = new StringBuilder();
+        if (childTableInfo != null) {
+            sb.append(childTableInfo);
+        }
         sb.append("(");
         String baseColumns = Joiner.on(",").join(columnRefPairs.stream().map(pair -> pair.first).collect(Collectors.toList()));
         sb.append(baseColumns);
         sb.append(")");
         sb.append(" REFERENCES ");
-        sb.append(parentTableInfo.toString());
+        sb.append(parentTableInfo);
         sb.append("(");
         String parentColumns = Joiner.on(",").join(columnRefPairs.stream().map(pair -> pair.second).collect(Collectors.toList()));
         sb.append(parentColumns);
@@ -103,30 +117,81 @@ public class ForeignKeyConstraint {
                 LOG.warn("invalid constraint:{}", foreignKeyConstraintDescs);
                 continue;
             }
-            String sourceColumns = foreignKeyMatcher.group(1);
-            String tablePath = foreignKeyMatcher.group(4);
-            String targetColumns = foreignKeyMatcher.group(6);
-            List<String> baseColumns = Arrays.asList(sourceColumns.split(","))
-                    .stream().map(String::trim).collect(Collectors.toList());
-            List<String> parentColumns = Arrays.asList(targetColumns.split(","))
-                    .stream().map(String::trim).collect(Collectors.toList());
 
-            String[] parts = tablePath.split("\\.");
-            Preconditions.checkState(parts.length == 3);
-            String catalogName = parts[0];
-            String db = parts[1];
-            String table = parts[2];
+            String sourceTablePath = foreignKeyMatcher.group(1);
+            String sourceColumns = foreignKeyMatcher.group(3);
+
+            String targetTablePath = foreignKeyMatcher.group(6);
+            String targetColumns = foreignKeyMatcher.group(8);
+
+            List<String> childColumns = Arrays.stream(sourceColumns.split(",")).
+                    map(String::trim).collect(Collectors.toList());
+            List<String> parentColumns = Arrays.stream(targetColumns.split(",")).
+                    map(String::trim).collect(Collectors.toList());
+
+            String[] targetTableParts = targetTablePath.split("\\.");
+            Preconditions.checkState(targetTableParts.length == 3);
+            String targetCatalogName = targetTableParts[0];
+            String targetDb = targetTableParts[1];
+            String targetTable = targetTableParts[2];
+            BaseTableInfo parentTableInfo = getTableBaseInfo(targetCatalogName, targetDb, targetTable);
+
+            BaseTableInfo childTableInfo = null;
+            if (!Strings.isNullOrEmpty(sourceTablePath)) {
+                String[] sourceTableParts = sourceTablePath.split("\\.");
+                Preconditions.checkState(sourceTableParts.length == 3);
+                String sourceCatalogName = sourceTableParts[0];
+                String sourceDb = sourceTableParts[1];
+                String sourceTable = sourceTableParts[2];
+                childTableInfo = getTableBaseInfo(sourceCatalogName, sourceDb, sourceTable);
+            }
 
             List<Pair<String, String>> columnRefPairs =
-                    Streams.zip(baseColumns.stream(), parentColumns.stream(), Pair::create).collect(Collectors.toList());
-            if (catalogName.equals(InternalCatalog.DEFAULT_INTERNAL_CATALOG_NAME)) {
-                BaseTableInfo parentTableInfo = new BaseTableInfo(Long.parseLong(db), Long.parseLong(table));
-                foreignKeyConstraints.add(new ForeignKeyConstraint(parentTableInfo, columnRefPairs));
-            } else {
-                BaseTableInfo parentTableInfo = new BaseTableInfo(catalogName, db, table);
-                foreignKeyConstraints.add(new ForeignKeyConstraint(parentTableInfo, columnRefPairs));
-            }
+                    Streams.zip(childColumns.stream(), parentColumns.stream(), Pair::create).collect(Collectors.toList());
+            foreignKeyConstraints.add(new ForeignKeyConstraint(parentTableInfo, childTableInfo, columnRefPairs));
         }
         return foreignKeyConstraints;
+    }
+
+    private static BaseTableInfo getTableBaseInfo(String catalog, String db, String table) {
+        BaseTableInfo baseTableInfo;
+        if (catalog.equals(InternalCatalog.DEFAULT_INTERNAL_CATALOG_NAME)) {
+            baseTableInfo = new BaseTableInfo(Long.parseLong(db), Long.parseLong(table));
+        } else {
+            baseTableInfo = new BaseTableInfo(catalog, db, table);
+        }
+        return baseTableInfo;
+    }
+
+    public static String getShowCreateTableConstraintDesc(List<ForeignKeyConstraint> constraints) {
+        StringBuilder sb = new StringBuilder();
+
+        List<String> constraintStrs = Lists.newArrayList();
+        for (ForeignKeyConstraint constraint : constraints) {
+            BaseTableInfo parentTableInfo = constraint.getParentTableInfo();
+            BaseTableInfo childTableInfo = constraint.getChildTableInfo();
+
+            StringBuilder constraintSb = new StringBuilder();
+            if (childTableInfo != null) {
+                constraintSb.append(childTableInfo.getReadableString());
+            }
+            constraintSb.append("(");
+            String baseColumns = Joiner.on(",").join(constraint.getColumnRefPairs()
+                    .stream().map(pair -> pair.first).collect(Collectors.toList()));
+            constraintSb.append(baseColumns);
+            constraintSb.append(")");
+            constraintSb.append(" REFERENCES ");
+            constraintSb.append(parentTableInfo.getReadableString());
+
+            constraintSb.append("(");
+            String parentColumns = Joiner.on(",").join(constraint.getColumnRefPairs()
+                    .stream().map(pair -> pair.second).collect(Collectors.toList()));
+            constraintSb.append(parentColumns);
+            constraintSb.append(")");
+            constraintStrs.add(constraintSb.toString());
+        }
+
+        sb.append(Joiner.on(";").join(constraintStrs));
+        return sb.toString();
     }
 }
