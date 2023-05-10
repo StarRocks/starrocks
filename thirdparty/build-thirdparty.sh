f0d6477ed06a20329149f84aace7de3477980145
Zaorang Yang <zaorangy@gmail.com>
2023-05-10 11:03:55 +0800

    Formate code
    Signed-off-by: Zaorang Yang <zaorangy@gmail.com>

--- thirdparty/build-thirdparty.sh
+++ thirdparty/build-thirdparty.sh
@@ -518,7 +518,7 @@ build_rocksdb() {
 build_sasl() {
     check_if_source_exist $SASL_SOURCE
     cd $TP_SOURCE_DIR/$SASL_SOURCE
-    CFLAGS=  ./autogen.sh --prefix=$TP_INSTALL_DIR --enable-gssapi=no --enable-static=yes --enable-shared=no
+    CFLAGS= ./autogen.sh --prefix=$TP_INSTALL_DIR --enable-gssapi=no --enable-static=yes --enable-shared=no
     make -j$PARALLEL
     make install
 }
