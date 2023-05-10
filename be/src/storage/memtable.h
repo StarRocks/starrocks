1126e890cd806e1f708e07f9ca2f8b20b1fa92d2
zhangqiang <qiangzh95@gmail.com>
2023-05-10 21:03:31 +0800

    [Enhancement] Support delete sql for primary key table with sort key (#22799)
    Primary key table with sort key does not support partial update so far, so we need to provide all columns data when we doing data import. But the delete sql only provide primary key columns and the delete request will be reject because the request is consider as partial update request. This pr will support the delete request for primary key table with sort key.
    
    Signed-off-by: zhangqiang <qiangzh95@gmail.com>

--- be/src/storage/memtable.h
+++ be/src/storage/memtable.h
@@ -64,6 +64,10 @@ public:
 
     void set_write_buffer_row(size_t max_buffer_row) { _max_buffer_row = max_buffer_row; }
 
+    void set_partial_schema_with_sort_key(bool partial_schema_with_sort_key) {
+        _partial_schema_with_sort_key = partial_schema_with_sort_key;
+    }
+
     static Schema convert_schema(const TabletSchema* tablet_schema, const std::vector<SlotDescriptor*>* slot_descs);
 
     ChunkPtr get_result_chunk() { return _result_chunk; }
@@ -120,6 +124,7 @@ private:
     size_t _chunk_bytes_usage = 0;
     size_t _aggregator_memory_usage = 0;
     size_t _aggregator_bytes_usage = 0;
+    bool _partial_schema_with_sort_key = false;
 };
 
 inline std::ostream& operator<<(std::ostream& os, const MemTable& table) {
