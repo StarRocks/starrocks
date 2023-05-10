9c6254d432792ed9fd3a8aaf4a8e315c09d13655
stdpain <34912776+stdpain@users.noreply.github.com>
2023-05-10 19:09:19 +0800

    [Enhancement] add some trace log for dictionary manager (#23132)
    Signed-off-by: stdpain <drfeng08@gmail.com>
    (cherry picked from commit 639eb30a55a74994403c62614cb46be61b682381)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/sql/optimizer/statistics/CacheDictManager.java
    #	fe/fe-core/src/main/java/com/starrocks/sql/optimizer/statistics/ColumnDict.java

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/statistics/ColumnDict.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/statistics/ColumnDict.java
@@ -1,4 +1,20 @@
+<<<<<<< HEAD
 // This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
+=======
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+>>>>>>> 639eb30a5 ([Enhancement] add some trace log for dictionary manager (#23132))
 
 package com.starrocks.sql.optimizer.statistics;
 
@@ -9,11 +25,13 @@ import java.nio.ByteBuffer;
 
 public final class ColumnDict {
     private final ImmutableMap<ByteBuffer, Integer> dict;
-    private final long versionTime;
+    private final long collectedVersionTime;
+    private long versionTime;
 
     public ColumnDict(ImmutableMap<ByteBuffer, Integer> dict, long versionTime) {
         Preconditions.checkState(dict.size() > 0 && dict.size() <= 256);
         this.dict = dict;
+        this.collectedVersionTime = versionTime;
         this.versionTime = versionTime;
     }
 
@@ -23,5 +41,13 @@ public final class ColumnDict {
 
     public long getVersionTime() {
         return versionTime;
+    }
+
+    public long getCollectedVersionTime() {
+        return collectedVersionTime;
+    }
+
+    void updateVersionTime(long versionTime) {
+        this.versionTime = versionTime;
     }
 }
