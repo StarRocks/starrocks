3fa7577654a669d76968b310aeb471e1ead46333
eyes_on_me <3675229+silverbullet233@users.noreply.github.com>
2023-05-10 19:01:36 +0800

    [Enhancement][BugFix] simplify cancel process of spilling (#22611)
    Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>
    (cherry picked from commit ab70429d75c67530df06779378912565e47c295f)
    
    # Conflicts:
    #	be/src/exec/pipeline/aggregate/spillable_aggregate_blocking_sink_operator.cpp

--- be/src/exec/pipeline/aggregate/spillable_aggregate_blocking_sink_operator.cpp
+++ be/src/exec/pipeline/aggregate/spillable_aggregate_blocking_sink_operator.cpp
@@ -39,22 +39,36 @@ bool SpillableAggregateBlockingSinkOperator::is_finished() const {
 }
 
 Status SpillableAggregateBlockingSinkOperator::set_finishing(RuntimeState* state) {
+<<<<<<< HEAD
+=======
+    if (_spill_strategy == spill::SpillStrategy::NO_SPILL) {
+        RETURN_IF_ERROR(AggregateBlockingSinkOperator::set_finishing(state));
+        return Status::OK();
+    }
+    if (state->is_cancelled()) {
+        _aggregator->spiller()->cancel();
+    }
+>>>>>>> ab70429d7 ([Enhancement][BugFix] simplify cancel process of spilling (#22611))
     // ugly code
     // TODO: fixme
     auto io_executor = _aggregator->spill_channel()->io_executor();
 
     auto flush_function = [this](RuntimeState* state, auto io_executor) {
-        return _aggregator->spiller()->flush(state, *io_executor, spill::MemTrackerGuard(tls_mem_tracker));
+        return _aggregator->spiller()->flush(
+                state, *io_executor,
+                spill::ResourceMemTrackerGuard(tls_mem_tracker, state->query_ctx()->weak_from_this()));
     };
     auto set_call_back_function = [this](RuntimeState* state, auto io_executor) {
         _aggregator->spill_channel()->set_finishing();
         RETURN_IF_ERROR(AggregateBlockingSinkOperator::set_finishing(state));
         return _aggregator->spiller()->set_flush_all_call_back(
-                [this]() {
+                [this, state]() {
                     _is_finished = true;
+                    RETURN_IF_ERROR(AggregateBlockingSinkOperator::set_finishing(state));
                     return Status::OK();
                 },
-                state, *io_executor, spill::MemTrackerGuard(tls_mem_tracker));
+                state, *io_executor,
+                spill::ResourceMemTrackerGuard(tls_mem_tracker, state->query_ctx()->weak_from_this()));
     };
 
     if (_aggregator->spill_channel()->is_working()) {
--- be/src/exec/pipeline/pipeline_driver_executor.cpp
+++ be/src/exec/pipeline/pipeline_driver_executor.cpp
@@ -125,7 +125,6 @@ void GlobalDriverExecutor::_worker_thread() {
         auto* runtime_state = runtime_state_ptr.get();
         {
             SCOPED_THREAD_LOCAL_MEM_TRACKER_SETTER(runtime_state->instance_mem_tracker());
-
             if (fragment_ctx->is_canceled()) {
                 driver->cancel_operators(runtime_state);
                 if (driver->is_still_pending_finish()) {
