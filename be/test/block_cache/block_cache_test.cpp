8b6d668194e4f319d613720a6634779d63dbeb62
GavinMar <yangguansuo@starrocks.com>
2023-05-11 07:01:29 +0000

    [Enhancement] Add a lru insertion point option to block cache to support inserting the new cache item to a given position of the lru list, instead of inserting into the lru head every time.
    Now, this option is only supported in fb cachelib engine.
    
    Signed-off-by: GavinMar <yangguansuo@starrocks.com>

--- be/test/block_cache/block_cache_test.cpp
+++ be/test/block_cache/block_cache_test.cpp
@@ -54,13 +54,14 @@ protected:
 
 TEST_F(BlockCacheTest, hybrid_cache) {
     std::unique_ptr<BlockCache> cache(new BlockCache);
-    const size_t block_size = 1 * 1024 * 1024;
+    const size_t block_size = 1024 * 1024;
 
     CacheOptions options;
     options.mem_space_size = 20 * 1024 * 1024;
     size_t quota = 500 * 1024 * 1024;
     options.disk_spaces.push_back({.path = "./ut_dir/block_disk_cache", .size = quota});
     options.block_size = block_size;
+    options.max_concurrent_inserts = 100000;
     options.engine = "starcache";
     Status status = cache->init(options);
     ASSERT_TRUE(status.ok());
@@ -102,10 +103,21 @@ TEST_F(BlockCacheTest, hybrid_cache) {
     // not found
     res = cache->read_cache(cache_key, block_size * 1000, batch_size, value);
     ASSERT_TRUE(res.status().is_not_found());
+
+    cache->shutdown();
 }
 
 TEST_F(BlockCacheTest, write_with_overwrite_option) {
-    BlockCache* cache = BlockCache::instance();
+    std::unique_ptr<BlockCache> cache(new BlockCache);
+    const size_t block_size = 1024 * 1024;
+
+    CacheOptions options;
+    options.mem_space_size = 20 * 1024 * 1024;
+    options.block_size = block_size;
+    options.max_concurrent_inserts = 100000;
+    options.engine = "starcache";
+    Status status = cache->init(options);
+    ASSERT_TRUE(status.ok());
 
     const size_t cache_size = 1024;
     const std::string cache_key = "test_file";
@@ -127,17 +139,20 @@ TEST_F(BlockCacheTest, write_with_overwrite_option) {
     std::string value3(cache_size, 'c');
     st = cache->write_cache(cache_key, 0, cache_size, value3.c_str(), 0, false);
     ASSERT_TRUE(st.is_already_exist());
+
+    cache->shutdown();
 }
 
 TEST_F(BlockCacheTest, auto_create_disk_cache_path) {
     std::unique_ptr<BlockCache> cache(new BlockCache);
-    const size_t block_size = 1 * 1024 * 1024;
+    const size_t block_size = 1024 * 1024;
 
     CacheOptions options;
     options.mem_space_size = 20 * 1024 * 1024;
     size_t quota = 500 * 1024 * 1024;
     options.disk_spaces.push_back({.path = "./ut_dir/final_entry_not_exist", .size = quota});
     options.block_size = block_size;
+    options.max_concurrent_inserts = 100000;
     options.engine = "starcache";
     Status status = cache->init(options);
     ASSERT_TRUE(status.ok());
@@ -170,5 +185,42 @@ TEST_F(BlockCacheTest, auto_create_disk_cache_path) {
 
     cache->shutdown();
 }
+
+#ifdef WITH_CACHELIB
+TEST_F(BlockCacheTest, custom_lru_insertion_point) {
+    std::unique_ptr<BlockCache> cache(new BlockCache);
+    const size_t block_size = 1024 * 1024;
+
+    CacheOptions options;
+    options.mem_space_size = 20 * 1024 * 1024;
+    options.block_size = block_size;
+    options.max_concurrent_inserts = 100000;
+    options.engine = "cachelib";
+    // insert in the 1/2 of the lru list
+    options.lru_insertion_point = 1;
+    Status status = cache->init(options);
+    ASSERT_TRUE(status.ok());
+
+    const size_t rounds = 20;
+    const size_t batch_size = block_size;
+    const std::string cache_key = "test_file";
+    // write cache
+    // only 12 blocks can be cached
+    for (size_t i = 0; i < rounds; ++i) {
+        char ch = 'a' + i % 26;
+        std::string value(batch_size, ch);
+        Status st = cache->write_cache(cache_key + std::to_string(i), 0, batch_size, value.c_str());
+        ASSERT_TRUE(st.ok());
+    }
+
+    // read cache
+    // with the 1/2 lru insertion point, the test_file1 items will not be evicted
+    char value[batch_size] = {0};
+    auto res = cache->read_cache(cache_key + std::to_string(1), 0, batch_size, value);
+    ASSERT_TRUE(res.status().ok());
+
+    cache->shutdown();
+}
+#endif
 
 } // namespace starrocks
