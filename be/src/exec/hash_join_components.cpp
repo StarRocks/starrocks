837db15eb78143b94b7a11d4785afffeb3bc1989
stdpain <34912776+stdpain@users.noreply.github.com>
2023-05-15 16:51:47 +0800

    [BugFix] Fix use wrong hash table when spill left/right outer join has other conjuncts (#23302)
    Signed-off-by: stdpain <drfeng08@gmail.com>

--- be/src/exec/hash_join_components.cpp
+++ be/src/exec/hash_join_components.cpp
@@ -34,7 +34,7 @@ StatusOr<ChunkPtr> HashJoinProber::probe_chunk(RuntimeState* state, JoinHashTabl
     if (!_current_probe_has_remain) {
         _probe_chunk = nullptr;
     }
-    RETURN_IF_ERROR(_hash_joiner.filter_probe_output_chunk(chunk));
+    RETURN_IF_ERROR(_hash_joiner.filter_probe_output_chunk(chunk, *hash_table));
     TRY_CATCH_ALLOC_SCOPE_END()
     return chunk;
 }
