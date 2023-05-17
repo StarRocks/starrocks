8d4ed150600c943e5ea8ae271d44186cc2054e93
Kevin Cai <caixiaohua@starrocks.com>
2023-05-17 19:01:30 +0800

    [Enhancement] Enable SyncPoint build even in Release mode (#23574)
    check BE_TEST macro as well for the condition compiling
    
    Signed-off-by: Kevin Xiaohua Cai <caixiaohua@starrocks.com>

--- be/src/testutil/sync_point_impl.cc
+++ be/src/testutil/sync_point_impl.cc
@@ -26,7 +26,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#ifndef NDEBUG
+#if !defined(NDEBUG) || defined(BE_TEST)
 namespace starrocks {
 KillPoint* KillPoint::GetInstance() {
     static KillPoint kp;
