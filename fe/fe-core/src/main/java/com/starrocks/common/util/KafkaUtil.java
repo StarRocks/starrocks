2104730808898db3f463c2d0ce8da0921c5aa619
Zaorang Yang <zaorangy@gmail.com>
2023-05-17 16:30:19 +0800

    [Enhancement] Add retry operation when getting kafka info time out
    Signed-off-by: Zaorang Yang <zaorangy@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/common/util/KafkaUtil.java
+++ fe/fe-core/src/main/java/com/starrocks/common/util/KafkaUtil.java
@@ -186,6 +186,24 @@ public class KafkaUtil {
                 address = new TNetworkAddress(be.getHost(), be.getBrpcPort());
 
                 // get info
+                int retryTimes = 0;
+                while (true) {
+                    request.timeout = Config.routine_load_kafka_timeout_second;
+                    Future<PProxyResult> future = BackendServiceClient.getInstance().getInfo(address, request);
+                    PProxyResult result = future.get(Config.routine_load_kafka_timeout_second, TimeUnit.SECONDS);
+                    TStatusCode code = TStatusCode.findByValue(result.status.statusCode);
+                    if (code != TStatusCode.OK) {
+                        LOG.warn("failed to send proxy request to " + address + " err " + result.status.errorMsgs);
+                        // When getting kafka info timed out, we tried again three times.
+                        if (++retryTimes > 3 || retryTimes * Config.routine_load_kafka_timeout_second >
+                                                                        Config.routine_load_task_timeout_second) {
+                            throw new UserException(
+                                    "failed to send proxy request to " + address + " err " + result.status.errorMsgs);
+                        }
+                    } else {
+                        return result;
+                    }
+                }
                 request.timeout = Config.routine_load_kafka_timeout_second;
                 Future<PProxyResult> future = BackendServiceClient.getInstance().getInfo(address, request);
                 PProxyResult result = future.get(Config.routine_load_kafka_timeout_second, TimeUnit.SECONDS);
