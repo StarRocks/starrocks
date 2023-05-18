cef677afd7e8b39084c66e8128ecd4941e347d92
stdpain <34912776+stdpain@users.noreply.github.com>
2023-05-18 08:35:26 +0800

    opt the performance of bitmap_agg
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/util/bitmap_value.h
+++ be/src/util/bitmap_value.h
@@ -145,24 +145,35 @@ public:
      * Add value x
      *
      */
-    void add(uint32_t x) { roarings[0].add(x); }
-
-    void add(uint64_t x) { roarings[highBytes(x)].add(lowBytes(x)); }
+    template <typename T>
+    void add(T x) {
+        if constexpr (sizeof(T) <= 4) {
+            roarings[0].add(x);
+        } else {
+            roarings[highBytes(x)].add(lowBytes(x));
+        }
+    }
 
     /**
      * Add value n_args from pointer vals
      *
      */
-    void addMany(size_t n_args, const uint32_t* vals) {
-        for (size_t lcv = 0; lcv < n_args; lcv++) {
-            roarings[0].add(vals[lcv]);
+    template <typename T>
+    void addMany(size_t n_args, const T* vals) {
+        if constexpr (std::is_same_v<T, int32_t> || std::is_same_v<T, uint32_t>) {
+            roarings[0].addMany(n_args, reinterpret_cast<const uint32_t*>(vals));
             roarings[0].setCopyOnWrite(copyOnWrite);
-        }
-    }
-    void addMany(size_t n_args, const uint64_t* vals) {
-        for (size_t lcv = 0; lcv < n_args; lcv++) {
-            roarings[highBytes(vals[lcv])].add(lowBytes(vals[lcv]));
-            roarings[highBytes(vals[lcv])].setCopyOnWrite(copyOnWrite);
+        } else if (sizeof(T) <= 4) {
+            for (size_t lcv = 0; lcv < n_args; lcv++) {
+                roarings[0].add(vals[lcv]);
+                roarings[0].setCopyOnWrite(copyOnWrite);
+            }
+        } else {
+            // TODO: optimize uint64_t*
+            for (size_t lcv = 0; lcv < n_args; lcv++) {
+                roarings[highBytes(vals[lcv])].add(lowBytes(vals[lcv]));
+                roarings[highBytes(vals[lcv])].setCopyOnWrite(copyOnWrite);
+            }
         }
     }
 
@@ -1005,33 +1016,11 @@ public:
         }
     }
 
-    void add(uint64_t value) {
-        switch (_type) {
-        case EMPTY:
-            _sv = value;
-            _type = SINGLE;
-            break;
-        case SINGLE:
-            //there is no need to convert the type if two variables are equal
-            if (_sv == value) {
-                break;
-            }
-            _set.insert(_sv);
-            _set.insert(value);
-            _type = SET;
-            break;
-        case BITMAP:
-            _bitmap->add(value);
-            break;
-        case SET:
-            if (_set.size() < 32) {
-                _set.insert(value);
-            } else {
-                to_bitmap();
-                _bitmap->add(value);
-            }
-        }
-    }
+    template <typename T>
+    void add(T value);
+
+    template <typename T>
+    void add_many(size_t n_args, const T* vals);
 
     void to_bitmap() {
         _bitmap = std::make_shared<detail::Roaring64Map>();
@@ -1774,5 +1763,47 @@ private:
     uint64_t _sv = 0; // store the single value when _type == SINGLE
     BitmapDataType _type{EMPTY};
 };
+
+template <typename T>
+void BitmapValue::add_many(size_t n_args, const T* vals) {
+    if (_type != BITMAP) {
+        for (size_t i = 0; i < n_args; i++) {
+            add(vals[i]);
+        }
+    } else {
+        _bitmap->template addMany(n_args, vals);
+    }
+}
+
+template <typename T>
+void BitmapValue::add(T value) {
+    switch (_type) {
+    case EMPTY:
+        _sv = value;
+        _type = SINGLE;
+        break;
+    case SINGLE:
+        //there is no need to convert the type if two variables are equal
+        if (_sv == value) {
+            break;
+        }
+
+        _set = std::make_unique<phmap::flat_hash_set<uint64_t>>();
+        _set->insert(_sv);
+        _set->insert(value);
+        _type = SET;
+        break;
+    case BITMAP:
+        _bitmap->add(value);
+        break;
+    case SET:
+        if (_set->size() < 32) {
+            _set->insert(value);
+        } else {
+            _from_set_to_bitmap();
+            _bitmap->add(value);
+        }
+    }
+}
 
 } // namespace starrocks
