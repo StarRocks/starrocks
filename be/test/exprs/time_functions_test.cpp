42c012262ec8ea46549d0d25da0a0eb2fcca3275
zhangruchubaba <zhangruchubaba@gmail.com>
2023-05-20 18:44:03 +0800

    [Feature] add time function: makedate #13051
    Signed-off-by: zhangruchubaba <zhangruchubaba@gmail.com>

--- be/test/exprs/time_functions_test.cpp
+++ be/test/exprs/time_functions_test.cpp
@@ -3212,4 +3212,64 @@ TEST_F(TimeFunctionsTest, DateSliceCeilTest) {
     }
 }
 
+TEST_F(TimeFunctionsTest, MakeDateTest) {
+
+    auto year_value = Int32Column::create();
+    auto day_of_year_value = Int32Column::create();
+
+    year_value->append(0);
+    day_of_year_value->append(1);
+
+
+    year_value->append(2023);
+    day_of_year_value->append(0);
+
+    year_value->append(2023);
+    day_of_year_value->append(32);
+
+    year_value->append(2023);
+    day_of_year_value->append(365);
+
+    year_value->append(2023);
+    day_of_year_value->append(366);
+
+    year_value->append(9999);
+    day_of_year_value->append(1);
+
+    year_value->append(9999);
+    day_of_year_value->append(365);
+
+    year_value->append(9999);
+    day_of_year_value->append(366);
+
+    year_value->append(10000);
+    day_of_year_value->append(1);
+
+    year_value->append(1);
+    day_of_year_value->append(-1);
+
+    year_value->append(1);
+    (void)day_of_year_value->append_nulls(1);
+
+    Columns columns;
+    columns.emplace_back(year_value);
+    columns.emplace_back(day_of_year_value);
+
+    ColumnPtr result = TimeFunctions::make_date(_utils->get_fn_ctx(), columns).value();
+    ASSERT_TRUE(result->is_nullable());
+
+    NullableColumn::Ptr nullable_col = ColumnHelper::as_column<NullableColumn>(result);
+
+    ASSERT_EQ(DateValue::create(0000, 1, 1), nullable_col->get(0).get_date());
+    ASSERT_TRUE(nullable_col->is_null(1));
+    ASSERT_EQ(DateValue::create(2023, 2, 1), nullable_col->get(2).get_date());
+    ASSERT_EQ(DateValue::create(2023, 12, 31), nullable_col->get(3).get_date());
+    ASSERT_TRUE(nullable_col->is_null(4));
+    ASSERT_EQ(DateValue::create(9999, 1, 1), nullable_col->get(5).get_date());
+    ASSERT_EQ(DateValue::create(9999, 12, 31), nullable_col->get(6).get_date());
+    ASSERT_TRUE(nullable_col->is_null(7));
+    ASSERT_TRUE(nullable_col->is_null(8));
+    ASSERT_TRUE(nullable_col->is_null(9));
+    ASSERT_TRUE(nullable_col->is_null(10));
+}
 } // namespace starrocks
