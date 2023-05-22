219620a63444d19cf7634a467c89482f3171c1e9
stdpain <34912776+stdpain@users.noreply.github.com>
2023-05-22 16:40:04 +0800

    [BugFix] Fix right outer/anti join got wrong result when probe side empty (#23720)
    Signed-off-by: stdpain <drfeng08@gmail.com>

--- be/src/exec/pipeline/aggregate/spillable_aggregate_blocking_sink_operator.cpp
+++ be/src/exec/pipeline/aggregate/spillable_aggregate_blocking_sink_operator.cpp
@@ -40,6 +40,7 @@ bool SpillableAggregateBlockingSinkOperator::is_finished() const {
 
 Status SpillableAggregateBlockingSinkOperator::set_finishing(RuntimeState* state) {
     if (_spill_strategy == spill::SpillStrategy::NO_SPILL) {
+        _is_finished = true;
         RETURN_IF_ERROR(AggregateBlockingSinkOperator::set_finishing(state));
         _aggregator->spill_channel()->set_finishing();
         return Status::OK();
