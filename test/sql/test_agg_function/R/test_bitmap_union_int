StarRocks/starrocks

commit a6bde6525e6b56b6ecd57c8a6fde873cb026b04e

[Enhancement] opt the performance of bitmap_agg<int> (#23737)
## Problem Summary:
Fixes # (issue)

Batch add value to bitmap to opt the performance ot bitmap_agg.

Before Optimization:

```
mysql> select bitmap_count(bitmap_agg(lo_orderkey)) from lineorder where lo_orderkey < 3000000;
+-----------------------------------------+
| bitmap_count(bitmap_agg(lo_orderkey)) |
+-----------------------------------------+
|                                  749999 |
+-----------------------------------------+
1 row in set (0.32 sec)
```

After Optimization

```
mysql> select bitmap_count(bitmap_agg(lo_orderkey)) from lineorder where lo_orderkey < 3000000;
+---------------------------------------+
| bitmap_count(bitmap_agg(lo_orderkey)) |
+---------------------------------------+
|                                749999 |
+---------------------------------------+
1 row in set (0.16 sec)
```

be/src/exprs/agg/bitmap_agg.h
@@ -38,6 +38,32 @@ public:
         }
     }
 
+    bool check_valid(const std::vector<InputCppType>& values, size_t count) const {
+        for (size_t i = 0; i < count; i++) {
+            auto value = values[i];
+            if (!(value >= 0 && value <= std::numeric_limits<uint64_t>::max())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void update_batch_single_state(FunctionContext* ctx, size_t chunk_size, const Column** columns,
+                                   AggDataPtr __restrict state) const override {
+        const auto& col = down_cast<const InputColumnType&>(*columns[0]);
+        const auto& values = col.get_data();
+        if (check_valid(values, chunk_size)) {
+            this->data(state).add_many(chunk_size, values.data());
+        } else {
+            for (size_t i = 0; i < chunk_size; i++) {
+                auto value = values[i];
+                if (value >= 0 && value <= std::numeric_limits<uint64_t>::max()) {
+                    this->data(state).add(value);
+                }
+            }
+        }
+    }
+
     void merge(FunctionContext* ctx, const Column* column, AggDataPtr __restrict state, size_t row_num) const override {
         const auto& col = down_cast<const BitmapColumn&>(*column);
         this->data(state) |= *(col.get_object(row_num));

be/src/types/bitmap_value.cpp
@@ -131,36 +131,6 @@ BitmapValue::BitmapValue(const std::vector<uint64_t>& bits) {
     }
 }
 
-void BitmapValue::add(uint64_t value) {
-    switch (_type) {
-    case EMPTY:
-        _sv = value;
-        _type = SINGLE;
-        break;
-    case SINGLE:
-        //there is no need to convert the type if two variables are equal
-        if (_sv == value) {
-            break;
-        }
-
-        _set = std::make_unique<phmap::flat_hash_set<uint64_t>>();
-        _set->insert(_sv);
-        _set->insert(value);
-        _type = SET;
-        break;
-    case BITMAP:
-        _bitmap->add(value);
-        break;
-    case SET:
-        if (_set->size() < 32) {
-            _set->insert(value);
-        } else {
-            _from_set_to_bitmap();
-            _bitmap->add(value);
-        }
-    }
-}
-
 void BitmapValue::_from_set_to_bitmap() {
     _bitmap = std::make_shared<detail::Roaring64Map>();
     for (auto x : *_set) {

be/src/types/bitmap_value.h
@@ -49,7 +49,9 @@
 
 #include "common/config.h"
 #include "common/logging.h"
+#include "types/bitmap_value_detail.h"
 #include "util/coding.h"
+#include "util/phmap/phmap.h"
 #include "util/phmap/phmap_fwd_decl.h"
 #include "util/slice.h"
 
@@ -84,7 +86,11 @@ public:
     // Construct a bitmap from given elements.
     explicit BitmapValue(const std::vector<uint64_t>& bits);
 
-    void add(uint64_t value);
+    template <typename T>
+    void add(T value);
+
+    template <typename T>
+    void add_many(size_t n_args, const T* vals);
 
     // Note: rhs BitmapValue is only readable after this method
     // Compute the union between the current bitmap and the provided bitmap.
@@ -171,5 +177,47 @@ private:
     uint64_t _sv = 0; // store the single value when _type == SINGLE
     BitmapDataType _type{EMPTY};
 };
+
+template <typename T>
+void BitmapValue::add_many(size_t n_args, const T* vals) {
+    if (_type != BITMAP) {
+        for (size_t i = 0; i < n_args; i++) {
+            add(vals[i]);
+        }
+    } else {
+        _bitmap->template addMany(n_args, vals);
+    }
+}
+
+template <typename T>
+void BitmapValue::add(T value) {
+    switch (_type) {
+    case EMPTY:
+        _sv = value;
+        _type = SINGLE;
+        break;
+    case SINGLE:
+        //there is no need to convert the type if two variables are equal
+        if (_sv == value) {
+            break;
+        }
+
+        _set = std::make_unique<phmap::flat_hash_set<uint64_t>>();
+        _set->insert(_sv);
+        _set->insert(value);
+        _type = SET;
+        break;
+    case BITMAP:
+        _bitmap->add(value);
+        break;
+    case SET:
+        if (_set->size() < 32) {
+            _set->insert(value);
+        } else {
+            _from_set_to_bitmap();
+            _bitmap->add(value);
+        }
+    }
+}
 
 } // namespace starrocks

be/src/types/bitmap_value_detail.h
@@ -45,6 +45,7 @@
 #include "roaring/containers/containers.h"
 #include "roaring/roaring.h"
 #include "roaring/roaring_array.h"
+#include "util/coding.h"
 
 namespace starrocks {
 
@@ -160,24 +161,35 @@ public:
      * Add value x
      *
      */
-    void add(uint32_t x) { roarings[0].add(x); }
-
-    void add(uint64_t x) { roarings[highBytes(x)].add(lowBytes(x)); }
+    template <typename T>
+    void add(T x) {
+        if constexpr (sizeof(T) <= 4) {
+            roarings[0].add(x);
+        } else {
+            roarings[highBytes(x)].add(lowBytes(x));
+        }
+    }
 
     /**
      * Add value n_args from pointer vals
      *
      */
-    void addMany(size_t n_args, const uint32_t* vals) {
-        for (size_t lcv = 0; lcv < n_args; lcv++) {
-            roarings[0].add(vals[lcv]);
+    template <typename T>
+    void addMany(size_t n_args, const T* vals) {
+        if constexpr (std::is_same_v<T, int32_t> || std::is_same_v<T, uint32_t>) {
+            roarings[0].addMany(n_args, reinterpret_cast<const uint32_t*>(vals));
             roarings[0].setCopyOnWrite(copyOnWrite);
-        }
-    }
-    void addMany(size_t n_args, const uint64_t* vals) {
-        for (size_t lcv = 0; lcv < n_args; lcv++) {
-            roarings[highBytes(vals[lcv])].add(lowBytes(vals[lcv]));
-            roarings[highBytes(vals[lcv])].setCopyOnWrite(copyOnWrite);
+        } else if (sizeof(T) <= 4) {
+            for (size_t lcv = 0; lcv < n_args; lcv++) {
+                roarings[0].add(vals[lcv]);
+                roarings[0].setCopyOnWrite(copyOnWrite);
+            }
+        } else {
+            // TODO: optimize uint64_t*
+            for (size_t lcv = 0; lcv < n_args; lcv++) {
+                roarings[highBytes(vals[lcv])].add(lowBytes(vals[lcv]));
+                roarings[highBytes(vals[lcv])].setCopyOnWrite(copyOnWrite);
+            }
         }
     }
 

commit 279c0bd9af9ec738c67e81e7eafd4264e144f633

[BugFix] fix the bug of bitmap_union_int (#23823)
## Problem Summary:
Fixes # (issue)

The bug is introduced by the pr:
https://github.com/StarRocks/starrocks/pull/23737

bitmap_union_in supports negative int.  

if distinct value count < 32, -1 will convert to 18446744073709551611
and then add to _set.

if distinct value count > 32, -1 will convert to 4294967295, and add to
_map, then the result is wrong.

```
template <typename T>
void BitmapValue::add(T value) {
    switch (_type) {
    case EMPTY:
        _sv = value;
        _type = SINGLE;
        break;
    case SINGLE:
        //there is no need to convert the type if two variables are equal
        if (_sv == value) {
            break;
        }

        _set = std::make_unique<phmap::flat_hash_set<uint64_t>>();
        _set->insert(_sv);
        _set->insert(value);
        _type = SET;
        break;
    case BITMAP:
        _bitmap->add(value);
        break;
    case SET:
        if (_set->size() < 32) {
            _set->insert(value);
        } else {
            _from_set_to_bitmap();
            _bitmap->add(value);
        }
    }
}
```

be/src/exprs/agg/bitmap_agg.h
@@ -52,14 +52,17 @@ public:
                                    AggDataPtr __restrict state) const override {
         const auto& col = down_cast<const InputColumnType&>(*columns[0]);
         const auto& values = col.get_data();
-        if (check_valid(values, chunk_size)) {
-            this->data(state).add_many(chunk_size, values.data());
-        } else {
-            for (size_t i = 0; i < chunk_size; i++) {
-                auto value = values[i];
-                if (value >= 0 && value <= std::numeric_limits<uint64_t>::max()) {
-                    this->data(state).add(value);
-                }
+        if constexpr (LT == TYPE_INT) {
+            if (check_valid(values, chunk_size)) {
+                // All the values is unsigned, can be safely converted to unsigned int.
+                this->data(state).add_many(chunk_size, reinterpret_cast<const uint32_t*>(values.data()));
+                return;
+            }
+        }
+        for (size_t i = 0; i < chunk_size; i++) {
+            auto value = values[i];
+            if (value >= 0 && value <= std::numeric_limits<uint64_t>::max()) {
+                this->data(state).add(value);
             }
         }
     }

be/src/types/bitmap_value.cpp
@@ -1009,4 +1009,44 @@ int64_t BitmapValue::bitmap_subset_in_range_internal(const int64_t& range_start,
 
     return count;
 }
+
+void BitmapValue::add_many(size_t n_args, const uint32_t* vals) {
+    if (_type != BITMAP) {
+        for (size_t i = 0; i < n_args; i++) {
+            add(vals[i]);
+        }
+    } else {
+        _bitmap->addMany(n_args, vals);
+    }
+}
+
+void BitmapValue::add(uint64_t value) {
+    switch (_type) {
+    case EMPTY:
+        _sv = value;
+        _type = SINGLE;
+        break;
+    case SINGLE:
+        //there is no need to convert the type if two variables are equal
+        if (_sv == value) {
+            break;
+        }
+
+        _set = std::make_unique<phmap::flat_hash_set<uint64_t>>();
+        _set->insert(_sv);
+        _set->insert(value);
+        _type = SET;
+        break;
+    case BITMAP:
+        _bitmap->add(value);
+        break;
+    case SET:
+        if (_set->size() < 32) {
+            _set->insert(value);
+        } else {
+            _from_set_to_bitmap();
+            _bitmap->add(value);
+        }
+    }
+}
 } // namespace starrocks

be/src/types/bitmap_value.h
@@ -86,11 +86,9 @@ public:
     // Construct a bitmap from given elements.
     explicit BitmapValue(const std::vector<uint64_t>& bits);
 
-    template <typename T>
-    void add(T value);
+    void add(uint64_t value);
 
-    template <typename T>
-    void add_many(size_t n_args, const T* vals);
+    void add_many(size_t n_args, const uint32_t* vals);
 
     // Note: rhs BitmapValue is only readable after this method
     // Compute the union between the current bitmap and the provided bitmap.
@@ -177,47 +175,4 @@ private:
     uint64_t _sv = 0; // store the single value when _type == SINGLE
     BitmapDataType _type{EMPTY};
 };
-
-template <typename T>
-void BitmapValue::add_many(size_t n_args, const T* vals) {
-    if (_type != BITMAP) {
-        for (size_t i = 0; i < n_args; i++) {
-            add(vals[i]);
-        }
-    } else {
-        _bitmap->template addMany(n_args, vals);
-    }
-}
-
-template <typename T>
-void BitmapValue::add(T value) {
-    switch (_type) {
-    case EMPTY:
-        _sv = value;
-        _type = SINGLE;
-        break;
-    case SINGLE:
-        //there is no need to convert the type if two variables are equal
-        if (_sv == value) {
-            break;
-        }
-
-        _set = std::make_unique<phmap::flat_hash_set<uint64_t>>();
-        _set->insert(_sv);
-        _set->insert(value);
-        _type = SET;
-        break;
-    case BITMAP:
-        _bitmap->add(value);
-        break;
-    case SET:
-        if (_set->size() < 32) {
-            _set->insert(value);
-        } else {
-            _from_set_to_bitmap();
-            _bitmap->add(value);
-        }
-    }
-}
-
 } // namespace starrocks

be/src/types/bitmap_value_detail.h
@@ -161,35 +161,23 @@ public:
      * Add value x
      *
      */
-    template <typename T>
-    void add(T x) {
-        if constexpr (sizeof(T) <= 4) {
-            roarings[0].add(x);
-        } else {
-            roarings[highBytes(x)].add(lowBytes(x));
-        }
-    }
+    void add(uint32_t x) { roarings[0].add(x); }
+
+    void add(uint64_t x) { roarings[highBytes(x)].add(lowBytes(x)); }
 
     /**
      * Add value n_args from pointer vals
      *
      */
-    template <typename T>
-    void addMany(size_t n_args, const T* vals) {
-        if constexpr (std::is_same_v<T, int32_t> || std::is_same_v<T, uint32_t>) {
-            roarings[0].addMany(n_args, reinterpret_cast<const uint32_t*>(vals));
-            roarings[0].setCopyOnWrite(copyOnWrite);
-        } else if (sizeof(T) <= 4) {
-            for (size_t lcv = 0; lcv < n_args; lcv++) {
-                roarings[0].add(vals[lcv]);
-                roarings[0].setCopyOnWrite(copyOnWrite);
-            }
-        } else {
-            // TODO: optimize uint64_t*
-            for (size_t lcv = 0; lcv < n_args; lcv++) {
-                roarings[highBytes(vals[lcv])].add(lowBytes(vals[lcv]));
-                roarings[highBytes(vals[lcv])].setCopyOnWrite(copyOnWrite);
-            }
+    void addMany(size_t n_args, const uint32_t* vals) {
+        roarings[0].addMany(n_args, vals);
+        roarings[0].setCopyOnWrite(copyOnWrite);
+    }
+
+    void addMany(size_t n_args, const uint64_t* vals) {
+        for (size_t lcv = 0; lcv < n_args; lcv++) {
+            roarings[highBytes(vals[lcv])].add(lowBytes(vals[lcv]));
+            roarings[highBytes(vals[lcv])].setCopyOnWrite(copyOnWrite);
         }
     }
 

test/sql/test_agg_function/R/test_bitmap_union_int
@@ -1,0 +1,23 @@
+-- name: test_bitmap_agg_int
+CREATE TABLE t1 (
+    c1 int,
+    c2 int
+    )
+DUPLICATE KEY(c1)
+DISTRIBUTED BY HASH(c1)
+BUCKETS 1
+PROPERTIES ("replication_num" = "1");
+-- result:
+-- !result
+INSERT INTO t1 values
+    (-1, -1),
+    (1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9),
+    (10, 10), (11, 11), (12, 12), (13, 13), (14, 14), (15, 15), (16, 16), (17, 17), (18, 18), (19, 19),
+    (20, 20), (21, 21), (22, 22), (23, 23), (24, 24), (25, 25), (26, 26), (27, 27), (28, 28), (29, 29),
+    (30, 30), (31, 31), (32, 32), (33, 33), (34, 34), (35, 35), (36, 36), (37, 37), (38, 38), (39, -1);
+-- result:
+-- !result
+select bitmap_union_int(c2) from t1;
+-- result:
+39
+-- !result

test/sql/test_agg_function/T/test_bitmap_union_int
@@ -1,0 +1,18 @@
+-- name: test_bitmap_agg_int
+CREATE TABLE t1 (
+    c1 int,
+    c2 int
+    )
+DUPLICATE KEY(c1)
+DISTRIBUTED BY HASH(c1)
+BUCKETS 1
+PROPERTIES ("replication_num" = "1");
+
+INSERT INTO t1 values
+    (-1, -1),
+    (1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9),
+    (10, 10), (11, 11), (12, 12), (13, 13), (14, 14), (15, 15), (16, 16), (17, 17), (18, 18), (19, 19),
+    (20, 20), (21, 21), (22, 22), (23, 23), (24, 24), (25, 25), (26, 26), (27, 27), (28, 28), (29, 29),
+    (30, 30), (31, 31), (32, 32), (33, 33), (34, 34), (35, 35), (36, 36), (37, 37), (38, 38), (39, -1);
+
+select bitmap_union_int(c2) from t1;