cb3829beb9f98064ad0c3faa3b1e118723151ea0
trueeyu <lxhhust350@qq.com>
2023-05-22 19:00:43 +0800

    [Enhancement] Optimization the performance of to_bitmap (#23824)
    ## Problem Summary:
    Fixes # (issue)
    
    Before optimization
    
    ```
    mysql> select count(to_bitmap(lo_orderkey)) from lineorder;
    +-------------------------------+
    | count(to_bitmap(lo_orderkey)) |
    +-------------------------------+
    |                     143999468 |
    +-------------------------------+
    1 row in set (2.38 sec)
    ```
    
    after optimization
    
    ```
    mysql> select count(to_bitmap(lo_orderkey)) from lineorder;
    +-------------------------------+
    | count(to_bitmap(lo_orderkey)) |
    +-------------------------------+
    |                     143999468 |
    +-------------------------------+
    1 row in set (2.02 sec)
    ```
    
    (cherry picked from commit bd2ce097f07bc2657c221db8ad6c6e8f1fc77993)

--- be/src/exprs/vectorized/bitmap_functions.cpp
+++ be/src/exprs/vectorized/bitmap_functions.cpp
@@ -61,8 +61,7 @@ ColumnPtr BitmapFunctions::to_bitmap(FunctionContext* context, const starrocks::
             }
         }
 
-        BitmapValue bitmap;
-        bitmap.add(value);
+        BitmapValue bitmap(value);
 
         builder.append(&bitmap);
     }
