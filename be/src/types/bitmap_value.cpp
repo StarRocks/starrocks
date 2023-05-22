59343421da912db257a97c76f5b1dcc6e28037ae
trueeyu <lxhhust350@qq.com>
2023-05-22 13:25:19 +0800

    format
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/types/bitmap_value.cpp
+++ be/src/types/bitmap_value.cpp
@@ -964,7 +964,7 @@ int64_t BitmapValue::bitmap_subset_limit_internal(const int64_t& range_start, co
         detail::Roaring64Map::const_iterator end = _bitmap->begin();
 
         int64_t offset = 0;
-        for (; end != _bitmap->end() && offset < abs_limit && * end <= range_start;) {
+        for (; end != _bitmap->end() && offset < abs_limit && *end <= range_start;) {
             ++end;
             ++offset;
         }
