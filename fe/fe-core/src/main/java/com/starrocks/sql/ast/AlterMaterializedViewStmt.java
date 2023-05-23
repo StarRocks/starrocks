3b59e545d4ebd85f03d7e17511dc7abd238d4319
Astralidea <astralidea@163.com>
2023-05-23 21:03:30 +0800

    Support alter materialized view to active
    Signed-off-by: Astralidea <astralidea@163.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/ast/AlterMaterializedViewStmt.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/ast/AlterMaterializedViewStmt.java
@@ -15,8 +15,11 @@
 
 package com.starrocks.sql.ast;
 
+import com.google.common.collect.Sets;
 import com.starrocks.analysis.TableName;
 import com.starrocks.sql.parser.NodePosition;
+
+import java.util.Set;
 
 /**
  * 1.Support for modifying the way of refresh and the cycle of asynchronous refresh;
@@ -28,24 +31,28 @@ public class AlterMaterializedViewStmt extends DdlStmt {
     private final TableName mvName;
     private final String newMvName;
     private final RefreshSchemeDesc refreshSchemeDesc;
-
     private final ModifyTablePropertiesClause modifyTablePropertiesClause;
+    private final String status;
+    public static final String ACTIVE = "active";
+    public static final String INACTIVE = "inactive";
+    public static final Set<String> SUPPORTED_MV_STATUS = Sets.newTreeSet(String.CASE_INSENSITIVE_ORDER);
 
-    public AlterMaterializedViewStmt(TableName mvName, String newMvName,
-                                     RefreshSchemeDesc refreshSchemeDesc,
-                                     ModifyTablePropertiesClause modifyTablePropertiesClause) {
-        this(mvName, newMvName, refreshSchemeDesc, modifyTablePropertiesClause, NodePosition.ZERO);
+    static {
+        SUPPORTED_MV_STATUS.add(ACTIVE);
+        SUPPORTED_MV_STATUS.add(INACTIVE);
     }
 
     public AlterMaterializedViewStmt(TableName mvName, String newMvName,
                                      RefreshSchemeDesc refreshSchemeDesc,
                                      ModifyTablePropertiesClause modifyTablePropertiesClause,
+                                     String status,
                                      NodePosition pos) {
         super(pos);
         this.mvName = mvName;
         this.newMvName = newMvName;
         this.refreshSchemeDesc = refreshSchemeDesc;
         this.modifyTablePropertiesClause = modifyTablePropertiesClause;
+        this.status = status;
     }
 
     public TableName getMvName() {
@@ -62,6 +69,10 @@ public class AlterMaterializedViewStmt extends DdlStmt {
 
     public ModifyTablePropertiesClause getModifyTablePropertiesClause() {
         return modifyTablePropertiesClause;
+    }
+
+    public String getStatus() {
+        return status;
     }
 
     @Override
