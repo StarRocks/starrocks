c7dd94034d2fbe63ed8973bafa18319accae63ab
Smith Cruise <chendingchao1@126.com>
2023-05-23 19:25:50 +0800

    improve
    Signed-off-by: Smith Cruise <chendingchao1@126.com>

--- be/src/formats/parquet/encoding.h
+++ be/src/formats/parquet/encoding.h
@@ -82,7 +82,7 @@ public:
     }
 
 protected:
-    Status check_dict_index_in_bounds(size_t index, size_t dict_size) {
+    inline Status check_dict_index_in_bounds(size_t index, size_t dict_size) {
         if (LIKELY(0 <= index && index < dict_size)) {
             return Status::OK();
         }
