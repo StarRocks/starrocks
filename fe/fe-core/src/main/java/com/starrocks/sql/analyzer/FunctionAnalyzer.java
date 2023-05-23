04427de9cf770faeba72d16c3cd69fa6fc321f04
trueeyu <lxhhust350@qq.com>
2023-05-23 11:21:12 +0800

    bitmap_agg support varchar type
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/FunctionAnalyzer.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/FunctionAnalyzer.java
@@ -321,10 +321,11 @@ public class FunctionAnalyzer {
                 throw new SemanticException(fnName + " function could only have one child", functionCallExpr.getPos());
             }
             Type inputType = functionCallExpr.getChild(0).getType();
-            if (!inputType.isIntegerType() && !inputType.isBoolean() && !inputType.isLargeIntType()) {
+            if (!inputType.isIntegerType() && !inputType.isBoolean() && !inputType.isLargeIntType()
+                    && !inputType.isStringType()) {
                 throw new SemanticException(
-                        fnName + " function's argument should be of int type or bool type, but was " + inputType,
-                        functionCallExpr.getChild(0).getPos());
+                        fnName + " function's argument should be of int type or bool type or string type, but was "
+                                + inputType, functionCallExpr.getChild(0).getPos());
             }
             return;
         }
