1a88c32e255347d42b167cf51df81fbb922cbd7d
Astralidea <astralidea@163.com>
2023-05-23 10:38:03 +0800

    Fix bug NPE when encounter unknown properties
    Signed-off-by: Astralidea <astralidea@163.com>

--- fe/fe-core/src/test/java/com/starrocks/catalog/CreateTableTest.java
+++ fe/fe-core/src/test/java/com/starrocks/catalog/CreateTableTest.java
@@ -339,6 +339,16 @@ public class CreateTableTest {
                                 + "Primary KEY (col1) distributed by hash(col1) buckets 1 \n"
                                 + "properties('replication_num' = '1', 'replicated_storage' = 'FALSE');"));
 
+        ExceptionChecker.expectThrowsWithMsg(DdlException.class,
+                "Unknown properties: {asd=true, enable_storage_cache=true, storage_cache_ttl=86400}",
+                () -> createTable("CREATE TABLE test.demo (k0 tinyint NOT NULL, k1 date NOT NULL, k2 int NOT NULL," +
+                        " k3 datetime not NULL, k4 bigint not NULL, k5 largeint not NULL) \n" +
+                        "ENGINE = OLAP \n" +
+                        "PRIMARY KEY( k0, k1, k2) \n" +
+                        "PARTITION BY RANGE (k1) (START (\"1970-01-01\") END (\"2022-09-30\") " +
+                        "EVERY (INTERVAL 60 day)) DISTRIBUTED BY HASH(k0) BUCKETS 1 " +
+                        "PROPERTIES (\"replication_num\"=\"1\",\"enable_persistent_index\" = \"false\"," +
+                        "\"enable_storage_cache\" = \"true\",\"storage_cache_ttl\" = \"86400\",\"asd\" = \"true\");"));
     }
 
     @Test
