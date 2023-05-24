fe87b90aa49b3a75f0ea6604c5ce9cd7c141a054
Zhuhe Fang <fzhedu@gmail.com>
2023-05-24 15:24:05 +0800

    [Enhancement] disable count distinct on array/map/json/struct types (#23589)
    ## Problem Summary:
    Fixes # (issue)
    disable count distinct on array/map/json/struct types
    depend on https://github.com/StarRocks/starrocks/pull/23786

--- fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzeAggregateTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzeAggregateTest.java
@@ -154,11 +154,47 @@ public class AnalyzeAggregateTest {
         analyzeFail("select distinct v1 from t0 order by sum(v2)");
 
         analyzeFail("select count(distinct v1), count(distinct v3) from tarray",
-                "No matching function with signature: multi_distinct_count(ARRAY)");
+                "No matching function with signature: multi_distinct_count(array");
 
         analyzeFail("select abs(distinct v1) from t0");
         analyzeFail("SELECT VAR_SAMP ( DISTINCT v2 ) FROM v0");
         analyzeFail("select distinct v1 from t0 having sum(v1) > 2");
+    }
+    @Test
+    public void testDistinctAggOnComplexTypes() {
+        analyzeSuccess("select count(distinct va) from ttypes group by v1");
+        analyzeSuccess("select count(*) from ttypes group by va");
+
+        // more than one count distinct
+        analyzeFail("select count(distinct va), count(distinct va1) from ttypes group by v1");
+        analyzeFail("select count(distinct va), count(distinct va1) from ttypes");
+        analyzeFail("select count(distinct vm), count(distinct vm1) from ttypes group by v1");
+        analyzeFail("select count(distinct vm), count(distinct vm1) from ttypes");
+        analyzeFail("select count(distinct vs), count(distinct vs1) from ttypes group by v1");
+        analyzeFail("select count(distinct vs), count(distinct vs1) from ttypes");
+        analyzeFail("select count(distinct vj), count(distinct vj1) from ttypes group by v1");
+        analyzeFail("select count(distinct vj), count(distinct vj1) from ttypes");
+
+        // single count distinct
+        analyzeFail("select count(distinct vm) from ttypes",
+                "No matching function with signature: multi_distinct_count(map");
+        analyzeFail("select count(distinct vs) from ttypes",
+                "No matching function with signature: multi_distinct_count(struct");
+        analyzeFail("select count(distinct vj) from ttypes",
+                "No matching function with signature: multi_distinct_count(json");
+        analyzeFail("select count(distinct vm) from ttypes group by v1",
+                "No matching function with signature: multi_distinct_count(map");
+        analyzeFail("select count(distinct vs) from ttypes group by v1",
+                "No matching function with signature: multi_distinct_count(struct");
+        analyzeFail("select count(distinct vj) from ttypes group by v1",
+                "No matching function with signature: multi_distinct_count(json");
+        analyzeFail("select count(distinct va),count(distinct vm) from ttypes group by v1",
+                "No matching function with signature: multi_distinct_count(array");
+
+        // group by complex types
+        analyzeFail("select count(*) from ttypes group by vm");
+        analyzeFail("select count(*) from ttypes group by vs");
+        analyzeFail("select count(*) from ttypes group by vj");
     }
 
     @Test
