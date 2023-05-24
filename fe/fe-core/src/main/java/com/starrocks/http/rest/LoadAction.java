3879a9282b0b9b54e9976cc3fd6540165d58c479
abc982627271 <liuxuefen@starrocks.com>
2023-05-24 14:45:56 +0800

    shuffle nodeIds
    Signed-off-by: abc982627271 <liuxuefen@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/http/rest/LoadAction.java
+++ fe/fe-core/src/main/java/com/starrocks/http/rest/LoadAction.java
@@ -55,6 +55,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LoadAction extends RestBaseAction {
@@ -106,6 +107,7 @@ public class LoadAction extends RestBaseAction {
                     nodeIds.add(nodeId);
                 }
             }
+            Collections.shuffle(nodeIds);
         } else {
             nodeIds = GlobalStateMgr.getCurrentSystemInfo().seqChooseBackendIds(1, true, false);
             if (CollectionUtils.isEmpty(nodeIds)) {
