d70f35ada6524c94c6d67429720f71d687e9553a
Felix Li <astralidea@163.com>
2023-05-24 22:54:07 +0800

    [Feature] Support drop task (#24073)
    Fixes #24015
    
    Signed-off-by: Astralidea <astralidea@163.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/ast/AstVisitor.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/ast/AstVisitor.java
@@ -235,7 +235,13 @@ public abstract class AstVisitor<R, C> {
         return visitStatement(statement, context);
     }
 
+    // ---------------------------------------- Task Statement ---------------------------------------------------------
+
     public R visitSubmitTaskStatement(SubmitTaskStmt statement, C context) {
+        return visitStatement(statement, context);
+    }
+
+    public R visitDropTaskStmt(DropTaskStmt statement, C context) {
         return visitStatement(statement, context);
     }
 
