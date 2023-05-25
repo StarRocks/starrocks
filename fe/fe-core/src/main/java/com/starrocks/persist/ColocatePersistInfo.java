220579e85b02a96c710983d85cdc214145270c20
sunny.xl <wxl24life@gmail.com>
2023-05-25 09:38:08 +0800

    remove previous invalid import statements that may block code compiling
    Signed-off-by: drake_wang <wxl250059@alibaba-inc.com>

--- fe/fe-core/src/main/java/com/starrocks/persist/ColocatePersistInfo.java
+++ fe/fe-core/src/main/java/com/starrocks/persist/ColocatePersistInfo.java
@@ -34,10 +34,10 @@
 
 package com.starrocks.persist;
 
+import com.google.common.base.Objects;
 import com.google.common.collect.Lists;
 import com.starrocks.catalog.ColocateTableIndex.GroupId;
 import com.starrocks.common.io.Writable;
-import org.spark_project.guava.base.Objects;
 
 import java.io.DataInput;
 import java.io.DataOutput;
