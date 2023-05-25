c7397a2dcba9f5c260fe99f156b5837486dbe410
gengjun-git <gengjun@starrocks.com>
2023-05-25 15:08:41 +0800

    fix
    Signed-off-by: gengjun-git <gengjun@starrocks.com>

--- build.sh
+++ build.sh
@@ -46,7 +46,7 @@ if [[ $OSTYPE == darwin* ]] ; then
     PARALLEL=$(sysctl -n hw.ncpu)
     # We know for sure that build-thirdparty.sh will fail on darwin platform, so just skip the step.
 else
-    if [[ ! -f ${STARROCKS_THIRDPARTY}/installed/async-profiler/profiler.sh ]]; then
+    if [[ ! -f ${STARROCKS_THIRDPARTY}/installed/include/datasketches/hll.hpp ]]; then
         echo "Thirdparty libraries need to be build ..."
         ${STARROCKS_THIRDPARTY}/build-thirdparty.sh
     fi
