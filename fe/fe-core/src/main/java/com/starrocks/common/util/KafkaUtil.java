d874fdad1982de3879060ae251b6fbb1a643b12b
Zaorang Yang <zaorangy@gmail.com>
2023-05-26 15:03:18 +0800

    [BugFix] Fix retry operations when getting kafka info timed out
    Signed-off-by: Zaorang Yang <zaorangy@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/common/util/KafkaUtil.java
+++ fe/fe-core/src/main/java/com/starrocks/common/util/KafkaUtil.java
@@ -39,6 +39,7 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.starrocks.common.Config;
 import com.starrocks.common.LoadException;
+import com.starrocks.common.TimeoutException;
 import com.starrocks.common.UserException;
 import com.starrocks.proto.PKafkaLoadInfo;
 import com.starrocks.proto.PKafkaMetaProxyRequest;
@@ -210,16 +211,23 @@ public class KafkaUtil {
                 while (true) {
                     request.timeout = Config.routine_load_kafka_timeout_second;
                     Future<PProxyResult> future = BackendServiceClient.getInstance().getInfo(address, request);
-                    PProxyResult result = future.get(Config.routine_load_kafka_timeout_second, TimeUnit.SECONDS);
-                    TStatusCode code = TStatusCode.findByValue(result.status.statusCode);
-                    if (code != TStatusCode.OK) {
-                        LOG.warn("failed to send proxy request to " + address + " err " + result.status.errorMsgs);
+                    PProxyResult result;
+                    try {
+                        result = future.get(Config.routine_load_kafka_timeout_second, TimeUnit.SECONDS);
+                    } catch (TimeoutException e) {
+                        LOG.warn("failed to send proxy request to " + address + " err " + e.getMessage());
                         // When getting kafka info timed out, we tried again three times.
                         if (++retryTimes > 3 || (retryTimes + 1) * Config.routine_load_kafka_timeout_second >
                                                                         Config.routine_load_task_timeout_second) {
-                            throw new UserException(
-                                    "failed to send proxy request to " + address + " err " + result.status.errorMsgs);
+                            throw e;
                         }
+                        continue;
+                    }
+                    TStatusCode code = TStatusCode.findByValue(result.status.statusCode);
+                    if (code != TStatusCode.OK) {
+                        LOG.warn("failed to send proxy request to " + address + " err " + result.status.errorMsgs);
+                        throw new UserException(
+                                "failed to send proxy request to " + address + " err " + result.status.errorMsgs);
                     } else {
                         return result;
                     }
