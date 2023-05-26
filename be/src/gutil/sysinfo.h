297126d69f2068ade5c85617c06c8b73fd7ca9e5
trueeyu <lxhhust350@qq.com>
2023-05-26 11:05:22 +0800

    [Refactor] Use CpuInfo::num_cores() instead of gutil::NumCPUs() (#24159)
    Use CpuInfo::num_cores() instead of gutil::NumCPUs()
    
    (cherry picked from commit d2ae2f66b52e0e1593c538326711562bc8b89bae)

--- be/src/gutil/sysinfo.h
+++ be/src/gutil/sysinfo.h
@@ -33,19 +33,6 @@
 #include <cstdint>
 
 namespace base {
-
-// Return the number of online CPUs. This is computed and cached the first time this or
-// NumCPUs() is called, so does not reflect any CPUs enabled or disabled at a later
-// point in time.
-//
-// Note that, if not all CPUs are online, this may return a value lower than the maximum
-// value of sched_getcpu().
-extern int NumCPUs();
-
-// Return the maximum CPU index that may be returned by sched_getcpu(). For example, on
-// an 8-core machine, this will return '7' even if some of the CPUs have been disabled.
-extern int MaxCPUIndex();
-
 void SleepForNanoseconds(int64_t nanoseconds);
 void SleepForMilliseconds(int64_t milliseconds);
 
