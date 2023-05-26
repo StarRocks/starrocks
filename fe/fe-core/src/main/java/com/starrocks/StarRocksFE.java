c565494dbff66463c5c445613a6fb50e9c3df11a
gengjun-git <gengjun@starrocks.com>
2023-05-26 11:31:29 +0800

    [Enhancement] Print Start log (#24079)
    Signed-off-by: gengjun-git <gengjun@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/StarRocksFE.java
+++ fe/fe-core/src/main/java/com/starrocks/StarRocksFE.java
@@ -180,6 +180,8 @@ public class StarRocksFE {
 
             addShutdownHook();
 
+            LOG.info("FE started successfully");
+
             while (!stopped) {
                 Thread.sleep(2000);
             }
