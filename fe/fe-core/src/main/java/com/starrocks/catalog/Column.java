93bd2a87a080021fb98998ac5c2d449ed742a8d7
Felix Li <astralidea@163.com>
2023-05-29 12:20:38 +0800

    [BugFix] Fix bug primary key table does not display current_timestamp (#24257)
    Fixes #24237
    
    Signed-off-by: Astralidea <astralidea@163.com>
    (cherry picked from commit 6fc5ca3ab3138714074fbc845bd17bd045f01da7)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/catalog/Column.java
    #	fe/fe-core/src/test/java/com/starrocks/analysis/ShowCreateTableStmtTest.java

--- fe/fe-core/src/main/java/com/starrocks/catalog/Column.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/Column.java
@@ -529,9 +529,25 @@ public class Column implements Writable {
         } else {
             sb.append("NOT NULL ");
         }
+<<<<<<< HEAD
+=======
+        if (defaultExpr == null && isAutoIncrement) {
+            sb.append("AUTO_INCREMENT ");
+        } else if (defaultExpr != null) {
+            if ("now()".equalsIgnoreCase(defaultExpr.getExpr())) {
+                // compatible with mysql
+                sb.append("DEFAULT ").append("CURRENT_TIMESTAMP").append(" ");
+            } else {
+                sb.append("DEFAULT ").append("(").append(defaultExpr.getExpr()).append(") ");
+            }
+        }
+>>>>>>> 6fc5ca3ab ([BugFix] Fix bug primary key table does not display current_timestamp (#24257))
         if (defaultValue != null && getPrimitiveType() != PrimitiveType.HLL &&
                 getPrimitiveType() != PrimitiveType.BITMAP) {
             sb.append("DEFAULT \"").append(defaultValue).append("\" ");
+        }
+        if (isMaterializedColumn()) {
+            sb.append("AS " + materializedColumnExpr.toSql() + " ");
         }
         sb.append("COMMENT \"").append(comment).append("\"");
 
