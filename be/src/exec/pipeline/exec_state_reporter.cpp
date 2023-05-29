244b7c3bf7533efb170ed65b9bad6d3812d9a6c1
liuyehcf <1559500551@qq.com>
2023-05-29 21:15:52 +0800

    [Refactor] Remove unnecessary warning logs (#24354)
    Fixes #issue
    
    ## What type of PR is this:
    - [ ] BugFix
    - [ ] Feature
    - [ ] Enhancement
    - [x] Refactor
    - [ ] UT
    - [ ] Doc
    - [ ] Tool
    
    ## Checklist:
    - [ ] I have added test cases for my bug fix or my new feature
    - [ ] This pr will affect users' behaviors
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    
    ## Bugfix cherry-pick branch check:
    - [x] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [x] 3.0
      - [x] 2.5
      - [x] 2.4
      - [x] 2.3
    
    Signed-off-by: liuyehcf <1559500551@qq.com>
    (cherry picked from commit ece0e51ecfd472b578a215c850e2f0cf6db0768f)

--- be/src/exec/pipeline/exec_state_reporter.cpp
+++ be/src/exec/pipeline/exec_state_reporter.cpp
@@ -134,18 +134,25 @@ Status ExecStateReporter::report_exec_status(const TReportExecStatusParams& para
     TReportExecStatusResult res;
     Status rpc_status;
 
-    VLOG_ROW << "debug: reportExecStatus params is " << apache::thrift::ThriftDebugString(params).c_str();
     try {
         try {
             coord->reportExecStatus(res, params);
         } catch (TTransportException& e) {
-            LOG(WARNING) << "Retrying ReportExecStatus: " << e.what();
-            rpc_status = coord.reopen();
+            TTransportException::TTransportExceptionType type = e.getType();
+            if (type != TTransportException::TTransportExceptionType::TIMED_OUT) {
+                // if not TIMED_OUT, retry
+                rpc_status = coord.reopen();
 
-            if (!rpc_status.ok()) {
-                return rpc_status;
+                if (!rpc_status.ok()) {
+                    return rpc_status;
+                }
+                coord->reportExecStatus(res, params);
+            } else {
+                std::stringstream msg;
+                msg << "ReportExecStatus() to " << fe_addr << " failed:\n" << e.what();
+                LOG(WARNING) << msg.str();
+                rpc_status = Status::InternalError(msg.str());
             }
-            coord->reportExecStatus(res, params);
         }
 
         rpc_status = Status(res.status);
@@ -237,18 +244,25 @@ Status ExecStateReporter::report_epoch(const TMVMaintenanceTasks& params, ExecEn
     TMVReportEpochResponse res;
     Status rpc_status;
 
-    VLOG_ROW << "debug: report_epoch params is " << apache::thrift::ThriftDebugString(params).c_str();
     try {
         try {
             coord->mvReport(res, params);
         } catch (TTransportException& e) {
-            LOG(WARNING) << "Retrying mvReport: " << e.what();
-            rpc_status = coord.reopen();
+            TTransportException::TTransportExceptionType type = e.getType();
+            if (type != TTransportException::TTransportExceptionType::TIMED_OUT) {
+                // if not TIMED_OUT, retry
+                rpc_status = coord.reopen();
 
-            if (!rpc_status.ok()) {
-                return rpc_status;
+                if (!rpc_status.ok()) {
+                    return rpc_status;
+                }
+                coord->mvReport(res, params);
+            } else {
+                std::stringstream msg;
+                msg << "mvReport() to " << fe_addr << " failed:\n" << e.what();
+                LOG(WARNING) << msg.str();
+                rpc_status = Status::InternalError(msg.str());
             }
-            coord->mvReport(res, params);
         }
 
         rpc_status = Status::OK();
