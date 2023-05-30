4aee28db0ed50dfa2dafd1cf6e7badce6cf79887
Youngwb <yangwenbo_mailbox@163.com>
2023-05-30 16:20:10 +0800

    [BugFix] Fix use mv stats as memo group stats (#24370)
    Fixes #24369
    
    Signed-off-by: Youngwb <yangwenbo_mailbox@163.com>
    (cherry picked from commit 64aa73f12ed1a4fcc181b992054be40a51fb78d2)

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/task/DeriveStatsTask.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/task/DeriveStatsTask.java
@@ -45,20 +45,25 @@ public class DeriveStatsTask extends OptimizerTask {
 
         Statistics currentStatistics = groupExpression.getGroup().getStatistics();
         // choose best statistics
+        // do set group statistics when the groupExpression is a materialized view scan
         if (currentStatistics == null ||
-                (expressionContext.getStatistics().getOutputRowCount() < currentStatistics.getOutputRowCount())) {
+                (expressionContext.getStatistics().getOutputRowCount() < currentStatistics.getOutputRowCount() &&
+                        !isMaterializedView())) {
             groupExpression.getGroup().setStatistics(expressionContext.getStatistics());
         }
         if (currentStatistics != null && !currentStatistics.equals(expressionContext.getStatistics())) {
-            if (groupExpression.getOp() instanceof LogicalOlapScanOperator) {
+            if (isMaterializedView()) {
                 LogicalOlapScanOperator scan = groupExpression.getOp().cast();
-                if (scan.getTable().isMaterializedView()) {
-                    MaterializedView mv = (MaterializedView) scan.getTable();
-                    groupExpression.getGroup().setMvStatistics(mv.getId(), expressionContext.getStatistics());
-                }
+                MaterializedView mv = (MaterializedView) scan.getTable();
+                groupExpression.getGroup().setMvStatistics(mv.getId(), expressionContext.getStatistics());
             }
         }
 
         groupExpression.setStatsDerived();
+    }
+
+    private boolean isMaterializedView() {
+        return groupExpression.getOp() instanceof LogicalOlapScanOperator
+                && ((LogicalOlapScanOperator) groupExpression.getOp()).getTable().isMaterializedView();
     }
 }
