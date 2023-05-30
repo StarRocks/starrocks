3dc3edf7ac7652e349ca866cc785769edde7d426
Letian Jiang <letian.jiang@outlook.com>
2023-05-30 11:39:29 +0800

    [BugFix] Fix driver pending due to unclosed sink buffer (#24057)
    In cases where creating a hdfs writer fails, we should close the sink
    buffer to avoid drivers forever pending.
    
    ---------
    
    Signed-off-by: Letian Jiang <letian.jiang@outlook.com>

--- be/src/exec/pipeline/sink/file_sink_operator.cpp
+++ be/src/exec/pipeline/sink/file_sink_operator.cpp
@@ -134,10 +134,12 @@ void FileSinkIOBuffer::_process_chunk(bthread::TaskIterator<ChunkPtr>& iter) {
             status = status.clone_and_prepend("open file writer failed, error");
             LOG(WARNING) << status;
             _fragment_ctx->cancel(status);
+            close(_state);
             return;
         }
         _is_writer_opened = true;
     }
+
     const auto& chunk = *iter;
     if (chunk == nullptr) {
         // this is the last chunk
@@ -145,9 +147,12 @@ void FileSinkIOBuffer::_process_chunk(bthread::TaskIterator<ChunkPtr>& iter) {
         close(_state);
         return;
     }
+
     if (Status status = _writer->append_chunk(chunk.get()); !status.ok()) {
-        LOG(WARNING) << "add chunk to file writer failed, error: " << status.to_string();
+        status = status.clone_and_prepend("add chunk to file writer failed, error");
+        LOG(WARNING) << status;
         _fragment_ctx->cancel(status);
+        close(_state);
         return;
     }
 }
