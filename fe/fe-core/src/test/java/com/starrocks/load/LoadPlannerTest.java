0c2a2cb8c3ec0bf034ffb1268946203bd5eabdf6
meegoo <meegoo.sr@gmail.com>
2023-05-31 10:36:39 +0800

    [Enhancement] Default enable replicated storage when ingestion need shuffle (#24232)
    Fixes #issue
    
    - [ ] BugFix
    - [ ] Feature
    - [x] Enhancement
    - [ ] Refactor
    - [ ] UT
    - [ ] Doc
    - [ ] Tool
    
    - [ ] I have added test cases for my bug fix or my new feature
    - [ ] This pr will affect users' behaviors
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    
    - [x] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [x] 3.0
      - [x] 2.5
      - [ ] 2.4
      - [ ] 2.3
    
    Signed-off-by: meegoo <meegoo.sr@gmail.com>
    (cherry picked from commit 93c378150c23d7d021cf0a9b6810e4499073295d)

--- fe/fe-core/src/test/java/com/starrocks/load/LoadPlannerTest.java
+++ fe/fe-core/src/test/java/com/starrocks/load/LoadPlannerTest.java
@@ -112,6 +112,7 @@ public class LoadPlannerTest {
 
         loadParallelInstanceNum = Config.load_parallel_instance_num;
         maxBrokerConcurrency = Config.max_broker_concurrency;
+        Config.eliminate_shuffle_load_by_replicated_storage = false;
 
         // backends
         Map<Long, Backend> idToBackendTmp = Maps.newHashMap();
@@ -129,6 +130,7 @@ public class LoadPlannerTest {
     public void tearDown() {
         Config.load_parallel_instance_num = loadParallelInstanceNum;
         Config.max_broker_concurrency = maxBrokerConcurrency;
+        Config.eliminate_shuffle_load_by_replicated_storage = true;
     }
 
     @Test
@@ -931,15 +933,29 @@ public class LoadPlannerTest {
         fileStatusList.add(new TBrokerFileStatus("path/k2=1/file1", false, 268435456, true));
         fileStatusesList.add(fileStatusList);
 
-        // plan
-        LoadPlanner planner = new LoadPlanner(jobId, loadId, txnId, db.getId(), table, strictMode,
-                timezone, timeoutS, startTime, partialUpdate, ctx, sessionVariables, loadMemLimit, execMemLimit,
-                brokerDesc, fileGroups, fileStatusesList, 1);
-        planner.plan();
+        {
+            // plan
+            LoadPlanner planner = new LoadPlanner(jobId, loadId, txnId, db.getId(), table, strictMode,
+                    timezone, timeoutS, startTime, partialUpdate, ctx, sessionVariables, loadMemLimit, execMemLimit,
+                    brokerDesc, fileGroups, fileStatusesList, 1);
+            planner.plan();
 
-        // check fragment
-        List<PlanFragment> fragments = planner.getFragments();
-        Assert.assertEquals(2, fragments.size());
+            // check fragment
+            List<PlanFragment> fragments = planner.getFragments();
+            Assert.assertEquals(2, fragments.size());
+        }
+        {
+            Config.eliminate_shuffle_load_by_replicated_storage = true;
+            // plan
+            LoadPlanner planner = new LoadPlanner(jobId, loadId, txnId, db.getId(), table, strictMode,
+                    timezone, timeoutS, startTime, partialUpdate, ctx, sessionVariables, loadMemLimit, execMemLimit,
+                    brokerDesc, fileGroups, fileStatusesList, 1);
+            planner.plan();
+
+            // check fragment
+            List<PlanFragment> fragments = planner.getFragments();
+            Assert.assertEquals(1, fragments.size());
+        }
     }
 
     @Test
@@ -1033,14 +1049,28 @@ public class LoadPlannerTest {
         fileStatusList.add(new TBrokerFileStatus("path/k2=1/file1", false, 268435456, true));
         fileStatusesList.add(fileStatusList);
 
-        // plan
-        LoadPlanner planner = new LoadPlanner(jobId, loadId, txnId, db.getId(), table, strictMode,
-                timezone, timeoutS, startTime, partialUpdate, ctx, sessionVariables, loadMemLimit, execMemLimit,
-                brokerDesc, fileGroups, fileStatusesList, 1);
-        planner.plan();
+        {
+            // plan
+            LoadPlanner planner = new LoadPlanner(jobId, loadId, txnId, db.getId(), table, strictMode,
+                    timezone, timeoutS, startTime, partialUpdate, ctx, sessionVariables, loadMemLimit, execMemLimit,
+                    brokerDesc, fileGroups, fileStatusesList, 1);
+            planner.plan();
 
-        // check fragment
-        List<PlanFragment> fragments = planner.getFragments();
-        Assert.assertEquals(2, fragments.size());
+            // check fragment
+            List<PlanFragment> fragments = planner.getFragments();
+            Assert.assertEquals(2, fragments.size());
+        }
+        {
+            Config.eliminate_shuffle_load_by_replicated_storage = true;
+            // plan
+            LoadPlanner planner = new LoadPlanner(jobId, loadId, txnId, db.getId(), table, strictMode,
+                    timezone, timeoutS, startTime, partialUpdate, ctx, sessionVariables, loadMemLimit, execMemLimit,
+                    brokerDesc, fileGroups, fileStatusesList, 1);
+            planner.plan();
+
+            // check fragment
+            List<PlanFragment> fragments = planner.getFragments();
+            Assert.assertEquals(1, fragments.size());
+        }
     }
 }
