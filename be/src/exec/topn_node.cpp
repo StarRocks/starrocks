b60ea548aee910891b81fd3d88b3e8c04e3b1b07
eyes_on_me <3675229+silverbullet233@users.noreply.github.com>
2023-06-01 15:11:25 +0800

    [BugFix] skip to interpolate spill process if sort spill is disabled (#24183)
    introduce by #23746
    ----
    Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>

--- be/src/exec/topn_node.cpp
+++ be/src/exec/topn_node.cpp
@@ -295,7 +295,7 @@ std::vector<std::shared_ptr<pipeline::OperatorFactory>> TopNNode::_decompose_to_
             std::make_shared<SpillProcessChannelFactory>(degree_of_parallelism, std::move(executor));
 
     // spill process operator
-    if (runtime_state()->enable_spill() && _limit < 0 && !is_partition_topn) {
+    if (runtime_state()->enable_spill() && runtime_state()->enable_sort_spill() && _limit < 0 && !is_partition_topn) {
         context->interpolate_spill_process(id(), spill_channel_factory, degree_of_parallelism);
     }
 
