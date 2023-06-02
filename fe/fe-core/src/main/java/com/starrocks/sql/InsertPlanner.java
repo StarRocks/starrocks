50d170ff46f310172aae38f5ccb736d66ce05891
meegoo <meegoo.sr@gmail.com>
2023-06-02 11:25:44 +0800

    [BugFix] Fix invalid open replicated storage when has limit (#24520)
    Fixes #issue
    
    ## What type of PR is this:
    - [ ] BugFix
    - [ ] Feature
    - [ ] Enhancement
    - [ ] Refactor
    - [ ] UT
    - [ ] Doc
    - [ ] Tool
    
    ## Checklist:
    - [ ] I have added test cases for my bug fix or my new feature
    - [ ] This pr will affect users' behaviors
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    
    ## Bugfix cherry-pick branch check:
    - [x] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [x] 3.0
      - [x] 2.5
      - [ ] 2.4
      - [ ] 2.3
    
    Signed-off-by: meegoo <meegoo.sr@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/InsertPlanner.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/InsertPlanner.java
@@ -108,6 +108,7 @@ public class InsertPlanner {
     // Only for unit test
     public static boolean enableSingleReplicationShuffle = false;
     private boolean shuffleServiceEnable = false;
+    private boolean forceReplicatedStorage = false;
 
     private static final Logger LOG = LogManager.getLogger(InsertPlanner.class);
 
@@ -163,14 +164,6 @@ public class InsertPlanner {
 
             Optimizer optimizer = new Optimizer();
             PhysicalPropertySet requiredPropertySet = createPhysicalPropertySet(insertStmt, outputColumns);
-
-            boolean forceReplicatedStorage = false;
-            if (!requiredPropertySet.equals(PhysicalPropertySet.EMPTY)) {
-                if (Config.eliminate_shuffle_load_by_replicated_storage) {
-                    requiredPropertySet = PhysicalPropertySet.EMPTY;
-                    forceReplicatedStorage = true;
-                }
-            }
 
             LOG.debug("property {}", requiredPropertySet);
             OptExpression optimizedPlan;
@@ -621,6 +614,11 @@ public class InsertPlanner {
                 new HashDistributionDesc(keyColumnIds, HashDistributionDesc.SourceType.SHUFFLE_AGG);
         DistributionSpec spec = DistributionSpec.createHashDistributionSpec(desc);
         DistributionProperty property = new DistributionProperty(spec);
+
+        if (Config.eliminate_shuffle_load_by_replicated_storage) {
+            forceReplicatedStorage = true;
+            return new PhysicalPropertySet();
+        }
 
         shuffleServiceEnable = true;
 
