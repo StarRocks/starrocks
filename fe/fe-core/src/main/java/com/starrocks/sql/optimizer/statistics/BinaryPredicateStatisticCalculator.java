8b7d635340c2a32143911dbbcd04db4056ad5d32
shuzz <shuzz@shuyilink.com>
2023-06-07 09:16:53 +0000

    [Enhancement] Reduce unnecessary computation
    Signed-off-by: shuzz <shuzz@shuyilink.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/statistics/BinaryPredicateStatisticCalculator.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/statistics/BinaryPredicateStatisticCalculator.java
@@ -86,8 +86,11 @@ public class BinaryPredicateStatisticCalculator {
             if (constant.isPresent()) {
                 Optional<Double> c = StatisticUtils.convertStatisticsToDouble(
                         constant.get().getType(), constant.get().toString());
-                predicateRange = c.map(aDouble -> new StatisticRangeValues(aDouble, aDouble, 1))
-                        .orElseGet(() -> new StatisticRangeValues(NEGATIVE_INFINITY, POSITIVE_INFINITY, 1));
+                if (c.isPresent()) {
+                    predicateRange = new StatisticRangeValues(c.get(), c.get(), 1);
+                } else {
+                    predicateRange = new StatisticRangeValues(NEGATIVE_INFINITY, POSITIVE_INFINITY, 1);
+                }
             } else {
                 predicateRange = new StatisticRangeValues(NEGATIVE_INFINITY, POSITIVE_INFINITY, 1);
             }
@@ -112,12 +115,11 @@ public class BinaryPredicateStatisticCalculator {
                     .build();
 
             double predicateFactor;
-            double rowCountInHistogram;
             Map<String, Long> histogramTopN = columnStatistic.getHistogram().getMCV();
             // If there is a constant key in mcv, the ratio in mcv is directly used for filtering estimation.
             // If it does not hit, filter out the key that appears in mcv, and then use the cardinality estimation
             if (histogramTopN.containsKey(constantOperator.toString())) {
-                rowCountInHistogram = histogramTopN.get(constantOperator.toString());
+                double rowCountInHistogram = histogramTopN.get(constantOperator.toString());
                 predicateFactor = rowCountInHistogram / columnStatistic.getHistogram().getTotalRows();
             } else {
                 Long mostCommonValuesCount = histogramTopN.values().stream().reduce(Long::sum).orElse(0L);
@@ -208,9 +210,7 @@ public class BinaryPredicateStatisticCalculator {
             Histogram estimatedHistogram = estimateLessThanWithHistogram(columnStatistic, constant.get(),
                     binaryType.equals(BinaryPredicateOperator.BinaryType.LE));
 
-            int bucketSize = estimatedHistogram.getBuckets().size();
-            long rowCountInHistogram = (bucketSize == 0 ? 0 : estimatedHistogram.getBuckets().get(bucketSize - 1).getCount())
-                    + estimatedHistogram.getMCV().values().stream().reduce(Long::sum).orElse(0L);
+            long rowCountInHistogram = estimatedHistogram.getTotalRows();
             double rowCount = statistics.getOutputRowCount()
                     * ((double) rowCountInHistogram / (double) columnStatistic.getHistogram().getTotalRows());
 
@@ -246,9 +246,7 @@ public class BinaryPredicateStatisticCalculator {
             Histogram estimatedHistogram = estimateGreaterThanWithHistogram(columnStatistic, constant.get(),
                     binaryType.equals(BinaryPredicateOperator.BinaryType.GE));
 
-            int bucketSize = estimatedHistogram.getBuckets().size();
-            long rowCountInHistogram = (bucketSize == 0 ? 0 : estimatedHistogram.getBuckets().get(bucketSize - 1).getCount())
-                    + estimatedHistogram.getMCV().values().stream().reduce(Long::sum).orElse(0L);
+            long rowCountInHistogram = estimatedHistogram.getTotalRows();
             double rowCount = statistics.getOutputRowCount()
                     * ((double) rowCountInHistogram / (double) columnStatistic.getHistogram().getTotalRows());
 
@@ -409,7 +407,6 @@ public class BinaryPredicateStatisticCalculator {
             if (bucket.getUpper() >= constantDouble && bucket.getLower() <= constantDouble) {
                 StatisticRangeValues bucketRange = new StatisticRangeValues(bucket.getLower(), bucket.getUpper(), NaN);
                 StatisticRangeValues columnRange = new StatisticRangeValues(bucket.getLower(), constantDouble, NaN);
-                double predicateFactor = bucketRange.overlapPercentWith(columnRange);
 
                 long bucketRowCount;
                 long repeat;
@@ -417,6 +414,7 @@ public class BinaryPredicateStatisticCalculator {
                     bucketRowCount = bucket.getCount() - previousTotalRowCount;
                     repeat = bucket.getUpperRepeats();
                 } else {
+                    double predicateFactor = bucketRange.overlapPercentWith(columnRange);
                     long bucketTotalRows = bucket.getCount() - bucket.getUpperRepeats() - previousTotalRowCount;
                     bucketRowCount = (long) (bucketTotalRows * predicateFactor);
                     repeat = 0;
@@ -467,7 +465,6 @@ public class BinaryPredicateStatisticCalculator {
             if (bucket.getUpper() >= constantDouble && bucket.getLower() <= constantDouble) {
                 StatisticRangeValues bucketRange = new StatisticRangeValues(bucket.getLower(), bucket.getUpper(), NaN);
                 StatisticRangeValues columnRange = new StatisticRangeValues(constantDouble, bucket.getUpper(), NaN);
-                double predicateFactor = bucketRange.overlapPercentWith(columnRange);
 
                 long bucketRowCount;
                 if (constantDouble == bucket.getUpper()) {
@@ -477,6 +474,7 @@ public class BinaryPredicateStatisticCalculator {
                         previousTotalRowCount = bucket.getCount();
                     }
                 } else {
+                    double predicateFactor = bucketRange.overlapPercentWith(columnRange);
                     long bucketTotalRows = bucket.getCount() - previousTotalRowCount;
                     bucketRowCount = (long) (bucketTotalRows * predicateFactor);
                     previousTotalRowCount = previousTotalRowCount + (bucketTotalRows - bucketRowCount);
