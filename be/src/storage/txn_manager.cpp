1ada70c36f5f1bae81a2fba916c10a939d05c89c
Yixin Luo <18810541851@163.com>
2023-06-08 14:14:15 +0800

    [Enhancement] add some logs for partial update by column (#24700)
    Add some logs for partial update by column

--- be/src/storage/txn_manager.cpp
+++ be/src/storage/txn_manager.cpp
@@ -297,7 +297,8 @@ Status TxnManager::commit_txn(KVStore* meta, TPartitionId partition_id, TTransac
         _insert_txn_partition_map_unlocked(transaction_id, partition_id);
         LOG(INFO) << "Commit txn successfully. "
                   << " tablet: " << tablet_id << ", txn_id: " << key.second << ", rowsetid: " << rowset_ptr->rowset_id()
-                  << " #segment:" << rowset_ptr->num_segments() << " #delfile:" << rowset_ptr->num_delete_files();
+                  << " #segment:" << rowset_ptr->num_segments() << " #delfile:" << rowset_ptr->num_delete_files()
+                  << " #uptfiles:" << rowset_ptr->num_update_files();
     }
     return Status::OK();
 }
