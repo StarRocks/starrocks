StarRocks/starrocks

commit 7d2a7bebafa89312f427b67d8d74c5b99c694178

[Enhancement] Enable physical split morsel to contain multiple rowsets and segments (#22892)
Signed-off-by: ZiheLiu <ziheliu1024@gmail.com>

be/src/exec/pipeline/scan/morsel.h
@@ -82,14 +82,16 @@ public:
     void set_from_version(int64_t from_version) { _from_version = from_version; }
     int64_t from_version() { return _from_version; }
 
-    void set_rowsets(std::vector<RowsetSharedPtr> rowsets) { _rowsets = std::move(rowsets); }
-    const std::vector<RowsetSharedPtr>& rowsets() const { return _rowsets; }
+    void set_rowsets(const std::vector<RowsetSharedPtr>& rowsets) { _rowsets = &rowsets; }
+    const std::vector<RowsetSharedPtr>& rowsets() const { return *_rowsets; }
 
 private:
     int32_t _plan_node_id;
     int64_t _from_version = 0;
 
-    std::vector<RowsetSharedPtr> _rowsets;
+    static const std::vector<RowsetSharedPtr> kEmptyRowsets;
+    // _rowsets is owned by MorselQueue, whose lifecycle is longer than that of Morsel.
+    const std::vector<RowsetSharedPtr>* _rowsets = &kEmptyRowsets;
 };
 
 class ScanMorsel : public Morsel {
@@ -105,6 +107,8 @@ public:
             _tablet_id = _scan_range->binlog_scan_range.tablet_id;
         }
     }
+
+    ~ScanMorsel() override = default;
 
     ScanMorsel(int32_t plan_node_id, const TScanRangeParams& scan_range)
             : ScanMorsel(plan_node_id, scan_range.scan_range) {}
@@ -128,6 +132,8 @@ public:
     PhysicalSplitScanMorsel(int32_t plan_node_id, const TScanRange& scan_range, RowidRangeOptionPtr rowid_range_option)
             : ScanMorsel(plan_node_id, scan_range), _rowid_range_option(std::move(rowid_range_option)) {}
 
+    ~PhysicalSplitScanMorsel() override = default;
+
     void init_tablet_reader_params(TabletReaderParams* params) override;
 
 private:
@@ -139,6 +145,8 @@ public:
     LogicalSplitScanMorsel(int32_t plan_node_id, const TScanRange& scan_range,
                            std::vector<ShortKeyRangeOptionPtr> short_key_ranges)
             : ScanMorsel(plan_node_id, scan_range), _short_key_ranges(std::move(short_key_ranges)) {}
+
+    ~LogicalSplitScanMorsel() override = default;
 
     void init_tablet_reader_params(TabletReaderParams* params) override;
 
@@ -316,6 +324,9 @@ private:
     // Load the meta of the new rowset and the index of the new segment,
     // and find the rowid range of each key range in this segment.
     Status _init_segment();
+    // Obtain row id ranges from multiple segments of multiple rowsets within a single tablet,
+    // until _splitted_scan_rows rows are retrieved.
+    StatusOr<RowidRangeOptionPtr> _try_get_split_from_single_tablet();
 
 private:
     std::mutex _mutex;

be/src/exec/pipeline/scan/morsel.cpp
@@ -27,6 +27,9 @@
 namespace starrocks::pipeline {
 
 /// Morsel.
+
+const std::vector<RowsetSharedPtr> Morsel::kEmptyRowsets;
+
 void PhysicalSplitScanMorsel::init_tablet_reader_params(TabletReaderParams* params) {
     params->rowid_range_option = _rowid_range_option;
 }
@@ -100,7 +103,7 @@ StatusOr<MorselPtr> FixedMorselQueue::try_get() {
     idx = _pop_index.fetch_add(1);
     if (idx < _num_morsels) {
         if (!_tablet_rowsets.empty()) {
-            _morsels[idx]->set_rowsets(std::move(_tablet_rowsets[idx]));
+            _morsels[idx]->set_rowsets(_tablet_rowsets[idx]);
         }
         return std::move(_morsels[idx]);
     } else {
@@ -128,6 +131,69 @@ void PhysicalSplitMorselQueue::set_key_ranges(const std::vector<std::unique_ptr<
     }
 }
 
+StatusOr<RowidRangeOptionPtr> PhysicalSplitMorselQueue::_try_get_split_from_single_tablet() {
+    size_t num_taken_rows = 0;
+    RowidRangeOptionPtr rowid_range = nullptr;
+    auto has_taken_from_tablet = [&rowid_range]() { return rowid_range != nullptr; };
+
+    while (num_taken_rows < _splitted_scan_rows) {
+        if (_tablet_idx >= _tablets.size()) {
+            return rowid_range;
+        }
+
+        // When it hasn't initialized any segment,
+        // or _segment_idx exceeds the segments of the current rowset,
+        // or current segment is empty or finished,
+        // we should pick up the next segment and init it.
+        while (!_has_init_any_segment || _cur_segment() == nullptr || _cur_segment()->num_rows() == 0 ||
+               !_segment_range_iter.has_more()) {
+            // Only pick up the segment in the same tablet.
+            if (has_taken_from_tablet() && _is_last_split_of_current_morsel()) {
+                return rowid_range;
+            }
+
+            if (!_next_segment()) {
+                return rowid_range;
+            }
+
+            if (auto status = _init_segment(); !status.ok()) {
+                // Morsel_queue cannot generate morsels after errors occurring.
+                _tablet_idx = _tablets.size();
+                return status;
+            }
+        }
+
+        if (rowid_range == nullptr) {
+            rowid_range = std::make_shared<RowidRangeOption>();
+        }
+
+        SparseRange taken_range;
+        _segment_range_iter.next_range(_splitted_scan_rows, &taken_range);
+        _num_segment_rest_rows -= taken_range.span_size();
+        if (_num_segment_rest_rows < _splitted_scan_rows) {
+            // If there are too few rows left in the segment, take them all this time.
+            _segment_range_iter.next_range(_splitted_scan_rows, &taken_range);
+            _num_segment_rest_rows = 0;
+        }
+
+        VLOG_ROW << "PhysicalSplitMorselQueue::_try_get_split_from_single_tablet "
+                 << "[rowid_range_addr=" << rowid_range.get() << "] "
+                 << "[tablet_idx=" << _tablet_idx << "] "
+                 << "[rowset_idx=" << _rowset_idx << "] "
+                 << "[segment_idx=" << _segment_idx << "] "
+                 << "[range=" << taken_range.to_string() << "] ";
+
+        num_taken_rows += taken_range.span_size();
+        rowid_range->add(_cur_rowset(), _cur_segment(), std::make_shared<SparseRange>(std::move(taken_range)));
+
+        if (_is_last_split_of_current_morsel()) {
+            return rowid_range;
+        }
+    }
+
+    return rowid_range;
+}
+
 StatusOr<MorselPtr> PhysicalSplitMorselQueue::try_get() {
     std::lock_guard<std::mutex> lock(_mutex);
     if (_unget_morsel != nullptr) {
@@ -137,41 +203,12 @@ StatusOr<MorselPtr> PhysicalSplitMorselQueue::try_get() {
     DCHECK(!_tablet_rowsets.empty());
     DCHECK_EQ(_tablets.size(), _tablet_rowsets.size());
 
-    if (_tablet_idx >= _tablets.size()) {
+    ASSIGN_OR_RETURN(auto rowid_range, _try_get_split_from_single_tablet());
+    if (rowid_range == nullptr) {
         return nullptr;
     }
 
-    // When it hasn't initialized any segment,
-    // or _segment_idx exceeds the segments of the current rowset,
-    // or current segment is empty or finished,
-    // we should pick up the next segment and init it.
-    while (!_has_init_any_segment || _cur_segment() == nullptr || _cur_segment()->num_rows() == 0 ||
-           !_segment_range_iter.has_more()) {
-        if (!_next_segment()) {
-            return nullptr;
-        }
-
-        if (auto status = _init_segment(); !status.ok()) {
-            // Morsel_queue cannot generate morsels after errors occurring.
-            _tablet_idx = _tablets.size();
-            return status;
-        }
-    }
-
-    SparseRange taken_range;
-    _segment_range_iter.next_range(_splitted_scan_rows, &taken_range);
-    _num_segment_rest_rows -= taken_range.span_size();
-    if (_num_segment_rest_rows < _splitted_scan_rows) {
-        // If there are too few rows left in the segment, take them all this time.
-        _segment_range_iter.next_range(_splitted_scan_rows, &taken_range);
-        _num_segment_rest_rows = 0;
-    }
-
     auto* scan_morsel = _cur_scan_morsel();
-    auto* rowset = _cur_rowset();
-    auto rowid_range = std::make_shared<RowidRangeOption>(rowset->rowset_id(), rowset->segments()[_segment_idx]->id(),
-                                                          std::move(taken_range));
-
     MorselPtr morsel = std::make_unique<PhysicalSplitScanMorsel>(
             scan_morsel->get_plan_node_id(), *(scan_morsel->get_scan_range()), std::move(rowid_range));
     morsel->set_rowsets(_tablet_rowsets[_tablet_idx]);
@@ -224,8 +261,11 @@ Segment* PhysicalSplitMorselQueue::_cur_segment() {
 }
 
 bool PhysicalSplitMorselQueue::_is_last_split_of_current_morsel() {
-    return _has_init_any_segment && _cur_segment() != nullptr && _cur_segment()->num_rows() != 0 &&
-           !_segment_range_iter.has_more();
+    if (_tablet_idx >= _tablet_rowsets.size()) {
+        return true;
+    }
+    return _num_segment_rest_rows == 0 && _rowset_idx + 1 == _tablet_rowsets[_tablet_idx].size() &&
+           _segment_idx + 1 == _cur_rowset()->segments().size();
 }
 
 bool PhysicalSplitMorselQueue::_next_segment() {

be/src/storage/binlog_reader.cpp
@@ -268,8 +268,7 @@ Status BinlogReader::_init_segment_iterator() {
     if (log_entry_info->start_seq_id < _next_seq_id) {
         // for duplicate key, LogEntryInfo#start_row_id is 0
         rowid_t start_row_id = _next_seq_id - log_entry_info->start_seq_id;
-        SparseRange range(start_row_id, seg_ptr->num_rows());
-        seg_options.rowid_range_option = std::make_shared<RowidRangeOption>(_rowset->rowset_id(), segment_index, range);
+        seg_options.rowid_range_option = std::make_shared<SparseRange>(start_row_id, seg_ptr->num_rows());
     }
     ASSIGN_OR_RETURN(_segment_iterator, seg_ptr->new_iterator(_data_schema, seg_options));
     VLOG(3) << "Create segment iterator for reader: " << _reader_id << ", tablet: " << _tablet->full_name()

be/src/storage/lake/rowset.cpp
@@ -97,9 +97,12 @@ StatusOr<ChunkIteratorPtr> Rowset::read(const Schema& schema, const RowsetReadOp
             continue;
         }
 
-        if (options.rowid_range_option != nullptr && !options.rowid_range_option->match_segment(seg_ptr.get())) {
-            continue;
-        }
+        //        if (options.rowid_range_option != nullptr) {
+        //            seg_options.rowid_range_option = options.rowid_range_option->get_segment_rowid_range(this, seg_ptr.get());
+        //            if (seg_options.rowid_range_option == nullptr) {
+        //                continue;
+        //            }
+        //        }
 
         auto res = seg_ptr->new_iterator(*segment_schema, seg_options);
         if (res.status().is_end_of_file()) {

be/src/storage/range.h
@@ -200,6 +200,7 @@ private:
 
     std::vector<Range> _ranges;
 };
+using SparseRangePtr = std::shared_ptr<SparseRange>;
 
 inline void SparseRange::_add_uncheck(const Range& r) {
     if (!r.empty()) {

be/src/storage/rowset/rowid_range_option.cpp
@@ -21,15 +21,32 @@
 
 namespace starrocks {
 
-RowidRangeOption::RowidRangeOption(const RowsetId& rowset_id, uint64_t segment_id, SparseRange rowid_range)
-        : rowset_id(rowset_id), segment_id(segment_id), rowid_range(std::move(rowid_range)) {}
+void RowidRangeOption::add(const Rowset* rowset, const Segment* segment, SparseRangePtr rowid_range) {
+    auto rowset_it = rowid_range_per_segment_per_rowset.find(rowset->rowset_id());
+    if (rowset_it == rowid_range_per_segment_per_rowset.end()) {
+        rowset_it = rowid_range_per_segment_per_rowset.emplace(rowset->rowset_id(), SetgmentRowidRangeMap()).first;
+    }
 
-bool RowidRangeOption::match_rowset(const Rowset* rowset) const {
-    return rowset->rowset_id() == rowset_id;
+    auto& segment_map = rowset_it->second;
+    segment_map.emplace(segment->id(), std::move(rowid_range));
 }
 
-bool RowidRangeOption::match_segment(const Segment* segment) const {
-    return segment->id() == segment_id;
+bool RowidRangeOption::match_rowset(const Rowset* rowset) const {
+    return rowid_range_per_segment_per_rowset.find(rowset->rowset_id()) != rowid_range_per_segment_per_rowset.end();
+}
+
+SparseRangePtr RowidRangeOption::get_segment_rowid_range(const Rowset* rowset, const Segment* segment) {
+    auto rowset_it = rowid_range_per_segment_per_rowset.find(rowset->rowset_id());
+    if (rowset_it == rowid_range_per_segment_per_rowset.end()) {
+        return nullptr;
+    }
+
+    auto& segment_map = rowset_it->second;
+    auto segment_it = segment_map.find(segment->id());
+    if (segment_it == segment_map.end()) {
+        return nullptr;
+    }
+    return segment_it->second;
 }
 
 } // namespace starrocks

be/src/storage/rowset/rowid_range_option.h
@@ -27,15 +27,18 @@ class Segment;
 // It represents a specific rowid range on the segment with `segment_id` of the rowset with `rowset_id`.
 struct RowidRangeOption {
 public:
-    RowidRangeOption(const RowsetId& rowset_id, uint64_t segment_id, SparseRange rowid_range);
+    RowidRangeOption() = default;
+
+    void add(const Rowset* rowset, const Segment* segment, SparseRangePtr rowid_range);
 
     bool match_rowset(const Rowset* rowset) const;
-    bool match_segment(const Segment* segment) const;
+    SparseRangePtr get_segment_rowid_range(const Rowset* rowset, const Segment* segment);
 
 public:
-    const RowsetId rowset_id;
-    const uint64_t segment_id;
-    const SparseRange rowid_range;
+    using SetgmentRowidRangeMap = std::unordered_map<uint64_t, SparseRangePtr>;
+    using RowsetRowidRangeMap = std::map<RowsetId, SetgmentRowidRangeMap>;
+
+    RowsetRowidRangeMap rowid_range_per_segment_per_rowset;
 };
 
 } // namespace starrocks

be/src/storage/rowset/rowset.cpp
@@ -225,9 +225,12 @@ StatusOr<int64_t> Rowset::estimate_compaction_segment_iterator_num() {
         // When creating segment iterators for compaction, we don't provide rowid_range_option and predicates_for_zone_map,
         // So here we don't need to consider the following two situation:
         //
-        //    if (options.rowid_range_option != nullptr && !options.rowid_range_option->match_segment(seg_ptr.get())) {
-        //       continue;
+        // if (options.rowid_range_option != nullptr) {
+        //    seg_options.rowid_range_option = options.rowid_range_option->get_segment_rowid_range(this, seg_ptr.get());
+        //    if (seg_options.rowid_range_option == nullptr) {
+        //        continue;
         //    }
+        // }
         //    auto res = seg_ptr->new_iterator(segment_schema, seg_options);
         //    if (res.status().is_end_of_file()) {
         //     continue;
@@ -501,7 +504,6 @@ Status Rowset::get_segment_iterators(const Schema& schema, const RowsetReadOptio
         seg_options.delvec_loader = std::make_shared<LocalDelvecLoader>(options.meta);
         seg_options.dcg_loader = std::make_shared<LocalDeltaColumnGroupLoader>(options.meta);
     }
-    seg_options.rowid_range_option = options.rowid_range_option;
     seg_options.short_key_ranges = options.short_key_ranges;
     if (options.runtime_state != nullptr) {
         seg_options.is_cancelled = &options.runtime_state->cancelled_ref();
@@ -529,8 +531,11 @@ Status Rowset::get_segment_iterators(const Schema& schema, const RowsetReadOptio
             continue;
         }
 
-        if (options.rowid_range_option != nullptr && !options.rowid_range_option->match_segment(seg_ptr.get())) {
-            continue;
+        if (options.rowid_range_option != nullptr) {
+            seg_options.rowid_range_option = options.rowid_range_option->get_segment_rowid_range(this, seg_ptr.get());
+            if (seg_options.rowid_range_option == nullptr) {
+                continue;
+            }
         }
 
         auto res = seg_ptr->new_iterator(segment_schema, seg_options);

commit 98376491521665cb6e312cf9baf31e80cdc02508

[Bugfix] Morsel::set_rowsets references local variables (#24902)
Fixes bug introduced by #22892, Morsel's internal rowsets references to
local variables in pick_morsels. In ASAN mode, tests on multiversion
query cache would crash.
```
==13613==ERROR: AddressSanitizer: stack-use-after-scope on address 0x7f92031bca78 at pc 0x00000a06a8cd bp 0x7f92031bb830 sp 0x7f92031bb828
READ of size 8 at 0x7f92031bca78 thread T211 (pip_wg_executor)
    #0 0xa06a8cc in std::vector<std::shared_ptr<starrocks::Rowset>, std::allocator<std::shared_ptr<starrocks::Rowset> > >::size() const /opt/gcc/usr/include/c++/10.3.0/bits/stl_vector.h:919
    #1 0xa0672e9 in std::vector<std::shared_ptr<starrocks::Rowset>, std::allocator<std::shared_ptr<starrocks::Rowset> > >::vector(std::vector<std::shared_ptr<starrocks::Rowset>, std::allocator<std::shared_ptr<starrocks::Rowset> > > const&) /opt/gcc/usr/include/c++/10.3.0/bits/stl_vector.h:555
    #2 0xfc210d5 in starrocks::TabletReader::TabletReader(std::shared_ptr<starrocks::Tablet>, starrocks::Version const&, starrocks::Schema, std::vector<std::shared_ptr<starrocks::Rowset>, std::allocator<std::shared_ptr<starrocks::Rowset> > > const&) /root/starrocks/be/src/storage/tablet_reader.cpp:53
    #3 0xbbb63e4 in void __gnu_cxx::new_allocator<starrocks::TabletReader>::construct<starrocks::TabletReader, std::shared_ptr<starrocks::Tablet>&, starrocks::Version, starrocks::Schema, std::vector<std::shared_ptr<starrocks::Rowset>, std::allocator<std::shared_ptr<starrocks::Rowset> > > const&>(starrocks::TabletReader*, std::shared_ptr<starrocks::Tablet>&, starrocks::Version&&, starrocks::Schema&&, std::vector<std::shared_ptr<starrocks::Rowset>, std::allocator<std::shared_ptr<starrocks::Rowset> > > const&) /opt/gcc/usr/include/c++/10.3.0/ext/new_allocator.h:150

be/src/exec/pipeline/scan/morsel.h
@@ -83,7 +83,14 @@ public:
     int64_t from_version() { return _from_version; }
 
     void set_rowsets(const std::vector<RowsetSharedPtr>& rowsets) { _rowsets = &rowsets; }
-    const std::vector<RowsetSharedPtr>& rowsets() const { return *_rowsets; }
+    void set_delta_rowsets(std::vector<RowsetSharedPtr>&& delta_rowsets) { _delta_rowsets = std::move(delta_rowsets); }
+    const std::vector<RowsetSharedPtr>& rowsets() const {
+        if (_delta_rowsets.has_value()) {
+            return _delta_rowsets.value();
+        } else {
+            return *_rowsets;
+        }
+    }
 
 private:
     int32_t _plan_node_id;
@@ -92,6 +99,7 @@ private:
     static const std::vector<RowsetSharedPtr> kEmptyRowsets;
     // _rowsets is owned by MorselQueue, whose lifecycle is longer than that of Morsel.
     const std::vector<RowsetSharedPtr>* _rowsets = &kEmptyRowsets;
+    std::optional<std::vector<RowsetSharedPtr>> _delta_rowsets;
 };
 
 class ScanMorsel : public Morsel {

be/src/exec/pipeline/scan/scan_operator.cpp
@@ -458,7 +458,7 @@ Status ScanOperator::_pickup_morsel(RuntimeState* state, int chunk_source_index)
                     // We must reset rowsets of Morsel to captured delta rowsets, because TabletReader now
                     // created from rowsets passed in to itself instead of capturing it from TabletManager again.
                     morsel->set_from_version(delta_version);
-                    morsel->set_rowsets(delta_rowsets);
+                    morsel->set_delta_rowsets(std::move(delta_rowsets));
                     break;
                 } else {
                     ASSIGN_OR_RETURN(morsel, _morsel_queue->try_get());
@@ -472,7 +472,7 @@ Status ScanOperator::_pickup_morsel(RuntimeState* state, int chunk_source_index)
                 auto [delta_verrsion, delta_rowsets] = _cache_operator->delta_version_and_rowsets(lane_owner);
                 if (!delta_rowsets.empty()) {
                     morsel->set_from_version(delta_verrsion);
-                    morsel->set_rowsets(delta_rowsets);
+                    morsel->set_delta_rowsets(std::move(delta_rowsets));
                 }
                 break;
             }