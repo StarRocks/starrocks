c46304943d21047d6d22aeb30e3469f7e21770b0
Yixin Luo <18810541851@163.com>
2023-06-09 13:40:19 +0800

    [Enhancement] support dynamice config apply thread count (#24948)
    Add config item `transaction_apply_worker_count` and make apply thread
    count configable. When `transaction_apply_worker_count` = 0, using cpu
    number as worker count.
    
    Signed-off-by: luohaha <18810541851@163.com>
    (cherry picked from commit e3765ff4346912e2b38eadedb8418201078090c6)
    
    # Conflicts:
    #	be/src/http/action/update_config_action.cpp

--- be/src/http/action/update_config_action.cpp
+++ be/src/http/action/update_config_action.cpp
@@ -102,6 +102,20 @@ Status UpdateConfigAction::update_config(const std::string& name, const std::str
             _exec_env->agent_server()->update_max_thread_by_type(TTaskType::CLONE,
                                                                  config::parallel_clone_task_per_path);
         });
+<<<<<<< HEAD
+=======
+        _config_callback.emplace("lake_metadata_cache_limit", [&]() {
+            auto tablet_mgr = _exec_env->lake_tablet_manager();
+            if (tablet_mgr != nullptr) tablet_mgr->update_metacache_limit(config::lake_metadata_cache_limit);
+        });
+        _config_callback.emplace("transaction_apply_worker_count", [&]() {
+            int max_thread_cnt = CpuInfo::num_cores();
+            if (config::transaction_apply_worker_count > 0) {
+                max_thread_cnt = config::transaction_apply_worker_count;
+            }
+            StorageEngine::instance()->update_manager()->apply_thread_pool()->update_max_threads(max_thread_cnt);
+        });
+>>>>>>> e3765ff43 ([Enhancement] support dynamice config apply thread count (#24948))
     });
 
     Status s = config::set_config(name, value);
