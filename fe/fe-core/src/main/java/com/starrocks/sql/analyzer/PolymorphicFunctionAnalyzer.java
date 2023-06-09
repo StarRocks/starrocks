c37db2855c00f0ee60ec73bc422b7da88a11aaa6
Seaven <seaven_7@qq.com>
2023-06-09 15:46:15 +0800

    fix type
    Signed-off-by: Seaven <seaven_7@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/PolymorphicFunctionAnalyzer.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/PolymorphicFunctionAnalyzer.java
@@ -92,12 +92,14 @@ public class PolymorphicFunctionAnalyzer {
         return newFn;
     }
 
-    private static Type[] resolveArgTypes(Type[] declTypes, Type[] inputArgTypes) {
+    private static Type[] resolveArgTypes(Function fn, Type[] inputArgTypes) {
         // Use inputArgTypes length, because function may be a variable arguments
         Type[] resolvedTypes = Arrays.copyOf(inputArgTypes, inputArgTypes.length);
+        Type[] argsTypes = fn.getArgs();
 
-        for (int i = 0; i < declTypes.length; ++i) {
-            Type declType = declTypes[i];
+        int size = Math.max(argsTypes.length, inputArgTypes.length);
+        for (int i = 0; i < size; ++i) {
+            Type declType = size >= argsTypes.length ? argsTypes[argsTypes.length - 1] : argsTypes[i];
             Type inputType = inputArgTypes[i];
 
             // If declaration type is not a pseudo type, use it.
@@ -121,7 +123,7 @@ public class PolymorphicFunctionAnalyzer {
     }
 
     private static Function resolveByReplacingInputs(Function fn, Type[] inputArgTypes) {
-        Type[] resolvedArgTypes = resolveArgTypes(fn.getArgs(), inputArgTypes);
+        Type[] resolvedArgTypes = resolveArgTypes(fn, inputArgTypes);
         if (fn instanceof ScalarFunction) {
             return newScalarFunction((ScalarFunction) fn, Arrays.asList(resolvedArgTypes), fn.getReturnType());
         }
@@ -229,7 +231,7 @@ public class PolymorphicFunctionAnalyzer {
         if (deduce == null) {
             return null;
         }
-        Type[] resolvedArgTypes = resolveArgTypes(fn.getArgs(), inputArgTypes);
+        Type[] resolvedArgTypes = resolveArgTypes(fn, inputArgTypes);
         Type newRetType = deduce.apply(resolvedArgTypes);
         if (fn instanceof ScalarFunction) {
             return newScalarFunction((ScalarFunction) fn, Arrays.asList(resolvedArgTypes), newRetType);
@@ -288,6 +290,8 @@ public class PolymorphicFunctionAnalyzer {
         Type retType = fn.getReturnType();
         Type[] declTypes = fn.getArgs();
         Function resolvedFunction;
+
+        paramTypes = Arrays.stream(paramTypes).map(AnalyzerUtils::replaceNullType2Boolean).toArray(Type[]::new);
         long numPseudoArgs = Arrays.stream(declTypes).filter(Type::isPseudoType).count();
         // resolve single pseudo type parameter, example: int array_length(ANY_ARRAY)
         if (!retType.isPseudoType() && numPseudoArgs == 1) {
