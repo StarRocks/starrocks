f897cb8de167c6efd451a2d3545f04e9dbc6838e
Astralidea <astralidea@163.com>
2023-06-12 10:44:16 +0800

    Fix.
    Signed-off-by: Astralidea <astralidea@163.com>

--- fe/fe-core/src/test/java/com/starrocks/pseudocluster/PseudoCluster.java
+++ fe/fe-core/src/test/java/com/starrocks/pseudocluster/PseudoCluster.java
@@ -74,7 +74,7 @@ public class PseudoCluster {
 
     private static volatile PseudoCluster instance;
 
-    public static boolean logToConsole = true;
+    public static boolean logToConsole = false;
 
     ClusterConfig config = new ClusterConfig();
 
