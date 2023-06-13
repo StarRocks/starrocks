356de8d9a28ae4c3324bd2632088b42f7ad9f944
Murphy <mofei@starrocks.com>
2023-06-13 00:20:40 -0700

    remove config variable
    Signed-off-by: Murphy <mofei@starrocks.com>

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -683,9 +683,6 @@ CONF_Int64(pipeline_sink_brpc_dop, "64");
 CONF_Int64(pipeline_max_num_drivers_per_exec_thread, "10240");
 CONF_mBool(pipeline_print_profile, "false");
 
-// Use resource group threads to do memtable write & flush
-CONF_mBool(enable_resource_group_memtable, "true");
-
 // The arguments of multilevel feedback pipeline_driver_queue. It prioritizes small queries over larger ones,
 // when the value of level_time_slice_base_ns is smaller and queue_ratio_of_adjacent_queue is larger.
 CONF_Int64(pipeline_driver_queue_level_time_slice_base_ns, "200000000");
