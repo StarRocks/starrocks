c9fb3fa7846623958b4124ee0fe015d0cc0e0311
starrocks-xupeng <xupeng@starrocks.com>
2023-06-13 15:29:44 +0800

    [BugFix] fix external table commit timeout treated as failure (#24856)
    Signed-off-by: starrocks-xupeng <xupeng@starrocks.com>
    (cherry picked from commit 3d1b121b77f2193e8439bca15f0a4d8a657cad11)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/catalog/ExternalOlapTable.java
    #	fe/fe-core/src/main/java/com/starrocks/common/Config.java
    #	fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java

--- fe/fe-core/src/main/java/com/starrocks/catalog/ExternalOlapTable.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/ExternalOlapTable.java
@@ -298,7 +298,6 @@ public class ExternalOlapTable extends OlapTable {
             throws DdlException, IOException {
         // no meta changed since last time, do nothing
         if (lastExternalMeta != null && meta.compareTo(lastExternalMeta) == 0) {
-            LOG.info("no meta changed since last time, do nothing");
             return;
         }
 
@@ -312,6 +311,7 @@ public class ExternalOlapTable extends OlapTable {
         }
         db.writeLock();
 
+<<<<<<< HEAD
         try {
             lastExternalMeta = meta;
 
@@ -319,6 +319,12 @@ public class ExternalOlapTable extends OlapTable {
             baseIndexId = meta.getBase_index_id();
             colocateGroup = meta.getColocate_group();
             bfFpp = meta.getBloomfilter_fpp();
+=======
+        state = OlapTableState.valueOf(meta.getState());
+        baseIndexId = meta.getBase_index_id();
+        colocateGroup = meta.getColocate_group();
+        bfFpp = meta.getBloomfilter_fpp();
+>>>>>>> 3d1b121b7 ([BugFix] fix external table commit timeout treated as failure (#24856))
 
             keysType = KeysType.valueOf(meta.getKey_type());
             tableProperty = new TableProperty(meta.getProperties());
@@ -522,5 +528,16 @@ public class ExternalOlapTable extends OlapTable {
         } finally {
             db.writeUnlock();
         }
+<<<<<<< HEAD
+=======
+
+        lastExternalMeta = meta;
+        LOG.info("TableMetaSyncer finish meta update. partition build cost: {}ms, " +
+                        "index meta build cost: {}ms, schema rebuild cost: {}ms, " +
+                        "tablet meta build cost: {}ms, total cost: {}ms",
+                endOfPartitionBuild - start, endOfIndexMetaBuild - endOfPartitionBuild,
+                endOfSchemaRebuild - endOfIndexMetaBuild, endOfTabletMetaBuild - endOfSchemaRebuild,
+                System.currentTimeMillis() - start);
+>>>>>>> 3d1b121b7 ([BugFix] fix external table commit timeout treated as failure (#24856))
     }
 }
