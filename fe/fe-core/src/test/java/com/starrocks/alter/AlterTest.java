da2d3f93d76b30a21bbd104981efc8b601fa82b3
Astralidea <astralidea@163.com>
2023-06-14 20:42:16 +0800

    Fix.
    Signed-off-by: Astralidea <astralidea@163.com>

--- fe/fe-core/src/test/java/com/starrocks/alter/AlterTest.java
+++ fe/fe-core/src/test/java/com/starrocks/alter/AlterTest.java
@@ -249,20 +249,6 @@ public class AlterTest {
                         "PROPERTIES(\n" +
                         "    \"partition_live_number\" = \"3\",\n" +
                         "    \"replication_num\" = \"1\"\n" +
-                        ");")
-                .withTable("CREATE TABLE t2 (\n" +
-                        "    dt datetime  not null,\n" +
-                        "    user_id  bigint  not null,\n" +
-                        "    recharge_money decimal(32,2) not null, \n" +
-                        "    province varchar(20) not null,\n" +
-                        "    id varchar(20) not null\n" +
-                        ") ENGINE=OLAP\n" +
-                        "DUPLICATE KEY(dt)\n" +
-                        "PARTITION BY (dt)\n" +
-                        "DISTRIBUTED BY HASH(`dt`) BUCKETS 10 \n" +
-                        "PROPERTIES (\n" +
-                        "\"replication_num\" = \"1\",\n" +
-                        "\"in_memory\" = \"false\"\n" +
                         ");");
     }
 
@@ -2599,6 +2585,20 @@ public class AlterTest {
 
     @Test(expected = DdlException.class)
     public void testAlterListPartitionUseBatchBuildPartition() throws Exception {
+        starRocksAssert.useDatabase("test").withTable("CREATE TABLE t2 (\n" +
+                "    dt datetime  not null,\n" +
+                "    user_id  bigint  not null,\n" +
+                "    recharge_money decimal(32,2) not null, \n" +
+                "    province varchar(20) not null,\n" +
+                "    id varchar(20) not null\n" +
+                ") ENGINE=OLAP\n" +
+                "DUPLICATE KEY(dt)\n" +
+                "PARTITION BY (dt)\n" +
+                "DISTRIBUTED BY HASH(`dt`) BUCKETS 10 \n" +
+                "PROPERTIES (\n" +
+                "\"replication_num\" = \"1\",\n" +
+                "\"in_memory\" = \"false\"\n" +
+                ");");
         ConnectContext ctx = starRocksAssert.getCtx();
         String sql = "ALTER TABLE t2 ADD PARTITIONS START (\"2021-01-04\") END (\"2021-01-06\") EVERY (INTERVAL 1 DAY);";
         AlterTableStmt alterTableStmt = (AlterTableStmt) UtFrameUtils.parseStmtWithNewParser(sql, ctx);
