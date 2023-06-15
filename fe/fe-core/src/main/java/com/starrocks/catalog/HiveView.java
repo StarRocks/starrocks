a1b988dde529529565d5c8c7e269b18f1ffc0a5d
Youngwb <yangwenbo_mailbox@163.com>
2023-06-15 17:13:58 +0800

    [BugFix] Fix show tables without hive view (#25085)
    Fixes #issue
    show tables do not have hive views, because of its privilege have
    problems, fix this issue
    
    Signed-off-by: Youngwb <yangwenbo_mailbox@163.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/HiveView.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/HiveView.java
@@ -14,26 +14,54 @@
 
 package com.starrocks.catalog;
 
+import com.google.common.base.Preconditions;
+import com.starrocks.analysis.ParseNode;
+import com.starrocks.qe.SessionVariable;
 import com.starrocks.sql.analyzer.AnalyzerUtils;
 import com.starrocks.sql.ast.QueryStatement;
 import com.starrocks.sql.ast.TableRelation;
+import com.starrocks.sql.common.ErrorType;
 import com.starrocks.sql.common.StarRocksPlannerException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
 import static java.util.Objects.requireNonNull;
 
-public class HiveView extends View {
+public class HiveView extends Table {
+    private static final Logger LOG = LogManager.getLogger(HiveView.class);
     private final String catalogName;
+    private final String inlineViewDef;
     public HiveView(long id, String catalogName, String name, List<Column> schema, String definition) {
-        super(id, name, schema);
+        super(id, name, TableType.HIVE_VIEW, schema);
         this.catalogName = requireNonNull(catalogName, "Hive view catalog name is null");
         this.inlineViewDef = requireNonNull(definition, "Hive view text is null");
     }
 
-    @Override
+    public QueryStatement getQueryStatementWithSRParser() throws StarRocksPlannerException {
+        Preconditions.checkNotNull(inlineViewDef);
+        ParseNode node;
+        try {
+            node = com.starrocks.sql.parser.SqlParser.parse(inlineViewDef, new SessionVariable()).get(0);
+        } catch (Exception e) {
+            LOG.warn("stmt is {}", inlineViewDef);
+            LOG.warn("exception because: ", e);
+            throw new StarRocksPlannerException(
+                    String.format("Failed to parse view-definition statement of view: %s", name),
+                    ErrorType.INTERNAL_ERROR);
+        }
+        // Make sure the view definition parses to a query statement.
+        if (!(node instanceof QueryStatement)) {
+            throw new StarRocksPlannerException(String.format("View definition of %s " +
+                    "is not a query statement", name), ErrorType.INTERNAL_ERROR);
+        }
+
+        return (QueryStatement) node;
+    }
+
     public QueryStatement getQueryStatement() throws StarRocksPlannerException {
-        QueryStatement queryStatement = super.getQueryStatement();
+        QueryStatement queryStatement = getQueryStatementWithSRParser();
         List<TableRelation> tableRelations = AnalyzerUtils.collectTableRelations(queryStatement);
         for (TableRelation tableRelation : tableRelations) {
             tableRelation.getName().setCatalog(catalogName);
@@ -41,5 +69,7 @@ public class HiveView extends View {
         return queryStatement;
     }
 
-
+    public String getInlineViewDef() {
+        return inlineViewDef;
+    }
 }
