ed48c92ab1dba243ae8fd72174c618e28ad9750a
Smith Cruise <chendingchao1@126.com>
2023-06-19 21:29:43 +0800

    rm unsed
    Signed-off-by: Smith Cruise <chendingchao1@126.com>

--- fe/fe-core/src/main/java/com/starrocks/planner/IcebergScanNode.java
+++ fe/fe-core/src/main/java/com/starrocks/planner/IcebergScanNode.java
@@ -65,7 +65,12 @@ import org.apache.iceberg.StructLike;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
