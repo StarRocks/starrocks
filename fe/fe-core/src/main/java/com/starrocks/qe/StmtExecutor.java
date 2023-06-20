5ed9efaeca735aa64fed57a092ab6aab24ebbd9a
Seaven <seaven_7@qq.com>
2023-06-20 11:01:44 +0800

    [BugFix] Fix statistcs collect bugs (#25369)
    Fixes #issue
    
    1. bug: drop invalid partition statistics overflow
    SqlParse.expr_children_limit bug
    the reason because SqlParse.parseFirstStatment doesn't use
    sessionVariables
    
    Fix: SqlParse.parseFirstStatement ->
    SqlParse.parseOneWithStarRocksDialect
    
    2. bug: will query statistics table when collect statistics, it's
    unnecessary
    Fix: add `isStatisticsConnection` in `ConnectContext`, and will check it
    in `statisticTableBlackListCheck`
    
    3. bug: the statistic collector will query old version data (commmited
    but doesn't publish)
    StatisticsMeta updated `updateRows` when the transaction commits (may be
    doesn't publish when high-concurrency load), then trigger statistics
    collector work
    
    Fix: StatisticsMeta will update `updateRows` when the transaction
    publish
    
    ---------
    
    Signed-off-by: Seaven <seaven_7@qq.com>
    (cherry picked from commit 4a7030a2cdd98a152e3d21dc628e3cf0583977f1)

--- fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
@@ -993,6 +993,7 @@ public class StmtExecutor {
         statsConnectCtx.getSessionVariable().setStatisticCollectParallelism(
                 context.getSessionVariable().getStatisticCollectParallelism());
         statsConnectCtx.setThreadLocalInfo();
+        statsConnectCtx.setStatisticsConnection(true);
         executeAnalyze(statsConnectCtx, analyzeStmt, analyzeStatus, db, table);
     }
 
