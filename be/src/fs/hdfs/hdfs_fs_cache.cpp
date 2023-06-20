5f978323e4685d9735a5436b07709d488fb653c2
Smith Cruise <chendingchao1@126.com>
2023-06-20 16:48:40 +0800

    format
    Signed-off-by: Smith Cruise <chendingchao1@126.com>

--- be/src/fs/hdfs/hdfs_fs_cache.cpp
+++ be/src/fs/hdfs/hdfs_fs_cache.cpp
@@ -16,9 +16,9 @@
 
 #include <memory>
 
+#include "common/config.h"
 #include "gutil/strings/substitute.h"
 #include "util/hdfs_util.h"
-#include "common/config.h"
 
 namespace starrocks {
 
@@ -72,8 +72,10 @@ static Status create_hdfs_fs_handle(const std::string& namenode, const std::shar
 
     // Set for hdfs client hedged read
     if (config::hdfs_client_enable_hedged_read) {
-        hdfsBuilderConfSetStr(hdfs_builder, "dfs.client.hedged.read.threadpool.size", std::to_string(config::hdfs_client_hedged_read_threadpool_size).data());
-        hdfsBuilderConfSetStr(hdfs_builder, "dfs.client.hedged.read.threshold.millis", std::to_string(config::hdfs_client_hedged_read_threshold_millis).data());
+        hdfsBuilderConfSetStr(hdfs_builder, "dfs.client.hedged.read.threadpool.size",
+                              std::to_string(config::hdfs_client_hedged_read_threadpool_size).data());
+        hdfsBuilderConfSetStr(hdfs_builder, "dfs.client.hedged.read.threshold.millis",
+                              std::to_string(config::hdfs_client_hedged_read_threshold_millis).data());
     }
 
     hdfs_client->hdfs_fs = hdfsBuilderConnect(hdfs_builder);
