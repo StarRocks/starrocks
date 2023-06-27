9ac1a901596c7f505573b7e64324c8406cc46ebe
Seaven <seaven_7@qq.com>
2023-06-27 15:24:50 +0800

    [BugFix] Fix create view with lateral join bug (#25824)
    Fixes #issue
    
    lateral join to sql print output column `ie(key,value) `:
    
    `KEY`,`VALUE` is keywords, we should print sql use quota, like
    ```ie(`key`,`value`)```
    
    the origin SQL:
    ```
    SELECT `test`.`ge`.`v_id`, get_json_string(`ie`.`value`, '$.b') AS `b`, get_json_string(`ie`.`value`, '$.c') AS `c`
    FROM `test`.`json_test` AS `ge` , LATERAL json_each(CAST(`test`.`ge`.`v_json` AS JSON)->'$.') ie
    ```
    
    but the view SQL:
    ```
    SELECT `test`.`ge`.`v_id`, get_json_string(`ie`.`value`, '$.b') AS `b`, get_json_string(`ie`.`value`, '$.c') AS `c`
    FROM `test`.`json_test` AS `ge` , LATERAL json_each(CAST(`test`.`ge`.`v_json` AS JSON)->'$.') ie(key,value)
    ```
    ---------
    
    Signed-off-by: Seaven <seaven_7@qq.com>
    (cherry picked from commit 31c173250d74f45c7d4e06d5c77b5369cbc7b79b)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/sql/analyzer/AstToSQLBuilder.java
    #	fe/fe-core/src/test/java/com/starrocks/sql/plan/ViewPlanTest.java

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/ViewPlanTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/ViewPlanTest.java
@@ -1716,6 +1716,7 @@ public class ViewPlanTest extends PlanTestBase {
         Assert.assertEquals(sqlPlan, viewPlan);
     }
 
+<<<<<<< HEAD
     @Test
     public void testListViews() throws TException {
         ExecuteEnv instance = ExecuteEnv.getInstance();
@@ -1727,5 +1728,36 @@ public class ViewPlanTest extends PlanTestBase {
         params.type = TTableType.VIEW;
         TListTableStatusResult result = frontendService.listTableStatus(params);
         Assert.assertTrue(result.tables.size() > 0);
+=======
+
+    @Test
+    public void testLateralJoin() throws Exception {
+        starRocksAssert.withTable("CREATE TABLE json_test (" +
+                " v_id INT," +
+                " v_json json, " +
+                " v_SMALLINT SMALLINT" +
+                ") DUPLICATE KEY (v_id) " +
+                "DISTRIBUTED BY HASH (v_id) " +
+                "properties(\"replication_num\"=\"1\") ;"
+        );
+        String sql = "    SELECT\n" +
+                "         v_id\n" +
+                "        , get_json_string(ie.value,'$.b') as b\n" +
+                "        ,get_json_string(ie.value,'$.c') as c\n" +
+                "    FROM\n" +
+                "      json_test ge\n" +
+                "      ,lateral json_each(cast (ge.v_json as json) -> '$.')ie";
+
+        testView(sql);
+
+        sql = "    SELECT\n" +
+                "         v_id\n" +
+                "        , get_json_string(ie.value,'$.b') as b\n" +
+                "        ,get_json_string(ie.value,'$.c') as c\n" +
+                "    FROM\n" +
+                "      json_test ge\n" +
+                "      ,lateral json_each(cast (ge.v_json as json) -> '$.') ie(`key`, `value`)";
+        testView(sql);
+>>>>>>> 31c173250 ([BugFix] Fix create view with lateral join bug (#25824))
     }
 }
