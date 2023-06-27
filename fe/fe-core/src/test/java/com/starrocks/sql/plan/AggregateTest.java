511432daf7158d7fb8333007598176395eb2fc08
packy92 <wangchao@starrocks.com>
2023-06-27 18:17:19 +0800

    fix ut
    Signed-off-by: packy92 <wangchao@starrocks.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/AggregateTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/AggregateTest.java
@@ -1489,11 +1489,11 @@ public class AggregateTest extends PlanTestBase {
         sql = "select avg(distinct t1b) as cn_t1b, sum(distinct t1b), " +
                 "count(distinct t1b, t1c) cn_t1b_t1c from test_all_type group by t1c";
         plan = getFragmentPlan(sql);
-        assertContains(plan, "13:HASH JOIN\n" +
+        assertContains(plan, "17:HASH JOIN\n" +
                 "  |  join op: INNER JOIN (BUCKET_SHUFFLE(S))\n" +
                 "  |  colocate: false, reason: \n" +
                 "  |  equal join conjunct: 15: t1c <=> 17: t1c\n");
-        assertContains(plan, "20:HASH JOIN\n" +
+        assertContains(plan, "26:HASH JOIN\n" +
                 "  |  join op: INNER JOIN (BUCKET_SHUFFLE(S))\n" +
                 "  |  colocate: false, reason: \n" +
                 "  |  equal join conjunct: 15: t1c <=> 20: t1c");
@@ -1505,7 +1505,7 @@ public class AggregateTest extends PlanTestBase {
                 "  |  <slot 2> : 2: t1b\n" +
                 "  |  <slot 3> : 3: t1c\n" +
                 "  |  <slot 11> : CAST(2: t1b AS INT) + 1");
-        assertContains(plan, "21:HASH JOIN\n" +
+        assertContains(plan, "27:HASH JOIN\n" +
                 "  |  join op: INNER JOIN (BUCKET_SHUFFLE(S))\n" +
                 "  |  colocate: false, reason: \n" +
                 "  |  equal join conjunct: 16: t1c <=> 23: t1c\n" +
@@ -1514,7 +1514,7 @@ public class AggregateTest extends PlanTestBase {
         sql = "select avg(distinct t1b) as cn_t1b, sum(t1b), " +
                 "count(distinct t1b, t1c) cn_t1b_t1c from test_all_type group by t1c, t1b+1";
         plan = getFragmentPlan(sql);
-        assertContains(plan, "27:HASH JOIN\n" +
+        assertContains(plan, "33:HASH JOIN\n" +
                 "  |  join op: INNER JOIN (BUCKET_SHUFFLE(S))\n" +
                 "  |  colocate: false, reason: \n" +
                 "  |  equal join conjunct: 16: t1c <=> 27: t1c\n" +
