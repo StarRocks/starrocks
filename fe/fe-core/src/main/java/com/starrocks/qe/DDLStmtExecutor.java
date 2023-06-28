2774cfff82cea8af455696cb3dd5c5d654cbeed7
Zijie Lu <wslzj40@gmail.com>
2023-06-28 11:16:30 +0800

    [BugFix] Return error when creating a storage volume which has existed (#26065)
    Fixes #26033 
    
    Signed-off-by: Zijie Lu <wslzj40@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/DDLStmtExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/DDLStmtExecutor.java
@@ -888,6 +888,8 @@ public class DDLStmtExecutor {
                 } catch (AlreadyExistsException e) {
                     if (stmt.isSetIfNotExists()) {
                         LOG.info("create storage volume[{}] which already exists", stmt.getName());
+                    } else {
+                        throw e;
                     }
                 }
             });
