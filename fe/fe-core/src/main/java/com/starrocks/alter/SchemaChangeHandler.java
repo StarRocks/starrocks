8cccade10f5fe893e7dec14664a57e7c49c4a415
Felix Li <astralidea@163.com>
2023-06-28 19:05:21 +0800

    [Feature] Support modify table property default storage medium (#26116)
    Fixes #25870
    
    Signed-off-by: Astralidea <astralidea@163.com>
    (cherry picked from commit c8a1b44da938690cd9ca9a013848d3f4bc23f67d)

--- fe/fe-core/src/main/java/com/starrocks/alter/SchemaChangeHandler.java
+++ fe/fe-core/src/main/java/com/starrocks/alter/SchemaChangeHandler.java
@@ -1282,6 +1282,9 @@ public class SchemaChangeHandler extends AlterHandler {
                 } else if (properties.containsKey(PropertyAnalyzer.PROPERTIES_PARTITION_LIVE_NUMBER)) {
                     GlobalStateMgr.getCurrentState().alterTableProperties(db, olapTable, properties);
                     return null;
+                } else if (properties.containsKey(PropertyAnalyzer.PROPERTIES_STORAGE_MEDIUM)) {
+                    GlobalStateMgr.getCurrentState().alterTableProperties(db, olapTable, properties);
+                    return null;
                 }
             }
 
