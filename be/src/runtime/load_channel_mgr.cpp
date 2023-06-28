ee3132928c6bd78da2c9b3a6be538077f8f2cc1c
meegoo <meegoo.sr@gmail.com>
2023-06-28 14:11:18 +0800

    [BugFix] Fix write fail error message (#25942)
    Fixes #25943
    
    Signed-off-by: meegoo <meegoo.sr@gmail.com>
    (cherry picked from commit e08494d2be9eb0116744b88eb9e8bb26edebea4b)
    
    # Conflicts:
    #	be/src/runtime/lake_tablets_channel.cpp
    #	be/src/runtime/load_channel.cpp
    #	be/src/runtime/local_tablets_channel.cpp
    #	be/src/runtime/local_tablets_channel.h
    #	be/src/runtime/tablets_channel.h

--- be/src/runtime/load_channel_mgr.cpp
+++ be/src/runtime/load_channel_mgr.cpp
@@ -164,7 +164,7 @@ void LoadChannelMgr::cancel(brpc::Controller* cntl, const PTabletWriterCancelReq
     if (request.has_tablet_id()) {
         auto channel = _find_load_channel(load_id);
         if (channel != nullptr) {
-            channel->abort(request.index_id(), {request.tablet_id()});
+            channel->abort(request.index_id(), {request.tablet_id()}, request.reason());
         }
     } else if (request.tablet_ids_size() > 0) {
         auto channel = _find_load_channel(load_id);
@@ -173,7 +173,7 @@ void LoadChannelMgr::cancel(brpc::Controller* cntl, const PTabletWriterCancelReq
             for (auto& tablet_id : request.tablet_ids()) {
                 tablet_ids.emplace_back(tablet_id);
             }
-            channel->abort(request.index_id(), tablet_ids);
+            channel->abort(request.index_id(), tablet_ids, request.reason());
         }
     } else {
         if (auto channel = remove_load_channel(load_id); channel != nullptr) {
