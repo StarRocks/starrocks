3f6ae69e5e3d0470184f53a5c7443ca73ff75ea5
Murphy <mofei@starrocks.com>
2023-06-29 10:23:54 +0800

    fix conflict
    Signed-off-by: Murphy <mofei@starrocks.com>

--- be/src/exec/pipeline/pipeline_driver.h
+++ be/src/exec/pipeline/pipeline_driver.h
@@ -424,13 +424,9 @@ protected:
     static constexpr int64_t YIELD_MAX_TIME_SPENT = 100'000'000L;
     // Yield PipelineDriver when maximum time in nano-seconds has spent in current execution round,
     // if it runs in the worker thread owned by other workgroup, which has running drivers.
-<<<<<<< HEAD
     static constexpr int64_t YIELD_PREEMPT_MAX_TIME_SPENT = 5'000'000L;
-=======
-    static constexpr int64_t YIELD_PREEMPT_MAX_TIME_SPENT_NS = 5'000'000L;
     // Execution time exceed this is considered overloaded
     static constexpr int64_t OVERLOADED_MAX_TIME_SPEND_NS = 150'000'000L;
->>>>>>> 7adb8c30a ([Enhancement] add  statistics to pipeline execution  (#26021))
 
     // check whether fragment is cancelled. It is used before pull_chunk and push_chunk.
     bool _check_fragment_is_canceled(RuntimeState* runtime_state);
