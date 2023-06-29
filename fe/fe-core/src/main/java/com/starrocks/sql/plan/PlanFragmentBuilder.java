9db777007f713a223160557a39f77c96eeeb8a10
rickif <rickif@qq.com>
2023-06-29 14:13:44 +0800

    [BugFix] Column mapping of table function (#25366)
    Fixes https://github.com/StarRocks/starrocks/issues/25218 #25288 #25358
    #25275 #25285
    
    This PR fixes the column mapping issue.

--- fe/fe-core/src/main/java/com/starrocks/sql/plan/PlanFragmentBuilder.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/plan/PlanFragmentBuilder.java
@@ -3123,7 +3123,7 @@ public class PlanFragmentBuilder {
 
             TableFunctionTable table = (TableFunctionTable) node.getTable();
 
-            TupleDescriptor tupleDesc = buildTupleDesc(context, table);
+            TupleDescriptor tupleDesc = context.getDescTbl().createTupleDescriptor();
 
             List<List<TBrokerFileStatus>> files = new ArrayList<>();
             files.add(table.fileList());
@@ -3147,6 +3147,7 @@ public class PlanFragmentBuilder {
             scanNode.setUseVectorizedLoad(true);
 
             Analyzer analyzer = new Analyzer(GlobalStateMgr.getCurrentState(), context.getConnectContext());
+            analyzer.setDescTbl(context.getDescTbl());
             try {
                 scanNode.init(analyzer);
                 scanNode.finalizeStats(analyzer);
