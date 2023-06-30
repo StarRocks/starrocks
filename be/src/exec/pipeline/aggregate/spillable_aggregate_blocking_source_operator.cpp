efd58c2863b53421c57241d06fd95ed6d21a4806
stdpain <34912776+stdpain@users.noreply.github.com>
2023-06-30 21:18:06 +0800

    [BugFix] Fix miss the last chunk in chunk accumulator (#26328)
    Signed-off-by: stdpain <drfeng08@gmail.com>

--- be/src/exec/pipeline/aggregate/spillable_aggregate_blocking_source_operator.cpp
+++ be/src/exec/pipeline/aggregate/spillable_aggregate_blocking_source_operator.cpp
@@ -40,6 +40,9 @@ bool SpillableAggregateBlockingSourceOperator::has_output() const {
     if (!_aggregator->spiller()->spilled()) {
         return false;
     }
+    if (_accumulator.has_output()) {
+        return true;
+    }
     // has output data from spiller.
     if (_aggregator->spiller()->has_output_data()) {
         return true;
@@ -57,6 +60,9 @@ bool SpillableAggregateBlockingSourceOperator::is_finished() const {
     }
     if (!_aggregator->spiller()->spilled()) {
         return AggregateBlockingSourceOperator::is_finished();
+    }
+    if (_accumulator.has_output()) {
+        return false;
     }
     if (_aggregator->spiller()->is_cancel()) {
         return true;
@@ -96,6 +102,11 @@ StatusOr<ChunkPtr> SpillableAggregateBlockingSourceOperator::_pull_spilled_chunk
     DCHECK(_accumulator.need_input());
     ChunkPtr res;
 
+    if (_accumulator.has_output()) {
+        auto accumulated = std::move(_accumulator.pull());
+        return accumulated;
+    }
+
     if (!_aggregator->is_spilled_eos()) {
         auto executor = _aggregator->spill_channel()->io_executor();
         ASSIGN_OR_RETURN(auto chunk,
@@ -108,7 +119,7 @@ StatusOr<ChunkPtr> SpillableAggregateBlockingSourceOperator::_pull_spilled_chunk
         ASSIGN_OR_RETURN(res, _stream_aggregator->streaming_compute_agg_state(chunk->num_rows(), false));
         _accumulator.push(std::move(res));
 
-    } else {
+    } else if (_has_last_chunk) {
         _has_last_chunk = false;
         ASSIGN_OR_RETURN(res, _stream_aggregator->pull_eos_chunk());
         if (res != nullptr && !res->is_empty()) {
