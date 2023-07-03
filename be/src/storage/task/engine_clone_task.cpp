78a8c87cd61b52961506ddf5fc705703b5398107
Jinho Kim <117620956+jkim650@users.noreply.github.com>
2023-07-02 22:44:19 -0700

    [Tool] Upgrade ftm from 7.0.3 to 8.1.1. (#26057)
    Upgrade fmt from 7.0.3 to 8.1.1.
    In my fork, I need to use a library that requires fmt-8.1.1.
    
    I have fixed a potential compatibility issue in
    https://github.com/StarRocks/starrocks/pull/25806.
    
    ---------
    
    Signed-off-by: jkim650 <117620956+jkim650@users.noreply.github.com>

--- be/src/storage/task/engine_clone_task.cpp
+++ be/src/storage/task/engine_clone_task.cpp
@@ -162,11 +162,12 @@ Status EngineCloneTask::_do_clone_primary_tablet(Tablet* tablet) {
         if (st.ok()) {
             st = _finish_clone_primary(tablet, download_path);
         } else if (st.is_not_found()) {
-            LOG(INFO) << "No missing version found from src replica. tablet: {}, src BE:{}:{}, type: {}, "
-                         "missing_version_ranges: {}, committed_version: {}"_format(
-                                 tablet->tablet_id(), _clone_req.src_backends[0].host,
-                                 _clone_req.src_backends[0].be_port, KeysType_Name(tablet->keys_type()),
-                                 version_range_list_to_string(missing_version_ranges), _clone_req.committed_version);
+            LOG(INFO) << fmt::format(
+                    "No missing version found from src replica. tablet: {}, src BE:{}:{}, type: {}, "
+                    "missing_version_ranges: {}, committed_version: {}",
+                    tablet->tablet_id(), _clone_req.src_backends[0].host, _clone_req.src_backends[0].be_port,
+                    KeysType_Name(tablet->keys_type()), version_range_list_to_string(missing_version_ranges),
+                    _clone_req.committed_version);
             return Status::OK();
         }
     }
