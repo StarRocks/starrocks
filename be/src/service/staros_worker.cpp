0331e18edbdca23848367947a43c68f31a6440b0
starrocks-xupeng <xupeng@starrocks.com>
2023-07-03 18:57:26 +0800

    [Feature] support mutable config for star cache
    Signed-off-by: starrocks-xupeng <xupeng@starrocks.com>

--- be/src/service/staros_worker.cpp
+++ be/src/service/staros_worker.cpp
@@ -26,6 +26,7 @@
 #include "file_store.pb.h"
 #include "fmt/format.h"
 #include "fslib/star_cache_configuration.h"
+#include "fslib/star_cache_handler.h"
 #include "gflags/gflags.h"
 #include "gutil/strings/fastmem.h"
 #include "util/await.h"
@@ -376,6 +377,14 @@ void shutdown_staros_worker() {
 
     LOG(INFO) << "Executing starlet shutdown hooks ...";
     staros::starlet::common::ShutdownHook::shutdown();
+}
+
+// must keep each config the same
+void update_staros_starcache() {
+    if (fslib::FLAGS_use_star_cache != config::starlet_use_star_cache) {
+        fslib::FLAGS_use_star_cache = config::starlet_use_star_cache;
+        (void)fslib::star_cache_init(fslib::FLAGS_use_star_cache);
+    }
 }
 
 } // namespace starrocks
