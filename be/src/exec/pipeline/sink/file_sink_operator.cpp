489831adb50152abe1a5e06eff7a4905aa148874
Letian Jiang <letian.jiang@outlook.com>
2023-07-05 11:40:35 +0800

    [BugFix] Fix SinkIOBuffer use-after-free by skipping stop task (#26028)
    Fixes https://github.com/StarRocks/StarRocksTest/issues/2691
    
    This PR is based on two guarantees:
    - Query context (including SinkIOBuffer) would not be destroyed if
    SinkIOBuffer is not finished yet. Once SinkIOBuffer becomes finished,
    the execution queue has no pending chunk tasks except a stop task
    following.
    - Execution queue would only enqueue a stop task once its refcnt becomes
    zero and ready to be destroyed.
    
    This PR makes the consumer thread skip the stop task, avoiding
    use-after-free error. You may refer to `sink_io_buffer_test.cpp` for
    more details.
    
    ## What type of PR is this:
    - [x] BugFix
    - [ ] Feature
    - [ ] Enhancement
    - [ ] Refactor
    - [ ] UT
    - [ ] Doc
    - [ ] Tool
    
    ## Checklist:
    - [x] I have added test cases for my bug fix or my new feature
    - [ ] This pr will affect users' behaviors
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    
    ## Bugfix cherry-pick branch check:
    - [x] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [ ] 3.1
      - [ ] 3.0
      - [ ] 2.5
      - [ ] 2.4
    
    Signed-off-by: Letian Jiang <letian.jiang@outlook.com>

--- be/src/exec/pipeline/sink/file_sink_operator.cpp
+++ be/src/exec/pipeline/sink/file_sink_operator.cpp
@@ -115,7 +115,11 @@ void FileSinkIOBuffer::close(RuntimeState* state) {
 }
 
 void FileSinkIOBuffer::_process_chunk(bthread::TaskIterator<ChunkPtr>& iter) {
-    --_num_pending_chunks;
+    DeferOp op([&]() {
+        --_num_pending_chunks;
+        DCHECK(_num_pending_chunks >= 0);
+    });
+
     // close is already done, just skip
     if (_is_finished) {
         return;
@@ -123,7 +127,7 @@ void FileSinkIOBuffer::_process_chunk(bthread::TaskIterator<ChunkPtr>& iter) {
 
     // cancelling has happened but close is not invoked
     if (_is_cancelled && !_is_finished) {
-        if (_num_pending_chunks == 0) {
+        if (_num_pending_chunks == 1) {
             close(_state);
         }
         return;
@@ -143,7 +147,7 @@ void FileSinkIOBuffer::_process_chunk(bthread::TaskIterator<ChunkPtr>& iter) {
     const auto& chunk = *iter;
     if (chunk == nullptr) {
         // this is the last chunk
-        DCHECK_EQ(_num_pending_chunks, 0);
+        DCHECK_EQ(_num_pending_chunks, 1);
         close(_state);
         return;
     }
