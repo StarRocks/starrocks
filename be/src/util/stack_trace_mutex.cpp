e87e984268f2e4683833433513eda4d6f4170ef5
Murphy <96611012+mofeiatwork@users.noreply.github.com>
2023-07-05 10:17:56 +0800

    [Enhancement] infer bucket number for materialized view (#26285)
    With simplified syntax we don't bother to specify the data distribution
    property.
    Now it could infer the bucket number base on bucket number of base
    table.
    
    ```sql
    CREATE MATERIALIZED VIEW mv
    REFRESH ASYNC
    AS 
    <query> 
    ```

--- be/src/util/stack_trace_mutex.cpp
+++ be/src/util/stack_trace_mutex.cpp
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <array>
 #include <iostream>
 #include <mutex>
 #include <string>
