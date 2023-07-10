0c16f526d39b0df0671b5934b253845ec34e2a11
stdpain <34912776+stdpain@users.noreply.github.com>
2023-07-10 08:54:27 +0800

    [BugFix] Fix use-after-free when set_call_back (#26738)
    Signed-off-by: stdpain <drfeng08@gmail.com>

--- be/src/exec/spill/spiller.h
+++ be/src/exec/spill/spiller.h
@@ -91,7 +91,7 @@ struct SpillProcessMetrics {
 };
 
 // major spill interfaces
-class Spiller {
+class Spiller : public std::enable_shared_from_this<Spiller> {
 public:
     Spiller(SpilledOptions opts, const std::shared_ptr<SpillerFactory>& factory)
             : _opts(std::move(opts)), _parent(factory) {}
@@ -139,6 +139,7 @@ public:
     Status set_flush_all_call_back(const FlushAllCallBack& callback, RuntimeState* state, IOTaskExecutor& executor,
                                    const MemGuard& guard) {
         auto flush_call_back = [this, callback, state, &executor, guard]() {
+            RETURN_IF(!guard.scoped_begin(), Status::Cancelled("cancelled"));
             RETURN_IF_ERROR(callback());
             if (!_is_cancel && spilled()) {
                 RETURN_IF_ERROR(_acquire_input_stream(state));
