f70516733dd6d600d4e639038cdf328efff61221
Murphy <96611012+mofeiatwork@users.noreply.github.com>
2023-07-11 18:36:27 +0800

    [BugFix] improve error message when creating table in system database (#26943)
    (cherry picked from commit ddab92cab0e30c14cf6e15d33bb7bbc8faa81bda)

--- fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
+++ fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
@@ -1983,7 +1983,8 @@ public class LocalMetastore implements ConnectorMetadata {
             }
 
             if (db.isSystemDatabase()) {
-                ErrorReport.reportDdlException(ErrorCode.ERR_CANT_CREATE_TABLE, table.getName(), "create denied");
+                ErrorReport.reportDdlException(ErrorCode.ERR_CANT_CREATE_TABLE, table.getName(),
+                        "cannot create table in system database");
             }
 
             db.writeLock();
