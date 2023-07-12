59f98d2c47d0cade541d05e6b8e702bc131310aa
Alex Zhu <zhuming9011@gmail.com>
2023-07-12 14:02:02 +0800

    [Enhancement] Refine trace log and delete txn log in background (#26986)
    - Refine publish version trace log
    - Move txn log deletion to background thread to reduce publish version
    task latency
    
    trace log format:
    ```
    I0711 19:08:21.350986 448396 lake_service.cpp:198] Published txn 5826. cost=46453us
    0711 19:08:21.304528 (+     0us) lake_service.cpp:142] got request. txn_id=5826 new_version=1093 #tablets=4
    Related trace 'PublishTablet':
    0711 19:08:21.304557 (+     0us) lake_service.cpp:155] start publish tablet 35405 at thread 463664
    0711 19:08:21.304572 (+    15us) tablet_manager.cpp:434] got cached tablet metadata
    0711 19:08:21.304611 (+    39us) tablet_manager.cpp:497] got cached txn log
    0711 19:08:21.329986 (+ 25375us) meta_file.cpp:159] end write delvel
    0711 19:08:21.350766 (+ 20780us) tablet_manager.cpp:406] end write tablet metadata
    0711 19:08:21.350957 (+   191us) lake_service.cpp:178] finished
    Related trace 'PublishTablet':
    0711 19:08:21.304559 (+     0us) lake_service.cpp:155] start publish tablet 35406 at thread 448068
    0711 19:08:21.304572 (+    13us) tablet_manager.cpp:434] got cached tablet metadata
    0711 19:08:21.304620 (+    48us) tablet_manager.cpp:497] got cached txn log
    0711 19:08:21.316021 (+ 11401us) meta_file.cpp:159] end write delvel
    0711 19:08:21.326712 (+ 10691us) tablet_manager.cpp:406] end write tablet metadata
    0711 19:08:21.326952 (+   240us) lake_service.cpp:178] finished
    Related trace 'PublishTablet':
    0711 19:08:21.304564 (+     0us) lake_service.cpp:155] start publish tablet 35407 at thread 463668
    0711 19:08:21.304579 (+    15us) tablet_manager.cpp:434] got cached tablet metadata
    0711 19:08:21.304635 (+    56us) tablet_manager.cpp:497] got cached txn log
    0711 19:08:21.318250 (+ 13615us) meta_file.cpp:159] end write delvel
    0711 19:08:21.327062 (+  8812us) tablet_manager.cpp:406] end write tablet metadata
    0711 19:08:21.327258 (+   196us) lake_service.cpp:178] finished
    Related trace 'PublishTablet':
    0711 19:08:21.304569 (+     0us) lake_service.cpp:155] start publish tablet 35408 at thread 463667
    0711 19:08:21.304580 (+    11us) tablet_manager.cpp:434] got cached tablet metadata
    0711 19:08:21.304627 (+    47us) tablet_manager.cpp:497] got cached txn log
    0711 19:08:21.317864 (+ 13237us) meta_file.cpp:159] end write delvel
    0711 19:08:21.326629 (+  8765us) tablet_manager.cpp:406] end write tablet metadata
    0711 19:08:21.326874 (+   245us) lake_service.cpp:178] finished
    Metrics: {"child_traces":[["PublishTablet",{"#deletes":0,"#new_del":16683,"#total_del":16683,"#upserts":1,"rowsetid":1092}],["PublishTablet",{"#deletes":0,"#new_del":16690,"#total_del":16690,"#upserts":1,"rowsetid":1092}],["PublishTablet",{"#deletes":0,"#new_del":16750,"#total_del":16750,"#upserts":1,"rowsetid":1092}],["PublishTablet",{"#deletes":0,"#new_del":16548,"#total_del":16548,"#upserts":1,"rowsetid":1092}]]}
    
    ```
    
    Signed-off-by: Alex Zhu <zhuming9011@gmail.com>

--- be/src/storage/lake/lake_primary_index.cpp
+++ be/src/storage/lake/lake_primary_index.cpp
@@ -27,13 +27,13 @@ static bvar::LatencyRecorder g_load_pk_index_latency("lake_load_pk_index");
 
 Status LakePrimaryIndex::lake_load(Tablet* tablet, const TabletMetadata& metadata, int64_t base_version,
                                    const MetaFileBuilder* builder) {
-    TRACE_COUNTER_SCOPE_LATENCY_US("load_pk_index");
     std::lock_guard<std::mutex> lg(_lock);
     if (_loaded) {
         return _status;
     }
     _status = _do_lake_load(tablet, metadata, base_version, builder);
     _loaded = true;
+    TRACE("end load pk index");
     if (!_status.ok()) {
         LOG(WARNING) << "load LakePrimaryIndex error: " << _status << " tablet:" << _tablet_id;
     }
