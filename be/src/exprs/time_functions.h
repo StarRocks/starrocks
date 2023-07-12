02af05bf4e8d64ccfa2980c25cca9cda6b2e5880
xuzifu666 <1206332514@qq.com>
2023-07-12 19:46:11 +0800

    comment change
    Signed-off-by: xuzifu666 <1206332514@qq.com>

--- be/src/exprs/time_functions.h
+++ be/src/exprs/time_functions.h
@@ -355,7 +355,7 @@ public:
     DEFINE_VECTORIZED_FN(time_diff);
 
     /**
-     * Calculate times from the first timestamp to the second timestamp. according to type return bigint in hour/minute/second/millisecond.
+     * Calculate times in multi type from the first timestamp to the second timestamp. according to type return bigint in hour/minute/second/millisecond.
      * @param context
      * @param columns [TimestampColumn] Columns that holds two groups timestamps for calculation.
      * @return  BigIntColumn Difference in times between the two timestamps. It can be negative.
