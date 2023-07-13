1b4ee96c8446156be561d45fd7ee75e25248c731
Smith Cruise <chendingchao1@126.com>
2023-07-13 13:06:06 +0800

    format
    Signed-off-by: Smith Cruise <chendingchao1@126.com>

--- be/test/formats/parquet/column_converter_test.cpp
+++ be/test/formats/parquet/column_converter_test.cpp
@@ -63,9 +63,6 @@ protected:
     static void check_chunk_values(std::shared_ptr<Chunk>& chunk, const std::string& expected_value) {
         chunk->check_or_die();
         size_t mid = chunk->num_rows() / 2;
-        if (mid == 0) {
-            mid = 1;
-        }
         for (size_t i = 0; i < chunk->num_rows(); i++) {
             if (i == mid) {
                 EXPECT_EQ("[NULL]", chunk->debug_row(i));
@@ -461,7 +458,7 @@ TEST_F(ColumnConverterTest, Int96Test) {
 TEST_F(ColumnConverterTest, Int96TimeZoneTest) {
     const std::string file_path =
             "./be/test/formats/parquet/test_data/column_converter/int96_timestamp_timezone.parquet";
-    const size_t expected_rows = 1;
+    const size_t expected_rows = 2;
 
     {
         const std::string col_name = "time_with_new_york";
