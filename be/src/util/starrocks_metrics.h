9239c2590a764ee73faeb8cee9d222b699ab4f18
ricky <rickif@qq.com>
2023-07-13 16:35:12 +0800

    [Feature] Table function table() supports HDFS (#26818)
    Fixes https://github.com/StarRocks/starrocks/issues/21159
    
    This PR adds the support of reading files from HDFS, including the
    following changes:
    
    1. execute RPC get_file_schema in pthread
    1. refactor RPC get_info/get_pulsar_info
    1. rename _async_thread_pool as _load_rpc_pool, which handles
    get_file_schema/get_info/get_pulsar_info
    1. remove be config internal_service_async_thread_num
    1. add metric load_rpc_threadpool_size
    
    Compared with the previous PR
    https://github.com/StarRocks/starrocks/pull/26133, this PR removes the
    `MonotonicStopWatch` in `get_info`/`get_pulsar_info`, which is
    destructed before used.
    
    
    ---------
    
    Signed-off-by: ricky <rickif@qq.com>

--- be/src/util/starrocks_metrics.h
+++ be/src/util/starrocks_metrics.h
@@ -255,6 +255,8 @@ public:
     METRIC_DEFINE_UINT_GAUGE(segment_flush_queue_count, MetricUnit::NOUNIT);
     METRIC_DEFINE_UINT_GAUGE(update_apply_queue_count, MetricUnit::NOUNIT);
 
+    METRIC_DEFINE_UINT_GAUGE(load_rpc_threadpool_size, MetricUnit::NOUNIT);
+
     static StarRocksMetrics* instance() {
         static StarRocksMetrics instance;
         return &instance;
