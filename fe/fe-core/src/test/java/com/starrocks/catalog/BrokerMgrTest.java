ed7675367b5eb24272cb717affed26211ea2318e
HangyuanLiu <460660596@qq.com>
2023-07-13 19:25:18 +0800

    [BugFix] Fix broker deserialized miss brokersMap bug (#27114)

--- fe/fe-core/src/test/java/com/starrocks/catalog/BrokerMgrTest.java
+++ fe/fe-core/src/test/java/com/starrocks/catalog/BrokerMgrTest.java
@@ -15,13 +15,59 @@
 
 package com.starrocks.catalog;
 
+import com.starrocks.common.DdlException;
+import com.starrocks.common.Pair;
+import com.starrocks.persist.EditLog;
+import com.starrocks.persist.gson.GsonUtils;
+import com.starrocks.qe.SessionVariable;
+import com.starrocks.server.GlobalStateMgr;
+import com.starrocks.utframe.UtFrameUtils;
+import mockit.Expectations;
+import mockit.Mocked;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class BrokerMgrTest {
-    
-    @Test    
+    @Mocked
+    private GlobalStateMgr globalStateMgr;
+    @Mocked
+    private EditLog editLog;
+
+    @Before
+    public void setUp() throws Exception {
+        UtFrameUtils.PseudoImage.setUpImageVersion();
+        new Expectations() {
+            {
+                globalStateMgr.getEditLog();
+                minTimes = 0;
+                result = editLog;
+
+                editLog.logGlobalVariable((SessionVariable) any);
+                minTimes = 0;
+            }
+        };
+    }
+
+    @Test
     public void testIPTitle() {
         Assert.assertTrue(BrokerMgr.BROKER_PROC_NODE_TITLE_NAMES.get(1).equals("IP"));
+    }
+
+    @Test
+    public void test() throws DdlException {
+        BrokerMgr brokerMgr = new BrokerMgr();
+        Collection<Pair<String, Integer>> addresses = new ArrayList<>();
+        Pair<String, Integer> pair = new Pair<String, Integer>("127.0.0.1", 8080);
+        addresses.add(pair);
+        brokerMgr.addBrokers("HDFS", addresses);
+
+        String json = GsonUtils.GSON.toJson(brokerMgr);
+
+        BrokerMgr replayBrokerMgr = GsonUtils.GSON.fromJson(json, BrokerMgr.class);
+        Assert.assertNotNull(replayBrokerMgr.getBroker("HDFS", "127.0.0.1", 8080));
     }
 }
