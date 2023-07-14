e4ffba20e18fd7a362c5ac705be72b99ffbbdb0c
ricky <rickif@qq.com>
2023-07-14 10:30:24 +0800

    [BugFix] date_trunc fmt in upper case (#27024)
    Fixes https://github.com/StarRocks/starrocks/issues/27022
    When the target table is created with `PARTITION BY date_trunc('YEAR',
    event_time)`, in which the fmt is in upper case, the insert would get
    failure. This PR fixes the issue.
    
    (cherry picked from commit b34c8952a15e807c74b07793b18915bfcdca4844)

--- be/src/exprs/time_functions.cpp
+++ be/src/exprs/time_functions.cpp
@@ -2070,6 +2070,10 @@ Status TimeFunctions::datetime_trunc_prepare(FunctionContext* context, FunctionC
     Slice slice = ColumnHelper::get_const_value<TYPE_VARCHAR>(column);
     auto format_value = slice.to_string();
 
+    // to lower case
+    std::transform(format_value.begin(), format_value.end(), format_value.begin(),
+                   [](unsigned char c) { return std::tolower(c); });
+
     ScalarFunction function;
     if (format_value == "second") {
         function = &TimeFunctions::datetime_trunc_second;
@@ -2185,6 +2189,10 @@ Status TimeFunctions::date_trunc_prepare(FunctionContext* context, FunctionConte
 
     Slice slice = ColumnHelper::get_const_value<TYPE_VARCHAR>(column);
     auto format_value = slice.to_string();
+
+    // to lower case
+    std::transform(format_value.begin(), format_value.end(), format_value.begin(),
+                   [](unsigned char c) { return std::tolower(c); });
 
     ScalarFunction function;
     if (format_value == "day") {
