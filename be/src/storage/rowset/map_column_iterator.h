96e4f1db8d85dc49f9a8ef35cf675d25f0855946
Seaven <seaven_7@qq.com>
2023-07-17 11:09:26 +0800

    up

--- be/src/storage/rowset/map_column_iterator.h
+++ be/src/storage/rowset/map_column_iterator.h
@@ -24,7 +24,7 @@ class MapColumnIterator final : public ColumnIterator {
 public:
     MapColumnIterator(ColumnReader* reader, std::unique_ptr<ColumnIterator> nulls,
                       std::unique_ptr<ColumnIterator> offsets, std::unique_ptr<ColumnIterator> keys,
-                      std::unique_ptr<ColumnIterator> values, ColumnAccessPath* path);
+                      std::unique_ptr<ColumnIterator> values, const ColumnAccessPath* path);
 
     ~MapColumnIterator() override = default;
 
