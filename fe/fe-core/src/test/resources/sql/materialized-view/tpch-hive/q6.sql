b98d409a603edfcd28c1a0bdc53956160586ae4c
Seaven <seaven_7@qq.com>
2023-07-17 15:45:37 +0800

    [BugFix] Decimal cast to string on FE (#27235)
    Fixes #27240
    
    Fix decimal cast to string will lose zero on FE, it's different with BE
    like:
    ```
    MySQL td>  insert into ds1 values (7, cast(7.00 as double), cast(7.0 as decimal(27, 9)));
    Query OK, 1 row affected
    Time: 0.269s
    MySQL td>  insert into ds1 select  8, cast(8.00 as double), cast(8.0 as decimal(27, 9));
    Query OK, 1 row affected
    Time: 0.048s
    MySQL td> select * from dd1; -- (int, double, decimal(27,9))
    +----+-----+-------------+
    | v1 | v2  | v3          |
    +----+-----+-------------+
    | 2  | 2.0 | 2.000000000 |
    | 1  | 1.0 | 1.000000000 |
    +----+-----+-------------+
    2 rows in set
    Time: 0.022s
    MySQL td> insert into ds1 select * from dd1;
    Query OK, 2 rows affected
    Time: 2.825s
    MySQL td>
    MySQL td> select * from ds1;
    +----+-----+-------------+
    | v1 | v2  | v3          |
    +----+-----+-------------+
    | 2  | 2   | 2.000000000 |
    | 1  | 1   | 1.000000000 |
    | 7  | 7.0 | 7.0         |
    | 8  | 8   | 8.000000000 |
    +----+-----+-------------+
    4 rows in set
    Time: 0.024s
    MySQL td>
    
    ```
    
    
    
    ## What type of PR is this:
    - [x] BugFix
    - [ ] Feature
    - [ ] Enhancement
    - [ ] Refactor
    - [ ] UT
    - [ ] Doc
    - [ ] Tool
    
    ## Checklist:
    - [x] I have added test cases for my bug fix or my new feature
    - [ ] This pr will affect users' behaviors
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    
    ## Bugfix cherry-pick branch check:
    - [x] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [x] 3.1
      - [x] 3.0
      - [x] 2.5
      - [ ] 2.4
    
    ---------
    
    Signed-off-by: Seaven <seaven_7@qq.com>

--- fe/fe-core/src/test/resources/sql/materialized-view/tpch-hive/q6.sql
+++ fe/fe-core/src/test/resources/sql/materialized-view/tpch-hive/q6.sql
@@ -12,6 +12,6 @@ where
 AGGREGATE ([GLOBAL] aggregate [{87: sum=sum(87: sum)}] group by [[]] having [null]
     EXCHANGE GATHER
         AGGREGATE ([LOCAL] aggregate [{87: sum=sum(81: revenue)}] group by [[]] having [null]
-            SCAN (mv[lineitem_agg_mv3] columns[78: l_shipdate, 79: l_discount, 80: l_quantity, 81: revenue] predicate[78: l_shipdate >= 1995-01-01 AND 78: l_shipdate < 1996-01-01 AND 79: l_discount >= 0.02 AND 79: l_discount <= 0.04 AND 80: l_quantity < 24])
+            SCAN (mv[lineitem_agg_mv3] columns[78: l_shipdate, 79: l_discount, 80: l_quantity, 81: revenue] predicate[78: l_shipdate >= 1995-01-01 AND 78: l_shipdate < 1996-01-01 AND 79: l_discount >= 0.02 AND 79: l_discount <= 0.04 AND 80: l_quantity < 24.0])
 [end]
 
--- fe/fe-core/src/test/resources/sql/materialized-view/tpch/q18.sql
+++ fe/fe-core/src/test/resources/sql/materialized-view/tpch/q18.sql
@@ -39,7 +39,7 @@ TOP-N (order by [[13: o_totalprice DESC NULLS LAST, 10: o_orderdate ASC NULLS FI
                 EXCHANGE SHUFFLE[9]
                     SCAN (mv[lineitem_mv] columns[90: c_name, 96: l_orderkey, 98: l_quantity, 105: o_custkey, 106: o_orderdate, 110: o_totalprice] predicate[null])
                 EXCHANGE SHUFFLE[35]
-                    AGGREGATE ([GLOBAL] aggregate [{163: sum=sum(163: sum)}] group by [[137: l_orderkey]] having [163: sum > 315]
+                    AGGREGATE ([GLOBAL] aggregate [{163: sum=sum(163: sum)}] group by [[137: l_orderkey]] having [163: sum > 315.0]
                         EXCHANGE SHUFFLE[137]
                             AGGREGATE ([LOCAL] aggregate [{163: sum=sum(141: sum_qty)}] group by [[137: l_orderkey]] having [null]
                                 SCAN (mv[lineitem_agg_mv1] columns[137: l_orderkey, 141: sum_qty] predicate[null])
