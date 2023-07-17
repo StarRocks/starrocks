StarRocks/starrocks

commit 00713c98963aacf229adcf85d54a863328cd9778

[Feature] Support runtime profile (#25131)
## Background

Sometimes, when a query is expected to take a considerable amount of
time to execute, such as 10 minutes, we are required to wait until its
completion before obtaining any information about the execution process.

Therefore, it is crucial to have a runtime profile available while the
query is still in progress.

## How to achieve

We continue to use the existing session variable `enable_profile` to
enable/disable this feature. Besides, we introduce another session
varaible `runtime_profile_report_interval`, which defines the time
interval between two consecutive profile reports, with a default value
of 10.

The implementation is quite straightforward:
* Each fragment instance report its execution state and profile with the
defined interval `runtime_profile_report_interval`
* Fe receive the state report, and update the final query content with
the defined interval `runtime_profile_report_interval`

## What type of PR is this:
- [ ] BugFix
- [x] Feature
- [ ] Enhancement
- [ ] Refactor
- [ ] UT
- [ ] Doc
- [ ] Tool

## Checklist:
- [ ] I have added test cases for my bug fix or my new feature
- [ ] This pr will affect users' behaviors
- [ ] This pr needs user documentation (for new or modified features or
behaviors)
  - [ ] I have added documentation for my new feature or new function

## Bugfix cherry-pick branch check:
- [x] I have checked the version labels which the pr will be
auto-backported to the target branch
  - [x] 3.1
  - [ ] 3.0
  - [ ] 2.5
  - [ ] 2.4

---------

Signed-off-by: liuyehcf <1559500551@qq.com>

be/src/exec/pipeline/pipeline_driver.cpp
@@ -416,6 +416,18 @@ void PipelineDriver::check_short_circuit() {
     }
 }
 
+bool PipelineDriver::need_report_exec_state() {
+    return _fragment_ctx->need_report_exec_state();
+}
+
+void PipelineDriver::report_exec_state() {
+    if (_state == DriverState::NOT_READY || _state == DriverState::FINISH || _state == DriverState::CANCELED ||
+        _state == DriverState::INTERNAL_ERROR) {
+        return;
+    }
+    _fragment_ctx->report_exec_state_if_necessary();
+}
+
 void PipelineDriver::mark_precondition_not_ready() {
     for (auto& op : _operators) {
         _operator_stages[op->get_id()] = OperatorStage::PRECONDITION_NOT_READY;

be/src/exec/pipeline/pipeline_driver.h
@@ -410,6 +410,9 @@ public:
     // Check whether an operator can be short-circuited, when is_precondition_block() becomes false from true.
     void check_short_circuit();
 
+    bool need_report_exec_state();
+    void report_exec_state();
+
     std::string to_readable_string() const;
 
     workgroup::WorkGroup* workgroup();

be/src/exec/pipeline/pipeline_driver_poller.cpp
@@ -139,7 +139,7 @@ void PipelineDriverPoller::run_internal() {
                 } else if (driver->is_finished()) {
                     remove_blocked_driver(_local_blocked_drivers, driver_it);
                     ready_drivers.emplace_back(driver);
-                } else if (driver->is_not_blocked()) {
+                } else if (driver->is_not_blocked() || driver->need_report_exec_state()) {
                     driver->set_driver_state(DriverState::READY);
                     remove_blocked_driver(_local_blocked_drivers, driver_it);
                     ready_drivers.emplace_back(driver);

be/src/exec/pipeline/adaptive/lazy_instantiate_drivers_operator.cpp
@@ -111,7 +111,7 @@ void LazyInstantiateDriversOperator::close(RuntimeState* state) {
         for (auto& pipeline : pipeline_group.pipelines) {
             auto sink_factory = pipeline->sink_operator_factory();
             if (typeid(*sink_factory) != typeid(ResultSinkOperatorFactory)) {
-                fragment_ctx->count_down_pipeline(state);
+                fragment_ctx->count_down_pipeline();
             } else {
                 // Closing ResultSinkOperator notifies FE not to wait fetch_data anymore.
                 pipeline->source_operator_factory()->set_degree_of_parallelism(1);

be/src/exec/pipeline/exec_state_reporter.cpp
@@ -42,11 +42,11 @@ std::string to_http_path(const std::string& token, const std::string& file_name)
 
 TReportExecStatusParams ExecStateReporter::create_report_exec_status_params(QueryContext* query_ctx,
                                                                             FragmentContext* fragment_ctx,
+                                                                            RuntimeProfile* profile,
                                                                             const Status& status, bool done) {
     TReportExecStatusParams params;
     auto* runtime_state = fragment_ctx->runtime_state();
     DCHECK(runtime_state != nullptr);
-    auto* profile = runtime_state->runtime_profile();
     DCHECK(profile != nullptr);
     auto* exec_env = fragment_ctx->runtime_state()->exec_env();
     DCHECK(exec_env != nullptr);
@@ -66,7 +66,7 @@ TReportExecStatusParams ExecStateReporter::create_report_exec_status_params(Quer
             runtime_state->update_report_load_status(&params);
             params.__set_load_type(runtime_state->query_options().load_job_type);
         }
-        if (query_ctx->is_report_profile()) {
+        if (query_ctx->enable_profile()) {
             profile->to_thrift(&params.profile);
             params.__isset.profile = true;
         }

be/src/exec/pipeline/exec_state_reporter.h
@@ -32,7 +32,8 @@ public:
     ExecStateReporter();
 
     static TReportExecStatusParams create_report_exec_status_params(QueryContext* query_ctx,
-                                                                    FragmentContext* fragment_ctx, const Status& status,
+                                                                    FragmentContext* fragment_ctx,
+                                                                    RuntimeProfile* profile, const Status& status,
                                                                     bool done);
     static Status report_exec_status(const TReportExecStatusParams& params, ExecEnv* exec_env,
                                      const TNetworkAddress& fe_addr);

be/src/exec/pipeline/fragment_context.cpp
@@ -22,6 +22,7 @@
 #include "runtime/exec_env.h"
 #include "runtime/stream_load/stream_load_context.h"
 #include "runtime/stream_load/transaction_mgr.h"
+#include "util/time.h"
 
 namespace starrocks::pipeline {
 
@@ -80,12 +81,13 @@ void FragmentContext::set_data_sink(std::unique_ptr<DataSink> data_sink) {
     _data_sink = std::move(data_sink);
 }
 
-void FragmentContext::count_down_pipeline(RuntimeState* state, size_t val) {
+void FragmentContext::count_down_pipeline(size_t val) {
     bool all_pipelines_finished = _num_finished_pipelines.fetch_add(val) + val == _pipelines.size();
     if (!all_pipelines_finished) {
         return;
     }
 
+    auto* state = runtime_state();
     auto* query_ctx = state->query_ctx();
 
     state->runtime_profile()->reverse_childs();
@@ -99,11 +101,38 @@ void FragmentContext::count_down_pipeline(RuntimeState* state, size_t val) {
 
     finish();
     auto status = final_status();
-    state->exec_env()->wg_driver_executor()->report_exec_state(query_ctx, this, status, true);
+    state->exec_env()->wg_driver_executor()->report_exec_state(query_ctx, this, status, true, true);
 
     destroy_pass_through_chunk_buffer();
 
     query_ctx->count_down_fragments();
+}
+
+bool FragmentContext::need_report_exec_state() {
+    auto* state = runtime_state();
+    auto* query_ctx = state->query_ctx();
+    if (!query_ctx->enable_profile()) {
+        return false;
+    }
+
+    return (MonotonicNanos() - _last_report_exec_state_ns) >= query_ctx->get_runtime_profile_report_interval_ns();
+}
+
+void FragmentContext::report_exec_state_if_necessary() {
+    auto* state = runtime_state();
+    auto* query_ctx = state->query_ctx();
+    if (!query_ctx->enable_profile()) {
+        return;
+    }
+    auto now = MonotonicNanos();
+    auto last_report_exec_state_ns = _last_report_exec_state_ns.load();
+    if (now - last_report_exec_state_ns < query_ctx->get_runtime_profile_report_interval_ns()) {
+        return;
+    }
+    if (_last_report_exec_state_ns.compare_exchange_strong(last_report_exec_state_ns, now)) {
+        LOG(ERROR) << "report runtime profile, query_id=" << print_id(_query_id);
+        state->exec_env()->wg_driver_executor()->report_exec_state(query_ctx, this, Status::OK(), false, true);
+    }
 }
 
 void FragmentContext::set_final_status(const Status& status) {

be/src/exec/pipeline/fragment_context.h
@@ -75,7 +75,10 @@ public:
     size_t num_drivers() const;
 
     bool all_pipelines_finished() const { return _num_finished_pipelines == _pipelines.size(); }
-    void count_down_pipeline(RuntimeState* state, size_t val = 1);
+    void count_down_pipeline(size_t val = 1);
+
+    bool need_report_exec_state();
+    void report_exec_state_if_necessary();
 
     void set_final_status(const Status& status);
 
@@ -190,6 +193,8 @@ private:
     AdaptiveDopParam _adaptive_dop_param;
 
     size_t _expired_log_count = 0;
+
+    std::atomic<int64_t> _last_report_exec_state_ns = MonotonicNanos();
 };
 
 class FragmentContextManager {

commit 081a3a2bab4dcd13bcf4d5c705146496d0cbcc25

[BugFix] Remove force scheduling logic for runtime profile report (#27194)
This issue is introduced by
https://github.com/StarRocks/starrocks/pull/25131, which allows the
drivers to be re-scheduled even if it's not actual ready in order to
trigger the report exec state process.

But if the dirver is waiting it's dependency to be finished, it may fail
when calling the has_output or other status checking interface.

And this pr is to remove the force scheduling mechanism.

## What type of PR is this:
- [x] BugFix
- [ ] Feature
- [ ] Enhancement
- [ ] Refactor
- [ ] UT
- [ ] Doc
- [ ] Tool

## Checklist:
- [ ] I have added test cases for my bug fix or my new feature
- [ ] This pr will affect users' behaviors
- [ ] This pr needs user documentation (for new or modified features or
behaviors)
  - [ ] I have added documentation for my new feature or new function

## Bugfix cherry-pick branch check:
- [x] I have checked the version labels which the pr will be
auto-backported to the target branch
  - [ ] 3.1
  - [ ] 3.0
  - [ ] 2.5
  - [ ] 2.4

Signed-off-by: liuyehcf <1559500551@qq.com>

be/src/exec/pipeline/pipeline_driver.cpp
@@ -431,10 +431,6 @@ void PipelineDriver::check_short_circuit() {
     }
 }
 
-bool PipelineDriver::need_report_exec_state() {
-    return _fragment_ctx->need_report_exec_state();
-}
-
 void PipelineDriver::report_exec_state_if_necessary() {
     if (_state == DriverState::NOT_READY || _state == DriverState::FINISH || _state == DriverState::CANCELED ||
         _state == DriverState::INTERNAL_ERROR) {

be/src/exec/pipeline/pipeline_driver.h
@@ -411,7 +411,6 @@ public:
     // Check whether an operator can be short-circuited, when is_precondition_block() becomes false from true.
     void check_short_circuit();
 
-    bool need_report_exec_state();
     void report_exec_state_if_necessary();
     void runtime_report_action();
 

be/src/exec/pipeline/pipeline_driver_poller.cpp
@@ -139,7 +139,7 @@ void PipelineDriverPoller::run_internal() {
                 } else if (driver->is_finished()) {
                     remove_blocked_driver(_local_blocked_drivers, driver_it);
                     ready_drivers.emplace_back(driver);
-                } else if (driver->is_not_blocked() || driver->need_report_exec_state()) {
+                } else if (driver->is_not_blocked()) {
                     driver->set_driver_state(DriverState::READY);
                     remove_blocked_driver(_local_blocked_drivers, driver_it);
                     ready_drivers.emplace_back(driver);