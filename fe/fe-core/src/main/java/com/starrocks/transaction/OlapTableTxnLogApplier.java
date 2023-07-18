e13c19b988121bbc2db904a618509e82767f3053
stdpain <34912776+stdpain@users.noreply.github.com>
2023-07-18 09:01:52 +0800

    [BugFix] Fix FE checkstyle error (#27394)

--- fe/fe-core/src/main/java/com/starrocks/transaction/OlapTableTxnLogApplier.java
+++ fe/fe-core/src/main/java/com/starrocks/transaction/OlapTableTxnLogApplier.java
@@ -14,7 +14,6 @@
 
 package com.starrocks.transaction;
 
-import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.starrocks.catalog.Database;
 import com.starrocks.catalog.LocalTablet;
