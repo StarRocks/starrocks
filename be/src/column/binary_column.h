f3cbdf71df6272498be097d15a7cfeadf573786a
Zhuhe Fang <fzhedu@gmail.com>
2023-07-18 18:57:55 +0800

    [Refactor] support compile c++20 with gcc11/clang15/16 (#27454)
    Signed-off-by: Zhuhe Fang <fzhedu@gmail.com>

--- be/src/column/binary_column.h
+++ be/src/column/binary_column.h
@@ -50,20 +50,20 @@ public:
 
     // TODO(kks): when we create our own vector, we could let vector[-1] = 0,
     // and then we don't need explicitly emplace_back zero value
-    BinaryColumnBase<T>() { _offsets.emplace_back(0); }
+    BinaryColumnBase() { _offsets.emplace_back(0); }
     // Default value is empty string
-    explicit BinaryColumnBase<T>(size_t size) : _offsets(size + 1, 0) {}
-    BinaryColumnBase<T>(Bytes bytes, Offsets offsets) : _bytes(std::move(bytes)), _offsets(std::move(offsets)) {
+    explicit BinaryColumnBase(size_t size) : _offsets(size + 1, 0) {}
+    BinaryColumnBase(Bytes bytes, Offsets offsets) : _bytes(std::move(bytes)), _offsets(std::move(offsets)) {
         if (_offsets.empty()) {
             _offsets.emplace_back(0);
         }
     }
 
     // NOTE: do *NOT* copy |_slices|
-    BinaryColumnBase<T>(const BinaryColumnBase<T>& rhs) : _bytes(rhs._bytes), _offsets(rhs._offsets) {}
+    BinaryColumnBase(const BinaryColumnBase<T>& rhs) : _bytes(rhs._bytes), _offsets(rhs._offsets) {}
 
     // NOTE: do *NOT* copy |_slices|
-    BinaryColumnBase<T>(BinaryColumnBase<T>&& rhs) noexcept
+    BinaryColumnBase(BinaryColumnBase<T>&& rhs) noexcept
             : _bytes(std::move(rhs._bytes)), _offsets(std::move(rhs._offsets)) {}
 
     BinaryColumnBase<T>& operator=(const BinaryColumnBase<T>& rhs) {
@@ -84,7 +84,7 @@ public:
 
     bool has_large_column() const override;
 
-    ~BinaryColumnBase<T>() override {
+    ~BinaryColumnBase() override {
         if (!_offsets.empty()) {
             DCHECK_EQ(_bytes.size(), _offsets.back());
         } else {
