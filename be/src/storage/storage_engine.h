b44f06a99b0a6b261c5fe29b990f92ba7822e4ce
trueeyu <lxhhust350@qq.com>
2023-07-18 17:35:25 +0800

    [Refactor] Remove heartbeat flags from StorageEngine (#27448)
    Fixes #27417
    
    Graceful startup and exit step 1: Remove heartbeat flags from
    StorageEngine
    
    `_heartbeat_flags` in storage engine is not used now.
    
    Signed-off-by: trueeyu <lxhhust350@qq.com>
    (cherry picked from commit 1788827de7335b8ee8b4616b9bd1edde3309a274)
    
    # Conflicts:
    #	be/src/storage/storage_engine.h

--- be/src/storage/storage_engine.h
+++ be/src/storage/storage_engine.h
@@ -85,9 +85,9 @@ struct DeltaColumnGroupKey {
     RowsetId rowsetid;
     uint32_t segment_id;
 
-    DeltaColumnGroupKey() {}
+    DeltaColumnGroupKey() = default;
     DeltaColumnGroupKey(int64_t tid, RowsetId rid, uint32_t sid) : tablet_id(tid), rowsetid(rid), segment_id(sid) {}
-    ~DeltaColumnGroupKey() {}
+    ~DeltaColumnGroupKey() = default;
 
     bool operator==(const DeltaColumnGroupKey& rhs) const {
         return tablet_id == rhs.tablet_id && segment_id == rhs.segment_id && rowsetid == rhs.rowsetid;
@@ -127,6 +127,9 @@ class StorageEngine {
 public:
     StorageEngine(const EngineOptions& options);
     virtual ~StorageEngine();
+
+    StorageEngine(const StorageEngine&) = delete;
+    const StorageEngine& operator=(const StorageEngine&) = delete;
 
     static Status open(const EngineOptions& options, StorageEngine** engine_ptr);
 
@@ -238,8 +241,6 @@ public:
     bool rowset_id_in_use(const RowsetId& rowset_id) { return _rowset_id_generator->id_in_use(rowset_id); }
 
     void release_rowset_id(const RowsetId& rowset_id) { return _rowset_id_generator->release_id(rowset_id); }
-
-    void set_heartbeat_flags(HeartbeatFlags* heartbeat_flags) { _heartbeat_flags = heartbeat_flags; }
 
     // start all backgroud threads. This should be call after env is ready.
     virtual Status start_bg_threads();
@@ -450,8 +451,6 @@ private:
     bool _need_report_tablet = false;
     bool _need_report_disk_stat = false;
 
-    std::mutex _engine_task_mutex;
-
     std::unique_ptr<TabletManager> _tablet_manager;
     std::unique_ptr<TxnManager> _txn_manager;
 
@@ -471,10 +470,13 @@ private:
 
     std::unique_ptr<CompactionManager> _compaction_manager;
 
+<<<<<<< HEAD
     std::unique_ptr<PublishVersionManager> _publish_version_manager;
 
     HeartbeatFlags* _heartbeat_flags = nullptr;
 
+=======
+>>>>>>> 1788827de7 ([Refactor] Remove heartbeat flags from StorageEngine (#27448))
     std::unordered_map<int64_t, std::shared_ptr<AutoIncrementMeta>> _auto_increment_meta_map;
 
     std::mutex _auto_increment_mutex;
@@ -486,9 +488,6 @@ private:
     std::mutex _delta_column_group_cache_lock;
     std::map<DeltaColumnGroupKey, DeltaColumnGroupList> _delta_column_group_cache;
     std::unique_ptr<MemTracker> _delta_column_group_cache_mem_tracker;
-
-    StorageEngine(const StorageEngine&) = delete;
-    const StorageEngine& operator=(const StorageEngine&) = delete;
 };
 
 /// Load min_garbage_sweep_interval and max_garbage_sweep_interval from config,
