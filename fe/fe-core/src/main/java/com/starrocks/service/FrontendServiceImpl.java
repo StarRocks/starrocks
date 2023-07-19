8c4187d5f2cde8ea4011805c0217000c869fd690
miomiocat <284487410@qq.com>
2023-07-19 21:22:23 +0800

    [BugFix] Fix the wrong IS_NULLABLE result of system table (#27573)
    Fixes #27572
    
    Signed-off-by: miomiocat <284487410@qq.com>
    (cherry picked from commit b4be96ad36b3f2c7851b841accde314a1222cbb8)

--- fe/fe-core/src/main/java/com/starrocks/service/FrontendServiceImpl.java
+++ fe/fe-core/src/main/java/com/starrocks/service/FrontendServiceImpl.java
@@ -905,6 +905,7 @@ public class FrontendServiceImpl implements FrontendService.Iface {
             if (decimalDigits != null) {
                 desc.setColumnScale(decimalDigits);
             }
+            desc.setAllowNull(column.isAllowNull());
             if (column.isKey()) {
                 // COLUMN_KEY (UNI, AGG, DUP, PRI)
                 desc.setColumnKey(tableKeysType);
