d9fdb47fb23d4ce69efc18440b617c9b3a056d59
iverson <58965658+wanpengfei-git@users.noreply.github.com>
2023-07-19 15:14:05 +0800

    Revert "[BugFix] Set ColocateGroupSchema bucketNum to the calculated num" (#27546)
    Reverts StarRocks/starrocks#27086

--- fe/fe-core/src/test/java/com/starrocks/analysis/CreateTableAutoTabletTest.java
+++ fe/fe-core/src/test/java/com/starrocks/analysis/CreateTableAutoTabletTest.java
@@ -14,8 +14,6 @@
 
 package com.starrocks.analysis;
 
-import com.starrocks.catalog.ColocateGroupSchema;
-import com.starrocks.catalog.ColocateTableIndex;
 import com.starrocks.catalog.Database;
 import com.starrocks.catalog.OlapTable;
 import com.starrocks.catalog.Partition;
@@ -254,12 +252,6 @@ public class CreateTableAutoTabletTest {
             db.readUnlock();
         }
         Assert.assertEquals(bucketNum, 10);
-
-        Long dbId = db.getId();
-        String fullGroupName = dbId + "_g1";
-        ColocateTableIndex index = GlobalStateMgr.getCurrentColocateIndex();
-        ColocateGroupSchema groupSchema = index.getGroupSchema(fullGroupName);
-        Assert.assertEquals(groupSchema.getBucketsNum(), 10);
     }
 
 
