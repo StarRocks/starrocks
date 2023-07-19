78fea29073611014f876a6c4a50b9925a3e0fa60
The85thLetter <m.brian0606@gmail.com>
2023-07-18 23:09:25 -0700

    Update AirByte Connector
    Updated AirByte Connector, with TLS support
    
    Signed-off-by: The85thLetter <m.brian0606@gmail.com>

--- contrib/destination-starrocks/src/main/java/io/airbyte/integrations/destination/starrocks/stream/StreamLoadUtils.java
+++ contrib/destination-starrocks/src/main/java/io/airbyte/integrations/destination/starrocks/stream/StreamLoadUtils.java
@@ -1,16 +1,20 @@
-// Copyright 2021-present StarRocks, Inc. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package io.airbyte.integrations.destination.starrocks.stream;
 
@@ -21,6 +25,7 @@ import org.apache.http.impl.client.DefaultRedirectStrategy;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicHeader;
+import io.airbyte.integrations.destination.starrocks.StarRocksConstants;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
@@ -78,6 +83,8 @@ public class StreamLoadUtils {
         headers.put("timeout", "600");
         headers.put(HttpHeaders.AUTHORIZATION, StreamLoadUtils.getBasicAuthHeader(user, pwd));
         headers.put(HttpHeaders.EXPECT, "100-continue");
+        headers.put("column_separator", StarRocksConstants.CsvFormat.COLUMN_DELIMITER);
+        headers.put("row_delimiter", StarRocksConstants.CsvFormat.LINE_DELIMITER);
         return headers.entrySet().stream()
                 .map(entry -> new BasicHeader(entry.getKey(), entry.getValue()))
                 .toArray(Header[]::new);
