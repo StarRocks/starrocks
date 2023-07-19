7a6b15b94651f38d64b8a955f5cc36072b941982
Zijie Lu <wslzj40@gmail.com>
2023-07-19 20:49:48 +0800

    [BugFix] Fix binding empty storage volume id to db (#27562)
    Signed-off-by: Zijie Lu <wslzj40@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
+++ fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
@@ -384,7 +384,6 @@ public class LocalMetastore implements ConnectorMetadata {
             } else {
                 id = getNextId();
                 Database db = new Database(id, dbName);
-                String storageVolumeId = "";
                 String volume = StorageVolumeMgr.DEFAULT;
                 if (properties != null && properties.containsKey(PropertyAnalyzer.PROPERTIES_STORAGE_VOLUME)) {
                     volume = properties.remove(PropertyAnalyzer.PROPERTIES_STORAGE_VOLUME);
@@ -393,6 +392,7 @@ public class LocalMetastore implements ConnectorMetadata {
                     throw new DdlException(String.format("Storage volume %s not exists", volume));
                 }
                 unprotectCreateDb(db);
+                String storageVolumeId = GlobalStateMgr.getCurrentState().getStorageVolumeMgr().getStorageVolumeIdOfDb(id);
                 GlobalStateMgr.getCurrentState().getEditLog().logCreateDb(db, storageVolumeId);
             }
         } finally {
