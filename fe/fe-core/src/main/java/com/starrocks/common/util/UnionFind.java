9d9aaa4dec38f47c61d282a9afbb335ac9c8df83
satanson <ranpanf@gmail.com>
2023-07-20 14:17:54 +0800

    [Refactor] Deduplicate UnionFind utilities (#27611)
    So many UnionFild utilities in code base, so unify it into one
    
    Signed-off-by: satanson <ranpanf@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/common/util/UnionFind.java
+++ fe/fe-core/src/main/java/com/starrocks/common/util/UnionFind.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.starrocks.sql.optimizer;
+package com.starrocks.common.util;
 
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
