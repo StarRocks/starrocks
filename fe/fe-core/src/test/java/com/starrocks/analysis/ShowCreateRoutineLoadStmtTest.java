d31245407d8d73b213c7ff080d4239a488468085
Zaorang Yang <zaorangy@gmail.com>
2023-07-20 19:32:12 +0800

    Add test case
    Signed-off-by: Zaorang Yang <zaorangy@gmail.com>

---
+++ fe/fe-core/src/test/java/com/starrocks/analysis/ShowCreateRoutineLoadStmtTest.java
@@ -0,0 +1,59 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+package com.starrocks.analysis;
+
+import com.starrocks.qe.ConnectContext;
+import com.starrocks.sql.ast.ShowCreateRoutineLoadStmt;
+import com.starrocks.sql.ast.ShowRoutineLoadStmt;
+import com.starrocks.sql.ast.StatementBase;
+import com.starrocks.utframe.UtFrameUtils;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ShowCreateRoutineLoadStmtTest {
+    private static ConnectContext ctx;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        // create connect context
+        ctx = UtFrameUtils.createDefaultCtx();
+    }
+
+    @Test
+    public void testNormal() throws Exception {
+        ctx = UtFrameUtils.createDefaultCtx();
+        ctx.setDatabase("testDb");
+        ShowCreateRoutineLoadStmt stmt = new ShowCreateRoutineLoadStmt(new LabelName("testDb", "testJob"));
+        com.starrocks.sql.analyzer.Analyzer.analyze(stmt, ctx);
+        Assert.assertEquals("testJob", stmt.getName());
+        Assert.assertEquals("testDb", stmt.getDbFullName());
+        Assert.assertEquals(2, stmt.getMetaData().getColumnCount());
+        Assert.assertEquals("Job", stmt.getMetaData().getColumn(0).getName());
+        Assert.assertEquals("Create Job", stmt.getMetaData().getColumn(1).getName());
+    }
+
+    @Test
+    public void testBackquote() throws SecurityException, IllegalArgumentException {
+        String sql = "SHOW CREATE ROUTINE LOAD FOR `rl_test`";
+        List<StatementBase> stmts = com.starrocks.sql.parser.SqlParser.parse(sql, ctx.getSessionVariable());
+
+        ShowCreateRoutineLoadStmt stmt = (ShowCreateRoutineLoadStmt) stmts.get(0);
+        Assert.assertEquals("rl_test", stmt.getName());
+    }
+}
