StarRocks/starrocks

commit 2bb0d6b0a834ab561ab25410661f804135c06884

[BugFix] Add invalid tablet meta cleaner (#26509)
Fixes SR-18438, SR-18724

Signed-off-by: Dejun Xia <xiadejun@starrocks.com>

fe/fe-core/src/main/java/com/starrocks/catalog/TabletMeta.java
@@ -34,29 +34,24 @@
 
 package com.starrocks.catalog;
 
-import com.google.common.base.Preconditions;
 import com.starrocks.thrift.TStorageMedium;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class TabletMeta {
-    private static final Logger LOG = LogManager.getLogger(TabletMeta.class);
-
     private final long dbId;
     private final long tableId;
     private final long partitionId;
     private final long indexId;
 
-    private int oldSchemaHash;
-    private int newSchemaHash;
+    private final int oldSchemaHash;
+    private final int newSchemaHash;
 
     private TStorageMedium storageMedium;
 
-    private boolean isLakeTablet = false;
+    private final boolean isLakeTablet;
 
-    private ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 
     public TabletMeta(long dbId, long tableId, long partitionId, long indexId, int schemaHash,
                       TStorageMedium storageMedium, boolean isLakeTablet) {
@@ -108,40 +103,6 @@ public class TabletMeta {
             return this.newSchemaHash;
         } finally {
             lock.readLock().unlock();
-        }
-    }
-
-    public void setNewSchemaHash(int newSchemaHash) {
-        lock.writeLock().lock();
-        try {
-            Preconditions.checkState(this.newSchemaHash == -1);
-            this.newSchemaHash = newSchemaHash;
-            LOG.debug("setNewSchemaHash: {}", toString());
-        } finally {
-            lock.writeLock().unlock();
-        }
-    }
-
-    public void updateToNewSchemaHash() {
-        lock.writeLock().lock();
-        try {
-            Preconditions.checkState(this.newSchemaHash != -1);
-            int tmp = this.oldSchemaHash;
-            this.oldSchemaHash = this.newSchemaHash;
-            this.newSchemaHash = tmp;
-            LOG.debug("updateToNewSchemaHash: " + toString());
-        } finally {
-            lock.writeLock().unlock();
-        }
-    }
-
-    public void deleteNewSchemaHash() {
-        lock.writeLock().lock();
-        try {
-            LOG.debug("deleteNewSchemaHash: " + toString());
-            this.newSchemaHash = -1;
-        } finally {
-            lock.writeLock().unlock();
         }
     }
 

fe/fe-core/src/main/java/com/starrocks/common/Config.java
@@ -1246,18 +1246,6 @@ public class Config extends ConfigBase {
     public static long tablet_sched_max_not_being_scheduled_interval_ms = 15 * 60 * 1000;
 
     /**
-     * enable replicated storage as default table engine
-     */
-    @ConfField(mutable = true)
-    public static boolean enable_replicated_storage_as_default_engine = true;
-
-    /**
-     * enable schedule insert query by row count
-     */
-    @ConfField(mutable = true)
-    public static boolean enable_schedule_insert_query_by_row_count = true;
-
-    /**
      * FOR DiskAndTabletLoadBalancer:
      * upper limit of the difference in disk usage of all backends, exceeding this threshold will cause
      * disk balance
@@ -1321,6 +1309,17 @@ public class Config extends ConfigBase {
     @ConfField
     public static boolean enable_metric_calculator = true;
 
+    /**
+     * enable replicated storage as default table engine
+     */
+    @ConfField(mutable = true)
+    public static boolean enable_replicated_storage_as_default_engine = true;
+
+    /**
+     * enable schedule insert query by row count
+     */
+    @ConfField(mutable = true)
+    public static boolean enable_schedule_insert_query_by_row_count = true;
 
     /**
      * the max concurrent routine load task num of a single routine load job

fe/fe-core/src/main/java/com/starrocks/consistency/ConsistencyChecker.java
@@ -70,11 +70,13 @@ public class ConsistencyChecker extends FrontendDaemon {
 
     private static final int MAX_JOB_NUM = 100;
 
+    private static final long DEFAULT_TABLET_META_CHECK_INTERVAL_MS = 2 * 3600 * 1000L; // every 2 hours
+
     private static final Comparator<MetaObject> COMPARATOR =
             (first, second) -> Long.signum(first.getLastCheckTime() - second.getLastCheckTime());
 
     // tabletId -> job
-    private Map<Long, CheckConsistencyJob> jobs;
+    private final Map<Long, CheckConsistencyJob> jobs;
 
     /*
      * ATTN:
@@ -83,12 +85,13 @@ public class ConsistencyChecker extends FrontendDaemon {
      *       CheckConsistencyJob's synchronized
      *       db lock
      *
-     * if reversal is inevitable. use db.tryLock() instead to avoid dead lock
+     * if reversal is inevitable. use db.tryLock() instead to avoid deadlock
      */
-    private ReentrantReadWriteLock jobsLock;
+    private final ReentrantReadWriteLock jobsLock;
 
     private int startTime;
     private int endTime;
+    private long lastTabletMetaCheckTime = 0;
 
     public ConsistencyChecker() {
         super("consistency checker");
@@ -120,8 +123,17 @@ public class ConsistencyChecker extends FrontendDaemon {
         return true;
     }
 
+    private void checkTabletMetaConsistency() {
+        GlobalStateMgr.getCurrentState().getTabletInvertedIndex().checkTabletMetaConsistency();
+    }
+
     @Override
     protected void runAfterCatalogReady() {
+        if (System.currentTimeMillis() - lastTabletMetaCheckTime > DEFAULT_TABLET_META_CHECK_INTERVAL_MS) {
+            checkTabletMetaConsistency();
+            lastTabletMetaCheckTime = System.currentTimeMillis();
+        }
+
         // for each round. try chose enough new tablets to check
         // only add new job when it's work time
         if (itsTime() && getJobNum() == 0) {
@@ -177,20 +189,12 @@ public class ConsistencyChecker extends FrontendDaemon {
         calendar.setTimeInMillis(System.currentTimeMillis());
         int currentTime = calendar.get(Calendar.HOUR_OF_DAY);
 
-        boolean isTime = false;
+        boolean isTime;
         if (startTime < endTime) {
-            if (currentTime >= startTime && currentTime <= endTime) {
-                isTime = true;
-            } else {
-                isTime = false;
-            }
+            isTime = currentTime >= startTime && currentTime <= endTime;
         } else {
             // startTime > endTime (across the day)
-            if (currentTime >= startTime || currentTime <= endTime) {
-                isTime = true;
-            } else {
-                isTime = false;
-            }
+            isTime = currentTime >= startTime || currentTime <= endTime;
         }
 
         if (!isTime) {
@@ -240,13 +244,13 @@ public class ConsistencyChecker extends FrontendDaemon {
     }
 
     /**
-     * choose a tablet to check it's consistency
+     * choose a tablet to check whether it's consistent
      * we use a priority queue to sort db/table/partition/index/tablet by 'lastCheckTime'.
      * chose a tablet which has the smallest 'lastCheckTime'.
      */
     protected List<Long> chooseTablets() {
         GlobalStateMgr globalStateMgr = GlobalStateMgr.getCurrentState();
-        MetaObject chosenOne = null;
+        MetaObject chosenOne;
 
         List<Long> chosenTablets = Lists.newArrayList();
 
@@ -255,7 +259,7 @@ public class ConsistencyChecker extends FrontendDaemon {
         if (dbIds.isEmpty()) {
             return chosenTablets;
         }
-        Queue<MetaObject> dbQueue = new PriorityQueue<>(Math.max(dbIds.size(), 1), COMPARATOR);
+        Queue<MetaObject> dbQueue = new PriorityQueue<>(dbIds.size(), COMPARATOR);
         for (Long dbId : dbIds) {
             if (dbId == 0L) {
                 // skip 'information_schema' database
@@ -315,11 +319,11 @@ public class ConsistencyChecker extends FrontendDaemon {
                             Partition partition = (Partition) chosenOne;
 
                             // sort materializedIndices
-                            List<MaterializedIndex> visibleIndexs =
+                            List<MaterializedIndex> visibleIndexes =
                                     partition.getMaterializedIndices(IndexExtState.VISIBLE);
                             Queue<MetaObject> indexQueue =
-                                    new PriorityQueue<>(Math.max(visibleIndexs.size(), 1), COMPARATOR);
-                            indexQueue.addAll(visibleIndexs);
+                                    new PriorityQueue<>(Math.max(visibleIndexes.size(), 1), COMPARATOR);
+                            indexQueue.addAll(visibleIndexes);
 
                             while ((chosenOne = indexQueue.poll()) != null) {
                                 MaterializedIndex index = (MaterializedIndex) chosenOne;
@@ -341,7 +345,7 @@ public class ConsistencyChecker extends FrontendDaemon {
                                     if (partition.getVisibleVersion() == tablet.getCheckedVersion()) {
                                         if (tablet.isConsistent()) {
                                             LOG.debug("tablet[{}]'s version[{}-{}] has been checked. ignore",
-                                                    chosenTabletId, tablet.getCheckedVersion());
+                                                    chosenTabletId, tablet.getCheckedVersion(), partition.getVisibleVersion());
                                         }
                                     } else {
                                         LOG.info("chose tablet[{}-{}-{}-{}-{}] to check consistency", db.getId(),

fe/fe-core/src/main/java/com/starrocks/alter/SystemHandler.java
@@ -75,22 +75,33 @@ import java.util.stream.Collectors;
 
 /*
  * SystemHandler is for
- * 1. add/drop/decommisson backends
+ * 1. add/drop/decommission backends
  * 2. add/drop frontends
  * 3. add/drop/modify brokers
  */
 public class SystemHandler extends AlterHandler {
     private static final Logger LOG = LogManager.getLogger(SystemHandler.class);
 
+    private static final long MAX_REMAINED_TABLET_TO_CHECK_ON_DECOMM = 1000;
+
     public SystemHandler() {
         super("cluster");
     }
-
 
     @Override
     protected void runAfterCatalogReady() {
         super.runAfterCatalogReady();
         runAlterJobV2();
+    }
+
+    private void dropDecommissionedBackend(SystemInfoService systemInfoService, long beId) {
+        try {
+            systemInfoService.dropBackend(beId);
+            LOG.info("no tablet on decommission backend {}, drop it", beId);
+        } catch (DdlException e) {
+            // does not matter, maybe backend not exists
+            LOG.info("backend {} drop failed after decommission {}", beId, e.getMessage());
+        }
     }
 
     // check all decommissioned backends, if there is no tablet on that backend, drop it.
@@ -105,19 +116,15 @@ public class SystemHandler extends AlterHandler {
             }
 
             List<Long> backendTabletIds = invertedIndex.getTabletIdsByBackendId(beId);
-            if (backendTabletIds.isEmpty() && Config.drop_backend_after_decommission) {
-                try {
-                    systemInfoService.dropBackend(beId);
-                    LOG.info("no tablet on decommission backend {}, drop it", beId);
-                } catch (DdlException e) {
-                    // does not matter, may be backend not exist
-                    LOG.info("backend {} is dropped failed after decommission {}", beId, e.getMessage());
+            if (backendTabletIds.isEmpty()) {
+                if (Config.drop_backend_after_decommission) {
+                    dropDecommissionedBackend(systemInfoService, beId);
                 }
-                continue;
+            } else {
+                LOG.info("backend {} lefts {} replicas to decommission(show up to 20): {}", beId,
+                        backendTabletIds.size(),
+                        backendTabletIds.stream().limit(20).collect(Collectors.toList()));
             }
-
-            LOG.info("backend {} lefts {} replicas to decommission(show up to 20): {}", beId, backendTabletIds.size(),
-                     backendTabletIds.stream().limit(20).collect(Collectors.toList()));
         }
     }
 
@@ -236,7 +243,6 @@ public class SystemHandler extends AlterHandler {
     @Override
     public synchronized void cancel(CancelStmt stmt) throws DdlException {
         CancelAlterSystemStmt cancelAlterSystemStmt = (CancelAlterSystemStmt) stmt;
-        cancelAlterSystemStmt.getHostPortPairs();
 
         SystemInfoService infoService = GlobalStateMgr.getCurrentSystemInfo();
         // check if backends is under decommission

fe/fe-core/src/main/java/com/starrocks/clone/ClusterLoadStatistic.java
@@ -39,7 +39,6 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.starrocks.catalog.TabletInvertedIndex;
 import com.starrocks.clone.BackendLoadStatistic.Classification;
-import com.starrocks.clone.BackendLoadStatistic.LoadScore;
 import com.starrocks.common.Config;
 import com.starrocks.system.Backend;
 import com.starrocks.system.SystemInfoService;
@@ -50,7 +49,6 @@ import org.apache.logging.log4j.Logger;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 /*
@@ -176,66 +174,6 @@ public class ClusterLoadStatistic {
         }
     }
 
-    /*
-     * Check whether the cluster can be more balance if we migrate a tablet with size 'tabletSize' from
-     * `srcBeId` to 'destBeId'
-     * 1. recalculate the load score of src and dest be after migrate the tablet.
-     * 2. if the summary of the diff between the new score and average score becomes smaller, we consider it
-     *    as more balance.
-     */
-    public boolean isMoreBalanced(long srcBeId, long destBeId, long tabletId, long tabletSize,
-                                  TStorageMedium medium) {
-        double currentSrcBeScore;
-        double currentDestBeScore;
-
-        BackendLoadStatistic srcBeStat = null;
-        Optional<BackendLoadStatistic> optSrcBeStat = beLoadStatistics.stream().filter(
-                t -> t.getBeId() == srcBeId).findFirst();
-        if (optSrcBeStat.isPresent()) {
-            srcBeStat = optSrcBeStat.get();
-        } else {
-            return false;
-        }
-
-        BackendLoadStatistic destBeStat = null;
-        Optional<BackendLoadStatistic> optDestBeStat = beLoadStatistics.stream().filter(
-                t -> t.getBeId() == destBeId).findFirst();
-        if (optDestBeStat.isPresent()) {
-            destBeStat = optDestBeStat.get();
-        } else {
-            return false;
-        }
-
-        if (!srcBeStat.hasMedium(medium) || !destBeStat.hasMedium(medium)) {
-            return false;
-        }
-
-        currentSrcBeScore = srcBeStat.getLoadScore(medium);
-        currentDestBeScore = destBeStat.getLoadScore(medium);
-
-        LoadScore newSrcBeScore = BackendLoadStatistic.calcSore(srcBeStat.getTotalUsedCapacityB(medium) - tabletSize,
-                srcBeStat.getTotalCapacityB(medium), srcBeStat.getReplicaNum(medium) - 1,
-                avgUsedCapacityPercentMap.get(medium), avgReplicaNumPercentMap.get(medium));
-
-        LoadScore newDestBeScore = BackendLoadStatistic.calcSore(destBeStat.getTotalUsedCapacityB(medium) + tabletSize,
-                destBeStat.getTotalCapacityB(medium), destBeStat.getReplicaNum(medium) + 1,
-                avgUsedCapacityPercentMap.get(medium), avgReplicaNumPercentMap.get(medium));
-
-        double currentDiff = Math.abs(currentSrcBeScore - avgLoadScoreMap.get(medium)) +
-                Math.abs(currentDestBeScore - avgLoadScoreMap.get(medium));
-        double newDiff = Math.abs(newSrcBeScore.score - avgLoadScoreMap.get(medium)) +
-                Math.abs(newDestBeScore.score - avgLoadScoreMap.get(medium));
-
-        LOG.debug("after migrate {}(size: {}) from {} to {}, medium: {}, the load score changed."
-                        + " src: {} -> {}, dest: {}->{}, average score: {}. current diff: {}, new diff: {},"
-                        + " more balanced: {}",
-                tabletId, tabletSize, srcBeId, destBeId, medium, currentSrcBeScore, newSrcBeScore.score,
-                currentDestBeScore, newDestBeScore.score, avgLoadScoreMap.get(medium), currentDiff, newDiff,
-                (newDiff < currentDiff));
-
-        return newDiff < currentDiff;
-    }
-
     public List<List<String>> getClusterStatistic(TStorageMedium medium) {
         List<List<String>> statistics = Lists.newArrayList();
 
@@ -270,56 +208,6 @@ public class ClusterLoadStatistic {
 
     public List<BackendLoadStatistic> getAllBackendLoadStatistic() {
         return beLoadStatistics;
-    }
-
-    /*
-     * If cluster is balance, all Backends will be in 'mid', and 'high' and 'low' is empty
-     * If both 'high' and 'low' has Backends, just return
-     * If no 'high' Backends, 'mid' Backends will be treated as 'high'
-     * If no 'low' Backends, 'mid' Backends will be treated as 'low'
-     */
-    public void getBackendStatisticByClass(
-            List<BackendLoadStatistic> low,
-            List<BackendLoadStatistic> mid,
-            List<BackendLoadStatistic> high,
-            TStorageMedium medium) {
-
-        for (BackendLoadStatistic beStat : beLoadStatistics) {
-            Classification clazz = beStat.getClazz(medium);
-            switch (clazz) {
-                case LOW:
-                    low.add(beStat);
-                    break;
-                case MID:
-                    mid.add(beStat);
-                    break;
-                case HIGH:
-                    high.add(beStat);
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        if (low.isEmpty() && high.isEmpty()) {
-            return;
-        }
-
-        // If there is no 'low' or 'high' backends, we treat 'mid' as 'low' or 'high'
-        if (low.isEmpty()) {
-            low.addAll(mid);
-            mid.clear();
-        } else if (high.isEmpty()) {
-            high.addAll(mid);
-            mid.clear();
-        }
-
-        sortBeStats(low, medium);
-        sortBeStats(mid, medium);
-        sortBeStats(high, medium);
-
-        LOG.debug("after adjust, backend classification low/mid/high: {}/{}/{}, medium: {}",
-                low.size(), mid.size(), high.size(), medium);
     }
 
     public List<BackendLoadStatistic> getSortedBeLoadStats(TStorageMedium medium) {

commit 3f58d05c40a1016fd175a0e7a86ecaf92303fc90

[BugFix] Fix tablet meta consistency checker concurrency issue (#27624)
Fixes SR-19107 This is introduced by #26509 .
Signed-off-by: Dejun Xia <xiadejun@starrocks.com>

fe/fe-core/src/main/java/com/starrocks/catalog/TabletInvertedIndex.java
@@ -304,9 +304,22 @@ public class TabletInvertedIndex {
                 tabletMigrationMap.size(), transactionsToClear.size(), transactionsToPublish.size(), (end - start));
     }
 
-    private void deleteTabletByConsistencyChecker(long tabletId, long backendId,
+    private void deleteTabletByConsistencyChecker(TabletMeta tabletMeta, long tabletId, long backendId,
                                                   String reason, Set<Long> invalidTablets) {
-        LOG.info("delete tablet {} on backend {} from inverted index by consistency checker, because: {}",
+        if (tabletMeta != null) {
+            Long toBeCleanedTime = tabletMeta.getToBeCleanedTime();
+            if (toBeCleanedTime == null) {
+                // init `toBeCleanedTime` and delay the actual deletion to next round of check
+                tabletMeta.setToBeCleanedTime(System.currentTimeMillis() +
+                        Config.consistency_tablet_meta_check_interval_ms / 2);
+                return;
+            } else if (System.currentTimeMillis() < toBeCleanedTime) {
+                return;
+            }
+        }
+
+        LOG.info("TabletMetaChecker: delete tablet {} on backend {} from inverted index by" +
+                        " consistency checker, because: {}",
                 tabletId, backendId, reason);
         deleteTablet(tabletId);
         invalidTablets.add(tabletId);
@@ -332,8 +345,11 @@ public class TabletInvertedIndex {
 
         long startTime = System.currentTimeMillis();
         long scannedTabletCount = 0;
+        long numIgnoredTabletCausedByAbnormalState = 0;
+        List<Long> ignoreTablets = new ArrayList<>();
 
         for (Long backendId : backendIds) {
+            LOG.info("TabletMetaChecker: start to check tablet meta consistency for backend {}", backendId);
             List<Long> tabletIds = getTabletIdsByBackendId(backendId);
             backendTabletNumReport.put(backendId, new Pair<>(0L, 0L));
 
@@ -343,7 +359,7 @@ public class TabletInvertedIndex {
 
                 TabletMeta tabletMeta = getTabletMeta(tabletId);
                 if (tabletMeta == null) {
-                    deleteTabletByConsistencyChecker(tabletId, backendId, "tablet meta is null", invalidTablets);
+                    deleteTabletByConsistencyChecker(null, tabletId, backendId, "tablet meta is null", invalidTablets);
                     continue;
                 }
 
@@ -355,7 +371,7 @@ public class TabletInvertedIndex {
                     if (database != null) {
                         isInRecycleBin = true;
                     } else {
-                        deleteTabletByConsistencyChecker(tabletId, backendId,
+                        deleteTabletByConsistencyChecker(tabletMeta, tabletId, backendId,
                                 "database " + dbId + " doesn't exist", invalidTablets);
                         continue;
                     }
@@ -372,10 +388,20 @@ public class TabletInvertedIndex {
                         if (table != null) {
                             isInRecycleBin = true;
                         } else {
-                            deleteTabletByConsistencyChecker(tabletId, backendId,
+                            deleteTabletByConsistencyChecker(tabletMeta, tabletId, backendId,
                                     "table " + dbId + "." + tableId + " doesn't exist", invalidTablets);
                             continue;
                         }
+                    }
+
+                    // To avoid delete tablet using by restore job, rollup job, schema change job etc.
+                    if (table instanceof OlapTable &&
+                            ((OlapTable) table).getState() != OlapTable.OlapTableState.NORMAL) {
+                        if (ignoreTablets.size() < 20) {
+                            ignoreTablets.add(tabletId);
+                        }
+                        numIgnoredTabletCausedByAbnormalState++;
+                        continue;
                     }
 
                     // validate partition
@@ -386,7 +412,7 @@ public class TabletInvertedIndex {
                         if (partition != null) {
                             isInRecycleBin = true;
                         } else {
-                            deleteTabletByConsistencyChecker(tabletId, backendId,
+                            deleteTabletByConsistencyChecker(tabletMeta, tabletId, backendId,
                                     "partition " + dbId + "." + tableId + "." + partitionId + " doesn't exist",
                                     invalidTablets);
                             continue;
@@ -397,7 +423,7 @@ public class TabletInvertedIndex {
                     long indexId = tabletMeta.getIndexId();
                     MaterializedIndex index = partition.getIndex(indexId);
                     if (index == null) {
-                        deleteTabletByConsistencyChecker(tabletId, backendId,
+                        deleteTabletByConsistencyChecker(tabletMeta, tabletId, backendId,
                                 "materialized index " + dbId + "." + tableId + "." +
                                         partitionId + "." + indexId + " doesn't exist",
                                 invalidTablets);
@@ -408,7 +434,7 @@ public class TabletInvertedIndex {
                         // validate tablet
                         Tablet tablet = index.getTablet(tabletId);
                         if (tablet == null) {
-                            deleteTabletByConsistencyChecker(tabletId, backendId,
+                            deleteTabletByConsistencyChecker(tabletMeta, tabletId, backendId,
                                     "tablet " + dbId + "." + tableId + "." +
                                             partitionId + "." + indexId + "." + tabletId + " doesn't exist",
                                     invalidTablets);
@@ -429,9 +455,13 @@ public class TabletInvertedIndex {
 
         // logging report
         LOG.info("TabletMetaChecker has cleaned {} invalid tablet(s), scanned {} tablet(s) on {} backend(s) in {}ms," +
-                " backend tablet count info(format: backend_id=curr_tablet_count:recycle_tablet_count): {}",
+                " total tablets in recycle bin: {}, total ignored tablets: {}, up to 20 of ignored tablets: {}, " +
+                        "backend tablet count info(format: backend_id=curr_tablet_count:recycle_tablet_count): {}",
                 invalidTablets.size(), scannedTabletCount, backendIds.size(),
-                System.currentTimeMillis() - startTime, backendTabletNumReport);
+                System.currentTimeMillis() - startTime,
+                backendTabletNumReport.values().stream().mapToLong(p -> p.second).sum(),
+                numIgnoredTabletCausedByAbnormalState, ignoreTablets,
+                backendTabletNumReport);
     }
 
     public Long getTabletIdByReplica(long replicaId) {

fe/fe-core/src/main/java/com/starrocks/catalog/TabletMeta.java
@@ -51,6 +51,11 @@ public class TabletMeta {
 
     private final boolean isLakeTablet;
 
+    /**
+     * If currentTimeMs is ahead of `toBeCleanedTimeMs`, the tablet meta will be cleaned from TabletInvertedIndex.
+     */
+    private Long toBeCleanedTimeMs = null;
+
     private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 
     public TabletMeta(long dbId, long tableId, long partitionId, long indexId, int schemaHash,
@@ -113,6 +118,14 @@ public class TabletMeta {
         } finally {
             lock.readLock().unlock();
         }
+    }
+
+    public Long getToBeCleanedTime() {
+        return toBeCleanedTimeMs;
+    }
+
+    public void setToBeCleanedTime(Long time) {
+        toBeCleanedTimeMs = time;
     }
 
     public boolean containsSchemaHash(int schemaHash) {

fe/fe-core/src/main/java/com/starrocks/common/Config.java
@@ -1000,6 +1000,8 @@ public class Config extends ConfigBase {
      */
     @ConfField(mutable = true)
     public static long check_consistency_default_timeout_second = 600; // 10 min
+    @ConfField(mutable = true)
+    public static long consistency_tablet_meta_check_interval_ms = 2 * 3600 * 1000L; // every 2 hours
 
     // Configurations for query engine
     /**

fe/fe-core/src/main/java/com/starrocks/consistency/ConsistencyChecker.java
@@ -69,9 +69,6 @@ public class ConsistencyChecker extends FrontendDaemon {
     private static final Logger LOG = LogManager.getLogger(ConsistencyChecker.class);
 
     private static final int MAX_JOB_NUM = 100;
-
-    private static final long DEFAULT_TABLET_META_CHECK_INTERVAL_MS = 2 * 3600 * 1000L; // every 2 hours
-
     private static final Comparator<MetaObject> COMPARATOR =
             (first, second) -> Long.signum(first.getLastCheckTime() - second.getLastCheckTime());
 
@@ -129,7 +126,7 @@ public class ConsistencyChecker extends FrontendDaemon {
 
     @Override
     protected void runAfterCatalogReady() {
-        if (System.currentTimeMillis() - lastTabletMetaCheckTime > DEFAULT_TABLET_META_CHECK_INTERVAL_MS) {
+        if (System.currentTimeMillis() - lastTabletMetaCheckTime > Config.consistency_tablet_meta_check_interval_ms) {
             checkTabletMetaConsistency();
             lastTabletMetaCheckTime = System.currentTimeMillis();
         }