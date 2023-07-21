327550f34021a74f10463f6949db6433857d83da
trueeyu <lxhhust350@qq.com>
2023-07-21 17:30:40 +0800

    fix
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/service/service.h
+++ be/src/service/service.h
@@ -25,7 +25,7 @@ extern std::atomic<bool> k_starrocks_exit;
 extern std::atomic<bool> k_starrocks_exit_quick;
 
 void wait_for_fragments_finish(ExecEnv* exec_env, size_t max_loop_cnt_cfg);
+
+void start_be();
 } // namespace starrocks
 
-void start_cn();
-void start_be();
