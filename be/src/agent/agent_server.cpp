6458938d2958821290efb0f881d7950c93b53752
trueeyu <lxhhust350@qq.com>
2023-07-21 20:01:53 +0800

    format
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/agent/agent_server.cpp
+++ be/src/agent/agent_server.cpp
@@ -71,7 +71,7 @@ AgentServer::AgentServer(ExecEnv* exec_env, const TMasterInfo& master_info)
     // Both PUSH and REALTIME_PUSH type use _push_workers
     CREATE_AND_START_POOL(PUSH, _push_workers,
                           config::push_worker_count_normal_priority + config::push_worker_count_high_priority);
-    CREATE_AND_START_POOL(PUBLISH_VERSION, _publish_version_workers, config::num_cores);
+    CREATE_AND_START_POOL(PUBLISH_VERSION, _publish_version_workers, CpuInfo::num_cores());
     CREATE_AND_START_POOL(CLEAR_TRANSACTION_TASK, _clear_transaction_task_workers,
                           config::clear_transaction_task_worker_count);
     CREATE_AND_START_POOL(DELETE, _delete_workers, config::delete_worker_count);
