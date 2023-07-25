f1db1fa60512cc3123b8892cddd0bd923d371881
xuzifu666 <1206332514@qq.com>
2023-07-25 11:13:02 +0800

    [Refactor] clear LOG in analysis module (#27762)
    Signed-off-by: xuzifu666 <1206332514@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/analysis/AggregateInfoBase.java
+++ fe/fe-core/src/main/java/com/starrocks/analysis/AggregateInfoBase.java
@@ -37,8 +37,6 @@ package com.starrocks.analysis;
 import com.google.common.base.MoreObjects;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 
@@ -47,8 +45,6 @@ import java.util.ArrayList;
  * tuple descriptors as well as their smaps for evaluating aggregate functions.
  */
 public abstract class AggregateInfoBase {
-    private static final Logger LOG =
-            LoggerFactory.getLogger(AggregateInfoBase.class);
 
     // For aggregations: All unique grouping expressions from a select block.
     // For analytics: Empty.
--- fe/fe-core/src/main/java/com/starrocks/analysis/RoutineLoadDataSourceProperties.java
+++ fe/fe-core/src/main/java/com/starrocks/analysis/RoutineLoadDataSourceProperties.java
@@ -43,15 +43,12 @@ import com.starrocks.common.Pair;
 import com.starrocks.load.routineload.LoadDataSourceType;
 import com.starrocks.sql.ast.CreateRoutineLoadStmt;
 import com.starrocks.sql.parser.NodePosition;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 public class RoutineLoadDataSourceProperties implements ParseNode {
-    private static final Logger LOG = LogManager.getLogger(RoutineLoadDataSourceProperties.class);
 
     private static final ImmutableSet<String> CONFIGURABLE_KAFKA_PROPERTIES_SET = new ImmutableSet.Builder<String>()
             .add(CreateRoutineLoadStmt.KAFKA_PARTITIONS_PROPERTY)
