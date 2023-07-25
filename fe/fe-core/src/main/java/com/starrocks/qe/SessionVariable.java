bdcabb9837f15e29122584aa0f2b9f9c5e79a560
Kevin Li <ming.moriarty@gmail.com>
2023-07-25 11:52:54 +0800

    [BugFix] Fix mv rewrite bug for aggregat with having expr (#27557)
    Fixes #27424

--- fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
@@ -1090,7 +1090,7 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
     private String queryExcludingMVNames = "";
 
     @VarAttr(name = QUERY_INCLUDING_MV_NAMES, flag = VariableMgr.INVISIBLE)
-    private String queryincludingMVNames = "";
+    private String queryIncludingMVNames = "";
 
     @VarAttr(name = ANALYZE_FOR_MV)
     private String analyzeTypeForMV = "sample";
@@ -2118,12 +2118,12 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
         this.queryExcludingMVNames = queryExcludingMVNames;
     }
 
-    public String getQueryincludingMVNames() {
-        return queryincludingMVNames;
+    public String getQueryIncludingMVNames() {
+        return queryIncludingMVNames;
     }
 
-    public void setQueryincludingMVNames(String queryincludingMVNames) {
-        this.queryincludingMVNames = queryincludingMVNames;
+    public void setQueryIncludingMVNames(String queryIncludingMVNames) {
+        this.queryIncludingMVNames = queryIncludingMVNames;
     }
 
     public String getAnalyzeForMV() {
