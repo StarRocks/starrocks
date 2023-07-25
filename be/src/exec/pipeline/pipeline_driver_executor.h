be914b84ed083e2a08d93c12324dc2fde7290d6f
trueeyu <lxhhust350@qq.com>
2023-07-25 13:14:58 +0800

    [Refactor] Add interface close() for GlobalDriverExecutor/DataStreamMgr/DataStreamRecvr/FragmentMgr/PassThroughChunkBufferManager/LoadStreamMgr (#27828)
    Fixes #27417
    
    graceful exit step 6: Add interface close() for
    GlobalDriverExecutor/DataStreamMgr/DataStreamRecvr/FragmentMgr/PassThroughChunkBufferManager/LoadStreamMgr
    
    (cherry picked from commit c021319d9f87050390d54fe5e1cb96a170ad9319)

--- be/src/exec/pipeline/pipeline_driver_executor.h
+++ be/src/exec/pipeline/pipeline_driver_executor.h
@@ -42,6 +42,7 @@ public:
     virtual void change_num_threads(int32_t num_threads) {}
     virtual void submit(DriverRawPtr driver) = 0;
     virtual void cancel(DriverRawPtr driver) = 0;
+    virtual void close() = 0;
 
     // When all the root drivers (the drivers have no successors in the same fragment) have finished,
     // just notify FE timely the completeness of fragment via invocation of report_exec_state, but
@@ -74,6 +75,7 @@ public:
     void change_num_threads(int32_t num_threads) override;
     void submit(DriverRawPtr driver) override;
     void cancel(DriverRawPtr driver) override;
+    void close() override;
     void report_exec_state(QueryContext* query_ctx, FragmentContext* fragment_ctx, const Status& status, bool done,
                            bool attach_profile) override;
     void report_audit_statistics(QueryContext* query_ctx, FragmentContext* fragment_ctx, bool* done) override;
