2701de629f844dcbaf0bebeca3374e2ae7e2fe44
silverbullet233 <3675229+silverbullet233@users.noreply.github.com>
2023-07-26 15:21:12 +0800

    merge query stats in eos request
    Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>

--- be/src/exec/pipeline/exchange/sink_buffer.cpp
+++ be/src/exec/pipeline/exchange/sink_buffer.cpp
@@ -230,6 +230,30 @@ void SinkBuffer::_process_send_window(const TUniqueId& instance_id, const int64_
     }
 }
 
+void SinkBuffer::_try_to_merge_query_statistics(TransmitChunkInfo& request) {
+    if (!request.params->has_query_statistics()) {
+        return;
+    }
+    auto& query_statistics = request.params->query_statistics();
+    bool need_merge = false;
+    if (query_statistics.scan_rows() > 0 || query_statistics.scan_bytes() > 0 || query_statistics.cpu_cost_ns() > 0) {
+        need_merge = true;
+    }
+    if (!need_merge && query_statistics.stats_items_size() > 0) {
+        for (int i = 0; i < query_statistics.stats_items_size(); i++) {
+            const auto& stats_item = query_statistics.stats_items(i);
+            if (stats_item.scan_rows() > 0 || stats_item.scan_bytes()) {
+                need_merge = true;
+                break;
+            }
+        }
+    }
+    if (need_merge) {
+        _eos_query_stats->merge_pb(query_statistics);
+        request.params->clear_query_statistics();
+    }
+}
+
 Status SinkBuffer::_try_to_send_rpc(const TUniqueId& instance_id, const std::function<void()>& pre_works) {
     std::lock_guard<Mutex> l(*_mutexes[instance_id.lo]);
     pre_works();
@@ -294,31 +318,10 @@ Status SinkBuffer::_try_to_send_rpc(const TUniqueId& instance_id, const std::fun
             // eos is the last packet to send to finish the input stream of the corresponding of
             // ExchangeSourceOperator and eos is sent exactly-once.
             if (_num_sinkers[instance_id.lo] > 1) {
+                // to reduce uncessary rpc requests, we merge all query statistics in eos requests into one and send it through the last eos request
+                _try_to_merge_query_statistics(request);
                 if (request.params->chunks_size() == 0) {
-                    bool should_skip = true;
-                    // if this request contains query statistics info, we should send it
-                    if (request.params->has_query_statistics()) {
-                        auto& query_statistics = request.params->query_statistics();
-                        if (query_statistics.scan_rows() > 0 || query_statistics.scan_bytes() > 0 ||
-                            query_statistics.cpu_cost_ns() > 0) {
-                            should_skip = false;
-                        }
-                        if (should_skip && query_statistics.stats_items_size() > 0) {
-                            for (int i = 0; i < query_statistics.stats_items_size(); ++i) {
-                                const auto& stats_item = query_statistics.stats_items(i);
-                                if (stats_item.scan_rows() > 0 || stats_item.scan_bytes() > 0) {
-                                    should_skip = false;
-                                    break;
-                                }
-                            }
-                        }
-                        if (!should_skip) {
-                            request.params->set_eos(false);
-                        }
-                    }
-                    if (should_skip) {
-                        continue;
-                    }
+                    continue;
                 } else {
                     request.params->set_eos(false);
                 }
@@ -330,6 +333,10 @@ Status SinkBuffer::_try_to_send_rpc(const TUniqueId& instance_id, const std::fun
                     need_wait = true;
                     return Status::OK();
                 }
+                // this is the last eos query, set query stats
+                _eos_query_stats->merge_pb(request.params->query_statistics());
+                request.params->clear_query_statistics();
+                _eos_query_stats->to_pb(request.params->mutable_query_statistics());
             }
         }
 
