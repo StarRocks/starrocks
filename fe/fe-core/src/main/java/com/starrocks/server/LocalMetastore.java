733d4ecd3f99ff49b6b8febb6be39a78ba3f5d3b
Murphy <96611012+mofeiatwork@users.noreply.github.com>
2023-07-26 08:42:00 +0800

    [Feature] support create or replace view (#27768)
    Fixes #27767
    
    `CREATE OR REPLACE VIEW` could be used to update existed view
    definition.
    
    (cherry picked from commit 6e1d5ec99bc54ca0b03a3c1cf7ac2253e24512ee)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/alter/Alter.java
    #	fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
    #	fe/fe-core/src/main/java/com/starrocks/sql/ast/AlterViewStmt.java
    #	fe/fe-core/src/main/java/com/starrocks/sql/ast/CreateViewStmt.java
    #	fe/fe-core/src/test/java/com/starrocks/catalog/CreateViewTest.java

--- fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
+++ fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
@@ -31,8 +31,15 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
+<<<<<<< HEAD
 import com.staros.proto.ShardStorageInfo;
 import com.starrocks.analysis.ColumnDef;
+=======
+import com.staros.proto.FilePathInfo;
+import com.starrocks.alter.AlterJobMgr;
+import com.starrocks.analysis.Expr;
+import com.starrocks.analysis.FunctionCallExpr;
+>>>>>>> 6e1d5ec99b ([Feature] support create or replace view (#27768))
 import com.starrocks.analysis.IntLiteral;
 import com.starrocks.analysis.KeysDesc;
 import com.starrocks.analysis.StringLiteral;
@@ -4240,12 +4247,16 @@ public class LocalMetastore implements ConnectorMetadata {
         }
 
         // check if table exists in db
+        boolean existed = false;
         db.readLock();
         try {
             if (db.getTable(tableName) != null) {
+                existed = true;
                 if (stmt.isSetIfNotExists()) {
                     LOG.info("create view[{}] which already exists", tableName);
                     return;
+                } else if (stmt.isReplace()) {
+                    LOG.info("view {} already exists, need to replace it", tableName);
                 } else {
                     ErrorReport.reportDdlException(ErrorCode.ERR_TABLE_EXISTS_ERROR, tableName);
                 }
@@ -4254,8 +4265,32 @@ public class LocalMetastore implements ConnectorMetadata {
             db.readUnlock();
         }
 
-        List<Column> columns = stmt.getColumns();
+        if (existed) {
+            // already existed, need to alter the view
+            AlterJobMgr alterJobMgr = GlobalStateMgr.getCurrentState().getAlterJobMgr();
+            try {
+                AlterViewStmt alterViewStmt = AlterViewStmt.fromReplaceStmt(stmt);
+                alterJobMgr.processAlterView(alterViewStmt, ConnectContext.get());
+                LOG.info("replace view {} successfully", tableName);
+            } catch (DdlException e) {
+                LOG.warn("replace view failed due to {}", e.getMessage(), e);
+                throw new DdlException("replace view failed due to " + e.getMessage(), e);
+            }
+        } else {
+            List<Column> columns = stmt.getColumns();
+            long tableId = getNextId();
+            View view = new View(tableId, tableName, columns);
+            view.setComment(stmt.getComment());
+            view.setInlineViewDefWithSqlMode(stmt.getInlineViewDef(),
+                    ConnectContext.get().getSessionVariable().getSqlMode());
+            // init here in case the stmt string from view.toSql() has some syntax error.
+            try {
+                view.init();
+            } catch (UserException e) {
+                throw new DdlException("failed to init view stmt", e);
+            }
 
+<<<<<<< HEAD
         long tableId = getNextId();
         View newView = new View(tableId, tableName, columns);
         newView.setComment(stmt.getComment());
@@ -4289,6 +4324,11 @@ public class LocalMetastore implements ConnectorMetadata {
         }
 
         LOG.info("successfully create view[" + tableName + "-" + newView.getId() + "]");
+=======
+            onCreate(db, view, "", stmt.isSetIfNotExists());
+            LOG.info("successfully create view[" + tableName + "-" + view.getId() + "]");
+        }
+>>>>>>> 6e1d5ec99b ([Feature] support create or replace view (#27768))
     }
 
     public void replayCreateCluster(Cluster cluster) {
--- fe/fe-core/src/main/java/com/starrocks/sql/ast/CreateViewStmt.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/ast/CreateViewStmt.java
@@ -9,17 +9,39 @@ import java.util.List;
 
 public class CreateViewStmt extends BaseViewStmt {
     private final boolean ifNotExists;
+    private final boolean replace;
     private final String comment;
 
+<<<<<<< HEAD
     public CreateViewStmt(boolean ifNotExists, TableName tableName, List<ColWithComment> cols,
                           String comment, QueryStatement queryStmt) {
         super(tableName, cols, queryStmt);
         this.ifNotExists = ifNotExists;
+=======
+    //Resolved by Analyzer
+    protected List<Column> columns;
+    private String inlineViewDef;
+
+    public CreateViewStmt(boolean ifNotExists, boolean replace,
+                          TableName tableName, List<ColWithComment> colWithComments,
+                          String comment,
+                          QueryStatement queryStmt,
+                          NodePosition pos) {
+        super(pos);
+        this.ifNotExists = ifNotExists;
+        this.replace = replace;
+        this.tableName = tableName;
+        this.colWithComments = colWithComments;
+>>>>>>> 6e1d5ec99b ([Feature] support create or replace view (#27768))
         this.comment = Strings.nullToEmpty(comment);
     }
 
     public boolean isSetIfNotExists() {
         return ifNotExists;
+    }
+
+    public boolean isReplace() {
+        return replace;
     }
 
     public String getComment() {
