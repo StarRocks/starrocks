8b0d8bc331237b90ab9ac76d77eb56d1dc087798
Zhuhe Fang <fzhedu@gmail.com>
2023-07-27 14:38:02 +0800

    [BugFix] fix deduce array_map error (#27963)
    Signed-off-by: Zhuhe Fang <fzhedu@gmail.com>
    (cherry picked from commit 993f401f158fef332d3ae5e6d72a927ed5a7ebb1)

--- fe/fe-core/src/main/java/com/starrocks/catalog/FunctionSet.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/FunctionSet.java
@@ -420,7 +420,13 @@ public class FunctionSet {
     public static final String MAP = "map";
     public static final String MAP_APPLY = "map_apply";
     public static final String MAP_FILTER = "map_filter";
+
+    public static final String DISTINCT_MAP_KEYS = "distinct_map_keys";
     public static final String MAP_VALUES = "map_values";
+
+    public static final String MAP_CONCAT= "map_concat";
+
+    public static final String MAP_FROM_ARRAYS = "map_from_arrays";
     public static final String MAP_KEYS = "map_keys";
     public static final String MAP_SIZE = "map_size";
     public static final String TRANSFORM_VALUES = "transform_values";
