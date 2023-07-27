47deede44100bfacecab27deaa33bdd3027a0279
Dejun Xia <xiadejun@starrocks.com>
2023-07-27 21:11:39 +0800

    fix ut
    Signed-off-by: Dejun Xia <xiadejun@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/Partition.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/Partition.java
@@ -221,7 +221,8 @@ public class Partition extends MetaObject implements Writable {
     }
 
     public void updateVisibleVersion(long visibleVersion, long visibleVersionTime, long visibleTxnId) {
-        setVisibleVersion(visibleVersion, visibleVersionTime, visibleTxnId);
+        updateVisibleVersion(visibleVersion, visibleVersionTime);
+        this.visibleTxnId = visibleTxnId;
     }
 
     public long getVisibleVersion() {
