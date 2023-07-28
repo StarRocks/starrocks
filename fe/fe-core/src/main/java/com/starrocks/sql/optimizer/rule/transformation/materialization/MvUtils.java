dc88ab0c40b2294b5f5817b1833a3932ca32fcce
ABing <101158374+ABingHuang@users.noreply.github.com>
2023-07-28 16:35:32 +0800

    [BugFix] fix outer join and anti join rewrite bug (#28028)
    Fixes #27163 
    
    The bug has the following causes:
    1. The invalid use of equivalence in outer join and anti join, which
    leads to the invalid use of output columns. The join on predicate A.a =
    B.b for outer join and anti join can not be used to construct
    equivalence class.
    2. the compatibility check should be more strict for outer and anti
    join, which should not consider the pushdown predicates derived from
    equivalence class. eg: the left outer join should make sure that the
    predicates of right child tree of query and mv should be equal. it means
    that the compensation predicates should not come from right child of
    left outer join.
    3. when clone equivalence class, should use the same
    Set<ColumnRefOperator> for the columns in the same ec.
    
    fixed by
    1. constructing eq without column equal predicates in outer and anti
    join
    2. add more strict compatibility check for outer and anti join,
    excluding pushdown ec derived predicates
    3. fix equivalence class clone bug.
    
    Signed-off-by: ABingHuang <codekhuang@163.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/materialization/MvUtils.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/materialization/MvUtils.java
@@ -184,7 +184,7 @@ public class MvUtils {
                         Integer id = scanContext.getTableIdMap().computeIfAbsent(table, t -> 0);
                         LogicalJoinOperator joinOperator = optExpression.getOp().cast();
                         TableScanDesc tableScanDesc = new TableScanDesc(
-                                table, id, scanOperator, joinOperator.getJoinType(), i == 0);
+                                table, id, scanOperator, optExpression, i == 0);
                         context.getTableScanDescs().add(tableScanDesc);
                         scanContext.getTableIdMap().put(table, ++id);
                     } else {
@@ -430,8 +430,10 @@ public class MvUtils {
         return new ReplaceColumnRefRewriter(mvLineage, true);
     }
 
+    // pushdown predicates are excluded when calculating comppensation predicates,
+    // because they are derived from equivalence class, the original predicates have be considered
     private static void collectValidPredicates(List<ScalarOperator> conjuncts, List<ScalarOperator> predicates) {
-        conjuncts.stream().filter(x -> !x.isRedundant()).forEach(predicates::add);
+        conjuncts.stream().filter(x -> !x.isRedundant() && !x.isPushdown()).forEach(predicates::add);
     }
 
     private static void getAllPredicates(OptExpression root, List<ScalarOperator> predicates) {
@@ -887,7 +889,7 @@ public class MvUtils {
         }
         return o.toString();
     }
-    
+
     /**
      * Return the max refresh timestamp of all partition infos.
      */
@@ -899,5 +901,32 @@ public class MvUtils {
                 .max(Long::compareTo)
                 .filter(Objects::nonNull)
                 .orElse(System.currentTimeMillis());
+    }
+
+    public static List<ScalarOperator> collectOnPredicate(OptExpression optExpression) {
+        List<ScalarOperator> onPredicates = Lists.newArrayList();
+        collectOnPredicate(optExpression, onPredicates, false);
+        return onPredicates;
+    }
+
+    public static List<ScalarOperator> collectOuterAntiJoinOnPredicate(OptExpression optExpression) {
+        List<ScalarOperator> onPredicates = Lists.newArrayList();
+        collectOnPredicate(optExpression, onPredicates, true);
+        return onPredicates;
+    }
+
+    public static void collectOnPredicate(
+            OptExpression optExpression, List<ScalarOperator> onPredicates, boolean onlyOuterAntiJoin) {
+        for (OptExpression child : optExpression.getInputs()) {
+            collectOnPredicate(child, onPredicates, onlyOuterAntiJoin);
+        }
+        if (optExpression.getOp() instanceof LogicalJoinOperator) {
+            LogicalJoinOperator joinOperator = optExpression.getOp().cast();
+            if (onlyOuterAntiJoin &&
+                    !(joinOperator.getJoinType().isOuterJoin() || joinOperator.getJoinType().isAntiJoin())) {
+                return;
+            }
+            onPredicates.addAll(Utils.extractConjuncts(joinOperator.getOnPredicate()));
+        }
     }
 }
