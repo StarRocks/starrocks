237dd1a5f56bcab35627cedc2e32e221c01f724c
eyes_on_me <3675229+silverbullet233@users.noreply.github.com>
2023-08-01 19:56:03 +0800

    [Enhancement] refactor date_diff function (#28276)
    Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>

--- fe/fe-core/src/main/java/com/starrocks/connector/parser/trino/ComplexFunctionCallTransformer.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/parser/trino/ComplexFunctionCallTransformer.java
@@ -22,6 +22,7 @@ import com.starrocks.analysis.IntLiteral;
 import com.starrocks.analysis.StringLiteral;
 import com.starrocks.analysis.TimestampArithmeticExpr;
 import com.starrocks.catalog.Type;
+import com.starrocks.sql.analyzer.SemanticException;
 import com.starrocks.sql.ast.MapExpr;
 
 import java.util.Collections;
@@ -65,6 +66,11 @@ public class ComplexFunctionCallTransformer {
         } else if (functionName.equalsIgnoreCase("last_day_of_month")) {
             Expr child = args[0];
             return new FunctionCallExpr("last_day", ImmutableList.of(child, new StringLiteral("month")));
+        } else if (functionName.equalsIgnoreCase("date_diff")) {
+            if (args.length != 3) {
+                throw new SemanticException("date_diff function must have 3 arguments");
+            }
+            return new FunctionCallExpr("date_diff", ImmutableList.of(args[0], args[2], args[1]));
         }
         return null;
     }
