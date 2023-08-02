7c49da75d7539aee42f7a5d9f33ed67b1a6a8aef
张田 <zhang_tian@inspur.com>
2023-08-02 16:36:30 +0800

    Fix the problem of configuration failure when Paimon Catalog uses S3-compatible storage system.
    Signed-off-by: 张田 <zhang_tian@inspur.com>

--- fe/fe-core/src/main/java/com/starrocks/connector/paimon/PaimonConnector.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/paimon/PaimonConnector.java
@@ -21,6 +21,7 @@ import com.starrocks.connector.ConnectorMetadata;
 import com.starrocks.connector.exception.StarRocksConnectorException;
 import com.starrocks.credential.CloudConfiguration;
 import com.starrocks.credential.CloudConfigurationFactory;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.paimon.catalog.Catalog;
@@ -28,7 +29,9 @@ import org.apache.paimon.catalog.CatalogContext;
 import org.apache.paimon.catalog.CatalogFactory;
 import org.apache.paimon.options.Options;
 
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import static org.apache.paimon.options.CatalogOptions.METASTORE;
 import static org.apache.paimon.options.CatalogOptions.URI;
@@ -39,30 +42,12 @@ public class PaimonConnector implements Connector {
     private static final String PAIMON_CATALOG_TYPE = "paimon.catalog.type";
     private static final String PAIMON_CATALOG_WAREHOUSE = "paimon.catalog.warehouse";
     private static final String HIVE_METASTORE_URIS = "hive.metastore.uris";
-    private static final String AWS_S3_ENABLE_SSL = "aws.s3.enable_ssl";
-    private static final String AWS_S3_ENABLE_PATH_STYLE_ACCESS = "aws.s3.enable_path_style_access";
-    private static final String AWS_S3_ENDPOINT = "aws.s3.endpoint";
-    private static final String AWS_S3_ACCESS_KEY = "aws.s3.access_key";
-    private static final String AWS_S3_SECRET_KEY = "aws.s3.secret_key";
-    private static final String PAIMON_S3A_ENABLE_SSL = "fs.s3a.enable.ssl";
-    private static final String PAIMON_S3A_ENABLE_PATH_STYLE_ACCESS = "fs.s3a.path.style.access";
-    private static final String PAIMON_S3A_ENDPOINT = "fs.s3a.endpoint";
-    private static final String PAIMON_S3A_ACCESS_KEY = "fs.s3a.access.key";
-    private static final String PAIMON_S3A_SECRET_KEY = "fs.s3a.secret.key";
-
     private final CloudConfiguration cloudConfiguration;
     private Catalog paimonNativeCatalog;
     private final String catalogType;
     private final String metastoreUris;
     private final String warehousePath;
     private final String catalogName;
-    private final String awsS3EnableSsl;
-    private final String awsS3EnablePathStyleAccess;
-    private final String awsS3Endpoint;
-    private final String awsS3AccessKey;
-    private final String awsS3SecretKey;
-
-
     private final Options paimonOptions;
 
     public PaimonConnector(ConnectorContext context) {
@@ -72,12 +57,6 @@ public class PaimonConnector implements Connector {
         this.catalogType = properties.get(PAIMON_CATALOG_TYPE);
         this.metastoreUris = properties.get(HIVE_METASTORE_URIS);
         this.warehousePath = properties.get(PAIMON_CATALOG_WAREHOUSE);
-
-        this.awsS3EnableSsl = properties.get(AWS_S3_ENABLE_SSL);
-        this.awsS3EnablePathStyleAccess = properties.get(AWS_S3_ENABLE_PATH_STYLE_ACCESS);
-        this.awsS3Endpoint = properties.get(AWS_S3_ENDPOINT);
-        this.awsS3AccessKey = properties.get(AWS_S3_ACCESS_KEY);
-        this.awsS3SecretKey = properties.get(AWS_S3_SECRET_KEY);
 
         this.paimonOptions = new Options();
         if (Strings.isNullOrEmpty(catalogType)) {
@@ -97,12 +76,14 @@ public class PaimonConnector implements Connector {
         }
         paimonOptions.setString(WAREHOUSE.key(), warehousePath);
 
-        if (!Strings.isNullOrEmpty(awsS3AccessKey) && !Strings.isNullOrEmpty(awsS3SecretKey)) {
-            paimonOptions.setString(PAIMON_S3A_ENABLE_SSL, awsS3EnableSsl);
-            paimonOptions.setString(PAIMON_S3A_ENABLE_PATH_STYLE_ACCESS, awsS3EnablePathStyleAccess);
-            paimonOptions.setString(PAIMON_S3A_ENDPOINT, awsS3Endpoint);
-            paimonOptions.setString(PAIMON_S3A_ACCESS_KEY, awsS3AccessKey);
-            paimonOptions.setString(PAIMON_S3A_SECRET_KEY, awsS3SecretKey);
+        Configuration storageConfig = new Configuration();
+        cloudConfiguration.applyToConfiguration(storageConfig);
+        Iterator<Entry<String, String>> propsIterator = storageConfig.iterator();
+        while (propsIterator.hasNext()) {
+            Entry<String, String> item = propsIterator.next();
+            if (item.getKey().startsWith("fs.s3")) {
+                paimonOptions.setString(item.getKey(), item.getValue());
+            }
         }
     }
 
