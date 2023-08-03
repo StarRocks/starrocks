1022ed8f35646d5788a7d92b69e958efdf1f1d36
Letian Jiang <letian.jiang@outlook.com>
2023-08-03 20:11:15 +0800

    [Enhancement] Not showing JDBC external table password (#28391)
    Fixes #28230
    
    Signed-off-by: Letian Jiang <letian.jiang@outlook.com>
    (cherry picked from commit 6eb595c21f0623fa4d57a5a97dbb61f88a9d8885)

--- fe/fe-core/src/main/java/com/starrocks/catalog/JDBCResource.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/JDBCResource.java
@@ -133,6 +133,9 @@ public class JDBCResource extends Resource {
     protected void getProcNodeData(BaseProcResult result) {
         String lowerCaseType = type.name().toLowerCase();
         for (Map.Entry<String, String> entry : configs.entrySet()) {
+            if (entry.getKey().equalsIgnoreCase(PASSWORD)) {
+                continue;
+            }
             result.addRow(Lists.newArrayList(name, lowerCaseType, entry.getKey(), entry.getValue()));
         }
     }
