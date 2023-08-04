04b3990a440648284e661bf7641d6d776c00d915
meegoo <meegoo.sr@gmail.com>
2023-08-04 18:39:58 +0800

    [BugFix] Fix insert job clean (#28636)
    Fixes #27975
    
    previous fix https://github.com/StarRocks/starrocks/pull/28311 has some
    problem when insert job for system table which has no transaction.
    
    Signed-off-by: meegoo <meegoo.sr@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/load/loadv2/LoadMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/load/loadv2/LoadMgr.java
@@ -402,7 +402,7 @@ public class LoadMgr implements Writable {
         List<LoadJob> insertJobs;
         try {
             insertJobs = idToLoadJob.values().stream()
-                    .filter(job -> job.getJobType() == EtlJobType.INSERT && !job.isCompleted())
+                    .filter(job -> job.hasTxn() && job.getJobType() == EtlJobType.INSERT && !job.isCompleted())
                     .collect(Collectors.toList());
         } finally {
             readUnlock();
@@ -411,7 +411,7 @@ public class LoadMgr implements Writable {
         insertJobs.forEach(job -> {
             TransactionState state = GlobalStateMgr.getCurrentGlobalTransactionMgr().getLabelTransactionState(
                     job.getDbId(), job.getLabel());
-            if (state != null && state.getTransactionStatus() == TransactionStatus.UNKNOWN) {
+            if (state == null || state.getTransactionStatus() == TransactionStatus.UNKNOWN) {
                 try {
                     recordFinishedOrCacnelledLoadJob(
                             job.getId(), EtlJobType.INSERT, "Cancelled since transaction status unknown", "");
