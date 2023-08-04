455716ce6d25aa121b5f1bce64ce5500035a6dfd
stdpain <34912776+stdpain@users.noreply.github.com>
2023-08-04 10:02:01 +0800

    [Refactor] Make FE udf error message more reasonable (#28478)
    Signed-off-by: stdpain <drfeng08@gmail.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/ast/CreateFunctionStmtTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/ast/CreateFunctionStmtTest.java
@@ -13,7 +13,12 @@
 // limitations under the License.
 package com.starrocks.sql.ast;
 
+import com.starrocks.analysis.FunctionName;
+import com.starrocks.catalog.Type;
+import com.starrocks.common.Config;
 import com.starrocks.qe.ConnectContext;
+import com.starrocks.sql.analyzer.AnalyzerUtils;
+import mockit.Expectations;
 import mockit.Mocked;
 import org.junit.Assert;
 import org.junit.Test;
@@ -33,9 +38,47 @@ public class CreateFunctionStmtTest {
                 + ");";
         CreateFunctionStmt stmt = (CreateFunctionStmt) com.starrocks.sql.parser.SqlParser.parse(
                 createFunctionSql, 32).get(0);
-        // com.starrocks.sql.analyzer.Analyzer.analyze(stmt, ctx);
 
         Assert.assertEquals("ABC", stmt.getFunctionName().getDb());
         Assert.assertEquals("my_udf_json_get", stmt.getFunctionName().getFunction());
     }
+
+    @Test(expected = Throwable.class)
+    public void testDisableUDF() throws Exception {
+        boolean val = Config.enable_udf;
+        try {
+            Config.enable_udf = false;
+            String createFunctionSql = "CREATE FUNCTION ABC.MY_UDF_JSON_GET(string, string) \n"
+                    + "RETURNS string \n"
+                    + "properties (\n"
+                    + "    \"symbol\" = \"com.starrocks.udf.sample.UDFJsonGet\",\n"
+                    + "    \"type\" = \"StarrocksJar\",\n"
+                    + "    \"file\" = \"http://http_host:http_port/udf-1.0-SNAPSHOT-jar-with-dependencies.jar\"\n"
+                    + ");";
+            CreateFunctionStmt stmt = (CreateFunctionStmt) com.starrocks.sql.parser.SqlParser.parse(
+                    createFunctionSql, 32).get(0);
+            stmt.analyze(ctx);
+        } finally {
+            Config.enable_udf = val;
+        }
+    }
+
+    @Test(expected = Throwable.class)
+    public void testGetIfDisableUDF() throws Exception {
+        boolean val = Config.enable_udf;
+        try {
+            Config.enable_udf = false;
+            FunctionName mock = FunctionName.createFnName("mock");
+            new Expectations(AnalyzerUtils.class) {
+                {
+                    AnalyzerUtils.getDBUdfFunction(ctx, mock, new Type[0]);
+                }
+            };
+            AnalyzerUtils.getUdfFunction(ctx, mock, new Type[0]);
+
+        } finally {
+            Config.enable_udf = val;
+        }
+    }
+
 }
