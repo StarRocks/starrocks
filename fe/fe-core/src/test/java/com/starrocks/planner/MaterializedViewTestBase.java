acd99e87230ea4b7e20044c2b3e0f7c1aaed54e7
Kevin Li <ming.moriarty@gmail.com>
2023-08-04 15:11:20 +0800

    fix bugs
    Signed-off-by: Kevin Li <ming.moriarty@gmail.com>

--- fe/fe-core/src/test/java/com/starrocks/planner/MaterializedViewTestBase.java
+++ fe/fe-core/src/test/java/com/starrocks/planner/MaterializedViewTestBase.java
@@ -70,7 +70,7 @@ public class MaterializedViewTestBase extends PlanTestBase {
 
         new MockUp<MaterializedView>() {
             @Mock
-            Set<String> getPartitionNamesToRefreshForMv() {
+            Set<String> getPartitionNamesToRefreshForMv(boolean isQueryRewrite) {
                 return Sets.newHashSet();
             }
         };
