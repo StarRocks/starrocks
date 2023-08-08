7f0005fdd08cb69bdcf7730cbafcdf3b207eb5cc
HangyuanLiu <460660596@qq.com>
2023-08-08 14:28:57 +0800

    [Enhancement] Support ignore show external catalog privilege item in grantsTo system table (#28644)
    Fixes #issue
    
    ## What type of PR is this:
    - [ ] BugFix
    - [ ] Feature
    - [ ] Enhancement
    - [ ] Refactor
    - [ ] UT
    - [ ] Doc
    - [ ] Tool
    
    ## Checklist:
    - [ ] I have added test cases for my bug fix or my new feature
    - [ ] This pr will affect users' behaviors
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    
    ## Bugfix cherry-pick branch check:
    - [x] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [x] 3.1
      - [x] 3.0
      - [ ] 2.5
      - [ ] 2.4
    
    Signed-off-by: HangyuanLiu <460660596@qq.com>
    (cherry picked from commit dfa6935dcf3a5841df4312e8670373f52bc46cb3)
    Signed-off-by: HangyuanLiu <460660596@qq.com>
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/catalog/system/sys/GrantsTo.java
    #	fe/fe-core/src/main/java/com/starrocks/common/Config.java

--- fe/fe-core/src/test/java/com/starrocks/catalog/InfoSchemaDbTest.java
+++ fe/fe-core/src/test/java/com/starrocks/catalog/InfoSchemaDbTest.java
@@ -14,20 +14,29 @@
 
 package com.starrocks.catalog;
 
+import com.google.common.collect.Lists;
 import com.starrocks.analysis.FunctionName;
 import com.starrocks.catalog.system.information.InfoSchemaDb;
 import com.starrocks.catalog.system.sys.GrantsTo;
 import com.starrocks.common.AnalysisException;
+import com.starrocks.common.Config;
+import com.starrocks.common.jmockit.Deencapsulation;
+import com.starrocks.connector.hive.HiveMetastore;
 import com.starrocks.privilege.AuthorizationMgr;
+import com.starrocks.privilege.ObjectType;
+import com.starrocks.privilege.PrivilegeEntry;
 import com.starrocks.qe.ConnectContext;
 import com.starrocks.qe.DDLStmtExecutor;
 import com.starrocks.server.GlobalStateMgr;
+import com.starrocks.server.MetadataMgr;
 import com.starrocks.sql.analyzer.PrivilegeStmtAnalyzer;
+import com.starrocks.sql.ast.CreateCatalogStmt;
 import com.starrocks.sql.ast.CreateFunctionStmt;
 import com.starrocks.sql.ast.CreateRoleStmt;
 import com.starrocks.sql.ast.CreateUserStmt;
 import com.starrocks.sql.ast.GrantPrivilegeStmt;
 import com.starrocks.sql.ast.RevokePrivilegeStmt;
+import com.starrocks.sql.ast.StatementBase;
 import com.starrocks.sql.ast.UserIdentity;
 import com.starrocks.thrift.TFunctionBinaryType;
 import com.starrocks.thrift.TGetGrantsToRolesOrUserItem;
@@ -36,13 +45,18 @@ import com.starrocks.thrift.TGetGrantsToRolesOrUserResponse;
 import com.starrocks.thrift.TGrantsToType;
 import com.starrocks.utframe.StarRocksAssert;
 import com.starrocks.utframe.UtFrameUtils;
+import mockit.Expectations;
 import mockit.Mock;
 import mockit.MockUp;
+import mockit.Mocked;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class InfoSchemaDbTest {
     ConnectContext ctx;
@@ -302,5 +316,105 @@ public class InfoSchemaDbTest {
         item.setPrivilege_type("USAGE");
         item.setIs_grantable(false);
         Assert.assertTrue(GrantsTo.getGrantsTo(request).grants_to.contains(item));
+    }
+
+    @Test
+    public void testShowExternalCatalogPrivilege(@Mocked HiveMetastore hiveMetastore) throws Exception {
+
+        String createCatalog = "CREATE EXTERNAL CATALOG hive_catalog_1 COMMENT \"hive_catalog\" PROPERTIES(\"type\"=\"hive\", " +
+                "\"hive.metastore.uris\"=\"thrift://127.0.0.1:9083\");";
+        StatementBase stmt = UtFrameUtils.parseStmtWithNewParser(createCatalog, ctx);
+        Assert.assertTrue(stmt instanceof CreateCatalogStmt);
+        ConnectContext connectCtx = new ConnectContext();
+        connectCtx.setGlobalStateMgr(GlobalStateMgr.getCurrentState());
+        CreateCatalogStmt statement = (CreateCatalogStmt) stmt;
+        DDLStmtExecutor.execute(statement, connectCtx);
+
+        new Expectations() {
+            {
+                hiveMetastore.getAllDatabaseNames();
+                result = Lists.newArrayList("db");
+                minTimes = 0;
+
+                hiveMetastore.getAllTableNames("db");
+                result = Lists.newArrayList("tbl");
+                minTimes = 0;
+            }
+        };
+
+        MetadataMgr metadataMgr = ctx.getGlobalStateMgr().getMetadataMgr();
+        new Expectations(metadataMgr) {
+            {
+                metadataMgr.getDb((String) any, (String) any);
+                result = new com.starrocks.catalog.Database(0, "db");
+                minTimes = 0;
+
+                metadataMgr.getTable((String) any, (String) any, (String) any);
+                result = HiveTable.builder().setHiveTableName("tbl")
+                        .setFullSchema(Lists.newArrayList(new Column("v1", Type.INT))).build();
+                minTimes = 0;
+            }
+        };
+
+        ctx.getGlobalStateMgr().changeCatalog(ctx, "hive_catalog_1");
+
+        String sql = "grant usage on catalog hive_catalog_1 to test_user";
+        GrantPrivilegeStmt grantStmt = (GrantPrivilegeStmt) UtFrameUtils.parseStmtWithNewParser(sql, ctx);
+        authorizationManager.grant(grantStmt);
+
+        sql = "grant all on all databases to test_user";
+        grantStmt = (GrantPrivilegeStmt) UtFrameUtils.parseStmtWithNewParser(sql, ctx);
+        authorizationManager.grant(grantStmt);
+
+        sql = "grant all on all tables in all databases to test_user";
+        grantStmt = (GrantPrivilegeStmt) UtFrameUtils.parseStmtWithNewParser(sql, ctx);
+        authorizationManager.grant(grantStmt);
+
+        TGetGrantsToRolesOrUserRequest request = new TGetGrantsToRolesOrUserRequest();
+        request.setType(TGrantsToType.USER);
+
+        TGetGrantsToRolesOrUserItem item1 = new TGetGrantsToRolesOrUserItem();
+        item1.setGrantee("'test_user'@'%'");
+        item1.setObject_catalog("hive_catalog_1");
+        item1.setObject_type("CATALOG");
+        item1.setPrivilege_type("USAGE");
+        item1.setIs_grantable(false);
+        Assert.assertTrue(GrantsTo.getGrantsTo(request).grants_to.contains(item1));
+
+        TGetGrantsToRolesOrUserItem item2 = new TGetGrantsToRolesOrUserItem();
+        item2.setGrantee("'test_user'@'%'");
+        item2.setObject_catalog("hive_catalog_1");
+        item2.setObject_database("db");
+        item2.setObject_name("tbl");
+        item2.setObject_type("TABLE");
+        item2.setPrivilege_type("DELETE, DROP, INSERT, SELECT, ALTER, EXPORT, UPDATE");
+        item2.setIs_grantable(false);
+        Assert.assertTrue(GrantsTo.getGrantsTo(request).grants_to.contains(item2));
+
+        TGetGrantsToRolesOrUserItem item3 = new TGetGrantsToRolesOrUserItem();
+        item3.setGrantee("'test_user'@'%'");
+        item3.setObject_catalog("hive_catalog_1");
+        item3.setObject_database("db");
+        item3.setObject_type("DATABASE");
+        item3.setPrivilege_type("CREATE TABLE, DROP, ALTER, CREATE VIEW, CREATE FUNCTION, CREATE MATERIALIZED VIEW");
+        item3.setIs_grantable(false);
+        Assert.assertTrue(GrantsTo.getGrantsTo(request).grants_to.contains(item3));
+
+        Config.enable_show_external_catalog_privilege = false;
+        if (GrantsTo.getGrantsTo(request).grants_to != null) {
+            Assert.assertFalse(GrantsTo.getGrantsTo(request).grants_to.contains(item1));
+            Assert.assertFalse(GrantsTo.getGrantsTo(request).grants_to.contains(item2));
+            Assert.assertFalse(GrantsTo.getGrantsTo(request).grants_to.contains(item3));
+        }
+        Config.enable_show_external_catalog_privilege = true;
+    }
+
+    @Test
+    public void testRoot() {
+        AuthorizationMgr authorizationMgr = GlobalStateMgr.getCurrentState().getAuthorizationMgr();
+        Map<ObjectType, List<PrivilegeEntry>> privileges =
+                authorizationManager.getTypeToPrivilegeEntryListByRole("root");
+        Set<TGetGrantsToRolesOrUserItem> s = Deencapsulation.invoke(GrantsTo.class, "getGrantItems",
+                authorizationMgr, "root", privileges);
     }
 }
