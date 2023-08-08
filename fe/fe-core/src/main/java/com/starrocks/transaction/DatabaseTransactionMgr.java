50b98daa65f6059ea99c49152a9e4b9624560346
yiming <107105845+nshangyiming@users.noreply.github.com>
2023-08-08 17:32:09 +0800

    [BugFix] Should not check `canLoad` state when finishing transaction (#28811)
    This is to fix issue introduced by #26592
    
    Txn begin before the replica is set decommissioned and publish after
    that
    will fail because of the check. In this case only check whether this txn
    has
    ever committed on the decommissioned replica is enough to prevent issue
    fixed by pr #26592 and #26788.
    
    Signed-off-by: Dejun Xia <xiadejun@starrocks.com>
    (cherry picked from commit 84276f0a22b63a685aa4f988f4981d315a4621f9)

--- fe/fe-core/src/main/java/com/starrocks/transaction/DatabaseTransactionMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/transaction/DatabaseTransactionMgr.java
@@ -820,10 +820,6 @@ public class DatabaseTransactionMgr {
                             // which means publish version task finished in replica  
                             for (Replica replica : ((LocalTablet) tablet).getImmutableReplicas()) {
                                 if (!errReplicas.contains(replica.getId())) {
-                                    // if replica not in can load state, skip it.
-                                    if (!replica.getState().canLoad()) {
-                                        continue;
-                                    }
                                     // success healthy replica condition:
                                     // 1. version is equal to partition's visible version
                                     // 2. publish version task in this replica has finished
@@ -965,10 +961,6 @@ public class DatabaseTransactionMgr {
                             for (Replica replica : ((LocalTablet) tablet).getImmutableReplicas()) {
                                 if (!errorReplicaIds.contains(replica.getId())
                                         && replica.getLastFailedVersion() < 0) {
-                                    // if replica not in can load state, skip it.
-                                    if (!replica.getState().canLoad()) {
-                                        continue;
-                                    }
                                     // if replica not commit yet, skip it. This may happen when it's just create by clone.
                                     if (!transactionState.tabletCommitInfosContainsReplica(tablet.getId(), 
                                             replica.getBackendId())) {
