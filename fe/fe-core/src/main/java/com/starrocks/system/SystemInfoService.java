0c49a89fbf9fceaf48cb8a53981229bd9c7d0847
Beans <jing.gao@outlook.com>
2023-08-09 14:06:17 +0800

    [Enhancement] show more info when calling seqChooseBackendIds (#28693)
    show more info after getting an empty list when choosing backends under
    condition
    
    Signed-off-by: beans <jing.gao@outlook.com>
    (cherry picked from commit b6d10fd2637509cef046f0adef0ce3d4287e08cd)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/system/SystemInfoService.java

--- fe/fe-core/src/main/java/com/starrocks/system/SystemInfoService.java
+++ fe/fe-core/src/main/java/com/starrocks/system/SystemInfoService.java
@@ -58,6 +58,7 @@ import com.starrocks.sql.ast.ModifyBackendAddressClause;
 import com.starrocks.system.Backend.BackendState;
 import com.starrocks.thrift.TStatusCode;
 import com.starrocks.thrift.TStorageMedium;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.validator.routines.InetAddressValidator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -75,6 +76,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class SystemInfoService {
@@ -604,28 +606,75 @@ public class SystemInfoService {
     }
 
     public List<Backend> getBackends() {
+<<<<<<< HEAD
         return idToBackendRef.values().asList();
+=======
+        return Lists.newArrayList(idToBackendRef.values());
+    }
+
+    public List<Backend> getAvailableBackends() {
+        return getBackends().stream()
+                .filter(v -> v.isAvailable())
+                .collect(Collectors.toList());
+    }
+
+    public List<ComputeNode> getComputeNodes() {
+        return Lists.newArrayList(idToComputeNodeRef.values());
+    }
+
+    public Stream<ComputeNode> backendAndComputeNodeStream() {
+        return Stream.concat(idToBackendRef.values().stream(), idToComputeNodeRef.values().stream());
+>>>>>>> b6d10fd263 ([Enhancement] show more info when calling seqChooseBackendIds (#28693))
     }
 
     public List<Long> seqChooseBackendIdsByStorageMedium(int backendNum, boolean needAvailable, boolean isCreate,
                                                          TStorageMedium storageMedium) {
-        final List<Backend> backends =
-                getBackends().stream().filter(v -> !v.diskExceedLimitByStorageMedium(storageMedium))
-                        .collect(Collectors.toList());
-        return seqChooseBackendIds(backendNum, needAvailable, isCreate, backends);
+
+        return seqChooseBackendIds(backendNum, needAvailable, isCreate,
+                v -> !v.diskExceedLimitByStorageMedium(storageMedium));
     }
 
     public List<Long> seqChooseBackendIds(int backendNum, boolean needAvailable, boolean isCreate) {
-        final List<Backend> backends =
-                getBackends().stream().filter(v -> !v.diskExceedLimit()).collect(Collectors.toList());
-        return seqChooseBackendIds(backendNum, needAvailable, isCreate, backends);
+
+        return seqChooseBackendIds(backendNum, needAvailable, isCreate, v -> !v.diskExceedLimit());
     }
 
-    // choose backends by round-robin
-    // return null if not enough backend
-    // use synchronized to run serially
-    public synchronized List<Long> seqChooseBackendIds(int backendNum, boolean needAvailable, boolean isCreate,
-                                                       final List<Backend> srcBackends) {
+    private List<Long> seqChooseBackendIds(int backendNum, boolean needAvailable, boolean isCreate,
+                                           Predicate<? super Backend> predicate) {
+
+        final List<Backend> candidateBackends = needAvailable ? getAvailableBackends() : getBackends();
+        if (CollectionUtils.isEmpty(candidateBackends)) {
+            LOG.warn("failed to find any backend, needAvailable={}", needAvailable);
+            return Collections.emptyList();
+        }
+
+        final List<Backend> filteredBackends = candidateBackends.stream()
+                .filter(predicate)
+                .collect(Collectors.toList());
+
+        if (CollectionUtils.isEmpty(filteredBackends)) {
+            String backendInfo = candidateBackends.stream()
+                    .map(backend -> "[host=" + backend.getHost() + ", bePort=" + backend.getBePort() + "]")
+                    .collect(Collectors.joining("; "));
+
+            LOG.warn(
+                    "failed to find any backend with qualified disk usage from {} candidate backends, needAvailable={}, [{}]",
+                    candidateBackends.size(), needAvailable, backendInfo);
+            return Collections.emptyList();
+        }
+        return seqChooseBackendIds(backendNum, isCreate, filteredBackends);
+    }
+
+    /**
+     * choose backends by round-robin
+     *
+     * @param backendNum  number of backend wanted
+     * @param isCreate    last backend id for creation
+     * @param srcBackends list of the candidate backends
+     * @return empty list if not enough backend, otherwise return a list of backend's id
+     */
+    private synchronized List<Long> seqChooseBackendIds(int backendNum, boolean isCreate, final List<Backend> srcBackends) {
+
         long lastBackendId;
 
         if (isCreate) {
@@ -637,18 +686,13 @@ public class SystemInfoService {
         // host -> BE list
         Map<String, List<Backend>> backendMaps = Maps.newHashMap();
         for (Backend backend : srcBackends) {
-            // If needAvailable is true, unavailable backend won't go into the pick list
-            if (needAvailable && !backend.isAvailable()) {
-                continue;
+            String host = backend.getHost();
+
+            if (!backendMaps.containsKey(host)) {
+                backendMaps.put(host, Lists.newArrayList());
             }
 
-            if (backendMaps.containsKey(backend.getHost())) {
-                backendMaps.get(backend.getHost()).add(backend);
-            } else {
-                List<Backend> list = Lists.newArrayList();
-                list.add(backend);
-                backendMaps.put(backend.getHost(), list);
-            }
+            backendMaps.get(host).add(backend);
         }
 
         // if more than one backend exists in same host, select a backend at random
@@ -700,21 +744,20 @@ public class SystemInfoService {
             failed = true;
         }
 
-        if (!failed) {
-            if (isCreate) {
-                lastBackendIdForCreation = lastBackendId;
-            } else {
-                lastBackendIdForOther = lastBackendId;
+        if (failed) {
+            // debug: print backend info when the selection failed
+            for (Backend backend : backends) {
+                LOG.debug("random select: {}", backend);
             }
-            return backendIds;
+            return Collections.emptyList();
         }
 
-        // debug
-        for (Backend backend : backends) {
-            LOG.debug("random select: {}", backend);
+        if (isCreate) {
+            lastBackendIdForCreation = lastBackendId;
+        } else {
+            lastBackendIdForOther = lastBackendId;
         }
-
-        return Collections.emptyList();
+        return backendIds;
     }
 
     public ImmutableMap<Long, Backend> getIdToBackend() {
