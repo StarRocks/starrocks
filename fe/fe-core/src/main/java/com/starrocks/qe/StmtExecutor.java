42941402054480c81509b0334411934e3d9a7c44
liuyehcf <1559500551@qq.com>
2023-08-09 08:33:14 +0800

    [Enhancement] Support text-based profile analysis(3) (#28813)
    ## Main work of this pr
    
    This pr is an continuation work of
    https://github.com/StarRocks/starrocks/pull/28479, solving the memory
    issue that the profile manager may use too many memory for caching the
    instance of `RuntimeProfile` and `ExecPlan`
    
    This pr introduce a `ProfilingExecPlan`, similar to `ExecPlan`, but only
    contains the necessary information that required by profile analysis.
    
    And we no longer cache the instance of `RuntimeProfile`, and we
    reconstructs the `RuntimeProfile` instance by parsing from plain text.
    
    ## Result
    
    The memory usage decrease from 3.1G to 287M after executing 500
    ssb/tpch/tpcds queries (the maximum cached profile is 500 by default)
    
    Signed-off-by: liuyehcf <1559500551@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
@@ -67,9 +67,12 @@ import com.starrocks.common.QueryDumpLog;
 import com.starrocks.common.Status;
 import com.starrocks.common.UserException;
 import com.starrocks.common.Version;
+import com.starrocks.common.util.CompressionUtils;
 import com.starrocks.common.util.DebugUtil;
 import com.starrocks.common.util.ProfileManager;
+import com.starrocks.common.util.ProfilingExecPlan;
 import com.starrocks.common.util.RuntimeProfile;
+import com.starrocks.common.util.RuntimeProfileParser;
 import com.starrocks.common.util.TimeUtils;
 import com.starrocks.common.util.UUIDUtil;
 import com.starrocks.connector.ConnectorMetadata;
@@ -586,7 +589,8 @@ public class StmtExecutor {
                                 writeProfile(execPlan, beginTimeInNanoSecond);
                                 if (parsedStmt.isExplain() &&
                                         StatementBase.ExplainLevel.ANALYZE.equals(parsedStmt.getExplainLevel())) {
-                                    handleExplainStmt(ExplainAnalyzer.analyze(execPlan, profile, null));
+                                    handleExplainStmt(ExplainAnalyzer.analyze(
+                                            ProfilingExecPlan.buildFrom(execPlan), profile, null));
                                 }
                             }
                             if (!isStatisticsJob) {
@@ -1065,7 +1069,9 @@ public class StmtExecutor {
         List<Integer> planNodeIds = analyzeProfileStmt.getPlanNodeIds();
         ProfileManager.ProfileElement profileElement = ProfileManager.getInstance().getProfileElement(queryId);
         Preconditions.checkNotNull(profileElement, "query not exists");
-        handleExplainStmt(ExplainAnalyzer.analyze(profileElement.plan, profileElement.profile, planNodeIds));
+        handleExplainStmt(ExplainAnalyzer.analyze(profileElement.plan,
+                RuntimeProfileParser.parseFrom(CompressionUtils.gzipDecompressString(profileElement.profileContent)),
+                planNodeIds));
     }
 
     private void executeAnalyze(AnalyzeStmt analyzeStmt, AnalyzeStatus analyzeStatus, Database db, Table table) {
@@ -1513,7 +1519,7 @@ public class StmtExecutor {
                 writeProfile(execPlan, beginTimeInNanoSecond);
                 if (parsedStmt.isExplain() &&
                         StatementBase.ExplainLevel.ANALYZE.equals(parsedStmt.getExplainLevel())) {
-                    handleExplainStmt(ExplainAnalyzer.analyze(execPlan, profile, null));
+                    handleExplainStmt(ExplainAnalyzer.analyze(ProfilingExecPlan.buildFrom(execPlan), profile, null));
                 }
             }
         } catch (Throwable t) {
