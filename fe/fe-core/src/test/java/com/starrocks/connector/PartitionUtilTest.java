f8fd9c65987461977f259e1c0e06561787f02c3d
Youngwb <yangwenbo_mailbox@163.com>
2023-08-09 20:06:36 +0800

    [BugFix] Fix query hive int partition column like '02' error (#28941)
    some hive int partition column could be '01','02', not '1','2', we need to store the origin string value.
    
    (cherry picked from commit 8b0a3f9f9161067bddcda661cb9cac55efbcf174)
    
    # Conflicts:
    #	fe/fe-core/src/test/java/com/starrocks/connector/PartitionUtilTest.java

--- fe/fe-core/src/test/java/com/starrocks/connector/PartitionUtilTest.java
+++ fe/fe-core/src/test/java/com/starrocks/connector/PartitionUtilTest.java
@@ -104,6 +104,57 @@ public class PartitionUtilTest {
     }
 
     @Test
+<<<<<<< HEAD
+=======
+    public void testHiveTimestampPartitionNames() throws AnalysisException {
+        List<String> partitionValues = Lists.newArrayList("2007-01-01 10:35:00.0", "2007-01-01 10:35:00.123");
+        List<Column> columns = new ArrayList<>();
+        columns.add(new Column("a", Type.fromPrimitiveType(PrimitiveType.DATETIME)));
+        columns.add(new Column("b", Type.fromPrimitiveType(PrimitiveType.DATETIME)));
+
+        PartitionKey partitionKey = PartitionUtil.createPartitionKey(partitionValues, columns, Table.TableType.HIVE);
+        List<String> res = PartitionUtil.fromPartitionKey(partitionKey);
+        Assert.assertEquals("2007-01-01 10:35:00.0", res.get(0));
+        Assert.assertEquals("2007-01-01 10:35:00.123", res.get(1));
+
+        partitionValues = Lists.newArrayList("2007-01-01 10:35:00", "2007-01-01 10:35:00.00",
+                "2007-01-01 10:35:00.000");
+        columns = new ArrayList<>();
+        columns.add(new Column("a", Type.fromPrimitiveType(PrimitiveType.DATETIME)));
+        columns.add(new Column("b", Type.fromPrimitiveType(PrimitiveType.DATETIME)));
+        columns.add(new Column("c", Type.fromPrimitiveType(PrimitiveType.DATETIME)));
+        partitionKey = PartitionUtil.createPartitionKey(partitionValues, columns, Table.TableType.HIVE);
+        res = PartitionUtil.fromPartitionKey(partitionKey);
+        Assert.assertEquals("2007-01-01 10:35:00", res.get(0));
+        Assert.assertEquals("2007-01-01 10:35:00.00", res.get(1));
+        Assert.assertEquals("2007-01-01 10:35:00.000", res.get(2));
+    }
+
+    @Test
+    public void testHiveIntPartitionNames() throws Exception {
+        List<String> partitionValues = Lists.newArrayList("2007-01-01", "01");
+        List<Column> columns = new ArrayList<>();
+        columns.add(new Column("a", Type.fromPrimitiveType(PrimitiveType.DATE)));
+        columns.add(new Column("b", Type.fromPrimitiveType(PrimitiveType.INT)));
+
+        PartitionKey partitionKey = PartitionUtil.createPartitionKey(partitionValues, columns, Table.TableType.HIVE);
+        List<String> res = PartitionUtil.fromPartitionKey(partitionKey);
+        Assert.assertEquals("2007-01-01", res.get(0));
+        Assert.assertEquals("01", res.get(1));
+
+        partitionValues = Lists.newArrayList("125", "0125");
+        columns = new ArrayList<>();
+        columns.add(new Column("a", Type.fromPrimitiveType(PrimitiveType.INT)));
+        columns.add(new Column("b", Type.fromPrimitiveType(PrimitiveType.INT)));
+
+        partitionKey = PartitionUtil.createPartitionKey(partitionValues, columns, Table.TableType.HIVE);
+        res = PartitionUtil.fromPartitionKey(partitionKey);
+        Assert.assertEquals("125", res.get(0));
+        Assert.assertEquals("0125", res.get(1));
+    }
+
+    @Test
+>>>>>>> 8b0a3f9f91 ([BugFix] Fix query hive int partition column like '02' error (#28941))
     public void testHivePartitionNames() {
         List<String> partitionValues = Lists.newArrayList("1", "2", "3");
         String partitionNames = "a=1/b=2/c=3";
