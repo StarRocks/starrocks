1bd70573a321628fefc196b5aba49dc97f9d5d2f
jijundu <jijundu@tencent.com>
2023-08-10 18:44:28 +0800

    [BugFix] Fix bug of upgrading from non-lsc version

--- be/src/storage/data_dir.cpp
+++ be/src/storage/data_dir.cpp
@@ -368,7 +368,7 @@ Status DataDir::load() {
             continue;
         }
         RowsetSharedPtr rowset;
-        Status create_status = RowsetFactory::create_rowset(rowset_meta->tablet_schema(), tablet->schema_hash_path(),
+        Status create_status = RowsetFactory::create_rowset(tablet->tablet_schema(), tablet->schema_hash_path(),
                                                             rowset_meta, &rowset);
         if (!create_status.ok()) {
             LOG(WARNING) << "Fail to create rowset from rowsetmeta,"
