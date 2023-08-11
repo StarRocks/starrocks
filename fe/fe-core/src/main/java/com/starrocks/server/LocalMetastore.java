789c23120c7279daa2531d0a00cd07a8039b1705
gengjun-git <gengjun@starrocks.com>
2023-08-11 11:10:55 +0800

    [BugFix] Fix table is lost bug where create/drop db and table concurrently (#28985)
    Fixes SR-19348
    The following scenes will lose table:
    T1: begin to create t (id=1) on db d(id=2)
    T2: db d(id=2) is dropped
    T3: db d(id=3) is created
    T4: table t(id=1) is created to db d(id=2)
    T5: table t(id=5) is created to db d(id=3)
    T6: there are some data load on t(id=5)
    When system restarts, there will be two create table log, but only the
    first will success, which is table t(id=1), but all the operations is
    based on t(id=5).
    
    The key point is on time T4, the check of database existence is based on
    db name, if we check based on id, the creation of t(id=1) will fail.
    
    ---------
    
    Signed-off-by: gengjun-git <gengjun@starrocks.com>
    (cherry picked from commit 00b4ae9333cc96a09a8bd88fbf24ff2d46cb10bf)

--- fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
+++ fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
@@ -1983,7 +1983,7 @@ public class LocalMetastore implements ConnectorMetadata {
         }
 
         try {
-            if (getDb(db.getFullName()) == null) {
+            if (getDb(db.getId()) == null) {
                 throw new DdlException("Database has been dropped when creating table");
             }
 
@@ -2007,6 +2007,10 @@ public class LocalMetastore implements ConnectorMetadata {
                         return;
                     }
                 }
+
+                // NOTE: The table has been added to the database, and the following procedure cannot throw exception.
+                LOG.info("Successfully create table: {}-{}, in database: {}-{}",
+                        table.getName(), table.getId(), db.getFullName(), db.getId());
 
                 CreateTableInfo createTableInfo = new CreateTableInfo(db.getFullName(), table, storageVolumeId);
                 GlobalStateMgr.getCurrentState().getEditLog().logCreateTable(createTableInfo);
