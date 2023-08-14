3c9c8578a5cc21fa3b713173f9250137a10d69d2
Alex Zhu <zhuming9011@gmail.com>
2023-08-14 19:19:38 +0800

    [Enhancement] Slows down transaction commit if compaction score is high
    Signed-off-by: Alex Zhu <zhuming9011@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/transaction/DatabaseTransactionMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/transaction/DatabaseTransactionMgr.java
@@ -398,6 +398,9 @@ public class DatabaseTransactionMgr {
                 && transactionState.getSourceType() != TransactionState.LoadJobSourceType.INSERT_STREAMING) {
             throw new TransactionCommitFailedException(TransactionCommitFailedException.NO_DATA_TO_LOAD_MSG);
         }
+        if (transactionState.getWriteEndTimeMs() < 0) {
+            transactionState.setWriteEndTimeMs(System.currentTimeMillis());
+        }
         if (!tabletCommitInfos.isEmpty()) {
             transactionState.setTabletCommitInfos(tabletCommitInfos);
         }
