5c6d7052652568aae5f08d610c0de8d2bdd24dfc
gengjun-git <gengjun@starrocks.com>
2023-08-14 14:33:20 +0800

    [Enhancement] Keep the number of migration tasks running on be to a fixed number (#29055)
    Currently, if there are huge number of tablets to migrate (SSD->HDD or
    HDD->SSD), FE will send 1000 tasks to BE every 1 minute(tablet report
    interval), which will cause tasks to pile up on BE, and end up with OOM.
    We can use AgentTaskQueue to get the number of task running on BE, and
    send (Config.tablet_sched_max_migration_task_sent_once - running_tasks)
    tasks to BE, so that the number of migration tasks running on BE can be
    kept in a fixed number.
    
    Signed-off-by: gengjun-git <gengjun@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/leader/ReportHandler.java
+++ fe/fe-core/src/main/java/com/starrocks/leader/ReportHandler.java
@@ -44,7 +44,6 @@ import com.google.common.collect.Sets;
 import com.starrocks.binlog.BinlogConfig;
 import com.starrocks.catalog.ColocateTableIndex;
 import com.starrocks.catalog.Database;
-import com.starrocks.catalog.KeysType;
 import com.starrocks.catalog.LocalTablet;
 import com.starrocks.catalog.LocalTablet.TabletStatus;
 import com.starrocks.catalog.MaterializedIndex;
@@ -982,20 +981,6 @@ public class ReportHandler extends Daemon {
             for (int i = 0; i < tabletMetaList.size(); i++) {
                 long tabletId = tabletIds.get(i);
                 TabletMeta tabletMeta = tabletMetaList.get(i);
-                Database db = GlobalStateMgr.getCurrentState().getDb(tabletMeta.getDbId());
-                if (db == null) {
-                    continue;
-                }
-                db.readLock();
-                try {
-                    OlapTable table = (OlapTable) db.getTable(tabletMeta.getTableId());
-                    if (table.getKeysType() == KeysType.PRIMARY_KEYS) {
-                        // Currently, primary key table doesn't support tablet migration between local disks.
-                        continue;
-                    }
-                } finally {
-                    db.readUnlock();
-                }
                 // always get old schema hash(as effective one)
                 int effectiveSchemaHash = tabletMeta.getOldSchemaHash();
                 StorageMediaMigrationTask task = new StorageMediaMigrationTask(backendId, tabletId,
@@ -1004,6 +989,7 @@ public class ReportHandler extends Daemon {
             }
         }
 
+        AgentTaskQueue.addBatchTask(batchTask);
         AgentTaskExecutor.submit(batchTask);
     }
 
