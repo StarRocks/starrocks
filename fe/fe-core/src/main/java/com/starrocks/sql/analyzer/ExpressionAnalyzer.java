d333ff42ce907139a5bb50dfb4714c2dfb4e776f
Zhuhe Fang <fzhedu@gmail.com>
2023-08-15 17:41:08 +0800

    updated
    Signed-off-by: Zhuhe Fang <fzhedu@gmail.com>
    (cherry picked from commit 7b33588156f4adea42a78f54220e96f8ab604917)

--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/ExpressionAnalyzer.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/ExpressionAnalyzer.java
@@ -1189,6 +1189,9 @@ public class ExpressionAnalyzer {
                     break;
                 case FunctionSet.GROUP_CONCAT:
                 case FunctionSet.ARRAY_AGG: {
+                    if (node.getChildren().size() == 0) {
+                        throw new SemanticException(fnName + " should have at least one inputs", node.getPos());
+                    }
                     int start = 1;
                     if (fnName.equals(FunctionSet.GROUP_CONCAT)) {
                         start = argumentTypes.length - node.getParams().getOrderByElemNum();
