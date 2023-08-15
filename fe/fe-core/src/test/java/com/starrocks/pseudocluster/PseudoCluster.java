27cdd80cbe44c8ccceac1dce1ab8a128242d7322
Murphy <96611012+mofeiatwork@users.noreply.github.com>
2023-08-15 10:50:42 +0800

    [BugFix] support rewrite element_at function (#29182)
    Fixes #issue
    
    ## What type of PR is this:
    - [x] BugFix
    - [ ] Feature
    - [ ] Enhancement
    - [ ] Refactor
    - [ ] UT
    - [ ] Doc
    - [ ] Tool
    
    ## Checklist:
    - [ ] I have added test cases for my bug fix or my new feature
    - [ ] This pr will affect users' behaviors
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    
    ## Bugfix cherry-pick branch check:
    - [ ] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [x] 3.1
      - [x] 3.0
      - [x] 2.5
      - [ ] 2.4
    
    Signed-off-by: Murphy <mofei@starrocks.com>

--- fe/fe-core/src/test/java/com/starrocks/pseudocluster/PseudoCluster.java
+++ fe/fe-core/src/test/java/com/starrocks/pseudocluster/PseudoCluster.java
@@ -327,6 +327,7 @@ public class PseudoCluster {
                     System.out.println("retry execute " + sql);
                     continue;
                 }
+                LOG.error(e);
                 throw e;
             }
         }
