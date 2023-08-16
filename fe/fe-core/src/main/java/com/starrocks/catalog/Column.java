775b98eb830a664b76a9d14da06dbf65e1f327f5
Murphy <mofei@starrocks.com>
2023-08-16 14:26:00 +0800

    fix column hashCode
    Signed-off-by: Murphy <mofei@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/Column.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/Column.java
@@ -623,7 +623,7 @@ public class Column implements Writable, GsonPreProcessable, GsonPostProcessable
 
     @Override
     public int hashCode() {
-        return Objects.hash(this.name, this.type);
+        return Objects.hash(this.name.toLowerCase(), this.type);
     }
 
     @Override
