f0b5a58a67f9bc9daa39db4e0e2d0bc3d13a997f
Seaven <seaven_7@qq.com>
2023-08-16 14:27:21 +0800

    [BugFix] Fix ConcurrentModificationException error on query (#29000)
    * [BugFix] Fix ConcurrentModificationException error on query
    
    Signed-off-by: Seaven <seaven_7@qq.com>
    (cherry picked from commit eba980601b0cf2f2f2ca64da27beb1dded1d103a)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/catalog/OlapTable.java

--- fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
@@ -283,6 +283,8 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
     public static final String ENABLE_OPTIMIZER_REWRITE_GROUPINGSETS_TO_UNION_ALL =
             "enable_rewrite_groupingsets_to_union_all";
 
+    public static final String CBO_USE_DB_LOCK = "cbo_use_lock_db";
+
     // --------  New planner session variables end --------
 
     // Type of compression of transmitted data
@@ -719,6 +721,9 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
 
     @VarAttr(name = ENABLE_SQL_DIGEST, flag = VariableMgr.INVISIBLE)
     private boolean enableSQLDigest = false;
+
+    @VarAttr(name = CBO_USE_DB_LOCK, flag = VariableMgr.INVISIBLE)
+    private boolean cboUseDBLock = false;
 
     /*
      * the parallel exec instance num for one Fragment in one BE
@@ -1301,6 +1306,10 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
 
     @VarAttr(name = ENABLE_STRICT_TYPE, flag = VariableMgr.INVISIBLE)
     private boolean enableStrictType = false;
+
+    public boolean isCboUseDBLock() {
+        return cboUseDBLock;
+    }
 
     public int getStatisticCollectParallelism() {
         return statisticCollectParallelism;
