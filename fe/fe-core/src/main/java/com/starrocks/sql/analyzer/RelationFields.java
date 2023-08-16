6f4eb24c77f2dd774a8ce7371ccc7743fb6e1bf9
Murphy <mofei@starrocks.com>
2023-08-16 15:15:02 +0800

    fix multimap
    Signed-off-by: Murphy <mofei@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/RelationFields.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/RelationFields.java
@@ -15,11 +15,11 @@
 package com.starrocks.sql.analyzer;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.TreeMultimap;
+import com.google.common.collect.ImmutableListMultimap;
+import com.google.common.collect.Multimap;
 import com.starrocks.analysis.SlotRef;
 import com.starrocks.analysis.TableName;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,7 +31,7 @@ public class RelationFields {
     private final List<Field> allFields;
 
     // NOTE: sort fields by name to speedup resolve performance
-    private final TreeMultimap<String, Field> names;
+    private final Multimap<String, Field> names;
     private final boolean resolveStruct;
 
     public RelationFields(Field... fields) {
@@ -43,11 +43,8 @@ public class RelationFields {
         this.allFields = ImmutableList.copyOf(fields);
         this.resolveStruct = fields.stream().anyMatch(x -> x.getType().isStructType());
         if (!resolveStruct) {
-            this.names =
-                    TreeMultimap.create(String.CASE_INSENSITIVE_ORDER, Comparator.comparing(Field::getName));
-            for (Field field : fields) {
-                this.names.put(field.getName(), field);
-            }
+            this.names = this.allFields.stream().collect(ImmutableListMultimap.toImmutableListMultimap(
+                    x -> x.getName().toLowerCase(), x -> x));
         } else {
             this.names = null;
         }
@@ -86,7 +83,8 @@ public class RelationFields {
         // Resolve the slot based on column name first, then table name
         // For the case a table with thousands of columns, resolve by table name could not reduce the cardinality,
         // but resolve by column name first could reduce it a lot
-        List<Field> resolved = names.get(name.getColumnName()).stream().collect(ImmutableList.toImmutableList());
+        List<Field> resolved =
+                names.get(name.getColumnName().toLowerCase()).stream().collect(ImmutableList.toImmutableList());
         if (name.getTblNameWithoutAnalyzed() == null) {
             return resolved;
         } else {
