3036a05fe84d4afb4e470c8c7a033d6f1ca7fcd5
Seaven <seaven_7@qq.com>
2023-08-16 14:48:56 +0800

    [Enhancment] support limit offset (#28726)
    Signed-off-by: Seaven <seaven_7@qq.com>

--- be/src/exec/exchange_node.cpp
+++ be/src/exec/exchange_node.cpp
@@ -254,7 +254,7 @@ pipeline::OpFactories ExchangeNode::decompose_to_pipeline(pipeline::PipelineBuil
         exchange_source_op->set_degree_of_parallelism(context->degree_of_parallelism());
         operators.emplace_back(exchange_source_op);
     } else {
-        if (_is_parallel_merge) {
+        if (_is_parallel_merge && !_sort_exec_exprs.is_constant_lhs_ordering()) {
             auto exchange_merge_sort_source_operator = std::make_shared<ExchangeParallelMergeSourceOperatorFactory>(
                     context->next_operator_id(), id(), _num_senders, _input_row_desc, &_sort_exec_exprs, _is_asc_order,
                     _nulls_first, _offset, _limit);
