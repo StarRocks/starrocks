8106abb1352efcf8f6eb6d493708143e44e71d20
Yixin Luo <18810541851@163.com>
2023-08-17 15:37:48 +0800

    [Feature] support new compaction strategy for persistent index (backport #29206) (#29251)
    * [Feature] support new compaction strategy for persistent index (#27577)
    
    More detail about this feature, check this #27581
    
    In this PR, it mainly includes the following modifications:
    
    1. Introducing the l2 layer to transform merge compaction into minor
    compaction. By moving l1 to l2 each time, minor compaction can be
    accomplished. Another benefit of introducing l2 is to facilitate
    concurrent control between the apply thread and major compaction thread
    in the code.
    2. Adding a major compaction thread pool to periodically merge multiple
    l2 files into one l2 file.
    
    And also add three configs:
    ```
    // Control if using the minor compaction strategy
    enable_pindex_minor_compaction = true
    // if l2 num is larger than this, stop doing minor compaction, add this config to prevent l2 grow too large.
    max_allow_pindex_l2_num = 5
    // control the background compaction threads, default value is equal to cpu number
    pindex_major_compaction_num_threads = 0
    // control the persistent index schedule compaction interval
    pindex_major_compaction_schedule_interval_seconds = 15
    ```
    
    By enable minor compaction, we can achieve better tail latency, thereby
    reducing overall latency fluctuations.
    
    total record count | record count per upsert | disable minor compaction
    | enable major compaction
    -- | -- | -- | --
    5000000 | 1000 | 2.485 s | 92 ms
    10000000 | 1000 | 5.069 s | 184 ms
    5000000 | 5000 | 2.452 s | 104 ms
    10000000 | 5000 | 5.021 s | 192 ms
    
    ---
    
    Signed-off-by: luohaha <18810541851@163.com>
    
    * [Enhancement] improve persistent index l0 memory usage (#28769)
    
    This PR will improve persistent index l0 memory usage, and add some
    config items:
    ```
    CONF_mInt64(l0_min_mem_usage, "2097152");   // 2MB
    CONF_mInt64(l0_max_mem_usage, "104857600"); // 100MB
    ```
    
    Whether l0 should flush or merge into l1, isn't longer controlled by
    `l0_l1_merge_ratio` any more. L0 should flush or merge into l1, when:
    ```
    1. l0 memory usage >= config::l0_max_mem_usage, or
    2. l0 memory usage >= config::l0_min_mem_usage && update module memory usage is in urgent
    ```
    
    ---------
    
    Signed-off-by: luohaha <18810541851@163.com>

--- be/src/storage/update_manager.h
+++ be/src/storage/update_manager.h
@@ -35,6 +35,7 @@ class MemTracker;
 class KVStore;
 class RowsetUpdateState;
 class Tablet;
+class PersistentIndexCompactionManager;
 
 class LocalDelvecLoader : public DelvecLoader {
 public:
@@ -76,6 +77,7 @@ public:
 
     ThreadPool* apply_thread_pool() { return _apply_thread_pool.get(); }
     ThreadPool* get_pindex_thread_pool() { return _get_pindex_thread_pool.get(); }
+    PersistentIndexCompactionManager* get_pindex_compaction_mgr() { return _persistent_index_compaction_mgr.get(); }
 
     DynamicCache<uint64_t, PrimaryIndex>& index_cache() { return _index_cache; }
 
@@ -129,6 +131,7 @@ private:
 
     std::unique_ptr<ThreadPool> _apply_thread_pool;
     std::unique_ptr<ThreadPool> _get_pindex_thread_pool;
+    std::unique_ptr<PersistentIndexCompactionManager> _persistent_index_compaction_mgr;
 
     UpdateManager(const UpdateManager&) = delete;
     const UpdateManager& operator=(const UpdateManager&) = delete;
