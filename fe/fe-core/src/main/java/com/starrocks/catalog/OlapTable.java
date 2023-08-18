cc1b093572c4aec46026694e3b986f7331af6c3d
kangkaisen <kangkaisen@apache.org>
2023-08-18 13:30:36 +0800

    [BugFix] Fix typo in TableProperty copy (#29463)
    Signed-off-by: kangkaisen <kangkaisen@apache.org>
    (cherry picked from commit 195bc8c8cc851c99b41d90374190f40fb113c8b7)

--- fe/fe-core/src/main/java/com/starrocks/catalog/OlapTable.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/OlapTable.java
@@ -301,7 +301,9 @@ public class OlapTable extends Table {
         olapTable.indexNameToId = Maps.newHashMap(this.indexNameToId);
         olapTable.indexIdToMeta = Maps.newHashMap(this.indexIdToMeta);
         olapTable.keysType = this.keysType;
-        olapTable.partitionInfo = (PartitionInfo) this.partitionInfo.clone();
+        if (this.partitionInfo != null) {
+            olapTable.partitionInfo = (PartitionInfo) this.partitionInfo.clone();
+        }
         olapTable.defaultDistributionInfo = this.defaultDistributionInfo;
         Map<Long, Partition> idToPartitions = new HashMap<>(this.idToPartition.size());
         Map<String, Partition> nameToPartitions = Maps.newTreeMap(String.CASE_INSENSITIVE_ORDER);
@@ -2071,7 +2073,7 @@ public class OlapTable extends Table {
 
     public void setHasDelete() {
         if (tableProperty == null) {
-            return;
+            tableProperty = new TableProperty(new HashMap<>());
         }
         tableProperty.setHasDelete(true);
     }
@@ -2094,7 +2096,7 @@ public class OlapTable extends Table {
 
     public void setHasForbitGlobalDict(boolean hasForbitGlobalDict) {
         if (tableProperty == null) {
-            return;
+            tableProperty = new TableProperty(new HashMap<>());
         }
         tableProperty.setHasForbitGlobalDict(hasForbitGlobalDict);
     }
