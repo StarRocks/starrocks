a9684fddb19ce00f41d18555744b295c5919c9e7
gengjun-git <gengjun@starrocks.com>
2023-08-18 10:12:07 +0800

    Fix dead lock on planner when there are multi dbs (#29432)
    Signed-off-by: gengjun-git <gengjun@starrocks.com>
    (cherry picked from commit 2e6b4604c818335c104b7de6eb7e0be33d1cfcf4)

--- fe/fe-core/src/main/java/com/starrocks/sql/StatementPlanner.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/StatementPlanner.java
@@ -44,6 +44,8 @@ import com.starrocks.sql.plan.ExecPlan;
 import com.starrocks.sql.plan.PlanFragmentBuilder;
 import com.starrocks.thrift.TResultSinkType;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -239,7 +241,9 @@ public class StatementPlanner {
         if (dbs == null) {
             return;
         }
-        for (Database db : dbs.values()) {
+        List<Database> dbList = new ArrayList<>(dbs.values());
+        dbList.sort(Comparator.comparingLong(Database::getId));
+        for (Database db : dbList) {
             db.readLock();
         }
     }
