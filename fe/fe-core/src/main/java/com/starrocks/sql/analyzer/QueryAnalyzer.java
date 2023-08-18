1802671efa11c372556a48e78c467c3d7314e797
Seaven <seaven_7@qq.com>
2023-08-18 10:52:12 +0800

    up
    Signed-off-by: Seaven <seaven_7@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/QueryAnalyzer.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/QueryAnalyzer.java
@@ -694,14 +694,15 @@ public class QueryAnalyzer {
                     .collect(Collectors.toMap(c -> c.getName().toLowerCase(), c -> c));
 
             for (Field field : fields) {
-                if (!columns.containsKey(field.getName().toLowerCase())) {
+                String name = field.getName().toLowerCase();
+                if (!columns.containsKey(name)) {
                     throw new SemanticException(
                             "Found undefined column[%s] from View[%s]'s query, " +
                                     "please check the source table has been modified", field.getName(),
                             node.getName().toSql());
                 }
 
-                Column column = columns.get(field.getName());
+                Column column = columns.get(name);
                 if (!column.getType().matchesType(field.getType())) {
                     throw new SemanticException("The type of column[%s] on View[%s] is different with query, " +
                             "please check the source table has been modified", column.getName(),
