5eaa9b113f01564ddbcc620e3a5b543f54e4f82a
Murphy <96611012+mofeiatwork@users.noreply.github.com>
2023-08-21 10:22:38 +0800

    fix leak of coordinator of DML (#29535)
    Signed-off-by: Murphy <mofei@starrocks.com>
    (cherry picked from commit 1bea933259bb19acf33ecda755fc64e6d61c58d4)

--- fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
@@ -1696,6 +1696,7 @@ public class StmtExecutor {
             }
             throw new UserException(t.getMessage());
         } finally {
+            QeProcessorImpl.INSTANCE.unregisterQuery(context.getExecutionId());
             if (insertError) {
                 try {
                     if (jobId != -1) {
