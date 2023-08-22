ed75344c699b6d78655f1c67fc0c6c40b7345fcc
Smith Cruise <chendingchao1@126.com>
2023-08-22 13:41:13 +0800

    [Enhancement][Cherry-Pick][Branch-3.1] Improve credential mask util (#29654)
    Signed-off-by: Smith Cruise <chendingchao1@126.com>

--- fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
@@ -141,7 +141,7 @@ import com.starrocks.connector.exception.StarRocksConnectorException;
 import com.starrocks.connector.hive.ConnectorTableMetadataProcessor;
 import com.starrocks.connector.hive.events.MetastoreEventsProcessor;
 import com.starrocks.consistency.ConsistencyChecker;
-import com.starrocks.credential.CloudCredentialUtil;
+import com.starrocks.credential.CredentialUtil;
 import com.starrocks.external.starrocks.StarRocksRepository;
 import com.starrocks.ha.FrontendNodeType;
 import com.starrocks.ha.HAProtocol;
@@ -2863,7 +2863,7 @@ public class GlobalStateMgr {
         } else if (table.getType() == TableType.FILE) {
             FileTable fileTable = (FileTable) table;
             Map<String, String> clonedFileProperties = new HashMap<>(fileTable.getFileProperties());
-            CloudCredentialUtil.maskCloudCredential(clonedFileProperties);
+            CredentialUtil.maskCredential(clonedFileProperties);
             if (!Strings.isNullOrEmpty(table.getComment())) {
                 sb.append("\nCOMMENT \"").append(table.getComment()).append("\"");
             }
