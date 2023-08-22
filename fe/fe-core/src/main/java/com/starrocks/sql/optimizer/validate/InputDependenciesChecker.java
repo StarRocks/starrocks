88c941656611358456977b218d1eace94ded0412
packy92 <110370499+packy92@users.noreply.github.com>
2023-08-22 11:08:08 +0800

    [BugFix] fix mistakenly pushed-down subfield to child (#29504)
    ---------
    
    Signed-off-by: packy <wangchao@starrocks.com>
    Signed-off-by: packy92 <wangchao@starrocks.com>
    (cherry picked from commit 6c652e22d1751684c56d5fea1e172203e0192304)

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/validate/InputDependenciesChecker.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/validate/InputDependenciesChecker.java
@@ -20,12 +20,17 @@ import com.starrocks.sql.optimizer.OptExpression;
 import com.starrocks.sql.optimizer.OptExpressionVisitor;
 import com.starrocks.sql.optimizer.RowOutputInfo;
 import com.starrocks.sql.optimizer.base.ColumnRefSet;
+import com.starrocks.sql.optimizer.operator.ColumnOutputInfo;
 import com.starrocks.sql.optimizer.operator.Operator;
 import com.starrocks.sql.optimizer.operator.logical.LogicalCTEConsumeOperator;
 import com.starrocks.sql.optimizer.operator.logical.LogicalJoinOperator;
+import com.starrocks.sql.optimizer.operator.logical.LogicalScanOperator;
 import com.starrocks.sql.optimizer.operator.logical.LogicalSetOperator;
+import com.starrocks.sql.optimizer.operator.logical.LogicalValuesOperator;
 import com.starrocks.sql.optimizer.operator.physical.PhysicalJoinOperator;
+import com.starrocks.sql.optimizer.operator.physical.PhysicalScanOperator;
 import com.starrocks.sql.optimizer.operator.physical.PhysicalSetOperation;
+import com.starrocks.sql.optimizer.operator.physical.PhysicalValuesOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ColumnRefOperator;
 import com.starrocks.sql.optimizer.task.TaskContext;
 
@@ -54,7 +59,7 @@ public class InputDependenciesChecker implements PlanValidator.Checker {
         public Void visit(OptExpression optExpression, Void context) {
             Operator operator = optExpression.getOp();
             if (optExpression.arity() == 0) {
-                return context;
+                checkOptWithoutChild(optExpression);
             } else if (optExpression.arity() == 1) {
                 checkOptExprWithOneChild(optExpression);
             } else if (operator instanceof LogicalJoinOperator || operator instanceof PhysicalJoinOperator) {
@@ -67,6 +72,27 @@ public class InputDependenciesChecker implements PlanValidator.Checker {
                 }
             }
             return context;
+        }
+
+        private void checkOptWithoutChild(OptExpression optExpression) {
+            RowOutputInfo rowOutputInfo = optExpression.getRowOutputInfo();
+            Operator operator = optExpression.getOp();
+            if (operator instanceof LogicalScanOperator || operator instanceof PhysicalScanOperator
+                    || operator instanceof LogicalValuesOperator || operator instanceof PhysicalValuesOperator) {
+                ColumnRefSet inputCols = ColumnRefSet.createByIds(rowOutputInfo.getOriginalColOutputInfo().keySet());
+                ColumnRefSet usedCols = new ColumnRefSet();
+                for (ColumnOutputInfo col : rowOutputInfo.getColumnOutputInfo()) {
+                    usedCols.union(col.getUsedColumns());
+                }
+                for (ColumnOutputInfo col : rowOutputInfo.getCommonColInfo()) {
+                    usedCols.union(col.getUsedColumns());
+                }
+
+                for (ColumnOutputInfo col : rowOutputInfo.getCommonColInfo()) {
+                    usedCols.except(col.getColumnRef().getUsedColumns());
+                }
+                checkInputCols(inputCols, usedCols, optExpression);
+            }
         }
 
         private void checkOptExprWithOneChild(OptExpression optExpression) {
