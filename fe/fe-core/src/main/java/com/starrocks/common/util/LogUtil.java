f61c83cf479f0c9aed4b8e376148fc54f50b28f9
packy92 <110370499+packy92@users.noreply.github.com>
2023-08-23 14:52:26 +0800

    [BugFix] keep comments in sql when audit it (#29747)
    Signed-off-by: packy <wangchao@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/common/util/LogUtil.java
+++ fe/fe-core/src/main/java/com/starrocks/common/util/LogUtil.java
@@ -68,7 +68,8 @@ public class LogUtil {
                 .collect(Collectors.joining(System.lineSeparator(), System.lineSeparator(), ""));
     }
 
-    public static String removeCommentAndLineSeparator(String origStmt) {
+    // just remove redundant spaces, tabs and line separators
+    public static String removeLineSeparator(String origStmt) {
         char inStringStart = '-';
 
         StringBuilder sb = new StringBuilder();
@@ -92,20 +93,12 @@ public class LogUtil {
                     character == '#') {
                 // process comment style like '-- comment' or '# comment'
                 while (idx < length - 1 && origStmt.charAt(idx) != '\n') {
+                    appendChar(sb, origStmt.charAt(idx));
                     ++idx;
                 }
-                appendChar(sb, ' ');
-            } else if (character == '/' && idx != length - 2 &&
-                    origStmt.charAt(idx + 1) == '*' && origStmt.charAt(idx + 2) != '+') {
-                //  process comment style like '/* comment */'
-                while (idx < length - 1 && (origStmt.charAt(idx) != '*' || origStmt.charAt(idx + 1) != '/')) {
-                    ++idx;
-                }
-                ++idx;
-                appendChar(sb, ' ');
-            } else if (character == '/' && idx != origStmt.length() - 2 &&
-                    origStmt.charAt(idx + 1) == '*' && origStmt.charAt(idx + 2) == '+') {
-                //  process hint
+                appendChar(sb, '\n');
+            } else if (character == '/' && idx != origStmt.length() - 1 && origStmt.charAt(idx + 1) == '*') {
+                //  process comment style like '/* comment */' or hint
                 while (idx < length - 1 && (origStmt.charAt(idx) != '*' || origStmt.charAt(idx + 1) != '/')) {
                     appendChar(sb, origStmt.charAt(idx));
                     idx++;
