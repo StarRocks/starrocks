9681002bdd70fae84bf353bb3d9f2b8edaa6c78d
Youngwb <yangwenbo_mailbox@163.com>
2023-08-23 10:44:45 +0800

    [Enhancement] Compatible with the default partition value of the new hudi version (#29606)
    Signed-off-by: Youngwb <yangwenbo_mailbox@163.com>

--- fe/fe-core/src/test/java/com/starrocks/connector/PartitionUtilTest.java
+++ fe/fe-core/src/test/java/com/starrocks/connector/PartitionUtilTest.java
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 package com.starrocks.connector;
 
 import com.google.common.collect.ImmutableList;
@@ -23,6 +22,7 @@ import com.starrocks.analysis.BoolLiteral;
 import com.starrocks.analysis.DateLiteral;
 import com.starrocks.analysis.LiteralExpr;
 import com.starrocks.catalog.Column;
+import com.starrocks.catalog.DeltaLakeTable;
 import com.starrocks.catalog.HiveTable;
 import com.starrocks.catalog.PartitionKey;
 import com.starrocks.catalog.PrimitiveType;
@@ -34,6 +34,7 @@ import com.starrocks.common.UserException;
 import com.starrocks.connector.exception.StarRocksConnectorException;
 import com.starrocks.connector.hive.HiveMetaClient;
 import com.starrocks.connector.hive.HivePartitionName;
+import com.starrocks.connector.iceberg.IcebergApiConverter;
 import mockit.Expectations;
 import mockit.Mock;
 import mockit.MockUp;
@@ -67,7 +68,39 @@ public class PartitionUtilTest {
     @Test
     public void testCreateHudiPartitionKey() throws AnalysisException {
         PartitionKey partitionKey = createPartitionKey(
-                Lists.newArrayList("1", "a", "3.0", HiveMetaClient.HUDI_PARTITION_NULL_VALUE), partColumns, Table.TableType.HUDI);
+                Lists.newArrayList("1", "a", "3.0", HiveMetaClient.HUDI_PARTITION_NULL_VALUE), partColumns,
+                Table.TableType.HUDI);
+        Assert.assertEquals("(\"1\", \"a\", \"3.0\", \"NULL\")", partitionKey.toSql());
+        List<String> res = PartitionUtil.fromPartitionKey(partitionKey);
+        Assert.assertEquals("1", res.get(0));
+        Assert.assertEquals("a", res.get(1));
+        Assert.assertEquals("3.0", res.get(2));
+        Assert.assertEquals(HiveMetaClient.HUDI_PARTITION_NULL_VALUE, res.get(3));
+
+        partitionKey = createPartitionKey(
+                Lists.newArrayList("1", "a", "3.0", HiveMetaClient.PARTITION_NULL_VALUE), partColumns,
+                Table.TableType.HUDI);
+        Assert.assertEquals("(\"1\", \"a\", \"3.0\", \"NULL\")", partitionKey.toSql());
+        res = PartitionUtil.fromPartitionKey(partitionKey);
+        Assert.assertEquals("1", res.get(0));
+        Assert.assertEquals("a", res.get(1));
+        Assert.assertEquals("3.0", res.get(2));
+        Assert.assertEquals(HiveMetaClient.PARTITION_NULL_VALUE, res.get(3));
+    }
+
+    @Test
+    public void testCreateIcebergPartitionKey() throws AnalysisException {
+        PartitionKey partitionKey = createPartitionKey(
+                Lists.newArrayList("1", "a", "3.0", IcebergApiConverter.PARTITION_NULL_VALUE), partColumns,
+                Table.TableType.ICEBERG);
+        Assert.assertEquals("(\"1\", \"a\", \"3.0\", \"NULL\")", partitionKey.toSql());
+    }
+
+    @Test
+    public void testCreateDeltaLakePartitionKey() throws AnalysisException {
+        PartitionKey partitionKey = createPartitionKey(
+                Lists.newArrayList("1", "a", "3.0", DeltaLakeTable.PARTITION_NULL_VALUE), partColumns,
+                Table.TableType.DELTALAKE);
         Assert.assertEquals("(\"1\", \"a\", \"3.0\", \"NULL\")", partitionKey.toSql());
     }
 
@@ -92,7 +125,7 @@ public class PartitionUtilTest {
 
     @Test
     public void testToPartitionValues() {
-        String  partitionNames = "a=1/b=2/c=3";
+        String partitionNames = "a=1/b=2/c=3";
         Assert.assertEquals(Lists.newArrayList("1", "2", "3"), toPartitionValues(partitionNames));
     }
 
