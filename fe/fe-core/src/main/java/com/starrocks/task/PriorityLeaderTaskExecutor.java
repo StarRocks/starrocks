d2c227443dc906780b5f4bf00025830878621ba5
wyb <wybb86@gmail.com>
2023-08-25 19:48:12 +0800

    [BugFix] Update corePoolSize before maximumPoolSize in task executor (#29964)
    Must set corePoolSize first, because setMaximumPoolSize() will throw IllegalArgumentException
    if maximumPoolSize < corePoolSize.
    
    Signed-off-by: wyb <wybb86@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/task/PriorityLeaderTaskExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/task/PriorityLeaderTaskExecutor.java
@@ -111,9 +111,11 @@ public class PriorityLeaderTaskExecutor {
     }
 
     public void setPoolSize(int poolSize) {
-        // corePoolSize and maximumPoolSize are same
-        executor.setMaximumPoolSize(poolSize);
+        // corePoolSize and maximumPoolSize are same.
+        // Must set corePoolSize first, because setMaximumPoolSize() will throw IllegalArgumentException
+        // if maximumPoolSize < corePoolSize.
         executor.setCorePoolSize(poolSize);
+        executor.setMaximumPoolSize(poolSize);
     }
 
     private class TaskChecker implements Runnable {
