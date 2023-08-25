8429c572012adb9befa7594d81a01f866af0d95d
ZiheLiu <ziheliu1024@gmail.com>
2023-08-25 14:26:10 +0800

    [BugFix] Fix split constant non-ASCII source and empty delimiter (#29818)
    Signed-off-by: ZiheLiu <ziheliu1024@gmail.com>

--- be/src/util/utf8.h
+++ be/src/util/utf8.h
@@ -164,4 +164,51 @@ static inline Slice utf8_char_start(const char* end) {
     return {p, count};
 }
 
+// Modify from https://github.com/lemire/fastvalidate-utf-8/blob/master/include/simdasciicheck.h
+static inline bool validate_ascii_fast(const char* src, size_t len) {
+#ifdef __AVX2__
+    size_t i = 0;
+    __m256i has_error = _mm256_setzero_si256();
+    if (len >= 32) {
+        for (; i <= len - 32; i += 32) {
+            __m256i current_bytes = _mm256_loadu_si256((const __m256i*)(src + i));
+            has_error = _mm256_or_si256(has_error, current_bytes);
+        }
+    }
+    int error_mask = _mm256_movemask_epi8(has_error);
+
+    char tail_has_error = 0;
+    for (; i < len; i++) {
+        tail_has_error |= src[i];
+    }
+    error_mask |= (tail_has_error & 0x80);
+
+    return !error_mask;
+#elif defined(__SSE2__)
+    size_t i = 0;
+    __m128i has_error = _mm_setzero_si128();
+    if (len >= 16) {
+        for (; i <= len - 16; i += 16) {
+            __m128i current_bytes = _mm_loadu_si128((const __m128i*)(src + i));
+            has_error = _mm_or_si128(has_error, current_bytes);
+        }
+    }
+    int error_mask = _mm_movemask_epi8(has_error);
+
+    char tail_has_error = 0;
+    for (; i < len; i++) {
+        tail_has_error |= src[i];
+    }
+    error_mask |= (tail_has_error & 0x80);
+
+    return !error_mask;
+#else
+    char tail_has_error = 0;
+    for (size_t i = 0; i < len; i++) {
+        tail_has_error |= src[i];
+    }
+    return !(tail_has_error & 0x80);
+#endif
+}
+
 } // namespace starrocks::vectorized
