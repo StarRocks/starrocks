661b31743e45133b216539cca28edf0c80c0fc45
leoyy0316 <571684903@qq.com>
2023-08-25 19:42:26 +0800

    fix brokerload failed when data size greater than the type defined by orc
    Signed-off-by: leoyy0316 <571684903@qq.com>

--- be/src/formats/orc/column_reader.cpp
+++ be/src/formats/orc/column_reader.cpp
@@ -547,7 +547,7 @@ Status StringColumnReader::get_next(orc::ColumnVectorBatch* cvb, ColumnPtr& col,
             if (strict_mode) {
                 for (int i = 0; i < size; i++) {
                     // overflow.
-                    if (nulls[i] == 0 && _type.len > 0 && data->length[i] > _type.len) {
+                    if (nulls[i] == 0 && type().len > 0 && data->length[i] > type().len) {
                         filter[i] = 0;
                         if (!reported) {
                             reported = true;
