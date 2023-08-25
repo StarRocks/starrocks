495528d05d050137c351570f034b789b104e7ad8
wyb <wybb86@gmail.com>
2023-08-25 16:28:25 +0800

    [BugFix] Fix BE crash on bitmap index filter (#29938)
    #26246 adds tablet_schema in SegmentReadOptions and tablet_schema is not set when creating cloud native table segment iterator.
    
    Signed-off-by: wyb <wybb86@gmail.com>

--- be/src/storage/lake/rowset.cpp
+++ be/src/storage/lake/rowset.cpp
@@ -51,6 +51,7 @@ StatusOr<std::vector<ChunkIteratorPtr>> Rowset::read(const Schema& schema, const
     seg_options.global_dictmaps = options.global_dictmaps;
     seg_options.unused_output_column_ids = options.unused_output_column_ids;
     seg_options.runtime_range_pruner = options.runtime_range_pruner;
+    seg_options.tablet_schema = options.tablet_schema;
     seg_options.fill_data_cache = options.fill_data_cache;
     if (options.is_primary_keys) {
         seg_options.is_primary_keys = true;
