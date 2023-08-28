141c4e66094954fe1413f908d8b33f6cef01b30b
Felix Li <astralidea@163.com>
2023-08-28 10:06:13 +0800

    Support storage_cooldown_ttl = "" (#29975)
    Signed-off-by: Astralidea <astralidea@163.com>
    (cherry picked from commit 93658a8b4a787d45c9b5903067bd054d59fc6f95)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/sql/ast/SinglePartitionDesc.java

--- fe/fe-core/src/main/java/com/starrocks/alter/AlterJobMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/alter/AlterJobMgr.java
@@ -140,6 +140,7 @@ import com.starrocks.thrift.TTabletMetaType;
 import com.starrocks.thrift.TTabletType;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.util.Strings;
 import org.threeten.extra.PeriodDuration;
 
 import java.io.IOException;
@@ -1105,7 +1106,9 @@ public class AlterJobMgr {
             if (!partitionInfo.isRangePartition()) {
                 throw new DdlException("Only support range partition table to modify storage_cooldown_ttl");
             }
-            periodDuration = TimeUtils.parseHumanReadablePeriodOrDuration(storageCoolDownTTL);
+            if (Strings.isNotBlank(storageCoolDownTTL)) {
+                periodDuration = TimeUtils.parseHumanReadablePeriodOrDuration(storageCoolDownTTL);
+            }
         }
         DataProperty newDataProperty =
                 PropertyAnalyzer.analyzeDataProperty(properties, null, false);
