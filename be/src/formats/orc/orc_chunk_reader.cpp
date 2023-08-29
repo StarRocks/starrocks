50327c19cedf5a8cb9b28e17e1e85fe12f2c149c
leoyy0316 <105328124+leoyy0316@users.noreply.github.com>
2023-08-29 17:05:21 +0800

    [BugFix]Fix brokerload failed when data size greater than the type defined by orc (#29971)
    Signed-off-by: leoyy0316 <571684903@qq.com>

--- be/src/formats/orc/orc_chunk_reader.cpp
+++ be/src/formats/orc/orc_chunk_reader.cpp
@@ -400,7 +400,7 @@ void OrcChunkReader::_try_implicit_cast(TypeDescriptor* from, const TypeDescript
         } else {
             from->type = LogicalType::TYPE_DECIMAL32;
         }
-    } else if (_broker_load_mode && !_strict_mode && is_string_type(t1) && is_string_type(t2)) {
+    } else if (_broker_load_mode && is_string_type(t1) && is_string_type(t2)) {
         // For broker load, the orc field length is larger than the maximum length of the starrocks field
         // will cause load failure in non-strict mode. Here we keep the maximum length of the orc field
         // the same as the maximum length of the starrocks field.
