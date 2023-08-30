d22c5f17412360ffd9ec20ece7612ab766566ae0
Dejun Xia <xiadejun@starrocks.com>
2023-08-29 17:45:44 -0700

    [Enhancement] Refine the priv check for be_tablets and show tablet
    Signed-off-by: Dejun Xia <xiadejun@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/common/proc/LocalTabletsProcDir.java
+++ fe/fe-core/src/main/java/com/starrocks/common/proc/LocalTabletsProcDir.java
@@ -93,7 +93,8 @@ public class LocalTabletsProcDir implements ProcDirInterface {
         throw new AnalysisException("Title name[" + columnName + "] does not exist");
     }
 
-    public List<List<Comparable>> fetchComparableResult(long version, long backendId, Replica.ReplicaState state) {
+    public List<List<Comparable>> fetchComparableResult(long version, long backendId, Replica.ReplicaState state,
+                                                        Boolean hideIpPort) {
         Preconditions.checkNotNull(db);
         Preconditions.checkNotNull(index);
         Preconditions.checkState(table.isOlapTableOrMaterializedView());
@@ -107,30 +108,8 @@ public class LocalTabletsProcDir implements ProcDirInterface {
             for (Tablet tablet : index.getTablets()) {
                 LocalTablet localTablet = (LocalTablet) tablet;
                 long tabletId = tablet.getId();
-                if (localTablet.getImmutableReplicas().size() == 0) {
-                    List<Comparable> tabletInfo = new ArrayList<Comparable>();
-                    tabletInfo.add(tabletId);
-                    tabletInfo.add(-1); // replica id
-                    tabletInfo.add(-1); // backend id
-                    tabletInfo.add(-1); // schema hash
-                    tabletInfo.add(-1); // version
-                    tabletInfo.add(0); // version hash
-                    tabletInfo.add(-1); // lst success version
-                    tabletInfo.add(0); // lst success version hash
-                    tabletInfo.add(-1); // lst failed version
-                    tabletInfo.add(0); // lst failed version hash
-                    tabletInfo.add(-1); // lst failed time
-                    tabletInfo.add(-1); // data size
-                    tabletInfo.add(-1); // row count
-                    tabletInfo.add(FeConstants.NULL_STRING); // state
-                    tabletInfo.add(-1); // lst consistency check time
-                    tabletInfo.add(-1); // check version
-                    tabletInfo.add(0); // check version hash
-                    tabletInfo.add(-1); // version count
-                    tabletInfo.add(-1); // path hash
-                    tabletInfo.add(FeConstants.NULL_STRING); // meta url
-                    tabletInfo.add(FeConstants.NULL_STRING); // compaction status
-
+                if (localTablet.getImmutableReplicas().isEmpty()) {
+                    List<Comparable> tabletInfo = createTabletInfo(tabletId);
                     tabletInfos.add(tabletInfo);
                 } else {
                     for (Replica replica : localTablet.getImmutableReplicas()) {
@@ -165,14 +144,14 @@ public class LocalTabletsProcDir implements ProcDirInterface {
                         String metaUrl;
                         String compactionUrl;
                         if (backend != null) {
-                            metaUrl = String.format("http://%s:%d/api/meta/header/%d",
-                                    backend.getHost(),
-                                    backend.getHttpPort(),
+                            metaUrl = String.format("http://%s:%s/api/meta/header/%d",
+                                    hideIpPort ? "*" : backend.getHost(),
+                                    hideIpPort ? "*" : backend.getHttpPort(),
                                     tabletId);
                             compactionUrl = String.format(
-                                    "http://%s:%d/api/compaction/show?tablet_id=%d",
-                                    backend.getHost(),
-                                    backend.getHttpPort(),
+                                    "http://%s:%s/api/compaction/show?tablet_id=%d",
+                                    hideIpPort ? "*" : backend.getHost(),
+                                    hideIpPort ? "*" : backend.getHttpPort(),
                                     tabletId);
                         } else {
                             metaUrl = "N/A";
@@ -191,8 +170,34 @@ public class LocalTabletsProcDir implements ProcDirInterface {
         return tabletInfos;
     }
 
+    private static List<Comparable> createTabletInfo(long tabletId) {
+        List<Comparable> tabletInfo = new ArrayList<Comparable>();
+        tabletInfo.add(tabletId);
+        tabletInfo.add(-1); // replica id
+        tabletInfo.add(-1); // backend id
+        tabletInfo.add(-1); // schema hash
+        tabletInfo.add(-1); // version
+        tabletInfo.add(0); // version hash
+        tabletInfo.add(-1); // lst success version
+        tabletInfo.add(0); // lst success version hash
+        tabletInfo.add(-1); // lst failed version
+        tabletInfo.add(0); // lst failed version hash
+        tabletInfo.add(-1); // lst failed time
+        tabletInfo.add(-1); // data size
+        tabletInfo.add(-1); // row count
+        tabletInfo.add(FeConstants.NULL_STRING); // state
+        tabletInfo.add(-1); // lst consistency check time
+        tabletInfo.add(-1); // check version
+        tabletInfo.add(0); // check version hash
+        tabletInfo.add(-1); // version count
+        tabletInfo.add(-1); // path hash
+        tabletInfo.add(FeConstants.NULL_STRING); // meta url
+        tabletInfo.add(FeConstants.NULL_STRING); // compaction status
+        return tabletInfo;
+    }
+
     private List<List<Comparable>> fetchComparableResult() {
-        return fetchComparableResult(-1, -1, null);
+        return fetchComparableResult(-1, -1, null, false);
     }
 
     @Override
