c1713733f42e7d8d9ba397fb40b32851304c6168
Yixin Luo <18810541851@163.com>
2023-08-31 14:13:48 +0800

    [BugFix] fix persistent index delete flag lost when flush l0 (#29997)
    Signed-off-by: luohaha <18810541851@163.com>

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -968,7 +968,7 @@ CONF_mInt64(l0_snapshot_size, "16777216"); // 16MB
 CONF_mInt64(max_tmp_l1_num, "10");
 CONF_mBool(enable_parallel_get_and_bf, "true");
 // Control if using the minor compaction strategy
-CONF_mBool(enable_pindex_minor_compaction, "true");
+CONF_Bool(enable_pindex_minor_compaction, "true");
 // if l2 num is larger than this, stop doing async compaction,
 // add this config to prevent l2 grow too large.
 CONF_mInt64(max_allow_pindex_l2_num, "5");
