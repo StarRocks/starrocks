d07f5a78046c40aea04d67c9db7939675791bd48
liuyehcf <1559500551@qq.com>
2023-08-31 12:59:55 +0800

    update 1
    Signed-off-by: liuyehcf <1559500551@qq.com>

--- be/src/exec/pipeline/fragment_executor.cpp
+++ be/src/exec/pipeline/fragment_executor.cpp
@@ -539,7 +539,7 @@ Status FragmentExecutor::_prepare_pipeline_driver(ExecEnv* exec_env, const Unifi
         const auto& tsink = request.output_sink();
         if (tsink.type == TDataSinkType::RESULT_SINK || tsink.type == TDataSinkType::OLAP_TABLE_SINK ||
             tsink.type == TDataSinkType::MEMORY_SCRATCH_SINK || tsink.type == TDataSinkType::ICEBERG_TABLE_SINK ||
-            tsink.type == TDataSinkType::HIVE_TABLE_SINK || tsink.type == TDataSinkType::EXPORT_SINK) {
+            tsink.type == TDataSinkType::EXPORT_SINK) {
             _query_ctx->set_final_sink();
         }
         RETURN_IF_ERROR(DataSink::create_data_sink(runtime_state, tsink, fragment.output_exprs, params,
