d487cef27d65ad884056b538b17fd49d67fe5063
iverson <58965658+wanpengfei-git@users.noreply.github.com>
2023-08-31 10:16:53 +0800

    [Enhancement] cache view analyze result(sync #29642) (#3875)
    * [Enhancement] cache view analyze result
    
    * resolve conflict (#3941)
    
    Signed-off-by: abc982627271 <liuxuefen@starrocks.com>
    
    ---------
    
    Signed-off-by: abc982627271 <liuxuefen@starrocks.com>
    Co-authored-by: Seaven <seaven_7@qq.com>
    Co-authored-by: Xuefen Liu <liuxuefen@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/server/MetadataMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/server/MetadataMgr.java
@@ -40,7 +40,6 @@ import com.starrocks.connector.PartitionInfo;
 import com.starrocks.connector.RemoteFileInfo;
 import com.starrocks.connector.exception.StarRocksConnectorException;
 import com.starrocks.qe.ConnectContext;
-import com.starrocks.sql.analyzer.AnalyzerUtils;
 import com.starrocks.sql.ast.AlterTableStmt;
 import com.starrocks.sql.ast.CreateTableStmt;
 import com.starrocks.sql.ast.DropTableStmt;
@@ -273,9 +272,8 @@ public class MetadataMgr {
         }
 
         for (View view : views) {
-            Map<TableName, com.starrocks.catalog.Table> usedTable =
-                    AnalyzerUtils.collectAllTableAndView(view.getQueryStatement());
-            if (CollectionUtils.containsAny(usedTable.keySet(), tableNames)) {
+            List<TableName> usedTables = view.getTableRefs();
+            if (CollectionUtils.containsAny(usedTables, tableNames)) {
                 view.setInvalid(reason);
             }
         }
