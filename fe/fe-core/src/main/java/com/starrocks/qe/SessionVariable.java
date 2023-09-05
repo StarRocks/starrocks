32922e0f9d7985815718194ef6be78d2bf2eef90
zihe.liu <ziheliu1024@gmail.com>
2023-09-05 11:53:11 +0800

    [Enhancement] Make assignment bucket sequences to backend more evenly  (#30248)
    It gives priority to selecting the bucket sequence with the highest sum of selection counts for backends where its replicas are located.
    
    Signed-off-by: ZiheLiu <ziheliu1024@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
@@ -199,6 +199,8 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
 
     public static final String ENABLE_PIPELINE_ENGINE = "enable_pipeline_engine";
 
+    public static final String MAX_BUCKETS_PER_BE_TO_USE_BALANCER_ASSIGNMENT = "max_buckets_per_be_to_use_balancer_assignment";
+
     public static final String ENABLE_MV_PLANNER = "enable_mv_planner";
     public static final String ENABLE_INCREMENTAL_REFRESH_MV = "enable_incremental_mv";
 
@@ -397,6 +399,7 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
             return EnumUtils.getEnumIgnoreCase(MaterializedViewRewriteMode.class, str);
         }
     }
+
     public static final String MATERIALIZED_VIEW_REWRITE_MODE = "materialized_view_rewrite_mode";
 
     public static final String ENABLE_SYNC_MATERIALIZED_VIEW_REWRITE = "enable_sync_materialized_view_rewrite";
@@ -522,6 +525,18 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
 
     @VariableMgr.VarAttr(name = ENABLE_PIPELINE, alias = ENABLE_PIPELINE_ENGINE, show = ENABLE_PIPELINE_ENGINE)
     private boolean enablePipelineEngine = true;
+
+    /**
+     * The threshold for determining whether to use a more evenly assignment bucket sequences to backend algorithm for query
+     * execution.
+     *
+     * <p> This algorithm is only used when {@code numBucketsPerBe} is smaller than this threshold, because the time complexity
+     * of it is {@code numBucketsPerBe} times than the previous algorithm.
+     *
+     * @see ColocatedBackendSelector
+     */
+    @VariableMgr.VarAttr(name = MAX_BUCKETS_PER_BE_TO_USE_BALANCER_ASSIGNMENT, flag = VariableMgr.INVISIBLE)
+    private int maxBucketsPerBeToUseBalancerAssignment = 6;
 
     @VariableMgr.VarAttr(name = ENABLE_RUNTIME_ADAPTIVE_DOP)
     private boolean enableRuntimeAdaptiveDop = false;
@@ -1902,6 +1917,14 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
         return true;
     }
 
+    public int getMaxBucketsPerBeToUseBalancerAssignment() {
+        return maxBucketsPerBeToUseBalancerAssignment;
+    }
+
+    public void setMaxBucketsPerBeToUseBalancerAssignment(int maxBucketsPerBeToUseBalancerAssignment) {
+        this.maxBucketsPerBeToUseBalancerAssignment = maxBucketsPerBeToUseBalancerAssignment;
+    }
+
     public void setPipelineDop(int pipelineDop) {
         this.pipelineDop = pipelineDop;
     }
@@ -1975,8 +1998,8 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
     }
 
     public boolean isEnableMaterializedViewForceRewrite() {
-        return materializedViewRewriteMode.equalsIgnoreCase(MaterializedViewRewriteMode.MODE_FORCE)  ||
-                        materializedViewRewriteMode.equalsIgnoreCase(MaterializedViewRewriteMode.MODE_FORCE_OR_ERROR);
+        return materializedViewRewriteMode.equalsIgnoreCase(MaterializedViewRewriteMode.MODE_FORCE) ||
+                materializedViewRewriteMode.equalsIgnoreCase(MaterializedViewRewriteMode.MODE_FORCE_OR_ERROR);
     }
 
     public boolean isEnableMaterializedViewRewriteOrError() {
