0c148153581e50b03794d683e455502853bb994f
Kevin Cai <caixiaohua@starrocks.com>
2023-09-06 11:45:58 +0800

    [BugFix] check table existence before heavy creating operation (#30404)
    * add the ahead existence check back which is removed in #25741
    * Fixes #30388
    
    Signed-off-by: Kevin Xiaohua Cai <caixiaohua@starrocks.com>
    (cherry picked from commit 62943334296fc930452e728ec78fd05b036cc5a0)
    
    Signed-off-by: Kevin Xiaohua Cai <caixiaohua@starrocks.com>

--- fe/fe-core/src/test/java/com/starrocks/server/LocalMetaStoreTest.java
+++ fe/fe-core/src/test/java/com/starrocks/server/LocalMetaStoreTest.java
@@ -38,6 +38,7 @@ import com.starrocks.qe.ConnectContext;
 import com.starrocks.thrift.TStorageMedium;
 import com.starrocks.utframe.StarRocksAssert;
 import com.starrocks.utframe.UtFrameUtils;
+import mockit.Expectations;
 import mockit.Mock;
 import mockit.MockUp;
 import org.junit.Assert;
@@ -152,5 +153,30 @@ public class LocalMetaStoreTest {
         Assert.assertNotNull(localMetaStore.getDb(InfoSchemaDb.DATABASE_NAME));
         Assert.assertNotNull(localMetaStore.getDb(SystemId.SYS_DB_ID));
         Assert.assertNotNull(localMetaStore.getDb(SysDb.DATABASE_NAME));
+    }
+
+    @Test
+    public void testCreateTableIfNotExists() throws Exception {
+        // create table if not exists, if the table already exists, do nothing
+        Database db = connectContext.getGlobalStateMgr().getDb("test");
+        Table table = db.getTable("t1");
+        Assert.assertTrue(table instanceof OlapTable);
+        LocalMetastore localMetastore = connectContext.getGlobalStateMgr().getLocalMetastore();
+
+        new Expectations(localMetastore) {
+            {
+                localMetastore.onCreate((Database) any, (Table) any, anyString, anyBoolean);
+                // don't expect any invoke to this method
+                minTimes = 0;
+                maxTimes = 0;
+                result = null;
+            }
+        };
+
+        starRocksAssert = new StarRocksAssert(connectContext);
+        // with IF NOT EXIST
+        starRocksAssert.useDatabase("test").withTable(
+                "CREATE TABLE IF NOT EXISTS test.t1(k1 int, k2 int, k3 int)" +
+                        " distributed by hash(k1) buckets 3 properties('replication_num' = '1');");
     }
 }
