c8f7966e229cee7c52bd65c9feeee62b2c2645ac
Felix Li <astralidea@163.com>
2023-09-07 13:44:58 +0800

    [Enhancement] Add more case and enhanced metadata partition repair capabilities (#30463)
    * Add more case and enhanced metadata partition repair capabilities
    
    Signed-off-by: Astralidea <astralidea@163.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/CatalogRecycleBin.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/CatalogRecycleBin.java
@@ -743,7 +743,12 @@ public class CatalogRecycleBin extends FrontendDaemon implements Writable {
             long tableId = olapTable.getId();
             for (Partition partition : olapTable.getAllPartitions()) {
                 long partitionId = partition.getId();
-                TStorageMedium medium = olapTable.getPartitionInfo().getDataProperty(partitionId).getStorageMedium();
+                DataProperty dataProperty = olapTable.getPartitionInfo().getDataProperty(partitionId);
+                if (dataProperty == null) {
+                    LOG.warn("can not find data property for table: {}, partitionId: {} ", table.getName(), partitionId);
+                    continue;
+                }
+                TStorageMedium medium = dataProperty.getStorageMedium();
                 for (PhysicalPartition physicalPartition : partition.getSubPartitions()) {
                     long physicalPartitionId = physicalPartition.getId();
                     for (MaterializedIndex index : physicalPartition.getMaterializedIndices(IndexExtState.ALL)) {
