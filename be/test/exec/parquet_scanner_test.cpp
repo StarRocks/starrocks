c80f63e782b6f52eebf7aeeaf31b98a2038c358f
ricky <rickif@qq.com>
2023-09-07 17:43:05 +0800

    test: add nested.parquet
    Signed-off-by: ricky <rickif@qq.com>

--- be/test/exec/parquet_scanner_test.cpp
+++ be/test/exec/parquet_scanner_test.cpp
@@ -304,7 +304,7 @@ class ParquetScannerTest : public ::testing::Test {
     }
 
     void check_schema(const std::string& path,
-                      const std::vector<std::pair<std::string, LogicalType>>& expected_schema) {
+                      const std::vector<std::pair<std::string, TypeDescriptor>>& expected_schema) {
         RuntimeProfile* profile = _obj_pool.add(new RuntimeProfile("test_prof", true));
         ScannerCounter* counter = _obj_pool.add(new ScannerCounter());
         auto query_globals = TQueryGlobals();
@@ -323,7 +323,7 @@ class ParquetScannerTest : public ::testing::Test {
         ASSERT_EQ(schema.size(), expected_schema.size());
         for (size_t i = 0; i < expected_schema.size(); ++i) {
             ASSERT_EQ(schema[i].col_name(), expected_schema[i].first);
-            ASSERT_EQ(schema[i].type().type, expected_schema[i].second) << schema[i].col_name();
+            ASSERT_TRUE(schema[i].type() == expected_schema[i].second) << schema[i].col_name();
         }
     }
 
@@ -669,46 +669,46 @@ TEST_F(ParquetScannerTest, int96_timestamp) {
 }
 
 TEST_F(ParquetScannerTest, get_file_schema) {
-    const std::vector<std::pair<std::string, std::vector<std::pair<std::string, LogicalType>>>> test_cases = {
-            {test_exec_dir + "/test_data/parquet_data/int96_timestamp.parquet", {{"col_datetime", TYPE_DATETIME}}},
+    const std::vector<std::pair<std::string, std::vector<std::pair<std::string, TypeDescriptor>>>> test_cases = {
+            {test_exec_dir + "/test_data/parquet_data/int96_timestamp.parquet",
+             {{"col_datetime", TypeDescriptor{TYPE_DATETIME}}}},
             {test_exec_dir + "/test_data/parquet_data/data_json.parquet",
-             {{"col_json_int8", TYPE_INT},
-              {"col_json_int16", TYPE_INT},
-              {"col_json_int32", TYPE_INT},
-              {"col_json_int64", TYPE_BIGINT},
-              {"col_json_uint8", TYPE_INT},
-              {"col_json_uint16", TYPE_INT},
-              {"col_json_uint32", TYPE_BIGINT},
-              {"col_json_uint64", TYPE_BIGINT},
-              {"col_json_timestamp", TYPE_DATETIME},
-              {"col_json_float32", TYPE_FLOAT},
-              {"col_json_float64", TYPE_DOUBLE},
-              {"col_json_bool", TYPE_BOOLEAN},
-              {"col_json_string", TYPE_VARCHAR},
-              // complex type is treat as VARCHAR now.
-              {"col_json_list", TYPE_ARRAY},
-              {"col_json_map", TYPE_MAP},
-              {"col_json_map_timestamp", TYPE_MAP},
-              {"col_json_struct", TYPE_VARCHAR},
-              {"col_json_list_list", TYPE_ARRAY},
-              {"col_json_map_list", TYPE_MAP},
-              {"col_json_list_struct", TYPE_ARRAY},
-              {"col_json_struct_struct", TYPE_VARCHAR},
-              {"col_json_struct_string", TYPE_VARCHAR},
-              {"col_json_json_string", TYPE_VARCHAR}}},
+             {{"col_json_int8", TypeDescriptor{TYPE_INT}},
+              {"col_json_int16", TypeDescriptor{TYPE_INT}},
+              {"col_json_int32", TypeDescriptor{TYPE_INT}},
+              {"col_json_int64", TypeDescriptor{TYPE_BIGINT}},
+              {"col_json_uint8", TypeDescriptor{TYPE_INT}},
+              {"col_json_uint16", TypeDescriptor{TYPE_INT}},
+              {"col_json_uint32", TypeDescriptor{TYPE_BIGINT}},
+              {"col_json_uint64", TypeDescriptor{TYPE_BIGINT}},
+              {"col_json_timestamp", TypeDescriptor{TYPE_DATETIME}},
+              {"col_json_float32", TypeDescriptor{TYPE_FLOAT}},
+              {"col_json_float64", TypeDescriptor{TYPE_DOUBLE}},
+              {"col_json_bool", TypeDescriptor{TYPE_BOOLEAN}},
+              {"col_json_string", TypeDescriptor{TYPE_VARCHAR}},
+              {"col_json_list", TypeDescriptor{TYPE_ARRAY}},
+              {"col_json_map", TypeDescriptor{TYPE_MAP}},
+              {"col_json_map_timestamp", TypeDescriptor{TYPE_MAP}},
+              {"col_json_struct", TypeDescriptor{TYPE_VARCHAR}},
+              {"col_json_list_list", TypeDescriptor{TYPE_ARRAY}},
+              {"col_json_map_list", TypeDescriptor{TYPE_MAP}},
+              {"col_json_list_struct", TypeDescriptor{TYPE_ARRAY}},
+              {"col_json_struct_struct", TypeDescriptor{TYPE_VARCHAR}},
+              {"col_json_struct_string", TypeDescriptor{TYPE_VARCHAR}},
+              {"col_json_json_string", TypeDescriptor{TYPE_VARCHAR}}}},
             {test_exec_dir + "/test_data/parquet_data/decimal.parquet",
-             {{"col_decimal32", TYPE_DECIMAL32},
-              {"col_decimal64", TYPE_DECIMAL64},
-              {"col_decimal128_byte_array", TYPE_DECIMAL128},
-              {"col_decimal128_fixed_len_byte_array", TYPE_DECIMAL128}}},
+             {{"col_decimal32", TypeDescriptor{TYPE_DECIMAL32}},
+              {"col_decimal64", TypeDescriptor{TYPE_DECIMAL64}},
+              {"col_decimal128_byte_array", TypeDescriptor{TYPE_DECIMAL128}},
+              {"col_decimal128_fixed_len_byte_array", TypeDescriptor{TYPE_DECIMAL128}}}},
             {test_exec_dir + "/test_data/parquet_data/nested.parquet",
-             {{"col_int", TYPE_BIGINT},
-              {"col_list_int", TYPE_ARRAY},
-              {"col_list_list_int", TYPE_ARRAY},
-              {"col_map_string_int", TYPE_MAP},
-              {"col_map_map_string_int", TYPE_MAP},
-              {"col_list_map_string_int", TYPE_ARRAY},
-              {"col_map_string_list_int", TYPE_MAP}}}};
+             {{"col_int", TypeDescriptor{TYPE_BIGINT}},
+              {"col_list_int", TypeDescriptor{TYPE_ARRAY}},
+              {"col_list_list_int", TypeDescriptor{TYPE_ARRAY}},
+              {"col_map_string_int", TypeDescriptor{TYPE_MAP}},
+              {"col_map_map_string_int", TypeDescriptor{TYPE_MAP}},
+              {"col_list_map_string_int", TypeDescriptor{TYPE_ARRAY}},
+              {"col_map_string_list_int", TypeDescriptor{TYPE_MAP}}}}};
 
     for (const auto& test_case : test_cases) {
         check_schema(test_case.first, test_case.second);
