48bc9b747bcad72bba0bb4cc3df67aa4dacd9b6c
lichaoyong <lichaoyong158@gmail.com>
2023-09-08 18:29:34 +0800

    [Improvement] Get rid of the database readlock when updateBackendReportVersion (#30499)
    The update of ReportVersion has also based on the AtomicLong.
    It's not necessary to have a database readlock
    
    (cherry picked from commit 407095780c47c17d39d541b5e60e3915a7bb2ccb)

--- fe/fe-core/src/main/java/com/starrocks/system/SystemInfoService.java
+++ fe/fe-core/src/main/java/com/starrocks/system/SystemInfoService.java
@@ -768,13 +768,8 @@ public class SystemInfoService {
         if ((atomicLong = idToReportVersionRef.get(backendId)) != null) {
             Database db = GlobalStateMgr.getCurrentState().getDb(dbId);
             if (db != null) {
-                db.readLock();
-                try {
-                    atomicLong.set(newReportVersion);
-                    LOG.debug("update backend {} report version: {}, db: {}", backendId, newReportVersion, dbId);
-                } finally {
-                    db.readUnlock();
-                }
+                atomicLong.set(newReportVersion);
+                LOG.debug("update backend {} report version: {}, db: {}", backendId, newReportVersion, dbId);
             } else {
                 LOG.warn("failed to update backend report version, db {} does not exist", dbId);
             }
