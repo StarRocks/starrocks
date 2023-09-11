ae287d612e1a73e278be53a872fa910ea3e26032
gengjun-git <gengjun@starrocks.com>
2023-09-11 21:59:21 +0800

    fix
    Signed-off-by: gengjun-git <gengjun@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/TabletInvertedIndex.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/TabletInvertedIndex.java
@@ -213,8 +213,15 @@ public class TabletInvertedIndex {
                             TStorageMedium storageMedium = storageMediumMap.get(partitionId);
                             if (storageMedium != null && backendTabletInfo.isSetStorage_medium()) {
                                 if (storageMedium != backendTabletInfo.getStorage_medium()) {
-                                    addToTabletMigrationMap(backendId, backendStorageTypeCnt, tabletId,
-                                            tabletMeta, tabletMigrationMap, storageMedium);
+                                    // If storage medium is less than 1, there is no need to send migration tasks to BE.
+                                    // Because BE will ignore this request.
+                                    if (backendStorageTypeCnt <= 1) {
+                                        LOG.debug("available storage medium type count is less than 1, " +
+                                                        "no need to send migrate task. tabletId={}, backendId={}.",
+                                                tabletMeta, backendId);
+                                    } else {
+                                        tabletMigrationMap.put(storageMedium, tabletId);
+                                    }
                                 }
                                 if (storageMedium != tabletMeta.getStorageMedium()) {
                                     tabletMeta.setStorageMedium(storageMedium);
@@ -299,55 +306,6 @@ public class TabletInvertedIndex {
                         + " cost: {} ms", backendId, tabletSyncMap.size(),
                 tabletDeleteFromMeta.size(), foundTabletsWithValidSchema.size(), foundTabletsWithInvalidSchema.size(),
                 tabletMigrationMap.size(), transactionsToClear.size(), transactionsToPublish.size(), (end - start));
-    }
-
-    private void addToTabletMigrationMap(long backendId, long backendStorageTypeCnt, long tabletId,
-                                         TabletMeta tabletMeta, ListMultimap<TStorageMedium, Long> tabletMigrationMap,
-                                         TStorageMedium storageMedium) {
-        // 1. If storage medium is less than 1, there is no need to send migration tasks to BE.
-        // Because BE will ignore this request.
-        if (backendStorageTypeCnt <= 1) {
-            LOG.debug("available storage medium type count is less than 1, " +
-                            "no need to send migrate task. tabletId={}, backendId={}.",
-                    tabletMeta, backendId);
-            return;
-        }
-
-        // 2. If size of tabletMigrationMap exceeds (Config.tablet_sched_max_migration_task_sent_once - running_tasks_on_be),
-        // dot not send more tasks. The number of tasks running on be cannot exceed Config.tablet_sched_max_migration_task_sent_once
-        if (tabletMigrationMap.size() >=
-                Config.tablet_sched_max_migration_task_sent_once
-                        - AgentTaskQueue.getTaskNum(backendId, TTaskType.STORAGE_MEDIUM_MIGRATE, false)) {
-            LOG.debug("size of tabletMigrationMap + size of running tasks on BE is bigger than {}",
-                    Config.tablet_sched_max_migration_task_sent_once);
-            return;
-        }
-
-        // 3. If the task already running on be, do not send again
-        if (AgentTaskQueue.getTask(backendId, TTaskType.STORAGE_MEDIUM_MIGRATE, tabletId) != null) {
-            LOG.debug("migrate of tablet:{} is already running on BE", tabletId);
-            return;
-        }
-
-        //4. If the table is Primary key, do not send
-        Database db = GlobalStateMgr.getCurrentState().getDb(tabletMeta.getDbId());
-        if (db == null) {
-            return;
-        }
-        // NOTICE: Database lock shouldn't be requested here:
-        // 1. the caller of addToTabletMigrationMap is holding TabletInvertedIndex's read lock,
-        //  and if the db lock is requested here, it can fall into dead lock,
-        //  because the lock order in other threads is db lock first.
-        // 2. db.getTable is thread safe, and table's key type will never be changed,
-        // so there is no need to request the db lock.
-        OlapTable table = (OlapTable) db.getTable(tabletMeta.getTableId());
-        if (table.getKeysType() == KeysType.PRIMARY_KEYS) {
-            LOG.debug("tablet:{} is primary key table, do not support migrate", tabletId);
-            // Currently, primary key table doesn't support tablet migration between local disks.
-            return;
-        }
-
-        tabletMigrationMap.put(storageMedium, tabletId);
     }
 
     private void deleteTabletByConsistencyChecker(TabletMeta tabletMeta, long tabletId, long backendId,
