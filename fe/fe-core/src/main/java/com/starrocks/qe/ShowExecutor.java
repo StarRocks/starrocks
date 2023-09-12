8cad8bd4a46dfdbe5553c59e416d0bf699b8aca4
Astralidea <astralidea@163.com>
2023-09-12 15:11:25 +0800

    Fix.
    Signed-off-by: Astralidea <astralidea@163.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/ShowExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/ShowExecutor.java
@@ -82,7 +82,6 @@ import com.starrocks.common.ConfigBase;
 import com.starrocks.common.DdlException;
 import com.starrocks.common.ErrorCode;
 import com.starrocks.common.ErrorReport;
-import com.starrocks.common.FeConstants;
 import com.starrocks.common.MetaNotFoundException;
 import com.starrocks.common.Pair;
 import com.starrocks.common.PatternMatcher;
