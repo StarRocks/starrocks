63d82c84a70e467a1e3b6224c94c282a9ec732f6
stdpain <34912776+stdpain@users.noreply.github.com>
2023-09-12 18:44:31 +0800

    [BugFix] Fix mem limited streaming agg may hang (#30228)
    Signed-off-by: stdpain <drfeng08@gmail.com>

--- be/src/exec/pipeline/aggregate/aggregate_distinct_streaming_sink_operator.cpp
+++ be/src/exec/pipeline/aggregate/aggregate_distinct_streaming_sink_operator.cpp
@@ -104,8 +104,7 @@ Status AggregateDistinctStreamingSinkOperator::_push_chunk_by_force_preaggregati
 
 Status AggregateDistinctStreamingSinkOperator::_push_chunk_by_limited_memory(const ChunkPtr& chunk,
                                                                              const size_t chunk_size) {
-    bool ht_needs_expansion = _aggregator->hash_set_variant().need_expand(chunk_size);
-    if (ht_needs_expansion && _limited_mem_state.has_limited(*_aggregator)) {
+    if (_limited_mem_state.has_limited(*_aggregator)) {
         RETURN_IF_ERROR(_push_chunk_by_force_streaming(chunk));
         _aggregator->set_streaming_all_states(true);
     } else {
