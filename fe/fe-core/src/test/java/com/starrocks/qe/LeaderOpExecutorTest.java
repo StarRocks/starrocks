b291287165cff95d67e8b31e75b4fa6d16ad7cd6
zihe.liu <ziheliu1024@gmail.com>
2023-09-12 15:32:23 +0800

    fix compile
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- fe/fe-core/src/test/java/com/starrocks/qe/LeaderOpExecutorTest.java
+++ fe/fe-core/src/test/java/com/starrocks/qe/LeaderOpExecutorTest.java
@@ -60,7 +60,6 @@ public class LeaderOpExecutorTest {
         cluster = PseudoCluster.getInstance();
 
         FeConstants.runningUnitTest = true;
-        Config.alter_scheduler_interval_millisecond = 100;
         Config.dynamic_partition_enable = true;
         Config.dynamic_partition_check_interval_seconds = 1;
         Config.enable_experimental_mv = true;
