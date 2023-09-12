6261fc04aa6fbcd97f3a1e4b622eb97b20a33945
Letian Jiang <letian.jiang@outlook.com>
2023-09-12 17:32:10 +0800

    [BugFix] Fix hive view authorization (#30803)
    Signed-off-by: Letian Jiang <letian.jiang@outlook.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/Authorizer.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/Authorizer.java
@@ -141,6 +141,7 @@ public class Authorizer {
             case MYSQL:
             case ELASTICSEARCH:
             case HIVE:
+            case HIVE_VIEW:
             case ICEBERG:
             case HUDI:
             case JDBC:
