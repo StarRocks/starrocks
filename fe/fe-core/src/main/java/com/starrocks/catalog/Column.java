123c54f5450b0049b580d32421790f0024572f92
Astralidea <astralidea@163.com>
2023-09-12 13:00:13 +0800

    Fix.
    Signed-off-by: Astralidea <astralidea@163.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/Column.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/Column.java
@@ -45,7 +45,6 @@ import com.starrocks.analysis.SlotRef;
 import com.starrocks.analysis.StringLiteral;
 import com.starrocks.common.CaseSensibility;
 import com.starrocks.common.DdlException;
-import com.starrocks.common.FeConstants;
 import com.starrocks.common.io.Text;
 import com.starrocks.common.io.Writable;
 import com.starrocks.common.util.TimeUtils;
