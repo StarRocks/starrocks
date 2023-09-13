53603630b1384d8887b402dbeaf4fa0ad1c8c34a
Xuefen Liu <liuxuefen@starrocks.com>
2023-09-13 10:06:58 +0800

    [BugFix]build failed because FeConstant not imported in com.starrocks.qe#ShowExecutor (#4567)
    Signed-off-by: abc982627271 <liuxuefen@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/ShowExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/ShowExecutor.java
@@ -86,6 +86,7 @@ import com.starrocks.common.ConfigBase;
 import com.starrocks.common.DdlException;
 import com.starrocks.common.ErrorCode;
 import com.starrocks.common.ErrorReport;
+import com.starrocks.common.FeConstants;
 import com.starrocks.common.MetaNotFoundException;
 import com.starrocks.common.Pair;
 import com.starrocks.common.PatternMatcher;
