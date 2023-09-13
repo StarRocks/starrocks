0c34457d991488b7a22a9fc561f0d0a1178ca1dd
Seaven <seaven_7@qq.com>
2023-09-13 12:11:30 +0800

    fix
    Signed-off-by: Seaven <seaven_7@qq.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/ReplayFromDumpTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/ReplayFromDumpTest.java
@@ -725,12 +725,4 @@ public class ReplayFromDumpTest extends ReplayFromDumpTestBase {
                 "  |  \n" +
                 "  |----25:EXCHANGE"));
     }
-
-    @Test
-    public void testCoalesceJoin() throws Exception {
-        Pair<QueryDumpInfo, String> replayPair =
-                getPlanFragment(getDumpInfoFromFile("query_dump/coalesce_join.json"),
-                        null, TExplainLevel.NORMAL);
-        Assert.assertTrue(replayPair.second, replayPair.second.contains("join op: INNER JOIN"));
-    }
 }
