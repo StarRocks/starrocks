924aa79364bf5bcbcbe0afd099bcb78cb2fc63c1
Zhuhe Fang <fzhedu@gmail.com>
2023-09-13 15:51:31 +0800

    [Refactor] refactor error msg for high-order functions (#30766)
    Signed-off-by: Zhuhe Fang <fzhedu@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/analysis/Expr.java
+++ fe/fe-core/src/main/java/com/starrocks/analysis/Expr.java
@@ -1392,17 +1392,17 @@ public abstract class Expr extends TreeNode<Expr> implements ParseNode, Cloneabl
         }
         if (num == 1 && (idx == 0 || idx == children.size() - 1)) {
             if (children.size() <= 1) {
-                throw new SemanticException("Lambda functions need array inputs in high-order functions.");
+                throw new SemanticException("Lambda functions need array/map inputs in high-order functions");
             }
             return true;
         } else if (num > 1) {
             throw new SemanticException("A high-order function should have only 1 lambda function, " +
-                    "but there are " + num + " lambda functions.");
+                    "but there are " + num + " lambda functions");
         } else if (idx > 0 && idx < children.size() - 1) {
             throw new SemanticException(
                     "Lambda functions should only be the first or last argument of any high-order function, " +
                             "or lambda arguments should be in () if there are more than one lambda arguments, " +
-                            "like (x,y)->x+y.");
+                            "like (x,y)->x+y");
         } else if (num == 0) {
             if (expression instanceof FunctionCallExpr) {
                 String funcName = ((FunctionCallExpr) expression).getFnName().getFunction();
