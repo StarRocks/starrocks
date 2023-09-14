525dc614bbbe256878259c53b9e707fd8cc66670
zihe.liu <ziheliu1024@gmail.com>
2023-09-14 17:25:04 +0800

    fix code style
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/LeaderOpExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/LeaderOpExecutor.java
@@ -115,7 +115,7 @@ public class LeaderOpExecutor {
 
         if (result.isSetResource_group_name()) {
             ctx.getAuditEventBuilder().setResourceGroup(result.getResource_group_name());
-            }
+        }
         if (result.isSetAudit_statistics()) {
             TAuditStatistics tAuditStatistics = result.getAudit_statistics();
             if (ctx.getExecutor() != null) {
