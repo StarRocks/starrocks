1d81bdc711aed06923357a3b4411e8d19fec6ecf
Zijie Lu <wslzj40@gmail.com>
2023-09-14 19:20:30 +0800

    [BugFix] Fix sort key bug in vertical compaction of cloud native table (#31097)

--- be/src/storage/lake/vertical_compaction_task.cpp
+++ be/src/storage/lake/vertical_compaction_task.cpp
@@ -133,7 +133,10 @@ Status VerticalCompactionTask::compact_column_group(bool is_key, int column_grou
                                                     const CancelFunc& cancel_func) {
     ASSIGN_OR_RETURN(auto chunk_size, calculate_chunk_size_for_column_group(column_group));
 
-    Schema schema = ChunkHelper::convert_schema(*_tablet_schema, column_group);
+    Schema schema = column_group_index == 0 ? ((*_tablet_schema).sort_key_idxes().empty()
+                                                       ? ChunkHelper::convert_schema(*_tablet_schema, column_group)
+                                                       : ChunkHelper::get_sort_key_schema(*_tablet_schema))
+                                            : ChunkHelper::convert_schema(*_tablet_schema, column_group);
     TabletReader reader(*_tablet, _version, schema, _input_rowsets, is_key, mask_buffer);
     RETURN_IF_ERROR(reader.prepare());
     TabletReaderParams reader_params;
