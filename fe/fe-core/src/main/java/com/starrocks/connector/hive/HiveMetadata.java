89da629f823dbf721d69d7213b8c4aeba315bc0f
stephen <stephen5217@163.com>
2023-09-14 15:42:14 +0800

    [BugFix] fix hive finish_sink without commit info
    Signed-off-by: stephen <stephen5217@163.com>

--- fe/fe-core/src/main/java/com/starrocks/connector/hive/HiveMetadata.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/hive/HiveMetadata.java
@@ -273,6 +273,10 @@ public class HiveMetadata implements ConnectorMetadata {
 
     @Override
     public void finishSink(String dbName, String tableName, List<TSinkCommitInfo> commitInfos) {
+        if (commitInfos.isEmpty()) {
+            LOG.warn("No commit info on {}.{} after hive sink", dbName, tableName);
+            return;
+        }
         HiveTable table = (HiveTable) getTable(dbName, tableName);
         String stagingDir = commitInfos.get(0).getStaging_dir();
         boolean isOverwrite = commitInfos.get(0).isIs_overwrite();
