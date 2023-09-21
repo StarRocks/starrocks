bb8a5eae0cb778c99fb70000a101d98277893788
abc982627271 <liuxuefen@starrocks.com>
2023-09-21 10:37:56 +0800

    fix bug
    Signed-off-by: abc982627271 <liuxuefen@starrocks.com>

--- fe/fe-core/src/test/java/com/starrocks/load/ExportCheckerTest.java
+++ fe/fe-core/src/test/java/com/starrocks/load/ExportCheckerTest.java
@@ -19,7 +19,6 @@ import com.starrocks.load.ExportJob.JobState;
 import com.starrocks.system.Backend;
 import com.starrocks.system.ComputeNode;
 import com.starrocks.system.SystemInfoService;
-import com.starrocks.task.ExportPendingTask;
 import mockit.Mock;
 import mockit.MockUp;
 import org.junit.Assert;
@@ -36,7 +35,7 @@ public class ExportCheckerTest {
     public void testCheckBeStatus() throws NoSuchFieldException, SecurityException, 
         IllegalArgumentException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
 
-        new MockUp<ExportPendingTask>() {
+        new MockUp<ExportJob>() {
             @Mock
             public synchronized void cancel(ExportFailMsg.CancelType type, String msg) throws UserException {
             }
