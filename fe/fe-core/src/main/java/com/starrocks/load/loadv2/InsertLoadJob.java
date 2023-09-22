bfb565c835db2743031577aa518c607dc3cba6d9
gengjun-git <gengjun@starrocks.com>
2023-09-22 18:38:15 +0800

    [BugFix] Fix load external OLAP table job cancelled job (#31634)
    Fixes
    ```java
    mysql> insert into ext_t select * from par_tbl;
    ERROR 1064 (HY000): LoadJob 14074 state CANCELLED, can not be cancelled/publish
    ```
    
    Signed-off-by: gengjun-git <gengjun@starrocks.com>
    (cherry picked from commit 9a7b935966d6f979851730866f11bd09d5d05fe3)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/load/loadv2/InsertLoadJob.java

--- fe/fe-core/src/main/java/com/starrocks/load/loadv2/InsertLoadJob.java
+++ fe/fe-core/src/main/java/com/starrocks/load/loadv2/InsertLoadJob.java
@@ -40,6 +40,11 @@ import com.google.common.collect.Sets;
 import com.google.gson.annotations.SerializedName;
 import com.starrocks.catalog.AuthorizationInfo;
 import com.starrocks.catalog.Database;
+<<<<<<< HEAD
+=======
+import com.starrocks.catalog.ExternalOlapTable;
+import com.starrocks.catalog.HiveTable;
+>>>>>>> 9a7b935966 ([BugFix] Fix load external OLAP table job cancelled job (#31634))
 import com.starrocks.catalog.IcebergTable;
 import com.starrocks.catalog.Table;
 import com.starrocks.catalog.system.SystemTable;
@@ -237,7 +242,14 @@ public class InsertLoadJob extends LoadJob {
             return true;
         }
 
+<<<<<<< HEAD
         if (table instanceof SystemTable || table instanceof IcebergTable) {
+=======
+        if (table instanceof SystemTable
+                || table instanceof IcebergTable
+                || table instanceof HiveTable
+                || table instanceof ExternalOlapTable) {
+>>>>>>> 9a7b935966 ([BugFix] Fix load external OLAP table job cancelled job (#31634))
             return false;
         } else {
             return true;
