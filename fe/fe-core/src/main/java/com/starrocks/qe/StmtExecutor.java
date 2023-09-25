e74fa2c6859339107b428f770e75f60299cf8fbd
meegoo <meegoo.sr@gmail.com>
2023-09-25 19:06:47 +0800

    [BugFix] Fix insert mysql external table fail (#31555)
    Signed-off-by: meegoo <meegoo.sr@gmail.com>
    (cherry picked from commit c65d72f1b714a4943a506424bc8a865fcceb61e1)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java

--- fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
@@ -1621,6 +1621,26 @@ public class StmtExecutor {
                 }
             }
 
+<<<<<<< HEAD
+=======
+            if (loadedRows == 0 && filteredRows == 0 && (stmt instanceof DeleteStmt || stmt instanceof InsertStmt
+                    || stmt instanceof UpdateStmt)) {
+                // when the target table is not ExternalOlapTable or OlapTable
+                // if there is no data to load, the result of the insert statement is success
+                // otherwise, the result of the insert statement is failed
+                GlobalTransactionMgr mgr = GlobalStateMgr.getCurrentGlobalTransactionMgr();
+                String errorMsg = TransactionCommitFailedException.NO_DATA_TO_LOAD_MSG;
+                if (!(targetTable instanceof ExternalOlapTable || targetTable instanceof OlapTable)) {
+                    if (!(targetTable instanceof SystemTable || targetTable instanceof IcebergTable)) {
+                        // schema table and iceberg table does not need txn
+                        mgr.abortTransaction(database.getId(), transactionId, errorMsg);
+                    }
+                    context.getState().setOk();
+                    insertError = true;
+                    return;
+                }
+            }
+>>>>>>> c65d72f1b7 ([BugFix] Fix insert mysql external table fail (#31555))
 
             if (targetTable instanceof ExternalOlapTable) {
                 ExternalOlapTable externalTable = (ExternalOlapTable) targetTable;
