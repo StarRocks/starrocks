a1c10a21819418251814fbe051d90ad3955d9430
Yixin Luo <18810541851@163.com>
2023-09-25 15:07:39 +0800

    [BugFix] fix cloud native pk index not load when handle partial update (#31597)
    Signed-off-by: luohaha <18810541851@163.com>

--- be/src/storage/lake/lake_primary_index.cpp
+++ be/src/storage/lake/lake_primary_index.cpp
@@ -33,6 +33,11 @@ Status LakePrimaryIndex::lake_load(Tablet* tablet, const TabletMetadata& metadat
         return _status;
     }
     _status = _do_lake_load(tablet, metadata, base_version, builder);
+    if (_status.ok()) {
+        // update data version when memory index or persistent index load finish.
+        _data_version = base_version;
+    }
+    _tablet_id = tablet->id();
     _loaded = true;
     TRACE("end load pk index");
     if (!_status.ok()) {
@@ -80,7 +85,7 @@ Status LakePrimaryIndex::_do_lake_load(Tablet* tablet, const TabletMetadata& met
                     RETURN_IF_ERROR(
                             StorageEngine::instance()->get_persistent_index_store()->create_dir_if_path_not_exists(
                                     path));
-                    _persistent_index = std::make_unique<LakeLocalPersistentIndex>(path, this);
+                    _persistent_index = std::make_unique<LakeLocalPersistentIndex>(path);
                     return ((LakeLocalPersistentIndex*)_persistent_index.get())
                             ->load_from_lake_tablet(tablet, metadata, base_version, builder);
                 }
@@ -154,8 +159,6 @@ Status LakePrimaryIndex::_do_lake_load(Tablet* tablet, const TabletMetadata& met
             itr->close();
         }
     }
-    _tablet_id = tablet->id();
-    _data_version = base_version;
     auto cost_ns = watch.elapsed_time();
     g_load_pk_index_latency << cost_ns / 1000;
     LOG_IF(INFO, cost_ns >= /*10ms=*/10 * 1000 * 1000)
