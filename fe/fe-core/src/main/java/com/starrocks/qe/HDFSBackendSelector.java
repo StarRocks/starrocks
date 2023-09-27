7abc479b137fa411abfa8b7a9a46ff500683844d
miomiocat <284487410@qq.com>
2023-09-27 19:25:14 +0800

    [BugFix] Fix the issue of scan range allocation when reading paimon table (#31886)
    Signed-off-by: miomiocat <284487410@qq.com>
    (cherry picked from commit 726b3e414ae2e09dce99860874c6e9950eb56e03)

--- fe/fe-core/src/main/java/com/starrocks/qe/HDFSBackendSelector.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/HDFSBackendSelector.java
@@ -55,6 +55,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 
 /**
@@ -180,7 +181,9 @@ public class HDFSBackendSelector implements BackendSelector {
             }
         }
         if (node == null) {
-            node = backends.get(0);
+            Random rand = new Random(System.currentTimeMillis());
+            int i = rand.nextInt(backends.size());
+            node = backends.get(i);
         }
         return node;
     }
