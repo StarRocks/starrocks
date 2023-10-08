8a7fb2b9ec521a6aa4825cbe87c588940c49d634
Gavin <yangguansuo@starrocks.com>
2023-10-08 15:10:35 +0800

    [Refactor] Seperate starcache from staros to avoid build staros modules if unnecessary. (#30687) (#32113)
    Signed-off-by: Gavin <yangguansuo@starrocks.com>

--- build.sh
+++ build.sh
@@ -317,6 +317,18 @@ if [ ${BUILD_BE} -eq 1 ] ; then
       fi
       export STARLET_INSTALL_DIR
     fi
+
+    # Temporarily keep the default behavior same as before to avoid frequent thirdparty update.
+    # Once the starcache version is stable, we will turn on it by default.
+    if [[ -z ${WITH_STARCACHE} ]]; then
+      WITH_STARCACHE=${USE_STAROS}
+    fi
+
+    if [[ "${WITH_STARCACHE}" == "ON" && ! -f ${STARROCKS_THIRDPARTY}/installed/starcache/lib/libstarcache.a ]]; then
+        echo "Missing depdency libraries(starcache), you can download and extract it to thirdparty installed directory."
+        exit 1
+    fi
+
     ${CMAKE_CMD} -G "${CMAKE_GENERATOR}"                                \
                   -DSTARROCKS_THIRDPARTY=${STARROCKS_THIRDPARTY}        \
                   -DSTARROCKS_HOME=${STARROCKS_HOME}                    \
@@ -330,8 +342,8 @@ if [ ${BUILD_BE} -eq 1 ] ; then
                   -DWITH_BENCH=${WITH_BENCH}                            \
                   -DWITH_COMPRESS=${WITH_COMPRESS}                      \
                   -DWITH_CACHELIB=${WITH_CACHELIB}                      \
+                  -DWITH_STARCACHE=${WITH_STARCACHE}                    \
                   -DUSE_STAROS=${USE_STAROS}                            \
-                  -DWITH_STARCACHE=${USE_STAROS}                        \
                   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON  ..
 
     time ${BUILD_SYSTEM} -j${PARALLEL}
