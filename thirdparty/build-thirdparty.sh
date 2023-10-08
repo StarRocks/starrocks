88551ca16081dd4cdecf253f5e86a7edf09fccab
Gavin <yangguansuo@starrocks.com>
2023-10-08 15:10:13 +0800

    [Refactor] Seperate `WITH_STARCACHE` from `use-staros` to avoid build staros modules if unnecessary. (#30687)
    Signed-off-by: Gavin <yangguansuo@starrocks.com>

--- thirdparty/build-thirdparty.sh
+++ thirdparty/build-thirdparty.sh
@@ -1081,6 +1081,11 @@ build_cachelib() {
     rm -rf $TP_INSTALL_DIR/$CACHELIB_SOURCE && mv $TP_SOURCE_DIR/$CACHELIB_SOURCE $TP_INSTALL_DIR/
 }
 
+build_starcache() {
+    check_if_source_exist $STARCACHE_SOURCE
+    rm -rf $TP_INSTALL_DIR/$STARCACHE_SOURCE && mv $TP_SOURCE_DIR/$STARCACHE_SOURCE $TP_INSTALL_DIR/
+}
+
 # streamvbyte
 build_streamvbyte() {
     check_if_source_exist $STREAMVBYTE_SOURCE
@@ -1271,6 +1276,7 @@ build_opentelemetry
 build_benchmark
 build_fast_float
 build_cachelib
+build_starcache
 build_streamvbyte
 build_jansson
 build_avro_c
