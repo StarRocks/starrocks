c41e6554fccceb8cb2248f1ff9aa68ff8cef5bec
shshenhua <364382466@qq.com>
2023-10-08 10:32:27 +0800

    [Enhancement] allow creating index on mv (#31547)
    Signed-off-by: shshenhua <364382466@qq.com>
    (cherry picked from commit 27d8dc1450eb3c62dccd3f139f3ab8949508d032)

--- fe/fe-core/src/main/java/com/starrocks/catalog/MaterializedView.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/MaterializedView.java
@@ -1043,6 +1043,15 @@ public class MaterializedView extends OlapTable implements GsonPreProcessable, G
             colDef.add(colSb.toString());
         }
         sb.append(Joiner.on(", ").join(colDef));
+
+        // bitmapIndex
+        if (CollectionUtils.isNotEmpty(getIndexes())) {
+            for (Index index : getIndexes()) {
+                sb.append(",\n");
+                sb.append("  ").append(index.toSql());
+            }
+        }
+
         sb.append(")");
         if (!Strings.isNullOrEmpty(this.getComment())) {
             sb.append("\nCOMMENT \"").append(getDisplayComment()).append("\"");
@@ -1127,6 +1136,14 @@ public class MaterializedView extends OlapTable implements GsonPreProcessable, G
         }
         if (!hasStorageMedium) {
             appendUniqueProperties(sb);
+        }
+
+        // bloom filter
+        Set<String> bfColumnNames = getCopiedBfColumns();
+        if (bfColumnNames != null) {
+            sb.append(StatsConstants.TABLE_PROPERTY_SEPARATOR).append(PropertyAnalyzer.PROPERTIES_BF_COLUMNS)
+                    .append("\" = \"");
+            sb.append(Joiner.on(", ").join(getCopiedBfColumns())).append("\"");
         }
 
         sb.append("\n)");
