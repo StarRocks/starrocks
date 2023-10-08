b4f501c7b77863fe3376007ed6a80886fa098672
Zhuhe Fang <fzhedu@gmail.com>
2023-10-08 15:27:24 +0800

    update tests
    Signed-off-by: Zhuhe Fang <fzhedu@gmail.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/AggregateTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/AggregateTest.java
@@ -1958,15 +1958,15 @@ public class AggregateTest extends PlanTestBase {
         // so count() will be a common expression
         assertContains(plan, "  3:Project\n" +
                 "  |  output columns:\n" +
-                "  |  18 <-> [25: sum, BIGINT, true] + [40: multiply, BIGINT, true]\n" +
-                "  |  19 <-> [27: sum, BIGINT, true] + [40: multiply, BIGINT, true]\n" +
+                "  |  18 <-> [25: sum, BIGINT, true] + [26: count, BIGINT, true] * 1\n" +
+                "  |  19 <-> [27: sum, BIGINT, true] + [28: count, BIGINT, true] * 1\n" +
                 "  |  20 <-> [29: sum, BIGINT, true] + [40: multiply, BIGINT, true]\n" +
                 "  |  21 <-> [32: sum, DOUBLE, true] + cast([33: count, BIGINT, true] as DOUBLE) * 1.0\n" +
                 "  |  22 <-> [34: sum, DOUBLE, true] + cast([35: count, BIGINT, true] as DOUBLE) * 1.0\n" +
                 "  |  23 <-> [36: sum, BIGINT, true] + [40: multiply, BIGINT, true]\n" +
-                "  |  24 <-> [38: sum, DECIMAL128(38,2), true] + cast([35: count, BIGINT, true] as DECIMAL128(18,0)) * 1\n" +
+                "  |  24 <-> [38: sum, DECIMAL128(38,2), true] + cast([39: count, BIGINT, true] as DECIMAL128(18,0)) * 1\n" +
                 "  |  common expressions:\n" +
-                "  |  40 <-> [35: count, BIGINT, true] * 1");
+                "  |  40 <-> [37: count, BIGINT, true] * 1");
 
         // 2. aggregate result reuse
         sql = "select sum(t1b), sum(t1b+1), sum(t1b+2) from test_all_type";
