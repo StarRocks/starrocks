0e0fce30f4b855181b11e72dcde2b1b156133fc6
Yixin Luo <18810541851@163.com>
2023-10-08 16:41:04 +0800

    [Enhancement] reduce disk cost of persistent index and do some refactoring (#31799)
    This PR contains several enhancement about disk cost:
    1. Not allow l0's memory size larger than index's total file size, because it is useless to improve IO, and it will also cost waste of disk.
    2. Delete useless l2 files immediately after persistent meta info.
    3. Allow major compaction when persistent index was expired.
    4. re-calculate `_usage_and_size_by_key_length`, to make estimated file size more smaller.
    
    And this PR also contains some refactoring:
    1. Remove `PersistentIndexMetaLock`, use index lock in TabletUpdate instead.
    2. Improve log print.

--- be/src/storage/primary_index.h
+++ be/src/storage/primary_index.h
@@ -27,7 +27,6 @@ namespace starrocks {
 
 class Tablet;
 class HashIndex;
-class PersistentIndexMetaLockGuard;
 
 const uint64_t ROWID_MASK = 0xffffffff;
 
@@ -111,8 +110,6 @@ public:
     Status commit(PersistentIndexMetaPB* index_meta);
 
     Status on_commited();
-
-    void get_persistent_index_meta_lock_guard(PersistentIndexMetaLockGuard* guard);
 
     double get_write_amp_score();
 
