10a9ea43f722ee4ddcea3d08009e4d6d427bca20
Smith Cruise <chendingchao1@126.com>
2023-10-09 04:25:41 -0500

    [Enhancement] Make iceberg aws authentication's error msg more clear (#32273)
    Signed-off-by: Smith Cruise <chendingchao1@126.com>

--- fe/fe-core/src/test/java/com/starrocks/connector/iceberg/io/IcebergCachingFileIOTest.java
+++ fe/fe-core/src/test/java/com/starrocks/connector/iceberg/io/IcebergCachingFileIOTest.java
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 package com.starrocks.connector.iceberg.io;
 
 import org.apache.hadoop.conf.Configuration;
