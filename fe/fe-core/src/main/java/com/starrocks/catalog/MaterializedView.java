6c02b6496c680b70fd85f73c4870f5008fa6cdbd
iverson <58965658+wanpengfei-git@users.noreply.github.com>
2023-10-10 19:38:43 +0800

    [Enhancement] allow creating index on mv (#5704)
    Co-authored-by: shshenhua <364382466@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/MaterializedView.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/MaterializedView.java
@@ -1017,6 +1017,15 @@ public class MaterializedView extends OlapTable implements GsonPreProcessable, G
             colDef.add(colSb.toString());
         }
         sb.append(Joiner.on(", ").join(colDef));
+
+        // bitmapIndex
+        if (CollectionUtils.isNotEmpty(getIndexes())) {
+            for (Index index : getIndexes()) {
+                sb.append(",\n");
+                sb.append("  ").append(index.toSql());
+            }
+        }
+
         sb.append(")");
         if (!Strings.isNullOrEmpty(this.getComment())) {
             sb.append("\nCOMMENT \"").append(this.getDisplayComment()).append("\"");
@@ -1101,6 +1110,14 @@ public class MaterializedView extends OlapTable implements GsonPreProcessable, G
         }
         if (!hasStorageMedium) {
             appendUniqueProperties(sb);
+        }
+
+        // bloom filter
+        Set<String> bfColumnNames = getCopiedBfColumns();
+        if (bfColumnNames != null) {
+            sb.append(StatsConstants.TABLE_PROPERTY_SEPARATOR).append(PropertyAnalyzer.PROPERTIES_BF_COLUMNS)
+                    .append("\" = \"");
+            sb.append(Joiner.on(", ").join(getCopiedBfColumns())).append("\"");
         }
 
         sb.append("\n)");
