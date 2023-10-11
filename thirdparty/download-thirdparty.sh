c1da5c9e23a79e3068a3cb3df9b781a750c47445
Your Name <you@example.com>
2023-10-11 07:37:38 +0000

    [Enhancement] SASL enables kerberos gssapi support
    Signed-off-by: Kevin Xiaohua Cai <caixiaohua@starrocks.com>

--- thirdparty/download-thirdparty.sh
+++ thirdparty/download-thirdparty.sh
@@ -477,6 +477,15 @@ fi
 echo "Finished patching $SERDES_SOURCE"
 cd -
 
+# patch sasl2
+cd $TP_SOURCE_DIR/$SASL_SOURCE
+if [ ! -f $PATCHED_MARK ] && [ $SASL_SOURCE = "cyrus-sasl-2.1.28" ]; then
+    patch -p1 < $TP_PATCH_DIR/sasl2-add-k5support-link.patch
+    touch $PATCHED_MARK
+fi
+echo "Finished patching $SASL_SOURCE"
+cd -
+
 # patch arrow
 if [[ -d $TP_SOURCE_DIR/$ARROW_SOURCE ]] ; then
     cd $TP_SOURCE_DIR/$ARROW_SOURCE
