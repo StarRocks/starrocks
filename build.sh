5910e78c39cafb979515a9245ef220237cc8d683
AndyZiYe <yeziyu@starrocks.com>
2023-10-12 15:31:58 +0800

    [Tool] add ci
    Signed-off-by: AndyZiYe <yeziyu@starrocks.com>

--- build.sh
+++ build.sh
@@ -15,6 +15,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+# test
 
 ##############################################################
 # This script is used to compile StarRocks
