7360323566060774f410b6cfc5243e670553b75b
trueeyu <lxhhust350@qq.com>
2023-10-12 20:15:27 +0800

    [Enhancement] Avoid build slice when building GlobalRuntimeFilter (#32557)
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/types/logical_type_infra.h
+++ be/src/types/logical_type_infra.h
@@ -47,6 +47,11 @@ namespace starrocks {
     M(TYPE_VARBINARY)                \
     M(TYPE_BOOLEAN)
 
+#define APPLY_FOR_ALL_STRING_TYPE(M) \
+    M(TYPE_VARCHAR)                  \
+    M(TYPE_CHAR)                     \
+    M(TYPE_VARBINARY)
+
 #define APPLY_FOR_ALL_SCALAR_TYPE_WITH_NULL(M) \
     APPLY_FOR_ALL_SCALAR_TYPE(M)               \
     M(TYPE_NULL)
