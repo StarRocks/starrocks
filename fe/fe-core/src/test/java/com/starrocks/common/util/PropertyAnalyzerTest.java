762016a718c91d012a9079ef9310eab495def222
xiangguangyxg <110401425+xiangguangyxg@users.noreply.github.com>
2023-10-12 20:41:24 +0800

    [Enhancement] Disable enable_async_write_back property (#32495)
    Signed-off-by: xiangguangyxg <xiangguangyxg@gmail.com>

---
+++ fe/fe-core/src/test/java/com/starrocks/common/util/PropertyAnalyzerTest.java
@@ -0,0 +1,39 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.starrocks.common.util;
+
+import com.starrocks.common.AnalysisException;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PropertyAnalyzerTest {
+
+    @Test
+    public void testAnalyzeDataCacheInfo() {
+        Map<String, String> properties = new HashMap<>();
+        properties.put(PropertyAnalyzer.PROPERTIES_DATACACHE_ENABLE, "true");
+        properties.put(PropertyAnalyzer.PROPERTIES_ENABLE_ASYNC_WRITE_BACK, "true");
+
+        try {
+            PropertyAnalyzer.analyzeDataCacheInfo(properties);
+            Assert.assertTrue(false);
+        } catch (AnalysisException e) {
+            Assert.assertEquals("enable_async_write_back is disabled since version 3.1.4", e.getMessage());
+        }
+    }
+}
