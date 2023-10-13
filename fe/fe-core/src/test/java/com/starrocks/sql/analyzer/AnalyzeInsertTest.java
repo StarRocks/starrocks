81ec3cff07362ceb9b0f932ef8bc3c39cec7935a
Letian Jiang <letian.jiang@outlook.com>
2023-10-13 23:55:19 +0800

    [BugFix] Ban compression codec of bz2 (#32708)
    Signed-off-by: Letian Jiang <letian.jiang@outlook.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzeInsertTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzeInsertTest.java
@@ -248,7 +248,7 @@ public class AnalyzeInsertTest {
                         "select \"abc\" as k1",
                 "compression is a mandatory property. " +
                 "Use \"compression\" = \"your_chosen_compression_type\". Supported compression types are" +
-                "(uncompressed, gzip, brotli, zstd, lz4, bz2).");
+                "(uncompressed, gzip, brotli, zstd, lz4).");
 
         analyzeFail("insert into files ( \n" +
                         "\t\"path\" = \"s3://path/to/directory/\", \n" +
@@ -256,10 +256,10 @@ public class AnalyzeInsertTest {
                         "\t\"compression\" = \"unknown\" ) \n" +
                         "select \"abc\" as k1",
                 "compression type unknown is not supported. " +
-                        "Use any of (uncompressed, gzip, brotli, zstd, lz4, bz2).");
+                        "Use any of (uncompressed, gzip, brotli, zstd, lz4).");
 
         analyzeFail("insert into files ( \n" +
-                        "\t\"path\" = \"oss://starrocks-dla-data-zhangjiakou/jiangletian/unload/test_partby_varchar/\", \n" +
+                        "\t\"path\" = \"s3://path/to/directory/\", \n" +
                         "\t\"format\"=\"parquet\", \n" +
                         "\t\"compression\" = \"uncompressed\", \n" +
                         "\t\"partition_by\"=\"k1\",\n" +
