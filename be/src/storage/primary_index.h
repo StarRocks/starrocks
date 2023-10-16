5a4bd3f3188658bcfccae709cfa445600ee1f9ff
luohaha <18810541851@163.com>
2023-10-16 11:46:36 +0800

    [Enhancement] reduce disk cost of persistent index and do some refactoring (backport #31799)
    Signed-off-by: luohaha <18810541851@163.com>

--- be/src/storage/primary_index.h
+++ be/src/storage/primary_index.h
@@ -15,7 +15,6 @@ namespace starrocks {
 
 class Tablet;
 class HashIndex;
-class PersistentIndexMetaLockGuard;
 
 const uint64_t ROWID_MASK = 0xffffffff;
 
@@ -99,8 +98,6 @@ public:
     Status commit(PersistentIndexMetaPB* index_meta);
 
     Status on_commited();
-
-    void get_persistent_index_meta_lock_guard(PersistentIndexMetaLockGuard* guard);
 
     double get_write_amp_score();
 
