b6907b80bf5ce0af0b51f4b972805a146dacfe30
ricky <rickif@qq.com>
2023-10-17 17:26:53 +0800

    [BugFix] optional map key for parquet (#28296)
    Fixes #32772
    If the parquet file is generated by hive, the key of map may be optional, which is not allowed by arrow.
    This PR adds the patch to arrow to remove the limit.

--- thirdparty/download-thirdparty.sh
+++ thirdparty/download-thirdparty.sh
@@ -482,6 +482,7 @@ if [[ -d $TP_SOURCE_DIR/$ARROW_SOURCE ]] ; then
     cd $TP_SOURCE_DIR/$ARROW_SOURCE
     if [ ! -f $PATCHED_MARK ] && [ $ARROW_SOURCE = "arrow-apache-arrow-5.0.0" ] ; then
         patch -p1 < $TP_PATCH_DIR/arrow-5.0.0-force-use-external-jemalloc.patch
+        patch -p1 < $TP_PATCH_DIR/arrow-5.0.0-parquet-map-key.patch
         touch $PATCHED_MARK
     fi
     cd -
