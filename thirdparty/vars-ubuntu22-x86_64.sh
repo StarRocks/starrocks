1707d06200bd4c29ddf776b46ff0e6a07114ea62
Gavin <yangguansuo@starrocks.com>
2023-10-18 04:01:19 +0000

    [BugFix] Choose the starcache library in starlet directory when building with staros.
    Signed-off-by: Gavin <yangguansuo@starrocks.com>

--- thirdparty/vars-ubuntu22-x86_64.sh
+++ thirdparty/vars-ubuntu22-x86_64.sh
@@ -28,7 +28,7 @@ JINDOSDK_SOURCE="jindosdk-4.6.8-linux-ubuntu22-x86_64"
 JINDOSDK_MD5SUM="52236053391091591c2d09684791e810"
 
 # starcache
-STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v2.0.0/ubuntu-x86_64/starcache.tar.gz"
+STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v2.0.1/starcache-ubuntu22_amd64.tar.gz"
 STARCACHE_NAME="starcache.tar.gz"
 STARCACHE_SOURCE="starcache"
-STARCACHE_MD5SUM="b8d9c0637e69a118e873eca48d3a5597"
+STARCACHE_MD5SUM="ae0ace22c981c96b1801589511a9e2b1"
