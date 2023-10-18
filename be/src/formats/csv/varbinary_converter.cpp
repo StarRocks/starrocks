e89101f9d7ca9b0b5121c6b871e1f8558d922e19
shuming.li <ming.moriarty@gmail.com>
2023-10-18 20:12:01 +0800

    fix bugs
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>

--- be/src/formats/csv/varbinary_converter.cpp
+++ be/src/formats/csv/varbinary_converter.cpp
@@ -93,7 +93,8 @@ bool VarBinaryConverter::read_string(Column* column, const Slice& s, const Optio
     return true;
 }
 
-bool VarBinaryConverter::read_quoted_string(Column* column, const Slice& s, const Options& options) const {
+bool VarBinaryConverter::read_quoted_string(Column* column, const Slice& tmp_s, const Options& options) const {
+    Slice s = tmp_s;
     // TODO: need write quote for binary?
     if (!remove_enclosing_quotes<'"'>(&s)) {
         return false;
