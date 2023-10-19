StarRocks/starrocks

commit 761167d2ab48b5d7b1eadbbd5d297630f69c1518

[Feature] Add materialized view metrics (#30735)
Signed-off-by: shuming.li <ming.moriarty@gmail.com>

fe/fe-core/src/main/java/com/starrocks/http/rest/MetricsAction.java
@@ -61,12 +61,48 @@ public class MetricsAction extends RestBaseAction {
     // `with_table_metrics=minified` : without tables that have empty values
     // `with_table_metrics=all` : with all table metrics
     protected static final String WITH_TABLE_METRICS_PARAM = "with_table_metrics";
-    protected static final String WITH_TABLE_METRICS_MINIFIED = "minified";
-    protected static final String WITH_TABLE_METRICS_ALL = "all";
+    protected static final String WITH_MATERIALIZED_VIEW_METRICS_PARAM = "with_materialized_view_metrics";
+    protected static final String COLLECT_MODE_METRICS_MINIFIED = "minified";
+    protected static final String COLLECT_MODE_METRICS_ALL = "all";
     public static final String API_PATH = "/metrics";
 
     public MetricsAction(ActionController controller) {
         super(controller);
+    }
+
+
+    public final class RequestParams {
+        // Whether to collect per table metrics
+        private final boolean collectTableMetrics;
+        // Whether to collect per table metrics in minified mode, Ignore some heavy metrics if true
+        private final boolean minifyTableMetrics;
+        // Whether to collect per materialized view metrics
+        private final boolean collectMVMetrics;
+        // Whether to collect per materialized view metrics in minified mode, Ignore some heavy metrics if true
+        private final boolean minifyMVMetrics;
+        RequestParams(boolean collectTableMetrics, boolean minifyTableMetrics,
+                      boolean collectMVMetrics, boolean minifyMVMetrics) {
+            this.collectTableMetrics = collectTableMetrics;
+            this.minifyTableMetrics = minifyTableMetrics;
+            this.collectMVMetrics = collectMVMetrics;
+            this.minifyMVMetrics = minifyMVMetrics;
+        }
+
+        public boolean isCollectTableMetrics() {
+            return collectTableMetrics;
+        }
+
+        public boolean isMinifyTableMetrics() {
+            return minifyTableMetrics;
+        }
+
+        public boolean isCollectMVMetrics() {
+            return collectMVMetrics;
+        }
+
+        public boolean isMinifyMVMetrics() {
+            return minifyMVMetrics;
+        }
     }
 
     public static void registerAction(ActionController controller) throws IllegalArgException {
@@ -75,6 +111,7 @@ public class MetricsAction extends RestBaseAction {
 
     @Override
     public void execute(BaseRequest request, BaseResponse response) throws DdlException {
+        // parse visitor type
         String type = request.getSingleParameter(TYPE_PARAM);
         MetricVisitor visitor = null;
         if (!Strings.isNullOrEmpty(type) && type.equalsIgnoreCase("core")) {
@@ -84,27 +121,34 @@ public class MetricsAction extends RestBaseAction {
         } else {
             visitor = new PrometheusMetricVisitor("starrocks_fe");
         }
-        boolean collectTableMetrics = false;
-        boolean minifyTableMetrics = true;
+
+        // parse request params
+        RequestParams requestParams = parseRequestParams(request);
+
+        response.setContentType("text/plain");
+        response.getContent().append(MetricRepo.getMetric(visitor, requestParams));
+        sendResult(request, response);
+    }
+
+    private RequestParams parseRequestParams(BaseRequest request) {
         String withTableMetrics = request.getSingleParameter(WITH_TABLE_METRICS_PARAM);
-        UserIdentity currentUser = null;
-        if (WITH_TABLE_METRICS_MINIFIED.equalsIgnoreCase(withTableMetrics) ||
-                WITH_TABLE_METRICS_ALL.equalsIgnoreCase(withTableMetrics)) {
+        String withMaterializedViewsMetrics = request.getSingleParameter(WITH_MATERIALIZED_VIEW_METRICS_PARAM);
+        // check request authorization
+        if (Strings.isNullOrEmpty(withMaterializedViewsMetrics) || Strings.isNullOrEmpty(withTableMetrics)) {
+            UserIdentity currentUser = null;
             try {
                 ActionAuthorizationInfo authInfo = getAuthorizationInfo(request);
                 currentUser = checkPassword(authInfo);
                 checkUserOwnsAdminRole(currentUser);
-                collectTableMetrics = true;
-                if (WITH_TABLE_METRICS_ALL.equalsIgnoreCase(withTableMetrics)) {
-                    minifyTableMetrics = false;
-                }
             } catch (AccessDeniedException e) {
                 LOG.warn("Auth failure when getting table level metrics, current user: {}, error msg: {}",
                         currentUser, e.getMessage(), e);
             }
         }
-        response.setContentType("text/plain");
-        response.getContent().append(MetricRepo.getMetric(visitor, collectTableMetrics, minifyTableMetrics));
-        sendResult(request, response);
+        boolean collectTableMetrics = COLLECT_MODE_METRICS_MINIFIED.equalsIgnoreCase(withTableMetrics);
+        boolean minifyTableMetrics = COLLECT_MODE_METRICS_ALL.equalsIgnoreCase(withTableMetrics);
+        boolean collectMVMetrics = COLLECT_MODE_METRICS_ALL.equalsIgnoreCase(withMaterializedViewsMetrics);
+        boolean minifyMVMetrics = COLLECT_MODE_METRICS_MINIFIED.equalsIgnoreCase(withMaterializedViewsMetrics);
+        return new RequestParams(collectTableMetrics, minifyTableMetrics, collectMVMetrics, minifyMVMetrics);
     }
 }

fe/fe-core/src/main/java/com/starrocks/metric/MaterializedViewMetricsEntity.java
@@ -1,0 +1,345 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+package com.starrocks.metric;
+
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.MetricRegistry;
+import com.google.common.collect.Lists;
+import com.starrocks.catalog.Database;
+import com.starrocks.catalog.MaterializedView;
+import com.starrocks.catalog.MvId;
+import com.starrocks.catalog.Table;
+import com.starrocks.common.Config;
+import com.starrocks.metric.Metric.MetricUnit;
+import com.starrocks.qe.ConnectContext;
+import com.starrocks.scheduler.PartitionBasedMvRefreshProcessor;
+import com.starrocks.scheduler.TaskBuilder;
+import com.starrocks.scheduler.TaskManager;
+import com.starrocks.server.GlobalStateMgr;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+
+public final class MaterializedViewMetricsEntity {
+    private static final Logger LOG = LogManager.getLogger(MaterializedViewMetricsEntity.class);
+
+
+    private final MvId mvId;
+    private final MetricRegistry metricRegistry;
+    private final List<Metric> metrics = Lists.newArrayList();
+
+    // refresh
+    // increased once the materialized view's refresh job is triggered.
+    public LongCounterMetric counterRefreshJobTotal;
+    // increased only if the materialized view's refresh job is success refreshed.
+    public LongCounterMetric counterRefreshJobSuccessTotal;
+    // increased once the materialized view's refresh job is failed.
+    public LongCounterMetric counterRefreshJobFailedTotal;
+    // increased once the materialized view's refresh job is not be triggered because of the refreshed data is empty.
+    public LongCounterMetric counterRefreshJobEmptyTotal;
+    // increased once the materialized view's refresh job checks whether the base table is changed or not.
+    public LongCounterMetric counterRefreshJobRetryCheckChangedTotal;
+
+    // query
+    // increased if the materialized view is considered in the preprocess for one query.
+    public LongCounterMetric counterQueryConsideredTotal;
+    // increased if the materialized view is successes to be rewritten from query, but it may be missed in the
+    // final plan because of the cost.
+    public LongCounterMetric counterQueryMatchedTotal;
+    // only increased if the materialized view is rewritten and not be queried directly
+    public LongCounterMetric counterQueryHitTotal;
+    // increased once the materialized view is used in the final plan no matter it is queried directly or rewritten.
+    public LongCounterMetric counterQueryMaterializedViewTotal;
+
+    // gauge
+    // the current pending refresh jobs for the materialized view
+    public GaugeMetric<Long> counterRefreshPendingJobs;
+    // the current running refresh jobs for the materialized view
+    public GaugeMetric<Long> counterRefreshRunningJobs;
+    // the current materialized view's row count
+    public GaugeMetric<Long> counterRowNums;
+    // the current materialized view's storage size, unit: byte
+    public GaugeMetric<Long> counterStorageSize;
+    // the current materialized view is active or not, 0: active, 1: inactive
+    public GaugeMetric<Integer> counterInactiveState;
+
+    // the current materialized view's partition count, 0 if the materialized view is not partitioned
+    public GaugeMetric<Integer> counterPartitionCount;
+
+    // histogram
+    // record the materialized view's refresh job duration only if it's refreshed successfully.
+    public Histogram histRefreshJobDuration;
+
+    public MaterializedViewMetricsEntity(MetricRegistry metricRegistry, MvId mvId) {
+        this.metricRegistry = metricRegistry;
+        this.mvId = mvId;
+
+        initMaterializedViewMetrics();
+    }
+
+    public List<Metric> getMetrics() {
+        return metrics;
+    }
+
+    protected void initMaterializedViewMetrics() {
+        // refresh metrics
+        counterRefreshJobTotal = new LongCounterMetric("mv_refresh_jobs", MetricUnit.REQUESTS,
+                "total materialized view's refresh jobs");
+        metrics.add(counterRefreshJobTotal);
+        counterRefreshJobSuccessTotal = new LongCounterMetric("mv_refresh_total_success_jobs", MetricUnit.REQUESTS,
+                "total materialized view's refresh success jobs");
+        metrics.add(counterRefreshJobSuccessTotal);
+        counterRefreshJobFailedTotal = new LongCounterMetric("mv_refresh_total_failed_jobs", MetricUnit.REQUESTS,
+                "total materialized view's refresh failed jobs");
+        metrics.add(counterRefreshJobFailedTotal);
+        counterRefreshJobEmptyTotal = new LongCounterMetric("mv_refresh_total_empty_jobs", MetricUnit.REQUESTS,
+                "total materialized view's refresh empty jobs");
+        metrics.add(counterRefreshJobEmptyTotal);
+        counterRefreshJobRetryCheckChangedTotal = new LongCounterMetric("mv_refresh_total_retry_meta_count", MetricUnit.REQUESTS,
+                "total materialized view's retry to check table change count");
+        metrics.add(counterRefreshJobRetryCheckChangedTotal);
+
+        // query metrics
+        counterQueryMaterializedViewTotal = new LongCounterMetric("mv_query_total_count", MetricUnit.REQUESTS,
+                "total materialized view's query count");
+        metrics.add(counterQueryMaterializedViewTotal);
+        counterQueryHitTotal = new LongCounterMetric("mv_query_total_hit_count", MetricUnit.REQUESTS,
+                "total hit materialized view's query count");
+        metrics.add(counterQueryHitTotal);
+        counterQueryConsideredTotal = new LongCounterMetric("mv_query_total_considered_count", MetricUnit.REQUESTS,
+                "total considered materialized view's query count");
+        metrics.add(counterQueryConsideredTotal);
+        counterQueryMatchedTotal = new LongCounterMetric("mv_query_total_matched_count", MetricUnit.REQUESTS,
+                "total matched materialized view's query count");
+        metrics.add(counterQueryMatchedTotal);
+
+        // histogram metrics
+        try {
+            Database db = GlobalStateMgr.getCurrentState().getDb(mvId.getDbId());
+            MaterializedView mv = (MaterializedView) db.getTable(mvId.getId());
+            histRefreshJobDuration = metricRegistry.histogram(MetricRegistry.name("mv_refresh_duration",
+                    db.getFullName(), mv.getName()));
+        } catch (Exception e) {
+            LOG.warn("Ignore histogram metrics for materialized view: {}", mvId);
+        }
+
+        // gauge metrics
+        counterRefreshPendingJobs = new GaugeMetric<Long>("mv_refresh_pending_jobs", MetricUnit.NOUNIT,
+                "current materialized view pending refresh jobs number") {
+            @Override
+            public Long getValue() {
+                String mvTaskName = TaskBuilder.getMvTaskName(mvId.getId());
+                TaskManager taskManager = GlobalStateMgr.getCurrentState().getTaskManager();
+                if (taskManager == null) {
+                    return 0L;
+                }
+                if (!taskManager.containTask(mvTaskName)) {
+                    return 0L;
+                }
+                Long taskId = taskManager.getTask(mvTaskName).getId();
+                return taskManager.getTaskRunManager().getPendingTaskRunCount(taskId);
+            }
+        };
+        metrics.add(counterRefreshPendingJobs);
+
+        counterRefreshRunningJobs = new GaugeMetric<Long>("mv_refresh_running_jobs", MetricUnit.NOUNIT,
+                "current materialized view running refresh jobs number") {
+            @Override
+            public Long getValue() {
+                String mvTaskName = TaskBuilder.getMvTaskName(mvId.getId());
+                TaskManager taskManager = GlobalStateMgr.getCurrentState().getTaskManager();
+                if (taskManager == null) {
+                    return 0L;
+                }
+                if (!taskManager.containTask(mvTaskName)) {
+                    return 0L;
+                }
+                Long taskId = taskManager.getTask(mvTaskName).getId();
+                if (taskManager.getTaskRunManager().containsTaskInRunningTaskRunMap(taskId)) {
+                    return 1L;
+                } else {
+                    return 0L;
+                }
+            }
+        };
+        metrics.add(counterRefreshRunningJobs);
+
+        counterRowNums = new GaugeMetric<Long>("mv_row_count", MetricUnit.NOUNIT,
+                "current materialized view's row count") {
+            @Override
+            public Long getValue() {
+                Database db = GlobalStateMgr.getCurrentState().getDb(mvId.getDbId());
+                if (db == null) {
+                    return 0L;
+                }
+                Table table = db.getTable(mvId.getId());
+                if (!table.isMaterializedView()) {
+                    return 0L;
+                }
+
+                db.readLock();
+                try {
+                    MaterializedView mv = (MaterializedView) table;
+                    return mv.getRowCount();
+                } catch (Exception e) {
+                    return 0L;
+                } finally {
+                    db.readUnlock();
+                }
+            }
+        };
+        metrics.add(counterRowNums);
+
+        counterStorageSize = new GaugeMetric<Long>("mv_storage_size", MetricUnit.NOUNIT,
+                "current materialized view's storage size") {
+            @Override
+            public Long getValue() {
+                Database db = GlobalStateMgr.getCurrentState().getDb(mvId.getDbId());
+                if (db == null) {
+                    return 0L;
+                }
+                Table table = db.getTable(mvId.getId());
+                if (!table.isMaterializedView()) {
+                    return 0L;
+                }
+
+                db.readLock();
+                try {
+                    MaterializedView mv = (MaterializedView) table;
+                    return mv.getDataSize();
+                } catch (Exception e) {
+                    return 0L;
+                } finally {
+                    db.readUnlock();
+                }
+            }
+        };
+        metrics.add(counterStorageSize);
+
+        counterInactiveState = new GaugeMetric<Integer>("mv_inactive_state", MetricUnit.NOUNIT,
+                "current materialized view's inactive or not, 0: active, 1: inactive") {
+            @Override
+            public Integer getValue() {
+                Database db = GlobalStateMgr.getCurrentState().getDb(mvId.getDbId());
+                if (db == null) {
+                    return 0;
+                }
+                Table table = db.getTable(mvId.getId());
+                if (!table.isMaterializedView()) {
+                    return 0;
+                }
+                db.readLock();
+                try {
+                    MaterializedView mv = (MaterializedView) table;
+                    return mv.isActive() ? 0 : 1;
+                } catch (Exception e) {
+                    return 0;
+                } finally {
+                    db.readUnlock();
+                }
+            }
+        };
+        metrics.add(counterInactiveState);
+
+        counterPartitionCount = new GaugeMetric<Integer>("mv_partition_count", MetricUnit.NOUNIT,
+                "current materialized view's partition count, 0 if the materialized view is not partitioned") {
+            @Override
+            public Integer getValue() {
+                Database db = GlobalStateMgr.getCurrentState().getDb(mvId.getDbId());
+                if (db == null) {
+                    return 0;
+                }
+                Table table = db.getTable(mvId.getId());
+                if (!table.isMaterializedView()) {
+                    return 0;
+                }
+                MaterializedView mv = (MaterializedView) table;
+
+                db.readLock();
+                try {
+                    if (!mv.getPartitionInfo().isPartitioned()) {
+                        return 0;
+                    }
+                    return mv.getPartitions().size();
+                } catch (Exception e) {
+                    return 0;
+                } finally {
+                    db.readUnlock();
+                }
+            }
+        };
+        metrics.add(counterInactiveState);
+    }
+
+    public static boolean isUpdateMaterializedViewMetrics(ConnectContext connectContext) {
+        if (connectContext == null) {
+            return false;
+        }
+        // ignore: explain queries
+        if (connectContext.getExplainLevel() != null) {
+            return false;
+        }
+        // ignore: queries that are not using materialized view rewrite(eg: stats jobs)
+        if (!connectContext.getSessionVariable().isEnableMaterializedViewRewrite() ||
+                !Config.enable_materialized_view) {
+            return false;
+        }
+        return true;
+    }
+
+    public void increaseQueryConsideredCount(long count) {
+        this.counterQueryConsideredTotal.increase(count);
+    }
+
+    public void increaseQueryMatchedCount(long count) {
+        this.counterQueryMatchedTotal.increase(count);
+    }
+
+    public void increaseQueryHitCount(long count) {
+        this.counterQueryHitTotal.increase(count);
+    }
+
+    public void increaseQueryMaterializedViewCount(long count) {
+        this.counterQueryMaterializedViewTotal.increase(count);
+    }
+
+    public void increaseRefreshJobStatus(PartitionBasedMvRefreshProcessor.RefreshJobStatus status) {
+        switch (status) {
+            case EMPTY:
+                this.counterRefreshJobEmptyTotal.increase(1L);
+                break;
+            case SUCCESS:
+                this.counterRefreshJobSuccessTotal.increase(1L);
+                break;
+            case FAILED:
+                this.counterRefreshJobFailedTotal.increase(1L);
+                break;
+            case TOTAL:
+                this.counterRefreshJobTotal.increase(1L);
+                break;
+        }
+    }
+
+    public void increaseRefreshRetryMetaCount(Long retryNum) {
+        this.counterRefreshJobRetryCheckChangedTotal.increase(retryNum);
+    }
+
+    public void updateRefreshDuration(long duration) {
+        this.histRefreshJobDuration.update(duration);
+    }
+}
+

commit c0442a2c9e928bb457c36f763ad99ec2486dfaa2

[BugFix] http metrics action fixes
* remove exception stacktrace log from auth check failure
* fix table metrics collection ignoring auth check result
  (regression in #30735)
* fix inverted use of `with_table_metrics` parameter
* fix inverted case of check auth

Fixes #32986

Signed-off-by: Kevin Cai <caixiaohua@starrocks.com>

fe/fe-core/src/main/java/com/starrocks/http/rest/MetricsAction.java
@@ -130,25 +130,36 @@ public class MetricsAction extends RestBaseAction {
         sendResult(request, response);
     }
 
-    private RequestParams parseRequestParams(BaseRequest request) {
+    protected RequestParams parseRequestParams(BaseRequest request) {
         String withTableMetrics = request.getSingleParameter(WITH_TABLE_METRICS_PARAM);
         String withMaterializedViewsMetrics = request.getSingleParameter(WITH_MATERIALIZED_VIEW_METRICS_PARAM);
+        /*
+         * Collect tableMetrics and MVMetrics in minified way by default.
+         * Full metrics collection is only enabled when the following conditions are all satisfied
+         * - explicitly has `?with_table_metrics=all` or `?with_materialized_view_metrics=all`
+         * - the user must have sufficient privileges by checking the request auth info
+         */
+        boolean collectTableMetrics = COLLECT_MODE_METRICS_ALL.equalsIgnoreCase(withTableMetrics);
+        boolean collectMVMetrics = COLLECT_MODE_METRICS_ALL.equalsIgnoreCase(withMaterializedViewsMetrics);
+
         // check request authorization
-        if (Strings.isNullOrEmpty(withMaterializedViewsMetrics) || Strings.isNullOrEmpty(withTableMetrics)) {
+        if (collectTableMetrics || collectMVMetrics) {
             UserIdentity currentUser = null;
             try {
                 ActionAuthorizationInfo authInfo = getAuthorizationInfo(request);
                 currentUser = checkPassword(authInfo);
                 checkUserOwnsAdminRole(currentUser);
             } catch (AccessDeniedException e) {
+                // disable Table related metrics collection due to AccessDenied
+                collectTableMetrics = false;
+                collectMVMetrics = false;
                 LOG.warn("Auth failure when getting table level metrics, current user: {}, error msg: {}",
-                        currentUser, e.getMessage(), e);
+                        currentUser, e.getMessage());
             }
         }
-        boolean collectTableMetrics = COLLECT_MODE_METRICS_MINIFIED.equalsIgnoreCase(withTableMetrics);
-        boolean minifyTableMetrics = COLLECT_MODE_METRICS_ALL.equalsIgnoreCase(withTableMetrics);
-        boolean collectMVMetrics = COLLECT_MODE_METRICS_ALL.equalsIgnoreCase(withMaterializedViewsMetrics);
-        boolean minifyMVMetrics = COLLECT_MODE_METRICS_MINIFIED.equalsIgnoreCase(withMaterializedViewsMetrics);
+
+        boolean minifyMVMetrics = !collectMVMetrics;
+        boolean minifyTableMetrics = !collectTableMetrics;
         return new RequestParams(collectTableMetrics, minifyTableMetrics, collectMVMetrics, minifyMVMetrics);
     }
 }

fe/fe-core/src/test/java/com/starrocks/http/MetricsActionTest.java
@@ -1,0 +1,129 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.starrocks.http;
+
+import com.starrocks.http.rest.MetricsAction;
+import io.netty.handler.codec.http.DefaultHttpHeaders;
+import io.netty.handler.codec.http.DefaultHttpRequest;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpVersion;
+import mockit.Expectations;
+import okhttp3.Credentials;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MetricsActionTest {
+    public static class MockMetricsAction extends MetricsAction {
+        public MockMetricsAction(ActionController controller) {
+            super(controller);
+        }
+
+        public MetricsAction.RequestParams  callParseRequestParams(BaseRequest request) {
+            return this.parseRequestParams(request);
+        }
+    }
+
+    private BaseRequest buildBaseRequest(String uri, boolean includeAuth) {
+        DefaultHttpHeaders headers = new DefaultHttpHeaders();
+        if (includeAuth) {
+            headers.add("Authorization", Credentials.basic("root", ""));
+        }
+        DefaultHttpRequest rawRequest =
+                new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, uri, headers);
+        return new BaseRequest(null, rawRequest, null);
+    }
+
+    @Test
+    public void testParseRequestParams() {
+        ActionController controller = new ActionController();
+        MockMetricsAction action = new MockMetricsAction(controller);
+
+        // All default, no parameter
+        {
+            BaseRequest request = buildBaseRequest("/metrics", true);
+            new Expectations(request) {
+                {
+                    request.getAuthorizationHeader();
+                    result = new Exception("Don't expect check auth header");
+                    minTimes = 0;
+                    maxTimes = 0;
+                }
+            };
+            MetricsAction.RequestParams params = action.callParseRequestParams(request);
+            Assert.assertNotNull(params);
+            Assert.assertFalse(params.isCollectMVMetrics());
+            Assert.assertTrue(params.isMinifyMVMetrics());
+            Assert.assertFalse(params.isCollectTableMetrics());
+            Assert.assertTrue(params.isMinifyTableMetrics());
+        }
+
+        // All default, want table_metrics, but no auth
+        {
+            BaseRequest request = buildBaseRequest("/metrics?with_table_metrics=all", false);
+            // expect check auth
+            new Expectations(request) {
+                {
+                    request.getAuthorizationHeader();
+                    minTimes = 1;
+                }
+            };
+            MetricsAction.RequestParams params = action.callParseRequestParams(request);
+            Assert.assertNotNull(params);
+            Assert.assertFalse(params.isCollectMVMetrics());
+            Assert.assertTrue(params.isMinifyMVMetrics());
+            // still minified way collecting table metrics
+            Assert.assertFalse(params.isCollectTableMetrics());
+            Assert.assertTrue(params.isMinifyTableMetrics());
+        }
+
+        // All default, has query parameter, has auth, only with_table_metrics
+        {
+            BaseRequest request = buildBaseRequest("/metrics?with_table_metrics=all", true);
+            // expect check auth
+            new Expectations(request) {
+                {
+                    // auth passed, retrieve the remote host from the request context
+                    request.getHostString();
+                    result = "127.0.0.1";
+                }
+            };
+            MetricsAction.RequestParams params = action.callParseRequestParams(request);
+            Assert.assertNotNull(params);
+            Assert.assertFalse(params.isCollectMVMetrics());
+            Assert.assertTrue(params.isMinifyMVMetrics());
+            Assert.assertTrue(params.isCollectTableMetrics());
+            Assert.assertFalse(params.isMinifyTableMetrics());
+        }
+
+        // All default, has query parameter, has auth, both with_table_metrics and with_materialized_view_metrics
+        {
+            BaseRequest request =
+                    buildBaseRequest("/metrics?with_table_metrics=all&with_materialized_view_metrics=all", true);
+            new Expectations(request) {
+                {
+                    // auth passed, retrieve the remote host from the request context
+                    request.getHostString();
+                    result = "127.0.0.1";
+                }
+            };
+            MetricsAction.RequestParams params = action.callParseRequestParams(request);
+            Assert.assertNotNull(params);
+            Assert.assertTrue(params.isCollectMVMetrics());
+            Assert.assertFalse(params.isMinifyMVMetrics());
+            Assert.assertTrue(params.isCollectTableMetrics());
+            Assert.assertFalse(params.isMinifyTableMetrics());
+        }
+    }
+}