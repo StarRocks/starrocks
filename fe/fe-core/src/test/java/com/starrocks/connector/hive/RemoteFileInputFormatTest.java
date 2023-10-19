ffe86e69ba3e621d4f753c7c49a4ab02307b53c2
Smith Cruise <chendingchao1@126.com>
2023-10-19 01:36:01 -0500

    [Enhancement] Add unkown file format in scan range (#33034)
    Signed-off-by: Smith Cruise <chendingchao1@126.com>
    (cherry picked from commit a5e193553411e0844cb721e560623ecf33656f50)
    
    # Conflicts:
    #	fe/fe-core/src/test/java/com/starrocks/connector/hive/RemoteFileInputFormatTest.java

--- fe/fe-core/src/test/java/com/starrocks/connector/hive/RemoteFileInputFormatTest.java
+++ fe/fe-core/src/test/java/com/starrocks/connector/hive/RemoteFileInputFormatTest.java
@@ -2,7 +2,11 @@
 
 package com.starrocks.connector.hive;
 
+<<<<<<< HEAD
 import com.starrocks.connector.hive.RemoteFileInputFormat;
+=======
+import com.starrocks.thrift.THdfsFileFormat;
+>>>>>>> a5e1935534 ([Enhancement] Add unkown file format in scan range (#33034))
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -13,5 +17,11 @@ public class RemoteFileInputFormatTest {
                 .fromHdfsInputFormatClass("org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat"));
         Assert.assertSame(RemoteFileInputFormat.ORC,
                 RemoteFileInputFormat.fromHdfsInputFormatClass("org.apache.hadoop.hive.ql.io.orc.OrcInputFormat"));
+    }
+
+    @Test
+    public void testUnknownFormat() {
+        RemoteFileInputFormat format = RemoteFileInputFormat.UNKNOWN;
+        Assert.assertEquals(THdfsFileFormat.UNKNOWN, format.toThrift());
     }
 }
