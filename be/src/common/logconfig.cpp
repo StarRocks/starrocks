03f02ddf6eed1fea3db0e3d0e5b5e79a1f81a521
Binglin Chang <decstery@gmail.com>
2023-10-20 23:28:54 +0800

    [Enhancement] Write fatal log to stderr together with crash stack trace (#33264)
    Signed-off-by: Binglin Chang <decstery@gmail.com>
    (cherry picked from commit 42c357cfb6dd53a37720e4e1836240fc57138297)

--- be/src/common/logconfig.cpp
+++ be/src/common/logconfig.cpp
@@ -133,8 +133,8 @@ bool init_glog(const char* basename, bool install_signal_handler) {
         google::InstallFailureSignalHandler();
     }
 
-    // Don't log to stderr.
-    FLAGS_stderrthreshold = 5;
+    // only write fatal log to stderr
+    FLAGS_stderrthreshold = 3;
     // Set glog log dir.
     FLAGS_log_dir = config::sys_log_dir;
     // 0 means buffer INFO only.
