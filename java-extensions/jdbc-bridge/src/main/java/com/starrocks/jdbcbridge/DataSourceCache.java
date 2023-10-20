67f0277695e413e1b7b8cb336974968c015c3f5e
eyes_on_me <3675229+silverbullet233@users.noreply.github.com>
2023-10-20 14:02:11 +0800

    [BugFix] enable non-general jdbc class can be loaded (#32702)
    Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>
    (cherry picked from commit ce80eca948bf8e895f574eff53ef4837092cc5c1)

--- java-extensions/jdbc-bridge/src/main/java/com/starrocks/jdbcbridge/DataSourceCache.java
+++ java-extensions/jdbc-bridge/src/main/java/com/starrocks/jdbcbridge/DataSourceCache.java
@@ -22,14 +22,45 @@ import java.util.function.Supplier;
 
 // a cache for get datasource
 public class DataSourceCache {
-    private final Map<String, HikariDataSource> sources = new ConcurrentHashMap<>();
+    public static final class DataSourceCacheItem {
+        private final HikariDataSource hikariDataSource;
+        private final ClassLoader classLoader;
+
+        public DataSourceCacheItem(HikariDataSource hikariDataSource, ClassLoader classLoader) {
+            this.hikariDataSource = hikariDataSource;
+            this.classLoader = classLoader;
+        }
+
+        public HikariDataSource getHikariDataSource() {
+            return hikariDataSource;
+        }
+
+        public ClassLoader getClassLoader() {
+            return classLoader;
+        }
+    }
+
+    private final Map<String, DataSourceCacheItem> sources = new ConcurrentHashMap<>();
     private static final DataSourceCache INSTANCE = new DataSourceCache();
+
+    // presumed existing private constructor
+    private DataSourceCache(){}
 
     public static DataSourceCache getInstance() {
         return INSTANCE;
     }
 
-    public HikariDataSource getSource(String driverId, Supplier<HikariDataSource> provider) {
-        return sources.computeIfAbsent(driverId, k -> provider.get());
+    public DataSourceCacheItem getSource(String driverId, Supplier<DataSourceCacheItem> provider) {
+        if (driverId == null || driverId.isEmpty()) {
+            throw new IllegalArgumentException("Driver ID cannot be null or empty");
+        }
+
+        return sources.computeIfAbsent(driverId, k -> {
+            DataSourceCacheItem item = provider.get();
+            if (item == null) {
+                throw new NullPointerException("DataSourceCacheItem supplier returned null");
+            }
+            return item;
+        });
     }
 }
