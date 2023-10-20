4be9ba3a6309f5320ff6712e8f89a9ec05962f04
gengjun-git <gengjun@starrocks.com>
2023-10-20 19:40:26 +0800

    resolve
    Signed-off-by: gengjun-git <gengjun@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/ConnectProcessor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/ConnectProcessor.java
@@ -62,11 +62,7 @@ import com.starrocks.rpc.RpcException;
 import com.starrocks.server.GlobalStateMgr;
 import com.starrocks.service.FrontendOptions;
 import com.starrocks.sql.analyzer.AstToSQLBuilder;
-<<<<<<< HEAD
 import com.starrocks.sql.analyzer.AstToStringBuilder;
-=======
-import com.starrocks.sql.ast.ExecuteStmt;
->>>>>>> 444cc3aeb1 ([BugFix] Fix sql is lost in audit log when creating OLAP table (#33176))
 import com.starrocks.sql.ast.KillStmt;
 import com.starrocks.sql.ast.QueryStatement;
 import com.starrocks.sql.ast.StatementBase;
