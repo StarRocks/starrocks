7b37fb4ee5d420b13be591f86f6ed49843dfdfb1
liuyehcf <1559500551@qq.com>
2023-10-20 09:50:27 +0800

    [BugFix] Fix 'remove_unused_buffer_values' of window function (#33032)
    Signed-off-by: liuyehcf <1559500551@qq.com>

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -714,6 +714,8 @@ CONF_Int64(pipeline_scan_queue_mode, "0");
 CONF_Int64(pipeline_scan_queue_level_time_slice_base_ns, "100000000");
 CONF_Double(pipeline_scan_queue_ratio_of_adjacent_queue, "1.5");
 
+CONF_Int32(pipeline_analytic_max_buffer_size, "128");
+
 /// For parallel scan on the single tablet.
 // These three configs are used to calculate the minimum number of rows picked up from a segment at one time.
 // It is `splitted_scan_bytes/scan_row_bytes` and restricted in the range [min_splitted_scan_rows, max_splitted_scan_rows].
