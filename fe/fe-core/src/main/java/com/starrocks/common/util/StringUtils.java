0a047f7d229811803fd3a56545ed2a8a454c0c99
shuming.li <ming.moriarty@gmail.com>
2023-10-20 13:22:16 +0800

    [BugFix] Make unqiue constraints case insensitive (#33234)
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/common/util/StringUtils.java
+++ fe/fe-core/src/main/java/com/starrocks/common/util/StringUtils.java
@@ -34,6 +34,8 @@
 
 package com.starrocks.common.util;
 
+import com.starrocks.catalog.Table;
+
 import java.security.SecureRandom;
 
 public class StringUtils {
@@ -52,4 +54,19 @@ public class StringUtils {
         return randomString.toString();
     }
 
+    /**
+     * Check two table names are equal or not.
+     * NOTE: OLAP table name is case-sensitive and other catalogs are not.
+     */
+    public static boolean areTableNamesEqual(Table table, String toCheck) {
+        if (table == null) {
+            return false;
+        }
+
+        if (table.isNativeTableOrMaterializedView()) {
+            return table.getName().equals(toCheck);
+        } else {
+            return table.getName().equalsIgnoreCase(toCheck);
+        }
+    }
 }
