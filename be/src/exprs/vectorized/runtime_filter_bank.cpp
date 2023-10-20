e43c4f5eea890f6cc4df8e149d575780426b9726
trueeyu <lxhhust350@qq.com>
2023-10-21 00:21:39 +0800

    [Enhancement] Avoid build slice when building GlobalRuntimeFilter (#32557) (#33282)
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/exprs/vectorized/runtime_filter_bank.cpp
+++ be/src/exprs/vectorized/runtime_filter_bank.cpp
@@ -93,21 +93,21 @@ struct FilterIniter {
 
         if (column->is_nullable()) {
             auto* nullable_column = ColumnHelper::as_raw_column<NullableColumn>(column);
-            auto& data_array = ColumnHelper::as_raw_column<ColumnType>(nullable_column->data_column())->get_data();
+            const auto& data_array = GetContainer<ptype>().get_data(nullable_column->data_column());
             for (size_t j = column_offset; j < data_array.size(); j++) {
                 if (!nullable_column->is_null(j)) {
-                    filter->insert(&data_array[j]);
+                    filter->insert(data_array[j]);
                 } else {
                     if (eq_null) {
-                        filter->insert(nullptr);
+                        filter->insert_null();
                     }
                 }
             }
 
         } else {
-            auto& data_ptr = ColumnHelper::as_raw_column<ColumnType>(column)->get_data();
+            const auto& data_ptr = GetContainer<ptype>().get_data(column);
             for (size_t j = column_offset; j < data_ptr.size(); j++) {
-                filter->insert(&data_ptr[j]);
+                filter->insert(data_ptr[j]);
             }
         }
         return nullptr;
