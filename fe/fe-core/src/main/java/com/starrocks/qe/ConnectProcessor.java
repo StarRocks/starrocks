b5a0543067598703a55702d2dc2da4bbdc44c77e
gengjun-git <gengjun@starrocks.com>
2023-10-20 19:30:05 +0800

    [BugFix] Fix sql is lost in audit log when creating OLAP table (#33176)
    Signed-off-by: gengjun-git <gengjun@starrocks.com>
    (cherry picked from commit 444cc3aeb183f5a978603473878913ea16838454)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/qe/ConnectProcessor.java

--- fe/fe-core/src/main/java/com/starrocks/qe/ConnectProcessor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/ConnectProcessor.java
@@ -62,7 +62,11 @@ import com.starrocks.rpc.RpcException;
 import com.starrocks.server.GlobalStateMgr;
 import com.starrocks.service.FrontendOptions;
 import com.starrocks.sql.analyzer.AstToSQLBuilder;
+<<<<<<< HEAD
 import com.starrocks.sql.analyzer.AstToStringBuilder;
+=======
+import com.starrocks.sql.ast.ExecuteStmt;
+>>>>>>> 444cc3aeb1 ([BugFix] Fix sql is lost in audit log when creating OLAP table (#33176))
 import com.starrocks.sql.ast.KillStmt;
 import com.starrocks.sql.ast.QueryStatement;
 import com.starrocks.sql.ast.StatementBase;
@@ -285,7 +289,7 @@ public class ConnectProcessor {
         }
         String sql;
         if (!ctx.getState().isQuery() && parsedStmt.needAuditEncryption()) {
-            sql = AstToStringBuilder.toString(parsedStmt);
+            sql = AstToSQLBuilder.toSQL(parsedStmt);
         } else {
             sql = parsedStmt.getOrigStmt().originStmt;
         }
