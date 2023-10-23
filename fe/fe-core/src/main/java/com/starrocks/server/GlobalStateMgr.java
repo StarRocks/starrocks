a9c20aebec9b5bded2201de032c8f792b9d745d7
yiming <107105845+nshangyiming@users.noreply.github.com>
2023-10-23 09:45:08 +0800

    [Enhancement] Support detect edit log port connectivity (#33117) (#33235)
    Signed-off-by: Dejun Xia <xiadejun@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
@@ -291,6 +291,7 @@ import com.starrocks.system.Backend;
 import com.starrocks.system.ComputeNode;
 import com.starrocks.system.Frontend;
 import com.starrocks.system.HeartbeatMgr;
+import com.starrocks.system.PortConnectivityChecker;
 import com.starrocks.system.SystemInfoService;
 import com.starrocks.task.AgentBatchTask;
 import com.starrocks.task.LeaderTaskExecutor;
@@ -377,6 +378,8 @@ public class GlobalStateMgr {
      * Alter Job Manager
      */
     private final AlterJobMgr alterJobMgr;
+
+    private final PortConnectivityChecker portConnectivityChecker;
 
     private Load load;
     private LoadMgr loadMgr;
@@ -665,6 +668,7 @@ public class GlobalStateMgr {
         // System Manager
         this.nodeMgr = new NodeMgr();
         this.heartbeatMgr = new HeartbeatMgr(!isCkptGlobalState);
+        this.portConnectivityChecker = new PortConnectivityChecker();
 
         // Alter Job Manager
         this.alterJobMgr = new AlterJobMgr();
@@ -1415,6 +1419,7 @@ public class GlobalStateMgr {
 
     // start threads that should run on all FE
     private void startAllNodeTypeDaemonThreads() {
+        portConnectivityChecker.start();
         tabletStatMgr.start();
         // load and export job label cleaner thread
         labelCleaner.start();
