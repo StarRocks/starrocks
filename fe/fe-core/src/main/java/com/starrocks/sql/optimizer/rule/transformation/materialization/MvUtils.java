c1c0768badad57dfab7fdf3c358ed8d68fe9e5f4
packy92 <110370499+packy92@users.noreply.github.com>
2023-10-24 13:35:02 +0800

    [BugFix] clone a scalarOperator before rewriting it (#33535)
    Signed-off-by: packy <wangchao@starrocks.com>
    (cherry picked from commit 2d6ba6abb1b405a0a126d68f336ff8a9f27646ed)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/materialization/MvUtils.java

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/materialization/MvUtils.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/materialization/MvUtils.java
@@ -497,6 +497,10 @@ public class MvUtils {
         if (predicate == null) {
             return null;
         }
+<<<<<<< HEAD
+=======
+        // do not change original predicate, clone it here
+>>>>>>> 2d6ba6abb1 ([BugFix] clone a scalarOperator before rewriting it (#33535))
         ScalarOperator cloned = predicate.clone();
         ScalarOperatorRewriter rewrite = new ScalarOperatorRewriter();
         return rewrite.rewrite(cloned, ScalarOperatorRewriter.DEFAULT_REWRITE_SCAN_PREDICATE_RULES);
@@ -513,6 +517,10 @@ public class MvUtils {
         if (predicate == null) {
             return null;
         }
+<<<<<<< HEAD
+=======
+        // do not change original predicate, clone it here
+>>>>>>> 2d6ba6abb1 ([BugFix] clone a scalarOperator before rewriting it (#33535))
         ScalarOperator cloned = predicate.clone();
         ScalarOperatorRewriter rewrite = new ScalarOperatorRewriter();
         return rewrite.rewrite(cloned, ScalarOperatorRewriter.MV_SCALAR_REWRITE_RULES);
