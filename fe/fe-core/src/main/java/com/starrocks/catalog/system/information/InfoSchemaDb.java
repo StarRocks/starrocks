64012861872cfbf4da07342f217fbac21ff93a6d
meegoo <meegoo.sr@gmail.com>
2023-10-23 19:26:32 -0700

    [Feature] Support query fe metrics from information_schea.fe_metris table (#33365)
    Signed-off-by: meegoo <meegoo.sr@gmail.com>
    (cherry picked from commit 3a3b531181ec93763e1934f938f124efb3354887)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/catalog/system/SystemId.java
    #	fe/fe-core/src/main/java/com/starrocks/catalog/system/information/InfoSchemaDb.java
    #	fe/fe-core/src/main/java/com/starrocks/planner/SchemaScanNode.java
    #	gensrc/thrift/Descriptors.thrift
    #	gensrc/thrift/PlanNodes.thrift

--- fe/fe-core/src/main/java/com/starrocks/catalog/system/information/InfoSchemaDb.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/system/information/InfoSchemaDb.java
@@ -28,6 +28,7 @@ public class InfoSchemaDb extends Database {
 
     public InfoSchemaDb() {
         super(SystemId.INFORMATION_SCHEMA_DB_ID, DATABASE_NAME);
+<<<<<<< HEAD
         super.registerTableUnlocked(TablesSystemTable.create());
         super.registerTableUnlocked(PartitionsSystemTableSystemTable.create());
         super.registerTableUnlocked(TablePrivilegesSystemTable.create());
@@ -66,6 +67,58 @@ public class InfoSchemaDb extends Database {
         super.registerTableUnlocked(BeLogsSystemTable.create());
         super.registerTableUnlocked(BeBvarsSystemTable.create());
         super.registerTableUnlocked(BeCloudNativeCompactionsSystemTable.create());
+=======
+        requireNonNull(catalogName, "catalogName is null");
+        super.setCatalogName(catalogName);
+
+        super.registerTableUnlocked(TablesSystemTable.create(catalogName));
+        super.registerTableUnlocked(PartitionsSystemTableSystemTable.create(catalogName));
+        super.registerTableUnlocked(TablePrivilegesSystemTable.create(catalogName));
+        super.registerTableUnlocked(ColumnPrivilegesSystemTable.create(catalogName));
+        super.registerTableUnlocked(ReferentialConstraintsSystemTable.create(catalogName));
+        super.registerTableUnlocked(KeyColumnUsageSystemTable.create(catalogName));
+        super.registerTableUnlocked(RoutinesSystemTable.create(catalogName));
+        super.registerTableUnlocked(SchemataSystemTable.create(catalogName));
+        super.registerTableUnlocked(ColumnsSystemTable.create(catalogName));
+        super.registerTableUnlocked(CharacterSetsSystemTable.create(catalogName));
+        super.registerTableUnlocked(CollationsSystemTable.create(catalogName));
+        super.registerTableUnlocked(TableConstraintsSystemTable.create(catalogName));
+        super.registerTableUnlocked(EnginesSystemTable.create(catalogName));
+        super.registerTableUnlocked(UserPrivilegesSystemTable.create(catalogName));
+        super.registerTableUnlocked(SchemaPrivilegesSystemTable.create(catalogName));
+        super.registerTableUnlocked(StatisticsSystemTable.create(catalogName));
+        super.registerTableUnlocked(TriggersSystemTable.create(catalogName));
+        super.registerTableUnlocked(EventsSystemTable.create(catalogName));
+        super.registerTableUnlocked(ViewsSystemTable.create(catalogName));
+
+        // tables that are only displayed in internal catalog
+        if (isInternalCatalog(catalogName)) {
+            super.registerTableUnlocked(TablesConfigSystemTable.create());
+            super.registerTableUnlocked(SessionVariablesSystemTable.create());
+            super.registerTableUnlocked(VerboseSessionVariablesSystemTable.create());
+            super.registerTableUnlocked(GlobalVariablesSystemTable.create());
+            super.registerTableUnlocked(TasksSystemTable.create());
+            super.registerTableUnlocked(TaskRunsSystemTable.create());
+            super.registerTableUnlocked(MaterializedViewsSystemTable.create());
+            super.registerTableUnlocked(LoadsSystemTable.create());
+            super.registerTableUnlocked(LoadTrackingLogsSystemTable.create());
+            super.registerTableUnlocked(RoutineLoadJobsSystemTable.create());
+            super.registerTableUnlocked(StreamLoadsSystemTable.create());
+            super.registerTableUnlocked(BeCompactionsSystemTable.create());
+            super.registerTableUnlocked(BeTabletsSystemTable.create());
+            super.registerTableUnlocked(BeMetricsSystemTable.create());
+            super.registerTableUnlocked(FeMetricsSystemTable.create());
+            super.registerTableUnlocked(BeTxnsSystemTable.create());
+            super.registerTableUnlocked(BeConfigsSystemTable.create());
+            super.registerTableUnlocked(FeTabletSchedulesSystemTable.create());
+            super.registerTableUnlocked(BeThreadsSystemTable.create());
+            super.registerTableUnlocked(BeLogsSystemTable.create());
+            super.registerTableUnlocked(BeBvarsSystemTable.create());
+            super.registerTableUnlocked(BeCloudNativeCompactionsSystemTable.create());
+            super.registerTableUnlocked(PipeFileSystemTable.create());
+            super.registerTableUnlocked(PipesSystemTable.create());
+        }
+>>>>>>> 3a3b531181 ([Feature] Support query fe metrics from information_schea.fe_metris table (#33365))
     }
 
     @Override
