e9348b0e6c9e3401fb8bda2b3ed7c6d8cd5fe24e
wyb <wybb86@gmail.com>
2023-10-24 21:40:21 +0800

    [BugFix] Check column exist in cloud native vertical compaction (#33553)
    delete where pk_column = xxx will generate an empty segment only has pk columns in footer,
    and footer schema is different from tablet schema.
    should check column exist when getting column reader from segment.
    
    Signed-off-by: wyb <wybb86@gmail.com>
    (cherry picked from commit b67624d3f6f01a358cd931f8101e20b5d59780d6)

--- be/src/storage/vertical_compaction_task.cpp
+++ be/src/storage/vertical_compaction_task.cpp
@@ -170,12 +170,7 @@ StatusOr<int32_t> VerticalCompactionTask::_calculate_chunk_size_for_column_group
         total_num_rows += rowset->num_rows();
         for (auto& segment : rowset->segments()) {
             for (uint32_t column_index : column_group) {
-                auto uid = _tablet_schema->column(column_index).unique_id();
-                if (!segment->is_valid_column(uid)) {
-                    continue;
-                }
-
-                const auto* column_reader = segment->column_with_uid(uid);
+                const auto* column_reader = segment->column(column_index);
                 if (column_reader == nullptr) {
                     continue;
                 }
