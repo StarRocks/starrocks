e190abc5402abd29eee93b57d140747e4e53765d
meegoo <meegoo.sr@gmail.com>
2023-10-23 18:43:52 -0700

    [BugFix] Fix replicated storage not fail directly when sink fail (#33507)
    Signed-off-by: meegoo <meegoo.sr@gmail.com>

--- be/src/exec/tablet_sink.cpp
+++ be/src/exec/tablet_sink.cpp
@@ -1188,7 +1188,8 @@ Status OlapTableSink::open_wait() {
             }
         });
 
-        if (has_intolerable_failure()) {
+        // when enable replicated storage, we only send to primary replica, one node channel fail lead to indicate whole load fail
+        if (has_intolerable_failure() || (_enable_replicated_storage && !err_st.ok())) {
             LOG(WARNING) << "Open channel failed. load_id: " << _load_id << ", error: " << err_st.to_string();
             return err_st;
         }
@@ -1205,7 +1206,7 @@ Status OlapTableSink::open_wait() {
                 }
             });
 
-            if (index_channel->has_intolerable_failure()) {
+            if (index_channel->has_intolerable_failure() || (_enable_replicated_storage && !err_st.ok())) {
                 LOG(WARNING) << "Open channel failed. load_id: " << _load_id << ", error: " << err_st.to_string();
                 return err_st;
             }
@@ -1312,7 +1313,7 @@ Status OlapTableSink::_incremental_open_node_channel(const std::vector<TOlapTabl
             }
         });
 
-        if (channel->has_intolerable_failure()) {
+        if (channel->has_intolerable_failure() || (_enable_replicated_storage && !err_st.ok())) {
             LOG(WARNING) << "Open channel failed. load_id: " << _load_id << ", error: " << err_st.to_string();
             return err_st;
         }
@@ -1683,7 +1684,7 @@ Status OlapTableSink::try_close(RuntimeState* state) {
         }
     }
 
-    if (intolerable_failure) {
+    if (intolerable_failure || (_enable_replicated_storage && !err_st.ok())) {
         return err_st;
     } else {
         return Status::OK();
@@ -1832,7 +1833,7 @@ Status OlapTableSink::close_wait(RuntimeState* state, Status close_status) {
                     }
                     ch->time_report(&node_add_batch_counter_map, &serialize_batch_ns, &actual_consume_ns);
                 });
-                if (has_intolerable_failure()) {
+                if (has_intolerable_failure() || (_enable_replicated_storage && !err_st.ok())) {
                     status = err_st;
                     for_each_node_channel([&status](NodeChannel* ch) { ch->cancel(status); });
                 }
@@ -1851,7 +1852,7 @@ Status OlapTableSink::close_wait(RuntimeState* state, Status close_status) {
                         }
                         ch->time_report(&node_add_batch_counter_map, &serialize_batch_ns, &actual_consume_ns);
                     });
-                    if (index_channel->has_intolerable_failure()) {
+                    if (index_channel->has_intolerable_failure() || (_enable_replicated_storage && !err_st.ok())) {
                         status = err_st;
                         index_channel->for_each_node_channel([&status](NodeChannel* ch) { ch->cancel(status); });
                     }
