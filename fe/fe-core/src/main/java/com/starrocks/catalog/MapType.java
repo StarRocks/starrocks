f45fe7fada9dc9f57ba9aa8377b0f7b4590ed307
Felix Li <astralidea@163.com>
2023-10-24 10:01:54 +0800

    [Enhancement] information_schema.COLUMNS support complex type (#33431)
    Signed-off-by: Astralidea <astralidea@163.com>
    (cherry picked from commit a1a8427d4e70a8bc454fbb68702c47bffe12a118)

--- fe/fe-core/src/main/java/com/starrocks/catalog/MapType.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/MapType.java
@@ -205,5 +205,14 @@ public class MapType extends Type {
             return new MapType(keyType, valueType);
         }
     }
+
+    public String toMysqlDataTypeString() {
+        return "map";
+    }
+
+    // This implementation is the same as BE schema_columns_scanner.cpp type_to_string
+    public String toMysqlColumnTypeString() {
+        return toSql();
+    }
 }
 
