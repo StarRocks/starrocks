e254ba11aafa1efc9119626ce5764461fa92abbe
pengbo <pegnbo.li@aloudata.com>
2023-10-25 17:09:23 +0800

    [Feature] support Oracle jdbc catalog (#30061)

--- fe/fe-core/src/main/java/com/starrocks/connector/jdbc/JDBCMetadata.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/jdbc/JDBCMetadata.java
@@ -59,6 +59,8 @@ public class JDBCMetadata implements ConnectorMetadata {
             schemaResolver = new MysqlSchemaResolver();
         } else if (properties.get(JDBCResource.DRIVER_CLASS).toLowerCase().contains("postgresql")) {
             schemaResolver = new PostgresSchemaResolver();
+        } else if (properties.get(JDBCResource.DRIVER_CLASS).toLowerCase().contains("oracledriver")) {
+            schemaResolver = new OracleSchemaResolver();
         } else {
             LOG.warn("{} not support yet", properties.get(JDBCResource.DRIVER_CLASS));
             throw new StarRocksConnectorException(properties.get(JDBCResource.DRIVER_CLASS) + " not support yet");
