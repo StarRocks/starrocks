fe08070a12a264bca68e519e88f265a9b22c201e
trueeyu <lxhhust350@qq.com>
2023-10-26 17:53:46 +0800

    [Enhancement] Avoid lock when get the length of pipeline block queue (#33721)
    Signed-off-by: trueeyu <lxhhust350@qq.com>
    (cherry picked from commit 575e3dacd6d4313552df4a85e001157513178048)
    
    # Conflicts:
    #	be/src/exec/pipeline/pipeline_driver_poller.h

--- be/src/exec/pipeline/pipeline_driver_poller.h
+++ be/src/exec/pipeline/pipeline_driver_poller.h
@@ -24,24 +24,20 @@ public:
             : _driver_queue(driver_queue),
               _polling_thread(nullptr),
               _is_polling_thread_initialized(false),
-              _is_shutdown(false) {}
+              _is_shutdown(false),
+              _blocked_driver_queue_len(0) {}
 
     using DriverList = std::list<DriverRawPtr>;
 
     ~PipelineDriverPoller() { shutdown(); };
-    // start poller thread
     void start();
-    // shutdown poller thread
     void shutdown();
     // add blocked driver to poller
     void add_blocked_driver(const DriverRawPtr driver);
     // remove blocked driver from poller
     void remove_blocked_driver(DriverList& local_blocked_drivers, DriverList::iterator& driver_it);
     // only used for collect metrics
-    size_t blocked_driver_queue_len() const {
-        std::shared_lock guard(_local_mutex);
-        return _local_blocked_drivers.size();
-    }
+    size_t blocked_driver_queue_len() const { return _blocked_driver_queue_len; }
 
     void iterate_immutable_driver(const IterateImmutableDriverFunc& call) const;
 
@@ -50,7 +46,6 @@ private:
     PipelineDriverPoller(const PipelineDriverPoller&) = delete;
     PipelineDriverPoller& operator=(const PipelineDriverPoller&) = delete;
 
-private:
     mutable std::mutex _global_mutex;
     std::condition_variable _cond;
     DriverList _blocked_drivers;
@@ -62,6 +57,16 @@ private:
     scoped_refptr<Thread> _polling_thread;
     std::atomic<bool> _is_polling_thread_initialized;
     std::atomic<bool> _is_shutdown;
+<<<<<<< HEAD
+=======
+
+    // NOTE: The `driver` can be stored in the parked drivers when it will never not be called to run.
+    // The parked driver needs to be actived when it needs to be triggered again.
+    mutable std::mutex _global_parked_mutex;
+    DriverList _parked_drivers;
+
+    std::atomic<size_t> _blocked_driver_queue_len;
+>>>>>>> 575e3dacd6 ([Enhancement] Avoid lock when get the length of pipeline block queue (#33721))
 };
 } // namespace pipeline
 } // namespace starrocks
