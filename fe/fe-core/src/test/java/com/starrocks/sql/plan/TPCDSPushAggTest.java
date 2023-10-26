eaa562a3eb8148ce01963ab5bbd9120e26a2a2a9
Youngwb <yangwenbo_mailbox@163.com>
2023-10-26 20:21:16 +0800

    fix ut
    Signed-off-by: Youngwb <yangwenbo_mailbox@163.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/TPCDSPushAggTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/TPCDSPushAggTest.java
@@ -17,9 +17,7 @@ package com.starrocks.sql.plan;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.stream.Stream;
