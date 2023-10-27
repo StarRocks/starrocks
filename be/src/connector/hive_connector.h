f98d44c22eba27ec1725148d37211c2be4c5ec2b
dirtysalt <dirtysalt1987@gmail.com>
2023-10-27 11:43:14 +0800

    [Feature] pass `hadoop-ext` related fields to jni scanner (#33660)
    Signed-off-by: yanz <dirtysalt1987@gmail.com>
    (cherry picked from commit 5848af94d0cbbb1ce8f0d02b19e782529b134947)
    
    # Conflicts:
    #	java-extensions/paimon-reader/src/main/java/com/starrocks/paimon/reader/PaimonSplitScanner.java

--- be/src/connector/hive_connector.h
+++ be/src/connector/hive_connector.h
@@ -78,8 +78,8 @@ private:
 
     Status _init_partition_values();
     Status _init_scanner(RuntimeState* state);
-    HdfsScanner* _create_hudi_jni_scanner();
-    HdfsScanner* _create_paimon_jni_scanner(FSOptions& options);
+    HdfsScanner* _create_hudi_jni_scanner(const FSOptions& options);
+    HdfsScanner* _create_paimon_jni_scanner(const FSOptions& options);
     Status _check_all_slots_nullable();
 
     // =====================================
