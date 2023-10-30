6972042d8cf9a0c22dfe0dfc58af10cb9c04d95b
liuyehcf <1559500551@qq.com>
2023-10-30 11:39:50 +0800

    [BugFix] Fix be crash of using closure that already released (#33876)
    Signed-off-by: liuyehcf <1559500551@qq.com>
    (cherry picked from commit 9c84d998cac6625020c837ccfa8a094051747021)

--- be/src/runtime/sender_queue.cpp
+++ be/src/runtime/sender_queue.cpp
@@ -576,6 +576,7 @@ void DataStreamRecvr::PipelineSenderQueue::clean_buffer_queues() {
                     item.closure->Run();
                 }
             }
+            chunk_queue.clear();
         }
     }
 }
