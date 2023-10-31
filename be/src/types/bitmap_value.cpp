95ff613f09c6508e992b1b134e25baa114e2b912
trueeyu <lxhhust350@qq.com>
2023-10-31 22:07:00 +0800

    [Enhancement] Optimize the performance of bitmap_xor using native roaring_bitmap function (#34069)
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/types/bitmap_value.cpp
+++ be/src/types/bitmap_value.cpp
@@ -464,10 +464,7 @@ BitmapValue& BitmapValue::operator^=(const BitmapValue& rhs) {
             _type = BITMAP;
             break;
         case BITMAP: {
-            BitmapValue rhs_bitmap(rhs);
-            *rhs_bitmap._bitmap -= *_bitmap;
-            *_bitmap -= *rhs._bitmap;
-            *_bitmap |= *rhs_bitmap._bitmap;
+            *_bitmap ^= *rhs._bitmap;
             break;
         }
         case SET: {
