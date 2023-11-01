099991a5b36319bd30b2abe426547c5e1ca0bbc4
trueeyu <lxhhust350@qq.com>
2023-11-01 10:39:01 +0800

    [Refactor] Add interface reset() for BitmapValue (#34014)
    Signed-off-by: trueeyu <lxhhust350@qq.com>
    (cherry picked from commit 67c928547d4279214d57a51770b343b83e10dcfd)

--- be/test/exprs/bitmap_functions_test.cpp
+++ be/test/exprs/bitmap_functions_test.cpp
@@ -2041,7 +2041,6 @@ TEST_F(VecBitmapFunctionsTest, bitmapToBase64Test) {
         Columns columns;
         auto s = BitmapColumn::create();
         BitmapValue empty;
-        empty.clear();
         s->append(&empty);
         columns.push_back(s);
 
@@ -2381,7 +2380,6 @@ TEST_F(VecBitmapFunctionsTest, sub_bitmap_special_cases) {
     {
         auto bitmap_column = BitmapColumn::create();
         BitmapValue empty;
-        empty.clear();
         bitmap_column->append(&empty);
 
         Columns columns;
@@ -2830,7 +2828,6 @@ TEST_F(VecBitmapFunctionsTest, sub_bitmap_limit_special_cases) {
     {
         auto bitmap_column = BitmapColumn::create();
         BitmapValue empty;
-        empty.clear();
         bitmap_column->append(&empty);
 
         Columns columns;
@@ -3299,7 +3296,6 @@ TEST_F(VecBitmapFunctionsTest, bitmap_subset_in_range_special_cases) {
     {
         auto bitmap_column = BitmapColumn::create();
         BitmapValue empty;
-        empty.clear();
         bitmap_column->append(&empty);
 
         Columns columns;
