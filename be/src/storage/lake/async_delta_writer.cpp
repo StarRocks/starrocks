d4ffd45bf80ea2f1319181462bbf721fcee3566b
Alex Zhu <zhuming9011@gmail.com>
2023-11-02 20:29:46 +0800

    [Enhancement] Improve aborting transactions in shared data mode (#34185)
    This patch contains the following changes:
    1. Enhance transaction abort handling by including the ability to cancel active tasks.
    2. Reduce the number of abort transaction tasks submitted to the thread pool to avoid filling up the thread pool queue.
    
    In the previous implementation, if a transaction is aborted by FE because of timeout, it will send abortTxn() request to BE, and BE will try to read the txn log and clear the files recorded in the txn log, but no files will be deleted because the ingestion task is still in progress at this time, and the txn log is not generated. When the ingestion task on the BE is finished, the loadTxnCommit() request is sent to the FE, at this time, the commit will fail because the transaction is already in the aborted state, however, the BE will not clean up the files that have been written after the commit fails, so garbage data files are left on the object store.
    
    Signed-off-by: Alex Zhu <zhuming9011@gmail.com>
    (cherry picked from commit d103dc9e06610ab34155605405e7a937d0e4794c)
    
    # Conflicts:
    #	be/src/storage/lake/async_delta_writer.cpp

--- be/src/storage/lake/async_delta_writer.cpp
+++ be/src/storage/lake/async_delta_writer.cpp
@@ -168,10 +168,26 @@ inline void AsyncDeltaWriterImpl::write(const Chunk* chunk, const uint32_t* inde
     task.cb = std::move(cb); // Do NOT touch |cb| since here
     task.finish_after_write = false;
     if (int r = bthread::execution_queue_execute(_queue_id, task); r != 0) {
-        task.cb(Status::InternalError("AsyncDeltaWriterImpl not open()ed or has been close()ed"));
+        task.cb(Status::InternalError("AsyncDeltaWriterImpl not opened or has been closed"));
     }
 }
 
+<<<<<<< HEAD
+=======
+inline void AsyncDeltaWriterImpl::flush(Callback cb) {
+    Task task;
+    task.chunk = nullptr;
+    task.indexes = nullptr;
+    task.indexes_size = 0;
+    task.flush_after_write = true;
+    task.cb = std::move(cb); // Do NOT touch |cb| since here
+    if (int r = bthread::execution_queue_execute(_queue_id, task); r != 0) {
+        LOG(WARNING) << "Fail to execution_queue_execute: " << r;
+        task.cb(Status::InternalError("AsyncDeltaWriterImpl not opened or has been closed"));
+    }
+}
+
+>>>>>>> d103dc9e06 ([Enhancement] Improve aborting transactions in shared data mode (#34185))
 inline void AsyncDeltaWriterImpl::finish(Callback cb) {
     Task task;
     task.chunk = nullptr;
@@ -184,7 +200,7 @@ inline void AsyncDeltaWriterImpl::finish(Callback cb) {
     // by the submitted tasks.
     if (int r = bthread::execution_queue_execute(_queue_id, task); r != 0) {
         LOG(WARNING) << "Fail to execution_queue_execute: " << r;
-        task.cb(Status::InternalError("AsyncDeltaWriterImpl not open()ed or has been close()ed"));
+        task.cb(Status::InternalError("AsyncDeltaWriterImpl not opened or has been closed"));
     }
 }
 
