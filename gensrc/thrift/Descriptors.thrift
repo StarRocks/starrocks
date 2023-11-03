087233a1070c2c6c69d253d521758fe064be0561
before-Sunrise <71162020+before-Sunrise@users.noreply.github.com>
2023-11-03 19:52:02 +0800

    [Feature] support avro/sequence file/ rcfile for hive table and external file table in jni scanner (#34028)
    Signed-off-by: before-Sunrise <unclejyj@gmail.com>

--- gensrc/thrift/Descriptors.thrift
+++ gensrc/thrift/Descriptors.thrift
@@ -65,15 +65,15 @@ struct TTupleDescriptor {
 enum THdfsFileFormat {
   TEXT = 0,
   LZO_TEXT = 1,
-  RC_FILE = 2,
-  SEQUENCE_FILE = 3,
+  RC_BINARY = 2,
+  RC_TEXT = 3,
   AVRO = 4,
   PARQUET = 5,
   ORC = 6,
+  SEQUENCE_FILE = 7,
 
   UNKNOWN = 100
 }
-
 
 // Text file desc
 struct TTextFileDesc {
@@ -372,6 +372,18 @@ struct THdfsTable {
 
     // The prefixes of locations of partitions in this table
     5: optional list<string> partition_prefixes
+
+    // hive table hive_column_names
+    6: optional string hive_column_names
+
+    // hive table hive_column_types
+    7: optional string hive_column_types
+
+    // hive table input_format
+    8: optional string input_format
+
+    // hive table serde_lib
+    9: optional string serde_lib
 }
 
 struct TFileTable {
@@ -380,6 +392,14 @@ struct TFileTable {
 
     // Schema columns
     2: optional list<TColumn> columns
+
+    3: optional string hive_column_names
+
+    4: optional string hive_column_types
+
+    5: optional string input_format
+
+    6: optional string serde_lib
 }
 
 struct TTableFunctionTable {
