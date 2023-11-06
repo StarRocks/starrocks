2580d581d1a1ef235a1fd8b55dc29e437188b570
ABingHuang <codekhuang@163.com>
2023-11-06 17:17:44 +0800

    fix ut
    Signed-off-by: ABingHuang <codekhuang@163.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/optimizer/rule/transformation/materialization/MvRefreshAndRewriteJDBCTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/optimizer/rule/transformation/materialization/MvRefreshAndRewriteJDBCTest.java
@@ -123,6 +123,7 @@ public class MvRefreshAndRewriteJDBCTest extends MvRewriteTestBase {
                 "distributed by hash(a) " +
                 "REFRESH DEFERRED MANUAL " +
                 "PROPERTIES (\n" +
+                "\"query_rewrite_consistency\" = \"disable\",\n" +
                 "'replication_num' = '1'" +
                 ") " +
                 "as select  t1.a, t2.b, t3.c, t1.d " +
