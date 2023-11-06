fb2baf442798d1881a87c08438b51c304d70face
leorishdu <18771113323@163.com>
2023-11-06 11:33:44 +0800

    [BugFix] Fix complete clucene deps

--- thirdparty/vars.sh
+++ thirdparty/vars.sh
@@ -411,9 +411,9 @@ LLVM_MD5SUM="dc13938a604f70379d3b38d09031de98"
 
 #clucene
 CLUCENE_DOWNLOAD="https://github.com/StarRocks/clucene/archive/refs/tags/starrocks-2023.11.06.tar.gz"
-CLUCENE_NAME="libclucene-v2.4.4.zip"
-CLUCENE_SOURCE="libclucene-v2.4.4"
-CLUCENE_MD5SUM="390b8df03f08ba0a20453d55c21aa9e5"
+CLUCENE_NAME="starrocks-clucene-2023.11.06.tar.gz"
+CLUCENE_SOURCE="starrocks-clucene-2023.11.06"
+CLUCENE_MD5SUM="6837d64c30c2559c6c512e279856776f"
 
 # all thirdparties which need to be downloaded is set in array TP_ARCHIVES
 TP_ARCHIVES="CLUCENE LIBEVENT OPENSSL THRIFT PROTOBUF GFLAGS GLOG GTEST RAPIDJSON SIMDJSON SNAPPY GPERFTOOLS ZLIB LZ4 BZIP CURL \
