527783687b672f0e55aa4b02fdbec2cd6068eb06
Felix Li <astralidea@163.com>
2023-11-07 10:30:19 +0800

    [BugFix] Enhance the robustness of dynamic partitioning. (#34289)
    Signed-off-by: Astralidea <astralidea@163.com>
    (cherry picked from commit bf8a3ba347c28d6ff71b6000437c47723380fd37)

--- fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
+++ fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
@@ -4258,6 +4258,8 @@ public class LocalMetastore implements ConnectorMetadata {
                     }
                 }
             }
+        } catch (Exception ex) {
+            LOG.warn("The replay log failed and this log was ignored.", ex);
         } finally {
             db.writeUnlock();
         }
