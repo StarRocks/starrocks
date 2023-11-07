34391aea9f2aca6963445aa9dbd5a995d7b53d2f
Alex Zhu <zhuming9011@gmail.com>
2023-11-08 04:40:23 +0800

    [Enhancement] Refine error prompt (#34322)
    Fixes #34321
    
    Signed-off-by: Alex Zhu <zhuming9011@gmail.com>
    (cherry picked from commit c1259dee3ab05035ec4f47a990299f73a8f9b085)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/fs/hdfs/HdfsFsManager.java

--- fe/fe-core/src/main/java/com/starrocks/fs/hdfs/HdfsFsManager.java
+++ fe/fe-core/src/main/java/com/starrocks/fs/hdfs/HdfsFsManager.java
@@ -1077,6 +1077,25 @@ public class HdfsFsManager {
         getFileSystem(path, loadProperties, tProperties);
     }
 
+<<<<<<< HEAD
+=======
+    public List<FileStatus> listFileMeta(String path, Map<String, String> properties) throws UserException {
+        WildcardURI pathUri = new WildcardURI(path);
+        HdfsFs fileSystem = getFileSystem(path, properties, null);
+        Path pathPattern = new Path(pathUri.getPath());
+        try {
+            FileStatus[] files = fileSystem.getDFSFileSystem().globStatus(pathPattern);
+            return Lists.newArrayList(files);
+        } catch (FileNotFoundException e) {
+            LOG.info("file not found: " + path, e);
+            throw new UserException("file not found: " + path, e);
+        } catch (Exception e) {
+            LOG.error("errors while get file status ", e);
+            throw new UserException("Fail to get file status: " + e.getMessage(), e);
+        }
+    }
+
+>>>>>>> c1259dee3a ([Enhancement] Refine error prompt (#34322))
     public List<TBrokerFileStatus> listPath(String path, boolean fileNameOnly, Map<String, String> loadProperties)
             throws UserException {
         List<TBrokerFileStatus> resultFileStatus = null;
@@ -1114,7 +1133,11 @@ public class HdfsFsManager {
             throw new UserException("file not found: " + e.getMessage());
         } catch (Exception e) {
             LOG.error("errors while get file status ", e);
+<<<<<<< HEAD
             throw new UserException("unknown error when get file status: " + e.getMessage());
+=======
+            throw new UserException("Fail to get file status: " + e.getMessage(), e);
+>>>>>>> c1259dee3a ([Enhancement] Refine error prompt (#34322))
         }
         return resultFileStatus;
     }
