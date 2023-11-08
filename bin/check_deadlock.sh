21978a5852963a9a1495f5e60a6819eb7a426cae
gengjun-git <gengjun@starrocks.com>
2023-11-08 17:29:17 +0800

    add
    Signed-off-by: gengjun-git <gengjun@starrocks.com>

---
+++ bin/check_deadlock.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#config
+user=root
+passwd=
+fe_ip=127.0.0.1
+http_port=8632
+timeout=30
+max_failed_time=5
+stop_bin=/home/disk4/gengjun/fe3/bin/stop_fe.sh
+start_bin=/home/disk4/gengjun/fe3/bin/start_fe.sh
+
+
+failed_time=0
+while true
+do
+    start_time=$(date +%s)
+    curl http://${user}:${passwd}@${fe_ip}:${http_port}/api/show_proc?path=/statistic --max-time ${timeout} --silent -o /dev/null
+    succ=$?
+    end_time=$(date +%s)
+    if [ !succ ] && [ $(($end_time - $start_time)) -ge ${timeout} ]; then
+        failed_time=$(($failed_time + 1))
+        echo "check failed, time used: $(($end_time - $start_time))"
+    else
+        failed_time=0
+        echo "check successfully, time used: $(($end_time - $start_time))"
+    fi
+
+    if [ ${failed_time} -ge ${max_failed_time} ]; then
+        echo "successive failed time is ${failed_time}, restart fe"
+        sh ${stop_bin}
+        sleep 10
+        sh ${start_bin} --daemon
+    fi
+
+    sleep 10
+done
+
