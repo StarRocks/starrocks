9b73c364f1ee9501a1cb4a78739de5744a3345d6
gengjun-git <gengjun@starrocks.com>
2023-11-08 22:10:58 +0800

    change
    Signed-off-by: gengjun-git <gengjun@starrocks.com>

--- bin/check_deadlock.sh
+++ bin/check_deadlock.sh
@@ -6,33 +6,57 @@ passwd=
 fe_ip=127.0.0.1
 http_port=8030
 timeout=30
-max_failed_time=5
-stop_bin=/home/starrocks/fe/bin/stop_fe.sh
-start_bin=/home/starrocks/fe/bin/start_fe.sh
+max_timeout_times=5
+fe_bin_path=/home/disk1/sr/app/starrocks-2.3.3/fe-5963b718-fdc4-42fc-a83c-5256cdcef805/bin
+jstack_path=/home/disk1/sr/app/starrocks-2.3.3/fe-5963b718-fdc4-42fc-a83c-5256cdcef805
 
 
-failed_time=0
+timeout_times=0
+interval=60
 while true
 do
+    # call show proc '/statistic' to check db deadlock
     start_time=$(date +%s)
-    curl http://${user}:${passwd}@${fe_ip}:${http_port}/api/show_proc?path=/statistic --max-time ${timeout} --silent -o /dev/null
+    curl -u ${user}:${passwd} http://${fe_ip}:${http_port}/api/show_proc?path=/statistic --max-time ${timeout}
     succ=$?
     end_time=$(date +%s)
-    if [ !succ ] && [ $(($end_time - $start_time)) -ge ${timeout} ]; then
-        failed_time=$(($failed_time + 1))
-        echo "check failed, time used: $(($end_time - $start_time))"
+
+    # check result, if return failed and time used is at least ${timeout}, increase timeout_times
+    if [ ${succ} -ne 0 ]; then
+	if [ $(($end_time - $start_time)) -ge ${timeout} ]; then
+       	    timeout_times=$(($timeout_times + 1))
+            # reduce interval, so that we can detect deadlock ASAP
+            interval=10
+            echo -e "\ntimeout, time used: $(($end_time - $start_time))"
+        else
+            timeout_times=0
+            echo -e "\ncheck failed, time used: $(($end_time - $start_time))"
+        fi
     else
-        failed_time=0
-        echo "check successfully, time used: $(($end_time - $start_time))"
+        timeout_times=0
+        interval=60
+        echo -e "\ncheck successfully, time used: $(($end_time - $start_time))"
     fi
 
-    if [ ${failed_time} -ge ${max_failed_time} ]; then
-        echo "successive failed time is ${failed_time}, restart fe"
-        sh ${stop_bin}
-        sleep 10
-        sh ${start_bin} --daemon
+    # check ${timeout_times}, if ${timeout_times} >= max_timeout_times print jstack and stop fe
+    # we will not start fe, Because the process is hosted by supervisor
+    if [ ${timeout_times} -ge ${max_timeout_times} ]; then
+        echo "successive timeout times is ${timeout_times}, print jstack and stop fe"
+        
+        echo "start to print jstack"
+        for ((i=0; i<5; i++));
+        do
+            pid=`cat ${fe_bin_path}/fe.pid`
+            jstack_file_name=${jstack_path}/jstack_$(date '+%Y%m%d-%H%M%S').txt
+            jstack -l ${pid} > ${jstack_file_name}
+            sleep 5
+        done
+        
+        echo "start to stop fe"
+        sh ${fe_bin_path}/stop_fe.sh
+
+        timeout_times=0
     fi
 
-    sleep 10
+    sleep ${interval}
 done
-
