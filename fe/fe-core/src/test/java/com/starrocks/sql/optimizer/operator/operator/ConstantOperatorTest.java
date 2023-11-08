2fd61a47f5ca20a631da5ac96e7892eef25f4a0e
packy <wangchao@starrocks.com>
2023-11-08 14:48:33 +0800

    [Enhancement] remove unnecessary exception thrown
    Signed-off-by: packy92 <wangchao@starrocks.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/optimizer/operator/operator/ConstantOperatorTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/optimizer/operator/operator/ConstantOperatorTest.java
@@ -21,7 +21,6 @@ import org.junit.Test;
 
 import java.math.BigInteger;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeParseException;
 
 public class ConstantOperatorTest {
     @Test
@@ -57,8 +56,8 @@ public class ConstantOperatorTest {
 
         for (String[] c : testCases) {
             ConstantOperator in = ConstantOperator.createVarchar(c[0]);
-            Assert.assertEquals(c[1], in.castTo(Type.DATE).getDate().toString());
-            Assert.assertEquals(c[2], in.castTo(Type.DATETIME).getDate().toString());
+            Assert.assertEquals(c[1], in.castTo(Type.DATE).get().getDate().toString());
+            Assert.assertEquals(c[2], in.castTo(Type.DATETIME).get().getDate().toString());
         }
     }
 
@@ -105,8 +104,8 @@ public class ConstantOperatorTest {
         };
         for (String c : testCases) {
             ConstantOperator in = ConstantOperator.createVarchar(c);
-            Assert.assertThrows(in.getVarchar(), DateTimeParseException.class, () -> in.castTo(Type.DATE));
-            Assert.assertThrows(in.getVarchar(), DateTimeParseException.class, () -> in.castTo(Type.DATETIME));
+            Assert.assertFalse(in.castTo(Type.DATE).isPresent());
+            Assert.assertFalse(in.castTo(Type.DATETIME).isPresent());
         }
     }
 
@@ -116,31 +115,31 @@ public class ConstantOperatorTest {
         ConstantOperator datetime = ConstantOperator.createDatetime(LocalDateTime.of(2023, 01, 01, 0, 0, 0));
 
         ConstantOperator intNumber = ConstantOperator.createInt(20230101);
-        Assert.assertEquals(intNumber, date.castTo(Type.INT));
+        Assert.assertEquals(intNumber, date.castTo(Type.INT).get());
 
         ConstantOperator dateBigintNumber = ConstantOperator.createBigint(20230101L);
-        Assert.assertEquals(dateBigintNumber, date.castTo(Type.BIGINT));
+        Assert.assertEquals(dateBigintNumber, date.castTo(Type.BIGINT).get());
 
         ConstantOperator datetimeBigintNumber = ConstantOperator.createBigint(20230101000000L);
-        Assert.assertEquals(datetimeBigintNumber, datetime.castTo(Type.BIGINT));
+        Assert.assertEquals(datetimeBigintNumber, datetime.castTo(Type.BIGINT).get());
 
         ConstantOperator dateLargeintNumber = ConstantOperator.createLargeInt(new BigInteger("20230101"));
-        Assert.assertEquals(dateLargeintNumber, date.castTo(Type.LARGEINT));
+        Assert.assertEquals(dateLargeintNumber, date.castTo(Type.LARGEINT).get());
 
         ConstantOperator datetimeLargeintNumber = ConstantOperator.createLargeInt(new BigInteger("20230101000000"));
-        Assert.assertEquals(datetimeLargeintNumber, datetime.castTo(Type.LARGEINT));
+        Assert.assertEquals(datetimeLargeintNumber, datetime.castTo(Type.LARGEINT).get());
 
         ConstantOperator dateFloatNumber = ConstantOperator.createFloat(20230101);
-        Assert.assertEquals(dateFloatNumber, date.castTo(Type.FLOAT));
+        Assert.assertEquals(dateFloatNumber, date.castTo(Type.FLOAT).get());
 
         ConstantOperator datetimeFloatNumber = ConstantOperator.createFloat(20230101000000L);
-        Assert.assertEquals(datetimeFloatNumber, datetime.castTo(Type.FLOAT));
+        Assert.assertEquals(datetimeFloatNumber, datetime.castTo(Type.FLOAT).get());
 
         ConstantOperator dateDoubleNumber = ConstantOperator.createDouble(20230101);
-        Assert.assertEquals(dateDoubleNumber, date.castTo(Type.DOUBLE));
+        Assert.assertEquals(dateDoubleNumber, date.castTo(Type.DOUBLE).get());
 
         ConstantOperator datetimeDoubleNumber = ConstantOperator.createDouble(20230101000000L);
-        Assert.assertEquals(datetimeDoubleNumber, datetime.castTo(Type.DOUBLE));
+        Assert.assertEquals(datetimeDoubleNumber, datetime.castTo(Type.DOUBLE).get());
     }
 
     @Test
