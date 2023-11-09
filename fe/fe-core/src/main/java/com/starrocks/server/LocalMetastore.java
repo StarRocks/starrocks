a8843bb9bc7d52c059eb2e47016a6a2f15a690f1
Felix Li <astralidea@163.com>
2023-11-09 14:20:27 +0800

    [BugFix] Fix bug list automatic partition lost meta info (#34590)
    Signed-off-by: Astralidea <astralidea@163.com>
    (cherry picked from commit cb267d1332a68a56ba0cbdeffde57ffe1e78e150)

--- fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
+++ fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
@@ -1254,7 +1254,8 @@ public class LocalMetastore implements ConnectorMetadata {
         }
     }
 
-    private void addListPartitionLog(Database db, OlapTable olapTable, List<PartitionDesc> partitionDescs,
+    @VisibleForTesting
+    public void addListPartitionLog(Database db, OlapTable olapTable, List<PartitionDesc> partitionDescs,
                                      AddPartitionClause addPartitionClause, PartitionInfo partitionInfo,
                                      List<Partition> partitionList, Set<String> existPartitionNameSet)
             throws DdlException {
@@ -1270,7 +1271,7 @@ public class LocalMetastore implements ConnectorMetadata {
             boolean isTempPartition = addPartitionClause.isTempPartition();
             if (existPartitionNameSet.contains(partition.getName())) {
                 LOG.info("add partition[{}] which already exists", partition.getName());
-                return;
+                continue;
             }
             long partitionId = partition.getId();
             PartitionPersistInfoV2 info = new ListPartitionPersistInfo(db.getId(), olapTable.getId(), partition,
