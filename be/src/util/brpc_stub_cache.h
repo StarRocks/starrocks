de5fcd6f4d32d1d9adacdd1b62dac764f134d5af
Zhuhe Fang <fzhedu@gmail.com>
2023-11-09 14:11:27 +0800

    [BugFix] reuse http brpc stub and config rpc_connect_timeout_ms (#33494)
    Signed-off-by: Zhuhe Fang <fzhedu@gmail.com>
    (cherry picked from commit 35cbd7085cdfbf258cbd4924ed86cd4ded0c178a)
    
    # Conflicts:
    #	be/src/exec/tablet_sink_index_channel.cpp
    #	be/src/util/brpc_stub_cache.h

--- be/src/util/brpc_stub_cache.h
+++ be/src/util/brpc_stub_cache.h
@@ -37,6 +37,7 @@
 #include <memory>
 #include <mutex>
 
+#include "common/config.h"
 #include "common/statusor.h"
 #include "gen_cpp/Types_types.h" // TNetworkAddress
 #include "gen_cpp/doris_internal_service.pb.h"
@@ -85,36 +86,6 @@ public:
         return stub;
     }
 
-    // rarely used, so create as needed
-    static StatusOr<std::unique_ptr<doris::PBackendService_Stub>> create_http_stub(const TNetworkAddress& taddr) {
-        butil::EndPoint endpoint;
-        std::string realhost;
-        realhost = taddr.hostname;
-        if (!is_valid_ip(taddr.hostname)) {
-            realhost = hostname_to_ip(taddr.hostname);
-            if (realhost == "") {
-                return Status::RuntimeError("failed to get ip from host " + taddr.hostname);
-            }
-        }
-        if (str2endpoint(realhost.c_str(), taddr.port, &endpoint)) {
-            return Status::RuntimeError("unknown endpoint, host = " + taddr.hostname);
-        }
-        // create
-        brpc::ChannelOptions options;
-        options.connect_timeout_ms = 3000;
-        options.protocol = "http";
-        // Explicitly set the max_retry
-        // TODO(meegoo): The retry strategy can be customized in the future
-        options.max_retry = 3;
-        std::unique_ptr<brpc::Channel> channel(new brpc::Channel());
-        if (channel->Init(endpoint, &options)) {
-            return Status::RuntimeError("init brpc http channel error on " + taddr.hostname + ":" +
-                                        std::to_string(taddr.port));
-        }
-        return std::make_unique<doris::PBackendService_Stub>(channel.release(),
-                                                             google::protobuf::Service::STUB_OWNS_CHANNEL);
-    }
-
     doris::PBackendService_Stub* get_stub(const TNetworkAddress& taddr) { return get_stub(taddr.hostname, taddr.port); }
 
     doris::PBackendService_Stub* get_stub(const std::string& host, int port) {
@@ -136,6 +107,108 @@ public:
     }
 
 private:
+<<<<<<< HEAD
+=======
+    // StubPool is used to store all stubs with a single endpoint, and the client in the same BE process maintains up to
+    // brpc_max_connections_per_server single connections with each server.
+    // These connections will be created during the first few accesses and will be reused later.
+    struct StubPool {
+        StubPool() { _stubs.reserve(config::brpc_max_connections_per_server); }
+
+        ~StubPool() {
+            for (auto& stub : _stubs) {
+                delete stub;
+            }
+        }
+
+        doris::PBackendService_Stub* get_or_create(const butil::EndPoint& endpoint) {
+            if (UNLIKELY(_stubs.size() < config::brpc_max_connections_per_server)) {
+                brpc::ChannelOptions options;
+                options.connect_timeout_ms = config::rpc_connect_timeout_ms;
+                // Explicitly set the max_retry
+                // TODO(meegoo): The retry strategy can be customized in the future
+                options.max_retry = 3;
+                // the single connection of brpc will only maintain one connection with the same server by default,
+                // all requests are sent on this connection and the throughput will be limited by this.
+                // we use `connection_group` to create multiple single connections to remove this bottleneck.
+                options.connection_group = std::to_string(_stubs.size());
+                std::unique_ptr<brpc::Channel> channel(new brpc::Channel());
+                if (channel->Init(endpoint, &options)) {
+                    return nullptr;
+                }
+                auto stub = new doris::PBackendService_Stub(channel.release(),
+                                                            google::protobuf::Service::STUB_OWNS_CHANNEL);
+                _stubs.push_back(stub);
+                return stub;
+            }
+            if (++_idx >= config::brpc_max_connections_per_server) {
+                _idx = 0;
+            }
+            return _stubs[_idx];
+        }
+
+        std::vector<doris::PBackendService_Stub*> _stubs;
+        int64_t _idx = -1;
+    };
+
+>>>>>>> 35cbd7085c ([BugFix] reuse http brpc stub and config rpc_connect_timeout_ms (#33494))
+    SpinLock _lock;
+    butil::FlatMap<butil::EndPoint, doris::PBackendService_Stub*> _stub_map;
+};
+
+class HttpBrpcStubCache {
+public:
+    static HttpBrpcStubCache* getInstance() {
+        static HttpBrpcStubCache cache;
+        return &cache;
+    }
+
+    StatusOr<doris::PBackendService_Stub*> get_http_stub(const TNetworkAddress& taddr) {
+        butil::EndPoint endpoint;
+        std::string realhost;
+        realhost = taddr.hostname;
+        if (!is_valid_ip(taddr.hostname)) {
+            realhost = hostname_to_ip(taddr.hostname);
+            if (realhost == "") {
+                return Status::RuntimeError("failed to get ip from host " + taddr.hostname);
+            }
+        }
+        if (str2endpoint(realhost.c_str(), taddr.port, &endpoint)) {
+            return Status::RuntimeError("unknown endpoint, host = " + taddr.hostname);
+        }
+        // get is exist
+        std::lock_guard<SpinLock> l(_lock);
+        auto stub_ptr = _stub_map.seek(endpoint);
+        if (stub_ptr != nullptr) {
+            return *stub_ptr;
+        }
+        // create
+        brpc::ChannelOptions options;
+        options.connect_timeout_ms = config::rpc_connect_timeout_ms;
+        options.protocol = "http";
+        // Explicitly set the max_retry
+        // TODO(meegoo): The retry strategy can be customized in the future
+        options.max_retry = 3;
+        std::unique_ptr<brpc::Channel> channel(new brpc::Channel());
+        if (channel->Init(endpoint, &options)) {
+            return Status::RuntimeError("init brpc http channel error on " + taddr.hostname + ":" +
+                                        std::to_string(taddr.port));
+        }
+        auto stub = new doris::PBackendService_Stub(channel.release(), google::protobuf::Service::STUB_OWNS_CHANNEL);
+        _stub_map.insert(endpoint, stub);
+        return stub;
+    }
+
+private:
+    HttpBrpcStubCache() { _stub_map.init(500); }
+    ~HttpBrpcStubCache() {
+        for (auto& stub : _stub_map) {
+            delete stub.second;
+        }
+    }
+    HttpBrpcStubCache(const HttpBrpcStubCache& cache) = delete;
+    HttpBrpcStubCache& operator=(const HttpBrpcStubCache& cache) = delete;
+
     SpinLock _lock;
     butil::FlatMap<butil::EndPoint, doris::PBackendService_Stub*> _stub_map;
 };
