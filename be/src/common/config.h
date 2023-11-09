35cbd7085cdfbf258cbd4924ed86cd4ded0c178a
Zhuhe Fang <fzhedu@gmail.com>
2023-11-09 14:11:27 +0800

    [BugFix] reuse http brpc stub and config rpc_connect_timeout_ms (#33494)
    Signed-off-by: Zhuhe Fang <fzhedu@gmail.com>

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -845,6 +845,8 @@ CONF_Int64(send_rpc_runtime_filter_timeout_ms, "1000");
 // this is a default value, maybe changed by global_runtime_filter_rpc_http_min_size in session variable.
 CONF_Int64(send_runtime_filter_via_http_rpc_min_size, "67108864");
 
+CONF_Int64(rpc_connect_timeout_ms, "30000");
+
 CONF_Int32(max_batch_publish_latency_ms, "100");
 
 // Config for opentelemetry tracing.
