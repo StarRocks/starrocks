bfa37df27fa873b2244627d44a2193ae773167cd
gengjun-git <gengjun@starrocks.com>
2023-11-09 11:15:40 +0800

    [BugFix] Fix skip journal failed bug (#33839)
    Signed-off-by: gengjun-git <gengjun@starrocks.com>
    (cherry picked from commit 40d7b0ddf79a23b5042a190a9b7e05b681a536b9)

--- fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
@@ -2291,6 +2291,7 @@ public class GlobalStateMgr {
         long lineCnt = 0;
         while (true) {
             JournalEntity entity = null;
+            boolean readSucc = false;
             try {
                 entity = cursor.next();
 
@@ -2298,6 +2299,8 @@ public class GlobalStateMgr {
                 if (entity == null) {
                     break;
                 }
+
+                readSucc = true;
 
                 // apply
                 EditLog.loadJournal(this, entity);
@@ -2307,7 +2310,9 @@ public class GlobalStateMgr {
                             replayedJournalId.incrementAndGet(),
                             entity == null ? null : entity.getData(),
                             e);
-                    cursor.skipNext();
+                    if (!readSucc) {
+                        cursor.skipNext();
+                    }
                     continue;
                 }
                 // handled in outer loop
