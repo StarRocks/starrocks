313a351adb686c76e4a2563e1bad54aab7f8aa9c
Zhuhe Fang <fzhedu@gmail.com>
2023-11-09 18:40:03 +0800

    [BugFix] fix http stub compile error (#34678)
    Signed-off-by: Zhuhe Fang <fzhedu@gmail.com>

--- be/src/runtime/runtime_filter_worker.cpp
+++ be/src/runtime/runtime_filter_worker.cpp
@@ -53,7 +53,7 @@ static void send_rpc_runtime_filter(const TNetworkAddress& dest, RuntimeFilterRp
     bool via_http = request.data().size() >= http_min_size;
     if (via_http) {
         if (auto res = HttpBrpcStubCache::getInstance()->get_http_stub(dest); res.ok()) {
-            stub = res.value().release();
+            stub = res.value();
         }
     } else {
         stub = ExecEnv::GetInstance()->brpc_stub_cache()->get_stub(dest);
@@ -62,11 +62,6 @@ static void send_rpc_runtime_filter(const TNetworkAddress& dest, RuntimeFilterRp
         LOG(WARNING) << strings::Substitute("The brpc stub of {}: {} is null.", dest.hostname, dest.port);
         return;
     }
-    DeferOp defer([&]() {
-        if (via_http) {
-            delete stub;
-        }
-    });
 
     rpc_closure->ref();
     rpc_closure->cntl.Reset();
