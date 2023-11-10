78531078f2f352b16864b0fd55b5d586fb133d12
dirtysalt <dirtysalt1987@gmail.com>
2023-11-10 11:30:53 +0800

    [Feature] support catalog to bind hadoop username

--- java-extensions/hadoop-ext/src/main/java/com/starrocks/connector/hadoop/HadoopExt.java
+++ java-extensions/hadoop-ext/src/main/java/com/starrocks/connector/hadoop/HadoopExt.java
@@ -24,13 +24,22 @@ import java.security.PrivilegedAction;
 public class HadoopExt {
     private static final Logger LOGGER =
             LoggerFactory.getLogger(HadoopExt.class);
+<<<<<<< HEAD
     private static final HadoopExt INSTANCE = new CelerDataHadoopExt();
     public static final String HDFS_CONFIG_RESOURCES = "hadoop.config.resources";
     public static final String HDFS_CONFIG_RESOURCES_LOADED = "hadoop.config.resources.loaded";
     public static final String HDFS_RUNTIME_JARS = "hadoop.runtime.jars";
     public static final String HDFS_CLOUD_CONFIGURATION_STRING = "hadoop.cloud.configuration.string";
     public static final String STARROCKS_HOME_ENV = "STARROCKS_HOME";
+=======
+    private static final HadoopExt INSTANCE = new HadoopExt();
+>>>>>>> af09804909 ([Feature] support catalog to bind hadoop username (#34671))
     public static final String LOGGER_MESSAGE_PREFIX = "[hadoop-ext]";
+
+    public static final String HADOOP_CONFIG_RESOURCES = "hadoop.config.resources";
+    public static final String HADOOP_RUNTIME_JARS = "hadoop.runtime.jars";
+    public static final String HADOOP_CLOUD_CONFIGURATION_STRING = "hadoop.cloud.configuration.string";
+    public static final String HADOOP_USERNAME = "hadoop.username";
 
     public static HadoopExt getInstance() {
         return INSTANCE;
@@ -40,7 +49,7 @@ public class HadoopExt {
     }
 
     public String getCloudConfString(Configuration conf) {
-        return conf.get(HDFS_CLOUD_CONFIGURATION_STRING, "");
+        return conf.get(HADOOP_CLOUD_CONFIGURATION_STRING, "");
     }
 
     public UserGroupInformation getHMSUGI(Configuration conf) {
