6a0dcc3f18c577b5ad4196a2fc1cc6cb8da62e45
liuyehcf <1559500551@qq.com>
2023-11-10 15:51:32 +0800

    [Enhancement] Refactor window function processing (#34312)
    Signed-off-by: liuyehcf <1559500551@qq.com>

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -717,6 +717,9 @@ CONF_Int64(pipeline_scan_queue_level_time_slice_base_ns, "100000000");
 CONF_Double(pipeline_scan_queue_ratio_of_adjacent_queue, "1.5");
 
 CONF_Int32(pipeline_analytic_max_buffer_size, "128");
+CONF_Int32(pipeline_analytic_removable_chunk_num, "128");
+CONF_Bool(pipeline_analytic_enable_streaming_process, "true");
+CONF_Bool(pipeline_analytic_enable_removable_cumulative_process, "true");
 
 /// For parallel scan on the single tablet.
 // These three configs are used to calculate the minimum number of rows picked up from a segment at one time.
