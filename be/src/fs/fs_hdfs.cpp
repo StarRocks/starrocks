56aea3ab3b075fc8ece3a54e7dff30876496317d
Smith Cruise <chendingchao1@126.com>
2023-11-10 17:19:13 +0800

    fix compile
    Signed-off-by: Smith Cruise <chendingchao1@126.com>

--- be/src/fs/fs_hdfs.cpp
+++ be/src/fs/fs_hdfs.cpp
@@ -8,7 +8,6 @@
 #include <atomic>
 #include <utility>
 
-#include "fs/fs.h"
 #include "gutil/strings/substitute.h"
 #include "runtime/file_result_writer.h"
 #include "runtime/hdfs/hdfs_fs_cache.h"
@@ -39,6 +38,8 @@ public:
     void set_size(int64_t size) override;
 
 private:
+    bool _is_hdfs_file() const;
+        
     hdfsFS _fs;
     hdfsFile _file;
     std::string _file_name;
@@ -98,9 +99,14 @@ void HdfsInputStream::set_size(int64_t value) {
     _file_size = value;
 }
 
+bool HdfsInputStream::_is_hdfs_file() const {
+    static const char* kFileSysPrefixJuicefs = "hdfs://";
+    return strncmp(_file_name.c_str(), kFileSysPrefixJuicefs, strlen(kFileSysPrefixJuicefs)) == 0;
+}
+
 StatusOr<std::unique_ptr<io::NumericStatistics>> HdfsInputStream::get_numeric_statistics() {
     // `GetReadStatistics` is not supported in juicefs hadoop sdk, and will cause the be crash
-    if (!is_hdfs_uri(_file_name)) {
+    if (!_is_hdfs_file()) {
         return nullptr;
     }
 
