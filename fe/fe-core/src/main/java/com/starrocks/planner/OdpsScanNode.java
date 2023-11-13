ae6a94dbb883e1ee3dc6671733bd5db8852bd4d3
鼎昕 <zhangdingxin.zdx@alibaba-inc.com>
2023-11-13 17:29:55 +0800

    optimize add thread pool

--- fe/fe-core/src/main/java/com/starrocks/planner/OdpsScanNode.java
+++ fe/fe-core/src/main/java/com/starrocks/planner/OdpsScanNode.java
@@ -44,7 +44,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
