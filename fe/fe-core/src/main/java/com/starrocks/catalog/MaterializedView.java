b24c6ede885c84bbf2babdf83d559d1b6e9b0552
zhangqiang <qiangzh95@gmail.com>
2023-11-13 19:20:05 +0800

    [Refactor] Rename light schema change to fast schema evolution (#34472)
    Signed-off-by: zhangqiang <qiangzh95@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/MaterializedView.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/MaterializedView.java
@@ -111,13 +111,12 @@ public class MaterializedView extends OlapTable implements GsonPreProcessable, G
     }
 
     @Override
-    public Boolean getUseLightSchemaChange() {
+    public Boolean getUseFastSchemaEvolution() {
         return false;
     }
 
     @Override
-    public void setUseLightSchemaChange(boolean useLightSchemaChange) {
-    }
+    public void setUseFastSchemaEvolution(boolean useFastSchemaEvolution) {}
 
     public static class BasePartitionInfo {
 
