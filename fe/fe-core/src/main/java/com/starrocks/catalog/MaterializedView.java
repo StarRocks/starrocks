d81903f02815a9c4479b92e540529f23e4fbb473
Murphy <96611012+mofeiatwork@users.noreply.github.com>
2023-11-13 13:07:13 +0800

    [Enhancement] skip optimizer lock for materialized view (#34569)
    Signed-off-by: Murphy <mofei@starrocks.com>
    (cherry picked from commit 97745418cdcae6c349876e11254cdb11c12bd775)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/catalog/MaterializedView.java
    #	fe/fe-core/src/main/java/com/starrocks/sql/StatementPlanner.java

--- fe/fe-core/src/main/java/com/starrocks/catalog/MaterializedView.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/MaterializedView.java
@@ -107,10 +107,20 @@ public class MaterializedView extends OlapTable implements GsonPreProcessable, G
         DEFERRED
     }
 
+<<<<<<< HEAD
     public enum PlanMode {
         VALID,
         INVALID,
         UNKNOWN
+=======
+    @Override
+    public Boolean getUseLightSchemaChange() {
+        return false;
+    }
+
+    @Override
+    public void setUseLightSchemaChange(boolean useLightSchemaChange) {
+>>>>>>> 97745418cd ([Enhancement] skip optimizer lock for materialized view (#34569))
     }
 
     public static class BasePartitionInfo {
@@ -345,6 +355,16 @@ public class MaterializedView extends OlapTable implements GsonPreProcessable, G
 
         public void setLastRefreshTime(long lastRefreshTime) {
             this.lastRefreshTime = lastRefreshTime;
+        }
+
+        public MvRefreshScheme copy() {
+            MvRefreshScheme res = new MvRefreshScheme();
+            res.type = this.type;
+            res.lastRefreshTime = this.lastRefreshTime;
+            if (this.asyncRefreshContext != null) {
+                res.asyncRefreshContext = this.asyncRefreshContext.copy();
+            }
+            return res;
         }
     }
 
@@ -740,6 +760,25 @@ public class MaterializedView extends OlapTable implements GsonPreProcessable, G
     public TTableDescriptor toThrift(List<ReferencedPartitionInfo> partitions) {
         return new TTableDescriptor(id, TTableType.MATERIALIZED_VIEW,
                 fullSchema.size(), 0, getName(), "");
+    }
+
+    @Override
+    public void copyOnlyForQuery(OlapTable olapTable) {
+        super.copyOnlyForQuery(olapTable);
+        MaterializedView mv = (MaterializedView) olapTable;
+        mv.dbId = this.dbId;
+        mv.active = this.active;
+        mv.refreshScheme = this.refreshScheme.copy();
+        mv.maxMVRewriteStaleness = this.maxMVRewriteStaleness;
+        if (this.baseTableIds != null) {
+            mv.baseTableIds = Sets.newHashSet(this.baseTableIds);
+        }
+        if (this.baseTableInfos != null) {
+            mv.baseTableInfos = Lists.newArrayList(this.baseTableInfos);
+        }
+        if (this.partitionRefTableExprs != null) {
+            mv.partitionRefTableExprs = Lists.newArrayList(this.partitionRefTableExprs);
+        }
     }
 
     @Override
