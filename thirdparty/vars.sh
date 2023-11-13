70f13fd3588c5d4778593bf0eea268eeebc37a34
leorishdu <18771113323@163.com>
2023-11-14 02:06:46 +0800

    [Refactor] Replace by experimental leak fixed version
    Signed-off-by: leorishdu <18771113323@163.com>

--- thirdparty/vars.sh
+++ thirdparty/vars.sh
@@ -410,10 +410,10 @@ LLVM_SOURCE="llvm-project-16.0.6.src"
 LLVM_MD5SUM="dc13938a604f70379d3b38d09031de98"
 
 #clucene
-CLUCENE_DOWNLOAD="https://github.com/StarRocks/clucene/archive/refs/tags/starrocks-2023.11.10.tar.gz"
-CLUCENE_NAME="starrocks-clucene-2023.11.10.tar.gz"
-CLUCENE_SOURCE="starrocks-clucene-2023.11.10"
-CLUCENE_MD5SUM="cb641cacf05387e2f4cc1f7fab24d018"
+CLUCENE_DOWNLOAD="https://github.com/dujijun007/clucene/archive/refs/tags/clucene-1114.tar.gz"
+CLUCENE_NAME="clucene-1114.tar.gz"
+CLUCENE_SOURCE="clucene-1114"
+CLUCENE_MD5SUM="43562d96dda9a8180a67c9ae4827ff65"
 
 # all thirdparties which need to be downloaded is set in array TP_ARCHIVES
 TP_ARCHIVES="CLUCENE LIBEVENT OPENSSL THRIFT PROTOBUF GFLAGS GLOG GTEST RAPIDJSON SIMDJSON SNAPPY GPERFTOOLS ZLIB LZ4 BZIP CURL \
