68fd8d5bf8a02a0dcdbe95b75f62f2e66fd7827a
PengFei Li <lpengfei2016@gmail.com>
2023-11-14 13:29:59 +0800

    [BugFix] Schema change ignoring PREPARED transaction may lead to data loss (#34381)
    Signed-off-by: PengFei Li <lpengfei2016@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/transaction/TransactionState.java
+++ fe/fe-core/src/main/java/com/starrocks/transaction/TransactionState.java
@@ -391,7 +391,8 @@ public class TransactionState implements Writable {
     }
 
     public boolean isRunning() {
-        return transactionStatus == TransactionStatus.PREPARE || transactionStatus == TransactionStatus.COMMITTED;
+        return transactionStatus == TransactionStatus.PREPARE || transactionStatus == TransactionStatus.PREPARED ||
+                transactionStatus == TransactionStatus.COMMITTED;
     }
 
     public void setTabletCommitInfos(List<TabletCommitInfo> infos) {
