c12e2b33a59415c1592f39fb6e5fa226909dcb5b
ZenoYang <cookie.yz@qq.com>
2023-11-14 21:52:20 -0600

    [Enhancement] Update regexp_extract function for trino parser (#34845)
    Signed-off-by: zenoyang <cookie.yz@qq.com>
    (cherry picked from commit c9cd165f1d1536425815ca9861769bbcce729203)

--- fe/fe-core/src/main/java/com/starrocks/connector/parser/trino/Trino2SRFunctionCallTransformer.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/parser/trino/Trino2SRFunctionCallTransformer.java
@@ -195,10 +195,6 @@ public class Trino2SRFunctionCallTransformer {
         // regexp_like -> regexp
         registerFunctionTransformer("regexp_like", 2, "regexp",
                 ImmutableList.of(Expr.class, Expr.class));
-
-        // regexp_extract(string, pattern) -> regexp_extract(str, pattern, 0)
-        registerFunctionTransformer("regexp_extract", 2, new FunctionCallExpr("regexp_extract",
-                ImmutableList.of(new PlaceholderExpr(1, Expr.class), new PlaceholderExpr(2, Expr.class), new IntLiteral(0L))));
     }
 
     private static void registerJsonFunctionTransformer() {
