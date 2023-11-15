c440e399149e1a53c43e3da7547d6231fb2bce52
Seaven <seaven_7@qq.com>
2023-11-15 19:31:39 +0800

    [BugFix][Cherry-Pick] fix cte output column duplicate (#34904) (#35085)
    Signed-off-by: Seaven <seaven_7@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/plan/PlanFragmentBuilder.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/plan/PlanFragmentBuilder.java
@@ -2807,7 +2807,7 @@ public class PlanFragmentBuilder {
                     cteFragment.getPlanRoot(), DistributionSpec.DistributionType.SHUFFLE);
 
             exchangeNode.setReceiveColumns(consume.getCteOutputColumnRefMap().values().stream()
-                    .map(ColumnRefOperator::getId).collect(Collectors.toList()));
+                    .map(ColumnRefOperator::getId).distinct().collect(Collectors.toList()));
             exchangeNode.setDataPartition(cteFragment.getDataPartition());
 
             exchangeNode.setNumInstances(cteFragment.getPlanRoot().getNumInstances());
