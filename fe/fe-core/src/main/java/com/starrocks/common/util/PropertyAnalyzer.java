870a235b88998c5a3570e58aac673192de004b56
stonechen111 <145319350+stonechen111@users.noreply.github.com>
2023-11-15 19:07:46 +0800

    [Feature] Support dynamic modification of datacache.partition_duration (backport #32844)
    Signed-off-by: stonechen111 <chenshi@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/common/util/PropertyAnalyzer.java
+++ fe/fe-core/src/main/java/com/starrocks/common/util/PropertyAnalyzer.java
@@ -1020,7 +1020,11 @@ public class PropertyAnalyzer {
             return null;
         }
         properties.remove(PROPERTIES_DATACACHE_PARTITION_DURATION);
-        return TimeUtils.parseHumanReadablePeriodOrDuration(text);
+        try {
+            return TimeUtils.parseHumanReadablePeriodOrDuration(text);
+        } catch (DateTimeParseException ex) {
+            throw new AnalysisException(ex.getMessage());
+        }
     }
 
     public static TPersistentIndexType analyzePersistentIndexType(Map<String, String> properties) throws AnalysisException {
