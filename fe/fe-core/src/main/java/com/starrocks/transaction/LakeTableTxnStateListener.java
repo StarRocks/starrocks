1b7b3f1ac118b7161190e23a35f7fc5eef690090
Alex Zhu <zhuming9011@gmail.com>
2023-11-16 12:16:55 +0800

    [Enhancement] Allow skipping the cleanup process for abort transactions
    This PR mainly aims to reduce unnecessary HEAD OBJECT requests.
    
    The abort transaction will attempt to read the txn log and delete the
    files recorded in the txn log. Typically, the transactions that will
    be aborted have no txn log, which means that many times the cleanup
    process for abort transactions will not have any real effect. In the
    current implementation, even reading a non-existent txn log will send
    a HEAD OBJECT request to the object store, and when the abort
    transaction involves a large number of tablets, a large number of
    HEAD OBJECT requests will be sent, which may result in the qps limit
    of the object store being exceeded, and also cause unnecessary billing.
    
    Signed-off-by: Alex Zhu <zhuming9011@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/transaction/LakeTableTxnStateListener.java
+++ fe/fe-core/src/main/java/com/starrocks/transaction/LakeTableTxnStateListener.java
@@ -19,28 +19,29 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
-import com.starrocks.catalog.Database;
 import com.starrocks.catalog.MaterializedIndex;
 import com.starrocks.catalog.OlapTable;
 import com.starrocks.catalog.Partition;
 import com.starrocks.catalog.Tablet;
 import com.starrocks.catalog.TabletInvertedIndex;
 import com.starrocks.catalog.TabletMeta;
-import com.starrocks.common.NoAliveBackendException;
-import com.starrocks.lake.Utils;
 import com.starrocks.proto.AbortTxnRequest;
 import com.starrocks.rpc.BrpcProxy;
-import com.starrocks.rpc.LakeService;
 import com.starrocks.server.GlobalStateMgr;
-import com.starrocks.system.Backend;
+import com.starrocks.system.ComputeNode;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
+import javax.annotation.Nullable;
 
 public class LakeTableTxnStateListener implements TransactionStateListener {
     private static final Logger LOG = LogManager.getLogger(LakeTableTxnStateListener.class);
@@ -66,6 +67,9 @@ public class LakeTableTxnStateListener implements TransactionStateListener {
     public void preCommit(TransactionState txnState, List<TabletCommitInfo> finishedTablets,
             List<TabletFailInfo> failedTablets) throws TransactionException {
         Preconditions.checkState(txnState.getTransactionStatus() != TransactionStatus.COMMITTED);
+        if (!finishedTablets.isEmpty()) {
+            txnState.setTabletCommitInfos(finishedTablets);
+        }
         if (table.getState() == OlapTable.OlapTableState.RESTORE) {
             throw new TransactionCommitFailedException("Cannot write RESTORE state table \"" + table.getName() + "\"");
         }
@@ -184,43 +188,63 @@ public class LakeTableTxnStateListener implements TransactionStateListener {
 
     @Override
     public void postAbort(TransactionState txnState, List<TabletFailInfo> failedTablets) {
-        Map<Long, List<Long>> tabletGroup = null;
-        Database db = GlobalStateMgr.getCurrentState().getDb(txnState.getDbId());
-        if (db == null) {
-            return;
+        if (CollectionUtils.isEmpty(txnState.getTabletCommitInfos())) {
+            abortTxnSkipCleanup(txnState);
+        } else {
+            abortTxnWithCleanup(txnState);
         }
+    }
 
-        db.readLock();
-        try {
-            // Preconditions: has acquired the database's reader or writer lock.
-            tabletGroup = Utils.groupTabletID(table);
-        } catch (NoAliveBackendException e) {
-            LOG.warn(e);
-        } finally {
-            db.readUnlock();
+    private void abortTxnSkipCleanup(TransactionState txnState) {
+        List<Long> txnIds = Collections.singletonList(txnState.getTransactionId());
+        List<ComputeNode> nodes = getAllAliveNodes();
+        for (ComputeNode node : nodes) { // Send abortTxn() request to all nodes
+            AbortTxnRequest request = new AbortTxnRequest();
+            request.txnIds = txnIds;
+            request.skipCleanup = true;
+            request.tabletIds = null; // unused when skipCleanup is true
+
+            sendAbortTxnRequestIgnoreResponse(request, node);
         }
+    }
 
-        if (tabletGroup == null) {
-            return;
+    private void abortTxnWithCleanup(TransactionState txnState) {
+        List<Long> txnIds = Collections.singletonList(txnState.getTransactionId());
+        Map<Long, List<Long>> tabletGroup = new HashMap<>();
+        for (TabletCommitInfo info : txnState.getTabletCommitInfos()) {
+            tabletGroup.computeIfAbsent(info.getBackendId(), k -> Lists.newArrayList()).add(info.getTabletId());
         }
-
         for (Map.Entry<Long, List<Long>> entry : tabletGroup.entrySet()) {
-            Backend backend = GlobalStateMgr.getCurrentSystemInfo().getBackend(entry.getKey());
-            if (backend == null) {
-                // It's ok to skip sending abort transaction request.
+            ComputeNode node = getAliveNode(entry.getKey());
+            if (node == null) {
                 continue;
             }
             AbortTxnRequest request = new AbortTxnRequest();
-            request.txnIds = Lists.newArrayList();
-            request.txnIds.add(txnState.getTransactionId());
+            request.txnIds = txnIds;
             request.tabletIds = entry.getValue();
+            request.skipCleanup = false;
 
-            try {
-                LakeService lakeService = BrpcProxy.getLakeService(backend.getHost(), backend.getBrpcPort());
-                lakeService.abortTxn(request);
-            } catch (Throwable e) {
-                LOG.error(e);
-            }
+            sendAbortTxnRequestIgnoreResponse(request, node);
         }
+    }
+
+    static void sendAbortTxnRequestIgnoreResponse(AbortTxnRequest request, ComputeNode node) {
+        try {
+            BrpcProxy.getLakeService(node.getHost(), node.getBrpcPort()).abortTxn(request);
+        } catch (Throwable e) {
+            LOG.error(e);
+        }
+    }
+
+    static List<ComputeNode> getAllAliveNodes() {
+        List<ComputeNode> nodes = new ArrayList<>();
+        nodes.addAll(GlobalStateMgr.getCurrentSystemInfo().getAvailableComputeNodes());
+        nodes.addAll(GlobalStateMgr.getCurrentSystemInfo().getAvailableBackends());
+        return nodes;
+    }
+
+    @Nullable
+    static ComputeNode getAliveNode(Long nodeId) {
+        return GlobalStateMgr.getCurrentSystemInfo().getBackendOrComputeNode(nodeId);
     }
 }
