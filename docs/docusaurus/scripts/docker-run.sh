94ceba043603a46ebcc100f839955d6f0653e943
DanRoscigno <dan@roscigno.com>
2023-11-18 10:43:35 -0500

    added locale switch
    Signed-off-by: DanRoscigno <dan@roscigno.com>

--- docs/docusaurus/scripts/docker-run.sh
+++ docs/docusaurus/scripts/docker-run.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+
+echo -n "Docusaurus builds only one locale in dev mode, please enter a locale (zh or en): "
+read locale
+
 DOCUSAURUS_DIR=`pwd`
 DOCS_DIR="$(dirname "$DOCUSAURUS_DIR")"
 docker run --rm --interactive --tty \
@@ -6,4 +10,4 @@ docker run --rm --interactive --tty \
 	--volume $DOCS_DIR/en:/app/docusaurus/docs \
 	--volume "$DOCS_DIR/zh:/app/docusaurus/i18n/zh/docusaurus-plugin-content-docs/current" \
 	-p 3000:3000 \
-	docs-build yarn start -p 3000 -h 0.0.0.0
+	docs-build yarn start -p 3000 -h 0.0.0.0 --locale $locale
