1ad57c1c4d48792c1765d1b3b2ea332d413e3042
packy92 <110370499+packy92@users.noreply.github.com>
2023-11-18 15:04:48 +0800

    [BugFix] re-generated fn signature when change the input of window function (#34878)
    Signed-off-by: packy92 <wangchao@starrocks.com>
    (cherry picked from commit 72c82b8bc5b02d8c09f3c424fe401a26b02751e9)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/tree/pdagg/PushDownDistinctAggregateRewriter.java
    #	fe/fe-core/src/test/java/com/starrocks/sql/plan/AggregatePushDownTest.java

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/AggregatePushDownTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/AggregatePushDownTest.java
@@ -42,4 +42,119 @@ public class AggregatePushDownTest extends PlanTestBase {
             connectContext.getSessionVariable().setCboPushDownAggregate("global");
         }
     }
+<<<<<<< HEAD
+=======
+
+    @Test
+    public void testPushDownDistinctAggBelowWindow()
+            throws Exception {
+        String q1 = "SELECT DISTINCT \n" +
+                "  COALESCE(region, 'Other') AS region, \n" +
+                "  order_date, \n" +
+                "  SUM(income) OVER ( PARTITION BY  COALESCE(region, 'Other'), " +
+                "   order_date) AS gp_income,  \n" +
+                "  SUM(income) OVER ( PARTITION BY  COALESCE(region, 'Other'), " +
+                "   MONTH(order_date) ORDER BY order_date) AS gp_income_MTD,\n" +
+                "  SUM(income) OVER ( PARTITION BY  COALESCE(region, 'Other'), " +
+                "   YEAR (order_date), QUARTER(order_date) ORDER BY order_date) AS gp_income_QTD,\n" +
+                "  SUM(income) OVER ( PARTITION BY  COALESCE(region, 'Other'), " +
+                "   YEAR (order_date) ORDER BY order_date) AS gp_income_YTD  \n" +
+                "FROM  trans\n" +
+                "where month(order_date)=1\n" +
+                "order by region, order_date";
+        String plan = UtFrameUtils.getVerboseFragmentPlan(connectContext, q1);
+        Assert.assertTrue(plan.contains("  1:AGGREGATE (update finalize)\n" +
+                "  |  aggregate: sum[([3: income, DECIMAL128(10,2), false]); args: DECIMAL128; " +
+                "result: DECIMAL128(38,2); args nullable: false; result nullable: true]\n" +
+                "  |  group by: [2: order_date, DATE, false], [1: region, VARCHAR, true]\n"));
+
+        Assert.assertTrue(plan.contains("  0:OlapScanNode\n" +
+                "     table: trans, rollup: trans\n" +
+                "     preAggregation: on\n" +
+                "     Predicates: month[([2: order_date, DATE, false]); args: DATE; result: TINYINT; " +
+                "args nullable: false; result nullable: false] = 1\n" +
+                ""));
+    }
+
+    @Test
+    public void testPushDownDistinctAggBelowWindow_1() throws Exception {
+        // unsupported window func ref cols from partition by cols
+        String sql = "select distinct t1d from (select *, sum(t1e) over (partition by t1d) as cnt from test_all_type ) " +
+                "t where cnt > 1 limit 10;";
+
+        String plan = getFragmentPlan(sql);
+        assertContains(plan, "5:SELECT\n" +
+                "  |  predicates: 11: sum(5: t1e) > 1.0\n" +
+                "  |  \n" +
+                "  4:ANALYTIC\n" +
+                "  |  functions: [, sum(12: sum), ]\n" +
+                "  |  partition by: 4: t1d\n" +
+                "  |  \n" +
+                "  3:SORT\n" +
+                "  |  order by: <slot 4> 4: t1d ASC\n" +
+                "  |  offset: 0\n" +
+                "  |  \n" +
+                "  2:AGGREGATE (update finalize)\n" +
+                "  |  output: sum(5: t1e)\n" +
+                "  |  group by: 4: t1d\n" +
+                "  |  \n" +
+                "  1:EXCHANGE");
+    }
+
+    @Test
+    public void testPushDownDistinctAggBelowWindow_2() throws Exception {
+        // unsupported window func ref cols from partition by cols
+        String sql = "select distinct t1d from (select *, sum(t1d) over (partition by t1d, t1e) as cnt from " +
+                "test_all_type ) t where cnt > 1 limit 10;";
+
+        String plan = getFragmentPlan(sql);
+        assertContains(plan, "4:ANALYTIC\n" +
+                "  |  functions: [, sum(12: sum), ]\n" +
+                "  |  partition by: 4: t1d, 5: t1e\n" +
+                "  |  \n" +
+                "  3:SORT\n" +
+                "  |  order by: <slot 4> 4: t1d ASC, <slot 5> 5: t1e ASC\n" +
+                "  |  offset: 0\n" +
+                "  |  \n" +
+                "  2:AGGREGATE (update finalize)\n" +
+                "  |  output: sum(4: t1d)\n" +
+                "  |  group by: 4: t1d, 5: t1e\n" +
+                "  |  \n" +
+                "  1:EXCHANGE");
+    }
+
+    @Test
+    public void testPushDownDistinctAggBelowWindow_3() throws Exception {
+        // unsupported window func ref cols from partition by cols
+        String sql = "select distinct t1c from (select *, sum(t1d) over (partition by t1e order by t1d) as cnt from " +
+                "test_all_type ) t where cnt > 1 limit 10;";
+
+        String plan = getFragmentPlan(sql);
+        assertContains(plan, "2:AGGREGATE (update finalize)\n" +
+                "  |  output: sum(4: t1d)\n" +
+                "  |  group by: 3: t1c, 4: t1d, 5: t1e\n" +
+                "  |  \n" +
+                "  1:EXCHANGE");
+    }
+
+    @Test
+    public void testNotPushDownDistinctAggBelowWindow_1() throws Exception {
+        // unsupported count function
+        String sql = "select distinct t1d from (select *, count(1) over (partition by t1d) as cnt from test_all_type ) " +
+                "t where cnt > 1 limit 10;";
+        String plan = getFragmentPlan(sql);
+        assertContains(plan, "4:SELECT\n" +
+                "  |  predicates: 11: count(1) > 1\n" +
+                "  |  \n" +
+                "  3:ANALYTIC\n" +
+                "  |  functions: [, count(1), ]\n" +
+                "  |  partition by: 4: t1d\n" +
+                "  |  \n" +
+                "  2:SORT\n" +
+                "  |  order by: <slot 4> 4: t1d ASC\n" +
+                "  |  offset: 0\n" +
+                "  |  \n" +
+                "  1:EXCHANGE");
+    }
+>>>>>>> 72c82b8bc5 ([BugFix] re-generated fn signature when change the input of window function (#34878))
 }
