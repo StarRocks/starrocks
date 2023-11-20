2ddd0aeb719591ffbf3fc30b34cd1dedca9fda94
shuming.li <ming.moriarty@gmail.com>
2023-11-20 17:22:48 +0800

    Fix date_trunc rewrite by using equlation prediate rewriter
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>

---
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/materialization/equivalent/TimeSliceReplaceChecker.java
@@ -0,0 +1,80 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.starrocks.sql.optimizer.rule.transformation.materialization.equivalent;
+
+import com.starrocks.catalog.FunctionSet;
+import com.starrocks.catalog.PrimitiveType;
+import com.starrocks.common.AnalysisException;
+import com.starrocks.sql.optimizer.operator.scalar.CallOperator;
+import com.starrocks.sql.optimizer.operator.scalar.ColumnRefOperator;
+import com.starrocks.sql.optimizer.operator.scalar.ConstantOperator;
+import com.starrocks.sql.optimizer.operator.scalar.ScalarOperator;
+import com.starrocks.sql.optimizer.rewrite.ScalarOperatorFunctions;
+
+public class TimeSliceReplaceChecker implements PredicateReplaceChecker {
+    public static final TimeSliceReplaceChecker INSTANCE = new TimeSliceReplaceChecker();
+
+    private CallOperator mvTimeSlice;
+
+    public TimeSliceReplaceChecker() {}
+
+    public TimeSliceReplaceChecker(CallOperator mvTimeSlice) {
+        this.mvTimeSlice = mvTimeSlice;
+    }
+
+    @Override
+    public boolean canReplace(ScalarOperator operator) {
+        if (mvTimeSlice == null) {
+            return false;
+        }
+
+        try {
+            if (operator.isConstantRef() && operator.getType().getPrimitiveType() == PrimitiveType.DATETIME) {
+                ConstantOperator sliced = ScalarOperatorFunctions.timeSlice(
+                        (ConstantOperator) operator,
+                        ((ConstantOperator) mvTimeSlice.getChild(1)),
+                        ((ConstantOperator) mvTimeSlice.getChild(2)),
+                        ((ConstantOperator) mvTimeSlice.getChild(3)));
+                return sliced.equals(operator);
+            }
+        } catch (AnalysisException e) {
+            return false;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean canReplace(ScalarOperator op1, ConstantOperator op2) {
+        if (!(op1 instanceof CallOperator)) {
+            return false;
+        }
+        CallOperator func = (CallOperator) op1;
+        if (!func.getFnName().equalsIgnoreCase(FunctionSet.TIME_SLICE)) {
+            return false;
+        }
+        if (!(func.getChild(0) instanceof ColumnRefOperator)) {
+            return false;
+        }
+        try {
+            ConstantOperator sliced = ScalarOperatorFunctions.timeSlice(
+                    op2,
+                    ((ConstantOperator) op1.getChild(1)),
+                    ((ConstantOperator) op1.getChild(2)),
+                    ((ConstantOperator) op1.getChild(3)));
+            return sliced.equals(op2);
+        } catch (AnalysisException e) {
+            return false;
+        }
+    }
+}
