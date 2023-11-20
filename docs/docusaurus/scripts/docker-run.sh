e4cb1e6cb325cd30c2f2c4c061f1f15f17bb3188
DanRoscigno <dan@roscigno.com>
2023-11-20 11:32:22 -0500

    mount sidebars file for reload
    Signed-off-by: DanRoscigno <dan@roscigno.com>

--- docs/docusaurus/scripts/docker-run.sh
+++ docs/docusaurus/scripts/docker-run.sh
@@ -7,6 +7,7 @@ DOCUSAURUS_DIR=`pwd`
 DOCS_DIR="$(dirname "$DOCUSAURUS_DIR")"
 docker run --rm --interactive --tty \
 	-e DISABLE_VERSIONING=true \
+	--volume $DOCS_DIR/docusaurus/sidebars.json:/app/docusaurus/sidebars.json \
 	--volume $DOCS_DIR/en:/app/docusaurus/docs \
 	--volume "$DOCS_DIR/zh:/app/docusaurus/i18n/zh/docusaurus-plugin-content-docs/current" \
 	-p 3000:3000 \
