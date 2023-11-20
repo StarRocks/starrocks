9503d7a3dbdce5ef19e4c2da22ff1114f14752ac
Xuefen Liu <liuxuefen@starrocks.com>
2023-11-20 15:06:32 +0800

    update codes (#8286)
    Signed-off-by: abc982627271 <liuxuefen@starrocks.com>

--- env.sh
+++ env.sh
@@ -85,11 +85,18 @@ fi
 
 # check java version
 export JAVA=${JAVA_HOME}/bin/java
-JAVA_VER=$(${JAVA} -version 2>&1 | sed 's/.* version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q' | cut -f1 -d " ")
-if [[ $JAVA_VER -lt 18 ]]; then
-    echo "Error: require JAVA with JDK version at least 1.8"
+JAVA_VER=$(${JAVA} -version 2>&1 | grep -oP 'version "\K[^"]+')
+
+# extract major_version
+IFS='.' read -ra ADDR <<< "$JAVA_VER"
+major_version=${ADDR[0]}
+
+# compare major_version
+if [[ $major_version -lt 11 ]]; then
+    echo "Error: require JAVA with JDK version at least 11"
     exit 1
 fi
+
 
 # check maven
 MVN_CMD=mvn
