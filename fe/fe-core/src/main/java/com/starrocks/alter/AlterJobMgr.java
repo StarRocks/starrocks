a7b3d8a375bd40fc95ff105db9be7e0036aee2fd
packy92 <110370499+packy92@users.noreply.github.com>
2023-11-22 19:34:49 +0800

    [Enhancement] add view def cache (#35499)
    Signed-off-by: packy92 <wangchao@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/alter/AlterJobMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/alter/AlterJobMgr.java
@@ -770,6 +770,7 @@ public class AlterJobMgr {
             LocalMetastore.inactiveRelatedMaterializedView(db, view, String.format("base view %s changed", viewName));
             db.dropTable(viewName);
             db.registerTableUnlocked(view);
+            view.lastSchemaUpdateTime.set(System.currentTimeMillis());
 
             LOG.info("replay modify view[{}] definition to {}", viewName, inlineViewDef);
         } finally {
