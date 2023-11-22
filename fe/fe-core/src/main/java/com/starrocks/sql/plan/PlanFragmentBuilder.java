dd2248854155121ca39ebdb7a1396dc48de81265
Xuefen Liu <liuxuefen@starrocks.com>
2023-11-22 19:04:29 +0800

    [BugFix] enable es table query on compute nodes (#35193)
    Signed-off-by: abc982627271 <liuxuefen@starrocks.com>
    (cherry picked from commit 4328d93015dfd9789d7d6625bffc52e030a1dc97)

--- fe/fe-core/src/main/java/com/starrocks/sql/plan/PlanFragmentBuilder.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/plan/PlanFragmentBuilder.java
@@ -1485,7 +1485,7 @@ public class PlanFragmentBuilder {
             scanNode.computeStatistics(optExpression.getStatistics());
             scanNode.setScanOptimzeOption(node.getScanOptimzeOption());
             try {
-                scanNode.assignBackends();
+                scanNode.assignNodes();
             } catch (UserException e) {
                 throw new StarRocksPlannerException(e.getMessage(), INTERNAL_ERROR);
             }
