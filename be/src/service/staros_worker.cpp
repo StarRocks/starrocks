0c10cffa6c6cd050b1bf5d97345170ed04a528c2
Alex Zhu <zhuming9011@gmail.com>
2023-11-23 11:08:02 +0800

    [Enhancement] Delete files with retry in shared data mode (#35566)
    Signed-off-by: Alex Zhu <zhuming9011@gmail.com>
    (cherry picked from commit 03df07a4f6e3b8ddba43805acce58bd12ead8860)

--- be/src/service/staros_worker.cpp
+++ be/src/service/staros_worker.cpp
@@ -405,6 +405,8 @@ Status to_status(absl::Status absl_status) {
         return Status::InvalidArgument(fmt::format("starlet err {}", absl_status.message()));
     case absl::StatusCode::kNotFound:
         return Status::NotFound(fmt::format("starlet err {}", absl_status.message()));
+    case absl::StatusCode::kResourceExhausted:
+        return Status::ResourceBusy(fmt::format("starlet err {}", absl_status.message()));
     default:
         return Status::InternalError(fmt::format("starlet err {}", absl_status.message()));
     }
