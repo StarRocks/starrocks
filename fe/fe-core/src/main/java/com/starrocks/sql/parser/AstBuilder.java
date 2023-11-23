4668c8c12936721d94aa413cf26f22effab31ffe
Binglin Chang <decstery@gmail.com>
2023-11-24 05:26:44 +0800

    [BugFix] mark using limit/offset in preparedstmt not suppported (#35675)
    Signed-off-by: Binglin Chang <decstery@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/parser/AstBuilder.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/parser/AstBuilder.java
@@ -4318,6 +4318,9 @@ public class AstBuilder extends StarRocksBaseVisitor<ParseNode> {
 
     @Override
     public ParseNode visitLimitElement(StarRocksParser.LimitElementContext context) {
+        if (context.limit.getText().equals("?") || (context.offset != null && context.offset.getText().equals("?"))) {
+            throw new ParsingException("using parameter(?) as limit or offset not supported");
+        }
         long limit = Long.parseLong(context.limit.getText());
         long offset = 0;
         if (context.offset != null) {
@@ -5491,7 +5494,7 @@ public class AstBuilder extends StarRocksBaseVisitor<ParseNode> {
                 exprs.add(value);
             }
         } catch (Exception e) {
-            throw  new IllegalArgumentException(String.format("Cast argument %s to int type failed.", value.toSql()));
+            throw new IllegalArgumentException(String.format("Cast argument %s to int type failed.", value.toSql()));
         }
     }
 
@@ -6657,7 +6660,7 @@ public class AstBuilder extends StarRocksBaseVisitor<ParseNode> {
         parameters.add(parameter);
         return parameter;
     }
-    
+
     // ------------------------------------------- Util Functions -------------------------------------------
 
     protected <T> List<T> visit(List<? extends ParserRuleContext> contexts, Class<T> clazz) {
