4ea76f2dea5d8ba3fe289a9cbd0c546e72c7a17b
stonechen111 <chenshi@starrocks.com>
2023-11-24 18:20:02 +0800

    [Feature] Add support datacache.partition_duration for mv.
    Add support datacache.partition_duration property for materialized view to
    control the validity time of its datacache.
    
    Signed-off-by: stonechen111 <chenshi@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
+++ fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
@@ -3433,6 +3433,12 @@ public class LocalMetastore implements ConnectorMetadata {
                 setLakeStorageInfo(materializedView, storageVolumeId, properties);
             }
 
+            // datacache.partition_duration
+            if (properties.containsKey(PropertyAnalyzer.PROPERTIES_DATACACHE_PARTITION_DURATION)) {
+                PeriodDuration duration = PropertyAnalyzer.analyzeDataCachePartitionDuration(properties);
+                materializedView.setDataCachePartitionDuration(duration);
+            }
+
             // session properties
             if (!properties.isEmpty()) {
                 // analyze properties
