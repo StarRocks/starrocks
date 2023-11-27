fe24c0610a68ab3af3b1ddb7fe03bc6777fc1d73
Alex Zhu <zhuming9011@gmail.com>
2023-11-27 18:53:04 +0800

    [BugFix] Fixed potential deadlock in compaction scheduler (#35829)
    Signed-off-by: Alex Zhu <zhuming9011@gmail.com>
    (cherry picked from commit f374aabc1e89cccd4c53f40171cc3bd705bdd7af)

--- be/src/storage/lake/compaction_scheduler.cpp
+++ be/src/storage/lake/compaction_scheduler.cpp
@@ -46,7 +46,7 @@ CompactionTaskCallback::CompactionTaskCallback(CompactionScheduler* scheduler, c
 }
 
 void CompactionTaskCallback::finish_task(std::unique_ptr<CompactionTaskContext>&& context) {
-    std::lock_guard l(_mtx);
+    std::unique_lock l(_mtx);
 
     if (!context->status.ok()) {
         // Add failed tablet for upgrade compatibility: older version FE relies on the failed tablet to determine
@@ -71,8 +71,11 @@ void CompactionTaskCallback::finish_task(std::unique_ptr<CompactionTaskContext>&
         _request = nullptr;
         _response = nullptr;
 
-        _scheduler->remove_states(_contexts);
-        STLClearObject(&_contexts);
+        std::vector<std::unique_ptr<CompactionTaskContext>> tmp;
+        tmp.swap(_contexts);
+
+        l.unlock();
+        _scheduler->remove_states(tmp);
     }
 }
 
@@ -251,11 +254,12 @@ bool CompactionScheduler::txn_log_exists(int64_t tablet_id, int64_t txn_id) cons
 }
 
 Status CompactionScheduler::abort(int64_t txn_id) {
-    std::lock_guard l(_contexts_lock);
+    std::unique_lock l(_contexts_lock);
     for (butil::LinkNode<CompactionTaskContext>* node = _contexts.head(); node != _contexts.end();
          node = node->next()) {
         CompactionTaskContext* context = node->value();
         if (context->txn_id == txn_id) {
+            l.unlock();
             context->callback->update_status(Status::Aborted("aborted on demand"));
             return Status::OK();
         }
