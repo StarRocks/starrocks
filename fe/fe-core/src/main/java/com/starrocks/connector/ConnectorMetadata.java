4707965f78720ae2d31f7f6ff76fe4f4c2f0717a
Husky Zeng <zengqingdong.zqd@alibaba-inc.com>
2023-11-27 10:03:18 +0800

    [BugFix] Revert "[Feature] support refresh paimon mv by partition. (#… (#35766)

--- fe/fe-core/src/main/java/com/starrocks/connector/ConnectorMetadata.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/ConnectorMetadata.java
@@ -279,9 +279,5 @@ public interface ConnectorMetadata {
     default CloudConfiguration getCloudConfiguration() {
         throw new StarRocksConnectorException("This connector doesn't support getting cloud configuration");
     }
-
-    default List<PartitionInfo> getChangedPartitionInfo(Table table, long latestSnapShotID) {
-        return Lists.newArrayList();
-    }
 }
 
