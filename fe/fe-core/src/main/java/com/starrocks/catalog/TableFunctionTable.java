cddb53ba9b315349763e263fc7a65e3c926089f5
ricky <rickif@qq.com>
2023-11-27 17:19:01 +0800

    table_function: throw ddl exception
    Signed-off-by: ricky <rickif@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/TableFunctionTable.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/TableFunctionTable.java
@@ -306,15 +306,14 @@ public class TableFunctionTable extends Table {
 
         PGetFileSchemaResult result;
         try {
-            // TODO(fw): more format support.
             PGetFileSchemaRequest request = getGetFileSchemaRequest(fileStatuses);
             Future<PGetFileSchemaResult> future = BackendServiceClient.getInstance().getFileSchema(address, request);
             result = future.get();
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
-            throw new DdlException("failed to get file schema", e);
+            throw new DdlException("failed to get file schema: " + e.getMessage());
         } catch (Exception e) {
-            throw new DdlException("failed to get file schema", e);
+            throw new DdlException("failed to get file schema: " + e.getMessage());
         }
 
         if (TStatusCode.findByValue(result.status.statusCode) != TStatusCode.OK) {
