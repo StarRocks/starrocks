a1a766ec82b185e0a8b3cbc8f0bfeb0478a07d51
stephen <stephen5217@163.com>
2023-11-29 19:28:14 +0800

    [Feature] disable iceberg column statistics when iceberg job planning
    Signed-off-by: stephen <stephen5217@163.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
@@ -1480,7 +1480,7 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
     private boolean enableReadIcebergPuffinNdv = true;
 
     @VarAttr(name = ENABLE_ICEBERG_COLUMN_STATISTICS)
-    private boolean enableIcebergColumnStatistics = true;
+    private boolean enableIcebergColumnStatistics = false;
 
     @VarAttr(name = LARGE_DECIMAL_UNDERLYING_TYPE)
     private String largeDecimalUnderlyingType = SessionVariableConstants.PANIC;
