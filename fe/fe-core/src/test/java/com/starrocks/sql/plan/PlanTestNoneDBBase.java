a1ca7086686f5a937c8eeb376e90e577b7260b26
packy92 <110370499+packy92@users.noreply.github.com>
2023-11-30 10:04:24 +0800

    [BugFix] split limit directly in some rules  (#35875) (#36051)
    Signed-off-by: packy92 <wangchao@starrocks.com>
    (cherry picked from commit 9cb912ddbc86fb7ec419b5933a9c9eca8c653997)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/PruneGroupByKeysRule.java

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/PlanTestNoneDBBase.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/PlanTestNoneDBBase.java
@@ -479,7 +479,7 @@ public class PlanTestNoneDBBase {
         String explainString = getFragmentPlan(sql);
 
         for (String expected : explain) {
-            Assert.assertTrue("expected is: " + expected + " but plan is \n" + explainString,
+            Assert.assertTrue("expected is:\n" + expected + "\n but plan is \n" + explainString,
                     StringUtils.containsIgnoreCase(explainString.toLowerCase(), expected));
         }
     }
