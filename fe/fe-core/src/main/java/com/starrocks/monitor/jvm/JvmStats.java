60b89305c6d7a84ca7d787ac782cfeeb0ec414cd
yiming <107105845+nshangyiming@users.noreply.github.com>
2023-11-30 15:50:19 +0800

    [Enhancement] Add more jvm stats into /metrics http api (#35768)
    Why I'm doing:
    Some of the key jvm stats, like yound/old heap metrics are not output in the `/metrics` http api.
    And the metrics are not aligned between prometheus and json format.
    
    What I'm doing:
    Add those missing metrics, and refactor some code to make it easy to maintain and read.
    
    Fixes SR-22790
    
    Signed-off-by: Dejun Xia <xiadejun@starrocks.com>
    (cherry picked from commit 63859036b0926a60e774c57ac29ee0305fdd293e)

--- fe/fe-core/src/main/java/com/starrocks/monitor/jvm/JvmStats.java
+++ fe/fe-core/src/main/java/com/starrocks/monitor/jvm/JvmStats.java
@@ -23,6 +23,7 @@ package com.starrocks.monitor.jvm;
 
 import com.starrocks.monitor.unit.ByteSizeValue;
 import com.starrocks.monitor.unit.TimeValue;
+import org.jetbrains.annotations.NotNull;
 
 import java.lang.management.BufferPoolMXBean;
 import java.lang.management.ClassLoadingMXBean;
@@ -69,7 +70,7 @@ public class JvmStats {
                 MemoryUsage usage = memoryPoolMXBean.getUsage();
                 MemoryUsage peakUsage = memoryPoolMXBean.getPeakUsage();
                 String name = GcNames.getByMemoryPoolName(memoryPoolMXBean.getName(), null);
-                if (name == null) { // if we can't resolve it, its not interesting.... (Per Gen, Code Cache)
+                if (name == null) { // if we can't resolve it, it's not interesting.... (Per Gen, Code Cache)
                     continue;
                 }
                 pools.add(new MemoryPool(name,
@@ -226,6 +227,7 @@ public class JvmStats {
             return this.collectors;
         }
 
+        @NotNull
         @Override
         public Iterator<GarbageCollector> iterator() {
             return Arrays.stream(collectors).iterator();
@@ -362,6 +364,7 @@ public class JvmStats {
             this.pools = pools;
         }
 
+        @NotNull
         @Override
         public Iterator<MemoryPool> iterator() {
             return pools.iterator();
