db61db0167b211c94427b7a58d04e88070fef0bc
Letian Jiang <letian.jiang@outlook.com>
2023-11-30 20:17:24 +0800

    update
    Signed-off-by: Letian Jiang <letian.jiang@outlook.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/ast/InsertStmt.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/ast/InsertStmt.java
@@ -31,7 +31,6 @@ import com.starrocks.sql.parser.NodePosition;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
