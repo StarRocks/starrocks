716e747fb8971d5c032124ccd584595c8b1c8a31
ricky <rickif@qq.com>
2023-12-02 15:48:12 +0800

    routine_load: add substate UNSTABLE
    Signed-off-by: ricky <rickif@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/common/Config.java
+++ fe/fe-core/src/main/java/com/starrocks/common/Config.java
@@ -2653,4 +2653,7 @@ public class Config extends ConfigBase {
 
     @ConfField(mutable = true)
     public static boolean use_lock_manager = false;
+
+    @ConfField(mutable = true)
+    public static long routine_load_unstable_threshold_second = 3600;
 }
