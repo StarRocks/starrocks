ef58592e597c49706a8d854839d88fa2ff72593b
Yixin Luo <18810541851@163.com>
2023-12-03 17:57:50 -0800

    [BugFix] fix update replica version by mistake in txn log applier (#36292)
    Why I'm doing:
    In previous PR #26788 , we store tablet commit info in transaction state, and when finish transaction, if this replica no exist in tablet commit info, we skip update its version to visible version. So we can fix issue like this:
    ```
    ERROR 1064 (HY000): capture_consistent_versions error: version not found. tablet_id: 12032, version: 6 tablet_max_version:2 backend [id=10145] [host=172.26.80.102]
    ```
    But in `OlapTableTxnLogApplier`, there is still can update replica's version to visible version, which will cause issue happen again.
    
    What I'm doing:
    Add tablet commit info check to `OlapTableTxnLogApplier`.
    
    Signed-off-by: luohaha <18810541851@163.com>
    (cherry picked from commit 2d14c8164818d53a2853ca0bc97d0b3a03df6422)
    
    # Conflicts:
    #	fe/fe-core/src/test/java/com/starrocks/catalog/GlobalStateMgrTestUtil.java
    #	fe/fe-core/src/test/java/com/starrocks/transaction/DatabaseTransactionMgrTest.java

--- fe/fe-core/src/test/java/com/starrocks/catalog/GlobalStateMgrTestUtil.java
+++ fe/fe-core/src/test/java/com/starrocks/catalog/GlobalStateMgrTestUtil.java
@@ -83,6 +83,13 @@ public class GlobalStateMgrTestUtil {
     public static String testTxnLable3 = "testTxnLable3";
     public static String testTxnLable4 = "testTxnLable4";
     public static String testTxnLable5 = "testTxnLable5";
+<<<<<<< HEAD
+=======
+    public static String testTxnLable6 = "testTxnLable6";
+    public static String testTxnLable7 = "testTxnLable7";
+    public static String testTxnLable8 = "testTxnLable8";
+    public static String testTxnLable9 = "testTxnLable9";
+>>>>>>> 2d14c81648 ([BugFix] fix update replica version by mistake in txn log applier (#36292))
     public static String testEsTable1 = "partitionedEsTable1";
     public static long testEsTableId1 = 14;
 
