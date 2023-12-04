d39509c1b63febb1d300e7fd1a0816864c2d3c4f
Murphy <96611012+mofeiatwork@users.noreply.github.com>
2023-12-04 13:36:11 +0800

    [Enhancement] try to avoid acquire lock in metric (#36180)
    Signed-off-by: Murphy <mofei@starrocks.com>
    (cherry picked from commit f2f40ce509b63e62813d186fcbb957931fd03d9e)
    Signed-off-by: Murphy <mofei@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/Database.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/Database.java
@@ -570,13 +570,10 @@ public class Database extends MetaObject implements Writable {
     }
 
     public List<MaterializedView> getMaterializedViews() {
-        List<MaterializedView> materializedViews = new ArrayList<>();
-        for (Table table : idToTable.values()) {
-            if (TableType.MATERIALIZED_VIEW == table.getType()) {
-                materializedViews.add((MaterializedView) table);
-            }
-        }
-        return materializedViews;
+        return idToTable.values().stream()
+                .filter(Table::isMaterializedView)
+                .map(x -> (MaterializedView) x)
+                .collect(Collectors.toList());
     }
 
     public Set<String> getTableNamesViewWithLock() {
