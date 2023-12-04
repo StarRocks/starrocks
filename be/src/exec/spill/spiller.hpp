476923b9c06be46cfdf1c64d5d8abc58e5076803
stdpain <34912776+stdpain@users.noreply.github.com>
2023-12-04 15:36:05 +0800

    [BugFix] Fix bucket colocate agg error when enable spill (#36346)
    Signed-off-by: stdpain <drfeng08@gmail.com>

--- be/src/exec/spill/spiller.hpp
+++ be/src/exec/spill/spiller.hpp
@@ -201,7 +201,7 @@ Status SpillerReader::trigger_restore(RuntimeState* state, TaskExecutor&& execut
             return Status::OK();
         }
         _running_restore_tasks++;
-        auto restore_task = [this, guard, trace = TraceInfo(state)](auto& yield_ctx) {
+        auto restore_task = [this, guard, trace = TraceInfo(state), _stream = _stream](auto& yield_ctx) {
             SCOPED_SET_TRACE_INFO({}, trace.query_id, trace.fragment_id);
             RETURN_IF(!guard.scoped_begin(), (void)0);
             DEFER_GUARD_END(guard);
