StarRocks/starrocks

commit cab91e3167afb5e0b0a5a18dabef3975299b1cd3

[BugFix] spillable partition sort sink use-after-free (#36223)
Signed-off-by: stdpain <drfeng08@gmail.com>

be/src/exec/pipeline/sort/spillable_partition_sort_sink_operator.cpp
@@ -69,13 +69,16 @@ Status SpillablePartitionSortSinkOperator::set_finishing(RuntimeState* state) {
     // if has spill task. we should wait all spill task finished then to call finished
     // TODO: test cancel case
     auto io_executor = _chunks_sorter->spill_channel()->io_executor();
-    auto set_call_back_function = [this](RuntimeState* state, auto io_executor) {
+    auto chunk_sorter = _chunks_sorter;
+    _sort_context->ref();
+    auto set_call_back_function = [this, chunk_sorter](RuntimeState* state, auto io_executor) {
         return _chunks_sorter->spiller()->set_flush_all_call_back(
-                [this]() {
+                [this, chunk_sorter]() {
                     // Current partition sort is ended, and
                     // the last call will drive LocalMergeSortSourceOperator to work.
-                    TRACE_SPILL_LOG << "finish partition rows:" << _chunks_sorter->get_output_rows();
-                    _sort_context->finish_partition(_chunks_sorter->get_output_rows());
+                    TRACE_SPILL_LOG << "finish partition rows:" << chunk_sorter->get_output_rows();
+                    _sort_context->finish_partition(chunk_sorter->get_output_rows());
+                    _sort_context->unref(runtime_state());
                     _is_finished = true;
                     return Status::OK();
                 },

be/src/exec/pipeline/spill_process_channel.h
@@ -25,6 +25,7 @@
 #include "exec/spill/spiller.h"
 #include "runtime/runtime_state.h"
 #include "util/blocking_queue.hpp"
+#include "util/defer_op.h"
 #include "util/runtime_profile.h"
 
 namespace starrocks {
@@ -87,7 +88,7 @@ public:
     bool add_last_task(SpillProcessTask&& task) {
         DCHECK(!_is_finishing);
         _is_working = true;
-        set_finishing();
+        auto defer = DeferOp([this]() { set_finishing(); });
         return _spill_tasks.put(std::move(task));
     }
 

test/sql/test_spill/R/test_short_circuit
@@ -1,0 +1,74 @@
+-- name: test_short_circuit
+set enable_spill=true;
+-- result:
+-- !result
+set spill_mode="force";
+-- result:
+-- !result
+set pipeline_dop=1;
+-- result:
+-- !result
+create table t0 (
+    c0 INT,
+    c1 BIGINT
+) DUPLICATE KEY(c0) DISTRIBUTED BY HASH(c0) BUCKETS 4 PROPERTIES('replication_num' = '1');
+-- result:
+-- !result
+insert into t0 SELECT generate_series, 4096 - generate_series FROM TABLE(generate_series(1,  6553500));
+-- result:
+-- !result
+create table t1 like t0;
+-- result:
+-- !result
+insert into t1 SELECT generate_series, 4096 - generate_series FROM TABLE(generate_series(4096,  8192));
+-- result:
+-- !result
+select count(*) from t0;
+-- result:
+6553500
+-- !result
+select count(*) from t1;
+-- result:
+4097
+-- !result
+create table empty_t like t0;
+-- result:
+-- !result
+insert into empty_t values (-1,-1);
+-- result:
+-- !result
+select count(*) from ( select l.c0,r.c1 from (select c0, c1, row_number() over (partition by c1) as rn from t0) l join empty_t r on l.c0=r.c0 and l.c1=r.c1 where r.c1 > 0) tb;
+-- result:
+0
+-- !result
+select count(*) from ( select l.c0,r.c1 from (select c0, c1, row_number() over (partition by c1) as rn from t0) l join [shuffle] empty_t r on l.c0=r.c0 and l.c1=r.c1 where r.c1 > 0) tb;
+-- result:
+0
+-- !result
+select count(*) from ( select * from (select c0,c1,max(c0),max(c1) from t0 group by c0,c1) l join empty_t r on l.c0=r.c0 and l.c1=r.c1 where r.c1 > 0) tb;
+-- result:
+0
+-- !result
+select count(*) from ( select * from (select c0,c1,max(c0),max(c1) from t0 group by c0,c1) l join [shuffle] empty_t r on l.c0=r.c0 and l.c1=r.c1 where r.c1 > 0) tb;
+-- result:
+0
+-- !result
+select count(*) from ( select * from (select t0.c0 as c0, t1.c1 as c1 from t0 join t1 on t0.c0=t1.c0) l join [shuffle] empty_t r on l.c0=r.c0 and l.c1=r.c1 where r.c1 > 0) tb;
+-- result:
+0
+-- !result
+set query_timeout=5;
+-- result:
+-- !result
+select count(*), count(rn) from (select c0, c0, row_number() over (partition by c0) as rn from TABLE(generate_series(1, 1000000000)) t(c0)) tb;
+-- result:
+E: (1064, 'Query exceeded time limit of 5 seconds')
+-- !result
+select count(*), count(c0), count(mc0) from (select max(c0) mc0, c0 from TABLE(generate_series(1, 1000000000)) t(c0) group by c0) tb;
+-- result:
+E: (1064, 'Query exceeded time limit of 5 seconds')
+-- !result
+select count(*) from ( select * from (select c0 from TABLE(generate_series(1, 1000000000)) t(c0)) l join [broadcast] (select c1 from TABLE(generate_series(1, 1000000000)) t(c1)) r on l.c0 = r.c1 )tb;
+-- result:
+E: (1064, 'Query exceeded time limit of 5 seconds')
+-- !result

test/sql/test_spill/T/test_short_circuit
@@ -1,0 +1,32 @@
+-- name: test_short_circuit
+set enable_spill=true;
+set spill_mode="force";
+set pipeline_dop=1;
+
+create table t0 (
+    c0 INT,
+    c1 BIGINT
+) DUPLICATE KEY(c0) DISTRIBUTED BY HASH(c0) BUCKETS 4 PROPERTIES('replication_num' = '1');
+insert into t0 SELECT generate_series, 4096 - generate_series FROM TABLE(generate_series(1,  6553500));
+create table t1 like t0;
+insert into t1 SELECT generate_series, 4096 - generate_series FROM TABLE(generate_series(4096,  8192));
+select count(*) from t0;
+select count(*) from t1;
+create table empty_t like t0;
+insert into empty_t values (-1,-1);
+-- full sort
+select count(*) from ( select l.c0,r.c1 from (select c0, c1, row_number() over (partition by c1) as rn from t0) l join empty_t r on l.c0=r.c0 and l.c1=r.c1 where r.c1 > 0) tb;
+select count(*) from ( select l.c0,r.c1 from (select c0, c1, row_number() over (partition by c1) as rn from t0) l join [shuffle] empty_t r on l.c0=r.c0 and l.c1=r.c1 where r.c1 > 0) tb;
+-- agg
+select count(*) from ( select * from (select c0,c1,max(c0),max(c1) from t0 group by c0,c1) l join empty_t r on l.c0=r.c0 and l.c1=r.c1 where r.c1 > 0) tb;
+select count(*) from ( select * from (select c0,c1,max(c0),max(c1) from t0 group by c0,c1) l join [shuffle] empty_t r on l.c0=r.c0 and l.c1=r.c1 where r.c1 > 0) tb;
+-- join
+select count(*) from ( select * from (select t0.c0 as c0, t1.c1 as c1 from t0 join t1 on t0.c0=t1.c0) l join [shuffle] empty_t r on l.c0=r.c0 and l.c1=r.c1 where r.c1 > 0) tb;
+-- test timeout case
+set query_timeout=5;
+-- timeout with spill full sort
+select count(*), count(rn) from (select c0, c0, row_number() over (partition by c0) as rn from TABLE(generate_series(1, 1000000000)) t(c0)) tb;
+-- timeout with spill agg
+select count(*), count(c0), count(mc0) from (select max(c0) mc0, c0 from TABLE(generate_series(1, 1000000000)) t(c0) group by c0) tb;
+-- timeout with join
+select count(*) from ( select * from (select c0 from TABLE(generate_series(1, 1000000000)) t(c0)) l join [broadcast] (select c1 from TABLE(generate_series(1, 1000000000)) t(c1)) r on l.c0 = r.c1 )tb;

commit c2258cbe830b3b69b928f1ec6dad60660c116244

[BugFix] Fix full sort circular reference leak
introduced by https://github.com/StarRocks/starrocks/pull/36223

ChunksSorter->Spiller->CallBackLambda->ChunksSorter->...

Signed-off-by: stdpain <drfeng08@gmail.com>

be/src/exec/pipeline/sort/spillable_partition_sort_sink_operator.cpp
@@ -69,7 +69,7 @@ Status SpillablePartitionSortSinkOperator::set_finishing(RuntimeState* state) {
     // if has spill task. we should wait all spill task finished then to call finished
     // TODO: test cancel case
     auto io_executor = _chunks_sorter->spill_channel()->io_executor();
-    auto chunk_sorter = _chunks_sorter;
+    auto chunk_sorter = _chunks_sorter.get();
     _sort_context->ref();
     auto set_call_back_function = [this, chunk_sorter](RuntimeState* state, auto io_executor) {
         return _chunks_sorter->spiller()->set_flush_all_call_back(