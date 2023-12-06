3809acb8da635c842f1828cb22286e0462cf4eae
meegoo <meegoo.sr@gmail.com>
2023-12-05 20:02:00 -0800

    [Enhancement] Add a runtime config to enable log stream load request/response (#36113)
    Signed-off-by: meegoo <meegoo.sr@gmail.com>
    (cherry picked from commit c5863c0daa09d7155bf376c80e4fcbbfee0e4cf3)

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -1036,6 +1036,8 @@ CONF_mInt32(primary_key_limit_size, "128");
 CONF_mBool(enable_http_stream_load_limit, "false");
 CONF_mInt32(finish_publish_version_internal, "100");
 
+CONF_mBool(enable_stream_load_verbose_log, "false");
+
 CONF_mInt32(get_txn_status_internal_sec, "30");
 
 // TOPN RuntimeFilter parameters
