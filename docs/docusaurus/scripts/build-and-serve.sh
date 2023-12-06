830f6c50946d0c8c2edf7114b53c1b0934eb7e8b
DanRoscigno <dan@roscigno.com>
2023-12-05 21:32:07 -0500

    revert
    Signed-off-by: DanRoscigno <dan@roscigno.com>

--- docs/docusaurus/scripts/build-and-serve.sh
+++ docs/docusaurus/scripts/build-and-serve.sh
@@ -1,8 +1,2 @@
 #! /bin/sh
-mkdir ./docs &&
-mkdir -p ./i18n/zh/docusaurus-plugin-content-docs/current &&
-cp -r ./english/* ./docs/ &&
-rm -rf ./docs/release_notes &&
-cp -r ./chinese/* ./i18n/zh/docusaurus-plugin-content-docs/current &&
-rm -rf ./i18n/zh/docusaurus-plugin-content-docs/current/release_notes &&
 yarn build && yarn serve -p 3000 -h 0.0.0.0
