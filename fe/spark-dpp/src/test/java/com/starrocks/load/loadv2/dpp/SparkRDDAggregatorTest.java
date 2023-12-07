21cb48ad80a1abac356f293d73d07ceaaac47a51
trueeyu <lxhhust350@qq.com>
2023-12-07 09:58:10 +0800

    fix
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- fe/spark-dpp/src/test/java/com/starrocks/load/loadv2/dpp/SparkRDDAggregatorTest.java
+++ fe/spark-dpp/src/test/java/com/starrocks/load/loadv2/dpp/SparkRDDAggregatorTest.java
@@ -14,7 +14,7 @@
 
 package com.starrocks.load.loadv2.dpp;
 
-import com.starrocks.common.io.BitmapValue;
+import com.starrocks.types.BitmapValue;
 import org.junit.Assert;
 import org.junit.Test;
 
