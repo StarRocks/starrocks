95bf56ec44712016d935d89bf48347e6570b14af
trueeyu <lxhhust350@qq.com>
2023-12-07 11:50:11 +0800

    [Refactor] Move BitmapValue from package spark-dpp to plugin-common (#36533)
    Signed-off-by: trueeyu <lxhhust350@qq.com>
    (cherry picked from commit 0f22dffbbda1390c51708823f42d3e0b6038ceb1)

--- fe/plugin-common/src/main/java/com/starrocks/types/BitmapValue.java
+++ fe/plugin-common/src/main/java/com/starrocks/types/BitmapValue.java
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-package com.starrocks.load.loadv2.dpp;
+package com.starrocks.types;
 
 import org.roaringbitmap.Util;
 
