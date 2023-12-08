24749edd789e36c9c7f7639e9aef6aa914661dc6
shuming.li <ming.moriarty@gmail.com>
2023-12-08 14:40:49 +0800

    fix bugs
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/MaterializedViewAnalyzer.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/MaterializedViewAnalyzer.java
@@ -157,7 +157,7 @@ public class MaterializedViewAnalyzer {
                 }
             }
 
-            if (table.isOlapView()) {
+            if (table.isView()) {
                 continue;
             }
 
