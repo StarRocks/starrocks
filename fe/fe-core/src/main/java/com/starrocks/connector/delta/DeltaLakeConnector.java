79d8ba807eee7bad266a3c135b11828fcd8c84f8
Letian Jiang <letian.jiang@outlook.com>
2023-12-08 16:09:56 +0800

    [BugFix][Cherry-Pick][Branch-3.1] Fix deltalake glue catalog (backport #31839) (#36687)
    Signed-off-by: Letian Jiang <letian.jiang@outlook.com>
    (cherry picked from commit 6d9cfde0177deb9ba956fec73422fda0e48b107b)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/connector/delta/DeltaLakeConnector.java

--- fe/fe-core/src/main/java/com/starrocks/connector/delta/DeltaLakeConnector.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/delta/DeltaLakeConnector.java
@@ -15,8 +15,6 @@
 
 package com.starrocks.connector.delta;
 
-import com.google.common.base.Preconditions;
-import com.starrocks.common.util.Util;
 import com.starrocks.connector.Connector;
 import com.starrocks.connector.ConnectorContext;
 import com.starrocks.connector.ConnectorMetadata;
@@ -46,6 +44,7 @@ public class DeltaLakeConnector implements Connector {
         HdfsEnvironment hdfsEnvironment = new HdfsEnvironment(cloudConfiguration);
         this.internalMgr = new DeltaLakeInternalMgr(catalogName, properties, hdfsEnvironment);
         this.metadataFactory = createMetadataFactory();
+<<<<<<< HEAD
         validate();
         onCreate();
     }
@@ -54,6 +53,9 @@ public class DeltaLakeConnector implements Connector {
         String hiveMetastoreUris = Preconditions.checkNotNull(properties.get(HIVE_METASTORE_URIS),
                 "%s must be set in properties when creating hive catalog", HIVE_METASTORE_URIS);
         Util.validateMetastoreUris(hiveMetastoreUris);
+=======
+        // TODO extract to ConnectorConfigFactory
+>>>>>>> 6d9cfde017 ([BugFix][Cherry-Pick][Branch-3.1] Fix deltalake glue catalog (backport #31839) (#36687))
     }
 
     @Override
@@ -67,7 +69,7 @@ public class DeltaLakeConnector implements Connector {
                 catalogName,
                 metastore,
                 internalMgr.getHiveMetastoreConf(),
-                properties.get(HIVE_METASTORE_URIS),
+                properties,
                 internalMgr.getHdfsEnvironment()
         );
     }
