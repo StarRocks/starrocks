3369de377b126441141b98dabac0da7180660b71
Youngwb <yangwenbo_mailbox@163.com>
2023-12-08 21:36:02 +0800

    fix ut
    Signed-off-by: Youngwb <yangwenbo_mailbox@163.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/SkewJoinTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/SkewJoinTest.java
@@ -146,12 +146,11 @@ public class SkewJoinTest extends PlanTestBase {
         String sql = "select struct_tbl.c0, struct_tbl.c2.a, t3.c2 from default_catalog.test.struct_tbl " +
                 "join[skew|test.struct_tbl.c1.a(1,2)] hive0.partitioned_db.t3 on c1.a = t3.c1 ";
         String sqlPlan = getFragmentPlan(sql);
-        assertCContains(sqlPlan, "  1:Project\n" +
+        assertCContains(sqlPlan, "1:Project\n" +
                 "  |  <slot 1> : 1: c0\n" +
-                "  |  <slot 10> : CASE WHEN 2: c1.a[false] IS NULL THEN round(rand() * 1000.0) WHEN 2: c1.a[false] " +
-                "IN (1, 2) THEN round(rand() * 1000.0) ELSE 0 END\n" +
-                "  |  <slot 19> : 2: c1.a[false]\n" +
-                "  |  <slot 21> : 3: c2.a[false]");
+                "  |  <slot 10> : CASE WHEN 2: c1.a IS NULL THEN round(rand() * 1000.0) WHEN 2: c1.a IN (1, 2) THEN round(rand() * 1000.0) ELSE 0 END\n" +
+                "  |  <slot 19> : 2: c1.a\n" +
+                "  |  <slot 21> : 3: c2.a");
     }
 
     @Test
