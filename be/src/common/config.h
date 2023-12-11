8bcadc78c81ca301e8de37c2af8b5c00ec3ed7e5
Zijie Lu <wslzj40@gmail.com>
2023-12-11 11:05:56 +0800

    [Feature] Evict lake persistent index files if disk is full
    Signed-off-by: Zijie Lu <wslzj40@gmail.com>

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -1036,8 +1036,8 @@ CONF_mInt64(max_allow_pindex_l2_num, "5");
 CONF_mInt64(pindex_major_compaction_num_threads, "0");
 // control the persistent index schedule compaction interval
 CONF_mInt64(pindex_major_compaction_schedule_interval_seconds, "15");
-// control the local persistent index in shared_data gc interval
-CONF_mInt64(pindex_shard_data_gc_interval_seconds, "18000"); // 5 hour
+// control the local persistent index in shared_data gc/evict interval
+CONF_mInt64(pindex_shared_data_gc_evict_interval_seconds, "18000"); // 5 hour
 // enable use bloom filter for pindex or not
 CONF_mBool(enable_pindex_filter, "true");
 // enable persistent index compression
@@ -1127,5 +1127,9 @@ CONF_mInt64(lake_vacuum_min_batch_delete_size, "1000");
 
 // TOPN RuntimeFilter parameters
 CONF_mInt32(desc_hint_split_range, "10");
+
+// If the local pk index file is older than this threshold
+// it may be evicted if the disk is full
+CONF_mInt64(lake_local_pk_index_unused_threshold_seconds, "86400"); // 1 day
 
 } // namespace starrocks::config
