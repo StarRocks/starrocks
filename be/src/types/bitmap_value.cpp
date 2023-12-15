1358bf0dc2cb8b085312aed2eb0270eac7ac938d
trueeyu <lxhhust350@qq.com>
2023-12-15 12:49:24 +0800

    Optimize the performance of bitmap_to_array
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/types/bitmap_value.cpp
+++ be/src/types/bitmap_value.cpp
@@ -862,9 +862,8 @@ void BitmapValue::to_array(std::vector<int64_t>* array) const {
         array->emplace_back(_sv);
         break;
     case BITMAP: {
-        for (unsigned long ptr_value : *_bitmap) {
-            array->emplace_back(ptr_value);
-        }
+        (*array).resize(_bitmap->cardinality());
+        _bitmap->toUint64Array((uint64_t*)(*array).data());
         break;
     }
     case SET:
