StarRocks/starrocks

commit adf159c9ed83d541982bdc55cbac8c7f4b543627

[Feature] Support a complete index metadata of GIN (#36765)
Why I'm doing:
The new index type, GIN, requires a metadata to accompany with all operations for index such as reading, writing, and compaction.

What I'm doing:
Supporting a complete metadata for GIN.

Signed-off-by: leorishdu <18771113323@163.com>

fe/fe-core/src/main/java/com/starrocks/alter/SchemaChangeHandler.java
@@ -2242,6 +2242,11 @@ public class SchemaChangeHandler extends AlterHandler {
         Set<String> newColset = Sets.newTreeSet(String.CASE_INSENSITIVE_ORDER);
         newColset.addAll(indexDef.getColumns());
         for (Index existedIdx : existedIndexes) {
+            if (existedIdx.getIndexId() >= newIndex.getIndexId()) {
+                throw new IllegalStateException(
+                        String.format("New index id %s should be lg than existed idx %s in OlapTable",
+                                newIndex.getIndexId(), existedIdx.getIndexId()));
+            }
             if (existedIdx.getIndexName().equalsIgnoreCase(indexDef.getIndexName())) {
                 throw new DdlException("index `" + indexDef.getIndexName() + "` already exist.");
             }
@@ -2261,7 +2266,8 @@ public class SchemaChangeHandler extends AlterHandler {
                 throw new DdlException("BITMAP column does not exist in table. invalid column: " + col);
             }
         }
-
+        Preconditions.checkArgument(newIndex.isValidIndex(),
+                "You should ensure that the indexId of the new type index was assigned when the table is OlapTable");
         newIndexes.add(newIndex);
     }
 

be/src/storage/CMakeLists.txt
@@ -60,6 +60,7 @@ add_library(Storage STATIC
     tablet_manager.cpp
     tablet_meta.cpp
     tablet_meta_manager.cpp
+    tablet_index.cpp
     tablet_schema.cpp
     tablet_schema_map.cpp
     tablet_updates.cpp

be/src/storage/metadata_util.cpp
@@ -22,6 +22,8 @@
 #include "storage/aggregate_type.h"
 #include "storage/olap_common.h"
 #include "storage/tablet_schema.h"
+#include "util/json_util.h"
+#include "utils.h"
 
 namespace starrocks {
 
@@ -239,6 +241,8 @@ Status convert_t_schema_to_pb_schema(const TTabletSchema& tablet_schema, uint32_
     // set column information
     uint32_t col_ordinal = 0;
     bool has_bf_columns = false;
+    std::unordered_map<std::string, ColumnPB*> column_map;
+
     uint32_t max_col_unique_id = 0;
     for (TColumn tcolumn : tablet_schema.columns) {
         convert_to_new_version(&tcolumn);
@@ -256,15 +260,54 @@ Status convert_t_schema_to_pb_schema(const TTabletSchema& tablet_schema, uint32_
 
         has_bf_columns |= column->is_bf_column();
 
-        if (tablet_schema.__isset.indexes) {
-            for (auto& index : tablet_schema.indexes) {
-                if (index.index_type == TIndexType::type::BITMAP) {
-                    DCHECK_EQ(index.columns.size(), 1);
-                    if (boost::iequals(tcolumn.column_name, index.columns[0])) {
-                        column->set_has_bitmap_index(true);
-                        break;
-                    }
+        column_map.emplace(boost::to_lower_copy(column->name()), column);
+    }
+
+    if (tablet_schema.__isset.indexes) {
+        for (auto& index : tablet_schema.indexes) {
+            TabletIndexPB* index_pb = schema->add_table_indices();
+            index_pb->set_index_id(index.index_id);
+            index_pb->set_index_name(index.index_name);
+            if (index.index_type == TIndexType::type::BITMAP) {
+                RETURN_IF(index.columns.size() != 1,
+                          Status::Cancelled("BITMAP index " + index.index_name +
+                                            " do not support to build with more than one column"));
+
+                index_pb->set_index_type(IndexType::BITMAP);
+                const auto& mit = column_map.find(boost::to_lower_copy(index.columns[0]));
+
+                // TODO: Fix abnormal scenes when index column can not be found
+                if (mit != column_map.end()) {
+                    mit->second->set_has_bitmap_index(true);
+                } else {
+                    LOG(WARNING) << "index column (" << index.columns[0] << ") can not be found in table columns";
                 }
+            } else if (index.index_type == TIndexType::type::GIN) {
+                RETURN_IF(index.columns.size() != 1,
+                          Status::Cancelled("GIN index " + index.index_name +
+                                            " do not support to build with more than one column"));
+                index_pb->set_index_type(IndexType::GIN);
+
+                const auto& index_col_name = index.columns[0];
+                const auto& mit = column_map.find(index_col_name);
+
+                // TODO: Fix abnormal scenes when index column can not be found
+                if (mit != column_map.end()) {
+                    index_pb->add_col_unique_id(mit->second->unique_id());
+                } else {
+                    LOG(WARNING) << "index " << index_col_name << " can not be found in table columns";
+                }
+
+                std::map<std::string, std::map<std::string, std::string>> properties_map;
+                properties_map.emplace(COMMON_PROPERTIES, index.common_properties);
+                properties_map.emplace(INDEX_PROPERTIES, index.index_properties);
+                properties_map.emplace(SEARCH_PROPERTIES, index.search_properties);
+                properties_map.emplace(EXTRA_PROPERTIES, index.extra_properties);
+                index_pb->set_index_properties(to_json(properties_map));
+            } else {
+                std::string index_type;
+                EnumToString(TIndexType, index.index_type, index_type);
+                return Status::Cancelled(strings::Substitute("Not supported index type $0", index_type));
             }
         }
     }

be/src/storage/metadata_util.h
@@ -26,6 +26,11 @@
 
 namespace starrocks {
 
+#define COMMON_PROPERTIES "common_properties"
+#define INDEX_PROPERTIES "index_properties"
+#define SEARCH_PROPERTIES "search_properties"
+#define EXTRA_PROPERTIES "extra_properties"
+
 class TTabletSchema;
 class TabletSchemaPB;
 enum RowsetTypePB : int;

be/src/storage/tablet_index.cpp
@@ -1,0 +1,115 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "storage/tablet_index.h"
+
+#include "storage/utils.h"
+#include "util/json_util.h"
+
+namespace starrocks {
+
+/******************************************************************
+ * TabletIndex
+ ******************************************************************/
+
+Status TabletIndex::init_from_thrift(const TOlapTableIndex& index, const TabletSchema& tablet_schema) {
+    _index_name = index.index_name;
+    _index_id = index.index_id;
+    // init col_unique_id in index at be side, since col_unique_id may be -1 at fe side
+    // get column unique id by name
+    std::vector<int32_t> col_unique_ids(index.columns.size());
+    for (size_t i = 0; i < index.columns.size(); i++) {
+        auto column_idx = tablet_schema.field_index(index.columns[i]);
+        col_unique_ids[i] = tablet_schema.column(column_idx).unique_id();
+    }
+    _col_unique_ids = std::move(col_unique_ids);
+
+    ASSIGN_OR_RETURN(_index_type, _convert_index_type_from_thrift(index.index_type));
+    FILL_INDEX_PARAM(common_properties)
+    FILL_INDEX_PARAM(index_properties)
+    FILL_INDEX_PARAM(search_properties)
+    FILL_INDEX_PARAM(extra_properties)
+    return Status::OK();
+}
+
+Status TabletIndex::init_from_pb(const TabletIndexPB& index) {
+    _index_id = index.index_id();
+    _index_name = index.index_name();
+    _index_type = index.index_type();
+    _col_unique_ids.clear();
+    for (const auto& col_unique_id : index.col_unique_id()) {
+        _col_unique_ids.push_back(col_unique_id);
+    }
+    if (index.has_index_properties()) {
+        const auto& serialized_prop_stream = index.index_properties();
+        std::map<std::string, std::map<std::string, std::string>> properties;
+        auto status = from_json(serialized_prop_stream, &properties);
+        if (status.ok()) {
+            FILL_INDEX_INTERNAL(common_properties)
+            FILL_INDEX_INTERNAL(index_properties)
+            FILL_INDEX_INTERNAL(search_properties)
+            FILL_INDEX_INTERNAL(extra_properties)
+        } else {
+            LOG(WARNING) << "parse json from serialized_prop_stream error, content is '" << serialized_prop_stream
+                         << "'";
+            return status;
+        }
+    }
+    return Status::OK();
+}
+
+void TabletIndex::to_schema_pb(TabletIndexPB* index) const {
+    index->set_index_id(_index_id);
+    index->set_index_name(_index_name);
+    index->clear_col_unique_id();
+    for (const auto& col_unique_id : _col_unique_ids) {
+        index->add_col_unique_id(col_unique_id);
+    }
+    index->set_index_type(_index_type);
+    std::map<std::string, std::map<std::string, std::string>> map;
+    FILL_INDEX_TO_MAP(common_properties, map)
+    FILL_INDEX_TO_MAP(index_properties, map)
+    FILL_INDEX_TO_MAP(search_properties, map)
+    FILL_INDEX_TO_MAP(extra_properties, map)
+
+    std::string meta_string = to_json(map);
+    index->set_index_properties(meta_string);
+}
+
+const std::string TabletIndex::properties_to_json() const {
+    std::map<std::string, std::map<std::string, std::string>> map;
+    FILL_INDEX_TO_MAP(common_properties, map)
+    FILL_INDEX_TO_MAP(index_properties, map)
+    FILL_INDEX_TO_MAP(search_properties, map)
+    FILL_INDEX_TO_MAP(extra_properties, map)
+
+    return to_json(map);
+}
+
+StatusOr<IndexType> TabletIndex::_convert_index_type_from_thrift(TIndexType::type index_type) {
+    switch (index_type) {
+    case TIndexType::BITMAP:
+        return IndexType::BITMAP;
+    case TIndexType::GIN:
+        return IndexType::GIN;
+    default:
+        // Handle other potential TIndexTypes or set a default value and/or log an error
+        std::string type_str;
+        EnumToString(TIndexType, index_type, type_str);
+        LOG(ERROR) << "Unhandled index type: " << type_str;
+        return Status::InternalError("Unexpect index type: " + type_str);
+    }
+}
+
+} // namespace starrocks

be/src/storage/tablet_index.h
@@ -1,0 +1,112 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "gen_cpp/Descriptors_types.h"
+#include "storage/tablet_schema.h"
+
+namespace starrocks {
+
+#define FILL_INDEX_PARAM(index_properties_name)              \
+    if (index.__isset.index_properties_name) {               \
+        for (const auto& kv : index.index_properties_name) { \
+            _##index_properties_name[kv.first] = kv.second;  \
+        }                                                    \
+    }
+
+#define FILL_INDEX_INTERNAL(properties_name)                                   \
+    if (auto it = properties.find(#properties_name); it != properties.end()) { \
+        _##properties_name = std::move(it->second);                            \
+        properties.erase(it);                                                  \
+    }
+
+#define FILL_INDEX_TO_MAP(properties_name, map_name) map_name.emplace(#properties_name, _##properties_name);
+
+class TabletSchema;
+
+// Tablet-level index metadata, can be utilized to build, query and compact indexes.
+class TabletIndex {
+public:
+    TabletIndex() = default;
+
+    Status init_from_thrift(const starrocks::TOlapTableIndex& index, const TabletSchema& tablet_schema);
+    Status init_from_pb(const TabletIndexPB& index);
+    void to_schema_pb(TabletIndexPB* index) const;
+    const std::string properties_to_json() const;
+
+    const int64_t index_id() const { return _index_id; }
+    const std::string& index_name() const { return _index_name; }
+    const IndexType index_type() const { return _index_type; }
+    const std::vector<int32_t>& col_unique_ids() const { return _col_unique_ids; }
+    const bool contains_column(int32_t column_uid) const {
+        for (const int32_t& uid : _col_unique_ids) {
+            if (uid == column_uid) return true;
+        }
+        return false;
+    }
+
+    const std::map<std::string, std::string>& common_properties() const { return _common_properties; }
+    const std::map<std::string, std::string>& index_properties() const { return _index_properties; }
+    const std::map<std::string, std::string>& search_properties() const { return _search_properties; }
+    const std::map<std::string, std::string>& extra_properties() const { return _extra_properties; }
+
+    // ================ for ut ================
+    void add_common_properties(const std::string& key, const std::string& value) {
+        _common_properties.insert(std::make_pair(key, value));
+    }
+    void add_index_properties(const std::string& key, const std::string& value) {
+        _index_properties.insert(std::make_pair(key, value));
+    }
+    void add_search_properties(const std::string& key, const std::string& value) {
+        _search_properties.insert(std::make_pair(key, value));
+    }
+    void add_extra_properties(const std::string& key, const std::string& value) {
+        _extra_properties.insert(std::make_pair(key, value));
+    }
+
+    int64_t mem_usage() const {
+        int64_t mem_usage = sizeof(TabletIndex);
+        for (const auto& p : _common_properties) {
+            mem_usage += sizeof(char) * (p.first.size() + p.second.size());
+        }
+        for (const auto& p : _index_properties) {
+            mem_usage += sizeof(char) * (p.first.size() + p.second.size());
+        }
+        for (const auto& p : _search_properties) {
+            mem_usage += sizeof(char) * (p.first.size() + p.second.size());
+        }
+        for (const auto& p : _extra_properties) {
+            mem_usage += sizeof(char) * (p.first.size() + p.second.size());
+        }
+        mem_usage += sizeof(int32_t) * _col_unique_ids.size();
+        mem_usage += sizeof(char) * _index_name.size();
+
+        return mem_usage;
+    }
+
+private:
+    int32_t _index_id = -1;
+    std::string _index_name;
+    IndexType _index_type = INDEX_UNKNOWN;
+    std::vector<int32_t> _col_unique_ids;
+    std::map<std::string, std::string> _common_properties;
+    std::map<std::string, std::string> _index_properties;
+    std::map<std::string, std::string> _search_properties;
+    std::map<std::string, std::string> _extra_properties;
+
+    StatusOr<IndexType> _convert_index_type_from_thrift(TIndexType::type index_type);
+};
+
+} // namespace starrocks

be/src/storage/tablet_schema.cpp
@@ -376,6 +376,16 @@ std::unique_ptr<TabletSchema> TabletSchema::copy(const std::shared_ptr<const Tab
     return t_ptr;
 }
 
+void TabletSchema::_fill_index_map(const TabletIndex& index) {
+    const auto idx_type = index.index_type();
+    if (_index_map_col_unique_id.count(idx_type) <= 0) {
+        auto col_unique_id_set = std::make_shared<std::unordered_set<int32_t>>();
+        _index_map_col_unique_id.emplace(idx_type, col_unique_id_set);
+    }
+    std::for_each(index.col_unique_ids().begin(), index.col_unique_ids().end(),
+                  [&](int32_t uid) { _index_map_col_unique_id[idx_type]->insert(uid); });
+}
+
 void TabletSchema::_init_schema() const {
     starrocks::Fields fields;
     for (ColumnId cid = 0; cid < num_columns(); ++cid) {
@@ -412,6 +422,8 @@ void TabletSchema::_init_from_pb(const TabletSchemaPB& schema) {
     _keys_type = static_cast<uint8_t>(schema.keys_type());
     _num_key_columns = 0;
     _num_columns = 0;
+    _indexes.clear();
+    _index_map_col_unique_id.clear();
     _cols.clear();
     _compression_type = schema.compression_type();
     for (auto& column_pb : schema.column()) {
@@ -423,6 +435,13 @@ void TabletSchema::_init_from_pb(const TabletSchemaPB& schema) {
         }
         _unique_id_to_index[column.unique_id()] = _num_columns;
         _num_columns++;
+    }
+
+    for (auto& index_pb : schema.table_indices()) {
+        TabletIndex index;
+        WARN_IF_ERROR(index.init_from_pb(index_pb), "Init from index pb error! ");
+        _indexes.emplace_back(index);
+        _fill_index_map(index);
     }
 
     // There are three conditions:
@@ -552,6 +571,46 @@ void TabletSchema::to_schema_pb(TabletSchemaPB* tablet_schema_pb) const {
     tablet_schema_pb->mutable_sort_key_idxes()->Add(_sort_key_idxes.begin(), _sort_key_idxes.end());
     tablet_schema_pb->mutable_sort_key_unique_ids()->Add(_sort_key_uids.begin(), _sort_key_uids.end());
     tablet_schema_pb->set_schema_version(_schema_version);
+    for (auto& index : _indexes) {
+        auto* tablet_index_pb = tablet_schema_pb->add_table_indices();
+        index.to_schema_pb(tablet_index_pb);
+    }
+}
+
+Status TabletSchema::get_indexes_for_column(int32_t col_unique_id,
+                                            std::unordered_map<IndexType, TabletIndex>* res) const {
+    RETURN_IF(res == nullptr, Status::InternalError("Index map should not be nullptr"));
+    for (const auto& index : _indexes) {
+        if (index.col_unique_ids().size() == 1) {
+            if (index.col_unique_ids()[0] == col_unique_id) {
+                res->emplace(index.index_type(), index);
+            }
+        } else if (index.col_unique_ids().size() > 1) {
+            // TODO: implement multi-column index
+            return Status::NotSupported("Multi-column index is not supported for now. ");
+        }
+    }
+    return Status::OK();
+}
+
+Status TabletSchema::get_indexes_for_column(int32_t col_unique_id, IndexType index_type,
+                                            std::shared_ptr<TabletIndex>& res) const {
+    std::unordered_map<IndexType, TabletIndex> map_res;
+    RETURN_IF_ERROR(get_indexes_for_column(col_unique_id, &map_res));
+    if (!map_res.empty()) {
+        const auto& it = map_res.find(index_type);
+        if (it != map_res.end()) {
+            res = std::make_shared<TabletIndex>(it->second);
+        }
+    }
+    return Status::OK();
+}
+
+bool TabletSchema::has_index(int32_t col_unique_id, IndexType index_type) const {
+    if (auto it = _index_map_col_unique_id.find(index_type); it != _index_map_col_unique_id.end()) {
+        return it->second->count(col_unique_id) > 0;
+    }
+    return false;
 }
 
 size_t TabletSchema::estimate_row_size(size_t variable_len) const {
@@ -675,6 +734,17 @@ std::string TabletSchema::debug_string() const {
        << ",num_short_key_columns=" << _num_short_key_columns << ",num_rows_per_row_block=" << _num_rows_per_row_block
        << ",next_column_unique_id=" << _next_column_unique_id << ",has_bf_fpp=" << _has_bf_fpp << ",bf_fpp=" << _bf_fpp;
     return ss.str();
+}
+
+int64_t TabletSchema::mem_usage() const {
+    int64_t mem_usage = sizeof(TabletSchema);
+    for (const auto& col : _cols) {
+        mem_usage += col.mem_usage();
+    }
+    for (const auto& index : _indexes) {
+        mem_usage += index.mem_usage();
+    }
+    return mem_usage;
 }
 
 } // namespace starrocks

be/src/storage/tablet_schema.h
@@ -40,9 +40,11 @@
 #include <vector>
 
 #include "column/chunk.h"
+#include "gen_cpp/Descriptors_types.h"
 #include "gen_cpp/olap_file.pb.h"
 #include "storage/aggregate_type.h"
 #include "storage/olap_define.h"
+#include "storage/tablet_index.h"
 #include "storage/type_utils.h"
 #include "storage/types.h"
 #include "util/c_string.h"
@@ -229,6 +231,8 @@ private:
 bool operator==(const TabletColumn& a, const TabletColumn& b);
 bool operator!=(const TabletColumn& a, const TabletColumn& b);
 
+class TabletIndex;
+
 class TabletSchema {
 public:
     using SchemaId = int64_t;
@@ -306,13 +310,7 @@ public:
 
     std::string debug_string() const;
 
-    int64_t mem_usage() const {
-        int64_t mem_usage = sizeof(TabletSchema);
-        for (const auto& col : _cols) {
-            mem_usage += col.mem_usage();
-        }
-        return mem_usage;
-    }
+    int64_t mem_usage() const;
 
     bool shared() const { return _schema_map != nullptr; }
 
@@ -320,6 +318,11 @@ public:
 
     Status build_current_tablet_schema(int64_t index_id, int32_t version, const POlapTableIndexSchema& index,
                                        const std::shared_ptr<const TabletSchema>& ori_tablet_schema);
+
+    const std::vector<TabletIndex>* indexes() const { return &_indexes; }
+    Status get_indexes_for_column(int32_t col_unique_id, std::unordered_map<IndexType, TabletIndex>* res) const;
+    Status get_indexes_for_column(int32_t col_unique_id, IndexType index_type, std::shared_ptr<TabletIndex>& res) const;
+    bool has_index(int32_t col_unique_id, IndexType index_type) const;
 
 private:
     friend class SegmentReaderWriterTest;
@@ -332,11 +335,15 @@ private:
     void _init_from_pb(const TabletSchemaPB& schema);
 
     void _init_schema() const;
+    void _fill_index_map(const TabletIndex& index);
 
     SchemaId _id = invalid_id();
     TabletSchemaMap* _schema_map = nullptr;
 
     double _bf_fpp = 0;
+
+    std::vector<TabletIndex> _indexes;
+    std::unordered_map<IndexType, std::shared_ptr<std::unordered_set<int32_t>>> _index_map_col_unique_id;
 
     std::vector<TabletColumn> _cols;
     size_t _num_rows_per_row_block = 0;

commit ac5f9b319391693b56b7a3f38d02384d32ea4cf6

[BugFix] create bitmap index failed if table already has another bitmap (#37116)
Why I'm doing:
If creating a Bitmap index for a table that already has a Bitmap index, it will fail.
This problem is introduced by pr(#36765) which introduced the meta data for GIN(inverted index).

This pr add a index id for every index. Bitmap index always has id = -1 and GIN has id id >= 0
In the process of CREATE INDEX, it will check the new added index' id is larger than the existed index'id
or not. This is make sense for GIN but not for BITMAP(always -1).
If we add two bitmap indexes consecutively, it will trigger a failure cause by (-1 > -1) is FALSE.

What I'm doing:
Check the id for GIN only

Signed-off-by: srlch <linzichao@starrocks.com>

fe/fe-core/src/main/java/com/starrocks/alter/SchemaChangeHandler.java
@@ -2242,7 +2242,11 @@ public class SchemaChangeHandler extends AlterHandler {
         Set<String> newColset = Sets.newTreeSet(String.CASE_INSENSITIVE_ORDER);
         newColset.addAll(indexDef.getColumns());
         for (Index existedIdx : existedIndexes) {
-            if (existedIdx.getIndexId() >= newIndex.getIndexId()) {
+            // check the index id only if the index is CompatibleIndex(GIN)
+            // Bitmap index's id is always be -1
+            if (IndexDef.IndexType.isCompatibleIndex(existedIdx.getIndexType()) &&
+                    IndexDef.IndexType.isCompatibleIndex(newIndex.getIndexType()) &&
+                        existedIdx.getIndexId() >= newIndex.getIndexId()) {
                 throw new IllegalStateException(
                         String.format("New index id %s should be lg than existed idx %s in OlapTable",
                                 newIndex.getIndexId(), existedIdx.getIndexId()));

test/sql/test_inverted_index/R/test_inverted_index
@@ -1,0 +1,99 @@
+-- name: test_basic_create_index
+CREATE TABLE `t_test_basic_create_index_pk` (
+  `id1` bigint(20) NOT NULL COMMENT "",
+  `id2` bigint(20) NOT NULL COMMENT "",
+  `id3` bigint(20) NOT NULL COMMENT ""
+) ENGINE=OLAP 
+PRIMARY KEY(`id1`)
+DISTRIBUTED BY HASH(`id1`) BUCKETS 1 
+PROPERTIES (
+"replication_num" = "1",
+"in_memory" = "false",
+"enable_persistent_index" = "false",
+"replicated_storage" = "true",
+"compression" = "LZ4"
+);
+-- result:
+-- !result
+CREATE INDEX index_1 ON t_test_basic_create_index_pk (id2) USING BITMAP;
+-- result:
+-- !result
+function: wait_alter_table_finish()
+-- result:
+None
+-- !result
+CREATE INDEX index_2 ON t_test_basic_create_index_pk (id3) USING BITMAP;
+-- result:
+-- !result
+function: wait_alter_table_finish()
+-- result:
+None
+-- !result
+CREATE TABLE `t_test_basic_create_index_dup` (
+  `id1` bigint(20) NOT NULL COMMENT "",
+  `id2` bigint(20) NOT NULL COMMENT "",
+  `id3` bigint(20) NOT NULL COMMENT "",
+  `id4` string NOT NULL COMMENT "",
+  `id5` string NOT NULL COMMENT "",
+  `id6` bigint(20) NOT NULL COMMENT "",
+  `id7` string NOT NULL COMMENT ""
+) ENGINE=OLAP 
+DUPLICATE KEY(`id1`)
+DISTRIBUTED BY HASH(`id1`) BUCKETS 1 
+PROPERTIES (
+"replication_num" = "1",
+"in_memory" = "false",
+"enable_persistent_index" = "false",
+"replicated_storage" = "true",
+"compression" = "LZ4"
+);
+-- result:
+-- !result
+CREATE INDEX index_1 ON t_test_basic_create_index_dup (id2) USING BITMAP;
+-- result:
+-- !result
+function: wait_alter_table_finish()
+-- result:
+None
+-- !result
+CREATE INDEX index_2 ON t_test_basic_create_index_dup (id3) USING BITMAP;
+-- result:
+-- !result
+function: wait_alter_table_finish()
+-- result:
+None
+-- !result
+CREATE INDEX index_3 ON t_test_basic_create_index_dup (id4) USING GIN;
+-- result:
+-- !result
+function: wait_alter_table_finish()
+-- result:
+None
+-- !result
+CREATE INDEX index_4 ON t_test_basic_create_index_dup (id5) USING GIN;
+-- result:
+-- !result
+function: wait_alter_table_finish()
+-- result:
+None
+-- !result
+CREATE INDEX index_5 ON t_test_basic_create_index_dup (id6) USING BITMAP;
+-- result:
+-- !result
+function: wait_alter_table_finish()
+-- result:
+None
+-- !result
+CREATE INDEX index_6 ON t_test_basic_create_index_dup (id7) USING GIN;
+-- result:
+-- !result
+function: wait_alter_table_finish()
+-- result:
+None
+-- !result
+DROP TABLE t_test_basic_create_index_pk;
+-- result:
+-- !result
+DROP TABLE t_test_basic_create_index_dup;
+-- result:
+-- !result

test/sql/test_inverted_index/T/test_inverted_index
@@ -1,0 +1,62 @@
+-- name: test_basic_create_index
+CREATE TABLE `t_test_basic_create_index_pk` (
+  `id1` bigint(20) NOT NULL COMMENT "",
+  `id2` bigint(20) NOT NULL COMMENT "",
+  `id3` bigint(20) NOT NULL COMMENT ""
+) ENGINE=OLAP 
+PRIMARY KEY(`id1`)
+DISTRIBUTED BY HASH(`id1`) BUCKETS 1 
+PROPERTIES (
+"replication_num" = "1",
+"in_memory" = "false",
+"enable_persistent_index" = "false",
+"replicated_storage" = "true",
+"compression" = "LZ4"
+);
+
+CREATE INDEX index_1 ON t_test_basic_create_index_pk (id2) USING BITMAP;
+function: wait_alter_table_finish()
+
+CREATE INDEX index_2 ON t_test_basic_create_index_pk (id3) USING BITMAP;
+function: wait_alter_table_finish()
+
+
+CREATE TABLE `t_test_basic_create_index_dup` (
+  `id1` bigint(20) NOT NULL COMMENT "",
+  `id2` bigint(20) NOT NULL COMMENT "",
+  `id3` bigint(20) NOT NULL COMMENT "",
+  `id4` string NOT NULL COMMENT "",
+  `id5` string NOT NULL COMMENT "",
+  `id6` bigint(20) NOT NULL COMMENT "",
+  `id7` string NOT NULL COMMENT ""
+) ENGINE=OLAP 
+DUPLICATE KEY(`id1`)
+DISTRIBUTED BY HASH(`id1`) BUCKETS 1 
+PROPERTIES (
+"replication_num" = "1",
+"in_memory" = "false",
+"enable_persistent_index" = "false",
+"replicated_storage" = "true",
+"compression" = "LZ4"
+);
+
+CREATE INDEX index_1 ON t_test_basic_create_index_dup (id2) USING BITMAP;
+function: wait_alter_table_finish()
+
+CREATE INDEX index_2 ON t_test_basic_create_index_dup (id3) USING BITMAP;
+function: wait_alter_table_finish()
+
+CREATE INDEX index_3 ON t_test_basic_create_index_dup (id4) USING GIN;
+function: wait_alter_table_finish()
+
+CREATE INDEX index_4 ON t_test_basic_create_index_dup (id5) USING GIN;
+function: wait_alter_table_finish()
+
+CREATE INDEX index_5 ON t_test_basic_create_index_dup (id6) USING BITMAP;
+function: wait_alter_table_finish()
+
+CREATE INDEX index_6 ON t_test_basic_create_index_dup (id7) USING GIN;
+function: wait_alter_table_finish()
+
+DROP TABLE t_test_basic_create_index_pk;
+DROP TABLE t_test_basic_create_index_dup;