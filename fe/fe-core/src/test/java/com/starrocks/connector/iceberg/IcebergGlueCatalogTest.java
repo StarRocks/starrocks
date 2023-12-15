03ba896c8a1a506c6f521305024af1ce9253bf14
Letian Jiang <letian.jiang@outlook.com>
2023-12-15 21:35:54 +0800

    [Feature] Support Iceberg Hadoop catalog (#31733)
    Support iceberg hadoop catalog. https://iceberg.apache.org/javadoc/latest/org/apache/iceberg/hadoop/HadoopCatalog.html
    
    ```sql
    CREATE EXTERNAL CATALOG `ice_hadoop`
    PROPERTIES (
    "type" = "iceberg",
    "iceberg.catalog.type" = "hadoop",
    "iceberg.catalog.warehouse" = "s3://path/to/warehouse"
    )
    ```
    
    User should make sure the warehouse directory exists.
    
    Signed-off-by: Letian Jiang <letian.jiang@outlook.com>
    Co-authored-by: imay <buaa.zhaoc@gmail.com>

--- fe/fe-core/src/test/java/com/starrocks/connector/iceberg/IcebergGlueCatalogTest.java
+++ fe/fe-core/src/test/java/com/starrocks/connector/iceberg/IcebergGlueCatalogTest.java
@@ -15,10 +15,13 @@
 
 package com.starrocks.connector.iceberg;
 
+import com.google.common.collect.ImmutableList;
 import com.starrocks.connector.iceberg.glue.IcebergGlueCatalog;
 import mockit.Expectations;
 import mockit.Mocked;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.iceberg.aws.glue.GlueCatalog;
+import org.apache.iceberg.catalog.Namespace;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -30,12 +33,12 @@ import java.util.Map;
 public class IcebergGlueCatalogTest {
 
     @Test
-    public void testListAllDatabases(@Mocked IcebergGlueCatalog glueCatalog) {
+    public void testListAllDatabases(@Mocked GlueCatalog glueCatalog) {
         new Expectations() {
             {
-                glueCatalog.listAllDatabases();
-                result = Arrays.asList("db1", "db2");
-                minTimes = 0;
+                glueCatalog.listNamespaces();
+                result = ImmutableList.of(Namespace.of("db1"), Namespace.of("db2"));
+                times = 1;
             }
         };
 
