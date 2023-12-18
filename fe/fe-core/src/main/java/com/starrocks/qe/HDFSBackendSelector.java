02ba2e767bf20c8dd91676313035cf4aa18c3173
zombee0 <ewang2027@gmail.com>
2023-12-18 14:08:48 +0800

    [Enhancement]enlarge default virtual node number be more friendly to cache (backport #36101) (#36937)
    Signed-off-by: zombee0 <ewang2027@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/HDFSBackendSelector.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/HDFSBackendSelector.java
@@ -90,7 +90,7 @@ public class HDFSBackendSelector implements BackendSelector {
     // After testing, this value can ensure that the scan range size assigned to each BE is as uniform as possible,
     // and the largest scan data is not more than 1.1 times of the average value
     private final double kMaxImbalanceRatio = 1.1;
-    public static final int CONSISTENT_HASH_RING_VIRTUAL_NUMBER = 32;
+    public static final int CONSISTENT_HASH_RING_VIRTUAL_NUMBER = 128;
 
     class HdfsScanRangeHasher {
         String basePath;
