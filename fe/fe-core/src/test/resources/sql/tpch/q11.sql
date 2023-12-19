d9cdf2a915f45697df0f414be8ae466a52f2b8ee
Seaven <seaven_7@qq.com>
2023-12-19 19:08:39 +0800

    [Refactor] remove duplicate tpch ut sql
    Signed-off-by: Seaven <seaven_7@qq.com>

--- fe/fe-core/src/test/resources/sql/tpch/q11.sql
+++ fe/fe-core/src/test/resources/sql/tpch/q11.sql
@@ -1,31 +1,3 @@
-[sql]
-select
-    ps_partkey,
-    sum(ps_supplycost * ps_availqty) as value
-from
-    partsupp,
-    supplier,
-    nation
-where
-    ps_suppkey = s_suppkey
-  and s_nationkey = n_nationkey
-  and n_name = 'PERU'
-group by
-    ps_partkey having
-    sum(ps_supplycost * ps_availqty) > (
-    select
-    sum(ps_supplycost * ps_availqty) * 0.0001000000
-    from
-    partsupp,
-    supplier,
-    nation
-    where
-    ps_suppkey = s_suppkey
-                  and s_nationkey = n_nationkey
-                  and n_name = 'PERU'
-    )
-order by
-    value desc ;
 [result]
 TOP-N (order by [[21: sum DESC NULLS LAST]])
     TOP-N (order by [[21: sum DESC NULLS LAST]])
--- fe/fe-core/src/test/resources/sql/tpchcost/q11.sql
+++ fe/fe-core/src/test/resources/sql/tpchcost/q11.sql
@@ -1,31 +1,3 @@
-[sql]
-select
-    ps_partkey,
-    sum(ps_supplycost * ps_availqty) as value
-from
-    partsupp,
-    supplier,
-    nation
-where
-    ps_suppkey = s_suppkey
-  and s_nationkey = n_nationkey
-  and n_name = 'PERU'
-group by
-    ps_partkey having
-    sum(ps_supplycost * ps_availqty) > (
-    select
-    sum(ps_supplycost * ps_availqty) * 0.0001000000
-    from
-    partsupp,
-    supplier,
-    nation
-    where
-    ps_suppkey = s_suppkey
-                  and s_nationkey = n_nationkey
-                  and n_name = 'PERU'
-    )
-order by
-    value desc ;
 [fragment]
 PLAN FRAGMENT 0
 OUTPUT EXPRS:1: PS_PARTKEY | 21: sum
