597de91134c01e8845f1504571a0824a314d23f2
Alex Zhu <zhuming9011@gmail.com>
2023-12-19 15:40:32 +0800

    [Refactor] Get tablet schema from metadata when doing shcema changes in shared data mode (#36825)
    Signed-off-by: Alex Zhu <zhuming9011@gmail.com>

--- be/src/storage/lake/schema_change.h
+++ be/src/storage/lake/schema_change.h
@@ -17,12 +17,14 @@
 #include "common/statusor.h"
 #include "gen_cpp/AgentService_types.h"
 #include "gen_cpp/olap_file.pb.h"
-#include "storage/lake/tablet.h"
 #include "storage/schema_change_utils.h"
 
 namespace starrocks::lake {
 
 class TabletManager;
+class VersionedTablet;
+class TxnLogPB_OpSchemaChange;
+struct SchemaChangeParams;
 
 class SchemaChangeHandler {
 public:
@@ -36,18 +38,6 @@ public:
     DISALLOW_COPY_AND_MOVE(SchemaChangeHandler);
 
 private:
-    struct SchemaChangeParams {
-        Tablet* base_tablet;
-        Tablet* new_tablet;
-        int64_t version;
-        int64_t txn_id;
-        MaterializedViewParamMap materialized_params_map;
-        std::unique_ptr<TExpr> where_expr;
-        bool sc_sorting = false;
-        bool sc_directly = false;
-        std::unique_ptr<ChunkChanger> chunk_changer = nullptr;
-    };
-
     Status do_process_alter_tablet(const TAlterTabletReqV2& request);
     Status convert_historical_rowsets(const SchemaChangeParams& sc_params, TxnLogPB_OpSchemaChange* op_schema_change);
 
