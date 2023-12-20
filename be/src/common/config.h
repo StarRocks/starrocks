949ab1e0d606c6c5b2781290d2e3980680a7c022
zhangqiang <qiangzh95@gmail.com>
2023-12-20 17:04:26 +0800

    disable pindex rebuild by default
    Signed-off-by: zhangqiang <qiangzh95@gmail.com>

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -988,7 +988,7 @@ CONF_mInt64(pindex_major_compaction_schedule_interval_seconds, "15");
 CONF_mBool(enable_pindex_compression, "false");
 
 // If primary compaction pick all rowsets, we could rebuild pindex directly and skip read from index.
-CONF_mBool(enable_pindex_rebuild_in_compaction, "true");
+CONF_mBool(enable_pindex_rebuild_in_compaction, "false");
 
 // Used by query cache, cache entries are evicted when it exceeds its capacity(500MB in default)
 CONF_Int64(query_cache_capacity, "536870912");
