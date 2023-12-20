7cf98383d4748a61047cccb2d87c265a734277d8
trueeyu <lxhhust350@qq.com>
2023-12-20 11:15:34 +0800

    [Enhancement] Optimize the performance of bitmap_to_array (#37114) (#37387)
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/types/bitmap_value.cpp
+++ be/src/types/bitmap_value.cpp
@@ -38,6 +38,7 @@
 #include "types/bitmap_value_detail.h"
 #include "util/defer_op.h"
 #include "util/phmap/phmap.h"
+#include "util/raw_container.h"
 
 namespace starrocks {
 
@@ -862,9 +863,8 @@ void BitmapValue::to_array(std::vector<int64_t>* array) const {
         array->emplace_back(_sv);
         break;
     case BITMAP: {
-        for (unsigned long ptr_value : *_bitmap) {
-            array->emplace_back(ptr_value);
-        }
+        raw::make_room(array, _bitmap->cardinality());
+        _bitmap->toUint64Array((uint64_t*)(*array).data());
         break;
     }
     case SET:
