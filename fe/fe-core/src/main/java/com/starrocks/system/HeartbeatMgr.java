0db3d995d689a99767538d85deee28e825f16e75
gengjun-git <gengjun@starrocks.com>
2023-12-20 16:23:14 +0800

    [Enhancement] Optimize heartbeat interval (#37187)
    Why I'm doing:
    In the case of machine shutdown, the heartbeat interval will be 2 * `Config.heartbeat_timeout_second`. As a result, the time to detect BE non-alive becomes longer.
    
    What I'm doing:
    Change the sleep time to (heartbeat_timeout - timeUsed), so that the frequency of calling the heartbeat rpc can be stabilized at `Config.heartbeat_timeout_second`.
    
    Signed-off-by: gengjun-git <gengjun@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/system/HeartbeatMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/system/HeartbeatMgr.java
@@ -124,6 +124,7 @@ public class HeartbeatMgr extends LeaderDaemon {
 
         List<Future<HeartbeatResponse>> hbResponses = Lists.newArrayList();
 
+        long startTime = System.currentTimeMillis();
         // send backend heartbeat
         for (Backend backend : idToBackendRef.values()) {
             BackendHeartbeatHandler handler = new BackendHeartbeatHandler(backend);
@@ -193,6 +194,10 @@ public class HeartbeatMgr extends LeaderDaemon {
 
         // write edit log
         GlobalStateMgr.getCurrentState().getEditLog().logHeartbeat(hbPackage);
+
+        // set sleep time to (heartbeat_timeout - timeUsed),
+        // so that the frequency of calling the heartbeat rpc can be stabilized at heartbeat_timeout
+        setInterval(Math.max(1L, Config.heartbeat_timeout_second * 1000L - (System.currentTimeMillis() - startTime)));
 
         ClientPool.beHeartbeatPool.setTimeoutMs(Config.heartbeat_timeout_second * 1000);
         ClientPool.brokerHeartbeatPool.setTimeoutMs(Config.heartbeat_timeout_second * 1000);
