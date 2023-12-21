8e727512a90fb8bd6bf8531674342d1373ddc6c3
fgump <smartlxh@gmail.com>
2023-12-21 13:03:06 +0800

    [Enhancement]Support delete txn_log when publish batch succeed (#35924)
    Signed-off-by: smartlxh <smartlxh@gmail.com>
    (cherry picked from commit 94fae9d4eef83973525f76f345ba71a2064f8adb)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/transaction/PublishVersionDaemon.java

--- fe/fe-core/src/test/java/com/starrocks/pseudocluster/PseudoBackend.java
+++ fe/fe-core/src/test/java/com/starrocks/pseudocluster/PseudoBackend.java
@@ -34,6 +34,8 @@ import com.starrocks.proto.DeleteDataRequest;
 import com.starrocks.proto.DeleteDataResponse;
 import com.starrocks.proto.DeleteTabletRequest;
 import com.starrocks.proto.DeleteTabletResponse;
+import com.starrocks.proto.DeleteTxnLogRequest;
+import com.starrocks.proto.DeleteTxnLogResponse;
 import com.starrocks.proto.DropTableRequest;
 import com.starrocks.proto.DropTableResponse;
 import com.starrocks.proto.ExecuteCommandRequestPB;
@@ -1075,6 +1077,11 @@ public class PseudoBackend {
 
         @Override
         public Future<DeleteTabletResponse> deleteTablet(DeleteTabletRequest request) {
+            return CompletableFuture.completedFuture(null);
+        }
+
+        @Override
+        public Future<DeleteTxnLogResponse> deleteTxnLog(DeleteTxnLogRequest request) {
             return CompletableFuture.completedFuture(null);
         }
 
