553d1143877d8628fb79e79d3fad99837b4e9a23
Felix Li <astralidea@163.com>
2023-12-21 14:41:56 +0800

    [Enhancement] Querying for non-existing partitions should report an error (backport #36950) (#37507)
    Signed-off-by: Astralidea <astralidea@163.com>
    (cherry picked from commit 737d2928e45341feadf924d20dfb211dc23866e5)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/sql/analyzer/QueryAnalyzer.java

--- fe/fe-core/src/main/java/com/starrocks/catalog/Table.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/Table.java
@@ -503,6 +503,9 @@ public class Table extends MetaObject implements Writable, GsonPostProcessable {
     public Partition getPartition(String partitionName) {
         return null;
     }
+    public Partition getPartition(String partitionName, boolean isTempPartition) {
+        return null;
+    }
 
     public Partition getPartition(long partitionId) {
         return null;
--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/QueryAnalyzer.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/QueryAnalyzer.java
@@ -32,6 +32,7 @@ import com.starrocks.catalog.Database;
 import com.starrocks.catalog.Function;
 import com.starrocks.catalog.HiveTable;
 import com.starrocks.catalog.OlapTable;
+import com.starrocks.catalog.Partition;
 import com.starrocks.catalog.Resource;
 import com.starrocks.catalog.Table;
 import com.starrocks.catalog.TableFunction;
@@ -49,6 +50,11 @@ import com.starrocks.sql.ast.ExceptRelation;
 import com.starrocks.sql.ast.FieldReference;
 import com.starrocks.sql.ast.IntersectRelation;
 import com.starrocks.sql.ast.JoinRelation;
+<<<<<<< HEAD
+=======
+import com.starrocks.sql.ast.NormalizedTableFunctionRelation;
+import com.starrocks.sql.ast.PartitionNames;
+>>>>>>> 737d2928e4 ([Enhancement] Querying for non-existing partitions should report an error (backport #36950) (#37507))
 import com.starrocks.sql.ast.QueryRelation;
 import com.starrocks.sql.ast.QueryStatement;
 import com.starrocks.sql.ast.Relation;
@@ -841,6 +847,22 @@ public class QueryAnalyzer {
                             || ((OlapTable) table).getState() == OlapTable.OlapTableState.RESTORE_WITH_LOAD)) {
                 ErrorReport.reportAnalysisException(ErrorCode.ERR_BAD_TABLE_STATE, "RESTORING");
             }
+
+            PartitionNames partitionNamesObject = tableRelation.getPartitionNames();
+            if (partitionNamesObject != null && table.isNativeTable()) {
+                List<String> partitionNames = partitionNamesObject.getPartitionNames();
+                if (partitionNames != null) {
+                    boolean isTemp = partitionNamesObject.isTemp();
+                    for (String partitionName : partitionNames) {
+                        Partition partition = table.getPartition(partitionName, isTemp);
+                        if (partition == null) {
+                            throw new SemanticException("Unknown partition '%s' in table '%s'", partitionName,
+                                    table.getName());
+                        }
+                    }
+                }
+            }
+
             return table;
         } catch (AnalysisException e) {
             throw new SemanticException(e.getMessage());
