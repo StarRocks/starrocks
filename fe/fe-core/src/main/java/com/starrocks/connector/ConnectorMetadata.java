1985fa2d3d234214c33dabe98d795461628bbc71
zengqingdong <zengqingdong.zqd@alibaba-inc.com>
2023-12-21 16:55:16 +0800

    [Feature] support refresh paimon mv by partition.
    Signed-off-by: zengqingdong <zengqingdong.zqd@alibaba-inc.com>

--- fe/fe-core/src/main/java/com/starrocks/connector/ConnectorMetadata.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/ConnectorMetadata.java
@@ -283,5 +283,9 @@ public interface ConnectorMetadata {
     default CloudConfiguration getCloudConfiguration() {
         throw new StarRocksConnectorException("This connector doesn't support getting cloud configuration");
     }
+
+    default List<PartitionInfo> getChangedPartitionInfo(Table table, long mvSnapShotID) {
+        return Lists.newArrayList();
+    }
 }
 
--- fe/fe-core/src/main/java/com/starrocks/scheduler/PartitionBasedMvRefreshProcessor.java
+++ fe/fe-core/src/main/java/com/starrocks/scheduler/PartitionBasedMvRefreshProcessor.java
@@ -966,7 +966,8 @@ public class PartitionBasedMvRefreshProcessor extends BaseTaskRunProcessor {
      */
     private boolean unSupportRefreshByPartition(Table table) {
         return !table.isOlapTableOrMaterializedView() && !table.isHiveTable()
-                && !table.isJDBCTable() && !table.isIcebergTable() && !table.isCloudNativeTable();
+                && !table.isJDBCTable() && !table.isIcebergTable() && !table.isCloudNativeTable()
+                && !table.isPaimonTable();
     }
 
     /**
@@ -1867,7 +1868,8 @@ public class PartitionBasedMvRefreshProcessor extends BaseTaskRunProcessor {
                 partitionInfos.put(partition.getName(), basePartitionInfo);
             }
             return partitionInfos;
-        } else if (baseTable.isHiveTable() || baseTable.isIcebergTable() || baseTable.isJDBCTable()) {
+        } else if (baseTable.isHiveTable() || baseTable.isIcebergTable() || baseTable.isJDBCTable() ||
+                baseTable.isPaimonTable()) {
             return getSelectedPartitionInfos(baseTable, Lists.newArrayList(refreshedPartitionNames),
                     baseTableInfo);
         } else {
