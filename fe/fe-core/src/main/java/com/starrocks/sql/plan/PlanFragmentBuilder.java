bd41be4668cc06484ed2ff7e0bbca6e7fa832055
zihe.liu <ziheliu1024@gmail.com>
2023-12-22 17:14:18 +0800

    [BugFix] Fix multi local shuffle aggregations on single BE (#37645)
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>
    (cherry picked from commit a95215aa84db6e7e9aa0b3fb0e2a658d06122f02)

--- fe/fe-core/src/main/java/com/starrocks/sql/plan/PlanFragmentBuilder.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/plan/PlanFragmentBuilder.java
@@ -1618,7 +1618,7 @@ public class PlanFragmentBuilder {
             PlanFragment originalInputFragment = visit(optExpr.inputAt(0), context);
 
             PlanFragment inputFragment = removeExchangeNodeForLocalShuffleAgg(originalInputFragment, context);
-            boolean withLocalShuffle = inputFragment != originalInputFragment;
+            boolean withLocalShuffle = inputFragment != originalInputFragment || inputFragment.isWithLocalShuffle();
 
             Map<ColumnRefOperator, CallOperator> aggregations = node.getAggregations();
             List<ColumnRefOperator> groupBys = node.getGroupBys();
@@ -1757,6 +1757,7 @@ public class PlanFragmentBuilder {
             if (node.isOnePhaseAgg() || node.isMergedLocalAgg() || node.getType().isDistinctGlobal()) {
                 // For ScanNode->LocalShuffle->AggNode, we needn't assign scan ranges per driver sequence.
                 inputFragment.setAssignScanRangesPerDriverSeq(!withLocalShuffle);
+                inputFragment.setWithLocalShuffleIfTrue(withLocalShuffle);
                 aggregationNode.setWithLocalShuffle(withLocalShuffle);
                 aggregationNode.setIdenticallyDistributed(true);
             }
