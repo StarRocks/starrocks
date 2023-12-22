f607e6113683748e26ab8dd9fead2b4c08523c21
shuming.li <ming.moriarty@gmail.com>
2023-12-22 13:54:31 +0800

    [BugFix] Reset thread local ConnectContext for each materialized view's refresh (#37551)
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>
    (cherry picked from commit 36e3cfcaeeb9c63db31ad71a7fedcf334c1e190a)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/scheduler/PartitionBasedMvRefreshProcessor.java

--- fe/fe-core/src/main/java/com/starrocks/sql/StatementPlanner.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/StatementPlanner.java
@@ -22,6 +22,7 @@ import com.starrocks.common.Config;
 import com.starrocks.planner.PlanFragment;
 import com.starrocks.planner.ResultSink;
 import com.starrocks.qe.ConnectContext;
+import com.starrocks.server.GlobalStateMgr;
 import com.starrocks.sql.analyzer.Analyzer;
 import com.starrocks.sql.analyzer.AnalyzerUtils;
 import com.starrocks.sql.analyzer.PrivilegeChecker;
@@ -101,6 +102,7 @@ public class StatementPlanner {
             if (needWholePhaseLock) {
                 unLock(dbs);
             }
+            GlobalStateMgr.getCurrentState().getMetadataMgr().removeQueryMetadata();
         }
 
         return null;
