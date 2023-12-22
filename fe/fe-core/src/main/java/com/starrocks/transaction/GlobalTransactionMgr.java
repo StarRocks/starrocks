73c33e6f56d5f86342a625e0b75fdec5e8969519
sunny.xl <wxl24life@gmail.com>
2023-12-22 18:05:49 +0800

    fix typos error
    Signed-off-by: drake_wang <wxl250059@alibaba-inc.com>

--- fe/fe-core/src/main/java/com/starrocks/transaction/GlobalTransactionMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/transaction/GlobalTransactionMgr.java
@@ -72,7 +72,6 @@ import org.apache.commons.lang3.time.StopWatch;
 import org.apache.hadoop.util.ThreadUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import scala.util.control.Exception;
 
 import java.io.DataInput;
 import java.io.DataInputStream;
