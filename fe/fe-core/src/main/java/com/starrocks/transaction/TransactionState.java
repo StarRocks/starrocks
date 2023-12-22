5114241e3c848cdce404963d376b18e10bf11e49
Alex Zhu <zhuming9011@gmail.com>
2023-12-22 14:15:01 +0800

    [BugFix] Fixed NPE throwed in getReadyToPublishTxnListBatch() (#37251)
    Signed-off-by: Alex Zhu <zhuming9011@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/transaction/TransactionState.java
+++ fe/fe-core/src/main/java/com/starrocks/transaction/TransactionState.java
@@ -80,6 +80,7 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import javax.annotation.Nullable;
 import javax.validation.constraints.NotNull;
 
 public class TransactionState implements Writable {
@@ -687,6 +688,7 @@ public class TransactionState implements Writable {
         idToTableCommitInfos.put(tableId, tableCommitInfo);
     }
 
+    @Nullable
     public TableCommitInfo getTableCommitInfo(long tableId) {
         return this.idToTableCommitInfos.get(tableId);
     }
