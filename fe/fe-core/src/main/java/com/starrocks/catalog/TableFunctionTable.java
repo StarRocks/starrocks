511b9eb04e5a6c758575ea744d2a238e7e8f665d
ricky <rickif@qq.com>
2023-12-25 17:18:47 +0800

    table_function: parse url error
    Signed-off-by: ricky <rickif@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/TableFunctionTable.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/TableFunctionTable.java
@@ -236,7 +236,7 @@ public class TableFunctionTable extends Table {
             }
         } catch (UserException e) {
             LOG.error("parse files error", e);
-            throw new DdlException("failed to parse files", e);
+            throw new DdlException("failed to parse files: " + e.getMessage());
         }
 
         if (fileStatuses.isEmpty()) {
