45334b27a078bce470816e03167788199de6c129
Zaorang Yang <zaorangy@gmail.com>
2023-12-26 20:39:54 +0800

    Format
    Signed-off-by: Zaorang Yang <zaorangy@gmail.com>

--- be/src/storage/page_cache.cpp
+++ be/src/storage/page_cache.cpp
@@ -137,7 +137,7 @@ void StoragePageCache::insert(const CacheKey& key, const Slice& data, PageCacheH
     tls_thread_status.mem_consume(mem_size);
 #endif
 
-    auto deleter = [](const starrocks::CacheKey& key, void* value) { delete[] (uint8_t*)value; };
+    auto deleter = [](const starrocks::CacheKey& key, void* value) { delete[](uint8_t*) value; };
 
     CachePriority priority = CachePriority::NORMAL;
     if (in_memory) {
