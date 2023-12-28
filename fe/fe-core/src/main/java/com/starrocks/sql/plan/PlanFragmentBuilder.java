56dbac2ae09acc78a1ebb9927de34bd37527a340
dx.dragon <50667759+zdx19981006@users.noreply.github.com>
2023-12-28 14:58:38 +0800

    [BugFix] OdpsTable method gets columns without including the partition column (#37971)
    Signed-off-by: 鼎昕 <zhangdingxin.zdx@alibaba-inc.com>
    Signed-off-by: 戴鑫铉 <xinxuan.dai@mihoyo.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/plan/PlanFragmentBuilder.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/plan/PlanFragmentBuilder.java
@@ -1180,7 +1180,6 @@ public class PlanFragmentBuilder {
 
         @Override
         public PlanFragment visitPhysicalOdpsScan(OptExpression optExpression, ExecPlan context) {
-            LOG.info("[debug] start visit physical odps scan");
             PhysicalOdpsScanOperator node = (PhysicalOdpsScanOperator) optExpression.getOp();
 
             Table referenceTable = node.getTable();
@@ -1225,7 +1224,6 @@ public class PlanFragmentBuilder {
             PlanFragment fragment =
                     new PlanFragment(context.getNextFragmentId(), odpsScanNode, DataPartition.RANDOM);
             context.getFragments().add(fragment);
-            LOG.info("[debug] finish visit physical odps scan");
             return fragment;
         }
 
