112ae9669e0ff29fb5b481872e9c9b7fe808f446
鼎昕 <zhangdingxin.zdx@alibaba-inc.com>
2023-12-28 11:14:23 +0800

    style: remove debug log and checkstyle
    Signed-off-by: 鼎昕 <zhangdingxin.zdx@alibaba-inc.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/OdpsTable.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/OdpsTable.java
@@ -23,7 +23,6 @@ import com.starrocks.thrift.TTableType;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
