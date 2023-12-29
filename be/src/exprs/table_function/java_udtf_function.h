75614f02be8b583d5d9382cf7ac2ed3c33c26eca
trueeyu <lxhhust350@qq.com>
2023-12-29 11:11:02 +0800

    [Refactor] Table function use the chunk size of runtime state (backport #37731) (#38007)
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/exprs/table_function/java_udtf_function.h
+++ be/src/exprs/table_function/java_udtf_function.h
@@ -28,7 +28,8 @@ public:
     Status init(const TFunction& fn, TableFunctionState** state) const override;
     Status prepare(TableFunctionState* state) const override;
     Status open(RuntimeState* runtime_state, TableFunctionState* state) const override;
-    std::pair<Columns, UInt32Column::Ptr> process(TableFunctionState* state) const override;
+    std::pair<Columns, UInt32Column::Ptr> process(RuntimeState* runtime_state,
+                                                  TableFunctionState* state) const override;
     Status close(RuntimeState* _runtime_state, TableFunctionState* state) const override;
 };
 } // namespace starrocks
