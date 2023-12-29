90ab9e38dc1765470dbbf71caaf46a67e31bc454
liuyehcf <1559500551@qq.com>
2023-12-29 13:38:49 +0800

    [Enhancement] Make audit report for insert into statement asynchronous (#38032)
    Signed-off-by: liuyehcf <1559500551@qq.com>
    Signed-off-by: 戴鑫铉 <xinxuan.dai@mihoyo.com>

--- be/src/exec/pipeline/sink/export_sink_operator.cpp
+++ be/src/exec/pipeline/sink/export_sink_operator.cpp
@@ -193,12 +193,18 @@ bool ExportSinkOperator::is_finished() const {
 
 Status ExportSinkOperator::set_finishing(RuntimeState* state) {
     if (_num_sinkers.fetch_sub(1, std::memory_order_acq_rel) == 1) {
-        state->exec_env()->wg_driver_executor()->report_audit_statistics(state->query_ctx(), state->fragment_ctx());
+        _is_audit_report_done = false;
+        state->exec_env()->wg_driver_executor()->report_audit_statistics(state->query_ctx(), state->fragment_ctx(),
+                                                                         &_is_audit_report_done);
     }
     return _export_sink_buffer->set_finishing();
 }
 
 bool ExportSinkOperator::pending_finish() const {
+    // audit report not finish, we need check until finish
+    if (!_is_audit_report_done) {
+        return true;
+    }
     return !_export_sink_buffer->is_finished();
 }
 
