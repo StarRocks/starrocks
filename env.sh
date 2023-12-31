cdb3fdab21028ed6f157a8c51183abde3898911a
immutablecodes <immutablecodes@126.com>
2023-12-31 09:45:17 +0000

    [BugFix] Improve JDK version determination in env.sh(#38186)

--- env.sh
+++ env.sh
@@ -85,7 +85,7 @@ fi
 
 # check java version
 export JAVA=${JAVA_HOME}/bin/java
-JAVA_VER=$(${JAVA} -version 2>&1 | sed 's/.* version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q' | cut -f1 -d " ")
+JAVA_VER=$(${JAVA} -version 2>&1 | sed 's/.* version "\([^.]*\)\.\([^.]*\)\..*"/\1\2/; 1q' | cut -f1 -d " ")
 if [[ $JAVA_VER -lt 18 ]]; then
     echo "Error: require JAVA with JDK version at least 1.8"
     exit 1
