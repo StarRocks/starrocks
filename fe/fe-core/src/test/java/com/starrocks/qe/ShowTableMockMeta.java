19f708c679458f80c8b6235f8370df463139d192
HangyuanLiu <460660596@qq.com>
2024-01-02 16:14:36 +0800

    [BugFix] Fix user show tables has result when no permission of external table bug (#38216)
    Signed-off-by: HangyuanLiu <460660596@qq.com>
    Signed-off-by: 戴鑫铉 <xinxuan.dai@mihoyo.com>

--- fe/fe-core/src/test/java/com/starrocks/qe/ShowTableMockMeta.java
+++ fe/fe-core/src/test/java/com/starrocks/qe/ShowTableMockMeta.java
@@ -13,15 +13,18 @@
 // limitations under the License.
 package com.starrocks.qe;
 
+import com.google.common.collect.Maps;
 import com.starrocks.analysis.TableName;
 import com.starrocks.catalog.Database;
 import com.starrocks.catalog.HiveTable;
 import com.starrocks.catalog.MaterializedView;
 import com.starrocks.catalog.OlapTable;
 import com.starrocks.catalog.Table;
+import com.starrocks.common.DdlException;
 import com.starrocks.connector.ConnectorMgr;
 import com.starrocks.connector.ConnectorTblMetaInfoMgr;
 import com.starrocks.privilege.IdGenerator;
+import com.starrocks.server.GlobalStateMgr;
 import com.starrocks.server.LocalMetastore;
 import com.starrocks.server.MetadataMgr;
 
@@ -34,18 +37,26 @@ import java.util.Optional;
 public class ShowTableMockMeta extends MetadataMgr {
     private final LocalMetastore localMetastore;
     private final IdGenerator idGenerator;
+
     private final Map<String, Database> databaseSet;
+    private final Map<String, Database> externalDbSet;
+
     private final Map<String, Table> tableMap;
+    private final Map<String, Table> externalTbSet;
 
     public ShowTableMockMeta(LocalMetastore localMetastore, ConnectorMgr connectorMgr) {
         super(localMetastore, connectorMgr, new ConnectorTblMetaInfoMgr());
         this.localMetastore = localMetastore;
         idGenerator = new IdGenerator();
+
         this.databaseSet = new HashMap<>();
+        this.externalDbSet = new HashMap<>();
+
         this.tableMap = new HashMap<>();
+        this.externalTbSet = new HashMap<>();
     }
 
-    public void init() {
+    public void init() throws DdlException {
         Database db = new Database(idGenerator.getNextId(), "testDb");
         databaseSet.put("testDb", db);
 
@@ -57,19 +68,32 @@ public class ShowTableMockMeta extends MetadataMgr {
         t0.setName("testTbl");
         tableMap.put("testTbl", t0);
 
-        HiveTable table = new HiveTable();
-        table.setId(idGenerator.getNextId());
-        table.setName("hive_test");
-        tableMap.put("hive_test", table);
-
         MaterializedView mv = new MaterializedView();
         mv.setId(idGenerator.getNextId());
         mv.setName("testMv");
         tableMap.put("testMv", mv);
+
+        Database db3 = new Database(idGenerator.getNextId(), "hive_db");
+        externalDbSet.put("hive_db", db3);
+
+        HiveTable table = new HiveTable();
+        table.setId(idGenerator.getNextId());
+        table.setName("hive_test");
+        externalTbSet.put("hive_test", table);
+
+        Map<String, String> properties = Maps.newHashMap();
+        properties.put("type", "hive");
+        properties.put("hive.metastore.uris", "thrift://127.0.0.1:9083");
+        GlobalStateMgr.getCurrentState().getCatalogMgr().createCatalog(
+                "hive", "hive_catalog", "", properties);
     }
 
     @Override
     public Database getDb(String catalogName, String dbName) {
+        if (catalogName.equals("hive_catalog")) {
+            return externalDbSet.get("hive_db");
+        }
+
         return databaseSet.get(dbName);
     }
 
@@ -86,6 +110,9 @@ public class ShowTableMockMeta extends MetadataMgr {
 
     @Override
     public List<String> listDbNames(String catalogName) {
+        if (catalogName.equals("hive_catalog")) {
+            return new ArrayList<>(externalDbSet.keySet());
+        }
         return new ArrayList<>(databaseSet.keySet());
     }
 
@@ -96,6 +123,9 @@ public class ShowTableMockMeta extends MetadataMgr {
 
     @Override
     public Table getTable(String catalogName, String dbName, String tblName) {
+        if (catalogName.equals("hive_catalog")) {
+            return externalTbSet.get(tblName);
+        }
         return tableMap.get(tblName);
     }
 
@@ -112,6 +142,9 @@ public class ShowTableMockMeta extends MetadataMgr {
 
     @Override
     public List<String> listTableNames(String catalogName, String dbName) {
+        if (catalogName.equals("hive_catalog")) {
+            return new ArrayList<>(externalTbSet.keySet());
+        }
         return new ArrayList<>(tableMap.keySet());
     }
 }
