859ef6b82f03279ec214600a53e752a3711625a2
Murphy <96611012+mofeiatwork@users.noreply.github.com>
2024-01-02 19:52:36 +0800

    [BugFix] fix compatibility between 2.5 FE and 3.0 BE (#38054)
    Signed-off-by: Murphy <mofei@starrocks.com>
    (cherry picked from commit 470c29eb09437f83f56cf09b9212ce1fac5cbbee)

--- be/src/exec/schema_scanner/schema_materialized_views_scanner.cpp
+++ be/src/exec/schema_scanner/schema_materialized_views_scanner.cpp
@@ -15,8 +15,10 @@
 #include "exec/schema_scanner/schema_materialized_views_scanner.h"
 
 #include "exec/schema_scanner/schema_helper.h"
+#include "runtime/runtime_state.h"
 #include "runtime/string_value.h"
 #include "types/logical_type.h"
+#include "util/thrift_rpc_helper.h"
 
 namespace starrocks {
 
@@ -47,6 +49,15 @@ SchemaScanner::ColumnDesc SchemaMaterializedViewsScanner::_s_tbls_columns[] = {
         {"LAST_REFRESH_ERROR_MESSAGE", TYPE_VARCHAR, sizeof(StringValue), false},
         {"TABLE_ROWS", TYPE_VARCHAR, sizeof(StringValue), false},
         {"MATERIALIZED_VIEW_DEFINITION", TYPE_VARCHAR, sizeof(StringValue), false},
+};
+
+SchemaScanner::ColumnDesc SchemaMaterializedViewsScanner::_s_tbls_columns_v1[] = {
+        //   name,       type,          size,     is_null
+        {"MATERIALIZED_VIEW_ID", TYPE_VARCHAR, sizeof(StringValue), false},
+        {"TABLE_SCHEMA", TYPE_VARCHAR, sizeof(StringValue), false},
+        {"TABLE_NAME", TYPE_VARCHAR, sizeof(StringValue), false},
+        {"MATERIALIZED_VIEW_DEFINITION", TYPE_VARCHAR, sizeof(StringValue), false},
+        {"TABLE_ROWS", TYPE_VARCHAR, sizeof(StringValue), false},
 };
 
 SchemaMaterializedViewsScanner::SchemaMaterializedViewsScanner()
@@ -81,213 +92,65 @@ Status SchemaMaterializedViewsScanner::start(RuntimeState* state) {
     return Status::OK();
 }
 
-Status SchemaMaterializedViewsScanner::fill_chunk(ChunkPtr* chunk) {
-    const TMaterializedViewStatus& tbl_status = _mv_results.materialized_views[_table_index];
-    const auto& slot_id_to_index_map = (*chunk)->get_slot_id_to_index_map();
-    for (const auto& [slot_id, index] : slot_id_to_index_map) {
-        ColumnPtr column = (*chunk)->get_column_by_slot_id(slot_id);
-        switch (slot_id) {
-        case 1: {
-            // TABLE_ID
-            {
-                const std::string* str = &tbl_status.id;
-                Slice value(str->c_str(), str->length());
-                fill_column_with_slot<TYPE_VARCHAR>(column.get(), (void*)&value);
-            }
-            break;
-        }
-        case 2: {
-            // TABLE_SCHEMA
-            {
-                std::string db_name = SchemaHelper::extract_db_name(_db_result.dbs[_db_index - 1]);
-                Slice value(db_name.c_str(), db_name.length());
-                fill_column_with_slot<TYPE_VARCHAR>(column.get(), (void*)&value);
-            }
-            break;
-        }
-        case 3: {
-            // TABLE_NAME
-            {
-                const std::string* str = &tbl_status.name;
-                Slice value(str->c_str(), str->length());
-                fill_column_with_slot<TYPE_VARCHAR>(column.get(), (void*)&value);
-            }
-            break;
-        }
-        case 4: {
-            // REFRESH_TYPE
-            {
-                const std::string* str = &tbl_status.refresh_type;
-                Slice value(str->c_str(), str->length());
-                fill_column_with_slot<TYPE_VARCHAR>(column.get(), (void*)&value);
-            }
-            break;
-        }
-        case 5: {
-            // is_active
-            {
-                const std::string* str = &tbl_status.is_active;
-                Slice value(str->c_str(), str->length());
-                fill_column_with_slot<TYPE_VARCHAR>(column.get(), (void*)&value);
-            }
-            break;
-        }
-        case 6: {
-            // inactive_reason
-            ColumnPtr column = (*chunk)->get_column_by_slot_id(slot_id);
-            const std::string* str = &tbl_status.inactive_reason;
-            Slice value(str->c_str(), str->length());
-            fill_column_with_slot<TYPE_VARCHAR>(column.get(), (void*)&value);
-            break;
-        }
-        case 7: {
-            // partition_type
-            {
-                const std::string* str = &tbl_status.partition_type;
-                Slice value(str->c_str(), str->length());
-                fill_column_with_slot<TYPE_VARCHAR>(column.get(), (void*)&value);
-            }
-            break;
-        }
-        case 8: {
-            // task_id
-            {
-                const std::string* str = &tbl_status.task_id;
-                Slice value(str->c_str(), str->length());
-                fill_column_with_slot<TYPE_VARCHAR>(column.get(), (void*)&value);
-            }
-            break;
-        }
-        case 9: {
-            // task_name
-            {
-                const std::string* str = &tbl_status.task_name;
-                Slice value(str->c_str(), str->length());
-                fill_column_with_slot<TYPE_VARCHAR>(column.get(), (void*)&value);
-            }
-            break;
-        }
-        case 10: {
-            // last_refresh_start_time
-            {
-                const std::string* str = &tbl_status.last_refresh_start_time;
-                Slice value(str->c_str(), str->length());
-                fill_column_with_slot<TYPE_VARCHAR>(column.get(), (void*)&value);
-            }
-            break;
-        }
-        case 11: {
-            // last_refresh_finished_time
-            {
-                const std::string* str = &tbl_status.last_refresh_finished_time;
-                Slice value(str->c_str(), str->length());
-                fill_column_with_slot<TYPE_VARCHAR>(column.get(), (void*)&value);
-            }
-            break;
-        }
-        case 12: {
-            // last_refresh_duration
-            {
-                const std::string* str = &tbl_status.last_refresh_duration;
-                Slice value(str->c_str(), str->length());
-                fill_column_with_slot<TYPE_VARCHAR>(column.get(), (void*)&value);
-            }
-            break;
-        }
-        case 13: {
-            // last_refresh_state
-            {
-                const std::string* str = &tbl_status.last_refresh_state;
-                Slice value(str->c_str(), str->length());
-                fill_column_with_slot<TYPE_VARCHAR>(column.get(), (void*)&value);
-            }
-            break;
-        }
-        case 14: {
-            // last_refresh_force_refresh
-            {
-                const std::string* str = &tbl_status.last_refresh_force_refresh;
-                Slice value(str->c_str(), str->length());
-                fill_column_with_slot<TYPE_VARCHAR>(column.get(), (void*)&value);
-            }
-            break;
-        }
-        case 15: {
-            // last_refresh_start_partition
-            {
-                ColumnPtr column = (*chunk)->get_column_by_slot_id(slot_id);
-                const std::string* str = &tbl_status.last_refresh_start_partition;
-                Slice value(str->c_str(), str->length());
-                fill_column_with_slot<TYPE_VARCHAR>(column.get(), (void*)&value);
-            }
-            break;
-        }
-        case 16: {
-            // last_refresh_end_partition
-            {
-                const std::string* str = &tbl_status.last_refresh_end_partition;
-                Slice value(str->c_str(), str->length());
-                fill_column_with_slot<TYPE_VARCHAR>(column.get(), (void*)&value);
-            }
-            break;
-        }
-        case 17: {
-            // last_refresh_base_refresh_partitions
-            {
-                const std::string* str = &tbl_status.last_refresh_base_refresh_partitions;
-                Slice value(str->c_str(), str->length());
-                fill_column_with_slot<TYPE_VARCHAR>(column.get(), (void*)&value);
-            }
-            break;
-        }
-        case 18: {
-            // last_refresh_mv_refresh_partitions
-            {
-                const std::string* str = &tbl_status.last_refresh_mv_refresh_partitions;
-                Slice value(str->c_str(), str->length());
-                fill_column_with_slot<TYPE_VARCHAR>(column.get(), (void*)&value);
-            }
-            break;
-        }
-        case 19: {
-            // last_refresh_error_code
-            {
-                const std::string* str = &tbl_status.last_refresh_error_code;
-                Slice value(str->c_str(), str->length());
-                fill_column_with_slot<TYPE_VARCHAR>(column.get(), (void*)&value);
-            }
-            break;
-        }
-        case 20: {
-            // last_refresh_error_message
-            {
-                const std::string* str = &tbl_status.last_refresh_error_message;
-                Slice value(str->c_str(), str->length());
-                fill_column_with_slot<TYPE_VARCHAR>(column.get(), (void*)&value);
-            }
-            break;
-        }
-        case 21: {
-            // table_rows
-            {
-                const std::string* str = &tbl_status.rows;
-                Slice value(str->c_str(), str->length());
-                fill_column_with_slot<TYPE_VARCHAR>(column.get(), (void*)&value);
-            }
-            break;
-        }
-        case 22: {
-            // view_definition
-            {
-                const std::string* str = &tbl_status.text;
-                Slice value(str->c_str(), str->length());
-                fill_column_with_slot<TYPE_VARCHAR>(column.get(), (void*)&value);
-            }
-            break;
-        }
-        default:
-            break;
-        }
+Status SchemaMaterializedViewsScanner::_change_to_v1_schema(RuntimeState* state) {
+    if (_v1_schema) {
+        return {};
+    }
+    _columns = _s_tbls_columns_v1;
+    _column_num = sizeof(_s_tbls_columns_v1) / sizeof(SchemaScanner::ColumnDesc);
+    RETURN_IF_ERROR(_create_slot_descs(state->global_obj_pool()));
+    _v1_schema = true;
+    return Status::EAgain("chunk schema changed");
+}
+
+Status SchemaMaterializedViewsScanner::_fill_chunk_v1(ChunkPtr* chunk) {
+    auto& slot_id_map = (*chunk)->get_slot_id_to_index_map();
+    const TTableStatus& info = _table_result.tables[_table_index];
+    std::string db_name = SchemaHelper::extract_db_name(_db_result.dbs[_db_index - 1]);
+    DatumArray datum_array{
+            Slice(info.id), Slice(db_name), Slice(info.name), Slice(info.ddl_sql), Slice(info.rows),
+    };
+    for (const auto& [slot_id, index] : slot_id_map) {
+        Column* column = (*chunk)->get_column_by_slot_id(slot_id).get();
+        column->append_datum(datum_array[slot_id - 1]);
+    }
+
+    _table_index++;
+    return {};
+}
+
+Status SchemaMaterializedViewsScanner::_fill_chunk_v2(ChunkPtr* chunk) {
+    auto& slot_id_map = (*chunk)->get_slot_id_to_index_map();
+    const TMaterializedViewStatus& info = _mv_results.materialized_views[_table_index];
+    std::string db_name = SchemaHelper::extract_db_name(_db_result.dbs[_db_index - 1]);
+    DatumArray datum_array{
+            Slice(info.id),
+            Slice(db_name),
+            Slice(info.name),
+            Slice(info.refresh_type),
+            Slice(info.is_active),
+            Slice(info.inactive_reason),
+            Slice(info.partition_type),
+            Slice(info.task_id),
+            Slice(info.task_name),
+            Slice(info.last_refresh_start_time),
+            Slice(info.last_refresh_finished_time),
+            Slice(info.last_refresh_duration),
+            Slice(info.last_refresh_state),
+            Slice(info.last_refresh_force_refresh),
+            Slice(info.last_refresh_start_partition),
+            Slice(info.last_refresh_end_partition),
+            Slice(info.last_refresh_base_refresh_partitions),
+            Slice(info.last_refresh_mv_refresh_partitions),
+            Slice(info.last_refresh_error_code),
+            Slice(info.last_refresh_error_message),
+            Slice(info.rows),
+            Slice(info.text),
+    };
+
+    for (const auto& [slot_id, index] : slot_id_map) {
+        Column* column = (*chunk)->get_column_by_slot_id(slot_id).get();
+        column->append_datum(datum_array[slot_id - 1]);
     }
     _table_index++;
     return Status::OK();
@@ -312,8 +175,17 @@ Status SchemaMaterializedViewsScanner::get_materialized_views() {
     table_params.__set_type(TTableType::MATERIALIZED_VIEW);
 
     if (nullptr != _param->ip && 0 != _param->port) {
-        RETURN_IF_ERROR(
-                SchemaHelper::list_materialized_view_status(*(_param->ip), _param->port, table_params, &_mv_results));
+        Status st =
+                SchemaHelper::list_materialized_view_status(*(_param->ip), _param->port, table_params, &_mv_results);
+        if (!st.ok()) {
+            if (((std::string_view)st.message()).find(ThriftRpcHelper::KInvalidMethodName) == std::string::npos) {
+                return st;
+            }
+            LOG(WARNING) << "Frontend running in old version, needs to fallback to V1 interface";
+            _mv_results.materialized_views.clear();
+            RETURN_IF_ERROR(SchemaHelper::list_table_status(*(_param->ip), _param->port, table_params, &_table_result));
+            RETURN_IF_ERROR(_change_to_v1_schema(_runtime_state));
+        }
     } else {
         return Status::InternalError("IP or port doesn't exists");
     }
@@ -328,7 +200,7 @@ Status SchemaMaterializedViewsScanner::get_next(ChunkPtr* chunk, bool* eos) {
     if (nullptr == chunk || nullptr == eos) {
         return Status::InternalError("input pointer is nullptr.");
     }
-    while (_table_index >= _mv_results.materialized_views.size()) {
+    while (_table_index >= _mv_results.materialized_views.size() && _table_index >= _table_result.tables.size()) {
         if (_db_index < _db_result.dbs.size()) {
             RETURN_IF_ERROR(get_materialized_views());
         } else {
@@ -337,7 +209,11 @@ Status SchemaMaterializedViewsScanner::get_next(ChunkPtr* chunk, bool* eos) {
         }
     }
     *eos = false;
-    return fill_chunk(chunk);
+    if (_mv_results.materialized_views.size() > 0) {
+        return _fill_chunk_v2(chunk);
+    } else {
+        return _fill_chunk_v1(chunk);
+    }
 }
 
 } // namespace starrocks
