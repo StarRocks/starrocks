e98d0a2417a91e05ed1c194e1e124a48c020cf28
meegoo <meegoo.sr@gmail.com>
2024-01-02 19:23:53 -0800

    [BugFix] Fix nullpointer exeception when create partition concurrent (#38194)
    Signed-off-by: meegoo <meegoo.sr@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/PartitionInfo.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/PartitionInfo.java
@@ -280,15 +280,15 @@ public class PartitionInfo implements Cloneable, Writable, GsonPreProcessable, G
         buff.append("type: ").append(type.typeString).append("; ");
 
         for (Map.Entry<Long, DataProperty> entry : idToDataProperty.entrySet()) {
-            buff.append(entry.getKey()).append("is HDD: ");
+            buff.append(entry.getKey()).append(" is HDD: ");
             if (entry.getValue().equals(new DataProperty(TStorageMedium.HDD))) {
                 buff.append(true);
             } else {
                 buff.append(false);
             }
-            buff.append("data_property: ").append(entry.getValue().toString());
-            buff.append("replica number: ").append(idToReplicationNum.get(entry.getKey()));
-            buff.append("in memory: ").append(idToInMemory.get(entry.getKey()));
+            buff.append(" data_property: ").append(entry.getValue().toString());
+            buff.append(" replica number: ").append(idToReplicationNum.get(entry.getKey()));
+            buff.append(" in memory: ").append(idToInMemory.get(entry.getKey()));
         }
 
         return buff.toString();
