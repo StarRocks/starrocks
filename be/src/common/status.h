659cb6b3c89d66f4f4041876f973247ac1048dad
Moonm3n <saxonzhan@gmail.com>
2024-01-03 20:59:19 +0800

    [Bugfix]: Fix bug by merge
    Signed-off-by: Moonm3n <saxonzhan@gmail.com>

--- be/src/common/status.h
+++ be/src/common/status.h
@@ -143,13 +143,11 @@ public:
 
     static Status EAgain(std::string_view msg) { return Status(TStatusCode::SR_EAGAIN, msg); }
 
-    static Status RemoteFileNotFound(const Slice& msg) { return Status(TStatusCode::REMOTE_FILE_NOT_FOUND, msg); }
-
-    static Status JitCompileError(const Slice& msg) { return Status(TStatusCode::JIT_COMPILE_ERROR, msg); }
-
-    static Status Yield() { return {TStatusCode::YIELD, ""}; }
+    static Status RemoteFileNotFound(std::string_view msg) { return Status(TStatusCode::REMOTE_FILE_NOT_FOUND, msg); }
 
     static Status JitCompileError(std::string_view msg) { return Status(TStatusCode::JIT_COMPILE_ERROR, msg); }
+
+    static Status Yield() { return {TStatusCode::YIELD, ""}; }
 
     bool ok() const { return _state == nullptr; }
 
