8fc132b5b1c711b51fccded835b878116c94b798
eyes_on_me <3675229+silverbullet233@users.noreply.github.com>
2024-01-03 18:29:54 +0800

    [Feature] add session variable enable_query_queue (#38341)
    Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>
    (cherry picked from commit 0ae325bba9ec69d279dc5bbd7b1ec58347df1019)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
    #	fe/fe-core/src/main/java/com/starrocks/qe/scheduler/dag/JobSpec.java

--- fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
@@ -343,6 +343,12 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
     public static final String PARSE_TOKENS_LIMIT = "parse_tokens_limit";
 
     public static final String ENABLE_SORT_AGGREGATE = "enable_sort_aggregate";
+<<<<<<< HEAD
+=======
+    public static final String ENABLE_PER_BUCKET_OPTIMIZE = "enable_per_bucket_optimize";
+    public static final String ENABLE_PARALLEL_MERGE = "enable_parallel_merge";
+    public static final String ENABLE_QUERY_QUEUE = "enable_query_queue";
+>>>>>>> 0ae325bba9 ([Feature] add session variable enable_query_queue (#38341))
 
     public static final String WINDOW_PARTITION_MODE = "window_partition_mode";
 
@@ -1022,6 +1028,18 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
     @VarAttr(name = ENABLE_SORT_AGGREGATE)
     private boolean enableSortAggregate = false;
 
+<<<<<<< HEAD
+=======
+    @VarAttr(name = ENABLE_PER_BUCKET_OPTIMIZE)
+    private boolean enablePerBucketComputeOptimize = true;
+
+    @VarAttr(name = ENABLE_PARALLEL_MERGE)
+    private boolean enableParallelMerge = true;
+
+    @VarAttr(name = ENABLE_QUERY_QUEUE, flag = VariableMgr.INVISIBLE)
+    private boolean enableQueryQueue = true;
+
+>>>>>>> 0ae325bba9 ([Feature] add session variable enable_query_queue (#38341))
     // 1: sort based, 2: hash based
     @VarAttr(name = WINDOW_PARTITION_MODE, flag = VariableMgr.INVISIBLE)
     private int windowPartitionMode = 1;
@@ -1064,8 +1082,23 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
         this.cboPushDownTopNLimit = cboPushDownTopNLimit;
     }
 
+<<<<<<< HEAD
     @VariableMgr.VarAttr(name = ENABLE_SCAN_BLOCK_CACHE)
     private boolean useScanBlockCache = false;
+=======
+    public boolean isEnableQueryQueue() {
+        return enableQueryQueue;
+    }
+
+    @VariableMgr.VarAttr(name = ENABLE_SCAN_DATACACHE, alias = ENABLE_SCAN_BLOCK_CACHE)
+    private boolean enableScanDataCache = false;
+
+    @VariableMgr.VarAttr(name = ENABLE_POPULATE_DATACACHE, alias = ENABLE_POPULATE_BLOCK_CACHE)
+    private boolean enablePopulateDataCache = true;
+
+    @VariableMgr.VarAttr(name = ENABLE_DYNAMIC_PRUNE_SCAN_RANGE)
+    private boolean enableDynamicPruneScanRange = true;
+>>>>>>> 0ae325bba9 ([Feature] add session variable enable_query_queue (#38341))
 
     @VariableMgr.VarAttr(name = IO_TASKS_PER_SCAN_OPERATOR)
     private int ioTasksPerScanOperator = 4;
