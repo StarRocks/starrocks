4587dc3c7f2a9a109b90588e794af539fe3d40d7
yanz <dirtysalt1987@gmail.com>
2024-01-05 09:12:08 +0800

    fix clang format
    Signed-off-by: yanz <dirtysalt1987@gmail.com>

--- be/src/connector/hive_connector.h
+++ be/src/connector/hive_connector.h
@@ -77,7 +77,8 @@ public:
     int64_t estimated_mem_usage() const override;
     bool can_estimate_mem_usage() const override { return true; }
 
-     void get_split_tasks(std::vector<pipeline::ScanSplitContextPtr>* split_tasks) override;
+    void get_split_tasks(std::vector<pipeline::ScanSplitContextPtr>* split_tasks) override;
+
 private:
     const HiveDataSourceProvider* _provider;
     const THdfsScanRange _scan_range;
