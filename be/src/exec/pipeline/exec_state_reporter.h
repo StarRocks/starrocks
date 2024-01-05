3ecf6e0b85ea14b03b5ba6ce1a81c2cf6c148e21
meegoo <meegoo.sr@gmail.com>
2024-01-05 03:57:04 -0800

    [BugFix] Fix exec state report lost lead to ingestion status getting stuck (#36688)
    Signed-off-by: meegoo <meegoo.sr@gmail.com>
    (cherry picked from commit d088b3f14cdfd34562a03c7bb462a58152295657)
    
    # Conflicts:
    #	be/src/exec/pipeline/exec_state_reporter.cpp
    #	be/src/exec/pipeline/exec_state_reporter.h

--- be/src/exec/pipeline/exec_state_reporter.h
+++ be/src/exec/pipeline/exec_state_reporter.h
@@ -23,11 +23,17 @@ public:
                                                                     bool done);
     static Status report_exec_status(const TReportExecStatusParams& params, ExecEnv* exec_env,
                                      const TNetworkAddress& fe_addr);
+<<<<<<< HEAD
     ExecStateReporter();
     void submit(std::function<void()>&& report_task);
+=======
+
+    void submit(std::function<void()>&& report_task, bool priority = false);
+>>>>>>> d088b3f14c ([BugFix] Fix exec state report lost lead to ingestion status getting stuck (#36688))
 
 private:
     std::unique_ptr<ThreadPool> _thread_pool;
+    std::unique_ptr<ThreadPool> _priority_thread_pool;
 };
 } // namespace pipeline
 } // namespace starrocks
