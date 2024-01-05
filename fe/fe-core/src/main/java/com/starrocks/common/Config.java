0451752c07740ce31a92f630b51323d672764c2e
Drake Wang <wxl24life@gmail.com>
2024-01-05 11:24:03 +0800

    [Enhancement] Use standalone lake compaction txn timeout config (#36249)
    Signed-off-by: drake_wang <wxl250059@alibaba-inc.com>
    Signed-off-by: Drake Wang <wxl24life@gmail.com>
    Co-authored-by: Alex Zhu <zhuming9011@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/common/Config.java
+++ fe/fe-core/src/main/java/com/starrocks/common/Config.java
@@ -897,7 +897,7 @@ public class Config extends ConfigBase {
     public static int prepared_transaction_default_timeout_second = 86400; // 1day
 
     /**
-     * Max load timeout applicable to all type of load except for stream load
+     * Max load timeout applicable to all type of load except for stream load and lake compaction
      */
     @ConfField(mutable = true)
     public static int max_load_timeout_second = 259200; // 3days
@@ -2387,6 +2387,13 @@ public class Config extends ConfigBase {
 
     @ConfField(mutable = true, comment = "the max number of threads for lake table delete txnLog when enable batch publish")
     public static int lake_publish_delete_txnlog_max_threads = 16;
+
+    /**
+     * Default lake compaction txn timeout
+     */
+    @ConfField(mutable = true)
+    public static int lake_compaction_default_timeout_second = 86400; // 1 day
+
 
     @ConfField(mutable = true, comment = "the max number of previous version files to keep")
     public static int lake_autovacuum_max_previous_versions = 0;
