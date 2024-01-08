ffe54cf8c97e9495c4c1917b37d022ff5179ffed
zihe.liu <ziheliu1024@gmail.com>
2024-01-08 20:58:18 +0800

    fix
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- be/src/exec/pipeline/result_sink_operator.cpp
+++ be/src/exec/pipeline/result_sink_operator.cpp
@@ -158,7 +158,9 @@ Status ResultSinkOperatorFactory::prepare(RuntimeState* state) {
 }
 
 void ResultSinkOperatorFactory::close(RuntimeState* state) {
-    WARN_IF_ERROR(_sender->close(_fragment_ctx->final_status()), "close sender failed");
+    if (_sender != nullptr) {
+        WARN_IF_ERROR(_sender->close(_fragment_ctx->final_status()), "close sender failed");
+    }
 
     Expr::close(_output_expr_ctxs, state);
     OperatorFactory::close(state);
