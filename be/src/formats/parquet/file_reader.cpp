f4a496bcc201ac4a25e3c5c0333fa70c1e3dfdfa
zombee0 <ewang2027@gmail.com>
2024-01-10 21:07:47 +0800

    [BugFix]support null partition_value to avoid crash
    Signed-off-by: zombee0 <ewang2027@gmail.com>

--- be/src/formats/parquet/file_reader.cpp
+++ be/src/formats/parquet/file_reader.cpp
@@ -317,8 +317,14 @@ Status FileReader::_read_min_max_chunk(const tparquet::RowGroup& row_group, cons
             } else {
                 // is partition column
                 auto* const_column = ColumnHelper::as_raw_column<ConstColumn>(ctx.partition_values[col_idx]);
-                (*min_chunk)->columns()[i]->append(*const_column->data_column(), 0, 1);
-                (*max_chunk)->columns()[i]->append(*const_column->data_column(), 0, 1);
+                ColumnPtr data_column = const_column->data_column();
+                if (data_column->is_nullable()) {
+                    (*min_chunk)->columns()[i]->append_nulls(1);
+                    (*max_chunk)->columns()[i]->append_nulls(1);
+                } else {
+                    (*min_chunk)->columns()[i]->append(*data_column, 0, 1);
+                    (*max_chunk)->columns()[i]->append(*data_column, 0, 1);
+                }
             }
         } else if (!column_meta->__isset.statistics) {
             // statistics not exist in parquet file
