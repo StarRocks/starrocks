159832f29675d5d5c05644209de611ca857908eb
zombee0 <ewang2027@gmail.com>
2024-01-11 18:56:15 +0800

    [BugFix]support null partition_value to avoid crash (#38888)
    Signed-off-by: zombee0 <ewang2027@gmail.com>
    (cherry picked from commit a5a4f3ee7f9b83083a01edd0588d00d12e748ef8)

--- be/src/exec/hdfs_scanner_orc.cpp
+++ be/src/exec/hdfs_scanner_orc.cpp
@@ -150,8 +150,14 @@ bool OrcRowReaderFilter::filterMinMax(size_t rowGroupIdx,
                 max_chunk->columns()[i]->append_nulls(1);
             } else {
                 auto* const_column = ColumnHelper::as_raw_column<ConstColumn>(_scanner_ctx.partition_values[part_idx]);
-                min_chunk->columns()[i]->append(*const_column->data_column(), 0, 1);
-                max_chunk->columns()[i]->append(*const_column->data_column(), 0, 1);
+                ColumnPtr data_column = const_column->data_column();
+                if (data_column->is_nullable()) {
+                    min_chunk->columns()[i]->append_nulls(1);
+                    max_chunk->columns()[i]->append_nulls(1);
+                } else {
+                    min_chunk->columns()[i]->append(*data_column, 0, 1);
+                    max_chunk->columns()[i]->append(*data_column, 0, 1);
+                }
             }
         }
     }
