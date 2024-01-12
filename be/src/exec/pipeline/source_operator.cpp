96298fea3b2ab1fafb7a2e06651473727f4f7a2f
zihe.liu <ziheliu1024@gmail.com>
2024-01-12 16:55:54 +0800

    [Enhancement] push down adaptive dop to UnionNode (#38860)
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- be/src/exec/pipeline/source_operator.cpp
+++ be/src/exec/pipeline/source_operator.cpp
@@ -19,6 +19,16 @@
 
 namespace starrocks::pipeline {
 
+void SourceOperatorFactory::adjust_dop() {
+    const size_t max_parent_dop = std::accumulate(
+            _upstream_sources.begin(), _upstream_sources.end(), static_cast<size_t>(0),
+            [](size_t max_dop, const auto* parent) { return std::max(max_dop, parent->degree_of_parallelism()); });
+
+    if (max_parent_dop > 0 && max_parent_dop < _degree_of_parallelism) {
+        _degree_of_parallelism = max_parent_dop;
+    }
+}
+
 void SourceOperatorFactory::add_group_dependent_pipeline(const Pipeline* dependent_op) {
     group_leader()->_group_dependent_pipelines.emplace_back(dependent_op);
 }
@@ -26,15 +36,26 @@ const std::vector<const Pipeline*>& SourceOperatorFactory::group_dependent_pipel
     return group_leader()->_group_dependent_pipelines;
 }
 
-void SourceOperatorFactory::set_group_leader(SourceOperatorFactory* parent) {
-    if (this == parent) {
-        return;
+void SourceOperatorFactory::add_upstream_source(SourceOperatorFactory* parent) {
+    _upstream_sources.emplace_back(parent);
+    if (_group_parent == this) { // Set the group parent once when adding the first upstream source.
+        _group_parent = parent->group_leader();
     }
-    _group_leader = parent->group_leader();
 }
 
 SourceOperatorFactory* SourceOperatorFactory::group_leader() const {
-    return _group_leader;
+    if (_group_parent != this) {
+        _group_parent = _group_parent->group_leader();
+    }
+    return _group_parent;
+}
+
+void SourceOperatorFactory::union_group(SourceOperatorFactory* other_group) {
+    auto* group_leader = this->group_leader();
+    auto* other_group_leader = other_group->group_leader();
+    if (group_leader != other_group_leader) {
+        other_group_leader->_group_parent = group_leader;
+    }
 }
 
 bool SourceOperatorFactory::is_adaptive_group_initial_active() const {
