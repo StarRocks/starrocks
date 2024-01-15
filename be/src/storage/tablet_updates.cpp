76440d013066afe906ad2d293bccd6646bceb005
zhangqiang <qiangzh95@gmail.com>
2024-01-15 21:28:36 +0800

    [BugFix] Persistent index does not flush advance during compaction (#38651)
    Why I'm doing:
    Pindex does not flush advance in compaction and it may cost too much memory.
    
    What I'm doing:
    Flush advance if pindex use too much memory during compaction.
    
    Signed-off-by: zhangqiang <qiangzh95@gmail.com>
    (cherry picked from commit 02983fbe93641fe890b1e0207cefd45399707c2c)
    
    # Conflicts:
    #	be/src/storage/persistent_index.cpp

--- be/src/storage/tablet_updates.cpp
+++ be/src/storage/tablet_updates.cpp
@@ -1768,6 +1768,7 @@ void TabletUpdates::_apply_compaction_commit(const EditVersionInfo& version_info
                 return;
             }
         }
+        manager->index_cache().update_object_size(index_entry, index.memory_usage());
         DelVectorPtr dv = std::make_shared<DelVector>();
         if (tmp_deletes.empty()) {
             dv->init(version.major(), nullptr, 0);
@@ -1789,6 +1790,7 @@ void TabletUpdates::_apply_compaction_commit(const EditVersionInfo& version_info
         _set_error(msg);
         return;
     }
+    manager->index_cache().update_object_size(index_entry, index.memory_usage());
 
     {
         std::lock_guard wl(_lock);
