56aae2d470c5945a7ec62f0935f9a1a958ebe147
gengjun-git <gengjun@starrocks.com>
2024-01-16 14:07:50 +0800

    [BugFix] Fix Log Print error (#39156)
    Signed-off-by: gengjun-git <gengjun@starrocks.com>
    (cherry picked from commit 527a62ab1cd7ced786bb3291e05b736748013901)

--- fe/fe-core/src/main/java/com/starrocks/leader/ReportHandler.java
+++ fe/fe-core/src/main/java/com/starrocks/leader/ReportHandler.java
@@ -1463,7 +1463,7 @@ public class ReportHandler extends Daemon {
                 TabletStatus status =
                         tablet.getColocateHealthStatus(visibleVersion, replicationNum, backendsSet);
                 if (status == TabletStatus.HEALTHY) {
-                    throw new MetaNotFoundException("colocate tablet [" + tableId + "] is healthy");
+                    throw new MetaNotFoundException("colocate tablet [" + tabletId + "] is healthy");
                 } else {
                     return;
                 }
