7d86c65870222bf5953a1868840204b321512698
Xueyan Li <astralidea@163.com>
2024-01-17 16:32:41 +0800

    [Enhancement] The decimal creation type is consistent with the display type (backport #38639) (#39248)
    Signed-off-by: Astralidea <astralidea@163.com>

--- fe/fe-core/src/test/resources/sql/materialized-view/tpch/q20.sql
+++ fe/fe-core/src/test/resources/sql/materialized-view/tpch/q20.sql
@@ -45,7 +45,7 @@ TOP-N (order by [[2: s_name ASC NULLS FIRST]])
                 EXCHANGE BROADCAST
                     SCAN (table[nation] columns[8: n_nationkey, 9: n_name] predicate[9: n_name = ARGENTINA])
             EXCHANGE SHUFFLE[13]
-                INNER JOIN (join-predicate [30: l_partkey = 12: ps_partkey AND 31: l_suppkey = 13: ps_suppkey AND cast(14: ps_availqty as decimal128(38, 3)) > multiply(0.5, 43: sum)] post-join-predicate [null])
+                INNER JOIN (join-predicate [30: l_partkey = 12: ps_partkey AND 31: l_suppkey = 13: ps_suppkey AND cast(14: ps_availqty as DECIMAL128(38,3)) > multiply(0.5, 43: sum)] post-join-predicate [null])
                     AGGREGATE ([GLOBAL] aggregate [{144: sum=sum(144: sum)}] group by [[131: l_partkey, 129: l_suppkey]] having [null]
                         EXCHANGE SHUFFLE[131, 129]
                             AGGREGATE ([LOCAL] aggregate [{144: sum=sum(132: sum_qty)}] group by [[131: l_partkey, 129: l_suppkey]] having [null]
