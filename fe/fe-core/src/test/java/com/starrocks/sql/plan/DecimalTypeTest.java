8069c78bc8a8c852774b06a3fd270695d5b25f94
Astralidea <astralidea@163.com>
2024-01-18 14:28:34 +0800

    Fix.
    Signed-off-by: Astralidea <astralidea@163.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/DecimalTypeTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/DecimalTypeTest.java
@@ -194,10 +194,10 @@ public class DecimalTypeTest extends PlanTestBase {
         try {
             String sql = "select array_agg(c_0_0) from tab0";
             String plan = getVerboseExplain(sql);
-            assertContains(plan, "array_agg[([16: array_agg, STRUCT<ARRAY<decimal128(26, 2)>>, true]); " +
+            assertContains(plan, "array_agg[([16: array_agg, STRUCT<ARRAY<DECIMAL128(26,2)>>, true]); " +
                     "args: DECIMAL128; result: ARRAY<DECIMAL128(26,2)>;");
             assertContains(plan, "array_agg[([1: c_0_0, DECIMAL128(26,2), false]); " +
-                    "args: DECIMAL128; result: STRUCT<ARRAY<decimal128(26, 2)>>;");
+                    "args: DECIMAL128; result: STRUCT<ARRAY<DECIMAL128(26,2)>>;");
         } finally {
             connectContext.getSessionVariable().setNewPlanerAggStage(stage);
         }
