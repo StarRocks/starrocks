70b07c9154bacefaf403eda17f45f436b06988da
packy92 <wangchao@starrocks.com>
2024-01-19 17:48:41 +0800

    resolve conflicts
    Signed-off-by: packy92 <wangchao@starrocks.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/PlanFragmentWithCostTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/PlanFragmentWithCostTest.java
@@ -2187,8 +2187,6 @@ public class PlanFragmentWithCostTest extends PlanTestBase {
                 "  4:HASH JOIN");
     }
 
-<<<<<<< HEAD
-=======
     @Test
     public void testOutputColNames() throws Exception {
         String sql = "select v1 as alias_1, v2 as alias_2, v2, abs(v2) as v2 from t0 where v3 = 1";
@@ -2198,5 +2196,4 @@ public class PlanFragmentWithCostTest extends PlanTestBase {
         Assert.assertEquals(4, tQueryPlanInfo.output_names.size());
         Assert.assertEquals("alias_1", tQueryPlanInfo.output_names.get(0));
     }
->>>>>>> 58b6071dea ([BugFix] add output col names in the TQueryPlanInfo (#39078))
 }
