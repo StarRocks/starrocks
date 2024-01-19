deaff51aa501c556c8fdf8b519f3103c17228194
Xueyan Li <astralidea@163.com>
2024-01-19 17:25:39 +0800

    [BugFix] Fix bug show columns does not filter by db (#39559)
    Signed-off-by: Astralidea <astralidea@163.com>
    (cherry picked from commit a6a3ae4175cb542be6432e319de4fd95e9c4b0e3)

--- fe/fe-core/src/main/java/com/starrocks/sql/ast/ShowColumnStmt.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/ast/ShowColumnStmt.java
@@ -16,9 +16,13 @@
 package com.starrocks.sql.ast;
 
 import com.google.common.base.Strings;
+import com.starrocks.analysis.BinaryPredicate;
+import com.starrocks.analysis.BinaryType;
+import com.starrocks.analysis.CompoundPredicate;
 import com.starrocks.analysis.Expr;
 import com.starrocks.analysis.ExprSubstitutionMap;
 import com.starrocks.analysis.SlotRef;
+import com.starrocks.analysis.StringLiteral;
 import com.starrocks.analysis.TableName;
 import com.starrocks.catalog.Column;
 import com.starrocks.catalog.ScalarType;
@@ -161,7 +165,9 @@ public class ShowColumnStmt extends ShowStmt {
         }
 
         where = where.substitute(aliasMap);
-
+        where = new CompoundPredicate(CompoundPredicate.Operator.AND, where,
+                new BinaryPredicate(BinaryType.EQ, new SlotRef(TABLE_NAME, "TABLE_SCHEMA"),
+                        new StringLiteral(tableName.getDb())));
         return new QueryStatement(new SelectRelation(selectList, new TableRelation(TABLE_NAME),
                 where, null, null), this.origStmt);
     }
