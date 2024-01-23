9694e107dfa6015e78d7fa06f104aba6b1a2f715
gengjun-git <gengjun@starrocks.com>
2024-01-23 10:45:37 +0800

    [Enhancement] Make some operation type ignorable when replaying journal fails (#39091)
    Why I'm doing:
    Journal replay failure is the main reason for FE startup failure, but most Journal replay failures only affect the use of a small function. Therefore, it is necessary to sort out the severity of each replay failure and ignore the OperationType with a relatively small impact.
    
    What I'm doing:
    The replay failure of OperationType with @IgnorableOnReplayFailed annotation will be ignored when `Config. metadata_journal_ignore_replay_failure` is set to true.
    
    Signed-off-by: gengjun-git <gengjun@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/common/Config.java
+++ fe/fe-core/src/main/java/com/starrocks/common/Config.java
@@ -553,12 +553,13 @@ public class Config extends ConfigBase {
 
     /**
      * If true, FE will reset bdbje replication group(that is, to remove all electable nodes' info)
-     * and is supposed to start as Leader.
-     * If all the electable nodes can not start, we can copy the metadata
-     * to another node and set this config to true to try to restart the FE.
+     * and is supposed to start as Leader. After reset, this node will be the only member in the cluster,
+     * and the others node should be rejoin to this cluster by `Alter system add/drop follower/observer 'xxx'`;
+     * Use this configuration only when the leader cannot be successfully elected
+     * (Because most of the follower data has been damaged).
      */
     @ConfField
-    public static String metadata_failure_recovery = "false";
+    public static String bdbje_reset_election_group = "false";
 
     /**
      * If the bdb data is corrupted, and you want to start the cluster only with image, set this param to true
@@ -2469,6 +2470,7 @@ public class Config extends ConfigBase {
 
     @ConfField(mutable = true)
     public static boolean enable_sync_publish = true;
+
     /**
      * Normally FE will quit when replaying a bad journal. This configuration provides a bypass mechanism.
      * If this was set to a positive value, FE will skip the corresponding bad journals before it quits.
@@ -2476,6 +2478,12 @@ public class Config extends ConfigBase {
      */
     @ConfField(mutable = true)
     public static String metadata_journal_skip_bad_journal_ids = "";
+
+    /**
+     * Set this configuration to true to ignore specific operation (with IgnorableOnReplayFailed annotation) replay failures.
+     */
+    @ConfField
+    public static boolean metadata_journal_ignore_replay_failure = false;
 
     /**
      * Number of profile infos reserved by `ProfileManager` for recently executed query.
