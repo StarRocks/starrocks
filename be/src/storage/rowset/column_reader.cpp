43ae28c3ac85bf23f749bbb5aa976afc0301f0ec
Seaven <seaven_7@qq.com>
2024-01-24 20:15:32 +0800

    [Feature][FlatJson] add flat json read process (#39798)
    Signed-off-by: Seaven <seaven_7@qq.com>

--- be/src/storage/rowset/column_reader.cpp
+++ be/src/storage/rowset/column_reader.cpp
@@ -43,6 +43,7 @@
 #include "column/column_access_path.h"
 #include "column/column_helper.h"
 #include "column/datum_convert.h"
+#include "common/compiler_util.h"
 #include "common/logging.h"
 #include "storage/column_predicate.h"
 #include "storage/rowset/array_column_iterator.h"
@@ -51,6 +52,7 @@
 #include "storage/rowset/bloom_filter.h"
 #include "storage/rowset/bloom_filter_index_reader.h"
 #include "storage/rowset/encoding_info.h"
+#include "storage/rowset/json_column_iterator.h"
 #include "storage/rowset/map_column_iterator.h"
 #include "storage/rowset/page_handle.h"
 #include "storage/rowset/page_io.h"
@@ -59,6 +61,7 @@
 #include "storage/rowset/struct_column_iterator.h"
 #include "storage/rowset/zone_map_index.h"
 #include "storage/types.h"
+#include "types/logical_type.h"
 #include "util/compression/block_compression.h"
 #include "util/rle_encoding.h"
 
@@ -107,6 +110,7 @@ Status ColumnReader::_init(ColumnMetaPB* meta) {
     _column_type = static_cast<LogicalType>(meta->type());
     _dict_page_pointer = PagePointer(meta->dict_page());
     _total_mem_footprint = meta->total_mem_footprint();
+    _name = meta->has_name() ? meta->name() : "None";
 
     if (meta->is_nullable()) _flags |= kIsNullableMask;
     if (meta->has_all_dict_encoded()) _flags |= kHasAllDictEncodedMask;
@@ -175,6 +179,16 @@ Status ColumnReader::_init(ColumnMetaPB* meta) {
         if (_ordinal_index == nullptr) {
             return Status::Corruption(
                     fmt::format("Bad file {}: missing ordinal index for column {}", file_name(), meta->column_id()));
+        }
+
+        if (_column_type == LogicalType::TYPE_JSON) {
+            _sub_readers = std::make_unique<SubReaderList>();
+            for (int i = 0; i < meta->children_columns_size(); ++i) {
+                auto res = ColumnReader::create(meta->mutable_children_columns(i), _segment);
+                RETURN_IF_ERROR(res);
+                _sub_readers->emplace_back(std::move(res).value());
+            }
+            return Status::OK();
         }
         return Status::OK();
     } else if (_column_type == LogicalType::TYPE_ARRAY) {
@@ -489,7 +503,52 @@ bool ColumnReader::segment_zone_map_filter(const std::vector<const ColumnPredica
 }
 
 StatusOr<std::unique_ptr<ColumnIterator>> ColumnReader::new_iterator(ColumnAccessPath* path) {
-    if (is_scalar_field_type(delegate_type(_column_type))) {
+    if (_column_type == LogicalType::TYPE_JSON) {
+        auto json_iter = std::make_unique<ScalarColumnIterator>(this);
+        if (path == nullptr || path->children().empty()) {
+            return json_iter;
+        }
+
+        std::vector<std::unique_ptr<ColumnIterator>> flat_iters;
+        // short name path, e.g. 'a'
+        std::vector<std::string> flat_paths;
+        // full json path, e.g. '$.a'
+        // std::vector<std::string> full_paths;
+        {
+            for (auto& p : path->children()) {
+                if (UNLIKELY(!p->children().empty())) {
+                    // @todo: support later
+                    return Status::InvalidArgument("doesn't support multi-layer json access path: " +
+                                                   p->absolute_path());
+                }
+                flat_paths.emplace_back(p->path());
+                // full_paths.emplace_back("$." + p->path());
+            }
+        }
+
+        int start = is_nullable() ? 1 : 0;
+        for (auto& p : flat_paths) {
+            for (size_t i = start; i < _sub_readers->size(); i++) {
+                const auto& rd = (*_sub_readers)[i];
+                if (rd->name() == p) {
+                    ASSIGN_OR_RETURN(auto iter, rd->new_iterator());
+                    flat_iters.emplace_back(std::move(iter));
+                    break;
+                }
+            }
+        }
+
+        if (flat_iters.size() != flat_paths.size()) {
+            // we must dynamic flat json, because we don't know other segment wasn't the paths
+            return create_json_dynamic_flat_iterator(std::move(json_iter), flat_paths, path);
+        }
+
+        std::unique_ptr<ColumnIterator> null_iterator;
+        if (is_nullable()) {
+            ASSIGN_OR_RETURN(null_iterator, (*_sub_readers)[0]->new_iterator());
+        }
+        return create_json_flat_iterator(this, std::move(null_iterator), std::move(flat_iters), flat_paths, path);
+    } else if (is_scalar_field_type(delegate_type(_column_type))) {
         return std::make_unique<ScalarColumnIterator>(this);
     } else if (_column_type == LogicalType::TYPE_ARRAY) {
         size_t col = 0;
