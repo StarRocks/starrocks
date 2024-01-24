ecb730be668f5fa62f0659a62cd0ff5203fe2f02
gengjun-git <gengjun@starrocks.com>
2024-01-24 19:43:14 +0800

    add ut
    Signed-off-by: gengjun-git <gengjun@starrocks.com>

--- fe/fe-core/src/test/java/com/starrocks/load/loadv2/LoadLoadingTaskTest.java
+++ fe/fe-core/src/test/java/com/starrocks/load/loadv2/LoadLoadingTaskTest.java
@@ -14,6 +14,12 @@
 
 package com.starrocks.load.loadv2;
 
+import com.starrocks.catalog.CatalogIdGenerator;
+import com.starrocks.catalog.Database;
+import com.starrocks.catalog.KeysType;
+import com.starrocks.catalog.OlapTable;
+import com.starrocks.common.IdGenerator;
+import com.starrocks.common.LoadException;
 import com.starrocks.common.jmockit.Deencapsulation;
 import com.starrocks.server.GlobalStateMgr;
 import mockit.Expectations;
@@ -46,5 +52,41 @@ public class LoadLoadingTaskTest {
 
         LoadJob.JSONOptions realJSONOptions =  Deencapsulation.getField(task, "jsonOptions");
         Assert.assertEquals(jsonOptions, realJSONOptions);
+    }
+
+    @Test
+    public void testExecuteTaskWhenMetaDropped(@Mocked CatalogIdGenerator idGenerator) {
+        new Expectations() {
+            {
+                idGenerator.getNextId();
+                result = 1;
+                times = 1;
+            }};
+
+        Database database = new Database(10000L, "test");
+        OlapTable olapTable = new OlapTable(10001L, "tbl", null, KeysType.AGG_KEYS, null, null);
+        LoadLoadingTask loadLoadingTask = new LoadLoadingTask.Builder().setDb(database).setTable(olapTable).build();
+
+        // database not exist
+        boolean exceptionThrown = false;
+        try {
+            loadLoadingTask.executeTask();
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof LoadException);
+            exceptionThrown = true;
+        }
+
+        Assert.assertTrue(exceptionThrown);
+
+        // table not exist
+        exceptionThrown = false;
+        GlobalStateMgr.getCurrentState().getLocalMetastore().unprotectCreateDb(database);
+        try {
+            loadLoadingTask.executeTask();
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof LoadException);
+            exceptionThrown = true;
+        }
+        Assert.assertTrue(exceptionThrown);
     }
 }
