0cce9fc255f69aefa7fb66fdd77f540793eb9481
Gavin <yangguansuo@starrocks.com>
2024-01-24 14:53:26 +0800

    [BugFix] Check datacache memtacker before updating it to avoid the visiting null pointer.(backport #39848)  (#39858)
    Signed-off-by: Gavin <yangguansuo@starrocks.com>

--- be/src/common/daemon.cpp
+++ be/src/common/daemon.cpp
@@ -209,14 +209,16 @@ void calculate_metrics(void* arg_this) {
         }
 
         // update datacache mem_tracker
-        auto datacache_mem_tracker = ExecEnv::GetInstance()->datacache_mem_tracker();
         int64_t datacache_mem_bytes = 0;
-        BlockCache* block_cache = BlockCache::instance();
-        if (block_cache->is_initialized()) {
-            auto datacache_metrics = block_cache->cache_metrics();
-            datacache_mem_bytes = datacache_metrics.mem_used_bytes + datacache_metrics.meta_used_bytes;
+        auto datacache_mem_tracker = ExecEnv::GetInstance()->datacache_mem_tracker();
+        if (datacache_mem_tracker) {
+            BlockCache* block_cache = BlockCache::instance();
+            if (block_cache->is_initialized()) {
+                auto datacache_metrics = block_cache->cache_metrics();
+                datacache_mem_bytes = datacache_metrics.mem_used_bytes + datacache_metrics.meta_used_bytes;
+            }
+            datacache_mem_tracker->set(datacache_mem_bytes);
         }
-        datacache_mem_tracker->set(datacache_mem_bytes);
 
         auto* mem_metrics = StarRocksMetrics::instance()->system_metrics()->memory_metrics();
 
