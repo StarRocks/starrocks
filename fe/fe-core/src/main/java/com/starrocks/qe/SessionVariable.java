f53141a0740dd89a80d4bed855028bac116b42c5
changxin <streakxin@foxmail.com>
2024-01-26 00:05:07 +0800

    [Feature] The parse_datetime function throws an exception when parsing fails in trino mode
    Signed-off-by: changxin <streakxin@foxmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
@@ -3198,6 +3198,7 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
         TQueryOptions tResult = new TQueryOptions();
         tResult.setMem_limit(maxExecMemByte);
         tResult.setQuery_mem_limit(queryMemLimit);
+        tResult.setSql_dialect(sqlDialect.toLowerCase());
 
         // Avoid integer overflow
         tResult.setQuery_timeout(Math.min(Integer.MAX_VALUE / 1000, queryTimeoutS));
