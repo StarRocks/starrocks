883f160adcf06422472743037b1f0c800a9133d8
zhangqiang <qiangzh95@gmail.com>
2024-01-25 10:59:16 +0800

    [Enhancement] Delete pindex file when disable pindex (#39312)
    Signed-off-by: zhangqiang <qiangzh95@gmail.com>
    (cherry picked from commit 08036d8a906de291d26cdfb0fa19c67dce20d381)

--- be/src/storage/persistent_index.h
+++ be/src/storage/persistent_index.h
@@ -784,6 +784,7 @@ public:
     Status reset(Tablet* tablet, EditVersion version, PersistentIndexMetaPB* index_meta);
 
     void reset_cancel_major_compaction();
+    Status delete_pindex_files();
 
     Status pk_dump(PrimaryKeyDump* dump, PrimaryIndexMultiLevelPB* dump_pb);
 
