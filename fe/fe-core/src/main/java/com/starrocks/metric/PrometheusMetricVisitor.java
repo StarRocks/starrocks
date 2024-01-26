StarRocks/starrocks

commit 3da9d51e36f118ce5f2b70c23fa9b047da8d55af

[Feature] add metric of the number of CN (#39371)
Signed-off-by: beans <jing.gao@outlook.com>

fe/fe-core/src/main/java/com/starrocks/metric/PrometheusMetricVisitor.java
@@ -220,6 +220,13 @@ public class PrometheusMetricVisitor extends MetricVisitor {
                                 .count())
                 .append("\n");
 
+        sb.append(NODE_INFO).append("{type=\"cn_node_num\", state=\"total\"} ")
+            .append(GlobalStateMgr.getCurrentSystemInfo().getTotalComputeNodeNumber()).append("\n");
+        sb.append(NODE_INFO).append("{type=\"cn_node_num\", state=\"alive\"} ")
+            .append(GlobalStateMgr.getCurrentSystemInfo().getAliveComputeNodeNumber()).append("\n");
+
+
+
         // only master FE has this metrics, to help the Grafana knows who is the leader
         if (GlobalStateMgr.getCurrentState().isLeader()) {
             sb.append(NODE_INFO).append("{type=\"is_master\"} ").append(1).append("\n");

commit 7a6fabdf2eab5f6164a21ebd61971cb36131b117

[BugFix] Fix compilation failure introduced by #39371
Signed-off-by: Dejun Xia <xiadejun@starrocks.com>

fe/fe-core/src/main/java/com/starrocks/metric/PrometheusMetricVisitor.java
@@ -43,6 +43,7 @@ import com.starrocks.monitor.jvm.JvmStats.GarbageCollector;
 import com.starrocks.monitor.jvm.JvmStats.MemoryPool;
 import com.starrocks.monitor.jvm.JvmStats.Threads;
 import com.starrocks.server.GlobalStateMgr;
+import com.starrocks.system.SystemInfoService;
 
 import java.util.HashSet;
 import java.util.List;
@@ -205,6 +206,7 @@ public class PrometheusMetricVisitor extends MetricVisitor {
     @Override
     public void getNodeInfo() {
         final String NODE_INFO = "node_info";
+        final SystemInfoService systemInfoService = GlobalStateMgr.getCurrentState().getNodeMgr().getClusterInfo();
         sb.append(Joiner.on(" ").join(TYPE, NODE_INFO, "gauge\n"));
         sb.append(NODE_INFO).append("{type=\"fe_node_num\", state=\"total\"} ")
                 .append(GlobalStateMgr.getCurrentState().getNodeMgr().getFrontends(null).size()).append("\n");
@@ -221,11 +223,9 @@ public class PrometheusMetricVisitor extends MetricVisitor {
                 .append("\n");
 
         sb.append(NODE_INFO).append("{type=\"cn_node_num\", state=\"total\"} ")
-            .append(GlobalStateMgr.getCurrentSystemInfo().getTotalComputeNodeNumber()).append("\n");
+            .append(systemInfoService.getTotalComputeNodeNumber()).append("\n");
         sb.append(NODE_INFO).append("{type=\"cn_node_num\", state=\"alive\"} ")
-            .append(GlobalStateMgr.getCurrentSystemInfo().getAliveComputeNodeNumber()).append("\n");
-
-
+            .append(systemInfoService.getAliveComputeNodeNumber()).append("\n");
 
         // only master FE has this metrics, to help the Grafana knows who is the leader
         if (GlobalStateMgr.getCurrentState().isLeader()) {