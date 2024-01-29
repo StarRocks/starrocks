55ba100c86564db0c693c7fd34331dc5311c8305
zihe.liu <ziheliu1024@gmail.com>
2024-01-29 12:50:34 +0800

    [Enhancement] Support profile for only big query (2) (backport #39855) (#40210)
    Signed-off-by: liuyehcf <1559500551@qq.com>
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>
    Co-authored-by: liuyehcf <1559500551@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/SetStmtAnalyzer.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/SetStmtAnalyzer.java
@@ -33,6 +33,7 @@ import com.starrocks.common.UserException;
 import com.starrocks.common.util.CompressionUtils;
 import com.starrocks.common.util.ParseUtil;
 import com.starrocks.common.util.TimeUtils;
+import com.starrocks.monitor.unit.TimeValue;
 import com.starrocks.mysql.MysqlPassword;
 import com.starrocks.qe.ConnectContext;
 import com.starrocks.qe.GlobalVariable;
@@ -220,6 +221,15 @@ public class SetStmtAnalyzer {
                         EnumUtils.getEnumList(SessionVariable.FollowerQueryForwardMode.class), ",");
                 throw new SemanticException(String.format("Unsupported follower query forward mode: %s, " +
                         "supported list is %s", queryFollowerForwardMode, supportedList));
+            }
+        }
+
+        // big_query_profile_threshold
+        if (variable.equalsIgnoreCase(SessionVariable.BIG_QUERY_PROFILE_THRESHOLD)) {
+            String timeStr = resolvedExpression.getStringValue();
+            TimeValue timeValue = TimeValue.parseTimeValue(timeStr, null);
+            if (timeValue == null) {
+                throw new SemanticException(String.format("failed to parse time value %s", timeStr));
             }
         }
 
