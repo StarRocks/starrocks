48709083d8213e691a0c10a7db4a2381796a37e9
Smith Cruise <chendingchao1@126.com>
2024-01-29 20:26:58 +0800

    [BugFix] Fix array/map's behavior in Trino mode
    Signed-off-by: Smith Cruise <chendingchao1@126.com>

--- be/src/exprs/function_call_expr.cpp
+++ be/src/exprs/function_call_expr.cpp
@@ -134,7 +134,7 @@ StatusOr<ColumnPtr> VectorizedFunctionCallExpr::evaluate_checked(starrocks::Expr
     Columns args;
     args.reserve(_children.size());
     for (Expr* child : _children) {
-        ColumnPtr column = EVALUATE_NULL_IF_ERROR(context, child, ptr);
+        ASSIGN_OR_RETURN(ColumnPtr column, context->evaluate(child, ptr));
         args.emplace_back(column);
     }
 
