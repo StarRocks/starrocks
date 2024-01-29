StarRocks/starrocks

commit 9daadfabf4b49bd87692f0c62686b052389507be

[BugFix] Fix count(*) dcheck failed in orc reader (#40007)
Signed-off-by: Smith Cruise <chendingchao1@126.com>

be/src/exec/hdfs_scanner_orc.cpp
@@ -432,7 +432,6 @@ Status HdfsOrcScanner::do_get_next(RuntimeState* runtime_state, ChunkPtr* chunk)
 
     DCHECK_EQ(rows_read, chunk->get()->num_rows());
 
-    _scanner_ctx.append_not_existed_columns_to_chunk(chunk, rows_read);
     _scanner_ctx.append_or_update_partition_column_to_chunk(chunk, rows_read);
 
     return Status::OK();
@@ -480,6 +479,9 @@ StatusOr<size_t> HdfsOrcScanner::_do_get_next(ChunkPtr* chunk) {
                 *chunk = std::move(ret.value());
             }
 
+            // we need to append none existed column before do eval, just for count(*) optimization
+            _scanner_ctx.append_not_existed_columns_to_chunk(chunk, rows_read);
+
             // do stats before we filter rows which does not match.
             _app_stats.raw_rows_read += rows_read;
             _chunk_filter.assign(rows_read, 1);
@@ -507,7 +509,6 @@ StatusOr<size_t> HdfsOrcScanner::_do_get_next(ChunkPtr* chunk) {
                 ck->filter(_chunk_filter);
             }
         }
-        ck->set_num_rows(rows_read);
 
         if (!_orc_reader->has_lazy_load_context()) {
             return rows_read;

commit 7b50cb9cef91a4e5bcad254ce71bf0ebe50d43f1

[BugFix] Fix orc daily bug introduced by #40007
Signed-off-by: Smith Cruise <chendingchao1@126.com>

be/src/exec/hdfs_scanner_orc.cpp
@@ -495,6 +495,8 @@ StatusOr<size_t> HdfsOrcScanner::_do_get_next(ChunkPtr* chunk) {
                     ASSIGN_OR_RETURN(rows_read,
                                      ExecNode::eval_conjuncts_into_filter(it.second, ck.get(), &_chunk_filter));
                     if (rows_read == 0) {
+                        // If rows_read = 0, we need to set chunk size = 0 and bypass filter chunk directly
+                        ck->set_num_rows(0);
                         break;
                     }
                 }