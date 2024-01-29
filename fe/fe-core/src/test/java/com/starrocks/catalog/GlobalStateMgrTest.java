fe912c91356caefb5fcc0e67d175cc840f88dbde
Wu Xueyang <45445803+wuxueyang96@users.noreply.github.com>
2024-01-29 10:13:32 +0800

    [BugFix] Remove version and role file after failing to start fe at first time. (#39672)
    Signed-off-by: 枢木 <wuxueyang.wxy@alibaba-inc.com>
    (cherry picked from commit 763e2aaad443911c2d76d26249d34e973522b2e4)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
    #	fe/fe-core/src/test/java/com/starrocks/catalog/GlobalStateMgrTest.java

--- fe/fe-core/src/test/java/com/starrocks/catalog/GlobalStateMgrTest.java
+++ fe/fe-core/src/test/java/com/starrocks/catalog/GlobalStateMgrTest.java
@@ -49,8 +49,12 @@ import com.starrocks.journal.bdbje.BDBEnvironment;
 import com.starrocks.load.Load;
 import com.starrocks.meta.MetaContext;
 import com.starrocks.persist.EditLog;
+<<<<<<< HEAD:fe/fe-core/src/test/java/com/starrocks/catalog/GlobalStateMgrTest.java
 import com.starrocks.server.GlobalStateMgr;
 import com.starrocks.server.NodeMgr;
+=======
+import com.starrocks.persist.OperationType;
+>>>>>>> 763e2aaad4 ([BugFix] Remove version and role file after failing to start fe at first time. (#39672)):fe/fe-core/src/test/java/com/starrocks/server/GlobalStateMgrTest.java
 import com.starrocks.sql.ast.ModifyFrontendAddressClause;
 import com.starrocks.system.Frontend;
 import mockit.Expectations;
@@ -60,6 +64,7 @@ import mockit.Mocked;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
@@ -71,13 +76,18 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.List;
+<<<<<<< HEAD:fe/fe-core/src/test/java/com/starrocks/catalog/GlobalStateMgrTest.java
 import java.util.Random;
+=======
+import java.util.UUID;
+>>>>>>> 763e2aaad4 ([BugFix] Remove version and role file after failing to start fe at first time. (#39672)):fe/fe-core/src/test/java/com/starrocks/server/GlobalStateMgrTest.java
 import java.util.concurrent.ConcurrentHashMap;
 
 public class GlobalStateMgrTest {
 
     @Before
     public void setUp() {
+<<<<<<< HEAD:fe/fe-core/src/test/java/com/starrocks/catalog/GlobalStateMgrTest.java
         MetaContext metaContext = new MetaContext();
         new Expectations(metaContext) {
             {
@@ -86,6 +96,11 @@ public class GlobalStateMgrTest {
                 result = metaContext;
             }
         };
+=======
+        Config.meta_dir = UUID.randomUUID().toString();
+        Config.plugin_dir = UUID.randomUUID().toString();
+        UtFrameUtils.PseudoImage.setUpImageVersion();
+>>>>>>> 763e2aaad4 ([BugFix] Remove version and role file after failing to start fe at first time. (#39672)):fe/fe-core/src/test/java/com/starrocks/server/GlobalStateMgrTest.java
     }
 
     public void mkdir(String dirString) {
@@ -292,5 +307,76 @@ public class GlobalStateMgrTest {
     public void testAddRepeatedFe() throws Exception {
         GlobalStateMgr globalStateMgr = mockGlobalStateMgr();
         globalStateMgr.addFrontend(FrontendNodeType.FOLLOWER, "127.0.0.1", 1000);
+    }
+
+    private static class MyGlobalStateMgr extends GlobalStateMgr {
+        public static final String ERROR_MESSAGE = "Create Exception here.";
+        private final boolean throwException;
+
+        public MyGlobalStateMgr(boolean throwException) {
+            this.throwException = throwException;
+        }
+
+        public MyGlobalStateMgr(boolean throwException, NodeMgr nodeMgr) {
+            super(nodeMgr);
+            this.throwException = throwException;
+        }
+
+        @Override
+        public void initJournal() throws JournalException, InterruptedException {
+            if (throwException) {
+                throw new UnsupportedOperationException(ERROR_MESSAGE);
+            }
+            super.initJournal();
+        }
+    }
+
+    @Test
+    public void testRemoveRoleAndVersionFileAtFirstTimeStarting() {
+        GlobalStateMgr globalStateMgr = new MyGlobalStateMgr(true);
+        NodeMgr nodeMgr = globalStateMgr.getNodeMgr();
+        Assert.assertTrue(nodeMgr.isVersionAndRoleFilesNotExist());
+        try {
+            globalStateMgr.initialize(new String[0]);
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof UnsupportedOperationException);
+            Assert.assertEquals(MyGlobalStateMgr.ERROR_MESSAGE, e.getMessage());
+        }
+        Assert.assertTrue(nodeMgr.isVersionAndRoleFilesNotExist());
+    }
+
+    @Test
+    public void testSuccessfullyInitializeGlobalStateMgr() {
+        GlobalStateMgr globalStateMgr = new MyGlobalStateMgr(false);
+        NodeMgr nodeMgr = globalStateMgr.getNodeMgr();
+        Assert.assertTrue(nodeMgr.isVersionAndRoleFilesNotExist());
+        try {
+            globalStateMgr.initialize(new String[0]);
+        } catch (Exception e) {
+            Assert.fail("No exception is expected here.");
+        }
+        Assert.assertFalse(nodeMgr.isVersionAndRoleFilesNotExist());
+    }
+
+    @Test
+    public void testErrorOccursWhileRemovingClusterIdAndRoleWhenStartAtFirstTime() {
+        final String removeFileErrorMessage = "Failed to delete role and version files.";
+
+        NodeMgr nodeMgr = Mockito.spy(new NodeMgr());
+        Mockito.doThrow(new RuntimeException(removeFileErrorMessage)).when(nodeMgr).removeClusterIdAndRole();
+
+        GlobalStateMgr globalStateMgr = new MyGlobalStateMgr(true, nodeMgr);
+        Assert.assertTrue(nodeMgr.isVersionAndRoleFilesNotExist());
+        try {
+            globalStateMgr.initialize(new String[0]);
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof UnsupportedOperationException);
+            Assert.assertEquals(MyGlobalStateMgr.ERROR_MESSAGE, e.getMessage());
+
+            Throwable[] suppressedExceptions = e.getSuppressed();
+            Assert.assertEquals(1, suppressedExceptions.length);
+            Assert.assertTrue(suppressedExceptions[0] instanceof RuntimeException);
+            Assert.assertEquals(removeFileErrorMessage, suppressedExceptions[0].getMessage());
+        }
     }
 }
