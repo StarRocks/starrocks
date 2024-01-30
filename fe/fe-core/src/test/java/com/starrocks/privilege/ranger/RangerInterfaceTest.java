f1315e1ae45de57da290109cdaa5f26540e5a981
HangyuanLiu <460660596@qq.com>
2024-01-30 19:47:06 +0800

    [Refactor] Move RangerAccessController into dir privilege/ranger (#40302)
    Signed-off-by: HangyuanLiu <460660596@qq.com>

--- fe/fe-core/src/test/java/com/starrocks/privilege/ranger/RangerInterfaceTest.java
+++ fe/fe-core/src/test/java/com/starrocks/privilege/ranger/RangerInterfaceTest.java
@@ -14,18 +14,30 @@
 package com.starrocks.privilege.ranger;
 
 import com.google.common.collect.Lists;
+import com.starrocks.analysis.ArithmeticExpr;
+import com.starrocks.analysis.BinaryPredicate;
+import com.starrocks.analysis.Expr;
+import com.starrocks.analysis.NullLiteral;
 import com.starrocks.analysis.TableName;
 import com.starrocks.catalog.Column;
 import com.starrocks.catalog.Type;
+import com.starrocks.common.Config;
 import com.starrocks.privilege.AccessControlProvider;
+import com.starrocks.privilege.AccessDeniedException;
 import com.starrocks.privilege.NativeAccessController;
+import com.starrocks.privilege.PrivilegeType;
 import com.starrocks.privilege.ranger.starrocks.RangerStarRocksAccessController;
+import com.starrocks.privilege.ranger.starrocks.RangerStarRocksResource;
 import com.starrocks.qe.ConnectContext;
 import com.starrocks.sql.ast.UserIdentity;
 import mockit.Expectations;
 import mockit.Mock;
 import mockit.MockUp;
+import org.apache.hadoop.security.UserGroupInformation;
+import org.apache.ranger.plugin.model.RangerPolicy;
+import org.apache.ranger.plugin.model.RangerServiceDef;
 import org.apache.ranger.plugin.policyengine.RangerAccessRequest;
+import org.apache.ranger.plugin.policyengine.RangerAccessRequestImpl;
 import org.apache.ranger.plugin.policyengine.RangerAccessResult;
 import org.apache.ranger.plugin.policyengine.RangerAccessResultProcessor;
 import org.apache.ranger.plugin.service.RangerBasePlugin;
@@ -33,14 +45,23 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class RangerInterfaceTest {
 
     @Before
     public void setUp() throws Exception {
-        new Expectations() {
-            {
+        new MockUp<RangerBasePlugin>() {
+            @Mock
+            void init() {
+            }
+
+            @Mock
+            RangerAccessResult evalDataMaskPolicies(RangerAccessRequest request, RangerAccessResultProcessor resultProcessor) {
+                return null;
             }
         };
     }
@@ -53,7 +74,6 @@ public class RangerInterfaceTest {
                 return null;
             }
         };
-
         ConnectContext connectContext = new ConnectContext();
         connectContext.setCurrentUserIdentity(UserIdentity.ROOT);
         TableName tableName = new TableName("db", "tbl");
@@ -69,34 +89,139 @@ public class RangerInterfaceTest {
 
     @Test
     public void testAccessControlProvider() {
-        new MockUp<RangerBasePlugin>() {
-            @Mock
-            void init() {
-            }
+        AccessControlProvider accessControlProvider = new AccessControlProvider(null,
+                new NativeAccessController());
+        accessControlProvider.removeAccessControl("hive");
 
-            @Mock
-            RangerAccessResult evalDataMaskPolicies(RangerAccessRequest request, RangerAccessResultProcessor resultProcessor) {
-                return null;
+        accessControlProvider.setAccessControl("hive", new NativeAccessController());
+        accessControlProvider.setAccessControl("hive", new RangerStarRocksAccessController());
+        Assert.assertTrue(accessControlProvider.getAccessControlOrDefault("hive")
+                instanceof RangerStarRocksAccessController);
+        accessControlProvider.removeAccessControl("hive");
+
+        Assert.assertTrue(accessControlProvider.getAccessControlOrDefault("hive")
+                instanceof NativeAccessController);
+
+        accessControlProvider.setAccessControl("hive", new RangerStarRocksAccessController());
+        Assert.assertTrue(accessControlProvider.getAccessControlOrDefault("hive")
+                instanceof RangerStarRocksAccessController);
+        accessControlProvider.setAccessControl("hive", new NativeAccessController());
+        Assert.assertTrue(accessControlProvider.getAccessControlOrDefault("hive")
+                instanceof NativeAccessController);
+    }
+
+    @Test
+    public void testKerberos() throws IOException {
+        new Expectations() {
+            {
+                UserGroupInformation.loginUserFromKeytab((String) any, (String) any);
+                minTimes = 0;
             }
         };
 
-        AccessControlProvider accessControlProvider = new AccessControlProvider(null, new NativeAccessController());
-        accessControlProvider.removeAccessControl("hive");
+        Config.ranger_spnego_kerberos_principal = "HTTP/172.26.92.195@EXAMPLE.COM";
+        Config.ranger_spnego_kerberos_keytab = this.getClass().getResource("/").getPath() + "rangadmin.keytab";
+        Config.ranger_kerberos_krb5_conf = this.getClass().getResource("/").getPath() + "krb5.conf";
 
-        accessControlProvider.setAccessControl("hive", new NativeAccessController());
-        accessControlProvider.setAccessControl("hive", new RangerStarRocksAccessController());
-        Assert.assertTrue(accessControlProvider.getAccessControlOrDefault("hive")
-                instanceof RangerStarRocksAccessController);
-        accessControlProvider.removeAccessControl("hive");
+        RangerStarRocksAccessController rangerStarRocksAccessController = new RangerStarRocksAccessController();
 
-        Assert.assertTrue(accessControlProvider.getAccessControlOrDefault("hive")
-                instanceof NativeAccessController);
+        Config.ranger_spnego_kerberos_principal = "";
+        Config.ranger_spnego_kerberos_keytab = "";
+    }
 
-        accessControlProvider.setAccessControl("hive", new RangerStarRocksAccessController());
-        Assert.assertTrue(accessControlProvider.getAccessControlOrDefault("hive")
-                instanceof RangerStarRocksAccessController);
-        accessControlProvider.setAccessControl("hive", new NativeAccessController());
-        Assert.assertTrue(accessControlProvider.getAccessControlOrDefault("hive")
-                instanceof NativeAccessController);
+    @Test
+    public void testMaskingExpr() {
+        new MockUp<RangerBasePlugin>() {
+            @Mock
+            RangerAccessResult evalDataMaskPolicies(RangerAccessRequest request, RangerAccessResultProcessor resultProcessor) {
+                RangerAccessResult result = new RangerAccessResult(1, "starrocks",
+                        new RangerServiceDef(), new RangerAccessRequestImpl());
+                result.setMaskType(RangerPolicy.MASK_TYPE_NULL);
+                return result;
+            }
+        };
+
+        RangerStarRocksAccessController rangerStarRocksAccessController = new RangerStarRocksAccessController();
+
+        ConnectContext connectContext = new ConnectContext();
+        connectContext.setCurrentUserIdentity(UserIdentity.ROOT);
+        TableName tableName = new TableName("db", "tbl");
+        List<Column> columns = Lists.newArrayList(new Column("v1", Type.INT));
+
+        Map<String, Expr> e = rangerStarRocksAccessController.getColumnMaskingPolicy(connectContext, tableName, columns);
+        Assert.assertTrue(new ArrayList<>(e.values()).get(0) instanceof NullLiteral);
+
+
+        new MockUp<RangerBasePlugin>() {
+            @Mock
+            RangerAccessResult evalDataMaskPolicies(RangerAccessRequest request, RangerAccessResultProcessor resultProcessor) {
+                RangerAccessResult result = new RangerAccessResult(1, "starrocks",
+                        new RangerServiceDef(), new RangerAccessRequestImpl());
+                result.setMaskType(RangerPolicy.MASK_TYPE_CUSTOM);
+                result.setMaskedValue("v + 1");
+                return result;
+            }
+        };
+
+        e = rangerStarRocksAccessController.getColumnMaskingPolicy(connectContext, tableName, columns);
+        Assert.assertTrue(new ArrayList<>(e.values()).get(0) instanceof ArithmeticExpr);
+    }
+
+    @Test
+    public void testRowAccessExpr() {
+        new MockUp<RangerBasePlugin>() {
+            @Mock
+            RangerAccessResult evalRowFilterPolicies(RangerAccessRequest request, RangerAccessResultProcessor resultProcessor) {
+                RangerAccessResult result = new RangerAccessResult(1, "starrocks",
+                        new RangerServiceDef(), new RangerAccessRequestImpl());
+                result.setFilterExpr("v1 = 1");
+                return result;
+            }
+        };
+        RangerStarRocksAccessController rangerStarRocksAccessController = new RangerStarRocksAccessController();
+
+        ConnectContext connectContext = new ConnectContext();
+        connectContext.setCurrentUserIdentity(UserIdentity.ROOT);
+        TableName tableName = new TableName("db", "tbl");
+
+        Expr rowFilter = rangerStarRocksAccessController.getRowAccessPolicy(connectContext, tableName);
+        Assert.assertTrue(rowFilter instanceof BinaryPredicate);
+    }
+
+    @Test
+    public void testPermission() {
+        new MockUp<RangerBasePlugin>() {
+            @Mock
+            RangerAccessResult isAccessAllowed(RangerAccessRequest request) {
+                RangerAccessResult result = new RangerAccessResult(1, "starrocks",
+                        new RangerServiceDef(), new RangerAccessRequestImpl());
+                result.setIsAllowed(true);
+                return result;
+            }
+        };
+        RangerStarRocksAccessController rangerStarRocksAccessController = new RangerStarRocksAccessController();
+
+        ConnectContext connectContext = new ConnectContext();
+        connectContext.setCurrentUserIdentity(UserIdentity.ROOT);
+
+        try {
+            rangerStarRocksAccessController.hasPermission(
+                    RangerStarRocksResource.builder().setSystem().build(), UserIdentity.ROOT, PrivilegeType.OPERATE);
+        } catch (Exception e) {
+            Assert.fail();
+        }
+
+        new MockUp<RangerBasePlugin>() {
+            @Mock
+            RangerAccessResult isAccessAllowed(RangerAccessRequest request) {
+                RangerAccessResult result = new RangerAccessResult(1, "starrocks",
+                        new RangerServiceDef(), new RangerAccessRequestImpl());
+                result.setIsAllowed(false);
+                return result;
+            }
+        };
+
+        Assert.assertThrows(AccessDeniedException.class, () -> rangerStarRocksAccessController.hasPermission(
+                RangerStarRocksResource.builder().setSystem().build(), UserIdentity.ROOT, PrivilegeType.OPERATE));
     }
 }
