d897f093ee1a2c72ac395f7b35160c00a05070f4
Astralidea <astralidea@163.com>
2024-01-30 16:13:15 +0800

    Fix bug dynamic partition date type does not support hour
    Signed-off-by: Astralidea <astralidea@163.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/PartitionInfo.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/PartitionInfo.java
@@ -38,7 +38,6 @@ import com.google.common.base.Preconditions;
 import com.google.gson.annotations.SerializedName;
 import com.starrocks.common.DdlException;
 import com.starrocks.common.FeMetaVersion;
-import com.starrocks.common.NotImplementedException;
 import com.starrocks.common.io.Text;
 import com.starrocks.common.io.Writable;
 import com.starrocks.lake.StorageCacheInfo;
@@ -54,6 +53,7 @@ import org.apache.logging.log4j.Logger;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -216,8 +216,8 @@ public class PartitionInfo implements Cloneable, Writable, GsonPreProcessable, G
         return "";
     }
 
-    public List<Column> getPartitionColumns() throws NotImplementedException {
-        throw new NotImplementedException("method not implemented yet");
+    public List<Column> getPartitionColumns() {
+        return Collections.emptyList();
     }
 
     @Override
