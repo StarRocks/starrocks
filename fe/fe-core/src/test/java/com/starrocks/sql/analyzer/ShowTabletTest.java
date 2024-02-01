52a43d003d72e9d1b481446347b2a6b7bbb90a89
yiming <107105845+nshangyiming@users.noreply.github.com>
2024-02-01 11:14:39 +0800

    [Enhancement] Refine the syntax of show tablet (#40449)
    Signed-off-by: Dejun Xia <xiadejun@starrocks.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/analyzer/ShowTabletTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/analyzer/ShowTabletTest.java
@@ -32,6 +32,7 @@ public class ShowTabletTest {
     public void normalTest() {
         analyzeSuccess("SHOW TABLET 10000;");
         analyzeSuccess("SHOW TABLET FROM test_tbl;");
+        analyzeSuccess("SHOW TABLETS FROM test_tbl;");
         analyzeSuccess("SHOW TABLET FROM example_db.table_name;");
         analyzeSuccess("SHOW TABLET FROM example_db.table_name partition(p1, p2);");
         analyzeSuccess("SHOW TABLET FROM example_db.table_name limit 10;");
