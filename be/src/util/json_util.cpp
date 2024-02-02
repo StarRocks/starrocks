3c136014f3cd4866153f912b8587b91c211cd025
Seaven <seaven_7@qq.com>
2024-02-02 20:02:38 +0800

    upte
    Signed-off-by: Seaven <seaven_7@qq.com>

--- be/src/util/json_util.cpp
+++ be/src/util/json_util.cpp
@@ -151,20 +151,21 @@ void JsonFlater::flatten(const Column* json_column, std::vector<ColumnPtr>* resu
 
         auto* obj = json_data->get_object(i);
         auto vslice = obj->to_vslice();
-        if (UNLIKELY(!vslice.isObject() || vslice.isEmptyObject())) {
+        if (vslice.isNone()) {
             for (size_t k = 0; k < result->size(); k++) {
                 (*result)[k]->append_nulls(1);
             }
             continue;
-        } else if (vslice.isNone()) {
+        } else if (UNLIKELY(!vslice.isObject() || vslice.isEmptyObject())) {
             for (size_t k = 0; k < result->size(); k++) {
-                (*result)[k]->append_nulls(1);
+                flat_jsons[k]->append(JsonValue::from_none());
+                flat_nulls[k]->append(0);
             }
             continue;
         }
         for (size_t k = 0; k < _flat_paths.size(); k++) {
             auto st = obj->get_obj(_flat_paths[k]);
-            if (st.ok()) {
+            if (st.ok() && !st.value().is_null()) {
                 flat_jsons[k]->append(st.value());
                 flat_nulls[k]->append(0);
             } else {
