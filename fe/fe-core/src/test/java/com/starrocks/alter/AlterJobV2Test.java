dca1f779bdd6973179af563ffdd32d2013d0151f
AndyZiYe <yeziyu@starrocks.com>
2024-02-04 18:57:58 +0800

    Revert "test"
    This reverts commit 72d5b07b90f87eb3c9f9fc6a6efb90eae237d28f.

--- fe/fe-core/src/test/java/com/starrocks/alter/AlterJobV2Test.java
+++ fe/fe-core/src/test/java/com/starrocks/alter/AlterJobV2Test.java
@@ -31,7 +31,6 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-// test
 
 package com.starrocks.alter;
 
