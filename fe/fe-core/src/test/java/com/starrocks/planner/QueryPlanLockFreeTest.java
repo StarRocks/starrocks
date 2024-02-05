ac9cf8b870a08582b63de439e348d90f442123fa
packy92 <110370499+packy92@users.noreply.github.com>
2024-02-05 09:59:07 +0800

    [BugFix] use db lock in follower or cbo_use_db_lock is true (#40713)
    Signed-off-by: packy92 <wangchao@starrocks.com>

---
+++ fe/fe-core/src/test/java/com/starrocks/planner/QueryPlanLockFreeTest.java
@@ -0,0 +1,96 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.starrocks.planner;
+
+import com.starrocks.catalog.OlapTable;
+import com.starrocks.common.FeConstants;
+import com.starrocks.common.Pair;
+import com.starrocks.common.util.UUIDUtil;
+import com.starrocks.ha.FrontendNodeType;
+import com.starrocks.qe.ConnectContext;
+import com.starrocks.server.GlobalStateMgr;
+import com.starrocks.sql.plan.ExecPlan;
+import com.starrocks.utframe.StarRocksAssert;
+import com.starrocks.utframe.UtFrameUtils;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class QueryPlanLockFreeTest {
+    private static ConnectContext connectContext;
+    private static StarRocksAssert starRocksAssert;
+    private static String DB_NAME = "test";
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        UtFrameUtils.createMinStarRocksCluster();
+
+        FeConstants.enablePruneEmptyOutputScan = false;
+
+        // create connect context
+        connectContext = UtFrameUtils.createDefaultCtx();
+        connectContext.setQueryId(UUIDUtil.genUUID());
+        starRocksAssert = new StarRocksAssert(connectContext);
+        starRocksAssert.withDatabase(DB_NAME).useDatabase(DB_NAME);
+
+        starRocksAssert.withTable("CREATE TABLE IF NOT EXISTS `t0` (\n" +
+                "  `k1` int(11) NULL,\n" +
+                "  `k2` int(11) NULL,\n" +
+                "  `k3` int(11) NULL,\n" +
+                "  `v1` int SUM NULL,\n" +
+                "  `v2` bigint SUM NULL,\n" +
+                "  `v3` largeint SUM NULL,\n" +
+                "  `v4` double SUM NULL,\n" +
+                "  `v5` decimal(10, 3) SUM NULL\n" +
+                ") ENGINE=OLAP\n" +
+                "AGGREGATE KEY(`k1`, `k2`, `k3`)\n" +
+                "DISTRIBUTED BY HASH(`k2`) BUCKETS 10\n" +
+                "PROPERTIES (\n" +
+                " \"replication_num\" = \"1\"\n" +
+                ");");
+    }
+
+    @Test
+    public void testPlanStrategy() throws Exception {
+        String sql = "select * from t0";
+        OlapTable table = (OlapTable) GlobalStateMgr.getCurrentState().getMetadataMgr()
+                .getTable("default_catalog", DB_NAME, "t0");
+        table.lastVersionUpdateStartTime.set(2);
+        table.lastVersionUpdateEndTime.set(1);
+        try {
+            UtFrameUtils.getPlanAndFragment(connectContext, sql);
+            fail("should fail here");
+        } catch (Exception e) {
+            Assert.assertTrue(e.getMessage(),
+                    e.getMessage().contains("The tablet write operation update metadata take a long time"));
+        }
+        connectContext.getSessionVariable().setCboUseDBLock(true);
+        Pair<String, ExecPlan> plan = UtFrameUtils.getPlanAndFragment(connectContext, sql);
+        Assert.assertTrue(plan.first, plan.first.contains("SCAN"));
+        GlobalStateMgr.getCurrentState().setFrontendNodeType(FrontendNodeType.FOLLOWER);
+        plan = UtFrameUtils.getPlanAndFragment(connectContext, sql);
+        Assert.assertTrue(plan.first, plan.first.contains("SCAN"));
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        connectContext.getSessionVariable().setCboUseDBLock(false);
+        GlobalStateMgr.getCurrentState().setFrontendNodeType(FrontendNodeType.LEADER);
+    }
+
+}
