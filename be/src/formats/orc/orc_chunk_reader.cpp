f03e25e1cd531cd2bc29b15278b909e648f41de8
ricky <rickif@qq.com>
2024-02-05 10:53:29 +0800

    [BugFix] Fixed orc varchar load failed (branch-2.5) (backport #26182) (#40720)
    Signed-off-by: Zaorang Yang <zaorangy@gmail.com>
    Signed-off-by: ricky <rickif@qq.com>
    Co-authored-by: zaorangyang <zaorangy@gmail.com>

--- be/src/formats/orc/orc_chunk_reader.cpp
+++ be/src/formats/orc/orc_chunk_reader.cpp
@@ -364,7 +364,7 @@ static Status _create_type_descriptor_by_orc(const TypeDescriptor& origin_type, 
     return Status::OK();
 }
 
-static void _try_implicit_cast(TypeDescriptor* from, const TypeDescriptor& to) {
+void OrcChunkReader::_try_implicit_cast(TypeDescriptor* from, const TypeDescriptor& to) {
     auto is_integer_type = [](PrimitiveType t) { return g_starrocks_int_type.count(t) > 0; };
     auto is_decimal_type = [](PrimitiveType t) { return g_starrocks_decimal_type.count(t) > 0; };
 
@@ -397,6 +397,12 @@ static void _try_implicit_cast(TypeDescriptor* from, const TypeDescriptor& to) {
         } else {
             from->type = PrimitiveType::TYPE_DECIMAL32;
         }
+    } else if (_broker_load_mode && !_strict_mode && t1 == PrimitiveType::TYPE_VARCHAR &&
+               t2 == PrimitiveType::TYPE_VARCHAR) {
+        // For broker load, the orc field length is larger than the maximum length of the starrocks field
+        // will cause load failure in non-strict mode. Here we keep the maximum length of the orc field
+        // the same as the maximum length of the starrocks field.
+        from->len = to.len;
     } else {
         // nothing to do.
     }
