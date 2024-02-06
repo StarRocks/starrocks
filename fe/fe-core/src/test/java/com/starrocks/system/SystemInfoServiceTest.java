9fb22cfa2a5a6d384131672d16dbd4691af500eb
gengjun-git <gengjun@starrocks.com>
2024-02-06 19:52:31 +0800

    recover
    Signed-off-by: gengjun-git <gengjun@starrocks.com>

--- fe/fe-core/src/test/java/com/starrocks/system/SystemInfoServiceTest.java
+++ fe/fe-core/src/test/java/com/starrocks/system/SystemInfoServiceTest.java
@@ -21,13 +21,8 @@ import java.lang.reflect.Field;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.List;
-<<<<<<< HEAD
-=======
-import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Collectors;
->>>>>>> f3233fa9f2 ([BugFix]Fix tablet delete by mistake because of the outdate report version (#40656))
 
 public class SystemInfoServiceTest {
 
