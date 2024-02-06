888a9ad81a692dc32efdbab940f1d912a81811ec
lichaoyong <lichaoyong158@gmail.com>
2024-02-06 01:23:42 -0800

    [BugFix] Get rid of overwriting JobState after create an image (#40672)
    (cherry picked from commit cc9bf31727d882dc30234fd3dbdce677c7d6d668)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/load/loadv2/LoadJob.java

--- fe/fe-core/src/main/java/com/starrocks/load/loadv2/LoadJob.java
+++ fe/fe-core/src/main/java/com/starrocks/load/loadv2/LoadJob.java
@@ -233,6 +233,11 @@ public abstract class LoadJob extends AbstractTxnStateChangeCallback implements 
         return id;
     }
 
+    // unit test
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public Database getDb() throws MetaNotFoundException {
         // get db
         Database db = GlobalStateMgr.getCurrentState().getDb(dbId);
@@ -284,7 +289,7 @@ public abstract class LoadJob extends AbstractTxnStateChangeCallback implements 
         }
         idToTasks.clear();
         finishedTaskIds.clear();
-        loadingStatus.setProgress(0);
+        loadingStatus.reset();
     }
 
     public boolean isTimeout() {
@@ -1011,6 +1016,7 @@ public abstract class LoadJob extends AbstractTxnStateChangeCallback implements 
             progress = 99;
             transactionId = txnState.getTransactionId();
             state = JobState.COMMITTED;
+            failMsg = null;
         } finally {
             writeUnlock();
         }
@@ -1112,7 +1118,12 @@ public abstract class LoadJob extends AbstractTxnStateChangeCallback implements 
             progress = 100;
             finishTimestamp = txnState.getFinishTime();
             state = JobState.FINISHED;
+<<<<<<< HEAD
             GlobalStateMgr.getCurrentGlobalTransactionMgr().getCallbackFactory().removeCallback(id);
+=======
+            failMsg = null;
+            GlobalStateMgr.getCurrentState().getGlobalTransactionMgr().getCallbackFactory().removeCallback(id);
+>>>>>>> cc9bf31727 ([BugFix] Get rid of overwriting JobState after create an image (#40672))
         } finally {
             writeUnlock();
         }
