04cd8aeb26df1d9733dc14bb336c52936b7977fd
gengjun-git <gengjun@starrocks.com>
2024-02-06 09:32:28 +0800

    [BugFix]Fix tablet delete by mistake because of the outdate report version (#40656)
    Why I'm doing:
    The report version in SystemInfoService maybe outdate in a concurrent scenario.
    
    What I'm doing:
    1. Update the report version incrementally.
    2. Fix the report version bug of PseudoBackend.
    
    Signed-off-by: gengjun-git <gengjun@starrocks.com>
    (cherry picked from commit f3233fa9f2285f80adc0cd9d19a9957b468b2c57)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/system/SystemInfoService.java
    #	fe/fe-core/src/test/java/com/starrocks/system/SystemInfoServiceTest.java

--- fe/fe-core/src/test/java/com/starrocks/system/SystemInfoServiceTest.java
+++ fe/fe-core/src/test/java/com/starrocks/system/SystemInfoServiceTest.java
@@ -34,6 +34,12 @@ import java.lang.reflect.Field;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.List;
+<<<<<<< HEAD
+=======
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicLong;
+>>>>>>> f3233fa9f2 ([BugFix]Fix tablet delete by mistake because of the outdate report version (#40656))
 import java.util.stream.Collectors;
 
 public class SystemInfoServiceTest {
@@ -261,5 +267,26 @@ public class SystemInfoServiceTest {
         service.addBackend(be);
         long backendId = service.getBackendIdWithStarletPort("newHost", 10001);
         Assert.assertEquals(be.getId(), backendId);
+    }
+
+    @Test
+    public void testUpdateReportVersionIncreasing() throws Exception {
+        long[] versions = new long[] {10, 5, 3, 2, 4, 1, 9, 7, 8, 6};
+        AtomicLong version = new AtomicLong();
+        version.set(0);
+
+        CountDownLatch latch = new CountDownLatch(10);
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            new Thread(() -> {
+                service.updateReportVersionIncrementally(version, versions[index]);
+                System.out.println("updated version: " + versions[index]);
+                latch.countDown();
+            }).start();
+        }
+
+        latch.await();
+
+        Assert.assertEquals(10L, version.get());
     }
 }
