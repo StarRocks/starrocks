949866ad0bfb86fa0231111f131a5b4d18cd63eb
xiangguangyxg <110401425+xiangguangyxg@users.noreply.github.com>
2024-02-07 10:21:39 +0800

    [BugFix] Fix the problem of column mode partial update in cross cluster replication (#40692)
    Signed-off-by: xiangguangyxg <xiangguangyxg@gmail.com>
    (cherry picked from commit 29d1008b63cbc1c6f031eba5c5f872ab7c15e616)
    
    # Conflicts:
    #	be/src/storage/replication_utils.cpp

--- be/src/storage/replication_utils.cpp
+++ be/src/storage/replication_utils.cpp
@@ -336,6 +336,7 @@ StatusOr<std::string> ReplicationUtils::download_remote_snapshot_file(
 
 Status ReplicationUtils::convert_rowset_txn_meta(RowsetTxnMetaPB* rowset_txn_meta,
                                                  const std::unordered_map<uint32_t, uint32_t>& column_unique_id_map) {
+<<<<<<< HEAD
     for (auto& column_unique_id : *rowset_txn_meta->mutable_partial_update_column_unique_ids()) {
         auto iter = column_unique_id_map.find(column_unique_id);
         if (iter == column_unique_id_map.end()) {
@@ -343,6 +344,19 @@ Status ReplicationUtils::convert_rowset_txn_meta(RowsetTxnMetaPB* rowset_txn_met
             return Status::InternalError("Column not found");
         }
         column_unique_id = iter->second;
+=======
+    RETURN_IF_ERROR(convert_column_unique_ids(rowset_txn_meta->mutable_partial_update_column_unique_ids(),
+                                              column_unique_id_map));
+
+    if (rowset_txn_meta->has_auto_increment_partial_update_column_uid()) {
+        auto iter = column_unique_id_map.find(rowset_txn_meta->auto_increment_partial_update_column_uid());
+        if (iter == column_unique_id_map.end()) {
+            LOG(ERROR) << "Column not found, column unique id: "
+                       << rowset_txn_meta->auto_increment_partial_update_column_uid();
+            return Status::InternalError("Column not found");
+        }
+        rowset_txn_meta->set_auto_increment_partial_update_column_uid(iter->second);
+>>>>>>> 29d1008b63 ([BugFix] Fix the problem of column mode partial update in cross cluster replication (#40692))
     }
     return Status::OK();
 }
