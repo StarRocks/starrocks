2024395e8e6276d6571d056fe31f7869838e8508
Smith Cruise <chendingchao1@126.com>
2024-02-07 23:05:43 +0800

    [Enhancement] Change hive file split logic (#40910)
    Signed-off-by: Smith Cruise <chendingchao1@126.com>
    (cherry picked from commit ff862de0d5220d91c97fbfc7c1c90cca7e8c539f)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/connector/RemoteScanRangeLocations.java
    #	fe/fe-core/src/test/java/com/starrocks/statistic/StatisticsCollectJobTest.java

--- fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
@@ -1695,6 +1695,10 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
         return hivePartitionStatsSampleSize;
     }
 
+    public void setForceScheduleLocal(boolean forceScheduleLocal) {
+        this.forceScheduleLocal = forceScheduleLocal;
+    }
+
     public boolean getEnableAdaptiveSinkDop() {
         return enableAdaptiveSinkDop;
     }
