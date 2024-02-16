9442c0f127c1446a9beba64ddf3f5344f558e028
DanRoscigno <dan@roscigno.com>
2024-02-16 11:24:25 -0500

    edits to reuse prod docusaurus conf
    Signed-off-by: DanRoscigno <dan@roscigno.com>

--- docs/docusaurus/docusaurus.config.js
+++ docs/docusaurus/docusaurus.config.js
@@ -5,12 +5,17 @@
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
 import {themes as prismThemes} from 'prism-react-renderer';
+import versions from './versions.json';
+
+// Used to limit build to just two versions for debugging
+const isBuildFast = !!process.env.BUILD_FAST;
 
 // if the env var DISABLE_VERSIONING is set
 // (example `export DISABLE_VERSIONING=true`) then build only the
-// content of `docs`. To build all versions remove the env var with
-// `unset DISABLE_VERSIONING` (don't set it to false, we are checking
-// to see if the var is set, not what the value is).
+// content of `docs/en` and `docs/zh`. To build all versions remove
+// the env var with `unset DISABLE_VERSIONING` 
+// (don't set it to false, we are checking to see if the var is set,
+// not what the value is).
 const isVersioningDisabled = !!process.env.DISABLE_VERSIONING || false;
 
 /** @type {import('@docusaurus/types').Config} */
@@ -32,7 +37,7 @@ const config = {
 
   onBrokenAnchors: 'ignore',
   onBrokenLinks: 'ignore',
-  onBrokenMarkdownLinks: 'throw',
+  onBrokenMarkdownLinks: 'ignore',
 
   i18n: {
     defaultLocale: 'en',
@@ -53,11 +58,14 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.json'),
-
+          sidebarPath: 'sidebars.json',
           // Edit links for English and Chinese
           editUrl: ({locale, docPath}) => {
-              return 'https://github.com/StarRocks/starrocks/edit/main/docs/' + locale + '/' + docPath
+            if (locale == 'en') {
+              return 'https://github.com/StarRocks/starrocks/edit/main/docs/en/' + docPath
+            } else {
+              return 'https://github.com/StarRocks/starrocks/edit/main/docs/zh/' + docPath
+            }
           },
           // Versions:
           // We don't want to show `main` or `current`, we want to show the released versions.
@@ -67,38 +75,39 @@ const config = {
           // versions, so the banner is set to none on the versions other than latest (latest
           // doesn't get a banner by default).
           lastVersion: (() => {
-              if (isVersioningDisabled) {
-                return 'current';
-              } else {
-                return '3.1';
-              }
-            })(),
+            if (isVersioningDisabled) {
+              return 'current';
+            } else {
+              return '3.2';
+            }
+          })(),
 
+          //onlyIncludeVersions: ['3.2', '3.1', '3.0', '2.5', '2.3', '2.2', '2.1'],
           onlyIncludeVersions: (() => {
-              if (isVersioningDisabled) {
-                return ['current'];
-              } else {
-                return ['3.1', '3.0', '2.5', '2.3', '2.2', '2.1', '2.0', '1.19'];
-              }
-            })(),
+            if (isVersioningDisabled) {
+              return ['current'];
+            } else if (isBuildFast){
+              return [...versions.slice(0, 1), '2.5'];
+            } else {
+              return ['3.2', '3.1', '3.0', '2.5', '2.3', '2.2', '2.1'];
+            }
+          })(),
 
           versions: (() => {
-              if (isVersioningDisabled) {
-                return { current: { label: 'current' } };
-              } else {
-                return {
-                  '3.1': { label: 'Latest-3.1' },
-                  '3.0': { label: '3.0', banner: 'none' },
-                  '2.5': { label: 'Stable-2.5', banner: 'none' },
-                  '2.3': { label: '2.3', banner: 'none' },
-                  '2.2': { label: '2.2', banner: 'none' },
-                  '2.1': { label: '2.1', banner: 'none' },
-                  '2.0': { label: '2.0', banner: 'none' },
-                  '1.19': { label: '1.19', banner: 'none' },
-                };
-              }
-            })(),
-
+            if (isVersioningDisabled) {
+              return { current: { label: 'current' } };
+            } else {
+              return {
+                '3.2': { label: 'Latest-3.2' },
+                '3.1': { label: '3.1', banner: 'none' },
+                '3.0': { label: '3.0', banner: 'none' },
+                '2.5': { label: 'Stable-2.5', banner: 'none' },
+                '2.3': { label: '2.3', banner: 'none' },
+                '2.2': { label: '2.2', banner: 'none' },
+                '2.1': { label: '2.1', banner: 'none' },
+              };
+            }
+          })(),
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -110,6 +119,40 @@ const config = {
       }),
     ],
   ],
+
+  plugins: [
+    'docusaurus-plugin-hubspot',
+    [
+      "@docusaurus/plugin-content-docs",
+      {
+        path: "releasenotes",
+        id: "releasenotes",
+        routeBasePath: "releasenotes",
+        sidebarPath: "./releasenotes-sidebars.json",
+        // Edit links for English and Chinese
+        editUrl: ({locale, docPath}) => {
+          if (locale == 'en') {
+            return 'https://github.com/StarRocks/starrocks/edit/main/docs/en/' + docPath
+          } else {
+            return 'https://github.com/StarRocks/starrocks/edit/main/docs/zh/' + docPath
+          }
+        },
+      },
+    ],
+    [
+    '@docusaurus/plugin-client-redirects',
+    {
+      redirects: [
+        // /docs/oldDoc -> /docs/newDoc
+        {
+          to: '/docs/quick_start/shared-nothing',
+          from: '/docs/quick_start/deploy_with_docker',
+        },
+      ],
+    },
+    ],
+  ],
+
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
@@ -126,28 +169,86 @@ const config = {
         logo: {
           alt: 'StarRocks Logo',
           src: 'img/logo.svg',
-          href: 'https://www.starrocks.io/',
+          href: 'https://starrocks.io/',
         },
         items: [
           {
             type: 'docSidebar',
             sidebarId: 'English',
+            docsPluginId: 'default',
             position: 'left',
-            label: 'Documentation',
+            label: 'Docs',
           },
           {
             type: 'docsVersionDropdown',
+            docsPluginId: 'default',
             position: 'left',
-            dropdownActiveClassDisabled: true,
-          },
-          {
-            href: 'https://github.com/StarRocks/starrocks',
-            label: 'GitHub',
-            position: 'right',
           },
           {
             type: 'localeDropdown',
+            docsPluginId: 'default',
             position: 'left',
+          },
+          // only for production begin
+          {
+            type: 'docSidebar',
+            docsPluginId: 'releasenotes',
+            sidebarId: 'English',
+            position: 'right',
+            label: 'Release Notes',
+          },
+          // end only for production
+          {
+            href: 'https://github.com/StarRocks/starrocks',
+            position: 'right',
+            className: 'header-github-link',
+            'aria-label': 'GitHub repository',
+          },
+          {
+            type: 'dropdown',
+            label: 'Community',
+            hoverable: true,
+            className: 'EnglishOnly',
+            position: 'right',
+            items: [
+              {
+                label: 'Slack',
+                to: 'https://join.slack.com/t/starrocks/shared_invite/zt-z5zxqr0k-U5lrTVlgypRIV8RbnCIAzg',
+                className: 'header-slack-link',
+                'aria-label': 'Slack workspace',
+              },
+              {
+                label: 'Forum',
+                to: 'https://forum.starrocks.io/',
+                className: 'header-discourse-link',
+                'aria-label': 'Forum',
+              },
+            ],
+          },
+          {
+            type: 'dropdown',
+            label: '社区群',
+            hoverable: true,
+            className: 'ChineseOnly',
+            position: 'right',
+            items: [
+              {
+                label: 'StarRocks中文社区论坛',
+                to: 'https://forum.mirrorship.cn/',
+                className: 'header-chinese-forum-link',
+                'aria-label': 'StarRocks中文社区论坛',
+              },
+              {
+                label: '技术支持渠道',
+                to: 'https://docs.starrocks.io/zh/docs/project_help/slack/',
+                'aria-label': '技术支持渠道',
+              },
+            ],
+          },
+          {
+            label: 'Privacy policy',
+            position: 'right',
+            to: 'https://www.starrocks.io/product/privacy-policy',
           },
         ],
       },
@@ -155,11 +256,14 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
             items: [
               {
-                label: 'Documentation',
-                to: '/docs/introduction/StarRocks_intro',
+                label: 'StarRocks.io',
+                to: 'https://starrocks.io/',
+              },
+              {
+                label: 'Privacy policy',
+                to: 'https://www.starrocks.io/product/privacy-policy',
               },
             ],
           },
@@ -199,6 +303,11 @@ const config = {
         // Optional: path for search page that enabled by default (`false` to disable it)
         searchPagePath: 'search',
 
+      },
+      hubspot: {
+        accountId: '21782839',
+        async: false,
+        defer: false,
       },
     }),
 };
