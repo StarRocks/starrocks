ebf1225c1db9e86e7199a1c42108840574494ac8
ricky <rickif@qq.com>
2024-02-19 10:48:01 +0800

    [Feature] `files()` supports CSV format (#37875)
    Signed-off-by: ricky <rickif@qq.com>
    Signed-off-by: rickif <rickif@qq.com>
    Co-authored-by: wyb <wybb86@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/plan/PlanFragmentBuilder.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/plan/PlanFragmentBuilder.java
@@ -3347,6 +3347,8 @@ public class PlanFragmentBuilder {
             int dop = ConnectContext.get().getSessionVariable().getSinkDegreeOfParallelism();
             scanNode.setLoadInfo(-1, -1, table, new BrokerDesc(table.getProperties()), fileGroups, false, dop);
             scanNode.setUseVectorizedLoad(true);
+            // table function enable flexible column mapping by default.
+            scanNode.setFlexibleColumnMapping(true);
 
             Analyzer analyzer = new Analyzer(GlobalStateMgr.getCurrentState(), context.getConnectContext());
             analyzer.setDescTbl(context.getDescTbl());
