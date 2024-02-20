286b550f6f8f97f9186911f699aa1023d4aa788d
kangkaisen <kangkaisen@apache.org>
2024-02-20 16:56:53 +0800

    [Feature] Support trino to_char function (#41217)
    Signed-off-by: kangkaisen <kangkaisen@apache.org>
    (cherry picked from commit f308f46c4ea47596f7b4d5d5e364b73b3adde9ef)

--- fe/fe-core/src/test/java/com/starrocks/connector/parser/trino/TrinoFunctionTransformTest.java
+++ fe/fe-core/src/test/java/com/starrocks/connector/parser/trino/TrinoFunctionTransformTest.java
@@ -154,6 +154,9 @@ public class TrinoFunctionTransformTest extends TrinoTestBase {
         sql = "select format_datetime(date '2023-06-25', 'yyyyMMdd HH:mm:ss');";
         assertPlanContains(sql, "jodatime_format('2023-06-25', 'yyyyMMdd HH:mm:ss')");
 
+        sql = "select to_char(TIMESTAMP '2023-06-25 11:10:20', 'yyyyMMdd HH:mm:ss');";
+        assertPlanContains(sql, "jodatime_format('2023-06-25 11:10:20', 'yyyyMMdd HH:mm:ss')");
+
         sql = "select parse_datetime('2023-08-02 14:37:02', 'yyyy-MM-dd HH:mm:ss')";
         assertPlanContains(sql, "str_to_jodatime('2023-08-02 14:37:02', 'yyyy-MM-dd HH:mm:ss')");
 
