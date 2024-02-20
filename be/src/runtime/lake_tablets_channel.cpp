7efc597d1b3a1dae99f78d5cc354142d0ee40859
meegoo <meegoo.sr@gmail.com>
2024-02-20 11:21:11 +0800

    [BugFix] Fix data ingestion stuck when the new automatic partition and the old partition are distributed on different backends (#40638)
    Signed-off-by: meegoo <meegoo.sr@gmail.com>

--- be/src/runtime/lake_tablets_channel.cpp
+++ be/src/runtime/lake_tablets_channel.cpp
@@ -206,6 +206,7 @@ Status LakeTabletsChannel::open(const PTabletWriterOpenRequest& params, PTabletW
         _senders[params.sender_id()].has_incremental_open = true;
     } else {
         _num_remaining_senders.store(params.num_senders(), std::memory_order_release);
+        _num_initial_senders.store(params.num_senders(), std::memory_order_release);
     }
     RETURN_IF_ERROR(_create_delta_writers(params, false));
 
@@ -412,7 +413,8 @@ void LakeTabletsChannel::add_chunk(Chunk* chunk, const PTabletWriterAddChunkRequ
 
     if (close_channel) {
         _load_channel->remove_tablets_channel(_index_id);
-    } else if (request.eos() && request.wait_all_sender_close()) {
+    } else if (request.wait_all_sender_close()) {
+        _num_initial_senders.fetch_sub(1);
         std::string msg = fmt::format("LakeTabletsChannel txn_id: {} load_id: {}", _txn_id, print_id(request.id()));
         // wait for senders to be closed, may be timed out
         auto remain = request.timeout_ms();
@@ -427,6 +429,8 @@ void LakeTabletsChannel::add_chunk(Chunk* chunk, const PTabletWriterAddChunkRequ
 
 int LakeTabletsChannel::_close_sender(const int64_t* partitions, size_t partitions_size) {
     int n = _num_remaining_senders.fetch_sub(1);
+    // if sender close means data send finished, we need to decrease _num_initial_senders
+    _num_initial_senders.fetch_sub(1);
     std::lock_guard l(_dirty_partitions_lock);
     for (int i = 0; i < partitions_size; i++) {
         _dirty_partitions.insert(partitions[i]);
@@ -560,7 +564,7 @@ void LakeTabletsChannel::cancel() {
 
 StatusOr<std::unique_ptr<LakeTabletsChannel::WriteContext>> LakeTabletsChannel::_create_write_context(
         Chunk* chunk, const PTabletWriterAddChunkRequest& request, PTabletWriterAddBatchResult* response) {
-    if (chunk == nullptr && !request.eos()) {
+    if (chunk == nullptr && !request.eos() && !request.wait_all_sender_close()) {
         return Status::InvalidArgument("PTabletWriterAddChunkRequest has no chunk or eos");
     }
 
