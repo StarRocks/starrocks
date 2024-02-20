630d4478d0bb65a9246188aaec1caddd183f0616
meegoo <meegoo.sr@gmail.com>
2024-02-20 10:02:51 +0800

    [BugFix] Fix spark load load start timestamp not set (#41082)
    Signed-off-by: meegoo <meegoo.sr@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/load/loadv2/LoadJob.java
+++ fe/fe-core/src/main/java/com/starrocks/load/loadv2/LoadJob.java
@@ -204,7 +204,7 @@ public abstract class LoadJob extends AbstractTxnStateChangeCallback implements 
     // only for persistence param. see readFields() for usage
     private boolean isJobTypeRead = false;
 
-    private boolean startLoad = false;
+    protected boolean startLoad = false;
 
     // only for log replay
     public LoadJob() {
@@ -330,6 +330,10 @@ public abstract class LoadJob extends AbstractTxnStateChangeCallback implements 
 
     public long getTransactionId() {
         return transactionId;
+    }
+
+    public long getLoadStartTimestamp() {
+        return loadStartTimestamp;
     }
 
     public void initLoadProgress(TUniqueId loadId, Set<TUniqueId> fragmentIds, List<Long> relatedBackendIds) {
