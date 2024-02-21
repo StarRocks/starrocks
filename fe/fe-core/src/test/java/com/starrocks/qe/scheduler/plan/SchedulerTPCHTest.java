78a6d70093cfbfca044dfe67f50df2120af51b77
packy92 <wangchao@starrocks.com>
2024-02-21 10:27:48 +0800

    [UT] fix ut assertion not work
    Signed-off-by: packy92 <wangchao@starrocks.com>

--- fe/fe-core/src/test/java/com/starrocks/qe/scheduler/plan/SchedulerTPCHTest.java
+++ fe/fe-core/src/test/java/com/starrocks/qe/scheduler/plan/SchedulerTPCHTest.java
@@ -19,6 +19,9 @@ import com.starrocks.planner.TpchSQL;
 import com.starrocks.qe.scheduler.SchedulerTestBase;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -30,6 +33,10 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Stream;
 
+/**
+ * Attention: cases need execute in a fix order or the result may change.
+ */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class SchedulerTPCHTest extends SchedulerTestBase {
     private static final String DIRECTORY = "scheduler/tpch/";
 
@@ -43,8 +50,16 @@ public class SchedulerTPCHTest extends SchedulerTestBase {
         SchedulerTestBase.beforeClass();
     }
 
+    @ParameterizedTest(name = "Tpch.{0}")
+    @MethodSource("tpchSource")
+    @Order(1)
+    public void testTPCH(String name, String sql, String resultFile) {
+        runFileUnitTest(sql, resultFile);
+    }
+
     @ParameterizedTest(name = "Tpch2.{0}")
     @MethodSource("listTest")
+    @Order(2)
     public void testTPCHOther(String name, String file) {
         runFileUnitTest(file);
     }
@@ -62,12 +77,6 @@ public class SchedulerTPCHTest extends SchedulerTestBase {
                 .map(file -> file.getName().replace(".sql", ""))
                 .filter(s -> !TpchSQL.contains(s))
                 .map(s -> Arguments.of(s, DIRECTORY + s));
-    }
-
-    @ParameterizedTest(name = "Tpch.{0}")
-    @MethodSource("tpchSource")
-    public void testTPCH(String name, String sql, String resultFile) {
-        runFileUnitTest(sql, resultFile);
     }
 
     private static Stream<Arguments> tpchSource() {
