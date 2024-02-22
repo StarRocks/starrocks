354c869f09d83139f321bb9bf7cb21c2e541a78e
Letian Jiang <letian.jiang@outlook.com>
2024-02-22 15:14:44 +0800

    [Enhancement] Add MetadataMgr#getTableBasicInfo to avoid network interaction for external table (backport #38276) (#40860)
    Signed-off-by: Letian Jiang <letian.jiang@outlook.com>
    (cherry picked from commit 30885efab5e1924820a497dc3dc304a17b60ff0b)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/sql/analyzer/Authorizer.java

--- fe/fe-core/src/main/java/com/starrocks/service/InformationSchemaDataSource.java
+++ fe/fe-core/src/main/java/com/starrocks/service/InformationSchemaDataSource.java
@@ -17,6 +17,7 @@ package com.starrocks.service;
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
 import com.google.gson.Gson;
+import com.starrocks.catalog.BasicTable;
 import com.starrocks.catalog.Column;
 import com.starrocks.catalog.Database;
 import com.starrocks.catalog.DistributionInfo;
@@ -65,7 +66,7 @@ public class InformationSchemaDataSource {
 
     private static final String DEF = "def";
     private static final String DEFAULT_EMPTY_STRING = "";
-    private static final long DEFAULT_EMPTY_NUM = -1L;
+    public static final long DEFAULT_EMPTY_NUM = -1L;
     public static final String UTF8_GENERAL_CI = "utf8_general_ci";
 
     @NotNull
@@ -319,9 +320,9 @@ public class InformationSchemaDataSource {
                 try {
                     List<String> tableNames = metadataMgr.listTableNames(catalogName, dbName);
                     for (String tableName : tableNames) {
-                        Table table = null;
+                        BasicTable table = null;
                         try {
-                            table = metadataMgr.getTable(catalogName, dbName, tableName);
+                            table = metadataMgr.getBasicTable(catalogName, dbName, tableName);
                         } catch (Exception e) {
                             LOG.warn(e.getMessage());
                         }
@@ -372,9 +373,9 @@ public class InformationSchemaDataSource {
                             // INLINE_VIEW (use default)
                             // VIEW (use default)
                             // BROKER (use default)
+                            // EXTERNAL TABLE (use default)
                             genDefaultConfigInfo(info);
                         }
-                        // TODO(cjs): other table type (HIVE, MYSQL, ICEBERG, HUDI, JDBC, ELASTICSEARCH)
                         infos.add(info);
                     }
                 } finally {
@@ -386,7 +387,7 @@ public class InformationSchemaDataSource {
         return response;
     }
 
-    public static TTableInfo genNormalTableInfo(Table table, TTableInfo info) {
+    public static TTableInfo genNormalTableInfo(BasicTable table, TTableInfo info) {
 
         OlapTable olapTable = (OlapTable) table;
         Collection<PhysicalPartition> partitions = olapTable.getPhysicalPartitions();
