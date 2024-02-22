6424ac5cfdab627e63fb3d5f2a36b539b209ad47
Letian Jiang <letian.jiang@outlook.com>
2024-02-22 22:15:50 +0800

    [Enhancement] Make catalog a session variable (backport #41329) (#41454)

--- fe/fe-core/src/main/java/com/starrocks/qe/ConnectContext.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/ConnectContext.java
@@ -38,7 +38,6 @@ import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
-import com.starrocks.catalog.InternalCatalog;
 import com.starrocks.cluster.ClusterNamespace;
 import com.starrocks.common.DdlException;
 import com.starrocks.common.util.TimeUtils;
@@ -131,8 +130,6 @@ public class ConnectContext {
     protected MysqlCapability capability;
     // Indicate if this client is killed.
     protected volatile boolean isKilled;
-    // catalog
-    protected volatile String currentCatalog = InternalCatalog.DEFAULT_INTERNAL_CATALOG_NAME;
     // Db
     protected String currentDb = "";
     // warehouse
@@ -664,11 +661,11 @@ public class ConnectContext {
     }
 
     public String getCurrentCatalog() {
-        return currentCatalog;
+        return this.sessionVariable.getCatalog();
     }
 
     public void setCurrentCatalog(String currentCatalog) {
-        this.currentCatalog = currentCatalog;
+        this.sessionVariable.setCatalog(currentCatalog);
     }
 
     public String getCurrentWarehouse() {
