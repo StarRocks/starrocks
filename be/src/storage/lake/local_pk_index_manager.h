af002ff9d31ac1a8f6f1d82b8427fb58d98aa193
Zijie Lu <wslzj40@gmail.com>
2024-02-23 20:36:40 +0800

    [BugFix] Fix LocalPkIndexManager compilation error (#41568)
    Signed-off-by: Zijie Lu <wslzj40@gmail.com>

--- be/src/storage/lake/local_pk_index_manager.h
+++ be/src/storage/lake/local_pk_index_manager.h
@@ -49,10 +49,10 @@ public:
     static void evict(UpdateManager* update_manager, DataDir* data_dir, std::set<std::string>& tablet_ids) {}
 
     // remove pk index meta first, and if success then remove dir.
-    static Status clear_persistent_index(int64_t tablet_id) {}
+    static Status clear_persistent_index(int64_t tablet_id) { return Status::OK(); }
 
 private:
-    static bool need_evict_tablet(const std::string& tablet_pk_path) {}
+    static bool need_evict_tablet(const std::string& tablet_pk_path) { return false; }
 };
 #endif // USE_STAROS
 
