c9148723cd1fdf2abc769fae42346ff9cee8bb0b
yuzhou.zk <yuzhou.zk@alibaba-inc.com>
2024-02-26 13:24:20 +0800

    [EMR][Feature] Support to enable profile by config file of FE

--- fe/fe-core/src/main/java/com/starrocks/common/Config.java
+++ fe/fe-core/src/main/java/com/starrocks/common/Config.java
@@ -3234,6 +3234,9 @@ public class Config extends ConfigBase {
     @ConfField(mutable = true)
     public static boolean enable_emr_product_restrictions = false;
 
+    @ConfField(mutable = true)
+    public static boolean profile_enable = false;
+
     /**
      * Enable auto create tablet when creating table and add partition
      **/
