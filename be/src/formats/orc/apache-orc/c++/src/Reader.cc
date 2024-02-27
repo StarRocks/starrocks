a8a1b97da8a0e6439392f4e40801f9a65fe94a2e
Smith Cruise <chendingchao1@126.com>
2024-02-27 19:33:48 +0800

    [Enhancement] Move orc stripe's stats evaluation before load stripe footer (#41751)
    Signed-off-by: Smith Cruise <chendingchao1@126.com>

--- be/src/formats/orc/apache-orc/c++/src/Reader.cc
+++ be/src/formats/orc/apache-orc/c++/src/Reader.cc
@@ -1063,11 +1063,28 @@ void RowReaderImpl::startNextStripe() {
 
         bool skipStripe = false;
         bool skipStripeByScanRangeMisMatch = false;
-        if (sargsApplier && sargsApplier->getRowReaderFilter()) {
-            if (sargsApplier->getRowReaderFilter()->filterOnOpeningStripe(currentStripe, &currentStripeInfo)) {
+        if (sargsApplier) {
+            // check is existed RowReaderFilter
+            if (sargsApplier->getRowReaderFilter() &&
+                sargsApplier->getRowReaderFilter()->filterOnOpeningStripe(currentStripe, &currentStripeInfo)) {
                 skipStripe = true;
                 skipStripeByScanRangeMisMatch = true;
                 goto end;
+            }
+
+            // TODO(SmithCruise)
+            // We should contribute this code to apache-orc, we need to eval stripe's stats before load stripe footer
+            // Because If this stripe can skip by stripe's stats, we don't need to load stripe's footer anymore
+            // Stripe's stats is placed in orc tail's metadata
+            if (contents->metadata) {
+                const auto& currentStripeStats = contents->metadata->stripestats(static_cast<int>(currentStripe));
+                // skip this stripe after stats fail to satisfy sargs
+                uint64_t stripeRowGroupCount =
+                        (rowsInCurrentStripe + footer->rowindexstride() - 1) / footer->rowindexstride();
+                if (!sargsApplier->evaluateStripeStatistics(currentStripeStats, stripeRowGroupCount)) {
+                    skipStripe = true;
+                    goto end;
+                }
             }
         }
 
@@ -1086,19 +1103,6 @@ void RowReaderImpl::startNextStripe() {
         }
 
         if (sargsApplier) {
-            bool isStripeNeeded = true;
-            if (contents->metadata) {
-                const auto& currentStripeStats = contents->metadata->stripestats(static_cast<int>(currentStripe));
-                // skip this stripe after stats fail to satisfy sargs
-                uint64_t stripeRowGroupCount =
-                        (rowsInCurrentStripe + footer->rowindexstride() - 1) / footer->rowindexstride();
-                isStripeNeeded = sargsApplier->evaluateStripeStatistics(currentStripeStats, stripeRowGroupCount);
-            }
-            if (!isStripeNeeded) {
-                skipStripe = true;
-                goto end;
-            }
-
             // read row group statistics and bloom filters of current stripe
             loadStripeIndex();
 
