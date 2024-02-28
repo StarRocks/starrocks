e42f4fb6be0da7fa14faad7111dd3064c816b681
Smith Cruise <chendingchao1@126.com>
2024-02-28 16:20:18 +0800

    [Enhancement] Optimize broker load small orc files (#41765)
    Signed-off-by: Smith Cruise <chendingchao1@126.com>
    (cherry picked from commit f1bf90f028fd1cb57bde592c0c79cd2caaf9192e)

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -814,6 +814,10 @@ CONF_mBool(orc_coalesce_read_enable, "true");
 // Default is 8MB for tiny stripe threshold size
 CONF_Int32(orc_tiny_stripe_threshold_size, "8388608");
 
+// When the ORC file file size is smaller than orc_loading_buffer_size,
+// we'll read the whole file at once instead of reading a footer first.
+CONF_Int32(orc_loading_buffer_size, "8388608");
+
 // parquet reader
 CONF_mBool(parquet_coalesce_read_enable, "true");
 CONF_Bool(parquet_late_materialization_enable, "true");
