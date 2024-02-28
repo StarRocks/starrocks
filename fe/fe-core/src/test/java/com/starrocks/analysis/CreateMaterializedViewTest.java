6ab7db8c283fb9bf41fc0389e35d9fcd80100d40
Murphy <96611012+mofeiatwork@users.noreply.github.com>
2024-02-28 13:58:18 +0800

    [BugFix] fix str_to_map function in MV (#41628)
    Signed-off-by: Murphy <mofei@starrocks.com>
    (cherry picked from commit 121a9730c58b7027cac1d3d646dc0ae39533b551)
    Signed-off-by: Murphy <mofei@starrocks.com>
    
    # Conflicts:
    #	fe/fe-core/src/test/java/com/starrocks/analysis/CreateMaterializedViewTest.java
    #	gensrc/script/functions.py

--- fe/fe-core/src/test/java/com/starrocks/analysis/CreateMaterializedViewTest.java
+++ fe/fe-core/src/test/java/com/starrocks/analysis/CreateMaterializedViewTest.java
@@ -3845,4 +3845,29 @@ public class CreateMaterializedViewTest {
         }
         starRocksAssert.dropView("view_1");
     }
+
+    /**
+     * https://github.com/StarRocks/starrocks/issues/40862
+     */
+    @Test
+    public void testSR40862() throws Exception {
+        new MockUp<StmtExecutor>() {
+            @Mock
+            public void handleDMLStmt(ExecPlan execPlan, DmlStmt stmt) throws Exception {
+            }
+        };
+        starRocksAssert.withTable("CREATE TABLE sr_ods_test_table (\n" +
+                "id bigint(20) NOT NULL COMMENT '主键id' ,\n" +
+                "name string COMMENT '名称'\n" +
+                ")\n" +
+                "PRIMARY KEY (ID)\n" +
+                "DISTRIBUTED BY HASH(ID) " +
+                "PROPERTIES (\"replication_num\" = \"1\")"
+        );
+        starRocksAssert.withRefreshedMaterializedView("CREATE MATERIALIZED VIEW sr_dw_test_table\n" +
+                "DISTRIBUTED BY HASH(id)\n" +
+                "REFRESH ASYNC\n" +
+                "AS\n" +
+                "SELECT id,name,str_to_map(CONCAT_WS(':',id,name),';',':') as mapvalue FROM sr_ods_test_table");
+    }
 }
