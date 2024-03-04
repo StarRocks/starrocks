e6c019adb8d98a177a1303057a72e7009c66850f
Letian Jiang <letian.jiang@outlook.com>
2024-03-04 19:31:20 +0800

    fix test
    Signed-off-by: Letian Jiang <letian.jiang@outlook.com>

--- be/src/column/binary_column.cpp
+++ be/src/column/binary_column.cpp
@@ -19,6 +19,7 @@
 #endif
 
 #include "column/bytes.h"
+#include "column/const_column.h"
 #include "column/vectorized_fwd.h"
 #include "common/logging.h"
 #include "gutil/bits.h"
@@ -62,6 +63,24 @@ void BinaryColumnBase<T>::append(const Column& src, size_t offset, size_t count)
 
 template <typename T>
 void BinaryColumnBase<T>::append_selective(const Column& src, const uint32_t* indexes, uint32_t from, uint32_t size) {
+    if (src.is_constant()) {
+        auto& const_column = down_cast<const ConstColumn&>(src);
+        auto& data_column = const_column.data_column();
+        auto concrete_data_column = down_cast<const BinaryColumnBase<T>*>(data_column.get());
+        auto length = concrete_data_column->get_offset()[1] - concrete_data_column->get_offset()[0];
+        auto bytes = concrete_data_column->get_bytes();
+
+        _offsets.reserve(_offsets.size() + size);
+        _bytes.resize(_bytes.size() + size * length);
+
+        for (size_t i = 0; i < size; i++) {
+            int offset = _offsets.back();
+            _offsets.push_back(offset + length);
+            strings::memcpy_inlined(_bytes.data() + offset, bytes.data(), length);
+        }
+        return;
+    }
+
     const auto& src_column = down_cast<const BinaryColumnBase<T>&>(src);
     const auto& src_offsets = src_column.get_offset();
     const auto& src_bytes = src_column.get_bytes();
--- be/src/exec/hdfs_scanner.cpp
+++ be/src/exec/hdfs_scanner.cpp
@@ -441,18 +441,9 @@ void HdfsScannerContext::append_or_update_partition_column_to_chunk(ChunkPtr* ch
     for (size_t i = 0; i < partition_columns.size(); i++) {
         SlotDescriptor* slot_desc = partition_columns[i].slot_desc;
         DCHECK(partition_values[i]->is_constant());
-        auto* const_column = ColumnHelper::as_raw_column<ConstColumn>(partition_values[i]);
-        ColumnPtr data_column = const_column->data_column();
-
-        if (data_column->is_nullable()) {
-            auto chunk_part_column = ColumnHelper::create_column(slot_desc->type(), slot_desc->is_nullable());
-            chunk_part_column->append_nulls(row_count);
-            ck->append_or_update_column(std::move(chunk_part_column), slot_desc->id());
-        } else {
-            auto chunk_part_column = partition_values[i]->clone();
-            chunk_part_column->resize(row_count);
-            ck->append_or_update_column(std::move(chunk_part_column), slot_desc->id());
-        }
+        auto partition_value = partition_values[i]->clone();
+        partition_value->resize(row_count);
+        ck->append_or_update_column(std::move(partition_value), slot_desc->id());
     }
 }
 
