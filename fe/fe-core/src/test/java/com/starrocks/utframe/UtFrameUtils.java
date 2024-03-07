0e7b5130139effec5bffb94cb58762a22fa71a88
ABing <101158374+ABingHuang@users.noreply.github.com>
2024-03-07 14:53:41 +0800

    [BugFix] fix bugs of query dump for view and add support of query dump for mv on view (#42132)
    Signed-off-by: ABingHuang <codekhuang@163.com>

--- fe/fe-core/src/test/java/com/starrocks/utframe/UtFrameUtils.java
+++ fe/fe-core/src/test/java/com/starrocks/utframe/UtFrameUtils.java
@@ -713,6 +713,9 @@ public class UtFrameUtils {
             }
         });
         for (Map.Entry<String, String> entry : replayDumpInfo.getCreateTableStmtMap().entrySet()) {
+            if (entry.getValue().contains("CREATE MATERIALIZED VIEW")) {
+                continue;
+            }
             String dbName = entry.getKey().split("\\.")[0];
             if (!starRocksAssert.databaseExist(dbName)) {
                 starRocksAssert.withDatabase(dbName);
@@ -722,19 +725,36 @@ public class UtFrameUtils {
         }
         // create view
         for (Map.Entry<String, String> entry : replayDumpInfo.getCreateViewStmtMap().entrySet()) {
+            String normalizedViewName = entry.getKey();
+            String[] nameParts = normalizedViewName.split("\\.");
+            String dbName = nameParts[0];
+            if (!starRocksAssert.databaseExist(dbName)) {
+                starRocksAssert.withDatabase(dbName);
+            }
+            String viewName = nameParts[1];
+            starRocksAssert.useDatabase(dbName);
+            String createView = String.format("create view `%s`.`%s` as %s",
+                    dbName, viewName, replayDumpInfo.getCreateViewStmtMap().get(normalizedViewName));
+            starRocksAssert.withView(createView);
+        }
+        // create materialized views
+        for (Map.Entry<String, String> entry : replayDumpInfo.getCreateTableStmtMap().entrySet()) {
+            if (!entry.getValue().contains("CREATE MATERIALIZED VIEW")) {
+                continue;
+            }
             String dbName = entry.getKey().split("\\.")[0];
             if (!starRocksAssert.databaseExist(dbName)) {
                 starRocksAssert.withDatabase(dbName);
             }
             starRocksAssert.useDatabase(dbName);
-            String createView = "create view " + entry.getKey() + " as " + entry.getValue();
-            starRocksAssert.withView(createView);
+            starRocksAssert.withSingleReplicaAsyncMv(entry.getValue());
         }
         // mock be num
         backendId = 10002;
         for (int i = 1; i < replayDumpInfo.getBeNum(); ++i) {
             UtFrameUtils.addMockBackend(backendId++);
         }
+
         // mock be core stat
         for (Map.Entry<Long, Integer> entry : replayDumpInfo.getNumOfHardwareCoresPerBe().entrySet()) {
             BackendCoreStat.setNumOfHardwareCoresOfBe(entry.getKey(), entry.getValue());
