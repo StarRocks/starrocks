a53fea928694147f71abf94842f1be159e997285
ABing <101158374+ABingHuang@users.noreply.github.com>
2024-03-07 14:53:41 +0800

    [BugFix] fix bugs of query dump for view and add support of query dump for mv on view (#42132)
    Signed-off-by: ABingHuang <codekhuang@163.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/ReplayFromDumpTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/ReplayFromDumpTest.java
@@ -768,4 +768,12 @@ public class ReplayFromDumpTest extends ReplayFromDumpTestBase {
                 "  |  group by: 1: TIME"));
     }
 
+    @Test
+    public void testMvOnView() throws Exception {
+        String dumpInfo = getDumpInfoFromFile("query_dump/mv_on_view");
+        QueryDumpInfo queryDumpInfo = getDumpInfoFromJson(dumpInfo);
+        SessionVariable sessionVariable = queryDumpInfo.getSessionVariable();
+        Pair<QueryDumpInfo, String> replayPair = getCostPlanFragment(dumpInfo, sessionVariable);
+        Assert.assertTrue(replayPair.second, replayPair.second.contains("mv_LEAF_ACC_CUBE_SHADOW_VIEW_fb70da80"));
+    }
 }
