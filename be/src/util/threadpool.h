085ad7a2ae4b6286083c82a3edd6913206166174
PengFei Li <lpengfei2016@gmail.com>
2024-03-11 23:27:09 +0800

    Use CoreLocalCounter
    Signed-off-by: PengFei Li <lpengfei2016@gmail.com>

--- be/src/util/threadpool.h
+++ be/src/util/threadpool.h
@@ -53,6 +53,7 @@
 #include "util/bthreads/semaphore.h"
 // resolve `barrier` macro conflicts with boost/thread.hpp header file
 #undef barrier
+#include "util/metrics.h"
 #include "util/monotime.h"
 #include "util/priority_queue.h"
 
@@ -248,11 +249,11 @@ public:
 
     int max_threads() const { return _max_threads.load(std::memory_order_acquire); }
 
-    int64_t total_executed_tasks() const { return _total_executed_tasks.load(); }
+    int64_t total_executed_tasks() const { return _total_executed_tasks.value(); }
 
-    int64_t total_pending_time_ns() const { return _total_pending_time_ns.load(); }
+    int64_t total_pending_time_ns() const { return _total_pending_time_ns.value(); }
 
-    int64_t total_execute_time_ns() const { return _total_execute_time_ns.load(); }
+    int64_t total_execute_time_ns() const { return _total_execute_time_ns.value(); }
 
 private:
     friend class ThreadPoolBuilder;
@@ -379,13 +380,13 @@ private:
     std::unique_ptr<ThreadPoolToken> _tokenless;
 
     // Total number of tasks that have finished
-    std::atomic<int64_t> _total_executed_tasks;
+    CoreLocalCounter<int64_t> _total_executed_tasks{MetricUnit::NOUNIT};
 
     // Total time in nanoseconds that tasks pending in the queue.
-    std::atomic<int64_t> _total_pending_time_ns;
+    CoreLocalCounter<int64_t> _total_pending_time_ns{MetricUnit::NOUNIT};
 
     // Total time in nanoseconds to execute tasks.
-    std::atomic<int64_t> _total_execute_time_ns;
+    CoreLocalCounter<int64_t> _total_execute_time_ns{MetricUnit::NOUNIT};
 
     ThreadPool(const ThreadPool&) = delete;
     const ThreadPool& operator=(const ThreadPool&) = delete;
