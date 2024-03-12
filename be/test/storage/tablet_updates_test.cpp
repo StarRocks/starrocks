0e5d3ab9267471f0ad512bbf5be8dda5f3b3b040
zhangqiang <qiangzh95@gmail.com>
2024-03-12 13:41:55 +0800

    code format
    Signed-off-by: zhangqiang <qiangzh95@gmail.com>

--- be/test/storage/tablet_updates_test.cpp
+++ be/test/storage/tablet_updates_test.cpp
@@ -566,8 +566,8 @@ public:
         return StorageEngine::instance()->tablet_manager()->get_tablet(tablet_id, false);
     }
 
-    void SetUp() override { 
-        _compaction_mem_tracker = std::make_unique<MemTracker>(-1); 
+    void SetUp() override {
+        _compaction_mem_tracker = std::make_unique<MemTracker>(-1);
         config::enable_pk_size_tiered_compaction_strategy = false;
     }
 
