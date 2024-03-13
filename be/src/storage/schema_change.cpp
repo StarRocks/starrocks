b9385578afd810b87a053f0b4c64094014611696
zhangqiang <qiangzh95@gmail.com>
2024-03-13 21:44:32 +0800

    code format
    Signed-off-by: zhangqiang <qiangzh95@gmail.com>

--- be/src/storage/schema_change.cpp
+++ be/src/storage/schema_change.cpp
@@ -810,8 +810,7 @@ Status SchemaChangeHandler::_do_process_alter_tablet_v2(const TAlterTabletReqV2&
         if (sc_params.sc_directly) {
             status = new_tablet->updates()->convert_from(base_tablet, request_version, sc_params.chunk_changer.get());
         } else if (sc_params.sc_sorting) {
-            status = new_tablet->updates()->reorder_from(base_tablet, request_version,
-                                                         sc_params.chunk_changer.get());
+            status = new_tablet->updates()->reorder_from(base_tablet, request_version, sc_params.chunk_changer.get());
         } else {
             status = new_tablet->updates()->link_from(base_tablet.get(), request_version);
         }
