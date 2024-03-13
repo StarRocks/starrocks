8e913baf6035eaad3028c5ab510abc075e9bc2ac
Zhuhe Fang <fzhedu@gmail.com>
2024-03-13 17:20:41 +0800

    [Enhancement] opt interleaving probe to avoid bad cases (#42500)
    Signed-off-by: Zhuhe Fang <fzhedu@gmail.com>

--- be/src/exec/join_hash_map.tpp
+++ be/src/exec/join_hash_map.tpp
@@ -668,6 +668,7 @@ void JoinHashMap<LT, BuildFunc, ProbeFunc>::_search_ht(RuntimeState* state, Chun
     if (!_probe_state->has_remain) {
         _probe_state->probe_row_count = (*probe_chunk)->num_rows();
         _probe_state->active_coroutines = state->query_options().interleaving_group_size;
+        // disable adaptively interleaving if the ht may encounter seriously cache misses.
         if (state->query_options().interleaving_group_size > 0 && !_table_items->ht_cache_miss_serious()) {
             _probe_state->active_coroutines = 0;
         }
--- be/test/simd/simd_test.cpp
+++ be/test/simd/simd_test.cpp
@@ -89,4 +89,49 @@ TEST_F(SIMDTest, count_nonzero) {
     EXPECT_EQ(30u, SIMD::count_nonzero(numbers));
 }
 
+TEST_F(SIMDTest, count_zeros_int32) {
+    EXPECT_EQ(0u, SIMD::count_zero(std::vector<uint32_t>{}));
+    EXPECT_EQ(3u, SIMD::count_zero(std::vector<uint32_t>{0, 0, 0}));
+    EXPECT_EQ(1u, SIMD::count_zero(std::vector<uint32_t>{0, 1, 2}));
+    EXPECT_EQ(1u, SIMD::count_zero(std::vector<uint32_t>{11, 0, 1}));
+
+    // size greater than 64 will use SSE2 instructions.
+    std::vector<uint32_t> nums(100, 0);
+    EXPECT_EQ(100u, SIMD::count_zero(nums));
+    nums.emplace_back(1);
+    EXPECT_EQ(100u, SIMD::count_zero(nums));
+    nums.emplace_back(0);
+    EXPECT_EQ(101u, SIMD::count_zero(nums));
+    nums.emplace_back(11);
+    EXPECT_EQ(101u, SIMD::count_zero(nums));
+}
+
+TEST_F(SIMDTest, count_nonzero_int32) {
+    // size less than 64, count by loop.
+    EXPECT_EQ(0u, SIMD::count_nonzero(std::vector<uint32_t>{}));
+    EXPECT_EQ(0u, SIMD::count_nonzero(std::vector<uint32_t>{0, 0, 0}));
+    EXPECT_EQ(3u, SIMD::count_nonzero(std::vector<uint32_t>{1, 1, 1}));
+    EXPECT_EQ(3u, SIMD::count_nonzero(std::vector<uint32_t>{11, 1, 2}));
+    EXPECT_EQ(2u, SIMD::count_nonzero(std::vector<uint32_t>{0, 1, 2}));
+
+    // size greater than 64 will use SSE2 instructions.
+    std::vector<uint32_t> numbers(100, 0);
+    EXPECT_EQ(0u, SIMD::count_nonzero(numbers));
+
+    for (int i = 1; i <= 10; i++) {
+        numbers.emplace_back(1);
+    }
+    EXPECT_EQ(10u, SIMD::count_nonzero(numbers));
+
+    for (int i = 1; i <= 10; i++) {
+        numbers.emplace_back(i);
+    }
+    EXPECT_EQ(20u, SIMD::count_nonzero(numbers));
+
+    for (int i = 1; i <= 10; i++) {
+        numbers.emplace_back(i + 100);
+    }
+    EXPECT_EQ(30u, SIMD::count_nonzero(numbers));
+}
+
 } // namespace starrocks
