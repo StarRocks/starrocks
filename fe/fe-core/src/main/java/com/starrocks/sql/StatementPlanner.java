ced8e02c9bdada858576e9ffc5b87456b3ad7d8f
ABingHuang <codekhuang@163.com>
2024-03-19 20:20:35 +0800

    fix ut
    Signed-off-by: ABingHuang <codekhuang@163.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/StatementPlanner.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/StatementPlanner.java
@@ -36,7 +36,6 @@ import com.starrocks.thrift.TResultSinkType;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
