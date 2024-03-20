2e5700d204d9023dcd8212340e60297d92f86610
zhangqiang <qiangzh95@gmail.com>
2024-03-20 20:54:17 +0800

    code format
    Signed-off-by: zhangqiang <qiangzh95@gmail.com>

--- be/src/storage/persistent_index.h
+++ be/src/storage/persistent_index.h
@@ -540,8 +540,7 @@ private:
                                         std::map<size_t, IndexPage>& pages) const;
 
     Status _get_in_shard_by_page(size_t shard_idx, size_t n, const Slice* keys, IndexValue* values,
-                                 KeysInfo* found_keys_info,
-                                 std::map<size_t, std::vector<KeyInfo>>& keys_info_by_page,
+                                 KeysInfo* found_keys_info, std::map<size_t, std::vector<KeyInfo>>& keys_info_by_page,
                                  IOStat* stat) const;
 
     Status _get_in_shard(size_t shard_idx, size_t n, const Slice* keys, std::vector<KeyInfo>& keys_info,
