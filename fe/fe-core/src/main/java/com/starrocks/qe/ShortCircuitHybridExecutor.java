102bcb54ce4e84799767cbb2ca2265a2febda0c7
Zhuhe Fang <fzhedu@gmail.com>
2024-03-21 16:38:33 +0800

    fix test
    Signed-off-by: Zhuhe Fang <fzhedu@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/ShortCircuitHybridExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/ShortCircuitHybridExecutor.java
@@ -95,7 +95,7 @@ public class ShortCircuitHybridExecutor extends ShortCircuitExecutor {
             PBackendService service = BrpcProxy.getBackendService(beAddress);
             try {
                 PExecShortCircuitRequest pRequest = new PExecShortCircuitRequest();
-                pRequest.setRequest(tRequest, "compact");
+                pRequest.setRequest(tRequest, protocol);
                 watch.start();
                 Future<PExecShortCircuitResult> future = service.execShortCircuit(pRequest);
                 PExecShortCircuitResult shortCircuitResult = future.get(
