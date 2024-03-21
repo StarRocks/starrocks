96025094ed7db882cd96840747eafb6d0e3e1772
Dejun Xia <xiadejun@starrocks.com>
2024-03-21 19:21:36 +0800

    Fix init cidr in fqdn mode
    Signed-off-by: Dejun Xia <xiadejun@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/service/FrontendOptions.java
+++ fe/fe-core/src/main/java/com/starrocks/service/FrontendOptions.java
@@ -153,6 +153,7 @@ public class FrontendOptions {
     @VisibleForTesting
     static void initAddrUseFqdn(List<InetAddress> addrs) {
         useFqdn = true;
+        analyzePriorityCidrs();
         String fqdn = null;
 
         if (PRIORITY_CIDRS.isEmpty()) {
