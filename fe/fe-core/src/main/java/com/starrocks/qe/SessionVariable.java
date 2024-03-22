d2d6ee7954b25c93db2c9c19e6ecc4d53cdda9cd
yanz <dirtysalt1987@gmail.com>
2024-03-22 14:27:46 -0700

    disable it by default
    Signed-off-by: yanz <dirtysalt1987@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
@@ -1500,7 +1500,7 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
     private int connectorIoTasksPerScanOperator = 16;
 
     @VariableMgr.VarAttr(name = ENABLE_CONNECTOR_ADAPTIVE_IO_TASKS)
-    private boolean enableConnectorAdaptiveIoTasks = true;
+    private boolean enableConnectorAdaptiveIoTasks = false;
 
     @VariableMgr.VarAttr(name = ENABLE_CONNECTOR_SPLIT_IO_TASKS)
     private boolean enableConnectorSplitIoTasks = false;
