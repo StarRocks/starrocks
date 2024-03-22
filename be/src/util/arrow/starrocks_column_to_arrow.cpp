884918d023d09c772d1f24510d9eae27f3b6901a
PengFei Li <lpengfei2016@gmail.com>
2024-03-22 15:15:14 +0800

    Fix test
    Signed-off-by: PengFei Li <lpengfei2016@gmail.com>

--- be/src/util/arrow/starrocks_column_to_arrow.cpp
+++ be/src/util/arrow/starrocks_column_to_arrow.cpp
@@ -422,12 +422,14 @@ struct ColumnToArrowConverter<LT, AT, is_nullable, ConvMapGuard<LT, AT>> {
 
         auto& key_type_desc = type_desc.children[0];
         auto& key_arrow_type = arrow_type->field(0)->type();
-        auto key_func = resolve_convert_func(key_type_desc.type, key_arrow_type->id(), data_column->is_nullable());
+        auto key_func = resolve_convert_func(key_type_desc.type, key_arrow_type->id(),
+                                             data_column->keys_column()->is_nullable());
         column_context->child_columns.push_back({key_type_desc, key_arrow_type, key_func});
 
         auto& value_type_desc = type_desc.children[1];
         auto& value_arrow_type = arrow_type->field(1)->type();
-        auto value_func = resolve_convert_func(key_type_desc.type, key_arrow_type->id(), data_column->is_nullable());
+        auto value_func = resolve_convert_func(value_type_desc.type, value_arrow_type->id(),
+                                               data_column->values_column()->is_nullable());
         column_context->child_columns.push_back({value_type_desc, value_arrow_type, value_func});
 
         return arrow::Status::OK();
