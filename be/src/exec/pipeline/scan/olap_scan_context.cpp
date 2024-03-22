e99ffaff90b823e369f1421bf1b34dfbf1b3de25
trueeyu <lxhhust350@qq.com>
2024-03-22 20:33:39 +0800

    [BugFix] Fix the has_output check bug of scan operator (#42994)
    Signed-off-by: trueeyu <lxhhust350@qq.com>
    (cherry picked from commit d9d9c7045341778d010ca55fc0e652ba4b96c01e)
    
    # Conflicts:
    #	be/src/exec/pipeline/scan/olap_scan_prepare_operator.cpp
    #	be/src/testutil/sync_point.h
    #	be/test/CMakeLists.txt

--- be/src/exec/pipeline/scan/olap_scan_context.cpp
+++ be/src/exec/pipeline/scan/olap_scan_context.cpp
@@ -68,6 +68,8 @@ Status OlapScanContext::capture_tablet_rowsets(const std::vector<TInternalScanRa
 
 Status OlapScanContext::parse_conjuncts(RuntimeState* state, const std::vector<ExprContext*>& runtime_in_filters,
                                         RuntimeFilterProbeCollector* runtime_bloom_filters) {
+    TEST_ERROR_POINT("OlapScanContext::parse_conjuncts");
+
     const TOlapScanNode& thrift_olap_scan_node = _scan_node->thrift_olap_scan_node();
     const TupleDescriptor* tuple_desc = state->desc_tbl().get_tuple_descriptor(thrift_olap_scan_node.tuple_id);
 
