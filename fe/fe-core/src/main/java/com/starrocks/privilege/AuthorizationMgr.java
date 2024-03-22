a7265f6b21312fe72bc778a07579ea83cf31503a
yiming <107105845+nshangyiming@users.noreply.github.com>
2024-03-22 10:22:10 +0800

    [Enhancement] Refine unauthorized error message when calling http api (#42856)
    Signed-off-by: Dejun Xia <xiadejun@starrocks.com>
    (cherry picked from commit 1b790dff8bf393f545f402c2f7da15e7363eca13)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/common/ErrorCode.java

--- fe/fe-core/src/main/java/com/starrocks/privilege/AuthorizationMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/privilege/AuthorizationMgr.java
@@ -58,9 +58,9 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -90,7 +90,8 @@ public class AuthorizationMgr {
             CacheBuilder.newBuilder()
                     .maximumSize(MAX_NUM_CACHED_MERGED_PRIVILEGE_COLLECTION)
                     .expireAfterAccess(CACHED_MERGED_PRIVILEGE_COLLECTION_EXPIRE_MIN, TimeUnit.MINUTES)
-                    .build(new CacheLoader<Pair<UserIdentity, Set<Long>>, PrivilegeCollectionV2>() {
+                    .build(new CacheLoader<>() {
+                        @NotNull
                         @Override
                         public PrivilegeCollectionV2 load(@NotNull Pair<UserIdentity, Set<Long>> userIdentitySetPair)
                                 throws Exception {
@@ -955,9 +956,7 @@ public class AuthorizationMgr {
     public Set<UserIdentity> getAllUserIdentities() {
         userReadLock();
         try {
-            List<String> users = Lists.newArrayList();
-            Set<UserIdentity> userIdentities = userToPrivilegeCollection.keySet();
-            return userIdentities;
+            return userToPrivilegeCollection.keySet();
         } finally {
             userReadUnlock();
         }
@@ -1418,6 +1417,32 @@ public class AuthorizationMgr {
         }
     }
 
+    public List<String> getRoleNamesByRoleIds(Set<Long> roleIds) {
+        List<String> roleNames = new ArrayList<>();
+        if (roleIds != null) {
+            for (Long roleId : roleIds) {
+                RolePrivilegeCollectionV2 roleCollection = getRolePrivilegeCollection(roleId);
+                if (roleCollection != null) {
+                    roleNames.add(roleCollection.getName());
+                }
+            }
+        }
+
+        return roleNames;
+    }
+
+    public List<String> getInactivatedRoleNamesByUser(UserIdentity userIdentity, List<String> activatedRoleNames) {
+        List<String> inactivatedRoleNames = new ArrayList<>();
+        try {
+            inactivatedRoleNames = getRoleNamesByUser(userIdentity);
+        } catch (PrivilegeException e) {
+            // ignore exception
+        }
+        inactivatedRoleNames.removeAll(activatedRoleNames);
+
+        return inactivatedRoleNames;
+    }
+
     // used in executing `set role` statement
     public Long getRoleIdByNameAllowNull(String name) {
         roleReadLock();
@@ -1456,7 +1481,7 @@ public class AuthorizationMgr {
      * remove invalid object periodically
      * <p>
      * lock order should always be:
-     * AuthenticationManager.lock -> AuthorizationManager.userLock -> AuthorizationManager.roleLock
+     * `AuthenticationManager.lock` -> `AuthorizationManager.userLock` -> `AuthorizationManager.roleLock`
      */
     public void removeInvalidObject() {
         userWriteLock();
@@ -1880,11 +1905,7 @@ public class AuthorizationMgr {
             // 1 json for myself
             AuthorizationMgr ret = reader.readJson(AuthorizationMgr.class);
             ret.globalStateMgr = globalStateMgr;
-            if (provider == null) {
-                ret.provider = new DefaultAuthorizationProvider();
-            } else {
-                ret.provider = provider;
-            }
+            ret.provider = Objects.requireNonNullElseGet(provider, DefaultAuthorizationProvider::new);
             ret.initBuiltinRolesAndUsers();
 
             // 1 json for num user
@@ -1924,7 +1945,6 @@ public class AuthorizationMgr {
                 ret.roleIdToPrivilegeCollection.put(roleId, collection);
             }
 
-            assert ret != null; // can't be NULL
             LOG.info("loaded {} users, {} roles",
                     ret.userToPrivilegeCollection.size(), ret.roleIdToPrivilegeCollection.size());
 
@@ -1955,19 +1975,13 @@ public class AuthorizationMgr {
             writer.writeJson(this);
             // 1 json for num user
             writer.writeJson(userToPrivilegeCollection.size());
-            Iterator<Map.Entry<UserIdentity, UserPrivilegeCollectionV2>> iterator =
-                    userToPrivilegeCollection.entrySet().iterator();
-            while (iterator.hasNext()) {
-                Map.Entry<UserIdentity, UserPrivilegeCollectionV2> entry = iterator.next();
+            for (Map.Entry<UserIdentity, UserPrivilegeCollectionV2> entry : userToPrivilegeCollection.entrySet()) {
                 writer.writeJson(entry.getKey());
                 writer.writeJson(entry.getValue());
             }
             // 1 json for num roles
             writer.writeJson(roleIdToPrivilegeCollection.size());
-            Iterator<Map.Entry<Long, RolePrivilegeCollectionV2>> roleIter =
-                    roleIdToPrivilegeCollection.entrySet().iterator();
-            while (roleIter.hasNext()) {
-                Map.Entry<Long, RolePrivilegeCollectionV2> entry = roleIter.next();
+            for (Map.Entry<Long, RolePrivilegeCollectionV2> entry : roleIdToPrivilegeCollection.entrySet()) {
                 RolePrivilegeCollectionV2 value = entry.getValue();
                 // Avoid newly added PEntryObject type corrupt forward compatibility,
                 // since built-in roles are always initialized on startup, we don't need to persist them.
