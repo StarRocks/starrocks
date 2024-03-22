25ef1b97bb9d5bfc3e37c48c1cfd270294f6bfc2
liubotao <316945435@qq.com>
2024-03-22 17:03:06 +0800

    remove unionOp isUnionAll conditional
    Signed-off-by: liubotao <316945435@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/UnionToValuesRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/UnionToValuesRule.java
@@ -105,7 +105,7 @@ public class UnionToValuesRule extends TransformationRule {
             }
         }
 
-        if (otherChildren.isEmpty() && !newRows.isEmpty() && unionOp.isUnionAll()) {
+        if (otherChildren.isEmpty() && !newRows.isEmpty()) {
             LogicalValuesOperator newValuesOperator =
                     new LogicalValuesOperator.Builder().setColumnRefSet(unionOp.getOutputColumnRefOp()).setRows(newRows)
                             .setLimit(unionOp.getLimit())
