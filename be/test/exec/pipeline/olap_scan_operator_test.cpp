2e80927e03d1b715e911e008b921f8a1e84427b9
trueeyu <lxhhust350@qq.com>
2024-03-25 13:09:25 +0800

    fix
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/test/exec/pipeline/olap_scan_operator_test.cpp
+++ be/test/exec/pipeline/olap_scan_operator_test.cpp
@@ -14,8 +14,8 @@
 
 #include "exec/pipeline/scan/olap_scan_operator.h"
 
-#include "exec/olap_scan_node.h"
 #include "exec/pipeline/scan/olap_scan_prepare_operator.h"
+#include "exec/vectorized/olap_scan_node.h"
 #include "gtest/gtest.h"
 #include "runtime/descriptors.h"
 #include "testutil/sync_point.h"
