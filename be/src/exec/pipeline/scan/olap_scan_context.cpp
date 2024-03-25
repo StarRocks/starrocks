23e39205a9cc5c4adca3323daf348c31a9d5c3fa
trueeyu <lxhhust350@qq.com>
2024-03-25 13:11:32 +0800

    fix
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/exec/pipeline/scan/olap_scan_context.cpp
+++ be/src/exec/pipeline/scan/olap_scan_context.cpp
@@ -5,6 +5,7 @@
 #include "exec/vectorized/olap_scan_node.h"
 #include "exprs/vectorized/runtime_filter_bank.h"
 #include "storage/tablet.h"
+#include "testutil/sync_point.h"
 
 namespace starrocks::pipeline {
 
