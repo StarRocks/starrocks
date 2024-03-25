81dfa242149438f256a820c68af785641e8de877
trueeyu <lxhhust350@qq.com>
2024-03-25 13:47:49 +0800

    fix
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/exec/pipeline/scan/scan_operator.cpp
+++ be/src/exec/pipeline/scan/scan_operator.cpp
@@ -25,9 +25,9 @@
 #include "exec/workgroup/work_group.h"
 #include "runtime/current_thread.h"
 #include "runtime/exec_env.h"
+#include "testutil/sync_point.h"
 #include "util/debug/query_trace.h"
 #include "util/runtime_profile.h"
-#include "testutil/sync_point.h"
 
 namespace starrocks::pipeline {
 
