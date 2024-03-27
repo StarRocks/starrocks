4735c64a4e07bb8562e6ff0897bf5f7777113b1b
trueeyu <lxhhust350@qq.com>
2024-03-27 18:15:13 +0800

    fix
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/SkewJoinTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/SkewJoinTest.java
@@ -32,7 +32,7 @@ public class SkewJoinTest extends PlanTestBase {
     @BeforeClass
     public static void beforeClass() throws Exception {
         PlanTestBase.beforeClass();
-        ConnectorPlanTestBase.mockCatalog(connectContext, temp.newFolder().toURI().toString());
+        ConnectorPlanTestBase.mockCatalog(connectContext);
         starRocksAssert.withTable("create table struct_tbl(c0 INT, " +
                 "c1 struct<a int, b array<struct<a int, b int>>>," +
                 "c2 struct<a int, b int>," +
@@ -148,10 +148,10 @@ public class SkewJoinTest extends PlanTestBase {
         String sqlPlan = getFragmentPlan(sql);
         assertCContains(sqlPlan, "  1:Project\n" +
                 "  |  <slot 1> : 1: c0\n" +
-                "  |  <slot 10> : CASE WHEN 2: c1.a[false] IS NULL THEN round(rand() * 1000.0) WHEN 2: c1.a[false] " +
-                "IN (1, 2) THEN round(rand() * 1000.0) ELSE 0 END\n" +
-                "  |  <slot 19> : 2: c1.a[false]\n" +
-                "  |  <slot 21> : 3: c2.a[false]");
+                "  |  <slot 10> : CASE WHEN 2: c1.a IS NULL THEN 26: " +
+                "round WHEN 2: c1.a IN (1, 2) THEN 26: round ELSE 0 END\n" +
+                "  |  <slot 19> : 2: c1.a\n" +
+                "  |  <slot 21> : 3: c2.a");
     }
 
     @Test
