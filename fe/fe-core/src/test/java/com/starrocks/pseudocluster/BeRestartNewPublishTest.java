d7d1e0ddde87c05506a73a373ea23007e0f5b66e
Binglin Chang <decstery@gmail.com>
2024-03-27 18:13:39 +0800

    [UT] Remove BeRestartNewPublishTest (#43242)
    Why I'm doing:
    BeRestartNewPublishTest takes long time to run, and there is already a BeRestartTest with similar function, and new publish is not actively used.
    
    What I'm doing:
    Remove BeRestartNewPublishTest
    
    Signed-off-by: Binglin Chang <decstery@gmail.com>
    (cherry picked from commit 71f8a4cf6710c990a04aabe775a478354a985aed)

--- fe/fe-core/src/test/java/com/starrocks/pseudocluster/BeRestartNewPublishTest.java
+++
@@ -1,8 +0,0 @@
-// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Inc.
-package com.starrocks.pseudocluster;
-
-public class BeRestartNewPublishTest extends BeRestartTest {
-    public BeRestartNewPublishTest() {
-        newPublish = true;
-    }
-}
