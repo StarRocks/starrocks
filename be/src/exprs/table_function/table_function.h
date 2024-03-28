44657a2ff450734c4796b54d4982fe4e71edd8a9
zombee0 <ewang2027@gmail.com>
2024-03-28 11:01:56 +0800

    [Feature] impl left join unnest on true (#42924)
    Signed-off-by: zombee0 <ewang2027@gmail.com>

--- be/src/exprs/table_function/table_function.h
+++ be/src/exprs/table_function/table_function.h
@@ -39,6 +39,10 @@ public:
 
     int64_t get_offset() { return _offset; }
 
+    void set_is_left_join(bool is_left_join) { this->_is_left_join = is_left_join; }
+
+    bool get_is_left_join() { return _is_left_join; }
+
     // How many rows of `get_columns()` have been processed/consumed by the table function.
     //
     // If `processed_rows()` < `input_rows()`, the table function will be invoked again with the same parameter columns.
@@ -72,6 +76,9 @@ private:
     int64_t _offset = 0;
 
     Status _status;
+
+    // used to identify left join for table function
+    bool _is_left_join = false;
 };
 
 class TableFunction {
