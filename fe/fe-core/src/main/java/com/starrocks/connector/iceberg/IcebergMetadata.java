c275a7054ee8de066668096230306a86dc4e0d22
Letian Jiang <letian.jiang@outlook.com>
2024-04-01 14:58:01 +0800

    update
    Signed-off-by: Letian Jiang <letian.jiang@outlook.com>

--- fe/fe-core/src/main/java/com/starrocks/connector/iceberg/IcebergMetadata.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/iceberg/IcebergMetadata.java
@@ -98,9 +98,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
-import java.net.URLDecoder;
 import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
