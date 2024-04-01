eeb47ba7c1eb0841483c7608a131ce08e15471bb
gengjun-git <gengjun@starrocks.com>
2024-04-01 10:49:15 +0800

    resolve
    Signed-off-by: gengjun-git <gengjun@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/clone/TabletChecker.java
+++ fe/fe-core/src/main/java/com/starrocks/clone/TabletChecker.java
@@ -74,7 +74,6 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
