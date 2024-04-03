ef8f40574d71eb5ba23a71dfdc9e7368c2acfc26
liubotao <316945435@qq.com>
2024-04-03 13:00:15 +0800

    remove unuse import
    Signed-off-by: liubotao <316945435@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/ConnectProcessor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/ConnectProcessor.java
@@ -96,7 +96,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.IntStream;
 
 /**
  * Process one mysql connection, receive one pakcet, process, send one packet.
