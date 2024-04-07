b90989565029eb105a711c88a7224c04789ce2ca
Seaven <seaven_7@qq.com>
2024-04-07 14:29:06 +0800

    update
    Signed-off-by: Seaven <seaven_7@qq.com>

--- be/test/exprs/flat_json_functions_test.cpp
+++ be/test/exprs/flat_json_functions_test.cpp
@@ -89,7 +89,7 @@ INSTANTIATE_TEST_SUITE_P(FlatJsonPathDeriver, FlatJsonDeriverPaths,
         std::make_tuple(R"({ "k1": "v1", "k2": [3,4,5], "k3": 1, "k4": 1.2344 })",  
                         R"({ "k1": "abc", "k2": [11,123,54], "k3": 23423, "k4": 1.2344 })", 
                         std::vector<std::string> {"k3", "k4", "k1", "k2"}, 
-                        std::vector<LogicalType> {TYPE_BIGINT, TYPE_DOUBLE, TYPE_VARCHAR, TYPE_JSON}),
+                        std::vector<LogicalType> {TYPE_LARGEINT, TYPE_DOUBLE, TYPE_VARCHAR, TYPE_JSON}),
         std::make_tuple(R"({ "k1": 1, "k2": "a" })", R"({ "k1": 3, "k2": null })", std::vector<std::string> {"k1", "k2"}, std::vector<LogicalType> {TYPE_SMALLINT, TYPE_JSON}),
         std::make_tuple(R"({ "k1": 1, "k2": 2 })", R"({ "k1": 3, "k2": 4 })", std::vector<std::string> {"k1", "k2"}, std::vector<LogicalType> {TYPE_SMALLINT, TYPE_SMALLINT})
 
