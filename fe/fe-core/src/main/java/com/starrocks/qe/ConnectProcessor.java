d5c799f1e01c0d2ae77922bb37e819b29e199e06
Xueyan Li <astralidea@163.com>
2024-04-08 11:21:52 +0800

    [Feature] Support log profile to file (backport #43652) (#43690)
    Signed-off-by: Astralidea <astralidea@163.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/ConnectProcessor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/ConnectProcessor.java
@@ -35,6 +35,7 @@
 package com.starrocks.qe;
 
 import com.google.common.base.Strings;
+import com.google.gson.Gson;
 import com.starrocks.catalog.Column;
 import com.starrocks.catalog.Database;
 import com.starrocks.catalog.Table;
@@ -95,6 +96,10 @@ public class ConnectProcessor {
     private ByteBuffer packetBuf;
 
     private StmtExecutor executor = null;
+
+    private static final Logger PROFILE_LOG = LogManager.getLogger("profile");
+
+    private static final Gson GSON = new Gson();
 
     public ConnectProcessor(ConnectContext context) {
         this.ctx = context;
@@ -292,6 +297,11 @@ public class ConnectProcessor {
             queryDetail.setCpuCostNs(statistics.cpuCostNs == null ? -1 : statistics.cpuCostNs);
             queryDetail.setMemCostBytes(statistics.memCostBytes == null ? -1 : statistics.memCostBytes);
             queryDetail.setSpillBytes(statistics.spillBytes == null ? -1 : statistics.spillBytes);
+        }
+
+        if (Config.enable_profile_log) {
+            String jsonString = GSON.toJson(queryDetail);
+            PROFILE_LOG.info(jsonString);
         }
 
         QueryDetailQueue.addQueryDetail(queryDetail);
