2537360939f4fc0795059b4e76af5490fbea44fb
wyb <wybb86@gmail.com>
2024-04-08 18:05:27 +0800

    Fix comment and add ut
    Signed-off-by: wyb <wybb86@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/planner/FileScanNode.java
+++ fe/fe-core/src/main/java/com/starrocks/planner/FileScanNode.java
@@ -101,6 +101,7 @@ import java.util.PriorityQueue;
 import java.util.Random;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.starrocks.catalog.DefaultExpr.SUPPORTED_DEFAULT_FNS;
 
@@ -505,8 +506,14 @@ public class FileScanNode extends LoadScanNode {
         Preconditions.checkState(fileStatusesList.size() == fileGroups.size());
 
         if (isLoad() && filesAdded == 0) {
-            List<String> filePaths =
+            // return at most 3 paths to users
+            int limit = 3;
+            List<String> allFilePaths =
                     fileGroups.stream().map(BrokerFileGroup::getFilePaths).flatMap(List::stream).collect(Collectors.toList());
+            List<String> filePaths = Lists.newArrayList(allFilePaths.subList(0, limit));
+            if (allFilePaths.size() > limit) {
+                filePaths.add("...");
+            }
             ErrorReport.reportAnalysisException(ErrorCode.ERR_NO_FILES_FOUND, String.join(", ", filePaths));
         }
 
