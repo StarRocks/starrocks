7e3adc21b9b70ae595255a47a4095640fb598070
stephen <91597003+stephen-shelby@users.noreply.github.com>
2024-04-09 13:54:07 +0800

    [BugFix] fix graceful exit crash when query iceberg eq-delete parquet file (backport #43707) (#43771)
    Signed-off-by: stephen <stephen5217@163.com>

--- be/src/exec/iceberg/iceberg_delete_builder.cpp
+++ be/src/exec/iceberg/iceberg_delete_builder.cpp
@@ -221,7 +221,7 @@ Status ParquetEqualityDeleteBuilder::build(const std::string& timezone, const st
     scanner_ctx->iceberg_schema = iceberg_equal_delete_schema;
     scanner_ctx->materialized_columns = std::move(columns);
     scanner_ctx->scan_ranges = scan_ranges;
-    scanner_ctx->lazy_column_coalesce_counter = new std::atomic<int32_t>(0);
+    scanner_ctx->lazy_column_coalesce_counter = &_lazy_column_coalesce_counter;
     RETURN_IF_ERROR(reader->init(scanner_ctx.get()));
 
     while (true) {
