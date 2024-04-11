921847bd4b9b120f78bdb79ca78aec3332ae5a85
ABing <101158374+ABingHuang@users.noreply.github.com>
2024-04-11 19:19:32 +0800

    [Enhancement] enhance view delta rewrite to support join derive rewrite (#43861)
    Signed-off-by: ABingHuang <codekhuang@163.com>

--- fe/fe-core/src/test/java/com/starrocks/planner/MaterializedViewTest.java
+++ fe/fe-core/src/test/java/com/starrocks/planner/MaterializedViewTest.java
@@ -2179,6 +2179,58 @@ public class MaterializedViewTest extends MaterializedViewTestBase {
     }
 
     @Test
+    public void testViewDeltaJoinUKFK16() {
+        // set join derive rewrite in view delta
+        String mv = "select emps.empid, emps.deptno, dependents.name from emps\n"
+                + "left outer join depts b on (emps.deptno=b.deptno)\n"
+                + "left outer join dependents using (empid)";
+
+        String query = "select emps.empid, dependents.name from emps\n"
+                + "left outer join dependents using (empid)\n"
+                + "where dependents.name = 'name1'";
+        testRewriteOK(mv, query);
+    }
+
+    @Test
+    public void testViewDeltaJoinUKFK17() {
+        // set join derive rewrite in view delta
+        String mv = "select emps.empid, emps.deptno, dependents.name from emps\n"
+                + "left outer join depts b on (emps.deptno=b.deptno)\n"
+                + "left outer join dependents using (empid)";
+
+        String query = "select emps.empid, dependents.name from emps\n"
+                + " join dependents using (empid)\n"
+                + "where dependents.name = 'name1'";
+        testRewriteOK(mv, query);
+    }
+
+    @Test
+    public void testViewDeltaJoinUKFK18() {
+        // set join derive rewrite in view delta
+        String mv = "select emps.empid, emps.deptno, dependents.name from emps\n"
+                + "left outer join depts b on (emps.deptno=b.deptno)\n"
+                + "full outer join dependents using (empid)";
+
+        String query = "select emps.empid, dependents.name from emps\n"
+                + " full join dependents using (empid)\n"
+                + "where emps.empid = '1'";
+        testRewriteOK(mv, query);
+    }
+
+    @Test
+    public void testViewDeltaJoinUKFK19() {
+        // set join derive rewrite in view delta
+        String mv = "select emps.empid, emps.deptno, dependents.name from emps\n"
+                + "left outer join depts b on (emps.deptno=b.deptno)\n"
+                + "full outer join dependents using (empid)";
+
+        String query = "select emps.empid, dependents.name from emps\n"
+                + " inner join dependents using (empid)\n"
+                + "where emps.empid = '1'";
+        testRewriteOK(mv, query);
+    }
+
+    @Test
     public void testViewDeltaJoinUKFKInMV1() {
         String mv = "select emps.empid, emps.deptno, dependents.name from emps\n"
                 + "join dependents using (empid)";
