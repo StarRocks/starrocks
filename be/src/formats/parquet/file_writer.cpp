49a66291dea1202aa428998574857e7a175f2ffc
Letian Jiang <letian.jiang@outlook.com>
2024-04-11 21:16:04 +0800

    update
    Signed-off-by: Letian Jiang <letian.jiang@outlook.com>

--- be/src/formats/parquet/file_writer.cpp
+++ be/src/formats/parquet/file_writer.cpp
@@ -35,6 +35,7 @@
 #include "util/priority_thread_pool.hpp"
 #include "util/runtime_profile.h"
 #include "util/slice.h"
+#include "utils.h"
 
 namespace starrocks::parquet {
 
@@ -272,7 +273,7 @@ arrow::Result<::parquet::schema::NodePtr> ParquetBuildHelper::_make_schema_node(
     case TYPE_DECIMAL128: {
         return ::parquet::schema::PrimitiveNode::Make(
                 name, rep_type, ::parquet::LogicalType::Decimal(type_desc.precision, type_desc.scale),
-                ::parquet::Type::FIXED_LEN_BYTE_ARRAY, 16, file_column_id.field_id);
+                ::parquet::Type::FIXED_LEN_BYTE_ARRAY, decimal_precision_to_byte_count(type_desc.precision), file_column_id.field_id);
     }
     case TYPE_STRUCT: {
         DCHECK(type_desc.children.size() == type_desc.field_names.size());
