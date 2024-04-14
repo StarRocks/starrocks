18438cb6097810c1977489107539a7b86754c925
zhangqiang <qiangzh95@gmail.com>
2024-04-14 19:41:09 +0800

    fix ut
    Signed-off-by: zhangqiang <qiangzh95@gmail.com>

--- be/test/storage/persistent_index_test.cpp
+++ be/test/storage/persistent_index_test.cpp
@@ -1964,7 +1964,7 @@ TEST_P(PersistentIndexTest, test_bloom_filter_for_pindex) {
         StorageEngine::instance()->update_manager()->set_keep_pindex_bf(true);
         std::vector<IndexValue> small_get_values(1);
         ASSERT_OK(index.get(1, key_slices.data(), small_get_values.data()));
-        ASSERT_EQ(values[i].get_value() * 4, small_get_values[0].get_value());
+        ASSERT_EQ(values[0].get_value() * 4, small_get_values[0].get_value());
         index.test_calc_memory_usage();
         small_get_values.clear();
         for (int i = 0; i < N; i++) {
