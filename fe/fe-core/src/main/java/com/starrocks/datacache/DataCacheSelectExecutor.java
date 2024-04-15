950aae36260c28e5c9582678392e82712b0520ce
Gavin <yangguansuo@starrocks.com>
2024-04-15 13:12:06 +0000

    [Feature] Support evicting old cache items with a given probability option to avoid frequet cache replacement.
    Signed-off-by: Gavin <yangguansuo@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/datacache/DataCacheSelectExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/datacache/DataCacheSelectExecutor.java
@@ -47,6 +47,7 @@ public class DataCacheSelectExecutor {
         // make sure all accessed data must be cached
         tmpSessionVariable.setEnableDataCacheAsyncPopulateMode(false);
         tmpSessionVariable.setEnableDataCacheIOAdaptor(false);
+        tmpSessionVariable.setDataCacheEvictProbability(100);
         connectContext.setSessionVariable(tmpSessionVariable);
 
         InsertStmt insertStmt = statement.getInsertStmt();
