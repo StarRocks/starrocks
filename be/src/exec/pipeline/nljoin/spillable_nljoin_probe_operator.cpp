3e1cdd5b1c4032c7ab6f3467cf1c4144014b0252
stdpain <34912776+stdpain@users.noreply.github.com>
2024-04-18 19:23:11 +0800

    [BugFix] Add protect code to fix NL join chunk stream NPE (#44314)
    Signed-off-by: stdpain <drfeng08@gmail.com>

--- be/src/exec/pipeline/nljoin/spillable_nljoin_probe_operator.cpp
+++ be/src/exec/pipeline/nljoin/spillable_nljoin_probe_operator.cpp
@@ -149,11 +149,19 @@ bool SpillableNLJoinProbeOperator::is_finished() const {
 }
 
 bool SpillableNLJoinProbeOperator::has_output() const {
+    if (!is_ready()) {
+        DCHECK(false) << "is_ready() must be true before call has_output";
+        return false;
+    }
     RETURN_TRUE_IF_SPILL_TASK_ERROR(_spiller);
     return !_is_current_build_probe_finished() && _chunk_stream && _chunk_stream->has_output();
 }
 
 bool SpillableNLJoinProbeOperator::need_input() const {
+    if (!is_ready()) {
+        DCHECK(false) << "is_ready() must be true before call has_output";
+        return false;
+    }
     return _prober.probe_finished() && _is_current_build_probe_finished();
 }
 
