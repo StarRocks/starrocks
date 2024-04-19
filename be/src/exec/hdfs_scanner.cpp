dfdb8db4c555810c5fd002e4770eea179fc8e8c2
dirtysalt <dirtysalt1987@gmail.com>
2024-04-19 08:18:04 -0700

    [BugFix] fix scan operator early quit when some splitted morsels not handled (#44388)
    (cherry picked from commit 5ac845ebef1922694d5f32adfcb1778441bde889)
    
    # Conflicts:
    #	be/src/exec/hdfs_scanner.cpp

--- be/src/exec/hdfs_scanner.cpp
+++ be/src/exec/hdfs_scanner.cpp
@@ -184,11 +184,25 @@ Status HdfsScanner::open(RuntimeState* runtime_state) {
     RETURN_IF_ERROR(do_open(runtime_state));
     RETURN_IF_ERROR(_mor_processor->build_hash_table(runtime_state));
     _opened = true;
-    VLOG_FILE << "open file success: " << _scanner_params.path;
+    VLOG_FILE << "open file success: " << _scanner_params.path << ", scan range = ["
+              << _scanner_params.scan_range->offset << ","
+              << (_scanner_params.scan_range->length + _scanner_params.scan_range->offset) << "]";
     return Status::OK();
 }
 
+<<<<<<< HEAD
 void HdfsScanner::close(RuntimeState* runtime_state) noexcept {
+=======
+void HdfsScanner::close() noexcept {
+    if (!_runtime_state) {
+        return;
+    }
+    VLOG_FILE << "close file success: " << _scanner_params.path << ", scan range = ["
+              << _scanner_params.scan_range->offset << ","
+              << (_scanner_params.scan_range->length + _scanner_params.scan_range->offset)
+              << "], rows = " << _app_stats.rows_read;
+
+>>>>>>> 5ac845ebef ([BugFix] fix scan operator early quit when some splitted morsels not handled (#44388))
     bool expect = false;
     if (!_closed.compare_exchange_strong(expect, true)) return;
     update_counter();
