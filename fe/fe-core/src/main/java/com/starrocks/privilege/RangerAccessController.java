f20a1eb828af3e0dd7867ca611598be9a06bfd08
HangyuanLiu <460660596@qq.com>
2024-04-22 17:43:21 +0800

    [BugFix] Fix Masking Policy rewrite fail when has alias (#44445)
    Signed-off-by: HangyuanLiu <460660596@qq.com>
    (cherry picked from commit 8f5fadb084619b343e4e9a3eb762f962f4d41b4b)
    
    # Conflicts:
    #	fe/fe-core/src/test/java/com/starrocks/privilege/ranger/RangerInterfaceTest.java

--- fe/fe-core/src/main/java/com/starrocks/privilege/RangerAccessController.java
+++ fe/fe-core/src/main/java/com/starrocks/privilege/RangerAccessController.java
@@ -135,7 +135,7 @@ public abstract class RangerAccessController extends ExternalAccessController im
         }
     }
 
-    protected void hasPermission(RangerAccessResourceImpl resource, UserIdentity user, PrivilegeType privilegeType)
+    public void hasPermission(RangerAccessResourceImpl resource, UserIdentity user, PrivilegeType privilegeType)
             throws AccessDeniedException {
         String accessType;
         if (privilegeType.equals(PrivilegeType.ANY)) {
