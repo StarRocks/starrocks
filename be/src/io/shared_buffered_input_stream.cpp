3b37d1b91c757832d6bccd5661fc2bd5e7fe56ae
smartlxh <smartlxh@gmail.com>
2024-04-24 14:02:14 +0800

    fix
    Signed-off-by: smartlxh <smartlxh@gmail.com>

--- be/src/io/shared_buffered_input_stream.cpp
+++ be/src/io/shared_buffered_input_stream.cpp
@@ -237,7 +237,7 @@ void SharedBufferedInputStream::release() {
 }
 
 void SharedBufferedInputStream::try_release() {
-    if (hold_count.fetch_sub(1) == 1) {
+    if (hold_count.fetch_sub(1) == 0) {
         _map.clear();
     }
 }
