2ab35b317c3eb5a1c142060d61a1c16e13a962b7
shuming.li <ming.moriarty@gmail.com>
2024-04-24 10:01:23 +0800

    [Refactor] [BugFix] Refactor all pending/running scheduler operations into TaskRunScheduler class (#44515)
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>
    (cherry picked from commit fbf23357799c49252d15a6fdc919bcce8fa53ba3)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/scheduler/TaskManager.java
    #	fe/fe-core/src/main/java/com/starrocks/scheduler/TaskRunManager.java

--- fe/fe-core/src/main/java/com/starrocks/metric/MaterializedViewMetricsEntity.java
+++ fe/fe-core/src/main/java/com/starrocks/metric/MaterializedViewMetricsEntity.java
@@ -28,6 +28,7 @@ import com.starrocks.metric.Metric.MetricUnit;
 import com.starrocks.scheduler.PartitionBasedMvRefreshProcessor;
 import com.starrocks.scheduler.TaskBuilder;
 import com.starrocks.scheduler.TaskManager;
+import com.starrocks.scheduler.TaskRunScheduler;
 import com.starrocks.server.GlobalStateMgr;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -164,7 +165,8 @@ public final class MaterializedViewMetricsEntity implements IMaterializedViewMet
                     return 0L;
                 }
                 Long taskId = taskManager.getTask(mvTaskName).getId();
-                return taskManager.getTaskRunManager().getPendingTaskRunCount(taskId);
+                TaskRunScheduler taskRunScheduler = taskManager.getTaskRunScheduler();
+                return taskRunScheduler.getTaskIdPendingTaskRunCount(taskId);
             }
         };
         metrics.add(counterRefreshPendingJobs);
@@ -182,7 +184,8 @@ public final class MaterializedViewMetricsEntity implements IMaterializedViewMet
                     return 0L;
                 }
                 Long taskId = taskManager.getTask(mvTaskName).getId();
-                if (taskManager.getTaskRunManager().containsTaskInRunningTaskRunMap(taskId)) {
+                TaskRunScheduler taskRunScheduler = taskManager.getTaskRunManager().getTaskRunScheduler();
+                if (taskRunScheduler.isTaskRunning(taskId)) {
                     return 1L;
                 } else {
                     return 0L;
