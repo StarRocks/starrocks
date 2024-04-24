fa10d71219d6f77c66686ac0acfb18b3beade527
Seaven <seaven_7@qq.com>
2024-04-24 14:22:41 +0800

    [BugFix] Fix get struct ordinal index error (backport #44608) (#44604)
    Signed-off-by: Seaven <seaven_7@qq.com>
    (cherry picked from commit d3ccad53956f76e055d725b2484cb304c6010fbf)
    
    # Conflicts:
    #	be/src/storage/rowset/struct_column_iterator.cpp

--- be/src/storage/rowset/struct_column_iterator.cpp
+++ be/src/storage/rowset/struct_column_iterator.cpp
@@ -18,6 +18,7 @@
 #include "column/nullable_column.h"
 #include "column/struct_column.h"
 #include "storage/rowset/column_reader.h"
+#include "storage/rowset/common.h"
 #include "storage/rowset/scalar_column_iterator.h"
 
 namespace starrocks {
@@ -39,7 +40,7 @@ public:
 
     Status seek_to_ordinal(ordinal_t ord) override;
 
-    ordinal_t get_current_ordinal() const override { return _field_iters[0]->get_current_ordinal(); }
+    ordinal_t get_current_ordinal() const override { return _current_ordinal; }
 
     /// for vectorized engine
     Status get_row_ranges_by_zone_map(const std::vector<const ColumnPredicate*>& predicates,
@@ -55,6 +56,7 @@ private:
     const ColumnAccessPath* _path;
 
     std::vector<uint8_t> _access_flags;
+    ordinal_t _current_ordinal = 0;
 };
 
 StatusOr<std::unique_ptr<ColumnIterator>> create_struct_iter(ColumnReader* _reader,
@@ -117,6 +119,11 @@ Status StructColumnIterator::next_batch(size_t* n, Column* dst) {
         if (_access_flags[i]) {
             auto num_to_read = *n;
             RETURN_IF_ERROR(_field_iters[i]->next_batch(&num_to_read, fields[i].get()));
+<<<<<<< HEAD
+=======
+            row_count = fields[i]->size();
+            _current_ordinal = _field_iters[i]->get_current_ordinal();
+>>>>>>> d3ccad5395 ([BugFix] Fix get struct ordinal index error (backport #44608) (#44604))
         }
     }
 
@@ -153,6 +160,7 @@ Status StructColumnIterator::next_batch(const SparseRange& range, Column* dst) {
         if (_access_flags[i]) {
             RETURN_IF_ERROR(_field_iters[i]->next_batch(range, fields[i].get()));
             row_count = fields[i]->size();
+            _current_ordinal = _field_iters[i]->get_current_ordinal();
         }
     }
 
@@ -183,6 +191,11 @@ Status StructColumnIterator::fetch_values_by_rowid(const rowid_t* rowids, size_t
     for (int i = 0; i < _field_iters.size(); ++i) {
         if (_access_flags[i]) {
             RETURN_IF_ERROR(_field_iters[i]->fetch_values_by_rowid(rowids, size, fields[i].get()));
+<<<<<<< HEAD
+=======
+            row_count = fields[i]->size();
+            _current_ordinal = _field_iters[i]->get_current_ordinal();
+>>>>>>> d3ccad5395 ([BugFix] Fix get struct ordinal index error (backport #44608) (#44604))
         }
     }
 
@@ -201,6 +214,7 @@ Status StructColumnIterator::seek_to_first() {
     for (auto& iter : _field_iters) {
         RETURN_IF_ERROR(iter->seek_to_first());
     }
+    _current_ordinal = _field_iters[0]->get_current_ordinal();
     return Status::OK();
 }
 
@@ -211,6 +225,7 @@ Status StructColumnIterator::seek_to_ordinal(ordinal_t ord) {
     for (auto& iter : _field_iters) {
         RETURN_IF_ERROR(iter->seek_to_ordinal(ord));
     }
+    _current_ordinal = _field_iters[0]->get_current_ordinal();
     return Status::OK();
 }
 
@@ -220,4 +235,157 @@ Status StructColumnIterator::get_row_ranges_by_zone_map(const std::vector<const 
     return Status::OK();
 }
 
+<<<<<<< HEAD
+=======
+Status StructColumnIterator::next_batch(size_t* n, Column* dst, ColumnAccessPath* path) {
+    if (path == nullptr || path->children().empty()) {
+        return next_batch(n, dst);
+    }
+
+    // 1. init predicate access path
+    std::vector<uint8_t> predicate_access_flags;
+    std::vector<ColumnAccessPath*> predicate_child_paths;
+    predicate_access_flags.resize(_field_iters.size(), 0);
+    predicate_child_paths.resize(_field_iters.size(), nullptr);
+    for (const auto& child : path->children()) {
+        predicate_access_flags[child->index()] = 1;
+        predicate_child_paths[child->index()] = child.get();
+    }
+
+    StructColumn* struct_column = nullptr;
+    NullColumn* null_column = nullptr;
+    if (dst->is_nullable()) {
+        auto* nullable_column = down_cast<NullableColumn*>(dst);
+        struct_column = down_cast<StructColumn*>(nullable_column->data_column().get());
+        null_column = down_cast<NullColumn*>(nullable_column->null_column().get());
+    } else {
+        struct_column = down_cast<StructColumn*>(dst);
+    }
+
+    // 2. Read null column
+    if (_null_iter != nullptr) {
+        RETURN_IF_ERROR(_null_iter->next_batch(n, null_column));
+        down_cast<NullableColumn*>(dst)->update_has_null();
+    }
+
+    // 3. Read fields
+    size_t row_count = 0;
+    auto& fields = struct_column->fields_column();
+    for (int i = 0; i < _field_iters.size(); ++i) {
+        if (predicate_access_flags[i]) {
+            auto num_to_read = *n;
+            RETURN_IF_ERROR(_field_iters[i]->next_batch(&num_to_read, fields[i].get(), predicate_child_paths[i]));
+            row_count = fields[i]->size();
+            _current_ordinal = _field_iters[i]->get_current_ordinal();
+        }
+    }
+
+    for (int i = 0; i < _field_iters.size(); ++i) {
+        if (!predicate_access_flags[i]) {
+            if (!fields[i]->is_constant()) {
+                fields[i]->append_default(1);
+                fields[i] = ConstColumn::create(fields[i], 1);
+            }
+            fields[i]->resize(row_count);
+        }
+    }
+    return Status::OK();
+}
+
+Status StructColumnIterator::next_batch(const SparseRange<>& range, Column* dst, ColumnAccessPath* path) {
+    if (path == nullptr || path->children().empty()) {
+        return next_batch(range, dst);
+    }
+
+    std::vector<uint8_t> predicate_access_flags;
+    std::vector<ColumnAccessPath*> predicate_child_paths;
+    predicate_access_flags.resize(_field_iters.size(), 0);
+    predicate_child_paths.resize(_field_iters.size(), nullptr);
+    for (const auto& child : path->children()) {
+        predicate_access_flags[child->index()] = 1;
+        predicate_child_paths[child->index()] = child.get();
+    }
+
+    StructColumn* struct_column = nullptr;
+    NullColumn* null_column = nullptr;
+    if (dst->is_nullable()) {
+        auto* nullable_column = down_cast<NullableColumn*>(dst);
+
+        struct_column = down_cast<StructColumn*>(nullable_column->data_column().get());
+        null_column = down_cast<NullColumn*>(nullable_column->null_column().get());
+    } else {
+        struct_column = down_cast<StructColumn*>(dst);
+    }
+    // Read null column
+    if (_null_iter != nullptr) {
+        RETURN_IF_ERROR(_null_iter->next_batch(range, null_column));
+        down_cast<NullableColumn*>(dst)->update_has_null();
+    }
+    // Read all fields
+    size_t row_count = 0;
+    auto& fields = struct_column->fields_column();
+    for (int i = 0; i < _field_iters.size(); ++i) {
+        if (!predicate_access_flags[i]) {
+            continue;
+        }
+        RETURN_IF_ERROR(_field_iters[i]->next_batch(range, fields[i].get(), predicate_child_paths[i]));
+        row_count = fields[i]->size();
+        _current_ordinal = _field_iters[i]->get_current_ordinal();
+    }
+
+    for (int i = 0; i < _field_iters.size(); ++i) {
+        if (!predicate_access_flags[i]) {
+            if (!fields[i]->is_constant()) {
+                fields[i]->append_default(1);
+                fields[i] = ConstColumn::create(fields[i], 1);
+            }
+            fields[i]->resize(row_count);
+        }
+    }
+    return Status::OK();
+}
+
+Status StructColumnIterator::fetch_subfield_by_rowid(const rowid_t* rowids, size_t size, Column* values) {
+    StructColumn* struct_column = nullptr;
+    // 1. null column was readed
+    if (_null_iter != nullptr) {
+        auto* nullable_column = down_cast<NullableColumn*>(values);
+        struct_column = down_cast<StructColumn*>(nullable_column->data_column().get());
+    } else {
+        struct_column = down_cast<StructColumn*>(values);
+    }
+
+    // read all fields
+    auto& fields = struct_column->fields_column();
+    size_t row_count = 0;
+    for (int i = 0; i < _field_iters.size(); ++i) {
+        if (_access_flags[i]) {
+            if (fields[i]->is_constant()) {
+                // doesn't meterialized
+                fields[i] = down_cast<ConstColumn*>(fields[i].get())->data_column()->clone_empty();
+                RETURN_IF_ERROR(_field_iters[i]->fetch_values_by_rowid(rowids, size, fields[i].get()));
+            } else {
+                // handle nested struct
+                // structA -> structB -> e (meterialized)
+                //                    -> f (un-meterialized)
+                RETURN_IF_ERROR(_field_iters[i]->fetch_subfield_by_rowid(rowids, size, fields[i].get()));
+                _current_ordinal = _field_iters[i]->get_current_ordinal();
+            }
+            row_count = fields[i]->size();
+        }
+    }
+
+    for (int i = 0; i < _field_iters.size(); ++i) {
+        if (!_access_flags[i]) {
+            if (!fields[i]->is_constant()) {
+                fields[i]->append_default(1);
+                fields[i] = ConstColumn::create(fields[i], 1);
+            }
+            fields[i]->resize(row_count);
+        }
+    }
+    return Status::OK();
+}
+
+>>>>>>> d3ccad5395 ([BugFix] Fix get struct ordinal index error (backport #44608) (#44604))
 } // namespace starrocks
