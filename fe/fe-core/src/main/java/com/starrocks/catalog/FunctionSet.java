dfb7b1ab0aa62fc49d1ddb41b948910648d5a854
packy92 <110370499+packy92@users.noreply.github.com>
2024-04-25 10:06:06 +0800

    [Enhancement] support calculate more kind of expression statistic (#44595)
    Signed-off-by: packy92 <wangchao@starrocks.com>
    (cherry picked from commit 1400afe5bdc36f38197299427463fcd7951174ca)

--- fe/fe-core/src/main/java/com/starrocks/catalog/FunctionSet.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/FunctionSet.java
@@ -105,6 +105,8 @@ public class FunctionSet {
     public static final String UTC_TIME = "utc_time";
     public static final String LOCALTIME = "localtime";
     public static final String LOCALTIMESTAMP = "localtimestamp";
+
+    public static final String WEEK = "week";
     public static final String WEEKOFYEAR = "weekofyear";
     public static final String WEEK = "week";
     public static final String YEAR = "year";
