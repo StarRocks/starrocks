7fdc786766304e19c15a855787bf28770037453e
stdpain <34912776+stdpain@users.noreply.github.com>
2024-04-26 10:39:44 +0800

    [BugFix] fix unsupported bucket agg in shared-data mode (#44741)
    Signed-off-by: stdpain <drfeng08@gmail.com>

--- be/src/connector/connector.cpp
+++ be/src/connector/connector.cpp
@@ -122,6 +122,27 @@ StatusOr<pipeline::MorselQueuePtr> DataSourceProvider::convert_scan_range_to_mor
             morsels.emplace_back(std::make_unique<pipeline::ScanMorsel>(node_id, scan_range));
         }
     }
+
+    if (partition_order_hint().has_value()) {
+        bool asc = partition_order_hint().value();
+        std::stable_sort(morsels.begin(), morsels.end(), [asc](auto& l, auto& r) {
+            auto l_partition_id = down_cast<pipeline::ScanMorsel*>(l.get())->partition_id();
+            auto r_partition_id = down_cast<pipeline::ScanMorsel*>(r.get())->partition_id();
+            if (asc) {
+                return std::less()(l_partition_id, r_partition_id);
+            } else {
+                return std::greater()(l_partition_id, r_partition_id);
+            }
+        });
+    }
+
+    if (output_chunk_by_bucket()) {
+        std::stable_sort(morsels.begin(), morsels.end(), [](auto& l, auto& r) {
+            return down_cast<pipeline::ScanMorsel*>(l.get())->owner_id() <
+                   down_cast<pipeline::ScanMorsel*>(r.get())->owner_id();
+        });
+    }
+
     return std::make_unique<pipeline::DynamicMorselQueue>(std::move(morsels));
 }
 
