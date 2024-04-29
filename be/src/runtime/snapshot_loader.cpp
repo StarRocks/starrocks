73b79aed1bcd57bce458f5610a92e323250129fa
srlch <linzichao@starrocks.com>
2024-04-29 11:33:53 +0800

    [BugFix] Fix restore download failure for the file of GIN
    Why I'm doing:
    Currently, we hard core two kinds of GIN index file which
    is end with segments_2 and segments.gen. But the index file
    maybe contains some file which are end with segments_1,
    segments_2,...,segments_n etc. We will download these files
    fail because the incorrent file name check.
    
    What I'm doing:
    Check whether the file name contain segments instead of hard
    coding the suffix.
    
    Signed-off-by: srlch <linzichao@starrocks.com>

--- be/src/runtime/snapshot_loader.cpp
+++ be/src/runtime/snapshot_loader.cpp
@@ -756,10 +756,17 @@ bool SnapshotLoader::_end_with(const std::string& str, const std::string& match)
     return false;
 }
 
+bool SnapshotLoader::_contains(const std::string& str, const std::string& match) {
+    if (str.size() >= match.size() && str.find(match) != std::string::npos) {
+        return true;
+    }
+    return false;
+}
+
 bool SnapshotLoader::_is_index_files(const std::string& str) {
     return _end_with(str, "fdt") || _end_with(str, "fdx") || _end_with(str, "fnm") || _end_with(str, "frq") ||
            _end_with(str, "nrm") || _end_with(str, "prx") || _end_with(str, "tii") || _end_with(str, "tis") ||
-           _end_with(str, "null_bitmap") || _end_with(str, "segments_2") || _end_with(str, "segments.gen");
+           _end_with(str, "null_bitmap") || _contains(str, "segments");
 }
 
 Status SnapshotLoader::_get_tablet_id_and_schema_hash_from_file_path(const std::string& src_path, int64_t* tablet_id,
