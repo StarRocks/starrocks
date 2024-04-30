bb823d832545b0804e2589b9e077cf9e777bcbf0
wyb <wybb86@gmail.com>
2024-04-30 10:14:43 +0800

    [BugFix] Fix Struct<ObjectColumn> checksum crash (#44906)
    Signed-off-by: wyb <wybb86@gmail.com>

--- be/src/storage/tablet_schema.h
+++ be/src/storage/tablet_schema.h
@@ -180,6 +180,8 @@ public:
         return mem_usage;
     }
 
+    bool is_support_checksum() const;
+
 private:
     inline static const std::string kEmptyDefaultValue;
     constexpr static uint8_t kIsKeyShift = 0;
