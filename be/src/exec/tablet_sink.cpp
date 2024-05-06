ba9d3c0e321848052efe02f29e5a34e66ac5185f
yandongxiao <yandongxiao@starrocks.com>
2024-05-06 18:47:05 +0800

    [BugFix] Use correct warehouseId when stream load is used to load data (#44695)
    Signed-off-by: yandongxiao <yandongxiao@starrocks.com>
    Signed-off-by: yandongxiao <dxyan06@gmail.com>
    Co-authored-by: Kevin Cai <caixh.kevin@gmail.com>

--- be/src/exec/tablet_sink.cpp
+++ be/src/exec/tablet_sink.cpp
@@ -452,6 +452,10 @@ Status OlapTableSink::_update_immutable_partition(const std::set<int64_t>& parti
     for (auto partition_id : partition_ids_to_be_updated) {
         request.partition_ids.push_back(partition_id);
     }
+    auto backend_id = get_backend_id();
+    if (backend_id.has_value()) {
+        request.__set_backend_id(backend_id.value());
+    }
 
     RETURN_IF_ERROR(_vectorized_partition->remove_partitions(request.partition_ids));
 
