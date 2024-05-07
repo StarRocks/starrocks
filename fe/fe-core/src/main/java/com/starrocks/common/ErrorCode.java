28df488d855ba4365fb8fbbec7558eacf72972b6
wyb <wybb86@gmail.com>
2024-05-07 11:40:36 +0800

    [Enhancement] Improve insert column mismatch error message (#44331)
    Signed-off-by: wyb <wybb86@gmail.com>
    (cherry picked from commit 35b0fac2e4d82f4601379b50d7591edd444bff5c)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/common/ErrorCode.java

--- fe/fe-core/src/main/java/com/starrocks/common/ErrorCode.java
+++ fe/fe-core/src/main/java/com/starrocks/common/ErrorCode.java
@@ -297,6 +297,15 @@ public enum ErrorCode {
     ERR_EXPR_REFERENCED_COLUMN_NOT_FOUND(5601, new byte[] {'4', '2', '0', '0', '0'},
             "Referenced column '%s' in expr '%s' can't be found in column list, derived column is '%s'"),
     ERR_MAPPING_EXPR_INVALID(5602, new byte[] {'4', '2', '0', '0', '0'}, "Expr '%s' analyze error: %s, derived column is '%s'"),
+<<<<<<< HEAD
+=======
+    ERR_NO_PARTITIONS_HAVE_DATA_LOAD(5603, new byte[] {'0', '2', '0', '0', '0'},
+            "No partitions have data available for loading. If you are sure there may be no data to be loaded, " +
+                    "you can use `ADMIN SET FRONTEND CONFIG ('empty_load_as_error' = 'false')` " +
+                    "to ensure such load jobs can succeed"),
+    ERR_INSERTED_COLUMN_MISMATCH(5604, new byte[] {'2', '2', '0', '0', '0'},
+            "Inserted target column count: %d doesn't match select/value column count: %d"),
+>>>>>>> 35b0fac2e4 ([Enhancement] Improve insert column mismatch error message (#44331))
 
     /**
      * 10000 - 10099: warehouse
