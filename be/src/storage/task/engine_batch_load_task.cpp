bf5dfec27ab83ff43bb47ca146a6a247d3d00598
Alex Zhu <zhuming9011@gmail.com>
2024-05-07 18:39:06 +0800

    [BugFix] Fix spark load failure due to not setting tablet version (#45174)
    Signed-off-by: Alex Zhu <zhuming9011@gmail.com>

--- be/src/storage/task/engine_batch_load_task.cpp
+++ be/src/storage/task/engine_batch_load_task.cpp
@@ -58,6 +58,20 @@ using std::vector;
 
 namespace starrocks {
 
+static StatusOr<int64_t> choose_any_version(int64_t tablet_id) {
+    auto tablet_mgr = ExecEnv::GetInstance()->lake_tablet_manager();
+    if (auto res = tablet_mgr->get_latest_cached_tablet_metadata(tablet_id); res != nullptr) {
+        return res->version();
+    }
+    ASSIGN_OR_RETURN(auto iter, tablet_mgr->list_tablet_metadata(tablet_id, true));
+    if (iter.has_next()) {
+        ASSIGN_OR_RETURN(auto metadata, iter.next());
+        return metadata->version();
+    } else {
+        return Status::NotFound(fmt::format("cannot find any metadata of tablet {}", tablet_id));
+    }
+}
+
 EngineBatchLoadTask::EngineBatchLoadTask(TPushReq& push_req, std::vector<TTabletInfo>* tablet_infos, int64_t signature,
                                          AgentStatus* res_status, MemTracker* mem_tracker)
         : _push_req(push_req), _tablet_infos(tablet_infos), _signature(signature), _res_status(res_status) {
@@ -181,11 +195,25 @@ Status EngineBatchLoadTask::_push(const TPushReq& request, std::vector<TTabletIn
     Status res;
 
     if (request.tablet_type == TTabletType::TABLET_TYPE_LAKE) {
+        auto tablet_id = request.tablet_id;
+        // Starting from 3.3.0, the tablet schema in different versions of the tablet metadata may be different. We
+        // need to read the tablet of a specific version (request.version) to ensure that the tablet schema used
+        // during import meets the expectations. However, if the FE version is lower than 3.3.0, the request.version
+        // will always be set to -1 by the FE. At this time, we can read any version of the tablet metadata, because
+        // if the FE version is lower than 3.3.0, it means that there will be no fast schema evolution, and the tablet
+        // schema in all versions of the tablet metadata will be the same.
+        auto tablet_version = request.version;
+        if (tablet_version == -1) {
+            LOG(WARNING) << "tablet version is missing from request, try to obtain any version number from cache or "
+                            "remote storage. tablet_id="
+                         << tablet_id;
+            ASSIGN_OR_RETURN(tablet_version, choose_any_version(tablet_id));
+        }
         auto tablet_manager = ExecEnv::GetInstance()->lake_tablet_manager();
-        auto tablet_or = tablet_manager->get_tablet(request.tablet_id, request.version);
+        auto tablet_or = tablet_manager->get_tablet(tablet_id, tablet_version);
         if (!tablet_or.ok()) {
             LOG(WARNING) << "Fail to read tablet metadata. res=" << res << ", txn_id: " << request.transaction_id
-                         << ", tablet=" << request.tablet_id << ", version=" << request.version
+                         << ", tablet=" << tablet_id << ", version=" << tablet_version
                          << ", cost=" << PrettyPrinter::print(duration_ns, TUnit::TIME_NS);
             StarRocksMetrics::instance()->push_requests_fail_total.increment(1);
             return tablet_or.status();
