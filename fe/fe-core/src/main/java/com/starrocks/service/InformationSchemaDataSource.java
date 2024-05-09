262f565563c3962341ce58d0a6bf3fc3d917b78f
HangyuanLiu <460660596@qq.com>
2024-05-09 16:14:48 +0800

    Support use table_name to filter scan in information_schema.tables
    Signed-off-by: HangyuanLiu <460660596@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/service/InformationSchemaDataSource.java
+++ fe/fe-core/src/main/java/com/starrocks/service/InformationSchemaDataSource.java
@@ -313,8 +313,9 @@ public class InformationSchemaDataSource {
     }
 
     private static void genPartitionMetaInfo(Database db, OlapTable table,
-            PartitionInfo partitionInfo, Partition partition, PhysicalPartition physicalPartition,
-            TPartitionMetaInfo partitionMetaInfo, boolean isTemp) {
+                                             PartitionInfo partitionInfo, Partition partition,
+                                             PhysicalPartition physicalPartition,
+                                             TPartitionMetaInfo partitionMetaInfo, boolean isTemp) {
         // PARTITION_NAME
         partitionMetaInfo.setPartition_name(partition.getName());
         // PARTITION_ID
@@ -377,7 +378,6 @@ public class InformationSchemaDataSource {
 
     // tables
     public static TGetTablesInfoResponse generateTablesInfoResponse(TGetTablesInfoRequest request) throws TException {
-
         TGetTablesInfoResponse response = new TGetTablesInfoResponse();
         List<TTableInfo> infos = new ArrayList<>();
 
@@ -393,73 +393,97 @@ public class InformationSchemaDataSource {
 
         for (String dbName : result.authorizedDbs) {
             Database db = metadataMgr.getDb(catalogName, dbName);
+            if (db == null) {
+                continue;
+            }
 
-            if (db != null) {
-                Locker locker = new Locker();
+            List<BasicTable> tables = new ArrayList<>();
+            Locker locker = new Locker();
+            try {
                 locker.lockDatabase(db, LockType.READ);
-                try {
-                    List<String> tableNames = metadataMgr.listTableNames(catalogName, dbName);
-                    for (String tableName : tableNames) {
-                        BasicTable table = null;
-                        try {
-                            table = metadataMgr.getBasicTable(catalogName, dbName, tableName);
-                        } catch (Exception e) {
-                            LOG.warn(e.getMessage());
-                        }
-
-                        if (table == null) {
+                List<String> tableNames = metadataMgr.listTableNames(catalogName, dbName);
+                for (String tableName : tableNames) {
+                    if (request.isSetTable_name()) {
+                        if (!tableName.equals(request.getTable_name())) {
                             continue;
                         }
-
-                        try {
-                            Authorizer.checkAnyActionOnTableLikeObject(result.currentUser, null, dbName, table);
-                        } catch (AccessDeniedException e) {
-                            continue;
-                        }
-
-                        TTableInfo info = new TTableInfo();
-
-                        // refer to https://dev.mysql.com/doc/refman/8.0/en/information-schema-tables-table.html
-                        // the catalog name is always `def`
-                        info.setTable_catalog(DEF);
-                        info.setTable_schema(dbName);
-                        info.setTable_name(table.getName());
-                        info.setTable_type(table.getMysqlType());
-                        info.setEngine(table.getEngine());
-                        info.setVersion(DEFAULT_EMPTY_NUM);
-                        // TABLE_ROWS (depend on the table type)
-                        // AVG_ROW_LENGTH (depend on the table type)
-                        // DATA_LENGTH (depend on the table type)
-                        info.setMax_data_length(DEFAULT_EMPTY_NUM);
-                        info.setIndex_length(DEFAULT_EMPTY_NUM);
-                        info.setData_free(DEFAULT_EMPTY_NUM);
-                        info.setAuto_increment(DEFAULT_EMPTY_NUM);
-                        info.setCreate_time(table.getCreateTime());
-                        // UPDATE_TIME (depend on the table type)
-                        info.setCheck_time(table.getLastCheckTime() / 1000);
-                        info.setTable_collation(UTF8_GENERAL_CI);
-                        info.setChecksum(DEFAULT_EMPTY_NUM);
-                        info.setTable_comment(table.getComment());
-
-                        if (table.isNativeTableOrMaterializedView() || table.getType() == TableType.OLAP_EXTERNAL) {
-                            // OLAP (done)
-                            // OLAP_EXTERNAL (done)
-                            // MATERIALIZED_VIEW (done)
-                            // LAKE (done)
-                            // LAKE_MATERIALIZED_VIEW (done)
-                            genNormalTableInfo(table, info);
-                        } else {
-                            // SCHEMA (use default)
-                            // INLINE_VIEW (use default)
-                            // VIEW (use default)
-                            // BROKER (use default)
-                            // EXTERNAL TABLE (use default)
-                            genDefaultConfigInfo(info);
-                        }
-                        infos.add(info);
                     }
+
+                    BasicTable table = null;
+                    try {
+                        table = metadataMgr.getBasicTable(catalogName, dbName, tableName);
+                    } catch (Exception e) {
+                        LOG.warn(e.getMessage());
+                    }
+                    if (table == null) {
+                        continue;
+                    }
+
+                    try {
+                        Authorizer.checkAnyActionOnTableLikeObject(result.currentUser, null, dbName, table);
+                    } catch (AccessDeniedException e) {
+                        continue;
+                    }
+
+                    tables.add(table);
+                }
+            } finally {
+                locker.unLockDatabase(db, LockType.READ);
+            }
+
+            for (BasicTable table : tables) {
+                Locker tableLocker = new Locker();
+                try {
+                    if (table.isNativeTableOrMaterializedView()) {
+                        tableLocker.lockTablesWithIntensiveDbLock(db, Lists.newArrayList(((OlapTable) table).getId()),
+                                LockType.READ);
+                    }
+
+                    TTableInfo info = new TTableInfo();
+
+                    // refer to https://dev.mysql.com/doc/refman/8.0/en/information-schema-tables-table.html
+                    // the catalog name is always `def`
+                    info.setTable_catalog(DEF);
+                    info.setTable_schema(dbName);
+                    info.setTable_name(table.getName());
+                    info.setTable_type(table.getMysqlType());
+                    info.setEngine(table.getEngine());
+                    info.setVersion(DEFAULT_EMPTY_NUM);
+                    // TABLE_ROWS (depend on the table type)
+                    // AVG_ROW_LENGTH (depend on the table type)
+                    // DATA_LENGTH (depend on the table type)
+                    info.setMax_data_length(DEFAULT_EMPTY_NUM);
+                    info.setIndex_length(DEFAULT_EMPTY_NUM);
+                    info.setData_free(DEFAULT_EMPTY_NUM);
+                    info.setAuto_increment(DEFAULT_EMPTY_NUM);
+                    info.setCreate_time(table.getCreateTime());
+                    // UPDATE_TIME (depend on the table type)
+                    info.setCheck_time(table.getLastCheckTime() / 1000);
+                    info.setTable_collation(UTF8_GENERAL_CI);
+                    info.setChecksum(DEFAULT_EMPTY_NUM);
+                    info.setTable_comment(table.getComment());
+
+                    if (table.isNativeTableOrMaterializedView() || table.getType() == TableType.OLAP_EXTERNAL) {
+                        // OLAP (done)
+                        // OLAP_EXTERNAL (done)
+                        // MATERIALIZED_VIEW (done)
+                        // LAKE (done)
+                        // LAKE_MATERIALIZED_VIEW (done)
+                        genNormalTableInfo(table, info);
+                    } else {
+                        // SCHEMA (use default)
+                        // INLINE_VIEW (use default)
+                        // VIEW (use default)
+                        // BROKER (use default)
+                        // EXTERNAL TABLE (use default)
+                        genDefaultConfigInfo(info);
+                    }
+                    infos.add(info);
                 } finally {
-                    locker.unLockDatabase(db, LockType.READ);
+                    if (table.isNativeTableOrMaterializedView()) {
+                        tableLocker.unLockTablesWithIntensiveDbLock(db, Lists.newArrayList(((OlapTable) table).getId()),
+                                LockType.READ);
+                    }
                 }
             }
         }
--- gensrc/thrift/FrontendService.thrift
+++ gensrc/thrift/FrontendService.thrift
@@ -1443,6 +1443,7 @@ struct TPartitionMetaInfo {
 
 struct TGetTablesInfoRequest {
     1: optional TAuthInfo auth_info
+    2: optional string table_name;
 }
 
 struct TGetTablesInfoResponse {
