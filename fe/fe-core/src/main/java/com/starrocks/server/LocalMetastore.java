ebc06a88bac9f8536399042459ff92249f417e5f
Murphy <96611012+mofeiatwork@users.noreply.github.com>
2024-05-10 22:20:43 -0700

    [BugFix] fix persistence of mv sort key (#45456)
    Why I'm doing:
    ORDER BY needs to be persistent into TableProperty.properties and put into TableProperty.mvSortKeys as in-memory structure
    What I'm doing:
    put the mvSortKeys into TableProperty.properties
    
    Signed-off-by: Murphy <mofei@starrocks.com>
    (cherry picked from commit 43829b5d9e515611448f320a12cfee0aa6dfccc6)

--- fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
+++ fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
@@ -3239,6 +3239,12 @@ public class LocalMetastore implements ConnectorMetadata {
                 colocateTableIndex.addTableToGroup(db, materializedView, colocateGroup,
                         materializedView.isCloudNativeMaterializedView());
             }
+
+            // ORDER BY() -> sortKeys
+            if (CollectionUtils.isNotEmpty(materializedView.getTableProperty().getMvSortKeys())) {
+                materializedView.getTableProperty().putMvSortKeys();
+            }
+
             // lake storage info
             if (materializedView.isCloudNativeMaterializedView()) {
                 String volume = "";
