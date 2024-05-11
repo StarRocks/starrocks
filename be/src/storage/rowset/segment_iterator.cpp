3ae87d0936795d1cbb5c0a7c6dc94552ee768bdc
zihe.liu <ziheliu1024@gmail.com>
2024-05-11 10:40:15 +0800

    update
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- be/src/storage/rowset/segment_iterator.cpp
+++ be/src/storage/rowset/segment_iterator.cpp
@@ -2019,7 +2019,7 @@ struct BloomFilterSupportChecker {
         const auto* col_pred = node.col_pred();
         const auto cid = col_pred->column_id();
         const bool support = column_iterators[cid]->has_bloom_filter_index() &&
-                             (col_pred->support_bloom_filter() || col_pred->support_ngram_bloom_filter());
+                             (col_pred->support_original_bloom_filter() || col_pred->support_ngram_bloom_filter());
         if (support) {
             used_nodes.emplace(&node);
         }
