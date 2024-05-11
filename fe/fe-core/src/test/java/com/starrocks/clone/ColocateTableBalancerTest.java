8a4f61303740abff6be97f16bfbbf40e1e0f259e
yiming <xiadejun@starrocks.com>
2024-05-11 16:31:53 +0800

    [UT] Fix ut for colocate balancer test (#45477)
    Signed-off-by: Dejun Xia <xiadejun@starrocks.com>

--- fe/fe-core/src/test/java/com/starrocks/clone/ColocateTableBalancerTest.java
+++ fe/fe-core/src/test/java/com/starrocks/clone/ColocateTableBalancerTest.java
@@ -107,6 +107,8 @@ public class ColocateTableBalancerTest {
         UtFrameUtils.createMinStarRocksCluster();
         ConnectContext ctx = UtFrameUtils.createDefaultCtx();
         starRocksAssert = new StarRocksAssert(ctx);
+        GlobalStateMgr.getCurrentState().getHeartbeatMgr().setStop();
+        GlobalStateMgr.getCurrentState().getTabletScheduler().setStop();
     }
 
     @Before
