78857e531fa7dc58396c0cab78273093f4a3cb75
hmx <1639097204@qq.com>
2024-05-13 19:56:38 +0800

    update variable computationFragmentSchedulingPolicy type to enum
    Signed-off-by: hmx <1639097204@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/lake/qe/scheduler/DefaultSharedDataWorkerProvider.java
+++ fe/fe-core/src/main/java/com/starrocks/lake/qe/scheduler/DefaultSharedDataWorkerProvider.java
@@ -21,6 +21,7 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.starrocks.common.FeConstants;
+import com.starrocks.qe.SessionVariable.ComputationFragmentSchedulingPolicy;
 import com.starrocks.qe.SimpleScheduler;
 import com.starrocks.qe.scheduler.NonRecoverableException;
 import com.starrocks.qe.scheduler.WorkerProvider;
@@ -62,10 +63,10 @@ public class DefaultSharedDataWorkerProvider implements WorkerProvider {
     public static class Factory implements WorkerProvider.Factory {
         @Override
         public DefaultSharedDataWorkerProvider captureAvailableWorkers(SystemInfoService systemInfoService,
-                                                                       boolean preferComputeNode,
-                                                                       int numUsedComputeNodes,
-                                                                       String computationFragmentSchedulingPolicy,
-                                                                       long warehouseId) {
+                                               boolean preferComputeNode,
+                                               int numUsedComputeNodes,
+                                               ComputationFragmentSchedulingPolicy computationFragmentSchedulingPolicy,
+                                               long warehouseId) {
 
             ImmutableMap.Builder<Long, ComputeNode> builder = ImmutableMap.builder();
             List<Long> computeNodeIds =
