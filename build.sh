cc2f80ae922f5abddab9836b9dbd1ad60ba49b24
Hector Gonzalez <hagonzal@users.noreply.github.com>
2024-05-13 18:23:10 -0700

    [BugFix] add option for with-brpc-keepalive to build.sh (#45566)
    Signed-off-by: Hector <h.gonzalez@celonis.com>

--- build.sh
+++ build.sh
@@ -128,6 +128,7 @@ OPTS=$(getopt \
   -l 'without-gcov' \
   -l 'without-java-ext' \
   -l 'without-starcache' \
+  -l 'with-brpc-keepalive' \
   -l 'use-staros' \
   -l 'enable-shared-data' \
   -l 'output-compile-time' \
