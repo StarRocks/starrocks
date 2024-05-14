0331ec37943da2ffbd2aab59c63d9fd9ed4f044d
trueeyu <lxhhust350@qq.com>
2024-05-14 13:19:16 +0800

    [Enhancement] Put del vec filter after all other index filter (#45569)
    Signed-off-by: trueeyu <lxhhust350@qq.com>
    (cherry picked from commit 718edb7b0a5fa3481698c4873dd0b0ede71b509a)
    
    # Conflicts:
    #	be/src/common/config.h
    #	be/src/storage/rowset/segment_iterator.cpp

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -1172,4 +1172,12 @@ CONF_mBool(enable_profile_for_external_plan, "false");
 CONF_mInt64(arrow_io_coalesce_read_max_buffer_size, "8388608");
 CONF_mInt64(arrow_io_coalesce_read_max_distance_size, "1048576");
 CONF_mInt64(arrow_read_batch_size, "4096");
+<<<<<<< HEAD
+=======
+
+// Set to true to enable socket_keepalive option in brpc
+CONF_mBool(brpc_socket_keepalive, "false");
+CONF_mBool(apply_del_vec_after_all_index_filter, "true");
+
+>>>>>>> 718edb7b0a ([Enhancement] Put del vec filter after all other index filter (#45569))
 } // namespace starrocks::config
