b269dbc0ad44c8b5726f3689cddc6862e56a00db
packy92 <wangchao@starrocks.com>
2024-05-15 16:35:45 +0800

    resolve ut
    Signed-off-by: packy92 <wangchao@starrocks.com>

--- fe/fe-core/src/test/resources/sql/tpch-histogram-cost/q19.sql
+++ fe/fe-core/src/test/resources/sql/tpch-histogram-cost/q19.sql
@@ -69,34 +69,22 @@ OutPut Exchange Id: 08
 |  cardinality: 21702
 |  column statistics:
 |  * L_EXTENDEDPRICE-->[901.0, 104949.5, 0.0, 8.0, 21702.324135626324] ESTIMATE
-|  * L_DISCOUNT-->[0.0, 0.1, 0.0, 8.0, 11.0] MCV: [[0.05:54639500][0.07:54619200][0.02:54617300][0.01:54583400][0.10:54581500]] ESTIMATE
+|  * L_DISCOUNT-->[0.0, 0.1, 0.0, 8.0, 11.0] ESTIMATE
 |
 5:HASH JOIN
 |  join op: INNER JOIN (PARTITIONED)
 |  equal join conjunct: [2: L_PARTKEY, INT, false] = [18: P_PARTKEY, INT, false]
 |  other join predicates: (((((21: P_BRAND = 'Brand#45') AND (24: P_CONTAINER IN ('SM CASE', 'SM BOX', 'SM PACK', 'SM PKG'))) AND ((5: L_QUANTITY >= 5.0) AND (5: L_QUANTITY <= 15.0))) AND (23: P_SIZE <= 5)) OR ((((21: P_BRAND = 'Brand#11') AND (24: P_CONTAINER IN ('MED BAG', 'MED BOX', 'MED PKG', 'MED PACK'))) AND ((5: L_QUANTITY >= 15.0) AND (5: L_QUANTITY <= 25.0))) AND (23: P_SIZE <= 10))) OR ((((21: P_BRAND = 'Brand#21') AND (24: P_CONTAINER IN ('LG CASE', 'LG BOX', 'LG PACK', 'LG PKG'))) AND ((5: L_QUANTITY >= 25.0) AND (5: L_QUANTITY <= 35.0))) AND (23: P_SIZE <= 15))
 |  build runtime filters:
-<<<<<<< HEAD
-|  - filter_id = 0, build_expr = (18: P_PARTKEY), remote = false
-|  output columns: 5, 6, 7, 21, 23, 24
-|  cardinality: 0
-|  column statistics:
-|  * L_PARTKEY-->[1.0, 2.0E7, 0.0, 8.0, 22735.940088981562] ESTIMATE
-|  * L_QUANTITY-->[NaN, NaN, 0.0, 8.0, 50.0] ESTIMATE
-|  * L_EXTENDEDPRICE-->[901.0, 104949.5, 0.0, 8.0, 22735.940088981562] ESTIMATE
-|  * L_DISCOUNT-->[0.0, 0.1, 0.0, 8.0, 11.0] ESTIMATE
-|  * P_PARTKEY-->[1.0, 2.0E7, 0.0, 8.0, 22735.940088981562] ESTIMATE
-=======
 |  - filter_id = 0, build_expr = (18: P_PARTKEY), remote = true
-|  output columns: 6, 7
+|  output columns: 5, 6, 7, 21, 23, 24
 |  cardinality: 21702
 |  column statistics:
 |  * L_PARTKEY-->[1.0, 2.0E7, 0.0, 8.0, 21702.324135626324] ESTIMATE
-|  * L_QUANTITY-->[NaN, NaN, 0.0, 8.0, 50.0] MCV: [[9.00:12050300][15.00:12032400][8.00:12015300][7.00:12011400][12.00:11991300]] ESTIMATE
+|  * L_QUANTITY-->[NaN, NaN, 0.0, 8.0, 50.0] ESTIMATE
 |  * L_EXTENDEDPRICE-->[901.0, 104949.5, 0.0, 8.0, 21702.324135626324] ESTIMATE
-|  * L_DISCOUNT-->[0.0, 0.1, 0.0, 8.0, 11.0] MCV: [[0.05:54639500][0.07:54619200][0.02:54617300][0.01:54583400][0.10:54581500]] ESTIMATE
+|  * L_DISCOUNT-->[0.0, 0.1, 0.0, 8.0, 11.0] ESTIMATE
 |  * P_PARTKEY-->[1.0, 2.0E7, 0.0, 8.0, 21702.324135626324] ESTIMATE
->>>>>>> 1e5626e9b8 ([BugFix] fix stale histogram lead to unexpected stats (#45614))
 |  * P_BRAND-->[-Infinity, Infinity, 0.0, 10.0, 3.0] ESTIMATE
 |  * P_SIZE-->[NaN, NaN, 0.0, 4.0, 50.0] ESTIMATE
 |  * P_CONTAINER-->[-Infinity, Infinity, 0.0, 10.0, 12.0] ESTIMATE
@@ -123,14 +111,14 @@ table: part, rollup: part
 preAggregation: on
 Predicates: 21: P_BRAND IN ('Brand#45', 'Brand#11', 'Brand#21'), [23: P_SIZE, INT, false] <= 15, 24: P_CONTAINER IN ('SM CASE', 'SM BOX', 'SM PACK', 'SM PKG', 'MED BAG', 'MED BOX', 'MED PKG', 'MED PACK', 'LG CASE', 'LG BOX', 'LG PACK', 'LG PKG'), [23: P_SIZE, INT, false] >= 1
 partitionsRatio=1/1, tabletsRatio=10/10
-tabletList=10263,10265,10267,10269,10271,10273,10275,10277,10279,10281
+tabletList=10266,10268,10270,10272,10274,10276,10278,10280,10282,10284
 actualRows=0, avgRowSize=32.0
 cardinality: 6051300
 column statistics:
 * P_PARTKEY-->[1.0, 2.0E7, 0.0, 8.0, 6051299.999999999] ESTIMATE
-* P_BRAND-->[-Infinity, Infinity, 0.0, 10.0, 25.0] MCV: [[Brand#35:823300][Brand#12:816700][Brand#52:815800][Brand#33:814100][Brand#53:808800]] ESTIMATE
-* P_SIZE-->[NaN, NaN, 0.0, 4.0, 50.0] MCV: [[10:417700][14:415300][3:412300][7:407900][5:406200]] ESTIMATE
-* P_CONTAINER-->[-Infinity, Infinity, 0.0, 10.0, 40.0] MCV: [[SM DRUM:515300][JUMBO JAR:511500][LG JAR:510300][LG BOX:509600][MED CAN:509100]] ESTIMATE
+* P_BRAND-->[-Infinity, Infinity, 0.0, 10.0, 25.0] ESTIMATE
+* P_SIZE-->[NaN, NaN, 0.0, 4.0, 50.0] ESTIMATE
+* P_CONTAINER-->[-Infinity, Infinity, 0.0, 10.0, 40.0] ESTIMATE
 
 PLAN FRAGMENT 3(F00)
 
@@ -168,27 +156,4 @@ column statistics:
 * L_DISCOUNT-->[0.0, 0.1, 0.0, 8.0, 11.0] ESTIMATE
 * L_SHIPINSTRUCT-->[-Infinity, Infinity, 0.0, 25.0, 1.0] ESTIMATE
 * L_SHIPMODE-->[-Infinity, Infinity, 0.0, 10.0, 2.0] ESTIMATE
-<<<<<<< HEAD
-
-PLAN FRAGMENT 2(F01)
-
-Input Partition: RANDOM
-OutPut Partition: UNPARTITIONED
-OutPut Exchange Id: 03
-
-2:OlapScanNode
-table: part, rollup: part
-preAggregation: on
-Predicates: 21: P_BRAND IN ('Brand#45', 'Brand#11', 'Brand#21'), [23: P_SIZE, INT, false] <= 15, 24: P_CONTAINER IN ('SM CASE', 'SM BOX', 'SM PACK', 'SM PKG', 'MED BAG', 'MED BOX', 'MED PKG', 'MED PACK', 'LG CASE', 'LG BOX', 'LG PACK', 'LG PKG'), [23: P_SIZE, INT, false] >= 1
-partitionsRatio=1/1, tabletsRatio=10/10
-tabletList=10266,10268,10270,10272,10274,10276,10278,10280,10282,10284
-actualRows=0, avgRowSize=32.0
-cardinality: 6051300
-column statistics:
-* P_PARTKEY-->[1.0, 2.0E7, 0.0, 8.0, 6051299.999999999] ESTIMATE
-* P_BRAND-->[-Infinity, Infinity, 0.0, 10.0, 25.0] ESTIMATE
-* P_SIZE-->[NaN, NaN, 0.0, 4.0, 50.0] ESTIMATE
-* P_CONTAINER-->[-Infinity, Infinity, 0.0, 10.0, 40.0] ESTIMATE
-=======
->>>>>>> 1e5626e9b8 ([BugFix] fix stale histogram lead to unexpected stats (#45614))
 [end]
