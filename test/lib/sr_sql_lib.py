c7be9aa57c654785d624a38ac395d1313d1b6509
stephen <91597003+stephen-shelby@users.noreply.github.com>
2024-05-19 12:49:43 +0800

    [UT] add the missing sql_test lib (#45853)
    Signed-off-by: stephen <stephen5217@163.com>

--- test/lib/sr_sql_lib.py
+++ test/lib/sr_sql_lib.py
@@ -1762,3 +1762,12 @@ class StarrocksSQLApiLib(object):
         res = self.execute_sql(sql, True)
         for expect in expects:
             tools.assert_true(str(res["result"]).find(expect) > 0, "assert expect %s is not found in plan, error msg is %s" % (expect, str(res["result"])))
+
+    def assert_explain_costs_contains(self, query, *expects):
+        """
+        assert explain costs result contains expect string
+        """
+        sql = "explain costs %s" % (query)
+        res = self.execute_sql(sql, True)
+        for expect in expects:
+            tools.assert_true(str(res["result"]).find(expect) > 0, "assert expect %s is not found in plan" % (expect))
