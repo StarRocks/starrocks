b8db3e71813185e827ed053ff83b745d78416431
Rohit Satardekar <rohitrs1983@gmail.com>
2024-05-20 23:06:24 +0530

    [BugFix] multiple sys_log_to_console=true entry in fe.conf every time fe is started with --logconsole option
    Signed-off-by: Rohit Satardekar <rohitrs1983@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/common/Log4jConfig.java
+++ fe/fe-core/src/main/java/com/starrocks/common/Log4jConfig.java
@@ -309,7 +309,8 @@ public class Log4jConfig extends XmlConfiguration {
         }
 
         String newXmlConfTemplate = APPENDER_TEMPLATE;
-        newXmlConfTemplate += Config.sys_log_to_console ? CONSOLE_LOGGER_TEMPLATE : FILE_LOGGER_TEMPLATE;
+        newXmlConfTemplate += System.getenv("SYS_LOG_TO_CONSOLE").trim().equals("1")
+            ? CONSOLE_LOGGER_TEMPLATE : FILE_LOGGER_TEMPLATE;
         newXmlConfTemplate = newXmlConfTemplate.replaceAll("<!--REPLACED BY AUDIT AND VERBOSE MODULE NAMES-->",
                 sb.toString());
         return newXmlConfTemplate;
