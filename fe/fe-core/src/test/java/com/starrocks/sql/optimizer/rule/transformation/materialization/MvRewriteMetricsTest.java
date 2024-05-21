08cfa23460f34bbdbcbd8b7465f5a6fa4e321618
shuming.li <ming.moriarty@gmail.com>
2024-05-21 19:51:02 +0800

    [BugFix] Fix mv metrics npe problem (#46014)
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/optimizer/rule/transformation/materialization/MvRewriteMetricsTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/optimizer/rule/transformation/materialization/MvRewriteMetricsTest.java
@@ -15,9 +15,11 @@
 package com.starrocks.sql.optimizer.rule.transformation.materialization;
 
 import com.starrocks.catalog.MaterializedView;
+import com.starrocks.catalog.MvId;
 import com.starrocks.common.Config;
 import com.starrocks.common.profile.Tracers;
 import com.starrocks.metric.IMaterializedViewMetricsEntity;
+import com.starrocks.metric.JsonMetricVisitor;
 import com.starrocks.metric.MaterializedViewMetricsBlackHoleEntity;
 import com.starrocks.metric.MaterializedViewMetricsEntity;
 import com.starrocks.metric.MaterializedViewMetricsRegistry;
@@ -181,5 +183,138 @@ public class MvRewriteMetricsTest extends MvRewriteTestBase {
             MaterializedViewMetricsBlackHoleEntity mvMetric = (MaterializedViewMetricsBlackHoleEntity) iEntity;
         });
         Config.enable_materialized_view_metrics_collect = true;
+    }
+
+    @Test
+    public void testMvMetricsWithValidMvId() {
+        String mvName = "mv0";
+        String sql = String.format("CREATE MATERIALIZED VIEW %s" +
+                " REFRESH DEFERRED MANUAL " +
+                " AS SELECT * FROM depts where deptno = 10", mvName);
+        starRocksAssert.withMaterializedView(sql, () -> {
+            refreshMaterializedView(DB_NAME, mvName);
+            MaterializedView mv = (MaterializedView) getTable(DB_NAME, mvName);
+            IMaterializedViewMetricsEntity iEntity =
+                    MaterializedViewMetricsRegistry.getInstance().getMetricsEntity(mv.getMvId());
+            Assert.assertTrue(iEntity instanceof MaterializedViewMetricsEntity);
+            MaterializedViewMetricsEntity mvMetric = (MaterializedViewMetricsEntity) iEntity;
+            Assert.assertTrue(mvMetric.counterRefreshJobSuccessTotal.getValue() == 1);
+            Assert.assertTrue(mvMetric.counterRefreshJobRetryCheckChangedTotal.getValue() == 1);
+            Assert.assertTrue(mvMetric.counterRefreshJobTotal.getValue() == 1);
+            {
+                // hit mv
+                String query = "select * from depts where deptno = 10";
+                String plan = getFragmentPlan(query);
+                PlanTestBase.assertContains(plan, mvName);
+                Assert.assertTrue(mvMetric.counterQueryHitTotal.getValue() == 1);
+                Assert.assertTrue(mvMetric.counterQueryConsideredTotal.getValue() == 1);
+                Assert.assertTrue(mvMetric.counterQueryMatchedTotal.getValue() == 1);
+                Assert.assertTrue(mvMetric.counterQueryMaterializedViewTotal.getValue() == 1);
+            }
+
+            {
+                JsonMetricVisitor visitor = new JsonMetricVisitor("starrocks_fe");
+                MaterializedViewMetricsRegistry.collectMaterializedViewMetrics(visitor, true);
+                String json = visitor.build();
+                System.out.println(json);
+                Assert.assertTrue(json.contains("mv_refresh_jobs"));
+                Assert.assertTrue(json.contains("mv_refresh_total_success_jobs"));
+                Assert.assertTrue(json.contains("mv_refresh_total_retry_meta_count"));
+                Assert.assertTrue(json.contains("mv_query_total_count"));
+                Assert.assertTrue(json.contains("mv_query_total_hit_count"));
+                Assert.assertTrue(json.contains("mv_query_total_considered_count"));
+                Assert.assertTrue(json.contains("mv_query_total_matched_count"));
+            }
+            {
+                JsonMetricVisitor visitor = new JsonMetricVisitor("starrocks_fe");
+                MaterializedViewMetricsRegistry.collectMaterializedViewMetrics(visitor, true);
+                String json = visitor.build();
+                System.out.println(json);
+                Assert.assertTrue(json.contains("mv_refresh_jobs"));
+                Assert.assertTrue(json.contains("mv_refresh_total_success_jobs"));
+                Assert.assertTrue(json.contains("mv_refresh_total_retry_meta_count"));
+                Assert.assertTrue(json.contains("mv_query_total_count"));
+                Assert.assertTrue(json.contains("mv_query_total_hit_count"));
+                Assert.assertTrue(json.contains("mv_query_total_considered_count"));
+                Assert.assertTrue(json.contains("mv_query_total_matched_count"));
+            }
+
+            {
+                // hit mv
+                String query = "select * from depts where deptno = 10";
+                String plan = getFragmentPlan(query);
+                PlanTestBase.assertContains(plan, mvName);
+                Assert.assertTrue(mvMetric.counterQueryHitTotal.getValue() == 2);
+                Assert.assertTrue(mvMetric.counterQueryConsideredTotal.getValue() == 2);
+                Assert.assertTrue(mvMetric.counterQueryMatchedTotal.getValue() == 2);
+                Assert.assertTrue(mvMetric.counterQueryMaterializedViewTotal.getValue() == 2);
+            }
+            // empty refresh
+            refreshMaterializedView(DB_NAME, mvName);
+            Assert.assertTrue(mvMetric.counterRefreshJobTotal.getValue() == 2);
+            Assert.assertTrue(mvMetric.counterRefreshJobSuccessTotal.getValue() == 1);
+            Assert.assertTrue(mvMetric.counterRefreshJobEmptyTotal.getValue() == 1);
+            Assert.assertTrue(mvMetric.counterRefreshJobRetryCheckChangedTotal.getValue() == 2);
+
+            {
+                JsonMetricVisitor visitor = new JsonMetricVisitor("starrocks_fe");
+                MaterializedViewMetricsRegistry.collectMaterializedViewMetrics(visitor, true);
+                String json = visitor.build();
+                System.out.println(json);
+                Assert.assertTrue(json.contains("mv_refresh_jobs"));
+                Assert.assertTrue(json.contains("mv_refresh_total_success_jobs"));
+                Assert.assertTrue(json.contains("mv_refresh_total_empty_jobs"));
+                Assert.assertTrue(json.contains("mv_refresh_total_retry_meta_count"));
+                Assert.assertTrue(json.contains("mv_query_total_count"));
+                Assert.assertTrue(json.contains("mv_query_total_hit_count"));
+                Assert.assertTrue(json.contains("mv_query_total_considered_count"));
+                Assert.assertTrue(json.contains("mv_query_total_matched_count"));
+            }
+        });
+    }
+
+    @Test
+    public void testMvMetricsWithInvalidMvId1() {
+        MvId invalid = new MvId(-1, -1);
+        MaterializedViewMetricsRegistry.getInstance().getMetricsEntity(invalid);
+        JsonMetricVisitor visitor = new JsonMetricVisitor("starrocks_fe");
+        MaterializedViewMetricsRegistry.collectMaterializedViewMetrics(visitor, true);
+        String json = visitor.build();
+        System.out.println(json);
+        Assert.assertTrue(json.equals("[]"));
+    }
+
+    @Test
+    public void testMvMetricsWithInvalidMvId2() {
+        // mv1: invalid
+        {
+            MvId invalid = new MvId(-1, -1);
+            MaterializedViewMetricsRegistry.getInstance().getMetricsEntity(invalid);
+            JsonMetricVisitor visitor = new JsonMetricVisitor("starrocks_fe");
+            MaterializedViewMetricsRegistry.collectMaterializedViewMetrics(visitor, true);
+            String json = visitor.build();
+            System.out.println(json);
+            Assert.assertTrue(json.equals("[]"));
+        }
+
+        // mv2: valid
+        String mvName = "mv0";
+        String sql = String.format("CREATE MATERIALIZED VIEW %s" +
+                " REFRESH DEFERRED MANUAL " +
+                " AS SELECT * FROM depts where deptno = 10", mvName);
+        starRocksAssert.withMaterializedView(sql, () -> {
+            refreshMaterializedView(DB_NAME, mvName);
+        });
+
+        // visit after drop mv
+        {
+            JsonMetricVisitor visitor = new JsonMetricVisitor("starrocks_fe");
+            MaterializedViewMetricsRegistry.collectMaterializedViewMetrics(visitor, true);
+            String json = visitor.build();
+            System.out.println(json);
+            Assert.assertTrue(json.contains("mv_refresh_jobs"));
+            Assert.assertTrue(json.contains("mv_refresh_total_success_jobs"));
+            Assert.assertTrue(json.contains("mv_refresh_total_retry_meta_count"));
+        }
     }
 }
