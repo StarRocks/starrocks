8c4e7f6b081246fcda313f2e1122b8f478758721
yanz <dirtysalt1987@gmail.com>
2024-05-22 09:51:30 -0700

    fix conflict
    Signed-off-by: yanz <dirtysalt1987@gmail.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/analyzer/MaterializedViewAnalyzerTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/analyzer/MaterializedViewAnalyzerTest.java
@@ -262,15 +262,6 @@ public class MaterializedViewAnalyzerTest {
                         "\"storage_medium\" = \"HDD\"\n" +
                         ")\n" +
                         "AS SELECT k1, k2, v1 from test.tbl1");
-<<<<<<< HEAD
-        ShowExecutor showExecutor = new ShowExecutor(starRocksAssert.getCtx(),
-                (ShowStmt) analyzeSuccess("show full columns from mv1"));
-        ShowResultSet showResultSet = showExecutor.execute();
-        Assert.assertEquals("[[a, date, , YES, YES, null, , , a1]," +
-                        " [b, int, , YES, YES, null, , , b2]," +
-                        " [c, int, , YES, YES, null, , , ]]",
-                showResultSet.getResultRows().toString());
-=======
         {
             ShowResultSet showResultSet = ShowExecutor.execute((ShowStmt) analyzeSuccess("show full columns from mv1"),
                     starRocksAssert.getCtx());
@@ -285,7 +276,6 @@ public class MaterializedViewAnalyzerTest {
             String result = showResultSet.getResultRows().toString();
             Assert.assertTrue(result.contains("zstd"));
         }
->>>>>>> 8755ada528 ([Enhancement] support specify compression in mv table (#46040))
     }
 
     @Test
