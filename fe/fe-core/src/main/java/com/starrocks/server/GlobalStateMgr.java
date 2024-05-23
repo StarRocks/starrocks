d154ecfc434e607300b4fb581fc364f9c71a6f00
yiming <xiadejun@starrocks.com>
2024-05-23 09:39:19 +0800

    Update GlobalStateMgr.java
    Signed-off-by: yiming <xiadejun@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
@@ -2426,7 +2426,7 @@ public class GlobalStateMgr {
             return GsonUtils.GSON.toJson(data);
         } catch (Exception e) {
             // In older version, data may not be json, here we just return the binary data.
-            return data.
+            return data;
         }
     }
 
