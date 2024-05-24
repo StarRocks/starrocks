d55827b18b7325bfe2d8821f10150355c6f17b31
Alex Zhu <zhuming9011@gmail.com>
2024-05-24 20:34:26 +0800

    [UT] Fix flaky test
    Signed-off-by: Alex Zhu <zhuming9011@gmail.com>

--- be/src/runtime/lake_tablets_channel.cpp
+++ be/src/runtime/lake_tablets_channel.cpp
@@ -275,6 +275,7 @@ LakeTabletsChannel::~LakeTabletsChannel() {
 
 Status LakeTabletsChannel::open(const PTabletWriterOpenRequest& params, PTabletWriterOpenResult* result,
                                 std::shared_ptr<OlapTableSchemaParam> schema, bool is_incremental) {
+    DCHECK_EQ(-1, _txn_id);
     SCOPED_TIMER(_open_timer);
     COUNTER_UPDATE(_open_counter, 1);
     std::unique_lock<bthreads::BThreadSharedMutex> l(_rw_mtx);
