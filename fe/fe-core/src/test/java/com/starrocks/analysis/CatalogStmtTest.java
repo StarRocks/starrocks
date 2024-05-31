0ede917a891d7e48086316aa973e994d3b3732a7
hezhao2 <hezhao2@cisco.com>
2024-05-31 17:19:22 +0800

    change UT

--- fe/fe-core/src/test/java/com/starrocks/analysis/CatalogStmtTest.java
+++ fe/fe-core/src/test/java/com/starrocks/analysis/CatalogStmtTest.java
@@ -63,7 +63,7 @@ public class CatalogStmtTest {
         Assert.assertEquals("CREATE EXTERNAL CATALOG 'catalog_5' PROPERTIES(\"type\"  =  \"hive\")", stmt2.toSql());
         String sql_7 = "CREATE EXTERNAL CATALOG catalog_6 IF NOT EXISTS PROPERTIES(\"type\"=\"hive\", \"hive.metastore.uris\"=\"thrift://127.0.0.1:9083\")";
         StatementBase stmt3 = AnalyzeTestUtil.analyzeSuccess(sql_7);
-        Assert.assertEquals("CREATE EXTERNAL CATALOG 'catalog_6' IF NOT EXISTS \"PROPERTIES(\"hive.metastore.uris\"  =  \"thrift://127.0.0.1:9083\", \"type\"  =  \"hive\")", stmt3.toSql());
+        Assert.assertEquals(sql_7, stmt3.toSql());
     }
 
     @Test
