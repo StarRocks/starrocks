4c4e2ea31ae6c79ed48ba8b3f51f122ad328cb56
tracymacding <tracymacding@gmail.com>
2024-06-03 06:56:37 +0800

    [Enhancement] Optimize fast schema evolution performance for cloud native table
    Signed-off-by: tracymacding <tracymacding@gmail.com>

--- be/src/runtime/exec_env.h
+++ be/src/runtime/exec_env.h
@@ -272,6 +272,7 @@ public:
     workgroup::ScanExecutor* connector_scan_executor() { return _connector_scan_executor; }
     ThreadPool* load_rowset_thread_pool() { return _load_rowset_thread_pool; }
     ThreadPool* load_segment_thread_pool() { return _load_segment_thread_pool; };
+    ThreadPool* update_tablet_meta_thread_pool() { return _update_tablet_meta_pool; }
 
     PriorityThreadPool* udf_call_pool() { return _udf_call_pool; }
     PriorityThreadPool* pipeline_prepare_pool() { return _pipeline_prepare_pool; }
@@ -354,6 +355,7 @@ private:
 
     ThreadPool* _load_segment_thread_pool = nullptr;
     ThreadPool* _load_rowset_thread_pool = nullptr;
+    ThreadPool* _update_tablet_meta_pool = nullptr;
 
     workgroup::ScanExecutor* _scan_executor = nullptr;
     workgroup::ScanExecutor* _connector_scan_executor = nullptr;
