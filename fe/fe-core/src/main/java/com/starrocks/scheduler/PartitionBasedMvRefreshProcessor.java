3949ad2f5e010071378a8b9208778940bd36116a
kaijian.ding <kaijian.ding@gmail.com>
2024-06-06 19:01:41 +0800

    little refactor
    Signed-off-by: kaijian.ding <kaijian.ding@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/scheduler/PartitionBasedMvRefreshProcessor.java
+++ fe/fe-core/src/main/java/com/starrocks/scheduler/PartitionBasedMvRefreshProcessor.java
@@ -117,7 +117,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -527,13 +526,13 @@ public class PartitionBasedMvRefreshProcessor extends BaseTaskRunProcessor {
         for (String partitionName : partitionsToRefresh) {
             mappedPartitionsToRefresh.put(partitionName, rangePartitionMap.get(partitionName));
         }
-        LinkedHashMap<String, Range<PartitionKey>> sortedPartition = mappedPartitionsToRefresh.entrySet().stream()
+        LinkedList<String> sortedPartition = mappedPartitionsToRefresh.entrySet().stream()
                 .sorted(Map.Entry.comparingByValue(RangeUtils.RANGE_COMPARATOR))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
-        LinkedList<String> partitionNames = new LinkedList<>(sortedPartition.keySet());
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toCollection (LinkedList::new));
 
         Iterator<String> partitionNameIter = Config.materialized_view_refresh_ascending
-                ? partitionNames.iterator() : partitionNames.descendingIterator();
+                ? sortedPartition.iterator() : sortedPartition.descendingIterator();
         String mvRefreshPartition = "";
         for (int i = 0; i < partitionRefreshNumber; i++) {
             if (partitionNameIter.hasNext()) {
@@ -565,7 +564,7 @@ public class PartitionBasedMvRefreshProcessor extends BaseTaskRunProcessor {
             }
         }
         if (!Config.materialized_view_refresh_ascending) {
-            partitionNameIter = partitionNames.iterator();
+            partitionNameIter = sortedPartition.iterator();
         }
         setNextPartitionStartAndEnd(partitionsToRefresh, mappedPartitionsToRefresh, partitionNameIter);
     }
