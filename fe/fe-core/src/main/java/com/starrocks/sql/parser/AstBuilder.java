6492ef9b848bf223c9e9263d0865fb2b7d0fd12a
HangyuanLiu <460660596@qq.com>
2024-06-07 10:04:01 +0800

    [Enhancement] Support add multi range partition manually to expression partition table (#46359)
    Signed-off-by: HangyuanLiu <460660596@qq.com>
    (cherry picked from commit c3fa54f626d2b88279f0439658075ef208b7ccfa)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/common/ErrorCode.java
    #	fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
    #	fe/fe-core/src/main/java/com/starrocks/sql/analyzer/CreateTableAnalyzer.java
    #	fe/fe-core/src/main/java/com/starrocks/sql/ast/DropPartitionClause.java
    #	fe/fe-core/src/main/java/com/starrocks/sql/parser/AstBuilder.java

--- fe/fe-core/src/main/java/com/starrocks/sql/parser/AstBuilder.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/parser/AstBuilder.java
@@ -1548,7 +1548,7 @@ public class AstBuilder extends StarRocksBaseVisitor<ParseNode> {
                 LocalDateTime startTime =
                         DateUtils.parseStringWithDefaultHSM(stringLiteral.getStringValue(), dateTimeFormatter);
                 schedule.setStartTime(startTime.atZone(TimeUtils.getTimeZone().toZoneId()).toEpochSecond());
-            } catch (AnalysisException e) {
+            } catch (SemanticException e) {
                 throw new ParsingException(PARSER_ERROR_MSG.invalidDateFormat(stringLiteral.getStringValue()),
                         timePos);
             }
@@ -7071,7 +7071,7 @@ public class AstBuilder extends StarRocksBaseVisitor<ParseNode> {
                             parseStringWithDefaultHSM(stringLiteral.getStringValue(), dateTimeFormatter);
                     startTime = tempStartTime;
                     defineStartTime = true;
-                } catch (AnalysisException e) {
+                } catch (SemanticException e) {
                     throw new ParsingException(PARSER_ERROR_MSG.invalidDateFormat(stringLiteral.getStringValue()),
                             timePos);
                 }
