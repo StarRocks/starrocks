4ca4a3683bc55f2465b24d70f87629cc8ebdbba7
stdpain <34912776+stdpain@users.noreply.github.com>
2024-06-12 10:12:47 +0800

    [BugFix] Fix inconsistent dictionary in replace partition (#46832)
    Signed-off-by: stdpain <drfeng08@gmail.com>
    (cherry picked from commit 85cbcfc3c631dcca4e3c2f0b997aa32ea99a72f9)

--- fe/fe-core/src/main/java/com/starrocks/catalog/OlapTable.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/OlapTable.java
@@ -98,6 +98,7 @@ import com.starrocks.sql.analyzer.AnalyzerUtils;
 import com.starrocks.sql.analyzer.SemanticException;
 import com.starrocks.sql.ast.PartitionValue;
 import com.starrocks.sql.common.SyncPartitionUtils;
+import com.starrocks.sql.optimizer.statistics.IDictManager;
 import com.starrocks.system.SystemInfoService;
 import com.starrocks.task.AgentBatchTask;
 import com.starrocks.task.AgentTask;
@@ -2632,6 +2633,10 @@ public class OlapTable extends Table {
                 renamePartition(tempPartitionNames.get(i), partitionNames.get(i));
             }
         }
+
+        for (Column column : getColumns()) {
+            IDictManager.getInstance().removeGlobalDict(this.getId(), column.getName());
+        }
     }
 
     // used for unpartitioned table in insert overwrite
@@ -2657,6 +2662,10 @@ public class OlapTable extends Table {
 
         // rename partition
         renamePartition(tempPartitionName, sourcePartitionName);
+
+        for (Column column : getColumns()) {
+            IDictManager.getInstance().removeGlobalDict(this.getId(), column.getName());
+        }
     }
 
     public void addTempPartition(Partition partition) {
