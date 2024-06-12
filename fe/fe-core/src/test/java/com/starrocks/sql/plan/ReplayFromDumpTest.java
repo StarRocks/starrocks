34a4cc8f471ed10da73c35d6438c6ac07409d112
packy92 <wangchao@starrocks.com>
2024-06-12 10:52:25 +0800

    resolve conflict
    Signed-off-by: packy92 <wangchao@starrocks.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/ReplayFromDumpTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/ReplayFromDumpTest.java
@@ -866,6 +866,7 @@ public class ReplayFromDumpTest extends ReplayFromDumpTestBase {
                 "  |  output: multi_distinct_count(NULL)"));
     }
 
+    @Test
     public void testNestedViewWithCTE() throws Exception {
 
         Pair<QueryDumpInfo, String> replayPair =
