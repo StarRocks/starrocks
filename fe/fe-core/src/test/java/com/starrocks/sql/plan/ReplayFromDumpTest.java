1a3414abf1ace5cad7780d58dcde05e3802bef4a
packy92 <110370499+packy92@users.noreply.github.com>
2024-06-13 01:58:16 +0800

    [BugFix] fix NaN result in expression statistics calculating  (#46890)
    Signed-off-by: packy92 <wangchao@starrocks.com>
    (cherry picked from commit c90a9d617704f1879aee5fcce60f1be7220967cf)
    
    # Conflicts:
    #	fe/fe-core/src/test/java/com/starrocks/sql/optimizer/statistics/ExpressionStatisticsCalculatorTest.java

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/ReplayFromDumpTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/ReplayFromDumpTest.java
@@ -954,4 +954,16 @@ public class ReplayFromDumpTest extends ReplayFromDumpTestBase {
         Assert.assertTrue(replayPair.second, replayPair.second.contains("9:AGGREGATE (update serialize)\n" +
                 "  |  output: multi_distinct_count(NULL)"));
     }
+
+    @Test
+    public void testSplitOrderBy() throws Exception {
+        Pair<QueryDumpInfo, String> replayPair =
+                getPlanFragment(getDumpInfoFromFile("query_dump/split_order_by"),
+                        null, TExplainLevel.NORMAL);
+        Assert.assertTrue(replayPair.second, replayPair.second.contains("21:MERGING-EXCHANGE"));
+        Assert.assertTrue(replayPair.second, replayPair.second.contains("20:TOP-N"));
+        Assert.assertTrue(replayPair.second, replayPair.second.contains("15:MERGING-EXCHANGE"));
+        Assert.assertTrue(replayPair.second, replayPair.second.contains("14:TOP-N"));
+
+    }
 }
