cc078d458e037a3eb5b11c7b222d88f366db784c
meegoo <meegoo.sr@gmail.com>
2024-06-14 02:16:33 +0800

    [BugFix] Fix brpc continuously fail after backend host restart
    Signed-off-by: meegoo <meegoo.sr@gmail.com>

--- be/src/exec/pipeline/exchange/sink_buffer.h
+++ be/src/exec/pipeline/exchange/sink_buffer.h
@@ -33,6 +33,7 @@
 #include "util/brpc_stub_cache.h"
 #include "util/defer_op.h"
 #include "util/disposable_closure.h"
+#include "util/internal_service_recoverable_stub.h"
 #include "util/phmap/phmap.h"
 
 namespace starrocks::pipeline {
@@ -49,7 +50,7 @@ struct TransmitChunkInfo {
     // a same exchange source fragment instance, so we should use fragment_instance_id
     // of the destination as the key of destination instead of channel_id.
     TUniqueId fragment_instance_id;
-    PInternalService_Stub* brpc_stub;
+    std::shared_ptr<PInternalService_RecoverableStub> brpc_stub;
     PTransmitChunkParamsPtr params;
     butil::IOBuf attachment;
     int64_t attachment_physical_bytes;
