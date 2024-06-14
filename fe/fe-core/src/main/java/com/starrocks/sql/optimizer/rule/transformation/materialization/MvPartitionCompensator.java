1bbdd84056aa625d05538937af42e25ad0ef20d0
kaijian.ding <kaijian.ding@gmail.com>
2024-06-14 17:17:34 +0800

    [BugFix] Fix compensate query partition predicate incorrect return
    Signed-off-by: kaijian.ding <kaijian.ding@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/materialization/MvPartitionCompensator.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/materialization/MvPartitionCompensator.java
@@ -742,7 +742,7 @@ public class MvPartitionCompensator {
                             .findFirst();
             // compensate nothing if there is no partition column predicate in the scan node.
             if (!partitionColumn.isPresent()) {
-                return null;
+                return partitionPredicates;
             }
 
             ExpressionMapping mapping =
