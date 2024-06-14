fd86ac621ccc4bd49c6276f73bf8d1fa4d11d3fa
predator4ann <yunlong.sun@hotmail.com>
2024-06-14 19:59:10 +0800

    [Enhancement] compatible with lower version kudu-client api (#46761)
    Signed-off-by: predator4ann <yunlong.sun@hotmail.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/KuduTable.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/KuduTable.java
@@ -27,6 +27,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -38,10 +39,12 @@ public class KuduTable extends Table {
     public static final Set<String> KUDU_INPUT_FORMATS = Sets.newHashSet(
             "org.apache.hadoop.hive.kudu.KuduInputFormat", "org.apache.kudu.mapreduce.KuduTableInputFormat");
     public static final String PARTITION_NULL_VALUE = "null";
+    public static final String PARAMETER_KEY_KUDU_TABLE_NAME = "kudu.table_name";
     private String masterAddresses;
     private String catalogName;
     private String databaseName;
     private String tableName;
+    private Optional<String> kuduTableName;
     private List<String> partColNames;
     private Map<String, String> properties;
 
@@ -49,20 +52,22 @@ public class KuduTable extends Table {
         super(TableType.KUDU);
     }
 
-    public KuduTable(String masterAddresses, String catalogName, String dbName, String tblName, List<Column> schema,
-                     List<String> partColNames) {
+    public KuduTable(String masterAddresses, String catalogName, String dbName, String tblName, String kuduTableName,
+                     List<Column> schema, List<String> partColNames) {
         super(CONNECTOR_ID_GENERATOR.getNextId().asInt(), tblName, TableType.KUDU, schema);
         this.masterAddresses = masterAddresses;
         this.catalogName = catalogName;
         this.databaseName = dbName;
         this.tableName = tblName;
+        this.kuduTableName = Optional.ofNullable(kuduTableName);
         this.partColNames = partColNames;
     }
 
     public static KuduTable fromMetastoreTable(org.apache.hadoop.hive.metastore.api.Table table, String catalogName,
                                                List<Column> fullSchema, List<String> partColNames) {
+        String kuduTableName = table.getParameters().get(PARAMETER_KEY_KUDU_TABLE_NAME);
         return new KuduTable(StringUtils.EMPTY, catalogName, table.getDbName(), table.getTableName(),
-                fullSchema, partColNames);
+                kuduTableName, fullSchema, partColNames);
     }
 
     public String getMasterAddresses() {
@@ -79,6 +84,9 @@ public class KuduTable extends Table {
 
     public String getTableName() {
         return tableName;
+    }
+    public Optional<String> getKuduTableName() {
+        return kuduTableName;
     }
 
     @Override
