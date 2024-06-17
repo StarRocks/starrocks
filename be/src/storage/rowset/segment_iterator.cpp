65a4e2c20a3415533173feb55ad39ca84d97cd32
tracymacding <tracymacding@gmail.com>
2024-06-17 09:35:30 +0800

    [BugFix] Use RandomAccessFile::get_size to get cloud native file size rather than StarletFS::get_file_size which is not implemented now (#47019)
    Signed-off-by: tracymacding <tracymacding@gmail.com>
    (cherry picked from commit 0e429a5d8dd1ddcf86ae738b4823eb6e0e92a906)

--- be/src/storage/rowset/segment_iterator.cpp
+++ be/src/storage/rowset/segment_iterator.cpp
@@ -576,7 +576,7 @@ Status SegmentIterator::_init_column_iterator_by_cid(const ColumnId cid, const C
         ASSIGN_OR_RETURN(auto rfile, _opts.fs->new_random_access_file(opts, _segment->file_info()));
         if (config::io_coalesce_lake_read_enable && !_segment->is_default_column(col) &&
             _segment->lake_tablet_manager() != nullptr) {
-            ASSIGN_OR_RETURN(auto file_size, _segment->get_data_size());
+            ASSIGN_OR_RETURN(auto file_size, rfile->get_size());
             auto shared_buffered_input_stream =
                     std::make_unique<io::SharedBufferedInputStream>(rfile->stream(), _segment->file_name(), file_size);
             auto options = io::SharedBufferedInputStream::CoalesceOptions{
