920c82017cb4b447e04af3636107e46bce6d4e8a
zhangqiang <qiangzh95@gmail.com>
2024-06-17 14:07:29 +0800

    [Enhancement] Reduce the memory usage during data ingestion (#47047)
    Signed-off-by: sevev <qiangzh95@gmail.com>

--- be/src/storage/delta_writer.cpp
+++ be/src/storage/delta_writer.cpp
@@ -365,6 +365,8 @@ Status DeltaWriter::_init() {
     VLOG(2) << "DeltaWriter [tablet_id=" << _opt.tablet_id << ", load_id=" << print_id(_opt.load_id)
             << ", replica_state=" << _replica_state_name(_replica_state) << "] open success.";
 
+    // no need after initialization.
+    _opt.ptable_schema_param = nullptr;
     return Status::OK();
 }
 
@@ -613,27 +615,29 @@ Status DeltaWriter::_flush_memtable() {
     return st;
 }
 
-Status DeltaWriter::_build_current_tablet_schema(int64_t index_id, const POlapTableSchemaParam& ptable_schema_param,
+Status DeltaWriter::_build_current_tablet_schema(int64_t index_id, const POlapTableSchemaParam* ptable_schema_param,
                                                  const TabletSchemaCSPtr& ori_tablet_schema) {
     // new tablet schema if new table
     // find the right index id
     int i = 0;
-    for (; i < ptable_schema_param.indexes_size(); i++) {
-        if (ptable_schema_param.indexes(i).id() == index_id) break;
-    }
-    if (i < ptable_schema_param.indexes_size()) {
-        if (ptable_schema_param.indexes_size() > 0 && ptable_schema_param.indexes(i).has_column_param() &&
-            ptable_schema_param.indexes(i).column_param().columns_desc_size() != 0 &&
-            ptable_schema_param.indexes(i).column_param().columns_desc(0).unique_id() >= 0 &&
-            ptable_schema_param.version() != ori_tablet_schema->schema_version()) {
-            ASSIGN_OR_RETURN(
-                    _tablet_schema,
-                    TabletSchema::create(*ori_tablet_schema, ptable_schema_param.indexes(i).schema_id(),
-                                         ptable_schema_param.version(), ptable_schema_param.indexes(i).column_param()));
-            if (_tablet_schema->schema_version() > ori_tablet_schema->schema_version()) {
-                _tablet->update_max_version_schema(_tablet_schema);
+    if (ptable_schema_param != nullptr) {
+        for (; i < ptable_schema_param->indexes_size(); i++) {
+            if (ptable_schema_param->indexes(i).id() == index_id) break;
+        }
+        if (i < ptable_schema_param->indexes_size()) {
+            if (ptable_schema_param->indexes_size() > 0 && ptable_schema_param->indexes(i).has_column_param() &&
+                ptable_schema_param->indexes(i).column_param().columns_desc_size() != 0 &&
+                ptable_schema_param->indexes(i).column_param().columns_desc(0).unique_id() >= 0 &&
+                ptable_schema_param->version() != ori_tablet_schema->schema_version()) {
+                ASSIGN_OR_RETURN(_tablet_schema,
+                                 TabletSchema::create(*ori_tablet_schema, ptable_schema_param->indexes(i).schema_id(),
+                                                      ptable_schema_param->version(),
+                                                      ptable_schema_param->indexes(i).column_param()));
+                if (_tablet_schema->schema_version() > ori_tablet_schema->schema_version()) {
+                    _tablet->update_max_version_schema(_tablet_schema);
+                }
+                return Status::OK();
             }
-            return Status::OK();
         }
     }
     _tablet_schema = ori_tablet_schema;
