cf939df806a5de54c167e3425bd7ae03fb3d5cbe
sevev <qiangzh95@gmail.com>
2024-06-17 17:09:19 +0800

    resolve conflict
    Signed-off-by: sevev <qiangzh95@gmail.com>

--- be/src/storage/delta_writer.cpp
+++ be/src/storage/delta_writer.cpp
@@ -617,20 +617,6 @@ Status DeltaWriter::_build_current_tablet_schema(int64_t index_id, const POlapTa
     // new tablet schema if new table
     // find the right index id
     int i = 0;
-<<<<<<< HEAD
-    for (; i < ptable_schema_param.indexes_size(); i++) {
-        if (ptable_schema_param.indexes(i).id() == index_id) break;
-    }
-    if (i < ptable_schema_param.indexes_size()) {
-        if (ptable_schema_param.indexes_size() > 0 && ptable_schema_param.indexes(i).has_column_param() &&
-            ptable_schema_param.indexes(i).column_param().columns_desc_size() != 0 &&
-            ptable_schema_param.indexes(i).column_param().columns_desc(0).unique_id() >= 0 &&
-            ptable_schema_param.version() != ori_tablet_schema->schema_version()) {
-            new_schema->copy_from(ori_tablet_schema);
-            RETURN_IF_ERROR(new_schema->build_current_tablet_schema(
-                    ptable_schema_param.indexes(i).schema_id(), ptable_schema_param.version(),
-                    ptable_schema_param.indexes(i).column_param(), ori_tablet_schema));
-=======
     if (ptable_schema_param != nullptr) {
         for (; i < ptable_schema_param->indexes_size(); i++) {
             if (ptable_schema_param->indexes(i).id() == index_id) break;
@@ -640,16 +626,11 @@ Status DeltaWriter::_build_current_tablet_schema(int64_t index_id, const POlapTa
                 ptable_schema_param->indexes(i).column_param().columns_desc_size() != 0 &&
                 ptable_schema_param->indexes(i).column_param().columns_desc(0).unique_id() >= 0 &&
                 ptable_schema_param->version() != ori_tablet_schema->schema_version()) {
-                ASSIGN_OR_RETURN(_tablet_schema,
-                                 TabletSchema::create(*ori_tablet_schema, ptable_schema_param->indexes(i).schema_id(),
-                                                      ptable_schema_param->version(),
-                                                      ptable_schema_param->indexes(i).column_param()));
-                if (_tablet_schema->schema_version() > ori_tablet_schema->schema_version()) {
-                    _tablet->update_max_version_schema(_tablet_schema);
-                }
-                return Status::OK();
+                new_schema->copy_from(ori_tablet_schema);
+                RETURN_IF_ERROR(new_schema->build_current_tablet_schema(
+                        ptable_schema_param.indexes(i).schema_id(), ptable_schema_param.version(),
+                        ptable_schema_param.indexes(i).column_param(), ori_tablet_schema));
             }
->>>>>>> 920c82017c ([Enhancement] Reduce the memory usage during data ingestion (#47047))
         }
     }
     if (new_schema->schema_version() > ori_tablet_schema->schema_version()) {
