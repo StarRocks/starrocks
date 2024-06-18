bf48bf8677292f80ee808fe01f8e06a78bcd5503
meegoo <meegoo.sr@gmail.com>
2024-06-18 14:33:54 +0800

    [Enhancement] Optimize performance of get physical partition from olap table
    Signed-off-by: meegoo <meegoo.sr@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
+++ fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
@@ -1686,6 +1686,7 @@ public class LocalMetastore implements ConnectorMetadata, MVRepairHandler {
             for (PhysicalPartition subPartition : subPartitions) {
                 // add sub partition
                 partition.addSubPartition(subPartition);
+                olapTable.addPhysicalPartition(subPartition);
             }
 
             // add partition log
@@ -1704,6 +1705,7 @@ public class LocalMetastore implements ConnectorMetadata, MVRepairHandler {
             Partition partition = olapTable.getPartition(info.getPartitionId());
             PhysicalPartition physicalPartition = info.getPhysicalPartition();
             partition.addSubPartition(physicalPartition);
+            olapTable.addPhysicalPartition(physicalPartition);
 
             if (!isCheckpointThread()) {
                 // add to inverted index
