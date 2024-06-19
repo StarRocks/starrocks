b30da1e088b0c9b5e77c22ff3cb3ce9067f3fcaa
Yixin Luo <18810541851@163.com>
2024-06-19 23:13:40 +0800

    [Enhancement] improve cloud native pk index rebuild and compaction strategy (#47065)
    Signed-off-by: luohaha <18810541851@163.com>
    (cherry picked from commit 6bd96a74d1c3fff4ff16abdbb84da7521460949f)
    
    # Conflicts:
    #	be/test/storage/lake/primary_key_publish_test.cpp

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -986,7 +986,9 @@ CONF_mInt64(lake_pk_compaction_min_input_segments, "5");
 // Used for control memory usage of update state cache and compaction state cache
 CONF_mInt32(lake_pk_preload_memory_limit_percent, "30");
 CONF_mInt32(lake_pk_index_sst_min_compaction_versions, "2");
-CONF_mInt32(lake_pk_index_sst_max_compaction_bytes, /*1GB*/ "1073741824");
+CONF_mInt32(lake_pk_index_sst_max_compaction_versions, "100");
+// When the ratio of cumulative level to base level is greater than this config, use base merge.
+CONF_mDouble(lake_pk_index_cumulative_base_compaction_ratio, "0.1");
 CONF_Int32(lake_pk_index_block_cache_limit_percent, "10");
 
 CONF_mBool(dependency_librdkafka_debug_enable, "false");
