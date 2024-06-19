bbe1f27dd4b6fbe6c60c743eb9e197339b287456
wyb <wybb86@gmail.com>
2024-06-19 17:00:16 +0800

    [Enhancement] Files table function support credential desensitization (#46893)
    Signed-off-by: wyb <wybb86@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/ast/InsertStmt.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/ast/InsertStmt.java
@@ -322,4 +322,12 @@ public class InsertStmt extends DmlStmt {
         List<Column> columns = collectSelectedFieldsFromQueryStatement();
         return new TableFunctionTable(columns, getTableFunctionProperties(), sessionVariable);
     }
+
+    @Override
+    public boolean needAuditEncryption() {
+        if (tableFunctionAsTargetTable) {
+            return true;
+        }
+        return false;
+    }
 }
