68d976c41dffcbb047b5ca04ceaff791ca65e536
gengjun-git <gengjun@starrocks.com>
2024-06-20 10:58:18 +0800

    [Refactor] Refine connection error (#46239)
    Signed-off-by: gengjun-git <gengjun@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/ConnectScheduler.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/ConnectScheduler.java
@@ -42,6 +42,7 @@ import com.starrocks.common.ThreadPoolManager;
 import com.starrocks.common.util.LogUtil;
 import com.starrocks.http.HttpConnectContext;
 import com.starrocks.mysql.MysqlProto;
+import com.starrocks.mysql.NegotiateState;
 import com.starrocks.mysql.nio.NConnectContext;
 import com.starrocks.privilege.AccessDeniedException;
 import com.starrocks.privilege.PrivilegeType;
@@ -236,7 +237,7 @@ public class ConnectScheduler {
                 MysqlProto.NegotiateResult result = null;
                 try {
                     result = MysqlProto.negotiate(context);
-                    if (!result.isSuccess()) {
+                    if (result.getState() != NegotiateState.OK) {
                         return;
                     }
 
@@ -249,8 +250,11 @@ public class ConnectScheduler {
                         return;
                     }
                 } finally {
-                    LogUtil.logConnectionInfoToAuditLogAndQueryQueue(context,
-                            result == null ? null : result.getAuthPacket());
+                    // Ignore the NegotiateState.READ_FIRST_AUTH_PKG_FAILED connections,
+                    // because this maybe caused by port probe.
+                    if (result != null && result.getState() != NegotiateState.READ_FIRST_AUTH_PKG_FAILED) {
+                        LogUtil.logConnectionInfoToAuditLogAndQueryQueue(context, result.getAuthPacket());
+                    }
                 }
 
                 context.setStartTime();
