8fc2479c6b0f51930dfde27ea9adc8036e57df71
Yixin Luo <18810541851@163.com>
2024-06-21 08:35:00 +0000

    resolve
    Signed-off-by: Yixin Luo <18810541851@163.com>

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -1317,22 +1317,8 @@ CONF_mInt64(arrow_read_batch_size, "4096");
 // Set to true to enable socket_keepalive option in brpc
 CONF_mBool(brpc_socket_keepalive, "false");
 
-CONF_mBool(apply_del_vec_after_all_index_filter, "true");
-<<<<<<< HEAD
-=======
-
 // .crm file can be removed after 1day.
 CONF_mInt32(unused_crm_file_threshold_second, "86400" /** 1day **/);
 
-// python envs config
-// create time worker timeout
-CONF_mInt32(create_child_worker_timeout_ms, "1000");
-// config ENV PYTHONPATH
-CONF_Strings(python_envs, "");
-// report python worker STDERR to client
-CONF_Bool(report_python_worker_error, "true");
-CONF_Bool(python_worker_reuse, "true");
-CONF_Int32(python_worker_expire_time_sec, "300");
-
->>>>>>> 131dbcee31 ([Enhancement] add backgroud gc for crm files (#47283))
+CONF_mBool(apply_del_vec_after_all_index_filter, "true");
 } // namespace starrocks::config
