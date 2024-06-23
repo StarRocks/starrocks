1f0066fcddf8975d22b30872516832c64f7c12cf
liubotao <316945435@qq.com>
2024-06-23 10:59:51 +0800

    add array sort by multi columns
    Signed-off-by: liubotao <316945435@qq.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzeExprTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzeExprTest.java
@@ -502,6 +502,7 @@ public class AnalyzeExprTest {
         analyzeFail("select array_to_bitmap('[1,2]')");
         analyzeFail("select array_sortby_multi('a')");
         analyzeFail("select array_sortby_multi([1, 2, 3], 'a')");
+        analyzeFail("select array_sortby_multi([map{'a':1, 'b':2, 'c':3}], [map{'c':4, 'd':5, 'e':6}])");
     }
 
     @Test
