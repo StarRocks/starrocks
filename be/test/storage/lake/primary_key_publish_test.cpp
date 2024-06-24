d1d932002a1432181bef5fabd888025af8ef9461
Yixin Luo <18810541851@163.com>
2024-06-24 06:59:47 +0000

    resolve
    Signed-off-by: Yixin Luo <18810541851@163.com>

--- be/test/storage/lake/primary_key_publish_test.cpp
+++ be/test/storage/lake/primary_key_publish_test.cpp
@@ -439,11 +439,11 @@ TEST_P(LakePrimaryKeyPublishTest, test_publish_with_oom) {
                                                    .set_txn_id(txn_id)
                                                    .set_partition_id(_partition_id)
                                                    .set_mem_tracker(_mem_tracker.get())
-                                                   .set_schema_id(_tablet_schema->id())
+                                                   .set_index_id(_tablet_schema->id())
                                                    .build());
         ASSERT_OK(delta_writer->open());
         ASSERT_OK(delta_writer->write(*chunk0, indexes.data(), indexes.size()));
-        ASSERT_OK(delta_writer->finish_with_txnlog());
+        ASSERT_OK(delta_writer->finish());
         delta_writer->close();
         // Publish version fail because of oom
         ASSERT_ERROR(publish_single_version(tablet_id, version + 1, txn_id).status());
