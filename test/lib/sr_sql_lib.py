c721731176fa32ac5cb04df39bef1c0785b3ee83
Letian Jiang <letian.jiang@outlook.com>
2024-06-27 10:29:53 +0800

    sink case
    Signed-off-by: Letian Jiang <letian.jiang@outlook.com>

--- test/lib/sr_sql_lib.py
+++ test/lib/sr_sql_lib.py
@@ -621,6 +621,7 @@ class StarrocksSQLApiLib(object):
             raise
 
     def conn_execute_sql(self, conn, sql):
+        var, sql = self.analyse_var(sql)
         try:
             cursor = conn.cursor()
             if sql.endswith(";"):
