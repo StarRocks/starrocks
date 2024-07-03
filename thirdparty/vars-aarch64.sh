85b88ebdd7318c7e4ef05ed181020b5bf9139ef8
starrocks-xupeng <xupeng@starrocks.com>
2024-07-03 13:05:52 +0800

    [Enhancement] upgrade staros to v3.2.2 (#47696)
    Signed-off-by: starrocks-xupeng <xupeng@starrocks.com>

--- thirdparty/vars-aarch64.sh
+++ thirdparty/vars-aarch64.sh
@@ -52,7 +52,7 @@ JINDOSDK_SOURCE="jindosdk-4.6.8-linux-el7-aarch64"
 JINDOSDK_MD5SUM="27a4e2cd9a403c6e21079a866287d88b"
 
 # starcache
-STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v3.2.4/starcache-centos7_arm64.tar.gz"
+STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v3.2.5/starcache-centos7_arm64.tar.gz"
 STARCACHE_NAME="starcache.tar.gz"
 STARCACHE_SOURCE="starcache"
-STARCACHE_MD5SUM="8617f5b3e67ccad59becbb54ec8c8c48"
+STARCACHE_MD5SUM="68db5036ede43571ecbce2b212bcc4df"
--- thirdparty/vars-x86_64.sh
+++ thirdparty/vars-x86_64.sh
@@ -52,7 +52,7 @@ JINDOSDK_SOURCE="jindosdk-4.6.8-linux"
 JINDOSDK_MD5SUM="5436e4fe39c4dfdc942e41821f1dd8a9"
 
 # starcache
-STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v3.2.4/starcache-centos7_amd64.tar.gz"
+STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v3.2.5/starcache-centos7_amd64.tar.gz"
 STARCACHE_NAME="starcache.tar.gz"
 STARCACHE_SOURCE="starcache"
-STARCACHE_MD5SUM="d63fb4136541e8bf9d6a3b9a488840a5"
+STARCACHE_MD5SUM="8530e713e76ea8294ed3ba2c5c717483"
