c642ccb87dc91fdac1f78b0ae3a0f829c5bf2de9
stephen <91597003+stephen-shelby@users.noreply.github.com>
2024-07-05 22:13:28 +0800

    [Feature] support create/replace branch based on iceberg (#47847)
    Signed-off-by: stephen <stephen5217@163.com>

--- fe/fe-core/src/main/java/com/starrocks/alter/AlterOpType.java
+++ fe/fe-core/src/main/java/com/starrocks/alter/AlterOpType.java
@@ -66,7 +66,7 @@ public enum AlterOpType {
 
     // Optimize table
     OPTIMIZE,
-
+    ALTER_BRANCH,
     INVALID_OP; // INVALID_OP must be the last one
 
     // true means 2 operations have no conflict.
--- fe/fe-core/src/main/java/com/starrocks/sql/ast/AstVisitor.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/ast/AstVisitor.java
@@ -1175,6 +1175,11 @@ public interface AstVisitor<R, C> {
         return visitNode(clause, context);
     }
 
+    // ------------------------------------------- Branch ----------------------------------==------------------------
+    default R visitCreateReplaceBranchClause(CreateOrReplaceBranchClause clause, C context) {
+        return visitNode(clause, context);
+    }
+
     // ------------------------------------------- Relation ----------------------------------==------------------------
 
     default R visitRelation(Relation node, C context) {
