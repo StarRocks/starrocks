4b6384b5dd1eb25a0ea4f0b46cb8fb27c803c56a
zihe.liu <ziheliu1024@gmail.com>
2024-07-09 17:09:59 +0800

    s
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- be/src/exprs/utility_functions.cpp
+++ be/src/exprs/utility_functions.cpp
@@ -317,9 +317,9 @@ StatusOr<ColumnPtr> UtilityFunctions::get_query_dump(FunctionContext* context, c
     std::vector<TGetQueryDumpRequestItem> req_items;
     for (size_t i = 0; i < columns[0]->size(); ++i) {
         TGetQueryDumpRequestItem item;
-        item.__set_catalog_name(catalogs.value(i));
-        item.__set_db_name(dbs.value(i));
-        item.__set_query_id(queries.value(i));
+        item.__set_catalog_name(catalogs.value(i).to_string());
+        item.__set_db_name(dbs.value(i).to_string());
+        item.__set_query_id(queries.value(i).to_string());
         item.__set_enable_mock(enable_mocks.value(i));
 
         req_items.emplace_back(std::move(item));
