66de81ceb611d5dcec6e92233a9e6d5dad75d681
Seaven <seaven_7@qq.com>
2024-07-11 16:14:28 +0800

    [BugFix] Fix concurrent execute let rewrite failed (#48180)
    Signed-off-by: Seaven <seaven_7@qq.com>
    (cherry picked from commit 0dbfb59ab5212a914118c95bea6e3ad0c6022046)

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/PushDownPredicateProjectRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/PushDownPredicateProjectRule.java
@@ -1,4 +1,16 @@
-// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Inc.
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package com.starrocks.sql.optimizer.rule.transformation;
 
@@ -34,8 +46,6 @@ public class PushDownPredicateProjectRule extends TransformationRule {
             new SimplifiedPredicateRule()
     );
 
-    private final ScalarOperatorRewriter scalarRewriter = new ScalarOperatorRewriter();
-
     public PushDownPredicateProjectRule() {
         super(RuleType.TF_PUSH_DOWN_PREDICATE_PROJECT,
                 Pattern.create(OperatorType.LOGICAL_FILTER).
@@ -68,15 +78,14 @@ public class PushDownPredicateProjectRule extends TransformationRule {
 
         // try rewrite new predicate
         // e.g. : select 1 as b, MIN(v1) from t0 having (b + 1) != b;
+        ScalarOperatorRewriter scalarRewriter = new ScalarOperatorRewriter();
         newPredicate = scalarRewriter.rewrite(newPredicate, PROJECT_REWRITE_PREDICATE_RULE);
 
         OptExpression newFilter = new OptExpression(new LogicalFilterOperator(newPredicate));
-
         newFilter.getInputs().addAll(child.getInputs());
-        child.getInputs().clear();
-        child.getInputs().add(newFilter);
 
-        return Lists.newArrayList(child);
+        OptExpression newProject = OptExpression.create(project, newFilter);
+        return Lists.newArrayList(newProject);
     }
 
 }
