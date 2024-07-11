41f4106381f65f839cebc58e255402431cc0c190
before-Sunrise <unclejyj@gmail.com>
2024-07-11 10:04:37 +0800

    fix another bug
    Signed-off-by: before-Sunrise <unclejyj@gmail.com>

--- be/src/runtime/multi_cast_data_stream_sink.h
+++ be/src/runtime/multi_cast_data_stream_sink.h
@@ -27,11 +27,11 @@ public:
     Status open(RuntimeState* state) override;
     Status close(RuntimeState* state, Status exec_status) override;
     RuntimeProfile* profile() override { return nullptr; }
-    std::vector<std::unique_ptr<DataStreamSender> >& get_sinks() { return _sinks; }
+    std::vector<std::unique_ptr<DataStreamSender>>& get_sinks() { return _sinks; }
     Status send_chunk(RuntimeState* state, Chunk* chunk) override;
 
-private:
-    std::vector<std::unique_ptr<DataStreamSender> > _sinks;
+protected:
+    std::vector<std::unique_ptr<DataStreamSender>> _sinks;
 };
 
 class SplitDataStreamSink : public MultiCastDataStreamSink {
