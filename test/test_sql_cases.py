31178c04853399446e6c2259c94017db9bcd3a43
AndyZiYe <yeziyu@starrocks.com>
2024-07-15 18:41:47 +0800

    ci ubuntu init
    Signed-off-by: AndyZiYe <yeziyu@starrocks.com>

--- test/test_sql_cases.py
+++ test/test_sql_cases.py
@@ -108,6 +108,8 @@ class TestSQLCases(sr_sql_lib.StarrocksSQLApiLib):
         if record_mode:
             tools.assert_true(res, "Save %s.%s result error" % (self.case_info.file, self.case_info.name))
 
+        log.info(f"Execution complete [{self.case_info.name}]!")
+
     # -------------------------------------------
     #         [CASE]
     # -------------------------------------------
