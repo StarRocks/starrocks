72f4ed27e62f438db82e733756027ee17ec3fdce
Murphy <mofei@starrocks.com>
2024-07-16 17:08:08 +0800

    support lookup_string function
    Signed-off-by: Murphy <mofei@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/function/MetaFunctions.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/function/MetaFunctions.java
@@ -18,6 +18,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
+import com.google.gson.annotations.SerializedName;
 import com.starrocks.analysis.TableName;
 import com.starrocks.catalog.Database;
 import com.starrocks.catalog.InternalCatalog;
@@ -32,9 +33,11 @@ import com.starrocks.common.util.concurrent.lock.Locker;
 import com.starrocks.connector.PartitionInfo;
 import com.starrocks.connector.PartitionUtil;
 import com.starrocks.connector.hive.Partition;
+import com.starrocks.load.pipe.filelist.RepoExecutor;
 import com.starrocks.memory.MemoryTrackable;
 import com.starrocks.memory.MemoryUsageTracker;
 import com.starrocks.monitor.unit.ByteSizeValue;
+import com.starrocks.persist.gson.GsonUtils;
 import com.starrocks.privilege.AccessDeniedException;
 import com.starrocks.privilege.ObjectType;
 import com.starrocks.privilege.PrivilegeType;
@@ -42,18 +45,28 @@ import com.starrocks.qe.ConnectContext;
 import com.starrocks.scheduler.TaskRunManager;
 import com.starrocks.server.GlobalStateMgr;
 import com.starrocks.sql.analyzer.Authorizer;
+import com.starrocks.sql.analyzer.SemanticException;
+import com.starrocks.sql.common.StarRocksPlannerException;
 import com.starrocks.sql.optimizer.CachingMvPlanContextBuilder;
 import com.starrocks.sql.optimizer.OptExpression;
 import com.starrocks.sql.optimizer.dump.QueryDumper;
 import com.starrocks.sql.optimizer.operator.scalar.ConstantOperator;
 import com.starrocks.sql.optimizer.rewrite.ConstantFunction;
+import com.starrocks.thrift.TResultBatch;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.http.HttpResponseStatus;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.ListUtils;
 import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.collections4.SetUtils;
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.spark.util.SizeEstimator;
 
 import java.lang.reflect.Field;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -362,6 +375,70 @@ public class MetaFunctions {
     @ConstantFunction(name = "get_query_dump", argTypes = {VARCHAR}, returnType = VARCHAR, isMetaFunction = true)
     public static ConstantOperator getQueryDump(ConstantOperator query) {
         return getQueryDump(query, ConstantOperator.createBoolean(false));
+    }
+
+    private static class LookupRecord {
+
+        @SerializedName("data")
+        public List<String> data;
+
+        public static LookupRecord fromJson(String json) {
+            return GsonUtils.GSON.fromJson(json, LookupRecord.class);
+        }
+    }
+
+    private static ConstantOperator deserializeBigint(List<TResultBatch> batches) {
+        for (TResultBatch batch : ListUtils.emptyIfNull(batches)) {
+            for (ByteBuffer buffer : batch.getRows()) {
+                ByteBuf copied = Unpooled.copiedBuffer(buffer);
+                String jsonString = copied.toString(Charset.defaultCharset());
+                List<String> data = LookupRecord.fromJson(jsonString).data;
+                if (CollectionUtils.isNotEmpty(data)) {
+                    return ConstantOperator.createVarchar(data.get(0));
+                } else {
+                    return ConstantOperator.NULL;
+                }
+            }
+        }
+        return ConstantOperator.NULL;
+    }
+
+    /**
+     * Lookup a value from a primary table, and evaluate in the optimizer
+     *
+     * @param tableName    table to lookup, must be a primary-key table
+     * @param lookupKey    key to lookup, must be a string type
+     * @param returnColumn column to return
+     * @return NULL if not found, otherwise return the value
+     */
+    @ConstantFunction(name = "lookup_string",
+            argTypes = {VARCHAR, VARCHAR, VARCHAR},
+            returnType = VARCHAR,
+            isMetaFunction = true)
+    public static ConstantOperator lookup_string(ConstantOperator tableName,
+                                                 ConstantOperator lookupKey,
+                                                 ConstantOperator returnColumn) {
+        TableName tableNameValue = TableName.fromString(tableName.getVarchar());
+        String sql = String.format("select cast(dict_mapping('%s', '%s', '%s') as string)",
+                tableNameValue.toString(), lookupKey.getVarchar(), returnColumn.getVarchar());
+        ConnectContext ctx = ConnectContext.get();
+        try {
+            List<TResultBatch> result = RepoExecutor.getInstance().executeDQL(sql);
+            return deserializeBigint(result);
+        } catch (Throwable e) {
+            final String notFoundMessage = "query failed if record not exist in dict table";
+            Throwable root = ExceptionUtils.getRootCause(e);
+            // Record not found
+            if (e.getMessage().contains(notFoundMessage) ||
+                    root != null && root.getMessage().contains(notFoundMessage)) {
+                return ConstantOperator.NULL;
+            }
+            if (root instanceof StarRocksPlannerException) {
+                throw new SemanticException("lookup failed: " + root.getMessage(), root);
+            } else {
+                throw new SemanticException("lookup failed: " + e.getMessage(), e);
+            }
+        }
     }
 
 }
