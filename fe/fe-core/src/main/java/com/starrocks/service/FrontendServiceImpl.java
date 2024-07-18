f14eb1f4f46ce61f9c5824e2898a51fa022837cc
wyb <wybb86@gmail.com>
2024-07-18 19:11:46 +0800

    [BugFix] Fix jobName filter not work when getting routine load jobs (#47907)
    Signed-off-by: wyb <wybb86@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/service/FrontendServiceImpl.java
+++ fe/fe-core/src/main/java/com/starrocks/service/FrontendServiceImpl.java
@@ -2679,6 +2679,7 @@ public class FrontendServiceImpl implements FrontendService.Iface {
         }
 
         result.setTrackingLoads(trackingLoadInfoList);
+        LOG.debug("get tracking load jobs size: {}", trackingLoadInfoList.size());
         return result;
     }
 
