64481023db6e74be5354a313d2f4ad119e8f0e75
zombee0 <ewang2027@gmail.com>
2024-07-18 17:25:01 +0800

    [Enhancement] optimize scan dop when shared scan
    Signed-off-by: zombee0 <ewang2027@gmail.com>

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -792,6 +792,8 @@ CONF_Int32(pipeline_analytic_removable_chunk_num, "128");
 CONF_Bool(pipeline_analytic_enable_streaming_process, "true");
 CONF_Bool(pipeline_analytic_enable_removable_cumulative_process, "true");
 CONF_Int32(pipline_limit_max_delivery, "4096");
+
+CONF_mBool(use_default_dop_when_shared_scan, "true");
 /// For parallel scan on the single tablet.
 // These three configs are used to calculate the minimum number of rows picked up from a segment at one time.
 // It is `splitted_scan_bytes/scan_row_bytes` and restricted in the range [min_splitted_scan_rows, max_splitted_scan_rows].
