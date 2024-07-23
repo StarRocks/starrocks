30104e04fe90ce85ed7ad27039a7cb5ef21dea0b
gengjun-git <gengjun@starrocks.com>
2024-07-23 14:08:36 +0800

    [BugFix] Fix Colocate Group marked as stable when checking urgent tables (#48680)
    Signed-off-by: gengjun-git <gengjun@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/http/meta/ColocateMetaService.java
+++ fe/fe-core/src/main/java/com/starrocks/http/meta/ColocateMetaService.java
@@ -295,7 +295,7 @@ public class ColocateMetaService {
     public static class BucketSeqAction extends ColocateMetaBaseAction {
         private static final Logger LOG = LogManager.getLogger(BucketSeqAction.class);
 
-        BucketSeqAction(ActionController controller) {
+        public BucketSeqAction(ActionController controller) {
             super(controller);
         }
 
@@ -353,11 +353,12 @@ public class ColocateMetaService {
             sendResult(request, response);
         }
 
-        private void updateBackendPerBucketSeq(GroupId groupId, List<List<Long>> backendsPerBucketSeq) {
+        public void updateBackendPerBucketSeq(GroupId groupId, List<List<Long>> backendsPerBucketSeq) {
             colocateIndex.addBackendsPerBucketSeq(groupId, backendsPerBucketSeq);
             ColocatePersistInfo info2 =
                     ColocatePersistInfo.createForBackendsPerBucketSeq(groupId, backendsPerBucketSeq);
             GlobalStateMgr.getCurrentState().getEditLog().logColocateBackendsPerBucketSeq(info2);
+            colocateIndex.markGroupUnstable(groupId, true);
         }
     }
 
