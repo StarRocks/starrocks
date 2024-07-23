4cfcd61d7b4dee32264119508ea193f4ad3d90d1
meegoo <meegoo.sr@gmail.com>
2024-07-23 11:59:49 +0800

    [Refactor] Add max column table limit max_column_number_per_table for table (#47869)
    Signed-off-by: meegoo <meegoo.sr@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/common/Config.java
+++ fe/fe-core/src/main/java/com/starrocks/common/Config.java
@@ -3127,4 +3127,7 @@ public class Config extends ConfigBase {
 
     @ConfField(mutable = true)
     public static long max_bucket_number_per_partition = 1024;
+
+    @ConfField(mutable = true)
+    public static int max_column_number_per_table = 10000;
 }
