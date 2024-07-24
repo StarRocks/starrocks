bfb84e986826e3c61103f8d0d8a63e13d75ac0c7
trueeyu <lxhhust350@qq.com>
2024-07-24 20:24:48 +0800

    [Enhancement] BE support don't dump retain pages of jemalloc (#48851)
    Signed-off-by: trueeyu <lxhhust350@qq.com>
    (cherry picked from commit b30d08d97ce1a37b8272b4d1f613b568631a0b9d)
    
    # Conflicts:
    #	be/src/common/config.h
    #	be/src/common/logconfig.cpp

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -1365,7 +1365,12 @@ CONF_mBool(enable_pk_strict_memcheck, "false");
 CONF_mBool(apply_del_vec_after_all_index_filter, "true");
 
 CONF_mBool(skip_lake_pk_preload, "false");
+<<<<<<< HEAD
 // Experimental feature, this configuration will be removed after testing is complete.
 CONF_mBool(lake_enable_alter_struct, "false");
+=======
+// Reduce core file size by not dumping jemalloc retain pages
+CONF_mBool(enable_core_file_size_optimization, "true");
+>>>>>>> b30d08d97c ([Enhancement] BE support don't dump retain pages of jemalloc (#48851))
 
 } // namespace starrocks::config
