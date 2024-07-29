4e126c287d0b1ae299d58c586917501c2b5d7840
packy92 <110370499+packy92@users.noreply.github.com>
2024-07-29 10:26:33 +0800

    [BugFix] fix incorrect healthy value in basicStatsMeta (#48935)
    Signed-off-by: packy92 <wangchao@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/statistic/BasicStatsMeta.java
+++ fe/fe-core/src/main/java/com/starrocks/statistic/BasicStatsMeta.java
@@ -58,6 +58,9 @@ public class BasicStatsMeta implements Writable {
     @SerializedName("updateRows")
     private long updateRows;
 
+    @SerializedName("deltaRows")
+    private long deltaRows;
+
     public BasicStatsMeta(long dbId, long tableId, List<String> columns,
                           StatsConstants.AnalyzeType type,
                           LocalDateTime updateTime,
@@ -144,7 +147,7 @@ public class BasicStatsMeta implements Writable {
                 updatePartitionCount++;
             }
         }
-        updatePartitionRowCount = Math.max(1, Math.max(tableRowCount, updateRows) - cachedTableRowCount);
+        updatePartitionRowCount = Math.max(1, Math.max(tableRowCount + deltaRows, updateRows) - cachedTableRowCount);
 
         double updateRatio;
         // 1. If none updated partitions, health is 1
@@ -153,9 +156,9 @@ public class BasicStatsMeta implements Writable {
         if (updatePartitionCount == 0) {
             return 1;
         } else if (updatePartitionCount < StatsConstants.STATISTICS_PARTITION_UPDATED_THRESHOLD) {
-            updateRatio = (updateRows * 1.0) / updatePartitionRowCount;
+            updateRatio = (updatePartitionRowCount * 1.0) / tableRowCount;
         } else {
-            double rowUpdateRatio = (updateRows * 1.0) / updatePartitionRowCount;
+            double rowUpdateRatio = (updatePartitionRowCount * 1.0) / tableRowCount;
             double partitionUpdateRatio = (updatePartitionCount * 1.0) / totalPartitionCount;
             updateRatio = Math.min(rowUpdateRatio, partitionUpdateRatio);
         }
@@ -170,8 +173,9 @@ public class BasicStatsMeta implements Writable {
         this.updateRows = updateRows;
     }
 
-    public void increaseUpdateRows(Long delta) {
+    public void increaseDeltaRows(Long delta) {
         updateRows += delta;
+        deltaRows += delta;
     }
 
     public boolean isInitJobMeta() {
