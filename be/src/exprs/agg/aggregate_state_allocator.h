a2b86a6b4e190884d58d0078cff585f3cd1d9d43
silverbullet233 <3675229+silverbullet233@users.noreply.github.com>
2024-07-31 16:21:08 +0800

    count approx_top_k&percentile_cont memory
    Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>

--- be/src/exprs/agg/aggregate_state_allocator.h
+++ be/src/exprs/agg/aggregate_state_allocator.h
@@ -86,4 +86,8 @@ template <typename T>
 using HashSetWithAggStateAllocator = phmap::flat_hash_set<T, StdHash<T>, phmap::priv::hash_default_eq<T>, AggregateStateAllocator<T>>;
 
 using SliceHashSetWithAggStateAllocator = phmap::flat_hash_set<SliceWithHash, HashOnSliceWithHash, EqualOnSliceWithHash, AggregateStateAllocator<SliceWithHash>>;
+
+template <typename T>
+using VectorWithAggStateAllocator = std::vector<T, AggregateStateAllocator<T>>;
+
 }
