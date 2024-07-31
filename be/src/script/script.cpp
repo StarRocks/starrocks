9bfb63c1774026b2082e0b98853206d86e6333a9
stdpain <34912776+stdpain@users.noreply.github.com>
2024-07-31 10:16:12 +0800

    [Enhancement] Support dynamic modification of VLOG level (#49149)
    Signed-off-by: stdpain <drfeng08@gmail.com>
    (cherry picked from commit 505a214747cd97df8d01349975ff3c904fbce91b)

--- be/src/script/script.cpp
+++ be/src/script/script.cpp
@@ -21,6 +21,7 @@
 #include "common/greplog.h"
 #include "common/logging.h"
 #include "common/prof/heap_prof.h"
+#include "common/vlog_cntl.h"
 #include "exec/schema_scanner/schema_be_tablets_scanner.h"
 #include "fs/key_cache.h"
 #include "gen_cpp/olap_file.pb.h"
@@ -261,6 +262,13 @@ void bind_exec_env(ForeignModule& m) {
         REG_METHOD(HeapProf, snapshot);
         REG_METHOD(HeapProf, to_dot_format);
         REG_METHOD(HeapProf, dump_dot_snapshot);
+    }
+    {
+        auto& cls = m.klass<VLogCntl>("VLogCntl");
+        REG_STATIC_METHOD(VLogCntl, getInstance);
+        REG_METHOD(VLogCntl, enable);
+        REG_METHOD(VLogCntl, disable);
+        REG_METHOD(VLogCntl, setLogLevel);
     }
 }
 
@@ -602,7 +610,7 @@ Status execute_script(const std::string& script, std::string& output) {
     bind_common(m);
     bind_exec_env(m);
     StorageEngineRef::bind(m);
-    vm.runFromSource("main", R"(import "starrocks" for ExecEnv, GlobalEnv, HeapProf, StorageEngine)");
+    vm.runFromSource("main", R"(import "starrocks" for ExecEnv, GlobalEnv, HeapProf, StorageEngine, VLogCntl)");
     try {
         vm.runFromSource("main", script);
     } catch (const std::exception& e) {
