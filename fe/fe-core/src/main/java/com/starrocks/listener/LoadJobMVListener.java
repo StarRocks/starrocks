85e17bfeb7d80f8dcfedc1c00f124b01ed009f95
shuming.li <ming.moriarty@gmail.com>
2024-07-31 17:34:34 +0800

    fix bugs
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/listener/LoadJobMVListener.java
+++ fe/fe-core/src/main/java/com/starrocks/listener/LoadJobMVListener.java
@@ -44,20 +44,36 @@ public class LoadJobMVListener implements LoadJobListener {
 
     public static final LoadJobMVListener INSTANCE = new LoadJobMVListener();
 
+    private static boolean isTriggerOnTransactionFinish(TransactionState transactionState) {
+        if (transactionState.getSourceType() == TransactionState.LoadJobSourceType.LAKE_COMPACTION) {
+            return false;
+        }
+        return true;
+    }
+
     @Override
     public void onStreamLoadTransactionFinish(TransactionState transactionState) {
+        if (!isTriggerOnTransactionFinish(transactionState)) {
+            return;
+        }
         // how to handle stream load transaction?
         triggerToRefreshRelatedMVs(transactionState, false);
     }
 
     @Override
     public void onLoadJobTransactionFinish(TransactionState transactionState) {
+        if (!isTriggerOnTransactionFinish(transactionState)) {
+            return;
+        }
         triggerToRefreshRelatedMVs(transactionState, false);
     }
 
     @Override
     public void onDMLStmtJobTransactionFinish(TransactionState transactionState, Database db, Table table) {
         if (table != null && table.isMaterializedView()) {
+            return;
+        }
+        if (!isTriggerOnTransactionFinish(transactionState)) {
             return;
         }
         triggerToRefreshRelatedMVs(db, table);
