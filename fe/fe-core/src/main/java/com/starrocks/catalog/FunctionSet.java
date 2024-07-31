594d07beaadd1c37e2752db92bee806cd11d4fda
packy92 <wangchao@starrocks.com>
2024-07-31 20:16:39 +0800

    resolve conflict
    Signed-off-by: packy92 <wangchao@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/FunctionSet.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/FunctionSet.java
@@ -105,8 +105,6 @@ public class FunctionSet {
     public static final String UTC_TIME = "utc_time";
     public static final String LOCALTIME = "localtime";
     public static final String LOCALTIMESTAMP = "localtimestamp";
-
-    public static final String WEEK = "week";
     public static final String WEEKOFYEAR = "weekofyear";
     public static final String WEEK = "week";
     public static final String YEAR = "year";
