2db7e4c4ce47e43504ca6ebc930785ac26a0c21a
stephen <91597003+stephen-shelby@users.noreply.github.com>
2024-08-01 20:15:33 +0800

    [Feature] support iceberg files table (#49270)
    Signed-off-by: stephen <stephen5217@163.com>

--- be/src/runtime/descriptors.cpp
+++ be/src/runtime/descriptors.cpp
@@ -740,7 +740,8 @@ Status DescriptorTbl::create(RuntimeState* state, ObjectPool* pool, const TDescr
         case TTableType::ICEBERG_HISTORY_TABLE:
         case TTableType::ICEBERG_METADATA_LOG_ENTRIES_TABLE:
         case TTableType::ICEBERG_SNAPSHOTS_TABLE:
-        case TTableType::ICEBERG_MANIFESTS_TABLE: {
+        case TTableType::ICEBERG_MANIFESTS_TABLE:
+        case TTableType::ICEBERG_FILES_TABLE: {
             desc = pool->add(new IcebergMetadataTableDescriptor(tdesc, pool));
             break;
         }
