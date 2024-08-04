1d7bfe9a22f978a4c4f716168e5691028cb5c67b
hmx <1639097204@qq.com>
2024-08-04 16:20:34 +0800

    adjust
    Signed-off-by: hmx <1639097204@qq.com>

--- fe/fe-core/src/test/java/com/starrocks/qe/scheduler/DefaultWorkerProviderTest.java
+++ fe/fe-core/src/test/java/com/starrocks/qe/scheduler/DefaultWorkerProviderTest.java
@@ -84,6 +84,89 @@ public class DefaultWorkerProviderTest {
         return ImmutableMap.copyOf(res);
     }
 
+    @Before
+    public void setUp() throws Exception {
+        Map<Long, ComputeNode> idToComputeNodeRef = new HashMap<>();
+        Map<Long, Backend> idToBackendRef = new HashMap<>();
+
+        new MockUp<SystemInfoService>() {
+            @Mock
+            public ImmutableMap<Long, ComputeNode> getIdComputeNode(Set<String> locations) {
+                Map<Long, ComputeNode> computeNodes = new HashMap<>();
+                for (String location : locations) {
+                    ImmutableMap<Long, ComputeNode> labelToComputeNodes = getIdComputeNode(location);
+                    computeNodes.putAll(labelToComputeNodes);
+                }
+                return ImmutableMap.copyOf(computeNodes);
+            }
+            @Mock
+            public ImmutableMap<Long, ComputeNode> getIdComputeNode(String location) {
+                if (location.equals(SessionVariable.GLOBAL_LABELS_LOCATION)) {
+                    return ImmutableMap.copyOf(idToComputeNodeRef);
+                }
+                Map<Long, ComputeNode> computeNodes = new HashMap<>();
+                for (Long id : idToComputeNodeRef.keySet()) {
+                    for (Map.Entry<String, String> entry :
+                            idToComputeNodeRef.get(id).getLocation().entrySet()) {
+                        String computeNodeLocation = entry.getKey() + ":" + entry.getValue();
+                        if (computeNodeLocation.equals(location)) {
+                            computeNodes.put(id, idToComputeNodeRef.get(id));
+                        }
+                    }
+                }
+                return computeNodes == null ? ImmutableMap.of() : ImmutableMap.copyOf(computeNodes);
+            }
+            @Mock
+            public ImmutableMap<Long, Backend> getIdToBackend(Set<String> locations) {
+                Map<Long, Backend> backends = new HashMap<>();
+                for (String location : locations) {
+                    ImmutableMap<Long, Backend> labelToBackends = getIdToBackend(location);
+                    backends.putAll(labelToBackends);
+                }
+                return ImmutableMap.copyOf(backends);
+            }
+            @Mock
+            public ImmutableMap<Long, Backend> getIdToBackend(String location) {
+                if (location.equals(SessionVariable.GLOBAL_LABELS_LOCATION)) {
+                    return ImmutableMap.copyOf(idToBackendRef);
+                }
+                Map<Long, Backend> backends = new HashMap<>();
+                for (Long id : idToBackendRef.keySet()) {
+                    for (Map.Entry<String, String> entry :
+                            idToBackendRef.get(id).getLocation().entrySet()) {
+                        String backendLocation = entry.getKey() + ":" + entry.getValue();
+                        if (backendLocation.equals(location)) {
+                            backends.put(id, idToBackendRef.get(id));
+                        }
+                    }
+                }
+                return backends == null ? ImmutableMap.of() : ImmutableMap.copyOf(backends);
+            }
+        };
+
+        //ComputeNode
+        for (long id = 10; id < 15; id++) {
+            idToComputeNodeRef.put(id, id2ComputeNode.get(id));
+        }
+        //Backend
+        for (long id = 0; id < 10; id++) {
+            idToBackendRef.put(id, (Backend) id2Backend.get(id));
+        }
+
+        ConnectContext connectContext = UtFrameUtils.initCtxForNewPrivilege(UserIdentity.ROOT);
+        UtFrameUtils.setUpForPersistTest();
+        String modifyUserPropSqlStr = "SET PROPERTY FOR 'root' '" +
+                PropertyAnalyzer.PROPERTIES_LABELS_LOCATION + "' = 'rack:all'";
+        DDLStmtExecutor.execute(UtFrameUtils.parseStmtWithNewParser(modifyUserPropSqlStr, connectContext),
+                connectContext);
+    }
+
+    @After
+    public void after() throws Exception {
+        System.out.println("after");
+        UtFrameUtils.tearDownForPersisTest();
+    }
+
     @Test
     public void testCaptureAvailableWorkers() {
 
@@ -162,12 +245,12 @@ public class DefaultWorkerProviderTest {
         new MockUp<SystemInfoService>() {
             @Mock
             public ImmutableMap<Long, ComputeNode> getIdToBackend() {
-                return availableId2Backend;
+                return id2Backend;
             }
 
             @Mock
             public ImmutableMap<Long, ComputeNode> getIdComputeNode() {
-                return availableId2ComputeNode;
+                return id2ComputeNode;
             }
         };
 
@@ -184,7 +267,7 @@ public class DefaultWorkerProviderTest {
             List<Long> selectedWorkerIdsList = workerProvider.getAllAvailableNodes();
             for (Long selectedWorkerId : selectedWorkerIdsList) {
                 Assert.assertTrue("selectedWorkerId:" + selectedWorkerId,
-                        availableId2ComputeNode.containsKey(selectedWorkerId));
+                        id2ComputeNode.containsKey(selectedWorkerId));
             }
         }
         // test Backend only
@@ -197,7 +280,7 @@ public class DefaultWorkerProviderTest {
             Assert.assertEquals(availableId2Backend.size(), selectedWorkerIdsList.size());
             for (Long selectedWorkerId : selectedWorkerIdsList) {
                 Assert.assertTrue("selectedWorkerId:" + selectedWorkerId,
-                        availableId2Backend.containsKey(selectedWorkerId));
+                        id2Backend.containsKey(selectedWorkerId));
             }
         }
         // test Backend and ComputeNode
@@ -209,118 +292,29 @@ public class DefaultWorkerProviderTest {
             List<Long> selectedWorkerIdsList = workerProvider.getAllAvailableNodes();
             Collections.reverse(selectedWorkerIdsList); //put ComputeNode id to the front,Backend id to the back
             //test ComputeNode
-            for (int i = 0; i < availableId2ComputeNode.size() && i < selectedWorkerIdsList.size(); i++) {
+            for (int i = 0; i < id2ComputeNode.size() && i < selectedWorkerIdsList.size(); i++) {
                 Assert.assertTrue("selectedWorkerId:" + selectedWorkerIdsList.get(i),
-                        availableId2ComputeNode.containsKey(selectedWorkerIdsList.get(i)));
+                        id2ComputeNode.containsKey(selectedWorkerIdsList.get(i)));
             }
             //test Backend
-            for (int i = availableId2ComputeNode.size(); i < selectedWorkerIdsList.size(); i++) {
+            for (int i = id2ComputeNode.size(); i < selectedWorkerIdsList.size(); i++) {
                 Assert.assertTrue("selectedWorkerId:" + selectedWorkerIdsList.get(i),
-                        availableId2Backend.containsKey(selectedWorkerIdsList.get(i)));
+                        id2Backend.containsKey(selectedWorkerIdsList.get(i)));
             }
         }
     }
 
     @Test
     public void testSelectNodeByLabel() throws Exception {
-        ConnectContext connectContext = UtFrameUtils.initCtxForNewPrivilege(UserIdentity.ROOT);
-        UtFrameUtils.setUpForPersistTest();
-
-        Map<Long, ComputeNode> idToComputeNodeRef = new HashMap<>();
-        Map<Long, Backend> idToBackendRef = new HashMap<>();
-
         Map<Long, ComputeNode> idToComputeNodeGroupA = new HashMap<>();
         Map<Long, Backend> idToBackendGroupA = new HashMap<>();
 
-        new MockUp<SystemInfoService>() {
-            @Mock
-            public Set<String> getLabelsLocation() {
-                String user = null;
-                try {
-                    user = ConnectContext.get().getCurrentUserIdentity().getUser();
-                } catch (Exception e) {
-                    if (user == null) {
-                        //heartbeat
-                        return new HashSet<>(Arrays.asList(SessionVariable.GLOBAL_LABELS_LOCATION));
-                    } else {
-                        //LOG.info("fail to get user info, msg: {}", e.getMessage());
-                    }
-                }
-                Set<String> locations = GlobalStateMgr.getCurrentState().getAuthenticationMgr().getLabelsLocation(user);
-                return locations;
-            }
-            @Mock
-            public ImmutableMap<Long, ComputeNode> getIdComputeNode() {
-                Set<String> locations = getLabelsLocation();
-                ImmutableMap<Long, ComputeNode> computeNodes = getIdComputeNode(locations);
-                return computeNodes;
-            }
-            @Mock
-            public ImmutableMap<Long, ComputeNode> getIdComputeNode(Set<String> locations) {
-                Map<Long, ComputeNode> computeNodes = new HashMap<>();
-                for (String location : locations) {
-                    ImmutableMap<Long, ComputeNode> labelToComputeNodes = getIdComputeNode(location);
-                    computeNodes.putAll(labelToComputeNodes);
-                }
-                return ImmutableMap.copyOf(computeNodes);
-            }
-            @Mock
-            public ImmutableMap<Long, ComputeNode> getIdComputeNode(String location) {
-                if (location.equals(SessionVariable.GLOBAL_LABELS_LOCATION)) {
-                    return ImmutableMap.copyOf(idToComputeNodeRef);
-                }
-                Map<Long, ComputeNode> computeNodes = new HashMap<>();
-                for (Long id : idToComputeNodeRef.keySet()) {
-                    for (Map.Entry<String, String> entry :
-                            idToComputeNodeRef.get(id).getLocation().entrySet()) {
-                        String computeNodeLocation = entry.getKey() + ":" + entry.getValue();
-                        if (computeNodeLocation.equals(location)) {
-                            computeNodes.put(id, idToComputeNodeRef.get(id));
-                        }
-                    }
-                }
-                return computeNodes == null ? ImmutableMap.of() : ImmutableMap.copyOf(computeNodes);
-            }
-            @Mock
-            public ImmutableMap<Long, Backend> getIdToBackend() {
-                Set<String> locations = getLabelsLocation();
-                ImmutableMap<Long, Backend> backends = getIdToBackend(locations);
-                return backends;
-            }
-            @Mock
-            public ImmutableMap<Long, Backend> getIdToBackend(Set<String> locations) {
-                Map<Long, Backend> backends = new HashMap<>();
-                for (String location : locations) {
-                    ImmutableMap<Long, Backend> labelToBackends = getIdToBackend(location);
-                    backends.putAll(labelToBackends);
-                }
-                return ImmutableMap.copyOf(backends);
-            }
-            @Mock
-            public ImmutableMap<Long, Backend> getIdToBackend(String location) {
-                if (location.equals(SessionVariable.GLOBAL_LABELS_LOCATION)) {
-                    return ImmutableMap.copyOf(idToBackendRef);
-                }
-                Map<Long, Backend> backends = new HashMap<>();
-                for (Long id : idToBackendRef.keySet()) {
-                    for (Map.Entry<String, String> entry :
-                            idToBackendRef.get(id).getLocation().entrySet()) {
-                        String backendLocation = entry.getKey() + ":" + entry.getValue();
-                        if (backendLocation.equals(location)) {
-                            backends.put(id, idToBackendRef.get(id));
-                        }
-                    }
-                }
-                return backends == null ? ImmutableMap.of() : ImmutableMap.copyOf(backends);
-            }
-        };
         //ComputeNode
         for (long id = 10; id < 15; id++) {
             String group = (id <= 12 ? "group_a" : "group_b");
             id2ComputeNode.get(id).setLocation(new HashMap<String, String>() {{
                     put("rack", group);
                 }});
-            idToComputeNodeRef.put(id, id2ComputeNode.get(id));
             if (id <= 12) {
                 idToComputeNodeGroupA.put(id, id2ComputeNode.get(id));
             }
@@ -331,11 +325,12 @@ public class DefaultWorkerProviderTest {
             ((Backend) id2Backend.get(id)).setLocation(new HashMap<String, String>() {{
                     put("rack", group);
                 }});
-            idToBackendRef.put(id, (Backend) id2Backend.get(id));
             if (id <= 4) {
                 idToBackendGroupA.put(id, (Backend) id2Backend.get(id));
             }
         }
+
+        ConnectContext connectContext = UtFrameUtils.initCtxForNewPrivilege(UserIdentity.ROOT);
 
         String modifyUserPropSqlStr = "SET PROPERTY FOR 'root' '" +
                 PropertyAnalyzer.PROPERTIES_LABELS_LOCATION + "' = 'rack:group_a'";
