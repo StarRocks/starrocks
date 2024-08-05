89ac3b7ddcf8411cb76782901e12623616bf9a90
xiangguangyxg <110401425+xiangguangyxg@users.noreply.github.com>
2024-08-05 14:29:42 +0800

    fix conflict (#49381)
    Signed-off-by: xiangguangyxg <xiangguangyxg@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/load/loadv2/LoadMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/load/loadv2/LoadMgr.java
@@ -231,17 +231,9 @@ public class LoadMgr implements Writable, MemoryTrackable {
         }
     }
 
-<<<<<<< HEAD
-    public long registerLoadJob(String label, String dbName, long tableId, EtlJobType jobType,
+    public long registerLoadJob(String label, String dbName, long tableId, long txnId, EtlJobType jobType,
             long createTimestamp, long estimateScanRows, TLoadJobType type, long timeout,
             Coordinator coordinator) throws UserException {
-=======
-    public long registerLoadJob(String label, String dbName, long tableId, long txnId, EtlJobType jobType,
-                                long createTimestamp, long estimateScanRows,
-                                int estimateFileNum, long estimateFileSize,
-                                TLoadJobType type, long timeout, Coordinator coordinator)
-            throws UserException {
->>>>>>> d9528297c4 ([BugFix] Fix transaction of insert load job can not be aborted when job has been cancelled (#48059))
 
         // get db id
         Database db = GlobalStateMgr.getCurrentState().getDb(dbName);
@@ -249,17 +241,11 @@ public class LoadMgr implements Writable, MemoryTrackable {
             throw new MetaNotFoundException("Database[" + dbName + "] does not exist");
         }
 
-        LoadJob loadJob;
+        InsertLoadJob loadJob;
         if (Objects.requireNonNull(jobType) == EtlJobType.INSERT) {
-<<<<<<< HEAD
             loadJob = new InsertLoadJob(label, db.getId(), tableId, createTimestamp, estimateScanRows, type, timeout,
                     coordinator);
-=======
-            loadJob = new InsertLoadJob(label, db.getId(), tableId, createTimestamp, type, timeout, coordinator);
-            loadJob.setLoadFileInfo(estimateFileNum, estimateFileSize);
-            loadJob.setEstimateScanRow(estimateScanRows);
             loadJob.setTransactionId(txnId);
->>>>>>> d9528297c4 ([BugFix] Fix transaction of insert load job can not be aborted when job has been cancelled (#48059))
         } else {
             throw new LoadException("Unknown job type [" + jobType.name() + "]");
         }
