bbd08e8b886aee4148ef3743c9ec73d9efdf6a87
wyb <wybb86@gmail.com>
2024-08-07 16:09:06 +0000

    Fix format

--- be/test/exec/json_parser_test.cpp
+++ be/test/exec/json_parser_test.cpp
@@ -541,6 +541,7 @@ PARALLEL_TEST(JsonParserTest, test_document_stream_parser_invalid_type_not_objec
     ASSERT_TRUE(st.is_data_quality_error());
     ASSERT_TRUE(st.get_error_msg().find("The value should be object type in json document stream") !=
                 std::string::npos);
+}
 
 PARALLEL_TEST(JsonParserTest, test_document_stream_parser_with_jsonroot_invalid_type_array) {
     // ndjson with ' ', '/t', '\n'
