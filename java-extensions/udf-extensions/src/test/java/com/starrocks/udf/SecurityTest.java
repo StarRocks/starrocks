de4636f99a61e5e65b0410c63490390044b7b6b7
Smith Cruise <chendingchao1@126.com>
2024-08-08 17:05:40 +0800

    [Enhancement] Fix cve problems in java-extensions module (#49425)
    Signed-off-by: Smith Cruise <chendingchao1@126.com>

--- java-extensions/udf-extensions/src/test/java/com/starrocks/udf/SecurityTest.java
+++ java-extensions/udf-extensions/src/test/java/com/starrocks/udf/SecurityTest.java
@@ -1,10 +1,12 @@
 package com.starrocks.udf;
 
-import org.junit.Test;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.security.AccessControlException;
 
@@ -17,9 +19,9 @@ public class SecurityTest {
     }
 
     public static class ScalarAdd {
-        public String evaluate(String v1) throws IOException {
-            File.createTempFile(v1, ".txt");
-            return v1;
+        public String evaluate(String path) {
+            File file = new File(path);
+            return file.getAbsolutePath();
         }
     }
 
@@ -33,10 +35,13 @@ public class SecurityTest {
         return call;
     }
 
-    @Test(expected = AccessControlException.class)
-    public void testUDFCreateFile()
-            throws NoSuchMethodException, ClassNotFoundException, IOException, InvocationTargetException,
-            IllegalAccessException {
+    @AfterEach
+    public void resetSecurityManager() {
+        System.setSecurityManager(null);
+    }
+
+    @Test
+    public void testUDFCreateFile() throws NoSuchMethodException, ClassNotFoundException {
         System.setSecurityManager(new UDFSecurityManager(TestClassLoader.class));
 
         Class<?> clazz = ScalarAdd.class;
@@ -50,17 +55,23 @@ public class SecurityTest {
         String[] inputs1 = new String[1];
         inputs1[0] = "prefix";
         ScalarAdd concat = new ScalarAdd();
-        batchCall.invoke(null, 1, concat, inputs1);
-
-        System.setSecurityManager(null);
+        Assertions.assertThrows(AccessControlException.class, () -> {
+            try {
+                batchCall.invoke(null, 1, concat, inputs1);
+            } catch (Exception e) {
+                // AccessControlException is root cause
+                Throwable rootCause = ExceptionUtils.getRootCause(e);
+                if (rootCause instanceof AccessControlException) {
+                    throw rootCause;
+                }
+            }
+        });
     }
 
     @Test
-    public void testNoUDFCreateFile() throws IOException {
+    public void testNoUDFCreateFile() {
         System.setSecurityManager(new UDFSecurityManager(TestClassLoader.class));
         ScalarAdd concat = new ScalarAdd();
-        concat.evaluate("test");
-        System.setSecurityManager(null);
+        Assertions.assertTrue(concat.evaluate("./test").contains("test"));
     }
-
 }
