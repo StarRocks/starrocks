c68aa79741461beaa41b99057dc7432fdea42815
Murphy <96611012+murphyatwork@users.noreply.github.com>
2024-08-13 22:48:48 +0800

    [Tool] enable the compiler compress (#49704)
    Signed-off-by: Murphy <mofei@starrocks.com>
    (cherry picked from commit 403f5526a614052d15d0d3100d34cda10752f5a8)

--- build.sh
+++ build.sh
@@ -145,6 +145,7 @@ RUN_UT=
 WITH_GCOV=OFF
 WITH_BENCH=OFF
 WITH_CLANG_TIDY=OFF
+WITH_COMPRESS=ON
 WITH_STARCACHE=ON
 USE_STAROS=OFF
 BUILD_JAVA_EXT=ON
@@ -261,6 +262,7 @@ echo "Get params:
     WITH_GCOV           -- $WITH_GCOV
     WITH_BENCH          -- $WITH_BENCH
     WITH_CLANG_TIDY     -- $WITH_CLANG_TIDY
+    WITH_COMPRESS       -- $WITH_COMPRESS
     WITH_STARCACHE      -- $WITH_STARCACHE
     ENABLE_SHARED_DATA  -- $USE_STAROS
     USE_AVX2            -- $USE_AVX2
@@ -340,6 +342,11 @@ if [ ${BUILD_BE} -eq 1 ] ; then
         STARLET_INSTALL_DIR=${STARROCKS_THIRDPARTY}/installed/starlet
       fi
       export STARLET_INSTALL_DIR
+    fi
+    
+    if [ "${WITH_CLANG_TIDY}" == "ON" ];then
+        # this option cannot work with clang-14
+        WITH_COMPRESS=OFF
     fi
 
     ${CMAKE_CMD} -G "${CMAKE_GENERATOR}"                                \
