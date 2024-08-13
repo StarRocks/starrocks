5073f6f7baff2f99c338ac1ab795f95cae499387
AndyZiYe <yeziyu@starrocks.com>
2024-08-13 19:46:58 +0800

    test code
    Signed-off-by: AndyZiYe <yeziyu@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/alter/AlterHandler.java
+++ fe/fe-core/src/main/java/com/starrocks/alter/AlterHandler.java
@@ -31,6 +31,7 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
+// test
 
 package com.starrocks.alter;
 
