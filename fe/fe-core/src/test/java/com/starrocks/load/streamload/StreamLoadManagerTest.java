990b5aab3de4f38ad8219b424b8599a29cea1460
meegoo <meegoo.sr@gmail.com>
2024-08-16 15:31:37 +0800

    Update StreamLoadManagerTest.java
    Signed-off-by: meegoo <meegoo.sr@gmail.com>

--- fe/fe-core/src/test/java/com/starrocks/load/streamload/StreamLoadManagerTest.java
+++ fe/fe-core/src/test/java/com/starrocks/load/streamload/StreamLoadManagerTest.java
@@ -282,11 +282,11 @@ public class StreamLoadManagerTest {
         String tableName = "test_tbl";
         String labelName = "label2";
         long timeoutMillis = 100000;
-        long warehouseId = 0;
+        int channelNum = 1;
+        int channelId = 0;
 
         TransactionResult resp = new TransactionResult();
-
-        streamLoadManager.beginLoadTask(dbName, tableName, labelName, timeoutMillis, resp, false, warehouseId);
+        streamLoadManager.beginLoadTask(dbName, tableName, labelName, timeoutMillis, channelNum, channelId, resp);
 
         Map<String, StreamLoadTask> idToStreamLoadTask =
                 Deencapsulation.getField(streamLoadManager, "idToStreamLoadTask");
@@ -302,9 +302,6 @@ public class StreamLoadManagerTest {
         state.setCommitTime(task.endTimeMs());
         task.replayOnCommitted(state);
         Assert.assertEquals(task.endTimeMs(), state.getCommitTime());
-
-        streamLoadManager.cleanSyncStreamLoadTasks();
-        Assert.assertEquals(0, streamLoadManager.getStreamLoadTaskCount());
     }
 
 }
