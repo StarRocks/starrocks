2cbdbe0dde56103da326dd42e109fa233f376256
Binglin Chang <decstery@gmail.com>
2024-08-21 23:14:23 +0800

    [BugFix] Fix TDE support for segment rewrite in cloud native
    Signed-off-by: Binglin Chang <decstery@gmail.com>

--- be/src/storage/rowset/segment_rewriter.h
+++ be/src/storage/rowset/segment_rewriter.h
@@ -26,21 +26,25 @@ public:
     // read from src, write to dest
     // this function will read data from src_file and write to dest file first
     // then append write_column to dest file
-    static Status rewrite(const std::string& src, FileInfo* dest, const std::shared_ptr<const TabletSchema>& tschema,
+    static Status rewrite_partial_update(const FileInfo& src, FileInfo* dest,
+                                         const std::shared_ptr<const TabletSchema>& tschema,
+                                         std::vector<uint32_t>& column_ids,
+                                         std::vector<std::unique_ptr<Column>>& columns, uint32_t segment_id,
+                                         const FooterPointerPB& partial_rowset_footer);
+    // this funciton will append write_column to src_file and rebuild segment footer
+    static Status rewrite(const std::string& src, const FileEncryptionInfo& einfo, const TabletSchemaCSPtr& tschema,
                           std::vector<uint32_t>& column_ids, std::vector<std::unique_ptr<Column>>& columns,
                           uint32_t segment_id, const FooterPointerPB& partial_rowseet_footer);
-    // this funciton will append write_column to src_file and rebuild segment footer
-    static Status rewrite(const std::string& src, const TabletSchemaCSPtr& tschema, std::vector<uint32_t>& column_ids,
-                          std::vector<std::unique_ptr<Column>>& columns, uint32_t segment_id,
-                          const FooterPointerPB& partial_rowseet_footer);
-    static Status rewrite(const std::string& src_path, const std::string& dest_path, const TabletSchemaCSPtr& tschema,
-                          AutoIncrementPartialUpdateState& auto_increment_partial_update_state,
-                          std::vector<uint32_t>& column_ids, std::vector<std::unique_ptr<Column>>* columns);
-    static Status rewrite(FileInfo* dest_path, const TabletSchemaCSPtr& tschema,
-                          starrocks::lake::AutoIncrementPartialUpdateState& auto_increment_partial_update_state,
-                          const std::vector<uint32_t>& unmodified_column_ids,
-                          std::vector<std::unique_ptr<Column>>* unmodified_column_data,
-                          const starrocks::TxnLogPB_OpWrite& op_write, const starrocks::lake::Tablet* tablet);
+    static Status rewrite_auto_increment(const std::string& src_path, const std::string& dest_path,
+                                         const TabletSchemaCSPtr& tschema,
+                                         AutoIncrementPartialUpdateState& auto_increment_partial_update_state,
+                                         std::vector<uint32_t>& column_ids,
+                                         std::vector<std::unique_ptr<Column>>* columns);
+    static Status rewrite_auto_increment_lake(
+            const FileInfo& src, FileInfo* dest, const TabletSchemaCSPtr& tschema,
+            starrocks::lake::AutoIncrementPartialUpdateState& auto_increment_partial_update_state,
+            const std::vector<uint32_t>& unmodified_column_ids,
+            std::vector<std::unique_ptr<Column>>* unmodified_column_data, const starrocks::lake::Tablet* tablet);
 };
 
 } // namespace starrocks
