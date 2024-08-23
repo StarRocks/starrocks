a5fd7b6a2fd7258be03619384755b94ca90850e5
yanz <dirtysalt1987@gmail.com>
2024-08-23 14:06:22 +0800

    deploy incremental scan ranges
    Signed-off-by: yanz <dirtysalt1987@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/planner/HdfsScanNode.java
+++ fe/fe-core/src/main/java/com/starrocks/planner/HdfsScanNode.java
@@ -280,4 +280,9 @@ public class HdfsScanNode extends ScanNode {
     protected boolean supportTopNRuntimeFilter() {
         return true;
     }
+
+    @Override
+    public boolean isIncrementalScanRangesSupported() {
+        return true;
+    }
 }
