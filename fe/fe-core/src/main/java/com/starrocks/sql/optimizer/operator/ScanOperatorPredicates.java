b67d69d6670b48b45e871c5263d1f7065b4d39da
Youngwb <yangwenbo_mailbox@163.com>
2024-08-23 05:14:52 -0700

    [BugFix] Fix clone ScanOperatorPredicates without hasPrunedPartition (#50191)

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/operator/ScanOperatorPredicates.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/operator/ScanOperatorPredicates.java
@@ -123,6 +123,7 @@ public class ScanOperatorPredicates {
         other.nonPartitionConjuncts.addAll(this.nonPartitionConjuncts);
         other.minMaxConjuncts.addAll(this.minMaxConjuncts);
         other.minMaxColumnRefMap.putAll(this.minMaxColumnRefMap);
+        other.hasPrunedPartition = this.hasPrunedPartition;
 
         return other;
     }
