2924e74ba27a28c10a7783607e22c16121dd8754
DanRoscigno <dan@roscigno.com>
2024-08-23 14:42:35 -0400

    init before changing to a single sidebar
    Signed-off-by: DanRoscigno <dan@roscigno.com>

--- docs/docusaurus/scripts/docker-build.sh
+++ docs/docusaurus/scripts/docker-build.sh
@@ -6,6 +6,7 @@ docker run --rm --interactive --tty \
 	-e DISABLE_VERSIONING=true \
 	--volume $DOCS_DIR/docusaurus/sidebars.json:/app/docusaurus/sidebars.json \
 	--volume $DOCS_DIR/en:/app/docusaurus/docs \
+	--volume "$DOCS_DIR/docusaurus/i18n:/app/docusaurus/i18n" \
 	--volume "$DOCS_DIR/zh:/app/docusaurus/i18n/zh/docusaurus-plugin-content-docs/current" \
 	-p 3000:3000 \
 	docs-build /app/docusaurus/scripts/build-and-serve.sh
