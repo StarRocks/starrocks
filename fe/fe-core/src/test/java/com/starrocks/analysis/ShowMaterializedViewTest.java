41e413ec27d466ed33030a8aa7cd5cd8ab119432
Murphy <mofei@starrocks.com>
2024-08-23 10:16:58 +0800

    fix test
    Signed-off-by: Murphy <mofei@starrocks.com>

--- fe/fe-core/src/test/java/com/starrocks/analysis/ShowMaterializedViewTest.java
+++ fe/fe-core/src/test/java/com/starrocks/analysis/ShowMaterializedViewTest.java
@@ -115,7 +115,8 @@ public class ShowMaterializedViewTest {
                         "information_schema.materialized_views.TABLE_ROWS AS rows, " +
                         "information_schema.materialized_views.MATERIALIZED_VIEW_DEFINITION AS text, " +
                         "information_schema.materialized_views.extra_message AS extra_message, " +
-                        "information_schema.materialized_views.query_rewrite_status AS query_rewrite_status " +
+                        "information_schema.materialized_views.query_rewrite_status AS query_rewrite_status, " +
+                        "information_schema.materialized_views.owner AS owner " +
                         "FROM information_schema.materialized_views " +
                         "WHERE (information_schema.materialized_views.TABLE_SCHEMA = 'abc') AND (information_schema.materialized_views.TABLE_NAME = 'mv1')",
                 AstToStringBuilder.toString(stmt.toSelectStmt()));
