738a2ac1ffd6bb556303b31279e7183583c9d470
Kevin Cai <caixiaohua@starrocks.com>
2024-08-27 01:53:51 +0800

    [Enhancement] don't print error if the configvar name appears to be an ENV var name (#50247)
    suppress errors such as Ignored unknown config: JAVA_OPTS
    
    Signed-off-by: Kevin Xiaohua Cai <caixiaohua@starrocks.com>

--- be/src/common/configbase.cpp
+++ be/src/common/configbase.cpp
@@ -153,7 +153,12 @@ inline bool parse_key_value_pairs(std::istream& input) {
 
         auto op_field = Field::get(kv.first);
         if (!op_field.has_value()) {
-            std::cerr << fmt::format("Ignored unknown config: {}\n", kv.first);
+            // A valid env var name should be: [A-Z_][A-Z0-9_]*
+            static const std::string ENV_CHARSET("ABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890_");
+            if (kv.first.find_first_not_of(ENV_CHARSET) != std::string::npos) {
+                // only report error when the var name appears not to be valid, not strict here.
+                std::cerr << fmt::format("Ignored unknown config: {}\n", kv.first);
+            }
             continue;
         }
         auto field = op_field.value();
