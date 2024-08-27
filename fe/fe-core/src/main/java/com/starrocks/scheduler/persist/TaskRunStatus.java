3d5f47f78de8bd84b553b00ada6fa0b52640ed77
shuming.li <ming.moriarty@gmail.com>
2024-08-27 11:03:02 +0800

    Use mv's session properties for task run instead of table's peroperties to avoid properties polllute
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/scheduler/persist/TaskRunStatus.java
+++ fe/fe-core/src/main/java/com/starrocks/scheduler/persist/TaskRunStatus.java
@@ -19,6 +19,7 @@ import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 import com.google.gson.annotations.SerializedName;
 import com.starrocks.cluster.ClusterNamespace;
+import com.starrocks.common.Config;
 import com.starrocks.common.io.Text;
 import com.starrocks.common.io.Writable;
 import com.starrocks.persist.gson.GsonUtils;
@@ -32,6 +33,8 @@ import io.netty.buffer.Unpooled;
 import org.apache.commons.collections.MapUtils;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.ListUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.DataInput;
 import java.io.DataOutput;
@@ -44,6 +47,7 @@ import java.util.Map;
 import java.util.Set;
 
 public class TaskRunStatus implements Writable {
+    private static final Logger LOG = LogManager.getLogger(TaskRun.class);
 
     // A refresh may contain a batch of task runs, startTaskRunId is to mark the unique id of the batch task run status.
     // You can use the startTaskRunId to find the batch of task runs.
@@ -493,8 +497,14 @@ public class TaskRunStatus implements Writable {
                     jsonString = copied.toString(Charset.defaultCharset());
                     res.addAll(ListUtils.emptyIfNull(TaskRunStatusJSONRecord.fromJson(jsonString).data));
                 } catch (Exception e) {
-                    throw new RuntimeException("Failed to deserialize TaskRunStatus from json， please delete it from " +
-                            "_statistics_.task_run_history table: " + jsonString, e);
+                    // If the task run history is corrupted, we can use `ignore_task_run_history_replay_error` config to ignore
+                    // it and continue to process the next one.
+                    if (!Config.ignore_task_run_history_replay_error) {
+                        LOG.warn("Failed to deserialize TaskRunStatus from json， please delete it from " +
+                                "_statistics_.task_run_history table: {}", jsonString, e);
+                        throw new RuntimeException("Failed to deserialize TaskRunStatus from json， please delete it from " +
+                                "_statistics_.task_run_history table: " + jsonString, e);
+                    }
                 }
             }
         }
