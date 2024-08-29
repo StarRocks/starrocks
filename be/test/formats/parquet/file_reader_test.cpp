4a90aa93603ca3357f8f10a1d750f76b9b3ff14d
Smith Cruise <chendingchao1@126.com>
2024-08-29 13:03:29 +0800

    format
    Signed-off-by: Smith Cruise <chendingchao1@126.com>

--- be/test/formats/parquet/file_reader_test.cpp
+++ be/test/formats/parquet/file_reader_test.cpp
@@ -3200,8 +3200,8 @@ TEST_F(FileReaderTest, TestReadFooterCache) {
 TEST_F(FileReaderTest, TestMapKeyIsStruct) {
     const std::string filename = "./be/test/formats/parquet/test_data/map_key_is_struct.parquet";
     auto file = _create_file(filename);
-    auto file_reader =
-            std::make_shared<FileReader>(config::vector_chunk_size, file.get(), std::filesystem::file_size(filename));
+    auto file_reader = std::make_shared<FileReader>(config::vector_chunk_size, file.get(),
+                                                    std::filesystem::file_size(filename), 100000);
 
     auto ctx = _create_file_random_read_context(filename);
     Status status = file_reader->init(ctx);
