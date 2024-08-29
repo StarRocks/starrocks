7168d53764dda1ca04a18f1f7de10b064a0afb82
Smith Cruise <chendingchao1@126.com>
2024-08-29 10:43:11 +0800

    [BugFix][Cherry-Pick][Branch-3.2] Fix serveral complex type bugs in parquet reader (backport #50355)
    Signed-off-by: Smith Cruise <chendingchao1@126.com>

--- be/test/formats/parquet/file_reader_test.cpp
+++ be/test/formats/parquet/file_reader_test.cpp
@@ -3197,4 +3197,16 @@ TEST_F(FileReaderTest, TestReadFooterCache) {
     ASSERT_EQ(ctx2->stats->footer_cache_write_count, 0);
 }
 
+TEST_F(FileReaderTest, TestMapKeyIsStruct) {
+    const std::string filename = "./be/test/formats/parquet/test_data/map_key_is_struct.parquet";
+    auto file = _create_file(filename);
+    auto file_reader =
+            std::make_shared<FileReader>(config::vector_chunk_size, file.get(), std::filesystem::file_size(filename));
+
+    auto ctx = _create_file_random_read_context(filename);
+    Status status = file_reader->init(ctx);
+    ASSERT_FALSE(status.ok());
+    ASSERT_EQ("Map keys must be primitive type.", status.message());
+}
+
 } // namespace starrocks::parquet
