1ab23d7d66c4d0af8f9c913d747f11eba0ecaf64
Smith Cruise <chendingchao1@126.com>
2024-08-29 14:43:34 +0800

    [BugFix][Cherry-Pick][Branch-3.3] Fix serveral complex type bugs in parquet reader (backport #50355) (#50397)
    Signed-off-by: Smith Cruise <chendingchao1@126.com>

--- be/src/formats/parquet/schema.cpp
+++ be/src/formats/parquet/schema.cpp
@@ -220,6 +220,13 @@ Status SchemaDescriptor::map_to_field(const std::vector<tparquet::SchemaElement>
     //   }
     // }
     //
+
+    // check map's key must be primitive type
+    ASSIGN_OR_RETURN(const auto* key_schema, _get_schema_element(t_schemas, pos + 2));
+    if (is_group(key_schema)) {
+        return Status::InvalidArgument("Map keys must be primitive type.");
+    }
+
     RETURN_IF_ERROR(node_to_field(t_schemas, pos + 2, cur_level_info, key_field, next_pos));
     RETURN_IF_ERROR(node_to_field(t_schemas, pos + 3, cur_level_info, value_field, next_pos));
 
