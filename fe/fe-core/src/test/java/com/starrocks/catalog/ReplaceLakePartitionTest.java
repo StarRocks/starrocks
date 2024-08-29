f940d90f5d1c8617fcbb8fe255dd9ac7a4b08c29
srlch <111035020+srlch@users.noreply.github.com>
2024-08-29 17:16:42 +0800

    [Enhancement] Delete garbage data throught recycle bin after truncate table in share data mode (#49975)
    Signed-off-by: srlch <linzichao@starrocks.com>

--- fe/fe-core/src/test/java/com/starrocks/catalog/ReplaceLakePartitionTest.java
+++ fe/fe-core/src/test/java/com/starrocks/catalog/ReplaceLakePartitionTest.java
@@ -14,6 +14,7 @@
 package com.starrocks.catalog;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Range;
 import com.staros.client.StarClientException;
 import com.staros.proto.FilePathInfo;
 import com.staros.proto.ShardInfo;
@@ -22,14 +23,17 @@ import com.starrocks.catalog.KeysType;
 import com.starrocks.catalog.MaterializedIndex;
 import com.starrocks.catalog.Partition;
 import com.starrocks.catalog.PartitionInfo;
+import com.starrocks.catalog.PartitionKey;
 import com.starrocks.catalog.PartitionType;
 import com.starrocks.catalog.TabletInvertedIndex;
 import com.starrocks.catalog.TabletMeta;
 import com.starrocks.catalog.Type;
+import com.starrocks.common.ExceptionChecker;
 import com.starrocks.lake.DataCacheInfo;
 import com.starrocks.lake.LakeTable;
 import com.starrocks.lake.LakeTablet;
 import com.starrocks.lake.StarOSAgent;
+import com.starrocks.persist.EditLog;
 import com.starrocks.server.GlobalStateMgr;
 import com.starrocks.server.WarehouseManager;
 import com.starrocks.thrift.TStorageMedium;
@@ -39,7 +43,10 @@ import mockit.Expectations;
 import mockit.Mock;
 import mockit.MockUp;
 import mockit.Mocked;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
 
 public class ReplaceLakePartitionTest {
     long dbId = 9010;
@@ -51,6 +58,8 @@ public class ReplaceLakePartitionTest {
     long nextTxnId = 20000;
     String partitionName = "p0";
     String tempPartitionName = "temp_" + partitionName;
+    long[] newTabletId = {9030, 90031};
+    long newPartitionId = 9040;
 
     LakeTable tbl = null;
     private final ShardInfo shardInfo;
@@ -60,6 +69,9 @@ public class ReplaceLakePartitionTest {
 
     @Mocked
     private WarehouseManager warehouseManager;
+
+    @Mocked
+    private EditLog editLog;
 
     public ReplaceLakePartitionTest() {
         shardInfo = ShardInfo.newBuilder().setFilePath(FilePathInfo.newBuilder().setFullPath("oss://1/2")).build();
@@ -78,7 +90,21 @@ public class ReplaceLakePartitionTest {
         Partition partition = new Partition(partitionId, partitionName, index, null);
         Partition tempPartition = new Partition(tempPartitionId, tempPartitionName, index, null);
 
-        PartitionInfo partitionInfo = new PartitionInfo(partitionType);
+        PartitionInfo partitionInfo = null;
+        if (partitionType == PartitionType.UNPARTITIONED) {
+            partitionInfo = new PartitionInfo(partitionType);
+        } else if (partitionType == PartitionType.LIST) {
+            partitionInfo = new ListPartitionInfo(PartitionType.LIST, Lists.newArrayList(new Column("c0", Type.BIGINT)));
+            List<String> values = Lists.newArrayList();
+            values.add("123");
+            ((ListPartitionInfo) partitionInfo).setValues(partitionId, values);
+        } else if (partitionType == PartitionType.RANGE) {
+            PartitionKey partitionKey = new PartitionKey();
+            Range<PartitionKey> range = Range.closedOpen(partitionKey, partitionKey);
+            partitionInfo = new RangePartitionInfo(Lists.newArrayList(new Column("c0", Type.BIGINT)));
+            ((RangePartitionInfo) partitionInfo).setRange(partitionId, false, range);
+        }
+
         partitionInfo.setReplicationNum(partitionId, (short) 1);
         partitionInfo.setIsInMemory(partitionId, false);
         partitionInfo.setDataCacheInfo(partitionId, new DataCacheInfo(true, false));
@@ -91,17 +117,32 @@ public class ReplaceLakePartitionTest {
         table.addTempPartition(tempPartition);
         return table;
     }
+    
+    Partition buildPartitionForTruncateTable() {
+        MaterializedIndex index = new MaterializedIndex(indexId);
+        TabletInvertedIndex invertedIndex = GlobalStateMgr.getCurrentState().getTabletInvertedIndex();
+        for (long id : newTabletId) {
+            TabletMeta tabletMeta = new TabletMeta(dbId, tableId, newPartitionId, 0, 0, TStorageMedium.HDD, true);
+            invertedIndex.addTablet(id, tabletMeta);
+            index.addTablet(new LakeTablet(id), tabletMeta);
+        }
 
-    @Test
-    public void testUnPartitionedLakeTableReplacePartition() {
-        LakeTable tbl = buildLakeTableWithTempPartition(PartitionType.UNPARTITIONED);
-        tbl.replacePartition(partitionName, tempPartitionName);
+        return new Partition(newPartitionId, partitionName, index, null);
+    }
 
+    private void erasePartition() {
         new Expectations() {
             {
                 GlobalStateMgr.getCurrentState().getWarehouseMgr();
                 minTimes = 0;
                 result = warehouseManager;
+            }
+        };
+
+        new MockUp<GlobalStateMgr>() {
+            @Mock
+            public EditLog getEditLog() {
+                return editLog;
             }
         };
 
@@ -119,6 +160,13 @@ public class ReplaceLakePartitionTest {
             }
         };
 
+        new MockUp<EditLog>() {
+            @Mock
+            public void logErasePartition(long partitionId) {
+                return;
+            }
+        };
+
         new MockUp<WarehouseManager>() {
             @Mock
             public Warehouse getBackgroundWarehouse() {
@@ -126,9 +174,46 @@ public class ReplaceLakePartitionTest {
             }
         };
 
-        try {
-            GlobalStateMgr.getCurrentState().getRecycleBin().erasePartition(Long.MAX_VALUE);
-        } catch (Exception ignore) {
-        }
+        ExceptionChecker.expectThrowsNoException(()
+                            -> GlobalStateMgr.getCurrentState().getRecycleBin().erasePartition(Long.MAX_VALUE));
+    }
+
+    @Test
+    public void testUnPartitionedLakeTableReplacePartition() {
+        LakeTable tbl = buildLakeTableWithTempPartition(PartitionType.UNPARTITIONED);
+        tbl.replacePartition(partitionName, tempPartitionName);
+        Assert.assertTrue(GlobalStateMgr.getCurrentState().getRecycleBin().isContainedInidToRecycleTime(partitionId));
+        erasePartition();
+        Assert.assertTrue(!GlobalStateMgr.getCurrentState().getRecycleBin().isContainedInidToRecycleTime(partitionId));
+    }
+
+    @Test
+    public void testUnPartitionedLakeTableReplacePartitionForTruncateTable() {
+        LakeTable tbl = buildLakeTableWithTempPartition(PartitionType.UNPARTITIONED);
+        Partition newPartition = buildPartitionForTruncateTable();
+        tbl.replacePartition(newPartition);
+        Assert.assertTrue(GlobalStateMgr.getCurrentState().getRecycleBin().isContainedInidToRecycleTime(partitionId));
+        erasePartition();
+        Assert.assertTrue(!GlobalStateMgr.getCurrentState().getRecycleBin().isContainedInidToRecycleTime(partitionId));
+    }
+
+    @Test
+    public void testListPartitionedLakeTableReplacePartitionForTruncateTable() {
+        LakeTable tbl = buildLakeTableWithTempPartition(PartitionType.LIST);
+        Partition newPartition = buildPartitionForTruncateTable();
+        tbl.replacePartition(newPartition);
+        Assert.assertTrue(GlobalStateMgr.getCurrentState().getRecycleBin().isContainedInidToRecycleTime(partitionId));
+        erasePartition();
+        Assert.assertTrue(!GlobalStateMgr.getCurrentState().getRecycleBin().isContainedInidToRecycleTime(partitionId));
+    }
+
+    @Test
+    public void testRangePartitionedLakeTableReplacePartitionForTruncateTable() {
+        LakeTable tbl = buildLakeTableWithTempPartition(PartitionType.RANGE);
+        Partition newPartition = buildPartitionForTruncateTable();
+        tbl.replacePartition(newPartition);
+        Assert.assertTrue(GlobalStateMgr.getCurrentState().getRecycleBin().isContainedInidToRecycleTime(partitionId));
+        erasePartition();
+        Assert.assertTrue(!GlobalStateMgr.getCurrentState().getRecycleBin().isContainedInidToRecycleTime(partitionId));
     }
 }
