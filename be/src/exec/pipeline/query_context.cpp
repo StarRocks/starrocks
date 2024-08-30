StarRocks/starrocks

commit d7ad29e7580853cb76c5fd97cf87f1d7127a7280

[Enhancement] support phased schedule (#47868)
Signed-off-by: stdpain <drfeng08@gmail.com>

be/src/exec/pipeline/query_context.cpp
@@ -100,6 +100,7 @@ FragmentContextManager* QueryContext::fragment_mgr() {
 }
 
 void QueryContext::cancel(const Status& status) {
+    _is_cancelled = true;
     _fragment_mgr->cancel(status);
 }
 
@@ -337,6 +338,16 @@ QueryContextManager::~QueryContextManager() {
     }
 }
 
+#define RETURN_NULL_IF_CTX_CANCELLED(query_ctx) \
+    if (query_ctx->is_cancelled()) {            \
+        return nullptr;                         \
+    }                                           \
+    query_ctx->increment_num_fragments();       \
+    if (query_ctx->is_cancelled()) {            \
+        query_ctx->rollback_inc_fragments();    \
+        return nullptr;                         \
+    }
+
 QueryContext* QueryContextManager::get_or_register(const TUniqueId& query_id) {
     size_t i = _slot_idx(query_id);
     auto& mutex = _mutexes[i];
@@ -348,7 +359,7 @@ QueryContext* QueryContextManager::get_or_register(const TUniqueId& query_id) {
         // lookup query context in context_map
         auto it = context_map.find(query_id);
         if (it != context_map.end()) {
-            it->second->increment_num_fragments();
+            RETURN_NULL_IF_CTX_CANCELLED(it->second);
             return it->second.get();
         }
     }
@@ -358,13 +369,13 @@ QueryContext* QueryContextManager::get_or_register(const TUniqueId& query_id) {
         auto it = context_map.find(query_id);
         auto sc_it = sc_map.find(query_id);
         if (it != context_map.end()) {
-            it->second->increment_num_fragments();
+            RETURN_NULL_IF_CTX_CANCELLED(it->second);
             return it->second.get();
         } else {
             // lookup query context for the second chance in sc_map
             if (sc_it != sc_map.end()) {
                 auto ctx = std::move(sc_it->second);
-                ctx->increment_num_fragments();
+                RETURN_NULL_IF_CTX_CANCELLED(ctx);
                 sc_map.erase(sc_it);
                 auto* raw_ctx_ptr = ctx.get();
                 context_map.emplace(query_id, std::move(ctx));

be/src/exec/CMakeLists.txt
@@ -21,6 +21,7 @@ set(LIBRARY_OUTPUT_PATH "${BUILD_DIR}/src/exec")
 set(EXECUTABLE_OUTPUT_PATH "${BUILD_DIR}/src/exec")
 
 set(EXEC_FILES
+    capture_version_node.cpp
     data_sink.cpp
     empty_set_node.cpp
     exec_node.cpp
@@ -166,6 +167,7 @@ set(EXEC_FILES
     sorting/sort_column.cpp
     sorting/sort_permute.cpp
     connector_scan_node.cpp
+    pipeline/capture_version_operator.cpp
     pipeline/exchange/exchange_merge_sort_source_operator.cpp
     pipeline/exchange/exchange_parallel_merge_source_operator.cpp
     pipeline/exchange/exchange_sink_operator.cpp

be/src/exec/capture_version_node.cpp
@@ -1,0 +1,36 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "exec/capture_version_node.h"
+
+#include "exec/pipeline/capture_version_operator.h"
+#include "exec/pipeline/pipeline_builder.h"
+
+namespace starrocks {
+
+pipeline::OpFactories CaptureVersionNode::decompose_to_pipeline(pipeline::PipelineBuilderContext* context) {
+    return OpFactories{std::make_shared<pipeline::CaptureVersionOpFactory>(context->next_operator_id(), id())};
+}
+
+StatusOr<pipeline::MorselQueueFactoryPtr> CaptureVersionNode::scan_range_to_morsel_queue_factory(
+        const std::vector<TScanRangeParams>& scan_ranges) {
+    pipeline::Morsels morsels;
+    for (const auto& scan_range : scan_ranges) {
+        morsels.emplace_back(std::make_unique<pipeline::ScanMorsel>(id(), scan_range));
+    }
+    auto morsel_queue = std::make_unique<pipeline::FixedMorselQueue>(std::move(morsels));
+    return std::make_unique<pipeline::SharedMorselQueueFactory>(std::move(morsel_queue), 1);
+}
+
+} // namespace starrocks

be/src/exec/capture_version_node.h
@@ -1,0 +1,32 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "exec/exec_node.h"
+#include "exec/scan_node.h"
+
+namespace starrocks {
+class CaptureVersionNode final : public ExecNode {
+public:
+    CaptureVersionNode(ObjectPool* pool, const TPlanNode& tnode, const DescriptorTbl& descs)
+            : ExecNode(pool, tnode, descs) {}
+    ~CaptureVersionNode() override = default;
+
+    StatusOr<pipeline::MorselQueueFactoryPtr> scan_range_to_morsel_queue_factory(
+            const std::vector<TScanRangeParams>& global_scan_ranges);
+
+    pipeline::OpFactories decompose_to_pipeline(pipeline::PipelineBuilderContext* context) override;
+};
+} // namespace starrocks

be/src/exec/exec_node.cpp
@@ -50,6 +50,7 @@
 #include "exec/aggregate/distinct_streaming_node.h"
 #include "exec/analytic_node.h"
 #include "exec/assert_num_rows_node.h"
+#include "exec/capture_version_node.h"
 #include "exec/connector_scan_node.h"
 #include "exec/cross_join_node.h"
 #include "exec/dict_decode_node.h"
@@ -74,6 +75,7 @@
 #include "exec/union_node.h"
 #include "exprs/dictionary_get_expr.h"
 #include "exprs/expr_context.h"
+#include "gen_cpp/PlanNodes_types.h"
 #include "gutil/strings/substitute.h"
 #include "runtime/descriptors.h"
 #include "runtime/exec_env.h"
@@ -562,6 +564,10 @@ Status ExecNode::create_vectorized_node(starrocks::RuntimeState* state, starrock
     }
     case TPlanNodeType::STREAM_AGG_NODE: {
         *node = pool->add(new StreamAggregateNode(pool, tnode, descs));
+        return Status::OK();
+    }
+    case TPlanNodeType::CAPTURE_VERSION_NODE: {
+        *node = pool->add(new CaptureVersionNode(pool, tnode, descs));
         return Status::OK();
     }
     default:

be/src/exec/olap_scan_node.cpp
@@ -753,6 +753,23 @@ StatusOr<TabletSharedPtr> OlapScanNode::get_tablet(const TInternalScanRange* sca
     return tablet;
 }
 
+StatusOr<std::vector<RowsetSharedPtr>> OlapScanNode::capture_tablet_rowsets(const TabletSharedPtr& tablet,
+                                                                            const TInternalScanRange* scan_range) {
+    std::vector<RowsetSharedPtr> rowsets;
+    if (scan_range->__isset.gtid) {
+        std::shared_lock l(tablet->get_header_lock());
+        RETURN_IF_ERROR(tablet->capture_consistent_rowsets(scan_range->gtid, &rowsets));
+        Rowset::acquire_readers(rowsets);
+    } else {
+        int64_t version = strtoul(scan_range->version.c_str(), nullptr, 10);
+        // Capture row sets of this version tablet.
+        std::shared_lock l(tablet->get_header_lock());
+        RETURN_IF_ERROR(tablet->capture_consistent_rowsets(Version(0, version), &rowsets));
+        Rowset::acquire_readers(rowsets);
+    }
+    return rowsets;
+}
+
 int OlapScanNode::estimated_max_concurrent_chunks() const {
     // We temporarily assume that the memory tried in the storage layer
     // is the same size as the chunk_size * _estimated_scan_row_bytes.

be/src/exec/olap_scan_node.h
@@ -87,6 +87,9 @@ public:
     int estimated_max_concurrent_chunks() const;
 
     static StatusOr<TabletSharedPtr> get_tablet(const TInternalScanRange* scan_range);
+    static StatusOr<std::vector<RowsetSharedPtr>> capture_tablet_rowsets(const TabletSharedPtr& tablet,
+                                                                         const TInternalScanRange* scan_range);
+
     static int compute_priority(int32_t num_submitted_tasks);
 
     int io_tasks_per_scan_operator() const override {

be/src/exec/pipeline/capture_version_operator.cpp
@@ -1,0 +1,52 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "exec/pipeline/capture_version_operator.h"
+
+#include "common/logging.h"
+#include "exec/olap_scan_node.h"
+#include "runtime/runtime_state.h"
+#include "storage/rowset/rowset.h"
+#include "util/defer_op.h"
+
+namespace starrocks::pipeline {
+Status CaptureVersionOperator::prepare(RuntimeState* state) {
+    RETURN_IF_ERROR(SourceOperator::prepare(state));
+    _capture_tablet_rowsets_timer = ADD_TIMER(_unique_metrics, "CaptureTabletRowsetsTime");
+
+    return Status::OK();
+}
+
+StatusOr<ChunkPtr> CaptureVersionOperator::pull_chunk(RuntimeState* state) {
+    // do capture rowset here
+    auto defer = DeferOp([this]() { _is_finished = true; });
+    std::vector<std::vector<RowsetSharedPtr>> tablet_rowsets;
+
+    {
+        SCOPED_TIMER(_capture_tablet_rowsets_timer);
+        auto scan_rages = _morsel_queue->prepare_olap_scan_ranges();
+        std::vector<std::vector<RowsetSharedPtr>> tablet_rowsets;
+        VLOG_QUERY << "capture rowset scan_ranges nums:" << scan_rages.size();
+        for (auto& scan_range : scan_rages) {
+            ASSIGN_OR_RETURN(TabletSharedPtr tablet, OlapScanNode::get_tablet(scan_range));
+            ASSIGN_OR_RETURN(auto rowset, OlapScanNode::capture_tablet_rowsets(tablet, scan_range));
+            tablet_rowsets.emplace_back(std::move(rowset));
+        }
+        //
+        _rowset_release_guard = MultiRowsetReleaseGuard(std::move(tablet_rowsets), adopt_acquire_t{});
+    }
+    return nullptr;
+}
+
+} // namespace starrocks::pipeline

commit f5c87de9f5e1c93cc39bb948b34f9aef31a17608

[BugFix] Fix QueryContextManager clear_thread crash
introduced by #47868

```
*** Aborted at 1724997275 (unix time) try "date -d @1724997275" if you are using GNU date ***
PC: @          0x5debccb starrocks::pipeline::QueryContextManager::_clean_slot_unlocked(unsigned long, std::vector<std::shared_ptr<starrocks::pipeline::QueryContext>, std::allocator<std::shared_ptr<starrocks::pipeline::QueryContext> > >&)
*** SIGSEGV (@0x68) received by PID 6669 (TID 0x7f1959fe6640) from PID 104; stack trace: ***
    @     0x7f19793aaf68 (/usr/lib/x86_64-linux-gnu/libc.so.6 (deleted)+0x99f67)
    @          0xefd0d19 google::(anonymous namespace)::FailureSignalHandler(int, siginfo_t*, void*)
    @     0x7f197a3ae916 os::Linux::chained_handler(int, siginfo_t*, void*)
    @     0x7f197a3b460b JVM_handle_linux_signal
    @     0x7f197a3a746c signalHandler(int, siginfo_t*, void*)
    @     0x7f1979353520 (/usr/lib/x86_64-linux-gnu/libc.so.6 (deleted)+0x4251f)
    @          0x5debccb starrocks::pipeline::QueryContextManager::_clean_slot_unlocked(unsigned long, std::vector<std::shared_ptr<starrocks::pipeline::QueryContext>, std::allocator<std::shared_ptr<starrocks::pipeline::QueryContext> > >&)
    @          0x5debf27 starrocks::pipeline::QueryContextManager::_clean_query_contexts()
    @          0x5dec191 starrocks::pipeline::QueryContextManager::_clean_func(starrocks::pipeline::QueryContextManager*)
    @          0x5df245b std::thread::_State_impl<std::thread::_Invoker<std::tuple<void (*)(starrocks::pipeline::QueryContextManager*), starrocks::pipeline::QueryContextManager*> > >::_M_run()
    @         0x115c1d44 execute_native_thread_routine
    @     0x7f19793a5b43 (/usr/lib/x86_64-linux-gnu/libc.so.6 (deleted)+0x94b42)
    @     0x7f1979437a00 (/usr/lib/x86_64-linux-gnu/libc.so.6 (deleted)+0x1269ff)
```

Signed-off-by: stdpain <drfeng08@gmail.com>

be/src/exec/pipeline/query_context.cpp
@@ -375,8 +375,8 @@ QueryContext* QueryContextManager::get_or_register(const TUniqueId& query_id) {
             // lookup query context for the second chance in sc_map
             if (sc_it != sc_map.end()) {
                 auto ctx = std::move(sc_it->second);
-                RETURN_NULL_IF_CTX_CANCELLED(ctx);
                 sc_map.erase(sc_it);
+                RETURN_NULL_IF_CTX_CANCELLED(ctx);
                 auto* raw_ctx_ptr = ctx.get();
                 context_map.emplace(query_id, std::move(ctx));
                 return raw_ctx_ptr;