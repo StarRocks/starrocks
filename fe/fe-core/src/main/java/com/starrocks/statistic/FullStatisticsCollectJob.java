ba9dbdc37162fa77d770ae38819b43aef968de58
Murphy <mofei@starrocks.com>
2024-08-30 15:42:55 +0800

    fix potential NPE of analyze job
    Signed-off-by: Murphy <mofei@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/statistic/FullStatisticsCollectJob.java
+++ fe/fe-core/src/main/java/com/starrocks/statistic/FullStatisticsCollectJob.java
@@ -132,7 +132,12 @@ public class FullStatisticsCollectJob extends StatisticsCollectJob {
             List<String> params = Lists.newArrayList();
             List<Expr> row = Lists.newArrayList();
 
-            String partitionName = StringEscapeUtils.escapeSql(table.getPartition(data.getPartitionId()).getName());
+            Partition partition = table.getPartition(data.getPartitionId());
+            if (partition == null) {
+                // The partition is dropped during analyze, we just need to skip it
+                continue;
+            }
+            String partitionName = StringEscapeUtils.escapeSql(partition.getName());
 
             params.add(String.valueOf(table.getId()));
             params.add(String.valueOf(data.getPartitionId()));
