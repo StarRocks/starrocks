b7128f81a4882f74cd4e92658eb9c7d37c612af0
yanz <dirtysalt1987@gmail.com>
2024-09-05 08:14:33 +0800

    fix ut -e Signed-off-by: yanz <dirtysalt1987@gmail.com>

--- be/src/connector/connector.h
+++ be/src/connector/connector.h
@@ -127,7 +127,7 @@ class DataSourceProvider {
 public:
     static constexpr int64_t MIN_DATA_SOURCE_MEM_BYTES = 16 * 1024 * 1024;  // 16MB
     static constexpr int64_t MAX_DATA_SOURCE_MEM_BYTES = 256 * 1024 * 1024; // 256MB
-    static constexpr int64_t PER_FIELD_MEM_BYTES = 4 * 1024 * 1024;         // 4MB
+    static constexpr int64_t PER_FIELD_MEM_BYTES = 1 * 1024 * 1024;         // 1MB
 
     virtual ~DataSourceProvider() = default;
 
