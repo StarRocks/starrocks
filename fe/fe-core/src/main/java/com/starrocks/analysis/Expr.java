b726fee7f1affe4167b8b7a6f938847328f4e51f
zihe.liu <ziheliu1024@gmail.com>
2024-09-05 10:18:25 +0800

    [BugFix] Clear probe RF whose probe expr contains dict mapping expr (#50690)
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>
    (cherry picked from commit a810948f01e46aec81a00145698df29375c4e973)
    
    # Conflicts:
    #	be/src/exprs/runtime_filter_bank.cpp
    #	fe/fe-core/src/main/java/com/starrocks/planner/ProjectNode.java

--- fe/fe-core/src/main/java/com/starrocks/analysis/Expr.java
+++ fe/fe-core/src/main/java/com/starrocks/analysis/Expr.java
@@ -1494,4 +1494,15 @@ public abstract class Expr extends TreeNode<Expr> implements ParseNode, Cloneabl
         return hints;
     }
 
+    public boolean containsDictMappingExpr() {
+        return containsDictMappingExpr(this);
+    }
+
+    private static boolean containsDictMappingExpr(Expr expr) {
+        if (expr instanceof DictMappingExpr) {
+            return true;
+        }
+        return expr.getChildren().stream().anyMatch(child -> containsDictMappingExpr(child));
+    }
+
 }
