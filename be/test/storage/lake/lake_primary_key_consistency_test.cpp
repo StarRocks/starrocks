b6d7d64eaf3344dbdcdf2cc2bcfbb5ba350d93f6
Yixin Luo <18810541851@163.com>
2024-09-06 16:36:31 +0000

    resolve
    Signed-off-by: Yixin Luo <18810541851@163.com>

--- be/test/storage/lake/lake_primary_key_consistency_test.cpp
+++ be/test/storage/lake/lake_primary_key_consistency_test.cpp
@@ -463,7 +463,7 @@ public:
                     delta_writer->write(*(chunk_index.first), chunk_index.second.data(), chunk_index.second.size()));
             _replayer->condition_update(chunk_index.first, merge_condition);
         }
-        RETURN_IF_ERROR(delta_writer->finish_with_txnlog());
+        RETURN_IF_ERROR(delta_writer->finish());
         delta_writer->close();
         // Publish version
         RETURN_IF_ERROR(publish_single_version(_tablet_metadata->id(), _version + 1, txn_id));
