f91fd6416ea0c964fb0bf57730a7baaaea601445
packy92 <110370499+packy92@users.noreply.github.com>
2024-09-09 16:15:32 +0800

    [Enhancement] support constant folding in translating scalarOperator phase (#50734)

--- fe/fe-core/src/test/java/com/starrocks/analysis/SelectStmtTest.java
+++ fe/fe-core/src/test/java/com/starrocks/analysis/SelectStmtTest.java
@@ -320,6 +320,7 @@ public class SelectStmtTest {
     }
 
     @Test
+<<<<<<< HEAD
     void testGroupByCountDistinctArrayWithSkewHint() throws Exception {
         FeConstants.runningUnitTest = true;
         // array is not supported now
@@ -354,6 +355,8 @@ public class SelectStmtTest {
     }
 
     @Test
+=======
+>>>>>>> 01f048a733 ([Enhancement] support constant folding in translating scalarOperator phase (#50734))
     void testGroupByMultiColumnCountDistinctWithSkewHint() throws Exception {
         FeConstants.runningUnitTest = true;
         String sql =
