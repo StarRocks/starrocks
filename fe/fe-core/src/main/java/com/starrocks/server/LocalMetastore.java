0a275ce8f003d7d6e5ce97e8a079da2dfccc35c0
edwinhzhang <edwinhzhang@tencent.com>
2024-09-12 20:07:44 +0800

    fix doc style
    Signed-off-by: edwinhzhang <edwinhzhang@tencent.com>

--- fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
+++ fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
@@ -4933,6 +4933,11 @@ public class LocalMetastore implements ConnectorMetadata, MVRepairHandler, Memor
         writer.close();
     }
 
+    /**
+     * When loading the LocalMetastore, the DB/Catalog that the materialized view depends
+     * on may not have been loaded yet, so you cannot reload the materialized view.
+     * The reload operation of a materialized view should be called by {@link GlobalStateMgr#postLoadImage()}.
+     */
     public void load(SRMetaBlockReader reader) throws IOException, SRMetaBlockException, SRMetaBlockEOFException {
         int dbSize = reader.readInt();
         for (int i = 0; i < dbSize; ++i) {
@@ -4946,11 +4951,6 @@ public class LocalMetastore implements ConnectorMetadata, MVRepairHandler, Memor
             idToDb.put(db.getId(), db);
             fullNameToDb.put(db.getFullName(), db);
             stateMgr.getGlobalTransactionMgr().addDatabaseTransactionMgr(db.getId());
-            /**
-             * When loading the LocalMetastore, the DB/Catalog that the materialized view depends
-             * on may not have been loaded yet, so you cannot reload the materialized view.
-             * The reload operation of a materialized view should be called by {@link GlobalStateMgr#postLoadImage()}.
-             */
             db.getTables().stream().filter(tbl -> !tbl.isMaterializedView()).forEach(tbl -> {
                 try {
                     tbl.onReload();
