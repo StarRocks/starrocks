fce44bb42e6dfbf997dc8364cb1664d02edf08ec
xiangguangyxg <xiangguangyxg@gmail.com>
2024-09-18 20:04:24 +0800

    [Enhancement] Optimize get tablet metadata to avoid not found exception
    Signed-off-by: xiangguangyxg <xiangguangyxg@gmail.com>

--- be/src/storage/lake/tablet.h
+++ be/src/storage/lake/tablet.h
@@ -66,6 +66,8 @@ public:
 
     Status delete_metadata(int64_t version);
 
+    Status metadata_exists(int64_t version);
+
     Status put_txn_log(const TxnLog& log);
 
     Status put_txn_log(const TxnLogPtr& log);
