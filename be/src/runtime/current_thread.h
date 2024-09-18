d9a620b3ad28b96a82199af10c0fa57a9033240b
trueeyu <lxhhust350@qq.com>
2024-09-18 20:03:00 +0800

    [BugFix] Fix thread local variable initialization problem (#50899)
    Signed-off-by: trueeyu <lxhhust350@qq.com>
    (cherry picked from commit 2d3ee0b6bfc99eb6300b6f03b9f4c802d471fef8)
    
    # Conflicts:
    #	be/src/runtime/current_thread.h

--- be/src/runtime/current_thread.h
+++ be/src/runtime/current_thread.h
@@ -52,6 +52,7 @@ inline thread_local MemTracker* tls_mem_tracker = nullptr;
 inline thread_local MemTracker* tls_operator_mem_tracker = nullptr;
 inline thread_local MemTracker* tls_exceed_mem_tracker = nullptr;
 inline thread_local bool tls_is_thread_status_init = false;
+inline thread_local bool tls_is_catched = false;
 
 class CurrentThread {
 private:
@@ -251,13 +252,23 @@ public:
 
     static void set_exceed_mem_tracker(starrocks::MemTracker* mem_tracker) { tls_exceed_mem_tracker = mem_tracker; }
 
+<<<<<<< HEAD
     bool set_is_catched(bool is_catched) {
         bool old = _is_catched;
         _is_catched = is_catched;
+=======
+    static void set_singleton_check_mem_tracker(starrocks::MemTracker* mem_tracker) {
+        tls_singleton_check_mem_tracker = mem_tracker;
+    }
+
+    static bool set_is_catched(bool is_catched) {
+        bool old = tls_is_catched;
+        tls_is_catched = is_catched;
+>>>>>>> 2d3ee0b6bf ([BugFix] Fix thread local variable initialization problem (#50899))
         return old;
     }
 
-    bool is_catched() const { return _is_catched; }
+    static bool is_catched() { return tls_is_catched; }
 
     void mem_consume(int64_t size) {
         _mem_cache_manager.consume(size);
@@ -339,7 +350,6 @@ private:
     TUniqueId _fragment_instance_id;
     std::string _custom_coredump_msg{};
     int32_t _driver_id = 0;
-    bool _is_catched = false;
     bool _check = true;
     bool _reserve_mod = false;
 };
