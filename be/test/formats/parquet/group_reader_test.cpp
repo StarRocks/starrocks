062dfd28154a83628f643c1a18d517a9e70bacd0
Youngwb <yangwenbo_mailbox@163.com>
2024-09-24 21:20:23 +0800

    fix be ut
    Signed-off-by: Youngwb <yangwenbo_mailbox@163.com>

--- be/test/formats/parquet/group_reader_test.cpp
+++ be/test/formats/parquet/group_reader_test.cpp
@@ -448,7 +448,8 @@ TEST_F(GroupReaderTest, ColumnReaderCreateTypeMismatch) {
     col_type.type = LogicalType::TYPE_VARCHAR;
 
     ColumnReaderOptions options;
-    Status st = ColumnReader::create(options, &field, col_type, nullptr);
+    const TPhysicalSchemaField* physical_schema_field = nullptr;
+    Status st = ColumnReader::create(options, &field, col_type, physical_schema_field, nullptr);
     ASSERT_FALSE(st.ok()) << st;
     std::cout << st.message() << "\n";
 }
