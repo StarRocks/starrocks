d5fe5eb38a64540dd73d8d18903246ce983d3c39
Dan Jing <jingdan@starrocks.com>
2024-09-25 14:33:30 +0800

    update metric name
    Signed-off-by: Dan Jing <jingdan@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/metric/MetricRepo.java
+++ fe/fe-core/src/main/java/com/starrocks/metric/MetricRepo.java
@@ -934,7 +934,7 @@ public final class MetricRepo {
         Map<Long, DatabaseTransactionMgr> dbIdToDatabaseTransactionMgrs =
                 GlobalStateMgr.getCurrentState().getGlobalTransactionMgr().getAllDatabaseTransactionMgrs();
         for (DatabaseTransactionMgr mgr : dbIdToDatabaseTransactionMgrs.values()) {
-            GaugeMetricImpl<Long> txnNum = new GaugeMetricImpl<>("running_txn_num", MetricUnit.NOUNIT,
+            GaugeMetricImpl<Long> txnNum = new GaugeMetricImpl<>("txn_running", MetricUnit.NOUNIT,
                      "number of running transactions");
             Database db = GlobalStateMgr.getCurrentState().getLocalMetastore().getDb(mgr.getDbId());
             if (null == db) {
