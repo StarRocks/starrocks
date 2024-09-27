c04dbbdd6d5ab5d79b87064f2325017a5123960d
zihe.liu <ziheliu1024@gmail.com>
2024-09-27 14:56:34 +0800

    fix
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- be/src/exec/workgroup/scan_task_queue.cpp
+++ be/src/exec/workgroup/scan_task_queue.cpp
@@ -200,7 +200,7 @@ bool WorkGroupScanTaskQueue::try_offer(ScanTask task) {
         task.peak_scan_task_queue_size_counter->set(_num_tasks);
     }
 
-    auto* wg_entity = _sched_entity(task.workgroup);
+    auto* wg_entity = _sched_entity(task.workgroup.get());
     wg_entity->set_in_queue(this);
     RETURN_IF_UNLIKELY(!wg_entity->queue()->try_offer(std::move(task)), false);
 
@@ -215,7 +215,7 @@ bool WorkGroupScanTaskQueue::try_offer(ScanTask task) {
 
 void WorkGroupScanTaskQueue::update_statistics(ScanTask& task, int64_t runtime_ns) {
     std::lock_guard<std::mutex> lock(_global_mutex);
-    auto* wg = task.workgroup;
+    auto* wg = task.workgroup.get();
     auto* wg_entity = _sched_entity(wg);
 
     // Update bandwidth control information.
