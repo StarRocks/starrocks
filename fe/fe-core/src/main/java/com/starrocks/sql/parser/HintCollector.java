aaf3c3ac69e930ba76cfe99bfe8c3a1e9b390fb2
stdpain <34912776+stdpain@users.noreply.github.com>
2024-10-02 09:53:47 +0800

    [BugFix] Fix unexpected Hint when UPDATE Stmt has CTE (#51458)
    Signed-off-by: stdpain <drfeng08@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/parser/HintCollector.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/parser/HintCollector.java
@@ -101,7 +101,7 @@ public class HintCollector extends StarRocksBaseVisitor<Void> {
 
     @Override
     public Void visitUpdateStatement(StarRocksParser.UpdateStatementContext context) {
-        extractHintToRight(context);
+        extractHintToRight(context, context.UPDATE().getSymbol().getTokenIndex());
         if (!(context.fromClause() instanceof StarRocksParser.DualContext)) {
             StarRocksParser.FromContext fromContext = (StarRocksParser.FromContext) context.fromClause();
             if (fromContext.relations() != null) {
@@ -113,7 +113,7 @@ public class HintCollector extends StarRocksBaseVisitor<Void> {
 
     @Override
     public Void visitDeleteStatement(StarRocksParser.DeleteStatementContext context) {
-        extractHintToRight(context);
+        extractHintToRight(context, context.DELETE().getSymbol().getTokenIndex());
         if (context.using != null) {
             context.using.relation().stream().forEach(this::visit);
         }
@@ -192,6 +192,13 @@ public class HintCollector extends StarRocksBaseVisitor<Void> {
         Token semi = ctx.start;
         int i = semi.getTokenIndex();
         List<Token> hintTokens = tokenStream.getHiddenTokensToRight(i, HINT_CHANNEL);
+        if (hintTokens != null) {
+            contextWithTokenMap.computeIfAbsent(ctx, e -> new ArrayList<>()).addAll(hintTokens);
+        }
+    }
+
+    private void extractHintToRight(ParserRuleContext ctx, int fromIndex) {
+        List<Token> hintTokens = tokenStream.getHiddenTokensToRight(fromIndex, HINT_CHANNEL);
         if (hintTokens != null) {
             contextWithTokenMap.computeIfAbsent(ctx, e -> new ArrayList<>()).addAll(hintTokens);
         }
