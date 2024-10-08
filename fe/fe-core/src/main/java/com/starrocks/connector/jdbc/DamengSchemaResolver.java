33211733b0246637a5021b20708a71a5c7169d55
刘晶 <liuj-ai@glodon.com>
2024-10-08 14:20:24 +0800

    jdbc支持达梦clob类型

--- fe/fe-core/src/main/java/com/starrocks/connector/jdbc/DamengSchemaResolver.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/jdbc/DamengSchemaResolver.java
@@ -104,6 +104,7 @@ public class DamengSchemaResolver extends JDBCSchemaResolver {
                 break;
             case Types.CHAR:
                 return ScalarType.createCharType(columnSize);
+            case Types.CLOB:
             case Types.LONGVARCHAR:
                 return ScalarType.createVarcharType(columnSize);
             case Types.VARCHAR:
