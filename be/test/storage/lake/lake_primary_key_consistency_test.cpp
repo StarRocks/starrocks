f02e8e1c5ca0f1e01f337a2feaaf034ac56d68a4
luohaha <18810541851@163.com>
2024-10-08 09:03:45 +0800

    g[BugFix] fix cloud native persistent index sst compaction when sst files contain same max_rss_rowid
    Signed-off-by: luohaha <18810541851@163.com>

--- be/test/storage/lake/lake_primary_key_consistency_test.cpp
+++ be/test/storage/lake/lake_primary_key_consistency_test.cpp
@@ -399,7 +399,14 @@ public:
                     delta_writer->write(*(chunk_index.first), chunk_index.second.data(), chunk_index.second.size()));
             _replayer->upsert(chunk_index.first);
         }
-        RETURN_IF_ERROR(delta_writer->finish());
+        size_t delete_size = _random_generator->random() % MaxUpsert;
+        for (int i = 0; i < delete_size; i++) {
+            auto chunk_index = gen_upsert_data(false);
+            RETURN_IF_ERROR(
+                    delta_writer->write(*(chunk_index.first), chunk_index.second.data(), chunk_index.second.size()));
+            _replayer->erase(chunk_index.first);
+        }
+        RETURN_IF_ERROR(delta_writer->finish_with_txnlog());
         delta_writer->close();
         // Publish version
         RETURN_IF_ERROR(publish_single_version(_tablet_metadata->id(), _version + 1, txn_id));
@@ -494,7 +501,14 @@ public:
                                                     chunk_index.second.size()));
                 _replayer->upsert(chunk_index.first);
             }
-            RETURN_IF_ERROR(delta_writer->finish());
+            size_t delete_size = _random_generator->random() % MaxUpsert;
+            for (int i = 0; i < delete_size; i++) {
+                auto chunk_index = gen_upsert_data(false);
+                RETURN_IF_ERROR(delta_writer->write(*(chunk_index.first), chunk_index.second.data(),
+                                                    chunk_index.second.size()));
+                _replayer->erase(chunk_index.first);
+            }
+            RETURN_IF_ERROR(delta_writer->finish_with_txnlog());
             delta_writer->close();
         }
         // Batch Publish version
