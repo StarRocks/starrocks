0ed2adfd5f12b65b74b90eab1502503b832cc65b
andyziye <108652123+andyziye@users.noreply.github.com>
2024-10-10 20:03:45 +0800

    Revert "[Enhancement] add metric for running txn number of per db" (#51394) (#28529)
    Co-authored-by: iverson <58965658+wanpengfei-git@users.noreply.github.com>

--- fe/fe-core/src/main/java/com/starrocks/transaction/DatabaseTransactionMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/transaction/DatabaseTransactionMgr.java
@@ -638,15 +638,6 @@ public class DatabaseTransactionMgr {
         return runningTxnNums;
     }
 
-    public long getRunningTxnNumsWithLock() {
-        readLock();
-        try {
-            return runningTxnNums;
-        } finally {
-            readUnlock();
-        }
-    }
-
     @VisibleForTesting
     protected int getRunningRoutineLoadTxnNums() {
         return runningRoutineLoadTxnNums;
