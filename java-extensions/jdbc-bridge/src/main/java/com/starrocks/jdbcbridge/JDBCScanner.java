5d0cdeb3af8887225f9579422a287c1e2d7cae26
Smith Cruise <chendingchao1@126.com>
2024-10-11 15:55:24 +0800

    [BugFix] Fix jdbc catalog change password not effect (backport #51748) (#51765)
    Signed-off-by: Smith Cruise <chendingchao1@126.com>
    (cherry picked from commit 67cfd77ae2be3068d5a340987c9edb332443a5f1)

--- java-extensions/jdbc-bridge/src/main/java/com/starrocks/jdbcbridge/JDBCScanner.java
+++ java-extensions/jdbc-bridge/src/main/java/com/starrocks/jdbcbridge/JDBCScanner.java
@@ -57,9 +57,9 @@ public class JDBCScanner {
     }
 
     public void open() throws Exception {
-        String key = scanContext.getUser() + "/" + scanContext.getJdbcURL();
+        String cacheKey = computeCacheKey(scanContext.getUser(), scanContext.getPassword(), scanContext.getJdbcURL());
         URL driverURL = new File(driverLocation).toURI().toURL();
-        DataSourceCache.DataSourceCacheItem cacheItem = DataSourceCache.getInstance().getSource(key, () -> {
+        DataSourceCache.DataSourceCacheItem cacheItem = DataSourceCache.getInstance().getSource(cacheKey, () -> {
             ClassLoader classLoader = URLClassLoader.newInstance(new URL[] {
                     driverURL,
             });
@@ -103,6 +103,10 @@ public class JDBCScanner {
                 resultChunk.add((Object[]) Array.newInstance(String.class, scanContext.getStatementFetchSize()));
             }
         }
+    }
+
+    private static String computeCacheKey(String username, String password, String jdbcUrl) {
+        return username + "/" + password + "/" + jdbcUrl;
     }
 
     private static final Set<Class<?>> GENERAL_JDBC_CLASS_SET =  new HashSet<>(Arrays.asList(
