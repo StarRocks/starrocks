fe05f3c84506717ebeefbc69b2d6ac164612ccdb
Yixin Luo <18810541851@163.com>
2024-10-11 15:57:39 -0700

    [UT] remove execute_script in UT (#51816)
    Signed-off-by: luohaha <18810541851@163.com>

--- be/test/storage/tablet_updates_test.cpp
+++ be/test/storage/tablet_updates_test.cpp
@@ -345,11 +345,6 @@ void TabletUpdatesTest::test_writeread(bool enable_persistent_index) {
     ASSERT_TRUE(_tablet->rowset_commit(2, rs0).ok());
     ASSERT_EQ(2, _tablet->updates()->max_version());
 
-    string o;
-    ASSERT_TRUE(execute_script(fmt::format("StorageEngine.reset_delvec({}, {}, 2)", _tablet->tablet_id(), 0), o).ok());
-    ASSERT_TRUE(execute_script("System.print(ExecEnv.grep_log_as_string(0,0,\"I\",\"tablet_manager\",1))", o).ok());
-    LOG(INFO) << "grep log: " << o;
-
     auto rs1 = create_rowset(_tablet, keys);
     ASSERT_TRUE(_tablet->rowset_commit(3, rs1).ok());
     ASSERT_EQ(3, _tablet->updates()->max_version());
