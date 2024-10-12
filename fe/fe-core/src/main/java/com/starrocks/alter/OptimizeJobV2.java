e275208026bfcfa2a7935d45a2695bdcc261ca9d
meegoo <meegoo.sr@gmail.com>
2024-10-12 13:29:56 +0800

    Update OptimizeJobV2.java
    Signed-off-by: meegoo <meegoo.sr@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/alter/OptimizeJobV2.java
+++ fe/fe-core/src/main/java/com/starrocks/alter/OptimizeJobV2.java
@@ -287,15 +287,9 @@ public class OptimizeJobV2 extends AlterJobV2 implements GsonPostProcessable {
         for (int i = 0; i < tmpPartitionNames.size(); ++i) {
             String tmpPartitionName = tmpPartitionNames.get(i);
             String partitionName = partitionNames.get(i);
-<<<<<<< HEAD
-            String rewriteSql = "insert into " + tableName + " TEMPORARY PARTITION ("
-                    + tmpPartitionName + ") select " + Joiner.on(", ").join(tableColumnNames)
-                    + " from " + tableName + " partition (" + partitionName + ")";
-=======
             String rewriteSql = "insert into " + ParseUtil.backquote(tableName) + " TEMPORARY PARTITION ("
                         + ParseUtil.backquote(tmpPartitionName) + ") select " + Joiner.on(", ").join(tableColumnNames)
                         + " from " + ParseUtil.backquote(tableName) + " partition (" + ParseUtil.backquote(partitionName) + ")";
->>>>>>> 2a1a68bf28 ([BugFix] Fix optimize table fail with special characters (#51709))
             String taskName = getName() + "_" + tmpPartitionName;
             OptimizeTask rewriteTask = TaskBuilder.buildOptimizeTask(taskName, properties, rewriteSql, dbName);
             rewriteTask.setPartitionName(partitionName);
