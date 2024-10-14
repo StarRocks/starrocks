d2008e00f7b92a8259cc6b1edf7b6450b74bf7ff
smartlxh <smartlxh@gmail.com>
2024-10-14 14:21:10 +0800

    fix
    Signed-off-by: smartlxh <smartlxh@gmail.com>

--- be/src/storage/lake/tablet_reader.h
+++ be/src/storage/lake/tablet_reader.h
@@ -57,7 +57,9 @@ class TabletReader final : public ChunkIterator {
 public:
     TabletReader(TabletManager* tablet_mgr, std::shared_ptr<const TabletMetadataPB> metadata, Schema schema);
     TabletReader(TabletManager* tablet_mgr, std::shared_ptr<const TabletMetadataPB> metadata, Schema schema,
-                 bool need_split, bool could_split_physically, std::vector<BaseRowsetSharedPtr> base_rowsets);
+                 bool need_split, bool could_split_physically);
+    TabletReader(TabletManager* tablet_mgr, std::shared_ptr<const TabletMetadataPB> metadata, Schema schema,
+                 bool need_split, bool could_split_physically, std::vector<RowsetPtr> rowsets);
     TabletReader(TabletManager* tablet_mgr, std::shared_ptr<const TabletMetadataPB> metadata, Schema schema,
                  std::vector<RowsetPtr> rowsets, std::shared_ptr<const TabletSchema> tablet_schema);
     TabletReader(TabletManager* tablet_mgr, std::shared_ptr<const TabletMetadataPB> metadata, Schema schema,
