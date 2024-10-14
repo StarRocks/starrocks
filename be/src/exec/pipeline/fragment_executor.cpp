2eb2f2770b1a45d8473c854333aed2ba645fb053
RyanZ <dirtysalt1987@gmail.com>
2024-10-14 14:40:06 +0800

    [Enhancement] delta lake support incremental scan ranges delivery (#51670)
    ## Why I'm doing:
    
    To support incremental scan ranges delivery in delta lake catalog. 
    
    ## What I'm doing:
    
    ### Old way
    
    In the old way
    - we collect partition values by scannning all scan ranges first
    - put partition values into a descriptor table, 
    - then send this descriptor table down to BE
    
    but clearly this way does not work well with incremental scan range delivery, because we have to "scanning all scan ranges at first".
    
    ### New way
    
    The main improvement right here is how to incrementally deliver partition value.  
    
    The improvement works as following:
    - When we build the scan range, we put the partition value on the scan range at the same time.
    - When building the scan range, we also add the partition value to the scan range, but then check to see if the same partition id has been issued to the fragment instance before.
    	- If it has not been issued before, then it continues to be issued.
    	- Otherwise, the partition value is erased from the scan range.
    	- This reduces the number of times the partition value is issued.
    - Finally, when BE receives the scan ranges, it adds the partition value to the table descriptor, and then continues execution.
    
    This can be illustrated by the following diagram
    - In the first round of sending, Partition Id 1,2,3 have not been sent, so each scan range will be accompanied by the corresponding partition value.
    - In the second round of sending, since Partition Id 1,3 have been sent, then the partition value can be set to None, and Partition Id 4 has not been sent, then Partition Value 4 will be sent.
    
    ![image](https://github.com/user-attachments/assets/d7105abd-1ad6-4b2c-a202-29e262040d2c)
    
    ### Benefit
    
    The benefits of this implementation are:
    - We can reuse scan range incremental delivery to implement this feature.
    - There are fewer partition values to be sent down to BE, because:
    	- The previous way is to send all the possible partition values to the worker nodes.
    	- but there are chances that some scan ranges on some worker nodes will not use any one partition value, which means we watse doing that.
    	- but in this way, we only sent partition values which are used by scan ranges, so the total plan size would be smaller
    
    Fixes #50196
    
    Signed-off-by: RyanZ <dirtysalt1987@gmail.com>

--- be/src/exec/pipeline/fragment_executor.cpp
+++ be/src/exec/pipeline/fragment_executor.cpp
@@ -369,6 +369,36 @@ static std::unordered_set<int32_t> collect_broadcast_join_right_offsprings(
     return offsprings;
 }
 
+// there will be partition values used by this batch of scan ranges and maybe following scan ranges
+// so before process this batch of scan ranges, we have to put partition values into the table associated with.
+static Status add_scan_ranges_partition_values(RuntimeState* runtime_state,
+                                               const std::vector<TScanRangeParams>& scan_ranges) {
+    auto* obj_pool = runtime_state->obj_pool();
+    const DescriptorTbl& desc_tbl = runtime_state->desc_tbl();
+    TTableId cache_table_id = -1;
+    TableDescriptor* table = nullptr;
+
+    for (const auto& scan_range_params : scan_ranges) {
+        const TScanRange& scan_range = scan_range_params.scan_range;
+        if (!scan_range.__isset.hdfs_scan_range) continue;
+        const THdfsScanRange& hdfs_scan_range = scan_range.hdfs_scan_range;
+        if (!hdfs_scan_range.__isset.partition_value) continue;
+        DCHECK(hdfs_scan_range.__isset.table_id);
+        DCHECK(hdfs_scan_range.__isset.partition_id);
+        TTableId table_id = hdfs_scan_range.table_id;
+        if (table_id != cache_table_id) {
+            table = desc_tbl.get_table_descriptor(table_id);
+            cache_table_id = table_id;
+        }
+        if (table == nullptr) continue;
+        // only HiveTableDescriptor(includes hive,iceberg,hudi,deltalake etc) supports this feature.
+        HiveTableDescriptor* hive_table = down_cast<HiveTableDescriptor*>(table);
+        RETURN_IF_ERROR(hive_table->add_partition_value(runtime_state, obj_pool, hdfs_scan_range.partition_id,
+                                                        hdfs_scan_range.partition_value));
+    }
+    return Status::OK();
+}
+
 Status FragmentExecutor::_prepare_exec_plan(ExecEnv* exec_env, const UnifiedExecPlanFragmentParams& request) {
     auto* runtime_state = _fragment_ctx->runtime_state();
     auto* obj_pool = runtime_state->obj_pool();
@@ -494,6 +524,7 @@ Status FragmentExecutor::_prepare_exec_plan(ExecEnv* exec_env, const UnifiedExec
             enable_shared_scan = false;
         }
 
+        RETURN_IF_ERROR(add_scan_ranges_partition_values(runtime_state, scan_ranges));
         ASSIGN_OR_RETURN(auto morsel_queue_factory,
                          scan_node->convert_scan_range_to_morsel_queue_factory(
                                  scan_ranges, scan_ranges_per_driver_seq, scan_node->id(), group_execution_scan_dop,
@@ -868,6 +899,7 @@ Status FragmentExecutor::append_incremental_scan_ranges(ExecEnv* exec_env, const
     if (query_ctx == nullptr) return Status::OK();
     FragmentContextPtr fragment_ctx = query_ctx->fragment_mgr()->get(instance_id);
     if (fragment_ctx == nullptr) return Status::OK();
+    RuntimeState* runtime_state = fragment_ctx->runtime_state();
 
     for (const auto& [node_id, scan_ranges] : params.per_node_scan_ranges) {
         auto iter = fragment_ctx->morsel_queue_factories().find(node_id);
@@ -879,6 +911,7 @@ Status FragmentExecutor::append_incremental_scan_ranges(ExecEnv* exec_env, const
             continue;
         }
 
+        RETURN_IF_ERROR(add_scan_ranges_partition_values(runtime_state, scan_ranges));
         pipeline::Morsels morsels;
         bool has_more_morsel = false;
         pipeline::ScanMorsel::build_scan_morsels(node_id, scan_ranges, true, &morsels, &has_more_morsel);
