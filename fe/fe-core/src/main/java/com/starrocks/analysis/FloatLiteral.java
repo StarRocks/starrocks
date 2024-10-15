f9a6691ad79a528afc288fda34cbb94c94db1f03
ShaoxunLi <79825592+ShaoxunLi@users.noreply.github.com>
2024-10-15 10:07:37 +0800

    [BugFix] fix setDouble issue in jdbc with prepare stmt (#51811)
    Signed-off-by: ShaoxunLi <446032208@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/analysis/FloatLiteral.java
+++ fe/fe-core/src/main/java/com/starrocks/analysis/FloatLiteral.java
@@ -76,6 +76,11 @@ public class FloatLiteral extends LiteralExpr {
         this(value, NodePosition.ZERO);
     }
 
+    public FloatLiteral(String value, Type type) throws AnalysisException {
+        this(value, NodePosition.ZERO);
+        this.type = type;
+    }
+
     public FloatLiteral(String value, NodePosition pos) throws AnalysisException {
         super(pos);
         Double floatValue = null;
