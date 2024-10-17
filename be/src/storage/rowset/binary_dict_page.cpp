a7ed814feca40d1671953c3ac67c4b844445bcf2
Binglin Chang <decstery@gmail.com>
2024-10-17 20:43:14 +0800

    [BugFix] Prevent crash if type mismatch in BinaryDictPageDecoder (#52036)
    Signed-off-by: Binglin Chang <decstery@gmail.com>

--- be/src/storage/rowset/binary_dict_page.cpp
+++ be/src/storage/rowset/binary_dict_page.cpp
@@ -261,7 +261,9 @@ Status BinaryDictPageDecoder<Type>::next_batch(const SparseRange<>& range, Colum
         }
     }
 
-    CHECK(dst->append_strings_overflow(slices, _max_value_legth));
+    bool ok = dst->append_strings_overflow(slices, _max_value_legth);
+    DCHECK(ok) << "append_strings_overflow failed";
+    RETURN_IF(!ok, Status::InternalError("BinaryDictPageDecoder::next_batch failed"));
     return Status::OK();
 }
 
