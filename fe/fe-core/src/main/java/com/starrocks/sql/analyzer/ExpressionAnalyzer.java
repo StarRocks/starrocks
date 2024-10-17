bcdea9de54bd6ced277bf1c26c72bb56e6d04bb1
before-Sunrise <71162020+before-Sunrise@users.noreply.github.com>
2024-10-17 10:38:36 +0800

    [BugFix] fix .net can not read sr (backport #51946) (#51961)
    Signed-off-by: before-Sunrise <unclejyj@gmail.com>
    (cherry picked from commit 0d22be523b7620cf9117f850060bb560002d0b23)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/sql/analyzer/ExpressionAnalyzer.java

--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/ExpressionAnalyzer.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/ExpressionAnalyzer.java
@@ -1661,6 +1661,7 @@ public class ExpressionAnalyzer {
         @Override
         public Void visitVariableExpr(VariableExpr node, Scope context) {
             try {
+<<<<<<< HEAD
                 if (node.getSetType() != null && node.getSetType().equals(SetType.USER)) {
                     UserVariable userVariable = session.getUserVariables(node.getName());
                     // If referring to an uninitialized variable, its value is NULL and a string type.
@@ -1685,6 +1686,17 @@ public class ExpressionAnalyzer {
                         node.setType(Type.VARCHAR);
                         node.setValue(SqlModeHelper.decode((long) node.getValue()));
                     }
+=======
+                VariableMgr.fillValue(session.getSessionVariable(), node);
+                if (!Strings.isNullOrEmpty(node.getName()) &&
+                        node.getName().equalsIgnoreCase(SessionVariable.SQL_MODE)) {
+                    node.setType(Type.VARCHAR);
+                    node.setValue(SqlModeHelper.decode((long) node.getValue()));
+                } else if (!Strings.isNullOrEmpty(node.getName()) &&
+                        node.getName().equalsIgnoreCase(SessionVariable.AUTO_COMMIT)) {
+                    node.setType(Type.BIGINT);
+                    node.setValue(((boolean) node.getValue()) ? (long) (1) : (long) 0);
+>>>>>>> 0d22be523b ([BugFix] fix .net can not read sr (backport #51946) (#51961))
                 }
             } catch (DdlException e) {
                 throw new SemanticException(e.getMessage());
