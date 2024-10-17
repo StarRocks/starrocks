65bd79042bd52132a7baa14defa5f70d8ef42996
eyes_on_me <nopainnofame@sina.com>
2024-10-17 10:39:08 +0800

    [BugFix] fix asan crash in array_map (#51966)
    Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>
    Signed-off-by: zhiminr.ren <1240388654@qq.com>

--- be/src/exprs/expr.h
+++ be/src/exprs/expr.h
@@ -149,6 +149,8 @@ public:
 
     virtual int get_subfields(std::vector<std::vector<std::string>>* subfields) const;
 
+    virtual void for_each_slot_id(const std::function<void(SlotId)>& cb) const;
+
     /// Create expression tree from the list of nodes contained in texpr within 'pool'.
     /// Returns the root of expression tree in 'expr' and the corresponding ExprContext in
     /// 'ctx'.
