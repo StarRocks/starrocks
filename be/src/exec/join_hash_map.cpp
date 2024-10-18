237e75514d726ff6d0d565fa3f6579f324918dfb
stdpain <34912776+stdpain@users.noreply.github.com>
2024-10-18 18:45:28 +0800

    [Enhancement] add default hash function and dump query resource usage (#52080)
    Signed-off-by: stdpain <drfeng08@gmail.com>
    (cherry picked from commit 22a1f917f87e60dafd3ccb49013a2a9c90a52313)
    
    # Conflicts:
    #	be/src/column/column_hash.h
    #	be/src/exec/join_hash_map.cpp
    #	be/src/exprs/ngram.cpp

--- be/src/exec/join_hash_map.cpp
+++ be/src/exec/join_hash_map.cpp
@@ -27,6 +27,55 @@
 
 namespace starrocks {
 
+<<<<<<< HEAD
+=======
+// if the same hash values are clustered, after the first probe, all related hash buckets are cached, without too many
+// misses. So check time locality of probe keys here.
+void HashTableProbeState::consider_probe_time_locality() {
+    if (active_coroutines > 0) {
+        // redo decision
+        if ((probe_chunks & (detect_step - 1)) == 0) {
+            int window_size = std::min(active_coroutines * 4, 50);
+            if (probe_row_count > window_size) {
+                phmap::flat_hash_map<uint32_t, uint32_t, StdHash<uint32_t>> occurrence;
+                occurrence.reserve(probe_row_count);
+                uint32_t unique_size = 0;
+                bool enable_interleaving = true;
+                uint32_t target = probe_row_count >> 3;
+                for (auto i = 0; i < probe_row_count; i++) {
+                    if (occurrence[next[i]] == 0) {
+                        ++unique_size;
+                        if (unique_size >= target) {
+                            break;
+                        }
+                    }
+                    occurrence[next[i]]++;
+                    if (i >= window_size) {
+                        occurrence[next[i - window_size]]--;
+                    }
+                }
+                if (unique_size < target) {
+                    active_coroutines = 0;
+                    enable_interleaving = false;
+                }
+                // enlarge step if the decision is the same, otherwise reduce it
+                if (enable_interleaving == last_enable_interleaving) {
+                    detect_step = detect_step >= 1024 ? detect_step : (detect_step << 1);
+                } else {
+                    last_enable_interleaving = enable_interleaving;
+                    detect_step = 1;
+                }
+            } else {
+                active_coroutines = 0;
+            }
+        } else if (!last_enable_interleaving) {
+            active_coroutines = 0;
+        }
+    }
+    ++probe_chunks;
+}
+
+>>>>>>> 22a1f917f8 ([Enhancement] add default hash function and dump query resource usage (#52080))
 void SerializedJoinBuildFunc::prepare(RuntimeState* state, JoinHashTableItems* table_items) {
     table_items->bucket_size = JoinHashMapHelper::calc_bucket_size(table_items->row_count + 1);
     table_items->first.resize(table_items->bucket_size, 0);
