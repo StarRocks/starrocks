420879b8b6eec1744f6be857be6f2a9061c12bb4
meegoo <meegoo.sr@gmail.com>
2024-10-22 09:26:22 -0700

    Update LocalMetastore.java
    Signed-off-by: meegoo <meegoo.sr@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
+++ fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
@@ -4971,21 +4971,13 @@ public class LocalMetastore implements ConnectorMetadata, MVRepairHandler, Memor
 
     public void onErasePartition(Partition partition) {
         // remove tablet in inverted index
-<<<<<<< HEAD
         TabletInvertedIndex invertedIndex = GlobalStateMgr.getCurrentInvertedIndex();
-        for (MaterializedIndex index : partition.getMaterializedIndices(MaterializedIndex.IndexExtState.ALL)) {
-            for (Tablet tablet : index.getTablets()) {
-                long tabletId = tablet.getId();
-                invertedIndex.deleteTablet(tabletId);
-=======
-        TabletInvertedIndex invertedIndex = GlobalStateMgr.getCurrentState().getTabletInvertedIndex();
         for (PhysicalPartition subPartition : partition.getSubPartitions()) {
             for (MaterializedIndex index : subPartition.getMaterializedIndices(MaterializedIndex.IndexExtState.ALL)) {
                 for (Tablet tablet : index.getTablets()) {
                     long tabletId = tablet.getId();
                     invertedIndex.deleteTablet(tabletId);
                 }
->>>>>>> 609614df99 ([BugFix] Fix physical partition for rebalance (#46402))
             }
         }
     }
