a6a6d3d4a7d24804e8629d3d6ca1927f7bd70d98
Felix Li <astralidea@163.com>
2024-10-21 19:16:05 -0700

    [Enhancement] Fix known XSS vulnerabilities (#52119)
    Signed-off-by: Astralidea <astralidea@163.com>
    Signed-off-by: zhiminr.ren <1240388654@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/http/action/QueryProfileAction.java
+++ fe/fe-core/src/main/java/com/starrocks/http/action/QueryProfileAction.java
@@ -42,9 +42,9 @@ import com.starrocks.http.BaseResponse;
 import com.starrocks.http.IllegalArgException;
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpResponseStatus;
-import org.apache.commons.text.StringEscapeUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.owasp.encoder.Encode;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -75,7 +75,7 @@ public class QueryProfileAction extends WebBaseAction {
         }
 
         // HTML encode the queryId to prevent XSS
-        String encodedQueryId = StringEscapeUtils.escapeHtml4(queryId);
+        String encodedQueryId = Encode.forHtml(queryId);
         String queryProfileStr = ProfileManager.getInstance().getProfile(queryId);
         if (queryProfileStr != null) {
             appendCopyButton(response.getContent());
