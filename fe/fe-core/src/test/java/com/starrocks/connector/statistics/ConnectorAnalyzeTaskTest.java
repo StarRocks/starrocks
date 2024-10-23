f778ff569d5bc8968ddba9cad386d83a68530d10
Youngwb <yangwenbo_mailbox@163.com>
2024-10-23 19:56:57 +0800

    [Enhancement] skip trigger analyze when table not update (#52203)
    Signed-off-by: zhiminr.ren <1240388654@qq.com>

--- fe/fe-core/src/test/java/com/starrocks/connector/statistics/ConnectorAnalyzeTaskTest.java
+++ fe/fe-core/src/test/java/com/starrocks/connector/statistics/ConnectorAnalyzeTaskTest.java
@@ -22,19 +22,26 @@ import com.starrocks.qe.ConnectContext;
 import com.starrocks.server.GlobalStateMgr;
 import com.starrocks.sql.plan.ConnectorPlanTestBase;
 import com.starrocks.statistic.AnalyzeStatus;
+import com.starrocks.statistic.ColumnStatsMeta;
 import com.starrocks.statistic.ExternalAnalyzeStatus;
+import com.starrocks.statistic.ExternalBasicStatsMeta;
+import com.starrocks.statistic.ExternalFullStatisticsCollectJob;
+import com.starrocks.statistic.StatisticUtils;
 import com.starrocks.statistic.StatsConstants;
 import com.starrocks.utframe.UtFrameUtils;
 import io.trino.hive.$internal.org.apache.commons.lang3.tuple.Triple;
 import mockit.Mock;
 import mockit.MockUp;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 public class ConnectorAnalyzeTaskTest {
     private static ConnectContext ctx;
@@ -44,6 +51,12 @@ public class ConnectorAnalyzeTaskTest {
         UtFrameUtils.createMinStarRocksCluster();
         ctx = UtFrameUtils.createDefaultCtx();
         ConnectorPlanTestBase.mockHiveCatalog(ctx);
+    }
+
+    @After
+    public void tearDown() {
+        GlobalStateMgr.getCurrentState().getAnalyzeMgr().getAnalyzeStatusMap().clear();
+        GlobalStateMgr.getCurrentState().getAnalyzeMgr().getExternalBasicStatsMetaMap().clear();
     }
 
     @Test
@@ -77,16 +90,86 @@ public class ConnectorAnalyzeTaskTest {
 
         new MockUp<ConnectorAnalyzeTask>() {
             @Mock
-            private AnalyzeStatus executeAnalyze(ConnectContext statsConnectCtx, AnalyzeStatus analyzeStatus) {
+            private AnalyzeStatus executeAnalyze(ConnectContext statsConnectCtx, AnalyzeStatus analyzeStatus,
+                                                 Set<String> updatedPartitions) {
+                analyzeStatus.setStatus(StatsConstants.ScheduleStatus.FINISH);
                 return analyzeStatus;
+            }
+        };
+
+        new MockUp<StatisticUtils>() {
+            @Mock
+            public LocalDateTime getTableLastUpdateTime(Table table) {
+                return LocalDateTime.now().minusDays(1);
             }
         };
         // execute analyze when last analyze status is finish
         externalAnalyzeStatus.setStatus(StatsConstants.ScheduleStatus.FINISH);
+        // add ExternalBasicStatsMeta when analyze finish
+        ExternalBasicStatsMeta externalBasicStatsMeta = new ExternalBasicStatsMeta("hive0", "partitioned_db",
+                "orders", List.of("o_custkey", "o_orderstatus"), StatsConstants.AnalyzeType.FULL,
+                LocalDateTime.now(), Maps.newHashMap());
+        externalBasicStatsMeta.addColumnStatsMeta(new ColumnStatsMeta("o_custkey", StatsConstants.AnalyzeType.FULL,
+                LocalDateTime.now()));
+        externalBasicStatsMeta.addColumnStatsMeta(new ColumnStatsMeta("o_orderstatus", StatsConstants.AnalyzeType.FULL,
+                LocalDateTime.now()));
+
+        GlobalStateMgr.getCurrentState().getAnalyzeMgr().addExternalBasicStatsMeta(externalBasicStatsMeta);
+
         result = task1.run();
         Assert.assertTrue(result.isPresent());
         Assert.assertTrue(result.get() instanceof ExternalAnalyzeStatus);
         ExternalAnalyzeStatus externalAnalyzeStatusResult = (ExternalAnalyzeStatus) result.get();
         Assert.assertEquals(List.of("o_orderkey"), externalAnalyzeStatusResult.getColumns());
+    }
+
+    @Test
+    public void testTaskRunWithTableUpdate() {
+        Table table = GlobalStateMgr.getCurrentState().getMetadataMgr().getTable("hive0",
+                "partitioned_db", "orders");
+        String tableUUID = table.getUUID();
+
+        Triple<String, Database, Table> tableTriple = StatisticsUtils.getTableTripleByUUID(tableUUID);
+        ConnectorAnalyzeTask task1 = new ConnectorAnalyzeTask(tableTriple, Sets.newHashSet("o_orderkey", "o_custkey"));
+        new MockUp<ExternalFullStatisticsCollectJob>() {
+            @Mock
+            public void collect(ConnectContext context, AnalyzeStatus analyzeStatus) throws Exception {
+                // do nothing
+            }
+        };
+
+        Optional<AnalyzeStatus> result = task1.run();
+        Assert.assertTrue(result.isPresent());
+        Map<String, ColumnStatsMeta>  columnStatsMetaMap = GlobalStateMgr.getCurrentState().getAnalyzeMgr().
+                getExternalTableBasicStatsMeta("hive0", "partitioned_db", "orders").getColumnStatsMetaMap();
+        Assert.assertEquals(2, columnStatsMetaMap.size());
+        Assert.assertTrue(columnStatsMetaMap.containsKey("o_orderkey"));
+        Assert.assertTrue(columnStatsMetaMap.containsKey("o_custkey"));
+
+        new MockUp<StatisticUtils>() {
+            @Mock
+            public LocalDateTime getTableLastUpdateTime(Table table) {
+                return LocalDateTime.now().minusDays(1);
+            }
+        };
+        // table not update, skip analyze
+        task1 = new ConnectorAnalyzeTask(tableTriple, Sets.newHashSet("o_orderkey", "o_custkey"));
+        result = task1.run();
+        Assert.assertTrue(result.isEmpty());
+
+        // table update, analyze again
+        new MockUp<StatisticUtils>() {
+            @Mock
+            public LocalDateTime getTableLastUpdateTime(Table table) {
+                return LocalDateTime.now().plusMinutes(10);
+            }
+        };
+
+        task1 = new ConnectorAnalyzeTask(tableTriple, Sets.newHashSet("o_orderkey", "o_custkey"));
+        result = task1.run();
+        Assert.assertTrue(result.isPresent());
+        Assert.assertTrue(result.get() instanceof ExternalAnalyzeStatus);
+        ExternalAnalyzeStatus externalAnalyzeStatusResult = (ExternalAnalyzeStatus) result.get();
+        Assert.assertEquals(2, externalAnalyzeStatusResult.getColumns().size());
     }
 }
