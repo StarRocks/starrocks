c0b4cac7af602912a928a5ef2dd304ffa513cd42
xyllq999 <734432375@qq.com>
2024-10-23 11:08:13 +0800

    [BugFix]Avoid printing token in be log (#51728)
    Signed-off-by: xyllq999 <734432375@qq.com>
    
    (cherry picked from commit 94938ddd608c497e46049cc3bd6095203051e5ed)
    Signed-off-by: xyllq999 <734432375@qq.com>

---
+++ be/test/agent/heartbeat_server_test.cpp
@@ -0,0 +1,63 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "agent/heartbeat_server.h"
+
+#include "gen_cpp/Types_types.h"
+#include "gtest/gtest.h"
+
+namespace starrocks {
+
+TEST(HeartbeatServerTest, test_print_master_info_with_token_null) {
+    HeartbeatServer server;
+    TMasterInfo master_info;
+
+    master_info.network_address.__set_hostname("127.0.0.1");
+    master_info.network_address.__set_port(8080);
+    master_info.__set_cluster_id(12345);
+    master_info.__set_epoch(100);
+    master_info.__set_backend_ip("192.168.1.1");
+
+    std::string expected_output =
+            "TMasterInfo(network_address=TNetworkAddress(hostname=127.0.0.1, port=8080), "
+            "cluster_id=12345, epoch=100, token=<null>, backend_ip=192.168.1.1, "
+            "http_port=<null>, heartbeat_flags=<null>, backend_id=<null>, "
+            "min_active_txn_id=0, run_mode=<null>, disabled_disks=<null>, "
+            "decommissioned_disks=<null>, encrypted=<null>)";
+
+    EXPECT_EQ(server.print_master_info(master_info), expected_output);
+}
+
+TEST(HeartbeatServerTest, test_print_master_info_with_token_hidden) {
+    HeartbeatServer server;
+    TMasterInfo master_info;
+
+    master_info.network_address.__set_hostname("127.0.0.1");
+    master_info.network_address.__set_port(8080);
+    master_info.__set_cluster_id(12345);
+    master_info.__set_epoch(100);
+    master_info.__set_token("secret_token");
+    master_info.__set_backend_ip("192.168.1.1");
+
+    std::string expected_output =
+            "TMasterInfo(network_address=TNetworkAddress(hostname=127.0.0.1, port=8080), "
+            "cluster_id=12345, epoch=100, token=<hidden>, backend_ip=192.168.1.1, "
+            "http_port=<null>, heartbeat_flags=<null>, backend_id=<null>, "
+            "min_active_txn_id=0, run_mode=<null>, disabled_disks=<null>, "
+            "decommissioned_disks=<null>, encrypted=<null>)";
+
+    EXPECT_EQ(server.print_master_info(master_info), expected_output);
+}
+
+} // namespace starrocks
