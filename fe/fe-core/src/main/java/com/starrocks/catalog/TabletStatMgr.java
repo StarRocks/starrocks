11e999a75e79e27b7b49c4aa6796d6f5b2f5a2bf
shuming.li <ming.moriarty@gmail.com>
2024-10-23 10:32:02 +0800

    fix cherry pick conflicts
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/TabletStatMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/TabletStatMgr.java
@@ -116,7 +116,7 @@ public class TabletStatMgr extends FrontendDaemon {
                 }
 
                 // NOTE: calculate the row first with read lock, then update the stats with write lock
-                locker.lockTableWithIntensiveDbLock(db, table, LockType.READ);
+                locker.lockTableWithIntensiveDbLock(db, table.getId(), LockType.READ);
                 Map<Pair<Long, Long>, Long> indexRowCountMap = Maps.newHashMap();
                 try {
                     OlapTable olapTable = (OlapTable) table;
