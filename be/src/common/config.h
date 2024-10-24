beace21de0e7efd08dcc5e3d13eecd97fd732591
sevev <qiangzh95@gmail.com>
2024-10-24 11:27:07 +0800

    update
    Signed-off-by: sevev <qiangzh95@gmail.com>

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -1303,7 +1303,7 @@ CONF_mBool(enable_lake_compaction_use_partial_segments, "false");
 // chunk size used by lake compaction
 CONF_mInt32(lake_compaction_chunk_size, "4096");
 
-CONF_mBool(skip_schema_in_rowset_meta, "false");
+CONF_mBool(skip_schema_in_rowset_meta, "true");
 
 CONF_mInt32(max_committed_without_schema_rowset, "1000");
 
