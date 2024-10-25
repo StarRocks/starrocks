c88d61da63158eed42aae667e21624947bad2dc9
shuming.li <ming.moriarty@gmail.com>
2024-10-25 15:37:39 +0800

    [Enhancement] Support push down avg aggregate function in mv rewrite (#52288)
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>
    Signed-off-by: zhiminr.ren <1240388654@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/tree/pdagg/AggregatePushDownContext.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/tree/pdagg/AggregatePushDownContext.java
@@ -17,13 +17,20 @@ package com.starrocks.sql.optimizer.rule.tree.pdagg;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.starrocks.catalog.Function;
+import com.starrocks.catalog.FunctionSet;
+import com.starrocks.common.Pair;
+import com.starrocks.sql.optimizer.base.ColumnRefFactory;
 import com.starrocks.sql.optimizer.operator.logical.LogicalAggregationOperator;
 import com.starrocks.sql.optimizer.operator.scalar.CallOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ColumnRefOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ScalarOperator;
+import com.starrocks.sql.optimizer.operator.scalar.ScalarOperatorUtil;
 
 import java.util.List;
 import java.util.Map;
+
+import static com.starrocks.sql.optimizer.rule.transformation.materialization.common.AggregatePushDownUtils.createNewCallOperator;
 
 public class AggregatePushDownContext {
     public static final AggregatePushDownContext EMPTY = new AggregatePushDownContext();
@@ -42,6 +49,9 @@ public class AggregatePushDownContext {
     // Query's aggregate call operator to push down aggregate call operator mapping,
     // those two operators are not the same so record it to be used later.
     public final Map<ColumnRefOperator, CallOperator> aggColRefToPushDownAggMap = Maps.newHashMap();
+    // For avg function, split it into sum and count function, this map records the mapping from avg function to sum and
+    // count's column ref operator.
+    public final Map<CallOperator, Pair<ColumnRefOperator, ColumnRefOperator>> avgToSumCountMapping = Maps.newHashMap();
 
     public boolean hasWindow = false;
 
@@ -59,6 +69,34 @@ public class AggregatePushDownContext {
     public void setAggregator(LogicalAggregationOperator aggregator) {
         this.origAggregator = aggregator;
         this.aggregations.putAll(aggregator.getAggregations());
+        aggregator.getGroupingKeys().forEach(c -> groupBys.put(c, c));
+        this.pushPaths.clear();
+    }
+
+    /**
+     * Set the aggregator and create new column ref operator for avg function.
+     */
+    public void setAggregator(ColumnRefFactory columnRefFactory,
+                              LogicalAggregationOperator aggregator) {
+        this.origAggregator = aggregator;
+        final Map<ColumnRefOperator, CallOperator> aggregations = aggregator.getAggregations();
+        for (Map.Entry<ColumnRefOperator, CallOperator> e : aggregations.entrySet()) {
+            if (e.getValue().getFunction().functionName().equalsIgnoreCase(FunctionSet.AVG)) {
+                CallOperator agg = e.getValue();
+                // for avg function, split it into sum and count function and push them down below join.
+                Function sumFn = ScalarOperatorUtil.findSumFn(agg.getFunction().getArgs());
+                Pair<ColumnRefOperator, CallOperator> sumCallOp =
+                        createNewCallOperator(columnRefFactory, aggregations, sumFn, agg.getChildren());
+                this.aggregations.put(sumCallOp.first, sumCallOp.second);
+                Function countFn = ScalarOperatorUtil.findArithmeticFunction(agg.getFunction().getArgs(), FunctionSet.COUNT);
+                Pair<ColumnRefOperator, CallOperator> countCallOp = createNewCallOperator(columnRefFactory, aggregations,
+                        countFn, agg.getChildren());
+                this.aggregations.put(countCallOp.first, countCallOp.second);
+                this.avgToSumCountMapping.put(e.getValue(), Pair.create(sumCallOp.first, countCallOp.first));
+            } else {
+                this.aggregations.put(e.getKey(), e.getValue());
+            }
+        }
         aggregator.getGroupingKeys().forEach(c -> groupBys.put(c, c));
         this.pushPaths.clear();
     }
@@ -88,6 +126,8 @@ public class AggregatePushDownContext {
     public void combine(AggregatePushDownContext ctx) {
         aggToFinalAggMap.putAll(ctx.aggToFinalAggMap);
         aggColRefToPushDownAggMap.putAll(ctx.aggColRefToPushDownAggMap);
+        aggToPartialAggMap.putAll(ctx.aggToPartialAggMap);
+        avgToSumCountMapping.putAll(ctx.avgToSumCountMapping);
     }
 
     public boolean isRewrittenByEquivalent(CallOperator aggCall) {
