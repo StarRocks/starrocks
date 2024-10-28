74afd3f17f7074e6ba8355b9b1550f7573597c7d
luohaha <18810541851@163.com>
2024-10-28 14:00:10 +0800

    update
    Signed-off-by: luohaha <18810541851@163.com>

--- be/test/storage/lake/alter_tablet_meta_test.cpp
+++ be/test/storage/lake/alter_tablet_meta_test.cpp
@@ -500,7 +500,6 @@ TEST_F(AlterTabletMetaTest, test_alter_persistent_index_type) {
 
     auto write_data_fn = [&]() {
         int64_t txn_id = next_id();
-        auto tablet_id = _tablet_metadata->id();
         std::vector<int> k0{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22};
         std::vector<int> v0{2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 41, 44};
         auto c0 = Int32Column::create();
