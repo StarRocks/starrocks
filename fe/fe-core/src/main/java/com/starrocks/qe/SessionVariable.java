4de5325dc45f8b195bd017f6b318a9c95403a5e2
Yixin Luo <18810541851@163.com>
2024-10-28 19:36:32 +0800

    [Enhancement] use cloud native persistent index by default (backport #52209) (#52387)
    Signed-off-by: luohaha <18810541851@163.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
@@ -439,6 +439,8 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
     public static final String CONNECTOR_SINK_SPILL_MEM_LIMIT_THRESHOLD = "connector_sink_spill_mem_limit_threshold";
     public static final String PIPELINE_SINK_DOP = "pipeline_sink_dop";
     public static final String ENABLE_ADAPTIVE_SINK_DOP = "enable_adaptive_sink_dop";
+    public static final String ENABLE_CLOUD_NATIVE_PERSISTENT_INDEX_BY_DEFAULT =
+            "enable_cloud_native_persistent_index_by_default";
     public static final String RUNTIME_FILTER_SCAN_WAIT_TIME = "runtime_filter_scan_wait_time";
     public static final String RUNTIME_FILTER_ON_EXCHANGE_NODE = "runtime_filter_on_exchange_node";
     public static final String ENABLE_MULTI_COLUMNS_ON_GLOBAL_RUNTIME_FILTER =
@@ -1393,6 +1395,9 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
 
     @VariableMgr.VarAttr(name = ENABLE_ADAPTIVE_SINK_DOP)
     private boolean enableAdaptiveSinkDop = false;
+
+    @VariableMgr.VarAttr(name = ENABLE_CLOUD_NATIVE_PERSISTENT_INDEX_BY_DEFAULT, flag = VariableMgr.INVISIBLE)
+    private boolean enableCloudNativePersistentIndexByDefault = false;
 
     @VariableMgr.VarAttr(name = JOIN_IMPLEMENTATION_MODE_V2, alias = JOIN_IMPLEMENTATION_MODE)
     private String joinImplementationMode = "auto"; // auto, merge, hash, nestloop
@@ -2447,6 +2452,10 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
 
     public void setEnableAdaptiveSinkDop(boolean e) {
         this.enableAdaptiveSinkDop = e;
+    }
+
+    public boolean getEnableCloudNativePersistentIndexByDefault() {
+        return enableCloudNativePersistentIndexByDefault;
     }
 
     public long getMaxExecMemByte() {
