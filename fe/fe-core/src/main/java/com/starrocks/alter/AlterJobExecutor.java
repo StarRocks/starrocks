d4686455dc9e4663b04327869c5e7fd3e8f453bc
meegoo <meegoo.sr@gmail.com>
2024-10-27 19:29:26 -0700

    [BugFix] Fix change shadow partition replication num fail (#52355)
    Signed-off-by: meegoo <meegoo.sr@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/alter/AlterJobExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/alter/AlterJobExecutor.java
@@ -727,10 +727,6 @@ public class AlterJobExecutor implements AstVisitor<Void, ConnectContext> {
             Partition partition = olapTable.getPartition(partitionName);
             // 1. date property
 
-            if (partitionName.startsWith(ExpressionRangePartitionInfo.SHADOW_PARTITION_PREFIX)) {
-                continue;
-            }
-
             if (newDataProperty != null) {
                 // for storage_cooldown_ttl
                 if (periodDuration != null) {
