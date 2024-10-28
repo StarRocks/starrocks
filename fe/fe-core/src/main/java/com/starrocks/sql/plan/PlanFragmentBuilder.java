24ea21a8d5ef1ec9b07f77a8ad64c425f21338e7
wyb <wybb86@gmail.com>
2024-10-28 14:34:04 +0800

    [BugFix] Column name case insensitive check in files() column materialization (#52358)
    Signed-off-by: wyb <wybb86@gmail.com>
    Signed-off-by: zhiminr.ren <1240388654@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/plan/PlanFragmentBuilder.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/plan/PlanFragmentBuilder.java
@@ -3789,7 +3789,7 @@ public class PlanFragmentBuilder {
                     "FileScanNode", files, table.loadFileList().size(), warehouseId);
 
             Set<String> scanColumns = tupleDesc.getSlots().stream().map(SlotDescriptor::getColumn).map(Column::getName).collect(
-                    Collectors.toCollection(TreeSet::new));
+                    Collectors.toCollection(() -> new TreeSet<>(String.CASE_INSENSITIVE_ORDER)));
             List<BrokerFileGroup> fileGroups = new ArrayList<>();
             try {
                 BrokerFileGroup grp = new BrokerFileGroup(table, scanColumns);
