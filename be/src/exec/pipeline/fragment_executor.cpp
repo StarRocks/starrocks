e0a29b00375d9205f28638d5dd3518f286601cfe
fgump <smartlxh@gmail.com>
2024-11-01 10:58:25 +0800

    [Enhancement] Support Query cache in shared_data (#42982)
    Signed-off-by: smartlxh <smartlxh@gmail.com>
    Signed-off-by: zhiminr.ren <1240388654@qq.com>

--- be/src/exec/pipeline/fragment_executor.cpp
+++ be/src/exec/pipeline/fragment_executor.cpp
@@ -482,6 +482,10 @@ Status FragmentExecutor::_prepare_exec_plan(ExecEnv* exec_env, const UnifiedExec
         cache_param.force_populate = tcache_param.force_populate;
         cache_param.entry_max_bytes = tcache_param.entry_max_bytes;
         cache_param.entry_max_rows = tcache_param.entry_max_rows;
+        if (tcache_param.__isset.is_lake) {
+            cache_param.is_lake = tcache_param.is_lake;
+        }
+
         for (auto& [slot, remapped_slot] : tcache_param.slot_remapping) {
             cache_param.slot_remapping[slot] = remapped_slot;
             cache_param.reverse_slot_remapping[remapped_slot] = slot;
--- be/src/storage/lake/tablet.h
+++ be/src/storage/lake/tablet.h
@@ -140,6 +140,18 @@ public:
 
     StatusOr<bool> has_delete_predicates(int64_t version);
 
+    StatusOr<bool> has_delete_predicates(const Version& version) override {
+        for (int64_t current_version = version.first; current_version < version.second; current_version++) {
+            ASSIGN_OR_RETURN(auto metadata, get_metadata(current_version));
+            for (const auto& rowset : metadata->rowsets()) {
+                if (rowset.has_delete_predicate() && rowset.delete_predicate().version() >= version.first) {
+                    return true;
+                }
+            }
+        };
+        return false;
+    }
+
     UpdateManager* update_mgr() const { return _mgr->update_mgr(); }
 
     TabletManager* tablet_mgr() const { return _mgr; }
