61b44749f638af206f13e44cf7d7c05130af5386
immutablecodes <151606644+immutablecodes@users.noreply.github.com>
2024-11-01 10:16:54 +0800

    Update fe/fe-core/src/main/java/com/starrocks/server/CatalogMgr.java
    Co-authored-by: alvin <115669851+alvin-celerdata@users.noreply.github.com>
    Signed-off-by: immutablecodes <151606644+immutablecodes@users.noreply.github.com>

--- fe/fe-core/src/main/java/com/starrocks/server/CatalogMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/server/CatalogMgr.java
@@ -340,7 +340,7 @@ public class CatalogMgr {
             String catalogName = log.getCatalogName();
             Map<String, String> properties = log.getProperties();
             String serviceName = properties.get("ranger.plugin.hive.service.name");
-            if (serviceName == null || serviceName.isEmpty()) {
+            if (Strings.isNullOrEmpty(serviceName)) {
                 if (Config.access_control.equals("ranger")) {
                     Authorizer.getInstance().setAccessControl(catalogName, new RangerStarRocksAccessController());
                 } else {
