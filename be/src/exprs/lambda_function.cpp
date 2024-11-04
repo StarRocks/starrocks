2273c5199ff7824c3dd3ba9808047eca2a88cfdc
eyes_on_me <nopainnofame@sina.com>
2024-11-04 10:09:20 +0800

    [Enhancement] avoid to extract constant expr as common expr in array_map (#52541)
    Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>

--- be/src/exprs/lambda_function.cpp
+++ be/src/exprs/lambda_function.cpp
@@ -67,7 +67,7 @@ Status LambdaFunction::extract_outer_common_exprs(RuntimeState* state, Expr* exp
 
         RETURN_IF_ERROR(extract_outer_common_exprs(state, child, ctx));
         // if child is a slotref or a lambda function or a literal, we can't replace it.
-        if (child->is_slotref() || child->is_lambda_function() || child->is_literal()) {
+        if (child->is_slotref() || child->is_lambda_function() || child->is_literal() || child->is_constant()) {
             continue;
         }
 
