7e76bd2b855901423d2fcaac4bcee0d00644bf4b
xiangguangyxg <xiangguangyxg@gmail.com>
2024-11-04 11:04:34 +0800

    [Enhancement] Optimize upload and download for backup restore
    Signed-off-by: xiangguangyxg <xiangguangyxg@gmail.com>

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -132,6 +132,10 @@ CONF_Int32(update_schema_worker_count, "3");
 CONF_mInt32(upload_worker_count, "0");
 // The count of thread to download.
 CONF_mInt32(download_worker_count, "0");
+// The buffer size to upload.
+CONF_mInt32(upload_buffer_size, "4194304");
+// The buffer size to download.
+CONF_mInt32(download_buffer_size, "4194304");
 // The count of thread to make snapshot.
 CONF_mInt32(make_snapshot_worker_count, "5");
 // The count of thread to release snapshot.
