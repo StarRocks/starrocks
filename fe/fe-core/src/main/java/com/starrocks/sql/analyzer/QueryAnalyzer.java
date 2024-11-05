3edc14a0983ad5d44501db7f084b49a5a2352b86
srlch <111035020+srlch@users.noreply.github.com>
2024-11-05 18:21:26 +0800

    [BugFix] Fix incorrect execution plan when generated column rewrite in join relation if left table and right table has the same column name (#52584)
    Signed-off-by: srlch <linzichao@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/QueryAnalyzer.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/QueryAnalyzer.java
@@ -132,6 +132,9 @@ public class QueryAnalyzer {
         }
 
         public Void process(ParseNode node, Scope scope) {
+            if (session.getSessionVariable().isDisableGeneratedColumnRewrite()) {
+                return null;
+            }
             return node.accept(this, scope);
         }
 
