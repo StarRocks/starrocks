f8d23562a0a153bea080a77bba8c9f2d5dfa0b0a
stephen <91597003+stephen-shelby@users.noreply.github.com>
2024-11-05 19:25:21 +0800

    [Enhancement] push runtime in_filter through agg node (#52625)
    Signed-off-by: stephen <stephen5217@163.com>

--- be/src/exec/aggregate/aggregate_base_node.cpp
+++ be/src/exec/aggregate/aggregate_base_node.cpp
@@ -67,6 +67,24 @@ void AggregateBaseNode::close(RuntimeState* state) {
     ExecNode::close(state);
 }
 
+void AggregateBaseNode::push_down_tuple_slot_mappings(RuntimeState* state,
+                                                      const std::vector<TupleSlotMapping>& parent_mappings) {
+    _tuple_slot_mappings = parent_mappings;
+
+    DCHECK(_tuple_ids.size() == 1);
+    for (auto& expr_ctx : _group_by_expr_ctxs) {
+        if (expr_ctx->root()->is_slotref()) {
+            auto ref = dynamic_cast<ColumnRef*>(expr_ctx->root());
+            DCHECK(ref != nullptr);
+            _tuple_slot_mappings.emplace_back(ref->tuple_id(), ref->slot_id(), _tuple_ids[0], ref->slot_id());
+        }
+    }
+
+    for (auto& child : _children) {
+        child->push_down_tuple_slot_mappings(state, _tuple_slot_mappings);
+    }
+}
+
 void AggregateBaseNode::push_down_join_runtime_filter(RuntimeState* state, RuntimeFilterProbeCollector* collector) {
     // accept runtime filters from parent if possible.
     _runtime_filter_collector.push_down(state, id(), collector, _tuple_ids, _local_rf_waiting_set);
