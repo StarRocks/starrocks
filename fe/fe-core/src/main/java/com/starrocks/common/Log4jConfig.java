f2fe95e200301f6388453b71515fd41aab599beb
RyanZ <dirtysalt1987@gmail.com>
2024-11-08 10:51:21 +0800

    [Enhancement] support sys_log_warn_modules in fe conf (#52709)
    Signed-off-by: yanz <dirtysalt1987@gmail.com>
    (cherry picked from commit a40c00f130207a92a2e0b1de14541cc292a161ff)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/common/Config.java
    #	fe/fe-core/src/main/java/com/starrocks/common/Log4jConfig.java

--- fe/fe-core/src/main/java/com/starrocks/common/Log4jConfig.java
+++ fe/fe-core/src/main/java/com/starrocks/common/Log4jConfig.java
@@ -161,10 +161,14 @@ public class Log4jConfig extends XmlConfiguration {
             "    <Logger name=\"com.starrocks.thrift\" level=\"DEBUG\"> \n" +
             "      <AppenderRef ref=\"Sys\"/>\n" +
             "    </Logger>\n" +
+<<<<<<< HEAD
             "    <Logger name=\"org.apache.kafka\" level=\"WARN\"> \n" +
             "      <AppenderRef ref=\"SysWF\"/>\n" +
             "    </Logger>\n" +
             "    <!--REPLACED BY AUDIT AND VERBOSE MODULE NAMES-->\n" +
+=======
+            "<!--REPLACED BY AUDIT AND VERBOSE MODULE NAMES-->" +
+>>>>>>> a40c00f130 ([Enhancement] support sys_log_warn_modules in fe conf (#52709))
             "  </Loggers>\n" +
             "</Configuration>";
 
@@ -183,6 +187,121 @@ public class Log4jConfig extends XmlConfiguration {
     private static String[] auditModules;
     private static String[] dumpModules;
     private static String[] bigQueryModules;
+<<<<<<< HEAD
+=======
+    private static String[] internalModules;
+    private static boolean compressSysLog;
+    private static boolean compressAuditLog;
+    private static String[] warnModules;
+    private static String[] builtinWarnModules = {
+            "org.apache.kafka",
+            "org.apache.hudi",
+            "org.apache.hadoop.io.compress",
+    };
+
+    @VisibleForTesting
+    static String generateActiveLog4jXmlConfig() throws IOException {
+        Map<String, String> properties = Maps.newHashMap();
+
+        // sys log config
+        if (!DEBUG_LEVELS.contains(StringUtils.upperCase(sysLogLevel))) {
+            throw new IOException("sys_log_level config error");
+        }
+        properties.put("sys_log_dir", Config.sys_log_dir);
+        properties.put("sys_roll_maxsize", String.valueOf(Config.log_roll_size_mb));
+        properties.put("sys_roll_num", String.valueOf(Config.sys_log_roll_num));
+        properties.put("sys_log_delete_age", String.valueOf(Config.sys_log_delete_age));
+        properties.put("sys_log_level", sysLogLevel);
+        properties.put("sys_file_pattern", getIntervalPattern("sys_log_roll_interval", Config.sys_log_roll_interval));
+        properties.put("sys_file_postfix", compressSysLog ? ".gz" : "");
+        properties.put("audit_file_postfix", compressAuditLog ? ".gz" : "");
+
+        // audit log config
+        properties.put("audit_log_dir", Config.audit_log_dir);
+        properties.put("audit_roll_maxsize", String.valueOf(Config.log_roll_size_mb));
+        properties.put("audit_roll_num", String.valueOf(Config.audit_log_roll_num));
+        properties.put("audit_log_delete_age", String.valueOf(Config.audit_log_delete_age));
+        properties.put("audit_file_pattern",
+                getIntervalPattern("audit_log_roll_interval", Config.audit_log_roll_interval));
+
+        // dump log config
+        properties.put("dump_log_dir", Config.dump_log_dir);
+        properties.put("dump_roll_maxsize", String.valueOf(Config.log_roll_size_mb));
+        properties.put("dump_roll_num", String.valueOf(Config.dump_log_roll_num));
+        properties.put("dump_log_delete_age", String.valueOf(Config.dump_log_delete_age));
+        properties.put("dump_file_pattern",
+                getIntervalPattern("dump_log_roll_interval", Config.dump_log_roll_interval));
+
+        // big query log config
+        properties.put("big_query_log_dir", Config.big_query_log_dir);
+        properties.put("big_query_roll_maxsize", String.valueOf(Config.log_roll_size_mb));
+        properties.put("big_query_log_roll_num", String.valueOf(Config.big_query_log_roll_num));
+        properties.put("big_query_log_delete_age", String.valueOf(Config.big_query_log_delete_age));
+        properties.put("big_query_file_pattern",
+                getIntervalPattern("big_query_log_roll_interval", Config.big_query_log_roll_interval));
+
+        // profile log config
+        properties.put("profile_log_dir", Config.profile_log_dir);
+        properties.put("profile_log_roll_size_mb", String.valueOf(Config.profile_log_roll_size_mb));
+        properties.put("profile_log_roll_num", String.valueOf(Config.profile_log_roll_num));
+        properties.put("profile_log_delete_age", String.valueOf(Config.profile_log_delete_age));
+        properties.put("profile_file_pattern",
+                getIntervalPattern("profile_log_roll_interval", Config.profile_log_roll_interval));
+
+        // internal log config
+        properties.put("internal_log_dir", Config.internal_log_dir);
+        properties.put("internal_roll_maxsize", String.valueOf(Config.log_roll_size_mb));
+        properties.put("internal_log_roll_num", String.valueOf(Config.internal_log_roll_num));
+        properties.put("internal_log_delete_age", String.valueOf(Config.internal_log_delete_age));
+        properties.put("internal_file_pattern",
+                getIntervalPattern("big_query_log_roll_interval", Config.internal_log_roll_interval));
+
+        // appender layout
+        final String jsonLoggingConfValue = "json";
+        if (jsonLoggingConfValue.equalsIgnoreCase(Config.sys_log_format)) {
+            // json logging, use `'` and replace them to `"` in batch to avoid too many escapes
+            String jsonConfig =
+                    "{'@timestamp':{'$resolver':'timestamp','pattern':{'format':'yyyy-MM-dd HH:mm:ss.SSSXXX','timeZone':'UTC'}}," +
+                            "'level':{'$resolver':'level','field':'name'}," +
+                            "'thread.name':{'$resolver':'thread','field':'name'}," +
+                            "'thread.id':{'$resolver':'thread','field':'id'}," +
+                            "'line':{'$resolver':'source','field':'lineNumber'}," +
+                            "'file':{'$resolver':'source','field':'fileName'}," +
+                            "'method':{'$resolver':'source','field':'methodName'}," +
+                            "'message':{'$resolver':'message','stringfield':'true'}," +
+                            "'exception':{'$resolver':'exception','field':'stackTrace','stackTrace':{'stringified':true,'full':true}}}";
+            jsonConfig = jsonConfig.replace("'", "\"");
+            String jsonLayoutFormatter = "<JsonTemplateLayout maxStringLength=\"%d\" locationInfoEnabled=\"true\">\n" +
+                    "<EventTemplate><![CDATA[" + jsonConfig + "]]></EventTemplate>\n" + "</JsonTemplateLayout>";
+            String jsonLayoutDefault = String.format(jsonLayoutFormatter, Config.sys_log_json_max_string_length);
+            String jsonLayoutProfile = String.format(jsonLayoutFormatter, Config.sys_log_json_profile_max_string_length);
+            properties.put("syslog_default_layout", jsonLayoutDefault);
+            properties.put("syslog_warning_layout", jsonLayoutDefault);
+            properties.put("syslog_audit_layout", jsonLayoutDefault);
+            properties.put("syslog_dump_layout", jsonLayoutDefault);
+            properties.put("syslog_bigquery_layout", jsonLayoutDefault);
+            properties.put("syslog_profile_layout", jsonLayoutProfile);
+        } else {
+            // fallback to plaintext logging
+            properties.put("syslog_default_layout",
+                    "<PatternLayout charset=\"UTF-8\" pattern=\"%d{yyyy-MM-dd HH:mm:ss.SSSXXX} %p (%t|%tid) [%C{1}.%M():%L] %m%n\"/>");
+            properties.put("syslog_warning_layout",
+                    "<PatternLayout charset=\"UTF-8\" pattern=\"%d{yyyy-MM-dd HH:mm:ss.SSSXXX} %p (%t|%tid) [%C{1}.%M():%L] %m%n %ex\"/>");
+            properties.put("syslog_audit_layout",
+                    "<PatternLayout charset=\"UTF-8\" pattern=\"%d{yyyy-MM-dd HH:mm:ss.SSSXXX} [%c{1}] %m%n\"/>");
+            properties.put("syslog_dump_layout",
+                    "<PatternLayout charset=\"UTF-8\" pattern=\"%d{yyyy-MM-dd HH:mm:ss.SSSXXX} [%c{1}] %m%n\"/>");
+            properties.put("syslog_bigquery_layout",
+                    "<PatternLayout charset=\"UTF-8\" pattern=\"%d{yyyy-MM-dd HH:mm:ss.SSSXXX} [%c{1}] %m%n\"/>");
+            properties.put("syslog_profile_layout",
+                    "<PatternLayout charset=\"UTF-8\" pattern=\"%d{yyyy-MM-dd HH:mm:ss.SSSXXX} [%c{1}] %m%n\"/>");
+        }
+
+        String xmlConfTemplate = generateXmlConfTemplate();
+        strSub = new StrSubstitutor(new Interpolator(properties));
+        return strSub.replace(xmlConfTemplate);
+    }
+>>>>>>> a40c00f130 ([Enhancement] support sys_log_warn_modules in fe conf (#52709))
 
     private static void reconfig() throws IOException {
         String newXmlConfTemplate = xmlConfTemplateAppenders;
@@ -319,10 +438,56 @@ public class Log4jConfig extends XmlConfiguration {
         public final Y y;
         public final Z z;
 
+<<<<<<< HEAD
         public Tuple(X x, Y y, Z z) {
             this.x = x;
             this.y = y;
             this.z = z;
+=======
+        for (String s : internalModules) {
+            sb.append("    <Logger name='internal.").append(s).append("' level=\"INFO\" additivity=\"false\"> \n");
+            if (log2Console) {
+                sb.append("      <AppenderRef ref=\"ConsoleErr\"/>\n");
+            } else {
+                sb.append("      <AppenderRef ref=\"InternalFile\"/>\n");
+            }
+            sb.append("    </Logger>\n");
+        }
+
+        for (String s : verboseModules) {
+            sb.append("    <Logger name='").append(s).append("' level='DEBUG'/>\n");
+        }
+        for (String s : auditModules) {
+            sb.append("    <Logger name='audit.").append(s).append("' level='INFO'/>\n");
+        }
+        for (String s : dumpModules) {
+            sb.append("    <Logger name='dump.").append(s).append("' level='INFO'/>\n");
+        }
+        for (String s : bigQueryModules) {
+            sb.append("    <Logger name='big_query.").append(s).append("' level='INFO'/>\n");
+        }
+        for (String s : builtinWarnModules) {
+            sb.append("    <Logger name='").append(s).append("' level='WARN'><AppenderRef ref='SysWF'/></Logger>\n");
+        }
+        for (String s : warnModules) {
+            sb.append("    <Logger name='").append(s).append("' level='WARN'><AppenderRef ref='SysWF'/></Logger>\n");
+        }
+
+        String newXmlConfTemplate = APPENDER_TEMPLATE;
+        newXmlConfTemplate += log2Console ? CONSOLE_LOGGER_TEMPLATE : FILE_LOGGER_TEMPLATE;
+        newXmlConfTemplate = newXmlConfTemplate.replaceAll("<!--REPLACED BY AUDIT AND VERBOSE MODULE NAMES-->",
+                sb.toString());
+        return newXmlConfTemplate;
+    }
+
+    private static String getIntervalPattern(String name, String config) throws IOException {
+        if (config.equalsIgnoreCase("HOUR")) {
+            return "%d{yyyyMMddHH}";
+        } else if (config.equalsIgnoreCase("DAY")) {
+            return "%d{yyyyMMdd}";
+        } else {
+            throw new IOException(name + " config error: " + config);
+>>>>>>> a40c00f130 ([Enhancement] support sys_log_warn_modules in fe conf (#52709))
         }
     }
 
@@ -341,6 +506,13 @@ public class Log4jConfig extends XmlConfiguration {
         auditModules = Config.audit_log_modules;
         dumpModules = Config.dump_log_modules;
         bigQueryModules = Config.big_query_log_modules;
+<<<<<<< HEAD
+=======
+        internalModules = Config.internal_log_modules;
+        compressSysLog = Config.sys_log_enable_compress;
+        compressAuditLog = Config.audit_log_enable_compress;
+        warnModules = Config.sys_log_warn_modules;
+>>>>>>> a40c00f130 ([Enhancement] support sys_log_warn_modules in fe conf (#52709))
         reconfig();
     }
 
