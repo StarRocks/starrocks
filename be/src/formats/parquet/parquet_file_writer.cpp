0db1256831b763dfe76eeb443f1a607798462059
Smith Cruise <chendingchao1@126.com>
2024-11-11 14:45:15 +0800

    [Refactor] Refactor parquet reader create logic (#52667)
    Signed-off-by: Smith Cruise <chendingchao1@126.com>

--- be/src/formats/parquet/parquet_file_writer.cpp
+++ be/src/formats/parquet/parquet_file_writer.cpp
@@ -28,7 +28,6 @@
 #include <ostream>
 #include <utility>
 
-#include "column/vectorized_fwd.h"
 #include "formats/file_writer.h"
 #include "formats/parquet/arrow_memory_pool.h"
 #include "formats/parquet/chunk_writer.h"
@@ -361,8 +360,8 @@ arrow::Result<::parquet::schema::NodePtr> ParquetFileWriter::_make_schema_node(c
         }
         return ::parquet::schema::PrimitiveNode::Make(
                 name, rep_type, ::parquet::LogicalType::Decimal(type_desc.precision, type_desc.scale),
-                ::parquet::Type::FIXED_LEN_BYTE_ARRAY, parquet::decimal_precision_to_byte_count(type_desc.precision),
-                file_column_id.field_id);
+                ::parquet::Type::FIXED_LEN_BYTE_ARRAY,
+                parquet::ParquetUtils::decimal_precision_to_byte_count(type_desc.precision), file_column_id.field_id);
     }
     case TYPE_STRUCT: {
         DCHECK(type_desc.children.size() == type_desc.field_names.size());
