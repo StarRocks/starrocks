0427b2efb3ba9380c660972ba346a416710ff4fb
lufeizxy <zhangxianyang02@58.com>
2024-11-12 23:45:31 +0800

    fix: address code review feedback for inet_aton function
    Signed-off-by: lufeizxy <zhangxianyang02@58.com>

--- be/src/common/format_ip.h
+++ be/src/common/format_ip.h
@@ -14,90 +14,59 @@
 
 #pragma once
 
-#include <cstring>
 #include <cstdint>
-
+#include <cstring>
 
 constexpr size_t IPV4_BINARY_LENGTH = 4;
-constexpr int IPV4_MAX_OCTET_VALUE = 255;         //max value of octet
+constexpr int IPV4_MAX_OCTET_VALUE = 255; //max value of octet
 constexpr size_t IPV4_OCTET_BITS = 8;
 constexpr size_t DECIMAL_BASE = 10;
 
-namespace starrocks{
-
-enum class IPConvertExceptionMode : uint8_t {
-    Throw, Default, Null
-};
+namespace starrocks {
 
 inline bool is_numeric_ascii(char c) {
-    /// This is faster than
-    /// return UInt8(UInt8(c) - UInt8('0')) < UInt8(10);
-    /// on Intel CPUs when compiled by gcc 8.
-    return (c>='0' && c<='9');
+    // This is faster than
+    // return UInt8(UInt8(c) - UInt8('0')) < UInt8(10);
+    // on Intel CPUs when compiled by gcc 8.
+    return (c >= '0' && c <= '9');
 }
 
-template<typename T, typename EOFfunction>
-requires(std::is_same<typename std::remove_cv<T>::type, char>::value)
-inline bool parse_ipv4(T * &src, EOFfunction eof, unsigned char* dst, int64_t first_octet = -1) {
-    if (src == nullptr || first_octet > IPV4_MAX_OCTET_VALUE) {
+inline bool parse_ipv4(const char* pos, int64_t& dst, int64_t first_octet = -1) {
+    if (pos == nullptr || first_octet > IPV4_MAX_OCTET_VALUE) {
         return false;
     }
 
     int64_t result = 0;
     int offset = (IPV4_BINARY_LENGTH - 1) * IPV4_OCTET_BITS;
+
     if (first_octet >= 0) {
         result |= first_octet << offset;
         offset -= IPV4_OCTET_BITS;
     }
 
-    for (; true; offset -= IPV4_OCTET_BITS, ++src) {
-        if(eof()){
+    for (; true; offset -= IPV4_OCTET_BITS, ++pos) {
+        int64_t value = 0;
+        size_t len = 0;
+
+        while (is_numeric_ascii(*pos) && len < 3) {
+            value = value * DECIMAL_BASE + (*pos - '0');
+            ++len;
+            ++pos;
+        }
+
+        if (len == 0 || value > IPV4_MAX_OCTET_VALUE || (offset > 0 && *pos != '.')) {
             return false;
         }
 
-        int64_t value =0;
-        size_t len = 0;
-        while (is_numeric_ascii(*src) && len <= 3) {
-            value = value * DECIMAL_BASE + (*src - '0');
-            ++len;
-            ++src;
-            if (eof()) {
-                break;
-            }
-        }
-        if (len == 0 || value > IPV4_MAX_OCTET_VALUE || (offset > 0 && (eof() || *src != '.'))) {
-            return false;
-        }
         result |= value << offset;
+
         if (offset == 0) {
             break;
         }
     }
-    memcpy(dst, &result, sizeof(result));
+
+    dst = result;
     return true;
 }
 
-inline const char* parse_ipv4(const char* src, const char* end, unsigned char* dst) {
-    if (parse_ipv4(src, [&src,end]() { return src == end; }, dst)) {
-        return src;
-    }
-    return nullptr;
-}
-
-//return true if whole buffer was parsed successfully
-inline bool parse_ipv4_whole(const char* src, const char* end, unsigned char* dst) {
-    return parse_ipv4(src, end, dst) == end;
-}
-
-inline const char* parse_ipv4(const char* src, unsigned char* dst) {
-    if (parse_ipv4(src, []() { return false; }, dst)) {
-        return src;
-    }
-    return nullptr;
-}
-
-inline bool parse_ipv4_whole(const char* src, unsigned char* dst) {
-    const char* end = parse_ipv4(src, dst);
-    return end != nullptr && *end == '\0';
-}
-}// namespace starrocks
+} // namespace starrocks
--- be/src/exprs/inet_aton.cpp
+++ be/src/exprs/inet_aton.cpp
@@ -12,103 +12,37 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "exprs/string_functions.h"
-
-#include <algorithm>
-
-#include "column/array_column.h"
-#include "column/binary_column.h"
-#include "column/column_viewer.h"
 #include "common/format_ip.h"
+#include "exprs/string_functions.h"
 
 namespace starrocks {
 
-    static inline bool try_parse_ipv4(const char* pos, int64& result_value) {
-        return parse_ipv4_whole(pos, reinterpret_cast<unsigned char*>(&result_value));
+static inline bool try_parse_ipv4(const char* pos, int64& result_value) {
+    return parse_ipv4(pos, result_value);
+}
+
+StatusOr<ColumnPtr> StringFunctions::inet_aton(FunctionContext* context, const Columns& columns) {
+    RETURN_IF_COLUMNS_ONLY_NULL(columns);
+
+    auto str_viewer = ColumnViewer<TYPE_VARCHAR>(columns[0]);
+    auto size = columns[0]->size();
+
+    ColumnBuilder<TYPE_BIGINT> result(size);
+    for (int row = 0; row < size; row++) {
+        if (str_viewer.is_null(row)) {
+            result.append_null();
+            continue;
+        }
+
+        auto str_value = str_viewer.value(row);
+        int64_t parsed_result;
+        if (try_parse_ipv4(str_value.get_data(), parsed_result)) {
+            result.append(parsed_result);
+        } else {
+            result.append_null();
+        }
     }
+    return result.build(ColumnHelper::is_all_const(columns));
+}
 
-    template <IPConvertExceptionMode exception_mode, typename ToColumn>
-    static ColumnPtr convert_to_ipv4(const ColumnPtr haystack_ptr, const UInt8Column::Container* null_map = nullptr) {
-
-        auto* column_binary = down_cast<BinaryColumn*>(ColumnHelper::get_data_column(haystack_ptr.get()));
-        if (!column_binary) {
-            throw std::invalid_argument("Illegal column type, expected Binary or String");
-        }
-
-        size_t column_size = column_binary->size();
-
-        // Prepare for nullable handling
-        UInt8Column::Ptr col_null_map_to;
-        UInt8Column::Container * vec_null_map_to = nullptr;
-
-        if constexpr (exception_mode == IPConvertExceptionMode::Null) {
-            col_null_map_to = UInt8Column::create(column_size, false);
-            vec_null_map_to = &col_null_map_to->get_data();
-        }
-
-        auto col_res = ToColumn::create();
-        auto& vec_res = col_res->get_data();
-        vec_res.resize(column_size);
-
-        // Fetch underlying binary data
-        const auto& vec_src = column_binary->get_bytes();
-        const auto& offsets_src = column_binary->get_offset();
-        size_t prev_offset = 0;
-
-        for (size_t i = 0; i < vec_res.size(); ++i) {
-            if (null_map != nullptr && i < null_map->size() && (*null_map)[i]) {
-                if constexpr (exception_mode == IPConvertExceptionMode::Throw) {
-                    throw std::invalid_argument(
-                            "Null Input, you may consider convert it to a valid default IPv4 value "
-                            "like '0.0.0.0' first");
-                }
-                vec_res[i] = 0;
-                prev_offset = offsets_src[i];
-                if constexpr (exception_mode == IPConvertExceptionMode::Null) {
-                    (*vec_null_map_to)[i] = true;
-                }
-                continue;
-            }
-
-            const char* src_start = reinterpret_cast<const char*>(&vec_src[prev_offset]);
-            size_t src_length = (i < vec_res.size() - 1) ? (offsets_src[i+1] - prev_offset)
-                                                         : (vec_src.size() - prev_offset);
-
-            std::string src(src_start, src_length);
-            bool parse_result = try_parse_ipv4(src.c_str(), vec_res[i]);
-
-            if (!parse_result) {
-                if constexpr (exception_mode == IPConvertExceptionMode::Throw) {
-                    throw std::invalid_argument("Invalid IPv4 value");
-                } else if constexpr (exception_mode == IPConvertExceptionMode::Default) {
-                    vec_res[i] = 0;
-                } else if constexpr (exception_mode == IPConvertExceptionMode::Null) {
-                    (*vec_null_map_to)[i] = true;
-                    vec_res[i] = 0;
-                }
-            }
-
-            prev_offset = offsets_src[i] + src_length;
-        }
-
-        if constexpr (exception_mode == IPConvertExceptionMode::Null) {
-            return NullableColumn::create(std::move(col_res), std::move(col_null_map_to));
-        }
-        return col_res;
-    }
-
-    StatusOr<ColumnPtr> StringFunctions::inet_aton(FunctionContext* context, const Columns& columns) {
-        RETURN_IF_COLUMNS_ONLY_NULL(columns);
-
-        ColumnPtr haystack = columns[0];
-        const UInt8Column::Container* null_map = nullptr;
-
-        if (haystack->is_nullable()) {
-            const auto* column_nullable = down_cast<const NullableColumn*>(haystack.get());
-            haystack = column_nullable->data_column();
-            null_map = &column_nullable->null_column()->get_data();
-        }
-
-        return convert_to_ipv4<IPConvertExceptionMode::Null, Int64Column>(haystack, null_map);
-    }
-}// namespace starrocks
+} // namespace starrocks
