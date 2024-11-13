fd72e3eb380461ff59f0c2284620f0894b21e19a
crossoverJie <crossoverJie@gmail.com>
2024-11-13 13:52:49 +0800

    [BugFix] Fix drop partition in different db could not refresh MV (#52618)
    Signed-off-by: crossoverJie <crossoverJie@gmail.com>
    (cherry picked from commit addcaa4edddea16b3eb89b0045636e5ef59c2878)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java

--- fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
+++ fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
@@ -1467,10 +1467,15 @@ public class LocalMetastore implements ConnectorMetadata, MVRepairHandler, Memor
             }
             try {
                 for (MvId mvId : olapTable.getRelatedMaterializedViews()) {
+<<<<<<< HEAD
                     MaterializedView materializedView = (MaterializedView) db.getTable(mvId.getId());
+=======
+                    MaterializedView materializedView = (MaterializedView) getTable(mvId.getDbId(), mvId.getId());
+>>>>>>> addcaa4edd ([BugFix] Fix drop partition in different db could not refresh MV (#52618))
                     if (materializedView != null && materializedView.isLoadTriggeredRefresh()) {
+                        Database mvDb = getDb(mvId.getDbId());
                         GlobalStateMgr.getCurrentState().getLocalMetastore().refreshMaterializedView(
-                                db.getFullName(), materializedView.getName(), false, null,
+                                mvDb.getFullName(), materializedView.getName(), false, null,
                                 Constants.TaskRunPriority.NORMAL.value(), true, false);
                     }
                 }
