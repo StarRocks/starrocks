19163e61c61533322aeb060633b7dee568608d63
starrocks-xupeng <xupeng@starrocks.com>
2024-11-15 16:32:13 +0800

    [Enhancement] update staros to 3.4-rc1 (#52901)
    Signed-off-by: starrocks-xupeng <xupeng@starrocks.com>

--- thirdparty/vars-ubuntu22-aarch64.sh
+++ thirdparty/vars-ubuntu22-aarch64.sh
@@ -22,7 +22,7 @@
 #####################################################
 
 # starcache
-STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v3.3.3/starcache-ubuntu22_arm64.tar.gz"
+STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v3.4.0-rc01/starcache-ubuntu22_arm64.tar.gz"
 STARCACHE_NAME="starcache.tar.gz"
 STARCACHE_SOURCE="starcache"
-STARCACHE_MD5SUM="0752ac974552a1caadd7ab6ce97878f9"
+STARCACHE_MD5SUM="494ae85999217ec6982f45d71c53a59b"
