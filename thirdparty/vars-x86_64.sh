a374cb50f480324a1f5511b3c9c2c38b4f7680c4
starrocks-xupeng <xupeng@starrocks.com>
2024-11-15 16:32:13 +0800

    [Enhancement] update staros to 3.4-rc1 (#52901)
    Signed-off-by: starrocks-xupeng <xupeng@starrocks.com>

--- thirdparty/vars-x86_64.sh
+++ thirdparty/vars-x86_64.sh
@@ -52,7 +52,7 @@ JINDOSDK_SOURCE="jindosdk-4.6.8-linux"
 JINDOSDK_MD5SUM="5436e4fe39c4dfdc942e41821f1dd8a9"
 
 # starcache
-STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v3.3.3/starcache-centos7_amd64.tar.gz"
+STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v3.4.0-rc01/starcache-centos7_amd64.tar.gz"
 STARCACHE_NAME="starcache.tar.gz"
 STARCACHE_SOURCE="starcache"
-STARCACHE_MD5SUM="595d1b5d0c7935e1d1f91bf43059e461"
+STARCACHE_MD5SUM="f4a08a1911f5dcfbbd1ef68d765cb7d2"
