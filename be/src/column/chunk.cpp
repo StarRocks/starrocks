bc48e2be02a9d2cb212b2f360d7015a75a980802
before-Sunrise <unclejyj@gmail.com>
2024-11-15 14:49:23 +0800

    defence
    Signed-off-by: before-Sunrise <unclejyj@gmail.com>

--- be/src/column/chunk.cpp
+++ be/src/column/chunk.cpp
@@ -143,6 +143,9 @@ void Chunk::append_vector_column(ColumnPtr column, const FieldPtr& field, SlotId
 
 void Chunk::append_column(ColumnPtr column, SlotId slot_id) {
     DCHECK(!_slot_id_to_index.contains(slot_id)) << "slot_id:" + std::to_string(slot_id) << std::endl;
+    if (UNLIKELY(_slot_id_to_index.contains(slot_id))) {
+        throw std::runtime_error(fmt::format("slot_id {} already exists", slot_id));
+    }
     _slot_id_to_index[slot_id] = _columns.size();
     _columns.emplace_back(std::move(column));
     check_or_die();
