0d33e4b64e2dba4c8bf98d75dd49ccf0d16b1c3d
srlch <linzichao@starrocks.com>
2024-11-18 10:59:36 +0800

    [Enhancement] adjust drop_tablet_worker_count to half of the core number
    Signed-off-by: srlch <linzichao@starrocks.com>

--- be/src/agent/agent_server.cpp
+++ be/src/agent/agent_server.cpp
@@ -199,8 +199,9 @@ void AgentServer::Impl::init_or_die() {
                                        _thread_pool_publish_version);
         REGISTER_THREAD_POOL_METRICS(publish_version, _thread_pool_publish_version);
 #endif
-
-        BUILD_DYNAMIC_TASK_THREAD_POOL("drop", 1, config::drop_tablet_worker_count, std::numeric_limits<int>::max(),
+        int32_t adjust_drop_tablet_worker_count = config::drop_tablet_worker_count;
+        adjust_drop_tablet_worker_count = (adjust_drop_tablet_worker_count < 0) ? -adjust_drop_tablet_worker_count : adjust_drop_tablet_worker_count;
+        BUILD_DYNAMIC_TASK_THREAD_POOL("drop", 1, (int32_t) (calc_real_num_threads(adjust_drop_tablet_worker_count) / 2), std::numeric_limits<int>::max(),
                                        _thread_pool_drop);
 
         BUILD_DYNAMIC_TASK_THREAD_POOL("create_tablet", 1, config::create_tablet_worker_count,
