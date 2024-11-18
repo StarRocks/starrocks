566a2cbcf56588b63116f358cff2c7cd98a1b34e
srlch <linzichao@starrocks.com>
2024-11-18 15:08:43 +0800

    fix
    Signed-off-by: srlch <linzichao@starrocks.com>

--- be/src/agent/agent_server.cpp
+++ be/src/agent/agent_server.cpp
@@ -199,10 +199,11 @@ void AgentServer::Impl::init_or_die() {
                                        _thread_pool_publish_version);
         REGISTER_THREAD_POOL_METRICS(publish_version, _thread_pool_publish_version);
 #endif
-        int real_drop_tablet_worker_count = (config::drop_tablet_worker_count != 0) ? std::abs(config::drop_tablet_worker_count)
-                                                                                    : (int32_t)(CpuInfo::num_cores() / 2);
-        BUILD_DYNAMIC_TASK_THREAD_POOL("drop", 1, real_drop_tablet_worker_count,
-                                       std::numeric_limits<int>::max(), _thread_pool_drop);
+        int real_drop_tablet_worker_count = (config::drop_tablet_worker_count != 0)
+                                                    ? std::abs(config::drop_tablet_worker_count)
+                                                    : (int32_t)(CpuInfo::num_cores() / 2);
+        BUILD_DYNAMIC_TASK_THREAD_POOL("drop", 1, real_drop_tablet_worker_count, std::numeric_limits<int>::max(),
+                                       _thread_pool_drop);
 
         BUILD_DYNAMIC_TASK_THREAD_POOL("create_tablet", 1, config::create_tablet_worker_count,
                                        std::numeric_limits<int>::max(), _thread_pool_create_tablet);
