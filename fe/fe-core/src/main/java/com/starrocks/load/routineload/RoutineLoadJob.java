cd9a7f89e5fe5d2a4a5648b5e792d510899c494b
hoffer <meihaifeng.hust@gmail.com>
2024-11-20 11:13:43 +0800

    [BugFix] Fix routine load job change to cancelled state when txn expired (#50334)
    Signed-off-by: hoffermei <meihaifeng.hust@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/load/routineload/RoutineLoadJob.java
+++ fe/fe-core/src/main/java/com/starrocks/load/routineload/RoutineLoadJob.java
@@ -915,7 +915,7 @@ public abstract class RoutineLoadJob extends AbstractTxnStateChangeCallback
             TransactionState txnState =
                     GlobalStateMgr.getCurrentState().getGlobalTransactionMgr().getTransactionState(db.getId(), txnId);
             if (txnState == null) {
-                throw new MetaNotFoundException("txn does not exist: " + txnId);
+                throw new UserException("txn does not exist: " + txnId);
             }
             txnState.addTableIndexes(planner.getDestTable());
 
