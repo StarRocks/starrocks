24b50ce449237ca7b5e78a11aad5520805db2398
Kevin Cai <caixiaohua@starrocks.com>
2024-11-20 09:32:37 +0800

    [BugFix] fix 100-continue compatibility issue for HTTP/1.0 (#53008)
    Signed-off-by: Kevin Xiaohua Cai <caixiaohua@starrocks.com>
    (cherry picked from commit bf78f6e38df36e607a435dbb4cd4685b71dc3ba2)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/http/rest/LoadAction.java
    #	fe/fe-core/src/test/java/com/starrocks/http/LoadActionTest.java

--- fe/fe-core/src/test/java/com/starrocks/http/LoadActionTest.java
+++ fe/fe-core/src/test/java/com/starrocks/http/LoadActionTest.java
@@ -14,6 +14,7 @@
 
 package com.starrocks.http;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.google.common.collect.Multimap;
 import com.starrocks.system.NodeSelector;
 import io.netty.handler.codec.http.HttpHeaderNames;
@@ -22,10 +23,12 @@ import io.netty.handler.codec.http.HttpResponseStatus;
 import mockit.Mock;
 import mockit.MockUp;
 import org.apache.http.HttpHeaders;
+import org.apache.http.ProtocolVersion;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultRedirectStrategy;
 import org.apache.http.impl.client.HttpClients;
@@ -37,9 +40,88 @@ import java.util.Arrays;
 import java.util.List;
 
 public class LoadActionTest extends StarRocksHttpTestCase {
+<<<<<<< HEAD
     private HttpPut buildPutRequest(int bodyLength) {
+=======
+
+    private OkHttpClient noRedirectClient = new OkHttpClient.Builder()
+            .readTimeout(100, TimeUnit.SECONDS)
+            .followRedirects(false)
+            .build();
+
+    @Test
+    public void testBatchWriteStreamLoadSuccess() throws Exception {
+        Map<String, String> map = new HashMap<>();
+        map.put(HTTP_ENABLE_BATCH_WRITE, "true");
+        Request request = buildRequest(map);
+        List<ComputeNode> computeNodes = new ArrayList<>();
+        List<String> redirectLocations = new ArrayList<>();
+        for (int i = 1; i <= 3; i++) {
+            String host = "192.0.0." + i;
+            int httpPort = 8040;
+            computeNodes.add(new ComputeNode(i, host, 9050));
+            computeNodes.get(i - 1).setHttpPort(httpPort);
+            redirectLocations.add(getLoadUrl(host, httpPort));
+        }
+
+        new MockUp<BatchWriteMgr>() {
+            @Mock
+            public RequestCoordinatorBackendResult requestCoordinatorBackends(TableId tableId, StreamLoadKvParams params) {
+                return new RequestCoordinatorBackendResult(new TStatus(TStatusCode.OK), computeNodes);
+            }
+        };
+
+        try (Response response = noRedirectClient.newCall(request).execute()) {
+            assertEquals(307, response.code());
+            String location = response.header("Location");
+            assertTrue(redirectLocations.contains(location));
+        }
+    }
+
+    @Test
+    public void testBatchWriteStreamLoadFailure() throws Exception {
+        Map<String, String> map = new HashMap<>();
+        map.put(HTTP_ENABLE_BATCH_WRITE, "true");
+        Request request = buildRequest(map);
+
+        new MockUp<BatchWriteMgr>() {
+            @Mock
+            public RequestCoordinatorBackendResult requestCoordinatorBackends(TableId tableId, StreamLoadKvParams params) {
+                TStatus status = new TStatus();
+                status.setStatus_code(TStatusCode.INTERNAL_ERROR);
+                status.addToError_msgs("artificial failure");
+                return new RequestCoordinatorBackendResult(status, null);
+            }
+        };
+
+        try (Response response = noRedirectClient.newCall(request).execute()) {
+            assertEquals(200, response.code());
+            Map<String, Object> result = parseResponseBody(response);
+            assertEquals("INTERNAL_ERROR", result.get("code"));
+            assertEquals("FAILED", result.get("status"));
+            assertEquals("artificial failure", result.get("message"));
+            assertEquals("artificial failure", result.get("msg"));
+        }
+    }
+
+    private Request buildRequest(Map<String, String> headers) {
+        Request.Builder builder = new Request.Builder();
+        builder.addHeader("Authorization", rootAuth);
+        builder.addHeader("Expect", "100-continue");
+        for (Map.Entry<String, String> entry : headers.entrySet()) {
+            builder.addHeader(entry.getKey(), entry.getValue());
+        }
+        builder.put(RequestBody.create(new byte[0]));
+        builder.url(String.format("%s/api/%s/%s/_stream_load", BASE_URL, DB_NAME, TABLE_NAME));
+        return builder.build();
+    }
+
+    private HttpPut buildPutRequest(int bodyLength, boolean setExpectHeader) {
+>>>>>>> bf78f6e38d ([BugFix] fix 100-continue compatibility issue for HTTP/1.0 (#53008))
         HttpPut put = new HttpPut(String.format("%s/api/%s/%s/_stream_load", BASE_URL, DB_NAME, TABLE_NAME));
-        put.setHeader(HttpHeaders.EXPECT, "100-continue");
+        if (setExpectHeader) {
+            put.setHeader(HttpHeaders.EXPECT, "100-continue");
+        }
         put.setHeader(HttpHeaders.AUTHORIZATION, rootAuth);
         StringEntity entity = new StringEntity(Arrays.toString(new byte[bodyLength]), "UTF-8");
         put.setEntity(entity);
@@ -80,7 +162,83 @@ public class LoadActionTest extends StarRocksHttpTestCase {
             // NOTE: Just a few bytes, so the next request header is corrupted but not completely available at all.
             // otherwise FE will discard bytes from the connection as many as X bytes, and possibly skip the
             // next request entirely, so it will be looked like the server never respond at all from client side.
-            HttpPut put = buildPutRequest(2);
+            HttpPut put = buildPutRequest(2, true);
+            try (CloseableHttpResponse response = client.execute(put)) {
+                Assert.assertEquals(HttpResponseStatus.TEMPORARY_REDIRECT.code(),
+                        response.getStatusLine().getStatusCode());
+                // The server indicates that the connection should be closed.
+                Assert.assertEquals(HttpHeaderValues.CLOSE.toString(),
+                        response.getFirstHeader(HttpHeaderNames.CONNECTION.toString()).getValue());
+            }
+        }
+        client.close();
+    }
+
+    @Test
+    public void testLoad100ContinueBackwardsCompatible() throws Exception {
+        new MockUp<NodeSelector>() {
+            @Mock
+            public List<Long> seqChooseBackendIds(int backendNum, boolean needAvailable,
+                                                  boolean isCreate, Multimap<String, String> locReq) {
+                List<Long> result = new ArrayList<>();
+                result.add(testBackendId1);
+                return result;
+            }
+        };
+
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectionRequestTimeout(3000)
+                .build();
+
+        CloseableHttpClient client = HttpClients
+                .custom()
+                .setRedirectStrategy(new DefaultRedirectStrategy() {
+                    @Override
+                    protected boolean isRedirectable(String method) {
+                        return false;
+                    }
+                })
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+
+        {
+            // HTTP/1.1, has 'Expect: 100-Continue', responds HTTP 307
+            HttpPut put = buildPutRequest(128, true);
+            put.setProtocolVersion(new ProtocolVersion("HTTP", 1, 1));
+            try (CloseableHttpResponse response = client.execute(put)) {
+                Assert.assertEquals(HttpResponseStatus.TEMPORARY_REDIRECT.code(),
+                        response.getStatusLine().getStatusCode());
+                // The server indicates that the connection should be closed.
+                Assert.assertEquals(HttpHeaderValues.CLOSE.toString(),
+                        response.getFirstHeader(HttpHeaderNames.CONNECTION.toString()).getValue());
+            }
+        }
+
+        {
+            // HTTP/1.1, no 'Expect: 100-Continue'. responds HTTP 200 with error msg
+            HttpPut put = buildPutRequest(256, false);
+            put.setProtocolVersion(new ProtocolVersion("HTTP", 1, 1));
+            try (CloseableHttpResponse response = client.execute(put)) {
+                Assert.assertEquals(HttpResponseStatus.OK.code(),
+                        response.getStatusLine().getStatusCode());
+                // The server indicates that the connection should be closed.
+                Assert.assertEquals(HttpHeaderValues.CLOSE.toString(),
+                        response.getFirstHeader(HttpHeaderNames.CONNECTION.toString()).getValue());
+
+                String body = new BasicResponseHandler().handleResponse(response);
+                Map<String, Object> result = objectMapper.readValue(body, new TypeReference<>() {});
+
+                // {"Status":"FAILED","Message":"class com.starrocks.common.DdlException: There is no 100-continue header"}
+                Assert.assertEquals("FAILED", result.get("Status"));
+                Assert.assertEquals("class com.starrocks.common.DdlException: There is no 100-continue header",
+                        result.get("Message"));
+            }
+        }
+
+        {
+            // HTTP/1.0, no 'Expect: 100-Continue'. responds HTTP 307
+            HttpPut put = buildPutRequest(512, false);
+            put.setProtocolVersion(new ProtocolVersion("HTTP", 1, 0));
             try (CloseableHttpResponse response = client.execute(put)) {
                 Assert.assertEquals(HttpResponseStatus.TEMPORARY_REDIRECT.code(),
                         response.getStatusLine().getStatusCode());
