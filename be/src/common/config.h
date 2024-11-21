abba00213cbd840dc086b3d361fa99918a2db0ad
wyb <wybb86@gmail.com>
2024-11-21 07:41:37 +0000

    Fix conflict

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -1447,22 +1447,7 @@ CONF_mBool(skip_schema_in_rowset_meta, "true");
 
 CONF_mInt32(max_committed_without_schema_rowset, "1000");
 
-<<<<<<< HEAD
-=======
-CONF_mInt32(apply_version_slow_log_sec, "30");
-
-CONF_Int32(batch_write_thread_pool_num_min, "0");
-CONF_Int32(batch_write_thread_pool_num_max, "512");
-CONF_Int32(batch_write_thread_pool_queue_size, "4096");
-CONF_mInt32(batch_write_default_timeout_ms, "600000");
-CONF_mInt32(batch_write_rpc_request_retry_num, "10");
-CONF_mInt32(batch_write_rpc_request_retry_interval_ms, "500");
-CONF_mInt32(batch_write_rpc_reqeust_timeout_ms, "10000");
-CONF_mInt32(batch_write_poll_load_status_interval_ms, "200");
-CONF_mBool(batch_write_trace_log_enable, "false");
-
 // ignore union type tag in avro kafka routine load
 CONF_mBool(avro_ignore_union_type_tag, "false");
 
->>>>>>> d42eb66004 ([Enhancement] Ignore union type tag when converting avro to json (#52973))
 } // namespace starrocks::config
