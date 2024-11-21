427afe6ebbca8587c8503cbf66d4435f31da9fce
RyanZ <dirtysalt1987@gmail.com>
2024-11-21 19:44:37 +0800

    [Refactor] move `getPartitions` API into `icebergCatalog` (#53007)
    Signed-off-by: yanz <dirtysalt1987@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/connector/iceberg/CachingIcebergCatalog.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/iceberg/CachingIcebergCatalog.java
@@ -16,8 +16,9 @@ package com.starrocks.connector.iceberg;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
 import com.starrocks.catalog.Database;
 import com.starrocks.common.Config;
 import com.starrocks.common.MetaNotFoundException;
@@ -30,23 +31,17 @@ import com.starrocks.qe.ConnectContext;
 import org.apache.iceberg.BaseTable;
 import org.apache.iceberg.DataFile;
 import org.apache.iceberg.DeleteFile;
-import org.apache.iceberg.FileScanTask;
 import org.apache.iceberg.ManifestFile;
 import org.apache.iceberg.PartitionSpec;
 import org.apache.iceberg.Schema;
 import org.apache.iceberg.StarRocksIcebergTableScan;
-import org.apache.iceberg.StructLike;
 import org.apache.iceberg.Table;
 import org.apache.iceberg.TableMetadata;
 import org.apache.iceberg.TableOperations;
-import org.apache.iceberg.TableScan;
-import org.apache.iceberg.io.CloseableIterable;
-import org.apache.iceberg.io.CloseableIterator;
 import org.apache.iceberg.view.View;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -58,7 +53,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.stream.Collectors;
 
-import static com.starrocks.connector.PartitionUtil.convertIcebergPartitionToPartitionName;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class CachingIcebergCatalog implements IcebergCatalog {
@@ -70,7 +64,6 @@ public class CachingIcebergCatalog implements IcebergCatalog {
     private final String catalogName;
     private final IcebergCatalog delegate;
     private final Cache<IcebergTableName, Table> tables;
-    private final Cache<IcebergTableName, List<String>> partitionNames;
     private final Cache<String, Database> databases;
     private final ExecutorService backgroundExecutor;
 
@@ -79,6 +72,8 @@ public class CachingIcebergCatalog implements IcebergCatalog {
     private final Cache<String, Set<DeleteFile>> deleteFileCache;
     private final Map<IcebergTableName, Long> tableLatestAccessTime = new ConcurrentHashMap<>();
     private final Map<IcebergTableName, Long> tableLatestRefreshTime = new ConcurrentHashMap<>();
+
+    private final LoadingCache<IcebergTableName, Map<String, Partition>> partitionCache;
 
     public CachingIcebergCatalog(String catalogName, IcebergCatalog delegate, IcebergCatalogProperties icebergProperties,
                                  ExecutorService executorService) {
@@ -90,8 +85,15 @@ public class CachingIcebergCatalog implements IcebergCatalog {
                 enableCache ? DEFAULT_CACHE_NUM : NEVER_CACHE).build();
         this.tables = newCacheBuilder(icebergProperties.getIcebergTableCacheTtlSec(),
                 enableCache ? DEFAULT_CACHE_NUM : NEVER_CACHE).build();
-        this.partitionNames = newCacheBuilder(icebergProperties.getIcebergMetaCacheTtlSec(),
-                enableCache ? DEFAULT_CACHE_NUM : NEVER_CACHE).build();
+        this.partitionCache = newCacheBuilder(icebergProperties.getIcebergMetaCacheTtlSec(),
+                enableCache ? DEFAULT_CACHE_NUM : NEVER_CACHE).build(
+                CacheLoader.asyncReloading(new CacheLoader<>() {
+                    @Override
+                    public Map<String, Partition> load(IcebergTableName key) throws Exception {
+                        // use default executor service.
+                        return delegate.getPartitions(key.dbName, key.tableName, key.snapshotId, null);
+                    }
+                }, executorService));
         this.dataFileCache = enableCache ?
                 newCacheBuilder(
                         icebergProperties.getIcebergMetaCacheTtlSec(), icebergProperties.getIcebergManifestCacheMaxNum()).build()
@@ -196,56 +198,10 @@ public class CachingIcebergCatalog implements IcebergCatalog {
     }
 
     @Override
-    public List<String> listPartitionNames(String dbName, String tableName, long snapshotId, ExecutorService executorService) {
-        IcebergTableName icebergTableName = new IcebergTableName(dbName, tableName, snapshotId);
-        if (partitionNames.asMap().containsKey(icebergTableName)) {
-            return partitionNames.getIfPresent(icebergTableName);
-        } else {
-            org.apache.iceberg.Table icebergTable = delegate.getTable(dbName, tableName);
-            List<String> partitionNames = Lists.newArrayList();
-
-            if (icebergTable.specs().values().stream().allMatch(PartitionSpec::isUnpartitioned)) {
-                return partitionNames;
-            }
-
-            if (snapshotId == -1) {
-                if (icebergTable.currentSnapshot() == null) {
-                    return partitionNames;
-                } else {
-                    snapshotId = icebergTable.currentSnapshot().snapshotId();
-                }
-            }
-
-            partitionNames = listPartitionNamesWithSnapshotId(icebergTable, dbName, tableName, snapshotId, executorService);
-            this.partitionNames.put(icebergTableName, partitionNames);
-            return partitionNames;
-        }
-    }
-
-    private List<String> listPartitionNamesWithSnapshotId(
-            Table table, String dbName, String tableName, long snapshotId, ExecutorService executorService) {
-        Set<String> partitionNames = Sets.newHashSet();
-        StarRocksIcebergTableScanContext scanContext = new StarRocksIcebergTableScanContext(
-                catalogName, dbName, tableName, PlanMode.LOCAL);
-        scanContext.setOnlyReadCache(true);
-        TableScan tableScan = getTableScan(table, scanContext)
-                .planWith(executorService)
-                .useSnapshot(snapshotId);
-
-        try (CloseableIterable<FileScanTask> fileScanTaskIterable = tableScan.planFiles();
-                CloseableIterator<FileScanTask> fileScanTaskIterator = fileScanTaskIterable.iterator()) {
-
-            while (fileScanTaskIterator.hasNext()) {
-                FileScanTask scanTask = fileScanTaskIterator.next();
-                StructLike partition = scanTask.file().partition();
-                partitionNames.add(convertIcebergPartitionToPartitionName(scanTask.spec(), partition));
-            }
-        } catch (IOException e) {
-            throw new StarRocksConnectorException(String.format("Failed to list iceberg partition names %s.%s",
-                    dbName, tableName), e);
-        }
-
-        return new ArrayList<>(partitionNames);
+    public Map<String, Partition> getPartitions(String dbName, String tableName, long snapshotId,
+                                                ExecutorService executorService) {
+        IcebergTableName key = new IcebergTableName(dbName, tableName, snapshotId);
+        return partitionCache.getUnchecked(key);
     }
 
     @Override
@@ -257,7 +213,7 @@ public class CachingIcebergCatalog implements IcebergCatalog {
     public synchronized void refreshTable(String dbName, String tableName, ExecutorService executorService) {
         IcebergTableName icebergTableName = new IcebergTableName(dbName, tableName);
         if (tables.getIfPresent(icebergTableName) == null) {
-            partitionNames.invalidate(icebergTableName);
+            partitionCache.invalidate(icebergTableName);
         } else {
             BaseTable currentTable = (BaseTable) tables.getIfPresent(icebergTableName);
             BaseTable updateTable = (BaseTable) delegate.getTable(dbName, tableName);
@@ -302,14 +258,10 @@ public class CachingIcebergCatalog implements IcebergCatalog {
         IcebergTableName updatedIcebergTableName = new IcebergTableName(dbName, tableName, updatedSnapshotId);
         long latestRefreshTime = tableLatestRefreshTime.computeIfAbsent(new IcebergTableName(dbName, tableName), ignore -> -1L);
 
-        List<String> updatedPartitionNames = updatedTable.spec().isPartitioned() ?
-                listPartitionNamesWithSnapshotId(updatedTable, dbName, tableName, updatedSnapshotId, executorService) :
-                new ArrayList<>();
-
+        partitionCache.invalidate(baseIcebergTableName);
+        partitionCache.getUnchecked(updatedIcebergTableName);
         synchronized (this) {
-            partitionNames.put(updatedIcebergTableName, updatedPartitionNames);
             tables.put(updatedIcebergTableName, updatedTable);
-            partitionNames.invalidate(baseIcebergTableName);
         }
 
         TableMetadata updatedTableMetadata = updatedTable.operations().current();
@@ -346,7 +298,7 @@ public class CachingIcebergCatalog implements IcebergCatalog {
                         Config.background_refresh_metadata_time_secs_since_last_access_secs) {
                     return;
                 }
-                
+
                 refreshTable(identifier.dbName, identifier.tableName, backgroundExecutor);
             } catch (Exception e) {
                 LOG.warn("refresh {}.{} metadata cache failed, msg : ", identifier.dbName, identifier.tableName, e);
@@ -356,12 +308,12 @@ public class CachingIcebergCatalog implements IcebergCatalog {
     }
 
     public void invalidateCacheWithoutTable(IcebergTableName icebergTableName) {
-        partitionNames.invalidate(icebergTableName);
+        partitionCache.invalidate(icebergTableName);
     }
 
     public void invalidateCache(IcebergTableName icebergTableName) {
         tables.invalidate(icebergTableName);
-        partitionNames.invalidate(icebergTableName);
+        partitionCache.invalidate(icebergTableName);
     }
 
     @Override
@@ -428,20 +380,29 @@ public class CachingIcebergCatalog implements IcebergCatalog {
         }
     }
 
+    private List<List<String>> getAllCachedPartitionNames() {
+        List<List<String>> ans = new ArrayList<>();
+        for (Map<String, Partition> kv : partitionCache.asMap().values()) {
+            ans.add(new ArrayList<>(kv.keySet()));
+        }
+        return ans;
+    }
+
     @Override
     public List<Pair<List<Object>, Long>> getSamples() {
         Pair<List<Object>, Long> dbSamples = Pair.create(databases.asMap().values()
-                .stream()
-                .limit(MEMORY_META_SAMPLES)
-                .collect(Collectors.toList()),
+                        .stream()
+                        .limit(MEMORY_META_SAMPLES)
+                        .collect(Collectors.toList()),
                 databases.size());
 
-        List<Object> partitions = partitionNames.asMap().values()
+        List<List<String>> partitionNames = getAllCachedPartitionNames();
+        List<Object> partitions = partitionNames
                 .stream()
                 .flatMap(List::stream)
                 .limit(MEMORY_FILE_SAMPLES)
                 .collect(Collectors.toList());
-        long partitionTotal = partitionNames.asMap().values()
+        long partitionTotal = partitionNames
                 .stream()
                 .mapToLong(List::size)
                 .sum();
@@ -473,13 +434,14 @@ public class CachingIcebergCatalog implements IcebergCatalog {
     @Override
     public Map<String, Long> estimateCount() {
         Map<String, Long> counter = new HashMap<>();
+        List<List<String>> partitionNames = getAllCachedPartitionNames();
         counter.put("Database", databases.size());
         counter.put("Table", tables.size());
-        counter.put("PartitionNames", partitionNames.asMap().values()
+        counter.put("PartitionNames", partitionNames
                 .stream()
                 .mapToLong(List::size)
                 .sum());
-        counter.put("ManifestOfDataFile",  dataFileCache.asMap().values()
+        counter.put("ManifestOfDataFile", dataFileCache.asMap().values()
                 .stream()
                 .mapToLong(Set::size)
                 .sum());
--- fe/fe-core/src/main/java/com/starrocks/connector/iceberg/IcebergCatalog.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/iceberg/IcebergCatalog.java
@@ -12,18 +12,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 package com.starrocks.connector.iceberg;
 
-import com.google.common.collect.Sets;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Maps;
 import com.starrocks.catalog.Database;
 import com.starrocks.common.MetaNotFoundException;
 import com.starrocks.common.Pair;
 import com.starrocks.connector.ConnectorViewDefinition;
+import com.starrocks.connector.PartitionUtil;
 import com.starrocks.connector.exception.StarRocksConnectorException;
 import com.starrocks.memory.MemoryTrackable;
 import org.apache.iceberg.FileScanTask;
+import org.apache.iceberg.MetadataTableUtils;
 import org.apache.iceberg.PartitionSpec;
+import org.apache.iceberg.PartitionsTable;
 import org.apache.iceberg.Schema;
 import org.apache.iceberg.StarRocksIcebergTableScan;
 import org.apache.iceberg.StructLike;
@@ -31,21 +34,27 @@ import org.apache.iceberg.Table;
 import org.apache.iceberg.TableScan;
 import org.apache.iceberg.exceptions.NoSuchTableException;
 import org.apache.iceberg.io.CloseableIterable;
-import org.apache.iceberg.io.CloseableIterator;
+import org.apache.iceberg.util.StructProjection;
 import org.apache.iceberg.view.View;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ExecutorService;
 
-import static com.starrocks.connector.PartitionUtil.convertIcebergPartitionToPartitionName;
 import static org.apache.iceberg.StarRocksIcebergTableScan.newTableScanContext;
 
 public interface IcebergCatalog extends MemoryTrackable {
+    Logger DEFAULT_LOGGER = LogManager.getLogger(IcebergCatalog.class);
+    String EMPTY_PARTITION_NAME = "";
+
+    default Logger getLogger() {
+        return DEFAULT_LOGGER;
+    }
 
     IcebergCatalogType getIcebergCatalogType();
 
@@ -99,32 +108,6 @@ public interface IcebergCatalog extends MemoryTrackable {
         throw new StarRocksConnectorException("This catalog doesn't loading iceberg view");
     }
 
-    default List<String> listPartitionNames(String dbName, String tableName,  long snapshotId, ExecutorService executorService) {
-        org.apache.iceberg.Table icebergTable = getTable(dbName, tableName);
-        Set<String> partitionNames = Sets.newHashSet();
-
-        if (icebergTable.specs().values().stream().allMatch(PartitionSpec::isUnpartitioned)) {
-            return new ArrayList<>();
-        }
-
-        TableScan tableScan = icebergTable.newScan().planWith(executorService);
-        try (CloseableIterable<FileScanTask> fileScanTaskIterable = tableScan.planFiles();
-                CloseableIterator<FileScanTask> fileScanTaskIterator = fileScanTaskIterable.iterator()) {
-
-            while (fileScanTaskIterator.hasNext()) {
-                FileScanTask scanTask = fileScanTaskIterator.next();
-                StructLike partition = scanTask.file().partition();
-                String partitionName = convertIcebergPartitionToPartitionName(scanTask.spec(), partition);
-                partitionNames.add(partitionName);
-            }
-        } catch (IOException e) {
-            throw new StarRocksConnectorException(String.format("Failed to list iceberg partition names %s.%s",
-                    dbName, tableName), e);
-        }
-
-        return new ArrayList<>(partitionNames);
-    }
-
     default void deleteUncommittedDataFiles(List<String> fileLocations) {
     }
 
@@ -159,5 +142,129 @@ public interface IcebergCatalog extends MemoryTrackable {
 
     default List<Pair<List<Object>, Long>> getSamples() {
         return new ArrayList<>();
+    }
+
+    // --------------- partition APIs ---------------
+    private Map<String, Partition> getPartitions(Table icebergTable, long snapshotId, ExecutorService executorService) {
+        Map<String, Partition> partitionMap = Maps.newHashMap();
+        PartitionsTable partitionsTable = (PartitionsTable) MetadataTableUtils.
+                createMetadataTableInstance(icebergTable, org.apache.iceberg.MetadataTableType.PARTITIONS);
+        TableScan tableScan = partitionsTable.newScan();
+        if (snapshotId != -1) {
+            tableScan = tableScan.useSnapshot(snapshotId);
+        }
+        if (executorService != null) {
+            tableScan = tableScan.planWith(executorService);
+        }
+        Logger logger = getLogger();
+
+        // TODO: ideally we should know if table is partitioned under a snapshotId.
+        // but currently we just did it in a very wild way.
+        if (icebergTable.spec().isUnpartitioned()) {
+            Partition partition = null;
+            try (CloseableIterable<FileScanTask> tasks = tableScan.planFiles()) {
+                for (FileScanTask task : tasks) {
+                    // partitionsTable Table schema :
+                    // record_count,
+                    // file_count,
+                    // total_data_file_size_in_bytes,
+                    // position_delete_record_count,
+                    // position_delete_file_count,
+                    // equality_delete_record_count,
+                    // equality_delete_file_count,
+                    // last_updated_at,
+                    // last_updated_snapshot_id
+                    CloseableIterable<StructLike> rows = task.asDataTask().rows();
+                    for (StructLike row : rows) {
+                        // Get the last updated time of the table according to the table schema
+                        long lastUpdated = -1;
+                        try {
+                            lastUpdated = row.get(7, Long.class);
+                        } catch (NullPointerException e) {
+                            logger.error("The table [{}] snapshot [{}] has been expired", icebergTable.name(), snapshotId, e);
+                        }
+                        partition = new Partition(lastUpdated);
+                        break;
+                    }
+                }
+                if (partition == null) {
+                    partition = new Partition(-1);
+                }
+                partitionMap.put(EMPTY_PARTITION_NAME, partition);
+            } catch (IOException e) {
+                throw new StarRocksConnectorException("Failed to get partitions for table: " + icebergTable.name(), e);
+            }
+        } else {
+            // For partition table, we need to get all partitions from PartitionsTable.
+            try (CloseableIterable<FileScanTask> tasks = tableScan.planFiles()) {
+                for (FileScanTask task : tasks) {
+                    // partitionsTable Table schema :
+                    // partition,
+                    // spec_id,
+                    // record_count,
+                    // file_count,
+                    // total_data_file_size_in_bytes,
+                    // position_delete_record_count,
+                    // position_delete_file_count,
+                    // equality_delete_record_count,
+                    // equality_delete_file_count,
+                    // last_updated_at,
+                    // last_updated_snapshot_id
+                    CloseableIterable<StructLike> rows = task.asDataTask().rows();
+                    for (StructLike row : rows) {
+                        // Get the partition data/spec id/last updated time according to the table schema
+                        StructProjection partitionData = row.get(0, StructProjection.class);
+                        int specId = row.get(1, Integer.class);
+                        PartitionSpec spec = icebergTable.specs().get(specId);
+
+                        String partitionName =
+                                PartitionUtil.convertIcebergPartitionToPartitionName(spec, partitionData);
+
+                        long lastUpdated = -1;
+                        try {
+                            lastUpdated = row.get(9, Long.class);
+                        } catch (NullPointerException e) {
+                            logger.error("The table [{}.{}] snapshot [{}] has been expired", icebergTable.name(), partitionName,
+                                    snapshotId, e);
+                        }
+                        Partition partition = new Partition(lastUpdated);
+                        partitionMap.put(partitionName, partition);
+                    }
+                }
+            } catch (IOException e) {
+                throw new StarRocksConnectorException("Failed to get partitions for table: " + icebergTable.name(), e);
+            }
+        }
+        return partitionMap;
+    }
+
+    default Map<String, Partition> getPartitions(String dbName, String tableName, long snapshotId,
+                                                 ExecutorService executorService) {
+        Table icebergTable = getTable(dbName, tableName);
+        return getPartitions(icebergTable, snapshotId, executorService);
+    }
+
+    default List<String> listPartitionNames(String dbName, String tableName, long snapshotId, ExecutorService executorService) {
+        Table icebergTable = getTable(dbName, tableName);
+        Map<String, Partition> partitionMap = getPartitions(icebergTable, snapshotId, executorService);
+        if (icebergTable.spec().isUnpartitioned()) {
+            return List.of();
+        } else {
+            return new ArrayList<>(partitionMap.keySet());
+        }
+    }
+
+    default List<Partition> getPartitionsByNames(String dbName, String tableName, long snapshotId,
+                                                 ExecutorService executorService,
+                                                 List<String> partitionNames) {
+        Table icebergTable = getTable(dbName, tableName);
+        Map<String, Partition> partitionMap = getPartitions(icebergTable, snapshotId, executorService);
+        if (icebergTable.spec().isUnpartitioned()) {
+            return List.of(partitionMap.get(EMPTY_PARTITION_NAME));
+        } else {
+            ImmutableList.Builder<Partition> partitions = ImmutableList.builder();
+            partitionNames.forEach(partitionName -> partitions.add(partitionMap.get(partitionName)));
+            return partitions.build();
+        }
     }
 }
