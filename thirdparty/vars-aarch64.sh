0da121676f1d58591df5e72d78f3112faacbf6ef
Kevin Xiaohua Cai <caixiaohua@starrocks.com>
2024-11-26 21:11:11 +0800

    [Enhancement] update staros version to v3.1.10 for branch-3.1
    Signed-off-by: Kevin Xiaohua Cai <caixiaohua@starrocks.com>

--- thirdparty/vars-aarch64.sh
+++ thirdparty/vars-aarch64.sh
@@ -52,7 +52,7 @@ JINDOSDK_SOURCE="jindosdk-4.6.8-linux-el7-aarch64"
 JINDOSDK_MD5SUM="27a4e2cd9a403c6e21079a866287d88b"
 
 # starcache
-STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v3.1.2/starcache-centos7_arm64.tar.gz"
+STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v3.1.3/starcache-centos7_arm64.tar.gz"
 STARCACHE_NAME="starcache.tar.gz"
 STARCACHE_SOURCE="starcache"
-STARCACHE_MD5SUM="20d4ce6173a6eeee53d423b6ecbf3a15"
+STARCACHE_MD5SUM="1e20d37bf983b302b23ded74c76984de"
--- thirdparty/vars-ubuntu22-x86_64.sh
+++ thirdparty/vars-ubuntu22-x86_64.sh
@@ -28,7 +28,7 @@ JINDOSDK_SOURCE="jindosdk-4.6.8-linux-ubuntu22-x86_64"
 JINDOSDK_MD5SUM="52236053391091591c2d09684791e810"
 
 # starcache
-STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v3.1.2/starcache-ubuntu22_amd64.tar.gz"
+STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v3.1.3/starcache-ubuntu22_amd64.tar.gz"
 STARCACHE_NAME="starcache.tar.gz"
 STARCACHE_SOURCE="starcache"
-STARCACHE_MD5SUM="0a024398153d57b5cff55b7cf7c043d7"
+STARCACHE_MD5SUM="57319025805c0f2ff2317aea7ccffc0c"
