38c4dfe41a008da49abdb5140f9950110d665131
Kevin Cai <caixiaohua@starrocks.com>
2024-11-28 13:59:48 +0800

    [Enhancement] update staros version to v3.2.5 (#53285)
    Signed-off-by: Kevin Xiaohua Cai <caixiaohua@starrocks.com>

--- thirdparty/vars-aarch64.sh
+++ thirdparty/vars-aarch64.sh
@@ -52,7 +52,7 @@ JINDOSDK_SOURCE="jindosdk-4.6.8-linux-el7-aarch64"
 JINDOSDK_MD5SUM="27a4e2cd9a403c6e21079a866287d88b"
 
 # starcache
-STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v3.2.6/starcache-centos7_arm64.tar.gz"
+STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v3.2.7/starcache-centos7_arm64.tar.gz"
 STARCACHE_NAME="starcache.tar.gz"
 STARCACHE_SOURCE="starcache"
-STARCACHE_MD5SUM="469d5ebe77c3edeffae37eff3613644c"
+STARCACHE_MD5SUM="b8e2e61ed73c3995299f52620bafe58d"
