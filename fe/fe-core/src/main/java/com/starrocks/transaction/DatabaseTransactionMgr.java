cb72f91ff3aef7d536bb5183c9bb82feca120bf4
zhanghe <edwinhzhang@tencent.com>
2024-11-29 15:51:24 +0800

    [BugFix] With a high streamload frequency and too many tablets involved, TabletCommitInfos will take up too much memory. (#53329)
    Signed-off-by: edwinhzhang <edwinhzhang@tencent.com>

--- fe/fe-core/src/main/java/com/starrocks/transaction/DatabaseTransactionMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/transaction/DatabaseTransactionMgr.java
@@ -1206,6 +1206,7 @@ public class DatabaseTransactionMgr {
             finishSpan.end();
         }
 
+        resetTransactionStateTabletCommitInfos(transactionState);
         transactionState.notifyVisible();
         // do after transaction finish
         GlobalStateMgr.getCurrentState().getOperationListenerBus().onStreamJobTransactionFinish(transactionState);
@@ -1906,6 +1907,7 @@ public class DatabaseTransactionMgr {
             finishSpan.end();
         }
 
+        resetTransactionStateTabletCommitInfos(transactionState);
         // do after transaction finish
         GlobalStateMgr.getCurrentState().getOperationListenerBus().onStreamJobTransactionFinish(transactionState);
         GlobalStateMgr.getCurrentState().getLocalMetastore().handleMVRepair(transactionState);
@@ -2065,6 +2067,15 @@ public class DatabaseTransactionMgr {
             return new ArrayList<>();
         } finally {
             readUnlock();
+        }
+    }
+
+    public void resetTransactionStateTabletCommitInfos(TransactionState transactionState) {
+        writeLock();
+        try {
+            transactionState.resetTabletCommitInfos();
+        } finally {
+            writeUnlock();
         }
     }
 }
