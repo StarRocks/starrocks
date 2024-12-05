34689b7574f8323012ae560425b465fed547b60d
before-Sunrise <71162020+before-Sunrise@users.noreply.github.com>
2024-12-05 10:51:13 +0800

    [BugFix] Revert [Enhancement] make connector scan operator profile less skew (#53465)
    (cherry picked from commit 2719242e5c4bd0a722584668cc8f9d3515559c05)
    
    # Conflicts:
    #	be/src/connector/lake_connector.cpp
    #	be/src/exec/pipeline/scan/olap_chunk_source.cpp

--- be/src/exec/pipeline/scan/olap_chunk_source.cpp
+++ be/src/exec/pipeline/scan/olap_chunk_source.cpp
@@ -341,7 +341,7 @@ Status OlapChunkSource::_init_column_access_paths(Schema* schema) {
     _params.column_access_paths = &_column_access_paths;
 
     // update counter
-    COUNTER_UPDATE(_pushdown_access_paths_counter, leaf_size);
+    COUNTER_SET(_pushdown_access_paths_counter, leaf_size);
     return Status::OK();
 }
 
@@ -464,6 +464,19 @@ Status OlapChunkSource::_init_olap_reader(RuntimeState* runtime_state) {
 
     if (!_scan_ctx->not_push_down_conjuncts().empty() || !_not_push_down_predicates.empty()) {
         _expr_filter_timer = ADD_CHILD_TIMER(_runtime_profile, "ExprFilterTime", IO_TASK_EXEC_TIMER_NAME);
+<<<<<<< HEAD
+=======
+
+        _non_pushdown_predicates_counter = ADD_COUNTER_SKIP_MERGE(_runtime_profile, "NonPushdownPredicates",
+                                                                  TUnit::UNIT, TCounterMergeType::SKIP_ALL);
+        COUNTER_SET(_non_pushdown_predicates_counter,
+                    static_cast<int64_t>(_scan_ctx->not_push_down_conjuncts().size() + _non_pushdown_pred_tree.size()));
+        if (runtime_state->fragment_ctx()->pred_tree_params().enable_show_in_profile) {
+            _runtime_profile->add_info_string(
+                    "NonPushdownPredicateTree",
+                    _non_pushdown_pred_tree.visit([](const auto& node) { return node.debug_string(); }));
+        }
+>>>>>>> 2719242e5c ([BugFix] Revert [Enhancement] make connector scan operator profile less skew (#53465))
     }
 
     DCHECK(_params.global_dictmaps != nullptr);
@@ -626,7 +639,7 @@ void OlapChunkSource::_update_counter() {
     COUNTER_UPDATE(_segments_read_count, _reader->stats().segments_read_count);
     COUNTER_UPDATE(_total_columns_data_page_count, _reader->stats().total_columns_data_page_count);
 
-    COUNTER_UPDATE(_pushdown_predicates_counter, (int64_t)_params.pred_tree.size());
+    COUNTER_SET(_pushdown_predicates_counter, (int64_t)_params.pred_tree.size());
 
     StarRocksMetrics::instance()->query_scan_bytes.increment(_scan_bytes);
     StarRocksMetrics::instance()->query_scan_rows.increment(_scan_rows_num);
