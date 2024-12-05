e58712af808a4552130156b37482fec068b40fc0
srlch <111035020+srlch@users.noreply.github.com>
2024-12-05 12:29:38 +0800

    [Enhancement] Use dynamic batch size for simdjson to parse multiple json document (backport #53056) (#53056) (#53586)
    Signed-off-by: srlch <linzichao@starrocks.com>

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -1455,4 +1455,7 @@ CONF_mBool(avro_ignore_union_type_tag, "false");
 
 CONF_mInt32(apply_version_slow_log_sec, "30");
 
+// default batch size for simdjson lib
+CONF_mInt32(json_parse_many_batch_size, "1000000");
+CONF_mBool(enable_dynamic_batch_size_for_json_parse_many, "true");
 } // namespace starrocks::config
