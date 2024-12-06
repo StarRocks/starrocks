3b0f582689d004bbacc27072ab228bb0bbd0e837
trueeyu <lxhhust350@qq.com>
2024-12-06 10:40:39 +0800

    [Enhancement] MinMaxPredicate support handing null (#53613)
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/exprs/runtime_filter_bank.cpp
+++ be/src/exprs/runtime_filter_bank.cpp
@@ -22,6 +22,7 @@
 #include "exprs/dictmapping_expr.h"
 #include "exprs/in_const_predicate.hpp"
 #include "exprs/literal.h"
+#include "exprs/min_max_predicate.h"
 #include "exprs/runtime_filter.h"
 #include "exprs/runtime_filter_layout.h"
 #include "gen_cpp/RuntimeFilter_types.h"
@@ -778,113 +779,10 @@ void RuntimeFilterProbeDescriptor::set_shared_runtime_filter(const std::shared_p
     }
 }
 
-// ========================================================
-template <LogicalType Type>
-class MinMaxPredicate : public Expr {
-public:
-    using CppType = RunTimeCppType<Type>;
-    MinMaxPredicate(SlotId slot_id, const CppType& min_value, const CppType& max_value)
-            : Expr(TypeDescriptor(Type), false), _slot_id(slot_id), _min_value(min_value), _max_value(max_value) {
-        _node_type = TExprNodeType::RUNTIME_FILTER_MIN_MAX_EXPR;
-    }
-    ~MinMaxPredicate() override = default;
-    Expr* clone(ObjectPool* pool) const override {
-        return pool->add(new MinMaxPredicate<Type>(_slot_id, _min_value, _max_value));
-    }
-
-    bool is_constant() const override { return false; }
-    bool is_bound(const std::vector<TupleId>& tuple_ids) const override { return false; }
-
-    StatusOr<ColumnPtr> evaluate_with_filter(ExprContext* context, Chunk* ptr, uint8_t* filter) override {
-        const ColumnPtr col = ptr->get_column_by_slot_id(_slot_id);
-        size_t size = col->size();
-
-        std::shared_ptr<BooleanColumn> result(new BooleanColumn(size, 1));
-        uint8_t* res = result->get_data().data();
-
-        if (col->only_null()) {
-            return result;
-        }
-
-        if (col->is_constant()) {
-            CppType value = ColumnHelper::get_const_value<Type>(col);
-            if (!(value >= _min_value && value <= _max_value)) {
-                memset(res, 0x0, size);
-            }
-            return result;
-        }
-
-        // NOTE(yan): make sure following code can be compiled into SIMD instructions:
-        // in original version, we use
-        //   1. memcpy filter -> res
-        //   2. res[i] = res[i] && (null_data[i] || (data[i] >= _min_value && data[i] <= _max_value));
-        // but they can not be compiled into SIMD instructions.
-        if (col->is_nullable()) {
-            auto tmp = ColumnHelper::as_raw_column<NullableColumn>(col);
-            uint8_t* __restrict__ null_data = tmp->null_column_data().data();
-            CppType* __restrict__ data = ColumnHelper::cast_to_raw<Type>(tmp->data_column())->get_data().data();
-            for (int i = 0; i < size; i++) {
-                res[i] = (data[i] >= _min_value && data[i] <= _max_value);
-            }
-            // we take null as true value.
-            for (int i = 0; i < size; i++) {
-                res[i] = res[i] | null_data[i];
-            }
-        } else {
-            CppType* data = ColumnHelper::cast_to_raw<Type>(col)->get_data().data();
-            for (int i = 0; i < size; i++) {
-                res[i] = (data[i] >= _min_value && data[i] <= _max_value);
-            }
-        }
-
-        // NOTE(yan): filter can be used optionally.
-        // if (filter != nullptr) {
-        //     for (int i = 0; i < size; i++) {
-        //         res[i] = res[i] & filter[i];
-        //     }
-        // }
-
-        return result;
-    }
-
-    StatusOr<ColumnPtr> evaluate_checked(ExprContext* context, Chunk* ptr) override {
-        return evaluate_with_filter(context, ptr, nullptr);
-    }
-
-    int get_slot_ids(std::vector<SlotId>* slot_ids) const override {
-        slot_ids->emplace_back(_slot_id);
-        return 1;
-    }
-
-private:
-    SlotId _slot_id;
-    const CppType _min_value;
-    const CppType _max_value;
-};
-
-class MinMaxPredicateBuilder {
-public:
-    MinMaxPredicateBuilder(ObjectPool* pool, SlotId slot_id, const JoinRuntimeFilter* filter)
-            : _pool(pool), _slot_id(slot_id), _filter(filter) {}
-
-    template <LogicalType ltype>
-    Expr* operator()() {
-        auto* bloom_filter = (RuntimeBloomFilter<ltype>*)(_filter);
-        MinMaxPredicate<ltype>* p =
-                _pool->add(new MinMaxPredicate<ltype>(_slot_id, bloom_filter->min_value(), bloom_filter->max_value()));
-        return p;
-    }
-
-private:
-    ObjectPool* _pool;
-    SlotId _slot_id;
-    const JoinRuntimeFilter* _filter;
-};
-
 void RuntimeFilterHelper::create_min_max_value_predicate(ObjectPool* pool, SlotId slot_id, LogicalType slot_type,
                                                          const JoinRuntimeFilter* filter, Expr** min_max_predicate) {
     *min_max_predicate = nullptr;
-    if (filter == nullptr || filter->has_null()) return;
+    if (filter == nullptr) return;
     if (slot_type == TYPE_CHAR || slot_type == TYPE_VARCHAR) return;
     auto res = type_dispatch_filter(slot_type, (Expr*)nullptr, MinMaxPredicateBuilder(pool, slot_id, filter));
     *min_max_predicate = res;
