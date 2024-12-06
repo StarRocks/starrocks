b5d2636aaa6d1d67bd5c1c3fd47300cc0fe09fbd
trueeyu <lxhhust350@qq.com>
2024-12-06 14:36:17 +0800

    fix
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/runtime/data_stream_mgr.h
+++ be/src/runtime/data_stream_mgr.h
@@ -46,6 +46,7 @@
 #include "common/object_pool.h"
 #include "common/status.h"
 #include "gen_cpp/Types_types.h" // for TUniqueId
+#include "gen_cpp/internal_service.pb.h"
 #include "runtime/descriptors.h" // for PlanNodeId
 #include "runtime/local_pass_through_buffer.h"
 #include "runtime/mem_tracker.h"
