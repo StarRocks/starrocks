b5287b95bfe8699c4c18fa6627398e26b5f984e3
shuming.li <ming.moriarty@gmail.com>
2024-12-10 12:54:12 +0800

    [UT] fix unstable test_mv_refresh_with_mv_reuse case (#53739)
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>

--- test/lib/sr_sql_lib.py
+++ test/lib/sr_sql_lib.py
@@ -1982,7 +1982,8 @@ class StarrocksSQLApiLib(object):
         """
         assert mv_name is hit in query
         """
-        tools.assert_true(str(res).find(mv_name) > 0, "assert mv %s is not found" % mv_name)
+        plan = str(res)
+        tools.assert_true(plan.find(mv_name) > 0, "assert mv %s is not found in plan: %s" % (mv_name, plan))
 
     def check_hit_materialized_view(self, query, *expects):
         """
@@ -1996,7 +1997,7 @@ class StarrocksSQLApiLib(object):
         tools.assert_true(res["status"])
         plan = str(res["result"])
         for expect in expects:
-            tools.assert_true(plan.find(expect) > 0, "assert expect %s is not found in plan" % (expect))
+            tools.assert_true(plan.find(expect) > 0, "assert expect %s is not found in plan: %s" % (expect, plan))
 
     def print_hit_materialized_view(self, query, *expects) -> bool:
         """
@@ -2077,8 +2078,9 @@ class StarrocksSQLApiLib(object):
         if not res["status"]:
             print(res)
         tools.assert_true(res["status"])
+        plan = str(res["result"])
         for expect in expects:
-            tools.assert_false(str(res["result"]).find(expect) > 0, "assert expect %s should not be found" % (expect))
+            tools.assert_false(plan.find(expect) > 0, "assert expect %s should not be found in plan: %s" % (expect, plan))
 
     def wait_alter_table_finish(self, alter_type="COLUMN", off=9):
         """
