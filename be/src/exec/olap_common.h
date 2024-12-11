1e73faceee293ebef777fd7dd6e479eeab478691
trueeyu <lxhhust350@qq.com>
2024-12-11 20:29:10 +0800

    [Enhancement] ColumnValueRange::add_range supports less than or equal to max and great than or equal to min (#53839)
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/exec/olap_common.h
+++ be/src/exec/olap_common.h
@@ -72,6 +72,7 @@ public:
     typedef typename std::set<T>::iterator iterator_type;
     ColumnValueRange();
     ColumnValueRange(std::string col_name, LogicalType type, T min, T max);
+    ColumnValueRange(std::string col_name, LogicalType type, T type_min, T type_max, T min, T max);
 
     Status add_fixed_values(SQLFilterOp op, const std::set<T>& values);
 
