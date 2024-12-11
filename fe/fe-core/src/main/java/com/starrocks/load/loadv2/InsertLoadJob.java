35fe316b55d29a1be2cfb05dda4a252e727159f6
zhanghe <edwinhzhang@tencent.com>
2024-12-11 19:03:36 +0800

    [BugFix]Memory leak for insert load job. (#53809)
    insert load job still leak mem
    
    Signed-off-by: edwinhzhang <edwinhzhang@tencent.com>

--- fe/fe-core/src/main/java/com/starrocks/load/loadv2/InsertLoadJob.java
+++ fe/fe-core/src/main/java/com/starrocks/load/loadv2/InsertLoadJob.java
@@ -157,6 +157,7 @@ public class InsertLoadJob extends LoadJob {
             this.coordinator = null;
         } finally {
             writeUnlock();
+            GlobalStateMgr.getCurrentState().getGlobalTransactionMgr().getCallbackFactory().removeCallback(this.id);
         }
         // persistent
         GlobalStateMgr.getCurrentState().getEditLog().logEndLoadJob(
