f46e6627eeb3b71ef660d712134d62546c243ea2
gengjun-git <gengjun@starrocks.com>
2024-12-18 20:47:49 +0800

    add
    Signed-off-by: gengjun-git <gengjun@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/load/loadv2/LoadMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/load/loadv2/LoadMgr.java
@@ -202,7 +202,7 @@ public class LoadMgr implements MemoryTrackable {
         }
     }
 
-    private void addLoadJob(LoadJob loadJob) {
+    protected void addLoadJob(LoadJob loadJob) {
         idToLoadJob.put(loadJob.getId(), loadJob);
         long dbId = loadJob.getDbId();
         if (!dbIdToLabelToLoadJobs.containsKey(dbId)) {
