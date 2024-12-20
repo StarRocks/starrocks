c644b4c563d4a677d613df71720c095545fba2a0
gengjun-git <gengjun@starrocks.com>
2024-12-20 19:10:30 +0800

    [Enhancement] Ignore ShowStmt for idle check (#54173)
    Signed-off-by: gengjun-git <gengjun@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
@@ -146,6 +146,7 @@ import com.starrocks.sql.analyzer.SemanticException;
 import com.starrocks.sql.analyzer.SetStmtAnalyzer;
 import com.starrocks.sql.ast.AddBackendBlackListStmt;
 import com.starrocks.sql.ast.AddSqlBlackListStmt;
+import com.starrocks.sql.ast.AdminSetConfigStmt;
 import com.starrocks.sql.ast.AnalyzeHistogramDesc;
 import com.starrocks.sql.ast.AnalyzeProfileStmt;
 import com.starrocks.sql.ast.AnalyzeStmt;
@@ -525,7 +526,7 @@ public class StmtExecutor {
             httpResultSender = new HttpResultSender((HttpConnectContext) context);
         }
 
-        if (!isInternalStmt) {
+        if (shouldMarkIdleCheck(parsedStmt)) {
             WarehouseIdleChecker.increaseRunningSQL(context.getCurrentWarehouseId());
         }
         try {
@@ -844,7 +845,7 @@ public class StmtExecutor {
             // restore session variable in connect context
             context.setSessionVariable(sessionVariableBackup);
 
-            if (!isInternalStmt) {
+            if (shouldMarkIdleCheck(parsedStmt)) {
                 WarehouseIdleChecker.decreaseRunningSQL(context.getCurrentWarehouseId());
             }
         }
@@ -2951,5 +2952,11 @@ public class StmtExecutor {
         queryDetail.setCatalog(ctx.getCurrentCatalog());
 
         QueryDetailQueue.addQueryDetail(queryDetail);
+    }
+
+    private boolean shouldMarkIdleCheck(StatementBase parsedStmt) {
+        return !isInternalStmt
+                && !(parsedStmt instanceof ShowStmt)
+                && !(parsedStmt instanceof AdminSetConfigStmt);
     }
 }
