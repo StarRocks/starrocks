feaf94696dc8a9a284c22dba11241f82ff83ae86
srlch <111035020+srlch@users.noreply.github.com>
2024-12-27 10:30:24 +0800

    [BugFix] Fix retry upload failure when backup files using HDFS filesystem interface (#53679)
    Signed-off-by: srlch <linzichao@starrocks.com>

--- be/test/fs/fs_hdfs_test.cpp
+++ be/test/fs/fs_hdfs_test.cpp
@@ -76,4 +76,54 @@ TEST_F(HdfsFileSystemTest, create_file_and_destroy) {
     thread.join();
 }
 
+TEST_F(HdfsFileSystemTest, create_file_with_open_truncate) {
+    auto fs = new_fs_hdfs(FSOptions());
+    const std::string filepath = "file://" + _root_path + "/create_file_with_open_truncate";
+    auto st = fs->path_exists(filepath);
+    EXPECT_TRUE(st.is_not_found());
+    std::string str1 = "123";
+    std::string str2 = "456";
+    Slice data1(str1);
+    Slice data2(str2);
+
+    WritableFileOptions opts{.sync_on_close = false, .mode = FileSystem::CREATE_OR_OPEN_WITH_TRUNCATE};
+
+    auto wfile_1 = fs->new_writable_file(opts, filepath);
+    EXPECT_TRUE(wfile_1.ok());
+    EXPECT_TRUE((*wfile_1)->append(data1).ok());
+    EXPECT_TRUE((*wfile_1)->flush(WritableFile::FlushMode::FLUSH_SYNC).ok());
+    EXPECT_TRUE((*wfile_1)->sync().ok());
+    (*wfile_1)->close();
+
+    st = fs->path_exists(filepath);
+    EXPECT_TRUE(st.ok());
+
+    auto open_res1 = fs->new_random_access_file(filepath);
+    EXPECT_TRUE(open_res1.ok());
+    auto rfile1 = std::move(open_res1.value());
+    auto read_res_1 = rfile1->read_all();
+    EXPECT_TRUE(read_res_1.ok());
+    EXPECT_TRUE(read_res_1.value() == str1);
+
+    auto wfile_2 = fs->new_writable_file(opts, filepath);
+    EXPECT_TRUE(wfile_2.ok());
+    EXPECT_TRUE((*wfile_2)->append(data2).ok());
+    EXPECT_TRUE((*wfile_2)->flush(WritableFile::FlushMode::FLUSH_SYNC).ok());
+    EXPECT_TRUE((*wfile_2)->sync().ok());
+    (*wfile_2)->close();
+
+    st = fs->path_exists(filepath);
+    EXPECT_TRUE(st.ok());
+
+    auto open_res2 = fs->new_random_access_file(filepath);
+    EXPECT_TRUE(open_res2.ok());
+    auto rfile2 = std::move(open_res2.value());
+    auto read_res_2 = rfile2->read_all();
+    EXPECT_TRUE(read_res_2.ok());
+    EXPECT_TRUE(read_res_2.value() == str2);
+
+    (*wfile_1).reset();
+    (*wfile_2).reset();
+}
+
 } // namespace starrocks
