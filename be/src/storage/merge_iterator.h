d8ad69f96161ffd44587eee1c49911dd9e1c11b2
luohaha <18810541851@163.com>
2025-01-09 15:11:07 +0800

    [BugFix] add limit to chunk rows count when generate blocks in load spill
    Signed-off-by: luohaha <18810541851@163.com>

--- be/src/storage/merge_iterator.h
+++ be/src/storage/merge_iterator.h
@@ -21,6 +21,8 @@
 
 namespace starrocks {
 
+static const size_t max_merge_chunk_size = 65536;
+
 // new_heap_merge_iterator create a sorted iterator based on merge-sort algorithm.
 // the order of rows is determined by the key columns.
 // if two rows compared equal, their order is determinate by the index of the source iterator
