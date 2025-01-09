92495f53f97c0c29d9d4be1349b1b32b917e767c
meegoo <meegoo.sr@gmail.com>
2025-01-08 19:33:41 -0800

    [Refactor] add some log for online optimize table (#54834)
    Signed-off-by: meegoo <meegoo.sr@gmail.com>

--- be/src/agent/publish_version.cpp
+++ be/src/agent/publish_version.cpp
@@ -189,10 +189,14 @@ void run_publish_version_task(ThreadPoolToken* token, const TPublishVersionReque
                         std::string_view msg = task.st.message();
                         tablet_span->SetStatus(trace::StatusCode::kError, {msg.data(), msg.size()});
                     } else {
-                        VLOG(2) << "Publish txn success tablet:" << tablet->tablet_id() << " version:" << task.version
-                                << " tablet_max_version:" << tablet->max_continuous_version()
-                                << " partition:" << task.partition_id << " txn_id: " << task.txn_id
-                                << " rowset:" << task.rowset->rowset_id();
+                        if (task.is_double_write || VLOG_ROW_IS_ON) {
+                            LOG(INFO) << "Publish txn success tablet:" << tablet->tablet_id()
+                                      << " version:" << task.version
+                                      << " tablet_max_version:" << tablet->max_continuous_version()
+                                      << " is_double_write:" << task.is_double_write
+                                      << " partition:" << task.partition_id << " txn_id: " << task.txn_id
+                                      << " rowset:" << task.rowset->rowset_id();
+                        }
                     }
                 }
             });
