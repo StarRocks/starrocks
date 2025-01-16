b02e904260d9fbcc6a844e265dded929ebf78f5c
Drake Wang <wxl24life@gmail.com>
2025-01-16 21:14:22 +0800

    [BugFix] Fix warehouse property might not take effect when executing load stmts (#49684)
    Signed-off-by: drake_wang <wxl250059@alibaba-inc.com>
    (cherry picked from commit c2c766692c5d6028950ce42fcf15537fdc63a23e)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/http/rest/transaction/TransactionWithChannelHandler.java

--- fe/fe-core/src/main/java/com/starrocks/http/rest/transaction/TransactionWithChannelHandler.java
+++ fe/fe-core/src/main/java/com/starrocks/http/rest/transaction/TransactionWithChannelHandler.java
@@ -22,6 +22,7 @@ import com.starrocks.http.rest.TransactionResult;
 import com.starrocks.http.rest.transaction.TransactionOperationParams.Channel;
 import com.starrocks.server.GlobalStateMgr;
 import com.starrocks.thrift.TNetworkAddress;
+import com.starrocks.warehouse.Warehouse;
 import org.apache.commons.lang3.Validate;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -57,9 +58,18 @@ public class TransactionWithChannelHandler implements TransactionOperationHandle
                     throw new DdlException(String.format(
                             "Channel ID should be between [0, %d].", (channel.getNum() - 1)));
                 }
+<<<<<<< HEAD
 
                 GlobalStateMgr.getCurrentState().getStreamLoadMgr().beginLoadTask(
                         dbName, tableName, label, "", "", timeoutMillis, channel.getNum(), channel.getId(), result);
+=======
+                String warehouseName = txnOperationParams.getWarehouseName();
+                Warehouse warehouse =
+                        GlobalStateMgr.getCurrentState().getWarehouseMgr().getWarehouse(warehouseName);
+                GlobalStateMgr.getCurrentState().getStreamLoadMgr().beginLoadTaskFromFrontend(
+                        dbName, tableName, label, "", "", timeoutMillis, channel.getNum(), channel.getId(), result,
+                        warehouse.getId());
+>>>>>>> c2c766692 ([BugFix] Fix warehouse property might not take effect when executing load stmts (#49684))
                 return new ResultWrapper(result);
             case TXN_PREPARE:
                 GlobalStateMgr.getCurrentState().getStreamLoadMgr().prepareLoadTask(
