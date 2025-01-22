376b5850e5b1e2208e93fd297a1c423a0ade9e48
stdpain <34912776+stdpain@users.noreply.github.com>
2025-01-22 11:22:20 +0800

    [BugFix] Fix wrong result when query cache work with select node (#55287)
    Signed-off-by: stdpain <drfeng08@gmail.com>
    (cherry picked from commit be65ed6621c3d30a079a3293b2dd14703ebf3589)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/planner/SelectNode.java

--- fe/fe-core/src/main/java/com/starrocks/planner/SelectNode.java
+++ fe/fe-core/src/main/java/com/starrocks/planner/SelectNode.java
@@ -37,14 +37,22 @@ package com.starrocks.planner;
 import com.starrocks.analysis.Analyzer;
 import com.starrocks.analysis.Expr;
 import com.starrocks.analysis.SlotId;
+<<<<<<< HEAD
 import com.starrocks.common.UserException;
+=======
+import com.starrocks.common.Pair;
+import com.starrocks.common.StarRocksException;
+>>>>>>> be65ed6621 ([BugFix] Fix wrong result when query cache work with select node (#55287))
 import com.starrocks.thrift.TExplainLevel;
+import com.starrocks.thrift.TNormalPlanNode;
+import com.starrocks.thrift.TNormalSelectNode;
 import com.starrocks.thrift.TPlanNode;
 import com.starrocks.thrift.TPlanNodeType;
 import com.starrocks.thrift.TSelectNode;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -82,6 +90,19 @@ public class SelectNode extends PlanNode {
 
     @Override
     public void computeStats(Analyzer analyzer) {
+    }
+
+    @Override
+    protected void toNormalForm(TNormalPlanNode planNode, FragmentNormalizer normalizer) {
+        TNormalSelectNode selectNode = new TNormalSelectNode();
+        if (commonSlotMap != null) {
+            Pair<List<Integer>, List<ByteBuffer>> slotIdsAndExprs = normalizer.normalizeSlotIdsAndExprs(commonSlotMap);
+            selectNode.setCse_slot_ids(slotIdsAndExprs.first);
+            selectNode.setCse_exprs(slotIdsAndExprs.second);
+        }
+        planNode.setSelect_node(selectNode);
+        planNode.setNode_type(TPlanNodeType.SELECT_NODE);
+        normalizeConjuncts(normalizer, planNode, conjuncts);
     }
 
     @Override
