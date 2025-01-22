5d35a7e032c2a29b411bc077f2a675997c866591
zhaohehuhu <luoyedeyi@163.com>
2025-01-22 16:24:29 +0800

    reformat
    Signed-off-by: zhaohehuhu <luoyedeyi@163.com>

--- java-extensions/hadoop-ext/src/main/java/com/starrocks/connector/share/iceberg/JavaImmutableMapSerializer.java
+++ java-extensions/hadoop-ext/src/main/java/com/starrocks/connector/share/iceberg/JavaImmutableMapSerializer.java
@@ -10,57 +10,57 @@ import java.util.Map;
 @SuppressWarnings("rawtypes")
 public class JavaImmutableMapSerializer extends Serializer<Map<?, ?>> {
 
-  @Override
-  public void write(Kryo kryo, Output output, Map<?, ?> map) {
-    ImmSerMapEntry[] entries = map
-      .entrySet()
-      .stream()
-      .map(ImmSerMapEntry::new)
-      .toArray(ImmSerMapEntry[]::new);
+    @Override
+    public void write(Kryo kryo, Output output, Map<?, ?> map) {
+        ImmSerMapEntry[] entries = map
+                .entrySet()
+                .stream()
+                .map(ImmSerMapEntry::new)
+                .toArray(ImmSerMapEntry[]::new);
 
-    kryo.writeObject(output, new ImmSerMap(entries));
-  }
-
-  @Override
-  public Map<?, ?> read(Kryo kryo, Input input, Class<Map<?, ?>> type) {
-    return Map.ofEntries(kryo.readObject(input, ImmSerMap.class).getEntries());
-  }
-
-
-  private static class ImmSerMap implements Serializable {
-
-    private final ImmSerMapEntry<?, ?>[] array;
-
-    private ImmSerMap(ImmSerMapEntry[] array) {
-      this.array = array;
-    }
-
-    private Map.Entry[] getEntries() {
-      return array;
-    }
-  }
-
-  private static class ImmSerMapEntry<K, V> implements Map.Entry<K, V>, Serializable {
-
-    private final Object[] entry;
-
-    private ImmSerMapEntry(Map.Entry<K, V> entry) {
-      this.entry = new Object[] { entry.getKey(), entry.getValue() };
+        kryo.writeObject(output, new ImmSerMap(entries));
     }
 
     @Override
-    public K getKey() {
-      return (K) entry[0];
+    public Map<?, ?> read(Kryo kryo, Input input, Class<Map<?, ?>> type) {
+        return Map.ofEntries(kryo.readObject(input, ImmSerMap.class).getEntries());
     }
 
-    @Override
-    public V getValue() {
-      return (V) entry[1];
+
+    private static class ImmSerMap implements Serializable {
+
+        private final ImmSerMapEntry<?, ?>[] array;
+
+        private ImmSerMap(ImmSerMapEntry[] array) {
+            this.array = array;
+        }
+
+        private Map.Entry[] getEntries() {
+            return array;
+        }
     }
 
-    @Override
-    public V setValue(V value) {
-      throw new UnsupportedOperationException();
+    private static class ImmSerMapEntry<K, V> implements Map.Entry<K, V>, Serializable {
+
+        private final Object[] entry;
+
+        private ImmSerMapEntry(Map.Entry<K, V> entry) {
+            this.entry = new Object[] { entry.getKey(), entry.getValue() };
+        }
+
+        @Override
+        public K getKey() {
+            return (K) entry[0];
+        }
+
+        @Override
+        public V getValue() {
+            return (V) entry[1];
+        }
+
+        @Override
+        public V setValue(V value) {
+            throw new UnsupportedOperationException();
+        }
     }
-  }
 }
