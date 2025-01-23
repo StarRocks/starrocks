d6f744496b52d7e77a920e12a6bc0d702200b7d3
silverbullet233 <3675229+silverbullet233@users.noreply.github.com>
2025-01-23 16:58:46 +0800

    fix compile error
    Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>

--- fe/fe-core/src/test/java/com/starrocks/utframe/StarRocksAssert.java
+++ fe/fe-core/src/test/java/com/starrocks/utframe/StarRocksAssert.java
@@ -631,6 +631,10 @@ public class StarRocksAssert {
         return this;
     }
 
+    public Database getDb(String dbName) {
+        return ctx.getGlobalStateMgr().getLocalMetastore().getDb(dbName);
+    }
+
     public StarRocksAssert withView(String sql, ExceptionRunnable action) throws Exception {
         CreateViewStmt createTableStmt = (CreateViewStmt) UtFrameUtils.parseStmtWithNewParser(sql, ctx);
         String viewName = createTableStmt.getTable();
