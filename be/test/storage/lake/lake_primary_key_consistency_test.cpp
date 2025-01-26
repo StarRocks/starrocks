9cc700a55c04af367e7c57ba820eec7b55041cdc
Yixin Luo <18810541851@163.com>
2025-01-26 10:48:24 +0800

    [Enhancement] enable load spill by default (#55361)
    Signed-off-by: luohaha <18810541851@163.com>

--- be/test/storage/lake/lake_primary_key_consistency_test.cpp
+++ be/test/storage/lake/lake_primary_key_consistency_test.cpp
@@ -390,6 +390,7 @@ public:
                                                    .set_mem_tracker(_mem_tracker.get())
                                                    .set_schema_id(_tablet_schema->id())
                                                    .set_slot_descriptors(&_slot_pointers)
+                                                   .set_profile(&_dummy_runtime_profile)
                                                    .build());
         RETURN_IF_ERROR(delta_writer->open());
         size_t upsert_size = _random_generator->random() % MaxUpsert;
@@ -425,6 +426,7 @@ public:
                                                    .set_schema_id(_tablet_schema->id())
                                                    .set_slot_descriptors(&_partial_slot_pointers)
                                                    .set_partial_update_mode(mode)
+                                                   .set_profile(&_dummy_runtime_profile)
                                                    .build());
         RETURN_IF_ERROR(delta_writer->open());
         size_t upsert_size = _random_generator->random() % MaxUpsert;
@@ -461,6 +463,7 @@ public:
                                                    .set_schema_id(_tablet_schema->id())
                                                    .set_slot_descriptors(&_slot_pointers)
                                                    .set_merge_condition(merge_condition)
+                                                   .set_profile(&_dummy_runtime_profile)
                                                    .build());
         RETURN_IF_ERROR(delta_writer->open());
         size_t upsert_size = _random_generator->random() % MaxUpsert;
@@ -492,6 +495,7 @@ public:
                                                        .set_mem_tracker(_mem_tracker.get())
                                                        .set_schema_id(_tablet_schema->id())
                                                        .set_slot_descriptors(&_slot_pointers)
+                                                       .set_profile(&_dummy_runtime_profile)
                                                        .build());
             RETURN_IF_ERROR(delta_writer->open());
             size_t upsert_size = _random_generator->random() % MaxUpsert;
@@ -529,6 +533,7 @@ public:
                                                    .set_mem_tracker(_mem_tracker.get())
                                                    .set_schema_id(_tablet_schema->id())
                                                    .set_slot_descriptors(&_slot_pointers)
+                                                   .set_profile(&_dummy_runtime_profile)
                                                    .build());
         RETURN_IF_ERROR(delta_writer->open());
         RETURN_IF_ERROR(
@@ -639,6 +644,7 @@ protected:
     std::vector<SlotDescriptor*> _partial_slot_pointers;
     Chunk::SlotHashMap _slot_cid_map;
     int64_t _version = 0;
+    RuntimeProfile _dummy_runtime_profile{"dummy"};
 
     int _seed = 0;
     int _run_second = 0;
