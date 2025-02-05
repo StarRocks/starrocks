4b6e6b83b49e5c35d16edbf8d867b6f3c15a409f
PengFei Li <lpengfei2016@gmail.com>
2025-02-05 14:41:01 +0800

    fix typo
    Signed-off-by: PengFei Li <lpengfei2016@gmail.com>

--- be/src/runtime/stream_load/stream_load_context.cpp
+++ be/src/runtime/stream_load/stream_load_context.cpp
@@ -316,7 +316,7 @@ bool StreamLoadContext::tsl_reach_idle_timeout(int32_t check_interval) {
     if (idle_timeout_sec <= 0) {
         return false;
     }
-    // if there is data to consume, the load is till active
+    // if there is data to consume, the load is still active
     std::shared_ptr<MessageBodySink> sink = body_sink;
     if (sink && !sink->exhausted()) {
         last_active_ts = UnixSeconds();
