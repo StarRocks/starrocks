bc534b95478a671e8fedc063a491fcee3aa1e9c0
sevev <qiangzh95@gmail.com>
2025-02-06 21:01:30 +0800

    Skip load primary index for empty rowset
    Signed-off-by: sevev <qiangzh95@gmail.com>

--- be/src/storage/lake/txn_log_applier.cpp
+++ be/src/storage/lake/txn_log_applier.cpp
@@ -249,11 +249,11 @@ private:
         _tablet.update_mgr()->lock_shard_pk_index_shard(_tablet.id());
         DeferOp defer([&]() { _tablet.update_mgr()->unlock_shard_pk_index_shard(_tablet.id()); });
 
-        RETURN_IF_ERROR(prepare_primary_index());
         if (op_write.dels_size() == 0 && op_write.rowset().num_rows() == 0 &&
             !op_write.rowset().has_delete_predicate()) {
             return Status::OK();
         }
+        RETURN_IF_ERROR(prepare_primary_index());
         if (is_column_mode_partial_update(op_write)) {
             return _tablet.update_mgr()->publish_column_mode_partial_update(op_write, txn_id, _metadata, &_tablet,
                                                                             &_builder, _base_version);
@@ -268,15 +268,19 @@ private:
         _tablet.update_mgr()->lock_shard_pk_index_shard(_tablet.id());
         DeferOp defer([&]() { _tablet.update_mgr()->unlock_shard_pk_index_shard(_tablet.id()); });
 
-        RETURN_IF_ERROR(prepare_primary_index());
         if (op_compaction.input_rowsets().empty()) {
             DCHECK(!op_compaction.has_output_rowset() || op_compaction.output_rowset().num_rows() == 0);
             // Apply the compaction operation to the cloud native pk index.
             // This ensures that the pk index is updated with the compaction changes.
             _builder.remove_compacted_sst(op_compaction);
+            if (op_compaction.input_sstables().empty() || !op_compaction.has_output_sstable()) {
+                return Status::OK();
+            }
+            RETURN_IF_ERROR(prepare_primary_index());
             RETURN_IF_ERROR(_index_entry->value().apply_opcompaction(*_metadata, op_compaction));
             return Status::OK();
         }
+        RETURN_IF_ERROR(prepare_primary_index());
         return _tablet.update_mgr()->publish_primary_compaction(op_compaction, txn_id, *_metadata, _tablet,
                                                                 _index_entry, &_builder, _base_version);
     }
