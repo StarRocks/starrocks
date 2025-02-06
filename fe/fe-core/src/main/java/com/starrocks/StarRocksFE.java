d45d55a25dfc3df8160b5d39fb37bd91ec3f6814
alprusty <alprusty77@gmail.com>
2025-02-06 02:36:14 -0800

    Add TLS/HTTPS support
    Signed-off-by: Amogh Margoor <amargoor@apache.org>

--- fe/fe-core/src/main/java/com/starrocks/StarRocksFE.java
+++ fe/fe-core/src/main/java/com/starrocks/StarRocksFE.java
@@ -183,9 +183,8 @@ public class StarRocksFE {
             // 4. ArrowFlightSqlService for Arrow Flight SQL Server
             QeService qeService = new QeService(Config.query_port, ExecuteEnv.getInstance().getScheduler());
             FrontendThriftServer frontendThriftServer = new FrontendThriftServer(Config.rpc_port);
-            HttpServer httpServer = new HttpServer(Config.http_port);
+            HttpServer httpServer = Config.enable_https ? new HttpServer(Config.https_port) : new HttpServer(Config.http_port);
             ArrowFlightSqlService arrowFlightSqlService = new ArrowFlightSqlService(Config.arrow_flight_port);
-
             httpServer.setup();
 
             frontendThriftServer.start();
