0c79371a7ecfdd20ee8f46b9af1b9148c1f15b24
gengjun-git <gengjun@starrocks.com>
2025-02-11 20:34:07 +0800

    [Enhancement] Compatible with mariadb client for ldap user login (#55720)
    Signed-off-by: gengjun-git <gengjun@starrocks.com>
    (cherry picked from commit fc39755dd79a2e6f95737c75dc3d622a9e4a561f)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/mysql/MysqlProto.java

--- fe/fe-core/src/main/java/com/starrocks/mysql/MysqlHandshakePacket.java
+++ fe/fe-core/src/main/java/com/starrocks/mysql/MysqlHandshakePacket.java
@@ -124,15 +124,15 @@ public class MysqlHandshakePacket extends MysqlPacket {
         }
     }
 
-    public boolean checkAuthPluginSameAsStarRocks(String pluginName) {
+    public static boolean checkAuthPluginSameAsStarRocks(String pluginName) {
         return SUPPORTED_PLUGINS.containsKey(pluginName) && SUPPORTED_PLUGINS.get(pluginName);
     }
 
     // If the auth default plugin in client is different from StarRocks
     // it will create a AuthSwitchRequest
-    public void buildAuthSwitchRequest(MysqlSerializer serializer) {
+    public void buildAuthSwitchRequest(MysqlSerializer serializer, String authPluginName) {
         serializer.writeInt1((byte) 0xfe);
-        serializer.writeNulTerminateString(NATIVE_AUTH_PLUGIN_NAME);
+        serializer.writeNulTerminateString(authPluginName);
         serializer.writeBytes(authPluginData);
         serializer.writeInt1(0);
     }
