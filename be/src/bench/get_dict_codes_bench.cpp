22a718aa3b0f48b642bc485d58ee6e843e9b48d7
trueeyu <lxhhust350@qq.com>
2025-02-12 19:41:52 +0800

    [Tool] Fix the compile problem of benchmark case (#55843)
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/bench/get_dict_codes_bench.cpp
+++ be/src/bench/get_dict_codes_bench.cpp
@@ -15,7 +15,6 @@
 #include <benchmark/benchmark.h>
 
 #include <map>
-#include <memory>
 #include <random>
 #include <vector>
 
@@ -75,7 +74,7 @@ static void BM_GetDictCodesWithMap(benchmark::State& state) {
         if (column->size() == 0) {
             continue;
         }
-        const std::vector<uint8_t>& null_data = down_cast<NullableColumn*>(column.get())->immutable_null_column_data();
+        const auto& null_data = down_cast<NullableColumn*>(column.get())->immutable_null_column_data();
         bool has_null = column->has_null();
         bool all_null = false;
 
@@ -90,7 +89,7 @@ static void BM_GetDictCodesWithMap(benchmark::State& state) {
 
         auto* dict_nullable_column = down_cast<NullableColumn*>(column.get());
         auto* dict_value_binary_column = down_cast<BinaryColumn*>(dict_nullable_column->data_column().get());
-        std::vector<Slice> dict_values_filtered = dict_value_binary_column->get_data();
+        auto dict_values_filtered = dict_value_binary_column->get_data();
         if (!has_null) {
             dict_codes.reserve(dict_values_filtered.size());
             for (size_t i = 0; i < dict_values_filtered.size(); i++) {
