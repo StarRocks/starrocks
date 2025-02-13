102cd27919a1b91b370ae58ac3daeca8511d180c
Murphy <96611012+murphyatwork@users.noreply.github.com>
2025-02-13 11:08:40 +0800

    [Feature] query cost predictor (#55759)
    Signed-off-by: Murphy <mofei@starrocks.com>
    (cherry picked from commit ed51cd1b63e04ba7ee50dbd4554bba7d35b4aac2)
    Signed-off-by: Murphy <mofei@starrocks.com>
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/qe/scheduler/slot/SlotEstimatorFactory.java
    #	fe/fe-core/src/test/java/com/starrocks/sql/optimizer/cost/PlanFeaturesTest.java

--- fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
@@ -195,6 +195,7 @@ import com.starrocks.sql.common.ErrorType;
 import com.starrocks.sql.common.MetaUtils;
 import com.starrocks.sql.common.StarRocksPlannerException;
 import com.starrocks.sql.optimizer.OptExpression;
+import com.starrocks.sql.optimizer.cost.feature.CostPredictor;
 import com.starrocks.sql.optimizer.dump.QueryDumpInfo;
 import com.starrocks.sql.optimizer.operator.physical.PhysicalValuesOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ScalarOperator;
@@ -1214,6 +1215,10 @@ public class StmtExecutor {
         return new DefaultCoordinator.Factory();
     }
 
+    private CostPredictor getCostPredictor() {
+        return CostPredictor.getServiceBasedCostPredictor();
+    }
+
     // Process a select statement.
     private void handleQueryStmt(ExecPlan execPlan) throws Exception {
         // Every time set no send flag and clean all data in buffer
@@ -1276,6 +1281,14 @@ public class StmtExecutor {
             coord = new FeExecuteCoordinator(context, execPlan);
         } else {
             coord = getCoordinatorFactory().createQueryScheduler(context, fragments, scanNodes, descTable);
+        }
+
+        // Predict the cost of this query
+        if (Config.enable_query_cost_prediction) {
+            CostPredictor predictor = getCostPredictor();
+            long memBytes = predictor.predictMemoryBytes(execPlan);
+            coord.setPredictedCost(memBytes);
+            context.getAuditEventBuilder().setPredictMemBytes(memBytes);
         }
 
         QeProcessorImpl.INSTANCE.registerQuery(context.getExecutionId(),
