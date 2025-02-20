b9baa2951d4f0acbbb40852e15e5118073a8d492
shuming.li <ming.moriarty@gmail.com>
2025-02-20 10:34:28 +0800

    [BugFix] Support text base mv rewrite for different dbs (#56001)
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/ast/CreateMaterializedViewStatement.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/ast/CreateMaterializedViewStatement.java
@@ -72,6 +72,8 @@ public class CreateMaterializedViewStatement extends DdlStmt {
     private String simpleViewDef;
     // original view definition of the mv query without any rewrite which can be used in text based rewrite.
     private String originalViewDefineSql;
+    // current db name when creating mv
+    private String originalDBName;
     private List<BaseTableInfo> baseTableInfos;
 
     // Maintenance information
@@ -110,6 +112,7 @@ public class CreateMaterializedViewStatement extends DdlStmt {
                                            Map<String, String> properties,
                                            QueryStatement queryStatement,
                                            int queryStartIndex,
+                                           String originalDBName,
                                            NodePosition pos) {
         super(pos);
         this.tableName = tableName;
@@ -124,6 +127,7 @@ public class CreateMaterializedViewStatement extends DdlStmt {
         this.properties = properties;
         this.queryStartIndex = queryStartIndex;
         this.queryStatement = queryStatement;
+        this.originalDBName = originalDBName;
     }
 
     public TableName getTableName() {
@@ -340,6 +344,9 @@ public class CreateMaterializedViewStatement extends DdlStmt {
 
     public Map<Expr, Expr> getPartitionByExprToAdjustExprMap() {
         return partitionByExprToAdjustExprMap;
+    }
+    public String getOriginalDBName() {
+        return originalDBName;
     }
 
     @Override
