c07b044111dd6fa6978a13605c3c1e370b93c177
PengFei Li <lpengfei2016@gmail.com>
2025-02-22 12:22:48 +0800

    support to get kafka partition/offset
    Signed-off-by: PengFei Li <lpengfei2016@gmail.com>

--- be/src/runtime/routine_load/kafka_consumer_pipe.h
+++ be/src/runtime/routine_load/kafka_consumer_pipe.h
@@ -55,6 +55,12 @@ public:
     ~KafkaConsumerPipe() override = default;
 
     Status append_with_row_delimiter(const char* data, size_t size, char row_delimiter) {
+        return append_with_row_delimiter(data, size, row_delimiter, -1, -1);
+    }
+
+    Status append_with_row_delimiter(const char* data, size_t size, char row_delimiter, int32_t partition,
+                                     int64_t offset) {
+        // TODO support to add partition and offset to buffer meta
         Status st = append(data, size);
         if (!st.ok()) {
             return st;
@@ -66,10 +72,22 @@ public:
     }
 
     Status append_json(const char* data, size_t size, char row_delimiter) {
+        return append_json(data, size, row_delimiter, -1, -1);
+    }
+
+    Status append_json(const char* data, size_t size, char row_delimiter, int32_t partition, int64_t offset) {
+        bool need_meta = partition >= 0 && offset >= 0;
         // For efficiency reasons, simdjson requires a string with a few bytes (simdjson::SIMDJSON_PADDING) at the end.
-        ASSIGN_OR_RETURN(auto buf, ByteBuffer::allocate_with_tracker(size + simdjson::SIMDJSON_PADDING));
+        ASSIGN_OR_RETURN(auto buf, ByteBuffer::allocate_with_tracker(
+                                           size + simdjson::SIMDJSON_PADDING,
+                                           need_meta ? ByteBufferMetaType::KAFKA : ByteBufferMetaType::NONE));
         buf->put_bytes(data, size);
         buf->flip();
+        if (need_meta) {
+            KafkaByteBufferMeta* meta = static_cast<KafkaByteBufferMeta*>(buf->meta());
+            meta->set_partition(partition);
+            meta->set_offset(offset);
+        }
         return append(std::move(buf));
     }
 };
