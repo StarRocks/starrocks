d8fce769c4bb6cdf85277fc1241286e55e5a494b
trueeyu <lxhhust350@qq.com>
2025-02-24 15:58:55 +0800

    format
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/util/lru_cache.cpp
+++ be/src/util/lru_cache.cpp
@@ -296,7 +296,7 @@ void LRUCache::_evict_one_entry(LRUHandle* e) {
 
 Cache::Handle* LRUCache::insert(const CacheKey& key, uint32_t hash, void* value, size_t value_size, size_t charge,
                                 void (*deleter)(const CacheKey& key, void* value), CachePriority priority) {
-    size_t key_mem_size = sizeof(LRUHandle) -1 + key.size();
+    size_t key_mem_size = sizeof(LRUHandle) - 1 + key.size();
     auto* e = reinterpret_cast<LRUHandle*>(malloc(key_mem_size));
     e->value = value;
     e->deleter = deleter;
