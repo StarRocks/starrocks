d3df1e778268a5c4e96a6f770352b22041148814
zhangqiang <qiangzh95@gmail.com>
2025-02-24 17:37:52 +0800

    [Enhancement] Optimize vacuum logic by adding vacuum version. (#55764)
    ## Why I'm doing:
    The FE (Frontend) will continuously issue vacuum requests to the CN (Compute Node) without any intelligent control, until the `lake_autovacuum_stale_partition_threshold` is reached, which is set to 12 hours by default. This approach presents two issues:
    
    1. **Incomplete Vacuuming**:  
       It is possible that after this time threshold has passed, the partition may not have completed the vacuum process. However, it will no longer be scheduled for vacuuming afterward, leaving some data unprocessed.
    
    2. **Redundant Requests**:  
       It is also possible that the partition has already been vacuumed, but the FE continues to issue vacuum requests indiscriminately. This results in unnecessary requests being sent, which not only wastes resources but also increases S3 API costs. 
    
    These issues highlight the need for a more intelligent scheduling mechanism to optimize vacuuming efficiency and reduce unnecessary costs.
    
    ## What I'm doing:
    Add  `lastSuccVacuumVersion` as the last success vacuum version, which means all garbage files before this version have been vacuumed and FE does not need to resend vacuum request if the `lastSuccVacuumVersion` is catch up the latest version.
    
    
    Signed-off-by: sevev <qiangzh95@gmail.com>

---
+++ fe/fe-core/src/test/java/com/starrocks/lake/VacuumTest.java
@@ -0,0 +1,190 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.starrocks.lake;
+
+import com.starrocks.catalog.Database;
+import com.starrocks.catalog.GlobalStateMgrTestUtil;
+import com.starrocks.catalog.OlapTable;
+import com.starrocks.catalog.PhysicalPartition;
+import com.starrocks.common.Config;
+import com.starrocks.common.FeConstants;
+import com.starrocks.lake.vacuum.AutovacuumDaemon;
+import com.starrocks.proto.StatusPB;
+import com.starrocks.proto.VacuumRequest;
+import com.starrocks.proto.VacuumResponse;
+import com.starrocks.qe.ConnectContext;
+import com.starrocks.rpc.BrpcProxy;
+import com.starrocks.rpc.LakeService;
+import com.starrocks.server.GlobalStateMgr;
+import com.starrocks.server.RunMode;
+import com.starrocks.server.WarehouseManager;
+import com.starrocks.system.ComputeNode;
+import com.starrocks.utframe.StarRocksAssert;
+import com.starrocks.utframe.UtFrameUtils;
+import com.starrocks.warehouse.Warehouse;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockedStatic;
+
+import java.util.Arrays;
+import java.util.concurrent.Future;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+
+public class VacuumTest {
+    private Database db;
+    private OlapTable olapTable;
+    private PhysicalPartition partition;
+    private WarehouseManager warehouseManager;
+    private ComputeNode computeNode;
+    private LakeService lakeService;
+    private ConnectContext connectContext;
+    protected StarRocksAssert starRocksAssert;
+
+
+    @Before
+    public void setUp() throws Exception {
+        FeConstants.runningUnitTest = true;
+        UtFrameUtils.createMinStarRocksCluster(RunMode.SHARED_DATA);
+        // create connect context
+        connectContext = UtFrameUtils.createDefaultCtx();
+        starRocksAssert = new StarRocksAssert(connectContext);
+        starRocksAssert.withDatabase(GlobalStateMgrTestUtil.testDb1)
+                    .useDatabase(GlobalStateMgrTestUtil.testDb1);
+
+        starRocksAssert.withTable("CREATE TABLE testTable1\n" +
+                    "(\n" +
+                    "    v1 date,\n" +
+                    "    v2 int,\n" +
+                    "    v3 int\n" +
+                    ")\n" +
+                    "DUPLICATE KEY(`v1`)\n" +
+                    "DISTRIBUTED BY HASH(v1) BUCKETS 1\n" +
+                    "PROPERTIES('replication_num' = '1');");
+
+        db = GlobalStateMgr.getCurrentState().getLocalMetastore().getDb(GlobalStateMgrTestUtil.testDb1);
+        olapTable = (OlapTable) GlobalStateMgr.getCurrentState().getLocalMetastore()
+                    .getTable(db.getFullName(), GlobalStateMgrTestUtil.testTable1);
+        partition = olapTable.getPhysicalPartitions().stream().findFirst().orElse(null);
+        partition.setVisibleVersion(10L, System.currentTimeMillis());
+        partition.setMinRetainVersion(10L);
+        partition.setLastSuccVacuumVersion(4L);
+
+        warehouseManager = mock(WarehouseManager.class);
+        computeNode = mock(ComputeNode.class);
+        
+
+        when(warehouseManager.getBackgroundWarehouse()).thenReturn(mock(Warehouse.class));
+        when(warehouseManager.getComputeNodeAssignedToTablet(anyString(), any(LakeTablet.class))).thenReturn(computeNode);
+
+        when(computeNode.getHost()).thenReturn("localhost");
+        when(computeNode.getBrpcPort()).thenReturn(8080);
+
+        
+    }
+
+    @After
+    public void clear() {
+        db.dropTable(olapTable.getName());
+    }
+
+    @Test
+    public void testLastSuccVacuumVersionUpdate() throws Exception {
+        GlobalStateMgr currentState = GlobalStateMgr.getCurrentState();
+        AutovacuumDaemon autovacuumDaemon = new AutovacuumDaemon();
+
+        VacuumResponse mockResponse = new VacuumResponse();
+        mockResponse.status = new StatusPB();
+        mockResponse.status.statusCode = 0;
+        mockResponse.vacuumedFiles = 10L;
+        mockResponse.vacuumedFileSize = 1024L;
+        mockResponse.vacuumedVersion = 5L;
+
+        Future<VacuumResponse> mockFuture = mock(Future.class);
+        when(mockFuture.get()).thenReturn(mockResponse);
+
+        lakeService = mock(LakeService.class);
+        when(lakeService.vacuum(any(VacuumRequest.class))).thenReturn(mockFuture);
+        try (MockedStatic<BrpcProxy> mockBrpcProxyStatic = mockStatic(BrpcProxy.class)) {
+            mockBrpcProxyStatic.when(() -> BrpcProxy.getLakeService(anyString(), anyInt())).thenReturn(lakeService);
+            autovacuumDaemon.testVacuumPartitionImpl(db, olapTable, partition);
+        }
+        
+        Assert.assertEquals(5L, partition.getLastSuccVacuumVersion());
+
+        mockResponse.vacuumedVersion = 7L;
+        try (MockedStatic<BrpcProxy> mockBrpcProxyStatic = mockStatic(BrpcProxy.class)) {
+            mockBrpcProxyStatic.when(() -> BrpcProxy.getLakeService(anyString(), anyInt())).thenReturn(lakeService);
+            autovacuumDaemon.testVacuumPartitionImpl(db, olapTable, partition);
+        }
+        Assert.assertEquals(7L, partition.getLastSuccVacuumVersion());
+    }
+
+    @Test
+    public void testLastSuccVacuumVersionUpdateFailed() throws Exception {
+        GlobalStateMgr currentState = GlobalStateMgr.getCurrentState();
+        AutovacuumDaemon autovacuumDaemon = new AutovacuumDaemon();
+
+        VacuumResponse mockResponse = new VacuumResponse();
+        mockResponse.status = new StatusPB();
+        mockResponse.status.statusCode = 1;
+        mockResponse.status.errorMsgs = Arrays.asList("internal failed");
+        mockResponse.vacuumedFiles = 10L;
+        mockResponse.vacuumedFileSize = 1024L;
+        mockResponse.vacuumedVersion = 5L;
+
+        Future<VacuumResponse> mockFuture = mock(Future.class);
+        when(mockFuture.get()).thenReturn(mockResponse);
+
+        lakeService = mock(LakeService.class);
+        when(lakeService.vacuum(any(VacuumRequest.class))).thenReturn(mockFuture);
+        try (MockedStatic<BrpcProxy> mockBrpcProxyStatic = mockStatic(BrpcProxy.class)) {
+            mockBrpcProxyStatic.when(() -> BrpcProxy.getLakeService(anyString(), anyInt())).thenReturn(lakeService);
+            autovacuumDaemon.testVacuumPartitionImpl(db, olapTable, partition);
+        }
+        
+        Assert.assertEquals(4L, partition.getLastSuccVacuumVersion());
+    }
+
+    @Test
+    public void testVacuumCheck() throws Exception {
+        AutovacuumDaemon autovacuumDaemon = new AutovacuumDaemon();
+        long current = System.currentTimeMillis();
+        // static
+        partition.setVisibleVersion(1L, current - Config.lake_autovacuum_stale_partition_threshold * 3600 * 1000);
+        Assert.assertFalse(autovacuumDaemon.shouldVacuum(partition));
+        // empty
+        partition.setVisibleVersion(1L, current);
+        Assert.assertFalse(autovacuumDaemon.shouldVacuum(partition));
+        // too frequency
+        partition.setVisibleVersion(10L, current);
+        partition.setLastVacuumTime(current);
+        Assert.assertFalse(autovacuumDaemon.shouldVacuum(partition));
+        // already vacuum success
+        partition.setLastVacuumTime(current - Config.lake_autovacuum_partition_naptime_seconds * 1000 * 6);
+        partition.setLastSuccVacuumVersion(10L);
+        Assert.assertFalse(autovacuumDaemon.shouldVacuum(partition));
+        // disable
+        Config.lake_autovacuum_detect_vaccumed_version = false;
+        Assert.assertTrue(autovacuumDaemon.shouldVacuum(partition));
+    }
+}
