745ffef8196bc3a24843ebf1e5d30460f05da79c
starrocks-xupeng <xupeng@starrocks.com>
2025-02-25 14:04:28 +0800

    [BugFix] fix rpc error for partitions_meta when partitions are too many (#56217)
    Signed-off-by: starrocks-xupeng <xupeng@starrocks.com>

--- gensrc/thrift/FrontendService.thrift
+++ gensrc/thrift/FrontendService.thrift
@@ -1476,6 +1476,8 @@ struct TGetTablesConfigResponse {
 
 struct TGetPartitionsMetaRequest {
     1: optional TAuthInfo auth_info
+    // get partitions where table id >= start_table_id_offset
+    2: optional i64 start_table_id_offset;
 }
 
 struct TPartitionMetaInfo {
@@ -1511,6 +1513,8 @@ struct TPartitionMetaInfo {
 
 struct TGetPartitionsMetaResponse {
     1: optional list<TPartitionMetaInfo> partitions_meta_infos
+    // max table id in partitions_meta_infos + 1, if set to 0, it means reaches end
+    2: optional i64 next_table_id_offset;
 }
 
 struct TGetTablesInfoRequest {
