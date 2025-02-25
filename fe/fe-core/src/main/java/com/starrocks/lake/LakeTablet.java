32f88abbc961fb464b41f0d0d0d3415fb4f18db0
xiangguangyxg <xiangguangyxg@gmail.com>
2025-02-25 11:42:18 +0800

    [Enhancement] Use min_version of tablet to avoid vacuum throwing file not found exception
    Signed-off-by: xiangguangyxg <xiangguangyxg@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/lake/LakeTablet.java
+++ fe/fe-core/src/main/java/com/starrocks/lake/LakeTablet.java
@@ -58,6 +58,8 @@ public class LakeTablet extends Tablet {
     @SerializedName(value = JSON_KEY_DATA_SIZE_UPDATE_TIME)
     private volatile long dataSizeUpdateTime = 0L;
 
+    private volatile long minVersion = 0L;
+
     public long rebuildPindexVersion = 0L;
 
     public LakeTablet(long id) {
@@ -84,6 +86,14 @@ public class LakeTablet extends Tablet {
 
     public long getDataSizeUpdateTime() {
         return dataSizeUpdateTime;
+    }
+
+    public long getMinVersion() {
+        return minVersion;
+    }
+
+    public void setMinVersion(long minVersion) {
+        this.minVersion = minVersion;
     }
 
     // version is not used
