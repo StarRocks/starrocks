66774e5781d3fd728896271e7d3cf0110cd61f66
Craig <117254390+click9000@users.noreply.github.com>
2025-02-25 11:37:54 -0800

    [Enhancement] Convert Trino function from_iso8601_timestamp -> timestamp (#55573)
    Signed-off-by: Craig <117254390+click9000@users.noreply.github.com>
    (cherry picked from commit f4d36ee1e498fafca72a29fbeab7c814b65918fa)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/connector/parser/trino/Trino2SRFunctionCallTransformer.java
    #	fe/fe-core/src/test/java/com/starrocks/connector/parser/trino/TrinoFunctionTransformTest.java

--- fe/fe-core/src/test/java/com/starrocks/connector/parser/trino/TrinoFunctionTransformTest.java
+++ fe/fe-core/src/test/java/com/starrocks/connector/parser/trino/TrinoFunctionTransformTest.java
@@ -211,6 +211,21 @@ public class TrinoFunctionTransformTest extends TrinoTestBase {
 
         sql = "select to_timestamp('2022-02-02', 'yyyy-mm-dd')";
         assertPlanContains(sql, " to_tera_timestamp('2022-02-02', 'yyyy-mm-dd')");
+<<<<<<< HEAD
+=======
+
+        sql = "select year_of_week('2022-02-02')";
+        assertPlanContains(sql, "<slot 2> : 2022");
+
+        sql = "select yow('2022-02-02')";
+        assertPlanContains(sql, "<slot 2> : 2022");
+
+        sql = "select from_iso8601_timestamp('2025-02-02 14:37:02')";
+        assertPlanContains(sql, "'2025-02-02 14:37:02'");
+
+        sql = "select from_iso8601_timestamp('2025-02-02')";
+        assertPlanContains(sql, "'2025-02-02 00:00:00'");
+>>>>>>> f4d36ee1e4 ([Enhancement] Convert Trino function from_iso8601_timestamp -> timestamp (#55573))
     }
 
     @Test
