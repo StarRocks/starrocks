73a4177da88e8e82a73679adf91eed5df7ac5bac
Gavin <yangguansuo@starrocks.com>
2025-02-27 15:10:28 +0800

    [Enhancement] Optimize the datacache automatical adjustment to support diffrent adjustment size for multiple disks. (#56235)
    Signed-off-by: GavinMar <yangguansuo@starrocks.com>

--- be/src/cache/block_cache/disk_space_monitor.h
+++ be/src/cache/block_cache/disk_space_monitor.h
@@ -14,6 +14,8 @@
 
 #pragma once
 
+#include <sys/stat.h>
+
 #include <atomic>
 #include <mutex>
 #include <thread>
@@ -27,15 +29,9 @@
 namespace starrocks {
 
 class BlockCache;
-class DiskSpaceMonitor {
-public:
-    struct DiskStats {
-        int disk_id = 0;
-        std::string path;
-        size_t capacity_bytes = 0;
-        size_t available_bytes = 0;
-    };
 
+class DiskSpace {
+public:
     // Wrap a new class to make it easy controlled in unittest.
     class FileSystemWrapper {
     public:
@@ -43,13 +39,73 @@ public:
 
         virtual StatusOr<size_t> directory_size(const std::string& dir);
 
-        virtual int disk_id(const std::string& path) { return DiskInfo::disk_id(path.c_str()); }
+        virtual dev_t device_id(const std::string& path);
 
         virtual ~FileSystemWrapper() {}
     };
 
+    struct DiskStats {
+        int64_t capacity_bytes = 0;
+        int64_t available_bytes = 0;
+
+        double used_rate() { return static_cast<double>(capacity_bytes - available_bytes) / capacity_bytes; }
+    };
+
+    struct AdjustContext {
+        size_t total_cache_quota = 0;
+        size_t total_cache_usage = 0;
+    };
+
+    DiskSpace(dev_t device_id, const std::string& path, std::shared_ptr<FileSystemWrapper> fs)
+            : _device_id(device_id), _path(path), _fs(fs) {}
+
+    Status init_spaces(const std::vector<DirSpace>& dir_spaces);
+
+    bool adjust_spaces(const AdjustContext& ctx);
+
+    std::vector<DirSpace>& dir_spaces() { return _dir_spaces; }
+
+    size_t total_cache_quota();
+
+    // The align unit when adjusting cache disk quota. We set it to 10G to keep consistent with underlying cache file size,
+    // which can help reduce processing the specail tail files.
+    const static size_t kQuotaAlignUnit;
+
+    // The cache usage threshold for automatically increase disk quota, if the cache usage is under this level,
+    // the disk expansion will be skipped.
+    // NOTICE: When users update the disk quota manually, this threshold will be ignored.
+    const static double kAutoIncreaseThreshold;
+
+private:
+    Status _update_disk_stats();
+
+    void _revise_disk_stats_by_cache_dir();
+
+    void _update_spaces_by_cache_usage(const AdjustContext& ctx);
+
+    void _update_spaces_by_cache_quota(size_t cache_avalil_bytes);
+
+    bool _allow_expansion(const AdjustContext& ctx);
+
+    size_t _check_cache_low_limit(int64_t cache_quota);
+
+    dev_t _device_id = 0;
+    std::string _path;
+    DiskStats _disk_stats;
+
+    // The datacache can be configured to have multiple data directories on the same physical disk.
+    std::vector<DirSpace> _dir_spaces;
+    std::shared_ptr<FileSystemWrapper> _fs = nullptr;
+    int64_t _disk_free_period = 0;
+    bool _disabled = false;
+};
+
+class DiskSpaceMonitor {
+public:
     DiskSpaceMonitor(BlockCache* cache);
     ~DiskSpaceMonitor();
+
+    Status init(std::vector<DirSpace>* dir_spaces);
 
     void start();
 
@@ -57,61 +113,30 @@ public:
 
     bool is_stopped();
 
-    bool adjust_spaces(std::vector<DirSpace>* dir_spaces);
+    std::vector<DirSpace> all_dir_spaces();
 
-    Status check_spaces();
-
-    Status adjust_cache_quota(const std::vector<DirSpace>& dir_spaces);
-
-    // The align unit when adjusting cache disk quota. We set it to 10G to keep consistent with underlying cache file size,
-    // which can help reduce processing the specail tail files.
-    const static size_t QUOTA_ALIGN_UNIT;
-    // The cache usage threshold for automatically increase disk quota, if the cache usage is under this level,
-    // the disk expansion will be skipped.
-    // NOTICE: When users update the disk quota manually, this threshold will be ignored.
-    const static int64_t AUTO_INCREASE_THRESHOLD;
+    static std::string to_string(const std::vector<DirSpace>& dir_spaces);
 
 private:
-    void _update_disk_stats();
-    void _update_cache_stats();
-    void _reset_spaces();
-    void _init_spaces_by_cache_dir();
-    void _update_spaces_by_cache_usage();
-    bool _adjust_spaces_by_disk_usage(bool immediate);
-
-    int64_t _max_disk_used_rate();
-
     void _adjust_datacache_callback();
 
-    void _reset() {
-        _dir_spaces.clear();
-        _disk_stats.clear();
-        _disk_to_dirs.clear();
-    }
+    bool _adjust_spaces_by_disk_usage();
 
-    std::vector<DirSpace> _dir_spaces;
-    // <disk_id, DiskStats>
-    std::unordered_map<int, DiskStats> _disk_stats;
+    void _update_cache_stats();
 
-    // The datacache can be configured to have multiple data directories on the same physical disk.
-    // <disk_id, dir_space_index_list>
-    std::unordered_map<int, std::vector<uint32_t>> _disk_to_dirs;
-    // Max directory count in one disk
-    size_t _max_disk_dirs = 0;
-    // Minimum directory count in one disk
-    size_t _min_disk_dirs = 0;
+    Status _update_cache_quota(const std::vector<DirSpace>& dir_spaces);
 
-    size_t _total_cache_usage = 0;
-    size_t _total_cache_quota = 0;
-    int64_t _disk_free_period = 0;
+    std::vector<DiskSpace> _disk_spaces;
 
     std::atomic<bool> _stopped = true;
-    std::atomic<bool> _adjusting = false;
+    std::atomic<bool> _updating = false;
     std::thread _adjust_datacache_thread;
     std::mutex _mutex;
 
-    std::unique_ptr<FileSystemWrapper> _fs = nullptr;
+    size_t _total_cache_usage = 0;
+    size_t _total_cache_quota = 0;
     BlockCache* _cache = nullptr;
+    std::shared_ptr<DiskSpace::FileSystemWrapper> _fs = nullptr;
 };
 
 } // namespace starrocks
