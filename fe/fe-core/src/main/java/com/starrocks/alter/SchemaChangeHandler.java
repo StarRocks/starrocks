2fe4a2357d2d36420854386328b71cfb377b5948
sevev <qiangzh95@gmail.com>
2025-02-28 00:10:19 +0800

    update
    Signed-off-by: sevev <qiangzh95@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/alter/SchemaChangeHandler.java
+++ fe/fe-core/src/main/java/com/starrocks/alter/SchemaChangeHandler.java
@@ -1833,6 +1833,9 @@ public class SchemaChangeHandler extends AlterHandler {
             throw new DdlException("olapTable is null");
         }
         boolean fastSchemaEvolution = olapTable.getUseFastSchemaEvolution();
+        if (RunMode.isSharedNothingMode() && olapTable.getKeysType() == PRIMARY_KEYS) {
+            fastSchemaEvolution = false;
+        }
         //for multi add colmuns clauses
         IntSupplier colUniqueIdSupplier = new IntSupplier() {
             private int pendingMaxColUniqueId = olapTable.getMaxColUniqueId();
