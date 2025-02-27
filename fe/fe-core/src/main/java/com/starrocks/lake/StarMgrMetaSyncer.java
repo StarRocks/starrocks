88c387e9536c5e9cb7a8f9e986b5102e4e7d25ee
srlch <linzichao@starrocks.com>
2025-02-27 11:30:06 +0800

    [BugFix] Fix files deleted incorrectly under cluster snapshot
    This pr fix several cases which will cause problem:
    
    case 1: In current impl, database deletion in Recycle Bin does not controlled cluster snapshot.
    If the database meta was deleted but the table should not be deleted. The StarMgrSyncer will
    get the wrong shard diff list and delete the table data incorrectly.
    Solution: Make sure database deletion was controlled by cluster snapshot.
    
    case 2: StarMgrSyncer::deleteUnusedShardAndShardGroup wa not controled by cluster snapshot
    which maybe delete the data incorrectly
    Solution: Make sure the table owned the group will be controled by cluster snapshot.
    
    Signed-off-by: srlch <linzichao@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/lake/StarMgrMetaSyncer.java
+++ fe/fe-core/src/main/java/com/starrocks/lake/StarMgrMetaSyncer.java
@@ -190,10 +190,24 @@ public class StarMgrMetaSyncer extends FrontendDaemon {
                 .collect(Collectors.toList());
         LOG.debug("diff.size is {}, diff: {}", diffList.size(), diffList);
 
+        Map<Long, Long> groupIdToTableId = new HashMap<>();
+        for (ShardGroupInfo shardInfo : shardGroupsInfo) {
+            String tableIdString = shardInfo.getLabels().get("tableId");
+            if (tableIdString != null) {
+                groupIdToTableId.put(shardInfo.getGroupId(), Long.parseLong(tableIdString));
+            }
+        }
+
         // 1.4.collect redundant shard groups and delete
         long nowMs = System.currentTimeMillis();
         List<Long> emptyShardGroup = new ArrayList<>();
         for (long groupId : diffList) {
+            long tableId = groupIdToTableId.get(groupId);
+            if (!GlobalStateMgr.getCurrentState().getClusterSnapshotMgr().isTableSafeToDeleteTablet(tableId)) {
+                LOG.debug("table with id: {} can not be delete shard for now, because of automated cluster snapshot", tableId);
+                continue;
+            }
+
             if (Config.shard_group_clean_threshold_sec * 1000L + Long.parseLong(groupToCreateTimeMap.get(groupId)) < nowMs) {
                 cleanOneGroup(groupId, starOSAgent, emptyShardGroup);
             }
