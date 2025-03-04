f671639257494a5d192e1cac529249118cd26da9
liaowanyou <67896450+liaowanyou@users.noreply.github.com>
2025-03-04 16:10:25 +0800

    [BugFix] Fix some odps bug (#55802)
    Signed-off-by: wanyou <liaowanyou.lwy@alibaba-inc.com>
    Co-authored-by: liaowanyou <liaowanyou.lwy@alibaba-inc.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/operator/pattern/MultiOpPattern.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/operator/pattern/MultiOpPattern.java
@@ -34,6 +34,7 @@ public class MultiOpPattern extends Pattern {
             .add(OperatorType.LOGICAL_BINLOG_SCAN)
             .add(OperatorType.LOGICAL_VIEW_SCAN)
             .add(OperatorType.LOGICAL_PAIMON_SCAN)
+            .add(OperatorType.LOGICAL_ODPS_SCAN)
             .add(OperatorType.PATTERN_SCAN)
             .build();
 
