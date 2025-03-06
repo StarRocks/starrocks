78d99c41e8eadf1d3739b10a5dabae82146574cf
srlch <111035020+srlch@users.noreply.github.com>
2025-03-06 13:49:10 +0800

    [BugFix] Reset index information when creating tablet in rollup job (#56553)
    Signed-off-by: srlch <linzichao@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/alter/LakeTableSchemaChangeJob.java
+++ fe/fe-core/src/main/java/com/starrocks/alter/LakeTableSchemaChangeJob.java
@@ -383,7 +383,8 @@ public class LakeTableSchemaChangeJob extends LakeTableSchemaChangeJobBase {
                             .setShortKeyColumnCount(shadowShortKeyColumnCount)
                             .setSortKeyIndexes(originIndexId == baseIndexId ? sortKeyIdxes : null)
                             .setSortKeyUniqueIds(originIndexId == baseIndexId ? sortKeyUniqueIds : null)
-                            .setIndexes(indexes)
+                            .setIndexes(originIndexId == baseIndexId ?
+                                        indexes : OlapTable.getIndexesBySchema(indexes, shadowSchema))
                             .setBloomFilterColumnNames(bfColumns)
                             .setBloomFilterFpp(bfFpp)
                             .setStorageType(TStorageType.COLUMN)
