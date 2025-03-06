2786f5962c6b776696532d06a93b851512fee42b
Yixin Luo <18810541851@163.com>
2025-03-06 02:29:54 +0000

    resolve
    Signed-off-by: Yixin Luo <18810541851@163.com>

--- be/test/storage/lake/partial_update_test.cpp
+++ be/test/storage/lake/partial_update_test.cpp
@@ -1486,7 +1486,7 @@ TEST_P(LakePartialUpdateTest, test_max_buffer_rows) {
                                                    .build());
         ASSERT_OK(delta_writer->open());
         ASSERT_OK(delta_writer->write(chunk0, indexes.data(), indexes.size()));
-        ASSERT_OK(delta_writer->finish_with_txnlog());
+        ASSERT_OK(delta_writer->finish());
         delta_writer->close();
         EXPECT_TRUE(_update_mgr->update_state_mem_tracker()->consumption() > 0);
         // Publish version
@@ -1523,7 +1523,7 @@ TEST_P(LakePartialUpdateTest, test_max_buffer_rows) {
         ASSERT_OK(delta_writer->open());
         ASSERT_OK(delta_writer->write(chunk1, indexes.data(), indexes.size()));
         ASSERT_OK(delta_writer->write(chunk1, indexes.data(), indexes.size()));
-        ASSERT_OK(delta_writer->finish_with_txnlog());
+        ASSERT_OK(delta_writer->finish());
         delta_writer->close();
         // Publish version
         ASSERT_OK(publish_single_version(tablet_id, version + 1, txn_id).status());
