5f95c32591f6761b21b0c439e74a42968f22a96a
zombee0 <ewang2027@gmail.com>
2025-03-07 09:41:20 +0800

    [Enhancement]cache aware dictionary decoder with filter
    Signed-off-by: zombee0 <ewang2027@gmail.com>

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -944,6 +944,7 @@ CONF_Bool(parquet_page_index_enable, "true");
 CONF_mBool(parquet_statistics_process_more_filter_enable, "true");
 CONF_mBool(parquet_fast_timezone_conversion, "false");
 CONF_mBool(parquet_push_down_filter_to_decoder_enable, "true");
+CONF_mBool(parquet_cache_aware_dict_decoder_enable, "true");
 
 CONF_Int32(io_coalesce_read_max_buffer_size, "8388608");
 CONF_Int32(io_coalesce_read_max_distance_size, "1048576");
