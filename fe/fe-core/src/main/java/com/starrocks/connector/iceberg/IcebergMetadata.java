d8c23794313855605ab6b2522a3e334fc58ae3f4
huangshiye <hpp@ccc.com>
2025-03-07 09:33:23 +0800

    [BugFix] should not clean data when CommitStateUnknownException, HiveMetaStoreClient send heartbeat

--- fe/fe-core/src/main/java/com/starrocks/connector/iceberg/IcebergMetadata.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/iceberg/IcebergMetadata.java
@@ -108,6 +108,7 @@ import org.apache.iceberg.StructLike;
 import org.apache.iceberg.TableScan;
 import org.apache.iceberg.Transaction;
 import org.apache.iceberg.catalog.TableIdentifier;
+import org.apache.iceberg.exceptions.CommitStateUnknownException;
 import org.apache.iceberg.exceptions.NoSuchNamespaceException;
 import org.apache.iceberg.exceptions.NoSuchTableException;
 import org.apache.iceberg.expressions.Expression;
@@ -1190,10 +1191,12 @@ public class IcebergMetadata implements ConnectorMetadata {
             transaction.commitTransaction();
             asyncRefreshOthersFeMetadataCache(dbName, tableName);
         } catch (Exception e) {
-            List<String> toDeleteFiles = dataFiles.stream()
-                    .map(TIcebergDataFile::getPath)
-                    .collect(Collectors.toList());
-            icebergCatalog.deleteUncommittedDataFiles(toDeleteFiles);
+            if (!(e instanceof CommitStateUnknownException)) {
+                List<String> toDeleteFiles = dataFiles.stream()
+                        .map(TIcebergDataFile::getPath)
+                        .collect(Collectors.toList());
+                icebergCatalog.deleteUncommittedDataFiles(toDeleteFiles);
+            }
             LOG.error("Failed to commit iceberg transaction on {}.{}", dbName, tableName, e);
             throw new StarRocksConnectorException(e.getMessage());
         } finally {
