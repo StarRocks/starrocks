9bb15cb0e4f46fba504c237a8a94bfff4246d19f
zhangqiang <qiangzh95@gmail.com>
2025-03-07 17:47:35 +0800

    [Refactor] Add brief log (#56300)
    Signed-off-by: sevev <qiangzh95@gmail.com>

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -1589,4 +1589,6 @@ CONF_mBool(enable_collect_table_metrics, "true");
 CONF_mInt64(rf_sample_rows, "1024");
 CONF_mInt64(rf_sample_ratio, "32");
 CONF_mInt64(rf_branchless_ratio, "8");
+
+CONF_mInt32(big_query_sec, "1");
 } // namespace starrocks::config
