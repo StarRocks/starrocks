b5db10f32e19bf1b9f0abc24430bdfd147987311
Craig <117254390+click9000@users.noreply.github.com>
2025-03-08 22:52:23 -0800

    Update ComplexFunctionCallTransformer.java
    Remove all ' characters for the format parameter, plus removing all Z characters because str_to_jodatime doesn't support it (when it's OK in Trino)
    
    Signed-off-by: Craig <117254390+click9000@users.noreply.github.com>

--- fe/fe-core/src/main/java/com/starrocks/connector/parser/trino/ComplexFunctionCallTransformer.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/parser/trino/ComplexFunctionCallTransformer.java
@@ -120,7 +120,7 @@ public class ComplexFunctionCallTransformer {
             // parse_datetime -> str_to_jodatime
             String formatString = ((StringLiteral) args[1]).getStringValue();
             // "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'" -> "yyyy-MM-ddTHH:mm:ss.SSS"
-            formatString = formatString.replace("'T'", "T").replace("'Z'", "");
+            formatString = formatString.replace("'", "").replace("Z", "");
             return new FunctionCallExpr("str_to_jodatime", java.util.List.of(args[0], new StringLiteral(formatString)));
         }
         return null;
