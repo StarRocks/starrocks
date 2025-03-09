a6ad55ce0feeeb2c9fb36e3c3652d0a55860a83c
ClownXC <598457447@qq.com>
2025-03-10 05:44:42 +0800

    [BugFix] Fix _wait_for_version timeout Caused by Compact
    Signed-off-by: ClownXC <598457447@qq.com>

--- be/src/storage/tablet_updates.cpp
+++ be/src/storage/tablet_updates.cpp
@@ -1895,10 +1895,10 @@ Status TabletUpdates::_wait_for_version(const EditVersion& version, int64_t time
             LOG(WARNING) << msg;
             return Status::InternalError(msg);
         }
-        if (now - wait_start > timeout_ms) {
+        if (!is_compaction && now - wait_start > timeout_ms) {
             string msg = strings::Substitute("wait_for_version timeout($0ms) version:$1 $2", now - wait_start,
                                              version.to_string(), _debug_string(false, true));
-            LOG_IF(WARNING, !is_compaction) << msg;
+            LOG_IF(WARNING) << msg;
             return Status::TimedOut(msg);
         }
     }
