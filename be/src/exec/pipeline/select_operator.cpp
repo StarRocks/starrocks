StarRocks/starrocks

commit e66fcdb8ad7b17ef9f6ec3eb2a8bed61172d591a

[Enhancement] support common expr reuse strategy on scan predicates (#52889)
Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>

be/src/exec/pipeline/select_operator.cpp
@@ -85,8 +85,26 @@ bool SelectOperator::need_input() const {
 }
 
 Status SelectOperator::push_chunk(RuntimeState* state, const ChunkPtr& chunk) {
-    RETURN_IF_ERROR(eval_conjuncts_and_in_filters(_conjunct_ctxs, chunk.get()));
-    _curr_chunk = chunk;
+    if (!_common_exprs.empty()) {
+        _curr_chunk = std::make_shared<Chunk>();
+        for (const auto& [slot_id, _] : chunk->get_slot_id_to_index_map()) {
+            _curr_chunk->append_column(chunk->get_column_by_slot_id(slot_id), slot_id);
+        }
+        SCOPED_TIMER(_conjuncts_timer);
+        for (const auto& [slot_id, expr] : _common_exprs) {
+            ASSIGN_OR_RETURN(auto col, expr->evaluate(_curr_chunk.get()));
+            _curr_chunk->append_column(col, slot_id);
+        }
+    } else {
+        _curr_chunk = chunk;
+    }
+    RETURN_IF_ERROR(eval_conjuncts_and_in_filters(_conjunct_ctxs, _curr_chunk.get()));
+    {
+        // common_exprs' slots are only needed inside this operator, no need to pass it downsteam
+        for (const auto& [slot_id, _] : _common_exprs) {
+            _curr_chunk->remove_column_by_slot_id(slot_id);
+        }
+    }
     return Status::OK();
 }
 
@@ -100,7 +118,14 @@ Status SelectOperator::reset_state(starrocks::RuntimeState* state, const std::ve
 Status SelectOperatorFactory::prepare(RuntimeState* state) {
     RETURN_IF_ERROR(OperatorFactory::prepare(state));
     RETURN_IF_ERROR(Expr::prepare(_conjunct_ctxs, state));
+    std::vector<ExprContext*> common_expr_ctxs;
+    for (const auto& [_, ctx] : _common_exprs) {
+        common_expr_ctxs.emplace_back(ctx);
+    }
+    RETURN_IF_ERROR(Expr::prepare(common_expr_ctxs, state));
+
     RETURN_IF_ERROR(Expr::open(_conjunct_ctxs, state));
+    RETURN_IF_ERROR(Expr::open(common_expr_ctxs, state));
     return Status::OK();
 }
 

be/src/exec/pipeline/select_operator.h
@@ -25,8 +25,10 @@ namespace pipeline {
 class SelectOperator final : public Operator {
 public:
     SelectOperator(OperatorFactory* factory, int32_t id, int32_t plan_node_id, int32_t driver_sequence,
-                   const std::vector<ExprContext*>& conjunct_ctxs)
-            : Operator(factory, id, "select", plan_node_id, false, driver_sequence), _conjunct_ctxs(conjunct_ctxs) {}
+                   const std::vector<ExprContext*>& conjunct_ctxs, const std::map<SlotId, ExprContext*>& common_exprs)
+            : Operator(factory, id, "select", plan_node_id, false, driver_sequence),
+              _conjunct_ctxs(conjunct_ctxs),
+              _common_exprs(common_exprs) {}
 
     ~SelectOperator() override = default;
     Status prepare(RuntimeState* state) override;
@@ -53,19 +55,24 @@ private:
     ChunkPtr _pre_output_chunk = nullptr;
 
     const std::vector<ExprContext*>& _conjunct_ctxs;
+    const std::map<SlotId, ExprContext*>& _common_exprs;
 
     bool _is_finished = false;
 };
 
 class SelectOperatorFactory final : public OperatorFactory {
 public:
-    SelectOperatorFactory(int32_t id, int32_t plan_node_id, std::vector<ExprContext*>&& conjunct_ctxs)
-            : OperatorFactory(id, "select", plan_node_id), _conjunct_ctxs(std::move(conjunct_ctxs)) {}
+    SelectOperatorFactory(int32_t id, int32_t plan_node_id, std::vector<ExprContext*>&& conjunct_ctxs,
+                          std::map<SlotId, ExprContext*>&& common_exprs)
+            : OperatorFactory(id, "select", plan_node_id),
+              _conjunct_ctxs(std::move(conjunct_ctxs)),
+              _common_exprs(std::move(common_exprs)) {}
 
     ~SelectOperatorFactory() override = default;
 
     OperatorPtr create(int32_t degree_of_parallelism, int32_t driver_sequence) override {
-        return std::make_shared<SelectOperator>(this, _id, _plan_node_id, driver_sequence, _conjunct_ctxs);
+        return std::make_shared<SelectOperator>(this, _id, _plan_node_id, driver_sequence, _conjunct_ctxs,
+                                                _common_exprs);
     }
 
     Status prepare(RuntimeState* state) override;
@@ -73,6 +80,7 @@ public:
 
 private:
     std::vector<ExprContext*> _conjunct_ctxs;
+    std::map<SlotId, ExprContext*> _common_exprs;
 };
 
 } // namespace pipeline

be/src/exec/select_node.cpp
@@ -51,6 +51,18 @@ SelectNode::~SelectNode() {
     }
 }
 
+Status SelectNode::init(const TPlanNode& tnode, RuntimeState* state) {
+    RETURN_IF_ERROR(ExecNode::init(tnode, state));
+    if (tnode.__isset.select_node && tnode.select_node.__isset.common_slot_map) {
+        for (const auto& [key, val] : tnode.select_node.common_slot_map) {
+            ExprContext* context;
+            RETURN_IF_ERROR(Expr::create_expr_tree(_pool, val, &context, state, true));
+            _common_expr_ctxs.insert({key, context});
+        }
+    }
+    return Status::OK();
+}
+
 Status SelectNode::prepare(RuntimeState* state) {
     RETURN_IF_ERROR(ExecNode::prepare(state));
     _conjunct_evaluate_timer = ADD_TIMER(_runtime_profile, "ConjunctEvaluateTime");
@@ -108,8 +120,8 @@ pipeline::OpFactories SelectNode::decompose_to_pipeline(pipeline::PipelineBuilde
 
     OpFactories operators = _children[0]->decompose_to_pipeline(context);
 
-    operators.emplace_back(
-            std::make_shared<SelectOperatorFactory>(context->next_operator_id(), id(), std::move(_conjunct_ctxs)));
+    operators.emplace_back(std::make_shared<SelectOperatorFactory>(
+            context->next_operator_id(), id(), std::move(_conjunct_ctxs), std::move(_common_expr_ctxs)));
 
     // Create a shared RefCountedRuntimeFilterCollector
     auto&& rc_rf_probe_collector = std::make_shared<RcRfProbeCollector>(1, std::move(this->runtime_filter_collector()));

be/src/exec/select_node.h
@@ -47,6 +47,7 @@ public:
     SelectNode(ObjectPool* pool, const TPlanNode& tnode, const DescriptorTbl& descs);
     ~SelectNode() override;
 
+    Status init(const TPlanNode& tnode, RuntimeState* state) override;
     Status prepare(RuntimeState* state) override;
     Status open(RuntimeState* state) override;
     Status get_next(RuntimeState* state, ChunkPtr* chunk, bool* eos) override;
@@ -55,6 +56,8 @@ public:
             pipeline::PipelineBuilderContext* context) override;
 
 private:
+    std::map<SlotId, ExprContext*> _common_expr_ctxs;
+
     RuntimeProfile::Counter* _conjunct_evaluate_timer = nullptr;
 };
 

fe/fe-core/src/main/java/com/starrocks/planner/SelectNode.java
@@ -36,20 +36,25 @@ package com.starrocks.planner;
 
 import com.starrocks.analysis.Analyzer;
 import com.starrocks.analysis.Expr;
+import com.starrocks.analysis.SlotId;
 import com.starrocks.common.StarRocksException;
 import com.starrocks.thrift.TExplainLevel;
 import com.starrocks.thrift.TPlanNode;
 import com.starrocks.thrift.TPlanNodeType;
+import com.starrocks.thrift.TSelectNode;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Node that applies conjuncts and a limit clause. Has exactly one child.
  */
 public class SelectNode extends PlanNode {
     private static final Logger LOG = LogManager.getLogger(SelectNode.class);
+    private Map<SlotId, Expr> commonSlotMap;
 
     public SelectNode(PlanNodeId id, PlanNode child, List<Expr> conjuncts) {
         super(id, child.getTupleIds(), "SELECT");
@@ -58,9 +63,17 @@ public class SelectNode extends PlanNode {
         this.conjuncts.addAll(conjuncts);
     }
 
+    public void setCommonSlotMap(Map<SlotId, Expr> commonSlotMap) {
+        this.commonSlotMap = commonSlotMap;
+    }
+
     @Override
     protected void toThrift(TPlanNode msg) {
         msg.node_type = TPlanNodeType.SELECT_NODE;
+        msg.select_node = new TSelectNode();
+        if (commonSlotMap != null) {
+            commonSlotMap.forEach((key, value) -> msg.select_node.putToCommon_slot_map(key.asInt(), value.treeToThrift()));
+        }
     }
 
     @Override
@@ -76,6 +89,13 @@ public class SelectNode extends PlanNode {
         StringBuilder output = new StringBuilder();
         if (!conjuncts.isEmpty()) {
             output.append(prefix + "predicates: " + getExplainString(conjuncts) + "\n");
+            if (commonSlotMap != null && !commonSlotMap.isEmpty()) {
+                output.append(prefix + "  common sub expr:" + "\n");
+                for (Map.Entry<SlotId, Expr> entry : commonSlotMap.entrySet()) {
+                    output.append(prefix + "  <slot " + entry.getKey().toString() + "> : "
+                            + getExplainString(Arrays.asList(entry.getValue())) + "\n");
+                }
+            }
         }
         return output.toString();
     }

fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
@@ -833,6 +833,8 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
 
     public static final String ENABLE_REWRITE_UNNEST_BITMAP_TO_ARRAY = "enable_rewrite_unnest_bitmap_to_array";
 
+    public static final String ENABLE_SCAN_PREDICATE_EXPR_REUSE = "enable_scan_predicate_expr_reuse";
+
     public static final List<String> DEPRECATED_VARIABLES = ImmutableList.<String>builder()
             .add(CODEGEN_LEVEL)
             .add(MAX_EXECUTION_TIME)
@@ -1624,6 +1626,9 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
 
     @VarAttr(name = DISABLE_GENERATED_COLUMN_REWRITE, flag = VariableMgr.INVISIBLE)
     private boolean disableGeneratedColumnRewrite = false;
+
+    @VarAttr(name = ENABLE_SCAN_PREDICATE_EXPR_REUSE, flag = VariableMgr.INVISIBLE)
+    private boolean enableScanPredicateExprReuse = true;
 
     public int getCboPruneJsonSubfieldDepth() {
         return cboPruneJsonSubfieldDepth;
@@ -4396,6 +4401,10 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
 
     public boolean isDisableGeneratedColumnRewrite() {
         return disableGeneratedColumnRewrite;
+    }
+
+    public boolean isEnableScanPredicateExprReuse() {
+        return enableScanPredicateExprReuse;
     }
 
     public int getConnectorIncrementalScanRangeNumber() {

fe/fe-core/src/main/java/com/starrocks/sql/optimizer/MaterializedViewOptimizer.java
@@ -51,6 +51,7 @@ public class MaterializedViewOptimizer {
         optimizerConfig.disableRule(RuleType.TF_MV_TEXT_MATCH_REWRITE_RULE);
         optimizerConfig.disableRule(RuleType.TF_MV_TRANSPARENT_REWRITE_RULE);
         optimizerConfig.disableRule(RuleType.TF_ELIMINATE_AGG);
+        optimizerConfig.disableRule(RuleType.TF_PULL_UP_PREDICATE_SCAN);
         // For sync mv, no rewrite query by original sync mv rule to avoid useless rewrite.
         if (mv.getRefreshScheme().isSync()) {
             optimizerConfig.disableRule(RuleType.TF_MATERIALIZED_VIEW);

fe/fe-core/src/main/java/com/starrocks/sql/optimizer/Optimizer.java
@@ -65,6 +65,7 @@ import com.starrocks.sql.optimizer.rule.transformation.OnPredicateMoveAroundRule
 import com.starrocks.sql.optimizer.rule.transformation.PartitionColumnMinMaxRewriteRule;
 import com.starrocks.sql.optimizer.rule.transformation.PartitionColumnValueOnlyOnScanRule;
 import com.starrocks.sql.optimizer.rule.transformation.PruneEmptyWindowRule;
+import com.starrocks.sql.optimizer.rule.transformation.PullUpScanPredicateRule;
 import com.starrocks.sql.optimizer.rule.transformation.PushDownAggregateGroupingSetsRule;
 import com.starrocks.sql.optimizer.rule.transformation.PushDownJoinOnExpressionToChildProject;
 import com.starrocks.sql.optimizer.rule.transformation.PushDownLimitRankingWindowRule;
@@ -694,6 +695,9 @@ public class Optimizer {
         ruleRewriteOnlyOnce(tree, rootTaskContext, UnionToValuesRule.getInstance());
 
         ruleRewriteOnlyOnce(tree, rootTaskContext, RuleSetType.VECTOR_REWRITE);
+        // this rule should be after mv
+        // @TODO: it can also be applied to other table scan operator
+        ruleRewriteOnlyOnce(tree, rootTaskContext, PullUpScanPredicateRule.OLAP_SCAN);
 
         tree = SimplifyCaseWhenPredicateRule.INSTANCE.rewrite(tree, rootTaskContext);
         deriveLogicalProperty(tree);

commit 659c1710b8422cd86e1d011f99064746664482a8

[BugFix] prevent to change column order in SelectOperator::push_chunk
bug introduced in https://github.com/StarRocks/starrocks/pull/52889

iteration order by unordered map is not defined `chunk->get_slot_id_to_index_map()`

so after generate new chunk later in SelectOperator::pull_chunk we can crash

because `_pre_output_chunk` and `_curr_chunk` can have different columns order inside

if types on `append` is matched, final data will be incorrect (merge data from different columns)

if types mismatched, BE will crash

Signed-off-by: Aliaksei Dziomin <diominay@gmail.com>

be/src/exec/pipeline/select_operator.cpp
@@ -85,18 +85,13 @@ bool SelectOperator::need_input() const {
 }
 
 Status SelectOperator::push_chunk(RuntimeState* state, const ChunkPtr& chunk) {
+    _curr_chunk = chunk;
     if (!_common_exprs.empty()) {
-        _curr_chunk = std::make_shared<Chunk>();
-        for (const auto& [slot_id, _] : chunk->get_slot_id_to_index_map()) {
-            _curr_chunk->append_column(chunk->get_column_by_slot_id(slot_id), slot_id);
-        }
         SCOPED_TIMER(_conjuncts_timer);
         for (const auto& [slot_id, expr] : _common_exprs) {
             ASSIGN_OR_RETURN(auto col, expr->evaluate(_curr_chunk.get()));
             _curr_chunk->append_column(col, slot_id);
         }
-    } else {
-        _curr_chunk = chunk;
     }
     RETURN_IF_ERROR(eval_conjuncts_and_in_filters(_conjunct_ctxs, _curr_chunk.get()));
     {