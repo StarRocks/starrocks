493a44c47cc6c81ca38b66261ab79977a3083fa5
before-Sunrise <71162020+before-Sunrise@users.noreply.github.com>
2025-03-12 13:39:52 +0800

    [BugFix] Fix array contains reuse null column (#56810)
    Signed-off-by: before-Sunrise <unclejyj@gmail.com>

--- be/src/exprs/array_functions.cpp
+++ be/src/exprs/array_functions.cpp
@@ -57,7 +57,8 @@ StatusOr<ColumnPtr> ArrayFunctions::array_length([[maybe_unused]] FunctionContex
 
         if (arg0->has_null()) {
             // Copy null flags.
-            return NullableColumn::create(std::move(col_result), down_cast<NullableColumn*>(arg0)->null_column());
+            return NullableColumn::create(std::move(col_result),
+                                          down_cast<const NullableColumn*>(arg0)->null_column()->clone());
         } else {
             return col_result;
         }
@@ -716,7 +717,7 @@ private:
         if (data_column->is_nullable()) {
             DCHECK_EQ(nullable_column->size(), result->size());
             if (nullable_column->has_null()) {
-                result = NullableColumn::create(std::move(result), nullable_column->null_column());
+                result = NullableColumn::create(std::move(result), nullable_column->null_column()->clone());
             }
         }
 
