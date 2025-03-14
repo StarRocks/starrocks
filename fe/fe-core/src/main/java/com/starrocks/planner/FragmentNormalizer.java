0ee5d8ecdcb724e385ddbe9787373af8d2fe2a4e
satanson <ranpanf@gmail.com>
2025-03-14 10:17:21 +0800

    [BugFix] MV partitioned by non-SlotRef Expr can not be decomposed in query cache (backport #56871)
    Signed-off-by: satanson <ranpanf@gmail.com>
    (cherry picked from commit 7b82d5fe2bf7655646a7894696f8e125002f36a4)
    Signed-off-by: satanson <ranpanf@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/planner/FragmentNormalizer.java
+++ fe/fe-core/src/main/java/com/starrocks/planner/FragmentNormalizer.java
@@ -61,6 +61,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.Stack;
 import java.util.stream.Collectors;
@@ -573,11 +574,17 @@ public class FragmentNormalizer {
             if (range.isEmpty()) {
                 continue;
             }
-            range = toClosedOpenRange(range);
+
+            Optional<Range> optRange = Optional.empty();
+            try {
+                optRange = Optional.ofNullable(toClosedOpenRange(range));
+            } catch (Throwable ignored) {
+            }
+
             Map.Entry<Long, Range<PartitionKey>> partitionKeyRange = rangeMap.get(i);
             // when the range is to total cover this partition, we also cache it
-            if (!range.isEmpty()) {
-                selectedRangeMap.put(partitionKeyRange.getKey(), range.toString());
+            if (optRange.isPresent() && !optRange.get().isEmpty()) {
+                selectedRangeMap.put(partitionKeyRange.getKey(), optRange.get().toString());
             }
         }
         // After we decompose the predicates, we should create a simple selectedRangeMap to turn on query cache if
