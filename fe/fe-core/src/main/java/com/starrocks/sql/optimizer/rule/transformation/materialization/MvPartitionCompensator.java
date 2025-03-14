c7caaf13ed10d54a7d8e6f38062a024220faa938
Wu Xueyang <45445803+wuxueyang96@users.noreply.github.com>
2025-03-14 17:39:43 +0800

    [Enhancement] MV support partition compensation for paimon table. (#56912)
    Signed-off-by: wuxueyang.wxy <wuxueyang.wxy@alibaba-inc.com>
    (cherry picked from commit e8edf1bf53248c33573378e105105c04eff2c798)

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/materialization/MvPartitionCompensator.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/materialization/MvPartitionCompensator.java
@@ -108,6 +108,7 @@ public class MvPartitionCompensator {
             ImmutableSet.<OperatorType>builder()
                     .add(OperatorType.LOGICAL_HIVE_SCAN)
                     .add(OperatorType.LOGICAL_ICEBERG_SCAN)
+                    .add(OperatorType.LOGICAL_PAIMON_SCAN)
                     .build();
 
     public static final ImmutableSet<OperatorType> SUPPORTED_PARTITION_COMPENSATE_SCAN_TYPES =
@@ -125,7 +126,7 @@ public class MvPartitionCompensator {
         if (t == null) {
             return false;
         }
-        if (t.isNativeTableOrMaterializedView() || t.isIcebergTable() || t.isHiveTable()) {
+        if (t.isNativeTableOrMaterializedView() || t.isIcebergTable() || t.isHiveTable() || t.isPaimonTable()) {
             return true;
         }
         return false;
