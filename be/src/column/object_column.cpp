f44ae3e90b1d1272d8696044324a19e514591fcf
eyes_on_me <nopainnofame@sina.com>
2025-03-14 21:11:08 +0800

    [BugFix] fix ObjectColumn::max_one_element_serialize_size (#56911)
    Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>

--- be/src/column/object_column.cpp
+++ be/src/column/object_column.cpp
@@ -174,6 +174,15 @@ uint32_t ObjectColumn<T>::serialize(size_t idx, uint8_t* pos) const {
 }
 
 template <typename T>
+uint32_t ObjectColumn<T>::max_one_element_serialize_size() const {
+    uint32_t max_size = 0;
+    for (size_t idx = 0; idx < size(); idx++) {
+        max_size = std::max(serialize_size(idx), max_size);
+    }
+    return max_size;
+}
+
+template <typename T>
 uint32_t ObjectColumn<T>::serialize_default(uint8_t* pos) const {
     DCHECK(false) << "Don't support object column serialize";
     return 0;
