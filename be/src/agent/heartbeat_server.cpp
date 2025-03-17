baa359ba3f50f783c3ad30506f763e278f8753af
dyp12 <49448072+dyp12@users.noreply.github.com>
2025-03-17 14:03:12 +0800

    [Enhancement]  FE sends node type along with heartbeat to BE/CN (#56874)
    Signed-off-by: dyp12 <49448072+dyp12@users.noreply.github.com>
    Co-authored-by: Kevin Cai <caixh.kevin@gmail.com>

--- be/src/agent/heartbeat_server.cpp
+++ be/src/agent/heartbeat_server.cpp
@@ -77,6 +77,8 @@ void HeartbeatServer::heartbeat(THeartbeatResult& heartbeat_result, const TMaste
     //print heartbeat in every minute
     LOG_EVERY_N(INFO, 12) << "get heartbeat from FE. host:" << master_info.network_address.hostname
                           << ", port:" << master_info.network_address.port << ", cluster id:" << master_info.cluster_id
+                          << ", node type:"
+                          << (master_info.__isset.node_type ? std::to_string(master_info.node_type) : "N/A")
                           << ", run_mode:" << master_info.run_mode << ", counter:" << google::COUNTER;
 
     if (master_info.encrypted != config::enable_transparent_data_encryption) {
@@ -187,6 +189,18 @@ StatusOr<HeartbeatServer::CmpResult> HeartbeatServer::compare_master_info(const 
     if ((master_info.network_address.hostname == LOCALHOST)) {
         if (!(master_info.backend_ip == LOCALHOST || master_info.backend_ip == LOCALHOST_IPV6)) {
             return Status::InternalError("FE heartbeat with localhost ip but BE is not deployed on the same machine");
+        }
+    }
+
+    if (master_info.__isset.node_type) {
+        if (master_info.node_type == TNodeType::Backend && BackendOptions::is_cn()) {
+            LOG_EVERY_N(ERROR, 12) << "FE heartbeat with BE node type,but the node is CN,node type mismatch!";
+            return Status::InternalError("expect to be BE but actually CN,Unmatched node type!");
+        }
+
+        if (master_info.node_type == TNodeType::Compute && !BackendOptions::is_cn()) {
+            LOG_EVERY_N(ERROR, 12) << "FE heartbeat with CN node type,but the node is BE,node type mismatch!";
+            return Status::InternalError("expect to be CN but actually BE,Unmatched node type!");
         }
     }
 
