e9ce3720f34adf733e4036f536a6bdab853f2415
meegoo <meegoo.sr@gmail.com>
2025-03-17 23:58:40 +0800

    [BugFix] Fix the temporary partition residue caused by optimize duplicate partitions
    Signed-off-by: meegoo <meegoo.sr@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/parser/AstBuilder.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/parser/AstBuilder.java
@@ -4915,6 +4915,7 @@ public class AstBuilder extends StarRocksBaseVisitor<ParseNode> {
         boolean temp = context.TEMPORARY() != null;
         boolean force = context.FORCE() != null;
         boolean exists = context.EXISTS() != null;
+        boolean dropAll = context.ALL() != null;
         Identifier identifier = null;
         StarRocksParser.IdentifierContext identifierContext = context.identifier();
         if (identifierContext != null) {
@@ -4935,6 +4936,8 @@ public class AstBuilder extends StarRocksBaseVisitor<ParseNode> {
         } else if (context.where != null) {
             Expr whereExpr = (Expr) visitIfPresent(context.where);
             return new DropPartitionClause(exists, whereExpr, temp, force, createPos(context));
+        } else if (dropAll) {
+            return new DropPartitionClause(temp, force, dropAll, createPos(context));
         } else {
             if (CollectionUtils.isNotEmpty(identifierList)) {
                 List<String> partitionNames = identifierList.stream().map(i -> i.getValue()).collect(toList());
--- fe/fe-core/src/main/java/com/starrocks/sql/parser/StarRocks.g4
+++ fe/fe-core/src/main/java/com/starrocks/sql/parser/StarRocks.g4
@@ -1189,6 +1189,7 @@ dropPartitionClause
     : DROP TEMPORARY? (PARTITION (IF EXISTS)? identifier | PARTITIONS (IF EXISTS)? identifierList) FORCE?
     | DROP TEMPORARY? PARTITIONS (IF EXISTS)? multiRangePartition FORCE?
     | DROP TEMPORARY? PARTITIONS (IF EXISTS)? WHERE where=expression FORCE?
+    | DROP ALL TEMPORARY PARTITIONS FORCE?
     ;
 
 truncatePartitionClause
