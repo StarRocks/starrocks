d824f254e464c39d76e628093f9703b38e27e673
silverbullet233 <3675229+silverbullet233@users.noreply.github.com>
2025-03-17 14:55:52 +0800

    fix conflicts
    Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>

--- be/src/column/object_column.cpp
+++ be/src/column/object_column.cpp
@@ -173,9 +173,6 @@ uint32_t ObjectColumn<T>::serialize(size_t idx, uint8_t* pos) {
 }
 
 template <typename T>
-<<<<<<< HEAD
-uint32_t ObjectColumn<T>::serialize_default(uint8_t* pos) {
-=======
 uint32_t ObjectColumn<T>::max_one_element_serialize_size() const {
     uint32_t max_size = 0;
     for (size_t idx = 0; idx < size(); idx++) {
@@ -185,8 +182,7 @@ uint32_t ObjectColumn<T>::max_one_element_serialize_size() const {
 }
 
 template <typename T>
-uint32_t ObjectColumn<T>::serialize_default(uint8_t* pos) const {
->>>>>>> f44ae3e90b ([BugFix] fix ObjectColumn::max_one_element_serialize_size (#56911))
+uint32_t ObjectColumn<T>::serialize_default(uint8_t* pos) {
     DCHECK(false) << "Don't support object column serialize";
     return 0;
 }
