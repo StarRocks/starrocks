95ee97ad68d8b1a0e234a6cb0fbec9505ba6f7d3
andyziye <108652123+andyziye@users.noreply.github.com>
2025-03-18 21:47:31 +0800

    [Enhancement] upgrade staros to v3.4-rc5(sync #56977) (#34320)
    * [Enhancement] upgrade staros to v3.4-rc5
    
    * fix conflict (#34322)
    
    Signed-off-by: Kevin Cai <kevin.cai@celerdata.com>
    
    ---------
    
    Signed-off-by: Kevin Cai <kevin.cai@celerdata.com>
    Co-authored-by: Kevin Cai <caixiaohua@starrocks.com>
    Co-authored-by: Kevin Cai <kevin.cai@celerdata.com>

--- thirdparty/vars-aarch64.sh
+++ thirdparty/vars-aarch64.sh
@@ -52,7 +52,7 @@ JINDOSDK_SOURCE="jindosdk-4.6.8-linux-el7-aarch64"
 JINDOSDK_MD5SUM="27a4e2cd9a403c6e21079a866287d88b"
 
 # starcache
-STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v3.4.0-rc02/starcache-centos7_arm64.tar.gz"
+STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v3.4.0-rc03/starcache-centos7_arm64.tar.gz"
 STARCACHE_NAME="starcache.tar.gz"
 STARCACHE_SOURCE="starcache"
-STARCACHE_MD5SUM="a623fd85c2083c89e3779402f212cdb4"
+STARCACHE_MD5SUM="0ae57f587e69f4c1424ddb2b15e1aaa9"
--- thirdparty/vars-ubuntu22-x86_64.sh
+++ thirdparty/vars-ubuntu22-x86_64.sh
@@ -28,7 +28,7 @@ JINDOSDK_SOURCE="jindosdk-4.6.8-linux-ubuntu22-x86_64"
 JINDOSDK_MD5SUM="52236053391091591c2d09684791e810"
 
 # starcache
-STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v3.4.0-rc02/starcache-ubuntu22_amd64.tar.gz"
+STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v3.4.0-rc03/starcache-ubuntu22_amd64.tar.gz"
 STARCACHE_NAME="starcache.tar.gz"
 STARCACHE_SOURCE="starcache"
-STARCACHE_MD5SUM="4fb74370d3a24d47a83b89ec0b4dfbf0"
+STARCACHE_MD5SUM="067a06c8416b20598461cc33903b474f"
