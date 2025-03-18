f71f0160c95bc0e8b7058bdaf00acfbded1431bb
dyp12 <49448072+dyp12@users.noreply.github.com>
2025-03-18 18:37:02 +0800

    [Enhancement]Add partition offset lag column in show routine load and information_schema.routine_load_jobs (backport #55559) (#56803)
    Signed-off-by: dyp12 <49448072+dyp12@users.noreply.github.com>

--- fe/fe-core/src/main/java/com/starrocks/load/routineload/RoutineLoadJob.java
+++ fe/fe-core/src/main/java/com/starrocks/load/routineload/RoutineLoadJob.java
@@ -658,6 +658,8 @@ public abstract class RoutineLoadJob extends AbstractTxnStateChangeCallback
 
     protected abstract String getSourceProgressString();
 
+    protected abstract String getSourceLagString(String progressJsonStr);
+
     public double getMaxFilterRatio() {
         return maxFilterRatio;
     }
@@ -1578,7 +1580,8 @@ public abstract class RoutineLoadJob extends AbstractTxnStateChangeCallback
             row.add(dataSourcePropertiesJsonToString());
             row.add(customPropertiesJsonToString());
             row.add(getStatistic());
-            row.add(getProgress().toJsonString());
+            String progressJsonStr = getProgress().toJsonString();
+            row.add(progressJsonStr);
             row.add(getTimestampProgress().toJsonString());
             switch (state) {
                 case PAUSED:
@@ -1616,7 +1619,7 @@ public abstract class RoutineLoadJob extends AbstractTxnStateChangeCallback
                 }
             }
             row.add(getSourceProgressString());
-
+            row.add(getSourceLagString(progressJsonStr));
             return row;
         } finally {
             readUnlock();
@@ -2077,7 +2080,8 @@ public abstract class RoutineLoadJob extends AbstractTxnStateChangeCallback
             info.setCustom_properties(customPropertiesJsonToString());
             info.setData_source_type(dataSourceType.name());
             info.setStatistic(getStatistic());
-            info.setProgress(getProgress().toJsonString());
+            String progressJsonStr = getProgress().toJsonString();
+            info.setProgress(progressJsonStr);
             switch (state) {
                 case PAUSED:
                     info.setReasons_of_state_changed(pauseReason == null ? "" : pauseReason.toString());
@@ -2095,6 +2099,8 @@ public abstract class RoutineLoadJob extends AbstractTxnStateChangeCallback
                 info.setTracking_sql("select tracking_log from information_schema.load_tracking_logs where job_id=" + id);
             }
             info.setOther_msg(otherMsg);
+            info.setLatest_source_position(getSourceProgressString());
+            info.setOffset_lag(getSourceLagString(progressJsonStr));
             return info;
         } finally {
             readUnlock();
