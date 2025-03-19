cdc8ab8c6b571d3f74e99a4fe161766a089b5ac5
silverbullet233 <3675229+silverbullet233@users.noreply.github.com>
2025-03-19 09:33:54 +0800

    move reset_schema to scan operator
    Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>

--- be/src/exec/pipeline/select_operator.cpp
+++ be/src/exec/pipeline/select_operator.cpp
@@ -87,7 +87,6 @@ bool SelectOperator::need_input() const {
 Status SelectOperator::push_chunk(RuntimeState* state, const ChunkPtr& chunk) {
     _curr_chunk = chunk;
     if (!_common_exprs.empty()) {
-        _curr_chunk->reset_schema();
         SCOPED_TIMER(_conjuncts_timer);
         for (const auto& [slot_id, expr] : _common_exprs) {
             ASSIGN_OR_RETURN(auto col, expr->evaluate(_curr_chunk.get()));
