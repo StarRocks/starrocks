10f9100c48c71b0bab08d18d3b12a3fbe6822d65
gengjun-git <gengjun@starrocks.com>
2025-03-19 18:37:43 +0800

    clang format
    Signed-off-by: gengjun-git <gengjun@starrocks.com>

--- be/src/agent/finish_task.cpp
+++ be/src/agent/finish_task.cpp
@@ -49,7 +49,7 @@ void finish_task(const TFinishTaskRequest& finish_task_request) {
                 max_retry_times = ALTER_FINISH_TASK_MAX_RETRY;
                 sleep_seconds = sleep_seconds * 2;
             } else if (result.status.status_code == TStatusCode::LEADER_TRANSFERRED &&
-                finish_task_request.task_type == TTaskType::CREATE) {
+                       finish_task_request.task_type == TTaskType::CREATE) {
                 sleep_seconds = sleep_seconds * 2;
             } else {
                 break;
