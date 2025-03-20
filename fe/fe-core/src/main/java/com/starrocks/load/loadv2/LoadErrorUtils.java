873ec662a638d0d29068282358b32cb8843ffe73
wyb <wybb86@gmail.com>
2025-03-20 19:24:37 +0800

    [Enhancement] Improve broker load job retry (#56987)
    Signed-off-by: wyb <wybb86@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/load/loadv2/LoadErrorUtils.java
+++ fe/fe-core/src/main/java/com/starrocks/load/loadv2/LoadErrorUtils.java
@@ -40,17 +40,6 @@ public class LoadErrorUtils {
     public static final ErrorMeta BACKEND_BRPC_TIMEOUT =
             new ErrorMeta("[E1008]Reached timeout", "Backend BRPC timeout");
 
-    private static final ErrorMeta[] LOADING_TASK_TIMEOUT_ERRORS = new ErrorMeta[] {BACKEND_BRPC_TIMEOUT};
-
-    public static boolean isTimeoutFromLoadingTaskExecution(String errorMsg) {
-        for (ErrorMeta errorMeta : LOADING_TASK_TIMEOUT_ERRORS) {
-            if (errorMsg.contains(errorMeta.keywords)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     public static boolean enableProfileAfterError(Coordinator coordinator) {
         if (!(coordinator instanceof DefaultCoordinator defaultCoordinator)) {
             return false;
