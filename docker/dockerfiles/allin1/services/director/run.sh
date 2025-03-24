98a1cfe8451ee9778486c2b442716fbce40acc82
Kevin Cai <caixiaohua@starrocks.com>
2025-03-24 11:44:10 +0800

    [Enhancement] remove broker from allin1 image (#57194)
    reduce allin1 CVEs introduced by the fs_broker
    broker is not required for local file loading in allin1 case
    
    Signed-off-by: Kevin Cai <kevin.cai@celerdata.com>

--- docker/dockerfiles/allin1/services/director/run.sh
+++ docker/dockerfiles/allin1/services/director/run.sh
@@ -17,7 +17,6 @@ SQL_RETRY_INTERNAL=5
 FE_HOME=$SR_HOME/fe
 BE_HOME=$SR_HOME/be
 MYCNF=$SR_HOME/director/my.cnf
-BROKER_HOME=$SR_HOME/apache_hdfs_broker
 source $FE_HOME/bin/common.sh
 PREVIOUS_FQDN=
 CURRENT_FQDN=`hostname -f`
@@ -78,12 +77,6 @@ be_heartbeat_service_port()
 {
     export_env_from_conf $BE_HOME/conf/be.conf
     echo ${heartbeat_service_port:-"9050"}
-}
-
-broker_ipc_port()
-{
-    export_env_from_conf $BROKER_HOME/conf/apache_hdfs_broker.conf 
-    echo ${broker_ipc_port:-"8000"}
 }
 
 check_fe_fqdn_mismatch()
@@ -190,42 +183,18 @@ check_and_add_be()
     done
 }
 
-check_and_add_broker()
-{
-    loginfo "check if need to add BROKER into FE service ..."
-    while true
-    do
-        result=`exec_sql_with_retry "SHOW BROKER;"`
-        ret=$?
-        if [ $ret -ne 0 ] ; then
-            hang_and_die
-        else
-            if echo "$result" | grep -q $MYHOST &>/dev/null ; then
-                loginfo "broker service already added into FE service ... "
-                return 0
-            else
-                brokerport=`broker_ipc_port`
-                loginfo "Add BROKER($MYHOST:$brokerport) into FE service ..."
-                exec_sql_with_retry "ALTER SYSTEM ADD BROKER allin1broker '$MYHOST:$brokerport';"
-            fi
-        fi
-    done
-}
-
-loginfo "checking if need to perform auto registring Backend and Broker ..."
+loginfo "checking if need to perform auto registring Backend ..."
 check_fe_fqdn_mismatch
 check_fe_liveness
 generate_my_cnf
 check_and_add_be
-check_and_add_broker
 loginfo "cluster initialization DONE!"
-loginfo "wait a few seconds for BE and Broker's heartbeat ..."
-# allow heartbeat from BE/BROKER to FE
+loginfo "wait a few seconds for BE's heartbeat ..."
+# allow heartbeat from BE to FE
 sleep 10
 loginfo "StarRocks Cluster information details:"
 exec_sql_with_column 'SHOW FRONTENDS\G'
 exec_sql_with_column 'SHOW BACKENDS\G'
-exec_sql_with_column 'SHOW BROKER\G'
 
 loginfo
 loginfo
