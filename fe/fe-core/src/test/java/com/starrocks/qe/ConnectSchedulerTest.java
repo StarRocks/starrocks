3a8d692c9146283d4ec28fc442c5a8641101a5a4
Harbor Liu <460660596@qq.com>
2025-03-25 16:44:23 +0800

    Support Global ConnectionId

--- fe/fe-core/src/test/java/com/starrocks/qe/ConnectSchedulerTest.java
+++ fe/fe-core/src/test/java/com/starrocks/qe/ConnectSchedulerTest.java
@@ -35,9 +35,20 @@
 package com.starrocks.qe;
 
 import com.starrocks.analysis.AccessTestUtil;
+import com.starrocks.common.DdlException;
+import com.starrocks.ha.FrontendNodeType;
+import com.starrocks.journal.bdbje.BDBJEJournal;
+import com.starrocks.leader.CheckpointController;
 import com.starrocks.mysql.MysqlChannel;
 import com.starrocks.mysql.MysqlProto;
+import com.starrocks.persist.EditLog;
+import com.starrocks.server.GlobalStateMgr;
+import com.starrocks.server.NodeMgr;
+import com.starrocks.system.Frontend;
+import com.starrocks.utframe.MockJournal;
 import mockit.Expectations;
+import mockit.Mock;
+import mockit.MockUp;
 import mockit.Mocked;
 import org.junit.Assert;
 import org.junit.Before;
@@ -82,5 +93,70 @@ public class ConnectSchedulerTest {
 
         Thread.sleep(1000);
         Assert.assertNull(scheduler.getContext(0));
+    }
+
+    @Test
+    public void testGlobalConnectionId() throws Exception {
+        new MockUp<NodeMgr>() {
+            @Mock
+            public Frontend getMySelf() {
+                return new Frontend(0, FrontendNodeType.LEADER, "", "", 0);
+            }
+        };
+
+        ConnectScheduler scheduler = new ConnectScheduler(10);
+        int threshold = 1 << 24;
+        for (int i = 1; i < threshold; i++) {
+            scheduler.getNextConnectionId();
+        }
+
+        Assert.assertEquals(0, scheduler.getNextConnectionId());
+
+        new MockUp<NodeMgr>() {
+            @Mock
+            public Frontend getMySelf() {
+                return new Frontend(1, FrontendNodeType.LEADER, "", "", 0);
+            }
+        };
+        Assert.assertEquals(threshold + 1, scheduler.getNextConnectionId());
+    }
+
+    @Test
+    public void testFrontendId() throws DdlException {
+        new MockUp<EditLog>() {
+            @Mock
+            public void logJsonObject(short op, Object obj) {
+            }
+        };
+
+        GlobalStateMgr.getCurrentState().setEditLog(new EditLog(null));
+        GlobalStateMgr.getCurrentState().setCheckpointController(new CheckpointController("fe", new BDBJEJournal(null, ""), ""));
+        GlobalStateMgr.getCurrentState().setHaProtocol(new MockJournal.MockProtocol());
+
+        NodeMgr nodeMgr = new NodeMgr();
+        nodeMgr.addFrontend(FrontendNodeType.FOLLOWER, "192.168.0.0", 9010);
+        nodeMgr.addFrontend(FrontendNodeType.FOLLOWER, "192.168.0.1", 9010);
+
+        Assert.assertNotNull(nodeMgr.getFrontend(0));
+        Assert.assertNotNull(nodeMgr.getFrontend(1));
+        Assert.assertNull(nodeMgr.getFrontend(2));
+        nodeMgr.setMySelf(nodeMgr.getFrontend(0));
+
+        for (int i = 2; i < 256; i++) {
+            nodeMgr.addFrontend(FrontendNodeType.FOLLOWER, "192.168.0." + i, 9010);
+        }
+
+        Frontend frontend = nodeMgr.checkFeExist("192.168.0.255", 9010);
+        Assert.assertEquals(255, frontend.getFid());
+
+        Assert.assertThrows(DdlException.class, () -> nodeMgr.addFrontend(FrontendNodeType.FOLLOWER, "192.168.1.20", 9010));
+
+        nodeMgr.dropFrontend(FrontendNodeType.FOLLOWER, "192.168.0.20", 9010);
+        nodeMgr.dropFrontend(FrontendNodeType.FOLLOWER, "192.168.0.50", 9010);
+        nodeMgr.dropFrontend(FrontendNodeType.FOLLOWER, "192.168.0.70", 9010);
+
+        nodeMgr.addFrontend(FrontendNodeType.FOLLOWER, "192.168.1.20", 9010);
+        frontend = nodeMgr.checkFeExist("192.168.1.20", 9010);
+        Assert.assertEquals(20, frontend.getFid());
     }
 }
