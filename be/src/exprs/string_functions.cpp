591c9206d48d04bc9055ed9fcdc9df46abc34e41
andyziye <108652123+andyziye@users.noreply.github.com>
2025-03-26 10:09:46 +0800

    fix conflict

--- be/src/exprs/string_functions.cpp
+++ be/src/exprs/string_functions.cpp
@@ -2042,13 +2042,6 @@ template ColumnPtr StringCaseToggleFunction<true>::evaluate<TYPE_VARCHAR, TYPE_V
 template struct StringCaseToggleFunction<false>;
 template ColumnPtr StringCaseToggleFunction<false>::evaluate<TYPE_VARCHAR, TYPE_VARCHAR>(const ColumnPtr& v1);
 
-
-template struct StringCaseToggleFunction<true>;
-template ColumnPtr StringCaseToggleFunction<true>::evaluate<TYPE_VARCHAR, TYPE_VARCHAR>(const ColumnPtr& v1);
-
-template struct StringCaseToggleFunction<false>;
-template ColumnPtr StringCaseToggleFunction<false>::evaluate<TYPE_VARCHAR, TYPE_VARCHAR>(const ColumnPtr& v1);
-
 template <bool to_upper>
 struct UTF8StringCaseToggleFunction {
 public:
