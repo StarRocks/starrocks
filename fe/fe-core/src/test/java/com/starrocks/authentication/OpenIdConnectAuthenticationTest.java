f71bebcd8d9c237aa4eb010bddecaf4975cba0d9
rstyp <06chilled_ponchos@icloud.com>
2025-03-27 14:25:25 -0700

    Add OIDC token expiry validation
    Signed-off-by: rstyp <06chilled_ponchos@icloud.com>

--- fe/fe-core/src/test/java/com/starrocks/authentication/OpenIdConnectAuthenticationTest.java
+++ fe/fe-core/src/test/java/com/starrocks/authentication/OpenIdConnectAuthenticationTest.java
@@ -14,7 +14,12 @@
 
 package com.starrocks.authentication;
 
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.crypto.RSASSASigner;
 import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import com.starrocks.mysql.MysqlSerializer;
 import com.starrocks.server.GlobalStateMgr;
 import com.starrocks.sql.ast.UserAuthOption;
@@ -29,7 +34,12 @@ import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.KeyFactory;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.text.ParseException;
+import java.util.Base64;
+import java.util.Date;
 
 public class OpenIdConnectAuthenticationTest {
 
@@ -39,6 +49,24 @@ public class OpenIdConnectAuthenticationTest {
             String path = ClassLoader.getSystemClassLoader().getResource("auth").getPath();
             InputStream jwksInputStream = new FileInputStream(path + "/" + jwksUrl);
             return JWKSet.load(jwksInputStream);
+        }
+
+        private static RSAPrivateKey loadPrivateKey(String privateKeyPem) throws IOException {
+            String path = ClassLoader.getSystemClassLoader().getResource("auth").getPath();
+            try (FileInputStream fis = new FileInputStream(path + "/" + privateKeyPem)) {
+                String keyContent = new String(fis.readAllBytes());
+                String privateKeyPEM = keyContent
+                        .replace("-----BEGIN PRIVATE KEY-----", "")
+                        .replace("-----END PRIVATE KEY-----", "")
+                        .replaceAll("\\R", "");
+                byte[] decodedKey = Base64.getDecoder().decode(privateKeyPEM);
+
+                PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(decodedKey);
+                KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+                return (RSAPrivateKey) keyFactory.generatePrivate(keySpec);
+            } catch (Exception e) {
+                throw new IOException("Failed to load private key", e);
+            }
         }
     }
 
@@ -56,15 +84,40 @@ public class OpenIdConnectAuthenticationTest {
         return sb.toString();
     }
 
+
+    private String generateTestOIDCToken(long validity) throws Exception {
+        MockJwkMgr mockJwkMgr = new MockJwkMgr();
+        RSASSASigner signer = new RSASSASigner(mockJwkMgr.loadPrivateKey("jwks-private-key.pem"));
+        JWKSet jwkSet = mockJwkMgr.getJwkSet("signer-jwks.json");
+
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+                .issuer("http://localhost:38080/realms/master")
+                .subject("8f7f0fa5-e1eb-45d0-8e82-8c89c1a45663")
+                .audience("12345")
+                .issueTime(new Date())
+                .expirationTime(new Date(new Date().getTime() + validity))
+                .claim("preferred_username", "harbor")
+                .build();
+
+        JWSHeader.Builder headerBuilder = new JWSHeader.Builder(JWSAlgorithm.RS256)
+                .keyID(jwkSet.getKeys().get(0).getKeyID());
+        SignedJWT signedJWT = new SignedJWT(
+                new JWSHeader(headerBuilder.build()),
+                claimsSet);
+        signedJWT.sign(signer);
+
+        return signedJWT.serialize();
+    }
+
     @Test
-    public void testAuthentication() throws AuthenticationException, IOException {
+    public void testAuthentication() throws Exception {
         GlobalStateMgr.getCurrentState().setJwkMgr(new MockJwkMgr());
 
         OpenIdConnectAuthenticationProvider provider =
                 new OpenIdConnectAuthenticationProvider("jwks.json", "preferred_username", "", "");
         provider.analyzeAuthOption(new UserIdentity("harbor", "%"),
                 new UserAuthOption("", null, null, true, NodePosition.ZERO));
-        String openIdConnectJson = getOpenIdConnect("oidc.json");
+        String openIdConnectJson = generateTestOIDCToken(3600 * 1000);
 
         MysqlSerializer serializer = MysqlSerializer.newInstance();
         serializer.writeInt1(0);
@@ -106,7 +159,7 @@ public class OpenIdConnectAuthenticationTest {
 
     @Test
     public void testIssuerAndAudience() throws Exception {
-        String openIdConnectJson = getOpenIdConnect("oidc.json");
+        String openIdConnectJson = generateTestOIDCToken(3600 * 1000);
         MockJwkMgr mockJwkMgr = new MockJwkMgr();
         JWKSet jwkSet = mockJwkMgr.getJwkSet("jwks.json");
 
@@ -148,6 +201,29 @@ public class OpenIdConnectAuthenticationTest {
             Assert.fail();
         } catch (AuthenticationException e) {
             Assert.assertTrue(e.getMessage().contains("Audience (aud) field [12345] is invalid"));
+        }
+    }
+
+    @Test
+    public void testExpiry() throws Exception {
+
+        MockJwkMgr mockJwkMgr = new MockJwkMgr();
+        JWKSet jwkSet = mockJwkMgr.getJwkSet("jwks.json");
+
+        try {
+            String unexpiredToken = generateTestOIDCToken(3600 * 1000);
+            OpenIdConnectVerifier.verify(unexpiredToken, "harbor",
+                    jwkSet, "preferred_username", "http://localhost:38080/realms/master", "12345");
+        } catch (Exception e) {
+            Assert.fail(e.getMessage());
+        }
+
+        try {
+            String unexpiredToken = generateTestOIDCToken(-60L);
+            OpenIdConnectVerifier.verify(unexpiredToken, "harbor",
+                    jwkSet, "preferred_username", "http://localhost:38080/realms/master", "12345");
+        } catch (AuthenticationException e) {
+            Assert.assertTrue(e.getMessage().contains("JWT expired at"));
         }
     }
 }
