4942e0a1d73b6fefea05ceb5cdc7e2d4d39380e0
Harbor Liu <460660596@qq.com>
2025-03-27 19:23:34 +0800

    [Feature] Support Global ConnectionId (#57276)

--- fe/fe-core/src/test/java/com/starrocks/ha/BDBHATest.java
+++ fe/fe-core/src/test/java/com/starrocks/ha/BDBHATest.java
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 package com.starrocks.ha;
 
 import com.starrocks.journal.bdbje.BDBEnvironment;
