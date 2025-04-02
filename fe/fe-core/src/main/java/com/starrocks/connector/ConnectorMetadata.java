9992854373f77021c2ef8baac0ab307b0339bbf6
mingye.jmy <mingye.jmy@alibaba-inc.com>
2025-04-02 16:53:42 +0800

    [Stella][BP-BugFix] Do not allow creating a view in unsupported external catalog (#57312)

--- fe/fe-core/src/main/java/com/starrocks/connector/ConnectorMetadata.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/ConnectorMetadata.java
@@ -318,6 +318,7 @@ public interface ConnectorMetadata {
     }
 
     default void createView(CreateViewStmt stmt) throws DdlException {
+        throw new StarRocksConnectorException("This connector doesn't support create view");
     }
 
     default void alterView(AlterViewStmt stmt) throws StarRocksException {
