StarRocks/starrocks

commit ce36ac7715babb4a26d2bb4a147bca2d343140ae

[Enhancement] reduce read io requests during spill restore phase  (#44971)
Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>

be/src/exec/spill/block_manager.h
@@ -19,12 +19,14 @@
 #include "common/status.h"
 #include "common/statusor.h"
 #include "gen_cpp/Types_types.h"
+#include "io/input_stream.h"
+#include "util/runtime_profile.h"
 #include "util/slice.h"
 
 namespace starrocks::spill {
 
 class BlockReader;
-
+class BlockReaderOptions;
 // Block represents a continuous storage space and is the smallest storage unit of flush and restore in spill task.
 // Block only supports append writing and sequential reading, and neither writing nor reading of Block is guaranteed to be thread-safe.
 class Block {
@@ -37,7 +39,9 @@ public:
     // flush block to somewhere
     virtual Status flush() = 0;
 
-    virtual std::shared_ptr<BlockReader> get_reader() = 0;
+    virtual StatusOr<std::unique_ptr<io::InputStreamWrapper>> get_readable() const = 0;
+
+    virtual std::shared_ptr<BlockReader> get_reader(const BlockReaderOptions& options) = 0;
 
     virtual std::string debug_string() const = 0;
 
@@ -62,13 +66,24 @@ protected:
 
 using BlockPtr = std::shared_ptr<Block>;
 
+struct BlockReaderOptions {
+    bool enable_buffer_read = false;
+    size_t max_buffer_bytes = std::numeric_limits<size_t>::max();
+
+    RuntimeProfile::Counter* read_io_timer = nullptr;
+    RuntimeProfile::Counter* read_io_count = nullptr;
+    RuntimeProfile::Counter* read_io_bytes = nullptr;
+};
+
 class BlockReader {
 public:
-    BlockReader(const Block* block) : _block(block) {}
+    BlockReader(const Block* block, const BlockReaderOptions& options)
+            : _block(block), _length(block->size()), _options(options) {}
+
     virtual ~BlockReader() = default;
     // read exacly the specified length of data from Block,
     // if the Block has reached the end, should return EndOfFile status
-    virtual Status read_fully(void* data, int64_t count) = 0;
+    virtual Status read_fully(void* data, int64_t count);
 
     virtual std::string debug_string() = 0;
 
@@ -76,6 +91,14 @@ public:
 
 protected:
     const Block* _block = nullptr;
+    std::unique_ptr<io::InputStreamWrapper> _readable;
+    size_t _length = 0;
+    size_t _offset = 0;
+
+    // used for buffer read
+    std::unique_ptr<uint8_t[]> _buffer;
+    Slice _slice;
+    BlockReaderOptions _options;
 };
 
 struct AcquireBlockOptions {

be/src/exec/spill/file_block_manager.cpp
@@ -16,6 +16,7 @@
 
 #include <utility>
 
+#include "exec/spill/block_manager.h"
 #include "exec/spill/common.h"
 #include "fmt/format.h"
 #include "gen_cpp/Types_types.h"
@@ -142,19 +143,13 @@ StatusOr<FileBlockContainerPtr> FileBlockContainer::create(const DirPtr& dir, co
 
 class FileBlockReader final : public BlockReader {
 public:
-    FileBlockReader(const Block* block) : BlockReader(block), _length(block->size()) {}
-    ~FileBlockReader() override = default;
+    FileBlockReader(const Block* block, const BlockReaderOptions& options = {}) : BlockReader(block, options) {}
 
-    Status read_fully(void* data, int64_t count) override;
+    ~FileBlockReader() override = default;
 
     std::string debug_string() override { return _block->debug_string(); }
 
     const Block* block() const override { return _block; }
-
-private:
-    std::unique_ptr<io::InputStreamWrapper> _readable;
-    size_t _length = 0;
-    size_t _offset = 0;
 };
 
 class FileBlock : public Block {
@@ -175,9 +170,13 @@ public:
 
     Status flush() override { return _container->flush(); }
 
-    StatusOr<std::unique_ptr<io::InputStreamWrapper>> get_readable() const { return _container->get_readable(); }
+    StatusOr<std::unique_ptr<io::InputStreamWrapper>> get_readable() const override {
+        return _container->get_readable();
+    }
 
-    std::shared_ptr<BlockReader> get_reader() override { return std::make_shared<FileBlockReader>(this); }
+    std::shared_ptr<BlockReader> get_reader(const BlockReaderOptions& options) override {
+        return std::make_shared<FileBlockReader>(this, options);
+    }
 
     std::string debug_string() const override {
 #ifndef BE_TEST
@@ -192,24 +191,6 @@ public:
 private:
     FileBlockContainerPtr _container;
 };
-
-Status FileBlockReader::read_fully(void* data, int64_t count) {
-    if (_readable == nullptr) {
-        auto file_block = down_cast<const FileBlock*>(_block);
-        ASSIGN_OR_RETURN(_readable, file_block->get_readable());
-    }
-
-    if (_offset + count > _length) {
-        return Status::EndOfFile("no more data in this block");
-    }
-
-    ASSIGN_OR_RETURN(auto read_len, _readable->read(data, count));
-    RETURN_IF(read_len == 0, Status::EndOfFile("no more data in this block"));
-    RETURN_IF(read_len != count, Status::InternalError(fmt::format(
-                                         "block's length is mismatched, expected: {}, actual: {}", count, read_len)));
-    _offset += count;
-    return Status::OK();
-}
 
 FileBlockManager::FileBlockManager(const TUniqueId& query_id, DirManager* dir_mgr)
         : _query_id(query_id), _dir_mgr(dir_mgr) {}

be/src/exec/spill/log_block_manager.cpp
@@ -17,10 +17,12 @@
 #include <memory>
 #include <mutex>
 #include <optional>
+#include <string_view>
 #include <unordered_map>
 #include <utility>
 
 #include "common/config.h"
+#include "common/status.h"
 #include "exec/spill/block_manager.h"
 #include "exec/spill/common.h"
 #include "fmt/format.h"
@@ -30,6 +32,7 @@
 #include "runtime/exec_env.h"
 #include "storage/options.h"
 #include "util/defer_op.h"
+#include "util/raw_container.h"
 #include "util/uid_util.h"
 
 namespace starrocks::spill {
@@ -162,19 +165,13 @@ StatusOr<LogBlockContainerPtr> LogBlockContainer::create(const DirPtr& dir, cons
 
 class LogBlockReader final : public BlockReader {
 public:
-    LogBlockReader(const Block* block) : BlockReader(block) {}
-    ~LogBlockReader() override = default;
+    LogBlockReader(const Block* block, const BlockReaderOptions& options = {}) : BlockReader(block, options) {}
 
-    Status read_fully(void* data, int64_t count) override;
+    ~LogBlockReader() override = default;
 
     std::string debug_string() override { return _block->debug_string(); }
 
     const Block* block() const override { return _block; }
-
-private:
-    std::unique_ptr<io::InputStreamWrapper> _readable;
-    size_t _offset = 0;
-    size_t _length = 0;
 };
 
 class LogBlock : public Block {
@@ -197,11 +194,13 @@ public:
 
     Status flush() override { return _container->flush(); }
 
-    StatusOr<std::unique_ptr<io::InputStreamWrapper>> get_readable() const {
+    StatusOr<std::unique_ptr<io::InputStreamWrapper>> get_readable() const override {
         return _container->get_readable(_offset, _size);
     }
 
-    std::shared_ptr<BlockReader> get_reader() override { return std::make_shared<LogBlockReader>(this); }
+    std::shared_ptr<BlockReader> get_reader(const BlockReaderOptions& options) override {
+        return std::make_shared<LogBlockReader>(this, options);
+    }
 
     std::string debug_string() const override {
 #ifndef BE_TEST
@@ -219,24 +218,6 @@ private:
     size_t _offset{};
 };
 
-Status LogBlockReader::read_fully(void* data, int64_t count) {
-    if (_readable == nullptr) {
-        auto log_block = down_cast<const LogBlock*>(_block);
-        ASSIGN_OR_RETURN(_readable, log_block->get_readable());
-        _length = log_block->size();
-    }
-
-    if (_offset + count > _length) {
-        return Status::EndOfFile("no more data in this block");
-    }
-
-    ASSIGN_OR_RETURN(auto read_len, _readable->read(data, count));
-    RETURN_IF(read_len == 0, Status::EndOfFile("no more data in this block"));
-    RETURN_IF(read_len != count, Status::InternalError(fmt::format(
-                                         "block's length is mismatched, expected: {}, actual: {}", count, read_len)));
-    _offset += count;
-    return Status::OK();
-}
 LogBlockManager::LogBlockManager(const TUniqueId& query_id, DirManager* dir_mgr)
         : _query_id(std::move(query_id)), _dir_mgr(dir_mgr) {
     _max_container_bytes = config::spill_max_log_block_container_bytes > 0 ? config::spill_max_log_block_container_bytes

be/src/exec/CMakeLists.txt
@@ -293,6 +293,7 @@ set(EXEC_FILES
     spill/serde.cpp
     spill/input_stream.cpp
     spill/data_stream.cpp
+    spill/block_reader.cpp
     spill/log_block_manager.cpp
     spill/file_block_manager.cpp
     spill/hybird_block_manager.cpp

be/src/exec/pipeline/aggregate/spillable_aggregate_blocking_sink_operator.cpp
@@ -305,6 +305,8 @@ Status SpillableAggregateBlockingSinkOperatorFactory::prepare(RuntimeState* stat
     _spill_options->plan_node_id = _plan_node_id;
     _spill_options->encode_level = state->spill_encode_level();
     _spill_options->wg = state->fragment_ctx()->workgroup();
+    _spill_options->enable_buffer_read = state->enable_spill_buffer_read();
+    _spill_options->max_read_buffer_bytes = state->max_spill_read_buffer_bytes_per_driver();
 
     return Status::OK();
 }

be/src/exec/pipeline/aggregate/spillable_aggregate_distinct_blocking_operator.cpp
@@ -154,6 +154,8 @@ Status SpillableAggregateDistinctBlockingSinkOperatorFactory::prepare(RuntimeSta
     _spill_options->plan_node_id = _plan_node_id;
     _spill_options->encode_level = state->spill_encode_level();
     _spill_options->wg = state->fragment_ctx()->workgroup();
+    _spill_options->enable_buffer_read = state->enable_spill_buffer_read();
+    _spill_options->max_read_buffer_bytes = state->max_spill_read_buffer_bytes_per_driver();
 
     return Status::OK();
 }

be/src/exec/pipeline/hashjoin/spillable_hash_join_build_operator.cpp
@@ -267,6 +267,9 @@ Status SpillableHashJoinBuildOperatorFactory::prepare(RuntimeState* state) {
             _hash_joiner_factory->hash_join_param()._distribution_mode == TJoinDistributionMode::LOCAL_HASH_BUCKET ||
             state->fragment_ctx()->enable_adaptive_dop();
 
+    _spill_options->enable_buffer_read = state->enable_spill_buffer_read();
+    _spill_options->max_read_buffer_bytes = state->max_spill_read_buffer_bytes_per_driver();
+
     const auto& param = _hash_joiner_factory->hash_join_param();
 
     _build_side_partition = param._build_expr_ctxs;

be/src/exec/pipeline/hashjoin/spillable_hash_join_probe_operator.cpp
@@ -533,6 +533,8 @@ Status SpillableHashJoinProbeOperatorFactory::prepare(RuntimeState* state) {
     _spill_options->plan_node_id = _plan_node_id;
     _spill_options->encode_level = state->spill_encode_level();
     _spill_options->wg = state->fragment_ctx()->workgroup();
+    _spill_options->enable_buffer_read = state->enable_spill_buffer_read();
+    _spill_options->max_read_buffer_bytes = state->max_spill_read_buffer_bytes_per_driver();
 
     return Status::OK();
 }

commit d58da8ac734e5d537d3ac235d5b71a450b3542a5

[BugFix] Fix log block container causing disk space statistics issues
This commit contains two parts
1. removes the block pre_allocate interface.
2. Fix the dir disk usage statistics error caused by #44971. Container reuse can result in consumed dir not being counted in the container, causing a leak.

Signed-off-by: stdpain <drfeng08@gmail.com>

be/src/exec/spill/block_manager.h
@@ -53,8 +53,6 @@ public:
     bool is_remote() const { return _is_remote; }
     void set_is_remote(bool is_remote) { _is_remote = is_remote; }
 
-    virtual bool preallocate(size_t write_size) = 0;
-
     void inc_num_rows(size_t num_rows) { _num_rows += num_rows; }
 
     void set_affinity_group(BlockAffinityGroup affinity_group) { _affinity_group = affinity_group; }

be/src/exec/spill/data_stream.cpp
@@ -53,7 +53,7 @@ private:
 };
 
 Status BlockSpillOutputDataStream::_prepare_block(RuntimeState* state, size_t write_size) {
-    if (_cur_block == nullptr || !_cur_block->preallocate(write_size)) {
+    if (_cur_block == nullptr) {
         // flush current block firstly
         RETURN_IF_ERROR(flush());
         // TODO: add profile for acquire block

be/src/exec/spill/dir_manager.h
@@ -113,4 +113,8 @@ private:
 #endif
 };
 
+#define DISK_ACQUIRE_ERROR(acquire_size, dir)                                                                      \
+    Status::RuntimeError(fmt::format("acquire size error: dir {} try acquire:{} usage:{} capacity:{}", dir->dir(), \
+                                     acquire_size, dir->get_current_size(), dir->get_max_size()))
+
 } // namespace starrocks::spill

be/src/exec/spill/file_block_manager.cpp
@@ -69,7 +69,7 @@ public:
 
     Status flush();
 
-    bool pre_allocate(size_t allocate_size) {
+    bool try_acquire_sizes(size_t allocate_size) {
         if (_data_size + allocate_size <= _acquired_data_size) {
             return true;
         }
@@ -121,6 +121,8 @@ Status FileBlockContainer::close() {
 }
 
 Status FileBlockContainer::append_data(const std::vector<Slice>& data, size_t total_size) {
+    auto* dir = _dir.get();
+    RETURN_IF(!try_acquire_sizes(total_size), DISK_ACQUIRE_ERROR(total_size, dir));
     RETURN_IF_ERROR(_writable_file->appendv(data.data(), data.size()));
     _data_size += total_size;
     return Status::OK();
@@ -190,8 +192,6 @@ public:
         return fmt::format("FileBlock[container={}]", _container->path());
 #endif
     }
-
-    bool preallocate(size_t write_size) override { return _container->pre_allocate(write_size); }
 
 private:
     FileBlockContainerPtr _container;

be/src/exec/spill/log_block_manager.cpp
@@ -82,7 +82,14 @@ public:
     std::string parent_path() const { return fmt::format("{}/{}", _dir->dir(), print_id(_query_id)); }
     uint64_t id() const { return _id; }
 
-    bool pre_allocate(size_t allocate_size) {
+    // Count acquired_size, then return the remaing size to dir to avoid too much error in counting.
+    void add_acquired_size(size_t acquired_size) {
+        size_t remaing_size = _acquired_data_size - _data_size;
+        _acquired_data_size += acquired_size - remaing_size;
+        _dir->dec_size(remaing_size);
+    }
+
+    bool try_acquire_sizes(size_t allocate_size) {
         if (_data_size + allocate_size <= _acquired_data_size) {
             return true;
         }
@@ -144,6 +151,8 @@ Status LogBlockContainer::close() {
 }
 
 Status LogBlockContainer::append_data(const std::vector<Slice>& data, size_t total_size) {
+    auto* dir = _dir.get();
+    RETURN_IF(!try_acquire_sizes(total_size), DISK_ACQUIRE_ERROR(total_size, dir));
     RETURN_IF_ERROR(_writable_file->pre_allocate(total_size));
     auto scope = IOProfiler::scope(IOProfiler::TAG::TAG_SPILL, 0);
     RETURN_IF_ERROR(_writable_file->appendv(data.data(), data.size()));
@@ -228,8 +237,6 @@ public:
 #endif
     }
 
-    bool preallocate(size_t write_size) override { return _container->pre_allocate(write_size); }
-
 private:
     LogBlockContainerPtr _container;
     size_t _offset{};
@@ -262,11 +269,7 @@ StatusOr<BlockPtr> LogBlockManager::acquire_block(const AcquireBlockOptions& opt
     DCHECK(opts.block_size > 0) << "block size should be larger than 0";
     AcquireDirOptions acquire_dir_opts;
     acquire_dir_opts.data_size = opts.block_size;
-#ifdef BE_TEST
     ASSIGN_OR_RETURN(auto dir, _dir_mgr->acquire_writable_dir(acquire_dir_opts));
-#else
-    ASSIGN_OR_RETURN(auto dir, ExecEnv::GetInstance()->spill_dir_mgr()->acquire_writable_dir(acquire_dir_opts));
-#endif
 
     ASSIGN_OR_RETURN(auto block_container,
                      get_or_create_container(dir, opts.fragment_instance_id, opts.plan_node_id, opts.name,
@@ -327,6 +330,7 @@ StatusOr<LogBlockContainerPtr> LogBlockManager::get_or_create_container(
         auto container = q->front();
         TRACE_SPILL_LOG << "return an existed container: " << container->path();
         q->pop();
+        container->add_acquired_size(block_size);
         return container;
     }
     uint64_t id = _next_container_id++;

be/src/storage/lake/spill_mem_table_sink.cpp
@@ -63,7 +63,7 @@ Status LoadSpillOutputDataStream::_freeze_current_block() {
 
 Status LoadSpillOutputDataStream::_preallocate(size_t block_size) {
     // Try to preallocate from current block first.
-    if (_block == nullptr || !_block->preallocate(block_size)) {
+    if (_block == nullptr) {
         // Freeze current block firstly.
         RETURN_IF_ERROR(_freeze_current_block());
         // Acquire new block.

be/test/io/spill_block_manager_test.cpp
@@ -344,51 +344,38 @@ TEST_F(SpillBlockManagerTest, dir_allocate_test) {
 }
 
 TEST_F(SpillBlockManagerTest, block_capacity_test) {
-    auto test_func = [&](std::shared_ptr<spill::BlockManager>& block_mgr, spill::DirPtr dir) {
-        ASSERT_OK(block_mgr->open());
-        {
-            spill::AcquireBlockOptions opts{.query_id = dummy_query_id,
-                                            .fragment_instance_id = dummy_query_id,
-                                            .plan_node_id = 1,
-                                            .name = "node1",
-                                            .block_size = 10};
-            auto res = block_mgr->acquire_block(opts);
-            ASSERT_TRUE(res.ok());
-            ASSERT_EQ(dir->get_current_size(), 10);
-            auto block = res.value();
+    {
+        auto log_block_mgr = std::make_shared<spill::LogBlockManager>(dummy_query_id, local_dir_mgr.get());
+        ASSERT_OK(log_block_mgr->open());
+        char vals[4096];
+        // 1. allocate the first block but not release it
+        spill::AcquireBlockOptions opts{.query_id = dummy_query_id,
+                                        .fragment_instance_id = dummy_query_id,
+                                        .plan_node_id = 1,
+                                        .name = "node1",
+                                        .block_size = 10};
+        opts.affinity_group = log_block_mgr->acquire_affinity_group();
+        auto res = log_block_mgr->acquire_block(opts);
+        ASSERT_TRUE(res.ok());
+        ASSERT_EQ(local_dir->get_current_size(), 10);
+        ASSERT_OK(res.value()->append(std::vector<Slice>{Slice(vals, 5)}));
+        ASSERT_EQ(local_dir->get_current_size(), 10);
+        ASSERT_OK(res.value()->append(std::vector<Slice>{Slice(vals, 5)}));
+        ASSERT_EQ(local_dir->get_current_size(), 10);
+        ASSERT_OK(res.value()->append(std::vector<Slice>{Slice(vals, 5)}));
+        ASSERT_EQ(local_dir->get_current_size(), 15);
+        ASSERT_OK(log_block_mgr->release_block(res.value()));
 
-            ASSERT_TRUE(block->preallocate(5));
-            // there are 10 bytes left unused, preallocate will not actually apply for space at this time
-            ASSERT_EQ(dir->get_current_size(), 10);
-            ASSERT_EQ(block->size(), 0);
-            char tmp[5];
-            ASSERT_OK(block->append({Slice(tmp, 5)}));
-            ASSERT_EQ(block->size(), 5);
+        res = log_block_mgr->acquire_block(opts);
+        ASSERT_TRUE(res.ok());
+        ASSERT_EQ(local_dir->get_current_size(), 25);
+        ASSERT_OK(log_block_mgr->release_block(res.value()));
 
-            // there are 5 bytes left unused, preallocate will not actually apply for space at this time
-            ASSERT_TRUE(block->preallocate(5));
-            ASSERT_EQ(dir->get_current_size(), 10);
-            ASSERT_OK(block->append({Slice(tmp, 5)}));
-            ASSERT_EQ(block->size(), 10);
-            ASSERT_EQ(dir->get_current_size(), 10);
-
-            // there is no remaining space, preallocate needs to actually apply for space
-            ASSERT_TRUE(block->preallocate(20));
-            ASSERT_EQ(block->size(), 10);
-            ASSERT_EQ(dir->get_current_size(), 30);
-        }
-
-        block_mgr.reset();
-        // after block_mgr is destroyed, all space should be released
-        ASSERT_EQ(dir->get_current_size(), 0);
-    };
-    std::shared_ptr<spill::BlockManager> log_block_mgr =
-            std::make_shared<spill::LogBlockManager>(dummy_query_id, local_dir_mgr.get());
-    test_func(log_block_mgr, local_dir);
-
-    std::shared_ptr<spill::BlockManager> file_block_mgr =
-            std::make_shared<spill::FileBlockManager>(dummy_query_id, remote_dir_mgr.get());
-    test_func(file_block_mgr, remote_dir);
+        res = log_block_mgr->acquire_block(opts);
+        ASSERT_TRUE(res.ok());
+        ASSERT_EQ(local_dir->get_current_size(), 25);
+    }
+    ASSERT_EQ(local_dir->get_current_size(), 0);
 }
 
 } // namespace starrocks::vectorized