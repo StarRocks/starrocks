d3396b2c9a24f1dde09f255e62675b0cc325bb91
Youngwb <yangwenbo_mailbox@163.com>
2025-04-08 10:08:33 +0800

    fix conflicts

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/operator/pattern/Pattern.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/operator/pattern/Pattern.java
@@ -32,6 +32,7 @@ public class Pattern {
             .add(OperatorType.LOGICAL_OLAP_SCAN)
             .add(OperatorType.LOGICAL_HIVE_SCAN)
             .add(OperatorType.LOGICAL_ICEBERG_SCAN)
+            .add(OperatorType.LOGICAL_DELTALAKE_SCAN)
             .add(OperatorType.LOGICAL_HUDI_SCAN)
             .add(OperatorType.LOGICAL_FILE_SCAN)
             .add(OperatorType.LOGICAL_SCHEMA_SCAN)
