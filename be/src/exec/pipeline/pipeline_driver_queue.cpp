0577e1aad0b291e98d20d6cfbe09f315a2c5faed
stdpain <drfeng08@gmail.com>
2025-04-08 15:00:34 +0800

    [Enhancement] support batch put back in pipeline driver queue
    Signed-off-by: stdpain <drfeng08@gmail.com>

--- be/src/exec/pipeline/pipeline_driver_queue.cpp
+++ be/src/exec/pipeline/pipeline_driver_queue.cpp
@@ -153,8 +153,6 @@ size_t QuerySharedDriverQueue::size() const {
 }
 
 void QuerySharedDriverQueue::update_statistics(const DriverRawPtr driver) {
-    std::lock_guard<std::mutex> lock(_global_mutex);
-
     _queues[driver->get_driver_queue_level()].update_accu_time(driver);
 }
 
@@ -232,8 +230,15 @@ void WorkGroupDriverQueue::close() {
 }
 
 void WorkGroupDriverQueue::put_back(const DriverRawPtr driver) {
-    std::lock_guard<std::mutex> lock(_global_mutex);
-    _put_back<false>(driver);
+    ::moodycamel::ProducerToken token(_local_queue);
+    CHECK(_local_queue.enqueue(token, driver));
+    AtomicRequestControler(_local_queue_cntl, [this]() {
+        std::unique_lock l(_global_mutex);
+        DriverRawPtr d;
+        while (_local_queue.try_dequeue(d)) {
+            _put_back<false>(d);
+        }
+    });
 }
 
 void WorkGroupDriverQueue::put_back(const std::vector<DriverRawPtr>& drivers) {
@@ -309,6 +314,11 @@ void WorkGroupDriverQueue::cancel(DriverRawPtr driver) {
 }
 
 void WorkGroupDriverQueue::update_statistics(const DriverRawPtr driver) {
+    // we don't have to update statistics when we only have one work group
+    if (ExecEnv::GetInstance()->workgroup_manager()->num_workgroups() <= 1) {
+        return;
+    }
+
     // TODO: reduce the lock scope
     std::lock_guard<std::mutex> lock(_global_mutex);
 
