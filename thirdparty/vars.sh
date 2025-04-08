97c9d034abab3f16d7f731bbdb912e9241063070
Smith Cruise <chendingchao1@126.com>
2025-04-08 12:19:01 +0000

    bump be also
    Signed-off-by: Smith Cruise <chendingchao1@126.com>

--- thirdparty/vars.sh
+++ thirdparty/vars.sh
@@ -97,10 +97,10 @@ OPENSSL_SOURCE=openssl-OpenSSL_1_1_1m
 OPENSSL_MD5SUM="710c2368d28f1a25ab92e25b5b9b11ec"
 
 # thrift
-THRIFT_DOWNLOAD="http://archive.apache.org/dist/thrift/0.20.0/thrift-0.20.0.tar.gz"
-THRIFT_NAME=thrift-0.20.0.tar.gz
-THRIFT_SOURCE=thrift-0.20.0
-THRIFT_MD5SUM="aadebde599e1f5235acd3c730721b873"
+THRIFT_DOWNLOAD="http://archive.apache.org/dist/thrift/0.21.0/thrift-0.21.0.tar.gz"
+THRIFT_NAME=thrift-0.21.0.tar.gz
+THRIFT_SOURCE=thrift-0.21.0
+THRIFT_MD5SUM="de687816fcde2b91a4dab31b8b02b1cf"
 
 # protobuf
 PROTOBUF_DOWNLOAD="https://github.com/google/protobuf/archive/v3.14.0.tar.gz"
