cd5c0fe6953370e42c197feda67caa0a5a1f96cf
zhangqiang <qiangzh95@gmail.com>
2025-04-14 11:36:09 +0800

    [Enhancement] Add enable_partition_aggregation into table property (#57881)
    Signed-off-by: sevev <qiangzh95@gmail.com>

--- fe/fe-core/src/test/java/com/starrocks/lake/CreateLakeTableTest.java
+++ fe/fe-core/src/test/java/com/starrocks/lake/CreateLakeTableTest.java
@@ -436,4 +436,19 @@ public class CreateLakeTableTest {
         Assert.assertTrue(nationProps.containsKey(PropertyAnalyzer.PROPERTIES_UNIQUE_CONSTRAINT));
         Assert.assertTrue(nationProps.containsKey(PropertyAnalyzer.PROPERTIES_FOREIGN_KEY_CONSTRAINT));
     }
+
+    @Test
+    public void testCreateTableWithPartitionAggregation() throws Exception {
+        ExceptionChecker.expectThrowsNoException(() -> createTable(
+                "create table lake_test.dup_test_enable_partition_agg (key1 int, key2 varchar(10))\n" +
+                        "distributed by hash(key1) buckets 3\n" +
+                        "properties('replication_num' = '1', 'enable_partition_aggregation' = 'true');"));
+        checkLakeTable("lake_test", "dup_test_enable_partition_agg");
+
+        String sql = "show create table lake_test.dup_test_enable_partition_agg";
+        ShowCreateTableStmt showCreateTableStmt =
+                (ShowCreateTableStmt) UtFrameUtils.parseStmtWithNewParser(sql, connectContext);
+        ShowResultSet resultSet = ShowExecutor.execute(showCreateTableStmt, connectContext);
+        Assert.assertFalse(resultSet.getResultRows().isEmpty());
+    }
 }
