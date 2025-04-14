df00fb05c6284234eba26b15dccd9b6ec808af8c
shuming.li <ming.moriarty@gmail.com>
2025-04-14 10:08:19 +0800

    [BugFix] Fix some cost predicate bugs (#57929)
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>
    (cherry picked from commit 8f3265c2dbbc222e59a60abd56d5d3e98e0c1170)

--- fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
@@ -1332,7 +1332,7 @@ public class StmtExecutor {
         // Predict the cost of this query
         if (Config.enable_query_cost_prediction) {
             CostPredictor predictor = getCostPredictor();
-            long memBytes = predictor.predictMemoryBytes(execPlan);
+            long memBytes = predictor.tryPredictMemoryBytes(execPlan);
             coord.setPredictedCost(memBytes);
             context.getAuditEventBuilder().setPredictMemBytes(memBytes);
         }
