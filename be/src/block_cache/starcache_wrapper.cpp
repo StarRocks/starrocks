6f9fd0fee02af757d099a9d3e8518a2674eeb055
GavinMar <yangguansuo@starrocks.com>
2025-04-16 09:37:33 +0800

    [BugFix] Replace the old disk_id with device_id for disk instances in datacache disk monitor because the `DiskInfo::disk_id()` may return -1 in some virtualization environments.
    Signed-off-by: GavinMar <yangguansuo@starrocks.com>

--- be/src/block_cache/starcache_wrapper.cpp
+++ be/src/block_cache/starcache_wrapper.cpp
@@ -54,7 +54,7 @@ Status StarCacheWrapper::init(const CacheOptions& options) {
 
     _enable_tiered_cache = options.enable_tiered_cache;
     _enable_datacache_persistence = options.enable_datacache_persistence;
-    _cache = std::make_unique<starcache::StarCache>();
+    _cache = std::make_shared<starcache::StarCache>();
     return to_status(_cache->init(opt));
 }
 
