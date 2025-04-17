57c21be59ed6a9d364fd19d50b813119bf8b85fd
Zac-saodiseng <3253345336@qq.com>
2025-04-17 14:41:11 +0800

    [Enhancement]Release off-heap memory and resource
    Signed-off-by: Zac-saodiseng <3253345336@qq.com>

--- fe/fe-core/src/test/java/com/starrocks/service/ArrowFlightSqlJDBCTestV2.java
+++ fe/fe-core/src/test/java/com/starrocks/service/ArrowFlightSqlJDBCTestV2.java
@@ -1,6 +1,25 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
 package com.starrocks.service;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ArrowFlightSqlJDBCTestV2 {
 
@@ -44,27 +63,29 @@ public class ArrowFlightSqlJDBCTestV2 {
         }
     }
 
-    private static void testQuery(Statement stmt, String sql) throws SQLException {
-        System.out.println("TestCase " + testCaseNum + " - QUERY: " + sql);
-        try (ResultSet rs = stmt.executeQuery(sql)) {
+    private static void testQuery(Statement stmt, String sql) throws Exception {
+        System.out.println("Test Case:" + testCaseNum);
+        System.out.println("Executing query:" + sql);
+        ResultSet rs = stmt.executeQuery(sql);
+        System.out.println("Result:" + sql);
+        while (rs.next()) {
             int columnCount = rs.getMetaData().getColumnCount();
-            System.out.println("Result:");
-            while (rs.next()) {
-                for (int i = 1; i <= columnCount; i++) {
-                    System.out.print(rs.getString(i) + "\t");
-                }
-                System.out.println();
+            for (int i = 1; i <= columnCount; i++) {
+                System.out.print(rs.getString(i) + "\t");
             }
+            System.out.println();
         }
-        System.out.println();
+        rs.close();
         testCaseNum++;
+        System.out.println();
     }
 
-    private static void testUpdate(Statement stmt, String sql) throws SQLException {
-        System.out.println("TestCase " + testCaseNum + " - UPDATE: " + sql);
-        int result = stmt.executeUpdate(sql);
-        System.out.println("Result: " + result + " rows affected");
-        System.out.println();
+    private static void testUpdate(Statement stmt, String sql) throws Exception {
+        System.out.println("Test Case:" + testCaseNum);
+        System.out.println("Executing query:" + sql);
+        stmt.executeUpdate(sql);
+        System.out.println("Result: Success");
         testCaseNum++;
+        System.out.println();
     }
 }
