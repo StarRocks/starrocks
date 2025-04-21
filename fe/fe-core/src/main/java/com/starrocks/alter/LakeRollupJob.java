6a4b5c0236b0eb494a40e959b7b9b093b2d627a0
fgump <smartlxh@gmail.com>
2025-04-21 16:32:12 +0800

    [BugFix] Fix ClassCastException caused by alter mv (#58187)
    Signed-off-by: smartlxh <smartlxh@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/alter/LakeRollupJob.java
+++ fe/fe-core/src/main/java/com/starrocks/alter/LakeRollupJob.java
@@ -38,7 +38,6 @@ import com.starrocks.common.ErrorReportException;
 import com.starrocks.common.FeConstants;
 import com.starrocks.common.util.TimeUtils;
 import com.starrocks.common.util.concurrent.MarkedCountDownLatch;
-import com.starrocks.lake.LakeTable;
 import com.starrocks.lake.LakeTablet;
 import com.starrocks.lake.Utils;
 import com.starrocks.proto.TxnInfoPB;
@@ -153,7 +152,7 @@ public class LakeRollupJob extends LakeTableSchemaChangeJobBase {
         AgentBatchTask batchTask = new AgentBatchTask();
         MarkedCountDownLatch<Long, Long> countDownLatch;
         try (ReadLockedDatabase db = getReadLockedDatabase(dbId)) {
-            LakeTable table = getTableOrThrow(db, tableId);
+            OlapTable table = getTableOrThrow(db, tableId);
             Preconditions.checkState(table.getState() == OlapTable.OlapTableState.ROLLUP);
 
             if (enableTabletCreationOptimization) {
@@ -242,7 +241,7 @@ public class LakeRollupJob extends LakeTableSchemaChangeJobBase {
 
         // Add shadow indexes to table.
         try (WriteLockedDatabase db = getWriteLockedDatabase(dbId)) {
-            LakeTable table = getTableOrThrow(db, tableId);
+            OlapTable table = getTableOrThrow(db, tableId);
             if (table.getState() != OlapTable.OlapTableState.ROLLUP) {
                 throw new IllegalStateException("Table State doesn't equal to ROLLUP, it is " + table.getState() + ".");
             }
@@ -294,7 +293,7 @@ public class LakeRollupJob extends LakeTableSchemaChangeJobBase {
 
         Map<Long, List<TColumn>> indexToThriftColumns = new HashMap<>();
         try (ReadLockedDatabase db = getReadLockedDatabase(dbId)) {
-            LakeTable tbl = getTableOrThrow(db, tableId);
+            OlapTable tbl = getTableOrThrow(db, tableId);
             Preconditions.checkState(tbl.getState() == OlapTable.OlapTableState.ROLLUP);
             // only needs to analyze once
             AlterReplicaTask.RollupJobV2Params rollupJobV2Params =
@@ -377,7 +376,7 @@ public class LakeRollupJob extends LakeTableSchemaChangeJobBase {
         }
 
         try (WriteLockedDatabase db = getWriteLockedDatabase(dbId)) {
-            LakeTable table = getTableOrThrow(db, tableId);
+            OlapTable table = getTableOrThrow(db, tableId);
             commitVersionMap = new HashMap<>();
             for (long physicalPartitionId : physicalPartitionIdToRollupIndex.keySet()) {
                 PhysicalPartition physicalPartition = table.getPhysicalPartition(physicalPartitionId);
@@ -416,7 +415,7 @@ public class LakeRollupJob extends LakeTableSchemaChangeJobBase {
         }
 
         try (WriteLockedDatabase db = getWriteLockedDatabase(dbId)) {
-            LakeTable table = (db != null) ? db.getTable(tableId) : null;
+            OlapTable table = (db != null) ? db.getTable(tableId) : null;
             if (table == null) {
                 LOG.info("database or table been dropped while doing schema change job {}", jobId);
                 return;
@@ -453,7 +452,7 @@ public class LakeRollupJob extends LakeTableSchemaChangeJobBase {
         }
 
         try (WriteLockedDatabase db = getWriteLockedDatabase(dbId)) {
-            LakeTable table = (db != null) ? db.getTable(tableId) : null;
+            OlapTable table = (db != null) ? db.getTable(tableId) : null;
             if (table != null) {
                 removeRollupIndex(table);
             }
@@ -536,7 +535,7 @@ public class LakeRollupJob extends LakeTableSchemaChangeJobBase {
         }
 
         try (WriteLockedDatabase db = getWriteLockedDatabase(dbId)) {
-            LakeTable table = (db != null) ? db.getTable(tableId) : null;
+            OlapTable table = (db != null) ? db.getTable(tableId) : null;
             if (table == null) {
                 return; // do nothing if the table has been dropped.
             }
@@ -607,7 +606,7 @@ public class LakeRollupJob extends LakeTableSchemaChangeJobBase {
         return GlobalStateMgr.getCurrentState().getGtidGenerator().nextGtid();
     }
 
-    void addRollIndexToCatalog(@NotNull LakeTable tbl) {
+    void addRollIndexToCatalog(@NotNull OlapTable tbl) {
         for (Partition partition : tbl.getPartitions()) {
             for (PhysicalPartition physicalPartition : partition.getSubPartitions()) {
                 long partitionId = physicalPartition.getId();
@@ -635,7 +634,7 @@ public class LakeRollupJob extends LakeTableSchemaChangeJobBase {
         }
     }
 
-    void updateNextVersion(@NotNull LakeTable table) {
+    void updateNextVersion(@NotNull OlapTable table) {
         for (long partitionId : physicalPartitionIdToRollupIndex.keySet()) {
             PhysicalPartition partition = table.getPhysicalPartition(partitionId);
             long commitVersion = commitVersionMap.get(partitionId);
@@ -647,7 +646,7 @@ public class LakeRollupJob extends LakeTableSchemaChangeJobBase {
 
     boolean readyToPublishVersion() throws AlterCancelException {
         try (ReadLockedDatabase db = getReadLockedDatabase(dbId)) {
-            LakeTable table = getTableOrThrow(db, tableId);
+            OlapTable table = getTableOrThrow(db, tableId);
             for (long partitionId : physicalPartitionIdToRollupIndex.keySet()) {
                 PhysicalPartition partition = table.getPhysicalPartition(partitionId);
                 Preconditions.checkState(partition != null, partitionId);
@@ -665,7 +664,7 @@ public class LakeRollupJob extends LakeTableSchemaChangeJobBase {
 
     protected boolean lakePublishVersion() {
         try (ReadLockedDatabase db = getReadLockedDatabase(dbId)) {
-            LakeTable table = getTableOrThrow(db, tableId);
+            OlapTable table = getTableOrThrow(db, tableId);
             for (long partitionId : physicalPartitionIdToRollupIndex.keySet()) {
                 PhysicalPartition physicalPartition = table.getPhysicalPartition(partitionId);
                 Preconditions.checkState(physicalPartition != null, partitionId);
@@ -705,7 +704,7 @@ public class LakeRollupJob extends LakeTableSchemaChangeJobBase {
         }
     }
 
-    void removeRollupIndex(@NotNull LakeTable table) {
+    void removeRollupIndex(@NotNull OlapTable table) {
         TabletInvertedIndex invertedIndex = GlobalStateMgr.getCurrentState().getTabletInvertedIndex();
         for (Long partitionId : physicalPartitionIdToRollupIndex.keySet()) {
             MaterializedIndex rollupIndex = physicalPartitionIdToRollupIndex.get(partitionId);
