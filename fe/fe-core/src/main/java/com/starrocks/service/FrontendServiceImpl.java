193815efad047356b80925b9740155b72f014f71
Wu Xueyang <45445803+wuxueyang96@users.noreply.github.com>
2025-04-21 16:24:35 +0800

    [BugFix] store exception at TransactionResult. (#57979)
    Signed-off-by: wuxueyang.wxy <wuxueyang.wxy@alibaba-inc.com>
    (cherry picked from commit eeb790609521bf7ee38a75b56101e23022fc5bbc)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/service/FrontendServiceImpl.java

--- fe/fe-core/src/main/java/com/starrocks/service/FrontendServiceImpl.java
+++ fe/fe-core/src/main/java/com/starrocks/service/FrontendServiceImpl.java
@@ -1232,7 +1232,11 @@ public class FrontendServiceImpl implements FrontendService.Iface {
         return result;
     }
 
+<<<<<<< HEAD
     private long loadTxnBeginImpl(TLoadTxnBeginRequest request, String clientIp) throws UserException {
+=======
+    private long loadTxnBeginImpl(TLoadTxnBeginRequest request, String clientIp) throws Exception {
+>>>>>>> eeb7906095 ([BugFix] store exception at TransactionResult. (#57979))
         checkPasswordAndLoadPriv(request.getUser(), request.getPasswd(), request.getDb(),
                 request.getTbl(), request.getUser_ip());
 
@@ -1283,7 +1287,11 @@ public class FrontendServiceImpl implements FrontendService.Iface {
                 request.getUser(), request.getUser_ip(), timeoutSecond * 1000, resp, false, warehouseId);
         if (!resp.stateOK()) {
             LOG.warn(resp.msg);
+<<<<<<< HEAD
             throw new UserException(resp.msg);
+=======
+            throw resp.getException();
+>>>>>>> eeb7906095 ([BugFix] store exception at TransactionResult. (#57979))
         }
 
         StreamLoadTask task = streamLoadManager.getTaskByLabel(request.getLabel());
