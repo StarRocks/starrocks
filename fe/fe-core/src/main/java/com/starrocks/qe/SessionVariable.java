d7c04ecc790d7afb0f1a66ab08985f559ec82958
shuming.li <ming.moriarty@gmail.com>
2025-04-24 09:54:37 +0800

    Disable broadcast join if broadcast_row_limit <= 0
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
@@ -3412,6 +3412,10 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
         return broadcastRowCountLimit;
     }
 
+    public void setBroadcastRowCountLimit(long broadcastRowCountLimit) {
+        this.broadcastRowCountLimit = broadcastRowCountLimit;
+    }
+
     public double getBroadcastRightTableScaleFactor() {
         return broadcastRightTableScaleFactor;
     }
