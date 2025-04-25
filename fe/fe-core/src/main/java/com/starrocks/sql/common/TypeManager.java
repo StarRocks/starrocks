480e99e627ea995acd82ca09192145b7239e55f9
pufan <pufan.kk@alibaba-inc.com>
2025-04-25 10:47:24 +0800

    [Enhancement] In the IN expression, date and string type casting are supported
    Signed-off-by: pufan <pufan.kk@alibaba-inc.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/common/TypeManager.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/common/TypeManager.java
@@ -156,6 +156,11 @@ public class TypeManager {
         Preconditions.checkState(!types.isEmpty());
         Type compatibleType = types.get(0);
 
+        //date_format(str_to_date('20241209','%Y%m%d'),'%Y-%m-%d 00:00:00') in (str_to_date('20241209','%Y%m%d'),str_to_date('20241208','%Y%m%d'))
+        //The above example needs to convert the string type to the date type
+        if (!isBetween && types.stream().skip(1).allMatch(e -> e == types.get(1))) {
+            return getCompatibleTypeForBinary(false, types.get(0), types.get(1));
+        }
         for (int i = 1; i < types.size(); i++) {
             compatibleType = getCompatibleTypeForBetweenAndIn(compatibleType, types.get(i), isBetween);
         }
