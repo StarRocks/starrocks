d5c6c9bf6c9c2ec9f5aaf64d99ff3834b7bc5501
shuming.li <ming.moriarty@gmail.com>
2025-04-25 16:00:06 +0800

    [BugFix] Disable broadcast join if broadcast_row_limit is set equal or below than 0 (#58307)
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
@@ -3531,6 +3531,10 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
         return broadcastRowCountLimit;
     }
 
+    public void setBroadcastRowCountLimit(long broadcastRowCountLimit) {
+        this.broadcastRowCountLimit = broadcastRowCountLimit;
+    }
+
     public double getBroadcastRightTableScaleFactor() {
         return broadcastRightTableScaleFactor;
     }
