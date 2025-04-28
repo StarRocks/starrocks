ddb17792d71da93adab9131b9e2c2193a9c4e290
hmx <1639097204@qq.com>
2025-04-28 13:57:43 +0800

    [Enhancement] filter node with shutdown status in primary replica selection (#58357)
    Signed-off-by: MatthewH00 <1639097204@qq.com>
    Signed-off-by: hmx <1639097204@qq.com>

--- fe/fe-core/src/test/java/com/starrocks/planner/OlapTableSinkTest.java
+++ fe/fe-core/src/test/java/com/starrocks/planner/OlapTableSinkTest.java
@@ -52,12 +52,16 @@ import com.starrocks.common.Status;
 import com.starrocks.common.jmockit.Deencapsulation;
 import com.starrocks.server.GlobalStateMgr;
 import com.starrocks.sql.ast.PartitionValue;
+import com.starrocks.system.Backend;
+import com.starrocks.system.BackendHbResponse;
+import com.starrocks.system.ComputeNode;
 import com.starrocks.system.SystemInfoService;
 import com.starrocks.thrift.TDataSink;
 import com.starrocks.thrift.TExplainLevel;
 import com.starrocks.thrift.TOlapTableLocationParam;
 import com.starrocks.thrift.TOlapTablePartition;
 import com.starrocks.thrift.TOlapTablePartitionParam;
+import com.starrocks.thrift.TStatusCode;
 import com.starrocks.thrift.TStorageMedium;
 import com.starrocks.thrift.TStorageType;
 import com.starrocks.thrift.TTabletLocation;
@@ -67,7 +71,9 @@ import com.starrocks.thrift.TWriteQuorumType;
 import com.starrocks.utframe.UtFrameUtils;
 import mockit.Expectations;
 import mockit.Injectable;
+import mockit.Mock;
 import mockit.Mocked;
+import mockit.MockUp;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
@@ -308,6 +314,25 @@ public class OlapTableSinkTest {
         long physicalPartitionId = 6L;
         long replicaId = 10L;
         long backendId = 20L;
+        
+        //init be node
+        Backend be1 = new Backend(1001L, "127.0.0.1", 9050);
+        Backend be2 = new Backend(1002L, "127.0.0.2", 9050);
+        Backend be3 = new Backend(1003L, "127.0.0.3", 9050);
+        be1.setAlive(true);
+        be2.setAlive(true);
+        be3.setAlive(true);
+        
+        Map<Long, Backend> idToBackendRef = new HashMap<>();
+        idToBackendRef.put(be1.getId(), be1);
+        idToBackendRef.put(be2.getId(), be2);
+        idToBackendRef.put(be3.getId(), be3);
+        new MockUp<SystemInfoService>() {
+            @Mock
+            public Backend getBackend(long backendId) {
+                return idToBackendRef.get(backendId);
+            }
+        };
 
         // Columns
         List<Column> columns = new ArrayList<Column>();
@@ -320,9 +345,9 @@ public class OlapTableSinkTest {
 
         for (int i = 0; i < 9; i++) {
             // Replica
-            Replica replica1 = new Replica(replicaId, backendId, Replica.ReplicaState.NORMAL, 1, 0);
-            Replica replica2 = new Replica(replicaId + 1, backendId + 1, Replica.ReplicaState.NORMAL, 1, 0);
-            Replica replica3 = new Replica(replicaId + 2, backendId + 2, Replica.ReplicaState.NORMAL, 1, 0);
+            Replica replica1 = new Replica(replicaId, be1.getId(), Replica.ReplicaState.NORMAL, 1, 0);
+            Replica replica2 = new Replica(replicaId + 1, be2.getId(), Replica.ReplicaState.NORMAL, 1, 0);
+            Replica replica3 = new Replica(replicaId + 2, be3.getId(), Replica.ReplicaState.NORMAL, 1, 0);
 
             // Tablet
             LocalTablet tablet = new LocalTablet(tabletId);
@@ -552,5 +577,70 @@ public class OlapTableSinkTest {
         sink.complete();
         LOG.info("sink is {}", sink.toThrift());
         LOG.info("{}", sink.getExplainString("", TExplainLevel.NORMAL));
+    }
+
+    @Test
+    public void testFindPrimaryReplica() throws StarRocksException {
+
+        //init be node
+        Backend be1 = new Backend(1001L, "127.0.0.1", 9050);
+        Backend be2 = new Backend(1002L, "127.0.0.2", 9050);
+        Backend be3 = new Backend(1003L, "127.0.0.3", 9050);
+        be1.setAlive(true);
+        be2.setAlive(false);
+        be3.setAlive(true);
+
+        Map<Long, Backend> idToBackendRef = new HashMap<>();
+        idToBackendRef.put(be1.getId(), be1);
+        idToBackendRef.put(be2.getId(), be2);
+        idToBackendRef.put(be3.getId(), be3);
+        new MockUp<SystemInfoService>() {
+            @Mock
+            public Backend getBackend(long backendId) {
+                return idToBackendRef.get(backendId);
+            }
+        };
+        //init primary replica num for be node
+        Map<Long, Long> bePrimaryMap = new HashMap<>();
+        bePrimaryMap.put(be1.getId(), 2L);
+        bePrimaryMap.put(be2.getId(), 0L);
+        bePrimaryMap.put(be3.getId(), 1L);
+
+        OlapTable olapTable = new OlapTable();
+        SystemInfoService infoService = GlobalStateMgr.getCurrentState().getNodeMgr().getClusterInfo();
+        MaterializedIndex index = new MaterializedIndex(1L, MaterializedIndex.IndexState.NORMAL);
+        List<Long> selectedBackedIds = Lists.newArrayList();
+
+        //1.check primary replica selection in multiple replica
+        Replica replica1 = new Replica(11L, be1.getId(), Replica.ReplicaState.NORMAL, 1, 0);
+        Replica replica2 = new Replica(22L, be2.getId(), Replica.ReplicaState.NORMAL, 1, 0);
+        Replica replica3 = new Replica(33L, be3.getId(), Replica.ReplicaState.NORMAL, 1, 0);
+        replica1.setLastWriteFail(false);
+        replica2.setLastWriteFail(false);
+        replica3.setLastWriteFail(false);
+        List<Replica> multipleReplicaList = new ArrayList<>();
+        multipleReplicaList.add(replica1);
+        multipleReplicaList.add(replica2);
+        multipleReplicaList.add(replica3);
+
+        int lowUsageIndex1 = OlapTableSink.findPrimaryReplica(olapTable, bePrimaryMap, infoService,
+                index, selectedBackedIds, multipleReplicaList);
+        //note: even though in bePrimaryMap, primary replica num in be2 < primary replica num in be3,
+        //      but be2 is in shutting down, so choose replica3 as primary replica.
+        Assert.assertEquals(multipleReplicaList.get(lowUsageIndex1).getId(), replica3.getId());
+        Assert.assertEquals(multipleReplicaList.get(lowUsageIndex1).getBackendId(), be3.getId());
+
+        //2.check primary replica selection in single replica
+        Replica replica4 = new Replica(44L, be2.getId(), Replica.ReplicaState.NORMAL, 1, 0);
+        replica4.setLastWriteFail(false);
+        List<Replica> singleReplicaList = new ArrayList<>();
+        singleReplicaList.add(replica4);
+
+        int lowUsageIndex2 = OlapTableSink.findPrimaryReplica(olapTable, bePrimaryMap, infoService,
+                index, selectedBackedIds, singleReplicaList);
+        //note: even though be2 is in shutting down, to ensure the load job could loading normally,
+        //      be2 SHUTDOWN status could not be checked, so choose replica4 as primary replica. 
+        Assert.assertEquals(singleReplicaList.get(lowUsageIndex2).getId(), replica4.getId());
+        Assert.assertEquals(singleReplicaList.get(lowUsageIndex2).getBackendId(), be2.getId());
     }
 }
