9a67925d9f06c9dca9a5ced334881e29c0144675
sevev <qiangzh95@gmail.com>
2025-04-29 20:43:25 +0800

    add some comment
    Signed-off-by: sevev <qiangzh95@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
+++ fe/fe-core/src/main/java/com/starrocks/server/LocalMetastore.java
@@ -1805,6 +1805,9 @@ public class LocalMetastore implements ConnectorMetadata, MVRepairHandler, Memor
         }
 
         TabletTaskExecutor.CreateTabletOption option = new TabletTaskExecutor.CreateTabletOption();
+        // Enable `tablet_creation_optimization` creates only one shared tablet metadata for all tablets under a partition. 
+        // Enable `enable_partition_aggregation` reuses the optimization logic.
+        // These two configure only use in shared-data mode
         option.setEnableTabletCreationOptimization(table.isCloudNativeTableOrMaterializedView()
                 && (Config.lake_enable_tablet_creation_optimization || table.enablePartitionAggregation()));
         option.setGtid(GlobalStateMgr.getCurrentState().getGtidGenerator().nextGtid());
