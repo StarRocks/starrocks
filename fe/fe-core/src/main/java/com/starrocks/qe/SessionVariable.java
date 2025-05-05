e815af6e151544b0bdbd403af74b6fac64c39fc0
PengFei Li <lpengfei2016@gmail.com>
2025-05-05 15:55:57 +0800

    [Enhancement] Merge commit supports to get load profile (#58545)
    Signed-off-by: PengFei Li <lpengfei2016@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
@@ -3668,6 +3668,10 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
         return profileTimeout;
     }
 
+    public void setRuntimeProfileReportInterval(int runtimeProfileReportInterval) {
+        this.runtimeProfileReportInterval = runtimeProfileReportInterval;
+    }
+
     public int getRuntimeProfileReportInterval() {
         return runtimeProfileReportInterval;
     }
