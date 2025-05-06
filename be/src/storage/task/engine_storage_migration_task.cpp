10f0889bbe172262702862a44d2b04b2888895fd
srlch <linzichao@starrocks.com>
2025-05-06 10:02:33 +0800

    fix
    Signed-off-by: srlch <linzichao@starrocks.com>

--- be/src/storage/task/engine_storage_migration_task.cpp
+++ be/src/storage/task/engine_storage_migration_task.cpp
@@ -538,9 +538,6 @@ Status EngineStorageMigrationTask::_finish_primary_key_migration(const TabletSha
 
         auto tablet_manager = StorageEngine::instance()->tablet_manager();
 
-        // don't wait rebuild pk index to finish because migration task does not support increment migration
-        // and needs to be completed as soon as possible
-
         // create_tablet_from_meta_snapshot does not reset rowset_seg_id in snapshot_meta. The GC progress for
         // the old tablet maybe conflict in rowset_seg_id with the new one. But it is safe because the rowset_seg_id
         // conflict in GC progress will only affect the delvector/dcg cache (delete the cache) for the new tablet but
