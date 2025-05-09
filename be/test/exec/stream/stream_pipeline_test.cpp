b52d46fc1405134c75e3e0d2e6c2637c782d6a09
Kevin Cai <caixiaohua@starrocks.com>
2025-05-10 00:32:44 +0800

    [UT] clean clang compiler warnings (#58840)
    Signed-off-by: Kevin Cai <kevin.cai@celerdata.com>

--- be/test/exec/stream/stream_pipeline_test.cpp
+++ be/test/exec/stream/stream_pipeline_test.cpp
@@ -207,7 +207,7 @@ Status StreamPipelineTest::wait_until_epoch_finished(const EpochInfo& epoch_info
     };
 
     while (!are_all_drivers_parked_func()) {
-        sleep(0.1);
+        usleep(100000);
     }
     VLOG_ROW << "WaitUntilEpochEnd Done " << epoch_info.debug_string();
     return Status::OK();
