001fdabb28e7b589150dd255ff09a750c2102855
sevev <qiangzh95@gmail.com>
2025-05-13 19:00:10 +0800

    update
    Signed-off-by: sevev <qiangzh95@gmail.com>

--- be/src/storage/utils.cpp
+++ be/src/storage/utils.cpp
@@ -321,9 +321,6 @@ bool valid_decimal(const string& value_str, uint32_t precision, uint32_t frac) {
 }
 
 bool valid_datetime(const string& value_str) {
-    //const char* datetime_pattern =
-    //        "((?:\\d){4})-((?:\\d){2})-((?:\\d){2})[ ]*"
-    //        "(((?:\\d){2}):((?:\\d){2}):((?:\\d){2}))?";
     const char* datetime_pattern =
             "((?:\\d){4})-((?:\\d){2})-((?:\\d){2})[ ]*"
             "(((?:\\d){2}):((?:\\d){2}):((?:\\d){2})(\\.(\\d{1,6}))?)?";
