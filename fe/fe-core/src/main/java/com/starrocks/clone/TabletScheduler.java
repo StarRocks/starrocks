c5b474fb6bb0b602e246abfc26a8670041078a40
Yixin Luo <18810541851@163.com>
2025-05-15 19:44:53 +0800

    [BugFix] Fix the issue of frequent version loss during imports (#59006)
    Signed-off-by: luohaha <18810541851@163.com>
    (cherry picked from commit 8650a3dbd87bac17104ca4b3dbdd6d69f2a35a4e)

--- fe/fe-core/src/main/java/com/starrocks/clone/TabletScheduler.java
+++ fe/fe-core/src/main/java/com/starrocks/clone/TabletScheduler.java
@@ -1757,7 +1757,6 @@ public class TabletScheduler extends FrontendDaemon {
 
     private void releaseTabletCtx(TabletSchedCtx tabletCtx, TabletSchedCtx.State state) {
         tabletCtx.setState(state);
-        tabletCtx.resetDecommissionedReplicaState();
         tabletCtx.releaseResource(this);
         tabletCtx.setFinishedTime(System.currentTimeMillis());
     }
