4b24dbb89b5cb8dcdaf390f76f76fe4b90c91f1e
PengFei Li <lpengfei2016@gmail.com>
2025-05-18 21:22:21 +0800

    init
    Signed-off-by: PengFei Li <lpengfei2016@gmail.com>

--- be/test/runtime/local_tablets_channel_test.cpp
+++ be/test/runtime/local_tablets_channel_test.cpp
@@ -76,9 +76,14 @@ protected:
         _tablets_channel.reset();
         _load_channel.reset();
         if (_tablet) {
-            _tablet.reset();
-            auto st = StorageEngine::instance()->tablet_manager()->drop_tablet(_tablet_id);
+            auto st = StorageEngine::instance()->tablet_manager()->drop_tablet(_tablet->tablet_id());
             ASSERT_OK(st);
+            _tablet.reset();
+        }
+        for (auto& tablet : _tablets) {
+            auto st = StorageEngine::instance()->tablet_manager()->drop_tablet(tablet->tablet_id());
+            ASSERT_OK(st);
+            tablet.reset();
         }
     }
 
@@ -171,6 +176,69 @@ protected:
         return request;
     }
 
+    struct TabletReplicas {
+        int64_t tablet_id;
+        ReplicaState replica_state;
+        std::vector<PNetworkAddress> nodes;
+    };
+
+    PTabletWriterOpenRequest _create_open_request(int64_t node_id, std::vector<TabletReplicas> tablet_vec) {
+        PTabletWriterOpenRequest request;
+        request.mutable_id()->CopyFrom(_load_id);
+        request.set_index_id(_index_id);
+        request.set_txn_id(_txn_id);
+        request.set_is_lake_tablet(false);
+        request.set_is_replicated_storage(true);
+        request.set_node_id(node_id);
+        request.set_sink_id(0);
+        request.set_write_quorum(WriteQuorumTypePB::ONE);
+        request.set_miss_auto_increment_column(false);
+        request.set_table_id(_table_id);
+        request.set_is_incremental(false);
+        request.set_num_senders(1);
+        request.set_sender_id(0);
+        request.set_need_gen_rollup(false);
+        request.set_load_channel_timeout_s(10);
+        request.set_is_vectorized(true);
+        request.set_timeout_ms(10000);
+        request.set_immutable_tablet_size(0);
+        for (auto& tablet_replicas : tablet_vec) {
+            auto tablet = request.add_tablets();
+            tablet->set_partition_id(_partition_id);
+            tablet->set_tablet_id(tablet_replicas.tablet_id);
+            for (auto& node : tablet_replicas.nodes) {
+                auto replica = tablet->add_replicas();
+                replica->CopyFrom(node);
+            }
+        }
+
+        auto schema = request.mutable_schema();
+        schema->set_db_id(_db_id);
+        schema->set_table_id(_table_id);
+        schema->set_version(1);
+        auto index = schema->add_indexes();
+        index->set_id(_index_id);
+        index->set_schema_hash(0);
+        for (int i = 0, sz = _tablet->tablet_schema()->num_columns(); i < sz; i++) {
+            auto slot = request.mutable_schema()->add_slot_descs();
+            auto& column = _tablet->tablet_schema()->column(i);
+            slot->set_id(i);
+            slot->set_byte_offset(i * sizeof(int) /*unused*/);
+            slot->set_col_name(std::string(column.name()));
+            slot->set_slot_idx(i);
+            slot->set_is_materialized(true);
+            slot->mutable_slot_type()->add_types()->mutable_scalar_type()->set_type(column.type());
+            index->add_columns(std::string(column.name()));
+        }
+        auto tuple_desc = schema->mutable_tuple_desc();
+        tuple_desc->set_id(1);
+        tuple_desc->set_byte_size(8 /*unused*/);
+        tuple_desc->set_num_null_bytes(0 /*unused*/);
+        tuple_desc->set_table_id(_table_id);
+
+        return request;
+    }
+
     Chunk generate_data(int64_t chunk_size) {
         std::vector<int> v0(chunk_size);
         std::vector<int> v1(chunk_size);
@@ -200,9 +268,10 @@ protected:
     std::unique_ptr<RuntimeProfile> _root_profile;
     std::unique_ptr<LoadChannelMgr> _load_channel_mgr;
     std::shared_ptr<LoadChannel> _load_channel;
-    std::shared_ptr<TabletsChannel> _tablets_channel;
+    std::shared_ptr<LocalTabletsChannel> _tablets_channel;
 
     TabletSharedPtr _tablet;
+    std::vector<TabletSharedPtr> _tablets;
     std::shared_ptr<Schema> _schema;
     std::shared_ptr<OlapTableSchemaParam> _schema_param;
 
@@ -421,6 +490,87 @@ TEST_F(LocalTabletsChannelTest, test_cancel_secondary_replica_rpc_fail) {
             << add_chunk_response.status().error_msgs(0);
     ASSERT_TRUE(close_channel);
     ASSERT_EQ(1, num_cancel);
+}
+
+using RpcReplicaStatusPair = std::pair<PLoadReplicaStatusRequest*, ReusableClosure<PLoadReplicaStatusResult>*>;
+
+TEST_F(LocalTabletsChannelTest, test_secondary_replicas_waiter) {
+    std::vector<TabletReplicas> tablet_vec;
+    for (int i = 0; i < 3; i++) {
+        TabletReplicas tablet_replicas;
+        tablet_replicas.tablet_id = rand();
+        for (int j = 0; j < 3; j++) {
+            PNetworkAddress node;
+            node.set_node_id(j);
+            node.set_host(fmt::format("127.0.0.{}", j));
+            tablet_replicas.nodes.push_back(node);
+        }
+        tablet_vec.push_back(tablet_replicas);
+    }
+
+    for (auto& tablet_replicas : tablet_vec) {
+        auto tablet = create_tablet(tablet_replicas.tablet_id, rand());
+        _tablets.push_back(tablet);
+    }
+
+    PTabletWriterOpenRequest open_request = _create_open_request(1, tablet_vec);
+    PTabletWriterOpenResult open_result;
+    ASSERT_OK(_tablets_channel->open(open_request, &open_result, _schema_param, false));
+
+    auto old_check_interval_on_success = config::load_replica_status_check_interval_ms_on_success;
+    auto old_check_interval_on_failure = config::load_replica_status_check_interval_ms_on_failure;
+    DeferOp defer([&] {
+        SyncPoint::GetInstance()->DisableProcessing();
+        SyncPoint::GetInstance()->ClearCallBack("LocalTabletsChannel::rpc::get_load_replica_status");
+        config::load_replica_status_check_interval_ms_on_success = old_check_interval_on_success;
+        config::load_replica_status_check_interval_ms_on_failure = old_check_interval_on_failure;
+    });
+
+    config::load_replica_status_check_interval_ms_on_success = 100;
+    config::load_replica_status_check_interval_ms_on_failure = 50;
+    int num_get_replica_status = 0;
+    SyncPoint::GetInstance()->EnableProcessing();
+    SyncPoint::GetInstance()->SetCallBack("LocalTabletsChannel::rpc::get_load_replica_status", [&](void* arg) {
+        RpcReplicaStatusPair* rpc_pair = (RpcReplicaStatusPair*)arg;
+        PLoadReplicaStatusRequest* request = (PLoadReplicaStatusRequest*)rpc_pair->first;
+        ReusableClosure<PLoadReplicaStatusResult>* result =
+                (ReusableClosure<PLoadReplicaStatusResult>*)rpc_pair->second;
+        EXPECT_EQ(print_id(_load_id), print_id(request->load_id()));
+        EXPECT_EQ(_txn_id, request->txn_id());
+        EXPECT_EQ(_index_id, request->index_id());
+        EXPECT_EQ(1, request->node_id());
+        EXPECT_EQ(0, request->sink_id());
+        if (num_get_replica_status < 2) {
+            EXPECT_EQ(3 - num_get_replica_status, request->tablet_ids().size());
+        }
+        num_get_replica_status += 1;
+    });
+
+    auto t = std::thread([&]() {
+        PTabletWriterAddChunkRequest add_chunk_request;
+        add_chunk_request.mutable_id()->CopyFrom(_load_id);
+        add_chunk_request.set_index_id(_index_id);
+        add_chunk_request.set_sender_id(0);
+        add_chunk_request.set_txn_id(_txn_id);
+        add_chunk_request.set_sink_id(0);
+        add_chunk_request.set_eos(true);
+        add_chunk_request.set_packet_seq(0);
+        add_chunk_request.set_wait_all_sender_close(true);
+        add_chunk_request.set_timeout_ms(3600000);
+
+        bool close_channel;
+        PTabletWriterAddBatchResult add_chunk_response;
+        _tablets_channel->add_chunk(nullptr, add_chunk_request, &add_chunk_response, &close_channel);
+        EXPECT_TRUE(close_channel);
+    });
+
+    std::vector<AsyncDeltaWriter*> writers = _tablets_channel->TEST_async_delta_writers();
+    ASSERT_EQ(3, writers.size());
+    for (auto& writer : writers) {
+        writer->cancel(Status::Cancelled("artificial failure"));
+    }
+
+    t.join();
 }
 
 } // namespace starrocks
