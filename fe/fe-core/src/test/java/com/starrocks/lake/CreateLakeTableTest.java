145abcf0f848f4a487aca71a86a5d2865614c4c2
shuming.li <ming.moriarty@gmail.com>
2025-05-19 18:02:02 +0800

    fix conflicts
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>

--- fe/fe-core/src/test/java/com/starrocks/lake/CreateLakeTableTest.java
+++ fe/fe-core/src/test/java/com/starrocks/lake/CreateLakeTableTest.java
@@ -437,23 +437,6 @@ public class CreateLakeTableTest {
         Assert.assertTrue(nationProps.containsKey(PropertyAnalyzer.PROPERTIES_UNIQUE_CONSTRAINT));
         Assert.assertTrue(nationProps.containsKey(PropertyAnalyzer.PROPERTIES_FOREIGN_KEY_CONSTRAINT));
     }
-<<<<<<< HEAD
-=======
-
-    @Test
-    public void testCreateTableWithPartitionAggregation() throws Exception {
-        ExceptionChecker.expectThrowsNoException(() -> createTable(
-                "create table lake_test.dup_test_enable_partition_agg (key1 int, key2 varchar(10))\n" +
-                        "distributed by hash(key1) buckets 3\n" +
-                        "properties('replication_num' = '1', 'enable_partition_aggregation' = 'true');"));
-        checkLakeTable("lake_test", "dup_test_enable_partition_agg");
-
-        String sql = "show create table lake_test.dup_test_enable_partition_agg";
-        ShowCreateTableStmt showCreateTableStmt =
-                (ShowCreateTableStmt) UtFrameUtils.parseStmtWithNewParser(sql, connectContext);
-        ShowResultSet resultSet = ShowExecutor.execute(showCreateTableStmt, connectContext);
-        Assert.assertFalse(resultSet.getResultRows().isEmpty());
-    }
 
     @Test
     public void testRangeTableWithRetentionCondition2() throws Exception {
@@ -509,5 +492,4 @@ public class CreateLakeTableTest {
                 ");";
         Assert.assertTrue(result.get(0).get(1).equals(expect));
     }
->>>>>>> bf2afac4d4 ([BugFix] Avoid to reactive the mv if it's inactived by specific reasons (#59051))
 }
