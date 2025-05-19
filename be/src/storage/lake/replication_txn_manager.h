4d7dfe765c4669e8b4cd0e333326958d39e9a0f3
sunny.xl <wxl24life@gmail.com>
2025-05-19 18:31:58 +0800

    shared-data migration part-2
    Signed-off-by: drake_wang <wxl250059@alibaba-inc.com>

--- be/src/storage/lake/replication_txn_manager.h
+++ be/src/storage/lake/replication_txn_manager.h
@@ -18,20 +18,34 @@
 #include "fs/encryption.h"
 #include "gen_cpp/AgentService_types.h"
 #include "gutil/macros.h"
+#include "storage/lake/remote_starlet_location_provider.h"
 #include "storage/lake/tablet_metadata.h"
 #include "storage/lake/txn_log.h"
 #include "storage/lake/types_fwd.h"
 #include "storage/olap_common.h"
 #include "storage/olap_define.h"
+#include "storage/replication_utils.h"
 #include "storage/rowset/rowset_meta.h"
+
+namespace starrocks {
+class FileSystem;
+} // namespace starrocks
+
+using starrocks::FileConverterCreatorFunc;
 
 namespace starrocks::lake {
 
 class TabletManager;
 
+struct RowsetMetaComparator {
+    bool operator()(const RowsetMetadataPB& rs_a, const RowsetMetadataPB& rs_b) const { return rs_a.id() < rs_b.id(); }
+};
+
 class ReplicationTxnManager {
 public:
-    explicit ReplicationTxnManager(lake::TabletManager* tablet_manager) : _tablet_manager(tablet_manager) {}
+    explicit ReplicationTxnManager(lake::TabletManager* tablet_manager)
+            : _tablet_manager(tablet_manager),
+              _remote_location_provider(std::make_unique<lake::RemoteStarletLocationProvider>()) {}
 
     Status remote_snapshot(const TRemoteSnapshotRequest& request, TSnapshotInfo* src_snapshot_info);
 
@@ -49,6 +63,28 @@ private:
     Status replicate_remote_snapshot(const TReplicateSnapshotRequest& request, const TSnapshotInfo& src_snapshot_info,
                                      const TabletMetadataPtr& tablet_metadata);
 
+    Status replicate_lake_remote_storage(const TReplicateSnapshotRequest& request);
+
+    StatusOr<TabletMetadataPtr> build_source_tablet_meta(int64_t src_tablet_id, int64_t version,
+                                                         const std::string& meta_dir,
+                                                         std::shared_ptr<FileSystem> shared_src_fs);
+
+    Status collect_all_rowsets_for_replication(bool incremental, int64_t data_version, int64_t src_visible_version,
+                                               int64_t src_tablet_id, int64_t target_tablet_id,
+                                               const std::string& src_meta_dir,
+                                               std::shared_ptr<FileSystem> shared_src_fs,
+                                               std::set<RowsetMetadataPB, RowsetMetaComparator>* new_rowsets);
+
+    // incrementally build mutable rowset metadata for txn log,
+    Status convert_lake_replicate_rowset_meta(const RowsetMetadataPB& src_rowset_meta, TxnLogPB::OpWrite* op_write);
+
+    FileConverterCreatorFunc build_file_converters(
+            const TReplicateSnapshotRequest& request,
+            const std::unordered_map<std::string, std::pair<std::string, FileEncryptionInfo>>& filename_map,
+            std::unordered_map<uint32_t, uint32_t>& column_unique_id_map,
+            std::vector<std::string>& files_to_delete) const;
+
+private:
     static Status convert_rowset_meta(
             const RowsetMeta& rowset_meta, TTransactionId transaction_id, TxnLogPB::OpWrite* op_write,
             std::unordered_map<std::string, std::pair<std::string, FileEncryptionInfo>>* segment_filename_map);
@@ -57,6 +93,8 @@ private:
 
 private:
     lake::TabletManager* _tablet_manager;
+    std::unique_ptr<lake::RemoteStarletLocationProvider> _remote_location_provider;
+    std::unordered_map<int64_t, std::shared_ptr<FileSystem>> _faked_starlet_fs_map;
 };
 
 } // namespace starrocks::lake
