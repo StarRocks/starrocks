8ce1abd6dface61aeb04ce0ed5192eb7404c43be
PengFei Li <lpengfei2016@gmail.com>
2025-05-20 10:01:52 +0800

    [BugFix] Primary replica should cancel secondary replica if rpc failed (#58854)
    Signed-off-by: PengFei Li <lpengfei2016@gmail.com>

--- be/test/runtime/local_tablets_channel_test.cpp
+++ be/test/runtime/local_tablets_channel_test.cpp
@@ -44,6 +44,7 @@ protected:
         srand(GetCurrentTimeMicros());
 
         _node_id = 100;
+        _secondary_node_id = 101;
         _load_id.set_hi(456789);
         _load_id.set_lo(987654);
         _txn_id = 10000;
@@ -61,10 +62,11 @@ protected:
         auto load_mem_tracker = std::make_unique<MemTracker>(-1, "", _mem_tracker.get());
         _load_channel = std::make_shared<LoadChannel>(_load_channel_mgr.get(), nullptr, _load_id, _txn_id, string(),
                                                       1000, std::move(load_mem_tracker));
-        _open_request = create_open_request();
+        _open_single_replica_request = create_open_request(true);
+        _open_primary_request = create_open_request(false);
         TabletsChannelKey key{_load_id, 0, _index_id};
         _schema_param.reset(new OlapTableSchemaParam());
-        ASSERT_OK(_schema_param->init(_open_request.schema()));
+        ASSERT_OK(_schema_param->init(_open_single_replica_request.schema()));
         _tablets_channel =
                 new_local_tablets_channel(_load_channel.get(), key, _load_channel->mem_tracker(), _root_profile.get());
     }
@@ -78,6 +80,8 @@ protected:
             ASSERT_OK(st);
         }
     }
+
+    void test_cancel_secondary_replica_base(bool is_empty_tablet);
 
     TabletSharedPtr create_tablet(int64_t tablet_id, int32_t schema_hash) {
         TCreateTabletReq request;
@@ -105,7 +109,7 @@ protected:
         return StorageEngine::instance()->tablet_manager()->get_tablet(tablet_id, false);
     }
 
-    PTabletWriterOpenRequest create_open_request() {
+    PTabletWriterOpenRequest create_open_request(bool single_replica) {
         PTabletWriterOpenRequest request;
         request.mutable_id()->CopyFrom(_load_id);
         request.set_index_id(_index_id);
@@ -113,7 +117,7 @@ protected:
         request.set_is_lake_tablet(false);
         request.set_is_replicated_storage(true);
         request.set_node_id(_node_id);
-        request.set_write_quorum(WriteQuorumTypePB::MAJORITY);
+        request.set_write_quorum(WriteQuorumTypePB::ONE);
         request.set_miss_auto_increment_column(false);
         request.set_table_id(_table_id);
         request.set_is_incremental(false);
@@ -132,6 +136,12 @@ protected:
         replica->set_host("127.0.0.1");
         replica->set_port(8060);
         replica->set_node_id(_node_id);
+        if (!single_replica) {
+            auto secondary_replica = tablet->add_replicas();
+            secondary_replica->set_host("127.0.0.2");
+            secondary_replica->set_port(8060);
+            secondary_replica->set_node_id(_secondary_node_id);
+        }
 
         auto schema = request.mutable_schema();
         schema->set_db_id(_db_id);
@@ -177,6 +187,7 @@ protected:
     }
 
     int64_t _node_id;
+    int64_t _secondary_node_id;
     PUniqueId _load_id;
     int64_t _txn_id;
     int64_t _db_id;
@@ -194,12 +205,13 @@ protected:
     std::shared_ptr<Schema> _schema;
     std::shared_ptr<OlapTableSchemaParam> _schema_param;
 
-    PTabletWriterOpenRequest _open_request;
+    PTabletWriterOpenRequest _open_single_replica_request;
+    PTabletWriterOpenRequest _open_primary_request;
     PTabletWriterOpenResult _open_response;
 };
 
 TEST_F(LocalTabletsChannelTest, test_profile) {
-    auto open_request = _open_request;
+    auto open_request = _open_single_replica_request;
     ASSERT_OK(_tablets_channel->open(open_request, &_open_response, _schema_param, false));
 
     PTabletWriterAddChunkRequest add_chunk_request;
@@ -240,6 +252,106 @@ TEST_F(LocalTabletsChannelTest, test_profile) {
     auto* primary_replicas_profile = profile->get_child("PrimaryReplicas");
     ASSERT_NE(nullptr, primary_replicas_profile);
     ASSERT_EQ(1, primary_replicas_profile->get_counter("TabletsNum")->value());
+}
+
+using RpcTabletWriterCancelTuple =
+        std::tuple<PTabletWriterCancelRequest*, google::protobuf::Closure*, brpc::Controller*>;
+
+void LocalTabletsChannelTest::test_cancel_secondary_replica_base(bool is_empty_tablet) {
+    ASSERT_OK(_tablets_channel->open(_open_primary_request, &_open_response, _schema_param, false));
+
+    PTabletWriterAddChunkRequest add_chunk_request;
+    add_chunk_request.mutable_id()->CopyFrom(_load_id);
+    add_chunk_request.set_index_id(_index_id);
+    add_chunk_request.set_sender_id(0);
+    add_chunk_request.set_txn_id(_txn_id);
+    add_chunk_request.set_eos(true);
+    add_chunk_request.set_packet_seq(0);
+    add_chunk_request.set_wait_all_sender_close(true);
+
+    auto chunk = generate_data(1);
+    ASSIGN_OR_ABORT(auto chunk_pb, serde::ProtobufChunkSerde::serialize(chunk));
+    if (!is_empty_tablet) {
+        add_chunk_request.mutable_chunk()->Swap(&chunk_pb);
+        add_chunk_request.add_tablet_ids(_tablet_id);
+        add_chunk_request.add_partition_ids(_partition_id);
+    }
+
+    DeferOp defer([&]() {
+        SyncPoint::GetInstance()->ClearCallBack("LocalTabletsChannel::rpc::tablet_writer_cancel");
+        SyncPoint::GetInstance()->DisableProcessing();
+    });
+
+    int num_cancel = 0;
+    SyncPoint::GetInstance()->EnableProcessing();
+    SyncPoint::GetInstance()->SetCallBack("LocalTabletsChannel::rpc::tablet_writer_cancel", [&](void* arg) {
+        RpcTabletWriterCancelTuple* rpc_tuple = (RpcTabletWriterCancelTuple*)arg;
+        PTabletWriterCancelRequest* request = std::get<0>(*rpc_tuple);
+        EXPECT_EQ(print_id(request->id()), print_id(_load_id));
+        EXPECT_EQ(request->index_id(), _index_id);
+        EXPECT_EQ(request->sender_id(), 0);
+        EXPECT_EQ(request->txn_id(), _txn_id);
+        EXPECT_EQ(1, request->tablet_ids().size());
+        EXPECT_EQ(_tablet_id, request->tablet_ids().Get(0));
+        EXPECT_EQ(request->reason(), is_empty_tablet ? "" : "primary replica failed to sync data");
+        google::protobuf::Closure* closure = std::get<1>(*rpc_tuple);
+        closure->Run();
+        num_cancel += 1;
+    });
+
+    bool close_channel;
+    PTabletWriterAddBatchResult add_chunk_response;
+    _tablets_channel->add_chunk(is_empty_tablet ? nullptr : &chunk, add_chunk_request, &add_chunk_response,
+                                &close_channel);
+    ASSERT_TRUE(add_chunk_response.status().status_code() == TStatusCode::OK)
+            << add_chunk_response.status().error_msgs(0);
+    ASSERT_TRUE(close_channel);
+    ASSERT_EQ(1, num_cancel);
+}
+
+TEST_F(LocalTabletsChannelTest, test_cancel_empty_secondary_replica) {
+    test_cancel_secondary_replica_base(true);
+}
+
+TEST_F(LocalTabletsChannelTest, test_cancel_failed_secondary_replica) {
+    test_cancel_secondary_replica_base(false);
+}
+
+TEST_F(LocalTabletsChannelTest, test_cancel_secondary_replica_rpc_fail) {
+    ASSERT_OK(_tablets_channel->open(_open_primary_request, &_open_response, _schema_param, false));
+
+    PTabletWriterAddChunkRequest add_chunk_request;
+    add_chunk_request.mutable_id()->CopyFrom(_load_id);
+    add_chunk_request.set_index_id(_index_id);
+    add_chunk_request.set_sender_id(0);
+    add_chunk_request.set_txn_id(_txn_id);
+    add_chunk_request.set_eos(true);
+    add_chunk_request.set_packet_seq(0);
+    add_chunk_request.set_wait_all_sender_close(true);
+
+    DeferOp defer([&]() {
+        SyncPoint::GetInstance()->ClearCallBack("LocalTabletsChannel::rpc::tablet_writer_cancel");
+        SyncPoint::GetInstance()->DisableProcessing();
+    });
+
+    int num_cancel = 0;
+    SyncPoint::GetInstance()->EnableProcessing();
+    SyncPoint::GetInstance()->SetCallBack("LocalTabletsChannel::rpc::tablet_writer_cancel", [&](void* arg) {
+        RpcTabletWriterCancelTuple* rpc_tuple = (RpcTabletWriterCancelTuple*)arg;
+        google::protobuf::Closure* closure = std::get<1>(*rpc_tuple);
+        brpc::Controller* cntl = std::get<2>(*rpc_tuple);
+        cntl->SetFailed("artificial intelligent rpc failure");
+        closure->Run();
+        num_cancel += 1;
+    });
+
+    bool close_channel;
+    PTabletWriterAddBatchResult add_chunk_response;
+    _tablets_channel->add_chunk(nullptr, add_chunk_request, &add_chunk_response, &close_channel);
+    ASSERT_TRUE(add_chunk_response.status().status_code() == TStatusCode::OK)
+            << add_chunk_response.status().error_msgs(0);
+    ASSERT_TRUE(close_channel);
+    ASSERT_EQ(1, num_cancel);
 }
 
 } // namespace starrocks
