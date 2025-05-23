093b83fd2a7804a6152a8835b1390f215b4b6134
Craig <117254390+click9000@users.noreply.github.com>
2025-05-22 23:58:57 -0700

    Update CustomLdapSslSocketFactoryTest.java
    Signed-off-by: Craig <117254390+click9000@users.noreply.github.com>

--- fe/fe-core/src/test/java/com/starrocks/mysql/security/CustomLdapSslSocketFactoryTest.java
+++ fe/fe-core/src/test/java/com/starrocks/mysql/security/CustomLdapSslSocketFactoryTest.java
@@ -17,16 +17,16 @@
 
 package com.starrocks.mysql.security;
 
-import javax.net.SocketFactory;
-
 import java.io.FileOutputStream;
 import java.security.KeyStore;
 
+import javax.net.SocketFactory;
+
 import static org.junit.AfterClass;
-import static org.junit.BeforeClass;
-import static org.junit.Test;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
+import static org.junit.BeforeClass;
+import static org.junit.Test;
 
 public class CustomLdapSslSocketFactoryTest {
     static String trustStorePath = "test-truststore.jks";
