7841fe5ebc5a8ebdf703abb26b045d7f06a26011
Craig <117254390+click9000@users.noreply.github.com>
2025-05-22 20:46:58 -0700

    Update LdapSecurityTest.java
    Signed-off-by: Craig <117254390+click9000@users.noreply.github.com>

--- fe/fe-core/src/test/java/com/starrocks/mysql/security/LdapSecurityTest.java
+++ fe/fe-core/src/test/java/com/starrocks/mysql/security/LdapSecurityTest.java
@@ -21,6 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 import com.starrocks.common.Config;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
