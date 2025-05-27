e76ae7c93ef229f5544d3878335b42fe8ae40d4b
shuming.li <ming.moriarty@gmail.com>
2025-05-27 19:13:36 +0800

    [BugFix] Fix possible duplicate finishInstance calls (backport #58753)
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/scheduler/dag/FragmentInstanceExecState.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/scheduler/dag/FragmentInstanceExecState.java
@@ -74,7 +74,7 @@ import java.util.stream.Collectors;
 public class FragmentInstanceExecState {
     private static final Logger LOG = LogManager.getLogger(FragmentInstanceExecState.class);
 
-    private State state = State.CREATED;
+    private volatile State state = State.CREATED;
 
     private final JobSpec jobSpec;
     private final PlanFragmentId fragmentId;
@@ -470,6 +470,10 @@ public class FragmentInstanceExecState {
         }
     }
 
+    public State getState() {
+        return state;
+    }
+
     public enum State {
         CREATED,
         DEPLOYING,
@@ -485,6 +489,10 @@ public class FragmentInstanceExecState {
 
         public boolean isTerminal() {
             return this == FINISHED || this == FAILED;
+        }
+
+        public boolean isFinished() {
+            return this == FINISHED;
         }
     }
 }
