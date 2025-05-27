5651c1ac7628967bcbdc3a6c86998790b04244ac
Zac-saodiseng <3253345336@qq.com>
2025-05-27 09:51:44 +0800

    [BugFix]Fix table_type
    Signed-off-by: Zac-saodiseng <3253345336@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/ExternalCatalogTableBasicInfo.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/ExternalCatalogTableBasicInfo.java
@@ -52,7 +52,7 @@ public class ExternalCatalogTableBasicInfo implements BasicTable {
 
     @Override
     public String getMysqlType() {
-        return Table.getTableTypeForMysql();
+        return "BASE TABLE";
     }
     @Override
     public String getEngine() {
