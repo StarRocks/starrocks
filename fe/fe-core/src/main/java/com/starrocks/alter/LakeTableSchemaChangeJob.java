323137fa9b0ae2a8a3c1a627d6b32ca70612bc2c
luohaha <18810541851@163.com>
2025-05-28 11:01:06 +0800

    [Enhancement] Enable aggregate publish for alter job
    Signed-off-by: luohaha <18810541851@163.com>

--- fe/fe-core/src/main/java/com/starrocks/alter/LakeTableSchemaChangeJob.java
+++ fe/fe-core/src/main/java/com/starrocks/alter/LakeTableSchemaChangeJob.java
@@ -162,6 +162,7 @@ public class LakeTableSchemaChangeJob extends LakeTableSchemaChangeJobBase {
     private MarkedCountDownLatch<Long, Long> createReplicaLatch = null;
     private AtomicBoolean waitingCreatingReplica = new AtomicBoolean(false);
     private AtomicBoolean isCancelling = new AtomicBoolean(false);
+    private boolean enablePartitionAggregation = false;
 
     // for deserialization
     public LakeTableSchemaChangeJob() {
@@ -767,6 +768,7 @@ public class LakeTableSchemaChangeJob extends LakeTableSchemaChangeJobBase {
     boolean readyToPublishVersion() throws AlterCancelException {
         try (ReadLockedDatabase db = getReadLockedDatabase(dbId)) {
             OlapTable table = getTableOrThrow(db, tableId);
+            enablePartitionAggregation = table.enablePartitionAggregation();
             for (long partitionId : physicalPartitionIndexMap.rowKeySet()) {
                 PhysicalPartition partition = table.getPhysicalPartition(partitionId);
                 Preconditions.checkState(partition != null, partitionId);
@@ -790,11 +792,21 @@ public class LakeTableSchemaChangeJob extends LakeTableSchemaChangeJobBase {
             txnInfo.txnType = TxnTypePB.TXN_NORMAL;
             txnInfo.commitTime = finishedTimeMs / 1000;
             txnInfo.gtid = watershedGtid;
+            List<Tablet> tablets = new ArrayList<>();
             for (long partitionId : physicalPartitionIndexMap.rowKeySet()) {
                 long commitVersion = commitVersionMap.get(partitionId);
+                tablets.clear();
                 Map<Long, MaterializedIndex> shadowIndexMap = physicalPartitionIndexMap.row(partitionId);
                 for (MaterializedIndex shadowIndex : shadowIndexMap.values()) {
-                    Utils.publishVersion(shadowIndex.getTablets(), txnInfo, 1, commitVersion, warehouseId);
+                    if (!enablePartitionAggregation) {
+                        Utils.publishVersion(shadowIndex.getTablets(), txnInfo, 1, commitVersion, warehouseId);
+                    } else {
+                        tablets.addAll(shadowIndex.getTablets());
+                    }
+                }
+                if (enablePartitionAggregation) {
+                    Utils.aggregatePublishVersion(tablets, Lists.newArrayList(txnInfo), 1, commitVersion,
+                            null, null, warehouseId, null);
                 }
             }
             return true;
