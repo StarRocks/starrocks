fd84215ae60b45680b097406e10ae842aa2d34dd
andyziye <108652123+andyziye@users.noreply.github.com>
2025-06-03 16:01:17 +0800

    [BugFix] Fix follower forward transaction statement fail bug (#36971)
    Co-authored-by: Harbor Liu <460660596@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/ConnectProcessor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/ConnectProcessor.java
@@ -386,7 +386,7 @@ public class ConnectProcessor {
                     }
                 }
 
-                if (ctx.getExplicitTxnState() != null &&
+                if (ctx.getTxnId() != 0 &&
                         !((parsedStmt instanceof InsertStmt && !((InsertStmt) parsedStmt).isOverwrite()) ||
                                 parsedStmt instanceof BeginStmt ||
                                 parsedStmt instanceof CommitStmt ||
@@ -858,6 +858,14 @@ public class ConnectProcessor {
             ctx.setForwardTimes(request.getForward_times());
         }
 
+        if (request.isSetConnectionId()) {
+            ctx.setConnectionId(request.getConnectionId());
+        }
+
+        if (request.isSetTxn_id()) {
+            ctx.setTxnId(request.getTxn_id());
+        }
+
         ctx.setThreadLocalInfo();
 
         if (ctx.getCurrentUserIdentity() == null) {
@@ -934,6 +942,9 @@ public class ConnectProcessor {
         result.setPacket(getResultPacket());
         result.setState(ctx.getState().getStateType().toString());
         result.setErrorMsg(ctx.getState().getErrorMessage());
+        //Put the txnId in connectContext into result and pass it back to the follower node
+        result.setTxn_id(ctx.getTxnId());
+
         if (executor != null) {
             if (executor.getProxyResultSet() != null) {  // show statement
                 result.setResultSet(executor.getProxyResultSet().tothrift());
