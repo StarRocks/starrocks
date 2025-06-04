35f0e8d8c7f153578b5ff565eb07a25ca7e7941d
Kevin Cai <caixiaohua@starrocks.com>
2025-06-04 17:28:34 +0800

    [BugFix] defensive coding against empty partition (#59553)
    Signed-off-by: Kevin Cai <kevin.cai@celerdata.com>
    (cherry picked from commit 76fb97f83e4f281fef02c9f39d0d305ef7f69fe8)
    
    # Conflicts:
    #	be/src/exec/tablet_info.cpp

--- be/src/exec/tablet_info.cpp
+++ be/src/exec/tablet_info.cpp
@@ -559,6 +559,7 @@ Status OlapTablePartitionParam::remove_partitions(const std::vector<int64_t>& pa
 
         _partitions.erase(it);
     }
+    LOG_IF(INFO, _partitions.empty()) << "Empty partitions for db:" << db_id() << ", table_id:" << table_id();
 
     return Status::OK();
 }
@@ -713,8 +714,19 @@ Status OlapTablePartitionParam::find_tablets(Chunk* chunk, std::vector<OlapTable
         auto& part_ids = _partitions_map.begin()->second;
         for (size_t i = 0; i < num_rows; ++i) {
             if ((*selection)[i]) {
+<<<<<<< HEAD
                 (*partitions)[i] = _partitions[part_ids[(*indexes)[i] % _partitions.size()]];
                 (*indexes)[i] = (*indexes)[i] % (*partitions)[i]->num_buckets;
+=======
+                if (_partitions.empty()) {
+                    // Don't know the reason yet, just defensive coding not crashing the process and possibly for further investigation
+                    LOG(WARNING) << "empty partition for selection[i=" << i << "]=" << (*selection)[i]
+                                 << ", db=" << db_id() << ", table_id=" << table_id();
+                    return Status::InternalError(
+                            fmt::format("empty partitions for db={}, table={}", db_id(), table_id()));
+                }
+                (*partitions)[i] = _partitions[part_ids[(*hashes)[i] % _partitions.size()]];
+>>>>>>> 76fb97f83e ([BugFix] defensive coding against empty partition (#59553))
             }
         }
     }
