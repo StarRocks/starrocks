d818cbddc600bd68f812304d947cdd2d37c85932
gengjun-git <gengjun@starrocks.com>
2025-06-04 11:30:16 +0800

    add
    Signed-off-by: gengjun-git <gengjun@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/authentication/SimpleLDAPSecurityIntegration.java
+++ fe/fe-core/src/main/java/com/starrocks/authentication/SimpleLDAPSecurityIntegration.java
@@ -21,6 +21,12 @@ import java.util.Map;
 public class SimpleLDAPSecurityIntegration extends SecurityIntegration {
     public static final String AUTHENTICATION_LDAP_SIMPLE_SERVER_HOST = "authentication_ldap_simple_server_host";
     public static final String AUTHENTICATION_LDAP_SIMPLE_SERVER_PORT = "authentication_ldap_simple_server_port";
+    public static final String AUTHENTICATION_LDAP_SIMPLE_SSL_CONN_ALLOW_INSECURE
+            = "authentication_ldap_simple_ssl_conn_allow_insecure";
+    public static final String AUTHENTICATION_LDAP_SIMPLE_SSL_CONN_TRUST_STORE_PATH
+            = "authentication_ldap_simple_ssl_conn_trust_store_path";
+    public static final String AUTHENTICATION_LDAP_SIMPLE_SSL_CONN_TRUST_STORE_PWD
+            = "authentication_ldap_simple_ssl_conn_trust_store_pwd";
     public static final String AUTHENTICATION_LDAP_SIMPLE_BIND_ROOT_DN = "authentication_ldap_simple_bind_root_dn";
     public static final String AUTHENTICATION_LDAP_SIMPLE_BIND_ROOT_PWD = "authentication_ldap_simple_bind_root_pwd";
     public static final String AUTHENTICATION_LDAP_SIMPLE_BIND_BASE_DN = "authentication_ldap_simple_bind_base_dn";
@@ -34,10 +40,16 @@ public class SimpleLDAPSecurityIntegration extends SecurityIntegration {
     public AuthenticationProvider getAuthenticationProvider() {
         String ldapServerHost = propertyMap.getOrDefault(AUTHENTICATION_LDAP_SIMPLE_SERVER_HOST,
                 Config.authentication_ldap_simple_server_host);
-
         int ldapServerPort = propertyMap.containsKey(AUTHENTICATION_LDAP_SIMPLE_SERVER_PORT) ?
                 Integer.parseInt(propertyMap.get(AUTHENTICATION_LDAP_SIMPLE_SERVER_PORT)) :
                 Config.authentication_ldap_simple_server_port;
+        boolean useSSL = !(propertyMap.containsKey(AUTHENTICATION_LDAP_SIMPLE_SSL_CONN_ALLOW_INSECURE) ?
+                Boolean.parseBoolean(propertyMap.get(AUTHENTICATION_LDAP_SIMPLE_SSL_CONN_ALLOW_INSECURE)) :
+                Config.authentication_ldap_simple_ssl_conn_allow_insecure);
+        String trustStorePath = propertyMap.getOrDefault(AUTHENTICATION_LDAP_SIMPLE_SSL_CONN_TRUST_STORE_PATH,
+                Config.authentication_ldap_simple_ssl_conn_trust_store_path);
+        String trustStorePwd = propertyMap.getOrDefault(AUTHENTICATION_LDAP_SIMPLE_SSL_CONN_TRUST_STORE_PWD,
+                Config.authentication_ldap_simple_ssl_conn_trust_store_pwd);
         String ldapBindRootDn = propertyMap.getOrDefault(AUTHENTICATION_LDAP_SIMPLE_BIND_ROOT_DN,
                 Config.authentication_ldap_simple_bind_root_dn);
         String ldapBindRootPwd = propertyMap.getOrDefault(AUTHENTICATION_LDAP_SIMPLE_BIND_ROOT_PWD,
@@ -47,7 +59,15 @@ public class SimpleLDAPSecurityIntegration extends SecurityIntegration {
         String ldapUserSearchAttr = propertyMap.getOrDefault(AUTHENTICATION_LDAP_SIMPLE_USER_SEARCH_ATTR,
                 Config.authentication_ldap_simple_user_search_attr);
 
-        return new LDAPAuthProviderForNative(ldapServerHost, ldapServerPort, ldapBindRootDn, ldapBindRootPwd, ldapBindBaseDn,
-                ldapUserSearchAttr, null);
+        return new LDAPAuthProvider(ldapServerHost,
+                ldapServerPort,
+                useSSL,
+                trustStorePath,
+                trustStorePwd,
+                ldapBindRootDn,
+                ldapBindRootPwd,
+                ldapBindBaseDn,
+                ldapUserSearchAttr,
+                null);
     }
 }
