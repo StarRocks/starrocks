91007737833ed23236c0941a38d75cd7b3c370d3
zhangqiang <qiangzh95@gmail.com>
2025-06-05 19:11:44 +0800

    [BugFix] Revert  Reduce the size of data written to edit log when schema changes in shared-data clusters with fast schema evolution enabled. (#55282) (#59640)
    Signed-off-by: sevev <qiangzh95@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/alter/LakeTableAlterMetaJobBase.java
+++ fe/fe-core/src/main/java/com/starrocks/alter/LakeTableAlterMetaJobBase.java
@@ -112,7 +112,7 @@ public abstract class LakeTableAlterMetaJobBase extends AlterJobV2 {
             this.watershedTxnId = globalStateMgr.getGlobalTransactionMgr().getTransactionIDGenerator()
                     .getNextTransactionId();
             this.watershedGtid = globalStateMgr.getGtidGenerator().nextGtid();
-            GlobalStateMgr.getCurrentState().getEditLog().logAlterJob(this.getShadowCopy());
+            GlobalStateMgr.getCurrentState().getEditLog().logAlterJob(this);
         }
 
         try {
@@ -172,7 +172,7 @@ public abstract class LakeTableAlterMetaJobBase extends AlterJobV2 {
             this.jobState = JobState.FINISHED_REWRITING;
             this.finishedTimeMs = System.currentTimeMillis();
 
-            GlobalStateMgr.getCurrentState().getEditLog().logAlterJob(this.getShadowCopy());
+            GlobalStateMgr.getCurrentState().getEditLog().logAlterJob(this);
 
             // NOTE: !!! below this point, this update meta job must success unless the database or table been dropped. !!!
             updateNextVersion(table);
@@ -215,7 +215,7 @@ public abstract class LakeTableAlterMetaJobBase extends AlterJobV2 {
             updateCatalog(db, table);
             this.jobState = JobState.FINISHED;
             this.finishedTimeMs = System.currentTimeMillis();
-            GlobalStateMgr.getCurrentState().getEditLog().logAlterJob(this.getShadowCopy());
+            GlobalStateMgr.getCurrentState().getEditLog().logAlterJob(this);
             // set visible version
             updateVisibleVersion(table);
             table.setState(OlapTable.OlapTableState.NORMAL);
@@ -443,9 +443,6 @@ public abstract class LakeTableAlterMetaJobBase extends AlterJobV2 {
         return watershedTxnId;
     }
 
-    // Only for reducing data writing after the first log, so we don't do deep copy
-    protected abstract LakeTableAlterMetaJobBase getShadowCopy();
-
     @Override
     protected boolean cancelImpl(String errMsg) {
         if (jobState == JobState.CANCELLED || jobState == JobState.FINISHED) {
@@ -542,25 +539,6 @@ public abstract class LakeTableAlterMetaJobBase extends AlterJobV2 {
         } finally {
             locker.unLockTablesWithIntensiveDbLock(db.getId(), Lists.newArrayList(table.getId()), LockType.WRITE);
         }
-    }
-
-    protected void copyOnlyForNonFirstLog(LakeTableAlterMetaJobBase copied) {
-        copied.watershedTxnId = this.watershedTxnId;
-        copied.watershedGtid = this.watershedGtid;
-        copied.physicalPartitionIndexMap = this.physicalPartitionIndexMap;
-        copied.commitVersionMap = this.commitVersionMap;
-
-        copied.type = this.type;
-        copied.jobId = this.jobId;
-        copied.jobState = this.jobState;
-        copied.dbId = this.dbId;
-        copied.tableId = this.tableId;
-        copied.tableName = this.tableName;
-        copied.errMsg = this.errMsg;
-        copied.createTimeMs = this.createTimeMs;
-        copied.finishedTimeMs = this.finishedTimeMs;
-        copied.timeoutMs = this.timeoutMs;
-        copied.warehouseId = this.warehouseId;
     }
 
     // for test
