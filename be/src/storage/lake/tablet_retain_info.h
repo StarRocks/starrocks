803393ef61fc7e66d20d836b061dbf30b586972b
srlch <linzichao@starrocks.com>
2025-06-06 10:22:14 +0800

    fix
    Signed-off-by: srlch <linzichao@starrocks.com>

--- be/src/storage/lake/tablet_retain_info.h
+++ be/src/storage/lake/tablet_retain_info.h
@@ -18,8 +18,6 @@
 #include <unordered_set>
 #include <vector>
 
-#include "storage/lake/tablet_metadata.h"
-
 namespace starrocks {
 class Status;
 }
@@ -33,45 +31,13 @@ public:
     TabletRetainInfo() = default;
     ~TabletRetainInfo() = default;
 
-    Status build_info(const std::vector<int64_t>& retain_versions, int64_t tablet_id, TabletManager* tablet_mgr) {
-        for (const auto& version : retain_versions) {
-            auto res = tablet_mgr->get_tablet_metadata(tablet_id, version, false);
-            if (!res.status().ok()) {
-                return res.status();
-            }
-            auto metadata = std::move(res).value();
-            for (const auto& rowset : metadata->rowsets()) {
-                _rowset_ids.insert(rowset.id());
-                for (const auto& segment : rowset.segments()) {
-                    _files.insert(segment);
-                }
-                for (const auto& del_file : rowset.del_files()) {
-                    _files.insert(del_file.name());
-                }
-            }
+    Status build_info(const std::vector<int64_t>& retain_versions, int64_t tablet_id, TabletManager* tablet_mgr);
 
-            for (const auto& [_, dcg] : (*metadata).dcg_meta().dcgs()) {
-                for (const auto& column_file : dcg.column_files()) {
-                    _files.insert(column_file);
-                }
-            }
-            for (const auto& [_, file] : (*metadata).delvec_meta().version_to_file()) {
-                _files.insert(file.name());
-            }
-            for (const auto& sstable : (*metadata).sstable_meta().sstables()) {
-                _files.insert(sstable.filename());
-            }
+    bool filter_by_file_name(const std::string& file_name) const;
 
-            _versions.insert(version);
-        }
-        return Status::OK();
-    }
+    bool filter_by_version(int64_t version) const;
 
-    bool filter_by_file_name(const std::string& file_name) const { return _files.find(file_name) != _files.end(); }
-
-    bool filter_by_version(int64_t version) const { return _versions.find(version) != _versions.end(); }
-
-    bool filter_by_rowset_id(uint32_t rowset_id) const { return _rowset_ids.find(rowset_id) != _rowset_ids.end(); }
+    bool filter_by_rowset_id(uint32_t rowset_id) const;
 
 private:
     std::unordered_set<int64_t> _versions;
