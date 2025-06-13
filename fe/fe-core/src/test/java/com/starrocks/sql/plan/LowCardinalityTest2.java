c2c07d8b4bebdee18eec7ee111c45f63d4dd7ef5
stdpain <34912776+stdpain@users.noreply.github.com>
2025-06-13 16:54:53 +0800

    [BugFix] Fix short circuit not work when table has global dict columns (#59844)
    Signed-off-by: stdpain <drfeng08@gmail.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/LowCardinalityTest2.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/LowCardinalityTest2.java
@@ -2237,4 +2237,12 @@ public class LowCardinalityTest2 extends PlanTestBase {
         Assert.assertFalse("table doesn't contain global dict, we can change its distribution",
                 execPlan.getOptExpression(1).isExistRequiredDistribution());
     }
+
+    @Test
+    public void testShortCircuitQuery() throws Exception {
+        connectContext.getSessionVariable().setEnableShortCircuit(true);
+        String sql = "select * from low_card_t2 where d_date='20160404' and c_mr = '12'";
+        final String plan = getFragmentPlan(sql);
+        assertContains(plan, "Short Circuit Scan: true");
+    }
 }
