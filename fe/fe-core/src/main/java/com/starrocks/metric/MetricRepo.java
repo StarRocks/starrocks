ea83cb7fd9e38dc4c3e83efca0539817ebaa2599
wyb <wybb86@gmail.com>
2025-06-17 16:19:08 +0800

    [BugFix] Fix starrocks_fe_routine_load_max_lag_of_partition metric in multi-warehouse case (#59924)
    Signed-off-by: wyb <wybb86@gmail.com>
    (cherry picked from commit 58ba5cce5c04d785b29d6382d370a56b44887097)

--- fe/fe-core/src/main/java/com/starrocks/metric/MetricRepo.java
+++ fe/fe-core/src/main/java/com/starrocks/metric/MetricRepo.java
@@ -768,6 +768,8 @@ public final class MetricRepo {
                 Collectors.groupingBy(RoutineLoadJob::getWarehouseId)
         );
 
+        List<GaugeMetricImpl<Long>> routineLoadLags = new ArrayList<>();
+
         // get all partitions offset in a batch api
         for (Map.Entry<Long, List<RoutineLoadJob>> entry : kafkaJobsMp.entrySet()) {
             long warehouseId = entry.getKey();
@@ -798,8 +800,6 @@ public final class MetricRepo {
                 LOG.warn("get batch offsets failed", e);
                 return;
             }
-
-            List<GaugeMetricImpl<Long>> routineLoadLags = new ArrayList<>();
 
             for (int i = 0; i < kafkaJobs.size(); i++) {
                 KafkaRoutineLoadJob kJob = (KafkaRoutineLoadJob) kafkaJobs.get(i);
@@ -833,9 +833,9 @@ public final class MetricRepo {
                     routineLoadLags.add(metric);
                 }
             }
-
-            GAUGE_ROUTINE_LOAD_LAGS = routineLoadLags;
         }
+
+        GAUGE_ROUTINE_LOAD_LAGS = routineLoadLags;
     }
 
     public static synchronized String getMetric(MetricVisitor visitor, MetricsAction.RequestParams requestParams) {
