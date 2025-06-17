0f56e126a57d4bd7da02a7eeb16dbb9e6737f36a
zombee0 <ewang2027@gmail.com>
2025-06-17 17:49:46 +0800

    [BugFix] Revert Catch cancel status from be to retry (#59642) (#59988)

--- be/src/exec/pipeline/query_context.h
+++ be/src/exec/pipeline/query_context.h
@@ -80,7 +80,7 @@ public:
     // now time point pass by deadline point.
     bool is_delivery_expired() const {
         auto now = duration_cast<milliseconds>(steady_clock::now().time_since_epoch()).count();
-        return now > _delivery_deadline || _cancelled_by_fe;
+        return now > _delivery_deadline || _is_cancelled;
     }
     bool is_query_expired() const {
         auto now = duration_cast<milliseconds>(steady_clock::now().time_since_epoch()).count();
@@ -89,15 +89,7 @@ public:
 
     bool is_cancelled() const { return _is_cancelled; }
 
-    Status get_cancelled_status() const {
-        auto* status = _cancelled_status.load();
-        return status == nullptr ? Status::Cancelled("Query has been cancelled") : *status;
-    }
-
-    bool is_dead() const {
-        return _num_active_fragments == 0 && (_num_fragments == _total_fragments || _cancelled_by_fe);
-    }
-
+    bool is_dead() const { return _num_active_fragments == 0 && (_num_fragments == _total_fragments || _is_cancelled); }
     // add expired seconds to deadline
     void extend_delivery_lifetime() {
         _delivery_deadline =
@@ -154,8 +146,7 @@ public:
 
     FragmentContextManager* fragment_mgr();
 
-    void cancel(const Status& status, bool cancelled_by_fe);
-    void set_cancelled_by_fe() { _cancelled_by_fe = true; }
+    void cancel(const Status& status);
 
     void set_is_runtime_filter_coordinator(bool flag) { _is_runtime_filter_coordinator = flag; }
 
@@ -274,10 +265,6 @@ private:
     std::atomic_bool _is_prepared = false;
     std::atomic_bool _is_cancelled = false;
 
-    std::atomic_bool _cancelled_by_fe = false;
-    std::atomic<Status*> _cancelled_status = nullptr;
-    Status _s_status;
-
     std::once_flag _init_query_once;
     int64_t _query_begin_time = 0;
     std::once_flag _init_spill_manager_once;
@@ -330,7 +317,7 @@ public:
     QueryContextManager(size_t log2_num_slots);
     ~QueryContextManager();
     Status init();
-    StatusOr<QueryContext*> get_or_register(const TUniqueId& query_id, bool return_error_if_not_exist = false);
+    QueryContext* get_or_register(const TUniqueId& query_id);
     QueryContextPtr get(const TUniqueId& query_id, bool need_prepared = false);
     size_t size();
     bool remove(const TUniqueId& query_id);
