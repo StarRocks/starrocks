32d509e59ef439fb2697e19ed51335852070cc2a
Deng Liu <d.liu@celonis.com>
2025-06-23 23:15:52 -0700

    [Enhancement] Add detailed exit code and restart decision logging to BE entrypoint
    Signed-off-by: Deng Liu <d.liu@celonis.com>

--- docker/dockerfiles/be/be_entrypoint.sh
+++ docker/dockerfiles/be/be_entrypoint.sh
@@ -129,6 +129,30 @@ fi
 while true; do
   $STARROCKS_HOME/bin/start_be.sh $addition_args
   ret=$?
+
+  # Enhanced logging for exit status analysis
+  log_stderr "start_be.sh exited with return code: $ret"
+
+  # Decode the exit status to provide more meaningful information
+  if [[ $ret -eq 0 ]]; then
+    log_stderr "Exit reason: Normal termination (exit code 0)"
+  elif [[ $ret -ge 128 && $ret -le 255 ]]; then
+    # Exit codes 128+n indicate termination by signal n
+    signal_num=$((ret - 128))
+    case $signal_num in
+      1)  log_stderr "Exit reason: Terminated by SIGHUP (signal 1) - Hangup" ;;
+      2)  log_stderr "Exit reason: Terminated by SIGINT (signal 2) - Interrupt" ;;
+      3)  log_stderr "Exit reason: Terminated by SIGQUIT (signal 3) - Quit" ;;
+      6)  log_stderr "Exit reason: Terminated by SIGABRT (signal 6) - Abort/Assert failure" ;;
+      9)  log_stderr "Exit reason: Terminated by SIGKILL (signal 9) - Kill" ;;
+      11) log_stderr "Exit reason: Terminated by SIGSEGV (signal 11) - Segmentation fault" ;;
+      15) log_stderr "Exit reason: Terminated by SIGTERM (signal 15) - Termination" ;;
+      *)  log_stderr "Exit reason: Terminated by signal $signal_num (exit code $ret)" ;;
+    esac
+  else
+    log_stderr "Exit reason: Process exited with code $ret (non-signal termination)"
+  fi
+
   if [[ $ret -ne 0 && "x$LOG_CONSOLE" != "x1" ]] ; then
       nol=50
       log_stderr "Last $nol lines of be.INFO ..."
@@ -142,16 +166,42 @@ while true; do
   #  b. coredump is enabled, and the error code is SIGABRT(6) or SIGSEGV(11);
   # otherwise BE process should still exit.
   should_exit=true
+  restart_reason=""
+
+  # Check DEBUG_MODE condition
   if [[ "$DEBUG_MODE" == "true" ]]; then
     should_exit=false
+    restart_reason="DEBUG_MODE is enabled"
+    log_stderr "Restart decision: should_exit=false (reason: $restart_reason)"
+  else
+    log_stderr "Restart decision: DEBUG_MODE is not enabled (DEBUG_MODE=$DEBUG_MODE)"
   fi
 
+  # Check COREDUMP_ENABLED condition for crash signals
   if [[ "$COREDUMP_ENABLED" == "true" && ($ret -eq 134 || $ret -eq 139) ]]; then
     should_exit=false
+    if [[ $ret -eq 134 ]]; then
+      restart_reason="COREDUMP_ENABLED and process crashed with SIGABRT (exit code 134)"
+    elif [[ $ret -eq 139 ]]; then
+      restart_reason="COREDUMP_ENABLED and process crashed with SIGSEGV (exit code 139)"
+    fi
+    log_stderr "Restart decision: should_exit=false (reason: $restart_reason)"
+  else
+    if [[ "$COREDUMP_ENABLED" == "true" ]]; then
+      log_stderr "Restart decision: COREDUMP_ENABLED is true but exit code $ret is not a crash signal (134=SIGABRT, 139=SIGSEGV)"
+    else
+      log_stderr "Restart decision: COREDUMP_ENABLED is not enabled (COREDUMP_ENABLED=$COREDUMP_ENABLED)"
+    fi
   fi
 
+  # Final decision logging
   if [[ "$should_exit" == "true" ]]; then
-    exit  $ret
+    log_stderr "Final decision: should_exit=true - BE process will NOT be restarted"
+    log_stderr "Exiting with return code: $ret"
+    exit $ret
+  else
+    log_stderr "Final decision: should_exit=false - BE process WILL be restarted"
+    log_stderr "Restart reason: $restart_reason"
   fi
 
   # Print a message indicating the failure
