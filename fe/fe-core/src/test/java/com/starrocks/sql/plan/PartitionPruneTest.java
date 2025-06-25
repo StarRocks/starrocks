3938868615b35f0749da71ecf312ceaffde8d616
stephen <stephen5217@163.com>
2025-06-25 14:06:51 +0800

    fix compile issue
    Signed-off-by: stephen <stephen5217@163.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/PartitionPruneTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/PartitionPruneTest.java
@@ -607,7 +607,7 @@ public class PartitionPruneTest extends PlanTestBase {
         starRocksAssert.getCtx().executeSql("insert into t4_range_minmax values('2025-04-29', 1, 'bar')");
         FeConstants.runningUnitTest = false;
         starRocksAssert.getTable("test", "t4_range_minmax")
-                .getPartition("p20250428").getDefaultPhysicalPartition().updateVisibleVersion(1);
+                .getPartition("p20250428").updateVisibleVersion(1);
         starRocksAssert.query("select min(dt), max(dt) from t4_range_minmax").explainContains("partitions=1/2");
         FeConstants.runningUnitTest = true;
 
