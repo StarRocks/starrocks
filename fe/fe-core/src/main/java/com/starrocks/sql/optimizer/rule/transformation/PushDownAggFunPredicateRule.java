0897aff05e4609c485e140fbe309320c8fc024f5
stdpain <drfeng08@gmail.com>
2025-06-27 00:20:59 +0800

    fix license

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/PushDownAggFunPredicateRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/PushDownAggFunPredicateRule.java
@@ -1,5 +1,18 @@
-package com.starrocks.sql.optimizer.rule.transformation;
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
+package com.starrocks.sql.optimizer.rule.transformation;
 
 import com.google.common.collect.Lists;
 import com.starrocks.analysis.BinaryType;
@@ -82,7 +95,8 @@ public class PushDownAggFunPredicateRule extends TransformationRule {
         if (aggFun.getFnName().equalsIgnoreCase("min") && matchedPredicate.getBinaryType().equals(BinaryType.EQ)) {
             resultPredicate = new BinaryPredicateOperator(BinaryType.LE, aggFun.getChildren().get(0),
                     matchedPredicate.getChild(1));
-        } else if (aggFun.getFnName().equalsIgnoreCase("max") && matchedPredicate.getBinaryType().equals(BinaryType.EQ)) {
+        } else if (aggFun.getFnName().equalsIgnoreCase("max") &&
+                matchedPredicate.getBinaryType().equals(BinaryType.EQ)) {
             resultPredicate = new BinaryPredicateOperator(BinaryType.GE, aggFun.getChildren().get(0),
                     matchedPredicate.getChild(1));
         } else {
