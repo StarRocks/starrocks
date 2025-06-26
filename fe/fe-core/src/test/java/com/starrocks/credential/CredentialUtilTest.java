a68422cf32cd8820aaf7e0275ebead543306ce0c
Youngwb <yangwenbo_mailbox@163.com>
2025-06-26 14:15:14 +0800

    fix ut

--- fe/fe-core/src/test/java/com/starrocks/credential/CredentialUtilTest.java
+++ fe/fe-core/src/test/java/com/starrocks/credential/CredentialUtilTest.java
@@ -16,7 +16,6 @@ package com.starrocks.credential;
 
 import com.starrocks.catalog.JDBCResource;
 import com.starrocks.connector.iceberg.IcebergCatalogProperties;
-import com.starrocks.connector.iceberg.rest.IcebergRESTCatalog;
 import com.starrocks.connector.iceberg.rest.OAuth2SecurityConfig;
 import com.starrocks.connector.share.credential.CloudConfigurationConstants;
 import com.starrocks.credential.azure.AzureCloudConfigurationProvider;
