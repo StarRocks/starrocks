ad7810e92fc2c18842ca63f692ea50412b96d951
edwinhzhang <edwinhzhang@tencent.com>
2025-06-29 23:41:04 +0800

    update class name
    Signed-off-by: edwinhzhang <edwinhzhang@tencent.com>

--- fe/fe-core/src/test/java/com/starrocks/http/TransactionLoadActionTest.java
+++ fe/fe-core/src/test/java/com/starrocks/http/TransactionLoadActionTest.java
@@ -568,7 +568,7 @@ public class TransactionLoadActionTest extends StarRocksHttpTestCase {
     @Test
     public void prepareTransactionWithoutChannelInfoTest() throws Exception {
         String label = RandomStringUtils.randomAlphanumeric(32);
-        setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TxnNodeCache() {
+        setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TransactionLoadLabelCache() {
             private static final long serialVersionUID = -4276328107866085321L;
 
             {
@@ -778,7 +778,7 @@ public class TransactionLoadActionTest extends StarRocksHttpTestCase {
                 }
             };
 
-            setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TxnNodeCache() {
+            setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TransactionLoadLabelCache() {
                 private static final long serialVersionUID = 5890524883711716645L;
 
                 {
@@ -808,7 +808,7 @@ public class TransactionLoadActionTest extends StarRocksHttpTestCase {
                 }
             };
 
-            setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TxnNodeCache() {
+            setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TransactionLoadLabelCache() {
                 private static final long serialVersionUID = -4276328107866085321L;
 
                 {
@@ -838,7 +838,7 @@ public class TransactionLoadActionTest extends StarRocksHttpTestCase {
                 }
             };
 
-            setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TxnNodeCache() {
+            setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TransactionLoadLabelCache() {
                 private static final long serialVersionUID = 8612091611347668755L;
 
                 {
@@ -868,7 +868,7 @@ public class TransactionLoadActionTest extends StarRocksHttpTestCase {
                 }
             };
 
-            setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TxnNodeCache() {
+            setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TransactionLoadLabelCache() {
                 private static final long serialVersionUID = 3214813746415023231L;
 
                 {
@@ -902,7 +902,7 @@ public class TransactionLoadActionTest extends StarRocksHttpTestCase {
                 }
             };
 
-            setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TxnNodeCache() {
+            setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TransactionLoadLabelCache() {
                 private static final long serialVersionUID = 6893430743492341004L;
 
                 {
@@ -936,7 +936,7 @@ public class TransactionLoadActionTest extends StarRocksHttpTestCase {
                 }
             };
 
-            setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TxnNodeCache() {
+            setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TransactionLoadLabelCache() {
                 private static final long serialVersionUID = 8165080593735535441L;
 
                 {
@@ -1245,7 +1245,7 @@ public class TransactionLoadActionTest extends StarRocksHttpTestCase {
                 }
             };
 
-            setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TxnNodeCache() {
+            setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TransactionLoadLabelCache() {
                 private static final long serialVersionUID = 5890524883711716645L;
 
                 {
@@ -1275,7 +1275,7 @@ public class TransactionLoadActionTest extends StarRocksHttpTestCase {
                 }
             };
 
-            setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TxnNodeCache() {
+            setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TransactionLoadLabelCache() {
                 private static final long serialVersionUID = -4276328107866085321L;
 
                 {
@@ -1305,7 +1305,7 @@ public class TransactionLoadActionTest extends StarRocksHttpTestCase {
                 }
             };
 
-            setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TxnNodeCache() {
+            setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TransactionLoadLabelCache() {
                 private static final long serialVersionUID = -5731416357248595041L;
 
                 {
@@ -1335,7 +1335,7 @@ public class TransactionLoadActionTest extends StarRocksHttpTestCase {
                 }
             };
 
-            setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TxnNodeCache() {
+            setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TransactionLoadLabelCache() {
                 private static final long serialVersionUID = -6655156575562250213L;
 
                 {
@@ -1369,7 +1369,7 @@ public class TransactionLoadActionTest extends StarRocksHttpTestCase {
                 }
             };
 
-            setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TxnNodeCache() {
+            setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TransactionLoadLabelCache() {
                 private static final long serialVersionUID = -891006164191904128L;
 
                 {
@@ -1402,7 +1402,7 @@ public class TransactionLoadActionTest extends StarRocksHttpTestCase {
                 }
             };
 
-            setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TxnNodeCache() {
+            setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TransactionLoadLabelCache() {
                 private static final long serialVersionUID = 4824168412840558066L;
 
                 {
@@ -1626,7 +1626,7 @@ public class TransactionLoadActionTest extends StarRocksHttpTestCase {
     @Test
     public void loadTransactionWithoutChannelInfoTest() throws Exception {
         String label = RandomStringUtils.randomAlphanumeric(32);
-        setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TxnNodeCache() {
+        setField(TransactionLoadAction.getAction(), "txnNodeMap", new TransactionLoadAction.TransactionLoadLabelCache() {
             private static final long serialVersionUID = -4276328107866085321L;
 
             {
