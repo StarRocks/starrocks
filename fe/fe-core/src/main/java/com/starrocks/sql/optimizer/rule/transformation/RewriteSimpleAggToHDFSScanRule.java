a6be4832da17b17639f5a5cd38a482cdf16dab48
yan zhang <dirtysalt1987@gmail.com>
2025-06-29 12:29:14 +0800

    remove unused code
    Signed-off-by: yan zhang <dirtysalt1987@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/RewriteSimpleAggToHDFSScanRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/RewriteSimpleAggToHDFSScanRule.java
@@ -22,7 +22,6 @@ import com.starrocks.catalog.AggregateFunction;
 import com.starrocks.catalog.Column;
 import com.starrocks.catalog.Function;
 import com.starrocks.catalog.FunctionSet;
-import com.starrocks.catalog.IcebergTable;
 import com.starrocks.catalog.Type;
 import com.starrocks.common.AnalysisException;
 import com.starrocks.sql.optimizer.OptExpression;
@@ -231,13 +230,6 @@ public class RewriteSimpleAggToHDFSScanRule extends TransformationRule {
         // no predicate on agg operator
         if (aggregationOperator.getPredicate() != null) {
             return false;
-        }
-
-        if (scanOperator.getOpType() == OperatorType.LOGICAL_ICEBERG_SCAN) {
-            IcebergTable icebergTable = (IcebergTable) scanOperator.getTable();
-            if (!icebergTable.isUnPartitioned() && !icebergTable.isAllPartitionColumnsAlwaysIdentity()) {
-                return false;
-            }
         }
 
         if (aggregationOperator.getAggregations().isEmpty()) {
