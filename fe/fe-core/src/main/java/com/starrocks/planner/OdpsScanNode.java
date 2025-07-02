d2cfa9b00b17ecfbc94c4a3ce4433823327d019e
yan zhang <dirtysalt1987@gmail.com>
2025-07-02 09:57:20 +0800

    [Enhancement] min/max optimization can be applied when group partition column (#60484)
    Signed-off-by: yan zhang <dirtysalt1987@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/planner/OdpsScanNode.java
+++ fe/fe-core/src/main/java/com/starrocks/planner/OdpsScanNode.java
@@ -172,7 +172,6 @@ public class OdpsScanNode extends ScanNode {
         msg.node_type = TPlanNodeType.HDFS_SCAN_NODE;
         THdfsScanNode tHdfsScanNode = new THdfsScanNode();
         tHdfsScanNode.setTuple_id(desc.getId().asInt());
-        tHdfsScanNode.setCan_use_min_max_opt(false);
 
         String explainString = getExplainString(conjuncts);
         LOG.info("Explain string: " + explainString);
