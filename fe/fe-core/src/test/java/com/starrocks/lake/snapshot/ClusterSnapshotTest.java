bb7200fb0978c6998e5b0611f6160c7718afcaa8
srlch <111035020+srlch@users.noreply.github.com>
2025-07-02 15:45:27 +0800

    [Refactor] refact the code of cluster snapshot job reset when FE restart (#60469) (#37994)
    Signed-off-by: srlch <linzichao@starrocks.com>

--- fe/fe-core/src/test/java/com/starrocks/lake/snapshot/ClusterSnapshotTest.java
+++ fe/fe-core/src/test/java/com/starrocks/lake/snapshot/ClusterSnapshotTest.java
@@ -415,7 +415,7 @@ public class ClusterSnapshotTest {
         job1.setState(ClusterSnapshotJobState.FINISHED);
         ClusterSnapshotJob job2 = localClusterSnapshotMgr.createAutomatedSnapshotJob();
         RestoredSnapshotInfo restoredSnapshotInfo = new RestoredSnapshotInfo(job2.getSnapshotName(), 666L, 6666L);
-        localClusterSnapshotMgr.setLastJobFinishedAfterRestored(restoredSnapshotInfo);
+        localClusterSnapshotMgr.setJobFinishedIfRestoredFromIt(restoredSnapshotInfo);
 
         Assert.assertTrue(job2.getFeJournalId() == 666L);
         Assert.assertTrue(job2.getStarMgrJournalId() == 6666L);
