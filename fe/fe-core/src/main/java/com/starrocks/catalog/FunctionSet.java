7003ad22dd41e62a856cc497766dd4e709786cac
Mesut Döner <81925277+Mesut-Doner@users.noreply.github.com>
2025-07-04 11:40:12 +0300

    [BugFix] split_part function should not return null when delimiter is not matched (#56967)
    Signed-off-by: stdpain <34912776+stdpain@users.noreply.github.com>
    Signed-off-by: stdpain <drfeng08@gmail.com>
    Co-authored-by: stdpain <34912776+stdpain@users.noreply.github.com>
    Co-authored-by: stdpain <drfeng08@gmail.com>
    (cherry picked from commit c1e47b2d3debc07cf2b5ac22f7088d097a712a8f)

--- fe/fe-core/src/main/java/com/starrocks/catalog/FunctionSet.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/FunctionSet.java
@@ -642,6 +642,7 @@ public class FunctionSet {
                     .add(FunctionSet.EXCHANGE_BYTES)
                     .add(FunctionSet.EXCHANGE_SPEED)
                     .add(FunctionSet.FIELD)
+                    .add(FunctionSet.SPLIT_PART)
                     .build();
 
     public static final Set<String> DECIMAL_ROUND_FUNCTIONS =
