b83afbe2477d094132f78a2eb5ddba4da4d8446c
wyb <wybb86@gmail.com>
2025-07-04 10:40:57 +0800

    [Enhancement] Support retry in getting kafka partition info (#60513)
    Signed-off-by: wyb <wybb86@gmail.com>
    (cherry picked from commit 49cbc4696e73fccb5b0166443e0c5bde7529a981)
    
    # Conflicts:
    #	fe/fe-core/src/test/java/com/starrocks/common/util/KafkaUtilTest.java

--- fe/fe-core/src/test/java/com/starrocks/common/util/KafkaUtilTest.java
+++ fe/fe-core/src/test/java/com/starrocks/common/util/KafkaUtilTest.java
@@ -17,6 +17,12 @@ package com.starrocks.common.util;
 import com.google.common.collect.Lists;
 import com.starrocks.common.LoadException;
 import com.starrocks.common.StarRocksException;
+<<<<<<< HEAD
+=======
+import com.starrocks.proto.PKafkaMetaProxyResult;
+import com.starrocks.proto.PKafkaOffsetBatchProxyResult;
+import com.starrocks.proto.PKafkaOffsetProxyResult;
+>>>>>>> 49cbc4696e ([Enhancement] Support retry in getting kafka partition info (#60513))
 import com.starrocks.proto.PProxyRequest;
 import com.starrocks.proto.PProxyResult;
 import com.starrocks.proto.StatusPB;
@@ -29,7 +35,11 @@ import com.starrocks.system.Backend;
 import com.starrocks.system.SystemInfoService;
 import com.starrocks.thrift.TNetworkAddress;
 import com.starrocks.utframe.MockedWarehouseManager;
+<<<<<<< HEAD
 import com.starrocks.utframe.UtFrameUtils;
+=======
+import com.starrocks.warehouse.cngroup.ComputeResource;
+>>>>>>> 49cbc4696e ([Enhancement] Support retry in getting kafka partition info (#60513))
 import mockit.Expectations;
 import mockit.Mock;
 import mockit.MockUp;
@@ -51,6 +61,8 @@ public class KafkaUtilTest {
     SystemInfoService service;
     @Mocked
     BackendServiceClient client;
+    @Mocked
+    WarehouseManager warehouseManager;
 
     @BeforeEach
     public void before() throws StarRocksException {
@@ -61,15 +73,28 @@ public class KafkaUtilTest {
             }
         };
 
+        new MockUp<GlobalStateMgr>() {
+            @Mock
+            public WarehouseManager getWarehouseMgr() {
+                return warehouseManager;
+            }
+        };
+
         new Expectations() {
             {
                 BackendServiceClient.getInstance();
                 minTimes = 0;
                 result = client;
+                warehouseManager.getAllComputeNodeIds((ComputeResource) any);
+                minTimes = 0;
+                result = Lists.newArrayList(1L, 2L);
             }
         };
+<<<<<<< HEAD
 
         UtFrameUtils.mockInitWarehouseEnv();
+=======
+>>>>>>> 49cbc4696e ([Enhancement] Support retry in getting kafka partition info (#60513))
     }
 
     @Test
@@ -154,45 +179,12 @@ public class KafkaUtilTest {
         backend.setBeRpcPort(8060);
         backend.setAlive(true);
 
-        PProxyResult proxyResult = new PProxyResult();
-        StatusPB status = new StatusPB();
-        // cancelled
-        status.statusCode = 1;
-        status.errorMsgs = Lists.newArrayList("be process failed");
-        proxyResult.status = status;
-
         new Expectations() {
             {
                 service.getBackendOrComputeNode(anyLong);
                 result = backend;
                 client.getInfo((TNetworkAddress) any, (PProxyRequest) any);
-                result = new Future<PProxyResult>() {
-                    @Override
-                    public boolean cancel(boolean mayInterruptIfRunning) {
-                        return false;
-                    }
-
-                    @Override
-                    public boolean isCancelled() {
-                        return false;
-                    }
-
-                    @Override
-                    public boolean isDone() {
-                        return true;
-                    }
-
-                    @Override
-                    public PProxyResult get() throws InterruptedException, ExecutionException {
-                        return proxyResult;
-                    }
-
-                    @Override
-                    public PProxyResult get(long timeout, @NotNull TimeUnit unit)
-                            throws InterruptedException, ExecutionException, TimeoutException {
-                        return proxyResult;
-                    }
-                };
+                result = createFailProxyResultFuture();
             }
         };
 
@@ -216,5 +208,147 @@ public class KafkaUtilTest {
         LoadException e = Assertions.assertThrows(LoadException.class, () -> api.getBatchOffsets(null));
         Assertions.assertEquals("Failed to send get kafka partition info request. err: Warehouse id: 1 not exist.",
                 e.getMessage());
+    }
+
+    private Future<PProxyResult> createSuccessProxyResultFuture() {
+        PProxyResult proxyResult = new PProxyResult();
+        StatusPB status = new StatusPB();
+        // success
+        status.statusCode = 0;
+        status.errorMsgs = Lists.newArrayList();
+        proxyResult.status = status;
+
+        PKafkaOffsetProxyResult offsetResult = new PKafkaOffsetProxyResult();
+        offsetResult.partitionIds = Lists.newArrayList(0);
+        offsetResult.beginningOffsets = Lists.newArrayList(200L);
+        offsetResult.latestOffsets = Lists.newArrayList(300L);
+        PKafkaOffsetBatchProxyResult offsetBatchResult = new PKafkaOffsetBatchProxyResult();
+        offsetBatchResult.results = Lists.newArrayList(offsetResult);
+        proxyResult.kafkaOffsetBatchResult = offsetBatchResult;
+
+        return new Future<PProxyResult>() {
+            @Override
+            public boolean cancel(boolean mayInterruptIfRunning) {
+                return false;
+            }
+
+            @Override
+            public boolean isCancelled() {
+                return false;
+            }
+
+            @Override
+            public boolean isDone() {
+                return true;
+            }
+
+            @Override
+            public PProxyResult get() throws InterruptedException, ExecutionException {
+                return proxyResult;
+            }
+
+            @Override
+            public PProxyResult get(long timeout, @NotNull TimeUnit unit)
+                    throws InterruptedException, ExecutionException, TimeoutException {
+                return proxyResult;
+            }
+        };
+    }
+
+    private Future<PProxyResult> createFailProxyResultFuture() {
+        PProxyResult proxyResult = new PProxyResult();
+        StatusPB status = new StatusPB();
+        // cancelled
+        status.statusCode = 1;
+        status.errorMsgs = Lists.newArrayList("be process failed");
+        proxyResult.status = status;
+
+        return new Future<PProxyResult>() {
+            @Override
+            public boolean cancel(boolean mayInterruptIfRunning) {
+                return false;
+            }
+
+            @Override
+            public boolean isCancelled() {
+                return false;
+            }
+
+            @Override
+            public boolean isDone() {
+                return true;
+            }
+
+            @Override
+            public PProxyResult get() throws InterruptedException, ExecutionException {
+                return proxyResult;
+            }
+
+            @Override
+            public PProxyResult get(long timeout, @NotNull TimeUnit unit)
+                    throws InterruptedException, ExecutionException, TimeoutException {
+                return proxyResult;
+            }
+        };
+    }
+
+    @Test
+    public void testGetInfoSuccess() throws StarRocksException, RpcException {
+        Backend backend = new Backend(1L, "127.0.0.1", 9050);
+        backend.setBeRpcPort(8060);
+        backend.setAlive(true);
+
+        new Expectations() {
+            {
+                service.getBackendOrComputeNode(anyLong);
+                result = backend;
+                client.getInfo((TNetworkAddress) any, (PProxyRequest) any);
+                result = createSuccessProxyResultFuture();
+            }
+        };
+
+        KafkaUtil.ProxyAPI api = new KafkaUtil.ProxyAPI();
+        List<PKafkaOffsetProxyResult> results = api.getBatchOffsets(null);
+        Assertions.assertEquals(1, results.size());
+        PKafkaOffsetProxyResult result = results.get(0);
+        Assertions.assertEquals(1, result.partitionIds.size());
+        Assertions.assertEquals(0, result.partitionIds.get(0).intValue());
+        Assertions.assertEquals(1, result.beginningOffsets.size());
+        Assertions.assertEquals(200L, result.beginningOffsets.get(0).longValue());
+        Assertions.assertEquals(1, result.latestOffsets.size());
+        Assertions.assertEquals(300L, result.latestOffsets.get(0).longValue());
+    }
+
+    @Test
+    public void testGetInfoRetry() throws StarRocksException, RpcException {
+        Backend backend1 = new Backend(1L, "127.0.0.1", 9050);
+        backend1.setBeRpcPort(8060);
+        backend1.setAlive(true);
+
+        Backend backend2 = new Backend(2L, "127.0.0.2", 9050);
+        backend2.setBeRpcPort(8060);
+        backend2.setAlive(true);
+
+        new Expectations() {
+            {
+                service.getBackendOrComputeNode(1L);
+                result = backend1;
+                service.getBackendOrComputeNode(2L);
+                result = backend2;
+                client.getInfo((TNetworkAddress) any, (PProxyRequest) any);
+                returns(createFailProxyResultFuture(), createSuccessProxyResultFuture());
+            }
+        };
+
+        KafkaUtil.ProxyAPI api = new KafkaUtil.ProxyAPI();
+        List<PKafkaOffsetProxyResult> results = api.getBatchOffsets(null);
+        Assertions.assertEquals(1, results.size());
+        PKafkaOffsetProxyResult result = results.get(0);
+        Assertions.assertEquals(1, result.partitionIds.size());
+        Assertions.assertEquals(0, result.partitionIds.get(0).intValue());
+        Assertions.assertEquals(1, result.beginningOffsets.size());
+        Assertions.assertEquals(200L, result.beginningOffsets.get(0).longValue());
+        Assertions.assertEquals(1, result.latestOffsets.size());
+        Assertions.assertEquals(300L, result.latestOffsets.get(0).longValue());
     }
 }
