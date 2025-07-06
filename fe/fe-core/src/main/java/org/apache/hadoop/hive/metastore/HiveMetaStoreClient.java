e9112e30e0819c93087280ba971cdf25d8445d32
yan zhang <dirtysalt1987@gmail.com>
2025-07-06 14:48:35 +0800

    replace get table
    Signed-off-by: yan zhang <dirtysalt1987@gmail.com>

--- fe/fe-core/src/main/java/org/apache/hadoop/hive/metastore/HiveMetaStoreClient.java
+++ fe/fe-core/src/main/java/org/apache/hadoop/hive/metastore/HiveMetaStoreClient.java
@@ -871,6 +871,11 @@ public class HiveMetaStoreClient implements IMetaStoreClient, AutoCloseable {
     }
 
     @Override
+    public List<String> getAllDatabases() throws MetaException, TException {
+        return client.get_all_databases();
+    }
+
+    @Override
     public List<String> getAllDatabases(String catName) throws MetaException, TException {
         return client.get_all_databases();
     }
