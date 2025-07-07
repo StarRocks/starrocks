b404f1498053462fb59e09787c532d5ea68e305b
stdpain <34912776+stdpain@users.noreply.github.com>
2025-07-07 17:52:05 +0800

    [BugFix] Fix memory/row count inaccuracies that can cause aggregate stucked (#60612)
    Signed-off-by: stdpain <drfeng08@gmail.com>

--- be/src/exec/limited_pipeline_chunk_buffer.h
+++ be/src/exec/limited_pipeline_chunk_buffer.h
@@ -29,14 +29,21 @@ public:
     LimitedPipelineChunkBuffer(BufferMetrics* metrics, size_t max_dop, size_t max_memory_usage, size_t max_chunk_count)
             : _metrics(metrics), _buffer_mem_manager(max_dop, max_memory_usage, max_chunk_count) {}
 
-    bool is_full() const { return _buffer_mem_manager.is_full(); }
+    bool is_full() const {
+#ifndef NDEBUG
+        std::lock_guard l(_buffer_mutex);
+        DCHECK(!(_buffer_mem_manager.is_full() && _element_size == 0));
+#endif
+        return _buffer_mem_manager.is_full();
+    }
 
     void push(const ChunkPtr& chunk) {
-        size_t mem_usage = chunk->memory_usage();
-        size_t num_rows = chunk->num_rows();
+        ChunkWithInfo chunk_with_info(chunk);
+        size_t mem_usage = chunk_with_info.mem_usage;
+        size_t num_rows = chunk_with_info.num_rows;
         {
             std::lock_guard l(_buffer_mutex);
-            _buffer.push(chunk);
+            _buffer.push(std::move(chunk_with_info));
         }
         _element_size++;
         _buffer_mem_manager.update_memory_usage(mem_usage, num_rows);
@@ -45,26 +52,29 @@ public:
     }
 
     ChunkPtr pull() {
-        ChunkPtr chunk;
+        ChunkWithInfo chunk_with_info;
         {
             std::lock_guard l(_buffer_mutex);
             if (_buffer.empty()) {
                 return nullptr;
             }
-            chunk = _buffer.front();
+            chunk_with_info = std::move(_buffer.front());
             _buffer.pop();
         }
         _element_size--;
-        size_t mem_usage = chunk->memory_usage();
-        size_t num_rows = chunk->num_rows();
+        size_t mem_usage = chunk_with_info.mem_usage;
+        size_t num_rows = chunk_with_info.num_rows;
         _buffer_mem_manager.update_memory_usage(-mem_usage, -num_rows);
+
+        DCHECK_EQ(mem_usage, chunk_with_info.chunk->memory_usage());
+        DCHECK_EQ(num_rows, chunk_with_info.chunk->num_rows());
 
         COUNTER_ADD(_metrics->chunk_buffer_peak_memory, -mem_usage);
         COUNTER_ADD(_metrics->chunk_buffer_peak_size, -1);
-        return chunk;
+        return chunk_with_info.chunk;
     }
 
-    bool is_empty() { return _element_size == 0; }
+    bool is_empty() const { return _element_size == 0; }
 
     void clear() {
         std::lock_guard l(_buffer_mutex);
@@ -75,8 +85,21 @@ public:
 
 private:
     std::atomic_size_t _element_size{};
-    std::mutex _buffer_mutex;
-    std::queue<ChunkPtr> _buffer;
+    mutable std::mutex _buffer_mutex;
+
+    struct ChunkWithInfo {
+        ChunkWithInfo() = default;
+        ChunkWithInfo(ChunkPtr chunk_) : chunk(std::move(chunk_)) {
+            mem_usage = chunk->memory_usage();
+            num_rows = chunk->num_rows();
+        }
+
+        size_t mem_usage;
+        size_t num_rows;
+        ChunkPtr chunk;
+    };
+
+    std::queue<ChunkWithInfo> _buffer;
     BufferMetrics* _metrics;
     pipeline::ChunkBufferMemoryManager _buffer_mem_manager;
 };
