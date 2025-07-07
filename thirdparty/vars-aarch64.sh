a52ff4d1e6b066ab122f972d300ad3c92e9d2864
starrocks-xupeng <xupeng@starrocks.com>
2025-07-07 14:44:09 +0800

    [Enhancement] update staros to v3.5-rc2 (#60602)
    Signed-off-by: starrocks-xupeng <xupeng@starrocks.com>

--- thirdparty/vars-aarch64.sh
+++ thirdparty/vars-aarch64.sh
@@ -52,7 +52,7 @@ JINDOSDK_SOURCE="jindosdk-4.6.8-linux-el7-aarch64"
 JINDOSDK_MD5SUM="27a4e2cd9a403c6e21079a866287d88b"
 
 # starcache
-STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v3.5-rc1/starcache-centos7_arm64.tar.gz"
+STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v3.5-rc2/starcache-centos7_arm64.tar.gz"
 STARCACHE_NAME="starcache.tar.gz"
 STARCACHE_SOURCE="starcache"
-STARCACHE_MD5SUM="f4a5f7d83508d2e29bf2935a5fb1bddd"
+STARCACHE_MD5SUM="7057fed85866ea0b1db209a33411a996"
