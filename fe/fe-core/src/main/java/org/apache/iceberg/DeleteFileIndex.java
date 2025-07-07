39ce0473acffd3149f661920e2e7d628f92f767c
yan zhang <dirtysalt1987@gmail.com>
2025-07-07 09:50:22 +0800

    [Refactor] use atomic type to avoid potential race condition (#60617)
    Signed-off-by: yan zhang <dirtysalt1987@gmail.com>
    (cherry picked from commit ac0956429475bd26e051113b7ed28707a08cb4f4)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/catalog/PhysicalPartition.java

--- fe/fe-core/src/main/java/org/apache/iceberg/DeleteFileIndex.java
+++ fe/fe-core/src/main/java/org/apache/iceberg/DeleteFileIndex.java
@@ -54,7 +54,6 @@ import org.apache.iceberg.types.Conversions;
 import org.apache.iceberg.types.Type;
 import org.apache.iceberg.types.Types;
 import org.apache.iceberg.util.ArrayUtil;
-import org.apache.iceberg.util.CharSequenceMap;
 import org.apache.iceberg.util.ContentFileUtil;
 import org.apache.iceberg.util.PartitionMap;
 import org.apache.iceberg.util.PartitionSet;
@@ -591,7 +590,7 @@ class DeleteFileIndex {
         private DeleteFile[] files = null;
 
         // a buffer that is used to hold files before indexing
-        private volatile List<DeleteFile> buffer = Lists.newArrayList();
+        private List<DeleteFile> buffer = Collections.synchronizedList(Lists.newArrayList());
 
         public void add(DeleteFile file) {
             Preconditions.checkState(buffer != null, "Can't add files upon indexing");
@@ -667,7 +666,7 @@ class DeleteFileIndex {
         private EqualityDeleteFile[] files = null;
 
         // a buffer that is used to hold files before indexing
-        private volatile List<EqualityDeleteFile> buffer = Lists.newArrayList();
+        private List<EqualityDeleteFile> buffer = Collections.synchronizedList(Lists.newArrayList());
 
         public void add(PartitionSpec spec, DeleteFile file) {
             Preconditions.checkState(buffer != null, "Can't add files upon indexing");
