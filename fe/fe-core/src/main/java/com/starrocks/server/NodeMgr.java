5533b1886c7390cd3cb182a446a6782e04b6ccee
gengjun-git <gengjun@starrocks.com>
2025-07-08 09:42:19 +0800

    [BugFix] Fix find master NPE bug when starting FE from cluster snapshot (#60604)
    ## Why I'm doing:
    Set selfNode to alive, because the init of ReplicationGroupAdmin only collect the alive frontends node. The reason this bug doesn't always appear is that HeartBeatMgr also sets selfNode to alive, but this is asynchronous and depends on how fast HeartBeatMgr runs.
    
    ## What I'm doing:
    
    Fixes 
    ``` java
    [StarRocksFE.start():210] StarRocksFE start failed
    java.lang.NullPointerException: null
            at com.sleepycat.je.rep.util.ReplicationGroupAdmin.getMasterSocket(ReplicationGroupAdmin.java:191) ~[starrocks-bdb-je-18.3.20.jar:?]
            at com.sleepycat.je.rep.util.ReplicationGroupAdmin.doMessageExchange(ReplicationGroupAdmin.java:607) ~[starrocks-bdb-je-18.3.20.jar:?]
            at com.sleepycat.je.rep.util.ReplicationGroupAdmin.getGroup(ReplicationGroupAdmin.java:406) ~[starrocks-bdb-je-18.3.20.jar:?]
            at com.starrocks.ha.BDBHA.removeNodeIfExist(BDBHA.java:219) ~[starrocks-fe.jar:?]
            at com.starrocks.server.NodeMgr.addFrontend(NodeMgr.java:791) ~[starrocks-fe.jar:?]
            at com.starrocks.lake.snapshot.RestoreClusterSnapshotMgr.updateFrontends(RestoreClusterSnapshotMgr.java:207) ~[starrocks-fe.jar:?]
            at com.starrocks.lake.snapshot.RestoreClusterSnapshotMgr.finishRestoring(RestoreClusterSnapshotMgr.java:93) ~[starrocks-fe.jar:?]
            at com.starrocks.StarRocksFE.start(StarRocksFE.java:199) ~[starrocks-fe.jar:?]
            at com.starrocks.StarRocksFE.main(StarRocksFE.java:99) ~[starrocks-fe.jar:?]
    ```
    Signed-off-by: gengjun-git <gengjun@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/server/NodeMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/server/NodeMgr.java
@@ -1269,6 +1269,7 @@ public class NodeMgr {
 
         int fid = allocateNextFrontendId();
         Frontend self = new Frontend(fid, role, nodeName, selfNode.first, selfNode.second);
+        self.setAlive(true);
         frontends.put(self.getNodeName(), self);
         frontendIds.put(fid, self);
         // reset helper nodes
