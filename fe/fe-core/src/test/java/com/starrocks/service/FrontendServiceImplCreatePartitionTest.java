a20840da0fa40b70acd62af96ae69bd03304ef80
zhaohehuhu <luoyedeyi@163.com>
2025-07-10 21:09:44 +0800

    add UTs
    Signed-off-by: zhaohehuhu <luoyedeyi@163.com>

--- fe/fe-core/src/test/java/com/starrocks/service/FrontendServiceImplCreatePartitionTest.java
+++ fe/fe-core/src/test/java/com/starrocks/service/FrontendServiceImplCreatePartitionTest.java
@@ -55,11 +55,7 @@ public class FrontendServiceImplCreatePartitionTest {
     @BeforeAll
     public static void beforeClass() throws Exception {
         FeConstants.runningUnitTest = true;
-        Config.alter_scheduler_interval_millisecond = 100;
-        Config.dynamic_partition_enable = true;
-        Config.dynamic_partition_check_interval_seconds = 1;
         Config.enable_strict_storage_medium_check = false;
-        Config.stream_load_max_txn_num_per_be = 5;
         UtFrameUtils.createMinStarRocksCluster(RunMode.SHARED_DATA);
         // create connect context
         connectContext = UtFrameUtils.createDefaultCtx();
