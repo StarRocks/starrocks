1a43f641f9a1afae81c177648bc292acfff5c310
xiangguangyxg <xiangguangyxg@gmail.com>
2025-07-14 15:45:22 +0800

    [Feature] Implement split tablet job in FE for dynamic tablet splitting and merging
    Signed-off-by: xiangguangyxg <xiangguangyxg@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
@@ -2231,6 +2231,10 @@ public class GlobalStateMgr {
         return this.backupHandler;
     }
 
+    public PublishVersionDaemon getPublishVersionDaemon() {
+        return this.publishVersionDaemon;
+    }
+
     public DeleteMgr getDeleteMgr() {
         return deleteMgr;
     }
