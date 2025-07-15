bfdf1f485f64adc7e109af89560da4055cc9072e
wyb <wybb86@gmail.com>
2025-07-15 09:34:58 +0800

    [Enhancement] Support parquet version in files unload (#60843)
    Signed-off-by: wyb <wybb86@gmail.com>

--- gensrc/thrift/Descriptors.thrift
+++ gensrc/thrift/Descriptors.thrift
@@ -488,6 +488,7 @@ struct TTableFunctionTable {
     9: optional string csv_column_seperator
 
     10: optional bool parquet_use_legacy_encoding
+    11: optional Types.TParquetOptions parquet_options
 }
 
 struct TIcebergSchemaField {
