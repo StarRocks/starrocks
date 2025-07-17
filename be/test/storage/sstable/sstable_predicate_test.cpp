3ebdecf94d1cb93635ed27014ccafe4bbfcb3992
srlch <linzichao@starrocks.com>
2025-07-17 12:27:36 +0800

    fix
    Signed-off-by: srlch <linzichao@starrocks.com>

--- be/test/storage/sstable/sstable_predicate_test.cpp
+++ be/test/storage/sstable/sstable_predicate_test.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "storage/lake/sstable_predicate.h"
+#include "storage/sstable/sstable_predicate.h"
 
 #include <gtest/gtest.h>
 
