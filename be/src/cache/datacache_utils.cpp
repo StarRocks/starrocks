49d9722061bd3fe4eadd8ea5ea326e99ac7586e4
starrocks-xupeng <xupeng@starrocks.com>
2025-07-22 14:32:30 +0800

    [BugFix] fix cache might not be used when upgraded from 3.3 (#60973)
    Signed-off-by: starrocks-xupeng <xupeng@starrocks.com>

--- be/src/cache/datacache_utils.cpp
+++ be/src/cache/datacache_utils.cpp
@@ -19,6 +19,8 @@
 
 #include <filesystem>
 
+#include "absl/status/statusor.h"
+#include "absl/strings/str_split.h"
 #include "fs/fs.h"
 #include "gutil/strings/split.h"
 #include "util/parse_util.h"
@@ -180,5 +182,65 @@ dev_t DataCacheUtils::disk_device_id(const std::string& disk_path) {
     }
     return s.st_dev;
 }
+
+#ifdef USE_STAROS
+StatusOr<std::vector<std::string>> DataCacheUtils::get_corresponding_starlet_cache_dir(
+        const std::vector<StorePath>& store_paths, const std::string& starlet_cache_dir) {
+    std::vector<std::string> corresponding_starlet_dirs;
+    if (starlet_cache_dir.empty()) {
+        return corresponding_starlet_dirs;
+    }
+    absl::StatusOr<std::vector<std::string>> vec_or = absl::StrSplit(starlet_cache_dir, ':', absl::SkipWhitespace());
+    if (!vec_or.ok()) {
+        std::string error_str = "Fail to parse starlet_cache_dir, error: " + std::string(vec_or.status().message());
+        return Status::InternalError(error_str);
+    }
+    std::vector<std::string> starlet_paths = *vec_or;
+    if (starlet_paths.empty()) {
+        return corresponding_starlet_dirs;
+    }
+    std::unordered_map<dev_t, std::string> starlet_devices;
+    for (auto& starlet_path : starlet_paths) {
+        auto id = DataCacheUtils::disk_device_id(starlet_path);
+        if (id == 0) {
+            std::string error_str =
+                    "Fail to get device id for " + starlet_path + ", error: " + std::string(strerror(errno));
+            return Status::InternalError(error_str);
+        }
+        auto iter = starlet_devices.find(id);
+        if (iter == starlet_devices.end()) {
+            starlet_devices[id] = starlet_path;
+        } else {
+            std::string error_str = "Find 2 starlet cache dir on same device, " + starlet_path + ":" + iter->second;
+            return Status::InternalError(error_str);
+        }
+    }
+
+    for (auto& store_path : store_paths) {
+        std::string root_path = store_path.path;
+        auto id = DataCacheUtils::disk_device_id(root_path);
+        if (id == 0) {
+            std::string error_str =
+                    "Fail to get device id for " + root_path + ", error: " + std::string(strerror(errno));
+            return Status::InternalError(error_str);
+        }
+        auto iter = starlet_devices.find(id);
+        if (iter != starlet_devices.end()) {
+            corresponding_starlet_dirs.push_back(iter->second + "/star_cache");
+            starlet_devices.erase(id);
+        } else {
+            corresponding_starlet_dirs.push_back(root_path + "/starlet_cache/star_cache");
+        }
+    }
+    if (!starlet_devices.empty()) {
+        std::string error_str = "can not find corresponding storage path for starlet cache dir, ";
+        for (auto& e : starlet_devices) {
+            error_str += e.second + ":";
+        }
+        return Status::InternalError(error_str);
+    }
+    return corresponding_starlet_dirs;
+}
+#endif
 
 } // namespace starrocks
