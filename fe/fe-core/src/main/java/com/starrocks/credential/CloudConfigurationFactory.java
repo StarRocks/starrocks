60e85454b6d0bd34587b78d441b33fb7bbbef2cb
Youngwb <yangwenbo_mailbox@163.com>
2025-07-23 14:34:30 +0800

    [Enhancement] Support iceberg REST catalog vended credentials for Azure (#61137)

--- fe/fe-core/src/main/java/com/starrocks/credential/CloudConfigurationFactory.java
+++ fe/fe-core/src/main/java/com/starrocks/credential/CloudConfigurationFactory.java
@@ -15,6 +15,7 @@
 package com.starrocks.credential;
 
 import com.google.common.collect.ImmutableList;
+import com.starrocks.common.StarRocksException;
 import com.starrocks.connector.share.credential.CloudConfigurationConstants;
 import com.starrocks.credential.aliyun.AliyunCloudConfigurationProvider;
 import com.starrocks.credential.aws.AwsCloudConfigurationProvider;
@@ -24,15 +25,22 @@ import com.starrocks.credential.gcp.GCPCloudConfigurationProvoder;
 import com.starrocks.credential.hdfs.HDFSCloudConfigurationProvider;
 import com.starrocks.credential.hdfs.StrictHDFSCloudConfigurationProvider;
 import com.starrocks.credential.tencent.TencentCloudConfigurationProvider;
+import com.starrocks.fs.azure.AzBlobURI;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.iceberg.aws.AwsClientProperties;
 import org.apache.iceberg.aws.s3.S3FileIOProperties;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class CloudConfigurationFactory {
+import static com.starrocks.credential.azure.AzureCloudConfigurationProvider.ADLS_ENDPOINT;
+import static com.starrocks.credential.azure.AzureCloudConfigurationProvider.ADLS_SAS_TOKEN;
+import static com.starrocks.credential.azure.AzureCloudConfigurationProvider.BLOB_ENDPOINT;
 
+public class CloudConfigurationFactory {
+    private static final Logger LOG = LogManager.getLogger(CloudConfigurationFactory.class);
     static ImmutableList<CloudConfigurationProvider> cloudConfigurationFactoryChain = ImmutableList.of(
             new AwsCloudConfigurationProvider(),
             new AzureCloudConfigurationProvider(),
@@ -83,7 +91,18 @@ public class CloudConfigurationFactory {
         return null;
     }
 
-    public static CloudConfiguration buildCloudConfigurationForVendedCredentials(Map<String, String> properties) {
+    public static CloudConfiguration buildCloudConfigurationForVendedCredentials(Map<String, String> properties,
+                                                                                 String path) {
+        CloudConfiguration cloudConfiguration = buildCloudConfigurationForAWSVendedCredentials(properties);
+        if (cloudConfiguration.getCloudType() != CloudType.DEFAULT) {
+            return cloudConfiguration;
+        }
+
+        cloudConfiguration = buildCloudConfigurationForAzureVendedCredentials(properties, path);
+        return cloudConfiguration;
+    }
+
+    public static CloudConfiguration buildCloudConfigurationForAWSVendedCredentials(Map<String, String> properties) {
         Map<String, String> copiedProperties = new HashMap<>();
         String sessionAk = properties.getOrDefault(S3FileIOProperties.ACCESS_KEY_ID, null);
         String sessionSk = properties.getOrDefault(S3FileIOProperties.SECRET_ACCESS_KEY, null);
@@ -106,6 +125,30 @@ public class CloudConfigurationFactory {
             }
             if (enablePathStyle != null) {
                 copiedProperties.put(CloudConfigurationConstants.AWS_S3_ENABLE_PATH_STYLE_ACCESS, enablePathStyle);
+            }
+        }
+        return buildCloudConfigurationForStorage(copiedProperties);
+    }
+
+    public static CloudConfiguration buildCloudConfigurationForAzureVendedCredentials(Map<String, String> properties,
+                                                                                      String path) {
+        Map<String, String> copiedProperties = new HashMap<>();
+        for (Map.Entry<String, String> entry : properties.entrySet()) {
+            if (entry.getKey().startsWith(ADLS_SAS_TOKEN) && entry.getKey().endsWith(ADLS_ENDPOINT)) {
+                String endpoint = entry.getKey().substring(ADLS_SAS_TOKEN.length());
+                copiedProperties.put(CloudConfigurationConstants.AZURE_ADLS2_ENDPOINT, endpoint);
+                copiedProperties.put(CloudConfigurationConstants.AZURE_ADLS2_SAS_TOKEN, entry.getValue());
+            } else if (entry.getKey().startsWith(ADLS_SAS_TOKEN) && entry.getKey().endsWith(BLOB_ENDPOINT)) {
+                try {
+                    AzBlobURI uri = AzBlobURI.parse(path);
+                    copiedProperties.put(CloudConfigurationConstants.AZURE_BLOB_STORAGE_ACCOUNT, uri.getAccount());
+                    copiedProperties.put(CloudConfigurationConstants.AZURE_BLOB_CONTAINER, uri.getContainer());
+                    copiedProperties.put(CloudConfigurationConstants.AZURE_BLOB_SAS_TOKEN, entry.getValue());
+                } catch (StarRocksException e) {
+                    String errorMessage = String.format("Failed to parse azure blob file for path: %s, properties: %s", path,
+                            properties);
+                    LOG.error(errorMessage, e);
+                }
             }
         }
         return buildCloudConfigurationForStorage(copiedProperties);
