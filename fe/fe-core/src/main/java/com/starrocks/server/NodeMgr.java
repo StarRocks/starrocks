12e25f36bedd6a31a4a95266f0ea9185a52e2437
gengjun-git <gengjun@starrocks.com>
2025-07-24 15:19:11 +0800

    [Enhancement] Collect FE's machine info including cpu and mac address (#61154)
    Signed-off-by: gengjun-git <gengjun@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/server/NodeMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/server/NodeMgr.java
@@ -1360,4 +1360,11 @@ public class NodeMgr {
     public static boolean isFeNodeNameValid(String nodeName, String host, int port) {
         return nodeName.startsWith(host + "_" + port);
     }
+
+    public long getTotalCpuCores() {
+        return frontends.values()
+                .stream()
+                .mapToLong(Frontend::getCpuCores)
+                .sum() + systemInfo.getTotalCpuCores();
+    }
 }
