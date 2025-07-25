7f1ad12927c5d3eada68bee2968732f39dac85f4
yan zhang <dirtysalt1987@gmail.com>
2025-07-25 10:58:27 +0800

    [Enhancement] share file io in iceberg metadata scanner (#61012)
    Signed-off-by: yan zhang <dirtysalt1987@gmail.com>
    (cherry picked from commit f4626f289f8782e03beb4fc34f8c844fa92be866)

---
+++ java-extensions/iceberg-metadata-reader/src/main/java/com/starrocks/connector/iceberg/TableFileIOCache.java
@@ -0,0 +1,51 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.starrocks.connector.iceberg;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import org.apache.iceberg.Table;
+import org.apache.iceberg.io.FileIO;
+
+import java.util.concurrent.TimeUnit;
+
+public class TableFileIOCache {
+    private final Cache<String, FileIO> cache;
+
+    public TableFileIOCache(long expireSeconds, long capacity) {
+        // note: reason why not use expireAfterAccess is that
+        // fileio could be bound to credentials, and credentials could be expired.
+        // some fileio implementations may not handle this well
+        // so for safety, we use expireAfterWrite
+        this.cache = CacheBuilder.newBuilder()
+                .expireAfterWrite(expireSeconds, TimeUnit.SECONDS)
+                .maximumSize(capacity)
+                .build();
+    }
+
+    public FileIO get(Table table) {
+        String cacheKey = generateCacheKey(table);
+        FileIO fileIO = cache.getIfPresent(cacheKey);
+        if (fileIO == null) {
+            fileIO = table.io();
+            cache.put(cacheKey, fileIO);
+        }
+        return fileIO;
+    }
+
+    private static String generateCacheKey(Table table) {
+        return table.name() + "_" + table.uuid();
+    }
+}
