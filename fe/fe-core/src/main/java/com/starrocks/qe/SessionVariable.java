2cec2a4868ec035df478560a14d0cdeac4843b48
Murphy <mofei@starrocks.com>
2025-08-06 11:15:05 +0800

    global dict
    Signed-off-by: Murphy <mofei@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
@@ -391,6 +391,7 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
             "cbo_enable_single_node_prefer_two_stage_aggregate";
 
     public static final String CBO_JSON_V2_REWRITE = "cbo_json_v2_rewrite";
+    public static final String CBO_JSON_V2_DICT_OPT = "cbo_json_v2_dict_opt";
 
     public static final String CBO_PUSH_DOWN_DISTINCT_BELOW_WINDOW = "cbo_push_down_distinct_below_window";
     public static final String CBO_PUSH_DOWN_AGGREGATE = "cbo_push_down_aggregate";
@@ -1249,6 +1250,16 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
     @VarAttr(name = CBO_JSON_V2_REWRITE)
     private boolean cboJSONV2Rewrite = true;
 
+    @VarAttr(name = CBO_JSON_V2_DICT_OPT)
+    private boolean cboJSONV2DictOPt = true;
+
+    public boolean isEnableJSONV2Rewrite() {
+        return cboJSONV2Rewrite;
+    }
+
+    public boolean isEnableJSONV2DictOpt() {
+        return cboJSONV2DictOPt;
+    }
 
     /*
      * the parallel exec instance num for one Fragment in one BE
