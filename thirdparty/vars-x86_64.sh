0045a6fbcebfa371d6642e8aedf7e8ef4ce1d42a
Kevin Cai <kevin.cai@celerdata.com>
2025-08-07 15:16:47 +0800

    [Enhancement] upgrade staros to v3.5-rc3
    * starmgr changelog:
    e51a86c7 [Enhancement] S3FileStore add path_style_access option (#1100)
    1898d398 add interface api for shared-data migration (#1085)
    fa282811 [Enhancement] allow interrupt in the middle of the shard/group check (#1092)
    07de21df [Enhancement] add tagging ability to s3 filesystem (#1090)
    f4567f8b [BugFix] add a few lines related to leaderInfo (#1088)
    
    * starlet changelog:
    2bdb461e [Enhancement] refactor fs metrics (#1101)
    fc240c95 [Enhancement] get detailed error root cause from hdfs jni call (#1106)
    3607ca5c update star cache to branch-3.5 b2152d1 (#1103)
    f61ef238 [Enhancement] refactor fs registering (#1094)
    
    Signed-off-by: Kevin Cai <kevin.cai@celerdata.com>

--- thirdparty/vars-x86_64.sh
+++ thirdparty/vars-x86_64.sh
@@ -52,7 +52,7 @@ JINDOSDK_SOURCE="jindosdk-4.6.8-linux"
 JINDOSDK_MD5SUM="5436e4fe39c4dfdc942e41821f1dd8a9"
 
 # starcache
-STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v3.5-rc2/starcache-centos7_amd64.tar.gz"
+STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v3.5-rc3/starcache-centos7_amd64.tar.gz"
 STARCACHE_NAME="starcache.tar.gz"
 STARCACHE_SOURCE="starcache"
-STARCACHE_MD5SUM="7f8f7d05d8402fcdec99c2e2bd997c34"
+STARCACHE_MD5SUM="9f066e061caaba25fa23e727edfde92f"
