2dc2508ec393ee01e0bee621ab2a5e1904b32b58
侯世涛 <shitaohou@kuainiugroup.com>
2025-08-10 20:05:53 +0800

    Unified catalog should support CTAS && Doc

--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/CreateTableAnalyzer.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/CreateTableAnalyzer.java
@@ -166,7 +166,7 @@ public class CreateTableAnalyzer {
                         } else if (ConnectorType.HIVE.getName().equals(unifiedCtasEngine) || ConnectorType.ICEBERG.getName().equals(unifiedCtasEngine)) {
                             engineName = unifiedCtasEngine;
                         } else {
-                            throw new SemanticException("Create table %s in unified catalog requires engine clause (ENGINE = ENGINE_NAME).", stmt.getDbTbl());
+                            throw new SemanticException("CTAS is not support in the %s catalog", PropertyAnalyzer.PROPERTIES_UNIFIED_CTAS_ENGINE);
                         }
                     }
                     stmt.setEngineName(engineName);
