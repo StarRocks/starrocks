ae7c7d2fe0cac72582387c1e6a7360c3ea70fa88
Kevin Cai <caixiaohua@starrocks.com>
2025-08-10 11:30:23 +0800

    [UT] skip caching mv plan in the background (#61750)
    avoid the background caching activity that interfers other unit tests in the same test suite.
    
    Signed-off-by: Kevin Cai <kevin.cai@celerdata.com>
    (cherry picked from commit 0673fc3b8f106d285d7a0b438cb63dedf1bf2645)
    
    # Conflicts:
    #	fe/fe-core/src/test/java/com/starrocks/server/LocalMetaStoreTest.java

--- fe/fe-core/src/test/java/com/starrocks/server/LocalMetaStoreTest.java
+++ fe/fe-core/src/test/java/com/starrocks/server/LocalMetaStoreTest.java
@@ -49,6 +49,11 @@ import com.starrocks.persist.metablock.SRMetaBlockReader;
 import com.starrocks.persist.metablock.SRMetaBlockReaderV2;
 import com.starrocks.qe.ConnectContext;
 import com.starrocks.sql.ast.ColumnRenameClause;
+<<<<<<< HEAD
+=======
+import com.starrocks.sql.optimizer.CachingMvPlanContextBuilder;
+import com.starrocks.sql.optimizer.MaterializedViewOptimizer;
+>>>>>>> 0673fc3b8f ([UT] skip caching mv plan in the background (#61750))
 import com.starrocks.thrift.TStorageMedium;
 import com.starrocks.utframe.StarRocksAssert;
 import com.starrocks.utframe.UtFrameUtils;
@@ -120,6 +125,25 @@ public class LocalMetaStoreTest {
 
     @Test
     public void testGetPartitionIdToStorageMediumMap() throws Exception {
+<<<<<<< HEAD
+=======
+        new MockUp<MaterializedViewOptimizer>() {
+            @Mock
+            public MvPlanContext optimize(MaterializedView mv,
+                                          ConnectContext connectContext) {
+                return null;
+            }
+        };
+
+        // Mock the cacheMaterializedView method to avoid actual caching in the background
+        new MockUp<CachingMvPlanContextBuilder>() {
+            @Mock
+            public void cacheMaterializedView(MaterializedView mv) {
+                // Do nothing to avoid actual caching
+            }
+        };
+
+>>>>>>> 0673fc3b8f ([UT] skip caching mv plan in the background (#61750))
         starRocksAssert.withMaterializedView(
                     "CREATE MATERIALIZED VIEW test.mv1\n" +
                                 "distributed by hash(k1) buckets 3\n" +
