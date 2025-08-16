921caf31342605df6ade42e6bebcb13f51ad4f2c
Murphy <mofei@starrocks.com>
2025-08-16 11:20:49 +0800

    fixup
    Signed-off-by: Murphy <mofei@starrocks.com>

--- be/src/storage/rowset/zone_map_index.cpp
+++ be/src/storage/rowset/zone_map_index.cpp
@@ -53,29 +53,6 @@
 
 namespace starrocks {
 
-// Truncate string min/max values at write time to reduce comparison/metadata overhead.
-// For max values that are truncated, append 0xFF to preserve an upper bound.
-template <LogicalType LT>
-static inline void _truncate_string_minmax_if_needed(ZoneMap<LT>* zm) {
-    if (!config::enable_string_prefix_zonemap) return;
-    const size_t kPrefixLen = std::max<int32_t>(0, config::string_prefix_zonemap_prefix_len);
-    if constexpr (LT == TYPE_CHAR || LT == TYPE_VARCHAR) {
-        auto& min_slice = zm->min_value.value;
-        auto& max_slice = zm->max_value.value;
-        if (min_slice.size > kPrefixLen) {
-            min_slice.size = kPrefixLen;
-        }
-        if (max_slice.size > kPrefixLen) {
-            // Safe, original buffer has length > kPrefixLen
-            // Ensure buffer has room for 0xFF; if exactly kPrefixLen length, keep as is.
-            if (max_slice.size > kPrefixLen) {
-                max_slice.data[kPrefixLen] = static_cast<char>(0xFF);
-                max_slice.size = kPrefixLen + 1;
-            }
-        }
-    }
-}
-
 template <LogicalType type>
 struct ZoneMapDatumBase {
     using CppType = typename TypeTraits<type>::CppType;
