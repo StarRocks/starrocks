ca2e1ff0c41a9a81c0d92dba78c8062ea57ae5a4
Kevin Cai <kevin.cai@celerdata.com>
2025-08-19 15:01:14 +0800

    [BugFix][CVE] patch io.netty CVE (#39589)
    Signed-off-by: Kevin Cai <kevin.cai@celerdata.com>

--- fix-cve/fix-cve.py
+++ fix-cve/fix-cve.py
@@ -6,15 +6,17 @@ import os
 import shutil
 import subprocess
 
+io_netty_clean_version = "4.1.124.Final"
+
 fixed_poms = [
     ("com.fasterxml.jackson.core:jackson-core", "2.15.0"),
     ("com.fasterxml.jackson.core:jackson-databind", "2.18.3"),
-    ("io.netty:netty-handler", "4.1.118.Final"),
-    ("io.netty:netty-codec-http2", "4.1.118.Final"),
-    ("io.netty:netty-codec-http", "4.1.118.Final"),
-    ("io.netty:netty-codec-haproxy", "4.1.118.Final"),
-    ("io.netty:netty-common", "4.1.118.Final"),
-    ("io.netty:netty-all", "4.1.118.Final"),
+    ("io.netty:netty-handler", io_netty_clean_version),
+    ("io.netty:netty-codec-http2", io_netty_clean_version),
+    ("io.netty:netty-codec-http", io_netty_clean_version),
+    ("io.netty:netty-codec-haproxy", io_netty_clean_version),
+    ("io.netty:netty-common", io_netty_clean_version),
+    ("io.netty:netty-all", io_netty_clean_version),
     ("com.google.protobuf:protobuf-java", "3.25.5"),
     ("org.apache.avro:avro", "1.11.4"),
     ("org.apache.commons:commons-compress", "1.26.0"),
