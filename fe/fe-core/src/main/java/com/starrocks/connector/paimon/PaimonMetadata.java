3568d07ed39aa22bb939868503c858d0924a9466
wangriyu.wry <wangriyu.wry@alibaba-inc.com>
2025-08-20 16:33:36 +0800

    [Stella][BugFix] Fix compatibility issues with paimon partition names

--- fe/fe-core/src/main/java/com/starrocks/connector/paimon/PaimonMetadata.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/paimon/PaimonMetadata.java
@@ -96,6 +96,7 @@ import org.apache.paimon.types.DateType;
 import org.apache.paimon.types.RowType;
 import org.apache.paimon.utils.DateTimeUtils;
 import org.apache.paimon.utils.PartitionPathUtils;
+import org.apache.paimon.utils.StringUtils;
 
 import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
@@ -314,7 +315,7 @@ public class PaimonMetadata implements ConnectorMetadata {
             String column = partitionColumnNames.get(i);
             String value = partitionValues[i].trim();
             if (partitionColumnTypes.get(i) instanceof DateType) {
-                if (!partitionKey.nullPartitionValueList().contains(value)) {
+                if (!partitionKey.nullPartitionValueList().contains(value) && StringUtils.isNumeric(value)) {
                     value = DateTimeUtils.formatDate(Integer.parseInt(value));
                 }
             }
