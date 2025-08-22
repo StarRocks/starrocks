73d3fca6060b3e4861b02047a7103891cf5305e8
Harbor Liu <460660596@qq.com>
2025-08-22 10:32:13 +0800

    [Refactor]  Refactors the partitionDesc analysis logic in the SQL analyzer (#62197)

--- fe/fe-core/src/main/java/com/starrocks/sql/ast/ExpressionPartitionDesc.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/ast/ExpressionPartitionDesc.java
@@ -17,23 +17,14 @@ package com.starrocks.sql.ast;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
-import com.starrocks.analysis.CastExpr;
 import com.starrocks.analysis.Expr;
 import com.starrocks.analysis.FunctionCallExpr;
 import com.starrocks.analysis.SlotRef;
 import com.starrocks.catalog.FunctionSet;
 import com.starrocks.catalog.Type;
-import com.starrocks.common.AnalysisException;
-import com.starrocks.sql.analyzer.AnalyzerUtils;
-import com.starrocks.sql.analyzer.PartitionDescAnalyzer;
-import com.starrocks.sql.analyzer.PartitionExprAnalyzer;
-import com.starrocks.sql.analyzer.PartitionFunctionChecker;
-import com.starrocks.sql.analyzer.SemanticException;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 public class ExpressionPartitionDesc extends PartitionDesc {
 
@@ -95,65 +86,9 @@ public class ExpressionPartitionDesc extends PartitionDesc {
         return partitionType;
     }
 
-    @Override
-    public void analyze(List<ColumnDef> columnDefs, Map<String, String> otherProperties) throws AnalysisException {
-        boolean hasExprAnalyze = false;
-        SlotRef slotRef;
-        if (rangePartitionDesc != null) {
-            // for automatic partition table
-            if (rangePartitionDesc.isAutoPartitionTable) {
-                rangePartitionDesc.setAutoPartitionTable(true);
-                slotRef = AnalyzerUtils.getSlotRefFromFunctionCall(expr);
-                if (expr instanceof FunctionCallExpr) {
-                    FunctionCallExpr functionCallExpr = (FunctionCallExpr) expr;
-                    if (!AUTO_PARTITION_SUPPORT_FUNCTIONS.contains(functionCallExpr.getFnName().getFunction())) {
-                        throw new SemanticException("Only support date_trunc and time_slice as partition expression");
-                    }
-                }
-            } else {
-                // for partition by range expr table
-                // The type of the partition field may be different from the type after the expression
-                if (expr instanceof CastExpr) {
-                    slotRef = AnalyzerUtils.getSlotRefFromCast(expr);
-                    partitionType = ((CastExpr) expr).getTargetTypeDef().getType();
-                } else if (expr instanceof FunctionCallExpr) {
-                    slotRef = AnalyzerUtils.getSlotRefFromFunctionCall(expr);
-
-                    Optional<ColumnDef> columnDef = columnDefs.stream()
-                            .filter(c -> c.getName().equals(slotRef.getColumnName())).findFirst();
-                    Preconditions.checkState(columnDef.isPresent());
-                    slotRef.setType(columnDef.get().getType());
-
-                    String functionName = ((FunctionCallExpr) expr).getFnName().getFunction().toLowerCase();
-                    if (functionName.equals(FunctionSet.STR2DATE)) {
-                        partitionType = Type.DATE;
-                        if (!PartitionFunctionChecker.checkStr2date(expr)) {
-                            throw new SemanticException("partition function check fail, only supports the result " +
-                                    "of the function str2date(VARCHAR str, VARCHAR format) as a strict DATE type");
-                        }
-                    }
-                } else {
-                    throw new AnalysisException("Unsupported expr:" + expr.toSql());
-                }
-            }
-            rangePartitionDesc.partitionType = partitionType;
-            PartitionDescAnalyzer.analyze(rangePartitionDesc);
-            rangePartitionDesc.analyze(columnDefs, otherProperties);
-        } else {
-            // for materialized view
-            slotRef = AnalyzerUtils.getSlotRefFromFunctionCall(expr);
-        }
-
-        for (ColumnDef columnDef : columnDefs) {
-            if (columnDef.getName().equalsIgnoreCase(slotRef.getColumnName())) {
-                slotRef.setType(columnDef.getType());
-                PartitionExprAnalyzer.analyzePartitionExpr(expr, slotRef);
-                partitionType = expr.getType();
-                hasExprAnalyze = true;
-            }
-        }
-        if (!hasExprAnalyze) {
-            throw new AnalysisException("Partition expr without analyzed.");
-        }
+    public void setPartitionType(Type partitionType) {
+        this.partitionType = partitionType;
     }
+
+
 }
