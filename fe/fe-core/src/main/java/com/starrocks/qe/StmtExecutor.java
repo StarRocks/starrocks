b8933f5f67668385732503a9f7a4bc3a43beb164
shuming.li <ming.moriarty@gmail.com>
2025-08-26 11:05:51 +0800

    [BugFix] Fix explain trace error (#62282)
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
@@ -185,6 +185,7 @@ import com.starrocks.sql.ast.LoadStmt;
 import com.starrocks.sql.ast.OriginStatement;
 import com.starrocks.sql.ast.PrepareStmt;
 import com.starrocks.sql.ast.QueryStatement;
+import com.starrocks.sql.ast.RefreshMaterializedViewStatement;
 import com.starrocks.sql.ast.SetCatalogStmt;
 import com.starrocks.sql.ast.SetDefaultRoleStmt;
 import com.starrocks.sql.ast.SetRoleStmt;
@@ -558,7 +559,6 @@ public class StmtExecutor {
         // execPlan is the output of planner
         ExecPlan execPlan = null;
         try (Timer ignored = Tracers.watchScope("Total")) {
-            redirectStatus = RedirectStatus.getRedirectStatus(parsedStmt);
             if (!isForwardToLeader()) {
                 if (context.shouldDumpQuery()) {
                     if (context.getDumpInfo() == null) {
@@ -676,8 +676,16 @@ public class StmtExecutor {
                 context.setExplainLevel(null);
             }
 
-            // for explain query(not explain analyze), we can trace even if optimizer fails and execPlan is null
-            if (parsedStmt.isExplainTrace()) {
+            redirectStatus = RedirectStatus.getRedirectStatus(parsedStmt);
+            // for explain query, we can trace even if optimizer fails and execPlan is null
+            // for refresh materialized view, use handleDdlStmt instead.
+            if (parsedStmt.isExplainTrace() && !(parsedStmt instanceof RefreshMaterializedViewStatement)) {
+                if (isForwardToLeader()) {
+                    context.setIsForward(true);
+                    forwardToLeader();
+                    return;
+                }
+
                 ExecPlan execPlan = null;
                 try {
                     execPlan = generateExecPlan();
