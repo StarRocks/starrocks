05765e1b885d354d013230d602837f648434fb00
Murphy <96611012+murphyatwork@users.noreply.github.com>
2025-08-27 20:14:53 +0800

    [BugFix] use fe id instead to replace the fe name (#62378)

--- fe/fe-core/src/main/java/com/starrocks/statistic/columns/PredicateColumnsStorage.java
+++ fe/fe-core/src/main/java/com/starrocks/statistic/columns/PredicateColumnsStorage.java
@@ -217,7 +217,7 @@ public class PredicateColumnsStorage {
 
             VelocityContext context = new VelocityContext();
             ColumnFullId fullId = usage.getColumnFullId();
-            context.put("feId", GlobalStateMgr.getCurrentState().getNodeMgr().getNodeName());
+            context.put("feId", GlobalStateMgr.getCurrentState().getNodeMgr().getMySelf().getFid());
             context.put("dbId", fullId.getDbId());
             context.put("tableId", fullId.getTableId());
             context.put("columnId", fullId.getColumnUniqueId());
@@ -241,7 +241,7 @@ public class PredicateColumnsStorage {
      * Restore all states
      */
     public List<ColumnUsage> restore() {
-        String selfName = GlobalStateMgr.getCurrentState().getNodeMgr().getNodeName();
+        String selfName = String.valueOf(GlobalStateMgr.getCurrentState().getNodeMgr().getMySelf().getFid());
 
         VelocityContext context = new VelocityContext();
         context.put("feId", selfName);
@@ -259,9 +259,19 @@ public class PredicateColumnsStorage {
      * Remove all records if the lastUsed < ttlTime
      */
     public void vacuum(LocalDateTime ttlTime) {
+        String selfName = String.valueOf(GlobalStateMgr.getCurrentState().getNodeMgr().getMySelf().getFid());
+        vacuum(ttlTime, selfName);
+        // compatible with old version fe name
+        // previously we use the fe_name as the feId, we also need to vacuum it otherwise nobody care about it
+        String oldName = GlobalStateMgr.getCurrentState().getNodeMgr().getNodeName();
+        vacuum(ttlTime, oldName);
+
+        LOG.info("vacuum column usage from storage before {}", ttlTime);
+    }
+
+    private void vacuum(LocalDateTime ttlTime, String feName) {
         VelocityContext context = new VelocityContext();
-        String selfName = GlobalStateMgr.getCurrentState().getNodeMgr().getNodeName();
-        context.put("feId", selfName);
+        context.put("feId", feName);
         context.put("lastUsed", DateUtils.formatDateTimeUnix(ttlTime));
 
         StringWriter sw = new StringWriter();
@@ -269,7 +279,6 @@ public class PredicateColumnsStorage {
 
         String sql = sw.toString();
         executor.executeDML(sql);
-        LOG.info("vacuum column usage from storage before {}", ttlTime);
     }
 
     public boolean isSystemTableReady() {
