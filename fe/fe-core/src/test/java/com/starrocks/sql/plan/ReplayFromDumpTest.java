6794a022567869cba299ec6e5690471e52e60228
satanson <ranpanf@gmail.com>
2025-08-29 10:25:00 +0800

    [BugFix] TableFunction not use low cardinality optimization (#62466)
    Signed-off-by: satanson <ranpanf@gmail.com>
    (cherry picked from commit 1ad0f4683e2d28ef8777059ebd81c1cbd27204c5)
    
    # Conflicts:
    #	fe/fe-core/src/test/java/com/starrocks/sql/plan/ReplayFromDumpTest.java

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/ReplayFromDumpTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/ReplayFromDumpTest.java
@@ -1090,4 +1090,95 @@ public class ReplayFromDumpTest extends ReplayFromDumpTestBase {
             FeConstants.USE_MOCK_DICT_MANAGER = false;
         }
     }
+<<<<<<< HEAD
+=======
+
+    @Test
+    public void testExpressionReuseTimeout() throws Exception {
+        String dumpString = getDumpInfoFromFile("query_dump/expr_reuse_timeout");
+        Tracers.register(connectContext);
+        Tracers.init(Tracers.Mode.TIMER, Tracers.Module.OPTIMIZER, false, false);
+        QueryDumpInfo queryDumpInfo = getDumpInfoFromJson(dumpString);
+        Pair<QueryDumpInfo, String> replayPair = getPlanFragment(dumpString, queryDumpInfo.getSessionVariable(),
+                TExplainLevel.NORMAL);
+        System.out.println(Tracers.printScopeTimer());
+        String ss = Tracers.printScopeTimer();
+        int start = ss.indexOf("PhysicalRewrite[") + "PhysicalRewrite[".length();
+        int end = ss.indexOf("]", start);
+        long count = Long.parseLong(ss.substring(start, end));
+        Assertions.assertTrue(count < 1000, ss);
+    }
+
+    @Test
+    public void testForceReuseCTEWithHugeCTE() throws Exception {
+        String dumpString = getDumpInfoFromFile("query_dump/big_cte_with_force_reuse");
+        QueryDumpInfo queryDumpInfo = getDumpInfoFromJson(dumpString);
+        Pair<QueryDumpInfo, String> replayPair = getPlanFragment(dumpString, queryDumpInfo.getSessionVariable(),
+                TExplainLevel.NORMAL);
+        Assertions.assertTrue(replayPair.second.contains("MultiCastDataSinks\n" +
+                "  STREAM DATA SINK\n" +
+                "    EXCHANGE ID: 1607\n" +
+                "    RANDOM\n" +
+                "  STREAM DATA SINK\n" +
+                "    EXCHANGE ID: 1612\n" +
+                "    RANDOM\n" +
+                "  STREAM DATA SINK\n" +
+                "    EXCHANGE ID: 1627\n" +
+                "    RANDOM\n" +
+                "  STREAM DATA SINK\n" +
+                "    EXCHANGE ID: 1644\n" +
+                "    RANDOM\n" +
+                "  STREAM DATA SINK\n" +
+                "    EXCHANGE ID: 1650\n" +
+                "    RANDOM\n" +
+                "  STREAM DATA SINK\n" +
+                "    EXCHANGE ID: 1664\n" +
+                "    RANDOM\n" +
+                "  STREAM DATA SINK\n" +
+                "    EXCHANGE ID: 1691\n" +
+                "    RANDOM\n" +
+                "  STREAM DATA SINK\n" +
+                "    EXCHANGE ID: 1697\n" +
+                "    RANDOM\n" +
+                "  STREAM DATA SINK\n" +
+                "    EXCHANGE ID: 1711\n" +
+                "    RANDOM"), replayPair.second);
+    }
+
+    @Test
+    public void testPruneUnionEmpty() throws Exception {
+        try {
+            FeConstants.enablePruneEmptyOutputScan = true;
+            String dumpString = getDumpInfoFromFile("query_dump/prune_union_empty");
+            QueryDumpInfo queryDumpInfo = getDumpInfoFromJson(dumpString);
+            Pair<QueryDumpInfo, String> replayPair = getPlanFragment(dumpString, queryDumpInfo.getSessionVariable(),
+                    TExplainLevel.NORMAL);
+            Assertions.assertTrue(replayPair.second.contains("1:EMPTYSET"), replayPair.second);
+        } finally {
+            FeConstants.enablePruneEmptyOutputScan = false;
+        }
+    }
+
+    @Test
+    public void testUnnestLowCardinalityOptimization() throws Exception {
+        try {
+            FeConstants.USE_MOCK_DICT_MANAGER = true;
+            String dumpString = getDumpInfoFromFile("query_dump/unnest_low_cardinality_optimization");
+            QueryDumpInfo queryDumpInfo = getDumpInfoFromJson(dumpString);
+            Pair<QueryDumpInfo, String> replayPair = getPlanFragment(dumpString, queryDumpInfo.getSessionVariable(),
+                    TExplainLevel.NORMAL);
+            String plan = replayPair.second;
+            Assertions.assertTrue(plan.contains("  30:Project\n" +
+                    "  |  <slot 113> : 113: mock_field_111\n" +
+                    "  |  <slot 278> : lower(142: jl_str)\n" +
+                    "  |  \n" +
+                    "  29:TableValueFunction\n" +
+                    "  |  tableFunctionName: unnest\n" +
+                    "  |  columns: [unnest]\n" +
+                    "  |  returnTypes: [VARCHAR(65533)]"), plan);
+        } finally {
+            FeConstants.USE_MOCK_DICT_MANAGER = false;
+        }
+    }
+>>>>>>> 1ad0f4683e ([BugFix] TableFunction not use low cardinality optimization (#62466))
 }
