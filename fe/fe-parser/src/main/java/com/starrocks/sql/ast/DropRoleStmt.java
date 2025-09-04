e9405257c97919a898d0333bae1a207c18308930
andyziye <108652123+andyziye@users.noreply.github.com>
2025-09-04 15:28:12 +0800

    [Refactor] Refactor some AST classes from fe-core to fe-parser (#40191)
    Co-authored-by: Harbor Liu <460660596@qq.com>

---
+++ fe/fe-parser/src/main/java/com/starrocks/sql/ast/DropRoleStmt.java
@@ -0,0 +1,47 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.starrocks.sql.ast;
+
+import com.starrocks.sql.parser.NodePosition;
+
+import java.util.List;
+
+public class DropRoleStmt extends DdlStmt {
+    private final List<String> roles;
+    private final boolean ifExists;
+
+    public DropRoleStmt(List<String> roles, boolean ifExists) {
+        this(roles, ifExists, NodePosition.ZERO);
+    }
+
+    public DropRoleStmt(List<String> roles, boolean ifExists, NodePosition pos) {
+        super(pos);
+        this.roles = roles;
+        this.ifExists = ifExists;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public boolean isIfExists() {
+        return ifExists;
+    }
+
+    @Override
+    public <R, C> R accept(AstVisitor<R, C> visitor, C context) {
+        return visitor.visitDropRoleStatement(this, context);
+    }
+}
