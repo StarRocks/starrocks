6cbd60c561f9930e6a9a2c93cd280e739ba42ece
trueeyu <lxhhust350@qq.com>
2025-09-05 23:09:14 +0800

    Fix compile
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/service/service_be/starrocks_be.cpp
+++ be/src/service/service_be/starrocks_be.cpp
@@ -124,13 +124,8 @@ void start_be(const std::vector<StorePath>& paths, bool as_cn) {
     LOG(INFO) << process_name << " start step " << start_step++ << ": storage engine start bg threads successfully";
 
 #ifdef USE_STAROS
-<<<<<<< HEAD
-    auto* local_cache = cache_env->local_cache();
-    if (config::datacache_unified_instance_enable && local_cache->is_initialized()) {
-=======
     auto* local_cache = cache_env->local_disk_cache();
     if (config::datacache_unified_instance_enable && local_cache && local_cache->is_initialized()) {
->>>>>>> 6fda0829d1 ([Refactor] Split data cache engine into disk cache engine and memory cache engine. (#62760))
         auto* starcache = reinterpret_cast<StarCacheEngine*>(local_cache);
         init_staros_worker(starcache->starcache_instance());
     } else {
