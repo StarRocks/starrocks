6a7ed65eff3b94183b526be0e4c210611a6fc5e3
Evgeniy Shishkin <eshishki@gmail.com>
2025-09-09 00:16:35 +0400

    initial

---
+++ be/src/cache/segment_cache_populator.cpp
@@ -0,0 +1,65 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "cache/segment_cache_populator.h"
+
+#include "cache/datacache.h"
+#include "fs/fs.h"
+#include "io/cache_input_stream.h"
+#include "gutil/strings/substitute.h"
+
+DEFINE_int32(segment_cache_populate_thread_num, 2, "Number of threads for async segment cache population");
+
+namespace starrocks {
+
+SegmentCachePopulator* SegmentCachePopulator::instance() {
+    static SegmentCachePopulator instance;
+    return &instance;
+}
+
+SegmentCachePopulator::SegmentCachePopulator() {
+    _cache_populate_pool = std::make_unique<ThreadPool>(FLAGS_segment_cache_populate_thread_num);
+    Status st = _cache_populate_pool->initialize("segment_cache_populate");
+    if (!st.ok()) {
+        LOG(WARNING) << "Failed to initialize segment cache populate thread pool: " << st;
+    }
+}
+
+SegmentCachePopulator::~SegmentCachePopulator() {
+    if (_cache_populate_pool) {
+        _cache_populate_pool->shutdown();
+    }
+}
+
+Status SegmentCachePopulator::populate_segment_to_cache(const std::string& segment_path) {
+
+    ASSIGN_OR_RETURN(auto fs, FileSystem::CreateUniqueFromString(segment_path));
+    ASSIGN_OR_RETURN(auto file, fs->new_random_access_file(segment_path));
+    ASSIGN_OR_RETURN(auto file_size, file->get_size());
+
+    auto cache_input_stream = std::make_unique<io::CacheInputStream>(
+        std::move(file), segment_path, file_size, io::CacheOptions{});
+
+    constexpr size_t kChunkSize = 1024 * 1024;
+    std::vector<uint8_t> buffer(kChunkSize);
+
+    for (int64_t offset = 0; offset < file_size; offset += kChunkSize) {
+        size_t to_read = std::min(kChunkSize, static_cast<size_t>(file_size - offset));
+        RETURN_IF_ERROR(cache_input_stream->read_at_fully(offset, buffer.data(), to_read));
+    }
+
+    return Status::OK();
+}
+
+}
---
+++ be/src/cache/segment_cache_populator.h
@@ -0,0 +1,39 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "common/status.h"
+#include "util/threadpool.h"
+
+namespace starrocks {
+
+class SegmentCachePopulator {
+public:
+    static SegmentCachePopulator* instance();
+    
+    // Sync version - does the actual work
+    static Status populate_segment_to_cache(const std::string& segment_path);
+    
+    // Async version - submits to thread pool
+    Status populate_segment_to_cache_async(const std::string& segment_path);
+
+private:
+    SegmentCachePopulator();
+    ~SegmentCachePopulator();
+    
+    std::unique_ptr<ThreadPool> _cache_populate_pool;
+};
+
+} // namespace starrocks
