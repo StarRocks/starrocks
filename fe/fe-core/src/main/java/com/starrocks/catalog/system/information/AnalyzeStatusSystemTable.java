e22ee44593084ca6bb9be4cc94d91b6544e1957e
Murphy <96611012+murphyatwork@users.noreply.github.com>
2025-09-08 10:19:30 +0800

    [BugFix] fix NPE of information_schema.analyze_status when db is dropped (#62796)
    (cherry picked from commit 6a908b4036af8e4cb02d97a386c0e5babec4af4c)
    
    # Conflicts:
    #	fe/fe-core/src/test/java/com/starrocks/statistic/AnalyzeMgrTest.java

--- fe/fe-core/src/main/java/com/starrocks/catalog/system/information/AnalyzeStatusSystemTable.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/system/information/AnalyzeStatusSystemTable.java
@@ -86,7 +86,6 @@ public class AnalyzeStatusSystemTable extends SystemTable {
                 }
             }
             TAnalyzeStatusItem item = new TAnalyzeStatusItem();
-            itemList.add(item);
             item.setCatalog_name("");
             item.setDatabase_name("");
             item.setTable_name("");
@@ -103,6 +102,7 @@ public class AnalyzeStatusSystemTable extends SystemTable {
                     continue;
                 }
             } catch (MetaNotFoundException ignored) {
+                continue;
             }
 
             String columnStr = "ALL";
@@ -120,6 +120,7 @@ public class AnalyzeStatusSystemTable extends SystemTable {
             item.setEnd_time(DateUtils.formatDateTimeUnix(analyze.getEndTime()));
             item.setProperties(analyze.getProperties() == null ? "{}" : analyze.getProperties().toString());
             item.setReason(analyze.getReason());
+            itemList.add(item);
         }
 
         return res;
