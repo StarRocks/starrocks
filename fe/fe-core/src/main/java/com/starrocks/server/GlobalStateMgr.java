0d9ccab3bfee6b4e22a9b415b78eef8e8ec46042
predator4ann <yunlong.sun@hotmail.com>
2025-09-09 17:20:35 +0800

    [BugFix] Fixed the MaterializedView was reloaded multiple times during startup (#62351)
    Signed-off-by: predator4ann <yunlong.sun@hotmail.com>
    (cherry picked from commit 58e8c6755a143f8bd9c478f1fea031c129be1a43)

--- fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
@@ -1616,6 +1616,11 @@ public class GlobalStateMgr {
             }
             for (Table table : db.getTables()) {
                 try {
+                    // Skip MaterializedView, which are processed in processMvRelatedMeta()
+                    if (table.isMaterializedView()) {
+                        continue;
+                    }
+
                     table.onReload();
 
                     if (table.isTemporaryTable()) {
