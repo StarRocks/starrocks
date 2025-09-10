f18f18601c6fe3a7a59806a2500a23294318dbd3
andyziye <108652123+andyziye@users.noreply.github.com>
2025-09-10 14:05:18 +0800

    [Tool] Update Checkstyle configuration and dependencies in gradle (#62778) (#40948)
    Co-authored-by: Harbor Liu <460660596@qq.com>

--- fe/fe-parser/src/main/java/com/starrocks/sql/ast/AstVisitor.java
+++ fe/fe-parser/src/main/java/com/starrocks/sql/ast/AstVisitor.java
@@ -1,4 +1,4 @@
-package com.starrocks.sql.ast;// Copyright 2021-present StarRocks, Inc. All rights reserved.
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +11,8 @@ package com.starrocks.sql.ast;// Copyright 2021-present StarRocks, Inc. All righ
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+package com.starrocks.sql.ast;
 
 import com.starrocks.sql.ast.group.CreateGroupProviderStmt;
 import com.starrocks.sql.ast.group.DropGroupProviderStmt;
