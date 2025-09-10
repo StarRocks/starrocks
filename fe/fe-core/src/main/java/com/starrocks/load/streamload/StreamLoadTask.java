678cb8fff44f63648ecbd3d1f22ade938c2c15ae
PengFei Li <lpengfei2016@gmail.com>
2025-09-10 10:25:16 +0800

    [BugFix] Fix collecting stream load profile failed (#62802)
    Signed-off-by: PengFei Li <lpengfei2016@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/load/streamload/StreamLoadTask.java
+++ fe/fe-core/src/main/java/com/starrocks/load/streamload/StreamLoadTask.java
@@ -1174,6 +1174,9 @@ public class StreamLoadTask extends AbstractStreamLoadTask {
         // sync stream load collect profile, here we collect profile only when be has reported
         if (isSyncStreamLoad() && coord != null && coord.isProfileAlreadyReported()) {
             collectProfile(false);
+        } else {
+            LOG.debug("stream load does not collect profile, txn_id: {}, label: {}, load id: {}",
+                    txnId, label, DebugUtil.printId(loadId));
         }
 
         writeLock();
