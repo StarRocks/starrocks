bb14c105f55b547ca676b38202b26eb3df856139
shuming.li <ming.moriarty@gmail.com>
2025-09-10 15:00:39 +0800

    fix mv view based rewrite bug
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/materialization/rule/OnlyJoinRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/materialization/rule/OnlyJoinRule.java
@@ -15,14 +15,23 @@
 
 package com.starrocks.sql.optimizer.rule.transformation.materialization.rule;
 
+import com.starrocks.sql.optimizer.OptExpression;
+import com.starrocks.sql.optimizer.OptimizerContext;
+import com.starrocks.sql.optimizer.operator.Operator;
 import com.starrocks.sql.optimizer.operator.OperatorType;
+import com.starrocks.sql.optimizer.operator.logical.LogicalFilterOperator;
+import com.starrocks.sql.optimizer.operator.logical.LogicalJoinOperator;
+import com.starrocks.sql.optimizer.operator.logical.LogicalOlapScanOperator;
+import com.starrocks.sql.optimizer.operator.logical.LogicalOperator;
+import com.starrocks.sql.optimizer.operator.logical.LogicalProjectOperator;
+import com.starrocks.sql.optimizer.operator.logical.LogicalScanOperator;
 import com.starrocks.sql.optimizer.operator.pattern.Pattern;
 import com.starrocks.sql.optimizer.rule.RuleType;
 
-/*
- *
- * Here is the rule for pattern Join
- *
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ *  OnlyJoinRule is used to match the only-join query pattern and rewrite it by mv.
  */
 public class OnlyJoinRule extends BaseMaterializedViewRewriteRule {
     private static final OnlyJoinRule INSTANCE = new OnlyJoinRule();
@@ -35,4 +44,45 @@ public class OnlyJoinRule extends BaseMaterializedViewRewriteRule {
         return INSTANCE;
     }
 
+    public static boolean isLogicalSPJ(OptExpression root,
+                                       AtomicBoolean hasJoin) {
+        if (root == null) {
+            return false;
+        }
+        Operator operator = root.getOp();
+        if (!(operator instanceof LogicalOperator)) {
+            return false;
+        }
+        if (!(operator instanceof LogicalScanOperator)
+                && !(operator instanceof LogicalProjectOperator)
+                && !(operator instanceof LogicalFilterOperator)
+                && !(operator instanceof LogicalJoinOperator)) {
+            return false;
+        }
+        if (operator instanceof LogicalJoinOperator) {
+            hasJoin.set(true);
+        }
+        if (operator instanceof LogicalOlapScanOperator) {
+            LogicalOlapScanOperator olapScanOperator = (LogicalOlapScanOperator) operator;
+            if (olapScanOperator.isSample()) {
+                return false;
+            }
+        }
+        for (OptExpression child : root.getInputs()) {
+            if (!isLogicalSPJ(child, hasJoin)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean check(OptExpression input, OptimizerContext context) {
+        // for only-join rule, only SPJ is supported
+        AtomicBoolean hasJoin = new AtomicBoolean(false);
+        if (!isLogicalSPJ(input, hasJoin) || !hasJoin.get()) {
+            return false;
+        }
+        return super.check(input, context);
+    }
 }
