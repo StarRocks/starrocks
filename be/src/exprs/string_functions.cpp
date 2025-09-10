efd17800bf0e853df0abff03223b30c2420d310a
eyes_on_me <nopainnofame@sina.com>
2025-09-10 17:28:55 +0800

    [BugFix] mutate input columns in functions' returning value (#62826)
    Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>

--- be/src/exprs/string_functions.cpp
+++ be/src/exprs/string_functions.cpp
@@ -3047,7 +3047,7 @@ static inline ColumnPtr concat_not_const(Columns const& columns) {
  */
 StatusOr<ColumnPtr> StringFunctions::concat(FunctionContext* context, const Columns& columns) {
     if (columns.size() == 1) {
-        return columns[0]->clone();
+        return std::move(*columns[0]).mutate();
     }
 
     RETURN_IF_COLUMNS_ONLY_NULL(columns);
@@ -3133,7 +3133,7 @@ StatusOr<ColumnPtr> StringFunctions::concat_ws(FunctionContext* context, const C
     }
 
     if (columns.size() == 2) {
-        return columns[1];
+        return std::move(*columns[1]).mutate();
     }
 
     const auto sep_size = ColumnHelper::compute_bytes_size(columns.begin(), columns.begin() + 1);
