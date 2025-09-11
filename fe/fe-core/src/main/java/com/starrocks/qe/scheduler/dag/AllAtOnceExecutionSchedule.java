9df217cb155a7a6b9bed7e1d1b6cce3a0a7a6610
yan zhang <dirtysalt1987@gmail.com>
2025-09-11 18:12:42 +0800

    fix conflict
    Signed-off-by: yan zhang <dirtysalt1987@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/scheduler/dag/AllAtOnceExecutionSchedule.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/scheduler/dag/AllAtOnceExecutionSchedule.java
@@ -57,22 +57,12 @@ public class AllAtOnceExecutionSchedule implements ExecutionSchedule {
                     for (DeployState state : states) {
                         deployer.deployFragments(state);
                     }
-                } catch (StarRocksException | RpcException e) {
+                } catch (Exception e) {
+                    // there could be a lot of reasons to fail, just cancel the query
                     LOG.warn("Failed to assign incremental scan ranges to deploy states", e);
                     coordinator.cancel(PPlanFragmentCancelReason.INTERNAL_ERROR, e.getMessage());
                     throw new RuntimeException(e);
                 }
-<<<<<<< HEAD
-=======
-                for (DeployState state : states) {
-                    deployer.deployFragments(state);
-                }
-            } catch (Exception e) {
-                // there could be a lot of reasons to fail, just cancel the query
-                LOG.warn("Failed to assign incremental scan ranges to deploy states", e);
-                coordinator.cancel(PPlanFragmentCancelReason.INTERNAL_ERROR, e.getMessage());
-                throw new RuntimeException(e);
->>>>>>> 79ee456b9e ([BugFix] fix iceberg table scan exception during scan range deploy (#62994))
             }
         }
 
