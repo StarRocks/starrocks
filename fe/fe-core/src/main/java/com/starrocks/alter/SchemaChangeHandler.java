147f9a8ef5dbff7c1347111572f2ff4d0f8ecc5b
PengFei Li <lpengfei2016@gmail.com>
2025-09-19 13:49:04 +0800

    [BugFix] Fix incompatible zonemap reuse for fast schema evolution in shared-data (#63143)
    Signed-off-by: PengFei Li <lpengfei2016@gmail.com>
    (cherry picked from commit b9fdfcd6c05c7f70e5883c40abfd9ce63952f691)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/alter/SchemaChangeHandler.java
    #	fe/fe-core/src/main/java/com/starrocks/catalog/SchemaChangeTypeCompatibility.java
    #	fe/fe-core/src/test/java/com/starrocks/alter/LakeTableAsyncFastSchemaChangeJobTest.java

--- fe/fe-core/src/main/java/com/starrocks/alter/SchemaChangeHandler.java
+++ fe/fe-core/src/main/java/com/starrocks/alter/SchemaChangeHandler.java
@@ -63,6 +63,7 @@ import com.starrocks.catalog.OlapTable.OlapTableState;
 import com.starrocks.catalog.Partition;
 import com.starrocks.catalog.PhysicalPartition;
 import com.starrocks.catalog.Replica;
+import com.starrocks.catalog.SchemaChangeTypeCompatibility;
 import com.starrocks.catalog.SchemaInfo;
 import com.starrocks.catalog.StructField;
 import com.starrocks.catalog.StructType;
@@ -891,6 +892,19 @@ public class SchemaChangeHandler extends AlterHandler {
                 || oriColumn.getType().isDecimalOfAnyVersion()) {
             fastSchemaEvolution = false;
         }
+<<<<<<< HEAD
+=======
+        for (Column column : otherIndexModifiedColumn) {
+            if (column.isKey()) {
+                return false;
+            }
+        }
+
+        if (!SchemaChangeTypeCompatibility.canReuseZonemapIndex(oriColumn.getType(), modColumn.getType())) {
+            fastSchemaEvolution = false;
+        }
+
+>>>>>>> b9fdfcd6c0 ([BugFix] Fix incompatible zonemap reuse for fast schema evolution in shared-data (#63143))
         return fastSchemaEvolution;
     }
 
