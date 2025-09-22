StarRocks/starrocks

commit 16af4ce6ab6c96c1fc5bee1084a70c9984cc8710

[BugFix] Fix auto increment value lost when partial update in COLUMN_UPSERT_MODE for share nothing (#61341)
Signed-off-by: srlch <linzichao@starrocks.com>

be/src/storage/rowset_column_update_state.cpp
@@ -688,7 +688,22 @@ Status RowsetColumnUpdateState::finalize(Tablet* tablet, Rowset* rowset, uint32_
     std::vector<ColumnUID> update_column_uids;
     std::vector<ColumnUID> unique_update_column_ids;
     const auto& tschema = rowset->schema();
+    /* 
+      * skip overwrite the auto increment column using data in .upt files if user partially update it for the existed keys
+
+      * In current implementation, if user partially update the auto increment column, it will also be included in partial schema
+      * in writing phrase. Because we need to allocate the id in this phrase. It means that .upt files will contains auto increment
+      * column data even it is partially updated (does not specfied by user).
+      * 
+      * For the keys which have already existed in the tablet, we will write "0" in .upt file. In the apply phrase, such "0" data is not
+      * used and we need to discard the column for the keys which have already existed in the tablet.
+    */
     for (ColumnId cid : txn_meta.partial_update_column_ids()) {
+        if (txn_meta.has_auto_increment_partial_update_column_id() &&
+            cid == txn_meta.auto_increment_partial_update_column_id()) {
+            // skip auto increment column if it is being used for partial update
+            continue;
+        }
         if (cid >= tschema->num_key_columns()) {
             update_column_ids.push_back(cid);
             update_column_uids.push_back((ColumnUID)cid);
@@ -702,7 +717,12 @@ Status RowsetColumnUpdateState::finalize(Tablet* tablet, Rowset* rowset, uint32_
             LOG(ERROR) << msg;
             return Status::InternalError(msg);
         }
-        if (!tschema->column(cid).is_key()) {
+        const auto& column = tschema->column(cid);
+        if (txn_meta.has_auto_increment_partial_update_column_id() && column.is_auto_increment()) {
+            // skip auto increment column if it is being used for partial update
+            continue;
+        }
+        if (!column.is_key()) {
             unique_update_column_ids.push_back(uid);
         }
     }

test/sql/test_auto_increment/R/test_auto_increment
@@ -741,3 +741,62 @@ SELECT * FROM t_auto_increment_insert_partial_update order by id;
 DROP TABLE t_auto_increment_insert_partial_update force;
 -- result:
 -- !result
+-- name: test_auto_increment_partial_update_column_upsert @sequential
+CREATE DATABASE test_auto_increment_partial_update_column_upsert;
+-- result:
+-- !result
+ADMIN SET FRONTEND CONFIG ("auto_increment_cache_size" = "0");
+USE test_auto_increment_partial_update_column_upsert;
+-- result:
+-- !result
+CREATE TABLE `t_auto_increment_partial_update_column_upsert` (
+  `k`  BIGINT NOT NULL COMMENT "",
+  `v1` BIGINT AUTO_INCREMENT,
+  `v2` BIGINT,
+  `v3` BIGINT
+) ENGINE=OLAP 
+PRIMARY KEY(`k`)
+DISTRIBUTED BY HASH(`k`) BUCKETS 1
+PROPERTIES (
+"replicated_storage" = "true",
+"replication_num" = "1"
+);
+-- result:
+-- !result
+INSERT INTO t_auto_increment_partial_update_column_upsert VALUES (1, DEFAULT, 3, 4);
+-- result:
+-- !result
+SELECT * FROM t_auto_increment_partial_update_column_upsert;
+-- result:
+1	1	3	4
+-- !result
+shell: curl --location-trusted -u root: -T ${root_path}/lib/../common/data/stream_load/sr_auto_increment_partial_update_only.csv -XPUT -H partial_update:true  -H partial_update_mode:column -H label:test_auto_increment_partial_update_column_upsert_12345 -H column_separator:, -H columns:k,v2,xx ${url}/api/test_auto_increment_partial_update_column_upsert/t_auto_increment_partial_update_column_upsert/_stream_load
+-- result:
+0
+{
+    "Status": "Success",
+    "Message": "OK"
+}
+-- !result
+sync;
+-- result:
+-- !result
+SELECT * FROM t_auto_increment_partial_update_column_upsert;
+-- result:
+1	1	20	4
+2	2	40	None
+-- !result
+INSERT INTO t_auto_increment_partial_update_column_upsert VALUES (1, 300, 20, 30), (2, 301, 40 ,50);
+-- result:
+-- !result
+SELECT * FROM t_auto_increment_partial_update_column_upsert;
+-- result:
+1	300	20	30
+2	301	40	50
+-- !result
+DROP TABLE t_auto_increment_partial_update_column_upsert;
+-- result:
+-- !result
+DROP DATABASE test_auto_increment_partial_update_column_upsert;
+-- result:
+-- !result

test/sql/test_auto_increment/T/test_auto_increment
@@ -325,3 +325,33 @@ insert into t_auto_increment_insert_partial_update (ts, testString) select 100, 
 insert into t_auto_increment_insert_partial_update (ts, testString) select 100, "abc";
 SELECT * FROM t_auto_increment_insert_partial_update order by id;
 DROP TABLE t_auto_increment_insert_partial_update force;
+
+-- name: test_auto_increment_partial_update_column_upsert @sequential
+CREATE DATABASE test_auto_increment_partial_update_column_upsert;
+ADMIN SET FRONTEND CONFIG ("auto_increment_cache_size" = "0");
+USE test_auto_increment_partial_update_column_upsert;
+CREATE TABLE `t_auto_increment_partial_update_column_upsert` (
+  `k`  BIGINT NOT NULL COMMENT "",
+  `v1` BIGINT AUTO_INCREMENT,
+  `v2` BIGINT,
+  `v3` BIGINT
+) ENGINE=OLAP 
+PRIMARY KEY(`k`)
+DISTRIBUTED BY HASH(`k`) BUCKETS 1
+PROPERTIES (
+"replicated_storage" = "true",
+"replication_num" = "1"
+);
+
+INSERT INTO t_auto_increment_partial_update_column_upsert VALUES (1, DEFAULT, 3, 4);
+SELECT * FROM t_auto_increment_partial_update_column_upsert;
+
+shell: curl --location-trusted -u root: -T ${root_path}/lib/../common/data/stream_load/sr_auto_increment_partial_update_only.csv -XPUT -H partial_update:true  -H partial_update_mode:column -H label:test_auto_increment_partial_update_column_upsert_12345 -H column_separator:, -H columns:k,v2,xx ${url}/api/test_auto_increment_partial_update_column_upsert/t_auto_increment_partial_update_column_upsert/_stream_load
+sync;
+
+SELECT * FROM t_auto_increment_partial_update_column_upsert;
+INSERT INTO t_auto_increment_partial_update_column_upsert VALUES (1, 300, 20, 30), (2, 301, 40 ,50);
+SELECT * FROM t_auto_increment_partial_update_column_upsert;
+
+DROP TABLE t_auto_increment_partial_update_column_upsert;
+DROP DATABASE test_auto_increment_partial_update_column_upsert;

commit be3784b08acd62a95cb507dff0ec78d871ed5ad3

[BugFix] Fix dcg meta inconsistency when partial update with auto increment column in column upsert mode.
## Why I'm doing:
This bug was introduced by the pr #61341 which is fix 0 value of auto increment column in column upsert mode.
But it handle the cid incorrectly when it used to filter out auto increment column just before generate the cols file.
In pr #61341 it use txn_meta.auto_increment_partial_update_column_id() to check if the column is auto increment column.
But it is incorrect because the txn_meta.auto_increment_partial_update_column_id() means the column offset in partial segment
schema but txn_meta.partial_update_column_ids() means the column offset in full tablet schema.

## What I'm doing:
Use TabletColumn::is_auto_increment() to check if the column is auto increment column instead.

Signed-off-by: srlch <linzichao@starrocks.com>

be/src/storage/rowset_column_update_state.cpp
@@ -701,7 +701,7 @@ Status RowsetColumnUpdateState::finalize(Tablet* tablet, Rowset* rowset, uint32_
     */
     for (ColumnId cid : txn_meta.partial_update_column_ids()) {
         if (txn_meta.has_auto_increment_partial_update_column_id() &&
-            cid == txn_meta.auto_increment_partial_update_column_id()) {
+            tschema->column(cid).is_auto_increment()) {
             // skip auto increment column if it is being used for partial update
             continue;
         }

test/sql/test_auto_increment/R/test_auto_increment
@@ -834,3 +834,63 @@ SELECT * FROM t_alter_auto_increment_counter ORDER BY k;
 DROP TABLE t_alter_auto_increment_counter;
 -- result:
 -- !result
+-- name: test_auto_increment_partial_update_column_upsert_2 @sequential
+CREATE DATABASE test_auto_increment_partial_update_column_upsert_2;
+-- result:
+-- !result
+ADMIN SET FRONTEND CONFIG ("auto_increment_cache_size" = "0");
+USE test_auto_increment_partial_update_column_upsert_2;
+-- result:
+-- !result
+CREATE TABLE `t_auto_increment_partial_update_column_upsert_2` (
+  `k`  BIGINT NOT NULL COMMENT "",
+  `v1` BIGINT,
+  `v2` BIGINT,
+  `v3` BIGINT AUTO_INCREMENT,
+  `v4` BIGINT
+) ENGINE=OLAP 
+PRIMARY KEY(`k`)
+DISTRIBUTED BY HASH(`k`) BUCKETS 1
+PROPERTIES (
+"replicated_storage" = "true",
+"replication_num" = "1"
+);
+-- result:
+-- !result
+INSERT INTO t_auto_increment_partial_update_column_upsert_2 VALUES (1, 2, 3, DEFAULT, 4);
+-- result:
+-- !result
+SELECT * FROM t_auto_increment_partial_update_column_upsert_2;
+-- result:
+1	2 3	1	4
+-- !result
+shell: curl --location-trusted -u root: -T ${root_path}/lib/../common/data/stream_load/sr_auto_increment_partial_update_only.csv -XPUT -H partial_update:true  -H partial_update_mode:column -H label:test_auto_increment_partial_update_column_upsert_12345 -H column_separator:, -H columns:k,v2,xx ${url}/api/test_auto_increment_partial_update_column_upsert_2/t_auto_increment_partial_update_column_upsert_2/_stream_load
+-- result:
+0
+{
+    "Status": "Success",
+    "Message": "OK"
+}
+-- !result
+sync;
+-- result:
+-- !result
+SELECT * FROM t_auto_increment_partial_update_column_upsert_2;
+-- result:
+1	1	20	1 4
+2	None	40	2 None
+-- !result
+INSERT INTO t_auto_increment_partial_update_column_upsert_2 VALUES (1, 300, 20, DEFAULT, 30), (2, 301, 40, DEFAULT, 50);
+-- result:
+-- !result
+SELECT * FROM t_auto_increment_partial_update_column_upsert_2;
+-- result:
+1	300	20	3 30
+2	301	40	4 50
+-- !result
+DROP TABLE t_auto_increment_partial_update_column_upsert_2;
+-- result:
+-- !result
+DROP DATABASE test_auto_increment_partial_update_column_upsert_2;
+-- result:
+-- !result

test/sql/test_auto_increment/T/test_auto_increment
@@ -373,3 +373,34 @@ ALTER TABLE t_alter_auto_increment_counter AUTO_INCREMENT = 300;
 INSERT INTO t_alter_auto_increment_counter VALUES (3, DEFAULT);
 SELECT * FROM t_alter_auto_increment_counter ORDER BY k;
 DROP TABLE t_alter_auto_increment_counter;
+
+-- name: test_auto_increment_partial_update_column_upsert_2 @sequential
+CREATE DATABASE test_auto_increment_partial_update_column_upsert_2;
+ADMIN SET FRONTEND CONFIG ("auto_increment_cache_size" = "0");
+USE test_auto_increment_partial_update_column_upsert_2;
+CREATE TABLE `t_auto_increment_partial_update_column_upsert_2` (
+  `k`  BIGINT NOT NULL COMMENT "",
+  `v1` BIGINT,
+  `v2` BIGINT,
+  `v3` BIGINT AUTO_INCREMENT,
+  `v4` BIGINT
+) ENGINE=OLAP 
+PRIMARY KEY(`k`)
+DISTRIBUTED BY HASH(`k`) BUCKETS 1
+PROPERTIES (
+"replicated_storage" = "true",
+"replication_num" = "1"
+);
+
+INSERT INTO t_auto_increment_partial_update_column_upsert_2 VALUES (1, 2, 3, DEFAULT, 4);
+SELECT * FROM t_auto_increment_partial_update_column_upsert_2;
+
+shell: curl --location-trusted -u root: -T ${root_path}/lib/../common/data/stream_load/sr_auto_increment_partial_update_only.csv -XPUT -H partial_update:true  -H partial_update_mode:column -H label:test_auto_increment_partial_update_column_upsert_12345 -H column_separator:, -H columns:k,v2,xx ${url}/api/test_auto_increment_partial_update_column_upsert_2/t_auto_increment_partial_update_column_upsert_2/_stream_load
+sync;
+
+SELECT * FROM t_auto_increment_partial_update_column_upsert_2;
+INSERT INTO t_auto_increment_partial_update_column_upsert_2 VALUES (1, 300, 20, DEFAULT, 30), (2, 301, 40, DEFAULT, 50);
+SELECT * FROM t_auto_increment_partial_update_column_upsert_2;
+
+DROP TABLE t_auto_increment_partial_update_column_upsert_2;
+DROP DATABASE test_auto_increment_partial_update_column_upsert_2;