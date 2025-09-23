4a46af5ffbfd3793b39a142d4d8e43f814426afd
chewbum <111036673+chewbum@users.noreply.github.com>
2025-09-23 16:09:35 +0800

    [BugFix] Call isQueryStmt in execute function (#63387)

--- fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
@@ -657,7 +657,7 @@ public class StmtExecutor {
         }
 
         try {
-            context.getState().setIsQuery(parsedStmt instanceof QueryStatement);
+            context.getState().setIsQuery(context.isQueryStmt(parsedStmt));
             if (parsedStmt.isExistQueryScopeHint()) {
                 processQueryScopeHint();
             }
