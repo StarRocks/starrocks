59eccac3acac822a62711b4725d549d0486316c1
before-Sunrise <unclejyj@gmail.com>
2025-09-24 13:52:25 +0800

    fix ut
    Signed-off-by: before-Sunrise <unclejyj@gmail.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/PredicatePushDownTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/PredicatePushDownTest.java
@@ -112,37 +112,6 @@ public class PredicatePushDownTest extends PlanTestBase {
             assertContains(plan, "<slot 5> : coalesce((3: v3 >= 1) AND (3: v3 <= 3), FALSE)");
         }
     }
-<<<<<<< HEAD
-=======
-
-    @Test
-    public void testRangeExtract() throws Exception {
-        {
-            String sql = "select * from t0 where (v1 < 3 and v1 > 2) and v2 > v1";
-            String plan = getFragmentPlan(sql);
-            assertContains(plan, "2: v2 > 2");
-        }
-        {
-            String sql = "select * from t0 where (v1 < 3 and v1 > 2) and v2 >= v1";
-            String plan = getFragmentPlan(sql);
-            assertContains(plan, "2: v2 > 2");
-        }
-        {
-            String sql = "select * from t0 where (v1 <= 3 and v1 >= 2) and v2 > v1";
-            String plan = getFragmentPlan(sql);
-            assertContains(plan, "2: v2 > 2");
-        }
-        {
-            String sql = "select * from t0 where (v1 <= 3 and v1 >= 2) and v2 >= v1";
-            String plan = getFragmentPlan(sql);
-            assertContains(plan, "2: v2 >= 2");
-        }
-        {
-            String sql = "select * from t0 join t1 where v1 <= 3 and v4 = v1 and v5 < v1";
-            String plan = getFragmentPlan(sql);
-            assertContains(plan, "5: v5 < 3");
-        }
-    }
 
     @Test
     public void testNonDeterministicFunctionInCTE() throws Exception {
@@ -169,5 +138,4 @@ public class PredicatePushDownTest extends PlanTestBase {
                 "  |  \n" +
                 "  0:OlapScanNode");
     }
->>>>>>> e0ce1d4b42 ([BugFix] fix non-deterministic predicate push down problem (#62827))
 }
