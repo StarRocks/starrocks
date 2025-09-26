ee93eaef3987f3d67ecf5843447936a0cd85b771
Yixin Luo <18810541851@163.com>
2025-09-26 20:13:16 +0800

    [Enhancement] Improve delvec file cleanup logic to properly handle pk index references (#63384)
    Signed-off-by: luohaha <18810541851@163.com>

--- be/src/storage/lake/compaction_task.cpp
+++ be/src/storage/lake/compaction_task.cpp
@@ -39,7 +39,7 @@ Status CompactionTask::execute_index_major_compaction(TxnLogPB* txn_log) {
         auto metadata = _tablet.metadata();
         if (metadata->enable_persistent_index() &&
             metadata->persistent_index_type() == PersistentIndexTypePB::CLOUD_NATIVE) {
-            RETURN_IF_ERROR(_tablet.tablet_manager()->update_mgr()->execute_index_major_compaction(*metadata, txn_log));
+            RETURN_IF_ERROR(_tablet.tablet_manager()->update_mgr()->execute_index_major_compaction(metadata, txn_log));
             if (txn_log->has_op_compaction() && !txn_log->op_compaction().input_sstables().empty()) {
                 size_t total_input_sstable_file_size = 0;
                 for (const auto& input_sstable : txn_log->op_compaction().input_sstables()) {
