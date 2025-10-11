bc1e3489dc472a585319c928d266ccb5cfb87203
shuming.li <ming.moriarty@gmail.com>
2025-10-11 10:04:35 +0800

    [BugFix] Fix agg push down type cast bug (#63875)
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>
    (cherry picked from commit d0b7c2226fdcd6d7f2f4deac198e369e1e1c4dcd)

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/materialization/AggregatedMaterializedViewPushDownRewriter.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/materialization/AggregatedMaterializedViewPushDownRewriter.java
@@ -468,8 +468,9 @@ public final class AggregatedMaterializedViewPushDownRewriter extends Materializ
                 remapping.put(aggColRef, newColRef);
                 ctx.aggColRefToPushDownAggMap.put(aggColRef, aggCall);
             }
-            Map<ColumnRefOperator, CallOperator> newAggregations = Maps.newHashMap();
-            uniqueAggregations.forEach((k, v) -> newAggregations.put(v, k));
+            Map<ColumnRefOperator, CallOperator> newAggregations = uniqueAggregations.entrySet().stream()
+                            .map(e -> Maps.immutableEntry(e.getValue(), e.getKey()))
+                            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
             LogicalAggregationOperator newAggOp = LogicalAggregationOperator.builder()
                     .setAggregations(newAggregations)
                     .setType(AggType.GLOBAL)
