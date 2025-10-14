0ff8217aa9741482b68659d789d8af5065334846
satanson <ranpanf@gmail.com>
2025-10-14 13:58:58 +0800

    [BugFix] window function with ignore-nulls-flag can not be consolidated with its counterpart without ignore-nulls-flag (#63958)
    Signed-off-by: satanson <ranpanf@gmail.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/WindowTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/WindowTest.java
@@ -15,11 +15,15 @@
 package com.starrocks.sql.plan;
 
 import com.starrocks.common.FeConstants;
+import com.starrocks.planner.AnalyticEvalNode;
 import com.starrocks.sql.analyzer.SemanticException;
 import com.starrocks.utframe.StarRocksAssert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -1683,19 +1687,26 @@ public class WindowTest extends PlanTestBase {
                 " last_value(a1 ignore nulls) over(partition by v1 order by v2)\n" +
                 "from s1;";
         String plan = getVerboseExplain(sql);
+        System.out.println(plan);
         Assertions.assertTrue(plan.contains("  4:ANALYTIC\n" +
                 "  |  functions: [, first_value[([3: a1, ARRAY<VARCHAR(65533)>, true]); " +
                 "args: INVALID_TYPE; result: ARRAY<VARCHAR>; args nullable: true; result nullable: true], ], " +
-                "[, last_value[([3: a1, ARRAY<VARCHAR(65533)>, true]); " +
-                "args: INVALID_TYPE; result: ARRAY<VARCHAR>; args nullable: true; result nullable: true], ]\n" +
+                "[, last_value[([3: a1, ARRAY<VARCHAR(65533)>, true]); args: INVALID_TYPE; result: ARRAY<VARCHAR>;" +
+                " args nullable: true; result nullable: true], ], " +
+                "[, first_value[([3: a1, ARRAY<VARCHAR(65533)>, true]); args: INVALID_TYPE; " +
+                "result: ARRAY<VARCHAR>; args nullable: true; result nullable: true], ], " +
+                "[, last_value[([3: a1, ARRAY<VARCHAR(65533)>, true]); args: INVALID_TYPE; " +
+                "result: ARRAY<VARCHAR>; args nullable: true; result nullable: true], ]\n" +
                 "  |  partition by: [1: v1, BIGINT, true]\n" +
                 "  |  order by: [2: v2, INT, true] ASC\n" +
                 "  |  window: ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW\n" +
                 "  |  cardinality: 1\n" +
                 "  |  \n" +
                 "  3:ANALYTIC\n" +
-                "  |  functions: [, lag[([3: a1, ARRAY<VARCHAR(65533)>, true], 1, NULL);" +
-                " args: INVALID_TYPE; result: ARRAY<VARCHAR>; args nullable: true; result nullable: true], ]\n" +
+                "  |  functions: [, lag[([3: a1, ARRAY<VARCHAR(65533)>, true], 1, NULL); args: INVALID_TYPE; " +
+                "result: ARRAY<VARCHAR>; args nullable: true; result nullable: true], ], " +
+                "[, lag[([3: a1, ARRAY<VARCHAR(65533)>, true], 1, NULL); args: INVALID_TYPE; " +
+                "result: ARRAY<VARCHAR>; args nullable: true; result nullable: true], ]\n" +
                 "  |  partition by: [1: v1, BIGINT, true]\n" +
                 "  |  order by: [2: v2, INT, true] ASC\n" +
                 "  |  window: ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING\n" +
@@ -1703,10 +1714,44 @@ public class WindowTest extends PlanTestBase {
                 "  |  \n" +
                 "  2:ANALYTIC\n" +
                 "  |  functions: [, lead[([3: a1, ARRAY<VARCHAR(65533)>, true], 1, NULL); " +
+                "args: INVALID_TYPE; result: ARRAY<VARCHAR>; args nullable: true; result nullable: true], ], " +
+                "[, lead[([3: a1, ARRAY<VARCHAR(65533)>, true], 1, NULL); " +
                 "args: INVALID_TYPE; result: ARRAY<VARCHAR>; args nullable: true; result nullable: true], ]\n" +
                 "  |  partition by: [1: v1, BIGINT, true]\n" +
                 "  |  order by: [2: v2, INT, true] ASC\n" +
                 "  |  window: ROWS BETWEEN UNBOUNDED PRECEDING AND 1 FOLLOWING\n" +
                 "  |  cardinality: 1"));
+    }
+
+    @Test
+    public void testFirstValueIgnoreNullsAndFirstValueMerging() throws Exception {
+
+        String sql = "select  \n" +
+                "  v1, v2,\n" +
+                "  first_value(v2 ignore nulls) over(order by v1, v2),\n" +
+                "  first_value(v2) over(order by v1, v2)\n" +
+                "from s1;";
+
+        ExecPlan execPlan = getExecPlan(sql);
+        List<AnalyticEvalNode> analyticNodes = new ArrayList<>();
+        execPlan.getTopFragment().getPlanRoot().collect(AnalyticEvalNode.class, analyticNodes);
+        Assertions.assertFalse(analyticNodes.isEmpty());
+        Assertions.assertEquals(analyticNodes.get(0).getAnalyticFnCalls().size(), 2);
+        Assertions.assertTrue(analyticNodes.get(0).getAnalyticFnCalls().get(0).getIgnoreNulls());
+        Assertions.assertFalse(analyticNodes.get(0).getAnalyticFnCalls().get(1).getIgnoreNulls());
+    }
+
+    @Test
+    public void testFirstValueValueRange() throws Exception {
+
+        String sql = "select v1,v2,v3,\n" +
+                "      first_value(v3 ignore nulls) \n" +
+                "      \tover(partition by v1 order by v2 range between UNBOUNDED preceding and current row)\n" +
+                "from t0;";
+
+        ExecPlan execPlan = getExecPlan(sql);
+        List<AnalyticEvalNode> analyticNodes = new ArrayList<>();
+        execPlan.getTopFragment().getPlanRoot().collect(AnalyticEvalNode.class, analyticNodes);
+        Assertions.assertFalse(analyticNodes.isEmpty());
     }
 }
