8dd787349d6abe4a8f132ee34a065d7b780866bd
zihe.liu <ziheliu1024@gmail.com>
2025-10-14 16:47:14 +0800

    [UT] Disable unstable UT (#63985)
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>
    (cherry picked from commit 6008fcc33d2209a5f008b743e7befe38d5718579)
    
    # Conflicts:
    #	fe/fe-core/src/test/java/com/starrocks/http/QueryDumpActionTest.java
    #	test/sql/test_iceberg/R/test_iceberg_parse_predicates_2
    #	test/sql/test_iceberg/T/test_iceberg_parse_predicates_2

--- fe/fe-core/src/test/java/com/starrocks/qe/scheduler/QueryQueueManagerTest.java
+++ fe/fe-core/src/test/java/com/starrocks/qe/scheduler/QueryQueueManagerTest.java
@@ -654,6 +654,7 @@ public class QueryQueueManagerTest extends SchedulerTestBase {
                 .until(() -> GlobalStateMgr.getCurrentState().getSlotManager().getSlots().isEmpty());
     }
 
+    @Disabled
     @Test
     public void testAllocatedSlotTimeout() throws Exception {
         final int concurrencyLimit = 3;
