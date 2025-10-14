e048d50745533843d9445ac1629542b89eec81cc
Youngwb <yangwenbo_mailbox@163.com>
2025-10-14 15:16:57 +0800

    [Enhancement] bump iceberg to 1.10.0 for gradle (#64000)

--- fe/build.gradle.kts
+++ fe/build.gradle.kts
@@ -58,7 +58,7 @@ subprojects {
         set("hikaricp.version", "3.4.5")
         set("hive-apache.version", "3.1.2-22")
         set("hudi.version", "1.0.2")
-        set("iceberg.version", "1.9.0")
+        set("iceberg.version", "1.10.0")
         set("io.netty.version", "4.1.125.Final")
         set("jackson.version", "2.15.2")
         set("jetty.version", "9.4.57.v20241219")
