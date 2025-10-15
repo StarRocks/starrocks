91b1b71dbeb2b3c3cde25abeda31f593a30ba00d
He Zhao <luoyedeyi@163.com>
2025-10-15 17:27:39 +0800

    [Enhancement] Implement  BackendActionV2 API to obtain all backend information  (#63444)
    Signed-off-by: zhaohehuhu <luoyedeyi@163.com>
    (cherry picked from commit 1a70f267223f7e0352fc8ea8f1f2e26e6ce82076)

---
+++ fe/fe-core/src/test/java/com/starrocks/http/rest/v2/ComputeNodeActionV2Test.java
@@ -0,0 +1,78 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.starrocks.http.rest.v2;
+
+import com.google.gson.reflect.TypeToken;
+import com.starrocks.http.StarRocksHttpTestCase;
+import com.starrocks.http.rest.v2.vo.BackendNodeInfo;
+import com.starrocks.http.rest.v2.vo.ComputeNodeInfo;
+import com.starrocks.persist.gson.GsonUtils;
+import com.starrocks.server.GlobalStateMgr;
+import com.starrocks.system.ComputeNode;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ComputeNodeActionV2Test extends StarRocksHttpTestCase {
+
+    private static final String QUERY_PLAN_URI = "/api/v2/computeNode";
+
+    @Override
+    protected void doSetUp() throws Exception {
+        assignCN();
+    }
+
+    @Test
+    public void testGetComputeNode() throws IOException {
+        Request request = new Request.Builder()
+                .get()
+                .addHeader("Authorization", rootAuth)
+                .url("http://localhost:" + HTTP_PORT + QUERY_PLAN_URI)
+                .build();
+        Response response = networkClient.newCall(request).execute();
+        String respStr = response.body().string();
+        RestBaseResultV2<List<ComputeNodeInfo>> resp = parseResponseBody(respStr);
+        List<ComputeNodeInfo> computeNodeInfos = resp.getResult();
+        Assertions.assertEquals(200, response.code());
+        Assertions.assertEquals(2, computeNodeInfos.size());
+    }
+
+    private static RestBaseResultV2<List<ComputeNodeInfo>> parseResponseBody(String body) {
+        try {
+            return GsonUtils.GSON.fromJson(
+                    body,
+                    new TypeToken<RestBaseResultV2<List<BackendNodeInfo>>>() {
+                    }.getType());
+        } catch (Exception e) {
+            fail(e.getMessage() + ", resp: " + body);
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private static void assignCN() {
+        ComputeNode cn1 = new ComputeNode(10001, "host1", 1003);
+        cn1.setAlive(true);
+        ComputeNode cn2 = new ComputeNode(10002, "host2", 1004);
+        cn2.setAlive(true);
+        GlobalStateMgr.getCurrentState().getNodeMgr().getClusterInfo().addComputeNode(cn1);
+        GlobalStateMgr.getCurrentState().getNodeMgr().getClusterInfo().addComputeNode(cn2);
+    }
+}
