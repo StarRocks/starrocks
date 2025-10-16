276b78463d0618c2250a9e0d1b4dd1cc795aa149
silverbullet233 <3675229+silverbullet233@users.noreply.github.com>
2025-10-16 16:53:07 +0800

    fix
    Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>

--- fe/fe-core/src/main/java/com/starrocks/planner/PlanFragment.java
+++ fe/fe-core/src/main/java/com/starrocks/planner/PlanFragment.java
@@ -652,7 +652,6 @@ public class PlanFragment extends TreeNode<PlanFragment> {
     }
 
     public PlanFragment getDestFragment() {
-        // @TODO what is the dest fragment of lookup fragment
         if (destNode == null) {
             return null;
         }
