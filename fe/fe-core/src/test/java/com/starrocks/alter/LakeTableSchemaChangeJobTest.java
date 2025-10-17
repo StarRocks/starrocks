12215c0af3496c4a3ae8c966942c353f7cd96a84
PengFei Li <lpengfei2016@gmail.com>
2025-10-17 11:19:19 +0800

    fix unstable test
    Signed-off-by: PengFei Li <lpengfei2016@gmail.com>

--- fe/fe-core/src/test/java/com/starrocks/alter/LakeTableSchemaChangeJobTest.java
+++ fe/fe-core/src/test/java/com/starrocks/alter/LakeTableSchemaChangeJobTest.java
@@ -68,6 +68,16 @@ public class LakeTableSchemaChangeJobTest {
     public static void setUp() throws Exception {
         UtFrameUtils.createMinStarRocksCluster(RunMode.SHARED_DATA);
         connectContext = UtFrameUtils.createDefaultCtx();
+        // Schema change job is executed manually, so stop the schema change daemon to avoid interfering with the test.
+        SchemaChangeHandler schemaChangeHandler = GlobalStateMgr.getCurrentState().getAlterJobMgr().getSchemaChangeHandler();
+        schemaChangeHandler.setStop();
+        schemaChangeHandler.interrupt();
+        long startTime = System.currentTimeMillis();
+        while (schemaChangeHandler.isRunning()) {
+            Thread.sleep(100);
+            Assertions.assertTrue(System.currentTimeMillis() - startTime < 60000,
+                    "Schema change handler is not stopped in 60 seconds");
+        }
     }
 
     private static LakeTable createTable(ConnectContext connectContext, String sql) throws Exception {
