55355862ced4b0fce0fcaf92f7d1d2049d78d682
Xingyuan Lin <x.lin@celonis.com>
2025-10-17 03:37:30 -0400

    [Feature] UI: Add functions to analyze profile and view formatted SQL (#63867)

--- fe/fe-core/src/main/java/com/starrocks/http/action/QueryProfileAction.java
+++ fe/fe-core/src/main/java/com/starrocks/http/action/QueryProfileAction.java
@@ -34,12 +34,16 @@
 
 package com.starrocks.http.action;
 
+import com.github.vertical_blank.sqlformatter.SqlFormatter;
 import com.google.common.base.Strings;
+import com.starrocks.common.util.CompressionUtils;
 import com.starrocks.common.util.ProfileManager;
+import com.starrocks.common.util.RuntimeProfileParser;
 import com.starrocks.http.ActionController;
 import com.starrocks.http.BaseRequest;
 import com.starrocks.http.BaseResponse;
 import com.starrocks.http.IllegalArgException;
+import com.starrocks.sql.ExplainAnalyzer;
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import org.apache.logging.log4j.LogManager;
@@ -49,10 +53,17 @@ import org.owasp.encoder.Encode;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.Collections;
+
+import static com.github.vertical_blank.sqlformatter.languages.Dialect.MySql;
+import static com.github.vertical_blank.sqlformatter.languages.Dialect.StandardSql;
 
 public class QueryProfileAction extends WebBaseAction {
 
     private static final Logger LOG = LogManager.getLogger(QueryProfileAction.class);
+    private static final String ANSI_REGEX = "\\u001B\\[[;?0-9]*[a-zA-Z]";
+    private static final SqlFormatter.Formatter MYSQL_FORMATTER = SqlFormatter.of(MySql);
+    private static final SqlFormatter.Formatter STANDARDSQL_FORMATTER = SqlFormatter.of(StandardSql);
 
     public QueryProfileAction(ActionController controller) {
         super(controller);
@@ -74,25 +85,65 @@ public class QueryProfileAction extends WebBaseAction {
             return;
         }
 
-        // HTML encode the queryId to prevent XSS
-        String encodedQueryId = Encode.forHtml(queryId);
-        String queryProfileStr = ProfileManager.getInstance().getProfile(queryId);
-        if (queryProfileStr != null) {
-            appendCopyButton(response.getContent());
-            appendQueryProfile(response.getContent(), queryProfileStr);
+        ProfileManager.ProfileElement queryProfile = ProfileManager.getInstance().getProfileElement(queryId);
+        if (queryProfile != null) {
+            String content;
+            String contentType = request.getSingleParameter("content_type");
+            if ("sql".equalsIgnoreCase(contentType)) {
+                content = getFormattedSql(queryProfile);
+            } else if ("analyze".equalsIgnoreCase(contentType)) {
+                content = getAnalyzeProfileResult(queryProfile);
+            } else {
+                // Profile String
+                content = ProfileManager.getInstance().getProfile(queryId);
+                if (content == null) {
+                    content = String.format("Failed to decompress profile content of %s\n", queryId);
+                }
+            }
+            appendButtons(response.getContent());
+            appendContent(response.getContent(), content);
             getPageFooter(response.getContent());
             writeResponse(request, response);
         } else {
-            appendQueryProfile(response.getContent(), "query id " + encodedQueryId + " not found.");
+            // HTML encode the queryId to prevent XSS
+            String encodedQueryId = Encode.forHtml(queryId);
+            appendContent(response.getContent(), "query id " + encodedQueryId + " not found.");
             getPageFooter(response.getContent());
             writeResponse(request, response, HttpResponseStatus.NOT_FOUND);
         }
     }
 
-    private void appendQueryProfile(StringBuilder buffer, String queryProfileStr) {
+    // com.github.vertical_blank.sqlformatter provides best-effort formatting. It doesn't  throw exceptions for malformed or
+    // invalid SQL syntax.
+    private String getFormattedSql(ProfileManager.ProfileElement queryProfile) {
+        String sqlStatement = queryProfile.infoStrings.getOrDefault(ProfileManager.SQL_STATEMENT, "");
+        String formattedSql;
+        if ("StarRocks".equalsIgnoreCase(queryProfile.infoStrings.get(ProfileManager.SQL_DIALECT))) {
+            formattedSql = MYSQL_FORMATTER.format(sqlStatement);
+        } else {
+            formattedSql = STANDARDSQL_FORMATTER.format(sqlStatement);
+        }
+        return formattedSql;
+    }
+
+    private String getAnalyzeProfileResult(ProfileManager.ProfileElement queryProfile) {
+        String analysis;
+        try {
+            analysis = ExplainAnalyzer.analyze(queryProfile.plan, RuntimeProfileParser.parseFrom(
+                    CompressionUtils.gzipDecompressString(queryProfile.profileContent)), Collections.emptyList(), false);
+            analysis = analysis.replaceAll(ANSI_REGEX, "");
+        } catch (Exception e) {
+            String queryId = queryProfile.infoStrings.getOrDefault(ProfileManager.QUERY_ID, "unknown");
+            LOG.warn("Failed to analyze profile of {}", queryId, e);
+            analysis = String.format("Failed to analyze profile of %s\n%s\n", queryId, e);
+        }
+        return analysis;
+    }
+
+    private void appendContent(StringBuilder buffer, String content) {
         buffer.append("<pre id='profile'>");
 
-        BufferedReader reader = new BufferedReader(new StringReader(queryProfileStr));
+        BufferedReader reader = new BufferedReader(new StringReader(content));
         String line;
         try {
             while ((line = reader.readLine()) != null) {
@@ -109,9 +160,27 @@ public class QueryProfileAction extends WebBaseAction {
         buffer.append("</pre>");
     }
 
-    private void appendCopyButton(StringBuilder buffer) {
+    private void appendButtons(StringBuilder buffer) {
         buffer.append("<script type=\"text/javascript\">\n" +
-                "function copyProfile(){\n" +
+                "function viewProfile() {\n" +
+                "  const params = new URLSearchParams(window.location.search);\n" +
+                "  const query_id = params.get('query_id');\n" +
+                "  window.location.href = '/query_profile?query_id=' + query_id + '&content_type=profile';\n" +
+                "}" + "</script>");
+        buffer.append("<script type=\"text/javascript\">\n" +
+                "function formattedSql() {\n" +
+                "  const params = new URLSearchParams(window.location.search);\n" +
+                "  const query_id = params.get('query_id');\n" +
+                "  window.location.href = '/query_profile?query_id=' + query_id + '&content_type=sql';\n" +
+                "}" + "</script>");
+        buffer.append("<script type=\"text/javascript\">\n" +
+                "function analyzeProfile() {\n" +
+                "  const params = new URLSearchParams(window.location.search);\n" +
+                "  const query_id = params.get('query_id');\n" +
+                "  window.location.href = '/query_profile?query_id=' + query_id + '&content_type=analyze';\n" +
+                "}" + "</script>");
+        buffer.append("<script type=\"text/javascript\">\n" +
+                "function copy(){\n" +
                 "  v = $('#profile').html()\n" +
                 "  const t = document.createElement('textarea')\n" +
                 "  t.style.cssText = 'position: absolute;top:0;left:0;opacity:0'\n" +
@@ -123,7 +192,7 @@ public class QueryProfileAction extends WebBaseAction {
                 "}\n" +
                 "</script>");
         buffer.append("<script type=\"text/javascript\">\n" +
-                "function downloadProfile() {\n" +
+                "function download() {\n" +
                 "  content = $('#profile').html()\n" +
                 "  const file = new Blob([content], { type: \"text/plain\" });\n" +
                 "  const params = new URLSearchParams(window.location.search);\n" +
@@ -135,7 +204,10 @@ public class QueryProfileAction extends WebBaseAction {
                 "\n" +
                 "  URL.revokeObjectURL(a.href);\n" +
                 "}" + "</script>");
-        buffer.append("<input type=\"button\" onclick=\"copyProfile();\" value=\"Copy Profile\"></input>");
-        buffer.append("<input type=\"button\" onclick=\"downloadProfile();\" value=\"Download Profile\"></input>");
+        buffer.append("<input type=\"button\" onclick=\"viewProfile();\" value=\"View Profile\"></input>");
+        buffer.append("<input type=\"button\" onclick=\"formattedSql();\" value=\"Formatted SQL\"></input>");
+        buffer.append("<input type=\"button\" onclick=\"analyzeProfile();\" value=\"Analyze Profile\"></input>");
+        buffer.append("<input type=\"button\" onclick=\"copy();\" value=\"Copy\"></input>");
+        buffer.append("<input type=\"button\" onclick=\"download();\" value=\"Download\"></input>");
     }
 }
