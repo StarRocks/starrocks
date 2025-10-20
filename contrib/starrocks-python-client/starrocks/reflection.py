c10eab49047fd7df6f33a946c2c31ac40b8896d3
janlink <janlink@outlook.com>
2025-10-20 05:15:36 +0200

    [BugFix](starrocks-python-client) Correct reflection of server_default and generated columns (#61795)
    Signed-off-by: janlink <janlink@outlook.com>
    Co-authored-by: Jan Link <link@gipmbh.de>

--- contrib/starrocks-python-client/starrocks/reflection.py
+++ contrib/starrocks-python-client/starrocks/reflection.py
@@ -108,17 +108,22 @@ class StarRocksTableDefinitionParser(object):
         Parse column from information_schema.columns table.
         It returns dictionary with column informations expected by sqlalchemy.
         """
-        return {
+        col_data = {
             "name": column["COLUMN_NAME"],
             "type": self._parse_column_type(column=column),
             "nullable": column["IS_NULLABLE"] == "YES",
             "default": column["COLUMN_DEFAULT"],
             "autoincrement": None,  # TODO: This is not specified
-            "computed": {
-                "sqltext": column["GENERATION_EXPRESSION"]
-            },
             "comment": column["COLUMN_COMMENT"],
         }
+
+        generation_expression = column.get("GENERATION_EXPRESSION")
+        if generation_expression:
+            col_data["computed"] = {
+                "sqltext": generation_expression
+            }
+
+        return col_data
 
     def _get_key_columns(self, columns: list[_DecodingRow]) -> list[str]:
         """
