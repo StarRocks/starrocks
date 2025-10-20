c89ebf397fbb864df3c6735a2335cdd20dd31b02
Murphy <mofei@starrocks.com>
2025-10-20 17:16:10 +0800

    fix run-fe-ut.sh with --test option
    Signed-off-by: Murphy <mofei@starrocks.com>

--- run-fe-ut.sh
+++ run-fe-ut.sh
@@ -173,6 +173,6 @@ else
         fi
 
         # set trimStackTrace to false to show full stack when debugging specified class or case
-        ${MVN_CMD} verify -DfailIfNoTests=false -DtrimStackTrace=false -D test="$TEST_NAME" -T $PARALLEL
+        ${MVN_CMD} verify -DfailIfNoTests=false -Dsurefire.failIfNoSpecifiedTests=false -DtrimStackTrace=false -D test="$TEST_NAME" -T $PARALLEL
     fi
 fi
