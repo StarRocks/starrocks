76755c931e59c86868b1675194263123b6fa2c88
andyziye <108652123+andyziye@users.noreply.github.com>
2025-10-20 10:53:50 +0800

    [UT] add profiler for FE UT(sync #64134) (#42083)
    * [UT] add profiler for FE UT
    
    * fix conflict
    
    Signed-off-by: Murphy <mofei@starrocks.com>
    
    ---------
    
    Signed-off-by: Murphy <mofei@starrocks.com>
    Co-authored-by: Murphy <96611012+murphyatwork@users.noreply.github.com>
    Co-authored-by: Murphy <mofei@starrocks.com>

--- run-fe-ut.sh
+++ run-fe-ut.sh
@@ -36,6 +36,7 @@ Usage: $0 <options>
      --dry-run                  dry-run unit tests
      --coverage                 run coverage statistic tasks
      --dumpcase [PATH]          run dump case and save to path
+     --enable-profiler [0|1]    enable/disable async-profiler for performance profiling
      -j [N]                     build parallel, default is ${FE_UT_PARALLEL:-4}
 
   Eg.
@@ -45,6 +46,8 @@ Usage: $0 <options>
     $0 --dry-run                                  dry-run unit tests
     $0 --coverage                                 run coverage statistic tasks
     $0 --dumpcase /home/disk1/                    run dump case and save to path
+    $0 --enable-profiler 1                        run tests with async-profiler enabled
+    $0 --enable-profiler 0                        run tests with async-profiler disabled
     $0 -j16 --test com.starrocks.utframe.Demo     run demo test with 16 parallel threads
   "
   exit 1
@@ -59,6 +62,7 @@ OPTS=$(getopt \
   -l 'dry-run' \
   -l 'coverage' \
   -l 'dumpcase' \
+  -l 'enable-profiler:' \
   -l 'help' \
   -l 'run' \
   -- "$@")
@@ -76,6 +80,7 @@ TEST_NAME=*
 FILTER_TEST=""
 COVERAGE=0
 DUMPCASE=0
+ENABLE_PROFILER=1
 PARALLEL=${FE_UT_PARALLEL:-4}
 while true; do
     case "$1" in
@@ -85,6 +90,7 @@ while true; do
         --run) shift ;; # only used for compatibility
         --dumpcase) DUMPCASE=1; shift ;;
         --dry-run) DRY_RUN=1 ; shift ;;
+        --enable-profiler) ENABLE_PROFILER=$2; shift 2;;
         --help) HELP=1 ; shift ;;
         -j) PARALLEL=$2; shift 2 ;;
         --) shift ;  break ;;
@@ -107,6 +113,15 @@ mkdir -p build/compile
 # Set FE_UT_PARALLEL if -j parameter is provided
 export FE_UT_PARALLEL=$PARALLEL
 echo "Unit test parallel is: $FE_UT_PARALLEL"
+
+# Set ASYNC_PROFILER_ENABLED based on --enable-profiler parameter
+if [ "${ENABLE_PROFILER}" = "1" ]; then
+    export ASYNC_PROFILER_ENABLED=true
+    echo "Async-profiler is enabled"
+else
+    export ASYNC_PROFILER_ENABLED=false
+    echo "Async-profiler is disabled"
+fi
 
 if [ -d "./mocked" ]; then
     rm -r ./mocked
