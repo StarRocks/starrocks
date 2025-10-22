17120a1ada154c6f7f7e41950a8fd18b05de201b
Murphy <96611012+murphyatwork@users.noreply.github.com>
2025-10-22 19:52:47 +0800

    [Enhancement] truncate the long sql statement in fe profiles (#64377)
    Signed-off-by: Murphy <mofei@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/http/action/QueryAction.java
+++ fe/fe-core/src/main/java/com/starrocks/http/action/QueryAction.java
@@ -122,9 +122,18 @@ public class QueryAction extends WebBaseAction {
                 if (columnHeaders.get(i).equals(ProfileManager.SQL_STATEMENT)) {
                     String statement = row.get(i);
                     if (statement != null && statement.length() > Config.ui_queries_sql_statement_max_length) {
-                        statement = statement.substring(0, Math.max(Config.ui_queries_sql_statement_max_length, 0)) + " ...";
+                        String truncatedStatement =
+                                statement.substring(0, Math.max(Config.ui_queries_sql_statement_max_length, 0))
+                                + " ...";
+                        // Add title attribute for hover tooltip to show full statement
+                        buffer.append("<span title=\"")
+                                .append(statement.replace("\"", "&quot;"))
+                                .append("\">")
+                                .append(truncatedStatement)
+                                .append("</span>");
+                    } else {
+                        buffer.append(statement);
                     }
-                    buffer.append(statement);
                 } else {
                     buffer.append(row.get(i));
                 }
