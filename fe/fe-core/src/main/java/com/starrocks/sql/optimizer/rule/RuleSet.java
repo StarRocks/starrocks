1eed0bd3414857b1ce2ba49d83ccbc5c406cd0f9
stephen <91597003+stephen-shelby@users.noreply.github.com>
2025-10-22 20:51:42 +0800

    [Enhancement] remove RewriteMinMaxByMonotonicFunctionRule from AGGREGATE_REWRITE_RULES group (#64392)
    Signed-off-by: stephen <stephen5217@163.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/RuleSet.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/RuleSet.java
@@ -152,7 +152,6 @@ import com.starrocks.sql.optimizer.rule.transformation.RewriteBitmapCountDistinc
 import com.starrocks.sql.optimizer.rule.transformation.RewriteCountIfFunction;
 import com.starrocks.sql.optimizer.rule.transformation.RewriteDuplicateAggregateFnRule;
 import com.starrocks.sql.optimizer.rule.transformation.RewriteHllCountDistinctRule;
-import com.starrocks.sql.optimizer.rule.transformation.RewriteMinMaxByMonotonicFunctionRule;
 import com.starrocks.sql.optimizer.rule.transformation.RewriteSimpleAggToHDFSScanRule;
 import com.starrocks.sql.optimizer.rule.transformation.RewriteSimpleAggToMetaScanRule;
 import com.starrocks.sql.optimizer.rule.transformation.RewriteSumByAssociativeRule;
@@ -328,8 +327,7 @@ public class RuleSet {
                     new RewriteHllCountDistinctRule(),
                     new RewriteDuplicateAggregateFnRule(),
                     new RewriteSumByAssociativeRule(),
-                    new RewriteCountIfFunction(),
-                    new RewriteMinMaxByMonotonicFunctionRule()
+                    new RewriteCountIfFunction()
             ));
 
     public static final Rule PRUNE_PROJECT_RULES = new CombinationRule(RuleType.GP_PRUNE_PROJECT, ImmutableList.of(
