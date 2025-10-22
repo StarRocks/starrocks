5878b03cacca1c7522f6cee8e6f9e1e52d7a068d
hmx <1639097204@qq.com>
2025-10-22 11:03:51 +0800

    [BugFix] Fix collect paimon table column statistics information report error (backport #63650)

--- fe/fe-core/src/test/java/com/starrocks/catalog/PaimonTableTest.java
+++ fe/fe-core/src/test/java/com/starrocks/catalog/PaimonTableTest.java
@@ -16,9 +16,23 @@ package com.starrocks.catalog;
 
 import com.google.common.collect.Lists;
 import com.starrocks.connector.ColumnTypeConverter;
+import com.starrocks.connector.ConnectorProperties;
+import com.starrocks.connector.ConnectorType;
+import com.starrocks.connector.HdfsEnvironment;
+import com.starrocks.connector.paimon.PaimonMetadata;
+import com.starrocks.qe.ConnectContext;
 import com.starrocks.thrift.TTableDescriptor;
+import com.starrocks.utframe.UtFrameUtils;
 import mockit.Expectations;
 import mockit.Mocked;
+import org.apache.paimon.CoreOptions;
+import org.apache.paimon.catalog.Catalog;
+import org.apache.paimon.catalog.CatalogContext;
+import org.apache.paimon.catalog.CatalogFactory;
+import org.apache.paimon.catalog.Identifier;
+import org.apache.paimon.fs.Path;
+import org.apache.paimon.options.Options;
+import org.apache.paimon.schema.Schema;
 import org.apache.paimon.table.FileStoreTable;
 import org.apache.paimon.types.DataField;
 import org.apache.paimon.types.DataType;
@@ -28,6 +42,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -107,5 +122,47 @@ public class PaimonTableTest {
         Assert.assertEquals(table, table2);
         Assert.assertEquals(table, table);
         Assert.assertNotEquals(table, null);
+    }
+
+    @Test
+    public void testGetUUID() throws Exception {
+        //1.initialize env、db、table
+        java.nio.file.Path tmpDir = Files.createTempDirectory("tmp_");
+        Catalog catalog = CatalogFactory.createCatalog(CatalogContext.create(new Path(tmpDir.toString())));
+        ConnectContext connectContext = UtFrameUtils.createDefaultCtx();
+        PaimonMetadata metadata = new PaimonMetadata("paimon_catalog", new HdfsEnvironment(), catalog,
+                new ConnectorProperties(ConnectorType.PAIMON));
+
+        catalog.createDatabase("test_db", true);
+
+        Schema.Builder schemaBuilder = Schema.newBuilder();
+        schemaBuilder.column("order_id", DataTypes.STRING());
+        schemaBuilder.column("order_date", DataTypes.STRING());
+        Options options = new Options();
+        options.set(CoreOptions.BUCKET, 2);
+        options.set(CoreOptions.BUCKET_KEY, "order_id");
+        schemaBuilder.options(options.toMap());
+        Schema schema = schemaBuilder.build();
+
+        Identifier identifier = Identifier.create("test_db", "test_table");
+        catalog.createTable(identifier, schema, true);
+
+        //2.check
+        //2.1 check not system table
+        PaimonTable paimonNotSystemTable = (PaimonTable) metadata.getTable(connectContext, "test_db", "test_table");
+        org.junit.jupiter.api.Assertions.assertEquals(paimonNotSystemTable.getUUID().split("\\.").length, 4);
+        org.junit.jupiter.api.Assertions.assertTrue(
+                paimonNotSystemTable.getUUID().contains("paimon_catalog.test_db.test_table"));
+
+        //2.2 check system table
+        PaimonTable paimonSystemTable = (PaimonTable) metadata.getTable(connectContext, "test_db", "test_table$snapshots");
+        org.junit.jupiter.api.Assertions.assertEquals(paimonSystemTable.getUUID().split("\\.").length, 4);
+        org.junit.jupiter.api.Assertions.assertTrue(
+                paimonSystemTable.getUUID().contains("paimon_catalog.test_db.test_table$snapshots"));
+
+        //3.clean env
+        catalog.dropTable(identifier, true);
+        catalog.dropDatabase("test_db", true, true);
+        Files.delete(tmpDir);
     }
 }
