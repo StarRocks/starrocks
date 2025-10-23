c3e57b00b074b18c1a897e4c98dc48f5c24ab300
yan zhang <dirtysalt1987@gmail.com>
2025-10-23 16:04:37 +0800

    fix ut
    Signed-off-by: yan zhang <dirtysalt1987@gmail.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/ReplayFromDumpTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/ReplayFromDumpTest.java
@@ -92,29 +92,16 @@ public class ReplayFromDumpTest extends ReplayFromDumpTestBase {
         sessionVariable.setNewPlanerAggStage(2);
         Pair<QueryDumpInfo, String> replayPair =
                 getCostPlanFragment(getDumpInfoFromFile("query_dump/join_eliminate_nulls"), sessionVariable);
-        Assertions.assertTrue(replayPair.second.contains("11:NESTLOOP JOIN\n" +
+        Assertions.assertTrue(replayPair.second.contains("  11:NESTLOOP JOIN\n" +
                 "  |  join op: INNER JOIN\n" +
-<<<<<<< HEAD
-                "  |  other join predicates: CASE 174: type WHEN '1' THEN concat('ocms_', 90: name) = " +
-                "'ocms_fengyang56' " +
-                "WHEN '0' THEN TRUE ELSE FALSE END\n" +
-                "  |  limit: 10"), replayPair.second);
-        Assertions.assertTrue(replayPair.second.contains("  4:HASH JOIN\n" +
-                "  |  join op: RIGHT OUTER JOIN (PARTITIONED)\n" +
-                "  |  equal join conjunct: [tid, BIGINT, true] = [5: customer_id, BIGINT, true]\n" +
-                "  |  build runtime filters:\n" +
-                "  |  - filter_id = 0, build_expr = (5: customer_id), remote = true"), replayPair.second);
-=======
-                "  |  other join predicates: CASE [174: type, VARCHAR, true] WHEN '1' THEN concat[('ocms_', [90: name, VARCHAR,"
-                + " true]); args: VARCHAR; result: VARCHAR; args nullable: true; result nullable: true] = 'ocms_fengyang56' "
-                + "WHEN '0' THEN TRUE ELSE FALSE END\n"
-                + "  |  limit: 10"), replayPair.second);
+                "  |  other join predicates: CASE 174: type WHEN '1' THEN concat('ocms_', 90: name) = 'ocms_fengyang56' WHEN '0' THEN TRUE ELSE FALSE END\n" +
+                "  |  limit: 10\n" +
+                "  |  cardinality: 1"), replayPair.second);
         Assertions.assertTrue(replayPair.second.contains("  4:HASH JOIN\n" +
                 "  |  join op: LEFT OUTER JOIN (PARTITIONED)\n" +
                 "  |  equal join conjunct: [5: customer_id, BIGINT, true] = [tid, BIGINT, true]\n" +
                 "  |  output columns: 3, 90\n" +
                 "  |  cardinality: 1"), replayPair.second);
->>>>>>> 957e63e662 ([Enhancement] unify jdbc/es/mysql default row count (#64446))
         sessionVariable.setNewPlanerAggStage(0);
     }
 
