50320126249fbc4951732fb979c7242d81eec7b8
m.bogusz <m.bogusz@celonis.com>
2025-10-25 18:59:55 -0400

    use mem_pool property to select mem_tracker
    Signed-off-by: m.bogusz <m.bogusz@celonis.com>

--- be/test/exec/workgroup/mem_tracker_manager.cpp
+++ be/test/exec/workgroup/mem_tracker_manager.cpp
@@ -27,4 +27,13 @@ PARALLEL_TEST(MemTrackerMangerTest, test_mem_tracker_for_custom_mem_pool) {
     ASSERT_EQ(manager.get_parent_mem_tracker(work_group1), manager.get_parent_mem_tracker(work_group2));
     ASSERT_NE(manager.get_parent_mem_tracker(work_group1), manager.get_parent_mem_tracker(work_group3));
 }
+PARALLEL_TEST(MemTrackerMangerTest, test_mem_tracker_for_custom_mem_pool_overwrite) {
+    MemTrackerManager manager;
+    const auto work_group1{std::make_shared<WorkGroup>("wg_1", 123, WorkGroup::DEFAULT_VERSION, 1, 0.5, 0, 0.9,
+                                                       WorkGroupType::WG_DEFAULT, "test_pool")};
+    const auto work_group2{std::make_shared<WorkGroup>("wg_2", 134, WorkGroup::DEFAULT_VERSION, 1, 0.7, 0, 0.9,
+                                                       WorkGroupType::WG_DEFAULT, "test_pool")};
+
+    ASSERT_NE(manager.get_parent_mem_tracker(work_group1), manager.get_parent_mem_tracker(work_group2));
+}
 } // namespace starrocks::workgroup
