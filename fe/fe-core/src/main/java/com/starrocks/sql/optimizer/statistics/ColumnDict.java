ec5f41a4f13856e0414e32a2079e0f54ca209f8e
zihe.liu <ziheliu1024@gmail.com>
2025-10-27 10:54:11 +0800

    [Enhancement] Add limited support for global low-cardinality optimization in join (#63919)
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/statistics/ColumnDict.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/statistics/ColumnDict.java
@@ -18,6 +18,7 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
 import com.starrocks.common.Config;
+import com.starrocks.common.Pair;
 import com.starrocks.persist.gson.GsonUtils;
 
 import java.nio.ByteBuffer;
@@ -69,5 +70,59 @@ public final class ColumnDict extends StatsVersion {
         jsonMap.put("collectedVersion", collectedVersion);
         jsonMap.put("version", version);
         return gson.toJson(jsonMap);
+    }
+
+    /**
+     * Merge two dictionaries and return two new dictionaries.
+     * The id of each dictionary is a continuous integer from 1 to n, where n is the number of words in the dictionary.
+     * Ensure that the lexicographical order of the dictionary words is consistent with the order of the ids.
+     * If the merged dictionary size exceeds Config.low_cardinality_threshold, return null.
+     */
+    public static Pair<ColumnDict, ColumnDict> merge(ColumnDict d1, ColumnDict d2) {
+        final int n1 = d1.getDict().size() + 1;
+        final int n2 = d2.getDict().size() + 1;
+        ByteBuffer[] sortedKeys1 = new ByteBuffer[n1];
+        ByteBuffer[] sortedKeys2 = new ByteBuffer[n2];
+        d1.getDict().forEach((key, idx) -> sortedKeys1[idx] = key);
+        d2.getDict().forEach((key, idx) -> sortedKeys2[idx] = key);
+
+        ImmutableMap.Builder<ByteBuffer, Integer> builder =
+                ImmutableMap.builderWithExpectedSize(Math.min(n1 + n2 - 2, Config.low_cardinality_threshold));
+        int idx1 = 1;
+        int idx2 = 1;
+        int newIdx = 1;
+        while (idx1 < n1 && idx2 < n2) {
+            ByteBuffer key1 = sortedKeys1[idx1];
+            ByteBuffer key2 = sortedKeys2[idx2];
+            int cmp = key1.compareTo(key2);
+            if (cmp == 0) {
+                builder.put(key1, newIdx);
+                idx1++;
+                idx2++;
+            } else if (cmp < 0) {
+                builder.put(key1, newIdx);
+                idx1++;
+            } else {
+                builder.put(key2, newIdx);
+                idx2++;
+            }
+            newIdx++;
+        }
+
+        while (idx1 < n1) {
+            builder.put(sortedKeys1[idx1++], newIdx++);
+        }
+        while (idx2 < n2) {
+            builder.put(sortedKeys2[idx2++], newIdx++);
+        }
+
+        if (newIdx > Config.low_cardinality_threshold) {
+            return null;
+        }
+
+        final ImmutableMap<ByteBuffer, Integer> newDict = builder.build();
+        ColumnDict newD1 = new ColumnDict(newDict, d1.getCollectedVersion(), d1.getVersion());
+        ColumnDict newD2 = new ColumnDict(newDict, d2.getCollectedVersion(), d2.getVersion());
+        return new Pair<>(newD1, newD2);
     }
 }
