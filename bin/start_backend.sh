7142edf669f5e99e95723fae101a709d067d013f
wangriyu.wry <wangriyu.wry@alibaba-inc.com>
2025-10-27 11:12:47 +0800

    [Stella][BugFix] Fix the wrong lib path of backend

--- bin/start_backend.sh
+++ bin/start_backend.sh
@@ -165,7 +165,7 @@ if [[ -d ${STARROCKS_HOME}/lib/paimon ]]; then
   export LD_LIBRARY_PATH="${STARROCKS_HOME}/lib/paimon:${LD_LIBRARY_PATH}"
 fi
 
-export LD_LIBRARY_PATH=$STARROCKS_HOME/lib:$STARROCKS_HOME/lib/jindosdk:$STARROCKS_HOME/lib/hadoop/native:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$STARROCKS_HOME/lib/jindosdk:$STARROCKS_HOME/lib/hadoop/native:$LD_LIBRARY_PATH
 export_cachelib_lib_path
 
 
