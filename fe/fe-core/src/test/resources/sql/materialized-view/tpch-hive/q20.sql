9d2f47087a3126acd03532264182e391a032df5b
zihe.liu <ziheliu1024@gmail.com>
2025-10-27 10:53:59 +0800

    [Enhancement] Relax rule for using local shuffle one-phase agg (#64450)
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- fe/fe-core/src/test/resources/sql/materialized-view/tpch-hive/q20.sql
+++ fe/fe-core/src/test/resources/sql/materialized-view/tpch-hive/q20.sql
@@ -6,27 +6,27 @@ from
     hive0.tpch.supplier,
     hive0.tpch.nation
 where
-        s_suppkey in (
+    s_suppkey in (
         select
             ps_suppkey
         from
             hive0.tpch.partsupp
         where
-                ps_partkey in (
+            ps_partkey in (
                 select
                     p_partkey
                 from
                     hive0.tpch.part
                 where
-                        p_name like 'sienna%'
+                    p_name like 'sienna%'
             )
           and ps_availqty > (
             select
-                    0.5 * sum(l_quantity)
+                0.5 * sum(l_quantity)
             from
                 hive0.tpch.lineitem
             where
-                    l_partkey = ps_partkey
+                l_partkey = ps_partkey
               and l_suppkey = ps_suppkey
               and l_shipdate >= date '1993-01-01'
               and l_shipdate < date '1994-01-01'
@@ -43,18 +43,18 @@ TOP-N (order by [[2: s_name ASC NULLS FIRST]])
             EXCHANGE SHUFFLE[13]
                 INNER JOIN (join-predicate [12: ps_partkey = 28: l_partkey AND 13: ps_suppkey = 29: l_suppkey AND cast(14: ps_availqty as DECIMAL128(38,3)) > multiply(0.5, 43: sum)] post-join-predicate [null])
                     LEFT SEMI JOIN (join-predicate [12: ps_partkey = 17: p_partkey] post-join-predicate [null])
-                        EXCHANGE SHUFFLE[12]
-                            HIVE SCAN (columns{12,13,14} predicate[13: ps_suppkey IS NOT NULL])
-                        EXCHANGE SHUFFLE[17]
+                        HIVE SCAN (columns{12,13,14} predicate[13: ps_suppkey IS NOT NULL])
+                        EXCHANGE BROADCAST
                             HIVE SCAN (columns{17,18} predicate[17: p_partkey IS NOT NULL AND 18: p_name LIKE sienna%])
-                    EXCHANGE SHUFFLE[28]
-                        AGGREGATE ([GLOBAL] aggregate [{145: sum=sum(145: sum)}] group by [[65: l_partkey, 63: l_suppkey]] having [null]
-                            EXCHANGE SHUFFLE[65, 63]
-                                AGGREGATE ([LOCAL] aggregate [{145: sum=sum(66: sum_qty)}] group by [[65: l_partkey, 63: l_suppkey]] having [null]
-                                    SCAN (mv[lineitem_agg_mv2] columns[63: l_suppkey, 64: l_shipdate, 65: l_partkey, 66: sum_qty] predicate[64: l_shipdate >= 1993-01-01 AND 64: l_shipdate < 1994-01-01])
+                    EXCHANGE BROADCAST
+                        AGGREGATE ([GLOBAL] aggregate [{138: sum=sum(138: sum)}] group by [[49: l_partkey, 47: l_suppkey]] having [null]
+                            EXCHANGE SHUFFLE[49, 47]
+                                AGGREGATE ([LOCAL] aggregate [{138: sum=sum(50: sum_qty)}] group by [[49: l_partkey, 47: l_suppkey]] having [null]
+                                    SCAN (mv[lineitem_agg_mv2] columns[47: l_suppkey, 48: l_shipdate, 49: l_partkey, 50: sum_qty] predicate[48: l_shipdate >= 1993-01-01 AND 48: l_shipdate < 1994-01-01])
             EXCHANGE SHUFFLE[1]
                 INNER JOIN (join-predicate [4: s_nationkey = 8: n_nationkey] post-join-predicate [null])
                     HIVE SCAN (columns{1,2,3,4} predicate[4: s_nationkey IS NOT NULL])
                     EXCHANGE BROADCAST
                         HIVE SCAN (columns{8,9} predicate[9: n_name = ARGENTINA])
 [end]
+
