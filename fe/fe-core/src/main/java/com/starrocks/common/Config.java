0a160260b25d1844f9dfa3928d9e3997b68976b9
crossoverJie <crossoverJie@gmail.com>
2025-10-31 10:07:57 +0800

    [Enhancement] internal.log support JSON format (#64660)
    Signed-off-by: crossoverJie <crossoverJie@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/common/Config.java
+++ fe/fe-core/src/main/java/com/starrocks/common/Config.java
@@ -211,6 +211,8 @@ public class Config extends ConfigBase {
     public static String internal_log_roll_interval = "DAY";
     @ConfField
     public static String internal_log_delete_age = "7d";
+    @ConfField
+    public static boolean internal_log_json_format = false;
 
     /**
      * dump_log_dir:
