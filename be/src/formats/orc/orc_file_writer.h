8fdab9add14c4670284426c5daad695d8e765e67
trueeyu <lxhhust350@qq.com>
2025-10-31 17:31:44 +0800

    Remove some unused code of orc reader and orc writer
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/formats/orc/orc_file_writer.h
+++ be/src/formats/orc/orc_file_writer.h
@@ -23,27 +23,6 @@
 
 namespace starrocks::formats {
 
-class OrcOutputStream : public orc::OutputStream {
-public:
-    OrcOutputStream(std::unique_ptr<starrocks::WritableFile> wfile);
-
-    ~OrcOutputStream() override;
-
-    uint64_t getLength() const override;
-
-    uint64_t getNaturalWriteSize() const override;
-
-    void write(const void* buf, size_t length) override;
-
-    void close() override;
-
-    const std::string& getName() const override;
-
-private:
-    std::unique_ptr<starrocks::WritableFile> _wfile;
-    bool _is_closed = false;
-};
-
 class AsyncOrcOutputStream : public orc::OutputStream {
 public:
     AsyncOrcOutputStream(io::AsyncFlushOutputStream* _stream);
