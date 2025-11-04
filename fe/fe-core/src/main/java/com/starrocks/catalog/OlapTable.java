c6b046d178e9ca619d5dcfcbc565c0cce117a572
shuming.li <ming.moriarty@gmail.com>
2025-11-04 16:51:28 +0800

    [BugFix] Fix MVPartitionExprResolver column name not matched bug (#64914)
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/OlapTable.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/OlapTable.java
@@ -2973,7 +2973,7 @@ public class OlapTable extends Table {
     }
 
     @Override
-    public void onCreate(Database db) {
+    public void onCreate(Database db) throws DdlException {
         super.onCreate(db);
 
         ColocateTableIndex colocateTableIndex = GlobalStateMgr.getCurrentState().getColocateTableIndex();
