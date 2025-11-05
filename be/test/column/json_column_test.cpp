2ff91b6f5911af80ae87370faaf70c7dc186279a
Murphy <mofei@starrocks.com>
2025-11-05 16:31:05 +0800

    fix format
    Signed-off-by: Murphy <mofei@starrocks.com>

--- be/test/column/json_column_test.cpp
+++ be/test/column/json_column_test.cpp
@@ -246,27 +246,27 @@ PARALLEL_TEST(JsonColumnTest, test_compare_array_large_integers) {
     // Test comparison with large integers to avoid overflow issues
     // This tests the bug where comparing [1] < [9223372036854775807] would incorrectly return false
     auto small_int_array = JsonValue::parse("[1]").value();
-    auto large_int_array = JsonValue::parse("[9223372036854775807]").value();  // INT64_MAX
+    auto large_int_array = JsonValue::parse("[9223372036854775807]").value(); // INT64_MAX
     auto medium_large_int_array = JsonValue::parse("[922337203685477580]").value();
     auto medium_int_array = JsonValue::parse("[92233720368547758]").value();
-    
+
     // Small integer should be less than large integer
     EXPECT_LT(small_int_array.compare(large_int_array), 0);
     EXPECT_LT(small_int_array.compare(medium_large_int_array), 0);
     EXPECT_LT(small_int_array.compare(medium_int_array), 0);
-    
+
     // Large integer should be greater than small integer
     EXPECT_GT(large_int_array.compare(small_int_array), 0);
     EXPECT_GT(medium_large_int_array.compare(small_int_array), 0);
     EXPECT_GT(medium_int_array.compare(small_int_array), 0);
-    
+
     // Test with negative numbers
     auto neg_small_int_array = JsonValue::parse("[-1]").value();
-    auto neg_large_int_array = JsonValue::parse("[-9223372036854775808]").value();  // INT64_MIN
-    
+    auto neg_large_int_array = JsonValue::parse("[-9223372036854775808]").value(); // INT64_MIN
+
     EXPECT_GT(neg_small_int_array.compare(neg_large_int_array), 0);
     EXPECT_LT(neg_large_int_array.compare(neg_small_int_array), 0);
-    
+
     // Compare positive and negative
     EXPECT_GT(small_int_array.compare(neg_small_int_array), 0);
     EXPECT_LT(neg_small_int_array.compare(small_int_array), 0);
