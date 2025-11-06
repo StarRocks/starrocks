895a025fa661d5a006e9c0bd4c5850a8983d9ee9
xiangguangyxg <110401425+xiangguangyxg@users.noreply.github.com>
2025-11-06 14:22:30 +0800

    [Refactor] Remove virtual bucket and simplify data distribution and bucket pruning (#64816)
    Signed-off-by: xiangguangyxg <xiangguangyxg@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/MvRewritePreprocessor.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/MvRewritePreprocessor.java
@@ -1072,7 +1072,7 @@ public class MvRewritePreprocessor {
                 selectedPartitionNames.add(p.getName());
                 for (PhysicalPartition physicalPartition : p.getSubPartitions()) {
                     MaterializedIndex materializedIndex = physicalPartition.getIndex(mv.getBaseIndexId());
-                    selectTabletIds.addAll(materializedIndex.getTabletIds());
+                    selectTabletIds.addAll(materializedIndex.getTabletIdsInOrder());
                 }
             }
         }
