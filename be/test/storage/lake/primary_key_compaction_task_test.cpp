cf32caf034645f0eda2d91bdb2e2eda7e6889524
Yixin Luo <18810541851@163.com>
2025-11-06 07:38:20 +0000

    resolve
    Signed-off-by: Yixin Luo <18810541851@163.com>

--- be/test/storage/lake/primary_key_compaction_task_test.cpp
+++ be/test/storage/lake/primary_key_compaction_task_test.cpp
@@ -1497,7 +1497,7 @@ TEST_P(LakePrimaryKeyCompactionTest, test_concurrent_compaction_and_publish) {
                                                    .build());
         ASSERT_OK(delta_writer->open());
         ASSERT_OK(delta_writer->write(chunk0, indexes.data(), indexes.size()));
-        ASSERT_OK(delta_writer->finish_with_txnlog());
+        ASSERT_OK(delta_writer->finish());
         delta_writer->close();
         // Publish version
         ASSERT_OK(publish_single_version(tablet_id, version + 1, txn_id).status());
