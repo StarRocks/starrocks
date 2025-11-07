b49caeaa3ee407d4f890aa3d18fca333889f6d7c
Yixin Luo <18810541851@163.com>
2025-11-07 02:52:31 +0000

    resolve
    Signed-off-by: Yixin Luo <18810541851@163.com>

--- be/src/storage/lake/meta_file.cpp
+++ be/src/storage/lake/meta_file.cpp
@@ -578,66 +578,6 @@ void MetaFileBuilder::finalize_sstable_meta(const PersistentIndexSstableMetaPB& 
     _tablet_meta->mutable_sstable_meta()->CopyFrom(sstable_meta);
 }
 
-<<<<<<< HEAD
-=======
-// get delvec by DelvecPagePB
-Status get_del_vec(TabletManager* tablet_mgr, const TabletMetadata& metadata, const DelvecPagePB& delvec_page,
-                   bool fill_cache, const LakeIOOptions& lake_io_opts, DelVector* delvec) {
-    VLOG(2) << fmt::format("get_del_vec {} tabletid {}", delvec_page.ShortDebugString(), metadata.id());
-    std::string buf;
-    raw::stl_string_resize_uninitialized(&buf, delvec_page.size());
-    // find in cache
-    std::string cache_key = delvec_cache_key(metadata.id(), delvec_page);
-    auto cached_delvec = tablet_mgr->metacache()->lookup_delvec(cache_key);
-    if (cached_delvec != nullptr) {
-        delvec->copy_from(*cached_delvec);
-        return Status::OK();
-    }
-
-    // lookup delvec file name and then read it
-    auto iter = metadata.delvec_meta().version_to_file().find(delvec_page.version());
-    if (iter == metadata.delvec_meta().version_to_file().end()) {
-        LOG(ERROR) << "Can't find delvec file name for tablet: " << metadata.id()
-                   << ", version: " << delvec_page.version();
-        return Status::InternalError("Can't find delvec file name");
-    }
-    const auto& delvec_name = iter->second.name();
-    RandomAccessFileOptions opts{.skip_fill_local_cache = !lake_io_opts.fill_data_cache};
-    std::unique_ptr<RandomAccessFile> rf;
-    if (lake_io_opts.fs && lake_io_opts.location_provider) {
-        ASSIGN_OR_RETURN(rf,
-                         lake_io_opts.fs->new_random_access_file(
-                                 opts, lake_io_opts.location_provider->delvec_location(metadata.id(), delvec_name)));
-    } else {
-        ASSIGN_OR_RETURN(rf, fs::new_random_access_file(opts, tablet_mgr->delvec_location(metadata.id(), delvec_name)));
-    }
-    RETURN_IF_ERROR(rf->read_at_fully(delvec_page.offset(), buf.data(), delvec_page.size()));
-    if (delvec_page.has_crc32c()) {
-        // check crc32c
-        uint32_t crc32c = crc32c::Value(buf.data(), delvec_page.size());
-        if (crc32c != crc32c::Unmask(delvec_page.crc32c())) {
-            LOG(ERROR) << fmt::format(
-                    "delvec crc32c mismatch, tabletid {}, delvecfile {}, offset {}, size {}, expect crc32c {}, actual "
-                    "crc32c {}",
-                    metadata.id(), delvec_name, delvec_page.offset(), delvec_page.size(),
-                    crc32c::Unmask(delvec_page.crc32c()), crc32c);
-            return Status::Corruption(fmt::format("delvec crc32c mismatch. expect crc32c {}, actual {}",
-                                                  crc32c::Unmask(delvec_page.crc32c()), crc32c));
-        }
-    }
-    // parse delvec
-    RETURN_IF_ERROR(delvec->load(delvec_page.version(), buf.data(), delvec_page.size()));
-    // put in cache
-    if (fill_cache) {
-        auto delvec_cache_ptr = std::make_shared<DelVector>();
-        delvec_cache_ptr->copy_from(*delvec);
-        tablet_mgr->metacache()->cache_delvec(cache_key, delvec_cache_ptr);
-    }
-    TRACE("end load delvec");
-    return Status::OK();
-}
-
->>>>>>> 29a64c1a84 ([BugFix] introduces checksum for pk table's delete vector (#65006))
 Status get_del_vec(TabletManager* tablet_mgr, const TabletMetadata& metadata, uint32_t segment_id, bool fill_cache,
                    DelVector* delvec) {
     // find delvec by segment id
@@ -666,6 +606,20 @@ Status get_del_vec(TabletManager* tablet_mgr, const TabletMetadata& metadata, ui
         ASSIGN_OR_RETURN(auto rf,
                          fs::new_random_access_file(opts, tablet_mgr->delvec_location(metadata.id(), delvec_name)));
         RETURN_IF_ERROR(rf->read_at_fully(iter->second.offset(), buf.data(), iter->second.size()));
+        if (iter->second.has_crc32c()) {
+            // check crc32c
+            uint32_t crc32c = crc32c::Value(buf.data(), iter->second.size());
+            if (crc32c != crc32c::Unmask(iter->second.crc32c())) {
+                LOG(ERROR) << fmt::format(
+                        "delvec crc32c mismatch, tabletid {}, delvecfile {}, offset {}, size {}, expect crc32c {}, "
+                        "actual "
+                        "crc32c {}",
+                        metadata.id(), delvec_name, iter->second.offset(), iter->second.size(),
+                        crc32c::Unmask(iter->second.crc32c()), crc32c);
+                return Status::Corruption(fmt::format("delvec crc32c mismatch. expect crc32c {}, actual {}",
+                                                      crc32c::Unmask(iter->second.crc32c()), crc32c));
+            }
+        }
         // parse delvec
         RETURN_IF_ERROR(delvec->load(iter->second.version(), buf.data(), iter->second.size()));
         // put in cache
--- be/test/storage/lake/primary_key_publish_test.cpp
+++ be/test/storage/lake/primary_key_publish_test.cpp
@@ -1862,137 +1862,6 @@ TEST_P(LakePrimaryKeyPublishTest, test_individual_index_compaction) {
     EXPECT_TRUE(new_tablet_metadata->orphan_files_size() >= (sst_cnt - 1));
 }
 
-<<<<<<< HEAD
-=======
-TEST_P(LakePrimaryKeyPublishTest, test_publish_with_lazy_load) {
-    const size_t N = 40000;
-    auto [chunk0, indexes] = gen_data_and_index(N, 0, true, true);
-    auto version = 1;
-    auto tablet_id = _tablet_metadata->id();
-    int64_t old_val = config::pk_column_lazy_load_threshold_bytes;
-    config::pk_column_lazy_load_threshold_bytes = 1;
-    for (int i = 0; i < 3; i++) {
-        int64_t txn_id = next_id();
-        ASSIGN_OR_ABORT(auto delta_writer, DeltaWriterBuilder()
-                                                   .set_tablet_manager(_tablet_mgr.get())
-                                                   .set_tablet_id(tablet_id)
-                                                   .set_txn_id(txn_id)
-                                                   .set_partition_id(_partition_id)
-                                                   .set_mem_tracker(_mem_tracker.get())
-                                                   .set_schema_id(_tablet_schema->id())
-                                                   .set_profile(&_dummy_runtime_profile)
-                                                   .build());
-        ASSERT_OK(delta_writer->open());
-        ASSERT_OK(delta_writer->write(*chunk0, indexes.data(), indexes.size()));
-        ASSERT_OK(delta_writer->finish_with_txnlog());
-        delta_writer->close();
-        ASSERT_OK(publish_single_version(tablet_id, version + 1, txn_id).status());
-        version++;
-    }
-    config::pk_column_lazy_load_threshold_bytes = old_val;
-    ASSERT_EQ(N, read_rows(tablet_id, version));
-    if (GetParam().enable_persistent_index && GetParam().persistent_index_type == PersistentIndexTypePB::LOCAL) {
-        check_local_persistent_index_meta(tablet_id, version);
-    }
-}
-
-TEST_P(LakePrimaryKeyPublishTest, test_aggregate_publish_version) {
-    const size_t N = 100;
-    auto [chunk0, indexes] = gen_data_and_index(N, 0, true, true);
-    auto version = 1;
-    std::vector<int64_t> tablet_ids;
-    // tablet 1
-    auto tablet_id1 = _tablet_metadata->id();
-    tablet_ids.push_back(tablet_id1);
-    // tablet 2
-    _tablet_metadata->set_id(next_id());
-    auto tablet_id2 = _tablet_metadata->id();
-    tablet_ids.push_back(tablet_id2);
-    CHECK_OK(_tablet_mgr->put_tablet_metadata(*_tablet_metadata));
-    // tablet 3
-    _tablet_metadata->set_id(next_id());
-    auto tablet_id3 = _tablet_metadata->id();
-    tablet_ids.push_back(tablet_id3);
-    CHECK_OK(_tablet_mgr->put_tablet_metadata(*_tablet_metadata));
-
-    int64_t txn_id = next_id();
-    for (int i = 0; i < tablet_ids.size(); i++) {
-        ASSIGN_OR_ABORT(auto delta_writer, DeltaWriterBuilder()
-                                                   .set_tablet_manager(_tablet_mgr.get())
-                                                   .set_tablet_id(tablet_ids[i])
-                                                   .set_txn_id(txn_id)
-                                                   .set_partition_id(_partition_id)
-                                                   .set_mem_tracker(_mem_tracker.get())
-                                                   .set_schema_id(_tablet_schema->id())
-                                                   .set_profile(&_dummy_runtime_profile)
-                                                   .build());
-        ASSERT_OK(delta_writer->open());
-        ASSERT_OK(delta_writer->write(*chunk0, indexes.data(), indexes.size()));
-        ASSERT_OK(delta_writer->finish_with_txnlog());
-        delta_writer->close();
-    }
-    {
-        ASSERT_OK(aggregate_publish_version(tablet_ids, version + 1, txn_id));
-        version++;
-    }
-
-    // check result.
-    for (int i = 0; i < tablet_ids.size(); i++) {
-        ASSERT_EQ(N, read_rows(tablet_ids[i], version));
-    }
-}
-
-TEST_P(LakePrimaryKeyPublishTest, test_data_file_sharing) {
-    auto [chunk0, indexes] = gen_data_and_index(kChunkSize, 0, true, true);
-    std::vector<int64_t> tablet_ids;
-    auto version = 1;
-    // tablet-1
-    auto tablet_id1 = _tablet_metadata->id();
-    tablet_ids.push_back(tablet_id1);
-    // tablet-2
-    _tablet_metadata->set_id(next_id());
-    ASSERT_OK(_tablet_mgr->put_tablet_metadata(*_tablet_metadata));
-    auto tablet_id2 = _tablet_metadata->id();
-    tablet_ids.push_back(tablet_id2);
-    // tablet-3
-    _tablet_metadata->set_id(next_id());
-    ASSERT_OK(_tablet_mgr->put_tablet_metadata(*_tablet_metadata));
-    auto tablet_id3 = _tablet_metadata->id();
-    tablet_ids.push_back(tablet_id3);
-    for (int i = 0; i < 3; i++) {
-        std::unique_ptr<BundleWritableFileContext> context = std::make_unique<BundleWritableFileContext>();
-        int64_t txn_id = next_id();
-        std::vector<std::unique_ptr<DeltaWriter>> delta_writers;
-        for (int64_t tid : tablet_ids) {
-            ASSIGN_OR_ABORT(auto delta_writer, DeltaWriterBuilder()
-                                                       .set_tablet_manager(_tablet_mgr.get())
-                                                       .set_tablet_id(tid)
-                                                       .set_txn_id(txn_id)
-                                                       .set_partition_id(_partition_id)
-                                                       .set_mem_tracker(_mem_tracker.get())
-                                                       .set_schema_id(_tablet_schema->id())
-                                                       .set_profile(&_dummy_runtime_profile)
-                                                       .set_bundle_writable_file_context(context.get())
-                                                       .build());
-            ASSERT_OK(delta_writer->open());
-            delta_writers.push_back(std::move(delta_writer));
-        }
-        for (auto& delta_writer : delta_writers) {
-            ASSERT_OK(delta_writer->write(*chunk0, indexes.data(), indexes.size()));
-            ASSERT_OK(delta_writer->finish_with_txnlog());
-            delta_writer->close();
-        }
-        // Publish version
-        for (int64_t tid : tablet_ids) {
-            ASSERT_OK(publish_single_version(tid, version + 1, txn_id).status());
-        }
-        version++;
-    }
-    for (int64_t tid : tablet_ids) {
-        ASSERT_EQ(kChunkSize, read_rows(tid, version));
-    }
-}
-
 TEST_P(LakePrimaryKeyPublishTest, test_write_with_delvec_corrupt) {
     auto [chunk0, indexes] = gen_data_and_index(kChunkSize, 0, true, true);
     auto version = 1;
@@ -2018,11 +1887,10 @@ TEST_P(LakePrimaryKeyPublishTest, test_write_with_delvec_corrupt) {
                                                    .set_partition_id(_partition_id)
                                                    .set_mem_tracker(_mem_tracker.get())
                                                    .set_schema_id(_tablet_schema->id())
-                                                   .set_profile(&_dummy_runtime_profile)
                                                    .build());
         ASSERT_OK(delta_writer->open());
         ASSERT_OK(delta_writer->write(*chunk0, indexes.data(), indexes.size()));
-        ASSERT_OK(delta_writer->finish_with_txnlog());
+        ASSERT_OK(delta_writer->finish());
         delta_writer->close();
         // Publish version
         ASSERT_OK(publish_single_version(tablet_id, version + 1, txn_id).status());
@@ -2037,7 +1905,6 @@ TEST_P(LakePrimaryKeyPublishTest, test_write_with_delvec_corrupt) {
     ASSERT_ERROR(read(tablet_id, version));
 }
 
->>>>>>> 29a64c1a84 ([BugFix] introduces checksum for pk table's delete vector (#65006))
 INSTANTIATE_TEST_SUITE_P(LakePrimaryKeyPublishTest, LakePrimaryKeyPublishTest,
                          ::testing::Values(PrimaryKeyParam{true}, PrimaryKeyParam{false},
                                            PrimaryKeyParam{true, PersistentIndexTypePB::CLOUD_NATIVE},
