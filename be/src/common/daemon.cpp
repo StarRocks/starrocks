9a0bd3d13134671833a9e46a71832db18e521adf
andyziye <108652123+andyziye@users.noreply.github.com>
2025-11-07 12:08:47 +0800

    [Tool] Fix memory_lock compile error (#42514)
    Co-authored-by: kangkaisen <kangkaisen@apache.org>
    Co-authored-by: kaisenkang <155127191+kaisenkang@users.noreply.github.com>

--- be/src/common/daemon.cpp
+++ be/src/common/daemon.cpp
@@ -301,9 +301,11 @@ void Daemon::init(bool as_cn, const std::vector<StorePath>& paths) {
 
     LOG(INFO) << get_version_string(false);
 
-#ifndef BE_TEST
+
+#if !defined(BE_TEST) && !defined(__APPLE__)
     starrocks::mlock_modules();
 #endif
+
 
     init_thrift_logging();
     CpuInfo::init();
