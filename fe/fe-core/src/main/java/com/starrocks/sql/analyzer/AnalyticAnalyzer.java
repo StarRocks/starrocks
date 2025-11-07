71fe7a8e7a5afff9122a7de621fab7a9df892233
satanson <ranpanf@gmail.com>
2025-11-07 18:02:02 +0800

    [Enhancement] Support distinct aggregation over window without sliding frames (backport #65030)
    Signed-off-by: satanson <ranpanf@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/AnalyticAnalyzer.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/AnalyticAnalyzer.java
@@ -56,7 +56,7 @@ public class AnalyticAnalyzer {
         }
 
         FunctionCallExpr analyticFunction = analyticExpr.getFnCall();
-        if (analyticFunction.getParams().isDistinct()) {
+        if (analyticFunction.getParams().isDistinct() && !analyticExpr.isUnboundedWindowWithoutSlidingFrame()) {
             throw new SemanticException("DISTINCT not allowed in analytic function: " + analyticFunction.toSql(),
                     analyticExpr.getPos());
         }
