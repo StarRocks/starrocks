bfb983ca9c4aac2162dce980651bad443513ada1
stephen <91597003+stephen-shelby@users.noreply.github.com>
2025-11-13 00:02:41 +0800

    [BugFix] Fix MaterializedView colocate metadata not persisted to EditLog (#65394)
    Signed-off-by: stephen <stephen5217@163.com>
    (cherry picked from commit 6464e35b4029a8215b9420af008e7418dc26c2d2)

--- fe/fe-core/src/main/java/com/starrocks/catalog/MaterializedView.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/MaterializedView.java
@@ -1140,6 +1140,7 @@ public class MaterializedView extends OlapTable implements GsonPreProcessable, G
      */
     @Override
     public void onCreate(Database database) throws DdlException {
+        super.onCreate(database);
         onReload(false, isActive(), true);
     }
 
