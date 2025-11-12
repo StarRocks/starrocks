e43409100895472b27451103bf08a0cf55628eb3
ruyliu <88014713+ruyliu@users.noreply.github.com>
2025-11-12 18:07:36 +0800

    [BugFix] Fix crash when spill large string encode (#61495)
    Signed-off-by: stdpain <drfeng08@gmail.com>
    Co-authored-by: stdpain <drfeng08@gmail.com>
    (cherry picked from commit 98a6e260f770ba696a9d94d4ca80af49878bea19)
    
    # Conflicts:
    #	be/src/exec/spill/spill_components.cpp

--- be/src/exec/spill/spill_components.cpp
+++ be/src/exec/spill/spill_components.cpp
@@ -33,6 +33,11 @@
 #include "exec/workgroup/scan_task_queue.h"
 #include "exec/workgroup/work_group.h"
 #include "exec/workgroup/work_group_fwd.h"
+<<<<<<< HEAD
+=======
+#include "runtime/current_thread.h"
+#include "runtime/runtime_state.h"
+>>>>>>> 98a6e260f7 ([BugFix] Fix crash when spill large string encode (#61495))
 #include "util/bit_util.h"
 #include "util/runtime_profile.h"
 
@@ -472,6 +477,8 @@ void PartitionedSpillerWriter::shuffle(std::vector<uint32_t>& dst, const SpillHa
 
 Status PartitionedSpillerWriter::spill_partition(workgroup::YieldContext& yield_ctx, SerdeContext& ctx,
                                                  SpilledPartition* partition) {
+    TRY_CATCH_ALLOC_SCOPE_START()
+
     auto mem_table = partition->spill_writer->mem_table();
     auto mem_table_mem_usage = mem_table->mem_usage();
     if (partition->spill_output_stream == nullptr) {
@@ -495,6 +502,7 @@ Status PartitionedSpillerWriter::spill_partition(workgroup::YieldContext& yield_
 
     mem_table->reset();
     TRACE_SPILL_LOG << fmt::format("spill partition[{}] done ", partition->debug_string());
+    TRY_CATCH_ALLOC_SCOPE_END()
     return Status::OK();
 }
 
