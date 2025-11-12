6d1a1036608beed4a775cf03a1f0755b14fc8a73
PengFei Li <lpengfei2016@gmail.com>
2025-11-12 13:55:52 +0800

    [BugFix] Fix load profile counters update duplicated (#65252)
    Signed-off-by: PengFei Li <lpengfei2016@gmail.com>

--- be/test/runtime/lake_tablets_channel_test.cpp
+++ be/test/runtime/lake_tablets_channel_test.cpp
@@ -953,18 +953,25 @@ TEST_F(LakeTabletsChannelTest, test_profile) {
     _tablets_channel->add_chunk(&chunk, add_chunk_request, &add_chunk_response, &close_channel);
     ASSERT_TRUE(add_chunk_response.status().status_code() == TStatusCode::OK);
     ASSERT_TRUE(close_channel);
-    _tablets_channel->update_profile();
 
-    auto* profile = _root_profile->get_child(fmt::format("Index (id={})", kIndexId));
-    ASSERT_NE(nullptr, profile);
-    ASSERT_EQ(1, profile->get_counter("OpenRpcCount")->value());
-    ASSERT_TRUE(profile->get_counter("OpenRpcTime")->value() > 0);
-    ASSERT_EQ(1, profile->get_counter("AddChunkRpcCount")->value());
-    ASSERT_TRUE(profile->get_counter("AddChunkRpcTime")->value() > 0);
-    ASSERT_EQ(chunk.num_rows(), profile->get_counter("AddRowNum")->value());
-    auto* replicas_profile = profile->get_child("PeerReplicas");
-    ASSERT_NE(nullptr, replicas_profile);
-    ASSERT_EQ(4, replicas_profile->get_counter("TabletsNum")->value());
+    // profile should be same if there is no new data no matter how many times we update the profile
+    for (int i = 0; i < 3; i++) {
+        _tablets_channel->update_profile();
+        auto* profile = _root_profile->get_child(fmt::format("Index (id={})", kIndexId));
+        ASSERT_NE(nullptr, profile);
+        ASSERT_EQ(1, profile->get_counter("OpenRpcCount")->value());
+        ASSERT_TRUE(profile->get_counter("OpenRpcTime")->value() > 0);
+        ASSERT_EQ(1, profile->get_counter("AddChunkRpcCount")->value());
+        ASSERT_TRUE(profile->get_counter("AddChunkRpcTime")->value() > 0);
+        ASSERT_EQ(chunk.num_rows(), profile->get_counter("AddRowNum")->value());
+        auto* replicas_profile = profile->get_child("PeerReplicas");
+        ASSERT_NE(nullptr, replicas_profile);
+        ASSERT_EQ(4, replicas_profile->get_counter("TabletsNum")->value());
+        ASSERT_EQ(8, replicas_profile->get_counter("WriterTaskCount")->value());
+        ASSERT_EQ(chunk.num_rows(), replicas_profile->get_counter("RowCount")->value());
+        ASSERT_EQ(4, replicas_profile->get_counter("MemtableInsertCount")->value());
+        ASSERT_EQ(4, replicas_profile->get_counter("MemtableFlushedCount")->value());
+    }
 }
 
 TEST_F(LakeTabletsChannelTest, test_missing_timeout_ms) {
