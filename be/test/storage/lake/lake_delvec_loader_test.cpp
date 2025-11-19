3584ad7e41c22dcefe31e6dd23d1dac5eef5e062
luohaha <18810541851@163.com>
2025-11-19 13:59:04 +0800

    update
    Signed-off-by: luohaha <18810541851@163.com>

--- be/test/storage/lake/lake_delvec_loader_test.cpp
+++ be/test/storage/lake/lake_delvec_loader_test.cpp
@@ -39,8 +39,7 @@ public:
         _location_provider = std::make_shared<FixedLocationProvider>(kTestDir);
         _mem_tracker = std::make_unique<MemTracker>(1024 * 1024);
         _update_manager = std::make_unique<lake::UpdateManager>(_location_provider, _mem_tracker.get());
-        _tablet_manager =
-                std::make_unique<lake::TabletManager>(_location_provider, _update_manager.get(), 1638400000);
+        _tablet_manager = std::make_unique<lake::TabletManager>(_location_provider, _update_manager.get(), 1638400000);
     }
 
     void TearDown() override { (void)FileSystem::Default()->delete_dir_recursive(kTestDir); }
