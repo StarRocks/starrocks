2f2d6d50ed544120855e52c2d63de7c62511d7a9
Murphy <96611012+murphyatwork@users.noreply.github.com>
2025-11-19 19:35:55 +0800

    [UT] fix thrift client test by using dynamic port (#65715)

--- be/test/util/thrift_client_test.cpp
+++ be/test/util/thrift_client_test.cpp
@@ -21,6 +21,7 @@
 #include <thrift/server/TThreadPoolServer.h>
 #include <thrift/transport/TServerSocket.h>
 #include <thrift/transport/TSocket.h>
+#include <unistd.h>
 
 #include <memory>
 
@@ -46,12 +47,14 @@ public:
         _thr->join();
     }
 
-    static inline const int MOCK_PORT = 18040;
+    int get_port() const { return _port; }
 
 private:
     std::unique_ptr<std::thread> _thr;
     std::shared_ptr<FrontendServiceProcessor> _processer;
     std::unique_ptr<apache::thrift::server::TSimpleServer> _server;
+    std::shared_ptr<apache::thrift::transport::TServerSocket> _server_transport;
+    int _port = 0;
 };
 
 void MockedFrontendService::init() {
@@ -62,14 +65,24 @@ void MockedFrontendService::init() {
     auto service = std::make_shared<MockedThriftService>();
     _processer = std::make_unique<FrontendServiceProcessor>(service);
 
-    auto serverTransport = std::make_shared<TServerSocket>(MOCK_PORT);
+    // Use port 0 to let the OS assign an available port
+    _server_transport = std::make_shared<TServerSocket>(0);
     auto transportFactory = std::make_shared<TBufferedTransportFactory>();
     auto protocolFactory = std::make_shared<TBinaryProtocolFactory>();
-    _server = std::make_unique<TSimpleServer>(_processer, serverTransport, transportFactory, protocolFactory);
+    _server = std::make_unique<TSimpleServer>(_processer, _server_transport, transportFactory, protocolFactory);
     _thr = std::make_unique<std::thread>([this]() { _server->serve(); });
     // thrift server don't provide a start function
-    // wait server ready
-    sleep(3);
+    // wait server ready and get the actual port that was assigned
+    // The port is assigned when the server starts listening
+    for (int i = 0; i < 30; ++i) {
+        _port = _server_transport->getPort();
+        if (_port > 0) {
+            break;
+        }
+        usleep(100000); // sleep 100ms
+    }
+    // Additional wait to ensure server is fully ready
+    sleep(1);
 }
 
 TEST(ThriftRpcClientCacheTest, test_all) {
@@ -79,7 +92,7 @@ TEST(ThriftRpcClientCacheTest, test_all) {
     TGetProfileRequest req;
 
     auto client_cache = std::make_unique<FrontendServiceClientCache>(config::max_client_cache_size_per_host);
-    TNetworkAddress address = make_network_address("127.0.0.1", MockedFrontendService::MOCK_PORT);
+    TNetworkAddress address = make_network_address("127.0.0.1", service.get_port());
     Status status;
     FrontendServiceConnection client(client_cache.get(), address, 1000, &status);
     ASSERT_OK(status);
