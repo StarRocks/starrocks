966574440d3026c3fefc54d2a631d621d4452c40
Wu Xueyang <wuxueyang.wxy@alibaba-inc.com>
2025-11-20 11:34:56 +0800

    [Enhancement] Add ngram index for the dictionary of inverted index.
    Signed-off-by: wuxueyang.wxy <wuxueyang.wxy@alibaba-inc.com>

--- be/src/storage/rowset/segment_iterator.cpp
+++ be/src/storage/rowset/segment_iterator.cpp
@@ -3303,6 +3303,9 @@ Status SegmentIterator::_apply_inverted_index() {
                 if (res.ok()) {
                     erased_preds.emplace(pred);
                     erased_pred_col_ids.emplace(cid);
+                } else {
+                    LOG(WARNING) << "Failed to seek inverted index for column " << column_name
+                                 << ", reason: " << res.detailed_message();
                 }
             }
         }
