ba8f8d3a8322eee2da2854bbf3a02ffbfb9d3057
xiaoxiaobinin <916352455@qq.com>
2025-11-21 13:21:31 +0800

    [fixBug]Refactor case statement for time zone types
    Signed-off-by: xiaoxiaobinin <916352455@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/connector/jdbc/ClickhouseSchemaResolver.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/jdbc/ClickhouseSchemaResolver.java
@@ -143,9 +143,7 @@ public class ClickhouseSchemaResolver extends JDBCSchemaResolver {
                     int scale = Integer.parseInt(precisionAndScale[1]);
                     return TypeFactory.createUnifiedDecimalType(precision, scale);
                 }
-            case Types.TIME_WITH_TIMEZONE:
-                // TIME_WITH_TIMEZONE
-            case Types.TIMESTAMP_WITH_TIMEZONE:
+            case Types.TIME_WITH_TIMEZONE, Types.TIMESTAMP_WITH_TIMEZONE::
                 return TypeFactory.createVarcharType(65533);
             default:
                 primitiveType = PrimitiveType.UNKNOWN_TYPE;
