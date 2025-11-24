22ca37dda95a841ae1ab39ad1a795a6ade89d394
Murphy <mofei@starrocks.com>
2025-11-24 13:50:11 +0800

    fix typo
    Signed-off-by: Murphy <mofei@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/statistic/sample/SampleInfo.java
+++ fe/fe-core/src/main/java/com/starrocks/statistic/sample/SampleInfo.java
@@ -145,7 +145,7 @@ public class SampleInfo {
                     .append(StringEscapeUtils.escapeSql(tableName)).append("'").append(sep);
             builder.append(addSingleQuote(dbName)).append(sep);
             builder.append(columnStats.getRowCount()).append(sep);
-            builder.append(columnStats.getDateSize()).append(sep);
+            builder.append(columnStats.getDataSize()).append(sep);
             builder.append(columnStats.getDistinctCount(0L)).append(sep);
             builder.append(columnStats.getNullCount()).append(sep);
             builder.append(columnStats.getMax()).append(sep);
@@ -277,7 +277,7 @@ public class SampleInfo {
                 .append(StringEscapeUtils.escapeSql(tableName)).append("'").append(sep);
         builder.append(addSingleQuote(dbName)).append(sep);
         builder.append(columnStats.getRowCount()).append(sep);
-        builder.append(columnStats.getDateSize()).append(sep);
+        builder.append(columnStats.getDataSize()).append(sep);
         builder.append(columnStats.getDistinctCount(rowSampleRatio)).append(sep);
         builder.append(columnStats.getNullCount()).append(sep);
         builder.append(columnStats.getMax()).append(sep);
