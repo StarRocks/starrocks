8c2b04efa1c44a6f49756c8d8e0a43565218822e
yan zhang <dirtysalt1987@gmail.com>
2025-11-25 12:01:22 +0800

    update files
    Signed-off-by: yan zhang <dirtysalt1987@gmail.com>

---
+++ fe/fe-core/src/main/java/com/starrocks/sql/ast/DropPartitionColumnClause.java
@@ -0,0 +1,43 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.starrocks.sql.ast;
+
+import com.starrocks.sql.ast.expression.Expr;
+import com.starrocks.sql.parser.NodePosition;
+
+// clause which is used to drop one partition column
+public class DropPartitionColumnClause implements ParseNode {
+    private final Expr partitionExpr;
+    private final NodePosition pos;
+
+    public Expr getPartitionExpr() {
+        return partitionExpr;
+    }
+
+    public DropPartitionColumnClause(Expr partitionExpr, NodePosition pos) {
+        this.partitionExpr = partitionExpr;
+        this.pos = pos;
+    }
+
+    @Override
+    public NodePosition getPos() {
+        return pos;
+    }
+
+    @Override
+    public <R, C> R accept(AstVisitor<R, C> visitor, C context) {
+        return ((AstVisitorExtendInterface<R, C>) visitor).visitDropPartitionColumnClause(this, context);
+    }
+}
