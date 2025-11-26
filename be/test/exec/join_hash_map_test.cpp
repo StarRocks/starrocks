ba1197b480ad86d3257ece4041e783914de34514
stdpain <34912776+stdpain@users.noreply.github.com>
2025-11-26 10:17:16 +0800

    [Enhancement] Accelerate the compilation speed of join_hash_map (#65918)
    Signed-off-by: stdpain <drfeng08@gmail.com>

--- be/test/exec/join_hash_map_test.cpp
+++ be/test/exec/join_hash_map_test.cpp
@@ -16,6 +16,12 @@
 
 #include <gtest/gtest.h>
 
+#include "exec/join/join_hash_map.hpp"
+#include "exec/join/join_hash_map_helper.h"
+#include "exec/join/join_hash_map_method.h"
+#include "exec/join/join_hash_map_method.hpp"
+#include "exec/join/join_hash_table.h"
+#include "exec/join/join_key_constructor.h"
 #include "runtime/descriptor_helper.h"
 #include "runtime/exec_env.h"
 #include "runtime/mem_tracker.h"
