a09d7c988040db8c30193a2c389d066e14db47a6
edwinhzhang <18700939@qq.com>
2025-11-27 20:59:32 +0800

    support is_json_scalar
    Signed-off-by: edwinhzhang <18700939@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/FunctionSet.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/FunctionSet.java
@@ -272,6 +272,8 @@ public class FunctionSet {
     public static final String GET_JSON_OBJECT = "get_json_object";
     public static final String JSON_LENGTH = "json_length";
     public static final String JSON_REMOVE = "json_remove";
+    public static final String IS_JSON_SCALAR = "is_json_scalar";
+
 
     // Matching functions:
     public static final String ILIKE = "ilike";
