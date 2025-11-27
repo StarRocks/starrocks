1a55e0d2322be5930b1df90f4e12124aeb08d393
before-Sunrise <unclejyj@gmail.com>
2025-11-27 11:02:58 +0800

    test
    Signed-off-by: before-Sunrise <unclejyj@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
@@ -3049,7 +3049,7 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
     private boolean enablePhasedScheduler = false;
 
     @VarAttr(name = ENABLE_SINGLE_NODE_SCHEDULE)
-    private boolean enableSingleNodeSchedule = true;
+    private boolean enableSingleNodeSchedule = false;
 
     @VarAttr(name = ENABLE_PIPELINE_EVENT_SCHEDULER)
     private boolean enablePipelineEventScheduler = true;
