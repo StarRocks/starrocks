d79229eae7de0a247fc0a2810122f289662c7f44
Yixin Luo <18810541851@163.com>
2025-11-28 01:12:09 +0800

    [BugFix] fix bundle meta parse failure when datacache corrupt (#66021)
    Signed-off-by: luohaha <18810541851@163.com>

--- be/src/storage/lake/tablet_manager.cpp
+++ be/src/storage/lake/tablet_manager.cpp
@@ -537,10 +537,10 @@ StatusOr<BundleTabletMetadataPtr> TabletManager::parse_bundle_tablet_metadata(co
     auto file_size = serialized_string.size();
     auto footer_size = sizeof(uint64_t);
     auto bundle_metadata_size = decode_fixed64_le((uint8_t*)(serialized_string.data() + file_size - footer_size));
-    RETURN_IF(file_size < footer_size + bundle_metadata_size,
+    RETURN_IF(file_size < footer_size + bundle_metadata_size || bundle_metadata_size == 0,
               Status::Corruption(strings::Substitute(
-                      "deserialized shared metadata($0) failed, file_size($1) < bundle_metadata_size($2)", path,
-                      file_size, bundle_metadata_size + footer_size)));
+                      "deserialized shared metadata($0) failed, file_size($1), bundle_metadata_size($2)", path,
+                      file_size, bundle_metadata_size)));
 
     auto bundle_metadata = std::make_shared<BundleTabletMetadataPB>();
     std::string_view bundle_metadata_str =
@@ -673,7 +673,10 @@ StatusOr<TabletMetadataPtr> TabletManager::get_single_tablet_metadata(int64_t ta
     auto metadata = std::make_shared<TabletMetadataPB>();
     std::string_view metadata_str = std::string_view(serialized_string.data() + offset);
     if (!metadata->ParseFromArray(metadata_str.data(), size)) {
-        return Status::Corruption(strings::Substitute("deserialized tablet $0 metadata failed", tablet_id));
+        auto corrupted_status =
+                Status::Corruption(strings::Substitute("deserialized tablet $0 metadata failed", tablet_id));
+        (void)corrupted_tablet_meta_handler(corrupted_status, path);
+        return corrupted_status;
     }
 
     FAIL_POINT_TRIGGER_EXECUTE(tablet_schema_not_found_in_bundle_metadata, { tablet_id = 10003; });
