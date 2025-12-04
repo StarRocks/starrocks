87a2b86545968cdbf3127b92500a85cddeec8a89
eyes_on_me <nopainnofame@sina.com>
2025-12-04 09:54:55 +0800

    [BugFix] fix query profile log of forward query from follower (#64395)
    Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>
    (cherry picked from commit 00c1bfae09e58a00615ee70a11824ace4a594b5e)

--- fe/fe-core/src/main/java/com/starrocks/qe/ConnectProcessor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/ConnectProcessor.java
@@ -983,6 +983,9 @@ public class ConnectProcessor {
                 executor = new StmtExecutor(ctx, statement);
             }
             ctx.setExecutor(executor);
+            if (ctx.getIsLastStmt()) {
+                executor.addRunningQueryDetail(statement);
+            }
             executor.setProxy();
             executor.execute();
         } catch (IOException e) {
@@ -995,6 +998,9 @@ public class ConnectProcessor {
             LOG.warn("Process one query failed because unknown reason: ", e);
             ctx.getState().setError(e.getMessage());
         } finally {
+            if (executor != null) {
+                executor.addFinishedQueryDetail();
+            }
             ctx.setExecutor(null);
         }
 
