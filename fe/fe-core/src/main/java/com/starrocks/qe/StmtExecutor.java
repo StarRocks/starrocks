3f02baef46fb7c03eaca996ac83ed2a776b90d9e
zihe.liu <ziheliu1024@gmail.com>
2025-12-08 14:09:07 +0800

    fix
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
@@ -664,8 +664,8 @@ public class StmtExecutor {
         }
 
         final boolean shouldMarkIdleCheck = shouldMarkIdleCheck(parsedStmt);
-        final long originWarehouseId = context.getCurrentWarehouseId();
-        if (shouldMarkIdleCheck) {
+        final Long originWarehouseId = context.getCurrentWarehouseIdAllowNull();
+        if (shouldMarkIdleCheck && originWarehouseId != null) {
             WarehouseIdleChecker.increaseRunningSQL(originWarehouseId);
         }
 
@@ -952,7 +952,7 @@ public class StmtExecutor {
             // restore session variable in connect context
             context.setSessionVariable(sessionVariableBackup);
 
-            if (shouldMarkIdleCheck) {
+            if (shouldMarkIdleCheck && originWarehouseId != null) {
                 WarehouseIdleChecker.decreaseRunningSQL(originWarehouseId);
             }
 
