ae26289eb9b965dfccd9203464fab076272fe8f4
andyziye <108652123+andyziye@users.noreply.github.com>
2025-12-08 11:48:59 +0800

    [Enhancement] support to alter iceberg table partiton spec(sync #65922) (#45098)
    * [Enhancement] support to alter iceberg table partiton spec
    
    * fix conflict
    
    Signed-off-by: yan zhang <dirtysalt1987@gmail.com>
    
    ---------
    
    Signed-off-by: yan zhang <dirtysalt1987@gmail.com>
    Co-authored-by: yan zhang <dirtysalt1987@gmail.com>

---
+++ fe/fe-core/src/main/java/com/starrocks/sql/ast/DropPartitionColumnClause.java
@@ -0,0 +1,39 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.starrocks.sql.ast;
+
+import com.starrocks.sql.ast.expression.Expr;
+import com.starrocks.sql.parser.NodePosition;
+
+import java.util.List;
+
+// clause which is used to drop one partition column
+public class DropPartitionColumnClause extends AlterTableClause {
+    private final List<Expr> partitionExprList;
+
+    public List<Expr> getPartitionExprList() {
+        return partitionExprList;
+    }
+
+    public DropPartitionColumnClause(List<Expr> partitionExprList, NodePosition pos) {
+        super(pos);
+        this.partitionExprList = partitionExprList;
+    }
+
+    @Override
+    public <R, C> R accept(AstVisitor<R, C> visitor, C context) {
+        return ((AstVisitorExtendInterface<R, C>) visitor).visitDropPartitionColumnClause(this, context);
+    }
+}
