28b6ab0c6367a481c1274497aecb2dbbbd2bac21
stdpain <34912776+stdpain@users.noreply.github.com>
2025-12-09 12:02:22 +0800

    [BugFix] avoid libhdfs crash when applied wrong JDK (backport #65882) (#66471)
    Signed-off-by: stdpain <34912776+stdpain@users.noreply.github.com>
    (cherry picked from commit ab8558fd3a92eea67522465eb1d3b1db26a5c2ce)
    
    # Conflicts:
    #	thirdparty/build-thirdparty.sh

--- build.sh
+++ build.sh
@@ -643,7 +643,6 @@ if [ ${BUILD_BE} -eq 1 ]; then
     if [ "${BUILD_JAVA_EXT}" == "ON" ]; then
         # note that conf files will not be overwritten when doing upgrade.
         # so we have to preserve directory structure to avoid upgrade incompatibility.
-        cp -r -p ${STARROCKS_THIRDPARTY}/installed/hadoop/lib/native ${STARROCKS_OUTPUT}/be/lib/hadoop/native
         cp -r -p ${STARROCKS_HOME}/java-extensions/hadoop-lib/target/hadoop-lib ${STARROCKS_OUTPUT}/be/lib/hadoop/common
         cp -r -p ${STARROCKS_HOME}/java-extensions/jdbc-bridge/target/starrocks-jdbc-bridge-jar-with-dependencies.jar ${STARROCKS_OUTPUT}/be/lib/jni-packages
         cp -r -p ${STARROCKS_HOME}/java-extensions/udf-extensions/target/udf-extensions-jar-with-dependencies.jar ${STARROCKS_OUTPUT}/be/lib/jni-packages
