88e6cce0b605a7bde467c7aff08a7138aeb90c6e
Pat Buxton <patrick.buxton@tartansolutions.com>
2025-12-09 09:46:47 +0000

    Allow disable type auto-detection on CSV Files
    Signed-off-by: Pat Buxton <patrick.buxton@tartansolutions.com>

--- gensrc/thrift/PlanNodes.thrift
+++ gensrc/thrift/PlanNodes.thrift
@@ -284,6 +284,7 @@ struct TBrokerScanRangeParams {
     31: optional i64 schema_sample_file_row_count
     32: optional bool flexible_column_mapping
     33: optional TFileScanType file_scan_type
+    34: optional bool schema_sample_types = true
 }
 
 // Broker scan range
