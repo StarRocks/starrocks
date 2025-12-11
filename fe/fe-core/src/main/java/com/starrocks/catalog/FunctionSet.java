f08832c64b8445128865e554b085499615bf2cf0
Arvind Kandpal <arvind.kandpal@ksolves.com>
2025-12-11 14:27:48 +0530

    [Feature] Add JSON_SET function (#66193)
    Signed-off-by: arvindksi274-ksolves <arvind.kandpal@ksolves.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/FunctionSet.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/FunctionSet.java
@@ -272,6 +272,7 @@ public class FunctionSet {
     public static final String GET_JSON_OBJECT = "get_json_object";
     public static final String JSON_LENGTH = "json_length";
     public static final String JSON_REMOVE = "json_remove";
+    public static final String JSON_SET = "json_set";
 
     // Variant functions:
     public static final String VARIANT_QUERY = "variant_query";
