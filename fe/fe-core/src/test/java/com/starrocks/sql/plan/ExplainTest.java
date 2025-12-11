25b2cd377435d90a053997fac87e474e71217fb4
crossoverJie <crossoverJie@gmail.com>
2025-12-11 14:19:18 +0800

    [Cleanup] Reorder import statements in ExplainTest.java
    Signed-off-by: crossoverJie <crossoverJie@gmail.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/ExplainTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/ExplainTest.java
@@ -17,9 +17,9 @@ package com.starrocks.sql.plan;
 import com.starrocks.common.Config;
 import com.starrocks.sql.Explain;
 import com.starrocks.sql.ast.QueryStatement;
-import org.junit.jupiter.api.Assertions;
 import com.starrocks.sql.ast.StatementBase;
 import com.starrocks.utframe.UtFrameUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ExplainTest extends PlanTestBase {
