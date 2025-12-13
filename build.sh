551009d651fabd3fead3e8ba2cf6f99c89999068
Kevin Cai <kevin.cai@celerdata.com>
2025-12-13 10:53:14 +0800

    [Tool] fix ccache sloppy when working with pch
    Signed-off-by: Kevin Cai <kevin.cai@celerdata.com>

--- build.sh
+++ build.sh
@@ -246,6 +246,7 @@ fi
 
 if [[ -z ${CCACHE} ]] && [[ -x "$(command -v ccache)" ]]; then
     CCACHE=ccache
+    export CCACHE_SLOPPINESS="pch_defines,time_macros"
 fi
 
 if [ -e /proc/cpuinfo ] ; then
