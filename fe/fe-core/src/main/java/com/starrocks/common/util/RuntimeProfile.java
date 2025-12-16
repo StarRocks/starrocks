43c2ea0750fff454c3ee120df5f5011d6dfb43ef
zhan7236 <76658920+zhan7236@users.noreply.github.com>
2025-12-16 06:55:15 +0000

    Fix: Add missing Optional import in RuntimeProfile.java
    Add the missing java.util.Optional import that was causing
    compilation failures in CI (FE Sonarcloud Check and FE UT).
    
    Signed-off-by: zhan7236 <76658920+zhan7236@users.noreply.github.com>

--- fe/fe-core/src/main/java/com/starrocks/common/util/RuntimeProfile.java
+++ fe/fe-core/src/main/java/com/starrocks/common/util/RuntimeProfile.java
@@ -62,6 +62,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Queue;
 import java.util.Set;
 import java.util.stream.Collectors;
