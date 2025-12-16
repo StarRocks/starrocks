6e748bdf70d1e1304727c26eecd2608c0230a901
Murphy <mofei@starrocks.com>
2025-12-16 19:14:46 +0800

    fix sql test
    Signed-off-by: Murphy <mofei@starrocks.com>

--- test/sql/test_aa_statistic_behavior/R/test_kill_analyze.sql
+++ test/sql/test_aa_statistic_behavior/R/test_kill_analyze.sql
@@ -5,17 +5,17 @@ CREATE TABLE test_kill_analyze (k1 int) properties("replication_num"="1");
 INSERT INTO test_kill_analyze VALUES(1);
 -- result:
 -- !result
-ADMIN ENABLE FAILPOINT 'statistic_executor_collect_statistics' WITH 3 TIMES ON FRONTEND;
+ADMIN ENABLE FAILPOINT 'statistic_executor_collect_statistics' WITH 1 TIMES ON FRONTEND;
 -- result:
 -- !result
-ANALYZE TABLE test_kill_analyze WITH ASYNC MODE;
+[UC]ANALYZE TABLE test_kill_analyze WITH ASYNC MODE;
 -- result:
-test_db_4da81432731c478cb2f92d4f2c2d7f16.test_kill_analyze	analyze	status	OK
+test_db_d97971d779144714a700ba1e4658d721.test_kill_analyze	analyze	status	OK
 -- !result
 SET @analyze_id = (SELECT cast(Id as int) FROM information_schema.analyze_status WHERE `Table` = 'test_kill_analyze' LIMIT 1);
 -- result:
 -- !result
-KILL ANALYZE @analyze_id;
+[UC]KILL ANALYZE @analyze_id;
 -- result:
-E: (1064, 'Getting analyzing error. Detail message: There is no running task with analyzeId 12177905.')
+E: (1064, 'Getting analyzing error. Detail message: There is no running task with analyzeId 12234897.')
 -- !result
