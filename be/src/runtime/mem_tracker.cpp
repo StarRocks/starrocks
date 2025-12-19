9ef8cc143472aa2afba4dd112c911b7e99cdaa6a
m.bogusz <m.bogusz@celonis.com>
2025-12-19 16:44:01 +0100

    formatting
    Signed-off-by: m.bogusz <m.bogusz@celonis.com>

--- be/src/runtime/mem_tracker.cpp
+++ be/src/runtime/mem_tracker.cpp
@@ -203,7 +203,8 @@ MemTracker::~MemTracker() {
 #ifdef DEBUG
     {
         std::unique_lock<std::mutex> lock(_child_trackers_lock);
-        DCHECK(_child_trackers.size() == 0) << err_msg("Child mem trackers have not been released, may cause corruption" );
+        DCHECK(_child_trackers.size() == 0)
+                << err_msg("Child mem trackers have not been released, may cause corruption");
     }
 #endif
 
