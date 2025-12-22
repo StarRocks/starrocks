c210e0e0b3c31909e8173289c9905339dd756f3a
silverbullet233 <3675229+silverbullet233@users.noreply.github.com>
2025-12-22 14:17:39 +0800

    fix
    Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/RewriteSimpleAggToMetaScanRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/RewriteSimpleAggToMetaScanRule.java
@@ -368,7 +368,7 @@ public class RewriteSimpleAggToMetaScanRule extends TransformationRule {
             // all aggregations can be replaced
             Preconditions.checkState(newAggCalls.isEmpty());
             LogicalValuesOperator row = new LogicalValuesOperator(scanOperator.getOutputColumns().subList(0, 1),
-                    List.of(List.of(ConstantOperator.createExampleValueByType(IntegerType.BIGINT))));
+                    List.of(List.of(ConstantOperator.createExampleValueByType(Type.BIGINT))));
             return Optional.of(OptExpression.create(project, OptExpression.create(row)));
         }
 
