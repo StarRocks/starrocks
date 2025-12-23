3f1c2b7488b1287d7d01083dfcacb9c18c6c95a0
kyle-goodale-klaviyo <kyle.goodale@klaviyo.com>
2025-12-23 06:02:27 -0500

    [BugFix] Fix infinite recursion in ReplaceColumnRefRewriter (#66974)
    Signed-off-by: kyle-goodale-klaviyo <kyle.goodale@klaviyo.com>
    (cherry picked from commit 30b65e4b8a747e36ac27091dc34b99220358a98a)
    
    # Conflicts:
    #	fe/fe-core/src/test/java/com/starrocks/sql/optimizer/rewrite/ReplaceColumnRefRewriterTest.java

--- fe/fe-core/src/test/java/com/starrocks/sql/optimizer/rewrite/ReplaceColumnRefRewriterTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/optimizer/rewrite/ReplaceColumnRefRewriterTest.java
@@ -15,13 +15,30 @@
 
 package com.starrocks.sql.optimizer.rewrite;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+<<<<<<< HEAD
 import com.starrocks.analysis.BinaryType;
 import com.starrocks.catalog.Type;
+=======
+import com.starrocks.catalog.FunctionSet;
+import com.starrocks.sql.ast.expression.BinaryType;
+>>>>>>> 30b65e4b8a ([BugFix] Fix infinite recursion in ReplaceColumnRefRewriter (#66974))
 import com.starrocks.sql.optimizer.operator.scalar.BinaryPredicateOperator;
+import com.starrocks.sql.optimizer.operator.scalar.CallOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ColumnRefOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ConstantOperator;
+import com.starrocks.sql.optimizer.operator.scalar.LambdaFunctionOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ScalarOperator;
+<<<<<<< HEAD
+=======
+import com.starrocks.sql.optimizer.operator.scalar.SubfieldOperator;
+import com.starrocks.type.ArrayType;
+import com.starrocks.type.IntegerType;
+import com.starrocks.type.StructField;
+import com.starrocks.type.StructType;
+import com.starrocks.type.VarcharType;
+>>>>>>> 30b65e4b8a ([BugFix] Fix infinite recursion in ReplaceColumnRefRewriter (#66974))
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -75,7 +92,61 @@ public class ReplaceColumnRefRewriterTest {
         Assertions.assertEquals(columnRef1, result2);
     }
 
+    @Test
+    public void testLambdaFunctionWithRecursiveRewrite() {
+        // Reproduces StackOverflowError when lambda parameter is in operatorMap with isRecursively=true
+        // Scenario: array_map(x -> named_struct('my_field', x.my_field), ...)
+        ColumnRefOperator lambdaParam = createColumnRef(100, "x");
+        SubfieldOperator subfield = new SubfieldOperator(lambdaParam, VarcharType.VARCHAR,
+                Lists.newArrayList("my_field"));
+
+        StructType structType = new StructType(Lists.newArrayList(
+                new StructField("my_field", VarcharType.VARCHAR)
+        ));
+        CallOperator namedStruct = new CallOperator(
+                FunctionSet.NAMED_STRUCT,
+                structType,
+                Lists.newArrayList(
+                        ConstantOperator.createVarchar("my_field"),
+                        subfield
+                )
+        );
+
+        LambdaFunctionOperator lambda = new LambdaFunctionOperator(
+                Lists.newArrayList(lambdaParam),
+                namedStruct,
+                ArrayType.ARRAY_VARCHAR);
+
+        // Map lambda parameter to subfield expression containing the lambda param
+        Map<ColumnRefOperator, ScalarOperator> operatorMap = Maps.newHashMap();
+        operatorMap.put(lambdaParam, subfield);
+
+        ReplaceColumnRefRewriter rewriter = new ReplaceColumnRefRewriter(operatorMap, true);
+
+        try {
+            ScalarOperator rewritten = rewriter.rewrite(lambda);
+
+            // Verify lambda parameter is NOT replaced
+            Assertions.assertInstanceOf(LambdaFunctionOperator.class, rewritten);
+            LambdaFunctionOperator rewrittenLambda = (LambdaFunctionOperator) rewritten;
+            Assertions.assertEquals(1, rewrittenLambda.getRefColumns().size());
+            Assertions.assertEquals(lambdaParam.getId(), rewrittenLambda.getRefColumns().get(0).getId());
+
+            ScalarOperator rewrittenExpr = rewrittenLambda.getLambdaExpr();
+            Assertions.assertInstanceOf(CallOperator.class, rewrittenExpr);
+            CallOperator rewrittenNamedStruct = (CallOperator) rewrittenExpr;
+            ScalarOperator secondArg = rewrittenNamedStruct.getChild(1);
+            Assertions.assertInstanceOf(SubfieldOperator.class, secondArg);
+        } catch (StackOverflowError e) {
+            Assertions.fail("StackOverflowError occurred - lambda parameter was incorrectly replaced");
+        }
+    }
+
     ColumnRefOperator createColumnRef(int id) {
         return new ColumnRefOperator(id, Type.INT, "ref" + id, false);
+    }
+
+    ColumnRefOperator createColumnRef(int id, String name) {
+        return new ColumnRefOperator(id, IntegerType.INT, name, false);
     }
 }
