2d297fad937cb9127f22d803606d6175986d40c0
eric-liu <126835597+ericyangliu@users.noreply.github.com>
2025-12-23 02:00:54 -0800

    [BugFix] Fix CN crash when querying non-partitioned Iceberg tables with DATE predicates (#66864)
    Signed-off-by: Eric Liu <y.liu@fetchrewards.com>
    (cherry picked from commit 51213777bb83754735e6c72fa3937ee16ed4f013)

--- be/src/formats/parquet/column_converter.cpp
+++ be/src/formats/parquet/column_converter.cpp
@@ -597,7 +597,7 @@ Status parquet::Int32ToDateConverter::convert(const ColumnPtr& src, Column* dst)
     auto& src_null_data = src_nullable_column->null_column()->get_data();
     auto& dst_null_data = dst_nullable_column->null_column()->get_data();
 
-    size_t size = src_column->size();
+    size_t size = dst_null_data.size();
     memcpy(dst_null_data.data(), src_null_data.data(), size);
     for (size_t i = 0; i < size; i++) {
         dst_data[i]._julian = src_data[i] + date::UNIX_EPOCH_JULIAN;
@@ -621,7 +621,7 @@ Status Int32ToTimeConverter::convert(const ColumnPtr& src, Column* dst) {
     auto& src_null_data = src_nullable_column->null_column()->get_data();
     auto& dst_null_data = dst_nullable_column->null_column()->get_data();
 
-    size_t size = src_column->size();
+    size_t size = dst_null_data.size();
 
     for (size_t i = 0; i < size; i++) {
         dst_null_data[i] = src_null_data[i];
@@ -648,7 +648,7 @@ Status parquet::Int32ToDateTimeConverter::convert(const ColumnPtr& src, Column* 
     auto& src_null_data = src_nullable_column->null_column()->get_data();
     auto& dst_null_data = dst_nullable_column->null_column()->get_data();
 
-    size_t size = src_column->size();
+    size_t size = dst_null_data.size();
     for (size_t i = 0; i < size; i++) {
         dst_null_data[i] = src_null_data[i];
         if (!src_null_data[i]) {
@@ -687,7 +687,7 @@ Status Int96ToDateTimeConverter::convert(const ColumnPtr& src, Column* dst) {
     auto& src_null_data = src_nullable_column->null_column()->get_data();
     auto& dst_null_data = dst_nullable_column->null_column()->get_data();
 
-    size_t size = src_column->size();
+    size_t size = dst_null_data.size();
 
     auto fill_dst_fn = [&]<bool FAST_TZ>() {
         for (size_t i = 0; i < size; i++) {
@@ -788,7 +788,7 @@ Status Int64ToDateTimeConverter::convert(const ColumnPtr& src, Column* dst) {
     auto& src_null_data = src_nullable_column->null_column()->get_data();
     auto& dst_null_data = dst_nullable_column->null_column()->get_data();
 
-    size_t size = src_column->size();
+    size_t size = dst_null_data.size();
     auto fill_dst_fn = [&]<bool UTC_TO_TZ, bool FAST_TZ>() {
         for (size_t i = 0; i < size; i++) {
             dst_null_data[i] = src_null_data[i];
@@ -840,7 +840,7 @@ Status Int64ToTimeConverter::convert(const ColumnPtr& src, Column* dst) {
     auto& src_null_data = src_nullable_column->null_column()->get_data();
     auto& dst_null_data = dst_nullable_column->null_column()->get_data();
 
-    size_t size = src_column->size();
+    size_t size = dst_null_data.size();
 
     for (size_t i = 0; i < size; i++) {
         dst_null_data[i] = src_null_data[i];
