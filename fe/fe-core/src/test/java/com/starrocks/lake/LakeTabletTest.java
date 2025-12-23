dfc92b030d002705cf91783ec0f648183bc4c911
srlch <111035020+srlch@users.noreply.github.com>
2025-12-23 14:15:48 +0800

    [BugFix] Fix tablet range is null for LakeTablet when FE restart (#67117)
    Signed-off-by: srlch <linzichao@starrocks.com>

---
+++ fe/fe-core/src/test/java/com/starrocks/lake/LakeTabletTest.java
@@ -0,0 +1,44 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.starrocks.lake;
+
+import com.starrocks.persist.gson.GsonUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LakeTabletTest {
+
+    @Test
+    public void testSerialization() {
+        LakeTablet tablet = new LakeTablet(10001L);
+        tablet.setDataSize(100L);
+        tablet.setRowCount(10L);
+
+        String json = GsonUtils.GSON.toJson(tablet);
+        LakeTablet deserializedTablet = GsonUtils.GSON.fromJson(json, LakeTablet.class);
+
+        Assertions.assertEquals(tablet.getId(), deserializedTablet.getId());
+        Assertions.assertEquals(tablet.getDataSize(true), deserializedTablet.getDataSize(true));
+        Assertions.assertEquals(tablet.getRowCount(0), deserializedTablet.getRowCount(0));
+        Assertions.assertNotNull(deserializedTablet.getRange());
+    }
+
+    @Test
+    public void testDefaultConstructor() {
+        LakeTablet tablet = new LakeTablet();
+        Assertions.assertNotNull(tablet.getRange());
+    }
+}
+
