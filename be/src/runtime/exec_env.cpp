080dd23ec007957ddd5b4e75338db6ace6e561cf
Martin Bogusz <36134723+martinr0x@users.noreply.github.com>
2025-12-25 01:33:16 +0100

    [BugFix] Change deallocation order in exec_env->destroy to prevent Use-After-Free in MemTracker::~MemTracker (#67027)
    Signed-off-by: m.bogusz <m.bogusz@celonis.com>
    Signed-off-by: Martin Bogusz <36134723+martinr0x@users.noreply.github.com>
    Signed-off-by: alvin <115669851+alvin-celerdata@users.noreply.github.com>
    Co-authored-by: alvin <115669851+alvin-celerdata@users.noreply.github.com>
    (cherry picked from commit 44fcefb1978b3e74352caf5d43036ea808b58f0a)
    
    # Conflicts:
    #	be/src/runtime/exec_env.cpp

--- be/src/runtime/exec_env.cpp
+++ be/src/runtime/exec_env.cpp
@@ -819,6 +819,8 @@ void ExecEnv::destroy() {
     SAFE_DELETE(_query_rpc_pool);
     SAFE_DELETE(_datacache_rpc_pool);
     _load_rpc_pool.reset();
+    SAFE_DELETE(_stream_mgr);
+    SAFE_DELETE(_query_context_mgr);
     _workgroup_manager->destroy();
     _workgroup_manager.reset();
     SAFE_DELETE(_thread_pool);
@@ -829,7 +831,6 @@ void ExecEnv::destroy() {
         _lake_tablet_manager->prune_metacache();
     }
 
-    SAFE_DELETE(_query_context_mgr);
     // WorkGroupManager should release MemTracker of WorkGroups belongs to itself before deallocate
     // _query_pool_mem_tracker.
     SAFE_DELETE(_runtime_filter_cache);
@@ -840,7 +841,11 @@ void ExecEnv::destroy() {
     SAFE_DELETE(_backend_client_cache);
     SAFE_DELETE(_result_queue_mgr);
     SAFE_DELETE(_result_mgr);
+<<<<<<< HEAD
     SAFE_DELETE(_stream_mgr);
+=======
+    SAFE_DELETE(_lookup_dispatcher_mgr);
+>>>>>>> 44fcefb197 ([BugFix] Change deallocation order in exec_env->destroy to prevent Use-After-Free in MemTracker::~MemTracker (#67027))
     SAFE_DELETE(_batch_write_mgr);
     SAFE_DELETE(_external_scan_context_mgr);
     SAFE_DELETE(_lake_tablet_manager);
