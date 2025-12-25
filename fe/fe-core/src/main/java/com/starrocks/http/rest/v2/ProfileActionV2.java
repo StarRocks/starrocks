a58535ab01219fc298f0254e7b2058c68503360a
zhaohehuhu <luoyedeyi@163.com>
2025-12-25 16:38:55 +0800

    reformat
    Signed-off-by: zhaohehuhu <luoyedeyi@163.com>

--- fe/fe-core/src/main/java/com/starrocks/http/rest/v2/ProfileActionV2.java
+++ fe/fe-core/src/main/java/com/starrocks/http/rest/v2/ProfileActionV2.java
@@ -24,7 +24,6 @@ import com.starrocks.http.rest.RestBaseAction;
 import com.starrocks.persist.gson.GsonUtils;
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpResponseStatus;
-import org.json.JSONException;
 
 import java.util.List;
 
@@ -69,18 +68,19 @@ public class ProfileActionV2 extends RestBaseAction {
             // If the query profile is not found in the local fe's ProfileManager,
             // we will query other frontend nodes to get the query profile.
             String queryPath = String.format(QUERY_PLAN_URI, queryId);
-            List<String> profileList = fetchResultFromOtherFrontendNodes(queryPath, authorization, HttpMethod.GET, false);
+            List<String> profileList =
+                    fetchResultFromOtherFrontendNodes(queryPath, authorization, HttpMethod.GET, false);
             for (String profile : profileList) {
                 if (profile != null) {
-                        RestBaseResultV2<String> queryProfileResult = GsonUtils.GSON.fromJson(
-                                profile,
-                                new TypeToken<RestBaseResultV2<String>>() {
-                                }.getType());
-                        if (queryProfileResult == null || queryProfileResult.getResult() == null) {
-                            continue;
-                        }
-                        sendSuccessResponse(response, queryProfileResult.getResult(), request);
-                        return;
+                    RestBaseResultV2<String> queryProfileResult = GsonUtils.GSON.fromJson(
+                            profile,
+                            new TypeToken<RestBaseResultV2<String>>() {
+                            }.getType());
+                    if (queryProfileResult == null || queryProfileResult.getResult() == null) {
+                        continue;
+                    }
+                    sendSuccessResponse(response, queryProfileResult.getResult(), request);
+                    return;
                 }
             }
         }
