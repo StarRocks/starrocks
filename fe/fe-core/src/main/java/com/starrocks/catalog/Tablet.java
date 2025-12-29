0d66202ad1475bb2e2eb9d392f4eae35ee98eea2
Kevin Cai <kevin.cai@celerdata.com>
2025-12-29 17:10:43 +0800

    [Enhancement] batch retrieve LakeTablet location info during physical planning
    * Batch retrieve tablet location info from StarOSAgent during physical planning
      in OlapScanNode/MetaScanNode to reduce RPC overhead in shared-data mode.
    * Previously, location info was retrieved one by one for each tablet, which could
      be slow for queries involving many tablets. This change introduces
      `getAllComputeNodeIdsAssignToTablets` in `WarehouseManager` and `StarOSAgent`
      to fetch locations in batch. Also updated `Tablet` interface and `LakeTablet`
      implementation to support passing pre-fetched location info.
    
    Signed-off-by: Kevin Cai <kevin.cai@celerdata.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/Tablet.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/Tablet.java
@@ -79,7 +79,7 @@ public abstract class Tablet extends MetaObject implements Writable {
 
     public abstract void getQueryableReplicas(List<Replica> allQuerableReplicas, List<Replica> localReplicas,
                                               long visibleVersion, long localBeId, int schemaHash,
-                                              ComputeResource computeResource);
+                                              ComputeResource computeResource, List<Long> locations);
 
     @Override
     public String toString() {
