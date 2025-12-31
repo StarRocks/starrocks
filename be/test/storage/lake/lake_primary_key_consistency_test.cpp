6fb45d894c50daed994ebb396ba85a3a786f7240
luohaha <18810541851@163.com>
2025-12-31 16:53:56 +0800

    [BugFix] local pk index doesn't support readonly execution mode
    Signed-off-by: luohaha <18810541851@163.com>

--- be/test/storage/lake/lake_primary_key_consistency_test.cpp
+++ be/test/storage/lake/lake_primary_key_consistency_test.cpp
@@ -737,7 +737,8 @@ TEST_P(LakePrimaryKeyConsistencyTest, test_random_seed_pk_consistency) {
 }
 
 INSTANTIATE_TEST_SUITE_P(LakePrimaryKeyConsistencyTest, LakePrimaryKeyConsistencyTest,
-                         ::testing::Values(PrimaryKeyParam{
-                                 .persistent_index_type = PersistentIndexTypePB::CLOUD_NATIVE}));
+                         ::testing::Values(PrimaryKeyParam{.persistent_index_type = PersistentIndexTypePB::LOCAL},
+                                           PrimaryKeyParam{
+                                                   .persistent_index_type = PersistentIndexTypePB::CLOUD_NATIVE}));
 
 } // namespace starrocks::lake
