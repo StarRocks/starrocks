f12e99a84cfa0f6d7a3400e86c793f1d20de2910
Gavin <ygsmangyuan@gmail.com>
2026-01-09 18:02:39 +0800

    [Enhancement] Prioritize using the "compression_codec" table property in connector sink modules. (#67205)
    Signed-off-by: Gavin <ygsmangyuan@gmail.com>
    Signed-off-by: 絵空事スピリット <wanglichen@starrocks.com>
    Co-authored-by: 絵空事スピリット <wanglichen@starrocks.com>
    (cherry picked from commit f0f10cddbfb6b1bcc7f88e7c4fc80df9ca568445)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/planner/IcebergTableSink.java

--- fe/fe-core/src/main/java/com/starrocks/planner/IcebergTableSink.java
+++ fe/fe-core/src/main/java/com/starrocks/planner/IcebergTableSink.java
@@ -15,7 +15,10 @@
 package com.starrocks.planner;
 
 import com.google.common.base.Preconditions;
+<<<<<<< HEAD
 import com.starrocks.analysis.TupleDescriptor;
+=======
+>>>>>>> f0f10cddbf ([Enhancement] Prioritize using the "compression_codec" table property in connector sink modules. (#67205))
 import com.starrocks.catalog.IcebergTable;
 import com.starrocks.connector.CatalogConnector;
 import com.starrocks.connector.iceberg.IcebergUtil;
@@ -35,6 +38,7 @@ import org.apache.iceberg.Table;
 import static com.starrocks.analysis.OutFileClause.PARQUET_COMPRESSION_TYPE_MAP;
 import static org.apache.iceberg.TableProperties.DEFAULT_FILE_FORMAT;
 import static org.apache.iceberg.TableProperties.DEFAULT_FILE_FORMAT_DEFAULT;
+import static org.apache.iceberg.TableProperties.PARQUET_COMPRESSION;
 
 public class IcebergTableSink extends DataSink {
     public final static int ICEBERG_SINK_MAX_DOP = 32;
@@ -61,7 +65,8 @@ public class IcebergTableSink extends DataSink {
         this.isStaticPartitionSink = isStaticPartitionSink;
         this.fileFormat = nativeTable.properties().getOrDefault(DEFAULT_FILE_FORMAT, DEFAULT_FILE_FORMAT_DEFAULT)
                 .toLowerCase();
-        this.compressionType = sessionVariable.getConnectorSinkCompressionCodec();
+        this.compressionType = nativeTable.properties().getOrDefault(PARQUET_COMPRESSION,
+                sessionVariable.getConnectorSinkCompressionCodec());
         this.targetMaxFileSize = sessionVariable.getConnectorSinkTargetMaxFileSize() > 0 ?
             sessionVariable.getConnectorSinkTargetMaxFileSize() : 1024L * 1024 * 1024;
         this.targetBranch = targetBranch;
@@ -110,6 +115,7 @@ public class IcebergTableSink extends DataSink {
         tIcebergTableSink.setFile_format(fileFormat);
         tIcebergTableSink.setIs_static_partition_sink(isStaticPartitionSink);
         TCompressionType compression = PARQUET_COMPRESSION_TYPE_MAP.get(compressionType);
+        Preconditions.checkState(compression != null, "compression type not supported");
         tIcebergTableSink.setCompression_type(compression);
         tIcebergTableSink.setTarget_max_file_size(targetMaxFileSize);
         TCloudConfiguration tCloudConfiguration = new TCloudConfiguration();
