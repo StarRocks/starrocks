151a3b8b810bf7fc7ecc96e7b784e54f68142f62
GavinMar <yangguansuo@starrocks.com>
2026-01-14 12:11:09 +0800

    [BugFix] Fix the unittests which use the non-exist functions for iceberg partition chunk writer.
    Signed-off-by: GavinMar <yangguansuo@starrocks.com>

--- be/test/connector_sink/partition_chunk_writer_test.cpp
+++ be/test/connector_sink/partition_chunk_writer_test.cpp
@@ -1114,10 +1114,9 @@ TEST_F(PartitionChunkWriterTest, test_connector_sink_profile_metrics) {
         // Write some chunks
         for (size_t i = 0; i < 3; ++i) {
             ChunkPtr chunk = ChunkHelper::new_chunk(*tuple_desc, 3);
-            chunk->get_column_raw_ptr_by_index(0)->append_datum(Slice("ccc" + std::to_string(i)));
-            chunk->get_column_raw_ptr_by_index(0)->append_datum(Slice("bbb" + std::to_string(i)));
-            chunk->get_column_raw_ptr_by_index(0)->append_datum(Slice("aaa" + std::to_string(i)));
-
+            chunk->get_column_by_index(0)->append_datum(Slice("ccc" + std::to_string(i)));
+            chunk->get_column_by_index(0)->append_datum(Slice("bbb" + std::to_string(i)));
+            chunk->get_column_by_index(0)->append_datum(Slice("aaa" + std::to_string(i)));
             auto ret = partition_writer->write(chunk);
             EXPECT_OK(ret);
         }
@@ -1211,10 +1210,9 @@ TEST_F(PartitionChunkWriterTest, test_buffer_partition_writer_profile_metrics) {
         // Write some chunks
         for (size_t i = 0; i < 2; ++i) {
             ChunkPtr chunk = ChunkHelper::new_chunk(*tuple_desc, 3);
-            chunk->get_column_raw_ptr_by_index(0)->append_datum(Slice("data" + std::to_string(i)));
-            chunk->get_column_raw_ptr_by_index(0)->append_datum(Slice("data" + std::to_string(i)));
-            chunk->get_column_raw_ptr_by_index(0)->append_datum(Slice("data" + std::to_string(i)));
-
+            chunk->get_column_by_index(0)->append_datum(Slice("data" + std::to_string(i)));
+            chunk->get_column_by_index(0)->append_datum(Slice("data" + std::to_string(i)));
+            chunk->get_column_by_index(0)->append_datum(Slice("data" + std::to_string(i)));
             auto ret = partition_writer->write(chunk);
             EXPECT_OK(ret);
         }
