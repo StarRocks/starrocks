e60628a97ebf3944459d00ead1657d883f697f67
j1wonpark <jpark92@outlook.kr>
2026-01-14 09:20:11 +0900

    Fix conflicts
    Signed-off-by: j1wonpark <jpark92@outlook.kr>

--- fe/fe-core/src/main/java/com/starrocks/connector/iceberg/io/IcebergCachingFileIO.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/iceberg/io/IcebergCachingFileIO.java
@@ -105,7 +105,7 @@ public class IcebergCachingFileIO implements FileIO, HadoopConfigurable {
     public static final long DISK_CACHE_EXPIRATION_SECONDS = Config.iceberg_metadata_disk_cache_expiration_seconds;
 
     private transient ContentCache fileContentCache;
-    private FileIO wrappedIO;
+    private ResolvingFileIO wrappedIO;
     private SerializableSupplier<Configuration> conf;
     private static final Pattern HADOOP_CATALOG_METADATA_JSON_PATTERN =
             Pattern.compile("^v\\d+(\\.gz)?\\.metadata\\.json(\\.gz)?$");
@@ -113,7 +113,6 @@ public class IcebergCachingFileIO implements FileIO, HadoopConfigurable {
     @Override
     public void initialize(Map<String, String> properties) {
         ResolvingFileIO resolvingFileIO = new ResolvingFileIO();
-        resolvingFileIO.setConf(conf.get());
         wrappedIO = resolvingFileIO;
         wrappedIO.initialize(properties);
 
