675894fd563d02ff066963bace9f50a6144342f2
shuming.li <ming.moriarty@gmail.com>
2026-01-17 01:40:25 +0800

    [BugFix] [FollowUp] Fix low cardinality bug with more lamda function (#67976)
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>
    (cherry picked from commit 7b13dddc2f77c0f7ce2985e8882935e86715e251)
    
    # Conflicts:
    #	be/src/exprs/array_sort_lambda_expr.cpp
    #	be/src/exprs/array_sort_lambda_expr.h

--- be/src/exprs/array_sort_lambda_expr.h
+++ be/src/exprs/array_sort_lambda_expr.h
@@ -14,48 +14,53 @@
 
 #pragma once
 
-#include <memory>
-#include <mutex>
-#include <unordered_map>
+#include <unordered_set>
 
 #include "column/nullable_column.h"
 #include "common/global_types.h"
 #include "common/object_pool.h"
-#include "exprs/column_ref.h"
 #include "exprs/expr.h"
-#include "glog/logging.h"
 
 namespace starrocks {
+// array_sort_lambda(array, lambda function)
+// Sorts array using a custom lambda comparator function
 
-// array_map(lambda function, array0, array1...)
-
-class ArrayMapExpr final : public Expr {
+class ArraySortLambdaExpr final : public Expr {
 public:
-    ArrayMapExpr(const TExprNode& node);
+    ArraySortLambdaExpr(const TExprNode& node);
 
     // for tests
-    explicit ArrayMapExpr(TypeDescriptor type);
+    explicit ArraySortLambdaExpr(TypeDescriptor type);
 
     Status prepare(RuntimeState* state, ExprContext* context) override;
+
     Status open(RuntimeState* state, ExprContext* context, FunctionContext::FunctionStateScope scope) override;
+
     void close(RuntimeState* state, ExprContext* context, FunctionContext::FunctionStateScope scope) override;
-    Expr* clone(ObjectPool* pool) const override { return pool->add(new ArrayMapExpr(*this)); }
+
+    Expr* clone(ObjectPool* pool) const override { return pool->add(new ArraySortLambdaExpr(*this)); }
 
     StatusOr<ColumnPtr> evaluate_checked(ExprContext* context, Chunk* ptr) override;
+
     std::string debug_string() const override;
+
     int get_slot_ids(std::vector<SlotId>* slot_ids) const override;
 
     Status do_for_each_child(const std::function<Status(Expr*)>& callback) override;
 
 private:
-    template <bool all_const_input, bool independent_lambda_expr>
-    StatusOr<ColumnPtr> evaluate_lambda_expr(ExprContext* context, Chunk* chunk, const Columns& arguments,
-                                             const NullColumnPtr& null_column);
+    StatusOr<ColumnPtr> evaluate_lambda_expr(ExprContext* context, Chunk* chunk, const Column* data_column);
 
+    Status validate_strict_weak_ordering(ExprContext* context, Chunk* chunk, const ArrayColumn* array_col);
+
+    Status check_lambda_only_depends_on_args(const LambdaFunction* lambda_function);
     // use map to make sure the order of execution
     std::map<SlotId, Expr*> _outer_common_exprs;
     // the slots initially required for lambda function evaluation, excluding lambda arguments,
     // other common expressions can be evaluated based on these slots.
     std::unordered_set<SlotId> _initial_required_slots;
+    // Cache for strict weak ordering validation result to avoid duplicate checks
+    mutable bool _comparator_validated = false;
+    mutable Status _comparator_validation_status = Status::OK();
 };
 } // namespace starrocks
