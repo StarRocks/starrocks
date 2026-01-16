642e5de68c9e3167862c541c37811a19a6b5a966
stdpain <34912776+stdpain@users.noreply.github.com>
2026-01-16 17:37:48 +0800

    [BugFix] Fix ScopedTimer cause Concurrency Exception (#67913)
    (cherry picked from commit 2615ed7c0eace8c3058aee2c86c472d3f26594fc)

--- fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
@@ -70,6 +70,7 @@ import com.starrocks.common.StarRocksException;
 import com.starrocks.common.Status;
 import com.starrocks.common.TimeoutException;
 import com.starrocks.common.Version;
+import com.starrocks.common.profile.RawScopedTimer;
 import com.starrocks.common.profile.Timer;
 import com.starrocks.common.profile.Tracers;
 import com.starrocks.common.util.CompressionUtils;
@@ -1562,8 +1563,9 @@ public class StmtExecutor {
         } else if (isSchedulerExplain) {
             // Do nothing.
         } else if (isExplainQuery) {
-            String explainString = buildExplainString(execPlan, parsedStmt, context, ResourceGroupClassifier.QueryType.SELECT,
-                    parsedStmt.getExplainLevel());
+            String explainString =
+                    buildExplainString(execPlan, parsedStmt, context, ResourceGroupClassifier.QueryType.SELECT,
+                            parsedStmt.getExplainLevel());
             if (executeInFe) {
                 explainString = "EXECUTE IN FE\n" + explainString;
             }
@@ -1638,6 +1640,7 @@ public class StmtExecutor {
             // 2. If this is a query, send the result expr fields first, and send result data back to client.
             MysqlChannel channel = context.getMysqlChannel();
             boolean isSendFields = false;
+            final RawScopedTimer rawScopedTimer = new RawScopedTimer();
             do {
                 batch = coord.getNext();
                 // for outfile query, there will be only one empty batch send back with eos flag
@@ -1645,7 +1648,7 @@ public class StmtExecutor {
                     // For some language driver, getting error packet after fields packet will be recognized as a success result
                     // so We need to send fields after first batch arrived
                     if (!isSendFields) {
-                        responseFields(colNames, outputExprs);
+                        responseFields(rawScopedTimer, colNames, outputExprs);
                         isSendFields = true;
                     }
                     if (!isProxy && channel.isSendBufferNull()) {
@@ -1657,7 +1660,7 @@ public class StmtExecutor {
                         channel.initBuffer(bufferSize + 8);
                     }
 
-                    responseRowBatch(batch, channel);
+                    responseRowBatch(rawScopedTimer, batch, channel);
                 }
                 if (batch.getBatch() != null) {
                     context.updateReturnRows(batch.getBatch().getRows().size());
@@ -1665,17 +1668,18 @@ public class StmtExecutor {
             } while (!batch.isEos());
 
             if (!isSendFields && !isOutfileQuery && !isExplainAnalyze && !isPlanAdvisorAnalyze && !isArrowFlight) {
-                responseFields(colNames, outputExprs);
+                responseFields(rawScopedTimer, colNames, outputExprs);
             }
-            final Long res = Tracers.getSpecifiedTimer("clientResponse").map(Timer::getTotalTime).orElse(0L);
-            context.getAuditEventBuilder().setWriteClientTimeMs(res);
+            
+            context.getAuditEventBuilder().setWriteClientTimeMs(rawScopedTimer.getTotalTime());
         }
 
         processQueryStatisticsFromResult(batch, execPlan, isOutfileQuery);
         GlobalStateMgr.getCurrentState().getQueryHistoryMgr().addQueryHistory(context, execPlan);
     }
-    private void responseRowBatch(RowBatch batch, MysqlChannel channel) throws IOException {
-        try (final Timer ignore = Tracers.watchScope(Tracers.Module.CLIENT, "clientResponse")) {
+
+    private void responseRowBatch(RawScopedTimer timer, RowBatch batch, MysqlChannel channel) throws IOException {
+        try (final RawScopedTimer.Guard ignore = timer.start()) {
             for (ByteBuffer row : batch.getBatch().getRows()) {
                 if (isProxy) {
                     proxyResultBuffer.add(row);
@@ -1686,8 +1690,8 @@ public class StmtExecutor {
         }
     }
 
-    private void responseFields(List<String> colNames, List<Expr> exprs) throws IOException {
-        try (final Timer ignore = Tracers.watchScope(Tracers.Module.CLIENT, "clientResponse")) {
+    private void responseFields(RawScopedTimer timer, List<String> colNames, List<Expr> exprs) throws IOException {
+        try (final RawScopedTimer.Guard ignore = timer.start()) {
             sendFields(colNames, exprs);
         }
     }
@@ -2644,7 +2648,7 @@ public class StmtExecutor {
         Database db =
                 GlobalStateMgr.getCurrentState().getMetadataMgr().getDb(context, tableRef.getCatalogName(), tableRef.getDbName());
         if (db == null) {
-            String catalogAndDb = tableRef.getCatalogName() != null ? 
+            String catalogAndDb = tableRef.getCatalogName() != null ?
                     tableRef.getCatalogName() + "." + tableRef.getDbName() : tableRef.getDbName();
             throw new SemanticException("Database %s is not found", catalogAndDb);
         }
