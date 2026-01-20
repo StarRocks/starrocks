b6df95b45a00d0c8250d89601534b73798c46e0b
Soungmin Son (Eddy) <41810556+happycastle114@users.noreply.github.com>
2026-01-20 15:41:10 +0900

    [BugFix] Stop StarMgr journal writer before BDB environment close during leader transfer (#68036)

--- fe/fe-core/src/main/java/com/starrocks/staros/StarOSBDBJEJournalSystem.java
+++ fe/fe-core/src/main/java/com/starrocks/staros/StarOSBDBJEJournalSystem.java
@@ -236,5 +236,11 @@ public class StarOSBDBJEJournalSystem implements JournalSystem {
     public JournalWriter getJournalWriter() {
         return journalWriter;
     }
+
+    public void markLeaderTransferred() {
+        if (journalWriter != null) {
+            journalWriter.setLeaderTransferred();
+        }
+    }
 }
 
