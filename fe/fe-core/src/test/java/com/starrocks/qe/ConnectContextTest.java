9f6701a38aeaaa504006744ecf45fb53675f2221
shuming.li <ming.moriarty@gmail.com>
2026-01-23 11:14:26 +0800

    [BugFix] Fix query queue allocation time and pending timeout (#65802)
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>
    (cherry picked from commit 5c97ea4fe70db4ff6688ffbfc17bd4f60bb8596d)
    
    # Conflicts:
    #	fe/fe-core/src/test/java/com/starrocks/qe/ConnectContextTest.java

--- fe/fe-core/src/test/java/com/starrocks/qe/ConnectContextTest.java
+++ fe/fe-core/src/test/java/com/starrocks/qe/ConnectContextTest.java
@@ -745,6 +745,7 @@ public class ConnectContextTest {
         // Should not throw exception even if getting CN group name fails
         Assertions.assertDoesNotThrow(() -> ctx.onQueryFinished());
     }
+<<<<<<< HEAD
 
     // -----------------------------------------------------------------------
     // Tests for changeCatalogDb() journal-replay wait on follower FE
@@ -1028,4 +1029,6 @@ public class ConnectContextTest {
         // The timeout exception is caught internally; ERR_BAD_DB_ERROR is still thrown
         Assertions.assertThrows(DdlException.class, () -> ctx.changeCatalogDb("nonexistent"));
     }
+=======
+>>>>>>> 5c97ea4fe7 ([BugFix] Fix query queue allocation time and pending timeout (#65802))
 }
