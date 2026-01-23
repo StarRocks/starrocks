dda85584fe54480563fe5a48281a9d5ceeb097d3
Yaqi Zhang <y.zhang@celonis.com>
2026-01-23 11:44:19 -0600

    [Refactor] Remove an outdated TODO in function_context.h (#68111)
    Signed-off-by: Yaqi Zhang <y.zhang@celonis.com>

--- be/src/exprs/function_context.h
+++ be/src/exprs/function_context.h
@@ -57,7 +57,6 @@ public:
         /// concurrently on a single host if the UDF will be evaluated in multiple plan
         /// fragments on that host. In general, read-only state that doesn't need to be
         /// recomputed for every UDF call should be fragment-local.
-        /// TODO: not yet implemented
         FRAGMENT_LOCAL,
 
         /// Indicates that the function state is local to the execution thread. This state
