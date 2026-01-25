954afe00d81bbf1ee0811df7cbd5b3827b524b51
yan zhang <dirtysalt1987@gmail.com>
2026-01-25 15:44:18 +0800

    fix conflict
    Signed-off-by: yan zhang <dirtysalt1987@gmail.com>

--- fe/fe-core/src/test/java/com/starrocks/connector/hive/MockedHiveMetadata.java
+++ fe/fe-core/src/test/java/com/starrocks/connector/hive/MockedHiveMetadata.java
@@ -880,7 +880,7 @@ public class MockedHiveMetadata implements ConnectorMetadata {
                     ImmutableList.of(new FieldSchema("l_shipdate", "Date", null)), Maps.newHashMap(),
                     null, null, "EXTERNAL_TABLE");
 
-            Column partitionColumn = new Column("l_shipdate", DateType.DATE);
+            Column partitionColumn = new Column("l_shipdate", Type.DATE);
 
             List<PartitionKey> lineitemPartitionKeyList = Lists.newArrayList();
             lineitemPartitionKeyList.add(
@@ -929,7 +929,7 @@ public class MockedHiveMetadata implements ConnectorMetadata {
                     ImmutableList.of(new FieldSchema("l_shipdate", "Date", null)), Maps.newHashMap(),
                     null, null, "EXTERNAL_TABLE");
 
-            Column partitionColumn = new Column("l_shipdate", DateType.DATE);
+            Column partitionColumn = new Column("l_shipdate", Type.DATE);
 
             List<PartitionKey> lineitemPartitionKeyList = Lists.newArrayList();
             lineitemPartitionKeyList.add(
