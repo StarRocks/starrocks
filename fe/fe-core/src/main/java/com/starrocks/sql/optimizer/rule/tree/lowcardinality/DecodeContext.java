d51c7cdf5e90684663f73633984f7af2ab8d1af0
farhad-celo <f.shahmohammadi@celonis.com>
2026-01-26 18:50:39 -0800

    [Enhancement] supporting dictification of union all with constants (#68302)
    Signed-off-by: Farhad Shahmohammadi <f.shahmohammadi@celonis.com>
    (cherry picked from commit 30af15ca796f2abc63d30c3b5a736a36f877862b)

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/tree/lowcardinality/DecodeContext.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/tree/lowcardinality/DecodeContext.java
@@ -97,6 +97,8 @@ class DecodeContext {
 
     Map<ScalarOperator, ScalarOperator> stringExprToDictExprMap = Maps.newHashMap();
 
+    UnionDictionaryManager unionDictionaryManager;
+
     DecodeContext(ColumnRefFactory factory) {
         this.factory = factory;
     }
