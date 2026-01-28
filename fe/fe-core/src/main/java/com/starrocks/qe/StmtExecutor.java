7968426bf6dc53f5cbe3af054660d8c2a0f3a3fb
stephen <91597003+stephen-shelby@users.noreply.github.com>
2026-01-28 10:50:19 +0800

    [BugFix] Fix ANALYZE TABLE UPDATE HISTOGRAM ON ALL COLUMNS (#68290)
    Signed-off-by: stephen <stephen5217@163.com>
    (cherry picked from commit af4b591831985a2cb2d69a264eedbf31d9d58701)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
    #	test/sql/test_analyze_statistics/R/test_histogram
    #	test/sql/test_analyze_statistics/T/test_histogram

--- fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
@@ -1763,9 +1763,20 @@ public class StmtExecutor {
         StatisticExecutor statisticExecutor = new StatisticExecutor();
         if (analyzeStmt.isExternal()) {
             if (analyzeTypeDesc.isHistogram()) {
+                List<com.starrocks.type.Type> columnTypes = analyzeStmt.getColumnTypes();
+                if (CollectionUtils.isEmpty(columnTypes) && CollectionUtils.isNotEmpty(analyzeStmt.getColumnNames())) {
+                    columnTypes = analyzeStmt.getColumnNames().stream()
+                            .map(col -> table.getColumn(col).getType())
+                            .collect(Collectors.toList());
+                }
                 statisticExecutor.collectStatistics(statsConnectCtx,
+<<<<<<< HEAD
                         new ExternalHistogramStatisticsCollectJob(analyzeStmt.getTableName().getCatalog(),
                                 db, table, analyzeStmt.getColumnNames(), analyzeStmt.getColumnTypes(),
+=======
+                        new ExternalHistogramStatisticsCollectJob(analyzeStmt.getCatalogName(),
+                                db, table, analyzeStmt.getColumnNames(), columnTypes,
+>>>>>>> af4b591831 ([BugFix] Fix ANALYZE TABLE UPDATE HISTOGRAM ON ALL COLUMNS (#68290))
                                 StatsConstants.AnalyzeType.HISTOGRAM, StatsConstants.ScheduleType.ONCE,
                                 analyzeStmt.getProperties()),
                         analyzeStatus,
@@ -1786,9 +1797,15 @@ public class StmtExecutor {
             }
         } else {
             if (analyzeTypeDesc.isHistogram()) {
+                List<com.starrocks.type.Type> columnTypes = analyzeStmt.getColumnTypes();
+                if (CollectionUtils.isEmpty(columnTypes) && CollectionUtils.isNotEmpty(analyzeStmt.getColumnNames())) {
+                    columnTypes = analyzeStmt.getColumnNames().stream()
+                            .map(col -> table.getColumn(col).getType())
+                            .collect(Collectors.toList());
+                }
                 statisticExecutor.collectStatistics(statsConnectCtx,
                         new HistogramStatisticsCollectJob(db, table, analyzeStmt.getColumnNames(),
-                                analyzeStmt.getColumnTypes(), StatsConstants.ScheduleType.ONCE,
+                                columnTypes, StatsConstants.ScheduleType.ONCE,
                                 analyzeStmt.getProperties()),
                         analyzeStatus,
                         // Sync load cache, auto-populate column statistic cache after Analyze table manually
