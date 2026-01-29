42578fe31a0e09b6b14f72e51ea23204b8473806
Youngwb <yangwenbo_mailbox@163.com>
2026-01-29 10:02:30 +0800

    [Enhancement] Manage the memory usage of the Iceberg delete sink writer with the SinkOperatorMemoryManager. (#68566)

--- be/src/connector/sink_memory_manager.h
+++ be/src/connector/sink_memory_manager.h
@@ -28,8 +28,8 @@ class SinkOperatorMemoryManager {
 public:
     SinkOperatorMemoryManager() = default;
 
-    void init(std::map<PartitionKey, PartitionChunkWriterPtr>* partition_chunk_writers,
-              AsyncFlushStreamPoller* io_poller, CommitFunc commit_func);
+    Status init(std::vector<PartitionChunkWriterPtr>* writers, AsyncFlushStreamPoller* io_poller,
+                CommitFunc commit_func);
 
     // return true if a victim is found and killed, otherwise return false
     bool kill_victim();
@@ -45,7 +45,7 @@ public:
     int64_t writer_occupied_memory() { return _writer_occupied_memory.load(); }
 
 private:
-    std::map<PartitionKey, PartitionChunkWriterPtr>* _candidates = nullptr; // reference, owned by sink operator
+    std::vector<PartitionChunkWriterPtr>* _candidates = nullptr; // reference, owned by sink operator
     CommitFunc _commit_func;
     AsyncFlushStreamPoller* _io_poller;
     std::atomic_int64_t _releasable_memory{0};
