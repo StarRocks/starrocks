341f5ea7c389a62bcb3c1392e707dd7e4056e627
farhad-celo <f.shahmohammadi@celonis.com>
2026-01-28 17:56:39 -0800

    [Enhancement] Supporting dictification for structs (#66713)
    Signed-off-by: Farhad Shahmohammadi <f.shahmohammadi@celonis.com>
    Signed-off-by: farhad-celo <f.shahmohammadi@celonis.com>
    (cherry picked from commit ee99b819eea87bb9f156622403f86f684439ba3c)

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/operator/scalar/DictMappingOperator.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/operator/scalar/DictMappingOperator.java
@@ -15,6 +15,7 @@
 
 package com.starrocks.sql.optimizer.operator.scalar;
 
+import com.google.common.base.Preconditions;
 import com.starrocks.catalog.Type;
 import com.starrocks.sql.optimizer.base.ColumnRefSet;
 import com.starrocks.sql.optimizer.operator.OperatorType;
@@ -64,16 +65,18 @@ public class DictMappingOperator extends ScalarOperator {
 
     @Override
     public List<ScalarOperator> getChildren() {
-        return Collections.emptyList();
+        return stringProvideOperator == null ? Collections.emptyList() : List.of(stringProvideOperator);
     }
 
     @Override
     public ScalarOperator getChild(int index) {
-        return null;
+        return index == 0 && stringProvideOperator != null ? stringProvideOperator : null;
     }
 
     @Override
     public void setChild(int index, ScalarOperator child) {
+        Preconditions.checkState(index == 0);
+        stringProvideOperator = child;
     }
 
     @Override
@@ -119,7 +122,13 @@ public class DictMappingOperator extends ScalarOperator {
 
     @Override
     public ColumnRefSet getUsedColumns() {
-        return dictColumn.getUsedColumns();
+        if (stringProvideOperator != null) {
+            // When stringProviderOperator is set, dictColumn and originScalaOperator are meta columns, only columns
+            // in stringProviderOperator are used in eval.
+            return stringProvideOperator.getUsedColumns();
+        } else {
+            return dictColumn.getUsedColumns();
+        }
     }
 
     @Override
