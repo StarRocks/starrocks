ff8c06f55373f220d935e250ef699a81119e9ad6
shuming.li <ming.moriarty@gmail.com>
2026-01-29 09:59:55 +0800

    [Enhancement] support varbinary for count distinct like agg functions (#68442)
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>
    (cherry picked from commit c35e344ffe78c62b4983612b16d3dec662a042b6)

--- be/src/exprs/agg/distinct.h
+++ be/src/exprs/agg/distinct.h
@@ -245,7 +245,7 @@ struct AdaptiveSliceHashSet {
 };
 
 template <LogicalType LT, LogicalType SumLT>
-struct DistinctAggregateState<LT, SumLT, StringLTGuard<LT>> {
+struct DistinctAggregateState<LT, SumLT, StringOrBinaryGuard<LT>> {
     DistinctAggregateState() = default;
     using KeyType = typename SliceHashSet::key_type;
 
@@ -376,7 +376,8 @@ struct DistinctAggregateStateV2Base<LT, SumLT, compute_sum, FixedLengthLTGuard<L
 };
 
 template <LogicalType LT, LogicalType SumLT>
-struct DistinctAggregateStateV2Base<LT, SumLT, false, StringLTGuard<LT>> : public DistinctAggregateState<LT, SumLT> {};
+struct DistinctAggregateStateV2Base<LT, SumLT, false, StringOrBinaryGuard<LT>>
+        : public DistinctAggregateState<LT, SumLT> {};
 
 template <LogicalType LT, LogicalType SumLT, typename = guard::Guard>
 struct DistinctAggregateStateV2 : public DistinctAggregateStateV2Base<LT, SumLT, false> {};
