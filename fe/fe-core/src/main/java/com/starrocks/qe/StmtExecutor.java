70d1c246d8058b1c1cdc2333338fda67c3db74c6
SevenJ <166966490+Wenjun7J@users.noreply.github.com>
2026-01-31 01:27:05 +0800

    [BugFix] fix lake table mem leak (#68678)
    Signed-off-by: SevenJ <wenjun7j@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
@@ -1142,6 +1142,10 @@ public class StmtExecutor {
                 coord.cancel(PPlanFragmentCancelReason.INTERNAL_ERROR, context.getState().getErrorMessage());
             }
 
+            if (coord != null) {
+                coord.clearExternalResources();
+            }
+
             if (parsedStmt != null && parsedStmt.isExistQueryScopeHint()) {
                 clearQueryScopeHintContext();
             }
