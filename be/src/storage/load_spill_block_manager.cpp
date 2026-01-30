b43114ba3a628cd9702998e61328feb2abcb3115
luohaha <18810541851@163.com>
2026-01-30 17:27:07 +0800

    [Enhancement] remove remote load spill dir at last
    Signed-off-by: luohaha <18810541851@163.com>

--- be/src/storage/load_spill_block_manager.cpp
+++ be/src/storage/load_spill_block_manager.cpp
@@ -106,6 +106,19 @@ spill::BlockPtr LoadSpillBlockContainer::get_block(size_t gid, size_t bid) {
 LoadSpillBlockManager::~LoadSpillBlockManager() {
     // release blocks before block manager
     _block_container.reset();
+    auto status = clear_parent_path();
+    if (!status.ok() && !status.is_not_found()) {
+        LOG(WARNING) << "Failed to clear load spill parent path, load_id=" << print_id(_load_id)
+                     << ", error=" << status;
+    }
+}
+
+Status LoadSpillBlockManager::clear_parent_path() {
+    spill::AcquireDirOptions acquire_dir_opts;
+    acquire_dir_opts.data_size = 1; // just need acquire a dir
+    ASSIGN_OR_RETURN(auto dir, _remote_dir_manager->acquire_writable_dir(acquire_dir_opts));
+    std::string parent_path = dir->dir() + "/" + print_id(_load_id);
+    return dir->fs()->delete_dir(parent_path);
 }
 
 Status LoadSpillBlockManager::init() {
@@ -146,6 +159,7 @@ StatusOr<spill::BlockPtr> LoadSpillBlockManager::acquire_block(size_t block_size
     opts.name = "load_spill";
     opts.block_size = block_size;
     opts.force_remote = force_remote;
+    opts.skip_parent_path_deletion = true;
     return _block_manager->acquire_block(opts);
 }
 
