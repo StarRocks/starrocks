105972b0a5fcd08a56599e369909dd2275306b02
SevenJ <166966490+Wenjun7J@users.noreply.github.com>
2026-01-31 01:27:05 +0800

    [BugFix] fix lake table mem leak (#68678)
    Signed-off-by: SevenJ <wenjun7j@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/DeltaLakeTable.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/DeltaLakeTable.java
@@ -114,6 +114,10 @@ public class DeltaLakeTable extends Table {
         }
     }
 
+    public void clearMetadata() {
+        this.deltaSnapshot = null;
+    }
+
     @Override
     public List<Column> getPartitionColumns() {
         return partColumnNames.stream()
