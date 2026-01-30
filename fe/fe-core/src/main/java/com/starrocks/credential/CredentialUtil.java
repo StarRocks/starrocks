0d920c459caaa0f68661e37278ea4d0d71097072
Mesut-Doner <81925277+Mesut-Doner@users.noreply.github.com>
2026-01-30 10:17:50 +0300

    [BugFix] masking OBS ak/sk in ui (#65462)
    Signed-off-by: Mesut-Doner <mesutdonerng@gmail.com>
    (cherry picked from commit 1cbe33774f212b0fd8683e610b578dca8c97a825)

--- fe/fe-core/src/main/java/com/starrocks/credential/CredentialUtil.java
+++ fe/fe-core/src/main/java/com/starrocks/credential/CredentialUtil.java
@@ -55,6 +55,12 @@ public class CredentialUtil {
         doMask(properties, CloudConfigurationConstants.ALIYUN_OSS_ACCESS_KEY);
         doMask(properties, CloudConfigurationConstants.ALIYUN_OSS_SECRET_KEY);
 
+        // Mask for huawei obs credential
+        doMask(properties, CloudConfigurationConstants.HUAWEI_OBS_ACCESS_KEY);
+        doMask(properties, CloudConfigurationConstants.HUAWEI_OBS_SECRET_KEY);
+        doMask(properties, CloudConfigurationConstants.HUAWEI_OBS_ACCESS_KEY_DOT);
+        doMask(properties, CloudConfigurationConstants.HUAWEI_OBS_SECRET_KEY_DOT);
+
         // Mask for iceberg rest catalog credential
         doMask(properties, IcebergCatalogProperties.ICEBERG_CUSTOM_PROPERTIES_PREFIX +
                 OAuth2SecurityConfig.OAUTH2_CREDENTIAL);
