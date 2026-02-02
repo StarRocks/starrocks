e456a3c7a7cca8b50e4742d943ed61cfe111072f
nshangyimingCD <tony.xia@celerdata.com>
2026-02-02 14:03:10 +0800

    Remove merge conflict markers from StmtExecutor.java
    Signed-off-by: nshangyimingCD <tony.xia@celerdata.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
@@ -3425,8 +3425,6 @@ public class StmtExecutor {
                 && !(parsedStmt instanceof ShowStmt)
                 && !(parsedStmt instanceof AdminSetConfigStmt);
     }
-<<<<<<< HEAD
-=======
 
     private String resolveImpersonatedUser() {
         String qualifiedUser = ClusterNamespace.getNameFromFullName(context.getQualifiedUser());
@@ -3449,5 +3447,4 @@ public class StmtExecutor {
         }
         return ConnectContext.get().getSessionVariable().getInsertMaxFilterRatio();
     }
->>>>>>> 79df62f360 ([Enhancement] Add impersonate user for query detail http api (#68674))
 }
