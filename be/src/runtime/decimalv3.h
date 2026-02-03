d2f209b83b9a263c61b6dc0b76819c7cb6b65289
alvin-celerdata <alvin.zhao@celerdata.com>
2026-02-04 06:43:05 +0800

    fix compile
    Signed-off-by: alvin-celerdata <alvin.zhao@celerdata.com>

--- be/src/runtime/decimalv3.h
+++ be/src/runtime/decimalv3.h
@@ -25,6 +25,7 @@
 #include "base/container/raw_container.h"
 #include "base/decimal_types.h"
 #include "base/string/string_parser.hpp"
+#include "base/utility/guard.h"
 #include "runtime/integer_overflow_arithmetics.h"
 
 typedef unsigned __int128 uint128_t;
