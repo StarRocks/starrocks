3256d573c4e5e369f69aa7ef8d4c805ba90821ff
shuming.li <ming.moriarty@gmail.com>
2026-02-05 19:36:46 +0800

    fix bugs
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/FunctionAnalyzer.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/FunctionAnalyzer.java
@@ -379,16 +379,16 @@ public class FunctionAnalyzer {
                     functionCallExpr.getPos());
         }
 
-        if ((fnName.equals(FunctionSet.MIN) || fnName.equals(FunctionSet.MAX))
+        if ((fnName.getFunction().equals(FunctionSet.MIN) || fnName.getFunction().equals(FunctionSet.MAX))
                 && !arg.getType().canApplyToNumeric()) {
             throw new SemanticException(Type.NOT_SUPPORT_AGG_ERROR_MSG);
         }
 
         // ndv and approx_count_distinct cannot be applied to non-numeric types
-        if ((fnName.equals(FunctionSet.NDV)
-                || fnName.equals(FunctionSet.APPROX_COUNT_DISTINCT)
-                || fnName.equals(FunctionSet.DS_THETA_COUNT_DISTINCT)
-                || fnName.equals(FunctionSet.DS_HLL_COUNT_DISTINCT))
+        if ((fnName.getFunction().equals(FunctionSet.NDV)
+                || fnName.getFunction().equals(FunctionSet.APPROX_COUNT_DISTINCT)
+                || fnName.getFunction().equals(FunctionSet.DS_THETA_COUNT_DISTINCT)
+                || fnName.getFunction().equals(FunctionSet.DS_HLL_COUNT_DISTINCT))
                 && !(arg.getType().canApplyToNumeric() || arg.getType().isBinaryType())) {
             throw new SemanticException(Type.NOT_SUPPORT_AGG_ERROR_MSG);
         }
