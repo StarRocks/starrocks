3e5259646a72efcb8969d5a246cae63494ce8d5a
edwinhzhang <edwinhzhang@tencent.com>
2026-02-10 23:48:47 +0800

    derive logical property
    Signed-off-by: edwinhzhang <edwinhzhang@tencent.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/QueryOptimizer.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/QueryOptimizer.java
@@ -783,6 +783,8 @@ public class QueryOptimizer extends Optimizer {
             OptimizerTraceUtil.logMVRewriteRule("VIEW_BASED_MV_REWRITE", "try VIEW_BASED_MV_REWRITE");
             // Clone the tree to avoid modifying the original tree stored in QueryMaterializationContext
             OptExpression treeWithView = MvUtils.cloneExpression(queryMaterializationContext.getQueryOptPlanWithView());
+            // Derive logical property for the cloned tree to ensure it has complete metadata
+            MvUtils.deriveLogicalProperty(treeWithView);
             OptimizerTraceUtil.logOptExpression("before ViewBasedMvRuleRewrite:\n%s", tree);
             // should add a LogicalTreeAnchorOperator for rewrite
             treeWithView = OptExpression.create(new LogicalTreeAnchorOperator(), treeWithView);
