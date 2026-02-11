18ff041470dc43dc1817b20a659976888e7ba922
shuming.li <ming.moriarty@gmail.com>
2026-02-12 01:18:11 +0800

    [Feature] Fast Schema Evolution for Materialized Views (#67915)
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>
    (cherry picked from commit 102b87e79b679c40c533b7d491fb306f68056386)

--- fe/fe-core/src/test/java/com/starrocks/analysis/AlterMaterializedViewTest.java
+++ fe/fe-core/src/test/java/com/starrocks/analysis/AlterMaterializedViewTest.java
@@ -36,10 +36,13 @@ import com.starrocks.scheduler.persist.TaskRunStatus;
 import com.starrocks.server.GlobalStateMgr;
 import com.starrocks.sql.analyzer.AnalyzeTestUtil;
 import com.starrocks.sql.analyzer.SemanticException;
+import com.starrocks.sql.ast.AddMVColumnClause;
 import com.starrocks.sql.ast.AlterMaterializedViewStmt;
+import com.starrocks.sql.ast.ColumnDef;
 import com.starrocks.sql.ast.InsertStmt;
 import com.starrocks.sql.ast.RefreshSchemeClause;
 import com.starrocks.sql.ast.SyncRefreshSchemeDesc;
+import com.starrocks.sql.ast.expression.IntLiteral;
 import com.starrocks.sql.optimizer.rule.transformation.materialization.MVTestBase;
 import com.starrocks.sql.plan.ExecPlan;
 import com.starrocks.utframe.UtFrameUtils;
@@ -175,6 +178,19 @@ public class AlterMaterializedViewTest extends MVTestBase  {
                     (AlterMaterializedViewStmt) UtFrameUtils.parseStmtWithNewParser(alterMvSql, connectContext);
             Assertions.assertThrows(SemanticException.class, () -> currentState.getLocalMetastore().alterMaterializedView(stmt));
         }
+    }
+
+    @Test
+    public void testAlterMVAddColumnWithDefaultValue() throws Exception {
+        String alterMvSql = "alter materialized view mv1 add column v1_default as v1 default 10";
+        AlterMaterializedViewStmt stmt =
+                (AlterMaterializedViewStmt) UtFrameUtils.parseStmtWithNewParser(alterMvSql, connectContext);
+        AddMVColumnClause clause = (AddMVColumnClause) stmt.getAlterTableClause();
+        ColumnDef.DefaultValueDef defaultValueDef = clause.getDefaultValueDef();
+        Assertions.assertTrue(defaultValueDef.isSet);
+        Assertions.assertTrue(defaultValueDef.expr.isConstant());
+        IntLiteral intLiteral = (IntLiteral) defaultValueDef.expr;
+        Assertions.assertEquals(10, intLiteral.getValue());
     }
 
     // TODO: consider to support alterjob for mv
