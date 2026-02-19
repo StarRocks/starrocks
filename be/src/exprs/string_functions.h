f2ab341ee0e266069d47333016e2f643aab48815
trueeyu <lxhhust350@qq.com>
2026-02-19 14:59:00 +0800

    [BugFix] Fix the concurrency bug of function field (#69315)
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/exprs/string_functions.h
+++ be/src/exprs/string_functions.h
@@ -56,6 +56,8 @@ template <LogicalType T>
 struct FieldFuncState {
     bool all_const = false;
     bool list_all_const = false;
+
+    int const_field_idx = 0;
     std::map<RunTimeCppType<T>, int> mp;
 };
 
@@ -783,9 +785,6 @@ Status StringFunctions::field_prepare(FunctionContext* context, FunctionContext:
     }
 
     if (state->list_all_const) {
-        if (context->is_constant_column(0)) {
-            state->all_const = true;
-        }
         for (int i = 1; i < context->get_num_args(); i++) {
             const auto list_col = context->get_constant_column(i);
             if (list_col->only_null()) {
@@ -793,6 +792,21 @@ Status StringFunctions::field_prepare(FunctionContext* context, FunctionContext:
             } else {
                 const auto list_val = ColumnHelper::get_const_value<Type>(list_col);
                 state->mp.emplace(list_val, i);
+            }
+        }
+        if (context->is_constant_column(0)) {
+            state->all_const = true;
+            auto const_column = context->get_constant_column(0);
+            if (const_column->only_null()) {
+                state->const_field_idx = 0;
+            } else {
+                auto const_value = ColumnHelper::get_const_value<Type>(const_column);
+                auto it = state->mp.find(const_value);
+                if (it != state->mp.end()) {
+                    state->const_field_idx = it->second;
+                } else {
+                    state->const_field_idx = 0;
+                }
             }
         }
     }
@@ -823,14 +837,7 @@ StatusOr<ColumnPtr> StringFunctions::field(FunctionContext* context, const Colum
         return result.build(true);
     } else if (state != nullptr) {
         if (state->all_const) {
-            const auto list_col = context->get_constant_column(0);
-            const auto list_val = ColumnHelper::get_const_value<Type>(list_col);
-            auto it = state->mp.find(list_val);
-            if (it != state->mp.end()) {
-                result.append(it->second);
-            } else {
-                result.append(0);
-            }
+            result.append(state->const_field_idx);
             return result.build(true);
         } else if (state->list_all_const) {
             const auto viewer = ColumnViewer<Type>(columns[0]);
