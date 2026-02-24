c4d9cc656e8c7a4fa2b23d5625746ead3e9f8174
trueeyu <lxhhust350@qq.com>
2026-02-24 14:16:28 +0800

    [UT] Refactor test case: ParquetFileWriterTest (#69405)
    Signed-off-by: trueeyu <lxhhust350@qq.com>
    (cherry picked from commit e7b438814ac733f24153d345a899b75f53b0c412)
    
    # Conflicts:
    #	be/src/testutil/column_test_helper.h
    #	be/test/formats/parquet/parquet_file_writer_test.cpp

--- be/src/testutil/column_test_helper.h
+++ be/src/testutil/column_test_helper.h
@@ -22,6 +22,7 @@ namespace starrocks {
 class ColumnTestHelper {
 public:
     template <class T>
+<<<<<<< HEAD
     static ColumnPtr build_column(const std::vector<T>& values) {
         if constexpr (std::is_same_v<T, uint8_t>) {
             auto data = UInt8Column::create();
@@ -33,16 +34,24 @@ public:
             return data;
         } else if constexpr (std::is_same_v<T, int64_t>) {
             auto data = Int64Column::create();
+=======
+    static MutableColumnPtr build_column(const std::vector<T>& values) {
+        if constexpr (std::is_integral_v<T> || std::is_floating_point_v<T>) {
+            auto data = ColumnTraits<T>::ColumnType::create();
+>>>>>>> e7b438814a ([UT] Refactor test case: ParquetFileWriterTest (#69405))
             data->append_numbers(values.data(), values.size() * sizeof(T));
             return data;
         } else if constexpr (std::is_same_v<T, Slice>) {
             auto data = BinaryColumn::create();
             data->append_strings(values.data(), values.size());
             return data;
+<<<<<<< HEAD
         } else if constexpr (std::is_same_v<T, double>) {
             auto data = DoubleColumn ::create();
             data->append_numbers(values.data(), values.size() * sizeof(T));
             return data;
+=======
+>>>>>>> e7b438814a ([UT] Refactor test case: ParquetFileWriterTest (#69405))
         } else {
             throw std::runtime_error("Type is not supported in build_column:%s" + std::string(typeid(T).name()));
         }
