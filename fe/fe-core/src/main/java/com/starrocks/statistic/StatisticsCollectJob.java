191d8633548d7107bbd8004ece6de5d5f468715f
stephen <91597003+stephen-shelby@users.noreply.github.com>
2026-02-28 10:04:06 +0800

    [BugFix] Fix KILL ANALYZE sometimes not stopping ANALYZE TABLE (#69592)
    Signed-off-by: stephen <stephen5217@163.com>
    (cherry picked from commit 3c026a22c4ea8e56f8815964ac898d8b6d7aecc9)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/statistic/hyper/HyperQueryJob.java
    #	fe/fe-core/src/test/java/com/starrocks/statistic/hyper/MultiColumnHyperJobTest.java

--- fe/fe-core/src/main/java/com/starrocks/statistic/StatisticsCollectJob.java
+++ fe/fe-core/src/main/java/com/starrocks/statistic/StatisticsCollectJob.java
@@ -36,6 +36,7 @@ import com.starrocks.qe.ConnectContext;
 import com.starrocks.qe.QueryState;
 import com.starrocks.qe.SessionVariable;
 import com.starrocks.qe.StmtExecutor;
+import com.starrocks.server.GlobalStateMgr;
 import com.starrocks.sql.ast.StatementBase;
 import com.starrocks.sql.parser.SqlParser;
 import org.apache.commons.collections.CollectionUtils;
@@ -118,6 +119,13 @@ public abstract class StatisticsCollectJob {
     }
 
     public abstract void collect(ConnectContext context, AnalyzeStatus analyzeStatus) throws Exception;
+
+    protected void checkCancelled(AnalyzeStatus analyzeStatus) throws DdlException {
+        if (analyzeStatus != null && GlobalStateMgr.getCurrentState().getAnalyzeMgr()
+                .isAnalyzeCancelled(analyzeStatus.getId())) {
+            throw new DdlException("USER_CANCEL: kill analyze");
+        }
+    }
 
     public String getCatalogName() {
         return InternalCatalog.DEFAULT_INTERNAL_CATALOG_NAME;
@@ -231,6 +239,7 @@ public abstract class StatisticsCollectJob {
         int count = 0;
         int maxRetryTimes = 5;
         do {
+            checkCancelled(analyzeStatus);
             // Calculate and set remaining timeout for this SQL task
             calculateAndSetRemainingTimeout(context, analyzeStatus);
 
@@ -250,6 +259,7 @@ public abstract class StatisticsCollectJob {
             context.setStartTime();
             executor.execute();
 
+            checkCancelled(analyzeStatus);
             if (context.getState().getStateType() == QueryState.MysqlStateType.ERR) {
                 LOG.warn("Statistics collect fail | Error Message [{}] | {} | SQL [{}]",
                         context.getState().getErrorMessage(), DebugUtil.printId(context.getQueryId()), sql);
