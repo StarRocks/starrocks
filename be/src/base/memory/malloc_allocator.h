1009dea1a2c0679ae5036e7efb41b2a879aebd58
eyes_on_me <nopainnofame@sina.com>
2026-03-03 14:24:28 +0800

    [Enhancement] introduce memory allocator (#68788)
    Signed-off-by: silverbullet233 <3675229+silverbullet233@users.noreply.github.com>

---
+++ be/src/base/memory/malloc_allocator.h
@@ -0,0 +1,37 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "base/memory/memory_allocator.h"
+
+namespace starrocks::memory {
+
+template <bool clear_memory>
+class MallocAllocator : public Allocator {
+public:
+    MallocAllocator() = default;
+    ~MallocAllocator() override = default;
+
+    void* alloc(size_t size, size_t alignment = 0) override;
+    void* realloc(void* ptr, size_t old_size, size_t new_size, size_t alignment = 0) override;
+    void free(void* ptr, size_t size) override;
+    int64_t nallox(size_t size, int flags = 0) const override;
+
+    Allocator::MemoryKind memory_kind() const override { return Allocator::MemoryKind::kMalloc; }
+
+    static constexpr bool throw_bad_alloc_on_failure() { return false; }
+};
+
+} // namespace starrocks::memory
