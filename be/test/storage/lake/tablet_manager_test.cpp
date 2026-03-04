6cc28437f8dba465a84a69947b38c9dcb7040dad
xiangguangyxg <110401425+xiangguangyxg@users.noreply.github.com>
2026-03-04 15:37:43 +0800

    [BugFix] Gate lake capture_tablet_and_rowsets behind experimental config (#69748)
    Signed-off-by: xiangguangyxg <xiangguangyxg@gmail.com>
    (cherry picked from commit ce279adf7954e096f969ea8d7f2b43ce4be99b03)
    
    # Conflicts:
    #	be/src/storage/lake/tablet_manager.cpp

--- be/test/storage/lake/tablet_manager_test.cpp
+++ be/test/storage/lake/tablet_manager_test.cpp
@@ -826,6 +826,12 @@ TEST_F(LakeTabletManagerTest, test_get_output_rorwset_schema) {
 }
 
 TEST_F(LakeTabletManagerTest, capture_tablet_and_rowsets) {
+    auto old_capture_tablet_and_rowsets_config = config::experimental_enable_lake_capture_tablet_and_rowsets;
+    DeferOp defer([old_capture_tablet_and_rowsets_config]() {
+        config::experimental_enable_lake_capture_tablet_and_rowsets = old_capture_tablet_and_rowsets_config;
+    });
+    config::experimental_enable_lake_capture_tablet_and_rowsets = true;
+
     starrocks::TabletMetadata metadata;
     auto schema = metadata.mutable_schema();
     schema->set_id(1);
@@ -876,6 +882,53 @@ TEST_F(LakeTabletManagerTest, capture_tablet_and_rowsets) {
         EXPECT_TRUE(res.ok());
         auto& [tablet, rowsets] = res.value();
         ASSERT_EQ(1, rowsets.size());
+    }
+}
+
+TEST_F(LakeTabletManagerTest, capture_tablet_and_rowsets_capture_delta_versions_disabled) {
+    auto old_capture_tablet_and_rowsets_config = config::experimental_enable_lake_capture_tablet_and_rowsets;
+    DeferOp defer([old_capture_tablet_and_rowsets_config]() {
+        config::experimental_enable_lake_capture_tablet_and_rowsets = old_capture_tablet_and_rowsets_config;
+    });
+    config::experimental_enable_lake_capture_tablet_and_rowsets = false;
+
+    starrocks::TabletMetadata metadata;
+    auto schema = metadata.mutable_schema();
+    schema->set_id(1);
+    auto tablet_id = next_id();
+    metadata.set_id(tablet_id);
+    metadata.set_version(1);
+    EXPECT_OK(_tablet_manager->put_tablet_metadata(metadata));
+
+    metadata.set_version(2);
+    auto rowset_meta_pb2 = metadata.add_rowsets();
+    rowset_meta_pb2->set_id(2);
+    rowset_meta_pb2->set_overlapped(false);
+    rowset_meta_pb2->set_data_size(1024);
+    rowset_meta_pb2->set_num_rows(5);
+    EXPECT_OK(_tablet_manager->put_tablet_metadata(metadata));
+
+    metadata.set_version(3);
+    auto rowset_meta_pb3 = metadata.add_rowsets();
+    rowset_meta_pb3->set_id(3);
+    rowset_meta_pb3->set_overlapped(false);
+    rowset_meta_pb3->set_data_size(1024);
+    rowset_meta_pb3->set_num_rows(5);
+    EXPECT_OK(_tablet_manager->put_tablet_metadata(metadata));
+
+    {
+        auto res = _tablet_manager->capture_tablet_and_rowsets(tablet_id, 1, 3);
+        EXPECT_TRUE(res.status().is_not_supported());
+    }
+
+    {
+        auto res = _tablet_manager->capture_tablet_and_rowsets(tablet_id, 2, 3);
+        EXPECT_TRUE(res.status().is_not_supported());
+    }
+
+    {
+        auto res = _tablet_manager->capture_tablet_and_rowsets(tablet_id, 0, 3);
+        EXPECT_TRUE(res.status().is_not_supported());
     }
 }
 
