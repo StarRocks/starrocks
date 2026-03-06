4ad018cb650fde66b5e8dbda30a4445fe937b077
srlch <111035020+srlch@users.noreply.github.com>
2026-03-06 13:16:37 +0800

    Add import for CompletableFuture in CatalogRecycleBinTest
    Signed-off-by: srlch <111035020+srlch@users.noreply.github.com>

--- fe/fe-core/src/test/java/com/starrocks/catalog/CatalogRecycleBinTest.java
+++ fe/fe-core/src/test/java/com/starrocks/catalog/CatalogRecycleBinTest.java
@@ -38,6 +38,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import static org.mockito.Mockito.doReturn;
 
