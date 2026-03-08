447e91db4e90a5e45ca04e2a03defcf5ed31fabc
Drake Wang <wxl24life@gmail.com>
2026-03-08 12:03:25 +0800

    [Enhancement] Support partitioned prefix for shared-data cross cluster migration (backport #67370) (#69931)
    Signed-off-by: Drake Wang <wxl24life@gmail.com>

--- be/src/fs/fs_starlet.h
+++ be/src/fs/fs_starlet.h
@@ -39,9 +39,18 @@ StatusOr<std::pair<std::string, int64_t>> parse_starlet_uri(std::string_view uri
 
 std::unique_ptr<FileSystem> new_fs_starlet();
 
-// starlet fs used for virtual shard id, a new fs starlet will be created on each call
-// to prevent shard fs re-create everty time, we will reuse the lru cache framework to cache the shard fs
-std::shared_ptr<FileSystem> new_fs_starlet(int64_t virtual_shard_id);
+// Create a starlet filesystem for cross-cluster migration with optional S3 raw path mode.
+//
+// When `use_raw_path` is true:
+//   - Sets s3.use_raw_path_with_scheme=true in starlet configuration
+//   - Starlet will use the input path as-is without normalize_path processing
+//   - This is required for S3 storage type to support partitioned prefix feature
+//
+// When `use_raw_path` is false:
+//   - Uses default starlet configuration
+//   - Starlet will use normalize_path to combine sys.root with the relative path
+//   - This is used for non-S3 storage types (OSS/Azure/HDFS/GFS)
+std::shared_ptr<FileSystem> new_fs_starlet(int64_t virtual_shard_id, bool use_raw_path);
 
 } // namespace starrocks
 
