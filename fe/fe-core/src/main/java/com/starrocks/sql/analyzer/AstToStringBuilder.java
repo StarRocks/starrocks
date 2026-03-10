ea597da6f53c1893708db57d3424c12f556e09fb
meegoo <meegoo.sr@gmail.com>
2026-03-10 10:37:40 +0800

    [BugFix] Hide expression partition generated columns in DESC/SHOW CREATE TABLE (#69793)
    Signed-off-by: meegoo <meegoo.sr@gmail.com>
    (cherry picked from commit ad22161a2dfeff85077e2ef51a7d6adbd6b4a9c8)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/service/FrontendServiceImpl.java
    #	fe/fe-core/src/main/java/com/starrocks/sql/analyzer/AstToStringBuilder.java
    #	test/sql/test_automatic_partition/R/test_multi_expr

--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/AstToStringBuilder.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/AstToStringBuilder.java
@@ -72,7 +72,11 @@ import com.starrocks.catalog.HudiTable;
 import com.starrocks.catalog.IcebergTable;
 import com.starrocks.catalog.Index;
 import com.starrocks.catalog.JDBCTable;
+<<<<<<< HEAD
 import com.starrocks.catalog.KeysType;
+=======
+import com.starrocks.catalog.ListPartitionInfo;
+>>>>>>> ad22161a2d ([BugFix] Hide expression partition generated columns in DESC/SHOW CREATE TABLE (#69793))
 import com.starrocks.catalog.MaterializedIndexMeta;
 import com.starrocks.catalog.MaterializedView;
 import com.starrocks.catalog.MysqlTable;
@@ -84,8 +88,12 @@ import com.starrocks.catalog.PartitionType;
 import com.starrocks.catalog.RangePartitionInfo;
 import com.starrocks.catalog.Table;
 import com.starrocks.catalog.View;
+<<<<<<< HEAD
 import com.starrocks.common.Pair;
 import com.starrocks.common.util.ParseUtil;
+=======
+import com.starrocks.common.FeConstants;
+>>>>>>> ad22161a2d ([BugFix] Hide expression partition generated columns in DESC/SHOW CREATE TABLE (#69793))
 import com.starrocks.common.util.PrintableMap;
 import com.starrocks.common.util.SqlCredentialRedactor;
 import com.starrocks.credential.CredentialUtil;
@@ -1699,6 +1707,10 @@ public class AstToStringBuilder {
         sb.append("`").append(table.getName()).append("` (\n");
         int idx = 0;
         for (Column column : table.getBaseSchema()) {
+            // Skip expression partition generated columns to show user-created DDL
+            if (column.isNameWithPrefix(FeConstants.GENERATED_PARTITION_COLUMN_PREFIX)) {
+                continue;
+            }
             if (idx++ != 0) {
                 sb.append(",\n");
             }
@@ -1749,7 +1761,14 @@ public class AstToStringBuilder {
                 partitionId = Lists.newArrayList();
             }
             if (partitionInfo.isRangePartition() || partitionInfo.getType() == PartitionType.LIST) {
-                sb.append("\n").append(partitionInfo.toSql(olapTable, partitionId));
+                // Use expression (not generated column name) for user-created DDL display
+                if (partitionInfo instanceof ListPartitionInfo) {
+                    ListPartitionInfo listPartitionInfo = (ListPartitionInfo) partitionInfo;
+                    sb.append("\n").append(listPartitionInfo.toSql(olapTable,
+                            listPartitionInfo.isAutomaticPartition(), false));
+                } else {
+                    sb.append("\n").append(partitionInfo.toSql(olapTable, partitionId));
+                }
             }
 
             // distribution
