88e57ffaad4186c6b07878320ba49d20cc56a587
stdpain <34912776+stdpain@users.noreply.github.com>
2026-03-10 10:18:50 +0800

    [BugFix] update jdbc fwd config (#70035)
    Signed-off-by: stdpain <drfeng08@gmail.com>

--- be/src/common/config_exec_flow_fwd.h
+++ be/src/common/config_exec_flow_fwd.h
@@ -121,6 +121,17 @@ CONF_Int16(jdbc_minimum_idle_connections, "1");
 // The minimum allowed value is 10000(10 seconds).
 CONF_Int32(jdbc_connection_idle_timeout_ms, "600000");
 
+// Maximum lifetime of a connection in the JDBC connection pool (ms).
+// Minimum allowed value is 30000 (30 seconds). Default: 300000 (5 minutes).
+CONF_Int64(jdbc_connection_max_lifetime_ms, "300000");
+
+// Keepalive interval for idle JDBC connections (ms).
+// Idle connections are tested at this interval to detect stale connections proactively.
+// Set to 0 to disable keepalive probing.
+// When enabled, must be >= 30000 and < jdbc_connection_max_lifetime_ms.
+// Invalid enabled values are silently disabled (reset to 0). Default: 30000 (30 seconds).
+CONF_Int64(jdbc_connection_keepalive_time_ms, "30000");
+
 // when spill occurs, whether enable skip synchronous flush
 CONF_mBool(experimental_spill_skip_sync, "true");
 
