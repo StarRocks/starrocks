48cac0ca4a938c018140e31914e3c4565b90ef9d
alvin-celerdata <alvin.zhao@celerdata.com>
2026-03-10 10:11:55 +0800

    Fix compile
    Signed-off-by: alvin-celerdata <alvin.zhao@celerdata.com>

--- be/test/storage/lake/primary_key_publish_test.cpp
+++ be/test/storage/lake/primary_key_publish_test.cpp
@@ -18,6 +18,7 @@
 
 #include "base/testutil/assert.h"
 #include "base/testutil/id_generator.h"
+#include "base/utility/defer_op.h"
 #include "column/chunk.h"
 #include "column/datum_tuple.h"
 #include "column/fixed_length_column.h"
