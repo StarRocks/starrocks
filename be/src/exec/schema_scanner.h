bed2464acf0850531cd9c8d42cb636bba80bd51b
alvin-celerdata <alvin.zhao@celerdata.com>
2026-03-10 10:19:46 +0800

    Fix compile
    Signed-off-by: alvin-celerdata <alvin.zhao@celerdata.com>

--- be/src/exec/schema_scanner.h
+++ be/src/exec/schema_scanner.h
@@ -22,6 +22,7 @@
 #include "common/status.h"
 #include "exprs/expr_context.h"
 #include "gen_cpp/Descriptors_types.h"
+#include "gen_cpp/FrontendService_types.h"
 #include "gen_cpp/Types_types.h"
 #include "runtime/descriptors.h"
 
