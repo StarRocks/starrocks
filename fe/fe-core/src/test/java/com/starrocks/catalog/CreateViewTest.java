491e2bf9231f36988c2d4f6f464b48f71d265bd5
Jelmer Kuperus <jkuperus@gmail.com>
2026-03-11 03:16:36 +0100

    [BugFix] IGNORE NULLS silently dropped from VIEW definition when window function argument is an expression (#69971)
    Signed-off-by: Jelmer Kuperus <jelmer@miro.com>
    (cherry picked from commit 541bcd2f15b5bed6900c50cb43d69b16c884fffb)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/sql/formatter/AST2StringVisitor.java
    #	fe/fe-core/src/test/java/com/starrocks/catalog/CreateViewTest.java

--- fe/fe-core/src/test/java/com/starrocks/catalog/CreateViewTest.java
+++ fe/fe-core/src/test/java/com/starrocks/catalog/CreateViewTest.java
@@ -146,4 +146,57 @@ public class CreateViewTest {
                 "(PARTITION BY `test`.`sample_data`.`username` ) AS `leadValue`\n" +
                 "FROM `test`.`sample_data`");
     }
+<<<<<<< HEAD
+=======
+
+    @Test
+    public void testCreateViewWithWindowFunctionIgnoreNullsWithExpression() throws Exception {
+        starRocksAssert.withTable("create table sample_data2 (\n" +
+                        "    timestamp DATETIME not null,\n" +
+                        "    username string,\n" +
+                        "    price int null\n" +
+                        ")PROPERTIES (\n" +
+                        "\"replication_num\" = \"1\");")
+                .withView("create view test_ignore_nulls_expr as select\n" +
+                        "    timestamp,\n" +
+                        "    username,\n" +
+                        "    last_value(if(price > 0, price, null) ignore nulls) over (partition by username) as price\n" +
+                        ", lead(if(price > 0, price, null) ignore nulls, 1, 0) over (partition by username) as leadValue\n" +
+                        "from sample_data2;");
+
+        Table view = starRocksAssert.getCtx().getGlobalStateMgr().getLocalMetastore()
+                .getDb("test").getTable("test_ignore_nulls_expr");
+        Assertions.assertTrue(view instanceof View);
+        String str = ((View) view).getInlineViewDef();
+        Assertions.assertTrue(str.contains("ignore nulls"),
+                "Expected 'ignore nulls' in view def but got: " + str);
+    }
+
+    @Test
+    public void createViewWithComment() throws Exception {
+        starRocksAssert.withTable("CREATE TABLE test_table1 (\n" +
+                "  `event_day` date NULL COMMENT \"\" ,\n" +
+                "  `site_id` int(11) NULL DEFAULT \"10\" COMMENT \"\",\n" +
+                "  `city_code` varchar(100) NULL COMMENT \"\",\n" +
+                "  `user_name` varchar(32) NULL DEFAULT \"\" COMMENT \"\",\n" +
+                "  `pv` bigint(20) NULL DEFAULT \"0\" COMMENT \"\"\n" +
+                ") \n" +
+                "DUPLICATE KEY(`event_day`)\n" +
+                "COMMENT \"OLAP\"\n" +
+                "DISTRIBUTED BY HASH(`event_day`)\n" +
+                "PROPERTIES (\n" +
+                "\"replication_num\" = \"1\"" +
+                ");");
+
+        // create non existed view
+        starRocksAssert.withView("create or replace view test_view1 as select \n" +
+                "`event_day` -- This is a comment from user\n" +
+                "from test_table1;");
+
+        List<List<String>> result = starRocksAssert.show("show create view test_view1;");
+        Assertions.assertEquals(1, result.size());
+        String createViewSql = result.get(0).get(1);
+        Assertions.assertTrue(createViewSql.contains("-- This is a comment from user"));
+    }
+>>>>>>> 541bcd2f15 ([BugFix] IGNORE NULLS silently dropped from VIEW definition when window function argument is an expression (#69971))
 }
