f6f0c89a4de4bdc2fe4bdfab81c93d18f1400d55
andyziye <108652123+andyziye@users.noreply.github.com>
2026-03-12 10:20:32 +0800

    [Enhancement]  Exclude Blacklisted Queries from Error Metrics(sync #69621) (#50615)
    * [Enhancement]  Exclude Blacklisted Queries from Error Metrics
    
    * fix conflict (#50993)
    
    Signed-off-by: before-Sunrise <unclejyj@gmail.com>
    
    ---------
    
    Signed-off-by: before-Sunrise <unclejyj@gmail.com>
    Co-authored-by: before-Sunrise <71162020+before-Sunrise@users.noreply.github.com>

--- fe/fe-core/src/main/java/com/starrocks/common/ErrorReport.java
+++ fe/fe-core/src/main/java/com/starrocks/common/ErrorReport.java
@@ -86,6 +86,16 @@ public class ErrorReport {
         reportDdlException(pattern, ErrorCode.ERR_UNKNOWN_ERROR, objs);
     }
 
+    public static void reportSqlBlackListException(ErrorCode errorCode, Object... objs)
+            throws SqlBlacklistedException {
+        reportSqlBlackListException(null, errorCode, objs);
+    }
+
+    public static void reportSqlBlackListException(String pattern, ErrorCode errorCode, Object... objs)
+            throws SqlBlacklistedException {
+        throw new SqlBlacklistedException(reportCommon(pattern, errorCode, objs));
+    }
+
     public static void reportDdlException(ErrorCode errorCode, Object... objs)
             throws DdlException {
         reportDdlException(null, errorCode, objs);
