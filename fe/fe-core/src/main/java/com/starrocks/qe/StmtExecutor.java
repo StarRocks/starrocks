ed5abccf86870aecdc28420a07648c693a79368c
andyziye <108652123+andyziye@users.noreply.github.com>
2026-03-16 10:30:13 +0800

    [BugFix] Fix the bug where graceful exit caused different transactions to publish the same version.(sync #69639) (#51007)
    * [BugFix] Fix the bug where graceful exit caused different transactions to publish the same version.
    
    * resolve (#51187)
    
    Signed-off-by: gengjun-git <gengjun@starrocks.com>
    
    ---------
    
    Signed-off-by: gengjun-git <gengjun@starrocks.com>
    Co-authored-by: gengjun-git <gengjun@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/StmtExecutor.java
@@ -143,7 +143,6 @@ import com.starrocks.qe.scheduler.FeExecuteCoordinator;
 import com.starrocks.qe.scheduler.dag.ExecutionFragment;
 import com.starrocks.qe.scheduler.dag.FragmentInstance;
 import com.starrocks.server.GlobalStateMgr;
-import com.starrocks.server.GracefulExitFlag;
 import com.starrocks.server.WarehouseManager;
 import com.starrocks.service.ExecuteEnv;
 import com.starrocks.service.arrow.flight.sql.ArrowFlightSqlConnectContext;
@@ -815,7 +814,6 @@ public class StmtExecutor {
         long beginTimeInNanoSecond = TimeUtils.getStartTime();
         context.setStmtId(STMT_ID_GENERATOR.incrementAndGet());
         context.setIsForward(false);
-        context.setIsLeaderTransferred(false);
         context.setCurrentThreadId(Thread.currentThread().getId());
 
         SessionVariable sessionVariableBackup = context.getSessionVariable();
@@ -1225,12 +1223,6 @@ public class StmtExecutor {
             }
 
             recordExecStatsIntoContext();
-
-            if (GracefulExitFlag.isGracefulExit() && context.isLeaderTransferred() && !isInternalStmt) {
-                LOG.info("leader is transferred during executing, forward to new leader");
-                isForwardToLeaderOpt = Optional.of(true);
-                forwardToLeader();
-            }
 
             // process post-action after query is finished
             context.onQueryFinished();
@@ -3360,9 +3352,6 @@ public class StmtExecutor {
                 txnStatus = TransactionStatus.COMMITTED;
                 final long waitInterval = 300;
                 while (publishWaitMs > 0) {
-                    if (GlobalStateMgr.getCurrentState().isLeaderTransferred()) {
-                        break;
-                    }
 
                     if (visibleWaiter.await(Math.min(publishWaitMs, waitInterval), TimeUnit.MILLISECONDS)) {
                         txnStatus = TransactionStatus.VISIBLE;
