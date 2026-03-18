94a0300f3db6e9e92a0db5018e728e5af817b384
farhad-celo <f.shahmohammadi@celonis.com>
2026-03-17 23:51:22 -0700

    [Enhancement] adding a config to limit the buffer size to dop * local_exchange_buffer_mem_limit_per_driver (#70393)
    Signed-off-by: Farhad Shahmohammadi <f.shahmohammadi@celonis.com>
    Signed-off-by: stdpain <34912776+stdpain@users.noreply.github.com>
    Signed-off-by: stdpain <drfeng08@gmail.com>
    Co-authored-by: stdpain <34912776+stdpain@users.noreply.github.com>
    Co-authored-by: wanpengfei-git <wanpengfei91@163.com>
    Co-authored-by: stdpain <drfeng08@gmail.com>

--- be/src/common/config_exec_flow_fwd.h
+++ be/src/common/config_exec_flow_fwd.h
@@ -178,6 +178,9 @@ CONF_mInt32(query_cache_num_lanes_per_driver, "4");
 // limit local exchange buffer's memory size per driver
 CONF_Int64(local_exchange_buffer_mem_limit_per_driver, "134217728"); // 128MB
 
+// limit local exchange buffer size by dop * local_exchange_buffer_mem_limit_per_driver for union operators.
+CONF_mBool(local_exchange_buffer_mem_limit_by_consumer_dop, "true");
+
 // only used for test. default: 128M
 CONF_mInt64(streaming_agg_limited_memory_size, "134217728");
 
