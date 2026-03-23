9daaf19dc538d3c227cf631017f4709c11f011db
stephen <91597003+stephen-shelby@users.noreply.github.com>
2026-03-23 20:54:01 +0800

    Revert "[Enhancement] Support explicit skew hint for window function (backpor…"
    This reverts commit 902c34cd6fa327a2dfa691fdeb17b215cd7f9e88.

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/implementation/WindowImplementationRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/implementation/WindowImplementationRule.java
@@ -43,8 +43,6 @@ public class WindowImplementationRule extends ImplementationRule {
                 logical.getEnforceSortColumns(),
                 logical.isUseHashBasedPartition(),
                 logical.isSkewed(),
-                logical.getSkewColumn(),
-                logical.getSkewValues(),
                 logical.isInputIsBinary(),
                 logical.getLimit(),
                 logical.getPredicate(),
