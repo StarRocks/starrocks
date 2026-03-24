80253065ca7d3844ff83ac604a4753e6f2325636
before-Sunrise <71162020+before-Sunrise@users.noreply.github.com>
2026-03-24 15:24:13 +0800

    [BugFix] fix optimizer timeout  (#70605)
    Signed-off-by: before-Sunrise <unclejyj@gmail.com>
    (cherry picked from commit 6ae49c393c2d6f341dee2d0682bb3697bfcdbd92)
    
    # Conflicts:
    #	fe/AGENTS.md

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/MultiDistinctByMultiFuncRewriter.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/transformation/MultiDistinctByMultiFuncRewriter.java
@@ -52,6 +52,7 @@ public class MultiDistinctByMultiFuncRewriter {
     public List<OptExpression> transformImpl(OptExpression input, OptimizerContext context) {
         LogicalAggregationOperator aggregationOperator = (LogicalAggregationOperator) input.getOp();
 
+        boolean changed = false;
         Map<ColumnRefOperator, CallOperator> newAggMap = new HashMap<>();
         for (Map.Entry<ColumnRefOperator, CallOperator> aggregation : aggregationOperator.getAggregations()
                 .entrySet()) {
@@ -61,12 +62,15 @@ public class MultiDistinctByMultiFuncRewriter {
 
                 if (oldFunctionCall.getFnName().equalsIgnoreCase(FunctionSet.COUNT)) {
                     newAggOperator = buildMultiCountDistinct(oldFunctionCall);
+                    changed = true;
                 } else if (oldFunctionCall.getFnName().equalsIgnoreCase(FunctionSet.SUM)) {
                     newAggOperator = buildMultiSumDistinct(oldFunctionCall);
+                    changed = true;
                 } else if (oldFunctionCall.getFnName().equals(FunctionSet.ARRAY_AGG)) {
                     if (oldFunctionCall.getColumnRefs().size() == 1 &&
                             !oldFunctionCall.getColumnRefs().get(0).getType().isDecimalOfAnyVersion()) {
                         newAggOperator = buildArrayAggDistinct(oldFunctionCall);
+                        changed = true;
                     } else {
                         newAggOperator = oldFunctionCall;
                     }
@@ -91,6 +95,7 @@ public class MultiDistinctByMultiFuncRewriter {
             CallOperator oldFunctionCall = aggMap.getValue();
             if (oldFunctionCall.isDistinct() && oldFunctionCall.getFnName().equals(FunctionSet.AVG)) {
                 hasAvg = true;
+                changed = true;
                 CallOperator count = buildMultiCountDistinct(oldFunctionCall);
                 ColumnRefOperator countColRef = null;
                 for (Map.Entry<ColumnRefOperator, CallOperator> entry : newAggMap.entrySet()) {
@@ -137,6 +142,10 @@ public class MultiDistinctByMultiFuncRewriter {
                 projections.put(aggMap.getKey(), aggMap.getKey());
                 newAggMapWithAvg.put(aggMap.getKey(), aggMap.getValue());
             }
+        }
+
+        if (!changed) {
+            return Lists.newArrayList();
         }
 
         OptExpression result;
