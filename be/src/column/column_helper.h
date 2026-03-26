7f667a9e7834d61a97a1ed5e637236bed5a6349a
kangkaisen <kangkaisen@apache.org>
2026-03-26 10:16:49 +0800

    [BugFix] Fix column_helper get_data_column is const bug (#70772)
    (cherry picked from commit db799752b94040a35aa69a605f7b738a08df6029)
    
    # Conflicts:
    #	be/src/column/column_helper.h
    #	be/test/column/column_helper_test.cpp

--- be/src/column/column_helper.h
+++ be/src/column/column_helper.h
@@ -383,12 +383,19 @@ public:
     }
 
     static Column* get_data_column(Column* column) {
-        if (column->is_nullable()) {
+        if (column->is_constant()) {
+            auto* const_column = down_cast<ConstColumn*>(column);
+            return get_data_column(const_column->data_column_raw_ptr());
+        } else if (column->is_nullable()) {
             auto* nullable_column = down_cast<NullableColumn*>(column);
+<<<<<<< HEAD
             return nullable_column->mutable_data_column();
         } else if (column->is_constant()) {
             auto* const_column = down_cast<ConstColumn*>(column);
             return const_column->mutable_data_column();
+=======
+            return nullable_column->data_column_raw_ptr();
+>>>>>>> db799752b9 ([BugFix] Fix column_helper get_data_column is const bug (#70772))
         } else {
             return column;
         }
@@ -397,12 +404,12 @@ public:
     template <LogicalType LT>
     static const RunTimeColumnType<LT>* get_data_column_by_type(const Column* column) {
         using ColumnType = RunTimeColumnType<LT>;
-        if (column->is_nullable()) {
+        if (column->is_constant()) {
+            const auto* const_column = down_cast<const ConstColumn*>(column);
+            return get_data_column_by_type<LT>(const_column->data_column().get());
+        } else if (column->is_nullable()) {
             const auto* nullable_column = down_cast<const NullableColumn*>(column);
             return down_cast<const ColumnType*>(&nullable_column->data_column_ref());
-        } else if (column->is_constant()) {
-            const auto* const_column = down_cast<const ConstColumn*>(column);
-            return down_cast<const ColumnType*>(const_column->data_column().get());
         } else {
             return reinterpret_cast<const ColumnType*>(column);
         }
@@ -422,12 +429,12 @@ public:
     }
 
     static const Column* get_data_column(const Column* column) {
-        if (column->is_nullable()) {
+        if (column->is_constant()) {
+            auto* const_column = down_cast<const ConstColumn*>(column);
+            return get_data_column(const_column->data_column().get());
+        } else if (column->is_nullable()) {
             auto* nullable_column = down_cast<const NullableColumn*>(column);
             return nullable_column->data_column().get();
-        } else if (column->is_constant()) {
-            auto* const_column = down_cast<const ConstColumn*>(column);
-            return const_column->data_column().get();
         } else {
             return column;
         }
