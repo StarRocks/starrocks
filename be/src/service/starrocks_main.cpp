538416f99e8b414422f55079189cf2b7cb364c0e
wanpengfei-git <wanpengfei91@163.com>
2026-03-26 06:29:37 +0000

    style: auto-fix clang-format issues

--- be/src/service/starrocks_main.cpp
+++ be/src/service/starrocks_main.cpp
@@ -60,7 +60,7 @@
 =======
 #include "runtime/current_thread.h"
 >>>>>>> cb56d3a319 ([Tool] Expose CurrentThread thread local offset (#70780))
-#include "runtime/exec_env.h"
+        #include "runtime/exec_env.h"
 #include "runtime/heartbeat_flags.h"
 #include "runtime/jdbc_driver_manager.h"
 #include "runtime/memory/roaring_hook.h"
@@ -80,7 +80,7 @@
 #error _GLIBCXX_USE_CXX11_ABI must be non-zero
 #endif
 
-DECLARE_bool(s2debug);
+        DECLARE_bool(s2debug);
 
 static void help(const char*);
 
