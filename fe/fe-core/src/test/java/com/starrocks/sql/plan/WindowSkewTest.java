93003879661c811a8f22e5deab3b9101ec50917a
Oliver Layer <o.layer@celonis.com>
2026-03-30 07:18:21 +0200

    [BugFix] Use explicit cast in skew rules for MCVs (#70858)
    Signed-off-by: Oliver Layer <o.layer@celonis.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/WindowSkewTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/WindowSkewTest.java
@@ -77,6 +77,7 @@ class WindowSkewTest extends PlanTestBase {
             StatisticsMetaManager m = new StatisticsMetaManager();
             m.createStatisticsTablesForTest();
         }
+
     }
 
     @BeforeEach
@@ -214,8 +215,7 @@ class WindowSkewTest extends PlanTestBase {
         assertContains(plan, "UNION");
         assertContains(plan, "Predicates: [1: p, INT, true] = 1");
         // Ensure that unskewed partition preserves NULLs
-        assertContains(plan, "Predicates: (cast([5: p, INT, true] as VARCHAR(1048576)) != '1') " +
-                "OR ([5: p, INT, true] IS NULL)");
+        assertContains(plan, "Predicates: ([5: p, INT, true] != 1) OR ([5: p, INT, true] IS NULL)");
 
         assertContains(plan,
                 "ANALYTIC\n" +
