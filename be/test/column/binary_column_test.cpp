e5cfde4bd1551adcd13671b3645a4b46db783202
trueeyu <lxhhust350@qq.com>
2026-03-30 16:23:33 +0800

    fix
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/test/column/binary_column_test.cpp
+++ be/test/column/binary_column_test.cpp
@@ -748,7 +748,7 @@ PARALLEL_TEST(BinaryColumnTest, test_append_cross_type_large_to_binary_unsupport
     src->append_string("hello");
     auto dst = BinaryColumn::create();
     dst->append_string("bar");
-    ASSERT_DEATH_IF_SUPPORTED(dst->append(*src, 0, 1), ".*");
+    ASSERT_DEATH_IF_SUPPORTED(dst->append(*src, 0, 1), "incompatible column type");
 }
 
 } // namespace starrocks
