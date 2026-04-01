44539adaf54b7e39e87730c37d8edbac022d23f8
srlch <linzichao@starrocks.com>
2026-04-01 14:41:44 +0800

    fix
    Signed-off-by: srlch <linzichao@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/service/FrontendServiceImpl.java
+++ fe/fe-core/src/main/java/com/starrocks/service/FrontendServiceImpl.java
@@ -1138,6 +1138,8 @@ public class FrontendServiceImpl implements FrontendService.Iface {
             final TMasterOpResult result = new TMasterOpResult();
             result.setErrorMsg(e.getMessage());
             return result;
+        } finally {
+            ConnectContext.remove();
         }
     }
 
