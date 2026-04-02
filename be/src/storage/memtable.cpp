d3df1b4c1c77f04a159077160fb8e5279c937d92
wyb <wybb86@gmail.com>
2026-04-02 11:04:04 +0800

    [Enhancement] Pause routine load job on non-retryable errors like primary key size exceed (#71161)
    Signed-off-by: wyb <wybb86@gmail.com>

--- be/src/storage/memtable.cpp
+++ be/src/storage/memtable.cpp
@@ -31,6 +31,7 @@
 #include "runtime/starrocks_metrics.h"
 #include "storage/chunk_helper.h"
 #include "storage/memtable_sink.h"
+#include "storage/non_retryable_load_errors.h"
 #include "storage/primary_key_encoder.h"
 #include "storage/row_store_encoder.h"
 #include "storage/row_store_encoder_factory.h"
@@ -303,7 +304,7 @@ Status MemTable::finalize() {
                 _aggregator.reset();
                 _aggregator_memory_usage = 0;
                 _aggregator_bytes_usage = 0;
-                return Status::Cancelled("primary key size exceed the limit.");
+                return Status::Cancelled(kPrimaryKeySizeExceedError);
             }
             if (_has_op_slot) {
                 // TODO(cbl): mem_tracker
