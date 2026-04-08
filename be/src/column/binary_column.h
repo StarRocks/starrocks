05037ef4478ac20939ef3b3cd26aa2bf67ebfccf
yan zhang <dirtysalt1987@gmail.com>
2026-04-08 15:42:30 +0800

    fix conflict
    Signed-off-by: yan zhang <dirtysalt1987@gmail.com>

--- be/src/column/binary_column.h
+++ be/src/column/binary_column.h
@@ -444,14 +444,11 @@ private:
     mutable GermanStringContainer _german_strings;
     mutable bool _german_strings_cache = false;
 
-<<<<<<< HEAD
     BinaryDataProxyContainer _immuable_container = BinaryDataProxyContainer(*this);
-=======
     // True when this column holds BINARY / VARBINARY data.  Causes put_mysql_row_buffer to
     // use push_binary (hex/base64 encoding) instead of push_string when inside a
     // nested type context.
     bool _is_binary_type = false;
->>>>>>> 00fd532d3a ([BugFix] Encode VARBINARY correctly inside nested types in MySQL result sets (#71346))
 };
 
 using Offsets = BinaryColumnBase<uint32_t>::Offsets;
