79c41195f4537bd16b44ced302b9af0260153e15
wangruin <913474924@qq.com>
2026-04-08 15:08:43 +0800

    [BugFix] Add missing Calendar import in JDBCScanner
    The backport of #71016 to branch-4.1 introduced Calendar usage in
    computeTimezoneOffsetMillis() but missed the import statement, causing
    compilation failure. The original PR on main already had this import
    from pre-existing Calendar member variables that don't exist on 4.1.

--- java-extensions/jdbc-bridge/src/main/java/com/starrocks/jdbcbridge/JDBCScanner.java
+++ java-extensions/jdbc-bridge/src/main/java/com/starrocks/jdbcbridge/JDBCScanner.java
@@ -41,6 +41,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoField;
+import java.util.Calendar;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
