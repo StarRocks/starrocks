7f687e7092d93959e3a5d5eb789c4e4c7c20bc1f
SevenJ <166966490+Wenjun7J@users.noreply.github.com>
2026-04-09 14:24:07 +0800

    [BugFix] Jdbc fix oracle nls format (#71412)
    Signed-off-by: SevenJ <wenjun7j@gmail.com>
    Signed-off-by: shiwenjun <wenjun7j@gmail.com>
    Signed-off-by: SevenJ <166966490+Wenjun7J@users.noreply.github.com>
    (cherry picked from commit b27ecfc6d9127a7bfff29e6c26752d36b58b67ed)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/planner/JDBCScanNode.java
    #	fe/fe-core/src/test/java/com/starrocks/planner/MySqlAndJDBCScanNodeTest.java

--- fe/fe-core/src/main/java/com/starrocks/catalog/JDBCTable.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/JDBCTable.java
@@ -53,6 +53,10 @@ public class JDBCTable extends Table {
     private String dbName;
     private List<Column> partitionColumns;
 
+    // Transient: original JDBC column types (java.sql.Types) from the external database.
+    // Used for Oracle datetime predicate pushdown to determine TO_DATE/TO_TIMESTAMP wrapping.
+    private transient Map<String, Integer> originalJdbcColumnTypes;
+
     public JDBCTable() {
         super(TableType.JDBC);
     }
@@ -102,6 +106,14 @@ public class JDBCTable extends Table {
     @Override
     public List<Column> getPartitionColumns() {
         return partitionColumns;
+    }
+
+    public Map<String, Integer> getOriginalJdbcColumnTypes() {
+        return originalJdbcColumnTypes;
+    }
+
+    public void setOriginalJdbcColumnTypes(Map<String, Integer> originalJdbcColumnTypes) {
+        this.originalJdbcColumnTypes = originalJdbcColumnTypes;
     }
 
     public Map<String, String> getConnectInfo() {
