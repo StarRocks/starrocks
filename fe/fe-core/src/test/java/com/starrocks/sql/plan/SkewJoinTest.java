72461cb9360a337bf3fd4b601779e9909edc884c
save-my-heart <save-my-heart@outlook.com>
2026-04-09 21:04:46 +0800

    [BugFix] Fix incorrect shuffle distribution due to missing project node (#71075)
    Co-authored-by: stdpain <drfeng08@gmail.com>
    (cherry picked from commit e8a3f3414f9210f52a172fe4b2918918c3e40197)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/sql/optimizer/QueryOptimizer.java
    #	fe/fe-core/src/test/java/com/starrocks/sql/plan/SkewJoinTest.java

--- fe/fe-core/src/test/java/com/starrocks/sql/plan/SkewJoinTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/plan/SkewJoinTest.java
@@ -216,6 +216,7 @@ public class SkewJoinTest extends PlanTestBase {
         sql = "select struct_tbl.c0, struct_tbl.c2.a, t0.v2 from default_catalog.test.struct_tbl " +
                 "join[skew|test.struct_tbl.c1.a(1,2)] test.t0 on t0.v1 = c1.a ";
         sqlPlan = getFragmentPlan(sql);
+<<<<<<< HEAD
         assertCContains(sqlPlan, "HASH JOIN\n" +
                 "  |  join op: INNER JOIN (PARTITIONED)\n" +
                 "  |  colocate: false, reason: \n" +
@@ -223,6 +224,13 @@ public class SkewJoinTest extends PlanTestBase {
                 "  |  equal join conjunct: 9: cast = 5: v1",
                 "<slot 10> : CASE WHEN 2: c1.a[true] IS NULL THEN 24: round WHEN 2: c1.a[true] IN (1, 2) THEN 24: " +
                         "round ELSE 0 END");
+=======
+        assertCContains(sqlPlan, "  14:HASH JOIN\n" +
+                "  |  join op: INNER JOIN (PARTITIONED)\n" +
+                "  |  colocate: false, reason: \n" +
+                "  |  equal join conjunct: 10: rand_col = 17: rand_col\n" +
+                "  |  equal join conjunct: 9: cast = 5: v1");
+>>>>>>> e8a3f3414f ([BugFix] Fix incorrect shuffle distribution due to missing project node (#71075))
     }
 
     @Test
