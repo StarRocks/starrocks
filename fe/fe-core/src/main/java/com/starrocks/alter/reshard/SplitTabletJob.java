abecd88f51a0bbd3340e5baad43bd0f00c9f9581
andyziye <108652123+andyziye@users.noreply.github.com>
2026-04-14 11:05:23 +0800

    [Enhancement] Add tablet reshard metrics for split and merge operations(sync #71205) (#52590)
    * [Enhancement] Add tablet reshard metrics for split and merge operations
    
    * fix conflict (#52960)
    
    Signed-off-by: xiangguangyxg <xiangguangyxg@gmail.com>
    
    ---------
    
    Signed-off-by: xiangguangyxg <xiangguangyxg@gmail.com>
    Co-authored-by: xiangguangyxg <110401425+xiangguangyxg@users.noreply.github.com>

--- fe/fe-core/src/main/java/com/starrocks/alter/reshard/SplitTabletJob.java
+++ fe/fe-core/src/main/java/com/starrocks/alter/reshard/SplitTabletJob.java
@@ -32,6 +32,7 @@ import com.starrocks.common.AnalysisException;
 import com.starrocks.common.util.TimeUtils;
 import com.starrocks.common.util.concurrent.lock.LockType;
 import com.starrocks.lake.Utils;
+import com.starrocks.metric.MetricRepo;
 import com.starrocks.proto.TxnInfoPB;
 import com.starrocks.proto.TxnTypePB;
 import com.starrocks.server.GlobalStateMgr;
@@ -303,6 +304,12 @@ public class SplitTabletJob extends TabletReshardJob {
         // 4. Set tablet state to NORMAL
         setTableState(OlapTable.OlapTableState.TABLET_RESHARD, OlapTable.OlapTableState.NORMAL);
 
+        // 5. Update metrics
+        if (MetricRepo.hasInit) {
+            MetricRepo.COUNTER_TABLET_RESHARD_SPLIT_JOB_FINISHED.increase(1L);
+            MetricRepo.HISTO_TABLET_RESHARD_JOB_DURATION.update(System.currentTimeMillis() - createdTimeMs);
+        }
+
         // 6. Set job state to FINISHED
         setJobState(JobState.FINISHED);
     }
@@ -316,7 +323,8 @@ public class SplitTabletJob extends TabletReshardJob {
      * 1. Unregister resharding tablets
      * 2. Remove new tablets from inverted index
      * 3. Set table state to NORMAL
-     * 4. Set job state to ABORTED
+     * 4. Update metrics
+     * 5. Set job state to ABORTED
      */
     @Override
     protected void runAbortingJob() {
@@ -333,7 +341,12 @@ public class SplitTabletJob extends TabletReshardJob {
             LOG.warn("Ignore exception when aborting tablet reshard job. {}. ", this, e);
         }
 
-        // 4. Set job state to ABORTED
+        // 4. Update metrics
+        if (MetricRepo.hasInit) {
+            MetricRepo.COUNTER_TABLET_RESHARD_SPLIT_JOB_ABORTED.increase(1L);
+        }
+
+        // 5. Set job state to ABORTED
         setJobState(JobState.ABORTED);
     }
 
