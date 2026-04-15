03a9cded2fc2f2e9bee072b0e21757a829325e95
andyziye <108652123+andyziye@users.noreply.github.com>
2026-04-15 14:58:46 +0800

    [BugFix] preserve varchar length after reduce cast with global variables (backport #70269)(sync #71463) (#52836)
    * [BugFix] preserve varchar length after reduce cast with global variables (backport #70269)
    
    * Update GlobalVariable.java
    
    ---------
    
    Co-authored-by: Planck Li <jaogoy@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/GlobalVariable.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/GlobalVariable.java
@@ -80,6 +80,10 @@ public final class GlobalVariable {
     public static final String ARROW_FLIGHT_PROXY = "arrow_flight_proxy";
     public static final String ARROW_FLIGHT_PROXY_ENABLED = "arrow_flight_proxy_enabled";
     public static final String MAX_UNKNOWN_STRING_META_LENGTH = "max_unknown_string_meta_length";
+    public static final String ENABLE_REDUCE_CAST_VARCHAR_LENGTH_INHERITANCE =
+            "enable_reduce_cast_varchar_length_inheritance";
+    public static final String ENABLE_REDUCE_CAST_VARCHAR_EXPR_SYNC_TYPE =
+            "enable_reduce_cast_varchar_expr_sync_type";
 
     //AutoMV's MVLifecycle
     public static final String ENABLE_AUTOMV_LIFECYCLE_KEEPER = "enable_automv_lifecycle_keeper";
@@ -326,6 +330,11 @@ public final class GlobalVariable {
     @VariableMgr.VarAttr(name = MAX_UNKNOWN_STRING_META_LENGTH, flag = VariableMgr.GLOBAL)
     private static int maxUnknownStringMetaLength = 64;
 
+    @VariableMgr.VarAttr(name = ENABLE_REDUCE_CAST_VARCHAR_LENGTH_INHERITANCE, flag = VariableMgr.GLOBAL)
+    private static boolean enableReduceCastVarcharLengthInheritance = false;
+
+    @VariableMgr.VarAttr(name = ENABLE_REDUCE_CAST_VARCHAR_EXPR_SYNC_TYPE, flag = VariableMgr.GLOBAL)
+    private static boolean enableReduceCastVarcharExprSyncType = false;
     @VariableMgr.VarAttr(name = CNGROUP_RESOURCE_USAGE_FRESH_RATIO)
     private static double cngroupResourceUsageFreshRatio = 0.5;
 
@@ -707,6 +716,22 @@ public final class GlobalVariable {
 
     public static void setCngroupResourceUsageFreshRatio(double value) {
         cngroupResourceUsageFreshRatio = value;
+    }
+
+    public static boolean isEnableReduceCastVarcharLengthInheritance() {
+        return enableReduceCastVarcharLengthInheritance;
+    }
+
+    public static void setEnableReduceCastVarcharLengthInheritance(boolean enableReduceCastVarcharLengthInheritance) {
+        GlobalVariable.enableReduceCastVarcharLengthInheritance = enableReduceCastVarcharLengthInheritance;
+    }
+
+    public static boolean isEnableReduceCastVarcharExprSyncType() {
+        return enableReduceCastVarcharExprSyncType;
+    }
+
+    public static void setEnableReduceCastVarcharExprSyncType(boolean enableReduceCastVarcharExprSyncType) {
+        GlobalVariable.enableReduceCastVarcharExprSyncType = enableReduceCastVarcharExprSyncType;
     }
 
     public static double getCngroupResourceUsageFreshRatio() {
