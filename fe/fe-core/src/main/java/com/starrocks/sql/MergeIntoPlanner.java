d657ce0bcd174f2833f16228186e0447c491a3eb
GavinMar <yangguansuo@starrocks.com>
2026-04-15 15:19:21 +0800

    [Feature] Add MergeIntoPlanner with EnforceUniqueNode and partition shuffle

---
+++ fe/fe-core/src/main/java/com/starrocks/sql/MergeIntoPlanner.java
@@ -0,0 +1,156 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.starrocks.sql;
+
+import com.google.common.base.Preconditions;
+import com.starrocks.catalog.Column;
+import com.starrocks.catalog.IcebergTable;
+import com.starrocks.catalog.Table;
+import com.starrocks.connector.iceberg.IcebergMetadata;
+import com.starrocks.planner.DataSink;
+import com.starrocks.planner.DescriptorTable;
+import com.starrocks.planner.EnforceUniqueNode;
+import com.starrocks.planner.IcebergRowDeltaSink;
+import com.starrocks.planner.PlanFragment;
+import com.starrocks.planner.PlanNode;
+import com.starrocks.planner.PlanNodeId;
+import com.starrocks.planner.SlotDescriptor;
+import com.starrocks.planner.TupleDescriptor;
+import com.starrocks.qe.ConnectContext;
+import com.starrocks.sql.analyzer.SemanticException;
+import com.starrocks.sql.ast.MergeIntoStmt;
+import com.starrocks.sql.ast.QueryRelation;
+import com.starrocks.sql.ast.expression.Expr;
+import com.starrocks.sql.optimizer.OptExpression;
+import com.starrocks.sql.optimizer.Optimizer;
+import com.starrocks.sql.optimizer.OptimizerContext;
+import com.starrocks.sql.optimizer.OptimizerFactory;
+import com.starrocks.sql.optimizer.base.ColumnRefFactory;
+import com.starrocks.sql.optimizer.base.ColumnRefSet;
+import com.starrocks.sql.optimizer.base.PhysicalPropertySet;
+import com.starrocks.sql.optimizer.operator.scalar.ColumnRefOperator;
+import com.starrocks.sql.optimizer.transformer.LogicalPlan;
+import com.starrocks.sql.optimizer.transformer.RelationTransformer;
+import com.starrocks.sql.plan.ExecPlan;
+import com.starrocks.sql.plan.PlanFragmentBuilder;
+import com.starrocks.thrift.TResultSinkType;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeIntoPlanner {
+
+    public ExecPlan plan(MergeIntoStmt mergeIntoStmt, ConnectContext session) {
+        QueryRelation query = mergeIntoStmt.getQueryStatement().getQueryRelation();
+        List<String> colNames = query.getColumnOutputNames();
+        ColumnRefFactory columnRefFactory = new ColumnRefFactory();
+        LogicalPlan logicalPlan = new RelationTransformer(columnRefFactory, session).transform(query);
+
+        List<ColumnRefOperator> outputColumns = logicalPlan.getOutputColumn();
+        Table targetTable = mergeIntoStmt.getTable();
+
+        if (!(targetTable instanceof IcebergTable)) {
+            throw new SemanticException("MERGE INTO is only supported for Iceberg tables");
+        }
+
+        IcebergTable icebergTable = (IcebergTable) targetTable;
+        PhysicalPropertySet requiredProperty = IcebergPlannerUtils.createShuffleProperty(icebergTable, outputColumns);
+        colNames = mergeIntoStmt.getIcebergColumnOutputNames();
+
+        return createMergePlan(mergeIntoStmt, session, logicalPlan.getRootBuilder().getRoot(),
+                columnRefFactory, outputColumns, colNames, icebergTable, requiredProperty);
+    }
+
+    private ExecPlan createMergePlan(MergeIntoStmt mergeIntoStmt, ConnectContext session,
+                                     OptExpression logicalRoot, ColumnRefFactory columnRefFactory,
+                                     List<ColumnRefOperator> outputColumns, List<String> colNames,
+                                     IcebergTable icebergTable, PhysicalPropertySet requiredProperty) {
+        boolean isEnablePipeline = session.getSessionVariable().isEnablePipelineEngine();
+        boolean canUsePipeline = isEnablePipeline && DataSink.canTableSinkUsePipeline(icebergTable);
+        boolean forceDisablePipeline = isEnablePipeline && !canUsePipeline;
+        boolean prevIsEnableLocalShuffleAgg = session.getSessionVariable().isEnableLocalShuffleAgg();
+        try {
+            if (forceDisablePipeline) {
+                session.getSessionVariable().setEnablePipelineEngine(false);
+            }
+            session.getSessionVariable().setEnableLocalShuffleAgg(false);
+
+            // Optimize
+            OptimizerContext optimizerContext = OptimizerFactory.initContext(session, columnRefFactory);
+            Optimizer optimizer = OptimizerFactory.create(optimizerContext);
+            OptExpression optimizedPlan = optimizer.optimize(
+                    logicalRoot, requiredProperty, new ColumnRefSet(outputColumns));
+
+            // Build physical plan
+            ExecPlan execPlan = PlanFragmentBuilder.createPhysicalPlan(optimizedPlan, session,
+                    outputColumns, columnRefFactory, colNames, TResultSinkType.MYSQL_PROTOCAL, false);
+
+            // Setup Iceberg sink and configure pipeline
+            setupIcebergMergeSink(execPlan, colNames, icebergTable, session);
+            IcebergPlannerUtils.configureIcebergSinkPipeline(execPlan, session, canUsePipeline);
+
+            return execPlan;
+        } finally {
+            session.getSessionVariable().setEnableLocalShuffleAgg(prevIsEnableLocalShuffleAgg);
+            if (forceDisablePipeline) {
+                session.getSessionVariable().setEnablePipelineEngine(true);
+            }
+        }
+    }
+
+    private void setupIcebergMergeSink(ExecPlan execPlan, List<String> colNames,
+                                       IcebergTable icebergTable, ConnectContext session) {
+        DescriptorTable descriptorTable = execPlan.getDescTbl();
+        TupleDescriptor rowDeltaTuple = descriptorTable.createTupleDescriptor();
+
+        List<Expr> outputExprs = execPlan.getOutputExprs();
+        Preconditions.checkArgument(colNames.size() == outputExprs.size(),
+                "output column size mismatch");
+        for (int index = 0; index < colNames.size(); ++index) {
+            SlotDescriptor slot = descriptorTable.addSlotDescriptor(rowDeltaTuple);
+            slot.setIsMaterialized(true);
+            slot.setType(outputExprs.get(index).getType());
+            slot.setColumn(new Column(colNames.get(index), outputExprs.get(index).getType()));
+            slot.setIsNullable(outputExprs.get(index).isNullable());
+        }
+        rowDeltaTuple.computeMemLayout();
+
+        descriptorTable.addReferencedTable(icebergTable);
+        IcebergRowDeltaSink dataSink = new IcebergRowDeltaSink(
+                icebergTable, rowDeltaTuple, session.getSessionVariable());
+        dataSink.init();
+
+        IcebergMetadata.IcebergSinkExtra icebergSinkExtra = new IcebergMetadata.IcebergSinkExtra();
+        org.apache.iceberg.expressions.Expression filterExpr = IcebergPlannerUtils.buildIcebergFilterExpr(execPlan);
+        if (filterExpr != null) {
+            icebergSinkExtra.setConflictDetectionFilter(filterExpr);
+        }
+        dataSink.setSinkExtraInfo(icebergSinkExtra);
+
+        execPlan.getFragments().get(0).setSink(dataSink);
+
+        // Insert EnforceUniqueNode to check that each target row is matched at most once.
+        // Key columns are _file (index 0) and _pos (index 1) in the output.
+        insertEnforceUniqueNode(execPlan, Arrays.asList(0, 1));
+    }
+
+    private void insertEnforceUniqueNode(ExecPlan execPlan, List<Integer> keyColIndices) {
+        PlanFragment sinkFragment = execPlan.getFragments().get(0);
+        PlanNode currentRoot = sinkFragment.getPlanRoot();
+        PlanNodeId nodeId = execPlan.getNextNodeId();
+        EnforceUniqueNode enforceNode = new EnforceUniqueNode(nodeId, currentRoot, keyColIndices);
+        sinkFragment.setPlanRoot(enforceNode);
+    }
+}
