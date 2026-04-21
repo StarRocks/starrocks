6c2ac6837cc0babca3aed92873080c3bda7007f6
kaijianding <kaijian.ding@gmail.com>
2026-04-21 17:03:49 +0800

    [Enhancement] Add tablet_num metric for share data cluster (#71444)
    Signed-off-by: kaijian.ding <kaijian.ding@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/system/SystemInfoService.java
+++ fe/fe-core/src/main/java/com/starrocks/system/SystemInfoService.java
@@ -251,6 +251,9 @@ public class SystemInfoService implements GsonPostProcessable {
         GlobalStateMgr.getCurrentState().getEditLog().logAddComputeNode(
                 newComputeNode, wal -> replayAddComputeNode((ComputeNode) wal));
         LOG.info("finished to add {} ", newComputeNode);
+
+        // compute nodes are changed, regenerate tablet number metrics
+        MetricRepo.generateBackendsTabletMetrics();
     }
 
     public boolean isSingleBackendAndComputeNode() {
@@ -661,6 +664,9 @@ public class SystemInfoService implements GsonPostProcessable {
                 new DropComputeNodeLog(dropComputeNode.getId()),
                 wal -> removeComputeNode((DropComputeNodeLog) wal));
         LOG.info("finished to drop {}", dropComputeNode);
+
+        // compute nodes are changed, regenerate tablet number metrics
+        MetricRepo.generateBackendsTabletMetrics();
     }
 
     public void dropBackends(DropBackendClause dropBackendClause) throws DdlException {
@@ -790,7 +796,7 @@ public class SystemInfoService implements GsonPostProcessable {
                 new DropBackendInfo(droppedBackend.getId()), wal -> removeBackend((DropBackendInfo) wal));
         LOG.info("finished to drop {}", droppedBackend);
 
-        // backends are changed, regenerated tablet number metrics
+        // backends are changed, regenerate tablet number metrics
         MetricRepo.generateBackendsTabletMetrics();
     }
 
