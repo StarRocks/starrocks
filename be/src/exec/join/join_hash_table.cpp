9e83ff66f8932b0554f6c583a278415d9ca84ea2
alvin-celerdata <alvin.zhao@celerdata.com>
2026-04-28 18:54:08 -0700

    Fix UT
    Signed-off-by: alvin-celerdata <alvin.zhao@celerdata.com>

--- be/src/exec/join/join_hash_table.cpp
+++ be/src/exec/join/join_hash_table.cpp
@@ -22,14 +22,13 @@
 #include "column/chunk.h"
 #include "column/vectorized_fwd.h"
 #include "common/runtime_profile.h"
-#include "common/statusor.h"
 #include "common/stack_util.h"
+#include "common/statusor.h"
 #include "common/system/cpu_info.h"
 #include "exec/join/join_hash_map_method.h"
 #include "exec/join/join_key_constructor.h"
 #include "exprs/column_ref.h"
 #include "runtime/descriptors.h"
-#include "serde/column_array_serde.h"
 #include "types/logical_type_infra.h"
 
 namespace starrocks {
