9fccbfc6389471a49aabff3bc8585b316273a90a
andyziye <108652123+andyziye@users.noreply.github.com>
2026-05-04 01:10:24 +0800

    [Enhancement] Add sort-key sampling to lake segments for accurate tablet split(sync #71788) (#53908)
    * [Enhancement] Add sort-key sampling to lake segments for accurate tablet split
    
    * Fix conflict
    
    Signed-off-by: alvin-celerdata <alvin.zhao@celerdata.com>
    
    ---------
    
    Signed-off-by: alvin-celerdata <alvin.zhao@celerdata.com>
    Co-authored-by: xiangguangyxg <110401425+xiangguangyxg@users.noreply.github.com>
    Co-authored-by: alvin-celerdata <alvin.zhao@celerdata.com>

--- be/src/storage/lake/general_tablet_writer.cpp
+++ be/src/storage/lake/general_tablet_writer.cpp
@@ -302,8 +302,7 @@ Status HorizontalGeneralTabletWriter::flush_segment_writer(SegmentPB* segment) {
             // This is a bundle data file.
             segment_file_info.bundle_file_offset = _seg_writer->bundle_file_offset();
         }
-        segment_file_info.sort_key_min = _seg_writer->get_sort_key_min();
-        segment_file_info.sort_key_max = _seg_writer->get_sort_key_max();
+        _seg_writer->write_sort_key_fields_to(segment_file_info);
         segment_file_info.num_rows = _seg_writer->num_rows();
         // Record which vector indexes need .vi files
         // Skip bundled segments: .vi files don't support bundle format yet
@@ -471,8 +470,7 @@ Status VerticalGeneralTabletWriter::finish(SegmentPB* segment) {
         segment_file_info.path = std::string(basename(segment_path));
         segment_file_info.size = segment_size;
         segment_file_info.encryption_meta = segment_writer->encryption_meta();
-        segment_file_info.sort_key_min = segment_writer->get_sort_key_min();
-        segment_file_info.sort_key_max = segment_writer->get_sort_key_max();
+        segment_writer->write_sort_key_fields_to(segment_file_info);
         segment_file_info.num_rows = segment_writer->num_rows();
         // Record which vector indexes need .vi files
         if (segment_writer->defer_vector_index_build()) {
