38b377def42ed8081d7e93851c294a1e40abe11d
trueeyu <lxhhust350@qq.com>
2026-05-04 19:53:18 +0800

    Revert "[BugFix] insert files credential redaction 3.5-cc (backport #71245) (#71865)"
    This reverts commit cc7b9754b1d347fa003443f52b0a5e4ca0cdf2b6.

--- fe/fe-core/src/test/java/com/starrocks/common/util/SqlCredentialRedactorTest.java
+++ fe/fe-core/src/test/java/com/starrocks/common/util/SqlCredentialRedactorTest.java
@@ -37,55 +37,6 @@ public class SqlCredentialRedactorTest {
     }
 
     @Test
-    public void testRedactFilesCredentialScenarios() {
-        // Case 1: INSERT INTO ... SELECT FROM FILES(...)
-        String insertSelectSql = "INSERT INTO t0 SELECT * FROM FILES(\n" +
-                "        \"path\" = \"s3://bucket/data.parquet\",\n" +
-                "        \"format\" = \"parquet\",\n" +
-                "        \"aws.s3.access_key\" = \"AKIA_INSERT_SOURCE\",\n" +
-                "        \"aws.s3.secret_key\" = \"SOURCE_SECRET\"\n" +
-                ")";
-        String insertSelectRedacted = SqlCredentialRedactor.redact(insertSelectSql);
-        String insertSelectExpected = "INSERT INTO t0 SELECT * FROM FILES(\n" +
-                "        \"path\" = \"s3://bucket/data.parquet\",\n" +
-                "        \"format\" = \"parquet\",\n" +
-                "        \"aws.s3.access_key\" = ***,\n" +
-                "        \"aws.s3.secret_key\" = ***\n" +
-                ")";
-        Assertions.assertEquals(insertSelectExpected, insertSelectRedacted);
-
-        // Case 2: INSERT INTO FILES(...) SELECT ...
-        String insertIntoFilesSql = "INSERT INTO FILES(\n" +
-                "        \"path\" = \"s3://bucket/output/\",\n" +
-                "        \"format\" = \"parquet\",\n" +
-                "        \"aws.s3.access_key\" = \"AKIA_INSERT_TARGET\",\n" +
-                "        \"aws.s3.secret_key\" = \"TARGET_SECRET\"\n" +
-                ")\n" +
-                "SELECT 1";
-        String insertIntoFilesRedacted = SqlCredentialRedactor.redact(insertIntoFilesSql);
-        Assertions.assertFalse(insertIntoFilesRedacted.contains("AKIA_INSERT_TARGET"),
-                "FILES target access key should be redacted");
-        Assertions.assertFalse(insertIntoFilesRedacted.contains("TARGET_SECRET"),
-                "FILES target secret key should be redacted");
-        Assertions.assertTrue(insertIntoFilesRedacted.contains("***"), "Should contain redacted marker");
-    }
-
-    @Test
-    public void testMayNeedCredentialRedactionScenarios() {
-        // Case 1: null/empty/plain SQL should be skipped.
-        Assertions.assertFalse(SqlCredentialRedactor.mayNeedCredentialRedaction(null));
-        Assertions.assertFalse(SqlCredentialRedactor.mayNeedCredentialRedaction(""));
-        Assertions.assertFalse(SqlCredentialRedactor.mayNeedCredentialRedaction("SELECT 1"));
-        Assertions.assertFalse(SqlCredentialRedactor.mayNeedCredentialRedaction("SELECT * FROM t0 WHERE id = 1"));
-
-        // Case 2: direct credential key markers should be detected.
-        Assertions.assertTrue(SqlCredentialRedactor.mayNeedCredentialRedaction(
-                "\"aws.s3.secret_key\" = \"x\""));
-        Assertions.assertTrue(SqlCredentialRedactor.mayNeedCredentialRedaction(
-                "\"password\" = \"x\""));
-    }
-
-    @Test
     public void testRedactAzureCredentials() {
         String sql = "CREATE EXTERNAL TABLE test (\n" +
                 "    id INT\n" +
@@ -102,74 +53,6 @@ public class SqlCredentialRedactorTest {
         Assertions.assertFalse(redacted.contains("abcdefg"), "oauth2_client_secret should be redacted");
         Assertions.assertFalse(redacted.contains("sv=2020-08-04"), "SAS token should be redacted");
         Assertions.assertTrue(redacted.contains("abfs://container@account.dfs.core.windows.net/path"), "Path should remain");
-    }
-
-    @Test
-    public void testRedactAzureOAuth2ClientId() {
-        String sql = "select * from FILES(\n" +
-                "    \"path\" = \"abfs://container@account.dfs.core.windows.net/path\",\n" +
-                "    \"format\" = \"parquet\",\n" +
-                "    \"azure.adls2.oauth2_client_id\" = \"fab0d1b8-3af6-4e75-b934-a6f7f93dedd7\",\n" +
-                "    \"azure.adls2.oauth2_client_secret\" = \"DevId_A08-MpqyRQGByA5\",\n" +
-                "    \"azure.adls2.oauth2_client_endpoint\" = \"https://login.microsoftonline.com/tenant-id/oauth2/token\"\n" +
-                ")";
-
-        String redacted = SqlCredentialRedactor.redact(sql);
-        Assertions.assertFalse(redacted.contains("fab0d1b8-3af6-4e75-b934-a6f7f93dedd7"),
-                "OAuth2 client_id should be redacted");
-        Assertions.assertFalse(redacted.contains("DevId_A08-MpqyRQGByA5"),
-                "OAuth2 client_secret should be redacted");
-        Assertions.assertTrue(redacted.contains("oauth2_client_endpoint"),
-                "oauth2_client_endpoint key should remain");
-        Assertions.assertTrue(redacted.contains("https://login.microsoftonline.com"),
-                "OAuth endpoint URL (non-credential) should remain");
-        Assertions.assertTrue(redacted.contains("***"), "Should contain redacted marker");
-    }
-
-    @Test
-    public void testRedactAzureBlobOAuth2ClientId() {
-        String sql = "select * from FILES(\n" +
-                "    \"path\" = \"abfs://container@blobaccount.dfs.core.windows.net/path\",\n" +
-                "    \"format\" = \"parquet\",\n" +
-                "    \"azure.blob.oauth2_client_id\" = \"11111111-2222-3333-4444-555555555555\",\n" +
-                "    \"azure.blob.oauth2_client_secret\" = \"BlobSecret123!\",\n" +
-                "    \"azure.blob.oauth2_client_endpoint\" = " +
-                "        \"https://login.microsoftonline.com/blob-tenant/oauth2/token\"\n" +
-                ")";
-
-        String redacted = SqlCredentialRedactor.redact(sql);
-        Assertions.assertFalse(redacted.contains("11111111-2222-3333-4444-555555555555"),
-                "Azure Blob OAuth2 client_id should be redacted");
-        Assertions.assertFalse(redacted.contains("BlobSecret123!"),
-                "Azure Blob OAuth2 client_secret should be redacted");
-        Assertions.assertTrue(redacted.contains("oauth2_client_endpoint"),
-                "Azure Blob oauth2_client_endpoint key should remain");
-        Assertions.assertTrue(redacted.contains("https://login.microsoftonline.com"),
-                "Azure Blob OAuth endpoint URL (non-credential) should remain");
-        Assertions.assertTrue(redacted.contains("***"), "Should contain redacted marker");
-    }
-
-    @Test
-    public void testRedactAzureAdls1OAuth2ClientId() {
-        String sql = "select * from FILES(\n" +
-                "    \"path\" = \"abfs://container@adls1account.dfs.core.windows.net/path\",\n" +
-                "    \"format\" = \"parquet\",\n" +
-                "    \"azure.adls1.oauth2_client_id\" = \"aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee\",\n" +
-                "    \"azure.adls1.oauth2_credential\" = \"Adls1Secret456!\",\n" +
-                "    \"azure.adls1.oauth2_client_endpoint\" = " +
-                "        \"https://login.microsoftonline.com/adls1-tenant/oauth2/token\"\n" +
-                ")";
-
-        String redacted = SqlCredentialRedactor.redact(sql);
-        Assertions.assertFalse(redacted.contains("aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee"),
-                "Azure ADLS1 OAuth2 client_id should be redacted");
-        Assertions.assertFalse(redacted.contains("Adls1Secret456!"),
-                "Azure ADLS1 OAuth2 credential should be redacted");
-        Assertions.assertTrue(redacted.contains("oauth2_client_endpoint"),
-                "Azure ADLS1 oauth2_client_endpoint key should remain");
-        Assertions.assertTrue(redacted.contains("https://login.microsoftonline.com"),
-                "Azure ADLS1 OAuth endpoint URL (non-credential) should remain");
-        Assertions.assertTrue(redacted.contains("***"), "Should contain redacted marker");
     }
 
     @Test
@@ -247,10 +130,10 @@ public class SqlCredentialRedactorTest {
         String redacted1 = SqlCredentialRedactor.redact(sql1);
         Assertions.assertFalse(redacted1.contains("AKIAIOSFODNN7EXAMPLE"));
 
-        // Test without quotes on key(not supported)
+        // Test without quotes on key
         String sql2 = "aws.s3.secret_key = \"wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY\"";
         String redacted2 = SqlCredentialRedactor.redact(sql2);
-        Assertions.assertTrue(redacted2.contains("wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"));
+        Assertions.assertFalse(redacted2.contains("wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"));
 
         // Test with extra spaces
         String sql3 = "\"aws.s3.access_key\"   =   \"AKIAIOSFODNN7EXAMPLE\"";
@@ -306,26 +189,5 @@ public class SqlCredentialRedactorTest {
             index += 3;
         }
         Assertions.assertEquals(2, count, "Should have exactly 2 redacted values");
-    }
-
-    @Test
-    public void testExplicitLongLiteralSql() {
-        // Use explicit literals instead of generated huge payloads to keep this deterministic in CI.
-        String longNonCredentialKey = "this_is_a_very_long_property_key_that_should_not_match_any_credential_pattern_"
-                + "because_it_is_plain_metadata_and_not_a_secret";
-        String longValue = "this_is_a_long_but_explicit_literal_value_used_to_verify_that_non_credential_entries_"
-                + "stay_unchanged_even_when_the_sql_text_is_lengthy_and_contains_many_characters_0123456789";
-
-        String nonCredentialSql = "ALTER TABLE t1 SET PROPERTIES(\"" + longNonCredentialKey + "\" = \"" + longValue + "\")";
-        Assertions.assertEquals(nonCredentialSql, SqlCredentialRedactor.redact(nonCredentialSql));
-
-        String credentialSql = "ALTER TABLE t1 SET PROPERTIES("
-                + "\"aws.s3.secret_key\" = \"LONG_SECRET_VALUE_ABCDEF1234567890\", "
-                + "\"comment\" = \"" + longValue + "\")";
-        String redacted = SqlCredentialRedactor.redact(credentialSql);
-        Assertions.assertFalse(redacted.contains("LONG_SECRET_VALUE_ABCDEF1234567890"));
-        Assertions.assertTrue(redacted.contains("\"aws.s3.secret_key\""));
-        Assertions.assertTrue(redacted.contains("***"));
-        Assertions.assertTrue(redacted.contains("\"comment\" = \"" + longValue + "\""));
     }
 }
