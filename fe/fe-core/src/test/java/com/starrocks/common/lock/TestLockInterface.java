638703dd770779c65fa929c6eafc0d4f85bc29e2
Kevin Cai <kevin.cai@celerdata.com>
2026-05-05 08:40:10 +0800

    fix compile
    Signed-off-by: Kevin Cai <kevin.cai@celerdata.com>

--- fe/fe-core/src/test/java/com/starrocks/common/lock/TestLockInterface.java
+++ fe/fe-core/src/test/java/com/starrocks/common/lock/TestLockInterface.java
@@ -33,6 +33,7 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import java.util.concurrent.TimeUnit;
 
