158dea844c139d73befdd2d9b92cc08d78b26def
stdpain <34912776+stdpain@users.noreply.github.com>
2026-05-13 10:43:24 +0800

    [BugFix] Resolve inconsistent global dictionary generation in flat JSON (#72953)
    Signed-off-by: stdpain <drfeng08@gmail.com>
    Signed-off-by: stdpain <34912776+stdpain@users.noreply.github.com>

--- be/src/storage/meta_reader.cpp
+++ be/src/storage/meta_reader.cpp
@@ -449,7 +449,7 @@ Status SegmentMetaCollecter::_collect_dict_for_column(ColumnIterator* column_ite
         ASSIGN_OR_RETURN(const TabletColumn* tablet_column, _get_tablet_column(cid));
         // For JSON data, the schema may be heterogeneous, meaning that some segments might not contain the dictionary column,
         // but a global dictionary could still be present and usable.
-        if (!tablet_column->is_extended()) {
+        if (!tablet_column->is_extended() || !column_iter->only_nulls()) {
             return Status::GlobalDictError("no global dict");
         } else {
             return Status::OK();
