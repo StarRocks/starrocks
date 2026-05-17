97206ebc9bdc9799eae4500841a4394bae13d704
alvin-celerdata <alvin.zhao@celerdata.com>
2026-05-17 13:36:14 -0700

    [Refactor] Introduce platform module
    Signed-off-by: alvin-celerdata <alvin.zhao@celerdata.com>

--- be/test/platform/download_util_test.cpp
+++ be/test/platform/download_util_test.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "util/download_util.h"
+#include "platform/download_util.h"
 
 #include <gtest/gtest.h>
 
