f14a8a8593195dff8437411a5e70e54a60fa6882
alvin-celerdata <alvin.zhao@celerdata.com>
2026-05-17 17:21:08 -0700

    [Refactor] Move arrow-to-json to Column
    Signed-off-by: alvin-celerdata <alvin.zhao@celerdata.com>

--- be/src/column/arrow/arrow_to_json_converter.cpp
+++ be/src/column/arrow/arrow_to_json_converter.cpp
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "column/arrow/arrow_to_json_converter.h"
+
 #include <velocypack/Builder.h>
 #include <velocypack/Value.h>
 
