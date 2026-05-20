a8cf27f2bc9c111c870001e2aeb21a80c4b1226f
yan zhang <dirtysalt1987@gmail.com>
2026-05-20 19:22:52 +0800

    fix zlib install
    Signed-off-by: yan zhang <dirtysalt1987@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
@@ -2786,7 +2786,7 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
     private boolean paimonForceJNIReader = false;
 
     @VariableMgr.VarAttr(name = AVRO_USE_JNI_READER)
-    private boolean avroUseJNIReader = false;
+    private boolean avroUseJNIReader = true;
 
     @VarAttr(name = ENABLE_QUERY_CACHE)
     private boolean enableQueryCache = false;
