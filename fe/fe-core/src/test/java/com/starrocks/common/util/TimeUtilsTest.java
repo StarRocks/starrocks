StarRocks/starrocks

commit b15e7036a34eac06714dcbd09003b8da4e99c3e9

[Refactor] TimeUtils upgrade to java.time.* (#66360)

fe/fe-core/src/main/java/com/starrocks/common/util/TimeUtils.java
@@ -34,7 +34,6 @@
 
 package com.starrocks.common.util;
 
-import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
 import com.starrocks.common.AnalysisException;
 import com.starrocks.common.DdlException;
@@ -43,22 +42,20 @@ import com.starrocks.common.ErrorReport;
 import com.starrocks.common.FeConstants;
 import com.starrocks.qe.ConnectContext;
 import com.starrocks.server.GlobalStateMgr;
-import com.starrocks.type.PrimitiveType;
-import com.starrocks.type.Type;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.threeten.extra.PeriodDuration;
 
-import java.text.ParseException;
 import java.text.ParsePosition;
-import java.text.SimpleDateFormat;
 import java.time.Clock;
 import java.time.DateTimeException;
+import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.Date;
-import java.util.SimpleTimeZone;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
@@ -70,17 +67,23 @@ public class TimeUtils {
 
     public static final String DEFAULT_TIME_ZONE = "Asia/Shanghai";
 
-    private static final TimeZone TIME_ZONE;
+    private static final ZoneId TIME_ZONE = ZoneOffset.ofTotalSeconds(8 * 3600);
 
     // set CST to +08:00 instead of America/Chicago
     public static final ImmutableMap<String, String> TIME_ZONE_ALIAS_MAP = ImmutableMap.of(
             "CST", DEFAULT_TIME_ZONE, "PRC", DEFAULT_TIME_ZONE);
 
-    // NOTICE: Date formats are not synchronized.
-    // it must be used as synchronized externally.
-    private static final SimpleDateFormat DATE_FORMAT;
-    private static final SimpleDateFormat DATETIME_FORMAT;
-    private static final SimpleDateFormat TIME_FORMAT;
+    //Used to convert a string to a date. Compatible: 2013-2-29
+    private static final DateTimeFormatter STRING_TO_DATE_FORMAT = DateTimeFormatter.ofPattern("y-M-d").withZone(TIME_ZONE);
+    //Used to convert a string to a datetime. Compatible: 2013-2-28 2:3:4
+    private static final DateTimeFormatter STRING_TO_DATETIME_FORMAT =
+            DateTimeFormatter.ofPattern("y-M-d H:m:s").withZone(TIME_ZONE);
+
+    //Used to convert a date to a string.
+    private static final DateTimeFormatter DATE_TO_STRING_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd").withZone(TIME_ZONE);
+    //Used to convert a datetime to a string.
+    private static final DateTimeFormatter DATETIME_TO_STRING_FORMAT =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(TIME_ZONE);
 
     private static final Pattern DATETIME_FORMAT_REG =
             Pattern.compile("^((\\d{2}(([02468][048])|([13579][26]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?"
@@ -90,7 +93,7 @@ public class TimeUtils {
                     + "[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?"
                     + "((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|(1[0-9])|(2[0-8]))))))"
                     + "(\\s(((0?[0-9])|([1][0-9])|([2][0-3]))\\:([0-5]?[0-9])((\\s)|(\\:([0-5]?[0-9])))))?$");
-    
+
     // A regular expressions that will support the pattern like ('Date/DateTime TimeZone').
     // For example ('2024-09-09 11:40:40.123 Asia/Shanghai'), ('2024-09-09 Asia/Shanghai').
     public static final Pattern DATETIME_WITH_TIME_ZONE_PATTERN =
@@ -100,40 +103,15 @@ public class TimeUtils {
 
     private static final Pattern TIMEZONE_OFFSET_FORMAT_REG = Pattern.compile("^[+-]{0,1}\\d{1,2}\\:\\d{2}$");
 
-    public static Date MIN_DATE = null;
-    public static Date MAX_DATE = null;
+    public static final LocalDate MIN_DATE = LocalDate.of(0, 1, 1);
+    public static final LocalDate MAX_DATE = LocalDate.of(9999, 12, 31);
 
-    public static Date MIN_DATETIME = null;
-    public static Date MAX_DATETIME = null;
+    public static final LocalDateTime MIN_DATETIME = LocalDateTime.of(0, 1, 1, 0, 0, 0);
+    public static final LocalDateTime MAX_DATETIME = LocalDateTime.of(9999, 12, 31, 23, 59, 59);
 
     // It's really hard to define max unix timestamp because of timezone.
     // so this value is 253402329599(UTC 9999-12-31 23:59:59) - 24 * 3600(for all timezones)
     public static Long MAX_UNIX_TIMESTAMP = 253402243199L;
-
-    static {
-        TIME_ZONE = new SimpleTimeZone(8 * 3600 * 1000, "");
-
-        DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
-        DATE_FORMAT.setTimeZone(TIME_ZONE);
-
-        DATETIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        DATETIME_FORMAT.setTimeZone(TIME_ZONE);
-
-        TIME_FORMAT = new SimpleDateFormat("HH");
-        TIME_FORMAT.setTimeZone(TIME_ZONE);
-
-        try {
-            MIN_DATE = DATE_FORMAT.parse("0000-01-01");
-            MAX_DATE = DATE_FORMAT.parse("9999-12-31");
-
-            MIN_DATETIME = DATETIME_FORMAT.parse("0000-01-01 00:00:00");
-            MAX_DATETIME = DATETIME_FORMAT.parse("9999-12-31 23:59:59");
-
-        } catch (ParseException e) {
-            LOG.error("invalid date format", e);
-            System.exit(-1);
-        }
-    }
 
     public static long getStartTime() {
         return System.nanoTime();
@@ -143,17 +121,12 @@ public class TimeUtils {
         return System.nanoTime() - startTime;
     }
 
-    public static synchronized String getCurrentFormatTime() {
-        return DATETIME_FORMAT.format(new Date());
+    public static String getCurrentFormatTime() {
+        return DATETIME_TO_STRING_FORMAT.format(LocalDateTime.now());
     }
 
     public static TimeZone getTimeZone() {
-        String timezone;
-        if (ConnectContext.get() != null) {
-            timezone = ConnectContext.get().getSessionVariable().getTimeZone();
-        } else {
-            timezone = GlobalStateMgr.getCurrentState().getVariableMgr().getDefaultSessionVariable().getTimeZone();
-        }
+        String timezone = getSessionTimeZone();
         return TimeZone.getTimeZone(ZoneId.of(timezone, TIME_ZONE_ALIAS_MAP));
     }
 
@@ -186,81 +159,60 @@ public class TimeUtils {
         return time.atZone(getSystemTimeZone().toZoneId()).toInstant().getEpochSecond();
     }
 
-    public static String longToTimeString(long timeStamp, SimpleDateFormat dateFormat) {
+    public static String longToTimeString(long timeStamp, DateTimeFormatter dateFormat) {
         if (timeStamp <= 0L) {
             return FeConstants.NULL_STRING;
         }
-        return dateFormat.format(new Date(timeStamp));
+        return dateFormat.format(Instant.ofEpochMilli(timeStamp));
     }
 
-    public static synchronized String longToTimeString(long timeStamp) {
-        TimeZone timeZone = getTimeZone();
-        SimpleDateFormat dateFormatTimeZone = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        dateFormatTimeZone.setTimeZone(timeZone);
-        return longToTimeString(timeStamp, dateFormatTimeZone);
-    }
-
-    public static synchronized Date getTimeAsDate(String timeString) {
-        try {
-            Date date = TIME_FORMAT.parse(timeString);
-            return date;
-        } catch (ParseException e) {
-            LOG.warn("invalid time format: {}", timeString);
-            return null;
+    public static String longToTimeString(long timeStamp) {
+        if (timeStamp <= 0L) {
+            return FeConstants.NULL_STRING;
         }
+        return longToTimeString(timeStamp, DATETIME_TO_STRING_FORMAT);
     }
 
-    public static synchronized Date parseDate(String dateStr, PrimitiveType type) throws AnalysisException {
-        Date date = null;
+    public static LocalDate parseDate(String dateStr) throws AnalysisException {
+        LocalDate date;
         Matcher matcher = DATETIME_FORMAT_REG.matcher(dateStr);
         if (!matcher.matches()) {
             throw new AnalysisException("Invalid date string: " + dateStr);
         }
-        if (type == PrimitiveType.DATE) {
-            ParsePosition pos = new ParsePosition(0);
-            date = DATE_FORMAT.parse(dateStr, pos);
-            if (pos.getIndex() != dateStr.length() || date == null) {
-                throw new AnalysisException("Invalid date string: " + dateStr);
-            }
-        } else if (type == PrimitiveType.DATETIME) {
-            try {
-                date = DATETIME_FORMAT.parse(dateStr);
-            } catch (ParseException e) {
-                throw new AnalysisException("Invalid date string: " + dateStr);
-            }
-        } else {
-            Preconditions.checkState(false, "error type: " + type);
+        ParsePosition pos = new ParsePosition(0);
+        try {
+            date = STRING_TO_DATE_FORMAT.parse(dateStr, pos).query(LocalDate::from);
+        } catch (RuntimeException e) {
+            throw new AnalysisException("Invalid date string: " + dateStr);
         }
-
+        if (pos.getIndex() != dateStr.length() || date == null) {
+            throw new AnalysisException("Invalid date string: " + dateStr);
+        }
         return date;
     }
 
-    public static synchronized Date parseDate(String dateStr, Type type) throws AnalysisException {
-        return parseDate(dateStr, type.getPrimitiveType());
-    }
-
-    public static synchronized String format(Date date, PrimitiveType type) {
-        if (type == PrimitiveType.DATE) {
-            return DATE_FORMAT.format(date);
-        } else if (type == PrimitiveType.DATETIME) {
-            return DATETIME_FORMAT.format(date);
-        } else {
-            return "INVALID";
+    public static LocalDateTime parseDateTime(String dateTimeStr) throws AnalysisException {
+        LocalDateTime dateTime;
+        Matcher matcher = DATETIME_FORMAT_REG.matcher(dateTimeStr);
+        if (!matcher.matches()) {
+            throw new AnalysisException("Invalid date string: " + dateTimeStr);
         }
-    }
-
-    public static synchronized String format(Date date, Type type) {
-        return format(date, type.getPrimitiveType());
+        try {
+            dateTime = STRING_TO_DATETIME_FORMAT.parse(dateTimeStr).query(LocalDateTime::from);
+        } catch (RuntimeException e) {
+            throw new AnalysisException("Invalid date string: " + dateTimeStr);
+        }
+        return dateTime;
     }
 
     public static long timeStringToLong(String timeStr) {
-        Date d;
+        LocalDateTime dateTime;
         try {
-            d = DATETIME_FORMAT.parse(timeStr);
-        } catch (ParseException e) {
+            dateTime = STRING_TO_DATETIME_FORMAT.parse(timeStr).query(LocalDateTime::from);
+        } catch (RuntimeException e) {
             return -1;
         }
-        return d.getTime();
+        return dateTime.atZone(TIME_ZONE).toInstant().toEpochMilli();
     }
 
     // Check if the time zone_value is valid
@@ -374,7 +326,7 @@ public class TimeUtils {
         }
         return timezone;
     }
-    
+
     /**
      * Parse the time zone of the given timestampLiteral
      * using DATETIME_WITH_TIME_ZONE_PATTERN regular expressions
@@ -390,7 +342,7 @@ public class TimeUtils {
         String timeZone = matcher.group("timezone");
         return timeZone == null ? null : ZoneId.of(timeZone);
     }
-    
+
     /**
      * Parse the date or dateTime string of the given timestampLiteral
      * using DATETIME_WITH_TIME_ZONE_PATTERN regular expressions

fe/fe-core/src/test/java/com/starrocks/common/util/TimeUtilsTest.java
@@ -22,7 +22,6 @@ import com.starrocks.common.DdlException;
 import com.starrocks.common.FeConstants;
 import com.starrocks.sql.ast.expression.DateLiteral;
 import com.starrocks.type.DateType;
-import com.starrocks.type.PrimitiveType;
 import mockit.Expectations;
 import mockit.Mocked;
 import org.junit.jupiter.api.Assertions;
@@ -60,15 +59,18 @@ public class TimeUtilsTest {
         Assertions.assertNotNull(TimeUtils.getCurrentFormatTime());
         Assertions.assertTrue(TimeUtils.getEstimatedTime(0L) > 0);
 
-        Assertions.assertEquals(-62167420800000L, TimeUtils.MIN_DATE.getTime());
-        Assertions.assertEquals(253402185600000L, TimeUtils.MAX_DATE.getTime());
-        Assertions.assertEquals(-62167420800000L, TimeUtils.MIN_DATETIME.getTime());
-        Assertions.assertEquals(253402271999000L, TimeUtils.MAX_DATETIME.getTime());
+        Assertions.assertEquals(-62167248343000L,
+                TimeUtils.MIN_DATE.atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli());
+        Assertions.assertEquals(253402185600000L,
+                TimeUtils.MAX_DATE.atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli());
+        Assertions.assertEquals(-62167248343000L,
+                TimeUtils.MIN_DATETIME.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
+        Assertions.assertEquals(253402271999000L,
+                TimeUtils.MAX_DATETIME.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
     }
 
     @Test
     public void testDateParse() {
-        // date
         List<String> validDateList = new LinkedList<>();
         validDateList.add("2013-12-02");
         validDateList.add("2013-12-02");
@@ -77,10 +79,9 @@ public class TimeUtilsTest {
         validDateList.add("9999-12-31");
         validDateList.add("1900-01-01");
         validDateList.add("2013-2-28");
-        validDateList.add("0000-01-01");
         for (String validDate : validDateList) {
             try {
-                TimeUtils.parseDate(validDate, PrimitiveType.DATE);
+                TimeUtils.parseDate(validDate);
             } catch (AnalysisException e) {
                 e.printStackTrace();
                 System.out.println(validDate);
@@ -98,14 +99,16 @@ public class TimeUtilsTest {
         invalidDateList.add("2013-2-28 2:3:4");
         for (String invalidDate : invalidDateList) {
             try {
-                TimeUtils.parseDate(invalidDate, PrimitiveType.DATE);
+                TimeUtils.parseDate(invalidDate);
                 Assertions.fail();
             } catch (AnalysisException e) {
                 Assertions.assertTrue(e.getMessage().contains("Invalid"));
             }
         }
+    }
 
-        // datetime
+    @Test
+    public void testDateTimeParse() {
         List<String> validDateTimeList = new LinkedList<>();
         validDateTimeList.add("2013-12-02 13:59:59");
         validDateTimeList.add("2013-12-2 13:59:59");
@@ -116,10 +119,9 @@ public class TimeUtilsTest {
         validDateTimeList.add("2013-2-28 23:59:59");
         validDateTimeList.add("2013-2-28 2:3:4");
         validDateTimeList.add("2014-05-07 19:8:50");
-        validDateTimeList.add("0000-01-01 00:00:00");
         for (String validDateTime : validDateTimeList) {
             try {
-                TimeUtils.parseDate(validDateTime, PrimitiveType.DATETIME);
+                TimeUtils.parseDateTime(validDateTime);
             } catch (AnalysisException e) {
                 e.printStackTrace();
                 System.out.println(validDateTime);
@@ -138,7 +140,7 @@ public class TimeUtilsTest {
         invalidDateTimeList.add("2013-13-01 12:12:12");
         for (String invalidDateTime : invalidDateTimeList) {
             try {
-                TimeUtils.parseDate(invalidDateTime, PrimitiveType.DATETIME);
+                TimeUtils.parseDateTime(invalidDateTime);
                 Assertions.fail();
             } catch (AnalysisException e) {
                 Assertions.assertTrue(e.getMessage().contains("Invalid"));
@@ -254,7 +256,7 @@ public class TimeUtilsTest {
             Assertions.fail(e.getMessage());
         }
     }
-    
+
     @Test
     public void testDateTimeWithTimeZonePattern() {
         // Case1: date time string is '2024-09-10 Asia/Shanghai'
@@ -269,7 +271,7 @@ public class TimeUtilsTest {
         Assertions.assertNull(matcher1.group("minute"));
         Assertions.assertNull(matcher1.group("second"));
         Assertions.assertNull(matcher1.group("fraction"));
-    
+
         // Case2: date time string is '2024-09-10 01:01:01.123 Asia/Shanghai'
         String value2 = "2024-09-10 01:01:01.123 Asia/Shanghai";
         Matcher matcher2 = DATETIME_WITH_TIME_ZONE_PATTERN.matcher(value2);
@@ -282,7 +284,7 @@ public class TimeUtilsTest {
         Assertions.assertEquals("01", matcher2.group("second"));
         Assertions.assertEquals("123", matcher2.group("fraction"));
         Assertions.assertEquals("Asia/Shanghai", matcher2.group("timezone"));
-    
+
         // Case3: date time string is ' 2024-09-10'. It will not match
         String value3 = " 2024-09-10";
         Matcher matcher3 = DATETIME_WITH_TIME_ZONE_PATTERN.matcher(value3);

commit e60fbb6c983e45c58ed0687c47a4b322f7a519c7

[BugFix] Restore TimeUtils session-tz, zone-aware formatting, and year-0000 parsing
Address regressions from PR #66360

- longToTimeString(long): rebuild a session-zoned formatter on each call
  via getTimeZone() instead of using the fixed +08:00 formatter. SHOW /
  proc / load / metadata paths now respect the session time_zone again.
- getCurrentFormatTime(): format Instant.now() instead of LocalDateTime
  .now() so that DateTimeFormatter.withZone(TIME_ZONE) actually applies.
  Output is once more pinned to +08:00 on hosts with a non-Shanghai JVM
  default zone.
- STRING_TO_DATE_FORMAT / STRING_TO_DATETIME_FORMAT: switch from year-of
  -era ("y") to proleptic year ("u") so that "0000-01-01" and
  "0000-01-01 00:00:00" parse instead of throwing AnalysisException /
  returning -1.
- Remove the unused DATE_TO_STRING_FORMAT private constant.
- Correct the misleading "Compatible: 2013-2-29" comment (2013 is not a
  leap year and the parser rejects it).
- Re-add the "0000-01-01" / "0000-01-01 00:00:00" valid-input cases
  that #66360 deleted to mask the year-of-era regression.

Signed-off-by: Kevin Cai <kevin.cai@phoenixdata.ai>

fe/fe-core/src/main/java/com/starrocks/common/util/TimeUtils.java
@@ -74,14 +74,12 @@ public class TimeUtils {
     public static final ImmutableMap<String, String> TIME_ZONE_ALIAS_MAP = ImmutableMap.of(
             "CST", DEFAULT_TIME_ZONE, "PRC", DEFAULT_TIME_ZONE);
 
-    //Used to convert a string to a date. Compatible: 2013-2-29
-    private static final DateTimeFormatter STRING_TO_DATE_FORMAT = DateTimeFormatter.ofPattern("y-M-d").withZone(TIME_ZONE);
-    //Used to convert a string to a datetime. Compatible: 2013-2-28 2:3:4
+    // Proleptic year ("u") is required so that year 0000 can be parsed.
+    // Single-letter components allow non-padded inputs, e.g. "2013-2-2".
+    private static final DateTimeFormatter STRING_TO_DATE_FORMAT = DateTimeFormatter.ofPattern("u-M-d").withZone(TIME_ZONE);
     private static final DateTimeFormatter STRING_TO_DATETIME_FORMAT =
-            DateTimeFormatter.ofPattern("y-M-d H:m:s").withZone(TIME_ZONE);
+            DateTimeFormatter.ofPattern("u-M-d H:m:s").withZone(TIME_ZONE);
 
-    //Used to convert a date to a string.
-    private static final DateTimeFormatter DATE_TO_STRING_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd").withZone(TIME_ZONE);
     //Used to convert a datetime to a string.
     private static final DateTimeFormatter DATETIME_TO_STRING_FORMAT =
             DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(TIME_ZONE);
@@ -122,8 +120,17 @@ public class TimeUtils {
         return System.nanoTime() - startTime;
     }
 
+    /**
+     * Returns the current wall-clock time formatted as "yyyy-MM-dd HH:mm:ss" in the
+     * cluster-wide storage zone (fixed +08:00), NOT the JVM default zone and NOT the
+     * caller's session timezone. Matches the pre-#66360 contract relied on by metadata
+     * and scheduler timestamps that must stay stable across FE hosts.
+     *
+     * <p>Note: the input must be an {@link Instant} - {@link DateTimeFormatter#withZone}
+     * is a no-op when formatting a {@link LocalDateTime}, which was the #66360 regression.
+     */
     public static String getCurrentFormatTime() {
-        return DATETIME_TO_STRING_FORMAT.format(LocalDateTime.now());
+        return DATETIME_TO_STRING_FORMAT.format(Instant.now());
     }
 
     public static TimeZone getTimeZone() {
@@ -167,11 +174,28 @@ public class TimeUtils {
         return dateFormat.format(Instant.ofEpochMilli(timeStamp));
     }
 
+    /**
+     * Formats an epoch-millisecond timestamp as "yyyy-MM-dd HH:mm:ss" in the caller's
+     * SESSION timezone (resolved via {@link #getTimeZone()}). Returns
+     * {@link FeConstants#NULL_STRING} for non-positive timestamps.
+     *
+     * <p>This is the user-facing default used by SHOW / proc / load / metadata code
+     * paths; the output is intentionally session-zone dependent so that two clients
+     * connected with different {@code time_zone} session variables see their own
+     * local wall-clock. Switching this to a fixed zone is what regressed in #66360;
+     * keep it session-zoned. Callers that need a fixed zone should build their own
+     * formatter and use {@link #longToTimeString(long, DateTimeFormatter)} instead.
+     */
     public static String longToTimeString(long timeStamp) {
+        // Fast-path sentinel timestamps (unset / negative) before touching the session
+        // timezone - those code paths are hot in SHOW / load metadata and resolving the
+        // session zone can fail when the thread has no ConnectContext.
         if (timeStamp <= 0L) {
             return FeConstants.NULL_STRING;
         }
-        return longToTimeString(timeStamp, DATETIME_TO_STRING_FORMAT);
+        // Reuse the pre-parsed pattern (DATETIME_TO_STRING_FORMAT) and only rebind the
+        // zone per call - .withZone(...) returns a thin wrapper without re-parsing.
+        return longToTimeString(timeStamp, DATETIME_TO_STRING_FORMAT.withZone(getTimeZone().toZoneId()));
     }
 
     /**
@@ -341,14 +365,20 @@ public class TimeUtils {
         }
     }
 
+    /**
+     * Returns the effective session timezone string:
+     *   1. the per-request value from the current {@link ConnectContext}, or
+     *   2. the cluster-wide default from GlobalStateMgr's VariableMgr, or
+     *   3. {@link #DEFAULT_TIME_ZONE} ("Asia/Shanghai") as a last-resort fallback.
+     */
     public static String getSessionTimeZone() {
-        String timezone;
+        String timezone = null;
         if (ConnectContext.get() != null) {
             timezone = ConnectContext.get().getSessionVariable().getTimeZone();
         } else {
             timezone = GlobalStateMgr.getCurrentState().getVariableMgr().getDefaultSessionVariable().getTimeZone();
         }
-        return timezone;
+        return timezone != null ? timezone : DEFAULT_TIME_ZONE;
     }
 
     /**

fe/fe-core/src/test/java/com/starrocks/common/util/TimeUtilsTest.java
@@ -22,13 +22,16 @@ import com.starrocks.common.DdlException;
 import com.starrocks.common.FeConstants;
 import com.starrocks.sql.ast.expression.DateLiteral;
 import com.starrocks.type.DateType;
-import mockit.Expectations;
-import mockit.Mocked;
+import mockit.Mock;
+import mockit.MockUp;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.TimeZone;
@@ -39,34 +42,15 @@ import static com.starrocks.common.util.TimeUtils.DATETIME_WITH_TIME_ZONE_PATTER
 
 public class TimeUtilsTest {
 
-    @Mocked
-    TimeUtils timeUtils;
-
-    @BeforeEach
-    public void setUp() {
-        TimeZone tz = TimeZone.getTimeZone(ZoneId.of("Asia/Shanghai"));
-        new Expectations(timeUtils) {
-            {
-                TimeUtils.getTimeZone();
-                minTimes = 0;
-                result = tz;
-            }
-        };
-    }
-
     @Test
     public void testNormal() {
         Assertions.assertNotNull(TimeUtils.getCurrentFormatTime());
         Assertions.assertTrue(TimeUtils.getEstimatedTime(0L) > 0);
 
-        Assertions.assertEquals(-62167248343000L,
-                TimeUtils.MIN_DATE.atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli());
-        Assertions.assertEquals(253402185600000L,
-                TimeUtils.MAX_DATE.atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli());
-        Assertions.assertEquals(-62167248343000L,
-                TimeUtils.MIN_DATETIME.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
-        Assertions.assertEquals(253402271999000L,
-                TimeUtils.MAX_DATETIME.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
+        Assertions.assertEquals(LocalDate.of(0, 1, 1), TimeUtils.MIN_DATE);
+        Assertions.assertEquals(LocalDate.of(9999, 12, 31), TimeUtils.MAX_DATE);
+        Assertions.assertEquals(LocalDateTime.of(0, 1, 1, 0, 0, 0), TimeUtils.MIN_DATETIME);
+        Assertions.assertEquals(LocalDateTime.of(9999, 12, 31, 23, 59, 59), TimeUtils.MAX_DATETIME);
     }
 
     @Test
@@ -79,6 +63,7 @@ public class TimeUtilsTest {
         validDateList.add("9999-12-31");
         validDateList.add("1900-01-01");
         validDateList.add("2013-2-28");
+        validDateList.add("0000-01-01");
         for (String validDate : validDateList) {
             try {
                 TimeUtils.parseDate(validDate);
@@ -119,6 +104,7 @@ public class TimeUtilsTest {
         validDateTimeList.add("2013-2-28 23:59:59");
         validDateTimeList.add("2013-2-28 2:3:4");
         validDateTimeList.add("2014-05-07 19:8:50");
+        validDateTimeList.add("0000-01-01 00:00:00");
         for (String validDateTime : validDateTimeList) {
             try {
                 TimeUtils.parseDateTime(validDateTime);
@@ -150,6 +136,13 @@ public class TimeUtilsTest {
 
     @Test
     public void testDateTrans() throws AnalysisException {
+        new MockUp<TimeUtils>() {
+            @Mock
+            public TimeZone getTimeZone() {
+                return TimeZone.getTimeZone(ZoneId.of("Asia/Shanghai"));
+            }
+        };
+
         Assertions.assertEquals(FeConstants.NULL_STRING, TimeUtils.longToTimeString(-2));
 
         long timestamp = 1426125600000L;
@@ -289,5 +282,67 @@ public class TimeUtilsTest {
         String value3 = " 2024-09-10";
         Matcher matcher3 = DATETIME_WITH_TIME_ZONE_PATTERN.matcher(value3);
         Assertions.assertFalse(matcher3.matches());
+    }
+
+    // Regressions introduced by the java.time refactor in PR #66360.
+    // These tests fail on the unfixed code and pass after the fix.
+
+    @Test
+    public void testLongToTimeStringHonorsSessionTimeZone() {
+        new MockUp<TimeUtils>() {
+            @Mock
+            public TimeZone getTimeZone() {
+                return TimeZone.getTimeZone(ZoneOffset.UTC);
+            }
+        };
+        // 1426125600000L = 2015-03-12 10:00:00 +08:00 = 2015-03-12 02:00:00 UTC
+        long timestamp = 1426125600000L;
+        Assertions.assertEquals("2015-03-12 02:00:00", TimeUtils.longToTimeString(timestamp));
+    }
+
+    @Test
+    public void testGetCurrentFormatTimeUsesFixedShanghaiTimeZone() {
+        // Pin Instant.now() so the assertion is deterministic; do not rely on wall-clock
+        // tolerance windows. Setting the JVM default to UTC simultaneously guards
+        // against a regression to LocalDateTime.now() (which would silently follow the
+        // JVM default zone instead of the fixed storage zone).
+        Instant fixed = Instant.parse("2024-06-15T07:30:45Z");
+        new MockUp<Instant>() {
+            @Mock
+            public Instant now() {
+                return fixed;
+            }
+        };
+        TimeZone original = TimeZone.getDefault();
+        try {
+            TimeZone.setDefault(TimeZone.getTimeZone(ZoneOffset.UTC));
+            // 2024-06-15 07:30:45 UTC = 2024-06-15 15:30:45 +08:00.
+            Assertions.assertEquals("2024-06-15 15:30:45", TimeUtils.getCurrentFormatTime());
+        } finally {
+            TimeZone.setDefault(original);
+        }
+    }
+
+    @Test
+    public void testParseSupportsYearZero() {
+        Assertions.assertEquals(LocalDate.of(0, 1, 1),
+                Assertions.assertDoesNotThrow(() -> TimeUtils.parseDate("0000-01-01")));
+        Assertions.assertEquals(LocalDateTime.of(0, 1, 1, 0, 0, 0),
+                Assertions.assertDoesNotThrow(() -> TimeUtils.parseDateTime("0000-01-01 00:00:00")));
+        Assertions.assertNotEquals(-1L, TimeUtils.timeStringToLong("0000-01-01 00:00:00"));
+    }
+
+    @Test
+    public void testMinMaxConstantsAreTimezoneIndependent() {
+        TimeZone original = TimeZone.getDefault();
+        try {
+            TimeZone.setDefault(TimeZone.getTimeZone(ZoneOffset.UTC));
+            Assertions.assertEquals(LocalDate.of(0, 1, 1), TimeUtils.MIN_DATE);
+            Assertions.assertEquals(LocalDate.of(9999, 12, 31), TimeUtils.MAX_DATE);
+            Assertions.assertEquals(LocalDateTime.of(0, 1, 1, 0, 0, 0), TimeUtils.MIN_DATETIME);
+            Assertions.assertEquals(LocalDateTime.of(9999, 12, 31, 23, 59, 59), TimeUtils.MAX_DATETIME);
+        } finally {
+            TimeZone.setDefault(original);
+        }
     }
 }