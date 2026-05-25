e2a4cc415ef165a41f10b03f29651490d1a84e54
alvin <115669851+alvin-phoenix-ai@users.noreply.github.com>
2026-05-25 13:40:57 -0700

    [Refactor] Move pipeline timer lifecycle into ComputeEnv (#73838)
    Signed-off-by: alvin-phoenix-ai <alvin.zhao@phoenixdata.ai>

--- be/src/compute_env/pipeline/driver_limiter.cpp
+++ be/src/compute_env/pipeline/driver_limiter.cpp
@@ -12,7 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "exec/pipeline/driver_limiter.h"
+#include "compute_env/pipeline/driver_limiter.h"
+
+#include "gutil/strings/substitute.h"
 
 namespace starrocks::pipeline {
 
