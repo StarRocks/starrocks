e7f12a400ff2ac320f099f77868d781502459d30
trueeyu <lxhhust350@qq.com>
2026-05-26 16:07:22 +0800

    [BugFix] Avoid set_thread_name race on data dir load threads (#73862)
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/src/common/thread/thread.cpp
+++ be/src/common/thread/thread.cpp
@@ -282,44 +282,24 @@ int64_t Thread::current_thread_id() {
 
 void Thread::set_thread_name(pthread_t t, const std::string& name) {
     // pthread_setname_np's length is restricted to 16 bytes, including the terminating null byte ('\0')
-    int ret;
-    if (name.length() < 16) {
+    std::string truncated = name.length() >= 16 ? name.substr(0, 15) : name;
 #ifdef __APPLE__
-        (void)t;
-        ret = pthread_setname_np(name.data());
+    (void)t;
+    int ret = pthread_setname_np(truncated.c_str());
 #else
-        ret = pthread_setname_np(t, name.data());
+    int ret = pthread_setname_np(t, truncated.c_str());
 #endif
-    } else {
-        std::string str = name;
-        str.at(15) = '\0';
-#ifdef __APPLE__
-        (void)t;
-        ret = pthread_setname_np(str.data());
-#else
-        ret = pthread_setname_np(t, str.data());
-#endif
-    }
     if (ret) {
-        LOG(WARNING) << "failed to set thread name: " << name;
+        LOG(WARNING) << "failed to set thread name, ret=" << ret << ", name: " << truncated;
     }
 }
 
 void Thread::set_thread_name(std::thread& t, std::string name) {
-    // pthread_setname_np's length is restricted to 16 bytes, including the terminating null byte ('\0')
-    if (name.length() >= 16) {
-        name.at(15) = '\0';
-    }
-    int ret;
 #ifdef __APPLE__
-    (void)t;
-    ret = pthread_setname_np(name.data());
+    set_thread_name(pthread_t{}, name);
 #else
-    ret = pthread_setname_np(t.native_handle(), name.data());
+    set_thread_name(t.native_handle(), name);
 #endif
-    if (ret) {
-        LOG(WARNING) << "failed to set thread name: " << name;
-    }
 }
 
 int64_t Thread::wait_for_tid() const {
