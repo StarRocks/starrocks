844dc1e28862fcec1d8213742a820056003caeaf
satanson <ranpanf@gmail.com>
2026-05-28 17:38:20 +0800

    [BugFix] Fix memory-leak introduced by udaf cache
    Signed-off-by: satanson <ranpanf@gmail.com>

--- be/src/exec/aggregator.cpp
+++ be/src/exec/aggregator.cpp
@@ -283,8 +283,6 @@ Status Aggregator::open(RuntimeState* state) {
         auto& opts = state->query_options();
         bool enable_cache = opts.__isset.enable_cache_udaf && opts.enable_cache_udaf;
         auto promise_st = call_function_in_pthread(state, [this, enable_cache]() {
-            std::vector<int> attached_udaf_idx;
-            attached_udaf_idx.reserve(_agg_fn_ctxs.size());
             for (int i = 0; i < _agg_fn_ctxs.size(); ++i) {
                 if (_fns[i].binary_type == TFunctionBinaryType::SRJAR) {
                     const auto& fn = _fns[i];
@@ -305,13 +303,7 @@ Status Aggregator::open(RuntimeState* state) {
                             COUNTER_UPDATE(_agg_stat->udaf_cache_populate_count, 1);
                         }
                     }
-                    if (!st.ok()) {
-                        for (int idx : attached_udaf_idx) {
-                            destroy_java_udaf_context(_agg_fn_ctxs[idx]);
-                        }
-                        return st;
-                    }
-                    attached_udaf_idx.emplace_back(i);
+                    RETURN_IF_ERROR(st);
                 }
             }
             return Status::OK();
@@ -680,7 +672,7 @@ Status Aggregator::_reset_state(RuntimeState* state, bool reset_sink_complete) {
 #ifndef __APPLE__
     for (int i = 0; i < _agg_functions.size(); i++) {
         if (_agg_fn_ctxs[i] != nullptr && _fns[i].binary_type == TFunctionBinaryType::SRJAR) {
-            clear_java_udaf_states(_agg_fn_ctxs[i]);
+            _agg_fn_ctxs[i]->release_mems();
         }
     }
 #endif
@@ -760,7 +752,7 @@ void Aggregator::close(RuntimeState* state) {
 #ifndef __APPLE__
         for (int i = 0; i < _agg_functions.size(); i++) {
             if (_agg_fn_ctxs[i] != nullptr && _fns[i].binary_type == TFunctionBinaryType::SRJAR) {
-                destroy_java_udaf_context(_agg_fn_ctxs[i]);
+                _agg_fn_ctxs[i]->release_mems();
             }
         }
 #endif
