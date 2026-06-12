d5fa8c065a3b8af11e025849b64845a53381e0af
Kevin Cai <kevin.cai@phoenixdata.ai>
2026-06-12 10:23:24 -0700

    [BugFix] incremental redeation of io.netty (#57100)
    Signed-off-by: Kevin Cai <kevin.cai@phoenixdata.ai>

--- fix-cve/fix-cve.py
+++ fix-cve/fix-cve.py
@@ -6,7 +6,7 @@ import os
 import shutil
 import subprocess
 
-io_netty_clean_version = "4.1.133.Final"
+io_netty_clean_version = "4.1.135.Final"
 
 fixed_poms = [
     ("com.fasterxml.jackson.core:jackson-core", "2.21.1"),
@@ -17,9 +17,12 @@ fixed_poms = [
     ("io.netty:netty-codec-http2", io_netty_clean_version),
     ("io.netty:netty-codec-http", io_netty_clean_version),
     ("io.netty:netty-codec-haproxy", io_netty_clean_version),
+    ("io.netty:netty-codec-redis", io_netty_clean_version),
     ("io.netty:netty-codec-smtp", io_netty_clean_version),
     ("io.netty:netty-common", io_netty_clean_version),
+    ("io.netty:netty-resolver-dns", io_netty_clean_version),
     ("io.netty:netty-transport-native-epoll", io_netty_clean_version),
+    ("io.netty:netty-transport-sctp", io_netty_clean_version),
     ("io.netty:netty-all", io_netty_clean_version),
     ("com.google.protobuf:protobuf-java", "3.25.5"),
     ("org.apache.avro:avro", "1.11.4"),
