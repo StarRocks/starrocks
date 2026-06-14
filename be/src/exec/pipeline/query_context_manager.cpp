31397a3299deaeee2114e570d124d5d7c7b3d568
alvin-phoenix-ai <alvin.zhao@phoenixdata.ai>
2026-06-14 16:55:55 -0700

    [Refactor] Split pipeline fragment reporting from QueryContextManager
    Signed-off-by: alvin-phoenix-ai <alvin.zhao@phoenixdata.ai>

--- be/src/exec/pipeline/query_context_manager.cpp
+++ be/src/exec/pipeline/query_context_manager.cpp
@@ -20,17 +20,8 @@
 #include <vector>
 
 #include "base/hash/hash.h"
-#include "common/config_rpc_client_fwd.h"
-#include "common/system/master_info.h"
 #include "common/thread/thread.h"
-#include "common/util/thrift_client_cache.h"
-#include "exec/pipeline/fragment_context.h"
-#include "exec/pipeline/fragment_context_cancel.h"
-#include "exec/pipeline/fragment_context_manager.h"
 #include "exec/pipeline/query_context.h"
-#include "platform/thrift_rpc_helper.h"
-#include "runtime/runtime_state.h"
-#include "runtime/runtime_state_helper.h"
 
 namespace starrocks::pipeline {
 
@@ -282,59 +273,6 @@ void QueryContextManager::clear() {
     _context_maps.clear();
 }
 
-void QueryContextManager::report_fragments_with_same_host(
-        const std::vector<std::shared_ptr<FragmentContext>>& need_report_fragment_context, std::vector<bool>& reported,
-        const TNetworkAddress& last_coord_addr, std::vector<TReportExecStatusParams>& report_exec_status_params_vector,
-        std::vector<int32_t>& cur_batch_report_indexes, std::vector<PipeLineReportTaskKey>& tasks_to_unregister) {
-    for (int i = 0; i < need_report_fragment_context.size(); i++) {
-        if (reported[i] == false) {
-            FragmentContext* fragment_ctx = need_report_fragment_context[i].get();
-
-            if (fragment_ctx->all_execution_groups_finished()) {
-                reported[i] = true;
-                continue;
-            }
-
-            Status fragment_ctx_status = fragment_ctx->final_status();
-            if (!fragment_ctx_status.ok()) {
-                reported[i] = true;
-                tasks_to_unregister.emplace_back(fragment_ctx->query_id(), fragment_ctx->fragment_instance_id());
-                continue;
-            }
-
-            auto fe_addr = fragment_ctx->fe_addr();
-            auto fragment_id = fragment_ctx->fragment_instance_id();
-            auto* runtime_state = fragment_ctx->runtime_state();
-            DCHECK(runtime_state != nullptr);
-
-            if (fe_addr == last_coord_addr) {
-                TReportExecStatusParams params;
-
-                params.protocol_version = FrontendServiceVersion::V1;
-                params.__set_query_id(fragment_ctx->query_id());
-                params.__set_backend_num(runtime_state->be_number());
-                params.__set_fragment_instance_id(fragment_id);
-                fragment_ctx_status.set_t_status(&params);
-                params.__set_done(false);
-
-                if (runtime_state->query_options().query_type == TQueryType::LOAD) {
-                    RuntimeStateHelper::update_report_load_status(runtime_state, &params);
-                    params.__set_load_type(runtime_state->query_options().load_job_type);
-                }
-
-                auto backend_id = get_backend_id();
-                if (backend_id.has_value()) {
-                    params.__set_backend_id(backend_id.value());
-                }
-
-                report_exec_status_params_vector.emplace_back(std::move(params));
-                cur_batch_report_indexes.push_back(i);
-                reported[i] = true;
-            }
-        }
-    }
-}
-
 void QueryContextManager::collect_query_statistics(const PCollectQueryStatisticsRequest* request,
                                                    PCollectQueryStatisticsResult* response) {
     for (int i = 0; i < request->query_ids_size(); i++) {
@@ -359,115 +297,6 @@ void QueryContextManager::collect_query_statistics(const PCollectQueryStatistics
             query_statistics->set_spill_bytes(query_ctx->get_spill_bytes());
         }
     }
-}
-
-std::vector<PipeLineReportTaskKey> QueryContextManager::report_fragments(
-        const std::vector<PipeLineReportTaskKey>& pipeline_need_report_query_fragment_ids) {
-    std::vector<std::shared_ptr<QueryContext>> need_report_query_ctx;
-    std::vector<std::shared_ptr<FragmentContext>> need_report_fragment_context;
-
-    std::vector<PipeLineReportTaskKey> tasks_to_unregister;
-
-    for (const auto& key : pipeline_need_report_query_fragment_ids) {
-        TUniqueId query_id = key.query_id;
-        TUniqueId fragment_instance_id = key.fragment_instance_id;
-        auto query_ctx = get(query_id);
-        if (!query_ctx) {
-            tasks_to_unregister.push_back(key);
-            continue;
-        }
-        need_report_query_ctx.push_back(query_ctx);
-        auto fragment_ctx = query_ctx->fragment_mgr()->get(fragment_instance_id);
-        if (!fragment_ctx) {
-            tasks_to_unregister.push_back(key);
-            continue;
-        }
-        need_report_fragment_context.push_back(fragment_ctx);
-    }
-
-    std::vector<bool> reported(need_report_fragment_context.size(), false);
-    for (int i = 0; i < need_report_fragment_context.size(); i++) {
-        if (reported[i] == false) {
-            reported[i] = true;
-
-            FragmentContext* fragment_ctx = need_report_fragment_context[i].get();
-
-            if (fragment_ctx->all_execution_groups_finished()) {
-                continue;
-            }
-
-            Status fragment_ctx_status = fragment_ctx->final_status();
-            if (!fragment_ctx_status.ok()) {
-                tasks_to_unregister.emplace_back(fragment_ctx->query_id(), fragment_ctx->fragment_instance_id());
-                continue;
-            }
-
-            auto fe_addr = fragment_ctx->fe_addr();
-            auto fragment_id = fragment_ctx->fragment_instance_id();
-            auto* runtime_state = fragment_ctx->runtime_state();
-            DCHECK(runtime_state != nullptr);
-
-            std::vector<TReportExecStatusParams> report_exec_status_params_vector;
-
-            TReportExecStatusParams params;
-
-            params.protocol_version = FrontendServiceVersion::V1;
-            params.__set_query_id(fragment_ctx->query_id());
-            params.__set_backend_num(runtime_state->be_number());
-            params.__set_fragment_instance_id(fragment_id);
-            fragment_ctx_status.set_t_status(&params);
-            params.__set_done(false);
-
-            if (runtime_state->query_options().query_type == TQueryType::LOAD) {
-                RuntimeStateHelper::update_report_load_status(runtime_state, &params);
-                params.__set_load_type(runtime_state->query_options().load_job_type);
-            }
-
-            auto backend_id = get_backend_id();
-            if (backend_id.has_value()) {
-                params.__set_backend_id(backend_id.value());
-            }
-
-            report_exec_status_params_vector.push_back(params);
-
-            std::vector<int32_t> cur_batch_report_indexes;
-            cur_batch_report_indexes.push_back(i);
-
-            report_fragments_with_same_host(need_report_fragment_context, reported, fe_addr,
-                                            report_exec_status_params_vector, cur_batch_report_indexes,
-                                            tasks_to_unregister);
-
-            TBatchReportExecStatusParams report_batch;
-            report_batch.__set_params_list(report_exec_status_params_vector);
-
-            TBatchReportExecStatusResult res;
-            Status rpc_status;
-
-            VLOG_ROW << "debug: reportExecStatus params is " << apache::thrift::ThriftDebugString(params).c_str();
-            rpc_status = ThriftRpcHelper::rpc<FrontendServiceClient>(
-                    fe_addr,
-                    [&res, &report_batch](FrontendServiceConnection& client) {
-                        client->batchReportExecStatus(res, report_batch);
-                    },
-                    config::thrift_rpc_timeout_ms);
-            if (!rpc_status.ok()) {
-                LOG(WARNING) << "thrift rpc error:" << rpc_status;
-                continue;
-            }
-
-            const std::vector<TStatus>& status_list = res.status_list;
-            for (int j = 0; j < status_list.size(); j++) {
-                Status rpc_status = Status(status_list[j]);
-                if (!rpc_status.ok()) {
-                    int32_t index = cur_batch_report_indexes[j];
-                    FragmentContext* fragment_ctx = need_report_fragment_context[index].get();
-                    cancel_fragment_context(fragment_ctx, rpc_status);
-                }
-            }
-        }
-    }
-
-    return tasks_to_unregister;
 }
 
 void QueryContextManager::for_each_active_ctx(const std::function<void(QueryContextPtr)>& func) {
