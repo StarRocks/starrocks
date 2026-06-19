c640a27e7a42be39f67fea2b8da2127f7f48cf79
wanpengfei-git <wanpengfei91@163.com>
2026-06-19 02:12:58 +0000

    style: auto-fix clang-format issues
    Signed-off-by: seokyun.ha <seokyun.ha@toss.im>

--- be/src/exec/runtime/fragment_context.cpp
+++ be/src/exec/runtime/fragment_context.cpp
@@ -304,7 +304,8 @@ void FragmentContext::set_final_status(const Status& status) {
                             : execution_services(_runtime_state.get()).workgroup_manager->shared_executors();
             auto* executor = executors->driver_executor();
             if (executor == nullptr) {
-                LOG(WARNING) << "[Driver] driver_executor is null, skip cancel drivers, query_id=" << print_id(_query_id);
+                LOG(WARNING) << "[Driver] driver_executor is null, skip cancel drivers, query_id="
+                             << print_id(_query_id);
             } else {
                 iterate_drivers([executor](const DriverPtr& driver) { executor->cancel(driver.get()); });
             }
