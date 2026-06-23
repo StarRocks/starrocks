c1ae46878bae6ac3b069ab4c97c3a2d65905dfac
stdpain <34912776+stdpain@users.noreply.github.com>
2026-06-23 15:47:30 +0800

    [BugFix] Fix spill partition sort sink cancel crash (#75140)
    (cherry picked from commit f2941db0573bbd127ec51c41032c39b45a098818)

--- be/src/exec/spillable_chunks_sorter.cpp
+++ be/src/exec/spillable_chunks_sorter.cpp
@@ -96,15 +96,7 @@ Status SpillableChunksSorter<TChunksSorter>::do_done(RuntimeState* state) {
 template <DerivedFromChunksSorter TChunksSorter>
 void SpillableChunksSorter<TChunksSorter>::cancel() {
     if (_spiller->spilled()) {
-        if (_spill_channel->has_task()) {
-            std::function<StatusOr<ChunkPtr>()> cancel_task = [this]() -> StatusOr<ChunkPtr> {
-                _spiller->cancel();
-                return Status::EndOfFile("eos");
-            };
-            _spill_channel->add_spill_task(std::move(cancel_task));
-        } else {
-            _spiller->cancel();
-        }
+        _spiller->cancel();
     }
 }
 
