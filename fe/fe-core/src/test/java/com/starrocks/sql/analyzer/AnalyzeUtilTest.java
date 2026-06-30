47b2e446086c9ff9965c515753b27bba536a0886
Harbor Liu <460660596@qq.com>
2026-06-30 18:10:43 +0800

    Fix 4.1 CTE backport test compile
    Signed-off-by: Harbor Liu <460660596@qq.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzeUtilTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzeUtilTest.java
@@ -41,6 +41,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
