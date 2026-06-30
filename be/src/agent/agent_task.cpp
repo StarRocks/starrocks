eb9be13b4a8f9ca47fc84df84cb89303e6720904
alvin-phoenix-ai <alvin.zhao@phoenixdata.ai>
2026-06-30 14:20:35 -0700

    Fix UT
    Signed-off-by: alvin-phoenix-ai <alvin.zhao@phoenixdata.ai>

--- be/src/agent/agent_task.cpp
+++ be/src/agent/agent_task.cpp
@@ -34,11 +34,8 @@
 #include "data_workflows/migration/engine_storage_migration_task.h"
 #include "data_workflows/schema_change/engine_alter_tablet_task.h"
 #include "data_workflows/snapshot/snapshot_loader.h"
-<<<<<<< HEAD
-#include "fs/fs.h"
-=======
 #include "exec/exec_env.h"
->>>>>>> ba8f293f468... [Refactor] Move runtime compatibility sources into Exec (#75565)
+#include "fs/fs.h"
 #include "gutil/strings/join.h"
 #include "io/io_profiler.h"
 #include "runtime/current_thread.h"
