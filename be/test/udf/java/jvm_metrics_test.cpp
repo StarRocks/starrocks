cdb06a69d5c33b32f6592e9f316ebcaf2f83ef1a
alvin <115669851+alvin-phoenix-ai@users.noreply.github.com>
2026-07-01 14:38:01 -0700

    [Refactor] Move JVM metrics into Udf (#75678)
    Signed-off-by: alvin-phoenix-ai <alvin.zhao@phoenixdata.ai>

--- be/test/udf/java/jvm_metrics_test.cpp
+++ be/test/udf/java/jvm_metrics_test.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "util/jvm_metrics.h"
+#include "udf/java/jvm_metrics.h"
 
 #include <gtest/gtest.h>
 
