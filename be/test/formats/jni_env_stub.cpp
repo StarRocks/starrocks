3a72a10098a13384c386efc7b4def7fb191d54c8
trueeyu <lxhhust350@qq.com>
2026-07-07 19:52:01 +0800

    [UT]  Add shared weak getJNIEnv stub for test binaries linking Runtime without libhdfs

--- be/test/formats/jni_env_stub.cpp
+++
@@ -1,7 +0,0 @@
-// Focused tests that do not exercise JVM integration can still pull Runtime's
-// Java helper objects into the final binary through static library links.
-#include <jni.h>
-
-extern "C" __attribute__((weak)) JNIEnv* getJNIEnv(void) {
-    return nullptr;
-}
