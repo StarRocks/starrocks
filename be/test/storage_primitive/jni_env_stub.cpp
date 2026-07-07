23a4a4b606fddb052f3906d6d3679f22365d050a
trueeyu <lxhhust350@qq.com>
2026-07-07 19:52:01 +0800

    [UT]  Add shared weak getJNIEnv stub for test binaries linking Runtime without libhdfs (#75955)
    Signed-off-by: trueeyu <lxhhust350@qq.com>

--- be/test/storage_primitive/jni_env_stub.cpp
+++
@@ -1,7 +0,0 @@
-// The focused storage primitive tests do not exercise JVM integration, but
-// Runtime can pull Java helper objects into this test binary.
-#include <jni.h>
-
-extern "C" __attribute__((weak)) JNIEnv* getJNIEnv(void) {
-    return nullptr;
-}
