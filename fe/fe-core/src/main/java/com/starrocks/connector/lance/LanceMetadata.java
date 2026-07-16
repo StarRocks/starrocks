020c7f4f05701fe8daaf2f134f339df6a04e2773
tianfy17 <52420476+tianfy17@users.noreply.github.com>
2026-07-16 22:35:43 +0800

    [CP a0f3bedb586] [Feature] Support native Lance JSON columns

--- fe/fe-core/src/main/java/com/starrocks/connector/lance/LanceMetadata.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/lance/LanceMetadata.java
@@ -24,24 +24,8 @@ import com.starrocks.connector.HdfsEnvironment;
 import com.starrocks.connector.exception.StarRocksConnectorException;
 import com.starrocks.planner.lance.LanceConfig;
 import com.starrocks.qe.ConnectContext;
-import com.starrocks.type.ArrayType;
-import com.starrocks.type.BooleanType;
-import com.starrocks.type.DateType;
-import com.starrocks.type.FloatType;
-import com.starrocks.type.IntegerType;
-import com.starrocks.type.MapType;
-import com.starrocks.type.StructField;
-import com.starrocks.type.StructType;
-import com.starrocks.type.Type;
-import com.starrocks.type.TypeFactory;
-import com.starrocks.type.VarbinaryType;
-import com.starrocks.type.VarcharType;
 import org.apache.arrow.memory.BufferAllocator;
 import org.apache.arrow.memory.RootAllocator;
-import org.apache.arrow.vector.types.FloatingPointPrecision;
-import org.apache.arrow.vector.types.pojo.ArrowType;
-import org.apache.arrow.vector.types.pojo.Field;
-import org.apache.arrow.vector.types.pojo.Schema;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
@@ -191,82 +175,11 @@ public class LanceMetadata implements ConnectorMetadata {
 
         try (BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
                 Dataset dataset = Dataset.open(allocator, datasetUri, builder.build())) {
-            Schema arrowSchema = dataset.getSchema();
-            List<Column> columns = new ArrayList<>(arrowSchema.getFields().size());
-            for (Field field : arrowSchema.getFields()) {
-                columns.add(new Column(field.getName(), fromArrowField(field), true));
-            }
-            return columns;
+            return LanceSchemaConverter.fromArrowSchema(dataset.getSchema());
         } catch (Exception e) {
             throw new StarRocksConnectorException("Failed to open lance dataset %s: %s", datasetUri,
                     ExceptionUtils.getRootCauseMessage(e));
         }
-    }
-
-    private static Type fromArrowField(Field field) {
-        ArrowType arrowType = field.getType();
-        switch (arrowType.getTypeID()) {
-            case Int:
-                int bitWidth = ((ArrowType.Int) arrowType).getBitWidth();
-                if (bitWidth == 8) {
-                    return IntegerType.TINYINT;
-                } else if (bitWidth == 16) {
-                    return IntegerType.SMALLINT;
-                } else if (bitWidth == 32) {
-                    return IntegerType.INT;
-                } else if (bitWidth == 64) {
-                    return IntegerType.BIGINT;
-                }
-                break;
-            case FloatingPoint:
-                return ((ArrowType.FloatingPoint) arrowType).getPrecision() == FloatingPointPrecision.SINGLE
-                        ? FloatType.FLOAT
-                        : FloatType.DOUBLE;
-            case Bool:
-                return BooleanType.BOOLEAN;
-            case Utf8:
-            case LargeUtf8:
-                return VarcharType.VARCHAR;
-            case Binary:
-            case LargeBinary:
-            case FixedSizeBinary:
-                return VarbinaryType.VARBINARY;
-            case Date:
-                return DateType.DATE;
-            case Timestamp:
-                return DateType.DATETIME;
-            case Decimal:
-                ArrowType.Decimal decimal = (ArrowType.Decimal) arrowType;
-                return TypeFactory.createUnifiedDecimalType(decimal.getPrecision(), decimal.getScale());
-            case List:
-            case LargeList:
-            case FixedSizeList:
-                return new ArrayType(fromArrowField(field.getChildren().get(0)));
-            case Map:
-                return mapType(field);
-            case Struct:
-                return structType(field);
-            default:
-                break;
-        }
-        throw new StarRocksConnectorException("Unsupported lance/arrow type: %s for column %s",
-                arrowType, field.getName());
-    }
-
-    private static Type mapType(Field field) {
-        if (field.getChildren().isEmpty() || field.getChildren().get(0).getChildren().size() < 2) {
-            throw new StarRocksConnectorException("Invalid lance map field: %s", field.getName());
-        }
-        List<Field> keyValueFields = field.getChildren().get(0).getChildren();
-        return new MapType(fromArrowField(keyValueFields.get(0)), fromArrowField(keyValueFields.get(1)));
-    }
-
-    private static Type structType(Field field) {
-        ArrayList<StructField> structFields = new ArrayList<>(field.getChildren().size());
-        for (Field child : field.getChildren()) {
-            structFields.add(new StructField(child.getName(), fromArrowField(child)));
-        }
-        return new StructType(structFields);
     }
 
     private List<FileStatus> listStatus(String path) {
