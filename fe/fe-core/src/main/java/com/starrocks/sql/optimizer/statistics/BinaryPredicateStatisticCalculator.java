74d062af3beebc57a323c62e293924978c037468
Oliver Layer <o.layer@celonis.com>
2026-08-03 09:16:23 +0200

    Fix import
    Signed-off-by: Oliver Layer <o.layer@celonis.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/statistics/BinaryPredicateStatisticCalculator.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/statistics/BinaryPredicateStatisticCalculator.java
@@ -30,7 +30,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 
 import static java.lang.Double.NEGATIVE_INFINITY;
