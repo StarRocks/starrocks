bdf38028ad481d1e50d326b190bdf4e24d715dc8
Harbor Liu <460660596@qq.com>
2026-08-03 13:59:40 +0800

    [BugFix] Use last freshness confirmed time as the MV rewrite staleness baseline (#76758)
    Signed-off-by: Harbor Liu <460660596@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/scheduler/mv/pct/MVPCTRefreshProcessor.java
+++ fe/fe-core/src/main/java/com/starrocks/scheduler/mv/pct/MVPCTRefreshProcessor.java
@@ -350,6 +350,7 @@ public final class MVPCTRefreshProcessor extends MVRefreshProcessor {
             long processStartTime = mvContext.getStatus().getProcessStartTime();
             newProperties.put(TaskRun.MV_FRESHNESS_BASELINE_TIME,
                     mvRefreshParams.isCompleteRefresh() && processStartTime > 0
+                            && !mvContext.isPartitionLimitExcludedPartitions()
                             ? String.valueOf(processStartTime) : "0");
         }
         // warehouse
