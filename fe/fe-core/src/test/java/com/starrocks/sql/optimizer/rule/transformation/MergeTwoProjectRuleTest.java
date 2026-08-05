3f143280bf87a6669d1c3392e55321f545b2ba61
m.bogusz <m.bogusz@celonis.com>
2026-08-05 15:52:18 +0200

    fix after backport
    Signed-off-by: m.bogusz <m.bogusz@celonis.com>

--- fe/fe-core/src/test/java/com/starrocks/sql/optimizer/rule/transformation/MergeTwoProjectRuleTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/optimizer/rule/transformation/MergeTwoProjectRuleTest.java
@@ -15,10 +15,10 @@
 package com.starrocks.sql.optimizer.rule.transformation;
 
 import com.google.common.collect.Maps;
-import com.starrocks.catalog.Type;
-import com.starrocks.sql.ast.expression.BinaryType;
+import com.starrocks.analysis.BinaryType;
 import com.starrocks.catalog.PrimitiveType;
 import com.starrocks.catalog.ScalarType;
+import com.starrocks.catalog.Type;
 import com.starrocks.sql.optimizer.OptExpression;
 import com.starrocks.sql.optimizer.OptimizerFactory;
 import com.starrocks.sql.optimizer.base.ColumnRefFactory;
