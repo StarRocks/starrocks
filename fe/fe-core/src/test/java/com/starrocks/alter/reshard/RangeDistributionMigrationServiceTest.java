406d0695d6cdf0ce7e215545efff102947778844
xiangguangyxg <xiangguangyxg@gmail.com>
2026-08-06 04:50:03 +0800

    [UT] Fix range migration test import ordering
    Signed-off-by: xiangguangyxg <xiangguangyxg@gmail.com>

--- fe/fe-core/src/test/java/com/starrocks/alter/reshard/RangeDistributionMigrationServiceTest.java
+++ fe/fe-core/src/test/java/com/starrocks/alter/reshard/RangeDistributionMigrationServiceTest.java
@@ -55,8 +55,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.awaitility.Awaitility;
+import org.mockito.Mockito;
 
 import java.time.Duration;
 import java.util.ArrayList;
