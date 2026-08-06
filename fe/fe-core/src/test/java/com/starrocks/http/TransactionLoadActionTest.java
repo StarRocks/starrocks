fb0646761abbc61b7aac023a9999fbf574969aec
Kevin Cai <kevin.cai@phoenixdata.ai>
2026-08-05 19:57:00 -0700

    [BugFix] Skip checkpoint thread pool metrics (#77367)
    Signed-off-by: Kevin Cai <kevin.cai@phoenixdata.ai>
    (cherry picked from commit e6e6b326792ae96e7e790b8c2f64da9e08358f88)

--- fe/fe-core/src/test/java/com/starrocks/http/TransactionLoadActionTest.java
+++ fe/fe-core/src/test/java/com/starrocks/http/TransactionLoadActionTest.java
@@ -116,6 +116,11 @@ public class TransactionLoadActionTest extends StarRocksHttpTestCase {
             boolean isLeader() {
                 return true;
             }
+
+            @Mock
+            public static boolean isCheckpointThread() {
+                return false;
+            }
         };
 
         new MockUp<TransactionLoadAction>() {
