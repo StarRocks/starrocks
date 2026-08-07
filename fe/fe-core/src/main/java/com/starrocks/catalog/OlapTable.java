47238a5d86434b8a0429db16574a46151ca08b6a
cccaarr <jiangxiaoyu@starrocks.com>
2026-08-07 10:02:17 +0800

    [BugFix] Assign index ids to GIN indexes created on a materialized view (backport #77340) (#77405)
    Signed-off-by: cccaarr <jiangxiaoyu@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/OlapTable.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/OlapTable.java
@@ -1934,6 +1934,7 @@ public class OlapTable extends Table {
             this.indexes = new TableIndexes(null);
         }
         this.indexes.setIndexes(indexes);
+        tryToAssignIndexId();
     }
 
     public String getColocateGroup() {
