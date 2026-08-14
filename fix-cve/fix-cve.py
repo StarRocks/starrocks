a2ba4c6303435ab4eeda6f2cfcb1a6d57b0dbf2f
Kevin Cai <kevin.cai@phoenixdata.ai>
2026-08-14 15:25:53 +0800

    [Bug][CVE] suppress HttpCore to 5.4.3 (#60540)
    Signed-off-by: Kevin Cai <kevin.cai@phoenixdata.ai>

--- fix-cve/fix-cve.py
+++ fix-cve/fix-cve.py
@@ -56,6 +56,7 @@ fixed_poms = [
     ("org.eclipse.jetty:jetty-io", "10.0.31"),
     ("commons-beanutils:commons-beanutils", "1.11.0"),
     ("org.apache.httpcomponents.client5:httpclient5", "5.4.3"),
+    ("org.apache.httpcomponents.core5:httpcore5", "5.4.3"),
     ("io.airlift:aircompressor", "2.0.3"),
     ("org.jline:jline-remote-telnet", "4.2.1"),
 ]
