32bbc881566728a36e3bd6adfd100738ff818da4
wanpengfei-git <wanpengfei91@163.com>
2026-08-14 16:11:08 +0000

    style: auto-fix clang-format issues

--- be/src/storage/lake/rowset.cpp
+++ be/src/storage/lake/rowset.cpp
@@ -385,9 +385,9 @@ Status Rowset::init_segment_read_options(const RowsetReadOptions& options, const
             }
             delvec_holder = _held_delvecs;
         }
-        segment_options->delvec_loader = std::make_shared<LakeDelvecLoader>(
-                _tablet_mgr, nullptr, segment_options->lake_io_opts.fill_data_cache, segment_options->lake_io_opts,
-                nullptr, std::move(delvec_holder));
+        segment_options->delvec_loader =
+                std::make_shared<LakeDelvecLoader>(_tablet_mgr, nullptr, segment_options->lake_io_opts.fill_data_cache,
+                                                   segment_options->lake_io_opts, nullptr, std::move(delvec_holder));
         segment_options->dcg_loader = std::make_shared<LakeDeltaColumnGroupLoader>(_tablet_metadata);
     }
     // The Index Delta Group (ADD INDEX fast-path) sidecar applies to ALL lake
