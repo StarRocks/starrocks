8c013b395a6c51bd6cf7459b67fdb82e558d57ea
Smith Cruise <chendingchao1@126.com>
2026-08-17 14:20:10 +0800

    fix
    Signed-off-by: Smith Cruise <chendingchao1@126.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/IcebergView.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/IcebergView.java
@@ -51,7 +51,10 @@ public class IcebergView extends ConnectorView {
 
             // iceberg view query statement with external catalog which created by starrocks must have catalog name
             if (Strings.isNullOrEmpty(name.getCatalog())) {
-                name.setCatalog(defaultCatalogName);
+                // iceberg view's default-catalog is optional
+                // When default-catalog is null or not set, the catalog in which the view is stored must
+                // be used as the default catalog.
+                name.setCatalog(Strings.isNullOrEmpty(defaultCatalogName) ? this.catalogName : defaultCatalogName);
             }
 
             if (Strings.isNullOrEmpty(tableRelation.getName().getDb())) {
