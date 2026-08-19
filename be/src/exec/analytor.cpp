9e56e3c2b1c66d4ea366befc30e3606c07531cdc
wanpengfei-git <wanpengfei91@163.com>
2026-08-19 10:23:58 +0000

    style: auto-fix clang-format issues

--- be/src/exec/analytor.cpp
+++ be/src/exec/analytor.cpp
@@ -18,7 +18,6 @@
 #include <ios>
 #include <memory>
 
-#include "util/failpoint/fail_point.h"
 #include "column/chunk.h"
 #include "column/column_helper.h"
 #include "column/nullable_column.h"
@@ -36,6 +35,7 @@
 #include "types/logical_type.h"
 #include "udf/java/utils.h"
 #include "util/defer_op.h"
+#include "util/failpoint/fail_point.h"
 #include "util/runtime_profile.h"
 
 // This macro is used to perform common pre-processing for each ProcessByPartitionIfNecessaryFunc
