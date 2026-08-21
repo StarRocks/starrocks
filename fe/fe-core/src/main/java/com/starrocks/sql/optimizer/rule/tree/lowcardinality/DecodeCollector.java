641f0fb9de9de5ca2178c31f8f2d3b01098c26c6
farhad-celo <f.shahmohammadi@celonis.com>
2026-08-21 00:46:11 -0700

    [BugFix] Fixing global dict expr propagation for cases where project hides a column used in predicates  (#78006)
    Signed-off-by: Farhad Shahmohammadi <f.shahmohammadi@celonis.com>
    (cherry picked from commit 71fa88d459b60af6d2fc28c2ceed4a5166177033)
    
    # Conflicts:
    #	fe/fe-core/src/test/java/com/starrocks/sql/plan/LowCardinalityTest2.java
    #	test/sql/test_low_cardinality/T/test_low_cardinality_array

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/tree/lowcardinality/DecodeCollector.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/tree/lowcardinality/DecodeCollector.java
@@ -1171,6 +1171,7 @@ public class DecodeCollector extends OptExpressionVisitor<DecodeInfo, DecodeInfo
             if (!expressions.isEmpty()) {
                 // predicate only translate to string expression
                 stringExpressions.computeIfAbsent(c, l -> Lists.newArrayList()).addAll(expressions);
+                info.usedStringColumns.union(c);
             }
         });
 
