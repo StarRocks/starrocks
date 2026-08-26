d73746450acfb0db9bd5ed8d70c353cfe54cb0da
srlch <111035020+srlch@users.noreply.github.com>
2026-08-26 15:13:24 +0800

    [BugFix] Fix compaction read chunk size estimation (#78192)
    Signed-off-by: srlch <linzichao@starrocks.com>
    Co-authored-by: wanpengfei-git <wanpengfei91@163.com>
    (cherry picked from commit 2e754f3fed63efb88532f36cd7e0dbca42f55efd)
    
    # Conflicts:
    #	be/src/storage/horizontal_compaction_task.cpp
    #	be/src/storage/rowset_merger.cpp
    #	be/test/storage/compaction_parallelization_test.cpp

--- be/test/storage/rowset_merger_test.cpp
+++ be/test/storage/rowset_merger_test.cpp
@@ -55,6 +55,7 @@ public:
 
     Status add_chunk(const Chunk& chunk, const std::vector<uint64_t>& rssid_rowids) override {
         added_chunks.emplace_back(&chunk);
+        added_chunk_num_rows.emplace_back(chunk.num_rows());
         all_pks->append(*(chunk.get_column_raw_ptr_by_index(0)), 0, chunk.num_rows());
         return Status::OK();
     }
@@ -107,6 +108,7 @@ public:
     MutableColumnPtr all_pks;
     vector<uint32_t> all_rssids;
     std::vector<const Chunk*> added_chunks;
+    std::vector<size_t> added_chunk_num_rows;
 
     MutableColumns non_key_columns;
 };
@@ -297,6 +299,40 @@ TEST_F(RowsetMergerTest, compaction_chunk_reset_memory_tracker_threshold_percent
     ASSERT_EQ(pks.size(), writer.all_pks->size());
     ASSERT_GE(writer.added_chunks.size(), 2);
     EXPECT_NE(writer.added_chunks[0], writer.added_chunks[1]);
+}
+
+TEST_F(RowsetMergerTest, chunk_size_estimation_loads_unloaded_rowset) {
+    const auto old_compaction_memory_limit_per_worker = config::compaction_memory_limit_per_worker;
+    const auto old_vector_chunk_size = config::vector_chunk_size;
+    DeferOp restore_config([&]() {
+        config::compaction_memory_limit_per_worker = old_compaction_memory_limit_per_worker;
+        config::vector_chunk_size = old_vector_chunk_size;
+    });
+    config::compaction_memory_limit_per_worker = 1;
+    config::vector_chunk_size = 4096;
+
+    create_tablet(GetCurrentTimeMicros(), GetCurrentTimeMicros() & 0x7fffffff);
+    std::vector<int64_t> pks = {1, 2, 3, 4, 5, 6, 7, 8};
+    auto rowset = create_rowset(_tablet, pks);
+    ASSERT_OK(_tablet->rowset_commit(2, rowset));
+    std::vector<RowsetSharedPtr> applied_rowsets;
+    ASSERT_OK(_tablet->updates()->get_applied_rowsets(2, &applied_rowsets));
+
+    RowsetSharedPtr unloaded_rowset;
+    ASSERT_OK(RowsetFactory::create_rowset(_tablet->tablet_schema(), rowset->rowset_path(), rowset->rowset_meta(),
+                                           &unloaded_rowset, _tablet->data_dir()->get_meta()));
+    ASSERT_TRUE(unloaded_rowset->segments().empty());
+
+    TestRowsetWriter writer;
+    Schema schema = ChunkHelper::convert_schema(_tablet->tablet_schema());
+    ASSERT_OK(PrimaryKeyEncoder::create_column(schema, &writer.all_pks, PrimaryKeyEncodingType::PK_ENCODING_TYPE_V1));
+    MergeConfig cfg;
+    ASSERT_OK(compaction_merge_rowsets(*_tablet, 2, {unloaded_rowset}, &writer, cfg));
+    ASSERT_EQ(pks.size(), writer.all_pks->size());
+    ASSERT_FALSE(writer.added_chunk_num_rows.empty());
+    for (size_t num_rows : writer.added_chunk_num_rows) {
+        EXPECT_EQ(1, num_rows);
+    }
 }
 
 TEST_F(RowsetMergerTest, horizontal_merge) {
