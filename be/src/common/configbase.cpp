b9d188c53a5c0bff6cfc117def4a4068372cb55e
tasenkov-long <tasenkov@proton.me>
2026-08-28 19:33:28 -0400

    be: fix whitespace in configbase

--- be/src/common/configbase.cpp
+++ be/src/common/configbase.cpp
@@ -1,3 +1,4 @@
+// poc/clang-format-probe
 // Licensed to the Apache Software Foundation (ASF) under one
 // or more contributor license agreements.  See the NOTICE file
 // distributed with this work for additional information
@@ -15,7 +16,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-#include <strings.h>
+#include   <strings.h>
 
 #include <algorithm>
 #include <cerrno>
