89caea0a97c7119ad207519a98e9d182e1428302
Yixin Luo <18810541851@163.com>
2026-09-02 23:27:13 +0800

    [BugFix] Fix FE max tablet compaction score collection (#78496)
    Signed-off-by: Yixin Luo <18810541851@163.com>
    (cherry picked from commit 9f3cccaaa00de67f0e2a349b3317d6c1c24f771e)
    
    # Conflicts:
    #	be/src/service/service_be/backend_service.cpp
    #	fe/fe-core/src/test/java/com/starrocks/leader/TabletCollectorTest.java

--- be/src/service/service_be/backend_service.cpp
+++ be/src/service/service_be/backend_service.cpp
@@ -35,10 +35,17 @@
 
 #include "backend_service.h"
 
+<<<<<<< HEAD
+=======
+#include <algorithm>
+#include <memory>
+
+>>>>>>> 9f3ccca ([BugFix] Fix FE max tablet compaction score collection (#78496))
 #include "agent/agent_server.h"
 #include "agent/task_worker_pool.h"
 #include "runtime/exec_env.h"
 #include "storage/storage_engine.h"
+#include "storage/storage_metrics.h"
 #include "storage/tablet_manager.h"
 
 namespace starrocks {
@@ -70,6 +77,9 @@ void BackendService::publish_cluster_state(TAgentResult& result, const TAgentPub
 
 void BackendService::get_tablets_info(TGetTabletsInfoResult& result_, const TGetTabletsInfoRequest& request) {
     result_.__set_report_version(curr_report_version());
+    result_.__set_tablet_max_compaction_score(
+            std::max(StorageMetrics::instance()->tablet_cumulative_max_compaction_score.value(),
+                     StorageMetrics::instance()->tablet_base_max_compaction_score.value()));
     result_.__isset.tablets = true;
     TStatus t_status;
     Status st_report = StorageEngine::instance()->tablet_manager()->report_all_tablets_info(&result_.tablets);
