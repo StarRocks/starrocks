142bf50e10aea9eaae10d2f10f67517c78999e3c
Vikas Kurapati <v.kurapati@celonis.com>
2026-09-14 10:13:44 +0200

    [Refactor] scope variables relevant only for LEAD in a constexpr
    Signed-off-by: Vikas Kurapati <v.kurapati@celonis.com>

--- be/src/exprs/agg/window.h
+++ be/src/exprs/agg/window.h
@@ -765,9 +765,11 @@ class LeadLagWindowFunction final : public ValueWindowFunction<LT, LeadLagState<
         if constexpr (ignoreNulls) {
             this->data(state).target_not_null_index = INT64_MIN;
             this->data(state).non_null_count = 0;
-            this->data(state).lead_ready_current_row = INT64_MIN;
-            this->data(state).lead_ready_scan_end = 0;
-            this->data(state).lead_ready_non_null_count = 0;
+            if constexpr (!isLag) {
+                this->data(state).lead_ready_current_row = INT64_MIN;
+                this->data(state).lead_ready_scan_end = 0;
+                this->data(state).lead_ready_non_null_count = 0;
+            }
         }
     }
 
