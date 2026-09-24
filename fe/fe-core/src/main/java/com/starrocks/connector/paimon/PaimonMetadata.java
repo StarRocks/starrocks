de873a4718c8d3107a452aa4533fdf705533fc35
Beinan <beinan@users.noreply.github.com>
2026-09-23 19:13:11 -0700

    [BugFix] Remove dropped Paimon partitions from metadata cache (#79442)
    Signed-off-by: Beinan <soros.wang@yahoo.com>
    (cherry picked from commit 53646fe62f1b545e8f5a8aea031f9087ad8330d5)
    
    # Conflicts:
    #	fe/fe-core/src/main/java/com/starrocks/connector/paimon/PaimonMetadata.java

--- fe/fe-core/src/main/java/com/starrocks/connector/paimon/PaimonMetadata.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/paimon/PaimonMetadata.java
@@ -84,9 +84,15 @@ import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+<<<<<<< HEAD
+=======
+import java.util.HashSet;
+import java.util.Iterator;
+>>>>>>> 53646fe ([BugFix] Remove dropped Paimon partitions from metadata cache (#79442))
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
@@ -157,8 +163,10 @@ public class PaimonMetadata implements ConnectorMetadata {
 
         try {
             List<org.apache.paimon.partition.Partition> partitions = paimonNativeCatalog.listPartitions(identifier);
+            Set<String> currentPartitionNames = new HashSet<>();
             boolean partitionLegacyName = getPartitionLegacyName(paimonTable);
             for (org.apache.paimon.partition.Partition partition : partitions) {
+<<<<<<< HEAD
                 String partitionPath = PartitionPathUtils.generatePartitionPath(partition.spec(), dataTableRowType);
                 String[] partitionValues = Arrays.stream(partitionPath.split("/"))
                         .map(part -> part.split("=")[1])
@@ -166,7 +174,18 @@ public class PaimonMetadata implements ConnectorMetadata {
                 Partition srPartition = getPartition(partition, partitionColumnNames, partitionColumnTypes,
                         partitionValues, partitionLegacyName);
                 this.partitionInfos.put(srPartition.getPartitionName(), srPartition);
+=======
+                String partitionPath = PartitionPathUtils.generatePartitionPath(partition.spec(), dataTableRowType,
+                        /* onlyValue */ false);
+                String[] partitionValues =
+                        Arrays.stream(partitionPath.split("/")).map(part -> part.split("=")[1]).toArray(String[]::new);
+                Partition srPartition =
+                        getPartition(partition, partitionColumnNames, partitionColumnTypes, partitionValues, partitionLegacyName);
+                currentPartitionNames.add(srPartition.getPartitionName());
+                this.partitionInfos.get(identifier).put(srPartition.getPartitionName(), srPartition);
+>>>>>>> 53646fe ([BugFix] Remove dropped Paimon partitions from metadata cache (#79442))
             }
+            this.partitionInfos.get(identifier).keySet().retainAll(currentPartitionNames);
         } catch (Catalog.TableNotExistException e) {
             LOG.error("Failed to update partition info of paimon table {}.{}.", databaseName, tableName, e);
         }
